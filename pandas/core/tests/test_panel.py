f0910fc1b83fe59674defd55cf9de78994342fa2
Wes McKinney <wesmckinn@gmail.com>
2009-12-03 19:49:22 +0000

    panel test method stubs
    git-svn-id: http://pandas.googlecode.com/svn/trunk@71 d5231056-7de3-11de-ac95-d976489f1ece

---
+++ pandas/core/tests/test_panel.py
@@ -0,0 +1,201 @@
+import unittest
+
+from pandas.core.panel import WidePanel, LongPanel
+import pandas.core.tests.common as common
+
+class TestWidePanel(unittest.TestCase):
+
+    def test_wide_axis_number(self):
+        pass
+
+    def test_wide_axis_name(self):
+        pass
+
+    def test_get_plane_axes(self):
+        pass
+
+    def test_fromDict(self):
+        pass
+
+    def test_keys(self):
+        pass
+
+    def test_iteritems(self):
+        pass
+
+    def test_values(self):
+        pass
+
+    def test_getitem(self):
+        pass
+
+    def test_setitem(self):
+        pass
+
+    def test_pickle(self):
+        pass
+
+    def test_conform(self):
+        pass
+
+    def test_reindex(self):
+        pass
+
+    def test_combineFrame(self):
+        pass
+
+    def test_combinePanel(self):
+        pass
+
+    def test_operators(self):
+        pass
+
+    def test_getMajorXS(self):
+        pass
+
+    def test_getMinorXS(self):
+        pass
+
+    def test_groupby(self):
+        pass
+
+    def test_swapaxes(self):
+        pass
+
+    def test_toLong(self):
+        pass
+
+    def test_filterItems(self):
+        pass
+
+    def test_apply(self):
+        pass
+
+    def test_shift(self):
+        pass
+
+
+class TestLongPanelIndex(unittest.TestCase):
+
+    def test_constructor(self):
+        pass
+
+    def test_isConsistent(self):
+        pass
+
+    def test_truncate(self):
+        pass
+
+    def test_getMajorBounds(self):
+        pass
+
+    def test_getAxisBounds(self):
+        pass
+
+    def test_getLabelBounds(self):
+        pass
+
+    def test_bounds(self):
+        pass
+
+    def test_makeMask(self):
+        pass
+
+    def test_dims(self):
+        pass
+
+class TestLongPanel(unittest.TestCase):
+
+    def test_constructor(self):
+        pass
+
+    def test_fromRecords(self):
+        pass
+
+    def test_columns(self):
+        pass
+
+    def test_copy(self):
+        pass
+
+    def test_values(self):
+        pass
+
+    def test_getitem(self):
+        pass
+
+    def test_setitem(self):
+        pass
+
+    def test_pickle(self):
+        pass
+
+    def test_combineFrame(self):
+        pass
+
+    def test_combinePanel(self):
+        pass
+
+    def test_operators(self):
+        pass
+
+    def test_sort(self):
+        pass
+
+    def test_toWide(self):
+        pass
+
+    def test_toCSV(self):
+        pass
+
+    def test_toString(self):
+        pass
+
+    def test_swapaxes(self):
+        pass
+
+    def test_truncate(self):
+        pass
+
+    def test_filterItems(self):
+        pass
+
+    def test_getAxisDummies(self):
+        pass
+
+    def test_getFrameDummies(self):
+        pass
+
+    def test_getItemDummies(self):
+        pass
+
+    def test_applyToAxis(self):
+        pass
+
+    def test_mean(self):
+        pass
+
+    def test_sum(self):
+        pass
+
+    def test_apply(self):
+        pass
+
+    def test_count(self):
+        pass
+
+    def test_leftJoin(self):
+        pass
+
+    def test_merge(self):
+        pass
+
+    def test_addPrefix(self):
+        pass
+
+class TestFactor(unittest.TestCase):
+
+    def test_constructor(self):
+        pass
+
+
