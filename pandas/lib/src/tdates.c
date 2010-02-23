a52776c445a919e2b85c1cfc21ba70cc5d08245e
Wes McKinney <wesmckinn@gmail.com>
2010-02-23 18:54:10 +0000

    fix for MSVC2008 compatibility
    git-svn-id: http://pandas.googlecode.com/svn/trunk@127 d5231056-7de3-11de-ac95-d976489f1ece

--- pandas/lib/src/tdates.c
+++ pandas/lib/src/tdates.c
@@ -4,43 +4,44 @@
 #include <stdio.h>
 
 static PyObject* isAllDates(PyObject *self, PyObject *args) {
-    PyDateTime_IMPORT;
-
     PyObject *input;
     PyArrayIterObject *iter;
     PyObject *obj;
 
+    PyDateTime_IMPORT;
 
     if (PyArg_ParseTuple(args, "O", &input)) {
-        if (!PyArray_Check(input)) {
-            PyErr_SetString(PyExc_RuntimeError, "Input was not ndarray!");
-            return NULL;
-        }
+      long size;
 
-        long size = PyArray_SIZE(input);
-    
-        if (size == 0) {
-            Py_RETURN_FALSE;	
-        }
-    
-        iter = (PyArrayIterObject *) PyArray_IterNew(input);
-    
-        while (iter->index < iter->size) {
-            obj = PyArray_GETITEM(input, (void *) iter->dataptr);
-            if (!PyDateTime_Check(obj)) {
-                Py_DECREF(obj);
-                Py_DECREF(iter);
-    
-                Py_RETURN_FALSE;
-            }
-    
-            Py_DECREF(obj);
-            PyArray_ITER_NEXT(iter);
-        }
-    
-        Py_DECREF(iter);
-        Py_RETURN_TRUE;
-    }	
+      if (!PyArray_Check(input)) {
+	PyErr_SetString(PyExc_RuntimeError, "Input was not ndarray!");
+	return NULL;
+      }
+
+      size = PyArray_SIZE(input);
+
+      if (size == 0) {
+	Py_RETURN_FALSE;
+      }
+
+      iter = (PyArrayIterObject *) PyArray_IterNew(input);
+
+      while (iter->index < iter->size) {
+	obj = PyArray_GETITEM(input, (void *) iter->dataptr);
+	if (!PyDateTime_Check(obj)) {
+	  Py_DECREF(obj);
+	  Py_DECREF(iter);
+
+	  Py_RETURN_FALSE;
+	}
+
+	Py_DECREF(obj);
+	PyArray_ITER_NEXT(iter);
+      }
+
+      Py_DECREF(iter);
+      Py_RETURN_TRUE;
+    }
     return NULL;
 }
 
