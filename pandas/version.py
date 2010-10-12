be59b1da0057d0c5c8a70b0719d6d483ba9cf596
Wes McKinney <wesmckinn@gmail.com>
2010-10-12 10:52:15 -0400

    temporary version.py post git migration

--- pandas/version.py
+++ pandas/version.py
@@ -1,17 +1,7 @@
-# THIS FILE IS GENERATED FROM PANDAS setup.py
-short_version='0.2.1'
-version='0.2.1'
+short_version='0.3.0'
+version='0.3.0'
 release=False
 
 if not release:
     version += '.dev'
-    import os
-    svn_version_file = os.path.join(os.path.dirname(__file__),
-                                    '__svn_version__.py')
-    if os.path.isfile(svn_version_file):
-        import imp
-        svn = imp.load_module('pandas.__svn_version__',
-                              open(svn_version_file),
-                              svn_version_file,
-                              ('.py','U',1))
-        version += svn.version
+
