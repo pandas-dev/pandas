55cc489d5bd2fe69614446370774a728a8190bd9
Wes McKinney <wesmckinn@gmail.com>
2011-09-28 09:52:24 -0400

    MSC: sampling script update

--- scripts/groupby_sample.py
+++ scripts/groupby_sample.py
@@ -3,7 +3,7 @@ import numpy as np
 import string
 
 g1 = np.array(list(string.letters))[:-1]
-g2 = np.arange(51)
+g2 = np.arange(5100)
 df_small = DataFrame({'group1' : ["a","b","a","a","b","c","c","c","c",
                                   "c","a","a","a","b","b","b","b"],
                       'group2' : [1,2,3,4,1,3,5,6,5,4,1,2,3,4,3,2,1],
@@ -14,7 +14,7 @@ df_small = DataFrame({'group1' : ["a","b","a","a","b","c","c","c","c",
                                   "grape"]})
 value = df_small['value'].values.repeat(3)
 df = DataFrame({'group1' : g1.repeat(40000),
-                'group2' : np.tile(g2, 40000),
+                'group2' : np.tile(g2, 400),
                 'value' : value.repeat(40000)})
 
 
@@ -24,3 +24,10 @@ def random_sample():
     choose = lambda group: choice(group.index)
     indices = grouped.apply(choose)
     return df.reindex(indices)
+
+def random_sample_v2():
+    grouped = df.groupby(['group1','group2'])['value']
+    from random import choice
+    choose = lambda group: choice(group.index)
+    indices = [choice(v) for k, v in grouped.groups.iteritems()]
+    return df.reindex(np.asarray(indices))
