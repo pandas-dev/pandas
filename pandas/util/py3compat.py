9fe43b7d11ad4b7fd88e39f3292c18f53e0327d2
Thomas Kluyver <takowl@gmail.com>
2011-10-13 16:56:48 +0100

    Add columns to IPython tab completions of DataFrame attributes.

--- pandas/util/py3compat.py
+++ pandas/util/py3compat.py
@@ -1,3 +1,14 @@
 import sys
 
 PY3 = (sys.version_info[0] >= 3)
+
+if PY3:
+    def isidentifier(s):
+        return s.isidentifier()
+
+else:
+    # Python 2
+    import re
+    _name_re = re.compile(r"[a-zA-Z_][a-zA-Z0-9_]*$")
+    def isidentifier(s, dotted=False):
+        return bool(_name_re.match(s))
