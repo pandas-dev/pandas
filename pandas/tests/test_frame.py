05050338a1173d4005fc06e8cb067b56261cf04e
Wes McKinney <wesmckinn@gmail.com>
2011-11-25 15:19:39 -0500

    ENH: can pass args, kwds to DataFrame.apply, GH #376

--- pandas/tests/test_frame.py
+++ pandas/tests/test_frame.py
@@ -2773,6 +2773,28 @@ class TestDataFrame(unittest.TestCase, CheckIndexing,
         result = no_cols.apply(lambda x: x.mean(), broadcast=True)
         self.assert_(isinstance(result, DataFrame))
 
+    def test_apply_with_args_kwds(self):
+        def add_some(x, howmuch=0):
+            return x + howmuch
+
+        def agg_and_add(x, howmuch=0):
+            return x.mean() + howmuch
+
+        def subtract_and_divide(x, sub, divide=1):
+            return (x - sub) / divide
+
+        result = self.frame.apply(add_some, howmuch=2)
+        exp = self.frame.apply(lambda x: x + 2)
+        assert_frame_equal(result, exp)
+
+        result = self.frame.apply(agg_and_add, howmuch=2)
+        exp = self.frame.apply(lambda x: x.mean() + 2)
+        assert_series_equal(result, exp)
+
+        res = self.frame.apply(subtract_and_divide, args=(2,), divide=2)
+        exp = self.frame.apply(lambda x: (x - 2.) / 2.)
+        assert_frame_equal(res, exp)
+
     def test_applymap(self):
         applied = self.frame.applymap(lambda x: x * 2)
         assert_frame_equal(applied, self.frame * 2)
