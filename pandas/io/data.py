3715dd2c767af2e34422c5f5fce3be4e7456468f
Jev Kuznetsov <jev.kuznetsov@gmail.com>
2011-12-07 23:56:39 +0100

    added yahoo quote fetcher

--- pandas/io/data.py
+++ pandas/io/data.py
@@ -7,6 +7,7 @@ Module contains tools for collecting data from various remote sources
 import numpy as np
 import datetime as dt
 import urllib
+import urllib2
 
 from zipfile import ZipFile
 from StringIO import StringIO
@@ -64,6 +65,46 @@ def _sanitize_dates(start, end):
     if end is None:
         end = dt.datetime.today()
     return start, end
+
+def get_quote_yahoo(symbols):
+    """ 
+    Get current yahoo quote
+    
+    Returns a DataFrame
+    """
+    if not isinstance(symbols,list):
+        raise TypeError, "symbols must be a list"
+    # for codes see: http://www.gummy-stuff.org/Yahoo-data.htm
+    codes = {'symbol':'s','last':'l1','change_pct':'p2','PE':'r','time':'t1','short_ratio':'s7'}
+    request = str.join('',codes.values()) # code request string
+    header = codes.keys()   
+    
+    data = dict(zip(codes.keys(),[[] for i in range(len(codes))]))
+    
+    urlStr = 'http://finance.yahoo.com/d/quotes.csv?s=%s&f=%s' % (str.join('+',symbols), request)
+    
+    try:
+        lines = urllib2.urlopen(urlStr).readlines()
+    except Exception, e:
+        s = "Failed to download:\n{0}".format(e);
+        print s
+        return None
+        
+    for line in lines:
+        fields = line.strip().split(',')
+        #print fields
+        for i,field in enumerate(fields):
+            if field[0] == '"':
+                data[header[i]].append( field.strip('"'))
+            else:
+                try:
+                    data[header[i]].append(float(field))
+                except ValueError:
+                    data[header[i]].append(np.nan)
+
+    idx = data.pop('symbol')
+    
+    return DataFrame(data,index=idx)
 
 def get_data_yahoo(name=None, start=None, end=None):
     """
