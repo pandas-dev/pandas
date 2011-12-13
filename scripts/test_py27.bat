5269ca53ca4513b299c11e398f01a26aff12a3b2
Wes McKinney <wesmckinn@gmail.com>
2011-12-13 15:20:46 -0600

    TST: python 3.1 test script

--- scripts/test_py27.bat
+++ scripts/test_py27.bat
@@ -1,5 +1,6 @@
 SET PATH=C:\MinGW\bin;C:\Python27;C:\Python27\Scripts;%PATH%
-del pandas\_tseries.pyd
-del pandas\_sparse.pyd
+
+python setup.py clean
 python setup.py build_ext -c mingw32 --inplace
+
 nosetests pandas
