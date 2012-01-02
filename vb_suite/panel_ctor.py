42a6594834f73372d03a7a5a4a9ad652b16f5af5
Wes McKinney <wesmckinn@gmail.com>
2012-01-02 15:14:19 -0500

    TST: make work on older version of pandas

--- vb_suite/panel_ctor.py
+++ vb_suite/panel_ctor.py
@@ -14,27 +14,29 @@ setup_same_index = common_setup + """
 dr = DateRange(datetime(1990,1,1), datetime(2012,1,1))
 data_frames = {}
 for x in xrange(1000):
-   df = DataFrame({"a": 0, "b": 1, "c": 2}, index=dr)
+   df = DataFrame({"a": [0]*len(dr), "b": [1]*len(dr),
+                   "c": [2]*len(dr)}, index=dr)
    data_frames[x] = df
 """
 
 panel_from_dict_same_index = \
     Benchmark("Panel.from_dict(data_frames)",
               setup_same_index, name='panel_from_dict_same_index',
-              start_date=START_DATE)
+              start_date=START_DATE, repeat=1)
 
 setup_equiv_indexes = common_setup + """
 data_frames = {}
 for x in xrange(1000):
    dr = DateRange(datetime(1990,1,1), datetime(2012,1,1))
-   df = DataFrame({"a": 0, "b": 1, "c": 2}, index=dr)
+   df = DataFrame({"a": [0]*len(dr), "b": [1]*len(dr),
+                   "c": [2]*len(dr)}, index=dr)
    data_frames[x] = df
 """
 
 panel_from_dict_equiv_indexes = \
     Benchmark("Panel.from_dict(data_frames)",
               setup_equiv_indexes, name='panel_from_dict_equiv_indexes',
-              start_date=START_DATE)
+              start_date=START_DATE, repeat=1)
 
 setup_all_different_indexes = common_setup + """
 data_frames = {}
@@ -43,14 +45,15 @@ end = datetime(2012,1,1)
 for x in xrange(1000):
    end += timedelta(days=1)
    dr = DateRange(start, end)
-   df = DataFrame({"a": 0, "b": 1, "c": 2}, index=dr)
+   df = DataFrame({"a": [0]*len(dr), "b": [1]*len(dr),
+                   "c": [2]*len(dr)}, index=dr)
    data_frames[x] = df
 """
 panel_from_dict_all_different_indexes = \
     Benchmark("Panel.from_dict(data_frames)",
               setup_all_different_indexes,
               name='panel_from_dict_all_different_indexes',
-              start_date=START_DATE)
+              start_date=START_DATE, repeat=1)
 
 setup_two_different_indexes = common_setup + """
 data_frames = {}
@@ -60,11 +63,12 @@ for x in xrange(1000):
    if x == 500:
        end += timedelta(days=1)
    dr = DateRange(start, end)
-   df = DataFrame({"a": 0, "b": 1, "c": 2}, index=dr)
+   df = DataFrame({"a": [0]*len(dr), "b": [1]*len(dr),
+                   "c": [2]*len(dr)}, index=dr)
    data_frames[x] = df
 """
 panel_from_dict_two_different_indexes = \
     Benchmark("Panel.from_dict(data_frames)",
               setup_two_different_indexes,
               name='panel_from_dict_two_different_indexes',
-              start_date=START_DATE)
+              start_date=START_DATE, repeat=1)
