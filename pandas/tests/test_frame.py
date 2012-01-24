de908a92029dd7a1d196f9f1401209d19a9414cf
Adam Klein <adamklein@gmail.com>
2012-01-24 09:43:36 -0500

    TST: added test coverage to unboxed arithmetic with dataframe

--- pandas/tests/test_frame.py
+++ pandas/tests/test_frame.py
@@ -2148,6 +2148,31 @@ class TestDataFrame(unittest.TestCase, CheckIndexing,
         assert_frame_equal(df.div(col, axis=0), (df.T / col).T)
         assert_frame_equal(df.mul(col, axis=0), (df.T * col).T)
 
+    def test_arith_non_pandas_object(self):
+        df = self.simple
+
+        val1 = df.xs('a').values
+        added = DataFrame(df.values + val1, index=df.index, columns=df.columns)
+        assert_frame_equal(df + val1, added)
+
+        added = DataFrame((df.values.T + val1).T,
+                          index=df.index, columns=df.columns)
+        assert_frame_equal(df.add(val1, axis=0), added)
+
+
+        val2 = list(df['two'])
+
+        added = DataFrame(df.values + val2, index=df.index, columns=df.columns)
+        assert_frame_equal(df + val2, added)
+
+        added = DataFrame((df.values.T + val2).T, index=df.index,
+                          columns=df.columns)
+        assert_frame_equal(df.add(val2, axis='index'), added)
+
+        val3 = np.random.rand(*df.shape)
+        added = DataFrame(df.values + val3, index=df.index, columns=df.columns)
+        assert_frame_equal(df.add(val3), added)
+
     def test_combineFrame(self):
         frame_copy = self.frame.reindex(self.frame.index[::2])
 
