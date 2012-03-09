0bf1d1774f941f5d88abf84ce5e46cd41f14f639
Josh <josh@josh-HP-Pavilion-dv6-Notebook-PC.(none)>
2012-03-08 22:36:37 -0700

    Fixed an issue where it was not possible to subclass Panel because various methods were calling the Panel constructor instead of the _constructor method. Also from_dict was returning Panel objects instead of __class__ objects.

--- pandas/core/panel.py
+++ pandas/core/panel.py
@@ -11,7 +11,7 @@ from pandas.core.common import (PandasError, _mut_exclusive,
                                 _try_sort, _default_index, _infer_dtype)
 from pandas.core.index import (Factor, Index, MultiIndex, _ensure_index,
                                _get_combined_index, NULL_INDEX)
-from pandas.core.indexing import _NDFrameIndexer, _maybe_droplevels
+from pandas.core.indexing import _NDFrameIndexer
 from pandas.core.internals import BlockManager, make_block, form_blocks
 from pandas.core.frame import DataFrame
 from pandas.core.generic import NDFrame
@@ -320,7 +320,7 @@ class Panel(NDFrame):
         data, index, columns = _homogenize_dict(data, intersect=intersect,
                                                 dtype=dtype)
         items = Index(sorted(data.keys()))
-        return Panel(data, items, index, columns)
+        return __class__(data, items, index, columns)
 
     def __getitem__(self, key):
         if isinstance(self.items, MultiIndex):
@@ -438,14 +438,14 @@ class Panel(NDFrame):
             columns = self.minor_axis
 
         return index, columns
-
+    
     @property
     def _constructor(self):
-        return Panel
+        return self.__class__
 
     # Fancy indexing
     _ix = None
-
+    
     @property
     def ix(self):
         if self._ix is None:
@@ -754,7 +754,7 @@ class Panel(NDFrame):
             return self._combine_frame(other, func, axis=axis)
         elif np.isscalar(other):
             new_values = func(self.values, other)
-            return Panel(new_values, self.items, self.major_axis,
+            return self._constructor(new_values, self.items, self.major_axis,
                              self.minor_axis)
 
     def __neg__(self):
@@ -775,7 +775,7 @@ class Panel(NDFrame):
             new_values = func(self.values.swapaxes(0, 2), other.values)
             new_values = new_values.swapaxes(0, 2)
 
-        return Panel(new_values, self.items, self.major_axis,
+        return self._constructor(new_values, self.items, self.major_axis,
                      self.minor_axis)
 
     def _combine_panel(self, other, func):
@@ -789,7 +789,7 @@ class Panel(NDFrame):
 
         result_values = func(this.values, other.values)
 
-        return Panel(result_values, items, major, minor)
+        return self._constructor(result_values, items, major, minor)
 
     def fillna(self, value=None, method='pad'):
         """
@@ -821,10 +821,10 @@ class Panel(NDFrame):
             for col, s in self.iterkv():
                 result[col] = s.fillna(method=method, value=value)
 
-            return Panel.from_dict(result)
+            return self._constructor.from_dict(result)
         else:
             new_data = self._data.fillna(value)
-            return Panel(new_data)
+            return self._constructor(new_data)
 
     add = _panel_arith_method(operator.add, 'add')
     subtract = sub = _panel_arith_method(operator.sub, 'subtract')
@@ -913,7 +913,7 @@ class Panel(NDFrame):
         """
         from pandas.core.groupby import PanelGroupBy
         axis = self._get_axis_number(axis)
-        return PanelGroupBy(self, function, axis=axis)
+        return self._constructorGroupBy(self, function, axis=axis)
 
     def swapaxes(self, axis1='major', axis2='minor'):
         """
@@ -935,7 +935,7 @@ class Panel(NDFrame):
                     for k in range(3))
         new_values = self.values.swapaxes(i, j).copy()
 
-        return Panel(new_values, *new_axes)
+        return self._constructor(new_values, *new_axes)
 
     def to_frame(self, filter_observations=True):
         """
@@ -1136,7 +1136,7 @@ class Panel(NDFrame):
         else:
             raise ValueError('Invalid axis')
 
-        return Panel(values, items=items, major_axis=major_axis,
+        return self._constructor(values, items=items, major_axis=major_axis,
                          minor_axis=minor_axis)
 
     def truncate(self, before=None, after=None, axis='major'):
