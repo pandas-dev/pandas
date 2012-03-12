65698e3b53fd976af7f05e52da439c0f19d6423f
aman-thakral <aman.thakral@gmail.com>
2012-03-12 11:08:31 -0300

    Patched _safe_fetch to ensure a list of tuples is returned.

--- pandas/io/sql.py
+++ pandas/io/sql.py
@@ -46,7 +46,7 @@ def execute(sql, con, retry=True, cur=None, params=None):
 
 def _safe_fetch(cur):
     try:
-        return cur.fetchall()
+        return [tuple(row) for row in cur]
     except Exception, e: # pragma: no cover
         excName = e.__class__.__name__
         if excName == 'OperationalError':
