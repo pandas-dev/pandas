219bdcb472a8fbcc7cd218f49e3ae5e4df757f78
Wouter Overmeire <lodagro@gmail.com>
2012-04-16 21:32:49 +0200

    BUG: fix to_html with colums MultiIndex without level names, close #1053

--- pandas/core/format.py
+++ pandas/core/format.py
@@ -262,6 +262,8 @@ class DataFrameFormatter(object):
             if isinstance(self.columns, MultiIndex):
                 if self.has_column_names:
                     row.append(single_column_table(self.columns.names))
+                else:
+                    row.append('')
                 row.extend([single_column_table(c) for c in self.columns])
             else:
                 row.append(self.columns.name or '')
@@ -290,9 +292,11 @@ class DataFrameFormatter(object):
                     row = frame.index.names + [''] * len(self.columns)
                     write_tr(row, indent, indent_delta, header=True)
 
+                indent -= indent_delta
                 write('</thead>', indent)
 
             write('<tbody>', indent)
+            indent += indent_delta
 
             _bold_row = self.kwds.get('bold_rows', False)
             def _maybe_bold_row(x):
