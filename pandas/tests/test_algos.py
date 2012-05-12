59f0ee735638c08b71f3d216d687e1b96185848a
Wes McKinney <wesmckinn@gmail.com>
2012-05-12 14:30:24 -0400

    ENH: add docs and add match function to API, close #502

--- pandas/tests/test_algos.py
+++ pandas/tests/test_algos.py
@@ -2,6 +2,31 @@ import unittest
 
 import numpy as np
 
+
 import pandas.core.algorithms as algos
 import pandas.util.testing as tm
+
+
+class TestMatch(unittest.TestCase):
+
+    def test_ints(self):
+        values = np.array([0, 2, 1])
+        to_match = np.array([0, 1, 2, 2, 0, 1, 3, 0])
+
+        result = algos.match(to_match, values)
+        expected = np.array([0, 2, 1, 1, 0, 2, -1, 0])
+        self.assert_(np.array_equal(result, expected))
+
+    def test_strings(self):
+        values = ['foo', 'bar', 'baz']
+        to_match = ['bar', 'foo', 'qux', 'foo', 'bar', 'baz', 'qux']
+
+        result = algos.match(to_match, values)
+        expected = np.array([1, 0, -1, 0, 1, 2, -1])
+        self.assert_(np.array_equal(result, expected))
+
+if __name__ == '__main__':
+    import nose
+    nose.runmodule(argv=[__file__,'-vvs','-x','--pdb', '--pdb-failure'],
+                   exit=False)
 
