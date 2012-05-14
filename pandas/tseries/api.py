cd9a136c53bef6b8fc0020b6d15dcbdea310cd96
Wes McKinney <wesmckinn@gmail.com>
2012-05-14 14:22:03 -0400

    BLD: add modified numpy Cython header

--- pandas/tseries/api.py
+++ pandas/tseries/api.py
@@ -8,3 +8,4 @@ from pandas.tseries.frequencies import infer_freq
 from pandas.tseries.offsets import *
 from pandas.tseries.period import PeriodIndex, period_range, pnow
 from pandas.tseries.resample import TimeGrouper
+import pandas.tseries.offsets as offsets
