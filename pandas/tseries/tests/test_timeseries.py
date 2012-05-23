dd6242a911a4c0fba90d58e7814da29c907790ad
Chang She <chang@lambdafoundry.com>
2012-05-23 17:41:23 -0400

    TST: using pandas.util.compat for py25 compat

--- pandas/tseries/tests/test_timeseries.py
+++ pandas/tseries/tests/test_timeseries.py
@@ -1,6 +1,6 @@
 # pylint: disable-msg=E1101,W0612
 from __future__ import with_statement # for Python 2.5
-import itertools
+import pandas.util.compat as itertools
 from datetime import datetime, time, timedelta
 import sys
 import os
