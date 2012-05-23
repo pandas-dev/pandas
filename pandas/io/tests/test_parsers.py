f0738c087b9507045a6fee184f219b3a3be309eb
Chang She <chang@lambdafoundry.com>
2012-05-23 11:43:28 -0400

    TST: using multiple date col as index

--- pandas/io/tests/test_parsers.py
+++ pandas/io/tests/test_parsers.py
@@ -197,6 +197,24 @@ KORD,19990127, 23:00:00, 22:56:00, -0.5900, 1.7100, 4.6000, 0.0000, 280.0000"""
         df = read_csv(StringIO(data), parse_dates={'nominal': [1, 2]})
         self.assert_(not isinstance(df.nominal[0], basestring))
 
+    def test_multiple_date_cols_index(self):
+        data = """\
+ID,date,NominalTime,ActualTime,TDew,TAir,Windspeed,Precip,WindDir
+KORD1,19990127, 19:00:00, 18:56:00, 0.8100, 2.8100, 7.2000, 0.0000, 280.0000
+KORD2,19990127, 20:00:00, 19:56:00, 0.0100, 2.2100, 7.2000, 0.0000, 260.0000
+KORD3,19990127, 21:00:00, 20:56:00, -0.5900, 2.2100, 5.7000, 0.0000, 280.0000
+KORD4,19990127, 21:00:00, 21:18:00, -0.9900, 2.0100, 3.6000, 0.0000, 270.0000
+KORD5,19990127, 22:00:00, 21:56:00, -0.5900, 1.7100, 5.1000, 0.0000, 290.0000
+KORD6,19990127, 23:00:00, 22:56:00, -0.5900, 1.7100, 4.6000, 0.0000, 280.0000"""
+
+        xp = read_csv(StringIO(data), parse_dates={'nominal': [1, 2]})
+        df = read_csv(StringIO(data), parse_dates={'nominal': [1, 2]},
+                      index_col='nominal')
+        assert_frame_equal(xp.set_index('nominal'), df)
+        df2 = read_csv(StringIO(data), parse_dates={'nominal': [1, 2]},
+                      index_col=0)
+        assert_frame_equal(df2, df)
+
     def test_index_col_named(self):
         data = """\
 ID,date,NominalTime,ActualTime,TDew,TAir,Windspeed,Precip,WindDir
