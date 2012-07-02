6692b096325d70e9b2e69779ad16c193d6b4290f
Vytautas Jancauskas <unaudio@gmail.com>
2012-07-02 17:57:41 +0300

    Better solution to setting smaller font sizes in tick labels

--- pandas/tools/plotting.py
+++ pandas/tools/plotting.py
@@ -196,7 +196,6 @@ def bootstrap_plot(series, fig=None, size=50, samples=500, **kwds):
     import random
     import matplotlib
     import matplotlib.pyplot as plt
-    matplotlib.rcParams.update({'font.size': 8})
     data = series.values
     samplings = [random.sample(data, size) for _ in range(samples)]
     means = np.array([np.mean(sampling) for sampling in samplings])
@@ -205,24 +204,34 @@ def bootstrap_plot(series, fig=None, size=50, samples=500, **kwds):
     if fig == None:
         fig = plt.figure()
     x = range(samples)
+    axes = []
     ax1 = fig.add_subplot(2, 3, 1)
     ax1.set_xlabel("Sample")
+    axes.append(ax1)
     ax1.plot(x, means, **kwds)
     ax2 = fig.add_subplot(2, 3, 2)
     ax2.set_xlabel("Sample")
+    axes.append(ax2)
     ax2.plot(x, medians, **kwds)
     ax3 = fig.add_subplot(2, 3, 3)
     ax3.set_xlabel("Sample")
+    axes.append(ax3)
     ax3.plot(x, midranges, **kwds)
     ax4 = fig.add_subplot(2, 3, 4)
     ax4.set_xlabel("Mean")
+    axes.append(ax4)
     ax4.hist(means, **kwds)
     ax5 = fig.add_subplot(2, 3, 5)
     ax5.set_xlabel("Median")
+    axes.append(ax5)
     ax5.hist(medians, **kwds)
     ax6 = fig.add_subplot(2, 3, 6)
     ax6.set_xlabel("Midrange")
+    axes.append(ax6)
     ax6.hist(midranges, **kwds)
+    for axis in axes:
+        plt.setp(axis.get_xticklabels(), fontsize=8)
+        plt.setp(axis.get_yticklabels(), fontsize=8)
     return fig
 
 def lag_plot(series, ax=None, **kwds):
