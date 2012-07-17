169b47f866174701dd853eb7b5fda006333843cf
Vytautas Jancauskas <unaudio@gmail.com>
2012-07-17 18:14:15 +0300

    Added an RPlot class and a simple geom class

--- pandas/tools/rplot.py
+++ pandas/tools/rplot.py
@@ -1,3 +1,21 @@
+import matplotlib.pyplot as plt
+
 class RPlot:
-	def __init__(self):
-		pass
+	def __init__(self, data):
+		self.data = data
+		self.ax = plt.gca()
+
+	def __add__(self, other):
+		other.plot(self.ax, self.data)
+
+class GeomPoint:
+	def __init__(self, x, y, marker='o', colour='grey', size=20, alpha=1.0):
+		self.x = x
+		self.y = y
+		self.marker = marker
+		self.colour = colour
+		self.size = size
+		self.alpha = alpha
+
+	def plot(self, ax, data):
+		ax.scatter(data[self.x], data[self.y], c=self.colour, marker=self.marker, s=self.size, alpha=self.alpha)
