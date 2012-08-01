6c7ec42cd3015b1cf547a01a5c664db3e3abf42b
Johnny <johnnybrown@counsyl.com>
2012-08-01 00:23:06 -0700

    bugfix in tools.scatter_plot

--- pandas/tools/plotting.py
+++ pandas/tools/plotting.py
@@ -1641,6 +1641,7 @@ def _grouped_plot(plotf, data, column=None, by=None, numeric_only=True,
                   figsize=None, sharex=True, sharey=True, layout=None,
                   rot=0, ax=None):
     from pandas.core.frame import DataFrame
+    import matplotlib.pyplot as plt
 
     # allow to specify mpl default with 'default'
     if figsize is None or figsize == 'default':
@@ -1661,9 +1662,15 @@ def _grouped_plot(plotf, data, column=None, by=None, numeric_only=True,
     fig, axes = _subplots(nrows=nrows, ncols=ncols, figsize=figsize,
                           sharex=sharex, sharey=sharey, ax=ax)
 
-    ravel_axes = []
-    for row in axes:
-        ravel_axes.extend(row)
+    if isinstance(axes, plt.Axes):
+        ravel_axes = [axes]
+    else:
+        ravel_axes = []
+        for row in axes:
+            if isinstance(row, plt.Axes):
+                ravel_axes.append(row)
+            else:
+                ravel_axes.extend(row)
 
     for i, (key, group) in enumerate(grouped):
         ax = ravel_axes[i]
