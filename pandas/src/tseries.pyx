cfd200594e3983bd2098b3e831ad14c5d83c83ad
Wes McKinney <wesmckinn@gmail.com>
2012-09-08 22:08:28 -0400

    BUG: correct lib.ismember 32-bit data access issue

--- pandas/src/tseries.pyx
+++ pandas/src/tseries.pyx
@@ -107,20 +107,17 @@ def ismember(ndarray arr, set values):
     '''
     cdef:
         Py_ssize_t i, n
-        flatiter it
         ndarray[uint8_t] result
         object val
 
-    it = <flatiter> PyArray_IterNew(arr)
     n = len(arr)
     result = np.empty(n, dtype=np.uint8)
     for i in range(n):
-        val = PyArray_GETITEM(arr, PyArray_ITER_DATA(it))
+        val = util.get_value_at(arr, i)
         if val in values:
             result[i] = 1
         else:
             result[i] = 0
-        PyArray_ITER_NEXT(it)
 
     return result.view(np.bool_)
 
