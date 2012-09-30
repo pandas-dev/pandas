57be5d43c39877d47f1a9bf8fd964fb81d26416e
y-p <yoval@gmx.com>
2012-09-30 12:33:39 +0200

    TST: tseries.converter.DateTimeConverter.convert() should accept basestring

--- pandas/tseries/tests/test_converter.py
+++ pandas/tseries/tests/test_converter.py
@@ -7,10 +7,20 @@ import nose
 
 import numpy as np
 
-from pandas.tseries import converter
+import pandas.tseries.converter as converter
 
 def test_timtetonum_accepts_unicode():
     assert(converter.time2num("00:01")==converter.time2num(u"00:01"))
+
+class TestDateTimeConverter(unittest.TestCase):
+
+    def setUp(self):
+        self.dtc = converter.DatetimeConverter()
+
+    def test_convert_accepts_unicode(self):
+        r1 = self.dtc.convert("12:22",None,None)
+        r2 = self.dtc.convert(u"12:22",None,None)
+        assert(r1==r2), "DatetimeConverter.convert should accept unicode"
 
 if __name__ == '__main__':
     import nose
