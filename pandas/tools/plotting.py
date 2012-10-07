b8b27463fbfbbb02a84cf74a5c99e296504f5c51
Brenda Moon <brenda@moon.net.au>
2012-10-07 17:31:19 +1100

    fixed color keyword in time series plot - issue #1890

--- pandas/tools/plotting.py
+++ pandas/tools/plotting.py
@@ -936,7 +936,8 @@ class LinePlot(MPLPlot):
 
         def _maybe_add_color(kwargs, style, i):
             if (not has_colors and
-                (style is None or re.match('[a-z]+', style) is None)):
+                (style is None or re.match('[a-z]+', style) is None)
+                   and 'color' not in kwargs):
                 kwargs['color'] = colors[i % len(colors)]
 
         def to_leg_label(label, i):
