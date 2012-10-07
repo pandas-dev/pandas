7c89dfa8c0415d6d376b32e51671878190ddc222
Abraham Flaxman <abie@alum.mit.edu>
2012-10-07 11:19:44 -0700

    TST: check for -inf dtype as well as +inf

--- pandas/tests/test_tseries.py
+++ pandas/tests/test_tseries.py
@@ -295,6 +295,11 @@ def test_convert_infs():
     result = lib.maybe_convert_numeric(arr, set(), False)
     assert(result.dtype == np.float64)
 
+    arr = np.array(['-inf', '-inf', '-inf'], dtype='O')
+    result = lib.maybe_convert_numeric(arr, set(), False)
+    assert(result.dtype == np.float64)
+
+
 def test_convert_objects_ints():
     # test that we can detect many kinds of integers
     dtypes = ['i1', 'i2', 'i4', 'i8', 'u1', 'u2', 'u4', 'u8']
