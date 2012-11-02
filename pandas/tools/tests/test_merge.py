8142167d8eda1dfd8a93caa11d6ad07591004b97
Wes McKinney <wesmckinn@gmail.com>
2012-11-02 17:07:00 -0400

    TST: test from #2098

--- pandas/tools/tests/test_merge.py
+++ pandas/tools/tests/test_merge.py
@@ -671,6 +671,29 @@ class TestMerge(unittest.TestCase):
         result = merge(right, left, on='key', how='right')
         assert_frame_equal(result, left)
 
+    def test_merge_nosort(self):
+        # #2098, anything to do?
+
+        from datetime import datetime
+
+        d = {"var1" : np.random.randint(0, 10, size=10),
+             "var2" : np.random.randint(0, 10, size=10),
+             "var3" : [datetime(2012, 1, 12), datetime(2011, 2, 4),
+                       datetime(2010, 2, 3), datetime(2012, 1, 12),
+                       datetime(2011, 2, 4), datetime(2012, 4, 3),
+                       datetime(2012, 3, 4), datetime(2008, 5, 1),
+                       datetime(2010, 2, 3), datetime(2012, 2, 3)]}
+        df = DataFrame.from_dict(d)
+        var3 = df.var3.unique()
+        var3.sort()
+        new = DataFrame.from_dict({"var3" : var3,
+                                   "var8" : np.random.random(7)})
+
+        result = df.merge(new, on="var3", sort=False)
+        exp = merge(df, new, on='var3', sort=False)
+        assert_frame_equal(result, exp)
+
+        self.assert_((df.var3.unique() == result.var3.unique()).all())
 
 def _check_merge(x, y):
     for how in ['inner', 'left', 'outer']:
