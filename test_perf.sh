6ad3afaaf14c107326525d2a0f1b5dac0a341b44
y-p <yoval@gmx.com>
2012-11-24 06:04:32 +0200

    ENH: add command line arguments to test_perf

--- test_perf.sh
+++ test_perf.sh
@@ -7,6 +7,6 @@ echo "Use vbench to compare HEAD against a known-good baseline."
 echo "Make sure the python 'vbench' library is installed..\n"
 
 cd "$BASEDIR/vb_suite/"
-python test_perf.py
+python test_perf.py $@
 
 cd "$CURDIR"
