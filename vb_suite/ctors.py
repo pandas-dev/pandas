f3858fd98d537ec62ab3e2ae41ba426dac769405
Wes McKinney <wesmckinn@gmail.com>
2012-11-26 14:01:08 -0500

    TST: vbench for Index.__new__

--- vb_suite/ctors.py
+++ vb_suite/ctors.py
@@ -12,7 +12,7 @@ data = np.random.randn(100)
 index = Index(np.arange(100))
 """
 
-series_constructor_ndarray = \
+ctor_series_ndarray = \
     Benchmark("Series(data, index=index)", setup=setup,
               name='series_constructor_ndarray')
 
@@ -20,6 +20,12 @@ setup = common_setup + """
 arr = np.random.randn(100, 100)
 """
 
-frame_constructor_ndarray = \
+ctor_frame_ndarray = \
     Benchmark("DataFrame(arr)", setup=setup,
               name='frame_constructor_ndarray')
+
+setup = common_setup + """
+data = np.array(['foo', 'bar', 'baz'], dtype=object)
+"""
+
+ctor_index_array_string = Benchmark('Index(data)', setup=setup)
