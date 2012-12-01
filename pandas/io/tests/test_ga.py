00caab3a21a8b094806b9c6ffe5897f09b2297d3
Chang She <changshe@gmail.com>
2012-12-01 00:43:49 -0500

    TST: add multiprocessing for ga tests and also limit dates requested

--- pandas/io/tests/test_ga.py
+++ pandas/io/tests/test_ga.py
@@ -1,26 +1,39 @@
 import unittest
 import nose
 import httplib2
+from datetime import datetime
 
 import pandas as pd
 import pandas.core.common as com
 from pandas import DataFrame
-from pandas.io.ga import GAnalytics, read_ga
-from pandas.io.auth import AuthenticationConfigError
 from pandas.util.testing import network, assert_frame_equal
 from numpy.testing.decorators import slow
 
 class TestGoogle(unittest.TestCase):
 
+    _multiprocess_can_split_ = True
+
     @slow
     @network
     def test_getdata(self):
         try:
+            from pandas.io.ga import GAnalytics, read_ga
+            from pandas.io.auth import AuthenticationConfigError
+        except ImportError:
+            raise nose.SkipTest
+
+        try:
+            end_date = datetime.now()
+            start_date = end_date - pd.offsets.Day() * 5
+            end_date = end_date.strftime('%Y-%m-%d')
+            start_date = start_date.strftime('%Y-%m-%d')
+
             reader = GAnalytics()
             df = reader.get_data(
                 metrics=['avgTimeOnSite', 'visitors', 'newVisits',
                          'pageviewsPerVisit'],
-                start_date='2005-1-1',
+                start_date = start_date,
+                end_date = end_date,
                 dimensions=['date', 'hour'],
                 parse_dates={'ts' : ['date', 'hour']})
 
@@ -38,11 +51,34 @@ class TestGoogle(unittest.TestCase):
             df2 = read_ga(
                 metrics=['avgTimeOnSite', 'visitors', 'newVisits',
                          'pageviewsPerVisit'],
-                start_date='2005-1-1',
+                start_date=start_date,
+                end_date=end_date,
                 dimensions=['date', 'hour'],
                 parse_dates={'ts' : ['date', 'hour']})
 
             assert_frame_equal(df, df2)
+
+        except AuthenticationConfigError:
+            raise nose.SkipTest
+        except httplib2.ServerNotFoundError:
+            try:
+                h = httplib2.Http()
+                response, content = h.request("http://www.google.com")
+                raise
+            except httplib2.ServerNotFoundError:
+                raise nose.SkipTest
+
+    @slow
+    @network
+    def test_iterator(self):
+        try:
+            from pandas.io.ga import GAnalytics, read_ga
+            from pandas.io.auth import AuthenticationConfigError
+        except ImportError:
+            raise nose.SkipTest
+
+        try:
+            reader = GAnalytics()
 
             it = reader.get_data(
                 metrics='visitors',
@@ -63,8 +99,6 @@ class TestGoogle(unittest.TestCase):
 
             assert (df2.index > df1.index).all()
 
-
-
         except AuthenticationConfigError:
             raise nose.SkipTest
         except httplib2.ServerNotFoundError:
@@ -74,7 +108,6 @@ class TestGoogle(unittest.TestCase):
                 raise
             except httplib2.ServerNotFoundError:
                 raise nose.SkipTest
-
 
 if __name__ == '__main__':
     import nose
