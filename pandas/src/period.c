007254cd81dbb37f0ec292a1f19bc7c5d568fd63
Phillip Cloud <cpcloud@gmail.com>
2012-12-19 18:32:27 -0500

    BUG: use i8 throughout to prevent gcc pointer cast warnings

--- pandas/src/period.c
+++ pandas/src/period.c
@@ -39,13 +39,13 @@ floordiv(i8 x, i8 divisor)
 static asfreq_info NULL_AF_INFO;
 
 /* Table with day offsets for each month (0-based, without and with leap) */
-static i4 month_offset[2][13] = {
+static i8 month_offset[2][13] = {
     { 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334, 365 },
     { 0, 31, 60, 91, 121, 152, 182, 213, 244, 274, 305, 335, 366 }
 };
 
 /* Table of number of days in a month (0-based, without and with leap) */
-static i4 days_in_month[2][12] = {
+static i8 days_in_month[2][12] = {
     { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 },
     { 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }
 };
