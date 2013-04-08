43558a4b111b9bc04ae716480634b71397086622
y-p <yoval@gmx.com>
2013-04-08 11:01:27 +0300

    DOC: modify sphinx theme to use global TOC

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -124,7 +124,7 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  Major themes that come with
 # Sphinx are currently 'default' and 'sphinxdoc'.
-html_theme = 'nature'
+html_theme = 'nature_with_gtoc'
 
 # The style sheet to use for HTML and HTML Help pages. A file of that name
 # must exist either in Sphinx' static/ path, or in one of the custom paths
