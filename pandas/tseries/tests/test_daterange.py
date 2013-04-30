669585d6d990a03f01c00ad590d259861193276d
Kevin Stone <kevinastone@gmail.com>
2013-04-30 01:23:36 -0700

    Added failing test case for union of two timezone-aware indexes with month end frequencies.
    Signed-off-by: Kevin Stone <kevinastone@gmail.com>

--- pandas/tseries/tests/test_daterange.py
+++ pandas/tseries/tests/test_daterange.py
@@ -340,6 +340,22 @@ class TestDateRange(unittest.TestCase):
         self.assert_(dr[0] == start)
         self.assert_(dr[2] == end)
 
+    def test_month_range_union_tz(self):
+        _skip_if_no_pytz()
+        from pytz import timezone
+        tz = timezone('US/Eastern')
+
+        early_start = datetime(2011, 1, 1)
+        early_end = datetime(2011, 3, 1)
+        
+        late_start = datetime(2011, 3, 1)
+        late_end = datetime(2011, 5, 1)
+
+        early_dr = date_range(start=early_start, end=early_end, tz=tz, freq=datetools.monthEnd)
+        late_dr = date_range(start=late_start, end=late_end, tz=tz, freq=datetools.monthEnd)
+        
+        early_dr.union(late_dr)
+
 
 if __name__ == '__main__':
     import nose
