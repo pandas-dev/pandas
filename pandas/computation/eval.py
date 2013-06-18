f1ca19f0612aabcbf2f688ae5782493a8e5996f9
Phillip Cloud <cpcloud@gmail.com>
2013-06-17 20:35:45 -0400

    CLN: remove engine detection and manip for datetimes

--- pandas/computation/eval.py
+++ pandas/computation/eval.py
@@ -2,38 +2,11 @@
 
 import sys
 import numbers
-import collections
-import itertools
 
 import numpy as np
 
-Scope = collections.namedtuple('Scope', 'globals locals')
-
-import pandas.core.common as com
-from pandas.computation.expr import Expr
+from pandas.computation.expr import Expr, Scope
 from pandas.computation.engines import _engines
-
-
-def _scope_has_series_and_frame_datetime_index(env):
-    from pandas import DatetimeIndex
-    series_index = frame_index = 0
-
-    for v in itertools.chain(env.locals.itervalues(),
-                             env.globals.itervalues()):
-        series_index += com.is_series(v) and isinstance(v.index, DatetimeIndex)
-        frame_index += com.is_frame(v) and isinstance(v.index, DatetimeIndex)
-    return series_index, frame_index
-
-
-def _maybe_convert_engine(env, engine):
-    assert isinstance(env, Scope), 'environment must be an instance of Scope'
-    assert isinstance(engine, basestring), 'engine name must be a string'
-
-    ret = engine
-
-    if all(_scope_has_series_and_frame_datetime_index(env)):
-        ret = 'python'
-    return ret
 
 
 def eval(expr, engine='numexpr', truediv=True, local_dict=None,
@@ -44,7 +17,8 @@ def eval(expr, engine='numexpr', truediv=True, local_dict=None,
                        ' {1}'.format(_engines.keys()))
 
     # 1 up in the call stack for locals/globals; see the documentation for the
-    # inspect module for why you must decrease the refcount of frame
+    # inspect module for why you must decrease the refcount of frame at all
+    # costs
     frame = sys._getframe(1)
 
     try:
@@ -53,8 +27,6 @@ def eval(expr, engine='numexpr', truediv=True, local_dict=None,
 
         # shallow copy the scope so we don't overwrite everything
         env = Scope(gbl.copy(), lcl.copy())
-
-        engine = _maybe_convert_engine(env, engine)
 
         # parse the expression
         parsed_expr = Expr(expr, engine, env, truediv)
