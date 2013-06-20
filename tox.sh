e9c2e93898b7484f7ae441dd90a3ca343d88172c
Phillip Cloud <cpcloud@gmail.com>
2013-06-19 23:40:45 -0400

    ENH: add ability to choose build cache, fast tests, and or full deps when using tox

--- tox.sh
+++ tox.sh
@@ -1,28 +1,52 @@
 #!/usr/bin/env bash
 
 
-fast=
+use_build_cache=
+slow_tests=
+full_deps=
 
-while getopts f opt; do
+while getopts bsd opt; do
     case $opt in
-        f) fast=true;;
+        b) use_build_cache=true;;
+        s) slow_tests=true;;
+        d) full_deps=true;;
     esac
 done
 
 
-if [ $fast ]; then
+if [ $use_build_cache ]; then
     scripts/use_build_cache.py
 fi
 
-ENVS=$(cat tox.ini | grep envlist | tr  "," " " | cut -d " " -f 3-)
+
+# choose to run all tests or just to run not network and not slow (default)
+posargs="-A 'not network and not slow'"
+if [ $slow_tests ]; then
+    posargs=
+fi
+
+
+# choose full or slim build deps
+start_i=3
+end_i=9
+if [ $full_deps ]; then
+    start_i=9
+    end_i=
+fi
+
+ENVS=$(cat tox.ini | grep envlist | tr ',' ' ' | cut -d " " -f ${start_i}-${end_i})
+
+
 TOX_INI_PAR="tox.ini"
 echo "[Creating distfile]"
 tox --sdistonly
 export DISTFILE="$(find .tox/dist -type f)"
 
+
+# run the tests
 echo -e "[Starting tests]\n"
-for e in $ENVS; do
+for e in "${ENVS}"; do
     echo "[launching tox for $e]"
-    tox -c "$TOX_INI_PAR" -e "$e" &
+    tox -c "$TOX_INI_PAR" -e "$e" -- "${posargs}" &
 done
 wait
