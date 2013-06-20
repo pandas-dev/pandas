58d258cf1bc9ad09609f57b69266ea1bedde8282
Phillip Cloud <cpcloud@gmail.com>
2013-06-19 22:56:15 -0400

    CLN: expunge serial tox

--- tox.sh
+++ tox.sh
@@ -2,12 +2,10 @@
 
 
 fast=
-parallel=
 
-while getopts fp opt; do
+while getopts f opt; do
     case $opt in
         f) fast=true;;
-        p) parallel=true;;
     esac
 done
 
@@ -16,19 +14,15 @@ if [ $fast ]; then
     scripts/use_build_cache.py
 fi
 
-if [ $parallel ]; then
-    ENVS=$(cat tox.ini | grep envlist | tr  "," " " | cut -d " " -f 3-)
-    TOX_INI_PAR="tox.ini"
-    echo "[Creating distfile]"
-    tox --sdistonly
-    export DISTFILE="$(find .tox/dist -type f)"
+ENVS=$(cat tox.ini | grep envlist | tr  "," " " | cut -d " " -f 3-)
+TOX_INI_PAR="tox.ini"
+echo "[Creating distfile]"
+tox --sdistonly
+export DISTFILE="$(find .tox/dist -type f)"
 
-    echo -e "[Starting tests]\n"
-    for e in $ENVS; do
-        echo "[launching tox for $e]"
-        tox -c "$TOX_INI_PAR" -e "$e" &
-    done
-    wait
-else
-    tox
-fi
+echo -e "[Starting tests]\n"
+for e in $ENVS; do
+    echo "[launching tox for $e]"
+    tox -c "$TOX_INI_PAR" -e "$e" &
+done
+wait
