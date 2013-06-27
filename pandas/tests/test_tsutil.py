8aa89316730749a0d56f9d45b4e93acee4f0008e
Tom Farnbauer <tfarnbauer@ahl.com>
2013-06-27 11:31:13 +0200

    ENH: (GH3863) Removed incorrect unit test

--- pandas/tests/test_tsutil.py
+++ pandas/tests/test_tsutil.py
@@ -13,11 +13,6 @@ class TestTsUtil(unittest.TestCase):
         # Ensure that Timestamp.max is a valid Timestamp
         pd.Timestamp(pd.Timestamp.max)
 
-    def test_datetime_convert_roundtrip(self):
-        # Ensure that min/max can be converted to python datetime and back without loss
-        self.assertEqual(pd.Timestamp.max, pd.Timestamp(pd.Timestamp.max.to_pydatetime()))
-        self.assertEqual(pd.Timestamp.min, pd.Timestamp(pd.Timestamp.min.to_pydatetime()))
-
 if __name__ == '__main__':
     import nose
     nose.runmodule(argv=[__file__, '-vvs', '-x', '--pdb', '--pdb-failure'],
