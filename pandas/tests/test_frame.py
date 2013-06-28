2ee1b3182fef45113246dc2f0fbcc6f8612d2038
Phillip Cloud <cpcloud@gmail.com>
2013-06-28 10:52:03 -0400

    BUG: fix 1xN mask on 1xN frame

--- pandas/tests/test_frame.py
+++ pandas/tests/test_frame.py
@@ -7675,6 +7675,13 @@ class TestDataFrame(unittest.TestCase, CheckIndexing,
         assert_frame_equal(rs, df.mask(df <= 0))
         assert_frame_equal(rs, df.mask(~cond))
 
+    def test_mask_edge_case_1xN_frame(self):
+        # GH4071
+        df = DataFrame([[1, 2]])
+        res = df.mask(np.array([[True, False]]))
+        expec = DataFrame([[nan, 2]])
+        assert_frame_equal(res, expec)
+
     #----------------------------------------------------------------------
     # Transposing
     def test_transpose(self):
