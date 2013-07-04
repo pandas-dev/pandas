1e9761bf43834b54309a11fea0201ba3fbc9edd1
Phillip Cloud <cpcloud@gmail.com>
2013-07-03 21:21:49 -0400

    CLN: have the Scope instance auto create the scope if none exists

--- pandas/computation/eval.py
+++ pandas/computation/eval.py
@@ -58,17 +58,9 @@ def eval(expr, engine='numexpr', truediv=True, local_dict=None,
     eng = _engines[engine]
 
     if isinstance(expr, basestring):
-        frame = sys._getframe(1)
-
-        # get the globals and locals
-        gbl, lcl = (global_dict or frame.f_globals,
-                    local_dict or frame.f_locals)
-
-        try:
-            # shallow copy the scope so we don't overwrite anything
-            env = Scope(gbl.copy(), lcl.copy())
-        finally:
-            del frame
+        # need to go 2 up in the call stack from the constructor since we want
+        # the calling scope's variables
+        env = Scope(global_dict, local_dict, frame_level=2)
         parsed_expr = Expr(expr, engine, env, truediv)
     elif isinstance(expr, Expr):
         parsed_expr = expr
@@ -80,7 +72,7 @@ def eval(expr, engine='numexpr', truediv=True, local_dict=None,
     # construct the engine and evaluate
     ret = eng(parsed_expr).evaluate()
 
-    # sanity check for a number
+    # sanity check for a number TODO: eventually take out
     if np.isscalar(ret):
         if not isinstance(ret, (np.number, numbers.Number, np.bool_, bool)):
             raise TypeError('scalar result must be numeric or bool, type is '
