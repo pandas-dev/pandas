bbb54fd045714858fe805c769ef0b55fe44632f4
jreback <jeff@reback.net>
2013-09-06 20:09:04 -0400

    BUG: reading from a store with duplicate columns across dtypes would raise (GH4767)

--- pandas/io/tests/test_pytables.py
+++ pandas/io/tests/test_pytables.py
@@ -2296,6 +2296,29 @@ class TestHDFStore(unittest.TestCase):
         wp = tm.makePanel()
         self._check_roundtrip_table(wp, assert_panel_equal)
 
+    def test_select_with_dups(self):
+
+
+        # single dtypes
+        df = DataFrame(np.random.randn(10,4),columns=['A','A','B','B'])
+        df.index = date_range('20130101 9:30',periods=10,freq='T')
+
+        with ensure_clean(self.path) as store:
+            store.append('df',df)
+            result = store.select('df')
+            assert_frame_equal(result,df)
+
+        # dups accross dtypes
+        df = concat([DataFrame(np.random.randn(10,4),columns=['A','A','B','B']),
+                     DataFrame(np.random.randint(0,10,size=20).reshape(10,2),columns=['A','C'])],
+                    axis=1)
+        df.index = date_range('20130101 9:30',periods=10,freq='T')
+
+        with ensure_clean(self.path) as store:
+            store.append('df',df)
+            result = store.select('df')
+            assert_frame_equal(result,df)
+
     def test_wide_table_dups(self):
         wp = tm.makePanel()
         with ensure_clean(self.path) as store:
