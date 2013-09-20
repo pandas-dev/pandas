b037b0edff84757792c844fa46ee81b90a3350ae
Phillip Cloud <cpcloud@gmail.com>
2013-09-20 18:38:51 -0400

    CLN: just call print_versions via subprocess

--- ci/print_versions.py
+++ ci/print_versions.py
@@ -1,21 +1,15 @@
 #!/usr/bin/env python
 
 
-try:
-    from pandas.util.print_versions import show_versions
-except Exception as e:
-
-    print("Failed to import pandas: %s" % e)
-
-    def show_versions():
-        import subprocess
-        import os
-        fn = __file__
-        this_dir = os.path.dirname(fn)
-        pandas_dir = os.path.dirname(this_dir)
-        sv_path = os.path.join(pandas_dir, 'pandas', 'util',
-                               'print_versions.py')
-        return subprocess.check_call(['python', sv_path])
+def show_versions():
+    import subprocess
+    import os
+    fn = __file__
+    this_dir = os.path.dirname(fn)
+    pandas_dir = os.path.dirname(this_dir)
+    sv_path = os.path.join(pandas_dir, 'pandas', 'util',
+                            'print_versions.py')
+    return subprocess.check_call(['python', sv_path])
 
 
 if __name__ == '__main__':
