1375c51b677c5dbdf6a92a1cab6ce08b9020e623
Phillip Cloud <cpcloud@gmail.com>
2013-09-27 13:52:57 -0400

    PERF: add vbench for datetime comparisons

--- vb_suite/eval.py
+++ vb_suite/eval.py
@@ -47,12 +47,12 @@ eval_frame_add_python_one_thread = \
 eval_frame_mult_all_threads = \
     Benchmark("pd.eval('df * df2 * df3 * df4')", common_setup,
               name='eval_frame_mult_all_threads',
-              start_date=datetime(2012, 7, 21))
+              start_date=datetime(2013, 7, 21))
 
 eval_frame_mult_one_thread = \
     Benchmark("pd.eval('df * df2 * df3 * df4')", setup,
               name='eval_frame_mult_one_thread',
-              start_date=datetime(2012, 7, 26))
+              start_date=datetime(2013, 7, 26))
 
 eval_frame_mult_python = \
     Benchmark("pdl.eval('df * df2 * df3 * df4', engine='python')",
@@ -62,7 +62,7 @@ eval_frame_mult_python = \
 eval_frame_mult_python_one_thread = \
     Benchmark("pd.eval('df * df2 * df3 * df4', engine='python')", setup,
               name='eval_frame_mult_python_one_thread',
-              start_date=datetime(2012, 7, 26))
+              start_date=datetime(2013, 7, 26))
 
 #----------------------------------------------------------------------
 # multi and
@@ -71,12 +71,12 @@ eval_frame_and_all_threads = \
     Benchmark("pd.eval('(df > 0) & (df2 > 0) & (df3 > 0) & (df4 > 0)')",
               common_setup,
               name='eval_frame_and_all_threads',
-              start_date=datetime(2012, 7, 21))
+              start_date=datetime(2013, 7, 21))
 
 eval_frame_and_one_thread = \
     Benchmark("pd.eval('(df > 0) & (df2 > 0) & (df3 > 0) & (df4 > 0)')", setup,
               name='eval_frame_and_one_thread',
-              start_date=datetime(2012, 7, 26))
+              start_date=datetime(2013, 7, 26))
 
 setup = common_setup
 eval_frame_and_python = \
@@ -88,19 +88,19 @@ eval_frame_and_one_thread = \
     Benchmark("pd.eval('(df > 0) & (df2 > 0) & (df3 > 0) & (df4 > 0)', engine='python')",
               setup,
               name='eval_frame_and_python_one_thread',
-              start_date=datetime(2012, 7, 26))
+              start_date=datetime(2013, 7, 26))
 
 #--------------------------------------------------------------------
 # chained comp
 eval_frame_chained_cmp_all_threads = \
     Benchmark("pd.eval('df < df2 < df3 < df4')", common_setup,
               name='eval_frame_chained_cmp_all_threads',
-              start_date=datetime(2012, 7, 21))
+              start_date=datetime(2013, 7, 21))
 
 eval_frame_chained_cmp_one_thread = \
     Benchmark("pd.eval('df < df2 < df3 < df4')", setup,
               name='eval_frame_chained_cmp_one_thread',
-              start_date=datetime(2012, 7, 26))
+              start_date=datetime(2013, 7, 26))
 
 setup = common_setup
 eval_frame_chained_cmp_python = \
@@ -111,4 +111,31 @@ eval_frame_chained_cmp_python = \
 eval_frame_chained_cmp_one_thread = \
     Benchmark("pd.eval('df < df2 < df3 < df4', engine='python')", setup,
               name='eval_frame_chained_cmp_python_one_thread',
-              start_date=datetime(2012, 7, 26))
+              start_date=datetime(2013, 7, 26))
+
+
+common_setup = """from pandas_vb_common import *
+"""
+
+setup = common_setup + """
+N = 1000000
+halfway = N // 2 - 1
+index = date_range('20010101', periods=N, freq='T')
+s = Series(index)
+ts = s.iloc[halfway]
+"""
+
+series_setup = setup + """
+df = DataFrame({'dates': s.values})
+"""
+
+query_datetime_series = Benchmark("df.query('dates < ts')",
+                                  series_setup,
+                                  start_date=datetime(2013, 9, 27))
+
+index_setup = setup + """
+df = DataFrame({'a': np.random.randn(N)}, index=index)
+"""
+
+query_datetime_index = Benchmark("df.query('index < ts')",
+                                 index_setup, start_date=datetime(2013, 9, 27))
