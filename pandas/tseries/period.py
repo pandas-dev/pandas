BUG: fix DatetimeIndex join with PeriodIndex stack overflow

pandas/tseries/period.py
@@ -12,8 +12,8 @@ from pandas.tseries.tools import parse_time_string
 import pandas.tseries.frequencies as _freq_mod
 
 import pandas.core.common as com
-from pandas.core.common import (isnull, _NS_DTYPE, _INT64_DTYPE,
-                                _maybe_box, _values_from_object)
+from pandas.core.common import (isnull, _INT64_DTYPE, _maybe_box,
+                                _values_from_object)
 from pandas import compat
 from pandas.lib import Timestamp
 import pandas.lib as lib
@@ -712,11 +712,9 @@ class PeriodIndex(Int64Index):
     def astype(self, dtype):
         dtype = np.dtype(dtype)
         if dtype == np.object_:
-            result = np.empty(len(self), dtype=dtype)
-            result[:] = [x for x in self]
-            return result
+            return Index(np.array([x for x in self], dtype), dtype)
         elif dtype == _INT64_DTYPE:
-            return self.values.copy()
+            return Index(self.values.copy(), dtype)
         else:  # pragma: no cover
             raise ValueError('Cannot cast PeriodIndex to dtype %s' % dtype)
 