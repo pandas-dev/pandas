5b6791ba13d40f3302f57856b4e40d33939c019d
Phillip Cloud <cpcloud@gmail.com>
2013-10-03 21:21:05 -0400

    BUG: read_html should convert array skiprows into a list

--- pandas/io/html.py
+++ pandas/io/html.py
@@ -90,8 +90,10 @@ def _get_skiprows(skiprows):
     """
     if isinstance(skiprows, slice):
         return lrange(skiprows.start or 0, skiprows.stop, skiprows.step or 1)
-    elif isinstance(skiprows, numbers.Integral) or com.is_list_like(skiprows):
+    elif isinstance(skiprows, numbers.Integral):
         return skiprows
+    elif com.is_list_like(skiprows):
+        return list(skiprows)
     elif skiprows is None:
         return 0
     raise TypeError('%r is not a valid type for skipping rows' %
