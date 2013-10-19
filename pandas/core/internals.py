8c502a2faf8a8fd2200f2ad076b5b3f76d8c38e9
Andreas Klostermann <andreasklostermann@googlemail.com>
2013-10-19 23:09:19 +0200

    Issue #5269: Fix unhelpful "length mismatch" error message on set_axis

--- pandas/core/internals.py
+++ pandas/core/internals.py
@@ -1881,8 +1881,8 @@ class BlockManager(PandasObject):
         value = _ensure_index(value)
 
         if check_axis and len(value) != len(cur_axis):
-            raise ValueError('Length mismatch (%d vs %d)'
-                            % (len(value), len(cur_axis)))
+            raise ValueError('Length mismatch: Expected %d elements, got %d elements'
+                            % (len(cur_axis), len(value)))
 
         self.axes[axis] = value
         self._shape = None
@@ -3361,8 +3361,8 @@ class SingleBlockManager(BlockManager):
         value = _ensure_index(value)
 
         if len(value) != len(cur_axis):
-            raise Exception('Length mismatch (%d vs %d)'
-                            % (len(value), len(cur_axis)))
+            raise ValueError('Length mismatch: Expected %d elements, got %d elements'
+                            % (len(cur_axis), len(value)))
         self.axes[axis] = value
         self._shape = None
         self._block.set_ref_items(self.items, maybe_rename=True)
