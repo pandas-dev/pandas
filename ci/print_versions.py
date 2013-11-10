5b38f390336ece39e9bc932aa763556ac0187a18
y-p <yoval@gmx.com>
2013-11-10 23:01:32 +0200

    BUG: fix path reolution in print_versions.py

--- ci/print_versions.py
+++ ci/print_versions.py
@@ -6,7 +6,7 @@ def show_versions():
     import os
     fn = __file__
     this_dir = os.path.dirname(fn)
-    pandas_dir = os.path.dirname(this_dir)
+    pandas_dir = os.path.abspath(os.path.join(this_dir,".."))
     sv_path = os.path.join(pandas_dir, 'pandas', 'util',
                             'print_versions.py')
     return subprocess.check_call(['python', sv_path])
