ea52fc3c79a49264b52a18e11edc54a4b81ee094
jreback <jeff@reback.net>
2013-12-16 19:12:21 -0500

    TST: win32 test fixes (GH5711)

--- pandas/tests/test_indexing.py
+++ pandas/tests/test_indexing.py
@@ -1382,7 +1382,7 @@ class TestIndexing(tm.TestCase):
         df_orig = DataFrame([['1','2','3','.4',5,6.,'foo']],columns=list('ABCDEFG'))
 
         df = df_orig.copy()
-        df.iloc[:,0:2] = df.iloc[:,0:2].astype(int)
+        df.iloc[:,0:2] = df.iloc[:,0:2].astype(np.int64)
         expected = DataFrame([[1,2,'3','.4',5,6.,'foo']],columns=list('ABCDEFG'))
         assert_frame_equal(df,expected)
 
@@ -1393,12 +1393,12 @@ class TestIndexing(tm.TestCase):
 
         # GH5702 (loc)
         df = df_orig.copy()
-        df.loc[:,'A'] = df.loc[:,'A'].astype(int)
+        df.loc[:,'A'] = df.loc[:,'A'].astype(np.int64)
         expected = DataFrame([[1,'2','3','.4',5,6.,'foo']],columns=list('ABCDEFG'))
         assert_frame_equal(df,expected)
 
         df = df_orig.copy()
-        df.loc[:,['B','C']] = df.loc[:,['B','C']].astype(int)
+        df.loc[:,['B','C']] = df.loc[:,['B','C']].astype(np.int64)
         expected =  DataFrame([['1',2,3,'.4',5,6.,'foo']],columns=list('ABCDEFG'))
         assert_frame_equal(df,expected)
 
