24a3acc5260a750d59a53b7555f08e8cf6642bd5
Jeff Tratner <jeffrey.tratner@gmail.com>
2014-01-04 23:12:14 -0800

    COMPAT: Require numexpr v2.0 or above.
    Otherwise ignore it (issue with API change pre-2.0)

--- pandas/computation/expressions.py
+++ pandas/computation/expressions.py
@@ -12,6 +12,15 @@ from pandas.core.common import _values_from_object
 try:
     import numexpr as ne
     _NUMEXPR_INSTALLED = True
+    import distutils.version
+    if distutils.version.LooseVersion(ne.__version__) < '2':
+        _NUMEXPR_INSTALLED = False
+        import warnings
+        warnings.warn("pandas requires at least numexpr version 2.0"
+                      " to accelerate with numexpr. Found numexpr version"
+                      " %s" % ne.__version__)
+    del ne
+    del distutils.version
 except ImportError:  # pragma: no cover
     _NUMEXPR_INSTALLED = False
 
