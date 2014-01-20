d3ed7b5d1a733dc8a7879ebf932577bf34cc1b5b
Jan Wagner <wagner@c3e.cs.tu-bs.de>
2014-01-20 12:20:54 +0100

    BUG: xlsxwriter was not able to write date and datetime objects

--- pandas/io/excel.py
+++ pandas/io/excel.py
@@ -780,12 +780,16 @@ class _XlsxWriter(ExcelWriter):
         style_dict: style dictionary to convert
         num_format_str: optional number format string
         """
-        if style_dict is None:
-            return None
 
         # Create a XlsxWriter format object.
         xl_format = self.book.add_format()
+        
+        if num_format_str is not None:
+            xl_format.set_num_format(num_format_str)
 
+        if style_dict is None:
+            return xl_format
+        
         # Map the cell font to XlsxWriter font properties.
         if style_dict.get('font'):
             font = style_dict['font']
@@ -805,9 +809,6 @@ class _XlsxWriter(ExcelWriter):
         # Map the cell borders to XlsxWriter border properties.
         if style_dict.get('borders'):
             xl_format.set_border()
-
-        if num_format_str is not None:
-            xl_format.set_num_format(num_format_str)
 
         return xl_format
 
