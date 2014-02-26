89fb3c0d7fdc6e200e6be3fdae9735315c1429d4
Kevin Sheppard <kevin.sheppard@economics.ox.ac.uk>
2014-02-26 14:34:21 +0000

    Fixed formatting in comparison after casting

--- pandas/io/tests/test_stata.py
+++ pandas/io/tests/test_stata.py
@@ -284,14 +284,17 @@ class TestStata(tm.TestCase):
         s1 = Series(2**9,dtype=np.int16)
         s2 = Series(2**17,dtype=np.int32)
         s3 = Series(2**33,dtype=np.int64)
-        original = DataFrame({'short':s1,'int':s2,'long':s3})
+        original = DataFrame({'int16':s1,'int32':s2,'int64':s3})
         original.index.name = 'index'
+
+        formatted = original
+        formatted['int64'] = formatted['int64'].astype(np.float64)
 
         with tm.ensure_clean() as path:
             original.to_stata(path)
             written_and_read_again = self.read_dta(path)
             tm.assert_frame_equal(written_and_read_again.set_index('index'),
-                                  original)
+                                  formatted)
 
 
 
