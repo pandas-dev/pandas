bb148fc78563e4cbeb2f0ea71a3c0fef547bdb4d
immerrr <immerrr@gmail.com>
2014-02-28 20:13:26 +0400

    BLD: fix indexing__* benchmarks to work in comparative mode

--- vb_suite/indexing.py
+++ vb_suite/indexing.py
@@ -217,25 +217,28 @@ frame_iloc_big = Benchmark('df.iloc[:100,0]', setup,
 # Thorough checks of all containers and all indexing types
 
 import pandas.util.testing as tm
-MAX_ENTRIES = 1000000
-_indices = {}
-
-
-def get_index(idx_type, nentries=MAX_ENTRIES):
-    assert nentries <= MAX_ENTRIES
-    global _indices
-    if not idx_type in _indices:
-        _indices[idx_type] = tm.makeCustomIndex(
-            nentries=MAX_ENTRIES, nlevels=1, idx_type=idx_type)
-    return _indices[idx_type][:nentries]
-
-mask = tm.np.arange(MAX_ENTRIES) % 3 == 0
-series_mask = tm.Series(mask)
 
 setup_template = common_setup + """
 import sys
 
-data = sys.modules[%(module_name)r]
+try:
+    make_index = tm.makeCustomIndexWithCache
+except AttributeError:
+    MAX_ENTRIES = 1000000
+    _indices = {}
+
+    def makeCustomIndexWithCache(nentries, **kwargs):
+        assert nentries < MAX_ENTRIES
+
+        key = tuple(kwargs.items())
+        try:
+            full_idx = _indices[key]
+        except KeyError:
+            full_idx = _indices[key] = tm.makeCustomIndex(nentries=MAX_ENTRIES,
+                                                          **kwargs)
+        return full_idx[:nentries]
+
+    make_index = tm.makeCustomIndexWithCache = makeCustomIndexWithCache
 
 obj = %(class_name)s(%(ctor_args)s)
 
@@ -244,10 +247,9 @@ axis = obj._get_axis(%(axis)r)
 label = axis[pos]
 arr_pos = np.arange(int(len(axis) / 2))
 arr_label = axis[arr_pos].values
-mask = data.mask[:len(axis)]
-series_mask = data.series_mask[:len(axis)]
+mask = tm.np.arange(len(axis)) %% 3 == 0
+series_mask = Series(mask)
 """
-
 
 def generate_index_benchmarks(klass, idx_type, shape):
     if not isinstance(shape, tuple):
@@ -261,8 +263,9 @@ def generate_index_benchmarks(klass, idx_type, shape):
         idx_types = idx_type
 
     axes = klass._AXIS_ORDERS
-    ctor_args = ',\n    '.join(['%s=data.get_index(%r, nentries=%s)' % v
-                                for v in zip(axes, idx_types, shape)])
+    ctor_args = ',\n    '.join([
+        '%s=make_index(idx_type=%r, nentries=%s, nlevels=1)' % v
+        for v in zip(axes, idx_types, shape)])
 
     def get_benchmark_name(indexer, axis):
         shape_type_str = 'x'.join([str(s) + str(t)
@@ -277,44 +280,44 @@ def generate_index_benchmarks(klass, idx_type, shape):
 
     def make_suffix(attrname, indexer_str, axis):
         if axis is not None:
-            indexers = [':'] * ndim
-            indexers[axis] = indexer_str
-            indexer_str = ','.join(indexers)
+            indexers = [':,'] * ndim
+            indexers[axis] = indexer_str + ','
+            indexer_str = ''.join(indexers)
         return '%s[%s]' % (attrname, indexer_str)
 
     benchmarked_axes = set([None, 0, ndim - 1])
 
-    return {
-        b.name: b
-        for b in [
-                Benchmark('obj%s' % params['suffix'],
+    result = {}
+    for axis in benchmarked_axes:
+        for params in [
+            {'indexer': 'basic_pos',
+             'suffix': make_suffix('.iloc', 'pos', axis)},
+            {'indexer': 'basic_label',
+             'suffix': make_suffix('.loc', 'label', axis)},
+
+            {'indexer': 'slice_pos',
+             'suffix': make_suffix('.iloc', ':pos', axis)},
+            {'indexer': 'slice_label',
+             'suffix': make_suffix('.loc', ':label', axis)},
+
+            {'indexer': 'arr_pos',
+             'suffix': make_suffix('.iloc', 'arr_pos', axis)},
+            {'indexer': 'arr_label',
+             'suffix': make_suffix('.loc', 'arr_label', axis)},
+
+            {'indexer': 'iloc_mask',
+             'suffix': make_suffix('.iloc', 'mask', axis)},
+            {'indexer': 'loc_mask',
+             'suffix': make_suffix('.loc', 'mask', axis)}, ]:
+
+            b = Benchmark('obj%s' % params['suffix'],
                           setup_template % {
-                              'module_name': __name__, 'class_name': klass.__name__,
+                              'class_name': klass.__name__,
                               'ctor_args': ctor_args, 'axis': axis or 0},
                           name=get_benchmark_name(params['indexer'], axis))
-                for axis in benchmarked_axes
-                for params in [
-                    {'indexer': 'basic_pos',
-                     'suffix': make_suffix('.iloc', 'pos', axis)},
-                    {'indexer': 'basic_label',
-                     'suffix': make_suffix('.loc', 'label', axis)},
+            result[b.name] = b
 
-                    {'indexer': 'slice_pos',
-                     'suffix': make_suffix('.iloc', ':pos', axis)},
-                    {'indexer': 'slice_label',
-                     'suffix': make_suffix('.loc', ':label', axis)},
-
-                    {'indexer': 'arr_pos',
-                     'suffix': make_suffix('.iloc', 'arr_pos', axis)},
-                    {'indexer': 'arr_label',
-                     'suffix': make_suffix('.loc', 'arr_label', axis)},
-
-                    {'indexer': 'iloc_mask',
-                     'suffix': make_suffix('.iloc', 'mask', axis)},
-                    {'indexer': 'loc_mask',
-                     'suffix': make_suffix('.loc', 'mask', axis)}, ]
-        ]
-    }
+    return result
 
 globals().update(generate_index_benchmarks(tm.Series, 's', 100000))
 globals().update(generate_index_benchmarks(tm.DataFrame, 's', (10, 100000)))
