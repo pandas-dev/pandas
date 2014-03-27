79795ad53df503357e9eca346a2567dc940d8f12
rockg <grant.roch@gmail.com>
2014-03-27 09:57:06 -0400

    Initial implementation of holidays and holiday calendars.
    Implementation of holidays and holiday calendars to be used with the
    CustomBusinessDay offset.  Also add an Easter holiday for use in
    calendars.

--- pandas/tseries/holiday.py
+++ pandas/tseries/holiday.py
@@ -1,43 +1,60 @@
-from pandas import DateOffset, date_range, DatetimeIndex, Series
-from datetime import datetime
-from pandas.tseries.offsets import Easter
+from pandas import DateOffset, date_range, Series
+from datetime import datetime, timedelta
 from dateutil.relativedelta import MO, TU, WE, TH, FR, SA, SU
 
-def Sunday(dt):
+def next_monday(dt):
     '''
-    If the holiday falls on Sunday, make Monday a holiday (nothing
-    happens for Saturday.
+    If holiday falls on Saturday, use following Monday instead;
+    if holiday falls on Sunday, use Monday instead
     '''
-    if dt.isoweekday() == 7:
-        return dt + DateOffset(+1)
-    else:
-        return dt
-    
-def Nearest(dt):
-    '''
-    If the holiday falls on a weekend, make it a 3-day weekend by making
-    Saturday a Friday holiday and Sunday a Monday holiday.
-    '''
-    if dt.isoweekday() == 6:
-        return dt + DateOffset(-1)
-    elif dt.isoweekday() == 7:
-        return dt + DateOffset(+1)
-    else:
-        return dt
+    if dt.weekday() == 5: 
+        return dt + timedelta(2)
+    elif dt.weekday() == 6: 
+        return dt + timedelta(1)
+    return dt
 
-#TODO: Need to add an observance function when a holiday
-# falls on a Tuesday and get a 4-day weekend
-# def Nearest4(dt):
-#     '''
-#     If the holiday falls on Tuesday,
-#     make Monday a holiday as well, otherwise
-#     follow the rules for Nearest (a
-#     3-day weekend).
-#     '''
-#     if dt.isoweekday() == 2:
-#         return dt - DateOffset()
-#     else:
-#         return Nearest(dt)
+def next_monday_or_tuesday(dt):
+    '''
+    For second holiday of two adjacent ones!
+    If holiday falls on Saturday, use following Monday instead;
+    if holiday falls on Sunday or Monday, use following Tuesday instead
+    (because Monday is already taken by adjacent holiday on the day before)
+    '''
+    dow = dt.weekday()
+    if dow == 5 or dow == 6: 
+        return dt + timedelta(2)
+    elif dow == 0: 
+        return dt + timedelta(1)
+    return dt
+
+def previous_friday(dt):
+    '''
+    If holiday falls on Saturday or Sunday, use previous Friday instead.
+    '''
+    if dt.weekday() == 5: 
+        return dt - timedelta(1)
+    elif dt.weekday() == 6: 
+        return dt - timedelta(2)
+    return dt
+
+def sunday_to_monday(dt):
+    '''
+    If holiday falls on Sunday, use day thereafter (Monday) instead.
+    '''
+    if dt.weekday() == 6: 
+        return dt + timedelta(1)
+    return dt
+
+def nearest_workday(dt):
+    '''
+    If holiday falls on Saturday, use day before (Friday) instead;
+    if holiday falls on Sunday, use day thereafter (Monday) instead.
+    '''
+    if dt.weekday() == 5: 
+        return dt - timedelta(1)
+    elif dt.weekday() == 6:
+        return dt + timedelta(1)
+    return dt
 
 class Holiday(object):
     '''
@@ -56,12 +73,31 @@ class Holiday(object):
         self.observance = observance
     
     def __repr__(self):
-        #FIXME: This should handle observance rules as well
-        return 'Holiday %s (%s, %s, %s)' % (self.name, self.month, self.day, 
-                                            self.offset)
-    
-    def dates(self, start_date, end_date):
+        info = ''
+        if self.year is not None:
+            info += 'year=%s, ' % self.year
+        info += 'month=%s, day=%s, ' % (self.month, self.day)
         
+        if self.offset is not None:
+            info += 'offset=%s' % self.offset
+            
+        if self.observance is not None:
+            info += 'observance=%s' % self.observance
+        
+        return 'Holiday: %s (%s)' % (self.name, info)
+    
+    def dates(self, start_date, end_date, return_name=False):
+        '''
+        Calculate holidays between start date and end date
+        
+        Parameters
+        ----------
+        start_date : starting date, datetime-like, optional
+        end_date : ending date, datetime-like, optional
+        return_name : bool, optional, default=False
+            If True, return a series that has dates and holiday names.
+            False will only return dates.
+        '''
         if self.year is not None:
             return datetime(self.year, self.month, self.day)
         
@@ -74,13 +110,11 @@ class Holiday(object):
         year_offset = DateOffset(years=1)   
         baseDate = datetime(start_date.year, self.month, self.day)
         dates = date_range(baseDate, end_date, freq=year_offset)
+        holiday_dates = self._apply_rule(dates)
+        if return_name:
+            return Series(self.name, index=holiday_dates)
         
-        return self._apply_rule(dates)
-    
-    def dates_with_name(self, start_date, end_date):
-        
-        dates = self.dates(start_date, end_date)
-        return Series(self.name, index=dates)
+        return holiday_dates
 
     def _apply_rule(self, dates):   
         '''
@@ -109,31 +143,67 @@ class Holiday(object):
             
         return dates
 
+#----------------------------------------------------------------------
+# Calendar class registration
+
+holiday_calendars = {}
+def register(cls):
+    try:
+        name = cls.name
+    except:
+        name = cls.__name__
+    holiday_calendars[name] = cls
+    
+def get_calendar(name):
+    '''
+    Return an instance of a calendar based on its name.
+    
+    Parameters
+    ----------
+    name : str
+        Calendar name to return an instance of
+    '''
+    return holiday_calendars[name]()
+
+#----------------------------------------------------------------------
+# Holiday classes
+class HolidayCalendarMetaClass(type):
+    def __new__(cls, clsname, bases, attrs):
+        calendar_class = super(HolidayCalendarMetaClass, cls).__new__(cls, clsname, bases, attrs)
+        register(calendar_class)
+        return calendar_class
+
 class AbstractHolidayCalendar(object):
     '''
     Abstract interface to create holidays following certain rules.
     '''
-    _rule_table = []
+    __metaclass__ = HolidayCalendarMetaClass
+    rules = []
+    _holiday_start_date = datetime(1970, 1, 1)
+    _holiday_end_date   = datetime(2030, 12, 31)
+    _holiday_cache = None
     
-    def __init__(self, rules=None):
+    def __init__(self, name=None, rules=None):
         '''
         Initializes holiday object with a given set a rules.  Normally
         classes just have the rules defined within them.
         
         Parameters
         ----------
+        name : str 
+            Name of the holiday calendar, defaults to class name
         rules : array of Holiday objects
             A set of rules used to create the holidays.
         '''
         super(AbstractHolidayCalendar, self).__init__()
-        if rules is not None:
-            self._rule_table = rules
+        if name is None:
+            name = self.__class__.__name__
+        self.name = name
         
-    @property
-    def holiday_rules(self):
-        return self._rule_table
+        if rules is not None:
+            self.rules = rules
 
-    def holidays(self, start=None, end=None, return_names=False):
+    def holidays(self, start=_holiday_start_date, end=_holiday_end_date, return_name=False):
         '''
         Returns a curve with holidays between start_date and end_date
         
@@ -149,40 +219,89 @@ class AbstractHolidayCalendar(object):
         -------
             DatetimeIndex of holidays
         '''
-        #FIXME: Where should the default limits exist?
-        if start is None:
-            start = datetime(1970, 1, 1)
-            
-        if end is None:
-            end = datetime(2030, 12, 31)
-            
-        if self.holiday_rules is None:
+        if self.rules is None:
             raise Exception('Holiday Calendar %s does not have any '\
-                            'rules specified' % self.calendarName)
-        
-        if return_names:
-            holidays = None
-        else:
-            holidays    = []
-        for rule in self.holiday_rules:
-            if return_names:
-                rule_holidays = rule.dates_with_name(start, end)
+                            'rules specified' % self.name)
+
+        holidays = None
+        # If we don't have a cache or the dates are outside the prior cache, we get them again
+        if self._cache is None or start < self._cache[0] or end > self._cache[1]: 
+            for rule in self.rules:
+                rule_holidays = rule.dates(start, end, return_name=True)
                 if holidays is None:
                     holidays = rule_holidays
                 else:
                     holidays = holidays.append(rule_holidays)
-            else:
-                holidays += rule.dates(start, end)
-
-        if return_names:
-            return holidays.sort_index()
+                    
+            self._cache = (start, end, holidays.sort_index())
+        
+        holidays = self._cache[2]
+        holidays = holidays[start:end]
+        
+        if return_name:
+            return holidays
         else:
-            return DatetimeIndex(holidays).order(False)
+            return holidays.index
+    
+    @property
+    def _cache(self):
+        return self.__class__._holiday_cache
+    
+    @_cache.setter
+    def _cache(self, values):
+        self.__class__._holiday_cache = values
+
+    @staticmethod
+    def merge_class(base, other):
+        '''
+        Merge holiday calendars together.  The base calendar
+        will take precedence to other. The merge will be done
+        based on each holiday's name.
+        
+        Parameters
+        ----------
+        base : AbstractHolidayCalendar instance of array of Holiday objects
+        other : AbstractHolidayCalendar instance or array of Holiday objects
+        '''
+        if isinstance(other, AbstractHolidayCalendar):
+            other = other.rules
+        if not isinstance(other, list):
+            other = [other]
+        other_holidays = {holiday.name: holiday for holiday in other}
+            
+        if isinstance(base, AbstractHolidayCalendar):
+            base = base.rules
+        if not isinstance(base, list):
+            base = [base]
+        base_holidays = {holiday.name: holiday for holiday in base}
+        
+        other_holidays.update(base_holidays)
+        return other_holidays.values()
+
+    def merge(self, other, inplace=False):
+        '''
+        Merge holiday calendars together.  The caller's class
+        rules take precedence.  The merge will be done
+        based on each holiday's name.
+        
+        Parameters
+        ----------
+        other : holiday calendar
+        inplace : bool (default=False)
+            If True set rule_table to holidays, else return array of Holidays
+        '''
+        holidays    =   self.merge_class(self, other)
+        if inplace:
+            self.rules = holidays
+        else:
+            return holidays
 
 USMemorialDay     = Holiday('MemorialDay', month=5, day=24, 
                             offset=DateOffset(weekday=MO(1)))
 USLaborDay        = Holiday('Labor Day', month=9, day=1, 
                             offset=DateOffset(weekday=MO(1)))
+USColumbusDay     = Holiday('Columbus Day', month=10, day=1,
+                            offset=DateOffset(weekday=MO(2)))
 USThanksgivingDay = Holiday('Thanksgiving', month=11, day=1, 
                             offset=DateOffset(weekday=TH(4)))
 USMartinLutherKingJr = Holiday('Dr. Martin Luther King Jr.', month=1, day=1, 
@@ -191,27 +310,24 @@ USPresidentsDay      = Holiday('President''s Day', month=2, day=1,
                                offset=DateOffset(weekday=MO(3)))
 
 class USFederalHolidayCalendar(AbstractHolidayCalendar):
-    
-    _rule_table = [ 
-        Holiday('New Years Day', month=1,  day=1,  observance=Nearest), 
+    '''
+    US Federal Government Holiday Calendar based on rules specified
+    by: https://www.opm.gov/policy-data-oversight/snow-dismissal-procedures/federal-holidays/
+    '''
+    rules = [ 
+        Holiday('New Years Day', month=1,  day=1,  observance=nearest_workday), 
         USMartinLutherKingJr,
         USPresidentsDay,
         USMemorialDay,
-        Holiday('July 4th', month=7,  day=4,  observance=Nearest),
-        USLaborDay, 
-        Holiday('Columbus Day', month=10, day=1,  offset=DateOffset(weekday=MO(2))),
-        Holiday('Veterans Day', month=11, day=11, observance=Nearest),
-        USThanksgivingDay,
-        Holiday('Christmas', month=12, day=25, observance=Nearest)
-        ]
-
-class NERCHolidayCalendar(AbstractHolidayCalendar):
-    
-    _rule_table = [
-        Holiday('New Years Day', month=1, day=1, observance=Sunday),
-        USMemorialDay,
-        Holiday('July 4th', month=7,  day=4, observance=Sunday),
+        Holiday('July 4th', month=7,  day=4,  observance=nearest_workday),
         USLaborDay,
+        USColumbusDay,
+        Holiday('Veterans Day', month=11, day=11, observance=nearest_workday),
         USThanksgivingDay,
-        Holiday('Christmas', month=12, day=25, observance=Sunday)
+        Holiday('Christmas', month=12, day=25, observance=nearest_workday)
         ]
+    
+def HolidayCalendarFactory(name, base, other, base_class=AbstractHolidayCalendar):
+    rules = AbstractHolidayCalendar.merge_class(base, other)
+    calendar_class = type(name, (base_class,), {"rules": rules, "name": name})
+    return calendar_class
--- pandas/tseries/tests/test_holiday.py
+++ pandas/tseries/tests/test_holiday.py
@@ -2,14 +2,17 @@
 from datetime import datetime
 import pandas.util.testing as tm
 from pandas.tseries.holiday import (
-    USFederalHolidayCalendar, USMemorialDay, USThanksgivingDay)
+    USFederalHolidayCalendar, USMemorialDay, USThanksgivingDay,
+    nearest_workday, next_monday_or_tuesday, next_monday,
+    previous_friday, sunday_to_monday)
 
 class TestCalendar(tm.TestCase):
     
     def test_calendar(self):
 
         calendar = USFederalHolidayCalendar()
-        holidays = calendar.holidays(datetime(2012, 1, 1), datetime(2012, 12, 31))
+        holidays = calendar.holidays(datetime(2012, 1, 1), 
+                                     datetime(2012, 12, 31))
         
         holidayList = [
                        datetime(2012, 1, 2),
@@ -23,7 +26,8 @@ class TestCalendar(tm.TestCase):
                        datetime(2012, 11, 22),
                        datetime(2012, 12, 25)]
         
-        self.assertEqual(list(holidays.to_pydatetime()), holidayList)
+        self.assertEqual(list(holidays.to_pydatetime()), 
+                         holidayList)
         
 class TestHoliday(tm.TestCase):
     
@@ -32,7 +36,8 @@ class TestHoliday(tm.TestCase):
         self.end_date   = datetime(2020, 12, 31)
     
     def test_usmemorialday(self):
-        holidays = USMemorialDay.dates(self.start_date, self.end_date)
+        holidays = USMemorialDay.dates(self.start_date, 
+                                       self.end_date)
         holidayList = [
                        datetime(2011, 5, 30),
                        datetime(2012, 5, 28),
@@ -45,10 +50,11 @@ class TestHoliday(tm.TestCase):
                        datetime(2019, 5, 27),
                        datetime(2020, 5, 25),
                        ]
-        self.assertEqual(holidays, holidayList)
+        self.assertEqual(list(holidays), holidayList)
         
     def test_usthanksgivingday(self):
-        holidays = USThanksgivingDay.dates(self.start_date, self.end_date)
+        holidays = USThanksgivingDay.dates(self.start_date, 
+                                           self.end_date)
         holidayList = [
                        datetime(2011, 11, 24),
                        datetime(2012, 11, 22),
@@ -61,4 +67,36 @@ class TestHoliday(tm.TestCase):
                        datetime(2019, 11, 28),
                        datetime(2020, 11, 26),
                        ]
-        self.assertEqual(holidays, holidayList)
+        self.assertEqual(list(holidays), holidayList)
+        
+class TestObservanceRules(tm.TestCase):
+    
+    def setUp(self):
+        self.we =   datetime(2014, 4, 9)
+        self.th =   datetime(2014, 4, 10)
+        self.fr =   datetime(2014, 4, 11)
+        self.sa =   datetime(2014, 4, 12)
+        self.su =   datetime(2014, 4, 13)
+        self.mo =   datetime(2014, 4, 14)
+        self.tu =   datetime(2014, 4, 15)
+    
+    def test_next_monday(self):
+        self.assertEqual(next_monday(self.sa), self.mo)
+        self.assertEqual(next_monday(self.su), self.mo)
+
+    def test_next_monday_or_tuesday(self):
+        self.assertEqual(next_monday_or_tuesday(self.sa), self.mo)
+        self.assertEqual(next_monday_or_tuesday(self.su), self.tu)
+        self.assertEqual(next_monday_or_tuesday(self.mo), self.tu)
+
+    def test_previous_friday(self):
+        self.assertEqual(previous_friday(self.sa), self.fr)
+        self.assertEqual(previous_friday(self.su), self.fr)
+
+    def test_sunday_to_monday(self):
+        self.assertEqual(sunday_to_monday(self.su), self.mo)
+
+    def test_nearest_workday(self):
+        self.assertEqual(nearest_workday(self.sa), self.fr)
+        self.assertEqual(nearest_workday(self.su), self.mo)
+        self.assertEqual(nearest_workday(self.mo), self.mo)
