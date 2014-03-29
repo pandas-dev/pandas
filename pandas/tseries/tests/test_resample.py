32b3a0617649ce468b97fc1065a662c6f48fba17
jreback <jeff@reback.net>
2014-03-28 20:40:24 -0400

    BUG: Bug in resample when how=None resample freq is the same as the axis frequency (GH5955)

--- pandas/tseries/tests/test_resample.py
+++ pandas/tseries/tests/test_resample.py
@@ -160,6 +160,32 @@ class TestResample(tm.TestCase):
         self.assertEquals(result.irow(5), s['1/9/2005'])
         self.assertEqual(result.index.name, 'index')
 
+    def test_resample_upsampling_picked_but_not_correct(self):
+
+        # Test for issue #3020
+        dates = date_range('01-Jan-2014','05-Jan-2014', freq='D')
+        series = Series(1, index=dates)
+
+        result = series.resample('D')
+        self.assertEquals(result.index[0], dates[0])
+
+        # GH 5955
+        # incorrect deciding to upsample when the axis frequency matches the resample frequency
+
+        import datetime
+        s = Series(np.arange(1.,6),index=[datetime.datetime(1975, 1, i, 12, 0) for i in range(1, 6)])
+        expected = Series(np.arange(1.,6),index=date_range('19750101',periods=5,freq='D'))
+
+        result = s.resample('D',how='count')
+        assert_series_equal(result,Series(1,index=expected.index))
+
+        result1 = s.resample('D',how='sum')
+        result2 = s.resample('D',how='mean')
+        result3 = s.resample('D')
+        assert_series_equal(result1,expected)
+        assert_series_equal(result2,expected)
+        assert_series_equal(result3,expected)
+
     def test_resample_frame_basic(self):
         df = tm.makeTimeDataFrame()
 
@@ -1077,15 +1103,6 @@ class TestResamplePeriodIndex(tm.TestCase):
 
         result = s.resample("A", how='mean')
         tm.assert_almost_equal(result[0], s.mean())
-
-    def test_resample_doesnt_truncate(self):
-        # Test for issue #3020
-        import pandas as pd
-        dates = pd.date_range('01-Jan-2014','05-Jan-2014', freq='D')
-        series = Series(1, index=dates)
-
-        result = series.resample('D')
-        self.assertEquals(result.index[0], dates[0])
 
     def test_evenly_divisible_with_no_extra_bins(self):
         # 4076
