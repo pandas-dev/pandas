3a4e770778febe9c37c3ccad921619f996290d8b
Dale Jung <dale@dalejung.com>
2014-03-30 22:23:55 -0400

    PRF: Fixed shift speed tests and added consolidated DataFrame shift perf test

--- vb_suite/frame_methods.py
+++ vb_suite/frame_methods.py
@@ -386,7 +386,7 @@ def test_equal(name):
 def test_unequal(name):
     df, df2 = pairs[name]
     return df.equals(df2)
-    
+
 float_df = DataFrame(np.random.randn(1000, 1000))
 object_df = DataFrame([['foo']*1000]*1000)
 nonunique_cols = object_df.copy()
@@ -434,11 +434,21 @@ frame_interpolate_some_good_infer = Benchmark('df.interpolate(downcast="infer")'
 # frame shift speedup issue-5609
 
 setup = common_setup + """
-df = pd.DataFrame(np.random.rand(10000,500))
+df = DataFrame(np.random.rand(10000,500))
+# note: df._data.blocks are f_contigous
 """
 frame_shift_axis0 = Benchmark('df.shift(1,axis=0)', setup,
-                    name = 'frame_shift_axis_0',
                     start_date=datetime(2014,1,1))
 frame_shift_axis1 = Benchmark('df.shift(1,axis=1)', setup,
-                    name = 'frame_shift_axis_1',
+                    start_date=datetime(2014,1,1))
+
+#
+setup = common_setup + """
+df = DataFrame(np.random.rand(10000,500))
+df = df.consolidate()
+# note: df._data.blocks are c_contigous
+"""
+frame_shift_c_order_axis0 = Benchmark('df.shift(1,axis=0)', setup,
+                    start_date=datetime(2014,1,1))
+frame_shift_c_order_axis1 = Benchmark('df.shift(1,axis=1)', setup,
                     start_date=datetime(2014,1,1))
