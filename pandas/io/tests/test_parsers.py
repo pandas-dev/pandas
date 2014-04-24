eb3f8a6fa47b6359ed115b860a6bae69dcf2ff49
hshimizu77 <shimizu.hirohisa@nifty.com>
2014-04-25 00:36:08 +0900

    change u('...') to b'...'.decode

--- pandas/io/tests/test_parsers.py
+++ pandas/io/tests/test_parsers.py
@@ -1823,7 +1823,8 @@ A,B,C
             self.assertEquals(len(result), 50)
 
     def test_read_csv_example_in_windows_filesystem(self):
-        path = tm.get_data_path(u('日本語ファイル名テスト_read_csv_in_win_filesystem.csv'))
+        path = b'\xe6\x97\xa5\xe6\x9c\xac\xe8\xaa\x9e\xe3\x83\x95\xe3\x82\xa1\xe3\x82\xa4\xe3\x83\xab\xe5\x90\x8d\xe3\x83\x86\xe3\x82\xb9\xe3\x83\x88_read_csv_in_win_filesystem.csv'
+        path = tm.get_data_path(path.decode())
         self.read_csv(path)
 
     def test_converters_corner_with_nas(self):
