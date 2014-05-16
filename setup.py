a5f1a5d2020125f6f695482dce4c12e35f2171a2
jreback <jeff@reback.net>
2014-05-16 18:18:55 -0400

    DOC: update setup.py with 3.4 note

--- setup.py
+++ setup.py
@@ -185,6 +185,7 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2.7',
     'Programming Language :: Python :: 3.2',
     'Programming Language :: Python :: 3.3',
+    'Programming Language :: Python :: 3.4',
     'Programming Language :: Cython',
     'Topic :: Scientific/Engineering',
 ]
