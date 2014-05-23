0ad9a8f7c7935b108cdd1ce177ce80f89c8cfccc
jreback <jeff@reback.net>
2014-05-23 12:22:44 -0400

    TST: tests for reindex_like consistency (GH7179)

--- pandas/tests/test_series.py
+++ pandas/tests/test_series.py
@@ -5187,6 +5187,18 @@ class TestSeries(tm.TestCase, CheckNameIntegration):
         assert_series_equal(self.ts.reindex(other.index),
                             self.ts.reindex_like(other))
 
+        # GH 7179
+        day1 = datetime(2013,3,5)
+        day2 = datetime(2013,5,5)
+        day3 = datetime(2014,3,5)
+
+        series1 = Series([5, None, None],[day1, day2, day3])
+        series2 = Series([None, None], [day1, day3])
+
+        result = series1.reindex_like(series2, method='pad')
+        expected = Series([5, np.nan], index=[day1, day3])
+        assert_series_equal(result, expected)
+
     def test_reindex_fill_value(self):
         #------------------------------------------------------------
         # floats
