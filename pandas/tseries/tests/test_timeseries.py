f5c7dad2bfddd1c79d26a29f268c0ae72ce3313f
sinhrks <sinhrks@gmail.com>
2014-06-03 21:03:32 +0900

    BUG: Revisit Index.delete preserves freq

--- pandas/tseries/tests/test_timeseries.py
+++ pandas/tseries/tests/test_timeseries.py
@@ -2293,33 +2293,82 @@ class TestDatetimeIndex(tm.TestCase):
         self.assertEqual(result.freqstr, 'M')
 
     def test_delete(self):
-        idx = date_range(start='2000-01-01', periods=4, freq='M', name='idx')
+        idx = date_range(start='2000-01-01', periods=5, freq='M', name='idx')
 
-        expected = date_range(start='2000-02-01', periods=3, freq='M', name='idx')
-        result = idx.delete(0)
-        self.assertTrue(result.equals(expected))
-        self.assertEqual(result.name, expected.name)
-        self.assertEqual(result.freqstr, 'M')
+        # prserve freq
+        expected_0 = date_range(start='2000-02-01', periods=4, freq='M', name='idx')
+        expected_4 = date_range(start='2000-01-01', periods=4, freq='M', name='idx')
 
-        expected = date_range(start='2000-01-01', periods=3, freq='M', name='idx')
-        result = idx.delete(-1)
-        self.assertTrue(result.equals(expected))
-        self.assertEqual(result.name, expected.name)
-        self.assertEqual(result.freqstr, 'M')
+        # reset freq to None
+        expected_1 = DatetimeIndex(['2000-01-31', '2000-03-31', '2000-04-30',
+                                    '2000-05-31'], freq=None, name='idx')
+
+        cases ={0: expected_0, -5: expected_0,
+                -1: expected_4, 4: expected_4,
+                1: expected_1}
+        for n, expected in compat.iteritems(cases):
+            result = idx.delete(n)
+            self.assertTrue(result.equals(expected))
+            self.assertEqual(result.name, expected.name)
+            self.assertEqual(result.freq, expected.freq)
 
         with tm.assertRaises((IndexError, ValueError)):
             # either depeidnig on numpy version
             result = idx.delete(5)
 
-        idx = date_range(start='2000-01-01', periods=4,
-                         freq='M', name='idx', tz='US/Pacific')
+        idx = date_range(start='2000-01-01', periods=5,
+                         freq='D', name='idx', tz='US/Pacific')
 
-        expected = date_range(start='2000-02-01', periods=3,
-                              freq='M', name='idx', tz='US/Pacific')
+        expected = date_range(start='2000-01-02', periods=4,
+                              freq='D', name='idx', tz='US/Pacific')
         result = idx.delete(0)
         self.assertTrue(result.equals(expected))
         self.assertEqual(result.name, expected.name)
-        self.assertEqual(result.freqstr, 'M')
+        self.assertEqual(result.freqstr, 'D')
+        self.assertEqual(result.tz, expected.tz)
+
+    def test_delete_slice(self):
+        idx = date_range(start='2000-01-01', periods=10, freq='D', name='idx')
+
+        # prserve freq
+        expected_0_2 = date_range(start='2000-01-04', periods=7, freq='D', name='idx')
+        expected_7_9 = date_range(start='2000-01-01', periods=7, freq='D', name='idx')
+
+        # reset freq to None
+        expected_3_5 = DatetimeIndex(['2000-01-01', '2000-01-02', '2000-01-03',
+                                    '2000-01-07', '2000-01-08', '2000-01-09',
+                                    '2000-01-10'], freq=None, name='idx')
+
+        cases ={(0, 1, 2): expected_0_2,
+                (7, 8, 9): expected_7_9,
+                (3, 4, 5): expected_3_5}
+        for n, expected in compat.iteritems(cases):
+            result = idx.delete(n)
+            self.assertTrue(result.equals(expected))
+            self.assertEqual(result.name, expected.name)
+            self.assertEqual(result.freq, expected.freq)
+
+            result = idx.delete(slice(n[0], n[-1] + 1))
+            self.assertTrue(result.equals(expected))
+            self.assertEqual(result.name, expected.name)
+            self.assertEqual(result.freq, expected.freq)
+
+        ts = pd.Series(1, index=pd.date_range('2000-01-01', periods=10,
+                                              freq='D', name='idx'))
+        # preserve freq
+        result = ts.drop(ts.index[:5]).index
+        expected = pd.date_range('2000-01-06', periods=5, freq='D', name='idx')
+        self.assertTrue(result.equals(expected))
+        self.assertEqual(result.name, expected.name)
+        self.assertEqual(result.freq, expected.freq)
+
+        # reset freq to None
+        result = ts.drop(ts.index[[1, 3, 5, 7, 9]]).index
+        expected = DatetimeIndex(['2000-01-01', '2000-01-03', '2000-01-05',
+                                    '2000-01-07', '2000-01-09'], freq=None, name='idx')
+        self.assertTrue(result.equals(expected))
+        self.assertEqual(result.name, expected.name)
+        self.assertEqual(result.freq, expected.freq)
 
     def test_map_bug_1677(self):
         index = DatetimeIndex(['2012-04-25 09:30:00.393000'])
