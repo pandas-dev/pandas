2edd3ba2ec80dc0ed3d8e809c5fbe6727dd17f42
jreback <jeff@reback.net>
2014-07-04 11:38:39 -0400

    BUG: Bug in multi-index slice setting, related GH3738

--- pandas/tests/test_indexing.py
+++ pandas/tests/test_indexing.py
@@ -1883,6 +1883,29 @@ class TestIndexing(tm.TestCase):
             df.loc['bar'] *= 2
         self.assertRaises(TypeError, f)
 
+        # from SO
+        #http://stackoverflow.com/questions/24572040/pandas-access-the-level-of-multiindex-for-inplace-operation
+        df_orig = DataFrame.from_dict({'price': {
+            ('DE', 'Coal', 'Stock'): 2,
+            ('DE', 'Gas', 'Stock'): 4,
+            ('DE', 'Elec', 'Demand'): 1,
+            ('FR', 'Gas', 'Stock'): 5,
+            ('FR', 'Solar', 'SupIm'): 0,
+            ('FR', 'Wind', 'SupIm'): 0}})
+        df_orig.index = MultiIndex.from_tuples(df_orig.index, names=['Sit', 'Com', 'Type'])
+
+        expected = df_orig.copy()
+        expected.iloc[[0,2,3]] *= 2
+
+        idx = pd.IndexSlice
+        df = df_orig.copy()
+        df.loc[idx[:,:,'Stock'],:] *= 2
+        assert_frame_equal(df, expected)
+
+        df = df_orig.copy()
+        df.loc[idx[:,:,'Stock'],'price'] *= 2
+        assert_frame_equal(df, expected)
+
     def test_getitem_multiindex(self):
 
         # GH 5725
