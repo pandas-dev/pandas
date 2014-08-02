8d80a4278bd20fad71ac85d0935f005d82424beb
jreback <jeff@reback.net>
2014-08-02 10:24:39 -0400

    TST: make removal tests exact for test_pytables

--- pandas/io/tests/test_pytables.py
+++ pandas/io/tests/test_pytables.py
@@ -2272,7 +2272,7 @@ class TestHDFStore(tm.TestCase):
 
             # try to remove non-table (with crit)
             # non-table ok (where = None)
-            wp = tm.makePanel()
+            wp = tm.makePanel(30)
             store.put('wp', wp, format='table')
             store.remove('wp', ["minor_axis=['A', 'D']"])
             rs = store.select('wp')
@@ -2303,59 +2303,66 @@ class TestHDFStore(tm.TestCase):
 
         with ensure_clean_store(self.path) as store:
 
-            wp = tm.makePanel()
+            wp = tm.makePanel(30)
 
             # start
+            _maybe_remove(store, 'wp1')
             store.put('wp1', wp, format='t')
             n = store.remove('wp1', start=32)
-            #assert(n == 120-32)
+            self.assertTrue(n == 120-32)
             result = store.select('wp1')
             expected = wp.reindex(major_axis=wp.major_axis[:32//4])
             assert_panel_equal(result, expected)
 
+            _maybe_remove(store, 'wp2')
             store.put('wp2', wp, format='t')
             n = store.remove('wp2', start=-32)
-            #assert(n == 32)
+            self.assertTrue(n == 32)
             result = store.select('wp2')
             expected = wp.reindex(major_axis=wp.major_axis[:-32//4])
             assert_panel_equal(result, expected)
 
             # stop
+            _maybe_remove(store, 'wp3')
             store.put('wp3', wp, format='t')
             n = store.remove('wp3', stop=32)
-            #assert(n == 32)
+            self.assertTrue(n == 32)
             result = store.select('wp3')
             expected = wp.reindex(major_axis=wp.major_axis[32//4:])
             assert_panel_equal(result, expected)
 
+            _maybe_remove(store, 'wp4')
             store.put('wp4', wp, format='t')
             n = store.remove('wp4', stop=-32)
-            #assert(n == 120-32)
+            self.assertTrue(n == 120-32)
             result = store.select('wp4')
             expected = wp.reindex(major_axis=wp.major_axis[-32//4:])
             assert_panel_equal(result, expected)
 
             # start n stop
+            _maybe_remove(store, 'wp5')
             store.put('wp5', wp, format='t')
             n = store.remove('wp5', start=16, stop=-16)
-            #assert(n == 120-32)
+            self.assertTrue(n == 120-32)
             result = store.select('wp5')
             expected = wp.reindex(major_axis=wp.major_axis[:16//4]+wp.major_axis[-16//4:])
             assert_panel_equal(result, expected)
 
+            _maybe_remove(store, 'wp6')
             store.put('wp6', wp, format='t')
             n = store.remove('wp6', start=16, stop=16)
-            #assert(n == 0)
+            self.assertTrue(n == 0)
             result = store.select('wp6')
             expected = wp.reindex(major_axis=wp.major_axis)
             assert_panel_equal(result, expected)
 
             # with where
+            _maybe_remove(store, 'wp7')
             date = wp.major_axis.take(np.arange(0,30,3))
             crit = Term('major_axis=date')
             store.put('wp7', wp, format='t')
             n = store.remove('wp7', where=[crit], stop=80)
-            #assert(n == 28)
+            self.assertTrue(n == 28)
             result = store.select('wp7')
             expected = wp.reindex(major_axis=wp.major_axis-wp.major_axis[np.arange(0,20,3)])
             assert_panel_equal(result, expected)
@@ -2364,9 +2371,10 @@ class TestHDFStore(tm.TestCase):
 
         with ensure_clean_store(self.path) as store:
 
-            wp = tm.makePanel()
+            wp = tm.makePanel(30)
 
             # group row removal
+            _maybe_remove(store, 'wp3')
             date4 = wp.major_axis.take([0, 1, 2, 4, 5, 6, 8, 9, 10])
             crit4 = Term('major_axis=date4')
             store.put('wp3', wp, format='t')
@@ -2377,6 +2385,7 @@ class TestHDFStore(tm.TestCase):
             assert_panel_equal(result, expected)
 
             # upper half
+            _maybe_remove(store, 'wp')
             store.put('wp', wp, format='table')
             date = wp.major_axis[len(wp.major_axis) // 2]
 
@@ -2394,6 +2403,7 @@ class TestHDFStore(tm.TestCase):
             assert_panel_equal(result, expected)
 
             # individual row elements
+            _maybe_remove(store, 'wp2')
             store.put('wp2', wp, format='table')
 
             date1 = wp.major_axis[1:3]
@@ -2420,6 +2430,7 @@ class TestHDFStore(tm.TestCase):
             assert_panel_equal(result, expected)
 
             # corners
+            _maybe_remove(store, 'wp4')
             store.put('wp4', wp, format='table')
             n = store.remove(
                 'wp4', where=[Term('major_axis>wp.major_axis[-1]')])
