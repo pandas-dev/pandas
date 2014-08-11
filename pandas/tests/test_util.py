18081a18ab4b0205be375b57873ff039c8f3ae9d
ischwabacher <ischwabacher@wisc.edu>
2014-08-11 15:12:09 -0500

    Allow deprecate_kwarg to transform arguments
    Add a keyword argument `mapping` to `pandas.util.deprecate_kwarg` to allow
    remapping deprecated keyword arguments.  For example, in #7963, which inspired
    this PR, one could write this:
    
    ```python
    @deprecate_kwarg(old_arg_name='infer_dst', new_arg_name='ambiguous',
                     mapping={True: 'infer', False: 'raise'})
    def tz_localize(self, tz, ambiguous=None):
        ...
    ```
    
    Tests of this functionality are added to `pandas/tests/test_util.py`, created
    for this purpose; the `if __name__ == '__main__'` section is cribbed directly
    from `test_index.py`.

---
+++ pandas/tests/test_util.py
@@ -0,0 +1,64 @@
+
+import warnings
+
+import nose
+
+import pandas.util
+from pandas.util.decorators import deprecate_kwarg
+import pandas.util.testing as tm
+
+class TestDecorators(tm.TestCase):
+    def setUp(self):
+        @deprecate_kwarg('old', 'new')
+        def _f1(new=False):
+            return new
+
+        @deprecate_kwarg('old', 'new', {'yes': True, 'no': False})
+        def _f2(new=False):
+            return new
+
+        @deprecate_kwarg('old', 'new', lambda x: x+1)
+        def _f3(new=0):
+            return new
+
+        self.f1 = _f1
+        self.f2 = _f2
+        self.f3 = _f3
+
+    def test_deprecate_kwarg(self):
+        x = 78
+        with tm.assert_produces_warning(FutureWarning):
+            result = self.f1(old=x)
+        self.assertIs(result, x)
+        with tm.assert_produces_warning(None):
+            self.f1(new=x)
+
+    def test_dict_deprecate_kwarg(self):
+        x = 'yes'
+        with tm.assert_produces_warning(FutureWarning):
+            result = self.f2(old=x)
+        self.assertEqual(result, True)
+
+    def test_missing_deprecate_kwarg(self):
+        x = 'bogus'
+        with tm.assert_produces_warning(FutureWarning):
+            result = self.f2(old=x)
+        self.assertEqual(result, 'bogus')
+
+    def test_callable_deprecate_kwarg(self):
+        x = 5
+        with tm.assert_produces_warning(FutureWarning):
+            result = self.f3(old=x)
+        self.assertEqual(result, x+1)
+        with tm.assertRaises(TypeError):
+            self.f3(old='hello')
+
+    def test_bad_deprecate_kwarg(self):
+        with tm.assertRaises(TypeError):
+            @deprecate_kwarg('old', 'new', 0)
+            def f4(new=None):
+                pass
+
+if __name__ == '__main__':
+    nose.runmodule(argv=[__file__, '-vvs', '-x', '--pdb', '--pdb-failure'],
+                   exit=False)
