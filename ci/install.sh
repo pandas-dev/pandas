790840b25190b76ed1ef74eaed459151b157a109
jreback <jeff@reback.net>
2014-08-29 15:54:58 -0400

    CI: change nose to be 1.3.4

--- ci/install.sh
+++ ci/install.sh
@@ -43,7 +43,8 @@ echo "home_dir: [$home_dir]"
 pip install -I -U pip
 pip install -I -U setuptools
 pip install wheel==0.22
-pip install nose==1.3.3
+#pip install nose==1.3.3
+pip install nose==1.3.4
 
 # comment this line to disable the fetching of wheel files
 base_url=http://pandas.pydata.org/pandas-build/dev/wheels
