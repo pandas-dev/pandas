Fixed precision test error for read_csv

pandas/io/tests/test_parsers.py
@@ -3187,7 +3187,8 @@ class TestCParserLowMemory(ParserTests, tm.TestCase):
 
     def test_precise_conversion(self):
         # GH #8002
-        from decimal import Decimal
+        from decimal import Decimal, getcontext
+        getcontext().prec = 100
         normal_errors = []
         precise_errors = []
         for num in np.linspace(1., 2., num=500): # test numbers between 1 and 2
@@ -3201,8 +3202,8 @@ class TestCParserLowMemory(ParserTests, tm.TestCase):
             normal_errors.append(error(normal_val))
             precise_errors.append(error(precise_val))
             self.assertEqual(roundtrip_val, float(text[2:])) # round-trip should match float()
-        self.assertTrue(sum(precise_errors) < sum(normal_errors))
-        self.assertTrue(max(precise_errors) < max(normal_errors))
+        self.assertTrue(sum(precise_errors) <= sum(normal_errors))
+        self.assertTrue(max(precise_errors) <= max(normal_errors))
 
     def test_pass_dtype(self):
         data = """\