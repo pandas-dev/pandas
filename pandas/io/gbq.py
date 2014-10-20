403f38da35ab04d3997f61db3c98134fe02910d3
pallav-fdsi <pallav@fielddiagnostics.com>
2014-10-19 20:49:21 -0400

    BUG: invalid import checking in gbq (GH8587)

--- pandas/io/gbq.py
+++ pandas/io/gbq.py
@@ -43,7 +43,7 @@ if not compat.PY3:
             _GOOGLE_API_CLIENT_INSTALLED=True
             _GOOGLE_API_CLIENT_VERSION = pkg_resources.get_distribution('google-api-python-client').version
 
-            if LooseVersion(_GOOGLE_API_CLIENT_VERSION >= '1.2.0'):
+            if LooseVersion(_GOOGLE_API_CLIENT_VERSION) >= '1.2.0':
                 _GOOGLE_API_CLIENT_VALID_VERSION = True
 
         except ImportError:
@@ -56,7 +56,7 @@ if not compat.PY3:
 
             _GOOGLE_FLAGS_VERSION = pkg_resources.get_distribution('python-gflags').version
 
-            if LooseVersion(_GOOGLE_FLAGS_VERSION >= '2.0.0'):
+            if LooseVersion(_GOOGLE_FLAGS_VERSION) >= '2.0':
                 _GOOGLE_FLAGS_VALID_VERSION = True
 
         except ImportError:
