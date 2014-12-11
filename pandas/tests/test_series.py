39048f62a53123ede2dee1f09a36afb2643cb32c
jreback <jeff@reback.net>
2014-12-10 19:38:21 -0500

    TST: fix related to dateutil test failure in test_series.py

--- pandas/tests/test_series.py
+++ pandas/tests/test_series.py
@@ -5020,7 +5020,8 @@ class TestSeries(tm.TestCase, CheckNameIntegration):
 
     def test_getitem_setitem_datetime_tz_dateutil(self):
         tm._skip_if_no_dateutil();
-        from dateutil.tz import gettz, tzutc
+        from dateutil.tz import tzutc
+        from dateutil.zoneinfo import gettz
         tz = lambda x: tzutc() if x == 'UTC' else gettz(x)  # handle special case for utc in dateutil
 
         from pandas import date_range
