1bcfe35d40755ad471695abb4e9b6aab2287c515
David Cottrell <david_cottrell@scotiacapital.com>
2015-02-10 22:21:42 +0000

    Fix bug in multiindex series groupby where sort argument is ignored (issue 9444). One test added.

--- pandas/tests/test_groupby.py
+++ pandas/tests/test_groupby.py
@@ -3270,6 +3270,23 @@ class TestGroupBy(tm.TestCase):
                                   self.df['B'].values]).sum()
         self.assertEqual(result.index.names, (None, None))
 
+    def test_groupby_sort_multiindex_series(self):
+        # series multiindex groupby sort argument was not being passed through _compress_group_index
+        # GH 9444
+        index = MultiIndex(levels=[[1, 2], [1, 2]],
+                           labels=[[0, 0, 0, 0, 1, 1], [1, 1, 0, 0, 0, 0]],
+                           names=['a', 'b'])
+        mseries = Series([0, 1, 2, 3, 4, 5], index=index)
+        index = MultiIndex(levels=[[1, 2], [1, 2]],
+                           labels=[[0, 0, 1], [1, 0, 0]],
+                           names=['a', 'b'])
+        mseries_result = Series([0, 2, 4], index=index)
+
+        result = mseries.groupby(level=['a', 'b'], sort=False).first()
+        assert_series_equal(result, mseries_result)
+        result = mseries.groupby(level=['a', 'b'], sort=True).first()
+        assert_series_equal(result, mseries_result.sort_index())
+
     def test_groupby_categorical(self):
         levels = ['foo', 'bar', 'baz', 'qux']
         codes = np.random.randint(0, 4, size=100)
