1b205937809df225bef1e49de217262bc0359ab0
David Stephens <dstephens99@gmail.com>
2015-03-28 12:14:32 -0700

    COMPAT: Fix dl_mult_symbols for Python3

--- pandas/io/data.py
+++ pandas/io/data.py
@@ -326,18 +326,23 @@ def _dl_mult_symbols(symbols, start, end, interval, chunksize, retry_count, paus
                      method):
     stocks = {}
     failed = []
+    passed = []
     for sym_group in _in_chunks(symbols, chunksize):
         for sym in sym_group:
             try:
                 stocks[sym] = method(sym, start, end, interval, retry_count, pause)
+                passed.append(sym)
             except IOError:
                 warnings.warn('Failed to read symbol: {0!r}, replacing with '
                               'NaN.'.format(sym), SymbolWarning)
                 failed.append(sym)
 
+    if len(passed) == 0:
+        raise RemoteDataError("No data fetched using "
+                              "{0!r}".format(method.__name__))
     try:
-        if len(stocks) > 0 and len(failed) > 0:
-            df_na = stocks.values()[0].copy()
+        if len(stocks) > 0 and len(failed) > 0 and len(passed) > 0:
+            df_na = stocks[passed[0]].copy()
             df_na[:] = np.nan
             for sym in failed:
                 stocks[sym] = df_na
@@ -346,7 +351,6 @@ def _dl_mult_symbols(symbols, start, end, interval, chunksize, retry_count, paus
         # cannot construct a panel with just 1D nans indicating no data
         raise RemoteDataError("No data fetched using "
                               "{0!r}".format(method.__name__))
-
 
 _source_functions = {'google': _get_hist_google, 'yahoo': _get_hist_yahoo}
 
