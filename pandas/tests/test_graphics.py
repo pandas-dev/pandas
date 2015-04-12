c10400abe0e51a7bac719b0133a0619a32f61c3c
sinhrks <sinhrks@gmail.com>
2015-04-12 11:09:58 +0900

    ENH/BUG: color cannot be applied to line subplots

--- pandas/tests/test_graphics.py
+++ pandas/tests/test_graphics.py
@@ -2593,6 +2593,67 @@ class TestDataFramePlots(TestPlotBase):
         tm.close()
 
     @slow
+    def test_line_colors_and_styles_subplots(self):
+        # GH 9894
+        from matplotlib import cm
+        default_colors = self.plt.rcParams.get('axes.color_cycle')
+
+        df = DataFrame(randn(5, 5))
+
+        axes = df.plot(subplots=True)
+        for ax, c in zip(axes, list(default_colors)):
+            self._check_colors(ax.get_lines(), linecolors=c)
+        tm.close()
+
+        # single color char
+        axes = df.plot(subplots=True, color='k')
+        for ax in axes:
+            self._check_colors(ax.get_lines(), linecolors=['k'])
+        tm.close()
+
+        # single color str
+        axes = df.plot(subplots=True, color='green')
+        for ax in axes:
+            self._check_colors(ax.get_lines(), linecolors=['green'])
+        tm.close()
+
+        custom_colors = 'rgcby'
+        axes = df.plot(color=custom_colors, subplots=True)
+        for ax, c in zip(axes, list(custom_colors)):
+            self._check_colors(ax.get_lines(), linecolors=[c])
+        tm.close()
+
+        axes = df.plot(color=list(custom_colors), subplots=True)
+        for ax, c in zip(axes, list(custom_colors)):
+            self._check_colors(ax.get_lines(), linecolors=[c])
+        tm.close()
+
+        rgba_colors = lmap(cm.jet, np.linspace(0, 1, len(df)))
+        for cmap in ['jet', cm.jet]:
+            axes = df.plot(colormap=cmap, subplots=True)
+            for ax, c in zip(axes, rgba_colors):
+                self._check_colors(ax.get_lines(), linecolors=[c])
+            tm.close()
+
+        # make color a list if plotting one column frame
+        # handles cases like df.plot(color='DodgerBlue')
+        axes = df.ix[:, [0]].plot(color='DodgerBlue', subplots=True)
+        self._check_colors(axes[0].lines, linecolors=['DodgerBlue'])
+
+        # single character style
+        axes = df.plot(style='r', subplots=True)
+        for ax in axes:
+            self._check_colors(ax.get_lines(), linecolors=['r'])
+        tm.close()
+
+        # list of styles
+        styles = list('rgcby')
+        axes = df.plot(style=styles, subplots=True)
+        for ax, c in zip(axes, styles):
+            self._check_colors(ax.get_lines(), linecolors=[c])
+        tm.close()
+
+    @slow
     def test_area_colors(self):
         from matplotlib import cm
         from matplotlib.collections import PolyCollection
@@ -2693,6 +2754,64 @@ class TestDataFramePlots(TestPlotBase):
         ax = df.plot(kind='kde', colormap=cm.jet)
         rgba_colors = lmap(cm.jet, np.linspace(0, 1, len(df)))
         self._check_colors(ax.get_lines(), linecolors=rgba_colors)
+
+    @slow
+    def test_kde_colors_and_styles_subplots(self):
+        tm._skip_if_no_scipy()
+        _skip_if_no_scipy_gaussian_kde()
+
+        from matplotlib import cm
+        default_colors = self.plt.rcParams.get('axes.color_cycle')
+
+        df = DataFrame(randn(5, 5))
+
+        axes = df.plot(kind='kde', subplots=True)
+        for ax, c in zip(axes, list(default_colors)):
+            self._check_colors(ax.get_lines(), linecolors=[c])
+        tm.close()
+
+        # single color char
+        axes = df.plot(kind='kde', color='k', subplots=True)
+        for ax in axes:
+            self._check_colors(ax.get_lines(), linecolors=['k'])
+        tm.close()
+
+        # single color str
+        axes = df.plot(kind='kde', color='red', subplots=True)
+        for ax in axes:
+            self._check_colors(ax.get_lines(), linecolors=['red'])
+        tm.close()
+
+        custom_colors = 'rgcby'
+        axes = df.plot(kind='kde', color=custom_colors, subplots=True)
+        for ax, c in zip(axes, list(custom_colors)):
+            self._check_colors(ax.get_lines(), linecolors=[c])
+        tm.close()
+
+        rgba_colors = lmap(cm.jet, np.linspace(0, 1, len(df)))
+        for cmap in ['jet', cm.jet]:
+            axes = df.plot(kind='kde', colormap=cmap, subplots=True)
+            for ax, c in zip(axes, rgba_colors):
+                self._check_colors(ax.get_lines(), linecolors=[c])
+            tm.close()
+
+        # make color a list if plotting one column frame
+        # handles cases like df.plot(color='DodgerBlue')
+        axes = df.ix[:, [0]].plot(kind='kde', color='DodgerBlue', subplots=True)
+        self._check_colors(axes[0].lines, linecolors=['DodgerBlue'])
+
+        # single character style
+        axes = df.plot(kind='kde', style='r', subplots=True)
+        for ax in axes:
+            self._check_colors(ax.get_lines(), linecolors=['r'])
+        tm.close()
+
+        # list of styles
+        styles = list('rgcby')
+        axes = df.plot(kind='kde', style=styles, subplots=True)
+        for ax, c in zip(axes, styles):
+            self._check_colors(ax.get_lines(), linecolors=[c])
+        tm.close()
 
     @slow
     def test_boxplot_colors(self):
