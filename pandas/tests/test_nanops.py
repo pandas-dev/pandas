820aa32bfbce43f01d029caa193706c9650999ec
Olga Botvinnik <olga.botvinnik@gmail.com>
2015-04-14 11:23:53 -0700

    add test

--- pandas/tests/test_nanops.py
+++ pandas/tests/test_nanops.py
@@ -583,6 +583,18 @@ class TestnanopsDataFrame(tm.TestCase):
         self.check_nancorr_nancov_1d(nanops.nancorr, targ0, targ1,
                                      method='spearman')
 
+
+    def test_nancorr_bicor(self):
+        targ0 = spearmanr(self.arr_float_2d, self.arr_float1_2d)[0]
+        targ1 = spearmanr(self.arr_float_2d.flat, self.arr_float1_2d.flat)[0]
+        self.check_nancorr_nancov_2d(nanops.nancorr, targ0, targ1,
+                                     method='bicor')
+
+        targ0 = spearmanr(self.arr_float_1d, self.arr_float1_1d)[0]
+        targ1 = spearmanr(self.arr_float_1d.flat, self.arr_float1_1d.flat)[0]
+        self.check_nancorr_nancov_1d(nanops.nancorr, targ0, targ1,
+                                     method='bicor')
+
     def test_nancov(self):
         targ0 = np.cov(self.arr_float_2d, self.arr_float1_2d)[0, 1]
         targ1 = np.cov(self.arr_float_2d.flat, self.arr_float1_2d.flat)[0, 1]
