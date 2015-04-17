899ed8e1106d9f7ed9eceec2607d2815484e53b2
Tom Ajamian <tc4484@gmail.com>
2015-04-17 11:27:53 -0400

    TST: adding sdist and install tests to the 2.6 regression - closes #9878

--- ci/script.sh
+++ ci/script.sh
@@ -23,6 +23,13 @@ else
     nosetests --exe -A "$NOSE_ARGS" pandas --with-xunit --xunit-file=/tmp/nosetests.xml
 fi
 
+if [ "$INSTALL_TEST" ]; then
+    echo "Starting installation test."
+    conda uninstall cython || exit 1
+    python "$TRAVIS_BUILD_DIR"/setup.py sdist --formats=zip,gztar || exit 1
+    pip install "$TRAVIS_BUILD_DIR"/dist/*tar.gz || exit 1
+    nosetests --exe -A "$NOSE_ARGS" pandas/tests/test_series.py --with-xunit --xunit-file=/tmp/nosetests_install.xml
+fi
 RET="$?"
 
 # wait until subprocesses finish (build_docs.sh)
