4dab44ce5cc730b69cfa786119406258b3d33920
ARF <none@none>
2015-04-23 23:32:54 +0200

    fix python3: use floor division

--- pandas/core/index.py
+++ pandas/core/index.py
@@ -3544,8 +3544,8 @@ class RangeIndex(Int64Index):
         
         # calculate parameters for the RangeIndex describing the intersection
         # disregarding the lower bounds
-        tmp_start = self.start + (other.start-self.start)*self.step/gcd*s
-        new_step = self.step * other.step / gcd
+        tmp_start = self.start + (other.start-self.start)*self.step//gcd*s
+        new_step = self.step * other.step // gcd
         assert new_step == int(new_step)
         new_index = RangeIndex(tmp_start, int_high, new_step)
 
@@ -3576,7 +3576,7 @@ class RangeIndex(Int64Index):
         t, old_t = 1, 0
         r, old_r = b, a
         while r:
-            quotient = old_r / r
+            quotient = old_r // r
             old_r, r = r, old_r - quotient * r
             old_s, s = s, old_s - quotient * s
             old_t, t = t, old_t - quotient * t
