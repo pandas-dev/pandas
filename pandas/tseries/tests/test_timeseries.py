61da6da44049c56840ded0c7153c47a42e8c308b
Jeff Reback <jeff@reback.net>
2015-05-19 11:34:14 -0400

    BUG: consistent datetime display format with < ms #10170

--- pandas/tseries/tests/test_timeseries.py
+++ pandas/tseries/tests/test_timeseries.py
@@ -791,7 +791,7 @@ class TestTimeSeries(tm.TestCase):
         series = Series([0, 1000, 2000, iNaT], dtype='M8[ns]')
 
         result = repr(series)
-        expected = ('0          1970-01-01 00:00:00\n'
+        expected = ('0   1970-01-01 00:00:00.000000\n'
                     '1   1970-01-01 00:00:00.000001\n'
                     '2   1970-01-01 00:00:00.000002\n'
                     '3                          NaT\n'
