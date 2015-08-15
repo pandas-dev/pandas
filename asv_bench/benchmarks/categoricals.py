0efe53f4bd9d788bada9373c5464bdec0dedd2f0
behzad nouri <behzadnouri@gmail.com>
2015-08-15 19:00:29 -0400

    PERF: uses bincount instead of hash table in categorical value counts

--- asv_bench/benchmarks/categoricals.py
+++ asv_bench/benchmarks/categoricals.py
@@ -9,3 +9,19 @@ class concat_categorical(object):
 
     def time_concat_categorical(self):
         concat([self.s, self.s])
+
+
+class categorical_value_counts(object):
+    goal_time = 1
+
+    def setup(self):
+        n = 500000
+        np.random.seed(2718281)
+        arr = ['s%04d' % i for i in np.random.randint(0, n // 10, size=n)]
+        self.ts = Series(arr).astype('category')
+
+    def time_value_counts(self):
+        self.ts.value_counts(dropna=False)
+
+    def time_value_counts_dropna(self):
+        self.ts.value_counts(dropna=True)
