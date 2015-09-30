7a2db2ba192b9db408e0e3b6d03f0abcc15cde23
Sheppard, Kevin <Kevin.Sheppard@treasury.gov>
2015-09-30 18:15:59 -0400

    FIX: Add boxing for to_numeric and add tests
    Added support for unboxing/boxing to to_numeric
    Added tests for to_numeric

--- pandas/tools/util.py
+++ pandas/tools/util.py
@@ -1,9 +1,9 @@
-import operator
-import warnings
+import numpy as np
+import pandas.lib as lib
+
+import pandas as pd
 from pandas.compat import reduce
 from pandas.core.index import Index
-import numpy as np
-from pandas import algos
 from pandas.core import common as com
 
 
@@ -50,47 +50,55 @@ def compose(*funcs):
     return reduce(_compose2, funcs)
 
 
-def to_numeric(arg, errors='raise', box=True, coerce=None):
+def to_numeric(arg, errors='raise'):
     """
     Convert argument to a numeric type.
 
     Parameters
     ----------
-    arg : string, datetime, array of strings (with possible NAs)
+    arg : list, tuple or array of objects, or Series
     errors : {'ignore', 'raise', 'coerce'}, default 'raise'
         - If 'raise', then invalid parsing will raise an exception
-        - If 'coerce', then invalid parsing will be set as NaT
+        - If 'coerce', then invalid parsing will be set as NaN
         - If 'ignore', then invalid parsing will return the input
-    box : boolean, default True
-        - If True returns a Series
-        - If False returns ndarray of values.
 
     Returns
     -------
     ret : numeric if parsing succeeded.
-        Return type depends on box
-
+        Return type depends on input.  Series if Series, otherwise ndarray
 
     Examples
     --------
-    Take separate series and convert to datetime
+    Take separate series and convert to numeric, coercing when told to
 
     >>> import pandas as pd
-    >>> df = pd.DataFrame(['1.0', '2', -3])
-    >>> pd.to_numeric(df)
-    >>> df = pd.DataFrame(['apple', '1.0', '2', -3])
-    >>> pd.to_numeric(df, errors='ignore')
-    >>> pd.to_numeric(df, errors='coerce')
+    >>> s = pd.Series(['1.0', '2', -3])
+    >>> pd.to_numeric(s)
+    >>> s = pd.Series(['apple', '1.0', '2', -3])
+    >>> pd.to_numeric(s, errors='ignore')
+    >>> pd.to_numeric(s, errors='coerce')
     """
-    #TODO: Fix examples
+
+    index = name = None
+    if isinstance(arg, pd.Series):
+        index, name = arg.index, arg.name
+    elif isinstance(arg, (list, tuple)):
+        arg = np.array(arg, dtype='O')
+
+    conv = arg
+    arg = com._ensure_object(arg)
 
     coerce_numeric = False if errors in ('ignore', 'raise') else True
-    if errors == 'ignore':
-        try:
-            values = lib.maybe_convert_numeric(arg,
-                                               set(),
-                                               coerce_numeric=coerce_numeric)
-            return values
-        except:
-            return arg
-    return lib.maybe_convert_numeric(arg, set(), coerce_numeric=coerce_numeric)
+
+    try:
+        conv = lib.maybe_convert_numeric(arg,
+                                         set(),
+                                         coerce_numeric=coerce_numeric)
+    except:
+        if errors == 'raise':
+            raise
+
+    if index is not None:
+        return pd.Series(conv, index=index, name=name)
+    else:
+        return conv
