f3cb8131fc96fb22680625b82f8dc18cb28676be
Kyle <khs26@cam.ac.uk>
2015-11-04 20:42:57 +0000

    PERF: Updated andrews_curves to use a Numpy array for samples DOC: Added some documentation to andrews_curves TST: Added a variable length test to TestDataFramePlots.test_andrews_curves

--- asv_bench/benchmarks/plotting.py
+++ asv_bench/benchmarks/plotting.py
@@ -2,9 +2,9 @@ from .pandas_vb_common import *
 try:
     from pandas import date_range
 except ImportError:
-
     def date_range(start=None, end=None, periods=None, freq=None):
         return DatetimeIndex(start, end, periods=periods, offset=freq)
+from pandas.tools.plotting import andrews_curves
 
 
 class plot_timeseries_period(object):
@@ -17,3 +17,16 @@ class plot_timeseries_period(object):
 
     def time_plot_timeseries_period(self):
         self.df.plot()
+
+class plot_andrews_curves(object):
+    goal_time = 0.6
+
+    def setup(self):
+        self.N = 500
+        self.M = 10
+	data_dict = {x: np.random.randn(self.N) for x in range(self.M)}
+	data_dict["Name"] = ["A"] * self.N
+        self.df = DataFrame(data_dict)
+
+    def time_plot_andrews_curves(self):
+        andrews_curves(self.df, "Name")
