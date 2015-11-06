9d7afa24b674db03559d883e5d960868a5eee742
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2015-11-06 11:45:14 +0100

    BUG: fix incorrect xticklabels when specifying xticks (GH11529)

--- pandas/tools/plotting.py
+++ pandas/tools/plotting.py
@@ -985,11 +985,11 @@ class MPLPlot(object):
         self._make_plot()
         self._add_table()
         self._make_legend()
+        self._adorn_subplots()
 
         for ax in self.axes:
             self._post_plot_logic_common(ax, self.data)
             self._post_plot_logic(ax, self.data)
-        self._adorn_subplots()
 
     def _args_adjust(self):
         pass
