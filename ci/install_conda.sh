dbf88854d53023680525afbc95611763b2a96d06
Jeff Reback <jeff@reback.net>
2015-11-13 13:58:28 -0500

    CI: remove matplotlib dev channel

--- ci/install_conda.sh
+++ ci/install_conda.sh
@@ -72,7 +72,6 @@ bash miniconda.sh -b -p $HOME/miniconda || exit 1
 
 conda config --set always_yes yes --set changeps1 no || exit 1
 conda update -q conda || exit 1
-conda config --add channels conda-forge || exit 1
 conda config --add channels http://conda.anaconda.org/pandas || exit 1
 conda config --set ssl_verify false || exit 1
 
