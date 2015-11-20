c76026e744acdd43749739c946b4f98040ab768b
Jeff Reback <jeff@reback.net>
2015-11-20 14:37:10 -0500

    add args to release_stats.sh

--- release_stats.sh
+++ release_stats.sh
@@ -1,15 +1,42 @@
-#!/bin/sh
+#!/bin/bash
 
-LAST=`git tag --sort v:refname | grep -v rc | tail -1`
-START=`git log $LAST.. --simplify-by-decoration --pretty="format:%ai %d" | tail -1 | gawk '{ print $1 }'`
-END=`git log $LAST.. --simplify-by-decoration --pretty="format:%ai %d" | head -1 | gawk '{ print $1 }'`
+while [[ $# > 1 ]]
+do
+key="$1"
 
-git log $LAST.. --format='%an#%s' | grep -v Merge > commits
+case $key in
+    --from)
+    FROM="$2"
+    shift # past argument
+    ;;
+    --to)
+    TO="$2"
+    shift # past argument
+    ;;
+    *)
+          # unknown option
+    ;;
+esac
+shift # past argument or value
+done
+
+if [ -z "$FROM" ]; then
+   FROM=`git tag --sort v:refname | grep -v rc | tail -1`
+fi
+
+if [ -z "$TO" ]; then
+   TO=""
+fi
+
+START=`git log $FROM.. --simplify-by-decoration --pretty="format:%ai %d" | tail -1 | gawk '{ print $1 }'`
+END=`git log $TO.. --simplify-by-decoration --pretty="format:%ai %d" | head -1 | gawk '{ print $1 }'`
+
+git log $FROM.. --format='%an#%s' | grep -v Merge > commits
 
 # Include a summary by contributor in release notes:
 # cat commits | gawk -F '#' '{ print "- " $1 }' | sort | uniq
 
-echo "Stats since <$LAST> [$START - $END]"
+echo "Stats since <$FROM> [$START - $END]"
 echo ""
 
 AUTHORS=`cat commits | gawk -F '#' '{ print $1 }' | sort | uniq | wc -l`
