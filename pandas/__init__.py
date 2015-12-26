a956340dd0d413f3e2f71713c38cbf46efab5f1c
Jeff Reback <jeff@reback.net>
2015-12-26 18:54:31 -0500

    ENH: add pd.test to enable nose test runnning from the imported session, #4327

--- pandas/__init__.py
+++ pandas/__init__.py
@@ -55,7 +55,12 @@ from pandas.tools.tile import cut, qcut
 from pandas.tools.util import to_numeric
 from pandas.core.reshape import melt
 from pandas.util.print_versions import show_versions
+
+# define the testing framework
 import pandas.util.testing
+from pandas.util.nosetester import NoseTester
+test = NoseTester().test
+del NoseTester
 
 # use the closest tagged version if possible
 from ._version import get_versions
