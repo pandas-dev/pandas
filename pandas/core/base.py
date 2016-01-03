3567227c907580c37ef3c0ad132da59f9bed0677
scls19fr <s.celles@gmail.com>
2016-01-03 17:38:08 +0100

    ENH: add is_unique attr to Series

--- pandas/core/base.py
+++ pandas/core/base.py
@@ -797,6 +797,17 @@ class IndexOpsMixin(object):
             n -= 1
         return n
 
+    @property
+    def is_unique(self):
+        """
+        Return if values in the object are unique or not.
+
+        Returns
+        -------
+        is_unique : bool 
+        """
+        return self.nunique() == len(self)
+
     def memory_usage(self, deep=False):
         """
         Memory usage of my values
