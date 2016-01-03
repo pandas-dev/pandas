b857336516f3d4a0daaa5221b54c84c72aeac3a4
Wes McKinney <wes@cloudera.com>
2016-01-03 11:16:47 -0800

    Add our own numpy.pxd so it can be modified as necessary

---
+++ src/pandas/compat.h
@@ -0,0 +1,9 @@
+// This file is a part of pandas. See LICENSE for details about reuse and
+// copyright holders
+
+#ifndef PANDAS_COMPAT_H
+#define PANDAS_COMPAT_H
+
+// #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
+
+#endif // PANDAS_COMPAT_H
