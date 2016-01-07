98fc5b647ea4474fbfcd3ad6b5f816d9917288d5
Skipper Seabold <jsseabold@gmail.com>
2016-01-07 12:25:41 -0600

    BUG: Let numpy typecodes pass through. Closes #11990 TST: Failing test for numpy typecode select
    BUG: Let numpy typecodes pass through.
    
    DOC: Document bug fix
    
    STY: Formatting

--- pandas/tests/test_frame.py
+++ pandas/tests/test_frame.py
@@ -16029,6 +16029,13 @@ starting,ending,measure
         with tm.assertRaisesRegexp(TypeError, 'data type.*not understood'):
             df.select_dtypes(['blargy, blarg, blarg'])
 
+    def test_select_dtypes_typecodes(self):
+        # GH 11990
+        df = mkdf(30, 3, data_gen_f=lambda x, y: np.random.random())
+        expected = df
+        FLOAT_TYPES = list(np.typecodes['AllFloat'])
+        assert_frame_equal(df.select_dtypes(FLOAT_TYPES), expected)
+
     def test_assign(self):
         df = DataFrame({'A': [1, 2, 3], 'B': [4, 5, 6]})
         original = df.copy()
