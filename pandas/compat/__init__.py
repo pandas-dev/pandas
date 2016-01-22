87d9e56741e8179c852bd46f5257c13fbd76a4a1
ranarag <anu15roy@gmail.com>
2016-01-22 22:45:03 +0530

    added inspect

--- pandas/compat/__init__.py
+++ pandas/compat/__init__.py
@@ -33,6 +33,7 @@ import sys
 import types
 from unicodedata import east_asian_width
 import struct
+import inspect
 
 PY2 = sys.version_info[0] == 2
 PY3 = (sys.version_info[0] >= 3)
@@ -66,6 +67,9 @@ if PY3:
 
     def bytes_to_str(b, encoding=None):
         return b.decode(encoding or 'utf-8')
+    
+    def signature(f):
+        return list(inspect.signature(f).parameters.keys())
 
     # have to explicitly put builtins into the namespace
     range = range
@@ -102,6 +106,9 @@ else:
 
     def bytes_to_str(b, encoding='ascii'):
         return b
+    
+    def signature(f):
+        return inspect.getargspec(f).args
 
     # import iterator versions of these functions
     range = xrange
