3152bdca818f44351ca1c8b63cb26fcfeae3cce7
Jeff Reback <jeff@reback.net>
2016-01-26 17:00:09 -0500

    BUG: getitem and a series with a non-ndarray values
    closes #12089
    closes #12151

--- pandas/index.pyx
+++ pandas/index.pyx
@@ -100,7 +100,7 @@ cdef class IndexEngine:
         hash(val)
         return val in self.mapping
 
-    cpdef get_value(self, ndarray arr, object key):
+    cpdef get_value(self, ndarray arr, object key, object tz=None):
         '''
         arr : 1-dimensional ndarray
         '''
@@ -113,7 +113,7 @@ cdef class IndexEngine:
             return arr[loc]
         else:
             if arr.descr.type_num == NPY_DATETIME:
-                return Timestamp(util.get_value_at(arr, loc))
+                return Timestamp(util.get_value_at(arr, loc), tz=tz)
             elif arr.descr.type_num == NPY_TIMEDELTA:
                 return Timedelta(util.get_value_at(arr, loc))
             return util.get_value_at(arr, loc)
