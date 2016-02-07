0b06d7614464a1c61be1439a232e7554aa95abee
Pietro Battiston <me@pietrobattiston.it>
2016-02-07 11:25:37 +0100

    BUG: Ensure data_columns is always a list (i.e. min_itemsize can extend it)
    closes #10381

--- pandas/io/pytables.py
+++ pandas/io/pytables.py
@@ -3250,7 +3250,7 @@ class Table(Fixed):
         # evaluate the passed data_columns, True == use all columns
         # take only valide axis labels
         if data_columns is True:
-            data_columns = axis_labels
+            data_columns = list(axis_labels)
         elif data_columns is None:
             data_columns = []
 
@@ -4087,7 +4087,7 @@ class AppendableSeriesTable(AppendableFrameTable):
             obj = DataFrame({name: obj}, index=obj.index)
             obj.columns = [name]
         return super(AppendableSeriesTable, self).write(
-            obj=obj, data_columns=obj.columns, **kwargs)
+            obj=obj, data_columns=list(obj.columns), **kwargs)
 
     def read(self, columns=None, **kwargs):
 
@@ -4188,7 +4188,7 @@ class AppendableMultiFrameTable(AppendableFrameTable):
         if data_columns is None:
             data_columns = []
         elif data_columns is True:
-            data_columns = obj.columns[:]
+            data_columns = list(obj.columns[:])
         obj, self.levels = self.validate_multiindex(obj)
         for n in self.levels:
             if n not in data_columns:
