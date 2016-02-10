0bdf6fd699991827fd4e56a474efe40a2ad7f8ca
Bran Yang <snowolfy@163.com>
2016-02-10 12:26:26 -0500

    Fix #12169 - Resample category data with timedelta index
    closes #12169
    
    Author: Bran Yang <snowolfy@163.com>
    
    Closes #12271 from BranYang/issue12169 and squashes the following commits:
    
    4a5605f [Bran Yang] add tests to Series/test_constructors; and update whatsnew
    7cf1be9 [Bran Yang] Fix #12169 - Resample category data with timedelta index

--- pandas/tests/series/test_constructors.py
+++ pandas/tests/series/test_constructors.py
@@ -519,6 +519,24 @@ class TestSeriesConstructors(TestData, tm.TestCase):
         ser = ser.reindex(index=expected.index)
         check(ser, expected)
 
+    def test_constructor_dict_timedelta_index(self):
+        # GH #12169 : Resample category data with timedelta index
+        # construct Series from dict as data and TimedeltaIndex as index
+        # will result NaN in result Series data
+        expected = Series(
+            data=['A', 'B', 'C'],
+            index=pd.to_timedelta([0, 10, 20], unit='s')
+        )
+
+        result = Series(
+            data={pd.to_timedelta(0, unit='s'): 'A',
+                  pd.to_timedelta(10, unit='s'): 'B',
+                  pd.to_timedelta(20, unit='s'): 'C'},
+            index=pd.to_timedelta([0, 10, 20], unit='s')
+        )
+        # this should work
+        assert_series_equal(result, expected)
+
     def test_constructor_subclass_dict(self):
         data = tm.TestSubDict((x, 10.0 * x) for x in range(10))
         series = Series(data)
