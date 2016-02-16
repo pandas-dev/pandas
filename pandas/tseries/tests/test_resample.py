99c97f4a598370167918d44d03eda90e3d705584
Jeff Reback <jeff@reback.net>
2016-02-16 10:59:55 -0500

    TST: confirming tests for #12348
    xref #12348
    
    Author: Jeff Reback <jeff@reback.net>
    
    Closes #12349 from jreback/resample_tests and squashes the following commits:
    
    545da27 [Jeff Reback] TST: confirming tests for #12348

--- pandas/tseries/tests/test_resample.py
+++ pandas/tseries/tests/test_resample.py
@@ -938,6 +938,18 @@ class TestResample(tm.TestCase):
         b = s.loc[:'4-14-2000'].resample('30T').ohlc()
         self.assertIsInstance(b, DataFrame)
 
+        # GH12348
+        # raising on odd period
+        rng = date_range('2013-12-30', '2014-01-07')
+        index = rng.drop([Timestamp('2014-01-01'),
+                          Timestamp('2013-12-31'),
+                          Timestamp('2014-01-04'),
+                          Timestamp('2014-01-05')])
+        df = DataFrame(data=np.arange(len(index)), index=index)
+        result = df.resample('B').mean()
+        expected = df.reindex(index=date_range(rng[0], rng[-1], freq='B'))
+        assert_frame_equal(result, expected)
+
     def test_resample_ohlc_dataframe(self):
         df = (
             pd.DataFrame({
