0ac1bf8dc7f8bc67ab463083303ffa47bb489cbf
Marco Farrugia <Marco.Farrugia@Twosigma.com>
2016-02-26 12:46:42 -0500

    MAINT: Nicely inform users if they're missing hard dependencies.
    closes #12176
    closes #12468

--- pandas/__init__.py
+++ pandas/__init__.py
@@ -4,8 +4,21 @@
 
 __docformat__ = 'restructuredtext'
 
+# Let users know if they're missing any of our hard dependencies
+hard_dependencies = ("numpy", "pytz", "dateutil")
+missing_dependencies = []
+
+for dependency in hard_dependencies:
+    try:
+        __import__(dependency)
+    except ImportError as e:
+        missing_dependencies.append(dependency)
+
+if missing_dependencies:
+    raise ImportError("Missing required dependencies {0}".format(missing_dependencies))
+
+
 # numpy compat
-import numpy as np
 from pandas.compat.numpy_compat import *
 
 try:
@@ -49,4 +62,3 @@ from ._version import get_versions
 v = get_versions()
 __version__ = v.get('closest-tag',v['version'])
 del get_versions, v
-
