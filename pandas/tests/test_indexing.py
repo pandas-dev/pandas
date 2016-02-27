f4917370fdce4e3bde421290e30ebb4a7d23a489
Jeff Reback <jeff@reback.net>
2016-02-27 11:38:18 -0500

    TST/DOC: some test cleanups

--- pandas/tests/test_indexing.py
+++ pandas/tests/test_indexing.py
@@ -699,6 +699,29 @@ class TestIndexing(tm.TestCase):
         expected = Series([0, 1, 0], index=[4, 5, 6])
         assert_series_equal(s, expected)
 
+    def test_loc_setitem_slice(self):
+        # GH10503
+
+        # assigning the same type should not change the type
+        df1 = DataFrame({'a': [0, 1, 1],
+                         'b': Series([100, 200, 300], dtype='uint32')})
+        ix = df1['a'] == 1
+        newb1 = df1.loc[ix, 'b'] + 1
+        df1.loc[ix, 'b'] = newb1
+        expected = DataFrame({'a': [0, 1, 1],
+                              'b': Series([100, 201, 301], dtype='uint32')})
+        assert_frame_equal(df1, expected)
+
+        # assigning a new type should get the inferred type
+        df2 = DataFrame({'a': [0, 1, 1], 'b': [100, 200, 300]},
+                        dtype='uint64')
+        ix = df1['a'] == 1
+        newb2 = df2.loc[ix, 'b']
+        df1.loc[ix, 'b'] = newb2
+        expected = DataFrame({'a': [0, 1, 1], 'b': [100, 200, 300]},
+                             dtype='uint64')
+        assert_frame_equal(df2, expected)
+
     def test_ix_loc_setitem_consistency(self):
 
         # GH 5771
