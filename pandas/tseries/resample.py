34eedf0874c01df20ecee2ae3ec3e6344f17b74b
Maximilian Roos <maximilian@sixtycapital.com>
2016-04-03 16:26:24 -0400

    BUG: Retain name in PeriodIndex resample
    closes #12769
    
    Author: Maximilian Roos <maximilian@sixtycapital.com>
    
    Closes #12771 from MaximilianR/retain-period-index-name-on-resample and squashes the following commits:
    
    7b17a7e [Maximilian Roos] name retained in PeriodIndex resample

--- pandas/tseries/resample.py
+++ pandas/tseries/resample.py
@@ -672,16 +672,18 @@ class PeriodIndexResampler(DatetimeIndexResampler):
     def _get_new_index(self):
         """ return our new index """
         ax = self.ax
+        ax_attrs = ax._get_attributes_dict()
+        ax_attrs['freq'] = self.freq
         obj = self._selected_obj
 
         if len(ax) == 0:
-            new_index = PeriodIndex(data=[], freq=self.freq)
+            new_index = PeriodIndex(data=[], **ax_attrs)
             return obj.reindex(new_index)
 
         start = ax[0].asfreq(self.freq, how=self.convention)
         end = ax[-1].asfreq(self.freq, how='end')
 
-        return period_range(start, end, freq=self.freq)
+        return period_range(start, end, **ax_attrs)
 
     def _downsample(self, how, **kwargs):
         """
