32ced80e3c3108da3e3ab89f1b961ee54df33323
sinhrks <sinhrks@gmail.com>
2016-04-30 11:58:13 +0900

    BUG: subclassed .align returns normal DataFrame

--- pandas/tests/frame/test_axis_select_reindex.py
+++ pandas/tests/frame/test_axis_select_reindex.py
@@ -597,6 +597,27 @@ class TestDataFrameSelectReindex(tm.TestCase, TestData):
         assert_frame_equal(expr, res1r)
         assert_frame_equal(expr, res2l)
 
+    def test_align_series_combinations(self):
+        df = pd.DataFrame({'a': [1, 3, 5],
+                           'b': [1, 3, 5]}, index=list('ACE'))
+        s = pd.Series([1, 2, 4], index=list('ABD'), name='x')
+
+        # frame + series
+        res1, res2 = df.align(s, axis=0)
+        exp1 = pd.DataFrame({'a': [1, np.nan, 3, np.nan, 5],
+                             'b': [1, np.nan, 3, np.nan, 5]},
+                            index=list('ABCDE'))
+        exp2 = pd.Series([1, 2, np.nan, 4, np.nan],
+                         index=list('ABCDE'), name='x')
+
+        tm.assert_frame_equal(res1, exp1)
+        tm.assert_series_equal(res2, exp2)
+
+        # series + frame
+        res1, res2 = s.align(df)
+        tm.assert_series_equal(res1, exp2)
+        tm.assert_frame_equal(res2, exp1)
+
     def test_filter(self):
         # items
         filtered = self.frame.filter(['A', 'B', 'E'])
