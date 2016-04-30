1296ab39d32acaf2c77ed0c185fafe4ebcfedcb3
Pawel Kordek <pawel.kordek@gmail.com>
2016-04-30 19:07:15 +0200

    BUG: Return non-dates in xls date cells directly
    closes #10001
    closes #13042
    
    If there is a date column in excel in which there are cells with some big
    integers, that during parsing to date cause int/long overflow, issue a
    warning and convert the value to int or float.

--- pandas/io/tests/test_excel.py
+++ pandas/io/tests/test_excel.py
@@ -481,6 +481,16 @@ class ReadingTestsBase(SharedItems):
             tm.assert_frame_equal(xlsdf_no_head, refdf)
             tm.assert_frame_equal(xlsdf_with_head, refdf)
 
+    def test_date_conversion_overflow(self):
+        # GH 10001 : pandas.ExcelFile ignore parse_dates=False
+        expected = pd.DataFrame([[pd.Timestamp('2016-03-12'), 'Marc Johnson'],
+                                 [pd.Timestamp('2016-03-16'), 'Jack Black'],
+                                 [1e+20, 'Timothy Brown']],
+                                columns=['DateColWithBigInt', 'StringCol'])
+
+        result = self.get_exceldf('testdateoverflow')
+        tm.assert_frame_equal(result, expected)
+
 
 class XlrdTests(ReadingTestsBase):
     """
