005fbc6631a76bf5e83e9154ee1b02798a4270b7
Thomas A Caswell <tcaswell@gmail.com>
2016-05-10 19:47:02 -0400

    BUG: mpl fix to AutoDatFromatter to fix second/us-second formatters
    revert part of #11770
    xref: https://github.com/matplotlib/matplotlib/issues/6365
    
    closes #13131
    
    The mistake in #11770 was missing that pandas had a 1/us not 1/s
    scaled bucket.

--- pandas/tseries/converter.py
+++ pandas/tseries/converter.py
@@ -23,6 +23,24 @@ import pandas.tseries.frequencies as frequencies
 from pandas.tseries.frequencies import FreqGroup
 from pandas.tseries.period import Period, PeriodIndex
 
+# constants
+HOURS_PER_DAY = 24.
+MIN_PER_HOUR = 60.
+SEC_PER_MIN = 60.
+
+SEC_PER_HOUR = SEC_PER_MIN * MIN_PER_HOUR
+SEC_PER_DAY = SEC_PER_HOUR * HOURS_PER_DAY
+
+MUSEC_PER_DAY = 1e6 * SEC_PER_DAY
+
+
+def _mpl_le_2_0_0():
+    try:
+        import matplotlib
+        return matplotlib.compare_versions('2.0.0', matplotlib.__version__)
+    except ImportError:
+        return False
+
 
 def register():
     units.registry[lib.Timestamp] = DatetimeConverter()
@@ -220,6 +238,13 @@ class PandasAutoDateFormatter(dates.AutoDateFormatter):
         # matplotlib.dates._UTC has no _utcoffset called by pandas
         if self._tz is dates.UTC:
             self._tz._utcoffset = self._tz.utcoffset(None)
+
+        # For mpl > 2.0 the format strings are controlled via rcparams
+        # so do not mess with them.  For mpl < 2.0 change the second
+        # break point and add a musec break point
+        if _mpl_le_2_0_0():
+            self.scaled[1. / SEC_PER_DAY] = '%H:%M:%S'
+            self.scaled[1. / MUSEC_PER_DAY] = '%H:%M:%S.%f'
 
 
 class PandasAutoDateLocator(dates.AutoDateLocator):
