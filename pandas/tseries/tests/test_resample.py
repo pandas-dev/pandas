4b501497a88c118c2bfdcdbc4a5b216b68b1b88c
Alex Alekseyev <alex.alekseyev@twosigma.com>
2016-05-18 18:08:39 -0400

    TST: Test resampling with NaT
    closes #13020
    
    Author: Alex Alekseyev <alex.alekseyev@twosigma.com>
    
    Closes #13164 from evectant/issue-13020 and squashes the following commits:
    
    c7e6f5f [Alex Alekseyev] TST: Test resampling with NaT

--- pandas/tseries/tests/test_resample.py
+++ pandas/tseries/tests/test_resample.py
@@ -1846,6 +1846,32 @@ class TestDatetimeIndex(Base, tm.TestCase):
                                        freq='D', tz='Europe/Paris')),
             'D Frequency')
 
+    def test_resample_with_nat(self):
+        # GH 13020
+        index = DatetimeIndex([pd.NaT,
+                               '1970-01-01 00:00:00',
+                               pd.NaT,
+                               '1970-01-01 00:00:01',
+                               '1970-01-01 00:00:02'])
+        frame = DataFrame([2, 3, 5, 7, 11], index=index)
+
+        index_1s = DatetimeIndex(['1970-01-01 00:00:00',
+                                  '1970-01-01 00:00:01',
+                                  '1970-01-01 00:00:02'])
+        frame_1s = DataFrame([3, 7, 11], index=index_1s)
+        assert_frame_equal(frame.resample('1s').mean(), frame_1s)
+
+        index_2s = DatetimeIndex(['1970-01-01 00:00:00',
+                                  '1970-01-01 00:00:02'])
+        frame_2s = DataFrame([5, 11], index=index_2s)
+        assert_frame_equal(frame.resample('2s').mean(), frame_2s)
+
+        index_3s = DatetimeIndex(['1970-01-01 00:00:00'])
+        frame_3s = DataFrame([7], index=index_3s)
+        assert_frame_equal(frame.resample('3s').mean(), frame_3s)
+
+        assert_frame_equal(frame.resample('60s').mean(), frame_3s)
+
 
 class TestPeriodIndex(Base, tm.TestCase):
     _multiprocess_can_split_ = True
