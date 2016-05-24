a7ccaee082066960cdc4844406753ebeb12dbc6a
Neil Parley <mail@neilparley.co.uk>
2016-05-24 14:12:56 +0100

    Print version info during test script

--- ci/script.sh
+++ ci/script.sh
@@ -17,6 +17,10 @@ if [ -n "$LOCALE_OVERRIDE" ]; then
     python -c "$pycmd"
 fi
 
+echo "Version info:"
+pycmd="import pandas; pandas.show_versions();"
+python -c "$pycmd"
+
 if [ "$BUILD_TEST" ]; then
     echo "We are not running nosetests as this is simply a build test."
 elif [ "$COVERAGE" ]; then
