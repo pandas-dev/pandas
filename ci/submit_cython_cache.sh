9158c0b5304e8347c7a67462133ecb5a52e06695
Neil Parley <mail@neilparley.co.uk>
2016-05-31 22:48:05 +0100

    Bug fix

--- ci/submit_cython_cache.sh
+++ ci/submit_cython_cache.sh
@@ -3,7 +3,7 @@
 CACHE_File="$HOME/.cache/cython_files.tar"
 home_dir=$(pwd)
 ls "$TRAVIS_BUILD_DIR/pandas/"
-tar cf $CACHE_DIR/cython_files.tar \
+tar cf "$CACHE_File" \
 "$TRAVIS_BUILD_DIR"/pandas/{index,algos,lib,tslib,parser,hashtable}.c \
 "$TRAVIS_BUILD_DIR"/pandas/src/{sparse,testing,period,}.c \
 "$TRAVIS_BUILD_DIR"/pandas/msgpack/{_packer,_unpacker,period_helper}.cpp
