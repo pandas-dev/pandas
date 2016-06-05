56cc93c782a07e5b244fe113cd09e13dbe5a0e73
Neil Parley <mail@neilparley.co.uk>
2016-06-05 11:25:34 +0100

    Add cython files to the cache even if we have not used the cache. Therefore keeping cache up to date for if it is turned on in the future. Add comments to the travis file about use_cache.

--- ci/submit_cython_cache.sh
+++ ci/submit_cython_cache.sh
@@ -1,13 +1,11 @@
 #!/bin/bash
 
-if [ "$USE_CACHE" ]
-then
-    CACHE_File="$HOME/.cache/cython_files.tar"
-    home_dir=$(pwd)
-    ls "$TRAVIS_BUILD_DIR/pandas/"
-    tar cf "$CACHE_File" \
-    "$TRAVIS_BUILD_DIR"/pandas/{index,algos,lib,tslib,parser,hashtable}.c \
-    "$TRAVIS_BUILD_DIR"/pandas/src/{sparse,testing,period,period_helper}.c \
-    "$TRAVIS_BUILD_DIR"/pandas/msgpack/{_packer,_unpacker}.cpp
-fi
+CACHE_File="$HOME/.cache/cython_files.tar"
+home_dir=$(pwd)
+ls "$TRAVIS_BUILD_DIR/pandas/"
+tar cf "$CACHE_File" \
+"$TRAVIS_BUILD_DIR"/pandas/{index,algos,lib,tslib,parser,hashtable}.c \
+"$TRAVIS_BUILD_DIR"/pandas/src/{sparse,testing,period,period_helper}.c \
+"$TRAVIS_BUILD_DIR"/pandas/msgpack/{_packer,_unpacker}.cpp
+
 exit 0
