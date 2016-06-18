b4b09bfcb025bb1b6794cc33886eeee9bd6348c7
cmazzullo <chris.mazzullo@gmail.com>
2016-06-17 20:08:33 -0400

    Compared whole dataframes in test_pivot_table_margins_name_with_aggfunc_list

--- pandas/tools/tests/test_pivot.py
+++ pandas/tools/tests/test_pivot.py
@@ -781,10 +781,19 @@ class TestPivotTable(tm.TestCase):
 
     def test_pivot_table_margins_name_with_aggfunc_list(self):
         # GH 13354
-        margins_name = 'Weekly'
-        table = self.data.pivot_table(index='A', columns='B', margins=True,
-                              margins_name=margins_name, aggfunc=[np.mean, max])
-        self.assertEqual(table[:][:].index[2], margins_name)
+        margins_name='Weekly'
+        costs = pd.DataFrame({'item':['bacon','cheese','bacon','cheese'],
+                              'cost':[2.5,4.5,3.2,3.3],
+                             'day':['M','M','T','T']})
+        table = costs.pivot_table(index="item",columns="day",margins=True,
+                                margins_name=margins_name,aggfunc=[np.mean,max])
+        ix = pd.Index(['bacon', 'cheese', margins_name], dtype='object', name='item')
+        tups = [('mean', 'cost', 'M'), ('mean', 'cost', 'T'),
+                ('mean', 'cost', margins_name), ('max', 'cost', 'M'),
+                ('max', 'cost', 'T'), ('max', 'cost', margins_name)]
+        cols = pd.MultiIndex.from_tuples(tups, names=[None, None, 'day'])
+        expected = pd.DataFrame(table.values, index=ix, columns=cols)
+        tm.assert_frame_equal(table, expected)
 
 
 class TestCrosstab(tm.TestCase):
