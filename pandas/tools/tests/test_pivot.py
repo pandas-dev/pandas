6017e252e81e2754e21b04e71d6c4226bf410016
cmazzullo <chris.mazzullo@gmail.com>
2016-06-18 00:51:40 -0400

    Fixed up whitespace for pep8

--- pandas/tools/tests/test_pivot.py
+++ pandas/tools/tests/test_pivot.py
@@ -781,13 +781,19 @@ class TestPivotTable(tm.TestCase):
 
     def test_pivot_table_margins_name_with_aggfunc_list(self):
         # GH 13354
-        margins_name='Weekly'
-        costs = pd.DataFrame({'item':['bacon','cheese','bacon','cheese'],
-                              'cost':[2.5,4.5,3.2,3.3],
-                             'day':['M','M','T','T']})
-        table = costs.pivot_table(index="item",columns="day",margins=True,
-                                margins_name=margins_name,aggfunc=[np.mean,max])
-        ix = pd.Index(['bacon', 'cheese', margins_name], dtype='object', name='item')
+        margins_name = 'Weekly'
+        costs = pd.DataFrame(
+            {'item': ['bacon', 'cheese', 'bacon', 'cheese'],
+             'cost': [2.5, 4.5, 3.2, 3.3],
+             'day': ['M', 'M', 'T', 'T']}
+        )
+        table = costs.pivot_table(
+            index="item", columns="day", margins=True,
+            margins_name=margins_name, aggfunc=[np.mean, max]
+        )
+        ix = pd.Index(
+            ['bacon', 'cheese', margins_name], dtype='object', name='item'
+        )
         tups = [('mean', 'cost', 'M'), ('mean', 'cost', 'T'),
                 ('mean', 'cost', margins_name), ('max', 'cost', 'M'),
                 ('max', 'cost', 'T'), ('max', 'cost', margins_name)]
