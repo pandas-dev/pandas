f607a6b9f93dc92b008b8f6cf491697d086e298b
Neil Parley <mail@neilparley.co.uk>
2016-06-19 21:33:34 +0100

    Bug fix

--- ci/check_cache.sh
+++ ci/check_cache.sh
@@ -1,6 +1,6 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-ci_changes=$(git diff HEAD~1 --numstat | grep -E "ci/"| wc -l)
+ci_changes=$(git diff HEAD~2 --numstat | grep -E "ci/"| wc -l)
 
 MINICONDA_DIR="$HOME/miniconda"
 CACHE_DIR="$HOME/.cache"
@@ -8,7 +8,7 @@ CCACHE_DIR="$HOME/.ccache"
 
 if [ $ci_changes -ne 0 ]
 then
-    echo "CI has been changed in the last commit deleting all caches"
+    echo "CI has been changed in the last two commits deleting all caches"
     rm -rf "$MINICONDA_DIR"
     rm -rf "$CACHE_DIR"
     rm -rf "$CCACHE_DIR"
