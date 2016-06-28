6cc3d6e2b3dc2fc3d88ebe658f89dcef5a4fe380
Neil Parley <mail@neilparley.co.uk>
2016-06-28 14:15:56 +0100

    Diff python pyx files as a test if they have changed

--- ci/prep_cython_cache.sh
+++ ci/prep_cython_cache.sh
@@ -1,31 +1,50 @@
 #!/bin/bash
 
 ls "$HOME/.cache/"
+
+PYX_CACHE_DIR="$HOME/.cache/pyxfiles"
+pyx_file_list=`find ${TRAVIS_BUILD_DIR} -name "*.pyx"`
 CACHE_File="$HOME/.cache/cython_files.tar"
 
 clear_cache=0
 home_dir=$(pwd)
 
-if [ -f "$CACHE_File" ] && [ "$USE_CACHE" ]; then
+if [ -f "$CACHE_File" ] && [ "$USE_CACHE" ] && [ -d "$PYX_CACHE_DIR" ]; then
 
-    echo "Cache available"
-    clear_cache=1
-    # did the last commit change cython files?
-    # go back 2 commits
+    echo "Cache available - checking pyx diff"
+
+    for i in ${pyx_file_list}
+    do
+            diff=`diff -u $i $PYX_CACHE_DIR${i}`
+            if [[ $? -ne 0 ]]
+            then
+                    echo "${i##*/} can't be diffed; probably not in cache"
+                    clear_cache=1
+            fi
+            if [[ ! -z $diff ]]
+            then
+                    echo "${i##*/} has changed:"
+                    echo $diff
+                    clear_cache=1
+            fi
+    done
+
     if [ "$TRAVIS_PULL_REQUEST" == "false" ]
     then
-        echo "Not a PR: checking for cython files changes from last 2 commits"
-        git diff HEAD~2 --numstat | grep -E "pyx|pxd"
-        retval=$(git diff HEAD~2 --numstat | grep -E "pyx|pxd"| wc -l)
+        echo "Not a PR"
+        # Uncomment next 2 lines to turn off cython caching not in a PR
+        # echo "Non PR cython caching is disabled"
+        # clear_cache=1
     else
-        echo "PR: checking for any cython file changes from last 5 commits"
-        git diff PR_HEAD~5 --numstat | grep -E "pyx|pxd"
-        retval=$(git diff PR_HEAD~5 --numstat | grep -E "pyx|pxd"| wc -l)
+        echo "In a PR"
+        # Uncomment next 2 lines to turn off cython caching in a PR
+        # echo "PR cython caching is disabled"
+        # clear_cache=1
     fi
-    echo "number of cython files changed: $retval"
+
 fi
 
-if [ $clear_cache -eq 1 ] && [ $retval -eq 0 ] && [ "$USE_CACHE" ]
+if [ $clear_cache -eq 1 ] && [ "$USE_CACHE" ]
 then
     # nope, reuse cython files
     echo "Will reuse cached cython file"
