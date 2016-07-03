1b13cc1c9f75e502f8f6c8b396182b5b7b589dd2
Neil Parley <mail@neilparley.co.uk>
2016-07-03 19:24:13 -0400

    Cython cache diff compare
    As talked about in #13425 with @gfyoung and @jreback this PR does
    cython caching by comparing the pyx files and not relying on the git
    history.
    
    Author: Neil Parley <mail@neilparley.co.uk>
    
    Closes #13526 from nparley/pyx-diff and squashes the following commits:
    
    8b4ad0b [Neil Parley] Remove test commit
    2dc2ce2 [Neil Parley] Merge branch 'upstream' into pyx-diff
    81cbb7b [Neil Parley] Reset pyx changes
    79fe0f0 [Neil Parley] Remove do_not_merge
    dca6e61 [Neil Parley] Test commit
    30bd9da [Neil Parley] Check for the case where less cython file has been deleted
    aaf4dd2 [Neil Parley] Merge branch 'upstream' into pyx-diff
    6cc3d6e [Neil Parley] Diff python pyx files as a test if they have changed

--- ci/submit_cython_cache.sh
+++ ci/submit_cython_cache.sh
@@ -1,17 +1,23 @@
 #!/bin/bash
 
 CACHE_File="$HOME/.cache/cython_files.tar"
+PYX_CACHE_DIR="$HOME/.cache/pyxfiles"
+pyx_file_list=`find ${TRAVIS_BUILD_DIR} -name "*.pyx"`
+
 rm -rf $CACHE_File
+rm -rf $PYX_CACHE_DIR
 
 home_dir=$(pwd)
 
-pyx_files=`find ${TRAVIS_BUILD_DIR} -name "*.pyx"`
+mkdir $PYX_CACHE_DIR
+rsync -Rv $pyx_file_list $PYX_CACHE_DIR
+
 echo "pyx files:"
-echo $pyx_files
+echo $pyx_file_list
 
 tar cf ${CACHE_File} --files-from /dev/null
 
-for i in ${pyx_files}
+for i in ${pyx_file_list}
 do
         f=${i%.pyx}
         ls $f.{c,cpp} | tar rf  ${CACHE_File} -T -
