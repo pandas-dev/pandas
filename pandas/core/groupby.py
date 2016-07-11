2f7fdd07eb0925016a28cf9ff324e351eac0c4df
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2016-07-11 17:02:24 +0200

    BUG: groupby apply on selected columns yielding scalar (GH13568) (#13585)
    closes #13568

--- pandas/core/groupby.py
+++ pandas/core/groupby.py
@@ -3403,11 +3403,14 @@ class NDFrameGroupBy(GroupBy):
 
                 return self._reindex_output(result)
 
+            # values are not series or array-like but scalars
             else:
                 # only coerce dates if we find at least 1 datetime
                 coerce = True if any([isinstance(x, Timestamp)
                                       for x in values]) else False
-                return (Series(values, index=key_index, name=self.name)
+                # self.name not passed through to Series as the result
+                # should not take the name of original selection of columns
+                return (Series(values, index=key_index)
                         ._convert(datetime=True,
                                   coerce=coerce))
 
