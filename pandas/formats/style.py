fa5ecf1e9906715ab6628e575f1bedaab83fb0ce
Jon Renner <rennerjc@gmail.com>
2016-07-14 11:14:55 -0500

    better error message for non-unique columns
    I ran into this error with my code and the error message didn't help me figure out I was dealing with duplicate columns that couldn't be handled. I think this is more helpful

--- pandas/formats/style.py
+++ pandas/formats/style.py
@@ -137,8 +137,10 @@ class Styler(object):
             raise TypeError("``data`` must be a Series or DataFrame")
         if data.ndim == 1:
             data = data.to_frame()
-        if not data.index.is_unique or not data.columns.is_unique:
+        if not data.index.is_unique:
             raise ValueError("style is not supported for non-unique indicies.")
+        if not data.columns.is_unique:
+            raise ValueError("style is not supported for non-unique columns.")
 
         self.data = data
         self.index = data.index
