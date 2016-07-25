cc23fc93ed85a4a7dc2214b554df312c18ba2ed9
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2016-07-25 21:58:12 +0200

    Clean-up timeseries/timedelta benchmarks

--- asv_bench/benchmarks/timedelta.py
+++ asv_bench/benchmarks/timedelta.py
@@ -2,36 +2,24 @@ from .pandas_vb_common import *
 from pandas import to_timedelta, Timestamp
 
 
-class timedelta_convert_int(object):
+class Timedelta(object):
     goal_time = 0.2
 
     def setup(self):
         self.arr = np.random.randint(0, 1000, size=10000)
+        self.arr2 = ['{0} days'.format(i) for i in self.arr]
 
-    def time_timedelta_convert_int(self):
+        self.arr3 = np.random.randint(0, 60, size=10000)
+        self.arr3 = ['00:00:{0:02d}'.format(i) for i in self.arr3]
+
+    def time_convert_int(self):
         to_timedelta(self.arr, unit='s')
 
+    def time_convert_string(self):
+        to_timedelta(self.arr2)
 
-class timedelta_convert_string(object):
-    goal_time = 0.2
-
-    def setup(self):
-        self.arr = np.random.randint(0, 1000, size=10000)
-        self.arr = ['{0} days'.format(i) for i in self.arr]
-
-    def time_timedelta_convert_string(self):
-        to_timedelta(self.arr)
-
-
-class timedelta_convert_string_seconds(object):
-    goal_time = 0.2
-
-    def setup(self):
-        self.arr = np.random.randint(0, 60, size=10000)
-        self.arr = ['00:00:{0:02d}'.format(i) for i in self.arr]
-
-    def time_timedelta_convert_string_seconds(self):
-        to_timedelta(self.arr)
+    def time_convert_string_seconds(self):
+        to_timedelta(self.arr3)
 
 
 class timedelta_convert_bad_parse(object):
