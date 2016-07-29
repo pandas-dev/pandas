94771fed550f72ee3005e8946fcf9ac22660b31e
Ivan Nazarov <ivannnnz@gmail.com>
2016-07-29 06:10:22 -0400

    BUG: group_shift_indexer checks for null group keys
    closes #13813
    
    Author: Ivan Nazarov <ivannnnz@gmail.com>
    
    Closes #13819 from ivannz/issue13813fix and squashes the following commits:
    
    bddf799 [Ivan Nazarov] Switched from float('nan') to np.nan
    eab8038 [Ivan Nazarov] Added bugfix description [ci skip]
    d92cf3c [Ivan Nazarov] minor flake8 style corrections
    94bae0b [Ivan Nazarov] Patched the template, and added a test for '.shift()'
    fe2f0ec [Ivan Nazarov] Treat incomplete group keys as distinct when shifting
    966d5c6 [Ivan Nazarov] BUG: group_shift_indexer checks for null group keys

--- pandas/tests/test_groupby.py
+++ pandas/tests/test_groupby.py
@@ -6560,6 +6560,27 @@ class TestGroupBy(tm.TestCase):
         expected = "Grouping(('A', 'a'))"
         tm.assert_equal(result, expected)
 
+    def test_group_shift_with_null_key(self):
+        # This test is designed to replicate the segfault in issue #13813.
+        n_rows = 1200
+
+        # Generate a moderately large dataframe with occasional missing
+        # values in column `B`, and then group by [`A`, `B`]. This should
+        # force `-1` in `labels` array of `g.grouper.group_info` exactly
+        # at those places, where the group-by key is partilly missing.
+        df = DataFrame([(i % 12, i % 3 if i % 3 else np.nan, i)
+                        for i in range(n_rows)], dtype=float,
+                       columns=["A", "B", "Z"], index=None)
+        g = df.groupby(["A", "B"])
+
+        expected = DataFrame([(i + 12 if i % 3 and i < n_rows - 12
+                              else np.nan)
+                             for i in range(n_rows)], dtype=float,
+                             columns=["Z"], index=None)
+        result = g.shift(-1)
+
+        assert_frame_equal(result, expected)
+
 
 def assert_fp_equal(a, b):
     assert (np.abs(a - b) < 1e-12).all()
