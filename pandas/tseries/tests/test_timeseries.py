1fe275def92418484bca7f95188cbbc8f9f6a0f7
Sumit Binnani <sumit.binnani@gmail.com>
2016-08-03 15:06:56 +0530

    CLN: Code Cleanup

--- pandas/tseries/tests/test_timeseries.py
+++ pandas/tseries/tests/test_timeseries.py
@@ -818,20 +818,20 @@ class TestTimeSeries(tm.TestCase):
 
                 for unit in units:
                     # errors = 'coerce'
-                    result = Series(pd.to_datetime(units_from_epoch, unit=unit,
-                                                   origin=origin, errors='coerce'))
+                    result = Series(pd.to_datetime(units_from_epoch,
+                                                   unit=unit,
+                                                   origin=origin,
+                                                   errors='coerce'))
                     expected = Series([tslib.NaT, tslib.NaT, tslib.NaT,
                                        tslib.NaT, tslib.NaT])
                     assert_series_equal(result, expected)
 
                     # errors = 'ignore'
-                    result = Series(pd.to_datetime(units_from_epoch, unit=unit,
-                                                   origin=origin, errors='ignore'))
+                    result = Series(pd.to_datetime(units_from_epoch,
+                                                   unit=unit, origin=origin,
+                                                   errors='ignore'))
                     expected = Series(units_from_epoch)
                     assert_series_equal(result, expected)
-
-
-
 
     def test_series_ctor_datetime64(self):
         rng = date_range('1/1/2000 00:00:00', '1/1/2000 1:59:50', freq='10s')
