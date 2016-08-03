c8dc92731545737722c680e71bb21258ac621072
Tom Augspurger <tom.augspurger88@gmail.com>
2016-08-03 16:47:43 -0500

    TST: Refactor to use setup_class
    Pytest won't use setUp unless you inherit from TestCase.
    These classes don't because they use nose-style generator
    tests. We change to setup_class becuase that is called by
    the pytest runner.

--- pandas/io/tests/test_packers.py
+++ pandas/io/tests/test_packers.py
@@ -793,18 +793,19 @@ TestPackers
     http://stackoverflow.com/questions/6689537/nose-test-generators-inside-class
     """
 
-    def setUp(self):
+    @classmethod
+    def setup_class(cls):
         from pandas.io.tests.generate_legacy_storage_files import (
             create_msgpack_data, create_data)
-        self.data = create_msgpack_data()
-        self.all_data = create_data()
-        self.path = u('__%s__.msgpack' % tm.rands(10))
-        self.minimum_structure = {'series': ['float', 'int', 'mixed',
-                                             'ts', 'mi', 'dup'],
-                                  'frame': ['float', 'int', 'mixed', 'mi'],
-                                  'panel': ['float'],
-                                  'index': ['int', 'date', 'period'],
-                                  'mi': ['reg2']}
+        cls.data = create_msgpack_data()
+        cls.all_data = create_data()
+        cls.path = u('__%s__.msgpack' % tm.rands(10))
+        cls.minimum_structure = {'series': ['float', 'int', 'mixed',
+                                            'ts', 'mi', 'dup'],
+                                 'frame': ['float', 'int', 'mixed', 'mi'],
+                                 'panel': ['float'],
+                                 'index': ['int', 'date', 'period'],
+                                 'mi': ['reg2']}
 
     def check_min_structure(self, data):
         for typ, v in self.minimum_structure.items():
