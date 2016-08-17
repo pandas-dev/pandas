5d39744ef0cbc9db4e0d6c2c712eda2fe81482aa
gfyoung <gfyoung17@gmail.com>
2016-08-17 00:07:09 -0400

    BUG: Parse NULL char as null value
    Fixes bug in C parser in which the NULL character
    ('\x00') was being interpreted as a true line terminator,
    escape character, or comment character because it was used
    to indicate that a user had not specified these values. As
    a result, if the data contains this value, it was being
    incorrectly parsed. It should be parsed as NULL.
    
    Closes gh-14012.

--- pandas/io/tests/parser/c_parser_only.py
+++ pandas/io/tests/parser/c_parser_only.py
@@ -543,3 +543,21 @@ No,No,No"""
 
         # Check for data corruption if there was no segfault
         tm.assert_frame_equal(result, expected)
+
+    def test_internal_null_byte(self):
+        # see gh-14012
+        #
+        # The null byte ('\x00') should not be used as a
+        # true line terminator, escape character, or comment
+        # character, only as a placeholder to indicate that
+        # none was specified.
+        #
+        # This test should be moved to common.py ONLY when
+        # Python's csv class supports parsing '\x00'.
+        names = ['a', 'b', 'c']
+        data = "1,2,3\n4,\x00,6\n7,8,9"
+        expected = pd.DataFrame([[1, 2.0, 3], [4, np.nan, 6],
+                                 [7, 8, 9]], columns=names)
+
+        result = self.read_csv(StringIO(data), names=names)
+        tm.assert_frame_equal(result, expected)
