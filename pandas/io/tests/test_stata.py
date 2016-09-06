929d8af33f82e6f315265c05314f981c28cc3285
Kerby Shedden <kshedden@umich.edu>
2016-09-05 20:23:48 -0400

    Fix flake8 errors

--- pandas/io/tests/test_stata.py
+++ pandas/io/tests/test_stata.py
@@ -1226,10 +1226,11 @@ class TestStata(tm.TestCase):
         # SAS when exporting to Stata format. We do not know of any
         # on-line documentation for this version.
         df = read_stata(self.dta24_111)
-        original = pd.DataFrame({'y': [1,1,1,1,1,0,0,np.NaN,0,0],
-                                 'x': [1,2,1,3,np.NaN,4,3,5,1,6],
-                                 'w': [2,np.NaN,5,2,4,4,3,1,2,3],
-                                 'z': ['a','b','c','d','e','','g','h','i','j']})
+        original = pd.DataFrame({'y': [1, 1, 1, 1, 1, 0, 0, np.NaN, 0, 0],
+                                 'x': [1, 2, 1, 3, np.NaN, 4, 3, 5, 1, 6],
+                                 'w': [2, np.NaN, 5, 2, 4, 4, 3, 1, 2, 3],
+                                 'z': ['a', 'b', 'c', 'd', 'e', '', 'g', 'h',
+                                       'i', 'j']})
         original = original[['y', 'x', 'w', 'z']]
         tm.assert_frame_equal(original, df)
 
