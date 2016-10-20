42d7a5ff47dd3546e4cac8462ecb174a5f1151a1
dubourg <vincent.dubourg@gmail.com>
2016-10-20 12:25:34 +0200

    Type codes and categories as lists instead of tuples in _factorize_from_iterables (fixes #14438) (#14449)

--- pandas/core/categorical.py
+++ pandas/core/categorical.py
@@ -2055,14 +2055,14 @@ def _factorize_from_iterables(iterables):
 
     Returns
     -------
-    codes_tuple : tuple of ndarrays
-    categories_tuple : tuple of Indexes
+    codes_list : list of ndarrays
+    categories_list : list of Indexes
 
     Notes
     -----
     See `_factorize_from_iterable` for more info.
     """
     if len(iterables) == 0:
-        # For consistency, it should return a list of 2 tuples.
-        return [(), ()]
-    return lzip(*[_factorize_from_iterable(it) for it in iterables])
+        # For consistency, it should return a list of 2 lists.
+        return [[], []]
+    return map(list, lzip(*[_factorize_from_iterable(it) for it in iterables]))
