ea9dd8a0619b5f5386a43d6016dd7171a53cafa0
Jeff Reback <jeff@reback.net>
2016-10-25 18:18:53 -0400

    Revert "BUG: downcast='unsigned' on 0 would would not downcast to unsigned."
    This reverts commit 337ece6922af823c28623cab4df225df11c0b50b.

--- pandas/tools/tests/test_util.py
+++ pandas/tools/tests/test_util.py
@@ -401,52 +401,6 @@ class TestToNumeric(tm.TestCase):
             res = pd.to_numeric(data, downcast=downcast)
             tm.assert_numpy_array_equal(res, expected)
 
-    def test_downcast_limits(self):
-        # Test the limits of each downcast. #14401
-        # uint64 is not fully supported ATM
-        dtype_downcast_min_max = [
-            ('int8', 'integer',
-             [np.iinfo(np.int8).min, np.iinfo(np.int8).max]),
-            ('int16', 'integer',
-             [np.iinfo(np.int16).min, np.iinfo(np.int16).max]),
-            ('int32', 'integer',
-             [np.iinfo(np.int32).min, np.iinfo(np.int32).max]),
-            ('int64', 'integer',
-             [np.iinfo(np.int64).min, np.iinfo(np.int64).max]),
-            ('uint8', 'unsigned',
-             [np.iinfo(np.uint8).min, np.iinfo(np.uint8).max]),
-            ('uint16', 'unsigned',
-             [np.iinfo(np.uint16).min, np.iinfo(np.uint16).max]),
-            ('uint32', 'unsigned',
-             [np.iinfo(np.uint32).min, np.iinfo(np.uint32).max]),
-            # ('uint64', 'unsigned',
-            # [np.iinfo(np.uint64).min, np.iinfo(np.uint64).max]),
-
-            ('int16', 'integer',
-             [np.iinfo(np.int8).min, np.iinfo(np.int8).max + 1]),
-            ('int32', 'integer',
-             [np.iinfo(np.int16).min, np.iinfo(np.int16).max + 1]),
-            ('int64', 'integer',
-             [np.iinfo(np.int32).min, np.iinfo(np.int32).max + 1]),
-            ('int16', 'integer',
-             [np.iinfo(np.int8).min - 1, np.iinfo(np.int16).max]),
-            ('int32', 'integer',
-             [np.iinfo(np.int16).min - 1, np.iinfo(np.int32).max]),
-            ('int64', 'integer',
-             [np.iinfo(np.int32).min - 1, np.iinfo(np.int64).max]),
-            ('uint16', 'unsigned',
-             [np.iinfo(np.uint8).min, np.iinfo(np.uint8).max + 1]),
-            ('uint32', 'unsigned',
-             [np.iinfo(np.uint16).min, np.iinfo(np.uint16).max + 1]),
-            # ('uint64', 'unsigned',
-            # [np.iinfo(np.uint32).min, np.iinfo(np.uint32).max + 1]),
-        ]
-
-        for dtype, downcast, min_max in dtype_downcast_min_max:
-            series = pd.to_numeric(pd.Series(min_max), downcast=downcast)
-            tm.assert_equal(series.dtype, dtype)
-
-
 if __name__ == '__main__':
     nose.runmodule(argv=[__file__, '-vvs', '-x', '--pdb', '--pdb-failure'],
                    exit=False)
