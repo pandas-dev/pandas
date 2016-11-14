52241a7d8a8be2cc1ae40cbde054b0983ac1f429
Jeff Reback <jeff@reback.net>
2016-11-14 15:49:01 -0500

    BLD: fix linting check for .pyx
    closes #14659

--- ci/lint.sh
+++ ci/lint.sh
@@ -10,18 +10,24 @@ if [ "$LINT" ]; then
     # pandas/rpy is deprecated and will be removed.
     # pandas/src is C code, so no need to search there.
     echo "Linting  *.py"
-    flake8 pandas --filename '*.py' --exclude pandas/rpy,pandas/src
+    flake8 pandas --filename=*.py --exclude pandas/rpy,pandas/src
+    if [ $? -ne "0" ]; then
+        RET=1
+    fi
     echo "Linting *.py DONE"
 
     echo "Linting *.pyx"
-    flake8 pandas --filename '*.pyx' --select=E501,E302,E203,E111,E114,E221,E303,E128,E231,E126
+    flake8 pandas --filename=*.pyx --select=E501,E302,E203,E111,E114,E221,E303,E128,E231,E126
+    if [ $? -ne "0" ]; then
+        RET=1
+    fi
     echo "Linting *.pyx DONE"
 
     echo "Linting *.pxi.in"
     for path in 'src'
     do
         echo "linting -> pandas/$path"
-        flake8 pandas/$path --filename '*.pxi.in' --select=E501,E302,E203,E111,E114,E221,E303,E231,E126
+        flake8 pandas/$path --filename=*.pxi.in --select=E501,E302,E203,E111,E114,E221,E303,E231,E126
         if [ $? -ne "0" ]; then
             RET=1
         fi
