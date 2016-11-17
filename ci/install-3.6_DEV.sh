d317905be258cc330ee8b133ceac56e3a5dc6ae9
Jeff Reback <jeff@reback.net>
2016-11-17 13:03:16 -0500

    BLD: cleaner 3.6 deps install
    (cherry picked from commit 45543ec22bb01d5c46eff9384491d218cffddd64)

--- ci/install-3.6_DEV.sh
+++ ci/install-3.6_DEV.sh
@@ -9,21 +9,8 @@ source activate pandas
 
 # ensure we have pip
 python -m ensurepip
-pip3.6 install nose
-
-# build cython
-git clone https://github.com/cython/cython.git
-cd cython
-git checkout 0.25.1
-python setup.py install
-cd ..
-
-# remove the system installed numpy
-pip3.6 uninstall numpy -y
 
 # install deps
-pip3.6 install numpy
-pip3.6 install pytz
-pip3.6 install python-dateutil
+pip3.6 install nose cython numpy pytz python-dateutil
 
 true
