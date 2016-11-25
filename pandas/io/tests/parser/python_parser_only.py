dda588b1d15df83f77f302282ec6f026a7e10591
gfyoung <gfyoung17@gmail.com>
2016-11-25 01:40:17 -0500

    BUG: Improve error message for multi-char sep and quotes in Python engine
    If there is a field counts mismatch, check whether
    a multi-char sep was used in conjunction with quotes.
    Currently, that setup is not respected and can result
    in improper line breaks.
    
    Closes gh-13374.

--- pandas/io/tests/parser/python_parser_only.py
+++ pandas/io/tests/parser/python_parser_only.py
@@ -7,6 +7,7 @@ these tests out of this module as soon as the C parser can accept further
 arguments when parsing.
 """
 
+import csv
 import sys
 import nose
 
@@ -204,3 +205,19 @@ x   q   30      3    -0.6662 -0.5243 -0.3580  0.89145  2.5838"""
                                        sep=sep, names=['a', 'b'],
                                        encoding=encoding)
                 tm.assert_frame_equal(result, expected)
+
+    def test_multi_char_sep_quotes(self):
+        # see gh-13374
+
+        data = 'a,,b\n1,,a\n2,,"2,,b"'
+        msg = 'ignored when a multi-char delimiter is used'
+
+        with tm.assertRaisesRegexp(ValueError, msg):
+            self.read_csv(StringIO(data), sep=',,')
+
+        # We expect no match, so there should be an assertion
+        # error out of the inner context manager.
+        with tm.assertRaises(AssertionError):
+            with tm.assertRaisesRegexp(ValueError, msg):
+                self.read_csv(StringIO(data), sep=',,',
+                              quoting=csv.QUOTE_NONE)
