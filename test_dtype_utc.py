ba83fc85ab207780d232d6533bde9221d93245e1
Rodolfo Fernandez <opensourceworkAR@users.noreply.github.com>
2016-12-17 10:08:57 -0800

    test

---
+++ test_dtype_utc.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Dec 17 09:34:47 2016
+
+@author: rodolfoxps
+"""
+
+import pandas as pd
+import datetime, pytz
+from pandas.util.testing import assert_frame_equal
+
+def test_dtype_utc(self):
+    
+    data=pd.Series( [pd.NaT, pd.NaT, datetime.datetime(2016, 12, 12, 22, 24, 6, 100001, tzinfo=pytz.utc) ] )
+  
+    filled=data.fillna(method='bfill')
+    
+    
+    expected=pd.Series([datetime.datetime(2016, 12, 12, 22, 24, 6, 100001, tzinfo=pytz.utc) ,
+                      datetime.datetime(2016, 12, 12, 22, 24, 6, 100001, tzinfo=pytz.utc) ,
+                      datetime.datetime(2016, 12, 12, 22, 24, 6, 100001, tzinfo=pytz.utc) ])
+
+    assert_frame_equal(filled, expected)
