77810e2b4422415b4565956e27e947e09267a866
Rodolfo Fernandez <opensourceworkAR@users.noreply.github.com>
2016-12-18 14:36:21 -0500

    BUG: .fillna() for datetime64 with tz is passing thru floats
    closes #14872
    
    Author: Rodolfo Fernandez <opensourceworkAR@users.noreply.github.com>
    
    Closes #14905 from RodolfoRFR/pandas-14872-e and squashes the following commits:
    
    18802b4 [Rodolfo Fernandez] added 'self' to test_dtype_utc function in pandas/tests/series/test_missing
    e0c6c7c [Rodolfo Fernandez] added line to whatsnew v0.19.2 and test to test_missing.py in series folder
    e4ba7e0 [Rodolfo Fernandez] removed all references to _DATELIKE_DTYPES from /pandas/core/missing.py
    5d37ce8 [Rodolfo Fernandez] added is_datetime64tz_dtype and changed evaluation from 'values' to dtype
    19eecb2 [Rodolfo Fernandez] fixed style errors using flake8
    59b91a1 [Rodolfo Fernandez] test modified
    5a59eac [Rodolfo Fernandez] test modified
    bc68bf7 [Rodolfo Fernandez] test modified
    ba83fc8 [Rodolfo Fernandez] test
    b7358de [Rodolfo Fernandez] bug fixed

--- pandas/core/missing.py
+++ pandas/core/missing.py
@@ -10,9 +10,8 @@ import pandas.lib as lib
 from pandas.compat import range, string_types
 from pandas.types.common import (is_numeric_v_string_like,
                                  is_float_dtype, is_datetime64_dtype,
-                                 is_integer_dtype, _ensure_float64,
-                                 is_scalar,
-                                 _DATELIKE_DTYPES,
+                                 is_datetime64tz_dtype, is_integer_dtype,
+                                 _ensure_float64, is_scalar,
                                  needs_i8_conversion)
 from pandas.types.missing import isnull
 
@@ -450,7 +449,7 @@ def pad_1d(values, limit=None, mask=None, dtype=None):
     _method = None
     if is_float_dtype(values):
         _method = getattr(algos, 'pad_inplace_%s' % dtype.name, None)
-    elif dtype in _DATELIKE_DTYPES or is_datetime64_dtype(values):
+    elif is_datetime64_dtype(dtype) or is_datetime64tz_dtype(dtype):
         _method = _pad_1d_datetime
     elif is_integer_dtype(values):
         values = _ensure_float64(values)
@@ -475,7 +474,7 @@ def backfill_1d(values, limit=None, mask=None, dtype=None):
     _method = None
     if is_float_dtype(values):
         _method = getattr(algos, 'backfill_inplace_%s' % dtype.name, None)
-    elif dtype in _DATELIKE_DTYPES or is_datetime64_dtype(values):
+    elif is_datetime64_dtype(dtype) or is_datetime64tz_dtype(dtype):
         _method = _backfill_1d_datetime
     elif is_integer_dtype(values):
         values = _ensure_float64(values)
@@ -501,7 +500,7 @@ def pad_2d(values, limit=None, mask=None, dtype=None):
     _method = None
     if is_float_dtype(values):
         _method = getattr(algos, 'pad_2d_inplace_%s' % dtype.name, None)
-    elif dtype in _DATELIKE_DTYPES or is_datetime64_dtype(values):
+    elif is_datetime64_dtype(dtype) or is_datetime64tz_dtype(dtype):
         _method = _pad_2d_datetime
     elif is_integer_dtype(values):
         values = _ensure_float64(values)
@@ -531,7 +530,7 @@ def backfill_2d(values, limit=None, mask=None, dtype=None):
     _method = None
     if is_float_dtype(values):
         _method = getattr(algos, 'backfill_2d_inplace_%s' % dtype.name, None)
-    elif dtype in _DATELIKE_DTYPES or is_datetime64_dtype(values):
+    elif is_datetime64_dtype(dtype) or is_datetime64tz_dtype(dtype):
         _method = _backfill_2d_datetime
     elif is_integer_dtype(values):
         values = _ensure_float64(values)
