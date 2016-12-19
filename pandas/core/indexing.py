7dae07798f31712d57ce3150861ce0290e12f67f
Chris Ham <chris@christopher-ham.com>
2016-12-19 18:44:05 -0500

    CLN: Resubmit of GH14700.  Fixes GH14554.  Errors other than Indexing…
    IdnexError and KeyError now bubble up appropriately.
    
    closes #14554
    
    Author: Chris Ham <chris@christopher-ham.com>
    
    Closes #14912 from clham/gh14554-b and squashes the following commits:
    
    458c0cc [Chris Ham] CLN: Resubmit of GH14700.  Fixes GH14554.  Errors other than IndexingError and KeyError now bubble up appropriately.

--- pandas/core/indexing.py
+++ pandas/core/indexing.py
@@ -848,7 +848,7 @@ class _NDFrameIndexer(object):
                 [(a, self._convert_for_reindex(t, axis=o._get_axis_number(a)))
                  for t, a in zip(tup, o._AXIS_ORDERS)])
             return o.reindex(**d)
-        except:
+        except(KeyError, IndexingError):
             raise self._exception
 
     def _convert_for_reindex(self, key, axis=0):
