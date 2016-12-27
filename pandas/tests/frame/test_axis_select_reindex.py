9c9ad956be81193bf5178e622178fc07a2b1976c
Guilherme Beltramini <guilherme.beltramini@nubank.com.br>
2016-12-27 15:16:16 -0200

    BUG: Add tests for sparse dataframe

--- pandas/tests/frame/test_axis_select_reindex.py
+++ pandas/tests/frame/test_axis_select_reindex.py
@@ -297,34 +297,43 @@ class TestDataFrameSelectReindex(tm.TestCase, TestData):
         newFrame = self.frame.reindex(columns=[])
         self.assertTrue(newFrame.empty)
 
+    def test_reindex_columns_method(self):
+
         # GH 14992, reindexing over columns ignored method
         df = DataFrame(data=[[11, 12, 13], [21, 22, 23], [31, 32, 33]],
                        index=[1, 2, 4],
                        columns=[1, 2, 4],
                        dtype=float)
-        expected_def = DataFrame(data=[[np.nan, 11, 12, np.nan, 13, np.nan],
-                                       [np.nan, 21, 22, np.nan, 23, np.nan],
-                                       [np.nan, 31, 32, np.nan, 33, np.nan]],
-                                 index=[1, 2, 4],
-                                 columns=range(6),
-                                 dtype=float)
-        expected_ffill = DataFrame(data=[[np.nan, 11, 12, 12, 13, 13],
-                                         [np.nan, 21, 22, 22, 23, 23],
-                                         [np.nan, 31, 32, 32, 33, 33]],
-                                   index=[1, 2, 4],
-                                   columns=range(6),
-                                   dtype=float)
-        expected_bfill = DataFrame(data=[[11, 11, 12, 13, 13, np.nan],
-                                         [21, 21, 22, 23, 23, np.nan],
-                                         [31, 31, 32, 33, 33, np.nan]],
-                                   index=[1, 2, 4],
-                                   columns=range(6),
-                                   dtype=float)
-        assert_frame_equal(df.reindex(columns=range(6)), expected_def)
-        assert_frame_equal(df.reindex(columns=range(6), method='ffill'),
-                           expected_ffill)
-        assert_frame_equal(df.reindex(columns=range(6), method='bfill'),
-                           expected_bfill)
+
+        # default method
+        result = df.reindex(columns=range(6))
+        expected = DataFrame(data=[[np.nan, 11, 12, np.nan, 13, np.nan],
+                                   [np.nan, 21, 22, np.nan, 23, np.nan],
+                                   [np.nan, 31, 32, np.nan, 33, np.nan]],
+                             index=[1, 2, 4],
+                             columns=range(6),
+                             dtype=float)
+        assert_frame_equal(result, expected)
+
+        # method='ffill'
+        result = df.reindex(columns=range(6), method='ffill')
+        expected = DataFrame(data=[[np.nan, 11, 12, 12, 13, 13],
+                                   [np.nan, 21, 22, 22, 23, 23],
+                                   [np.nan, 31, 32, 32, 33, 33]],
+                             index=[1, 2, 4],
+                             columns=range(6),
+                             dtype=float)
+        assert_frame_equal(result, expected)
+
+        # method='bfill'
+        result = df.reindex(columns=range(6), method='bfill')
+        expected = DataFrame(data=[[11, 11, 12, 13, 13, np.nan],
+                                   [21, 21, 22, 23, 23, np.nan],
+                                   [31, 31, 32, 33, 33, np.nan]],
+                             index=[1, 2, 4],
+                             columns=range(6),
+                             dtype=float)
+        assert_frame_equal(result, expected)
 
     def test_reindex_axes(self):
         # GH 3317, reindexing by both axes loses freq of the index
