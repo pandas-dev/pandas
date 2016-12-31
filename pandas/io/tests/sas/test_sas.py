bf60d23063168b8264b14e232c669637abfa54e0
tomrod <thomas.roderick@gmail.com>
2016-12-30 22:38:45 -0500

    Adding tests, creating updated information

--- pandas/io/tests/sas/test_sas.py
+++ pandas/io/tests/sas/test_sas.py
@@ -1,13 +1,9 @@
-from pandas.io.sas.sasreader import read_sas
 import pandas.util.testing as tm
-import pandas as pd
 
 class TestSasBuff(tm.TestCase):
-
     def test_sas_buffer_format(self):
         import StringIO
         from pandas.io.sas.sasreader import read_sas
         b = StringIO.StringIO("")
         with self.assertRaises(TypeError):
-            result=read_sas(b)
-
+            read_sas(b)
