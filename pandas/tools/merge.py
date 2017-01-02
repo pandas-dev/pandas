afa750252ea9d46f8e318d8d58dff6547af7b575
palewire <ben.welsh@gmail.com>
2017-01-02 11:50:45 -0800

    Added keys example

--- pandas/tools/merge.py
+++ pandas/tools/merge.py
@@ -1471,6 +1471,33 @@ def concat(objs, axis=0, join='outer', join_axes=None, ignore_index=False,
     3    d
     dtype: object
 
+    Add a ``hierarchical index`` at the outermost level of
+    the data.
+
+    >>> s1 = pd.Series(['a', 'b', 'c'])
+    >>> s2 = pd.Series(['c', 'd', 'e'])
+    >>> c = pd.concat([s1, s2], keys=["s1", 's2',])
+    >>> c
+    s1  0    a
+        1    b
+        2    c
+    s2  0    c
+        1    d
+        2    e
+    dtype: object
+    >>> c.ix['s1']
+    0    a
+    1    b
+    2    c
+    dtype: object
+    >>> c.ix['s2']
+    0    c
+    1    d
+    2    e
+    dtype: object
+    >>> c.ix['s1'].ix[0]
+    'a'
+
     Combine two ``DataFrame`` objects with identical columns.
 
     >>> df1 = pd.DataFrame(
