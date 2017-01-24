a0435f928032e114d6f52f7c177af6bd2515b3b6
Jeff Reback <jeff@reback.net>
2017-01-24 13:51:51 -0500

    COMPAT: don't rely on dtype being False after numpy >= 1.13
    closes #15199
    
    Author: Jeff Reback <jeff@reback.net>
    
    Closes #15200 from jreback/numpy-dev and squashes the following commits:
    
    53d663e [Jeff Reback] BLD: pull wheel for numpy builds
    5475db3 [Jeff Reback] COMPAT: numpy-dev compat on bool(dtype) change

--- ci/install_travis.sh
+++ ci/install_travis.sh
@@ -16,28 +16,29 @@
 function edit_init()
 {
     if [ -n "$LOCALE_OVERRIDE" ]; then
-        echo "Adding locale to the first line of pandas/__init__.py"
+        echo "[Adding locale to the first line of pandas/__init__.py]"
         rm -f pandas/__init__.pyc
         sedc="3iimport locale\nlocale.setlocale(locale.LC_ALL, '$LOCALE_OVERRIDE')\n"
         sed -i "$sedc" pandas/__init__.py
-        echo "head -4 pandas/__init__.py"
+        echo "[head -4 pandas/__init__.py]"
         head -4 pandas/__init__.py
         echo
     fi
 }
 
+echo "[install_travis]"
 edit_init
 
 home_dir=$(pwd)
-echo "home_dir: [$home_dir]"
+echo "[home_dir: $home_dir]"
 
 MINICONDA_DIR="$HOME/miniconda3"
 
 if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ] && [ "$USE_CACHE" ]; then
-    echo "Miniconda install already present from cache: $MINICONDA_DIR"
+    echo "[Miniconda install already present from cache: $MINICONDA_DIR]"
 
     conda config --set always_yes yes --set changeps1 no || exit 1
-    echo "update conda"
+    echo "[update conda]"
     conda update -q conda || exit 1
 
     # Useful for debugging any issues with conda
@@ -45,18 +46,18 @@ if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ] && [ "$USE_CACHE
 
     # set the compiler cache to work
     if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-        echo "Using ccache"
+        echo "[Using ccache]"
         export PATH=/usr/lib/ccache:/usr/lib64/ccache:$PATH
         gcc=$(which gcc)
-        echo "gcc: $gcc"
+        echo "[gcc: $gcc]"
         ccache=$(which ccache)
-        echo "ccache: $ccache"
+        echo "[ccache: $ccache]"
         export CC='ccache gcc'
     fi
 
 else
-    echo "Using clean Miniconda install"
-    echo "Not using ccache"
+    echo "[Using clean Miniconda install]"
+    echo "[Not using ccache]"
     rm -rf "$MINICONDA_DIR"
     # install miniconda
     if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
@@ -66,14 +67,14 @@ else
     fi
     bash miniconda.sh -b -p "$MINICONDA_DIR" || exit 1
 
-    echo "update conda"
+    echo "[update conda]"
     conda config --set ssl_verify false || exit 1
     conda config --set always_yes true --set changeps1 false || exit 1
     conda update -q conda
 
     # add the pandas channel to take priority
     # to add extra packages
-    echo "add channels"
+    echo "[add channels]"
     conda config --add channels pandas || exit 1
     conda config --remove channels defaults || exit 1
     conda config --add channels defaults || exit 1
@@ -103,11 +104,17 @@ else
 fi
 
 # build deps
+echo "[build installs]"
 REQ="ci/requirements-${PYTHON_VERSION}${JOB_TAG}.build"
-
-# install deps
 if [ -e ${REQ} ]; then
     time conda install -n pandas --file=${REQ} || exit 1
+fi
+
+# may have addtl installation instructions for this build
+echo "[build addtl installs]"
+REQ="ci/requirements-${PYTHON_VERSION}${JOB_TAG}.build.sh"
+if [ -e ${REQ} ]; then
+    time bash $REQ || exit 1
 fi
 
 source activate pandas
@@ -122,24 +129,25 @@ if [ "$BUILD_TEST" ]; then
 else
 
     # build but don't install
-    echo "build em"
+    echo "[build em]"
     time python setup.py build_ext --inplace || exit 1
 
     # we may have run installations
-    echo "conda installs"
+    echo "[conda installs]"
     REQ="ci/requirements-${PYTHON_VERSION}${JOB_TAG}.run"
     if [ -e ${REQ} ]; then
         time conda install -n pandas --file=${REQ} || exit 1
     fi
 
     # we may have additional pip installs
-    echo "pip installs"
+    echo "[pip installs]"
     REQ="ci/requirements-${PYTHON_VERSION}${JOB_TAG}.pip"
     if [ -e ${REQ} ]; then
        pip install --upgrade -r $REQ
     fi
 
     # may have addtl installation instructions for this build
+    echo "[addtl installs]"
     REQ="ci/requirements-${PYTHON_VERSION}${JOB_TAG}.sh"
     if [ -e ${REQ} ]; then
         time bash $REQ || exit 1
@@ -147,14 +155,14 @@ else
 
     # remove any installed pandas package
     # w/o removing anything else
-    echo "removing installed pandas"
+    echo "[removing installed pandas]"
     conda remove pandas --force
 
     # install our pandas
-    echo "running setup.py develop"
+    echo "[running setup.py develop]"
     python setup.py develop  || exit 1
 
 fi
 
-echo "done"
+echo "[done]"
 exit 0
--- ci/requirements-3.5_NUMPY_DEV.build.sh
+++ ci/requirements-3.5_NUMPY_DEV.build.sh
@@ -7,11 +7,6 @@ echo "install numpy master wheel"
 # remove the system installed numpy
 pip uninstall numpy -y
 
-# we need these for numpy
-
-# these wheels don't play nice with the conda libgfortran / openblas
-# time conda install -n pandas libgfortran openblas || exit 1
-
 # install numpy wheel from master
 pip install --pre --upgrade --no-index --timeout=60 --trusted-host travis-dev-wheels.scipy.org -f http://travis-dev-wheels.scipy.org/ numpy scipy
 
