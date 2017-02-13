f22559876b130891e7c834cf269d1289307e3fa3
Francesc Alted <francesc@continuum.io>
2017-02-13 13:04:58 +0100

    Updated test_compat for numexpr 2.4.6

--- pandas/tests/computation/test_compat.py
+++ pandas/tests/computation/test_compat.py
@@ -21,9 +21,7 @@ def test_compat():
     try:
         import numexpr as ne
         ver = ne.__version__
-        if ver == LooseVersion('2.4.4'):
-            assert not _NUMEXPR_INSTALLED
-        elif ver < LooseVersion('2.1'):
+        elif ver < LooseVersion('2.4.6'):
             with tm.assert_produces_warning(UserWarning,
                                             check_stacklevel=False):
                 assert not _NUMEXPR_INSTALLED
