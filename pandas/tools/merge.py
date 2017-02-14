5959fe1fffe4b5749de63d6a26ac64349bc791ac
Jeff Reback <jeff@reback.net>
2017-02-14 17:35:05 -0500

    CLN: create core/sorting.py
    just a small reorg to put sorting / grouping utilities into a separate
    area
    
    Author: Jeff Reback <jeff@reback.net>
    
    Closes #15402 from jreback/sorting and squashes the following commits:
    
    fdcf9a1 [Jeff Reback] change a couple of sorting.py functions to be non-private (public to pandas internals)
    90ff22d [Jeff Reback] split up some value_counts groupby tests a bit
    18ea902 [Jeff Reback] CLN: create core/sorting.py
    92dcb07 [Jeff Reback] CLN: remove numpy_groupby as not used

--- pandas/tools/merge.py
+++ pandas/tools/merge.py
@@ -34,6 +34,7 @@ from pandas.core.internals import (items_overlap_with_suffix,
                                    concatenate_block_managers)
 from pandas.util.decorators import Appender, Substitution
 
+from pandas.core.sorting import is_int64_overflow_possible
 import pandas.core.algorithms as algos
 import pandas.core.common as com
 
@@ -1397,10 +1398,9 @@ def _sort_labels(uniques, left, right):
 
 
 def _get_join_keys(llab, rlab, shape, sort):
-    from pandas.core.groupby import _int64_overflow_possible
 
     # how many levels can be done without overflow
-    pred = lambda i: not _int64_overflow_possible(shape[:i])
+    pred = lambda i: not is_int64_overflow_possible(shape[:i])
     nlev = next(filter(pred, range(len(shape), 0, -1)))
 
     # get keys for the first `nlev` levels
