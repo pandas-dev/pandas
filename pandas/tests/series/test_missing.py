ff0deecbc8f8e9ae3d274e5e7cd7c0056de1a6c2
Matt Roeschke <emailformattr@gmail.com>
2017-02-14 08:33:34 -0500

    Bug: Raise ValueError with interpolate & fillna limit = 0 (#9217)
    closes #9217
    
    Author: Matt Roeschke <emailformattr@gmail.com>
    
    Closes #14994 from mroeschke/fix_9217 and squashes the following commits:
    
    c1790ee [Matt Roeschke] Unify ValueError message and correct cython limits
    6f041e6 [Matt Roeschke] Bug: Raise ValueError with interpolate limit = 0

--- pandas/tests/series/test_missing.py
+++ pandas/tests/series/test_missing.py
@@ -295,6 +295,13 @@ class TestSeriesMissingData(TestData, tm.TestCase):
         self.assertRaises(TypeError, s.fillna, [1, 2])
         self.assertRaises(TypeError, s.fillna, (1, 2))
 
+        # related GH 9217, make sure limit is an int and greater than 0
+        s = Series([1, 2, 3, None])
+        for limit in [-1, 0, 1., 2.]:
+            for method in ['backfill', 'bfill', 'pad', 'ffill', None]:
+                with tm.assertRaises(ValueError):
+                    s.fillna(1, limit=limit, method=method)
+
     def test_fillna_nat(self):
         series = Series([0, 1, 2, tslib.iNaT], dtype='M8[ns]')
 
@@ -864,6 +871,17 @@ class TestSeriesInterpolateData(TestData, tm.TestCase):
         expected = Series([1., 3., 5., 7., np.nan, 11.])
         result = s.interpolate(method='linear', limit=2)
         assert_series_equal(result, expected)
+
+        # GH 9217, make sure limit is an int and greater than 0
+        methods = ['linear', 'time', 'index', 'values', 'nearest', 'zero',
+                   'slinear', 'quadratic', 'cubic', 'barycentric', 'krogh',
+                   'polynomial', 'spline', 'piecewise_polynomial', None,
+                   'from_derivatives', 'pchip', 'akima']
+        s = pd.Series([1, 2, np.nan, np.nan, 5])
+        for limit in [-1, 0, 1., 2.]:
+            for method in methods:
+                with tm.assertRaises(ValueError):
+                    s.interpolate(limit=limit, method=method)
 
     def test_interp_limit_forward(self):
         s = Series([1, 3, np.nan, np.nan, np.nan, 11])
