e2a30f2cff36e2056fe3570282df3786360ee334
Pietro Battiston <me@pietrobattiston.it>
2017-02-20 09:36:19 -0500

    BUG: MultiIndex indexing with passed Series/DataFrame/ndarray as indexers
    closes #15424
    closes #15434
    
    Author: Pietro Battiston <me@pietrobattiston.it>
    
    Closes #15425 from toobaz/mi_indexing and squashes the following commits:
    
    2ba2d5d [Pietro Battiston] Updated comment
    900e3ce [Pietro Battiston] whatsnew
    8467b57 [Pietro Battiston] Tests for previous commit
    17209f3 [Pietro Battiston] BUG: support indexing MultiIndex with 1-D array
    7606114 [Pietro Battiston] Whatsnew
    0b719f5 [Pietro Battiston] Test for previous commit
    1f2f385 [Pietro Battiston] BUG: Fix indexing MultiIndex with Series with 0 not index

--- pandas/tests/indexing/test_multiindex.py
+++ pandas/tests/indexing/test_multiindex.py
@@ -158,10 +158,44 @@ class TestMultiIndexBasic(tm.TestCase):
         result = x.loc[[1, 3]]
         tm.assert_series_equal(result, expected)
 
+        # GH15424
+        y1 = Series([1, 3], index=[1, 2])
+        result = x.loc[y1]
+        tm.assert_series_equal(result, expected)
+
         empty = Series(data=[], dtype=np.float64)
         expected = Series([], index=MultiIndex(
             levels=index.levels, labels=[[], []], dtype=np.float64))
         result = x.loc[empty]
+        tm.assert_series_equal(result, expected)
+
+    def test_loc_getitem_array(self):
+        # GH15434
+        # passing an array as a key with a MultiIndex
+        index = MultiIndex.from_product([[1, 2, 3], ['A', 'B', 'C']])
+        x = Series(index=index, data=range(9), dtype=np.float64)
+        y = np.array([1, 3])
+        expected = Series(
+            data=[0, 1, 2, 6, 7, 8],
+            index=MultiIndex.from_product([[1, 3], ['A', 'B', 'C']]),
+            dtype=np.float64)
+        result = x.loc[y]
+        tm.assert_series_equal(result, expected)
+
+        # empty array:
+        empty = np.array([])
+        expected = Series([], index=MultiIndex(
+            levels=index.levels, labels=[[], []], dtype=np.float64))
+        result = x.loc[empty]
+        tm.assert_series_equal(result, expected)
+
+        # 0-dim array (scalar):
+        scalar = np.int64(1)
+        expected = Series(
+            data=[0, 1, 2],
+            index=['A', 'B', 'C'],
+            dtype=np.float64)
+        result = x.loc[scalar]
         tm.assert_series_equal(result, expected)
 
     def test_iloc_getitem_multiindex(self):
