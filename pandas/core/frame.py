32f459c195336c1861efc9489b0df7125c40ee92
palewire <ben.welsh@gmail.com>
2017-02-19 16:43:24 -0800

    lint

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -1862,8 +1862,8 @@ class DataFrame(NDFrame):
         ----------
         index : row label
         col : column label
-        takeable : interpret the index and col parameters as indexers. The default
-        is ``False``.
+        takeable : interpret the index and col parameters as indexers.
+        The default is ``False``.
 
         Returns
         -------
