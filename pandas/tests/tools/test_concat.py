e9d00dec17bd6952048a46ae130ab8e79cba286b
Justin Solinsky <justinsolinsky@Justins-MacBook-Pro.local>
2017-02-21 22:01:49 -0500

    GH15219 Documentation fixes based on feedback

--- pandas/tests/tools/test_concat.py
+++ pandas/tests/tools/test_concat.py
@@ -1663,6 +1663,7 @@ class TestConcatenate(ConcatenateBase):
             union_categoricals([c1, c2])
 
     def test_union_categoricals_ignore_order(self):
+        # GH 15219
         c1 = Categorical([1, 2, 3], ordered=True)
         c2 = Categorical([1, 2, 3], ordered=False)
 
@@ -1670,8 +1671,17 @@ class TestConcatenate(ConcatenateBase):
         exp = Categorical([1, 2, 3, 1, 2, 3])
         tm.assert_categorical_equal(res, exp)
 
+        msg = 'Categorical.ordered must be the same'
+        with tm.assertRaisesRegexp(TypeError, msg):
+            union_categoricals([c1, c2], ignore_order=False)
+
         res = union_categoricals([c1, c1], ignore_order=True)
         exp = Categorical([1, 2, 3, 1, 2, 3])
+        tm.assert_categorical_equal(res, exp)
+
+        res = union_categoricals([c1, c1], ignore_order=False)
+        exp = Categorical([1, 2, 3, 1, 2, 3],
+                          categories=[1, 2, 3], ordered=True)
         tm.assert_categorical_equal(res, exp)
 
         c1 = Categorical([1, 2, 3, np.nan], ordered=True)
@@ -1688,7 +1698,8 @@ class TestConcatenate(ConcatenateBase):
         exp = Categorical([1, 2, 3, 1, 2, 3])
         tm.assert_categorical_equal(res, exp)
 
-        res = union_categoricals([c2, c1], ignore_order=True, sort_categories=True)
+        res = union_categoricals([c2, c1], ignore_order=True,
+                                 sort_categories=True)
         exp = Categorical([1, 2, 3, 1, 2, 3], categories=[1, 2, 3])
         tm.assert_categorical_equal(res, exp)
 
@@ -1697,6 +1708,13 @@ class TestConcatenate(ConcatenateBase):
         result = union_categoricals([c1, c2], ignore_order=True)
         expected = Categorical([1, 2, 3, 4, 5, 6])
         tm.assert_categorical_equal(result, expected)
+
+        msg = "to union ordered Categoricals, all categories must be the same"
+        with tm.assertRaisesRegexp(TypeError, msg):
+            union_categoricals([c1, c2], ignore_order=False)
+
+        with tm.assertRaisesRegexp(TypeError, msg):
+            union_categoricals([c1, c2])
 
     def test_union_categoricals_sort(self):
         # GH 13846
