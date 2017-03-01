58b6f2fd42bc083d45fda5abde3877e1b0a3d802
Jeff Reback <jeff@reback.net>
2017-03-01 09:19:30 -0500

    BLD: fix 2.7_LOCALE build
    simplify install_travis.sh, always using a clean miniconda install
    
    bump matplotlib to 1.4.0 for _LOCALE, numpy to 1.8.2

--- ci/install_travis.sh
+++ ci/install_travis.sh
@@ -32,58 +32,50 @@ edit_init
 home_dir=$(pwd)
 echo "[home_dir: $home_dir]"
 
+# install miniconda
+echo "[Using clean Miniconda install]"
+
 MINICONDA_DIR="$HOME/miniconda3"
-
-if [ -d "$MINICONDA_DIR" ] && [ -e "$MINICONDA_DIR/bin/conda" ] && [ "$USE_CACHE" ]; then
-    echo "[Miniconda install already present from cache: $MINICONDA_DIR]"
-
-    conda config --set always_yes yes --set changeps1 no || exit 1
-    echo "[update conda]"
-    conda update -q conda || exit 1
-
-    # Useful for debugging any issues with conda
-    conda info -a || exit 1
-
-    # set the compiler cache to work
-    if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-        echo "[Using ccache]"
-        export PATH=/usr/lib/ccache:/usr/lib64/ccache:$PATH
-        gcc=$(which gcc)
-        echo "[gcc: $gcc]"
-        ccache=$(which ccache)
-        echo "[ccache: $ccache]"
-        export CC='ccache gcc'
-    fi
-
-else
-    echo "[Using clean Miniconda install]"
-    echo "[Not using ccache]"
+if [ -d "$MINICONDA_DIR" ]; then
     rm -rf "$MINICONDA_DIR"
-    # install miniconda
-    if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-        wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh || exit 1
-    else
-        wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh || exit 1
-    fi
-    bash miniconda.sh -b -p "$MINICONDA_DIR" || exit 1
+fi
 
-    echo "[update conda]"
-    conda config --set ssl_verify false || exit 1
-    conda config --set always_yes true --set changeps1 false || exit 1
-    conda update -q conda
+# install miniconda
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
+    wget http://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh || exit 1
+else
+    wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh || exit 1
+fi
+bash miniconda.sh -b -p "$MINICONDA_DIR" || exit 1
 
-    # add the pandas channel to take priority
-    # to add extra packages
-    echo "[add channels]"
-    conda config --add channels pandas || exit 1
-    conda config --remove channels defaults || exit 1
-    conda config --add channels defaults || exit 1
+echo "[update conda]"
+conda config --set ssl_verify false || exit 1
+conda config --set always_yes true --set changeps1 false || exit 1
+conda update -q conda
 
-    conda install anaconda-client
+# add the pandas channel to take priority
+# to add extra packages
+echo "[add channels]"
+conda config --add channels pandas || exit 1
+conda config --remove channels defaults || exit 1
+conda config --add channels defaults || exit 1
 
-    # Useful for debugging any issues with conda
-    conda info -a || exit 1
+conda install anaconda-client
 
+# Useful for debugging any issues with conda
+conda info -a || exit 1
+
+# set the compiler cache to work
+if [ "$USE_CACHE" ] && "${TRAVIS_OS_NAME}" == "linux" ]; then
+    echo "[Using ccache]"
+    export PATH=/usr/lib/ccache:/usr/lib64/ccache:$PATH
+    gcc=$(which gcc)
+    echo "[gcc: $gcc]"
+    ccache=$(which ccache)
+    echo "[ccache: $ccache]"
+    export CC='ccache gcc'
+else
+    echo "[Not using ccache]"
 fi
 
 # may have installation instructions for this build
