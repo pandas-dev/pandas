4b11833c0cc3709b1c15405d9f6e62daa0c21c10
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-03-08 21:11:28 +0100

    DOC: make it possible to run doctests

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -1,5 +1,8 @@
 import pytest
 
+import numpy
+import pandas
+
 
 def pytest_addoption(parser):
     parser.addoption("--skip-slow", action="store_true",
@@ -19,3 +22,11 @@ def pytest_runtest_setup(item):
 
     if 'skip' in item.keywords and item.config.getoption("--skip-network"):
         pytest.skip("skipping due to --skip-network")
+
+
+# For running doctests: make np and pd names available
+
+@pytest.fixture(autouse=True)
+def add_imports(doctest_namespace):
+    doctest_namespace['np'] = numpy
+    doctest_namespace['pd'] = pandas
