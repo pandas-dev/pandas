cd0845bf18077bd507880d5410f12e5dd7625e21
Jim Clauwaert <jimclauwaert@gmail.com>
2017-03-09 11:29:44 +0100

    change miindex to midx

--- pandas/core/indexing.py
+++ pandas/core/indexing.py
@@ -49,10 +49,10 @@ class _IndexSlice(object):
     Examples
     --------
 
-    >>> miindex = pd.MultiIndex.from_product([['A0','A1'],['B0','B1','B2','B3']])
+    >>> midx = pd.MultiIndex.from_product([['A0','A1'],['B0','B1','B2','B3']])
     >>> columns = ['foo','bar']
-    >>> dfmi = pd.DataFrame(np.arange(16).reshape((len(miindex),len(columns))),
-                          index=miindex,columns=columns)
+    >>> dfmi = pd.DataFrame(np.arange(16).reshape((len(midx),len(columns))),
+                          index=midx,columns=columns)
 
     Using the default slice command:
 
