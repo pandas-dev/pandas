eff151e74acc32b54beffc5f4600ca20c3e9673b
Michael Delgado <delgado.michaelt@gmail.com>
2017-03-11 09:53:53 -0800

    fix bug in wide_to_long_test, add GH issue numbers

--- pandas/tests/frame/test_subclass.py
+++ pandas/tests/frame/test_subclass.py
@@ -289,6 +289,31 @@ class TestDataFrameSubclassing(TestData):
         tm.assert_frame_equal(melted, expected)
         tm.assertIsInstance(melted, tm.SubclassedDataFrame)
 
+    def test_subclassed_wide_to_long(self):
+        # GH 9762
+
+        np.random.seed(123)
+        x = np.random.randn(3)
+        df = tm.SubclassedDataFrame({
+            "A1970": {0: "a", 1: "b", 2: "c"},
+            "A1980": {0: "d", 1: "e", 2: "f"},
+            "B1970": {0: 2.5, 1: 1.2, 2: .7},
+            "B1980": {0: 3.2, 1: 1.3, 2: .1},
+            "X": dict(zip(range(3), x))})
+
+        df["id"] = df.index
+        exp_data = {"X": x.tolist() + x.tolist(),
+                    "A": ['a', 'b', 'c', 'd', 'e', 'f'],
+                    "B": [2.5, 1.2, 0.7, 3.2, 1.3, 0.1],
+                    "year": ['1970', '1970', '1970', '1980', '1980', '1980'],
+                    "id": [0, 1, 2, 0, 1, 2]}
+        expected = tm.SubclassedDataFrame(exp_data)
+        expected = expected.set_index(['id', 'year'])[["X", "A", "B"]]
+        long_frame = pd.wide_to_long(df, ["A", "B"], i="id", j="year")
+        
+        tm.assert_frame_equal(long_frame, expected)
+        tm.assertIsInstance(long_frame, tm.SubclassedDataFrame)
+
     def test_to_panel_expanddim(self):
         # GH 9762
 
