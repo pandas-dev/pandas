56b5a30937e79335029b1727a80f109b7eb0840a
Jeff Reback <jeff@reback.net>
2017-03-12 19:27:30 -0400

    TST: skip scipy tests for >= 0.19.0 as needed in interpolation / window / sparse
    closes #15668

--- pandas/tests/sparse/test_frame.py
+++ pandas/tests/sparse/test_frame.py
@@ -1129,10 +1129,10 @@ class TestSparseDataFrame(tm.TestCase, SharedWithSparse):
 @pytest.mark.parametrize('index', [None, list('ab')])    # noqa: F811
 @pytest.mark.parametrize('columns', [None, list('cd')])
 @pytest.mark.parametrize('fill_value', [None, 0, np.nan])
-@pytest.mark.parametrize('dtype', [object, bool, int, float, np.uint16])
+@pytest.mark.parametrize('dtype', [bool, int, float, np.uint16])
 def test_from_to_scipy(spmatrix, index, columns, fill_value, dtype):
     # GH 4343
-    tm._skip_if_no_scipy()
+    tm.skip_if_no_package('scipy')
 
     # Make one ndarray and from it one sparse matrix, both to be used for
     # constructing frames and comparing results
@@ -1178,6 +1178,51 @@ def test_from_to_scipy(spmatrix, index, columns, fill_value, dtype):
     # However, adding a str column results in an upcast to object
     sdf['strings'] = np.arange(len(sdf)).astype(str)
     tm.assert_equal(sdf.to_coo().dtype, np.object_)
+
+
+@pytest.mark.parametrize('fill_value', [None, 0, np.nan])    # noqa: F811
+def test_from_to_scipy_object(spmatrix, fill_value):
+    # GH 4343
+    dtype = object
+    columns = list('cd')
+    index = list('ab')
+    tm.skip_if_no_package('scipy', max_version='0.19.0')
+
+    # Make one ndarray and from it one sparse matrix, both to be used for
+    # constructing frames and comparing results
+    arr = np.eye(2, dtype=dtype)
+    try:
+        spm = spmatrix(arr)
+        assert spm.dtype == arr.dtype
+    except (TypeError, AssertionError):
+        # If conversion to sparse fails for this spmatrix type and arr.dtype,
+        # then the combination is not currently supported in NumPy, so we
+        # can just skip testing it thoroughly
+        return
+
+    sdf = pd.SparseDataFrame(spm, index=index, columns=columns,
+                             default_fill_value=fill_value)
+
+    # Expected result construction is kind of tricky for all
+    # dtype-fill_value combinations; easiest to cast to something generic
+    # and except later on
+    rarr = arr.astype(object)
+    rarr[arr == 0] = np.nan
+    expected = pd.SparseDataFrame(rarr, index=index, columns=columns).fillna(
+        fill_value if fill_value is not None else np.nan)
+
+    # Assert frame is as expected
+    sdf_obj = sdf.astype(object)
+    tm.assert_sp_frame_equal(sdf_obj, expected)
+    tm.assert_frame_equal(sdf_obj.to_dense(), expected.to_dense())
+
+    # Assert spmatrices equal
+    tm.assert_equal(dict(sdf.to_coo().todok()), dict(spm.todok()))
+
+    # Ensure dtype is preserved if possible
+    res_dtype = object
+    tm.assert_contains_all(sdf.dtypes, {np.dtype(res_dtype)})
+    tm.assert_equal(sdf.to_coo().dtype, res_dtype)
 
 
 class TestSparseDataFrameArithmetic(tm.TestCase):
