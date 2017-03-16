6bad6602ae66f358d7b062d64669abdfd928afa3
Matthew Brett <matthew.brett@gmail.com>
2017-03-16 12:12:08 -0700

    MAINT: test with manylinux numpy/scipy pre-release (#15702)
    Numpy switching to daily manylinux wheels of trunk, instead of building
    wheels specific to Ubuntu 12.04 for every commit.  Use these new wheels
    for numpy pre-release testing.

--- ci/requirements-3.5_NUMPY_DEV.build.sh
+++ ci/requirements-3.5_NUMPY_DEV.build.sh
@@ -8,6 +8,7 @@ echo "install numpy master wheel"
 pip uninstall numpy -y
 
 # install numpy wheel from master
-pip install --pre --upgrade --no-index --timeout=60 --trusted-host travis-dev-wheels.scipy.org -f http://travis-dev-wheels.scipy.org/ numpy
+PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf2.rackcdn.com"
+pip install --pre --upgrade --timeout=60 -f $PRE_WHEELS numpy scipy
 
 true
