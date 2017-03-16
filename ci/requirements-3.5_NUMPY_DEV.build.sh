04974dd5b7decd3fcc4774124400c6da146c46d7
Jeff Reback <jeff@reback.net>
2017-03-16 12:20:37 -0400

    CI: remove dev-scipy from testing on numpy-dev build as really old wheels (#15699)
    closes #15696

--- ci/requirements-3.5_NUMPY_DEV.build.sh
+++ ci/requirements-3.5_NUMPY_DEV.build.sh
@@ -8,6 +8,6 @@ echo "install numpy master wheel"
 pip uninstall numpy -y
 
 # install numpy wheel from master
-pip install --pre --upgrade --no-index --timeout=60 --trusted-host travis-dev-wheels.scipy.org -f http://travis-dev-wheels.scipy.org/ numpy scipy
+pip install --pre --upgrade --no-index --timeout=60 --trusted-host travis-dev-wheels.scipy.org -f http://travis-dev-wheels.scipy.org/ numpy
 
 true
