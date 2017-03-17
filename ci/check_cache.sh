a5e1ab8653f1229fc0ec234af74309c3c556bd71
Jeff Reback <jeff@reback.net>
2017-03-17 15:24:35 -0400

    CI: remove miniconda from actual cache scripts

--- ci/check_cache.sh
+++ ci/check_cache.sh
@@ -12,14 +12,12 @@ else
     ci_changes=$(git diff PR_HEAD~2 --numstat | grep -E "ci/"| wc -l)
 fi
 
-MINICONDA_DIR="$HOME/miniconda/"
 CACHE_DIR="$HOME/.cache/"
 CCACHE_DIR="$HOME/.ccache/"
 
 if [ $ci_changes -ne 0 ]
 then
     echo "Files have changed in ci/ deleting all caches"
-    rm -rf "$MINICONDA_DIR"
     rm -rf "$CACHE_DIR"
     rm -rf "$CCACHE_DIR"
 fi
