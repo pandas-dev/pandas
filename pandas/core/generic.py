48e5e7942d4edb962c4fa51d69d070f87f985372
Sébastien de Menten <sdementen@gmail.com>
2017-03-20 06:20:43 +0100

    use rs[mask] instead of rs.mask(mask,...

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -5784,7 +5784,7 @@ class NDFrame(PandasObject):
                                   **kwargs)) - 1)
         if freq is None:
             mask = isnull(_values_from_object(self))
-            rs.mask(mask, np.nan, inplace=True)
+            rs[mask] = np.nan
         return rs
 
     def _agg_by_level(self, name, axis=0, level=0, skipna=True, **kwargs):
