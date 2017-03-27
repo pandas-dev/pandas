13ecd21eac6e042931075e373daf610d321288fc
Jeff Reback <jeff@reback.net>
2017-03-27 10:06:32 -0400

    CI: make generic 3.5 builds
    CI: rename BUILD_DOC -> DOC

--- ci/script_multi.sh
+++ ci/script_multi.sh
@@ -4,11 +4,6 @@ echo "[script multi]"
 
 source activate pandas
 
-# don't run the tests for the doc build
-if [ x"$DOC_BUILD" != x"" ]; then
-    exit 0
-fi
-
 if [ -n "$LOCALE_OVERRIDE" ]; then
     export LC_ALL="$LOCALE_OVERRIDE";
     echo "Setting LC_ALL to $LOCALE_OVERRIDE"
@@ -26,6 +21,8 @@ echo PYTHONHASHSEED=$PYTHONHASHSEED
 if [ "$BUILD_TEST" ]; then
     cd /tmp
     python -c "import pandas; pandas.test(['-n 2'])"
+elif [ "$DOC" ]; then
+    echo "We are not running pytest as this is a doc-build"
 elif [ "$COVERAGE" ]; then
     echo pytest -s -n 2 -m "not single" --cov=pandas --cov-report xml:/tmp/cov-multiple.xml --junitxml=/tmp/multiple.xml $TEST_ARGS pandas
     pytest -s -n 2 -m "not single" --cov=pandas --cov-report xml:/tmp/cov-multiple.xml --junitxml=/tmp/multiple.xml $TEST_ARGS pandas
