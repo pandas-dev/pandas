512d5ef94ab9fa63f66d9b92a22e644c2033200a
Jeff Reback <jeff@reback.net>
2017-03-28 12:48:41 -0400

    BUG: bug in .at/.loc indexing with a tz-aware columns
    closes #15822
    
    Author: Jeff Reback <jeff@reback.net>
    
    Closes #15827 from jreback/at and squashes the following commits:
    
    4fcd2c6 [Jeff Reback] BUG: bug in .at/.loc indexing with a tz-aware columns

--- pandas/tests/indexing/test_scalar.py
+++ pandas/tests/indexing/test_scalar.py
@@ -154,3 +154,18 @@ class TestScalar(Base, tm.TestCase):
         # Check that we get the correct value in the KeyError
         self.assertRaisesRegexp(KeyError, r"\['y'\] not in index",
                                 lambda: df[['x', 'y', 'z']])
+
+    def test_at_with_tz(self):
+        # gh-15822
+        df = DataFrame({'name': ['John', 'Anderson'],
+                        'date': [Timestamp(2017, 3, 13, 13, 32, 56),
+                                 Timestamp(2017, 2, 16, 12, 10, 3)]})
+        df['date'] = df['date'].dt.tz_localize('Asia/Shanghai')
+
+        expected = Timestamp('2017-03-13 13:32:56+0800', tz='Asia/Shanghai')
+
+        result = df.loc[0, 'date']
+        assert result == expected
+
+        result = df.at[0, 'date']
+        assert result == expected
