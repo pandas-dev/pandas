0ab081345eb191937fd4152eba48b8c9692b02bf
Brian <sbhuey@gmail.com>
2017-03-29 19:24:40 -0400

    ENH: read_html() handles tables with multiple header rows #13434
    closes #13434
    
    Author: Brian <sbhuey@gmail.com>
    Author: S. Brian Huey <brianhuey@users.noreply.github.com>
    
    Closes #15242 from brianhuey/thead-improvement and squashes the following commits:
    
    fc1c80e [S. Brian Huey] Merge branch 'master' into thead-improvement
    b54aa0c [Brian] removed duplicate test case
    6ae2860 [Brian] updated docstring and io.rst
    41fe8cd [Brian] review changes
    873ea58 [Brian] switched from range to lrange
    cd70225 [Brian] ENH:read_html() handles tables with multiple header rows #13434

--- pandas/tests/io/test_html.py
+++ pandas/tests/io/test_html.py
@@ -760,6 +760,18 @@ class TestReadHtml(tm.TestCase, ReadHtmlMixin):
         html_df = read_html(html_data, keep_default_na=True)[0]
         tm.assert_frame_equal(expected_df, html_df)
 
+    def test_multiple_header_rows(self):
+        # Issue #13434
+        expected_df = DataFrame(data=[("Hillary", 68, "D"),
+                                      ("Bernie", 74, "D"),
+                                      ("Donald", 69, "R")])
+        expected_df.columns = [["Unnamed: 0_level_0", "Age", "Party"],
+                               ["Name", "Unnamed: 1_level_1",
+                                "Unnamed: 2_level_1"]]
+        html = expected_df.to_html(index=False)
+        html_df = read_html(html, )[0]
+        tm.assert_frame_equal(expected_df, html_df)
+
 
 def _lang_enc(filename):
     return os.path.splitext(os.path.basename(filename))[0].split('_')
