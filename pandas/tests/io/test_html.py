b54aa0cc8d4afaae33ff57907699d819b9866dde
Brian <sbhuey@gmail.com>
2017-03-29 14:04:12 -0700

    removed duplicate test case

--- pandas/tests/io/test_html.py
+++ pandas/tests/io/test_html.py
@@ -881,18 +881,6 @@ class TestReadHtmlLxml(tm.TestCase, ReadHtmlMixin):
         data = os.path.join(DATA_PATH, 'computer_sales_page.html')
         self.read_html(data, header=[0, 1])
 
-    def test_multiple_header_rows(self):
-        # Issue #13434
-        expected_df = DataFrame(data=[("Hillary", 68, "D"),
-                                      ("Bernie", 74, "D"),
-                                      ("Donald", 69, "R")])
-        expected_df.columns = [["Unnamed: 0_level_0", "Age", "Party"],
-                               ["Name", "Unnamed: 1_level_1",
-                                "Unnamed: 2_level_1"]]
-        html = expected_df.to_html(index=False)
-        html_df = read_html(html, )[0]
-        tm.assert_frame_equal(expected_df, html_df)
-
 
 def test_invalid_flavor():
     url = 'google.com'
