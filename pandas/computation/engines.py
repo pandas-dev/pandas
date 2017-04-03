ccb44cceb10974111586cf4c97954aed865ac2e8
gfyoung <gfyoung17@gmail.com>
2017-04-02 22:36:30 -0400

    CLN: Make engines.py flake8-able

--- pandas/computation/engines.py
+++ pandas/computation/engines.py
@@ -1,13 +1,11 @@
-"""Engine classes for :func:`~pandas.eval`
 """
-
-# flake8: noqa
+Engine classes for :func:`~pandas.eval`
+"""
 
 import abc
 
 from pandas import compat
-from pandas.compat import DeepChainMap, map
-import pandas.core.common as com
+from pandas.compat import map
 import pandas.formats.printing as printing
 from pandas.computation.align import _align, _reconstruct_object
 from pandas.computation.ops import (UndefinedVariableError,
