c25fbde09272f369f280212e5216441d5975687c
Jeff Reback <jeff@reback.net>
2017-04-07 15:09:09 -0400

    DEPR: Panel deprecated
    closes #13563
    on top of #15677
    
    Author: Jeff Reback <jeff@reback.net>
    
    Closes #15601 from jreback/panel and squashes the following commits:
    
    04104a7 [Jeff Reback] fine grained catching warnings in tests
    f8800dc [Jeff Reback] add numpy reference for searchsorted
    fa136dd [Jeff Reback] doc correction
    c39453a [Jeff Reback] add perf optimization in searchsorted for FrozenNDArray
    0e9c4a4 [Jeff Reback] fix docs as per review & column name changes
    3df0abe [Jeff Reback] remove Panel from doc-strings, catch internal warning on Panel construction
    755606d [Jeff Reback] more docs
    d04db2e [Jeff Reback] add deprecate_panel section to docs
    538b8e8 [Jeff Reback] pep fix
    912d523 [Jeff Reback] TST: separate out test_append_to_multiple_dropna to two tests; when drop=False this is sometimes failing
    a2625ba [Jeff Reback] remove most Term references in test_pytables.py
    cd5b6b8 [Jeff Reback] DEPR: Panel deprecated
    6b20ddc [Jeff Reback] fix names on return structure
    f41d3df [Jeff Reback] API: df.rolling(..).corr()/cov() when pairwise=True to return MI DataFrame
    84e788b [Jeff Reback] BUG/PERF: handle a slice correctly in get_level_indexer

--- pandas/indexes/frozen.py
+++ pandas/indexes/frozen.py
@@ -117,6 +117,30 @@ class FrozenNDArray(PandasObject, np.ndarray):
                              quote_strings=True)
         return "%s(%s, dtype='%s')" % (type(self).__name__, prepr, self.dtype)
 
+    def searchsorted(self, v, side='left', sorter=None):
+        """
+        Find indices where elements of v should be inserted
+        in a to maintain order.
+
+        For full documentation, see `numpy.searchsorted`
+
+        See Also
+        --------
+        numpy.searchsorted : equivalent function
+        """
+
+        # we are much more performant if the searched
+        # indexer is the same type as the array
+        # this doesn't matter for int64, but DOES
+        # matter for smaller int dtypes
+        # https://github.com/numpy/numpy/issues/5370
+        try:
+            v = self.dtype.type(v)
+        except:
+            pass
+        return super(FrozenNDArray, self).searchsorted(
+            v, side=side, sorter=sorter)
+
 
 def _ensure_frozen(array_like, categories, copy=False):
     array_like = coerce_indexer_dtype(array_like, categories)
