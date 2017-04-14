b88f06b838856c1ba7472ded159984309bb49055
Sarma Tangirala <tvssarma.omega9@gmail.com>
2017-04-14 09:28:03 -0400

    ENH: add option to sort class labels in parallel_coordinates (#15908)
    closes #15908
    
    Author: Sarma Tangirala <tvssarma.omega9@gmail.com>
    
    Closes #15935 from stangirala/master and squashes the following commits:
    
    1467f9f [Sarma Tangirala] Add minor code change, what's new doc fix
    3ede37a [Sarma Tangirala] Move feature test to new method, add to whatsnew
    756e8d8 [Sarma Tangirala] ENH: Minor change to parallel_coordinates (#15908)

--- pandas/tests/plotting/test_misc.py
+++ pandas/tests/plotting/test_misc.py
@@ -241,6 +241,26 @@ class TestDataFramePlots(TestPlotBase):
         with tm.assert_produces_warning(FutureWarning):
             parallel_coordinates(df, 'Name', colors=colors)
 
+    def test_parallel_coordinates_with_sorted_labels(self):
+        """ For #15908 """
+        from pandas.tools.plotting import parallel_coordinates
+
+        df = DataFrame({"feat": [i for i in range(30)],
+                        "class": [2 for _ in range(10)] +
+                        [3 for _ in range(10)] +
+                        [1 for _ in range(10)]})
+        ax = parallel_coordinates(df, 'class', sort_labels=True)
+        polylines, labels = ax.get_legend_handles_labels()
+        color_label_tuples = \
+            zip([polyline.get_color() for polyline in polylines], labels)
+        ordered_color_label_tuples = sorted(color_label_tuples,
+                                            key=lambda x: x[1])
+        prev_next_tupels = zip([i for i in ordered_color_label_tuples[0:-1]],
+                               [i for i in ordered_color_label_tuples[1:]])
+        for prev, nxt in prev_next_tupels:
+            # lables and colors are ordered strictly increasing
+            assert prev[1] < nxt[1] and prev[0] < nxt[0]
+
     @slow
     def test_radviz(self):
         from pandas.tools.plotting import radviz
