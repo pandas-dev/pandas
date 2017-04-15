f1505d7367ed6a4e5290d33919dd1de806fd5191
Jeff Reback <jeff.reback@twosigma.com>
2017-04-15 17:42:29 -0400

    try with pyargs

--- pandas/util/_tester.py
+++ pandas/util/_tester.py
@@ -1,11 +1,6 @@
 """
 Entrypoint for testing from the top-level namespace
 """
-import os
-import sys
-
-PKG = os.path.dirname(os.path.dirname(__file__))
-
 
 try:
     import pytest
@@ -19,7 +14,7 @@ else:
             if not isinstance(extra_args, list):
                 extra_args = [extra_args]
             cmd = extra_args
-        cmd += [PKG]
+        cmd += ['--pyargs pandas']
         print("running: pytest {}".format(' '.join(cmd)))
         sys.exit(pytest.main(cmd))
 
