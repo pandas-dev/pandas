467c264867ddabbbfab70dc948c1715076e6979a
Jeff Reback <jeff.reback@twosigma.com>
2017-04-16 14:31:12 -0400

    CLN: move infer_dtype to pandas.api.types

--- pandas/api/lib/__init__.py
+++
@@ -1,5 +0,0 @@
-# flake8: noqa
-
-""" public toolkit API """
-
-from pandas._libs.lib import infer_dtype
--- pandas/api/types/__init__.py
+++ pandas/api/types/__init__.py
@@ -2,4 +2,5 @@
 
 from pandas.core.dtypes.api import *  # noqa
 from pandas.core.dtypes.concat import union_categoricals  # noqa
+from pandas._libs.lib import infer_dtype  # noqa
 del np  # noqa
