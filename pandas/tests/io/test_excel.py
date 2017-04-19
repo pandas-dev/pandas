e2cfa779d91dfbc828880dbe04216a5ea4dca811
Joel Nothman <joel.nothman@gmail.com>
2017-04-19 10:05:23 +1000

    Test Styler.to_excel

--- pandas/tests/io/test_excel.py
+++ pandas/tests/io/test_excel.py
@@ -16,6 +16,7 @@ import numpy as np
 
 import pandas as pd
 from pandas import DataFrame, Index, MultiIndex
+from pandas.formats.excel import ExcelFormatter
 from pandas.io.parsers import read_csv
 from pandas.io.excel import (
     ExcelFile, ExcelWriter, read_excel, _XlwtWriter, _Openpyxl1Writer,
@@ -25,10 +26,6 @@ from pandas.io.common import URLError
 from pandas.util.testing import ensure_clean, makeCustomDataframe as mkdf
 from pandas.core.config import set_option, get_option
 import pandas.util.testing as tm
-
-
-# FIXME: run all/some tests with plain Styler instead of DataFrame
-# FIXME: run some tests with styled Styler
 
 
 def _skip_if_no_xlrd():
@@ -2152,6 +2149,126 @@ class Openpyxl22Tests(ExcelWriterBase, tm.TestCase):
             xcell_a2 = wks['A2']
             self.assertEqual(xcell_b1.font, openpyxl_sty_merged)
             self.assertEqual(xcell_a2.font, openpyxl_sty_merged)
+
+    def test_styler_to_excel(self):
+        if not openpyxl_compat.is_compat(major_ver=2):
+            pytest.skip('incompatible openpyxl version')
+
+        def style(df):
+            return DataFrame([['font-weight: bold', '', ''],
+                              ['', 'color: blue', ''],
+                              ['', '', 'text-decoration: underline'],
+                              ['border-style: solid', '', ''],
+                              ['', 'font-style: italic', ''],
+                              ['', '', 'text-align: right'],
+                              ['background-color: red', '', ''],
+                              ['', '', ''],
+                              ['', '', ''],
+                              ['', '', '']],
+                             index=df.index, columns=df.columns)
+
+        def assert_equal_style(cell1, cell2):
+            # XXX: should find a better way to check equality
+            assert cell1.alignment.__dict__ == cell2.alignment.__dict__
+            assert cell1.border.__dict__ == cell2.border.__dict__
+            assert cell1.fill.__dict__ == cell2.fill.__dict__
+            assert cell1.font.__dict__ == cell2.font.__dict__
+            assert cell1.number_format == cell2.number_format
+            assert cell1.protection.__dict__ == cell2.protection.__dict__
+
+        def custom_converter(css):
+            # use bold iff there is custom style attached to the cell
+            if css.strip(' \n;'):
+                return {'font': {'bold': True}}
+            return {}
+
+        # Prepare spreadsheets
+
+        df = DataFrame(np.random.randn(10, 3))
+        with ensure_clean('.xlsx') as path:
+            writer = _Openpyxl22Writer(path)
+            df.to_excel(writer, engine='openpyxl', sheet_name='frame')
+            df.style.to_excel(writer, engine='openpyxl', sheet_name='unstyled')
+            styled = df.style.apply(style, axis=None)
+            styled.to_excel(writer, engine='openpyxl', sheet_name='styled')
+            ExcelFormatter(styled, style_converter=custom_converter).write(
+                writer, engine='openpyxl', sheet_name='custom')
+
+        # (1) compare DataFrame.to_excel and Styler.to_excel when unstyled
+        n_cells = 0
+        for col1, col2 in zip(writer.sheets['frame'],
+                              writer.sheets['unstyled']):
+            assert len(col1) == len(col2)
+            for cell1, cell2 in zip(col1, col2):
+                assert cell1.value == cell2.value
+                assert_equal_style(cell1, cell2)
+                n_cells += 1
+
+        # ensure iteration actually happened:
+        assert n_cells == (10 + 1) * (3 + 1)
+
+        # (2) check styling with default converter
+        n_cells = 0
+        for col1, col2 in zip(writer.sheets['frame'],
+                              writer.sheets['styled']):
+            assert len(col1) == len(col2)
+            for cell1, cell2 in zip(col1, col2):
+                ref = '%s%d' % (cell2.column, cell2.row)
+                # XXX: this isn't as strong a test as ideal; we should
+                #      differences are exclusive
+                if ref == 'B2':
+                    assert not cell1.font.bold
+                    assert cell2.font.bold
+                elif ref == 'C3':
+                    assert cell1.font.color.rgb != cell2.font.color.rgb
+                    assert cell2.font.color.rgb == '000000FF'
+                elif ref == 'D4':
+                    assert cell1.font.underline != cell2.font.underline
+                    assert cell2.font.underline == 'single'
+                elif ref == 'B5':
+                    assert not cell1.border.left.style
+                    assert (cell2.border.top.style ==
+                            cell2.border.right.style ==
+                            cell2.border.bottom.style ==
+                            cell2.border.left.style ==
+                            'medium')
+                elif ref == 'C6':
+                    assert not cell1.font.italic
+                    assert cell2.font.italic
+                elif ref == 'D7':
+                    assert (cell1.alignment.horizontal !=
+                            cell2.alignment.horizontal)
+                    assert cell2.alignment.horizontal == 'right'
+                elif ref == 'B8':
+                    assert cell1.fill.fgColor.rgb != cell2.fill.fgColor.rgb
+                    assert cell1.fill.patternType != cell2.fill.patternType
+                    assert cell2.fill.fgColor.rgb == '00FF0000'
+                    assert cell2.fill.patternType == 'solid'
+                else:
+                    assert_equal_style(cell1, cell2)
+
+                assert cell1.value == cell2.value
+                n_cells += 1
+
+        assert n_cells == (10 + 1) * (3 + 1)
+
+        # (3) check styling with custom converter
+        n_cells = 0
+        for col1, col2 in zip(writer.sheets['frame'],
+                              writer.sheets['custom']):
+            assert len(col1) == len(col2)
+            for cell1, cell2 in zip(col1, col2):
+                ref = '%s%d' % (cell2.column, cell2.row)
+                if ref in ('B2', 'C3', 'D4', 'B5', 'C6', 'D7', 'B8'):
+                    assert not cell1.font.bold
+                    assert cell2.font.bold
+                else:
+                    assert_equal_style(cell1, cell2)
+
+                assert cell1.value == cell2.value
+                n_cells += 1
+
+        assert n_cells == (10 + 1) * (3 + 1)
 
 
 class XlwtTests(ExcelWriterBase, tm.TestCase):
