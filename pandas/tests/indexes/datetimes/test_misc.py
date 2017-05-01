9d0b634cab7335d092365672ca2eb79a0aec53ef
Matthew Roeschke <emailformattr@gmail.com>
2017-05-01 04:34:19 -0700

    TST: DatetimeIndex and its Timestamp elements returning same .weekofyear with tz (#16181)

--- pandas/tests/indexes/datetimes/test_misc.py
+++ pandas/tests/indexes/datetimes/test_misc.py
@@ -334,6 +334,14 @@ class TestDatetime64(tm.TestCase):
         for ts, value in tests:
             assert ts == value
 
+        # GH 6538: Check that DatetimeIndex and its TimeStamp elements
+        # return the same weekofyear accessor close to new year w/ tz
+        dates = ["2013/12/29", "2013/12/30", "2013/12/31"]
+        dates = DatetimeIndex(dates, tz="Europe/Brussels")
+        expected = [52, 1, 1]
+        assert dates.weekofyear.tolist() == expected
+        assert [d.weekofyear for d in dates] == expected
+
     def test_nanosecond_field(self):
         dti = DatetimeIndex(np.arange(10))
 
