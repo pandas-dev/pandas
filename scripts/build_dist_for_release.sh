b9798161b37109020048c101753fd904536970ad
Jeff Reback <jeff@reback.net>
2017-05-06 18:05:06 -0400

    TST: release testing of downstream packages (#16261)

--- scripts/build_dist_for_release.sh
+++ scripts/build_dist_for_release.sh
@@ -2,7 +2,7 @@
 
 # this requires cython to be installed
 
-# this builds the release cleanly
+# this builds the release cleanly & is building on the current checkout
 rm -rf dist
 git clean -xfd
 python setup.py clean
