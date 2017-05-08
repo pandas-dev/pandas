0bf7a1904ff5884bdae4f9b637f3dbd82b967ba1
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-05-08 15:46:11 -0500

    DOC: Start docs on mypy

--- ci/typing.sh
+++ ci/typing.sh
@@ -6,18 +6,13 @@ source activate pandas
 
 RET=0
 
-if [ "$TYPING" ]; then
-
-    echo "Typing  *.py"
-    mypy \
-        pandas/core/base.py
-    if [ $? -ne "0" ]; then
-        RET=1
-    fi
-    echo "Typing *.py DONE"
-
-else
-    echo "NOT checking typing"
+echo "Typing  *.py"
+mypy \
+    pandas/core/base.py
+if [ $? -ne "0" ]; then
+    RET=1
 fi
+echo "Typing *.py DONE"
+
 
 exit $RET
