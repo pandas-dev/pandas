2c5715b7530c28031903a401385bac17712f1de3
gfyoung <gfyoung17@gmail.com>
2017-05-09 11:04:46 -0400

    DEPS: Drop Python 3.4 support
    Closes gh-15251.

--- setup.py
+++ setup.py
@@ -246,7 +246,6 @@ CLASSIFIERS = [
     'Programming Language :: Python :: 2',
     'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 2.7',
-    'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
     'Programming Language :: Cython',
