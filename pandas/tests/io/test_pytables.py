0605fd50b794af6223d61d2b90d4bff5a5dc38c8
chris-b1 <cbartak@gmail.com>
2017-05-12 08:29:59 -0500

    BUG: pathlib.Path in io (#16292)
    * BUG: pathlib.Path in io
    
    * CLN: factor out pathlib roundtrip
    
    * add localpath tests for other io
    
    * fixup
    
    * xfail SAS; type in parser
    
    * missing import
    
    * xfail for #14704
    
    * fix to_csv
    
    * lint
    
    * lint cleanup
    
    * add feather (xfail)
    
    (cherry picked from commit 4cd84582d5ad0fdac5085b12a1affeb6300ba3a3)

--- pandas/tests/io/test_pytables.py
+++ pandas/tests/io/test_pytables.py
@@ -4282,6 +4282,49 @@ class TestHDFStore(Base):
             result = store.select('frame', [crit])
             tm.assert_frame_equal(result, df.loc[:, df.columns[:75:2]])
 
+    def test_path_pathlib(self):
+        df = tm.makeDataFrame()
+
+        result = tm.round_trip_pathlib(
+            lambda p: df.to_hdf(p, 'df'),
+            lambda p: pd.read_hdf(p, 'df'))
+        tm.assert_frame_equal(df, result)
+
+    @pytest.mark.xfail(reason='pathlib currently doesnt work with HDFStore')
+    def test_path_pathlib_hdfstore(self):
+        df = tm.makeDataFrame()
+
+        def writer(path):
+            with pd.HDFStore(path) as store:
+                df.to_hdf(store, 'df')
+
+        def reader(path):
+            with pd.HDFStore(path) as store:
+                pd.read_hdf(store, 'df')
+        result = tm.round_trip_pathlib(writer, reader)
+        tm.assert_frame_equal(df, result)
+
+    def test_pickle_path_localpath(self):
+        df = tm.makeDataFrame()
+        result = tm.round_trip_pathlib(
+            lambda p: df.to_hdf(p, 'df'),
+            lambda p: pd.read_hdf(p, 'df'))
+        tm.assert_frame_equal(df, result)
+
+    @pytest.mark.xfail(reason='localpath currently doesnt work with HDFStore')
+    def test_path_localpath_hdfstore(self):
+        df = tm.makeDataFrame()
+
+        def writer(path):
+            with pd.HDFStore(path) as store:
+                df.to_hdf(store, 'df')
+
+        def reader(path):
+            with pd.HDFStore(path) as store:
+                pd.read_hdf(store, 'df')
+        result = tm.round_trip_localpath(writer, reader)
+        tm.assert_frame_equal(df, result)
+
     def _check_roundtrip(self, obj, comparator, compression=False, **kwargs):
 
         options = {}
