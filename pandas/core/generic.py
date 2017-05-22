4d0ce5da8be9966ed26dd39eb3c977a0f3eafa24
VincentLa <vincela14@gmail.com>
2017-05-22 16:24:14 -0700

    adding examples for drop method

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -2055,6 +2055,36 @@ class NDFrame(PandasObject, SelectionMixin):
         Returns
         -------
         dropped : type of caller
+
+        Examples
+        --------
+        >>> df = pd.DataFrame([[np.nan, 2, np.nan, 0],
+        ...                    [3, 4, np.nan, 1],
+        ...                    [np.nan, np.nan, np.nan, 5],
+        ...                    [np.nan, 3, np.nan, 4]
+        ...                   ],
+        ...                   columns=list('ABCD'))
+        >>> df
+            A   B   C   D
+        0   NaN 2.0 NaN 0
+        1   3.0 4.0 NaN 1
+        2   NaN NaN NaN 5
+        3   NaN 3.0 NaN 4
+
+        Drop a row by index
+
+        >>> df.drop([0, 1])
+            A   B   C   D
+        2   NaN NaN NaN 5
+        3   NaN 3.0 NaN 4
+
+        Drop columns
+        >>> df.drop(['A', 'B'], axis=1)
+            C   D
+        0   NaN 0
+        1   NaN 1
+        2   NaN 5
+        3   NaN 4
         """
         inplace = validate_bool_kwarg(inplace, 'inplace')
         axis = self._get_axis_number(axis)
