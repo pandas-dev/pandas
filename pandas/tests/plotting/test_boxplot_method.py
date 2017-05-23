bab6ea2813a37d52dba36f4c6415936209143d4a
Hugues Valois <huvalo@microsoft.com>
2017-05-23 10:42:13 -0700

    Fix style violations.

--- pandas/tests/plotting/test_boxplot_method.py
+++ pandas/tests/plotting/test_boxplot_method.py
@@ -162,8 +162,9 @@ class TestDataFramePlots(TestPlotBase):
 
     @slow
     def test_figsize(self):
-        df = DataFrame(np.random.rand(10, 5), columns=['A', 'B', 'C', 'D', 'E'])
-        result = df.boxplot(return_type='axes', figsize=(12,8))
+        df = DataFrame(np.random.rand(10, 5),
+                       columns=['A', 'B', 'C', 'D', 'E'])
+        result = df.boxplot(return_type='axes', figsize=(12, 8))
         assert result.figure.bbox_inches.width == 12
         assert result.figure.bbox_inches.height == 8
 
