BUG: fixed wrong order of ordered labels in pd.cut()
closes #16459

Author: economy <the.economy@gmail.com>

This patch had conflicts when merged, resolved by
Committer: Jeff Reback <jeff.reback@twosigma.com>

Closes #16466 from economy/fix_cut and squashes the following commits:

29128b3 [economy] comments and whatsnew edits
3898b72 [economy] BUG: fixed wrong order of ordered labels in pd.cut()

(cherry picked from commit d419be4333dcb8cf643bdd04c7c4e990feae49f9)

pandas/core/reshape/tile.py
@@ -252,7 +252,7 @@ def _bins_to_cuts(x, bins, right=True, labels=None,
                 raise ValueError('Bin labels must be one fewer than '
                                  'the number of bin edges')
         if not is_categorical_dtype(labels):
-            labels = Categorical(labels, ordered=True)
+            labels = Categorical(labels, categories=labels, ordered=True)
 
         np.putmask(ids, na_mask, 0)
         result = algos.take_nd(labels, ids - 1)