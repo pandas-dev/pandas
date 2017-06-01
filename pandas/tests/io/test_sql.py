7b106e44286187f6b8fed2d6124c8b3c33a922e9
Hugues Valois <huvalo@microsoft.com>
2017-06-01 12:31:52 -0700

    TST: writing invalid table names to sqlite (#16464)
    * Add test for bug #13206.
    
    * Improve test by reading back the values from sql and comparing. Also fixes coding style violation.

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -816,6 +816,16 @@ class _TestSQLApi(PandasSQLTest):
         df = DataFrame([[1, 2], [3, 4]], columns=[u'\xe9', u'b'])
         df.to_sql('test_unicode', self.conn, index=False)
 
+    def test_escaped_table_name(self):
+        # GH 13206
+        df = DataFrame({'A': [0, 1, 2], 'B': [0.2, np.nan, 5.6]})
+        df.to_sql('d1187b08-4943-4c8d-a7f6', self.conn, index=False)
+
+        res = sql.read_sql_query('SELECT * FROM `d1187b08-4943-4c8d-a7f6`',
+                                 self.conn)
+
+        tm.assert_frame_equal(res, df)
+
 
 @pytest.mark.single
 class TestSQLApi(SQLAlchemyMixIn, _TestSQLApi):
