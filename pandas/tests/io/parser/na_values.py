966fe2c6c07a5a7648fce125669a8f7cce15d783
Chris Filo Gorgolewski <krzysztof.gorgolewski@gmail.com>
2017-06-01 17:28:09 -0700

    Adding 'n/a' to list of strings denoting missing values (#16079)

--- pandas/tests/io/parser/na_values.py
+++ pandas/tests/io/parser/na_values.py
@@ -70,8 +70,8 @@ NaN,nan
 
     def test_default_na_values(self):
         _NA_VALUES = set(['-1.#IND', '1.#QNAN', '1.#IND', '-1.#QNAN',
-                          '#N/A', 'N/A', 'NA', '#NA', 'NULL', 'null', 'NaN',
-                          'nan', '-NaN', '-nan', '#N/A N/A', ''])
+                          '#N/A', 'N/A', 'n/a', 'NA', '#NA', 'NULL', 'null',
+                          'NaN', 'nan', '-NaN', '-nan', '#N/A N/A', ''])
         assert _NA_VALUES == parsers._NA_VALUES
         nv = len(_NA_VALUES)
 
