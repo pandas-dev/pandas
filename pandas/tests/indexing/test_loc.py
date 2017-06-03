d13082430513a0ebcb61f0c4ad79eebeab27194f
Margaret Sy <margaretsy1016@gmail.com>
2017-06-03 10:39:10 -0700

    add tests for direct slicing

--- pandas/tests/indexing/test_loc.py
+++ pandas/tests/indexing/test_loc.py
@@ -636,6 +636,7 @@ Region_1,Site_2,3977723089,A,5/20/2015 8:33,5/20/2015 9:09,Yes,No"""
         original_df = DataFrame({'a': [1, 2, 3]})
         sliced_df = original_df.loc[:]
         assert sliced_df is not original_df
+        assert original_df[:] is not original_df
 
         # should be a shallow copy
         original_df['a'] = [4, 4, 4]
@@ -650,6 +651,7 @@ Region_1,Site_2,3977723089,A,5/20/2015 8:33,5/20/2015 9:09,Yes,No"""
         original_series = Series([1, 2, 3, 4, 5, 6])
         sliced_series = original_series.loc[:]
         assert sliced_series is not original_series
+        assert original_series[:] is not original_series
 
         original_series[:3] = [7, 8, 9]
         assert all(sliced_series[:3] == [7, 8, 9])
