6d236258f9690ec94a50cb69f369c2f572bb4b05
Jean-Mathieu Deschenes <jean-mathieu.deschenes@cae.com>
2017-06-08 09:40:32 -0400

    BUG: TimedeltaIndex raising ValueError when slice indexing (#16637)
    closes #16637

--- pandas/tests/indexing/test_timedelta.py
+++ pandas/tests/indexing/test_timedelta.py
@@ -18,3 +18,20 @@ class TestTimedeltaIndexing(object):
                                     index=pd.to_timedelta(range(10), unit='s'),
                                     columns=['x'])
             tm.assert_frame_equal(expected, result)
+
+    def test_slice_indexing(self):
+        # GH 16637
+        df = pd.DataFrame({'x': range(10)})
+        df.index = pd.to_timedelta(range(10), unit='s')
+
+        conditions = [df.index[0], df.index[4:8], df.index[[3, 5]]]
+        expected_data = [[20, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+                         [0, 1, 2, 3, 20, 20, 20, 20, 8, 9],
+                         [0, 1, 2, 20, 4, 20, 6, 7, 8, 9]]
+        for cond, data in zip(conditions, expected_data):
+            result = df.copy()
+            result.loc[cond, 'x'] = 20
+            expected = pd.DataFrame(data,
+                                    index=pd.to_timedelta(range(10), unit='s'),
+                                    columns=['x'])
+            tm.assert_frame_equal(expected, result)
