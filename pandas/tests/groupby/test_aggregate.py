5afc7436a49f79bf55526b7719de1a80ec31c0c5
gfyoung <gfyoung17@gmail.com>
2017-06-11 16:08:56 -0700

    TST: Add test for groupby mean of ints (#16670)
    Closes gh-11199.

--- pandas/tests/groupby/test_aggregate.py
+++ pandas/tests/groupby/test_aggregate.py
@@ -234,6 +234,27 @@ class TestGroupByAggregate(object):
         expected = grouped.mean()
         tm.assert_frame_equal(result, expected)
 
+    def test_aggregate_float64_no_int64(self):
+        # see gh-11199
+        df = DataFrame({"a": [1, 2, 3, 4, 5],
+                        "b": [1, 2, 2, 4, 5],
+                        "c": [1, 2, 3, 4, 5]})
+
+        expected = DataFrame({"a": [1, 2.5, 4, 5]},
+                             index=[1, 2, 4, 5])
+        expected.index.name = "b"
+
+        result = df.groupby("b")[["a"]].mean()
+        tm.assert_frame_equal(result, expected)
+
+        expected = DataFrame({"a": [1, 2.5, 4, 5],
+                              "c": [1, 2.5, 4, 5]},
+                             index=[1, 2, 4, 5])
+        expected.index.name = "b"
+
+        result = df.groupby("b")[["a", "c"]].mean()
+        tm.assert_frame_equal(result, expected)
+
     def test_aggregate_api_consistency(self):
         # GH 9052
         # make sure that the aggregates via dict
