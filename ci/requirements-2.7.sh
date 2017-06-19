a6fac413bebc0b31479806670666067015225d7b
Jeff Reback <jeff.reback@twosigma.com>
2017-06-19 06:11:58 -0400

    CI: pin jemalloc=4.4.0

--- ci/requirements-2.7.sh
+++ ci/requirements-2.7.sh
@@ -4,4 +4,4 @@ source activate pandas
 
 echo "install 27"
 
-conda install -n pandas -c conda-forge feather-format
+conda install -n pandas -c conda-forge feather-format jemalloc=4.4.0
--- ci/requirements-3.6_DOC.sh
+++ ci/requirements-3.6_DOC.sh
@@ -6,6 +6,6 @@ echo "[install DOC_BUILD deps]"
 
 pip install pandas-gbq
 
-conda install -n pandas -c conda-forge feather-format nbsphinx pandoc
+conda install -n pandas -c conda-forge feather-format nbsphinx pandoc jemalloc=4.4.0
 
 conda install -n pandas -c r r rpy2 --yes
