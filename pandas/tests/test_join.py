081c3ced906411c78e6b290efd3e9892de1ef76b
thequackdaddy <pquack@gmail.com>
2017-06-28 19:54:30 -0500

    Fixed some documentation/formatting issues, clarified the purpose of the test case.

--- pandas/tests/test_join.py
+++ pandas/tests/test_join.py
@@ -207,12 +207,13 @@ def test_merge_join_categorical_multiindex():
          'Factor': [1.1, 1.2, 1.3, 1.4, 1.5, 1.6]}
     b = DataFrame(b).set_index(['Cat', 'Int'])['Factor']
 
-    c = merge(a, b.reset_index(), left_on=['Cat1', 'Int1'],
-              right_on=['Cat', 'Int'], how='left')
-    d = a.join(b, on=['Cat1', 'Int1'])
-    c = c.drop(['Cat', 'Int'], axis=1)
-    assert_frame_equal(c, d)
+    expected = merge(a, b.reset_index(), left_on=['Cat1', 'Int1'],
+                     right_on=['Cat', 'Int'], how='left')
+    result = a.join(b, on=['Cat1', 'Int1'])
+    expected = expected.drop(['Cat', 'Int'], axis=1)
+    assert_frame_equal(expected, result)
 
+    # Same test, but with ordered categorical
     a = {'Cat1': Categorical(['a', 'b', 'a', 'c', 'a', 'b'],
                              ['b', 'a', 'c'],
                              ordered=True),
@@ -226,8 +227,8 @@ def test_merge_join_categorical_multiindex():
          'Factor': [1.1, 1.2, 1.3, 1.4, 1.5, 1.6]}
     b = DataFrame(b).set_index(['Cat', 'Int'])['Factor']
 
-    c = merge(a, b.reset_index(), left_on=['Cat1', 'Int1'],
-              right_on=['Cat', 'Int'], how='left')
-    d = a.join(b, on=['Cat1', 'Int1'])
-    c = c.drop(['Cat', 'Int'], axis=1)
-    assert_frame_equal(c, d)
+    expected = merge(a, b.reset_index(), left_on=['Cat1', 'Int1'],
+                     right_on=['Cat', 'Int'], how='left')
+    result = a.join(b, on=['Cat1', 'Int1'])
+    expected = expected.drop(['Cat', 'Int'], axis=1)
+    assert_frame_equal(expected, result)
