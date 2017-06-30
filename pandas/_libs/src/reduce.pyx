38e0f530007d7b55718f56ced5365aab182b290b
Florian Wilhelm <FlorianWilhelm@users.noreply.github.com>
2017-06-30 22:25:15 +0200

    FIX: Allow aggregate to return dictionaries again #16741 (#16752)

--- pandas/_libs/src/reduce.pyx
+++ pandas/_libs/src/reduce.pyx
@@ -419,7 +419,7 @@ cdef class SeriesGrouper:
 cdef inline _extract_result(object res):
     """ extract the result object, it might be a 0-dim ndarray
         or a len-1 0-dim, or a scalar """
-    if hasattr(res, 'values'):
+    if hasattr(res, 'values') and isinstance(res.values, np.ndarray):
         res = res.values
     if not np.isscalar(res):
         if isinstance(res, np.ndarray):
