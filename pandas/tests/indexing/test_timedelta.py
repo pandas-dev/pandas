c96ac16976f5222419f3b8e48b020aedd2bb69ba
jdeschenes <deschenes.j.m@gmail.com>
2017-07-06 08:23:38 -0400

    BUG: TimedeltaIndex raising ValueError when slice indexing (#16637) (#16638)
    (cherry picked from commit cc5d20f50c059f02472b029f4455b2609620374e)

--- pandas/tests/indexing/test_timedelta.py
+++ pandas/tests/indexing/test_timedelta.py
@@ -1,3 +1,5 @@
+import pytest
+
 import pandas as pd
 from pandas.util import testing as tm
 
@@ -16,5 +18,25 @@ class TestTimedeltaIndexing(object):
             result = df.assign(x=df.mask(cond, 10).astype('int64'))
             expected = pd.DataFrame(data,
                                     index=pd.to_timedelta(range(10), unit='s'),
-                                    columns=['x'])
+                                    columns=['x'],
+                                    dtype='int64')
             tm.assert_frame_equal(expected, result)
+
+    @pytest.mark.parametrize(
+        "indexer, expected",
+        [(0, [20, 1, 2, 3, 4, 5, 6, 7, 8, 9]),
+         (slice(4, 8), [0, 1, 2, 3, 20, 20, 20, 20, 8, 9]),
+         ([3, 5], [0, 1, 2, 20, 4, 20, 6, 7, 8, 9])])
+    def test_list_like_indexing(self, indexer, expected):
+        # GH 16637
+        df = pd.DataFrame({'x': range(10)}, dtype="int64")
+        df.index = pd.to_timedelta(range(10), unit='s')
+
+        df.loc[df.index[indexer], 'x'] = 20
+
+        expected = pd.DataFrame(expected,
+                                index=pd.to_timedelta(range(10), unit='s'),
+                                columns=['x'],
+                                dtype="int64")
+
+        tm.assert_frame_equal(expected, df)
