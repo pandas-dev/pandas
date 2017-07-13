21b5bd22b4720977bc86ab917f963c744508b1dd
Jean-Mathieu Deschenes <jean-mathieu.deschenes@cae.com>
2017-07-13 14:16:18 -0400

    Changed location of the assert
    The "not" are all grouped together and appear first.

--- pandas/tests/dtypes/test_common.py
+++ pandas/tests/dtypes/test_common.py
@@ -202,12 +202,13 @@ def test_is_timedelta64_dtype():
     assert not com.is_timedelta64_dtype(None)
     assert not com.is_timedelta64_dtype([1, 2, 3])
     assert not com.is_timedelta64_dtype(np.array([], dtype=np.datetime64))
-    assert com.is_timedelta64_dtype(np.timedelta64)
-    assert com.is_timedelta64_dtype(pd.Series([], dtype="timedelta64[ns]"))
     assert not com.is_timedelta64_dtype('0 days')
     assert not com.is_timedelta64_dtype("0 days 00:00:00")
     assert not com.is_timedelta64_dtype(["0 days 00:00:00"])
     assert not com.is_timedelta64_dtype("NO DATE")
+
+    assert com.is_timedelta64_dtype(np.timedelta64)
+    assert com.is_timedelta64_dtype(pd.Series([], dtype="timedelta64[ns]"))
 
 
 def test_is_period_dtype():
