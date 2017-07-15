053e306fdda0fcc4d7378884263c526c047927d4
Alan Velasco <AlanVelasco.A@gmail.com>
2017-07-15 16:28:23 -0500

    Support non unique period indexes on join and merge operations (#16949)
    * Support non unique period indexes on join and merge operations
    
    * Add frame assertion on tests and release notes
    
    * Explicitly use dtype int64 on arange

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3119,14 +3119,14 @@ class Index(IndexOpsMixin, StringAccessorMixin, PandasObject):
     def _join_non_unique(self, other, how='left', return_indexers=False):
         from pandas.core.reshape.merge import _get_join_indexers
 
-        left_idx, right_idx = _get_join_indexers([self.values],
+        left_idx, right_idx = _get_join_indexers([self._values],
                                                  [other._values], how=how,
                                                  sort=True)
 
         left_idx = _ensure_platform_int(left_idx)
         right_idx = _ensure_platform_int(right_idx)
 
-        join_index = np.asarray(self.values.take(left_idx))
+        join_index = np.asarray(self._values.take(left_idx))
         mask = left_idx == -1
         np.putmask(join_index, mask, other._values.take(right_idx))
 
