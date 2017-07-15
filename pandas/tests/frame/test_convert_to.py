96cc9baabbcbcfd3efae09f3a01425c9f0856bde
Alan Velasco <AlanVelasco.A@gmail.com>
2017-07-15 09:59:39 -0500

    Add github issue to documentation on to_dict() warning test

--- pandas/tests/frame/test_convert_to.py
+++ pandas/tests/frame/test_convert_to.py
@@ -217,8 +217,8 @@ class TestDataFrameConvertTo(TestData):
             df.to_dict(into=mapping)
 
     def test_to_dict_not_unique_warning(self):
-        # When converting to a dict, if a column has a non-unique name it will
-        # be dropped, throwing a warning.
+        # GH16927: When converting to a dict, if a column has a non-unique name
+        # it will be dropped, throwing a warning.
         df = DataFrame([[1, 2, 3]], columns=['a', 'a', 'b'])
         with tm.assert_produces_warning(UserWarning):
             df.to_dict()
