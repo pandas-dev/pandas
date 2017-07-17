81f8acef11e8d1e2f0ea78a7b57ee04bef1f6038
gfyoung <gfyoung17@gmail.com>
2017-07-17 16:29:57 -0700

    COMPAT: Add back remove_na for seaborn (#16992)
    Closes gh-16971.

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -37,7 +37,6 @@ from pandas.core.dtypes.cast import (
     maybe_convert_platform,
     maybe_cast_to_datetime, maybe_castable)
 from pandas.core.dtypes.missing import isnull, notnull, remove_na_arraylike
-
 from pandas.core.common import (is_bool_indexer,
                                 _default_index,
                                 _asarray_tuplesafe,
@@ -86,6 +85,17 @@ _shared_doc_kwargs = dict(
     unique='np.ndarray', duplicated='Series',
     optional_by='',
     versionadded_to_excel='\n    .. versionadded:: 0.20.0\n')
+
+
+# see gh-16971
+def remove_na(arr):
+    """
+    DEPRECATED : this function will be removed in a future version.
+    """
+
+    warnings.warn("remove_na is deprecated and is a private "
+                  "function. Do not use.", FutureWarning, stacklevel=2)
+    return remove_na_arraylike(arr)
 
 
 def _coerce_method(converter):
