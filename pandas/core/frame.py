08f2e3bda4f745210d2005963a957f9eec576b77
Brock Mendel <jbrockmendel@gmail.com>
2017-07-19 00:06:36 -0700

    Define DataFrame plot methods in DataFrame
    instead of pinning them on at the bottom of the module.
    
    Tried doing this with Series last week and it caused a circular import problem.  Hopefully
    cleaning this but of DataFrame up won't be as difficult

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -5651,11 +5651,31 @@ it is assumed to be aliases for the column names')
                                 values).reshape(self.shape), self.index,
                 self.columns)
 
+    # ----------------------------------------------------------------------
+    # Add plotting methods to DataFrame
+    plot = base.AccessorProperty(gfx.FramePlotMethods, gfx.FramePlotMethods)
+    hist = gfx.hist_frame
+
+    @Appender(_shared_docs['boxplot'] % _shared_doc_kwargs)
+    def boxplot(self, column=None, by=None, ax=None, fontsize=None, rot=0,
+                grid=True, figsize=None, layout=None,
+                return_type=None, **kwds):
+        from pandas.plotting._core import boxplot
+        import matplotlib.pyplot as plt
+        ax = boxplot(self, column=column, by=by, ax=ax, fontsize=fontsize,
+                     grid=grid, rot=rot, figsize=figsize, layout=layout,
+                     return_type=return_type, **kwds)
+        plt.draw_if_interactive()
+        return ax
+
 
 DataFrame._setup_axes(['index', 'columns'], info_axis=1, stat_axis=0,
                       axes_are_reversed=True, aliases={'rows': 0})
 DataFrame._add_numeric_operations()
 DataFrame._add_series_or_dataframe_operations()
+
+ops.add_flex_arithmetic_methods(DataFrame, **ops.frame_flex_funcs)
+ops.add_special_arithmetic_methods(DataFrame, **ops.frame_special_funcs)
 
 _EMPTY_SERIES = Series([])
 
@@ -6002,28 +6022,3 @@ def _from_nested_dict(data):
 
 def _put_str(s, space):
     return ('%s' % s)[:space].ljust(space)
-
-
-# ----------------------------------------------------------------------
-# Add plotting methods to DataFrame
-DataFrame.plot = base.AccessorProperty(gfx.FramePlotMethods,
-                                       gfx.FramePlotMethods)
-DataFrame.hist = gfx.hist_frame
-
-
-@Appender(_shared_docs['boxplot'] % _shared_doc_kwargs)
-def boxplot(self, column=None, by=None, ax=None, fontsize=None, rot=0,
-            grid=True, figsize=None, layout=None, return_type=None, **kwds):
-    from pandas.plotting._core import boxplot
-    import matplotlib.pyplot as plt
-    ax = boxplot(self, column=column, by=by, ax=ax, fontsize=fontsize,
-                 grid=grid, rot=rot, figsize=figsize, layout=layout,
-                 return_type=return_type, **kwds)
-    plt.draw_if_interactive()
-    return ax
-
-
-DataFrame.boxplot = boxplot
-
-ops.add_flex_arithmetic_methods(DataFrame, **ops.frame_flex_funcs)
-ops.add_special_arithmetic_methods(DataFrame, **ops.frame_special_funcs)
