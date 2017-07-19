71bc3485c97624125d5163e669699ed8cff1e786
rosygupta <rosy01.gupta@gmail.com>
2017-07-20 01:06:30 +0530

    removed pep styling from unnecessary code

--- pandas/tests/indexes/datetimes/test_tools.py
+++ pandas/tests/indexes/datetimes/test_tools.py
@@ -296,23 +296,11 @@ class TestToDatetime(object):
     def test_to_datetime_tz_pytz(self):
         # see gh-8260
         us_eastern = pytz.timezone('US/Eastern')
-        arr = np.array(
-            [
-                us_eastern.localize(
-                    datetime(
-                        year=2000,
-                        month=1,
-                        day=1,
-                        hour=3,
-                        minute=0)),
-                us_eastern.localize(
-                    datetime(
-                        year=2000,
-                        month=6,
-                        day=1,
-                        hour=3,
-                        minute=0))],
-            dtype=object)
+        arr = np.array([us_eastern.localize(datetime(year=2000, month=1, day=1,
+                                                    hour=3, minute=0)),
+                        us_eastern.localize(datetime(year=2000, month=6, day=1,
+                                                    hour=3, minute=0))],
+                        dtype=object)
         result = pd.to_datetime(arr, utc=True)
         expected = DatetimeIndex(['2000-01-01 08:00:00+00:00',
                                   '2000-06-01 07:00:00+00:00'],
