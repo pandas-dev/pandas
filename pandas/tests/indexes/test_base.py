c0a84b59f5b9638ddabcd49ad664fa5850f02135
ysau <ysau@users.noreply.github.com>
2017-07-21 21:58:41 -0700

    TST: Fix error message check in np.argsort comparision (#17051)
    Closes gh-17046.

--- pandas/tests/indexes/test_base.py
+++ pandas/tests/indexes/test_base.py
@@ -1846,7 +1846,7 @@ class TestMixedIntIndex(Base):
     def test_argsort(self):
         idx = self.create_index()
         if PY36:
-            with tm.assert_raises_regex(TypeError, "'>' not supported"):
+            with tm.assert_raises_regex(TypeError, "'>|<' not supported"):
                 result = idx.argsort()
         elif PY3:
             with tm.assert_raises_regex(TypeError, "unorderable types"):
@@ -1859,7 +1859,7 @@ class TestMixedIntIndex(Base):
     def test_numpy_argsort(self):
         idx = self.create_index()
         if PY36:
-            with tm.assert_raises_regex(TypeError, "'>' not supported"):
+            with tm.assert_raises_regex(TypeError, "'>|<' not supported"):
                 result = np.argsort(idx)
         elif PY3:
             with tm.assert_raises_regex(TypeError, "unorderable types"):
