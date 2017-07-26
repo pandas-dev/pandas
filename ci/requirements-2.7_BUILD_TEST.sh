8018797af0ad69d759af4393415ae4635a49d419
Jeff Reback <jeff@reback.net>
2017-07-26 06:29:23 -0400

    CI: partially revert #17065, un-pin pyarrow on some builds

--- ci/requirements-2.7_BUILD_TEST.sh
+++ ci/requirements-2.7_BUILD_TEST.sh
@@ -4,4 +4,4 @@ source activate pandas
 
 echo "install 27 BUILD_TEST"
 
-conda install -n pandas -c conda-forge pyarrow=0.4.1 dask
+conda install -n pandas -c conda-forge pyarrow dask
--- ci/requirements-3.6_DOC.sh
+++ ci/requirements-3.6_DOC.sh
@@ -6,6 +6,6 @@ echo "[install DOC_BUILD deps]"
 
 pip install pandas-gbq
 
-conda install -n pandas -c conda-forge feather-format pyarrow=0.4.1 nbsphinx pandoc
+conda install -n pandas -c conda-forge feather-format pyarrow nbsphinx pandoc
 
 conda install -n pandas -c r r rpy2 --yes
