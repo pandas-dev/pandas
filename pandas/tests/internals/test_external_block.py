dcd3c0f2abb4b1497319c989cdac22ef94a97422
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-08-03 23:47:59 +0200

    remove usage of fastpath in test example

--- pandas/tests/internals/test_external_block.py
+++ pandas/tests/internals/test_external_block.py
@@ -4,7 +4,7 @@
 import numpy as np
 
 import pandas as pd
-from pandas.core.internals import Block, BlockManager
+from pandas.core.internals import Block, BlockManager, SingleBlockManager
 
 
 class CustomBlock(Block):
@@ -18,7 +18,8 @@ def test_custom_repr():
 
     # series
     block = CustomBlock(values, placement=slice(0, 3))
-    s = pd.Series(block, index=pd.RangeIndex(3), fastpath=True)
+    
+    s = pd.Series(SingleBlockManager(block, pd.RangeIndex(3)))
     assert repr(s) == '0    Val: 0\n1    Val: 1\n2    Val: 2\ndtype: int64'
 
     # dataframe
