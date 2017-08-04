a569538b251588df67c116ffefa6d7ba9393b987
fxocena <fx860110@gmail.com>
2017-08-04 08:02:51 -0500

    fix PEP8 issues

--- pandas/tests/test_window.py
+++ pandas/tests/test_window.py
@@ -433,7 +433,8 @@ class TestRolling(Base):
         expected_data = np.append([np.NaN, 1.], np.arange(3., 27., 3))
         for window in [timedelta(days=3), pd.Timedelta(days=3)]:
             result_roll_sum = df.rolling(window=window, min_periods=2).sum()
-            result_roll_generic = df.rolling(window=window, min_periods=2).apply(sum)
+            result_roll_generic = df.rolling(window=window, 
+                                             min_periods=2).apply(sum)
             expected = pd.DataFrame({'value': expected_data},
                                     index=pd.date_range('2015-12-24',
                                                         periods=n,
@@ -442,7 +443,7 @@ class TestRolling(Base):
             tm.assert_frame_equal(result_roll_generic, expected)
             expected = df.rolling('3D', min_periods=2).sum()
             tm.assert_frame_equal(result_roll_generic, expected)
-            
+
     def test_numpy_compat(self):
         # see gh-12811
         r = rwindow.Rolling(Series([2, 4, 6]), window=2)
