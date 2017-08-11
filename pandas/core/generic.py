2b8c5ea88678e1275d0e77fce8331588fb078b56
Raghav Batra <rb698@cornell.edu>
2017-08-11 16:47:29 +0530

    DOC: Updated pd.Series.replace
    Updated documentation so that it matched the functionality of
    pd.Series.replace and NOT pd.DataFrame.replace
    Fixes #13852

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -4108,11 +4108,9 @@ class NDFrame(PandasObject, SelectionMixin):
 
             * dict:
 
-                - Nested dictionaries, e.g., {'a': {'b': nan}}, are read as
-                  follows: look in column 'a' for the value 'b' and replace it
-                  with nan. You can nest regular expressions as well. Note that
-                  column names (the top-level dictionary keys in a nested
-                  dictionary) **cannot** be regular expressions.
+                - Dictionaries, e.g., {'a': 'b'}, are read as
+                  follows: look for the value 'a' and replace it
+                  with 'b'.
                 - Keys map to column names and values map to substitution
                   values. You can treat this as a special case of passing two
                   lists except that you are specifying the column to search in.
@@ -4126,14 +4124,10 @@ class NDFrame(PandasObject, SelectionMixin):
 
             See the examples section for examples of each of these.
         value : scalar, dict, list, str, regex, default None
-            Value to use to fill holes (e.g. 0), alternately a dict of values
-            specifying which value to use for each column (columns not in the
-            dict will not be filled). Regular expressions, strings and lists or
-            dicts of such objects are also allowed.
+            Value to use to fill holes (e.g. 0). Regular expressions, strings 
+            and lists or dicts of such objects are also allowed.
         inplace : boolean, default False
-            If True, in place. Note: this will modify any
-            other views on this object (e.g. a column form a DataFrame).
-            Returns the caller if this is True.
+            If True, in place. Returns the caller if this is True.
         limit : int, default None
             Maximum size gap to forward or backward fill
         regex : bool or same types as `to_replace`, default False
@@ -4148,13 +4142,13 @@ class NDFrame(PandasObject, SelectionMixin):
 
         See Also
         --------
-        NDFrame.reindex
-        NDFrame.asfreq
-        NDFrame.fillna
+        Series.reindex
+        Series.asfreq
+        Series.fillna
 
         Returns
         -------
-        filled : NDFrame
+        filled : Series
 
         Raises
         ------
