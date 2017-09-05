a74e7faf7c631bec45a342b22498faf61548d76c
Brock Mendel <jbrockmendel@gmail.com>
2017-09-05 07:21:26 -0700

    Remove unused imports

--- pandas/_libs/tslibs/timezones.pyx
+++ pandas/_libs/tslibs/timezones.pyx
@@ -1,37 +1,9 @@
 # -*- coding: utf-8 -*-
 # cython: profile=False
 
-from pandas.compat import string_types, is_platform_windows
-
-cdef extern from "Python.h":
-    Py_ssize_t PY_SSIZE_T_MAX
-
-cdef extern from "datetime.h":
-    void PyDateTime_IMPORT()
-
-# import datetime C API
-PyDateTime_IMPORT
-
-import numpy as np
-cimport numpy as np
-from numpy cimport ndarray, int64_t, float64_t
-np.import_array()
-
-
 # dateutil compat
-from dateutil.tz import (tzoffset,
-                         tzlocal as _dateutil_tzlocal,
-                         tzfile as _dateutil_tzfile,
-                         tzutc as _dateutil_tzutc,
-                         tzstr as _dateutil_tzstr)
+from dateutil.tz import tzutc as _dateutil_tzutc
 
-if is_platform_windows():
-    from dateutil.zoneinfo import gettz as _dateutil_gettz
-else:
-    from dateutil.tz import gettz as _dateutil_gettz
-
-
-from pytz.tzinfo import BaseTzInfo as _pytz_BaseTzInfo
 import pytz
 UTC = pytz.utc
 
