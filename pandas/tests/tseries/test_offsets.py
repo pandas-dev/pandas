3b4b854a55c5780ed199d4f7569810c11c76e7ea
Brock Mendel <jbrockmendel@gmail.com>
2017-09-06 20:15:07 -0700

    Add GH reference to test per reviewer request

--- pandas/tests/tseries/test_offsets.py
+++ pandas/tests/tseries/test_offsets.py
@@ -1953,6 +1953,7 @@ class CustomBusinessMonthBase(object):
         _check_roundtrip(self._object() * 2)
 
     def test_copy(self):
+        # GH 17452
         off = self._object(weekmask='Mon Wed Fri')
         assert off == off.copy()
 
