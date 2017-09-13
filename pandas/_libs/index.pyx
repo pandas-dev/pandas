0ddd2ef7dc76aa168cea583007e2312d01102f1b
Brock Mendel <jbrockmendel@gmail.com>
2017-09-13 10:07:54 -0700

    Fixup import get_utcoffset into index.pyx

--- pandas/_libs/index.pyx
+++ pandas/_libs/index.pyx
@@ -17,7 +17,7 @@ cimport tslib
 
 from hashtable cimport HashTable
 
-from tslibs.timezones cimport is_utc
+from tslibs.timezones cimport is_utc, get_utcoffset
 from pandas._libs import tslib, algos, hashtable as _hash
 from pandas._libs.tslib import Timestamp, Timedelta
 from datetime import datetime, timedelta
@@ -552,7 +552,7 @@ cdef inline _to_i8(object val):
             # Save the original date value so we can get the utcoffset from it.
             ival = _pydatetime_to_dts(val, &dts)
             if tzinfo is not None and not is_utc(tzinfo):
-                offset = tslib._get_utcoffset(tzinfo, val)
+                offset = get_utcoffset(tzinfo, val)
                 ival -= tslib._delta_to_nanoseconds(offset)
             return ival
         return val
