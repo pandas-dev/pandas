e060482bd1605c6c7cdad412efb0839beb1374d9
AJ Pryor <apryor6@gmail.com>
2017-09-15 14:28:30 -0400

    added tests

--- pandas/tests/io/formats/test_to_html.py
+++ pandas/tests/io/formats/test_to_html.py
@@ -1881,3 +1881,37 @@ class TestToHTML(object):
                                                         name='myindexname'))
         result = df.to_html(index_names=False)
         assert 'myindexname' not in result
+
+    def test_to_html_styler(self):
+        df = pd.DataFrame({"A":[-1,1],"B":[-2,2]}).style.applymap(lambda x: 'color: {}'.format('red' if x < 0 else 'black'))
+        df.uuid = 42
+        expected = """<style  type="text/css" >
+    #T_42row0_col0 {
+            color:  red;
+        }    #T_42row0_col1 {
+            color:  red;
+        }    #T_42row1_col0 {
+            color:  black;
+        }    #T_42row1_col1 {
+            color:  black;
+        }</style>  
+<table id="T_42" > 
+<thead>    <tr> 
+        <th class="blank level0" ></th> 
+        <th class="col_heading level0 col0" >A</th> 
+        <th class="col_heading level0 col1" >B</th> 
+    </tr></thead> 
+<tbody>    <tr> 
+        <th id="T_42level0_row0" class="row_heading level0 row0" >0</th> 
+        <td id="T_42row0_col0" class="data row0 col0" >-1</td> 
+        <td id="T_42row0_col1" class="data row0 col1" >-2</td> 
+    </tr>    <tr> 
+        <th id="T_42level0_row1" class="row_heading level0 row1" >1</th> 
+        <td id="T_42row1_col0" class="data row1 col0" >1</td> 
+        <td id="T_42row1_col1" class="data row1 col1" >2</td> 
+    </tr></tbody> 
+</table> \
+"""
+        result = df.to_html()
+        assert(result == expected)
+
