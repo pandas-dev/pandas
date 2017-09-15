b70a19f67988f4eea2e1e0f0b694ba32a418716c
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-09-15 10:27:01 +0200

    TST: sql flaky test: check less decimals (#17510)

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -2124,7 +2124,7 @@ class TestXSQLite(SQLiteMixIn):
 
         result = sql.read_sql("select * from test", con=self.conn)
         result.index = frame.index
-        tm.assert_frame_equal(result, frame)
+        tm.assert_frame_equal(result, frame, check_less_precise=True)
 
     def test_execute(self):
         frame = tm.makeTimeDataFrame()
@@ -2409,7 +2409,7 @@ class TestXMySQL(MySQLMixIn):
 
         result = sql.read_sql("select * from test", con=self.conn)
         result.index = frame.index
-        tm.assert_frame_equal(result, frame)
+        tm.assert_frame_equal(result, frame, check_less_precise=True)
 
     def test_chunksize_read_type(self):
         _skip_if_no_pymysql()
