0782df968b7d34818a535536e2adc4fe0fd6549d
louispotok <louispotok@gmail.com>
2017-09-15 13:09:41 -0700

    update benchmarks

--- asv_bench/benchmarks/packers.py
+++ asv_bench/benchmarks/packers.py
@@ -97,12 +97,11 @@ class packers_read_json_lines(_Packers):
 class packers_read_json_lines_chunks(_Packers):
     def setup(self):
         self._setup()
-        self.df.to_json('abc.json', orient="records", lines=True)
-        self.df.index = np.arange(self.N)
+        self.df.to_json(self.f, orient="records", lines=True)
 
     def time_packers_read_json_lines_chunks(self):
         chunksize = int(self.C / 5.0)
-        pd.read_json('abc.json', lines=True, chunksize=chunksize)
+        next([c for c in pd.read_json(self.f, lines=True, chunksize=chunksize)])
 
 class packers_read_json_date_index(_Packers):
 
