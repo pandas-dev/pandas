9730757a4443caab0cecd7bda93a65228aae2b58
Giftlin Rajaiah <giftlin.rgn@gmail.com>
2017-09-18 01:01:15 +0530

    Lint error

--- pandas/tests/test_categorical.py
+++ pandas/tests/test_categorical.py
@@ -59,15 +59,15 @@ class TestCategorical(object):
 
     @pytest.mark.parametrize("method",
                              [
-                                 lambda x: x.cat.set_categories([1, 2, 3]), 
+                                 lambda x: x.cat.set_categories([1, 2, 3]),
                                  lambda x: x.cat.reorder_categories([2, 3, 1]
-                                                                    , ordered=True), 
-                                 lambda x: x.cat.rename_categories([1,2, 3]), 
-                                 lambda x: x.cat.remove_unused_categories(), 
-                                 lambda x: x.cat.remove_categories([2]), 
-                                 lambda x: x.cat.add_categories([4]), 
-                                 lambda x: x.cat.as_ordered(), 
-                                 lambda x: x.cat.as_unordered(), 
+                                                              ,ordered=True), 
+                                 lambda x: x.cat.rename_categories([1, 2, 3]),
+                                 lambda x: x.cat.remove_unused_categories(),
+                                 lambda x: x.cat.remove_categories([2]),
+                                 lambda x: x.cat.add_categories([4]),
+                                 lambda x: x.cat.as_ordered(),
+                                 lambda x: x.cat.as_unordered(),
                              ])
     def test_getname_categorical_accessor(self, method):
         s = pd.Series([1, 2, 3], name='A').astype('category')
