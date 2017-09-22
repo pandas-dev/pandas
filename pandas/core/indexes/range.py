ecbc8269c218450071d0e504c9ac6eac3619564a
jschendel <jschendel@users.noreply.github.com>
2017-09-22 07:15:12 -0600

    PERF: Implement RangeIndex min/max using RangeIndex properties (#17611)

--- pandas/core/indexes/range.py
+++ pandas/core/indexes/range.py
@@ -269,6 +269,24 @@ class RangeIndex(Int64Index):
         return RangeIndex(name=name, fastpath=True,
                           **dict(self._get_data_as_items()))
 
+    def _minmax(self, meth):
+        no_steps = len(self) - 1
+        if no_steps == -1:
+            return np.nan
+        elif ((meth == 'min' and self._step > 0) or
+              (meth == 'max' and self._step < 0)):
+            return self._start
+
+        return self._start + self._step * no_steps
+
+    def min(self):
+        """The minimum value of the RangeIndex"""
+        return self._minmax('min')
+
+    def max(self):
+        """The maximum value of the RangeIndex"""
+        return self._minmax('max')
+
     def argsort(self, *args, **kwargs):
         """
         Returns the indices that would sort the index and its
