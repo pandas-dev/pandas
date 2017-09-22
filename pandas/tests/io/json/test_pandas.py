1d03d7af0c71a56df6855cb273c90afe0cfa88e7
Kevin Kuhl <kevin@wayblazer.com>
2017-09-21 22:19:34 -0500

    remove bad dev deps. fix tempfile context in tests

--- pandas/tests/io/json/test_pandas.py
+++ pandas/tests/io/json/test_pandas.py
@@ -1023,9 +1023,10 @@ DataFrame\\.index values are different \\(100\\.0 %\\)
 
     def test_read_local_jsonl(self):
         # GH17200
-        with ensure_clean('tmp_items.json') as infile:
-            infile.write('{"a": 1, "b": 2}\n{"b":2, "a" :1}\n')
-            result = read_json(fname, lines=True)
+        with ensure_clean('tmp_items.json') as path:
+            with open(path, 'w') as infile:
+                infile.write('{"a": 1, "b": 2}\n{"b":2, "a" :1}\n')
+            result = read_json(path, lines=True)
             expected = DataFrame([[1, 2], [1, 2]], columns=['a', 'b'])
             assert_frame_equal(result, expected)
 
