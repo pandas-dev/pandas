43f90cc13786b57b89709cdb7dd8d2c023adaee6
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-09-23 11:33:03 -0500

    PEP8

--- pandas/tests/io/test_parquet.py
+++ pandas/tests/io/test_parquet.py
@@ -7,7 +7,7 @@ from warnings import catch_warnings
 
 import numpy as np
 import pandas as pd
-from pandas.compat import PY3, is_platform_windows
+from pandas.compat import PY3
 from pandas.io.parquet import (to_parquet, read_parquet, get_engine,
                                PyArrowImpl, FastParquetImpl)
 from pandas.util import testing as tm
