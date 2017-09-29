c8be3e6e45df294a44c61f8c9cb88bea15a4b098
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-09-29 18:36:02 +0200

    parametrize a little bit more

--- pandas/tests/indexes/test_interval_new.py
+++ pandas/tests/indexes/test_interval_new.py
@@ -25,26 +25,26 @@ class TestIntervalIndex_new(Base):
 
     @pytest.mark.xfail(reason="new indexing tests for issue 16316")
     @pytest.mark.parametrize("idx_side", ['right', 'left', 'both', 'neither'])
-    def test_get_loc_interval_updated_behavior(self, idx_side):
+    @pytest.mark.parametrize("side", ['right', 'left', 'both', 'neither'])
+    def test_get_loc_interval_updated_behavior(self, idx_side, side):
 
         idx = IntervalIndex.from_tuples([(0, 1), (2, 3)], closed=idx_side)
 
         for bound in [[0, 1], [1, 2], [2, 3], [3, 4],
                       [0, 2], [2.5, 3], [-1, 4]]:
-            for side in ['right', 'left', 'both', 'neither']:
-                # if get_loc is supplied an interval, it should only search
-                # for exact matches, not overlaps or covers, else KeyError.
-                if idx_side == side:
-                    if bound == [0, 1]:
-                        assert idx.get_loc(Interval(0, 1, closed=side)) == 0
-                    elif bound == [2, 3]:
-                        assert idx.get_loc(Interval(2, 3, closed=side)) == 1
-                    else:
-                        pytest.raises(KeyError, idx.get_loc,
-                                      Interval(*bound, closed=side))
+            # if get_loc is supplied an interval, it should only search
+            # for exact matches, not overlaps or covers, else KeyError.
+            if idx_side == side:
+                if bound == [0, 1]:
+                    assert idx.get_loc(Interval(0, 1, closed=side)) == 0
+                elif bound == [2, 3]:
+                    assert idx.get_loc(Interval(2, 3, closed=side)) == 1
                 else:
-                    pytest.raises(KeyError, idx.get_loc,
-                                  Interval(*bound, closed=side))
+                    with pytest.raises(KeyError):
+                        idx.get_loc(Interval(*bound, closed=side))
+            else:
+                with pytest.raises(KeyError):
+                    idx.get_loc(Interval(*bound, closed=side))
 
     @pytest.mark.xfail(reason="new indexing tests for issue 16316")
     @pytest.mark.parametrize("idx_side", ['right', 'left', 'both', 'neither'])
