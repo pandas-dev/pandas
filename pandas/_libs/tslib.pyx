95f778a6890b445fff82f2245f8fabbef79c5a5d
Brock Mendel <jbrockmendel@gmail.com>
2017-10-01 09:46:59 -0700

    use Timestamp per reviewier request

--- pandas/_libs/tslib.pyx
+++ pandas/_libs/tslib.pyx
@@ -3444,13 +3444,7 @@ cdef inline _to_i8(object val):
         if is_datetime64_object(val):
             return get_datetime64_value(val)
         elif PyDateTime_Check(val):
-            tzinfo = getattr(val, 'tzinfo', None)
-            # Save the original date value so we can get the utcoffset from it.
-            ival = _pydatetime_to_dts(val, &dts)
-            if tzinfo is not None and not is_utc(tzinfo):
-                offset = get_utcoffset(tzinfo, val)
-                ival -= _delta_to_nanoseconds(offset)
-            return ival
+            return Timestamp(val).value
         return val
 
 cpdef pydt_to_i8(object pydt):
