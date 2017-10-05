f15a2566a00078afd283b1f4ea0ee4487bb32a00
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2017-10-05 06:30:06 -0500

    Use argument dtype to inform coercion (#17779)
    * Use argument dtype to inform coercion
    
    Master:
    
    ```python
    >>> import dask.dataframe as dd
    
    >>> s = dd.core.Scalar({('s', 0): 10}, 's', 'i8')
    >>> pdf = pd.DataFrame({'a': [1, 2, 3, 4, 5, 6, 7],
    ...                     'b': [7, 6, 5, 4, 3, 2, 1]})
    >>> (pdf + s).dtypes
    a    object
    b    object
    dtype: object
    
    Head:
    
    ```
    >>> (pdf + s).dtypes
    a    int64
    b    int64
    dtype: object
    ```
    
    This is more consistent with 0.20.3, while still most of the changes in
    https://github.com/pandas-dev/pandas/pull/16821
    
    Closes https://github.com/pandas-dev/pandas/issues/17767
    
    * Compat for older numpy where bool(dtype) is False
    
    * Added timedelta

--- pandas/tests/internals/test_internals.py
+++ pandas/tests/internals/test_internals.py
@@ -2,6 +2,7 @@
 # pylint: disable=W0102
 
 from datetime import datetime, date
+import operator
 import sys
 import pytest
 import numpy as np
@@ -1213,3 +1214,64 @@ class TestBlockPlacement(object):
 
             with pytest.raises(ValueError):
                 BlockPlacement(slice(2, None, -1)).add(-1)
+
+
+class DummyElement(object):
+    def __init__(self, value, dtype):
+        self.value = value
+        self.dtype = np.dtype(dtype)
+
+    def __array__(self):
+        return np.array(self.value, dtype=self.dtype)
+
+    def __str__(self):
+        return "DummyElement({}, {})".format(self.value, self.dtype)
+
+    def __repr__(self):
+        return str(self)
+
+    def astype(self, dtype, copy=False):
+        self.dtype = dtype
+        return self
+
+    def view(self, dtype):
+        return type(self)(self.value.view(dtype), dtype)
+
+    def any(self, axis=None):
+        return bool(self.value)
+
+
+class TestCanHoldElement(object):
+    @pytest.mark.parametrize('value, dtype', [
+        (1, 'i8'),
+        (1.0, 'f8'),
+        (1j, 'complex128'),
+        (True, 'bool'),
+        (np.timedelta64(20, 'ns'), '<m8[ns]'),
+        (np.datetime64(20, 'ns'), '<M8[ns]'),
+    ])
+    @pytest.mark.parametrize('op', [
+        operator.add,
+        operator.sub,
+        operator.mul,
+        operator.truediv,
+        operator.mod,
+        operator.pow,
+    ], ids=lambda x: x.__name__)
+    def test_binop_other(self, op, value, dtype):
+        skip = {(operator.add, 'bool'),
+                (operator.sub, 'bool'),
+                (operator.mul, 'bool'),
+                (operator.truediv, 'bool'),
+                (operator.mod, 'i8'),
+                (operator.mod, 'complex128'),
+                (operator.mod, '<M8[ns]'),
+                (operator.mod, '<m8[ns]'),
+                (operator.pow, 'bool')}
+        if (op, dtype) in skip:
+            pytest.skip("Invalid combination {},{}".format(op, dtype))
+        e = DummyElement(value, dtype)
+        s = pd.DataFrame({"A": [e.value, e.value]}, dtype=e.dtype)
+        result = op(s, e).dtypes
+        expected = op(s, value).dtypes
+        assert_series_equal(result, expected)
