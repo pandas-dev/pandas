3b4121b5fca88a84bcc9dba56af568c8a2f901c0
Simon Gibbons <simongibbons@gmail.com>
2017-10-06 15:08:22 +0100

    ENH: Add tranparent compression to json reading/writing (#17798)
    * ENH: Add tranparent compression to json reading/writing
    
    This works in the same way as the argument to ``read_csv``
    and ``to_csv``.
    
    I've added tests confirming that it works with both file
    paths, as well and file URLs and S3 URLs.
    
    * Fix PEP8 violations
    
    * Add PR number to whatsnew entry
    
    * Remove problematic Windows test (The S3 test hits the same edge case)
    
    * Extract decompress file function so that pytest.paramatrize can be used cleanly
    
    * Fix typo in whatsnew entry

--- pandas/tests/io/json/test_readlines.py
+++ pandas/tests/io/json/test_readlines.py
@@ -128,7 +128,7 @@ def test_readjson_chunks_closes(chunksize):
             path, orient=None, typ="frame", dtype=True, convert_axes=True,
             convert_dates=True, keep_default_dates=True, numpy=False,
             precise_float=False, date_unit=None, encoding=None,
-            lines=True, chunksize=chunksize)
+            lines=True, chunksize=chunksize, compression=None)
         reader.read()
         assert reader.open_stream.closed, "didn't close stream with \
             chunksize = %s" % chunksize
