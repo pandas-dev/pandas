39451075320b18d7d06ff1da3b687cfc57d59dde
Jon M. Mease <jon.mease@jhuapl.edu>
2017-10-13 10:04:05 -0400

    Parameterize and fixturize tests

--- pandas/tests/groupby/test_index_as_string.py
+++ pandas/tests/groupby/test_index_as_string.py
@@ -6,137 +6,111 @@ from pandas.util.testing import assert_frame_equal, assert_series_equal
 import pandas.util.testing as tm
 
 
-def build_df_multi():
-    idx = pd.MultiIndex.from_tuples([('a', 1), ('a', 2), ('a', 3),
-                                     ('b', 1), ('b', 2), ('b', 3)])
-    idx.names = ['outer', 'inner']
-    df_multi = pd.DataFrame({"A": np.arange(6),
-                             'B': ['one', 'one', 'two',
-                                   'two', 'one', 'one']},
-                            index=idx)
-    return df_multi
+@pytest.fixture(params=[['inner'], ['inner', 'outer']])
+def frame(request):
+    levels = request.param
+    df = pd.DataFrame({'outer': ['a', 'a', 'a', 'b', 'b', 'b'],
+                       'inner': [1, 2, 3, 1, 2, 3],
+                       'A': np.arange(6),
+                       'B': ['one', 'one', 'two', 'two', 'one', 'one']})
+    if levels:
+        df = df.set_index(levels)
+
+    return df
 
 
-def build_df_single():
-    df_single = build_df_multi().reset_index('outer')
-    return df_single
+@pytest.fixture()
+def series():
+    df = pd.DataFrame({'outer': ['a', 'a', 'a', 'b', 'b', 'b'],
+                       'inner': [1, 2, 3, 1, 2, 3],
+                       'A': np.arange(6),
+                       'B': ['one', 'one', 'two', 'two', 'one', 'one']})
+    s = df.set_index(['outer', 'inner', 'B'])['A']
+
+    return s
 
 
-def build_test_series():
-    series_multi = build_df_multi().set_index('B', append=True)['A']
-    return series_multi
+@pytest.mark.parametrize('key_strs,groupers', [
+    ('inner',  # Index name
+     pd.Grouper(level='inner')
+     ),
+    (['inner'],  # List of index name
+     [pd.Grouper(level='inner')]
+     ),
+    (['B', 'inner'],  # Column and index
+     ['B', pd.Grouper(level='inner')]
+     ),
+    (['inner', 'B'],  # Index and column
+     [pd.Grouper(level='inner'), 'B'])])
+def test_grouper_index_level_as_string(frame, key_strs, groupers):
+    result = frame.groupby(key_strs).mean()
+    expected = frame.groupby(groupers).mean()
+    assert_frame_equal(result, expected)
 
 
-class TestGroupByIndexAsString(object):
+@pytest.mark.parametrize('levels', [
+    'inner', 'outer', 'B',
+    ['inner'], ['outer'], ['B'],
+    ['inner', 'outer'], ['outer', 'inner'],
+    ['inner', 'outer', 'B'], ['B', 'outer', 'inner']
+])
+def test_grouper_index_level_as_string_series(series, levels):
 
-    @pytest.mark.parametrize('frame', [build_df_multi(), build_df_single()])
-    def test_grouper_index_level_as_string(self, frame):
-        # Column and Index
-        result = frame.groupby(['B', 'inner']).mean()
-        expected = frame.groupby(['B', pd.Grouper(level='inner')]).mean()
-        assert_frame_equal(result, expected)
+    # Compute expected result
+    if isinstance(levels, list):
+        groupers = [pd.Grouper(level=lv) for lv in levels]
+    else:
+        groupers = pd.Grouper(level=levels)
 
-        # Index and Column
-        result = frame.groupby(['inner', 'B']).mean()
-        expected = frame.groupby([pd.Grouper(level='inner'), 'B']).mean()
-        assert_frame_equal(result, expected)
+    expected = series.groupby(groupers).mean()
 
-        # Single element list of Index
-        result = frame.groupby(['inner']).mean()
-        expected = frame.groupby(pd.Grouper(level='inner')).mean()
-        assert_frame_equal(result, expected)
+    # Compute and check result
+    result = series.groupby(levels).mean()
+    assert_series_equal(result, expected)
 
-        # Index name
-        result = frame.groupby('inner').mean()
-        expected = frame.groupby(pd.Grouper(level='inner')).mean()
-        assert_frame_equal(result, expected)
 
-    @pytest.mark.parametrize('levels', [
-        'inner', 'outer', 'B',
-        ['inner'], ['outer'], ['B'],
-        ['inner', 'outer'], ['inner', 'outer', 'B']
-    ])
-    def test_grouper_index_level_as_string_series(self, levels):
-        s = build_test_series()
+@pytest.mark.parametrize('key_strs,key_groupers,level_groupers', [
+    ('inner',  # Index name
+     pd.Grouper(key='inner'),
+     pd.Grouper(level='inner'),
+     ),
+    (['inner'],  # List of index name
+     [pd.Grouper(key='inner')],
+     [pd.Grouper(level='inner')]
+     ),
+    (['B', 'inner'],  # Column and index
+     ['B', pd.Grouper(key='inner')],
+     ['B', pd.Grouper(level='inner')]
+     ),
+    (['inner', 'B'],  # Index and column
+     [pd.Grouper(key='inner'), 'B'],
+     [pd.Grouper(level='inner'), 'B'])])
+def test_grouper_column_index_level_precedence(frame,
+                                               key_strs,
+                                               key_groupers,
+                                               level_groupers):
 
-        # Compute expected result
-        if isinstance(levels, list):
-            groupers = [pd.Grouper(level=lv) for lv in levels]
-        else:
-            groupers = pd.Grouper(level=levels)
+    # GH 5677, when a string passed as the `by` parameter
+    # matches a column and an index level the column takes
+    # precedence and a FutureWarning is raised
 
-        expected = s.groupby(groupers).mean()
+    # Add 'inner' column to frame
+    # (frame already has an 'inner' index)
+    frame['inner'] = [1, 1, 1, 1, 1, 1]
 
-        # Compute and check result
-        result = s.groupby(levels).mean()
-        assert_series_equal(result, expected)
+    # Performing a groupby with strings should produce warning
+    with tm.assert_produces_warning(FutureWarning, check_stacklevel=False):
+        result = frame.groupby(key_strs).mean()
 
-    @pytest.mark.parametrize('frame', [build_df_multi(), build_df_single()])
-    def test_grouper_column_index_level_precedence(self, frame):
-        # GH 5677, when a string passed as the `by` parameter
-        # matches a column and an index level the column takes
-        # precedence
+    # Grouping with key Grouper should produce the same result and no warning
+    with tm.assert_produces_warning(False):
+        expected = frame.groupby(key_groupers).mean()
 
-        # Add 'inner' column to frame
-        # (frame already has an 'inner' index)
-        frame['inner'] = [1, 1, 1, 1, 1, 1]
+    assert_frame_equal(result, expected)
 
-        # Group by single key
-        with tm.assert_produces_warning(FutureWarning, check_stacklevel=False):
-            result = frame.groupby('inner').mean()
+    # Grouping with level Grouper should produce a difference result but
+    # still no warning
+    with tm.assert_produces_warning(False):
+        not_expected = frame.groupby(level_groupers).mean()
 
-        with tm.assert_produces_warning(False):
-            expected = frame.groupby(pd.Grouper(key='inner')).mean()
-
-        assert_frame_equal(result, expected)
-
-        with tm.assert_produces_warning(False):
-            not_expected = frame.groupby(pd.Grouper(level='inner')).mean()
-
-        assert not result.index.equals(not_expected.index)
-
-        # Group by single key list
-        with tm.assert_produces_warning(FutureWarning, check_stacklevel=False):
-            result = frame.groupby(['inner']).mean()
-
-        with tm.assert_produces_warning(False):
-            expected = frame.groupby([pd.Grouper(key='inner')]).mean()
-
-        assert_frame_equal(result, expected)
-
-        with tm.assert_produces_warning(False):
-            not_expected = frame.groupby(pd.Grouper(level='inner')).mean()
-
-        assert not result.index.equals(not_expected.index)
-
-        # Group by two keys ('B', 'inner')
-        with tm.assert_produces_warning(FutureWarning, check_stacklevel=False):
-            result = frame.groupby(['B', 'inner']).mean()
-
-        with tm.assert_produces_warning(False):
-            expected = frame.groupby(['B',
-                                      pd.Grouper(key='inner')]).mean()
-
-        assert_frame_equal(result, expected)
-
-        with tm.assert_produces_warning(False):
-            not_expected = frame.groupby(['B',
-                                          pd.Grouper(level='inner')
-                                          ]).mean()
-
-        assert not result.index.equals(not_expected.index)
-
-        # Group by two keys ('inner', 'B')
-        with tm.assert_produces_warning(FutureWarning, check_stacklevel=False):
-            result = frame.groupby(['inner', 'B']).mean()
-
-        with tm.assert_produces_warning(False):
-            expected = frame.groupby([pd.Grouper(key='inner'),
-                                      'B']).mean()
-
-        assert_frame_equal(result, expected)
-
-        with tm.assert_produces_warning(False):
-            not_expected = frame.groupby([pd.Grouper(level='inner'),
-                                          'B']).mean()
-        assert not result.index.equals(not_expected.index)
+    assert not result.index.equals(not_expected.index)
