715b74024fbfaca182eb1869ac816f87e4ef4b81
Brock Mendel <jbrockmendel@gmail.com>
2017-10-20 08:35:31 -0700

    fix timestamp test

--- pandas/tests/scalar/test_timestamp.py
+++ pandas/tests/scalar/test_timestamp.py
@@ -1093,7 +1093,7 @@ class TestTimestamp(object):
 
         if PY3:
             dt = ts.to_pydatetime()
-            assert dt.timestamp() == ts.timestamp() - ts.nanosecond
+            assert dt.timestamp() == round(ts.timestamp(), 6)
 
 
 class TestTimestampNsOperations(object):
