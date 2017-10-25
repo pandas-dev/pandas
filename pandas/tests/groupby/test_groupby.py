2af9aba277e48942bd3edd45cb503a110ba37ab3
Chris Mazzullo <chris.mazzullo@gmail.com>
2017-10-24 22:02:41 -0400

    Add comments about change

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -411,6 +411,7 @@ class TestGroupBy(MixIn):
         desc_groups = []
         for col in self.tsframe:
             group = grouped[col].describe()
+            # GH 17464 - Remove duplicate MultiIndex levels
             group_col = pd.MultiIndex(
                 levels=[[col], group.columns],
                 labels=[[0]*len(group.columns), range(len(group.columns))])
@@ -425,6 +426,7 @@ class TestGroupBy(MixIn):
                                          'C': 1, 'D': 1}, axis=1)
         result = groupedT.describe()
         expected = self.tsframe.describe().T
+        # GH 17464 - Remove duplicate MultiIndex levels
         expected.index = pd.MultiIndex(levels=[[0, 1], expected.index],
                                        labels=[[0, 0, 1, 1], range(len(expected.index))])
         tm.assert_frame_equal(result, expected)
