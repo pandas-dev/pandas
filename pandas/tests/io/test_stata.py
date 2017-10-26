bfd520da6663ba2fa25b597fb92e1af76397717d
Mike Richards <miker985@uw.edu>
2017-10-26 11:24:12 -0700

    Update comment per @gfyoung comment

--- pandas/tests/io/test_stata.py
+++ pandas/tests/io/test_stata.py
@@ -1333,18 +1333,16 @@ class TestStata(object):
     @pytest.mark.parametrize(
         'column', ['ms', 'day', 'week', 'month', 'qtr', 'half', 'yr'])
     def test_date_parsing_ignores_format_details(self, column):
-        """
-        Test that display formats are ignored when determining if a numeric
-        column is a date value.
-
-        All date types are stored as numbers and format associated with the
-        column denotes both the type of the date and the display format.
-
-        STATA supports 9 date types which each have distinct units. We test 7
-        of the 9 types, ignoring %tC and %tb. %tC is a variant of %tc that
-        accounts for leap seconds and %tb relies on STATAs business calendar.
-        """
         # GH 17797
+        # Test that display formats are ignored when determining if a numeric
+        # column is a date value.
+        #
+        # All date types are stored as numbers and format associated with the
+        # column denotes both the type of the date and the display format.
+        #
+        # STATA supports 9 date types which each have distinct units. We test 7
+        # of the 9 types, ignoring %tC and %tb. %tC is a variant of %tc that
+        # accounts for leap seconds and %tb relies on STATAs business calendar.
         df = read_stata(self.stata_dates)
         unformatted = df.loc[0, column]
         formatted = df.loc[0, column + "_fmt"]
