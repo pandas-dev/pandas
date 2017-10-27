df1e81060cf1b1ef9ffcdf38270851cf9961b99c
jbrockmendel <jbrockmendel@gmail.com>
2017-10-27 13:30:47 -0700

    Checks for tz/tzinfo validity in Timestamp.__new__ (#17943)

--- pandas/_libs/tslib.pyx
+++ pandas/_libs/tslib.pyx
@@ -30,6 +30,7 @@ from util cimport (is_integer_object, is_float_object, is_datetime64_object,
                    is_timedelta64_object, INT64_MAX)
 cimport util
 
+from cpython.datetime cimport PyTZInfo_Check
 # this is our datetime.pxd
 from datetime cimport (
     pandas_datetimestruct,
@@ -68,7 +69,7 @@ from .tslibs.parsing import parse_datetime_string
 
 cimport cython
 
-from pandas.compat import iteritems, callable
+from pandas.compat import iteritems
 
 import collections
 import warnings
@@ -373,12 +374,23 @@ class Timestamp(_Timestamp):
                           FutureWarning)
             freq = offset
 
+        if tzinfo is not None:
+            if not PyTZInfo_Check(tzinfo):
+                # tzinfo must be a datetime.tzinfo object, GH#17690
+                raise TypeError('tzinfo must be a datetime.tzinfo object, '
+                                'not %s' % type(tzinfo))
+            elif tz is not None:
+                raise ValueError('Can provide at most one of tz, tzinfo')
+
         if ts_input is _no_input:
             # User passed keyword arguments.
+            if tz is None:
+                # Handle the case where the user passes `tz` and not `tzinfo`
+                tz = tzinfo
             return Timestamp(datetime(year, month, day, hour or 0,
                                       minute or 0, second or 0,
                                       microsecond or 0, tzinfo),
-                             tz=tzinfo)
+                             tz=tz)
         elif is_integer_object(freq):
             # User passed positional arguments:
             # Timestamp(year, month, day[, hour[, minute[, second[,
@@ -386,6 +398,10 @@ class Timestamp(_Timestamp):
             return Timestamp(datetime(ts_input, freq, tz, unit or 0,
                                       year or 0, month or 0, day or 0,
                                       hour), tz=hour)
+
+        if tzinfo is not None:
+            # User passed tzinfo instead of tz; avoid silently ignoring
+            tz, tzinfo = tzinfo, None
 
         ts = convert_to_tsobject(ts_input, tz, unit, 0, 0)
 
