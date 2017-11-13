a447b3cbcee491b54a2f76788ea0629139ee4b07
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2017-11-13 13:01:44 +0100

    fixup

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -280,7 +280,7 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
         warnings.warn("'from_array' is deprecated and will be removed in a "
                       "future version. Please use the pd.Series(..) "
                       "constructor instead.", FutureWarning, stacklevel=2)
-        cls.from_array(arr, index=index, name=name, dtype=dtype, copy=copy,
+        cls._from_array(arr, index=index, name=name, dtype=dtype, copy=copy,
                        fastpath=fastpath)
 
     @classmethod
