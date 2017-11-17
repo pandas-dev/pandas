a69e6964ff0e615437934989251f05cc470ca1df
Paul Reidy <paul_reidy@outlook.com>
2017-11-17 22:07:10 +0000

    cleanup existing tests in frame/test_missing.py

--- pandas/tests/frame/test_missing.py
+++ pandas/tests/frame/test_missing.py
@@ -270,33 +270,33 @@ class TestDataFrameMissingData(TestData):
                                   pd.Timestamp('2012-11-11 00:00:00+01:00')]})
         assert_frame_equal(df.fillna(method='bfill'), exp)
 
-    def test_na_actions(self):
+    def test_na_actions_categorical(self):
 
-        cat = pd.Categorical([1, 2, 3, np.nan], categories=[1, 2, 3])
+        cat = Categorical([1, 2, 3, np.nan], categories=[1, 2, 3])
         vals = ["a", "b", np.nan, "d"]
-        df = pd.DataFrame({"cats": cat, "vals": vals})
-        cat2 = pd.Categorical([1, 2, 3, 3], categories=[1, 2, 3])
+        df = DataFrame({"cats": cat, "vals": vals})
+        cat2 = Categorical([1, 2, 3, 3], categories=[1, 2, 3])
         vals2 = ["a", "b", "b", "d"]
-        df_exp_fill = pd.DataFrame({"cats": cat2, "vals": vals2})
-        cat3 = pd.Categorical([1, 2, 3], categories=[1, 2, 3])
+        df_exp_fill = DataFrame({"cats": cat2, "vals": vals2})
+        cat3 = Categorical([1, 2, 3], categories=[1, 2, 3])
         vals3 = ["a", "b", np.nan]
-        df_exp_drop_cats = pd.DataFrame({"cats": cat3, "vals": vals3})
-        cat4 = pd.Categorical([1, 2], categories=[1, 2, 3])
+        df_exp_drop_cats = DataFrame({"cats": cat3, "vals": vals3})
+        cat4 = Categorical([1, 2], categories=[1, 2, 3])
         vals4 = ["a", "b"]
-        df_exp_drop_all = pd.DataFrame({"cats": cat4, "vals": vals4})
+        df_exp_drop_all = DataFrame({"cats": cat4, "vals": vals4})
 
         # fillna
         res = df.fillna(value={"cats": 3, "vals": "b"})
         tm.assert_frame_equal(res, df_exp_fill)
 
-        def f():
+        with tm.assert_raises_regex(ValueError, "fill value must be "
+                                                "in categories"):
             df.fillna(value={"cats": 4, "vals": "c"})
-
-        pytest.raises(ValueError, f)
 
         res = df.fillna(method='pad')
         tm.assert_frame_equal(res, df_exp_fill)
 
+        # dropna
         res = df.dropna(subset=["cats"])
         tm.assert_frame_equal(res, df_exp_drop_cats)
 
@@ -308,19 +308,20 @@ class TestDataFrameMissingData(TestData):
         df = pd.DataFrame({"cats": c, "vals": [1, 2, 3]})
 
         cat_exp = Categorical(["a", "b", "a"], categories=["a", "b"])
-        df_exp = pd.DataFrame({"cats": cat_exp, "vals": [1, 2, 3]})
+        df_exp = DataFrame({"cats": cat_exp, "vals": [1, 2, 3]})
 
         res = df.fillna("a")
         tm.assert_frame_equal(res, df_exp)
 
+    def test_fillna_categorical_nan(self):
         # GH 14021
-        # np.nan should always be a is a valid filler
+        # np.nan should always be a valid filler
         cat = Categorical([np.nan, 2, np.nan])
         val = Categorical([np.nan, np.nan, np.nan])
         df = DataFrame({"cats": cat, "vals": val})
         res = df.fillna(df.median())
         v_exp = [np.nan, np.nan, np.nan]
-        df_exp = pd.DataFrame({"cats": [2, 2, 2], "vals": v_exp},
+        df_exp = DataFrame({"cats": [2, 2, 2], "vals": v_exp},
                               dtype='category')
         tm.assert_frame_equal(res, df_exp)
 
@@ -331,17 +332,17 @@ class TestDataFrameMissingData(TestData):
 
         idx = pd.DatetimeIndex(['2011-01-01 09:00', '2016-01-01 23:45',
                                 '2011-01-01 09:00', pd.NaT, pd.NaT])
-        df = DataFrame({'a': pd.Categorical(idx)})
+        df = DataFrame({'a': Categorical(idx)})
         tm.assert_frame_equal(df.fillna(value=pd.NaT), df)
 
         idx = pd.PeriodIndex(['2011-01', '2011-01', '2011-01',
                               pd.NaT, pd.NaT], freq='M')
-        df = DataFrame({'a': pd.Categorical(idx)})
+        df = DataFrame({'a': Categorical(idx)})
         tm.assert_frame_equal(df.fillna(value=pd.NaT), df)
 
         idx = pd.TimedeltaIndex(['1 days', '2 days',
                                  '1 days', pd.NaT, pd.NaT])
-        df = pd.DataFrame({'a': pd.Categorical(idx)})
+        df = DataFrame({'a': Categorical(idx)})
         tm.assert_frame_equal(df.fillna(value=pd.NaT), df)
 
     def test_fillna_downcast(self):
