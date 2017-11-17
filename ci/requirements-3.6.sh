e7113555e8ea3cc1c275d77933f2a8f72ff82bf5
Jeff Reback <jeff@reback.net>
2017-11-17 07:10:01 -0500

    CI: remove pandas-gbqg

--- ci/requirements-3.6.sh
+++ ci/requirements-3.6.sh
@@ -4,4 +4,4 @@ source activate pandas
 
 echo "[install 3.6 downstream deps]"
 
-conda install -n pandas -c conda-forge pandas-gbq pandas-datareader xarray geopandas seaborn statsmodels scikit-learn dask
+conda install -n pandas -c conda-forge pandas-datareader xarray geopandas seaborn statsmodels scikit-learn dask
