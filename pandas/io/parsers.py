50d827ba6b577f82101a2c8c2c0d9910b4385df7
Ondrej Kokes <ondrej.kokes@gmail.com>
2017-11-22 11:25:46 +0100

    DOC: escaping code examples properly in read_csv docstring (#18412)

--- pandas/io/parsers.py
+++ pandas/io/parsers.py
@@ -246,8 +246,8 @@ comment : str, default None
     of a line, the line will be ignored altogether. This parameter must be a
     single character. Like empty lines (as long as ``skip_blank_lines=True``),
     fully commented lines are ignored by the parameter `header` but not by
-    `skiprows`. For example, if comment='#', parsing '#empty\\na,b,c\\n1,2,3'
-    with `header=0` will result in 'a,b,c' being
+    `skiprows`. For example, if ``comment='#'``, parsing
+    ``#empty\\na,b,c\\n1,2,3`` with ``header=0`` will result in 'a,b,c' being
     treated as the header.
 encoding : str, default None
     Encoding to use for UTF when reading/writing (ex. 'utf-8'). `List of Python
