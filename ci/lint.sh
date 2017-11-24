371c22f0142d8b679465719117d68b65b99e0f09
Matt Roeschke <emailformattr@gmail.com>
2017-11-23 17:13:06 -0800

    Remove lint rule; postpone to new issue

--- ci/lint.sh
+++ ci/lint.sh
@@ -90,25 +90,12 @@ if [ "$LINT" ]; then
     #
     # Check the following functions:
     # any(), all(), sum(), max(), min(), list(), dict(), set(), frozenset(), tuple(), str.join()
-    grep -R --include="*.py*" -E "[^_](any|all|sum|max|min|list|dict|set|frozenset|tuple|join)\(\[.* for .* in .*\]\)" .
+    grep -R --include="*.py*" -E "[^_](any|all|sum|max|min|list|dict|set|frozenset|tuple|join)\(\[.* for .* in .*\]\)"
 
     if [ $? = "0" ]; then
         RET=1
     fi
     echo "Check for use of lists instead of generators in built-in Python functions DONE"
-
-    echo "Check for use of comprehensions using set(), dict() or list()"
-
-    # Example: Avoid `list(i for i in some_iterator)` in favor of `[i for i in some_iterator]`
-    #
-    # Check the following functions:
-    # list(), set(), dict()
-    grep -R --include="*.py*" -E "(list|dict|[^frozen]set)\([^\{)=]* for .* in [^}]*\)" .
-
-    if [ $? = "0" ]; then
-        RET=1
-    fi
-    echo "Check for use of comprehensions using set(), dict() or list() DONE"
 
 else
     echo "NOT Linting"
