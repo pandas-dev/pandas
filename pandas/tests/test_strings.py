a08e9403b796743ecd00980e57f85cfbb051166f
Will Ayd <william.ayd@icloud.com>
2017-11-23 21:46:57 -0500

    Propogating NaN values when using str.split (#18450)

--- pandas/tests/test_strings.py
+++ pandas/tests/test_strings.py
@@ -2086,6 +2086,15 @@ class TestStringMethods(object):
         tm.assert_index_equal(result, exp)
         assert result.nlevels == 2
 
+    def test_split_nan_expand(self):
+        s = Series(["foo,bar,baz", NA])
+        result = s.str.split(",", expand=True)
+        exp = DataFrame([["foo", "bar", "baz"], [NA, NA, NA]])
+        tm.assert_frame_equal(result, exp)
+
+        # extra nan check - see GH 18463
+        assert all(np.isnan(x) for x in result.iloc[1])
+
     def test_split_with_name(self):
         # GH 12617
 
