73d918f953f301f1da43e085329f8e9de9d24f02
jbrockmendel <jbrockmendel@gmail.com>
2017-11-26 07:08:19 -0800

    fix missing arg in asvs (#18503)

--- asv_bench/benchmarks/timestamp.py
+++ asv_bench/benchmarks/timestamp.py
@@ -13,55 +13,55 @@ class TimestampProperties(object):
     def setup(self, tz):
         self.ts = Timestamp('2017-08-25 08:16:14', tzinfo=tz)
 
-    def time_tz(self):
+    def time_tz(self, tz):
         self.ts.tz
 
-    def time_offset(self):
+    def time_offset(self, tz):
         self.ts.offset
 
-    def time_dayofweek(self):
+    def time_dayofweek(self, tz):
         self.ts.dayofweek
 
-    def time_weekday_name(self):
+    def time_weekday_name(self, tz):
         self.ts.weekday_name
 
-    def time_dayofyear(self):
+    def time_dayofyear(self, tz):
         self.ts.dayofyear
 
-    def time_week(self):
+    def time_week(self, tz):
         self.ts.week
 
-    def time_quarter(self):
+    def time_quarter(self, tz):
         self.ts.quarter
 
-    def time_days_in_month(self):
+    def time_days_in_month(self, tz):
         self.ts.days_in_month
 
-    def time_freqstr(self):
+    def time_freqstr(self, tz):
         self.ts.freqstr
 
-    def time_is_month_start(self):
+    def time_is_month_start(self, tz):
         self.ts.is_month_start
 
-    def time_is_month_end(self):
+    def time_is_month_end(self, tz):
         self.ts.is_month_end
 
-    def time_is_quarter_start(self):
+    def time_is_quarter_start(self, tz):
         self.ts.is_quarter_start
 
-    def time_is_quarter_end(self):
+    def time_is_quarter_end(self, tz):
         self.ts.is_quarter_end
 
-    def time_is_year_start(self):
+    def time_is_year_start(self, tz):
         self.ts.is_quarter_end
 
-    def time_is_year_end(self):
+    def time_is_year_end(self, tz):
         self.ts.is_quarter_end
 
-    def time_is_leap_year(self):
+    def time_is_leap_year(self, tz):
         self.ts.is_quarter_end
 
-    def time_microsecond(self):
+    def time_microsecond(self, tz):
         self.ts.microsecond
 
 
@@ -74,13 +74,13 @@ class TimestampOps(object):
     def setup(self, tz):
         self.ts = Timestamp('2017-08-25 08:16:14', tz=tz)
 
-    def time_replace_tz(self):
+    def time_replace_tz(self, tz):
         self.ts.replace(tzinfo=pytz.timezone('US/Eastern'))
 
-    def time_replace_None(self):
+    def time_replace_None(self, tz):
         self.ts.replace(tzinfo=None)
 
-    def time_to_pydatetime(self):
+    def time_to_pydatetime(self, tz):
         self.ts.to_pydatetime()
 
 
