0596c21672bc2e8579c5a7646860f86130b58ce7
Brock Mendel <jbrockmendel@gmail.com>
2017-12-03 10:12:03 -0800

    add issue-specific test case

--- pandas/tests/tseries/offsets/test_fiscal.py
+++ pandas/tests/tseries/offsets/test_fiscal.py
@@ -619,3 +619,9 @@ def test_bunched_yearends():
     assert fy.apply(dt) == Timestamp('2004-01-03')
     assert fy + dt == Timestamp('2004-01-03')
     assert dt + fy == Timestamp('2004-01-03')
+
+    # Same thing, but starting from a Timestamp in the previous year.
+    dt = Timestamp('2003-12-31')
+    assert fy.rollback(dt) == Timestamp('2002-12-28')
+    assert (-fy).apply(dt) == Timestamp('2002-12-28')
+    assert dt - fy == Timestamp('2002-12-28')
