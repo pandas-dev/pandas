3f079083796e7323dd7951fec94ab57274d9da3a
Sven <sven.schellenberg@paradynsystems.com>
2017-12-05 13:47:42 +1100

    Fixed #18413, but test case not passing * Handle all-NaN columns differently when building metadata for categorical axes on saving hdf5 file * Categorical axes fail test case comparison due to type difference (even though there isn't a visibly type difference)

--- pandas/tests/io/test_pytables.py
+++ pandas/tests/io/test_pytables.py
@@ -4927,6 +4927,25 @@ class TestHDFStore(Base):
             result = read_hdf(path, 'df', where='obsids=B')
             tm.assert_frame_equal(result, expected)
 
+    def test_categorical_nan_only_columns(self):
+        # GH18413
+        # Check that read_hdf with categorical columns with NaN-only values can
+        # be read back.
+        df = pd.DataFrame({
+            'a': ['a', 'b', 'c', np.nan],
+            'b': [np.nan, np.nan, np.nan, np.nan],
+            'c': [1, 2, 3, 4]
+        })
+        df['a'] = df.a.astype('category')
+        df['b'] = df.b.astype('category')
+        expected = df.copy()
+        with ensure_clean_path(self.path) as path:
+            df.to_hdf(path, 'df', format='table', data_columns=True)
+            result = read_hdf(path, 'df')
+            print 'result', result.dtypes
+            print 'expected', expected.dtypes
+            tm.assert_frame_equal(result, expected)
+
     def test_duplicate_column_name(self):
         df = DataFrame(columns=["a", "a"], data=[[0, 0]])
 
