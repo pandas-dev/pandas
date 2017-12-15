d2a2372bf85a22a26ce422becfd108650fe67c6d
Tom Augspurger <tom.w.augspurger@gmail.com>
2017-12-15 15:08:58 -0600

    update

--- pandas/core/groupby.py
+++ pandas/core/groupby.py
@@ -2861,14 +2861,14 @@ def _get_grouper(obj, key=None, axis=0, level=None, sort=True,
     is_tuple = isinstance(key, tuple)
     all_hashable = is_tuple and all(is_hashable(x) for x in key)
 
-    original_grouper = None
     if is_tuple:
-        if not all_hashable or key not in obj:
+        if (all_hashable and key not in obj and set(key).issubset(obj)) or not all_hashable:
+            # column names ('a', 'b') -> ['a', 'b']
+            # arrays like (a, b) -> [a, b]
             msg = ("Interpreting tuple 'by' as a list of keys, rather than "
                    "a single key. Use 'by=[...]' instead of 'by=(...)'. In "
                    "the future, a tuple will always mean a single key.")
             warnings.warn(msg, FutureWarning, stacklevel=5)
-            original_grouper = key
             key = list(key)
 
     if not isinstance(key, list):
@@ -2943,8 +2943,6 @@ def _get_grouper(obj, key=None, axis=0, level=None, sort=True,
                 # Want to raise with the correct KeyError here
                 # The deprecation in #18731 means we may have
                 # the wrong error message here.
-                if original_grouper:
-                    gpr = original_grouper
                 raise KeyError(gpr)
         elif isinstance(gpr, Grouper) and gpr.key is not None:
             # Add key to exclusions
