1d7903ef549e79878d2411abf4366df706ba3ed6
Brock Mendel <jbrockmendel@gmail.com>
2017-12-18 17:36:36 -0800

    revert out of scope

--- pandas/core/ops.py
+++ pandas/core/ops.py
@@ -487,70 +487,47 @@ class _TimeOp(_Op):
                             'of a series/ndarray of type datetime64[ns] '
                             'or a timedelta')
 
-    def _convert_datetimelike_to_array(self, raw_values, values, other):
-        """
-        By the time we get here, we know that:
-            * inferred_type in ('datetime64', 'datetime', 'date', 'time') or
-                is_datetimetz(inferred_type)
-            * is_list_like(values)
-        """
-        supplied_dtype = None
-        if raw_values is not values:
-            # skip unnecessary isinstance call because we know values is an
-            # ndarray
-            pass
-        elif (isinstance(values, pd.Series) and
-              (is_timedelta64_dtype(values) or is_datetime64_dtype(values))):
-            # if this is a Series that contains relevant dtype info, then use
-            # this instead of the inferred type; this avoids coercing
-            # Series([NaT], dtype='datetime64[ns]') to
-            # Series([NaT], dtype='timedelta64[ns]')
-            supplied_dtype = values.dtype
-
-        inferred_type = lib.infer_dtype(values)
-
-        if (supplied_dtype is None and other is not None and
-            (other.dtype in ('timedelta64[ns]', 'datetime64[ns]')) and
-                isna(values).all()):
-            # if we have a other of timedelta, but use pd.NaT here we
-            # we are in the wrong path
-            values = np.empty(values.shape, dtype='timedelta64[ns]')
-            values[:] = iNaT
-
-        elif isinstance(values, pd.DatetimeIndex):
-            # a datelike
-            values = values.to_series()
-
-        elif (isinstance(raw_values, datetime.datetime) and
-              hasattr(raw_values, 'tzinfo')):
-            # datetime with tz
-            values = pd.DatetimeIndex(values)
-
-        elif is_datetimetz(values):
-            # datetime array with tz
-            if isinstance(values, ABCSeries):
-                values = values._values
-
-        elif not (isinstance(values, (np.ndarray, ABCSeries)) and
-                  is_datetime64_dtype(values)):
-            values = libts.array_to_datetime(values)
-
-        return values
-
     def _convert_to_array(self, values, name=None, other=None):
         """converts values to ndarray"""
         from pandas.core.tools.timedeltas import to_timedelta
 
         ovalues = values
+        supplied_dtype = None
         if not is_list_like(values):
             values = np.array([values])
 
-        inferred_type = lib.infer_dtype(values)
+        # if this is a Series that contains relevant dtype info, then use this
+        # instead of the inferred type; this avoids coercing Series([NaT],
+        # dtype='datetime64[ns]') to Series([NaT], dtype='timedelta64[ns]')
+        elif (isinstance(values, pd.Series) and
+              (is_timedelta64_dtype(values) or is_datetime64_dtype(values))):
+            supplied_dtype = values.dtype
 
+        inferred_type = lib.infer_dtype(values)
         if (inferred_type in ('datetime64', 'datetime', 'date', 'time') or
                 is_datetimetz(inferred_type)):
-            return self._convert_datetimelike_to_array(ovalues, values, other)
+            # if we have a other of timedelta, but use pd.NaT here we
+            # we are in the wrong path
+            if (supplied_dtype is None and other is not None and
+                (other.dtype in ('timedelta64[ns]', 'datetime64[ns]')) and
+                    isna(values).all()):
+                values = np.empty(values.shape, dtype='timedelta64[ns]')
+                values[:] = iNaT
 
+            # a datelike
+            elif isinstance(values, pd.DatetimeIndex):
+                values = values.to_series()
+            # datetime with tz
+            elif (isinstance(ovalues, datetime.datetime) and
+                  hasattr(ovalues, 'tzinfo')):
+                values = pd.DatetimeIndex(values)
+            # datetime array with tz
+            elif is_datetimetz(values):
+                if isinstance(values, ABCSeries):
+                    values = values._values
+            elif not (isinstance(values, (np.ndarray, ABCSeries)) and
+                      is_datetime64_dtype(values)):
+                values = libts.array_to_datetime(values)
         elif inferred_type in ('timedelta', 'timedelta64'):
             # have a timedelta, convert to to ns here
             values = to_timedelta(values, errors='coerce', box=False)
