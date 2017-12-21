6ddf34297d23ede61bdce7cfa3877b2e365f3a0f
topper-123 <terji78@gmail.com>
2017-12-21 15:02:36 +0000

    ENH: Let Categorical.rename_categories take a callable (#18862)

--- pandas/tests/categorical/test_api.py
+++ pandas/tests/categorical/test_api.py
@@ -71,9 +71,14 @@ class TestCategoricalAPI(object):
 
         exp_cat = Index(["a", "b", "c"])
         tm.assert_index_equal(cat.categories, exp_cat)
-        res = cat.rename_categories([1, 2, 3], inplace=True)
+
+        # GH18862 (let rename_categories take callables)
+        result = cat.rename_categories(lambda x: x.upper())
+        expected = Categorical(["A", "B", "C", "A"])
+        tm.assert_categorical_equal(result, expected)
 
         # and now inplace
+        res = cat.rename_categories([1, 2, 3], inplace=True)
         assert res is None
         tm.assert_numpy_array_equal(cat.__array__(), np.array([1, 2, 3, 1],
                                                               dtype=np.int64))
