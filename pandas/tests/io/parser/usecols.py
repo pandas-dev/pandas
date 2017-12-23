604b2a631e41c94fed2abab69bb2f5770c465170
Aaron Critchley <aaron.critchley@gmail.com>
2017-12-23 02:41:10 +0000

    Remembering Flake8 for once

--- pandas/tests/io/parser/usecols.py
+++ pandas/tests/io/parser/usecols.py
@@ -492,11 +492,13 @@ a,b,c
         tm.assert_frame_equal(df, expected)
 
         usecols = ['a', 'b', 'c', 'f']
-        with tm.assert_raises_regex(ValueError, msg.format(missing=r"\['f'\]")):
+        with tm.assert_raises_regex(
+                ValueError, msg.format(missing=r"\['f'\]")):
             self.read_csv(StringIO(data), usecols=usecols)
 
         usecols = ['a', 'b', 'f']
-        with tm.assert_raises_regex(ValueError, msg.format(missing=r"\['f'\]")):
+        with tm.assert_raises_regex(
+                ValueError, msg.format(missing=r"\['f'\]")):
             self.read_csv(StringIO(data), usecols=usecols)
 
         usecols = ['a', 'b', 'f', 'g']
@@ -525,9 +527,11 @@ a,b,c
         # tm.assert_frame_equal(df, expected)
 
         usecols = ['A', 'B', 'C', 'f']
-        with tm.assert_raises_regex(ValueError, msg.format(missing=r"\['f'\]")):
+        with tm.assert_raises_regex(
+                ValueError, msg.format(missing=r"\['f'\]")):
             self.read_csv(StringIO(data), header=0, names=names,
                           usecols=usecols)
         usecols = ['A', 'B', 'f']
-        with tm.assert_raises_regex(ValueError, msg.format(missing=r"\['f'\]")):
+        with tm.assert_raises_regex(
+                ValueError, msg.format(missing=r"\['f'\]")):
             self.read_csv(StringIO(data), names=names, usecols=usecols)
