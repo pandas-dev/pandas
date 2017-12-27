445bda4b7d858c5d37ac900527f82ed707f34091
topper-123 <terji78@gmail.com>
2017-12-27 19:39:23 +0000

    Update doc strings, pd.Panel is deprecated (#18956)

--- pandas/core/panel.py
+++ pandas/core/panel.py
@@ -111,6 +111,13 @@ class Panel(NDFrame):
     """
     Represents wide format panel data, stored as 3-dimensional array
 
+   .. deprecated:: 0.20.0
+       The recommended way to represent 3-D data are with a MultiIndex on a
+       DataFrame via the :attr:`~Panel.to_frame()` method or with the
+       `xarray package <http://xarray.pydata.org/en/stable/>`__.
+       Pandas provides a :attr:`~Panel.to_xarray()` method to automate this
+       conversion.
+
     Parameters
     ----------
     data : ndarray (items x major x minor), or dict of DataFrames
