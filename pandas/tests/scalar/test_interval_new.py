d7818205362785bae50dfa0655a2d714b2eafd43
Alexander Lenail <zfrenchee@gmail.com>
2017-12-30 18:39:29 -0500

    Tiny restructuring

--- pandas/tests/scalar/test_interval_new.py
+++ pandas/tests/scalar/test_interval_new.py
@@ -44,49 +44,6 @@ class TestIntervalIndex(Base):
         other = Interval(1, 3, closed=other_side)
         assert ivl.covers(other) == should_cover[ivl_side][other_side]
 
-    @pytest.mark.parametrize("idx_side", ['right', 'left', 'both', 'neither'])
-    @pytest.mark.parametrize("ivl_side", ['right', 'left', 'both', 'neither'])
-    @pytest.mark.parametrize("ivl_range", [(1, 3), (0, 3), (0, 2), (2, 4)])
-    def test_interval_covers_intervalIndex(self, idx_side, ivl_side, ivl_range):
-
-        # class Interval:
-        #     def covers(self, other: IntervalIndex) -> IntegerArray1D
-
-        idx = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed=idx_side)
-        ivl = Interval(*ivl_range, closed=ivl_side)
-
-        should_cover = {
-            # idx_side:
-            #   ivl_side: {ivl_range: expected_result}
-            'right': {
-                'right': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
-                'left': {(1, 3): [], (0, 3): [0], (0, 2): [0], (2, 4): [1]},
-                'both': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
-                'neither': {(1, 3): [], (0, 3): [0], (0, 2): [0], (2, 4): [1]}
-            },
-            'left': {
-                'right': {(1, 3): [1], (0, 3): [1, 2], (0, 2): [], (2, 4): []},
-                'left': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
-                'both': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
-                'neither': {(1, 3): [1], (0, 3): [1, 2], (0, 2): [], (2, 4): []}
-            },
-            'both': {
-                'right': {(1, 3): [1], (0, 3): [1, 2], (0, 2): [], (2, 4): []},
-                'left': {(1, 3): [], (0, 3): [0], (0, 2): [0], (2, 4): [1]},
-                'both': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
-                'neither': {(1, 3): [], (0, 3): [], (0, 2): [], (2, 4): []}
-            },
-            'neither': {
-                'right': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
-                'left': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
-                'both': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
-                'neither': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]}
-            }
-        }
-
-        tm.assert_numpy_array_equal(ivl.covers(idx),
-                                    should_cover[idx_side][ivl_side][ivl_range])
-
     @pytest.mark.parametrize("ivl_side", ['right', 'left', 'both', 'neither'])
     @pytest.mark.parametrize("other_side", ['right', 'left', 'both', 'neither'])
     @pytest.mark.parametrize("ivl_range", [(1, 3), (-1, 1), (3, 5)])
@@ -140,46 +97,46 @@ class TestIntervalIndex(Base):
 
     @pytest.mark.parametrize("idx_side", ['right', 'left', 'both', 'neither'])
     @pytest.mark.parametrize("ivl_side", ['right', 'left', 'both', 'neither'])
-    @pytest.mark.parametrize("ivl_range", [(1, 3), (1, 2), (0, 2), (3, 4)])
-    def test_interval_overlaps_intervalIndex(self, idx_side, ivl_side, ivl_range):
+    @pytest.mark.parametrize("ivl_range", [(1, 3), (0, 3), (0, 2), (2, 4)])
+    def test_interval_covers_intervalIndex(self, idx_side, ivl_side, ivl_range):
 
         # class Interval:
-        #     def overlaps(self, other: IntervalIndex) -> IntegerArray1D
+        #     def covers(self, other: IntervalIndex) -> IntegerArray1D
 
         idx = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed=idx_side)
         ivl = Interval(*ivl_range, closed=ivl_side)
 
-        should_overlap = {
+        should_cover = {
             # idx_side:
             #   ivl_side: {ivl_range: expected_result}
             'right': {
-                'right': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
-                'left': {(1, 3): [0, 1, 2], (1, 2): [0, 2], (0, 2): [0, 2], (3, 4): [1, 2]},
-                'both': {(1, 3): [0, 1, 2], (1, 2): [0, 2], (0, 2): [0, 2], (3, 4): [1, 2]},
-                'neither': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'right': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
+                'left': {(1, 3): [], (0, 3): [0], (0, 2): [0], (2, 4): [1]},
+                'both': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
+                'neither': {(1, 3): [], (0, 3): [0], (0, 2): [0], (2, 4): [1]}
             },
             'left': {
-                'right': {(1, 3): [1, 2], (1, 2): [1, 2], (0, 2): [0, 1, 2], (3, 4): []},
-                'left': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
-                'both': {(1, 3): [1, 2], (1, 2): [1, 2], (0, 2): [0, 1, 2], (3, 4): []},
-                'neither': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'right': {(1, 3): [1], (0, 3): [1, 2], (0, 2): [], (2, 4): []},
+                'left': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
+                'both': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
+                'neither': {(1, 3): [1], (0, 3): [1, 2], (0, 2): [], (2, 4): []}
             },
             'both': {
-                'right': {(1, 3): [1, 2], (1, 2): [1, 2], (0, 2): [0, 1, 2], (3, 4): []},
-                'left': {(1, 3): [0, 1, 2], (1, 2): [0, 2], (0, 2): [0, 2], (3, 4): [1, 2]},
-                'both': {(1, 3): [0, 1, 2], (1, 2): [0, 1, 2], (0, 2): [0, 1, 2], (3, 4): [1, 2]},
-                'neither': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'right': {(1, 3): [1], (0, 3): [1, 2], (0, 2): [], (2, 4): []},
+                'left': {(1, 3): [], (0, 3): [0], (0, 2): [0], (2, 4): [1]},
+                'both': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
+                'neither': {(1, 3): [], (0, 3): [], (0, 2): [], (2, 4): []}
             },
             'neither': {
-                'right': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
-                'left': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
-                'both': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
-                'neither': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'right': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
+                'left': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
+                'both': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]},
+                'neither': {(1, 3): [1, 2], (0, 3): [0, 1, 2], (0, 2): [0], (2, 4): [1]}
             }
         }
 
-        tm.assert_numpy_array_equal(ivl.overlaps(idx),
-                                    should_overlap[idx_side][ivl_side][ivl_range])
+        tm.assert_numpy_array_equal(ivl.covers(idx),
+                                    should_cover[idx_side][ivl_side][ivl_range])
 
     @pytest.mark.parametrize("idx_side", ['right', 'left', 'both', 'neither'])
     @pytest.mark.parametrize("ivl_side", ['right', 'left', 'both', 'neither'])
@@ -224,18 +181,48 @@ class TestIntervalIndex(Base):
         tm.assert_numpy_array_equal(idx.covers(ivl),
                                     should_cover[idx_side][ivl_side][ivl_range])
 
-    def test_intervalIndex_covers_intervalIndex(self):
+    @pytest.mark.parametrize("idx_side", ['right', 'left', 'both', 'neither'])
+    @pytest.mark.parametrize("ivl_side", ['right', 'left', 'both', 'neither'])
+    @pytest.mark.parametrize("ivl_range", [(1, 3), (1, 2), (0, 2), (3, 4)])
+    def test_interval_overlaps_intervalIndex(self, idx_side, ivl_side, ivl_range):
 
-        # class IntervalIndex:
-        #     def covers(self, other: IntervalIndex) -> Tuple[IntegerArray1D, IntegerArray1D]
+        # class Interval:
+        #     def overlaps(self, other: IntervalIndex) -> IntegerArray1D
 
-        idx1 = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed='right')
-        idx2 = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed='left')
-        idx3 = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed='both')
+        idx = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed=idx_side)
+        ivl = Interval(*ivl_range, closed=ivl_side)
 
-        self._compare_tuple_of_numpy_array(idx.covers(idx1), (np.array([0,1,2,2]), np.array([0,1,1,2])))
-        self._compare_tuple_of_numpy_array(idx.covers(idx2), (np.array([2]), np.array([1])))
-        self._compare_tuple_of_numpy_array(idx.covers(idx3), (np.array([0,1,2,2]), np.array([0,1,1,2])))
+        should_overlap = {
+            # idx_side:
+            #   ivl_side: {ivl_range: expected_result}
+            'right': {
+                'right': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'left': {(1, 3): [0, 1, 2], (1, 2): [0, 2], (0, 2): [0, 2], (3, 4): [1, 2]},
+                'both': {(1, 3): [0, 1, 2], (1, 2): [0, 2], (0, 2): [0, 2], (3, 4): [1, 2]},
+                'neither': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+            },
+            'left': {
+                'right': {(1, 3): [1, 2], (1, 2): [1, 2], (0, 2): [0, 1, 2], (3, 4): []},
+                'left': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'both': {(1, 3): [1, 2], (1, 2): [1, 2], (0, 2): [0, 1, 2], (3, 4): []},
+                'neither': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+            },
+            'both': {
+                'right': {(1, 3): [1, 2], (1, 2): [1, 2], (0, 2): [0, 1, 2], (3, 4): []},
+                'left': {(1, 3): [0, 1, 2], (1, 2): [0, 2], (0, 2): [0, 2], (3, 4): [1, 2]},
+                'both': {(1, 3): [0, 1, 2], (1, 2): [0, 1, 2], (0, 2): [0, 1, 2], (3, 4): [1, 2]},
+                'neither': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+            },
+            'neither': {
+                'right': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'left': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'both': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+                'neither': {(1, 3): [1, 2], (1, 2): [2], (0, 2): [0, 2], (3, 4): []},
+            }
+        }
+
+        tm.assert_numpy_array_equal(ivl.overlaps(idx),
+                                    should_overlap[idx_side][ivl_side][ivl_range])
 
     @pytest.mark.parametrize("idx_side", ['right', 'left', 'both', 'neither'])
     @pytest.mark.parametrize("ivl_side", ['right', 'left', 'both', 'neither'])
@@ -279,6 +266,19 @@ class TestIntervalIndex(Base):
 
         tm.assert_numpy_array_equal(idx.overlaps(ivl),
                                     should_overlap[idx_side][ivl_side][ivl_range])
+
+    def test_intervalIndex_covers_intervalIndex(self):
+
+        # class IntervalIndex:
+        #     def covers(self, other: IntervalIndex) -> Tuple[IntegerArray1D, IntegerArray1D]
+
+        idx1 = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed='right')
+        idx2 = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed='left')
+        idx3 = IntervalIndex.from_tuples([(0, 1), (2, 3), (1, 3)], closed='both')
+
+        self._compare_tuple_of_numpy_array(idx.covers(idx1), (np.array([0,1,2,2]), np.array([0,1,1,2])))
+        self._compare_tuple_of_numpy_array(idx.covers(idx2), (np.array([2]), np.array([1])))
+        self._compare_tuple_of_numpy_array(idx.covers(idx3), (np.array([0,1,2,2]), np.array([0,1,1,2])))
 
     def test_intervalIndex_overlaps_intervalIndex(self):
 
