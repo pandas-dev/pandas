d023d9b2eaf2c3eefcef4b052320a5ee82fe92da
jbrockmendel <jbrockmendel@gmail.com>
2018-01-02 03:23:49 -0800

    Fix Series[timedelta64]+DatetimeIndex[tz] bugs (#18884)
    closes #13905

--- pandas/tests/indexes/datetimes/test_arithmetic.py
+++ pandas/tests/indexes/datetimes/test_arithmetic.py
@@ -364,6 +364,33 @@ class TestDatetimeIndexArithmetic(object):
             with pytest.raises(OverflowError):
                 dtimin - variant
 
+    @pytest.mark.parametrize('names', [('foo', None, None),
+                                       ('baz', 'bar', None),
+                                       ('bar', 'bar', 'bar')])
+    @pytest.mark.parametrize('tz', [None, 'America/Chicago'])
+    def test_dti_add_series(self, tz, names):
+        # GH#13905
+        index = DatetimeIndex(['2016-06-28 05:30', '2016-06-28 05:31'],
+                              tz=tz, name=names[0])
+        ser = Series([Timedelta(seconds=5)] * 2,
+                     index=index, name=names[1])
+        expected = Series(index + Timedelta(seconds=5),
+                          index=index, name=names[2])
+
+        # passing name arg isn't enough when names[2] is None
+        expected.name = names[2]
+        assert expected.dtype == index.dtype
+        result = ser + index
+        tm.assert_series_equal(result, expected)
+        result2 = index + ser
+        tm.assert_series_equal(result2, expected)
+
+        expected = index + Timedelta(seconds=5)
+        result3 = ser.values + index
+        tm.assert_index_equal(result3, expected)
+        result4 = index + ser.values
+        tm.assert_index_equal(result4, expected)
+
     @pytest.mark.parametrize('box', [np.array, pd.Index])
     def test_dti_add_offset_array(self, tz, box):
         # GH#18849
