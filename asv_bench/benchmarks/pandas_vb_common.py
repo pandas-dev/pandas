4fe43e441782fbe02467c9fce4bf0dfca931f99c
Matt Roeschke <emailformattr@gmail.com>
2018-01-03 19:58:27 -0800

    CLN: ASV Strings

--- asv_bench/benchmarks/pandas_vb_common.py
+++ asv_bench/benchmarks/pandas_vb_common.py
@@ -1,27 +1,29 @@
 import os
-from pandas import *
-import pandas as pd
-from numpy.random import randn
-from numpy.random import randint
-import pandas.util.testing as tm
-import random
-import numpy as np
-import threading
 from importlib import import_module
 
+import numpy as np
 try:
-    from pandas.compat import range
+    from pandas import Panel
 except ImportError:
-    pass
+    from pandas import WidePanel as Panel  # noqa
+
+# Compatibility import for lib
+for imp in ['pandas._libs.lib', 'pandas.lib', 'pandas_tseries']:
+    try:
+        lib = import_module(imp)
+        break
+    except:
+        pass
 
 numeric_dtypes = [np.int64, np.int32, np.uint32, np.uint64, np.float32,
                   np.float64, np.int16, np.int8, np.uint16, np.uint8]
 datetime_dtypes = [np.datetime64, np.timedelta64]
 
-# This function just needs to be imported into each benchmark file in order to
-# sets up the random seed before each function.
-# http://asv.readthedocs.io/en/latest/writing_benchmarks.html
+
 def setup(*args, **kwargs):
+    # This function just needs to be imported into each benchmark file to
+    # set up the random seed before each function.
+    # http://asv.readthedocs.io/en/latest/writing_benchmarks.html
     np.random.seed(1234)
 
 
@@ -42,22 +44,3 @@ class BaseIO(object):
 
     def teardown(self, *args, **kwargs):
         self.remove(self.fname)
-
-# Compatibility import for lib
-for imp in ['pandas._libs.lib', 'pandas.lib', 'pandas_tseries']:
-    try:
-        lib = import_module(imp)
-        break
-    except:
-        pass
-
-try:
-    Panel = Panel
-except Exception:
-    Panel = WidePanel
-
-# didn't add to namespace until later
-try:
-    from pandas.core.index import MultiIndex
-except ImportError:
-    pass
