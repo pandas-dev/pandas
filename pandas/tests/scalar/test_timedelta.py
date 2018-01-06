f101378da9f156f8d563d0566d087a9bdd71dc14
Brock Mendel <jbrockmendel@gmail.com>
2018-01-06 09:58:29 -0800

    requested comments

--- pandas/tests/scalar/test_timedelta.py
+++ pandas/tests/scalar/test_timedelta.py
@@ -209,6 +209,11 @@ class TestTimedeltaArithmetic(object):
         scalar = Timedelta(hours=3, minutes=4)
 
         # scalar others
+        # x // Timedelta is defined only for timedelta-like x. int-like,
+        # float-like, and date-like, in particular, should all either
+        # a) raise TypeError directly or
+        # b) return NotImplemented, following which the reversed
+        #    operation will raise TypeError.
         assert td.__rfloordiv__(scalar) == 1
         assert (-td).__rfloordiv__(scalar.to_pytimedelta()) == -2
         assert (2 * td).__rfloordiv__(scalar.to_timedelta64()) == 0
