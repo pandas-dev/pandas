df545a0d65444c685201aa336a0e0c009785d507
Brock Mendel <jbrockmendel@gmail.com>
2018-01-10 17:57:31 -0800

    docstring, tests for NullFrequencyError in shift

--- pandas/tseries/offsets.py
+++ pandas/tseries/offsets.py
@@ -1233,6 +1233,17 @@ class SemiMonthEnd(SemiMonthOffset):
         return roll
 
     def _apply_index_days(self, i, roll):
+        """Add days portion of offset to DatetimeIndex i
+
+        Parameters
+        ----------
+        i : DatetimeIndex
+        roll : ndarray[int64_t]
+
+        Returns
+        -------
+        result : DatetimeIndex
+        """
         nanos = (roll % 2) * Timedelta(days=self.day_of_month).value
         i += nanos.astype('timedelta64[ns]')
         return i + Timedelta(days=-1)
@@ -1279,6 +1290,17 @@ class SemiMonthBegin(SemiMonthOffset):
         return roll
 
     def _apply_index_days(self, i, roll):
+        """Add days portion of offset to DatetimeIndex i
+
+        Parameters
+        ----------
+        i : DatetimeIndex
+        roll : ndarray[int64_t]
+
+        Returns
+        -------
+        result : DatetimeIndex
+        """
         nanos = (roll % 2) * Timedelta(days=self.day_of_month - 1).value
         return i + nanos.astype('timedelta64[ns]')
 
