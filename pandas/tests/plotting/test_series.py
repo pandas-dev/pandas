1961718d73fe9809bed49502d02474595f0d87eb
tommyod <tommy.odland@gmail.com>
2018-01-13 19:33:42 +0100

    Added a test for plot.kde with  as an integer

--- pandas/tests/plotting/test_series.py
+++ pandas/tests/plotting/test_series.py
@@ -622,6 +622,7 @@ class TestSeriesPlots(TestPlotBase):
             pytest.skip("mpl is not supported")
 
         from numpy import linspace
+        _check_plot_works(self.ts.plot.kde, bw_method=None, ind=20)
         _check_plot_works(self.ts.plot.kde, bw_method=.5,
                           ind=linspace(-100, 100, 20))
         _check_plot_works(self.ts.plot.density, bw_method=.5,
@@ -897,3 +898,4 @@ class TestSeriesPlots(TestPlotBase):
             freq=CustomBusinessDay(holidays=['2014-05-26'])))
 
         _check_plot_works(s.plot)
+
