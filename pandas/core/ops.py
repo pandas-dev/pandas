e93e879cfe740268fc6fbe6505df268f4ee51b28
Brock Mendel <jbrockmendel@gmail.com>
2018-01-17 10:23:00 -0800

    Move ndarray and Index cases to the end

--- pandas/core/ops.py
+++ pandas/core/ops.py
@@ -809,9 +809,30 @@ def _comp_method_SERIES(op, name, str_rep, masker=False):
             return self._constructor(res, index=self.index, name=res_name)
 
         elif isinstance(other, ABCSeries):
+            # Note: Ordering matters; this needs to go before
+            # is_categorical_dtype(self) branch.
             res = na_op(self.values, other.values)
             return self._constructor(res,
                                      index=self.index, name=res_name)
+
+        elif is_categorical_dtype(self):
+            # cats are a special case as get_values() would return an ndarray,
+            # which would then not take categories ordering into account
+            # we can go directly to op, as the na_op would just test again and
+            # dispatch to it.
+            with np.errstate(all='ignore'):
+                res = op(self.values, other)
+                # Note: self.values is a pd.Categorical object
+            return self._constructor(res, index=self.index,
+                                     name=res_name, dtype='bool')
+
+        elif isinstance(other, pd.Categorical):
+            # Ordering of conditions here matters; we know at this point
+            # that not is_categorical_dtype(self)
+            msg = ("Cannot compare a Categorical for op {op} with Series "
+                   "of dtype {typ}.\nIf you want to compare values, use "
+                   "'series <op> np.asarray(other)'.")
+            raise TypeError(msg.format(op=op, typ=self.dtype))
 
         elif isinstance(other, (np.ndarray, pd.Index)):
             # do not check length of zerodim array
@@ -824,24 +845,6 @@ def _comp_method_SERIES(op, name, str_rep, masker=False):
             return self._constructor(res,
                                      index=self.index).__finalize__(self)
             # TODO: Why __finalize__ here but not elsewhere?
-
-        elif is_categorical_dtype(self):
-            # cats are a special case as get_values() would return an ndarray,
-            # which would then not take categories ordering into account
-            # we can go directly to op, as the na_op would just test again and
-            # dispatch to it.
-            with np.errstate(all='ignore'):
-                res = op(self.values, other)
-            return self._constructor(res, index=self.index,
-                                     name=res_name, dtype='bool')
-
-        elif isinstance(other, pd.Categorical):
-            # Ordering of conditions here matters; we know at this point
-            # that not is_categorical_dtype(self)
-            msg = ("Cannot compare a Categorical for op {op} with Series "
-                   "of dtype {typ}.\nIf you want to compare values, use "
-                   "'series <op> np.asarray(other)'.")
-            raise TypeError(msg.format(op=op, typ=self.dtype))
 
         else:
             values = self.get_values()
