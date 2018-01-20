6bcb2d704c9094f08403796ca70c8579ffb882e2
blueTaxi <32769797+blueTaxi@users.noreply.github.com>
2018-01-21 08:59:07 +1100

    Add logic to fix CI failure from new test

--- pandas/core/reshape/pivot.py
+++ pandas/core/reshape/pivot.py
@@ -455,16 +455,21 @@ def crosstab(index, columns, values=None, rownames=None, colnames=None,
 
     from pandas import DataFrame
     df = DataFrame(data, index=common_idx)
+    original_columns_set = set(df.columns.values)
     if values is None:
         df['__dummy__'] = 0
         kwargs = {'aggfunc': len, 'fill_value': 0}
     else:
         df['__dummy__'] = values
         kwargs = {'aggfunc': aggfunc}
+    added_column = list(set(df.columns.values) - original_columns_set)[0]
 
     table = df.pivot_table('__dummy__', index=rownames, columns=colnames,
                            margins=margins, margins_name=margins_name,
                            dropna=dropna, **kwargs)
+
+    if not table.empty:
+        table = table[added_column]
 
     # Post-process
     if normalize is not False:
