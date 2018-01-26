d0baf8f309b6281225f3cdf764b95b747d84dea5
Ming Li <ming.george.li@gmail.com>
2018-01-26 12:17:53 +0000

    update pct_change test case with freq shift

--- pandas/tests/frame/test_timeseries.py
+++ pandas/tests/frame/test_timeseries.py
@@ -108,7 +108,9 @@ class TestDataFrameTimeSeriesMethods(TestData):
 
         rs = self.tsframe.pct_change(freq='5D')
         filled = self.tsframe.fillna(method='pad')
-        assert_frame_equal(rs, filled / filled.shift(freq='5D') - 1)
+        assert_frame_equal(rs,
+                           (filled / filled.shift(freq='5D') - 1)
+                           .reindex_like(filled))
 
     def test_pct_change_shift_over_nas(self):
         s = Series([1., 1.5, np.nan, 2.5, 3.])
