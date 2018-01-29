f67d7f483e29d45dd0cdd90dafbcfcbacef24d8a
Matthew Kirk <matt@matthewkirk.com>
2018-01-29 15:02:34 +0700

    BUG: Fix problem with SparseDataFrame not persisting to csv

--- pandas/core/internals.py
+++ pandas/core/internals.py
@@ -704,7 +704,9 @@ class Block(PandasObject):
                         **kwargs):
         """ convert to our native types format, slicing if desired """
 
-        values = self.values
+        # values = self.values
+        values = self.get_values()
+
         if slicer is not None:
             values = values[:, slicer]
         mask = isna(values)
