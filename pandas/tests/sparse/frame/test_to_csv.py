8ba1efe00cca5d032f7606ad14728d8b95401c02
Matthew Kirk <matt@matthewkirk.com>
2018-01-31 06:52:57 +0700

    Fix failing test due to blank lines

--- pandas/tests/sparse/frame/test_to_csv.py
+++ pandas/tests/sparse/frame/test_to_csv.py
@@ -4,7 +4,7 @@ from pandas.util import testing as tm
 
 
 def test_to_csv_sparse_dataframe():
-    fill_values = [0, 1, np.nan, None]
+    fill_values = [np.nan, 0, None, 1]
 
     for fill_value in fill_values:
         sdf = SparseDataFrame({'a': fill_values},
@@ -12,6 +12,6 @@ def test_to_csv_sparse_dataframe():
 
         with tm.ensure_clean('sparse_df.csv') as path:
             sdf.to_csv(path, index=False)
-            df = read_csv(path)
-
+            df = read_csv(path, skip_blank_lines=False)
+            
             tm.assert_sp_frame_equal(df.to_sparse(fill_value=fill_value), sdf)
