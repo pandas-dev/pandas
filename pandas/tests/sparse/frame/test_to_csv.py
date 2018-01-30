6538f1980d81edd2394852fd99d718102c656093
Matthew Kirk <matt@matthewkirk.com>
2018-01-30 09:45:57 +0700

    FIX: Flake8 issues cleanup

--- pandas/tests/sparse/frame/test_to_csv.py
+++ pandas/tests/sparse/frame/test_to_csv.py
@@ -1,14 +1,14 @@
-import pytest
 import numpy as np
-from pandas import SparseDataFrame, DataFrame, SparseSeries, read_csv
+from pandas import SparseDataFrame, read_csv
 from pandas.util import testing as tm
-from itertools import product
+
 
 def test_to_csv_sparse_dataframe():
     fill_values = [0, 1, np.nan, None]
 
     for fill_value in fill_values:
-        sdf = SparseDataFrame({'a': fill_values}, default_fill_value = fill_value)
+        sdf = SparseDataFrame({'a': fill_values},
+                              default_fill_value=fill_value)
 
         with tm.ensure_clean('sparse_df.csv') as path:
             sdf.to_csv(path, index=False)
