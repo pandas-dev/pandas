3b66275f39c1aa0f3141d414b6b0679a069eb5d1
Joerg <joerg.doepfert@flixbus.com>
2018-02-02 11:46:24 +0100

    rearrange if clause

--- pandas/core/algorithms.py
+++ pandas/core/algorithms.py
@@ -13,14 +13,14 @@ from pandas.core.dtypes.generic import (
     ABCIndexClass, ABCCategorical)
 from pandas.core.dtypes.common import (
     is_unsigned_integer_dtype, is_signed_integer_dtype,
-    is_integer_dtype, is_complex_dtype,
+    is_integer_dtype, is_complex_dtype, is_integer, is_float,
     is_object_dtype,
     is_categorical_dtype, is_sparse,
     is_period_dtype,
     is_numeric_dtype, is_float_dtype,
     is_bool_dtype, needs_i8_conversion,
     is_categorical, is_datetimetz, is_datetime_or_timedelta_dtype,
-    is_datetime64_any_dtype, is_datetime64tz_dtype,
+    is_datetime64_any_dtype, is_datetime64tz_dtype, is_datetimelike,
     is_timedelta64_dtype, is_interval_dtype,
     is_scalar, is_list_like,
     _ensure_platform_int, _ensure_object,
@@ -404,16 +404,18 @@ def isin(comps, values):
     if not isinstance(values, (ABCIndex, ABCSeries, np.ndarray)):
         values = construct_1d_object_array_from_listlike(list(values))
 
-    from pandas.core.dtypes.common import is_datetimelike, is_float
     comps, dtype, _ = _ensure_data(comps)
-    # Convert `values` to `dtype` if `values` is datetime/float-like and `dtype` is datetime/float-like
-    if (is_datetime_or_timedelta_dtype(dtype) and
-        (is_datetime_or_timedelta_dtype(values) or
-         all(is_datetimelike(i) for i in values))):
-        values, _, _ = _ensure_data(values, dtype=dtype)
-    elif (is_float_dtype(dtype) and
-          (is_float_dtype(values) or
-           all(is_float(i) for i in values))):
+    # Convert `values` to `dtype` if dtype of `values` is like `dtype`
+    check_int = (is_integer_dtype(dtype) and
+                 (is_integer_dtype(values) or
+                  all(is_integer(i) for i in values)))
+    check_float = (is_float_dtype(dtype) and
+                   (is_float_dtype(values) or
+                    all(is_float(i) for i in values)))
+    check_datetime = (is_datetime_or_timedelta_dtype(dtype) and
+                      (is_datetime_or_timedelta_dtype(values) or
+                       all(is_datetimelike(i) for i in values)))
+    if check_int or check_float or check_datetime:
         values, _, _ = _ensure_data(values, dtype=dtype)
     else:
         values, _, _ = _ensure_data(values)
