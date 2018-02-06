2de02687943b0c2278206ae7842c17b49f40fa16
Brock Mendel <jbrockmendel@gmail.com>
2018-02-05 18:32:10 -0800

    test, whatsnew note

--- pandas/tests/frame/test_operators.py
+++ pandas/tests/frame/test_operators.py
@@ -451,6 +451,18 @@ class TestDataFrameOperators(TestData):
         with tm.assert_raises_regex(NotImplementedError, 'fill_value'):
             self.frame.add(self.frame.iloc[0], axis='index', fill_value=3)
 
+    def test_arith_flex_zero_len_raises(self):
+        # GH#19522 passing fill_value to frame flex arith methods should
+        # raise even in the zero-length special cases
+        ser = pd.Series([])
+        df = pd.DataFrame([[1, 2], [3, 4]])
+
+        with tm.assert_raises_regex(NotImplementedError, 'fill_value'):
+            df.add(ser, fill_value='E')
+
+        with tm.assert_raises_regex(NotImplementedError, 'fill_value'):
+            ser.to_frame().sub(df[0], axis=None, fill_value=3)
+
     def test_binary_ops_align(self):
 
         # test aligning binary ops
