6e8c0414b00caf02148fdebddf68bc18136b8567
Matthew Kirk <matt@matthewkirk.com>
2018-02-07 10:23:20 +0700

    Prefer cmath over math.h for cpp

--- pandas/_libs/src/headers/math.h
+++ pandas/_libs/src/headers/math.h
@@ -2,10 +2,10 @@
 #define _PANDAS_MATH_H_
 
 #if defined(_MSC_VER) && (_MSC_VER < 1800)
-#include <math.h>
+#include <cmath>
 __inline int signbit(double num) { return _copysign(1.0, num) < 0; }
 #else
-#include <math.h>
+#include <cmath>
 #endif
 
 #endif
