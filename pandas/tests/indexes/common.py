051ee37f9cc9d0d51fb3e6ed474d1688107cdc0e
Brock Mendel <jbrockmendel@gmail.com>
2018-02-07 08:32:46 -0800

    dispatch categorical series op to Categorical

--- pandas/tests/indexes/common.py
+++ pandas/tests/indexes/common.py
@@ -788,8 +788,14 @@ class Base(object):
         series_d = Series(array_d)
         with tm.assert_raises_regex(ValueError, "Lengths must match"):
             index_a == series_b
-        tm.assert_numpy_array_equal(index_a == series_a, expected1)
-        tm.assert_numpy_array_equal(index_a == series_c, expected2)
+
+        if isinstance(index_a, CategoricalIndex):
+            # GH#???
+            tm.assert_series_equal(index_a == series_a, Series(expected1))
+            tm.assert_series_equal(index_a == series_c, Series(expected2))
+        else:
+            tm.assert_numpy_array_equal(index_a == series_a, expected1)
+            tm.assert_numpy_array_equal(index_a == series_c, expected2)
 
         # cases where length is 1 for one of them
         with tm.assert_raises_regex(ValueError, "Lengths must match"):
