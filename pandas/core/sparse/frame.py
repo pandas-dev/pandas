2e45a27d7142cf52ce57924926b957a0a68187c3
Jeff Reback <jeff@reback.net>
2018-02-07 08:06:46 -0500

    API/BUG: .apply will correctly infer output shape when axis=1 (#18577)
    closes #16353
    closes #17348
    closes #17437
    closes #18573
    closes #17970
    closes #17892
    closes #17602
    closes #18775
    closes #18901
    closes #18919

--- pandas/core/sparse/frame.py
+++ pandas/core/sparse/frame.py
@@ -829,7 +829,8 @@ class SparseDataFrame(DataFrame):
         return self._apply_columns(lambda x: x.notna())
     notnull = notna
 
-    def apply(self, func, axis=0, broadcast=False, reduce=False):
+    def apply(self, func, axis=0, broadcast=None, reduce=None,
+              result_type=None):
         """
         Analogous to DataFrame.apply, for SparseDataFrame
 
@@ -841,6 +842,35 @@ class SparseDataFrame(DataFrame):
         broadcast : bool, default False
             For aggregation functions, return object of same size with values
             propagated
+
+            .. deprecated:: 0.23.0
+               This argument will be removed in a future version, replaced
+               by result_type='broadcast'.
+
+        reduce : boolean or None, default None
+            Try to apply reduction procedures. If the DataFrame is empty,
+            apply will use reduce to determine whether the result should be a
+            Series or a DataFrame. If reduce is None (the default), apply's
+            return value will be guessed by calling func an empty Series (note:
+            while guessing, exceptions raised by func will be ignored). If
+            reduce is True a Series will always be returned, and if False a
+            DataFrame will always be returned.
+
+            .. deprecated:: 0.23.0
+               This argument will be removed in a future version, replaced
+               by result_type='reduce'.
+
+        result_type : {'expand', 'reduce', 'broadcast, None}
+            These only act when axis=1 {columns}
+            * 'expand' : list-like results will be turned into columns
+            * 'reduce' : return a Series if possible rather than expanding
+              list-like results. This is the opposite to 'expand'
+            * 'broadcast' : scalar results will be broadcast to all columns
+            * None : list-like results will be returned as a list
+              in a single column. However if the apply function
+              returns a Series these are expanded to columns.
+
+            .. versionadded:: 0.23.0
 
         Returns
         -------
@@ -865,12 +895,10 @@ class SparseDataFrame(DataFrame):
         op = frame_apply(self,
                          func=func,
                          axis=axis,
-                         reduce=reduce)
-
-        if broadcast:
-            return op.apply_broadcast()
-
-        return op.apply_standard()
+                         reduce=reduce,
+                         broadcast=broadcast,
+                         result_type=result_type)
+        return op.get_result()
 
     def applymap(self, func):
         """
