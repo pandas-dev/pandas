49cbe974e179b88d12106e10167881dbae710ef6
Brock Mendel <jbrockmendel@gmail.com>
2018-02-08 20:25:53 -0800

    whitespace fixup

--- pandas/tests/tslibs/test_tslib.py
+++ pandas/tests/tslibs/test_tslib.py
@@ -10,7 +10,7 @@ from pandas import Timestamp
 class TestArrayToDatetime(object):
     def test_coerce_out_of_bounds_utc(self):
         ts = pd.Timestamp('1900-01-01', tz='US/Pacific')
-        dt = ts.to_pydatetime() - timedelta(days=365*300)  # ~1600AD
+        dt = ts.to_pydatetime() - timedelta(days=365 * 300)  # ~1600AD
         arr = np.array([dt])
         result = array_to_datetime(arr, utc=True, errors='coerce')
         assert (result == iNaT).all()
