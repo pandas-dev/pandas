68a11b3cbfdaff9837675b1509a8e49f0eda0a1e
Brock Mendel <jbrockmendel@gmail.com>
2018-02-08 22:10:36 -0800

    fixup namerror

--- pandas/tests/tslibs/test_tslib.py
+++ pandas/tests/tslibs/test_tslib.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from datetime import datetime, timedelta
+from datetime import timedelta
 
 import numpy as np
 
@@ -9,7 +9,7 @@ from pandas import Timestamp
 
 class TestArrayToDatetime(object):
     def test_coerce_out_of_bounds_utc(self):
-        ts = pd.Timestamp('1900-01-01', tz='US/Pacific')
+        ts = Timestamp('1900-01-01', tz='US/Pacific')
         dt = ts.to_pydatetime() - timedelta(days=365 * 300)  # ~1600AD
         arr = np.array([dt])
         result = array_to_datetime(arr, utc=True, errors='coerce')
