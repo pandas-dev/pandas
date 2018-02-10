007340de7cd013f1eaa89e8cb87241b71f0f4272
jschendel <jschendel@users.noreply.github.com>
2018-02-09 18:12:08 -0700

    expand whatsnew and comments

--- pandas/core/dtypes/dtypes.py
+++ pandas/core/dtypes/dtypes.py
@@ -206,6 +206,17 @@ class CategoricalDtype(PandasExtensionDtype):
         return int(self._hash_categories(self.categories, self.ordered))
 
     def __eq__(self, other):
+        """
+        Rules for CDT equality:
+        1) Any CDT is equal to the string 'category'
+        2) Any CDT is equal to a CDT with categories=None regardless of ordered
+        3) A CDT with ordered=True is only equal to another CDT with
+           ordered=True and identical categories in the same order
+        4) A CDT with ordered={False, None} is only equal to another CDT with
+           ordered={False, None} and identical categories, but same order is
+           not required. There is no distinction between False/None.
+        5) Any other comparison returns False
+        """
         if isinstance(other, compat.string_types):
             return other == self.name
 
@@ -219,11 +230,15 @@ class CategoricalDtype(PandasExtensionDtype):
             # CDT(., .) = CDT(None, True).
             return True
         elif self.ordered or other.ordered:
-            # at least one ordered
+            # At least one has ordered=True; equal if both have ordered=True
+            # and the same values for categories in the same order.
             return ((self.ordered == other.ordered) and
                     self.categories.equals(other.categories))
         else:
-            # both unordered; this could probably be optimized / cached
+            # Neither has ordered=True; equal if both have the same categories,
+            # but same order is not necessary.  There is no distinction between
+            # ordered=False and ordered=None: CDT(., False) and CDT(., None)
+            # will be equal if they have the same categories.
             return hash(self) == hash(other)
 
     def __repr__(self):
