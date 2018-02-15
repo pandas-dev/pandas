c280eaa7fcbfb806d7651370c0e985282fadd4d6
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-02-15 08:36:49 -0600

    Parametrized

--- pandas/tests/categorical/test_missing.py
+++ pandas/tests/categorical/test_missing.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+import pytest
 
 import pandas.util.testing as tm
 from pandas import (Categorical, Index, isna)
@@ -54,22 +55,17 @@ class TestCategoricalMissing(object):
         exp = Categorical([1, np.nan, 3], categories=[1, 2, 3])
         tm.assert_categorical_equal(cat, exp)
 
-    def test_fillna_raises(self):
+    @pytest.mark.parametrize('fillna_kwargs, msg', [
+        (dict(value=1, method='ffill'),
+         "Cannot specify both 'value' and 'method'."),
+        (dict(),
+         "Must specify a fill 'value' or 'method'."),
+        (dict(method='bad'),
+         "Invalid fill method. Expecting .* bad"),
+    ])
+    def test_fillna_raises(self, fillna_kwargs, msg):
         # https://github.com/pandas-dev/pandas/issues/19682
-
         cat = Categorical([1, 2, 3])
 
-        xpr = "Cannot specify both 'value' and 'method'."
-
-        with tm.assert_raises_regex(ValueError, xpr):
-            cat.fillna(value=1, method='ffill')
-
-        xpr = "Must specify a fill 'value' or 'method'."
-
-        with tm.assert_raises_regex(ValueError, xpr):
-            cat.fillna()
-
-        xpr = "Invalid fill method. Expecting .* bad"
-
-        with tm.assert_raises_regex(ValueError, xpr):
-            cat.fillna(method='bad')
+        with tm.assert_raises_regex(ValueError, msg):
+            cat.fillna(**fillna_kwargs)
