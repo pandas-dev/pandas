0a6be971755dd529293d0a717c087d1ec354a9a4
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-02-18 18:42:09 +0100

    install from conda-forge instead

--- ci/requirements-3.6_DOC.sh
+++ ci/requirements-3.6_DOC.sh
@@ -6,6 +6,6 @@ echo "[install DOC_BUILD deps]"
 
 pip install pandas-gbq
 
-conda install -n pandas -c conda-forge feather-format pyarrow nbsphinx pandoc fastparquet
+conda install -n pandas -c conda-forge sphinx feather-format pyarrow nbsphinx pandoc fastparquet
 
 conda install -n pandas -c r r rpy2 --yes
