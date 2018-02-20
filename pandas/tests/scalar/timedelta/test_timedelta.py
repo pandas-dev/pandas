16f7bd2dd801ff9261030bbf850c17b882003be6
Mike Kutzma <michaelkutzma@gmail.com>
2018-02-19 22:47:33 -0500

    BUG: GH19458 fixes precision issue in TimeDelta.total_seconds()\n\n

--- pandas/tests/scalar/timedelta/test_timedelta.py
+++ pandas/tests/scalar/timedelta/test_timedelta.py
@@ -706,6 +706,15 @@ class TestTimedeltas(object):
         with pytest.raises(OverflowError):
             Timedelta(max_td.value + 1, 'ns')
 
+    def test_total_seconds_precision(self):
+        # GH 19458
+        assert Timedelta('30S').total_seconds() == 30.0
+        assert Timedelta('0').total_seconds() == 0.0
+        assert Timedelta('-2S').total_seconds() == -2.0
+        assert Timedelta('5.324S').total_seconds() == 5.324
+        assert (Timedelta('30S').total_seconds() - 30.0)<1e-20
+        assert (30.0 - Timedelta('30S').total_seconds())<1e-20
+
     def test_timedelta_arithmetic(self):
         data = pd.Series(['nat', '32 days'], dtype='timedelta64[ns]')
         deltas = [timedelta(days=1), Timedelta(1, unit='D')]
