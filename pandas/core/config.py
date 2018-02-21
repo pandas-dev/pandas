8d4f6d642acdcb7227d1e157e0f157bdbd5a841f
Jay Foad <jay.foad@gmail.com>
2018-02-21 14:52:52 +0000

    Use try/except instead.

--- pandas/core/config.py
+++ pandas/core/config.py
@@ -196,10 +196,10 @@ class DictWrapper(object):
         if prefix:
             prefix += "."
         prefix += key
-        d = object.__getattribute__(self, "d")
-        if key not in d:
+        try:
+            v = object.__getattribute__(self, "d")[key]
+        except KeyError:
             raise OptionError("No such option")
-        v = d[key]
         if isinstance(v, dict):
             return DictWrapper(v, prefix)
         else:
