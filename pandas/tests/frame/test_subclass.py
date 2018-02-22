06e11d21bd3c8df865357d51e8c15e1189ac5564
Jaume Bonet <jaume.bonet@gmail.com>
2018-02-22 16:30:36 +0100

    test fix

--- pandas/tests/frame/test_subclass.py
+++ pandas/tests/frame/test_subclass.py
@@ -546,7 +546,7 @@ class TestDataFrameSubclassing(TestData):
             [1, 2, 3],
             [1, 2, 3]])
 
-        expected3 = DesignSeries([[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]])
+        expected3 = tm.SubclassedSeries([[1, 2, 3], [1, 2, 3], [1, 2, 3], [1, 2, 3]])
 
         df.apply(lambda x: check_row_subclass(x))
         df.apply(lambda x: check_row_subclass(x), axis=1)
