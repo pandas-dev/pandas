b87cef1930040d487a8b785c071b09dfe6e130b0
Clemens Brunner <clemens.brunner@gmail.com>
2018-02-22 09:28:48 +0100

    Address comments

--- pandas/tests/io/formats/test_format.py
+++ pandas/tests/io/formats/test_format.py
@@ -1011,25 +1011,25 @@ class TestDataFrameFormatting(object):
         reset_option('display.expand_frame_repr')
 
     def test_wide_repr_multiindex(self):
-        with option_context('mode.sim_interactive', True):
+        with option_context('mode.sim_interactive', True,
+                            'display.max_columns', 20):
             midx = MultiIndex.from_arrays(tm.rands_array(5, size=(2, 10)))
-            with option_context('display.max_columns', 20):
-                max_cols = get_option('display.max_columns')
-                df = DataFrame(tm.rands_array(25, size=(10, max_cols - 1)),
-                               index=midx)
-                df.index.names = ['Level 0', 'Level 1']
-                set_option('display.expand_frame_repr', False)
-                rep_str = repr(df)
-                set_option('display.expand_frame_repr', True)
-                wide_repr = repr(df)
-                assert rep_str != wide_repr
+            max_cols = get_option('display.max_columns')
+            df = DataFrame(tm.rands_array(25, size=(10, max_cols - 1)),
+                            index=midx)
+            df.index.names = ['Level 0', 'Level 1']
+            set_option('display.expand_frame_repr', False)
+            rep_str = repr(df)
+            set_option('display.expand_frame_repr', True)
+            wide_repr = repr(df)
+            assert rep_str != wide_repr
 
-                with option_context('display.width', 150):
-                    wider_repr = repr(df)
-                    assert len(wider_repr) < len(wide_repr)
+            with option_context('display.width', 150):
+                wider_repr = repr(df)
+                assert len(wider_repr) < len(wide_repr)
 
-                for line in wide_repr.splitlines()[1::13]:
-                    assert 'Level 0 Level 1' in line
+            for line in wide_repr.splitlines()[1::13]:
+                assert 'Level 0 Level 1' in line
 
         reset_option('display.expand_frame_repr')
 
