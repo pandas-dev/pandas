e6237e59221c6a8b547146df32893b27e36e83dd
Will Ayd <william.ayd@icloud.com>
2018-02-23 16:07:42 -0800

    Added test cases for pct_change op

--- pandas/tests/generic/test_generic.py
+++ pandas/tests/generic/test_generic.py
@@ -592,6 +592,32 @@ class Generic(object):
                 assert obj_copy is not obj
                 self._compare(obj_copy, obj)
 
+    @pytest.mark.parametrize("periods,fill_method,limit,exp", [
+        (1, "ffill", None, [np.nan, np.nan, np.nan, 1, 1, 1.5, 0, 0]),
+        (1, "ffill", 1, [np.nan, np.nan, np.nan, 1, 1, 1.5, 0, np.nan]),
+        (1, "bfill", None, [np.nan, 0, 0, 1, 1, 1.5, np.nan, np.nan]),
+        (1, "bfill", 1, [np.nan, np.nan, 0, 1, 1, 1.5, np.nan, np.nan]),
+        (-1, "ffill", None, [np.nan, np.nan, -.5, -.5, -.6, 0, 0, np.nan]),
+        (-1, "ffill", 1, [np.nan, np.nan, -.5, -.5, -.6, 0, np.nan, np.nan]),
+        (-1, "bfill", None, [0, 0, -.5, -.5, -.6, np.nan, np.nan, np.nan]),
+        (-1, "bfill", 1, [np.nan, 0, -.5, -.5, -.6, np.nan, np.nan, np.nan])
+    ])
+    def test_pct_change(self, periods, fill_method, limit, exp):
+        obj = self._construct(0)
+        if type(obj) is Panel:
+            pytest.skip("Not testing deprecated Panel")
+        is_frame = type(obj) is DataFrame
+        ser = pd.Series([np.nan, np.nan, 1, 2, 4, 10, np.nan, np.nan])
+        obj = pd.concat((obj, ser))
+
+        func = getattr(obj, 'pct_change')
+        res = func(periods=periods, fill_method=fill_method, limit=limit)
+
+        # Convert output into Series, regardless of input type
+        if is_frame:
+            res = res[0]
+            res.name = None
+        tm.assert_series_equal(res, pd.Series(exp))
 
 class TestNDFrame(object):
     # tests that don't fit elsewhere
