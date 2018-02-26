123763756c16857f11030d3cadb815b59a27a341
Will Ayd <william.ayd@icloud.com>
2018-02-25 18:50:41 -0800

    Moved group_any from template to .pyx

--- pandas/_libs/groupby.pyx
+++ pandas/_libs/groupby.pyx
@@ -312,5 +312,50 @@ def group_fillna_indexer(ndarray[int64_t] out, ndarray[int64_t] labels,
                 filled_vals = 0
 
 
+@cython.boundscheck(False)
+@cython.wraparound(False)
+def group_any(ndarray[int64_t] out,
+              ndarray values,
+              ndarray[int64_t] labels,
+              bint skipna):
+    """Aggregated boolean values to show if any group element is truthful
+
+    Parameters
+    ----------
+    out : array of int64_t values which this method will write its results to
+    values : array of values to be truth-tested
+    labels : array containing unique label for each group, with its ordering
+        matching up to the corresponding record in `values`
+    skipna : boolean
+        Flag to ignore nan values during truth testing
+
+    Notes
+    -----
+    This method modifies the `out` parameter rather than returning an object.
+    The returned values will either be 0 or 1 (False or True, respectively).
+    """
+    cdef:
+        Py_ssize_t i, N=len(labels)
+        int64_t lab
+        ndarray[int64_t] bool_mask
+        ndarray[uint8_t] isna_mask
+
+    if values.dtype == 'object':
+        bool_mask = np.array([bool(x) for x in values]).astype(np.int64)
+        isna_mask = missing.isnaobj(values).astype(np.uint8)
+    else:
+        bool_mask = values.astype(np.bool).astype(np.int64)
+        isna_mask = np.isnan(values).astype(np.uint8)
+
+    with nogil:
+        for i in range(N):
+            lab = labels[i]
+            if lab < 0:
+                continue
+
+            if bool_mask[i] and not (skipna and isna_mask[i]):
+                out[lab] = 1
+
+
 # generated from template
 include "groupby_helper.pxi"
