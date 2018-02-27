79da90e8696ddc5921ba3b7e8b42eb369eca11c6
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-02-27 06:08:29 -0600

    Docstring

--- pandas/core/internals.py
+++ pandas/core/internals.py
@@ -816,11 +816,24 @@ class Block(PandasObject):
         return self if kwargs['inplace'] else self.copy()
 
     def setitem(self, indexer, value, mgr=None):
-        """ set the value inplace; return a new block (of a possibly different
-        dtype)
+        """Set the value inplace, returning a a maybe different typed block.
 
-        indexer is a direct slice/positional indexer; value must be a
-        compatible shape
+        Parameters
+        ----------
+        indexer : tuple, list-like, array-like, slice
+            The subset of self.values to set
+        value : object
+            The value being set
+        mgr : BlockPlacement
+
+        Returns
+        -------
+        Block
+
+        Notes
+        -----
+        'indexer' is a direct slice/positional indexer. 'value' must
+        be a compatible shape.
         """
         # coerce None values, if appropriate
         if value is None:
@@ -1885,6 +1898,28 @@ class ExtensionBlock(NonConsolidatableMixIn, Block):
         return False
 
     def setitem(self, indexer, value, mgr=None):
+        """Set the value inplace, returning a same-typed block.
+
+        This differs from Block.setitem by not allowing setitem to change
+        the dtype of the Block.
+
+        Parameters
+        ----------
+        indexer : tuple, list-like, array-like, slice
+            The subset of self.values to set
+        value : object
+            The value being set
+        mgr : BlockPlacement
+
+        Returns
+        -------
+        Block
+
+        Notes
+        -----
+        'indexer' is a direct slice/positional indexer. 'value' must
+        be a compatible shape.
+        """
         if isinstance(indexer, tuple):
             # we are always 1-D
             indexer = indexer[0]
