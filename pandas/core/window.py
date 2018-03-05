38e57875b1c1d40864b5559804aadba2594ceb41
Will Ayd <william.ayd@icloud.com>
2018-03-05 12:22:49 -0800

    Fixed docstr indentation

--- pandas/core/window.py
+++ pandas/core/window.py
@@ -901,39 +901,39 @@ class _Rolling_and_Expanding(_Rolling):
 
     _shared_docs['kurt'] = dedent("""Calculate unbiased %(name)s kurtosis.
 
-This function uses Fisher's definition of kurtosis (kurtosis of normal
-== 0.0) without bias.
+    This function uses Fisher's definition of kurtosis (kurtosis of normal
+    == 0.0) without bias.
 
-Returns
--------
-same type as input
+    Returns
+    -------
+    same type as input
 
-See Also
---------
-scipy.stats.kurtosis
-pandas.DataFrame.kurtosis
-pandas.Series.kurtosis
+    See Also
+    --------
+    scipy.stats.kurtosis
+    pandas.DataFrame.kurtosis
+    pandas.Series.kurtosis
 
-Notes
------
-A minimum of 4 periods is required for the rolling calculation
+    Notes
+    -----
+    A minimum of 4 periods is required for the rolling calculation
 
-Examples
---------
->>> arr = [1, 2, 3, 4, 5]
->>> import scipy.stats
->>> scipy.stats.kurtosis(arr, bias=False)
--1.200000000000000
+    Examples
+    --------
+    >>> arr = [1, 2, 3, 4, 5]
+    >>> import scipy.stats
+    >>> scipy.stats.kurtosis(arr, bias=False)
+    -1.200000000000000
 
->>> df = pd.DataFrame(arr)
->>> df.rolling(5).kurt()
-    0
-0  NaN
-1  NaN
-2  NaN
-3  NaN
-4 -1.2
-""")
+    >>> df = pd.DataFrame(arr)
+    >>> df.rolling(5).kurt()
+        0
+    0  NaN
+    1  NaN
+    2  NaN
+    3  NaN
+    4 -1.2
+    """)
 
     def kurt(self):
         return self._apply('roll_kurt', 'kurt',
