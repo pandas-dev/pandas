2fb401166c9cb41b8d2f6b03294c8274ce0a5ba1
Will Ayd <william.ayd@icloud.com>
2018-03-08 13:13:53 -0800

    Added See Also descriptions

--- pandas/core/window.py
+++ pandas/core/window.py
@@ -916,12 +916,12 @@ class _Rolling_and_Expanding(_Rolling):
 
     See Also
     --------
-    pandas.Series.%(name)s
-    pandas.DataFrame.%(name)s
-    pandas.Series.kurtosis
-    pandas.DataFrame.kurtosis
-    scipy.stats.skew
-    scipy.stats.kurtosis
+    pandas.Series.%(name)s : Calling object with Series data
+    pandas.DataFrame.%(name)s : Calling object with DataFrames
+    pandas.Series.kurtosis : Similar method for Series
+    pandas.DataFrame.kurtosis : Similar method for DataFrame
+    scipy.stats.skew : Third moment of a probability density
+    scipy.stats.kurtosis : Reference SciPy method
 
     Notes
     -----
