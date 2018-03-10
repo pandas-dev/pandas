608cd1e4380c0a7f1633106caae5036f9223d94b
Asier Zugarramurdi <axi.zugarramurdi@gmail.com>
2018-03-10 14:21:18 +0100

    Remove trailing space from docstring in DataFrame.applymap

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -5007,7 +5007,7 @@ class DataFrame(NDFrame):
         """
         Apply a function to a Dataframe elementwise.
 
-        This method applies a function to a DataFrame that is intended to 
+        This method applies a function to a DataFrame that is intended to
         operate elementwise, i.e. like doing map(func, series) for each series
         in the DataFrame.
 
