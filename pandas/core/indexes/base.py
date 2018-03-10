b4cd28bead822885b327a5ffa9fb0dd9ac18aba3
stijnvanhoey <stijnvanhoey@gmail.com>
2018-03-10 13:18:51 +0100

    Fix docstring guide errors

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -4023,7 +4023,7 @@ class Index(IndexOpsMixin, PandasObject):
 
     def duplicated(self, keep='first'):
         """
-        Indicate duplicate index values 
+        Indicate duplicate index values.
 
         Duplicated values are indicated as ``True`` values in the resulting
         array. Either all duplicates, all except the first or all except the 
@@ -4045,23 +4045,23 @@ class Index(IndexOpsMixin, PandasObject):
 
         >>> idx = pd.Index(['lama', 'cow', 'lama', 'beetle', 'lama', 'hippo'])
         >>> idx.duplicated()
-        array([False, False,  True, False,  True, False])
+        array([False, False,  True, False,  True, False], dtype=bool)
 
         which is equivalent to 
 
         >>> idx.duplicated(keep='first')
-        array([False, False,  True, False,  True, False])
+        array([False, False,  True, False,  True, False], dtype=bool)
 
         By using 'last', the last occurrence of each set of duplicated values is 
         set on False and all others on True:
 
         >>> idx.duplicated(keep='last')
-        array([ True, False,  True, False, False, False])
+        array([ True, False,  True, False, False, False], dtype=bool)
 
         By setting keep on ``False``, all duplicates are True:
         
         >>> idx.duplicated(keep=False)
-        array([ True, False,  True, False,  True, False])
+        array([ True, False,  True, False,  True, False], dtype=bool)
         
         Returns
         -------
