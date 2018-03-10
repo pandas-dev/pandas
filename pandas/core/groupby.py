37e1cbb9818a91eac90341b914dedbd7e4b4e9f4
William Ayd <william.ayd@icloud.com>
2018-03-09 18:06:43 -0800

    Cythonized GroupBy pct_change (#19919)

--- pandas/core/groupby.py
+++ pandas/core/groupby.py
@@ -2046,6 +2046,23 @@ class GroupBy(_GroupBy):
 
     @Substitution(name='groupby')
     @Appender(_doc_template)
+    def pct_change(self, periods=1, fill_method='pad', limit=None, freq=None,
+                   axis=0):
+        """Calcuate pct_change of each value to previous entry in group"""
+        if freq is not None or axis != 0:
+            return self.apply(lambda x: x.pct_change(periods=periods,
+                                                     fill_method=fill_method,
+                                                     limit=limit, freq=freq,
+                                                     axis=axis))
+
+        filled = getattr(self, fill_method)(limit=limit).drop(
+            self.grouper.names, axis=1)
+        shifted = filled.shift(periods=periods, freq=freq)
+
+        return (filled / shifted) - 1
+
+    @Substitution(name='groupby')
+    @Appender(_doc_template)
     def head(self, n=5):
         """
         Returns first n rows of each group.
@@ -3883,6 +3900,13 @@ class SeriesGroupBy(GroupBy):
     def _apply_to_column_groupbys(self, func):
         """ return a pass thru """
         return func(self)
+
+    def pct_change(self, periods=1, fill_method='pad', limit=None, freq=None):
+        """Calculate percent change of each value to previous entry in group"""
+        filled = getattr(self, fill_method)(limit=limit)
+        shifted = filled.shift(periods=periods, freq=freq)
+
+        return (filled / shifted) - 1
 
 
 class NDFrameGroupBy(GroupBy):
