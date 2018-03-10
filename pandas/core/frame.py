8d76f60adc4af740eb6fc44c59753ab84ca93f5a
Jonathan Enders <jonathan.enders@gmx.de>
2018-03-10 16:52:02 -0500

    review corrections

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -5595,14 +5595,14 @@ class DataFrame(NDFrame):
         Count non-NA cells for each column or row.
 
         Return Series with number of non-NA observations over requested
-        axis. Works with non-floating point data as well (detects `NaN` and
-        `None`)
+        axis. Works with non-floating point data as well (detects `None`,
+        `NaN` and `NaT`)
 
         Parameters
         ----------
         axis : {0 or 'index', 1 or 'columns'}, default 0
-            If equal 0 or 'index' counts are generated for each column.
-            If equal 1 or 'columns' counts are generated for each row.
+            If 0 or 'index' counts are generated for each column.
+            If 1 or 'columns' counts are generated for each row.
         level : int or str, optional
             If the axis is a `MultiIndex` (hierarchical), count along a
             particular level, collapsing into a `DataFrame`.
@@ -5626,24 +5626,34 @@ class DataFrame(NDFrame):
 
         Examples
         --------
-        >>> df=pd.DataFrame({ "Person":["John","Myla",None],
-        ...                   "Age":[24.,np.nan,21.],
-        ...                   "Single":[False,True,True]     })
+        >>> df = pd.DataFrame({"Person":
+        ...                    ["John", "Myla", None, "John", "Myla"],
+        ...                    "Age": [24., np.nan, 21., 33, 26],
+        ...                    "Single": [False, True, True, True, False]})
         >>> df
            Person   Age  Single
         0    John  24.0   False
         1    Myla   NaN    True
         2    None  21.0    True
+        3    John  33.0    True
+        4    Myla  26.0   False
         >>> df.count()
-        Person    2
-        Age       2
-        Single    3
+        Person    4
+        Age       4
+        Single    5
         dtype: int64
         >>> df.count(axis=1)
         0    3
         1    2
         2    2
+        3    3
+        4    3
         dtype: int64
+        >>> df.set_index(["Person", "Single"]).count(level="Person")
+                Age
+        Person
+        John      2
+        Myla      1
         """
         axis = self._get_axis_number(axis)
         if level is not None:
