3b5d1019796f834e2a2af1b272e880b66c13be70
Jenna Vergeynst <jenna.vergeynst@ugent.be>
2018-03-10 13:33:54 +0100

    DOC: seperate drop doc for series and dataframes

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -3035,6 +3035,128 @@ class DataFrame(NDFrame):
                                         method=method, level=level, copy=copy,
                                         limit=limit, fill_value=fill_value)
 
+    def drop(self, labels=None, axis=0, index=None, columns=None,
+                     level=None, inplace=False, errors='raise'):
+        """
+        Drop rows or columns.
+
+        Remove rows or columns by specifying label names and corresponding axis,
+        or by specifying directly index or column names. When using a
+        multi-index, labels on different levels can be removed by specifying
+        the level name or int.
+
+        Parameters
+        ----------
+        labels : single label or list-like
+            Index or column labels to drop.
+        axis : int or axis name, default 0
+            Whether to drop labels from the index (0 / 'index') or
+            columns (1 / 'columns').
+        index : None
+            Redundant for application on Series, but index can be used instead
+            of labels.
+        columns : None
+            Redundant for application on Series, but index can be used instead
+            of labels.
+
+            .. versionadded:: 0.21.0
+        level : int or level name, optional
+            For MultiIndex.
+        inplace : bool, default False
+            If True, do operation inplace and return None.
+        errors : {'ignore', 'raise'}, default 'raise'
+            If 'ignore', suppress error and existing labels are dropped.
+
+        Returns
+        -------
+        dropped : type of caller
+
+        See Also
+        --------
+        DataFrame.dropna : Return DataFrame with labels on given axis omitted
+            where (all or any) data are missing
+        DataFrame.drop_duplicates : Return DataFrame with duplicate rows removed,
+            optionally only considering certain columns
+
+        Raises
+        ------
+        KeyError
+            If none of the labels are found in the selected axis
+
+        Examples
+        --------
+        >>> df = pd.DataFrame(np.arange(12).reshape(3,4),
+        ...                   columns=['A', 'B', 'C', 'D'])
+        >>> df
+           A  B   C   D
+        0  0  1   2   3
+        1  4  5   6   7
+        2  8  9  10  11
+
+        Drop columns
+
+        >>> df.drop(['B', 'C'], axis=1)
+           A   D
+        0  0   3
+        1  4   7
+        2  8  11
+
+        >>> df.drop(columns=['B', 'C'])
+           A   D
+        0  0   3
+        1  4   7
+        2  8  11
+
+        Drop a row by index
+
+        >>> df.drop([0, 1])
+           A  B   C   D
+        2  8  9  10  11
+
+        Drop columns and/or rows of MultiIndex
+
+        >>> midx = pd.MultiIndex(levels=[['lama','cow','falcon'],
+        ...                              ['speed','weight','length']],
+        ...                      labels=[[0, 0, 0, 1, 1, 1, 2, 2, 2],
+        ...                              [0, 1, 2, 0, 1, 2, 0, 1, 2]])
+        >>> df = pd.DataFrame(index=midx, columns=['big','small'],
+        ...                   data=[[45,30],[200,100],[1.5,1],[30,20],[250,150],
+        ...                         [1.5,0.8],[320,250],[1,0.8],[0.3,0.2]])
+        >>> df
+        		        big 	small
+        lama 	speed 	45.0 	30.0
+                weight 	200.0 	100.0
+                length 	1.5 	1.0
+        cow 	speed 	30.0 	20.0
+                weight 	250.0 	150.0
+                length 	1.5 	0.8
+        falcon 	speed 	320.0 	250.0
+                weight 	1.0 	0.8
+                length 	0.3 	0.2
+
+        >>> df.drop(index='cow',columns='small')
+                        big
+        lama 	speed 	45.0
+                weight 	200.0
+                length 	1.5
+        falcon 	speed 	320.0
+                weight 	1.0
+                length 	0.3
+
+        >>> df.drop(index='length', level=1)
+        		        big 	small
+        lama 	speed 	45.0 	30.0
+                weight 	200.0 	100.0
+        cow 	speed 	30.0 	20.0
+                weight 	250.0 	150.0
+        falcon 	speed 	320.0 	250.0
+                weight 	1.0 	0.8
+        """
+        return super(DataFrame,
+                     self).drop(labels=labels, axis=axis, index=index,
+                                columns=columns, level=level, inplace=inplace,
+                                errors=errors)
+
     @rewrite_axis_style_signature('mapper', [('copy', True),
                                              ('inplace', False),
                                              ('level', None)])
--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -2797,75 +2797,10 @@ class NDFrame(PandasObject, SelectionMixin):
 
         return self.reindex(**d)
 
+
     def drop(self, labels=None, axis=0, index=None, columns=None, level=None,
              inplace=False, errors='raise'):
-        """
-        Return new object with labels in requested axis removed.
-
-        Parameters
-        ----------
-        labels : single label or list-like
-            Index or column labels to drop.
-        axis : int or axis name
-            Whether to drop labels from the index (0 / 'index') or
-            columns (1 / 'columns').
-        index, columns : single label or list-like
-            Alternative to specifying `axis` (``labels, axis=1`` is
-            equivalent to ``columns=labels``).
-
-            .. versionadded:: 0.21.0
-        level : int or level name, default None
-            For MultiIndex
-        inplace : bool, default False
-            If True, do operation inplace and return None.
-        errors : {'ignore', 'raise'}, default 'raise'
-            If 'ignore', suppress error and existing labels are dropped.
-
-        Returns
-        -------
-        dropped : type of caller
-
-        Raises
-        ------
-        KeyError
-            If none of the labels are found in the selected axis
-
-        Examples
-        --------
-        >>> df = pd.DataFrame(np.arange(12).reshape(3,4),
-                              columns=['A', 'B', 'C', 'D'])
-        >>> df
-           A  B   C   D
-        0  0  1   2   3
-        1  4  5   6   7
-        2  8  9  10  11
-
-        Drop columns
-
-        >>> df.drop(['B', 'C'], axis=1)
-           A   D
-        0  0   3
-        1  4   7
-        2  8  11
-
-        >>> df.drop(columns=['B', 'C'])
-           A   D
-        0  0   3
-        1  4   7
-        2  8  11
-
-        Drop a row by index
-
-        >>> df.drop([0, 1])
-           A  B   C   D
-        2  8  9  10  11
-
-        Notes
-        -----
-        Specifying both `labels` and `index` or `columns` will raise a
-        ValueError.
-
-        """
+             
         inplace = validate_bool_kwarg(inplace, 'inplace')
 
         if labels is not None:
