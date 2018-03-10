232520fc6625005ec6cc0d86e7d979ee1adbdb94
Janelle <jzoutkamp@live.com>
2018-03-10 15:23:56 +0100

    DOC: update documentation for pandas.Series.dt.is_month_last

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -1756,7 +1756,32 @@ class DatetimeIndex(DatelikeOps, TimelikeOps, DatetimeIndexOpsMixin,
     is_month_end = _field_accessor(
         'is_month_end',
         'is_month_end',
-        "Logical indicating if last day of month (defined by frequency)")
+        """
+        Return a boolean indicating if the date is the last day of the month.
+
+        Returns
+        -------
+        is_month_end : Series of boolean
+
+        See Also
+        --------
+        is_month_start : Returns a boolean indicating if the date is the first day of the month.
+
+        Examples
+        --------
+        >>> import pandas as pd
+        >>> dates = pd.Series(pd.date_range("2018-02-27", periods = 3))
+        >>> dates
+        0   2018-02-27
+        1   2018-02-28
+        2   2018-03-01
+        dtype: datetime64[ns]
+        >>> dates.dt.is_month_end
+        0    False
+        1    True
+        2    False
+        dtype: bool
+        """)
     is_quarter_start = _field_accessor(
         'is_quarter_start',
         'is_quarter_start',
