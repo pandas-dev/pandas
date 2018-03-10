4dfaa4057210dd3d13d584571ea8410ef5de7e0e
Vimal Romeo <masterromaeo@gmail.com>
2018-03-10 12:09:38 +0100

    DOC: Improved parameter description of sortlevel

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -1853,12 +1853,14 @@ class MultiIndex(Index):
         Parameters
         ----------
         level : list-like, int or str, default 0
-            If a string is given, must be a name of the level
+            If a string is given, must be a name of the level.
             If list-like must be names or ints of levels.
         ascending : boolean, default True
-            False to sort in descending order
-            Can also be a list to specify a directed ordering
-        sort_remaining : sort by the remaining levels after level.
+            False to sort in descending order.
+            Can also be a list to specify a directed ordering.
+        sort_remaining : boolean, default True
+            False to sort only the requested level.
+            True to sort also all the levels after the requested one.
 
         Returns
         -------
