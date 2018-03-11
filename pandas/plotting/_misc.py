bb99426d6986b4b02f43f4260fafe12315dd2aee
Antonio Molina <amolina@ialab.es>
2018-03-11 17:50:41 +0100

    Changed kwds for **kwds

--- pandas/plotting/_misc.py
+++ pandas/plotting/_misc.py
@@ -344,7 +344,7 @@ def bootstrap_plot(series, fig=None, size=50, samples=500, **kwds):
         greater or equal than the length of the `series`.
     samples : int, default 500
         Number of times the bootstrap procedure is performed.
-    kwds : keywords
+    **kwds : keywords
         Options to pass to matplotlib plotting method.
 
     Returns
