fc388ad6ae6c5d2720423f63fd22243c50b5602b
Jeff Reback <jeff@reback.net>
2018-03-11 18:19:06 -0400

    CI: update tests for dateutil >= 2.7.0
    closes #18332

--- ci/requirements-3.6_NUMPY_DEV.build.sh
+++ ci/requirements-3.6_NUMPY_DEV.build.sh
@@ -12,8 +12,7 @@ PRE_WHEELS="https://7933911d6844c6c53a7d-47bd50c35cd79bd838daf386af554a83.ssl.cf
 pip install --pre --upgrade --timeout=60 -f $PRE_WHEELS numpy scipy
 
 # install dateutil from master
-# pip install -U git+git://github.com/dateutil/dateutil.git
-pip install dateutil
+pip install -U git+git://github.com/dateutil/dateutil.git
 
 # cython via pip
 pip install cython
