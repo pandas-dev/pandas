51ad0cb6c3cfddc68719d07a07c3a2801492e35b
mdeboc <m.de-beaucorps@talentoday.com>
2018-03-12 13:22:44 +0100

    DOC: update the pandas.DataFrame.apply docstring

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -4823,9 +4823,9 @@ class DataFrame(NDFrame):
 
         Objects passed to the function are Series objects whose index is
         either the DataFrame's index (``axis=0``) or the DataFrame's columns
-        (``axis=1``). If ``result_type is None``, the final return type is the
-        return type of the applied function. Otherwise, it depends on the
-        `result_type` argument.
+        (``axis=1``). If ``result_type is None``, the final return type is
+        inferred from the return type of the applied function. Otherwise, it
+        depends on the `result_type` argument.
 
         Parameters
         ----------
