e14315d8cbff67f7a80e36d78878ce52aae38935
astrastefania <contact@stefaniadelprete.com>
2018-03-13 17:31:40 +0100

    DOC: Added DataFrame example

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -2963,21 +2963,23 @@ class NDFrame(PandasObject, SelectionMixin):
 
     def add_prefix(self, prefix):
         """
-        Concatenate prefix string with Series items names.
+        Prefix labels with string `prefix`.
+
+        For Series, the row labels are prefixed. For DataFrame, the column labels are prefixed.
 
         Parameters
         ----------
         prefix : str
-            The string to add before each item name.
+            The string to add before each label.
 
         Returns
         -------
-        Series
-            Original Series with updated item names.
+        Series or DataFrame
+            Original Series or DataFrame with updated labels.
 
         See Also
         --------
-        Series.add_suffix: Add a suffix string to Series items names.
+        Series.add_suffix: Suffix labels with string `suffix`.
 
         Examples
         --------
@@ -2994,6 +2996,20 @@ class NDFrame(PandasObject, SelectionMixin):
         item_2    3
         item_3    4
         dtype: int64
+
+        >>> df = pd.DataFrame({'A': [1, 2, 3, 4],  'B': [3, 4, 5, 6]})
+        >>> df
+           A  B
+        0  1  3
+        1  2  4
+        2  3  5
+        3  4  6
+        >>> df.add_suffix('_item')
+           A_item  B_item
+        0       1       3
+        1       2       4
+        2       3       5
+        3       4       6
         """
         new_data = self._data.add_prefix(prefix)
         return self._constructor(new_data).__finalize__(self)
