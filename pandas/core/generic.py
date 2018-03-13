744dc6162e8ab30a0a99b11df1874a1457fa9c51
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-03-13 14:49:10 +0100

    Update generic.py

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -5656,10 +5656,10 @@ class NDFrame(PandasObject, SelectionMixin):
         Detect existing (non-missing) values.
 
         Return a boolean same-sized object indicating if the values are not NA.
-        Non-missing values gets mapped to True. Characters such as empty
+        Non-missing values get mapped to True. Characters such as empty
         strings ``''`` or :attr:`numpy.inf` are not considered NA values
         (unless you set ``pandas.options.mode.use_inf_as_na = True``).
-        NA values, such as None or :attr:`numpy.NaN`, gets mapped to False
+        NA values, such as None or :attr:`numpy.NaN`, get mapped to False
         values.
 
         Returns
