721c955fe4062b231220fc7364d5c9d12dfdfd96
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-03-14 13:40:48 -0500

    Updated [ci skip]
    [ci skip]

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -1740,31 +1740,42 @@ class DatetimeIndex(DatelikeOps, TimelikeOps, DatetimeIndexOpsMixin,
         'is_quarter_end',
         'is_quarter_end',
         """
-        Return a boolean indicating whether the date is the last day of a
-        quarter.
+        Indicator for whether the date is the last day of a quarter.
 
         Returns
         -------
-        is_quarter_end : Series of boolean.
+        is_quarter_end : Series or DatetimeIndex
+            The same type as the original data with boolean values. Series will
+            have the same name and index. DatetimeIndex will have the same
+            name.
 
         See Also
         --------
         quarter : Return the quarter of the date.
-
-        is_quarter_start : Return a boolean indicating whether the date is the
-            first day of the quarter.
+        is_quarter_start : Similar method indicating the quarter start.
 
         Examples
         --------
+        This method is available on Series with datetime values under
+        the ``.dt`` accessor, and directly on DatetimeIndex.
+
         >>> df = pd.DataFrame({'dates': pd.date_range("2017-03-30",
-        ...                   periods=4)})
-        >>> df.assign(quarter = df.dates.dt.quarter,
-        ...          is_quarter_end = df.dates.dt.is_quarter_end)
+        ...                    periods=4)})
+        >>> df.assign(quarter=df.dates.dt.quarter,
+        ...           is_quarter_end=df.dates.dt.is_quarter_end)
                dates  quarter    is_quarter_end
         0 2017-03-30        1             False
         1 2017-03-31        1              True
         2 2017-04-01        2             False
         3 2017-04-02        2             False
+
+        >>> idx = pd.date_range('2017-03-30', periods=4)
+        >>> idx
+        DatetimeIndex(['2017-03-30', '2017-03-31', '2017-04-01', '2017-04-02'],
+                      dtype='datetime64[ns]', freq='D')
+
+        >>> idx.is_quarter_end
+        array([False,  True, False, False])
         """)
     is_year_start = _field_accessor(
         'is_year_start',
