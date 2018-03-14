93d2257837c893c9b21ea5e964f8b1b660da4049
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-03-14 13:28:42 -0500

    Fixups [ci skip]
    [ci skip]

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -211,19 +211,26 @@ def str_count(arr, pat, flags=0):
     pat : str
         Valid regular expression.
     flags : int, default 0, meaning no flags
-        Flags for re module, for a complete list
-        `look at this
+        Flags for the `re` module. For a complete list, `see here
         <https://docs.python.org/3/howto/regex.html#compilation-flags>`_.
+    **kwargs
+        For compatability with other string methods. Not used.
 
     Returns
     -------
-    counts : Series/Index of integer values
+    counts : Series or Index
+        Same type as the calling object containing the integer counts.
 
     Notes
     -----
-    Some characters need to be escaped when passing in pat.
-    eg. '$' has a special meaning in regex and must be escaped when finding
-    specifically this char.
+    Some characters need to be escaped when passing in `pat`.
+    eg. ``'$'`` has a special meaning in regex and must be escaped when
+    finding this literal character.
+
+    See Also
+    --------
+    re : Standard library module for regular expressions.
+    str.count : Standard library version, without regular expression support.
 
     Examples
     --------
@@ -238,6 +245,8 @@ def str_count(arr, pat, flags=0):
     6    1.0
     dtype: float64
 
+    Escape ``'$'`` to find the literal dollar sign.
+
     >>> s = pd.Series(['$', 'B', 'Aab$', '$$ca', 'C$B$', 'cat'])
     >>> s.str.count('\$')
     0    1
@@ -247,6 +256,11 @@ def str_count(arr, pat, flags=0):
     4    2
     5    0
     dtype: int64
+
+    This is also available on Index
+
+    >>> pd.Index(['A', 'A', 'Aaba', 'cat']).str.count('a')
+    Int64Index([0, 0, 2, 1], dtype='int64')
     """
     regex = re.compile(pat, flags=flags)
     f = lambda x: len(regex.findall(x))
