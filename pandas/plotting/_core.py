347a012ce51f9604dfb52f664592217b0260fd3b
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-03-14 13:39:10 +0100

    add back changes + changes for feedback

--- pandas/plotting/_core.py
+++ pandas/plotting/_core.py
@@ -3174,18 +3174,18 @@ class FramePlotMethods(BasePlotMethods):
     def hexbin(self, x, y, C=None, reduce_C_function=None, gridsize=None,
                **kwds):
         """
-        Generate an hexagonal binning plot.
+        Generate a hexagonal binning plot.
 
-        Generate an hexagonal binning plot of `x` versus `y`. If `C` is `None`
-        (the default), this is an histogram of the number of occurrences
+        Generate a hexagonal binning plot of `x` versus `y`. If `C` is `None`
+        (the default), this is a histogram of the number of occurrences
         of the observations at ``(x[i], y[i])``.
 
         If `C` is specified, specifies values at given coordinates
         ``(x[i], y[i])``. These values are accumulated for each hexagonal
         bin and then reduced according to `reduce_C_function`,
-        having as default the numpy's mean function (:meth:`numpy.mean`).
+        having as default the NumPy's mean function (:meth:`numpy.mean`).
         (If `C` is specified, it must also be a 1-D sequence
-        of the same length as `x` and `y`.)
+        of the same length as `x` and `y`, or a column label.)
 
         Parameters
         ----------
@@ -3211,22 +3211,24 @@ class FramePlotMethods(BasePlotMethods):
 
         Returns
         -------
-        axes : matplotlib.AxesSubplot
+        matplotlib.AxesSubplot
+            The matplotlib ``Axes`` on which the hexbin is plotted.
 
         See Also
         --------
+        DataFrame.plot : Make plots of a DataFrame.
         matplotlib.pyplot.hexbin : hexagonal binning plot using matplotlib,
             the matplotlib function that is used under the hood.
 
         Examples
         --------
-        The following examples are generated with random data.
+        The following examples are generated with random data from
+        a normal distribution.
 
         .. plot::
             :context: close-figs
 
             >>> n = 10000
-            >>> # Make a dataframe with normal distributed data
             >>> df = pd.DataFrame({'x': np.random.randn(n),
             ...                    'y': np.random.randn(n)})
             >>> ax = df.plot.hexbin(x='x', y='y', gridsize=20)
