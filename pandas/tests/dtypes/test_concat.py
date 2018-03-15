6795d435585737f2ec8b2269e38fc596cd38c479
jschendel <jschendel@users.noreply.github.com>
2018-03-15 04:26:20 -0600

    TST: Parametrize dtypes tests - test_common.py and test_concat.py (#20340)

--- pandas/tests/dtypes/test_concat.py
+++ pandas/tests/dtypes/test_concat.py
@@ -1,77 +1,53 @@
 # -*- coding: utf-8 -*-
 
-import pandas as pd
+import pytest
 import pandas.core.dtypes.concat as _concat
+from pandas import (
+    Index, DatetimeIndex, PeriodIndex, TimedeltaIndex, Series, Period)
 
 
-class TestConcatCompat(object):
+@pytest.mark.parametrize('to_concat, expected', [
+    # int/float/str
+    ([['a'], [1, 2]], ['i', 'object']),
+    ([[3, 4], [1, 2]], ['i']),
+    ([[3, 4], [1, 2.1]], ['i', 'f']),
 
-    def check_concat(self, to_concat, exp):
-        for klass in [pd.Index, pd.Series]:
-            to_concat_klass = [klass(c) for c in to_concat]
-            res = _concat.get_dtype_kinds(to_concat_klass)
-            assert res == set(exp)
+    # datetimelike
+    ([DatetimeIndex(['2011-01-01']), DatetimeIndex(['2011-01-02'])],
+     ['datetime']),
+    ([TimedeltaIndex(['1 days']), TimedeltaIndex(['2 days'])],
+     ['timedelta']),
 
-    def test_get_dtype_kinds(self):
-        to_concat = [['a'], [1, 2]]
-        self.check_concat(to_concat, ['i', 'object'])
+    # datetimelike object
+    ([DatetimeIndex(['2011-01-01']),
+      DatetimeIndex(['2011-01-02'], tz='US/Eastern')],
+     ['datetime', 'datetime64[ns, US/Eastern]']),
+    ([DatetimeIndex(['2011-01-01'], tz='Asia/Tokyo'),
+      DatetimeIndex(['2011-01-02'], tz='US/Eastern')],
+     ['datetime64[ns, Asia/Tokyo]', 'datetime64[ns, US/Eastern]']),
+    ([TimedeltaIndex(['1 days']), TimedeltaIndex(['2 hours'])],
+     ['timedelta']),
+    ([DatetimeIndex(['2011-01-01'], tz='Asia/Tokyo'),
+      TimedeltaIndex(['1 days'])],
+     ['datetime64[ns, Asia/Tokyo]', 'timedelta'])])
+@pytest.mark.parametrize('klass', [Index, Series])
+def test_get_dtype_kinds(klass, to_concat, expected):
+    to_concat_klass = [klass(c) for c in to_concat]
+    result = _concat.get_dtype_kinds(to_concat_klass)
+    assert result == set(expected)
 
-        to_concat = [[3, 4], [1, 2]]
-        self.check_concat(to_concat, ['i'])
 
-        to_concat = [[3, 4], [1, 2.1]]
-        self.check_concat(to_concat, ['i', 'f'])
-
-    def test_get_dtype_kinds_datetimelike(self):
-        to_concat = [pd.DatetimeIndex(['2011-01-01']),
-                     pd.DatetimeIndex(['2011-01-02'])]
-        self.check_concat(to_concat, ['datetime'])
-
-        to_concat = [pd.TimedeltaIndex(['1 days']),
-                     pd.TimedeltaIndex(['2 days'])]
-        self.check_concat(to_concat, ['timedelta'])
-
-    def test_get_dtype_kinds_datetimelike_object(self):
-        to_concat = [pd.DatetimeIndex(['2011-01-01']),
-                     pd.DatetimeIndex(['2011-01-02'], tz='US/Eastern')]
-        self.check_concat(to_concat,
-                          ['datetime', 'datetime64[ns, US/Eastern]'])
-
-        to_concat = [pd.DatetimeIndex(['2011-01-01'], tz='Asia/Tokyo'),
-                     pd.DatetimeIndex(['2011-01-02'], tz='US/Eastern')]
-        self.check_concat(to_concat,
-                          ['datetime64[ns, Asia/Tokyo]',
-                           'datetime64[ns, US/Eastern]'])
-
-        # timedelta has single type
-        to_concat = [pd.TimedeltaIndex(['1 days']),
-                     pd.TimedeltaIndex(['2 hours'])]
-        self.check_concat(to_concat, ['timedelta'])
-
-        to_concat = [pd.DatetimeIndex(['2011-01-01'], tz='Asia/Tokyo'),
-                     pd.TimedeltaIndex(['1 days'])]
-        self.check_concat(to_concat,
-                          ['datetime64[ns, Asia/Tokyo]', 'timedelta'])
-
-    def test_get_dtype_kinds_period(self):
-        # because we don't have Period dtype (yet),
-        # Series results in object dtype
-        to_concat = [pd.PeriodIndex(['2011-01'], freq='M'),
-                     pd.PeriodIndex(['2011-01'], freq='M')]
-        res = _concat.get_dtype_kinds(to_concat)
-        assert res == set(['period[M]'])
-
-        to_concat = [pd.Series([pd.Period('2011-01', freq='M')]),
-                     pd.Series([pd.Period('2011-02', freq='M')])]
-        res = _concat.get_dtype_kinds(to_concat)
-        assert res == set(['object'])
-
-        to_concat = [pd.PeriodIndex(['2011-01'], freq='M'),
-                     pd.PeriodIndex(['2011-01'], freq='D')]
-        res = _concat.get_dtype_kinds(to_concat)
-        assert res == set(['period[M]', 'period[D]'])
-
-        to_concat = [pd.Series([pd.Period('2011-01', freq='M')]),
-                     pd.Series([pd.Period('2011-02', freq='D')])]
-        res = _concat.get_dtype_kinds(to_concat)
-        assert res == set(['object'])
+@pytest.mark.parametrize('to_concat, expected', [
+    # because we don't have Period dtype (yet),
+    # Series results in object dtype
+    ([PeriodIndex(['2011-01'], freq='M'),
+      PeriodIndex(['2011-01'], freq='M')], ['period[M]']),
+    ([Series([Period('2011-01', freq='M')]),
+      Series([Period('2011-02', freq='M')])], ['object']),
+    ([PeriodIndex(['2011-01'], freq='M'),
+      PeriodIndex(['2011-01'], freq='D')], ['period[M]', 'period[D]']),
+    ([Series([Period('2011-01', freq='M')]),
+      Series([Period('2011-02', freq='D')])], ['object'])])
+def test_get_dtype_kinds_period(to_concat, expected):
+    result = _concat.get_dtype_kinds(to_concat)
+    assert result == set(expected)
