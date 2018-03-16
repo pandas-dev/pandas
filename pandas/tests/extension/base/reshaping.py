f16f59fae11c1374cfb721870df694e63d7472e6
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-03-16 15:37:01 -0500

    Fix boxing

--- pandas/tests/extension/base/reshaping.py
+++ pandas/tests/extension/base/reshaping.py
@@ -25,7 +25,7 @@ class BaseReshapingTests(BaseExtensionTests):
         assert dtype == data.dtype
         assert isinstance(result._data.blocks[0], ExtensionBlock)
 
-    @pytest.mark.parametrize('in_frame', [True])
+    @pytest.mark.parametrize('in_frame', [True, False])
     def test_concat_all_na_block(self, data_missing, in_frame):
         valid_block = pd.Series(data_missing.take([1, 1]), index=[0, 1])
         na_block = pd.Series(data_missing.take([0, 0]), index=[2, 3])
@@ -37,7 +37,7 @@ class BaseReshapingTests(BaseExtensionTests):
             expected = pd.DataFrame({"a": data_missing.take([1, 1, 0, 0])})
             self.assert_frame_equal(result, expected)
         else:
-            expected = pd.DataFrame({"a": data_missing.take([1, 1, 0, 0])})
+            expected = pd.Series(data_missing.take([1, 1, 0, 0]))
             self.assert_series_equal(result, expected)
 
     def test_align(self, data, na_value):
