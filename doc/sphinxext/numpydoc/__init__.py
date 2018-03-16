a4330f3dde692da6a7dd0dbf8ec81a09c0858a6b
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-03-16 15:01:14 +0100

    DOC: update vendored numpydoc version
    Numpydoc upstream at 21a194e9b42ebe95a70475e35cb69bcb4801ff36

--- doc/sphinxext/numpydoc/__init__.py
+++ doc/sphinxext/numpydoc/__init__.py
@@ -1,3 +1,8 @@
 from __future__ import division, absolute_import, print_function
 
-from .numpydoc import setup
+__version__ = '0.8.0.dev0'
+
+
+def setup(app, *args, **kwargs):
+    from .numpydoc import setup
+    return setup(app, *args, **kwargs)
