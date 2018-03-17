59eeb7f15d59a9592f1839f321cd46f21bd12b4b
Iain Barr <ijmbarr@users.noreply.github.com>
2018-03-17 13:09:49 +0100

    DOC: Improved the docstring of pandas.Series.dt.to_pytimedelta (#20142)

--- pandas/core/indexes/accessors.py
+++ pandas/core/indexes/accessors.py
@@ -200,6 +200,39 @@ class TimedeltaProperties(Properties):
     """
 
     def to_pytimedelta(self):
+        """
+        Return an array of native `datetime.timedelta` objects.
+
+        Python's standard `datetime` library uses a different representation
+        timedelta's. This method converts a Series of pandas Timedeltas
+        to `datetime.timedelta` format with the same length as the original
+        Series.
+
+        Returns
+        -------
+        a : numpy.ndarray
+            1D array containing data with `datetime.timedelta` type.
+
+        Examples
+        --------
+        >>> s = pd.Series(pd.to_timedelta(np.arange(5), unit='d'))
+        >>> s
+        0   0 days
+        1   1 days
+        2   2 days
+        3   3 days
+        4   4 days
+        dtype: timedelta64[ns]
+
+        >>> s.dt.to_pytimedelta()
+        array([datetime.timedelta(0), datetime.timedelta(1),
+               datetime.timedelta(2), datetime.timedelta(3),
+               datetime.timedelta(4)], dtype=object)
+
+        See Also
+        --------
+        datetime.timedelta
+        """
         return self._get_values().to_pytimedelta()
 
     @property
