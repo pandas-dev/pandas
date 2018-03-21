ea192267e62dc7b0a451d6e0039ca743f6e008db
minggli <ming.george.li@gmail.com>
2018-03-21 16:02:35 +0000

    more compact styling of parameters

--- pandas/tests/scalar/timedelta/test_construction.py
+++ pandas/tests/scalar/timedelta/test_construction.py
@@ -195,17 +195,13 @@ def test_iso_constructor_raises(fmt):
         Timedelta(fmt)
 
 
-@pytest.mark.parametrize('constructed_td, conversion',
-                         [(Timedelta(nanoseconds=100), '100ns'),
-                          (Timedelta(days=1, hours=1, minutes=1, weeks=1,
-                                     seconds=1, milliseconds=1, microseconds=1,
-                                     nanoseconds=1), 694861001001001),
-                          (Timedelta(microseconds=1) +
-                           Timedelta(nanoseconds=1), '1us1ns'),
-                          (Timedelta(microseconds=1) -
-                           Timedelta(nanoseconds=1), '999ns'),
-                          (Timedelta(microseconds=1) +
-                           5 * Timedelta(nanoseconds=-2), '990ns')])
+@pytest.mark.parametrize('constructed_td, conversion', [
+    (Timedelta(nanoseconds=100), '100ns'),
+    (Timedelta(days=1, hours=1, minutes=1, weeks=1, seconds=1, milliseconds=1,
+               microseconds=1, nanoseconds=1), 694861001001001),
+    (Timedelta(microseconds=1) + Timedelta(nanoseconds=1), '1us1ns'),
+    (Timedelta(microseconds=1) - Timedelta(nanoseconds=1), '999ns'),
+    (Timedelta(microseconds=1) + 5 * Timedelta(nanoseconds=-2), '990ns')])
 def test_td_constructor_on_nanoseconds(constructed_td, conversion):
     # GH#9273
     assert constructed_td == Timedelta(conversion)
