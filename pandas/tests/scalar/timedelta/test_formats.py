f0687d6c87695faabf41895dc6ada7f2ef01648b
Ming Li <14131823+minggli@users.noreply.github.com>
2018-03-22 10:40:38 +0000

    parameterize tests in scalar/timedelta (#20428)

--- pandas/tests/scalar/timedelta/test_formats.py
+++ pandas/tests/scalar/timedelta/test_formats.py
@@ -1,48 +1,28 @@
 # -*- coding: utf-8 -*-
+import pytest
+
 from pandas import Timedelta
 
 
-def test_repr():
-    assert (repr(Timedelta(10, unit='d')) ==
-            "Timedelta('10 days 00:00:00')")
-    assert (repr(Timedelta(10, unit='s')) ==
-            "Timedelta('0 days 00:00:10')")
-    assert (repr(Timedelta(10, unit='ms')) ==
-            "Timedelta('0 days 00:00:00.010000')")
-    assert (repr(Timedelta(-10, unit='ms')) ==
-            "Timedelta('-1 days +23:59:59.990000')")
+@pytest.mark.parametrize('td, expected_repr', [
+    (Timedelta(10, unit='d'), "Timedelta('10 days 00:00:00')"),
+    (Timedelta(10, unit='s'), "Timedelta('0 days 00:00:10')"),
+    (Timedelta(10, unit='ms'), "Timedelta('0 days 00:00:00.010000')"),
+    (Timedelta(-10, unit='ms'), "Timedelta('-1 days +23:59:59.990000')")])
+def test_repr(td, expected_repr):
+    assert repr(td) == expected_repr
 
 
-def test_isoformat():
-    td = Timedelta(days=6, minutes=50, seconds=3,
-                   milliseconds=10, microseconds=10, nanoseconds=12)
-    expected = 'P6DT0H50M3.010010012S'
-    result = td.isoformat()
-    assert result == expected
-
-    td = Timedelta(days=4, hours=12, minutes=30, seconds=5)
-    result = td.isoformat()
-    expected = 'P4DT12H30M5S'
-    assert result == expected
-
-    td = Timedelta(nanoseconds=123)
-    result = td.isoformat()
-    expected = 'P0DT0H0M0.000000123S'
-    assert result == expected
-
+@pytest.mark.parametrize('td, expected_iso', [
+    (Timedelta(days=6, minutes=50, seconds=3, milliseconds=10, microseconds=10,
+               nanoseconds=12), 'P6DT0H50M3.010010012S'),
+    (Timedelta(days=4, hours=12, minutes=30, seconds=5), 'P4DT12H30M5S'),
+    (Timedelta(nanoseconds=123), 'P0DT0H0M0.000000123S'),
     # trim nano
-    td = Timedelta(microseconds=10)
-    result = td.isoformat()
-    expected = 'P0DT0H0M0.00001S'
-    assert result == expected
-
+    (Timedelta(microseconds=10), 'P0DT0H0M0.00001S'),
     # trim micro
-    td = Timedelta(milliseconds=1)
-    result = td.isoformat()
-    expected = 'P0DT0H0M0.001S'
-    assert result == expected
-
+    (Timedelta(milliseconds=1), 'P0DT0H0M0.001S'),
     # don't strip every 0
-    result = Timedelta(minutes=1).isoformat()
-    expected = 'P0DT0H1M0S'
-    assert result == expected
+    (Timedelta(minutes=1), 'P0DT0H1M0S')])
+def test_isoformat(td, expected_iso):
+    assert td.isoformat() == expected_iso
