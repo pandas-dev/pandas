7deea54d4674bbb1d0752530cca5fce7a54073d8
Igor C. A. de Lima <igorcadelima@gmail.com>
2018-03-23 01:07:55 -0300

    DOC: Add note and example about how transposing can change dtype
    Signed-off-by: Igor C. A. de Lima <igorcadelima@gmail.com>

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -2313,8 +2313,15 @@ class DataFrame(NDFrame):
         --------
         numpy.transpose : Permute the dimensions of a given array.
 
+        Notes
+        -----
+        Transposing a DataFrame with mixed dtypes will result in a homogeneous
+        DataFrame with the `object` dtype.
+
         Examples
         --------
+        **Square DataFrame with homogeneous dtype**
+
         >>> d1 = {'col1': [1, 2], 'col2': [3, 4]}
         >>> df1 = pd.DataFrame(data=d1)
         >>> df1
@@ -2322,42 +2329,57 @@ class DataFrame(NDFrame):
         0     1     3
         1     2     4
 
-        >>> df1.T
+        >>> df1_transposed = df1.T # or df1.transpose()
+        >>> df1_transposed
               0  1
         col1  1  2
         col2  3  4
 
-        >>> df1.transpose()
-              0  1
-        col1  1  2
-        col2  3  4
+        When the dtype is homogeneous in the original DataFrame, we get a
+        transposed DataFrame with the same dtype:
 
-        **Non-square DataFrame and mixed data types**
+        >>> df1.dtypes
+        col1    int64
+        col2    int64
+        dtype: object
+        >>> df1_transposed.dtypes
+        0    int64
+        1    int64
+        dtype: object
+
+        **Non-square DataFrame with mixed dtypes**
 
         >>> d2 = {'name': ['Alice', 'Bob'],
         ...       'score': [9.5, 8],
         ...       'employed': [False, True],
         ...       'kids': [0, 0]}
-
         >>> df2 = pd.DataFrame(data=d2)
         >>> df2
             name  score  employed  kids
         0  Alice    9.5     False     0
         1    Bob    8.0      True     0
 
-        >>> df2.T
+        >>> df2_transposed = df2.T # or df2.transpose()
+        >>> df2_transposed
                       0     1
         name      Alice   Bob
         score       9.5     8
         employed  False  True
         kids          0     0
 
-        >>> df2.transpose()
-                      0     1
-        name      Alice   Bob
-        score       9.5     8
-        employed  False  True
-        kids          0     0
+        When the DataFrame has mixed dtypes, we get a transposed DataFrame with
+        the `object` dtype:
+
+        >>> df2.dtypes
+        name         object
+        score       float64
+        employed       bool
+        kids          int64
+        dtype: object
+        >>> df2_transposed.dtypes
+        0    object
+        1    object
+        dtype: object
         """
         nv.validate_transpose(args, dict())
         return super(DataFrame, self).transpose(1, 0, **kwargs)
