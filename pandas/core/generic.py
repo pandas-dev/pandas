18c7be3e60b2025aee8e64cca4224c1cf5fd0e5d
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-03-23 09:49:54 +0100

    Update generic.py

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -4508,21 +4508,21 @@ class NDFrame(PandasObject, SelectionMixin):
         """
         Make a copy of this object's indices and data.
 
-        When `deep=True` (default), a new object will be created with a
+        When ``deep=True`` (default), a new object will be created with a
         copy of the calling object's data and indices. Modifications to
         the data or indices of the copy will not be reflected in the
         original object (see notes below).
 
-        When `deep=False`, a new object will be created without copying
+        When ``deep=False``, a new object will be created without copying
         the calling object's data or index (only references to the data
         and index are copied). Any changes to the data of the original
         will be reflected in the shallow copy (and vice versa).
 
         Parameters
         ----------
-        deep : boolean or str, default `True`
+        deep : bool, default True
             Make a deep copy, including a copy of the data and the indices.
-            With `deep=False` neither the indices nor the data are copied.
+            With ``deep=False`` neither the indices nor the data are copied.
 
         Returns
         -------
