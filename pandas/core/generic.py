881b6b23035f770d24384eb089f7eabc56903bee
Maximiliano Greco <mmngreco@gmail.com>
2018-03-31 18:20:16 +0200

    CLN: Added doc and fixed variable names.

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -8763,7 +8763,18 @@ class NDFrame(PandasObject, SelectionMixin):
         scalar : type of index
         """
 
-    def _find_first_valid(self, direction=1):
+    def _find_valid_index(self, how):
+        """Retrieves the index of the first valid value.
+
+        Parameters
+        ----------
+        how : {'first', 'last'}
+            Use this parameter to change between the first or last valid index.
+
+        Returns
+        -------
+        idx_first_valid : type of index
+        """
         if len(self) == 0:  # early stop
             return None
         is_valid = ~self.isna()
@@ -8771,28 +8782,32 @@ class NDFrame(PandasObject, SelectionMixin):
         if self.ndim == 2:
             is_valid = is_valid.any(1)  # reduce axis 1
 
-        if direction == 1:
+        if how == 'first':
+            # First valid value case
             i = is_valid.idxmax()
             if not is_valid[i]:
                 return None
             else:
                 return i
-        elif direction == -1:
+        elif how == 'last':
+            # Last valid value case
             i = is_valid.values[::-1].argmax()
             if not is_valid.iat[len(self) - i - 1]:
                 return None
             else:
                 return self.index[len(self) - i - 1]
+        else:
+            raise ValueError
 
     @Appender(_shared_docs['valid_index'] % {'position': 'first',
                                              'klass': 'NDFrame'})
     def first_valid_index(self):
-        return self._find_first_valid(1)
+        return self._find_valid_index('first')
 
     @Appender(_shared_docs['valid_index'] % {'position': 'last',
                                              'klass': 'NDFrame'})
     def last_valid_index(self):
-        return self._find_first_valid(-1)
+        return self._find_valid_index('last')
 
 
 def _doc_parms(cls):
