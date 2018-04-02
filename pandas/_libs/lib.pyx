571d5c473b0a9d082426f99a0165f285d071f91e
Nikos Karagiannakis <nikoskaragiannakis@gmail.com>
2018-04-02 03:53:54 +0100

    pep8 correction

--- pandas/_libs/lib.pyx
+++ pandas/_libs/lib.pyx
@@ -466,7 +466,10 @@ cpdef ndarray[object] astype_unicode(ndarray arr):
         # we can use the unsafe version because we know `result` is mutable
         # since it was created from `np.empty`
         arr_i = arr[i]
-        util.set_value_at_unsafe(result, i, unicode(arr_i) if arr_i is not np.nan else np.nan)
+        util.set_value_at_unsafe(
+            result,
+            i,
+            unicode(arr_i) if arr_i is not np.nan else np.nan)
 
     return result
 
@@ -480,7 +483,10 @@ cpdef ndarray[object] astype_str(ndarray arr):
         # we can use the unsafe version because we know `result` is mutable
         # since it was created from `np.empty`
         arr_i = arr[i]
-        util.set_value_at_unsafe(result, i, str(arr_i) if arr_i is not np.nan else np.nan)
+        util.set_value_at_unsafe(
+            result,
+            i,
+            str(arr_i) if arr_i is not np.nan else np.nan)
 
     return result
 
