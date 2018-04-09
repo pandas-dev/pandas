10cf824d2136e834de5152c6f669582fd1ad8afc
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-04-09 15:59:20 +0200

    add read functions

--- ci/doctests.sh
+++ ci/doctests.sh
@@ -12,25 +12,35 @@ if [ "$LINT" ]; then
 
     echo "Running doctests"
 
-    pytest --doctest-modules \
-           pandas/core/reshape/concat.py \
-           pandas/core/reshape/pivot.py \
-           pandas/core/reshape/reshape.py \
-           pandas/core/reshape/tile.py -v
+    # top-level reshaping functions
+    pytest --doctest-modules -v \
+        pandas/core/reshape/concat.py \
+        pandas/core/reshape/pivot.py \
+        pandas/core/reshape/reshape.py \
+        pandas/core/reshape/tile.py
+
+    if [ $? -ne "0" ]; then
+        RET=1
+    fi
+
+    # top-level io functionality
+    pytest --doctest-modules -v pandas/io/* -k "read_"
 
     if [ $? -ne "0" ]; then
         RET=1
     fi
 
     # DataFrame docstrings
-    pytest --doctest-modules pandas/core/frame.py -k"-agg -apply -applymap -assign -eval -isin -itertuples -join -merge -nlargest -nsmallest -nunique -pivot -pivot_table -quantile -query -reindex -reindex_axis -replace -round -select_dtypes -set_index -stack -to_dict -to_excel -to_stata" -v
+    pytest --doctest-modules -v \
+        pandas/core/frame.py -k"-agg -apply -applymap -assign -eval -isin -itertuples -join -merge -nlargest -nsmallest -nunique -pivot -pivot_table -quantile -query -reindex -reindex_axis -replace -round -select_dtypes -set_index -stack -to_dict -to_excel -to_stata"
 
     if [ $? -ne "0" ]; then
         RET=1
     fi
 
     # Series docstrings
-    pytest --doctest-modules pandas/core/series.py -k"-agg -apply -map -nlargest -nonzero -nsmallest -reindex -replace -searchsorted -sort_values -to_dict -to_excel" -v
+    pytest --doctest-modules -v \
+        pandas/core/series.py -k"-agg -apply -map -nlargest -nonzero -nsmallest -reindex -replace -searchsorted -to_dict -to_excel"
 
     if [ $? -ne "0" ]; then
         RET=1
