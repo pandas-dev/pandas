e524003ecd764cbd2172dc3df5f18cf86e512f50
Xavier Choudhury <xchoudhury@gmail.com>
2018-04-12 16:47:41 -0400

    fixed pep8 issues

--- pandas/tests/indexes/multi/test_drop.py
+++ pandas/tests/indexes/multi/test_drop.py
@@ -14,6 +14,7 @@ import pandas.util.testing as tm
 
 from pandas.tests.indexes.common import Base
 
+
 class TestDrop(Base):
     _holder = MultiIndex
     _compat_props = ['shape', 'ndim', 'size', 'itemsize']
