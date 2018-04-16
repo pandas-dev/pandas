2ac734c25a4163e65693c622247e3e3398705e5f
kornilova-l <kornilova203@gmail.com>
2018-04-16 13:29:21 +0300

    Use _np_version_under1p12 instead of NumpyVersion

--- pandas/tests/test_window.py
+++ pandas/tests/test_window.py
@@ -6,7 +6,7 @@ from warnings import catch_warnings
 from datetime import datetime, timedelta
 from numpy.random import randn
 import numpy as np
-from scipy._lib._version import NumpyVersion
+from pandas import _np_version_under1p12
 
 import pandas as pd
 from pandas import (Series, DataFrame, bdate_range,
@@ -1177,7 +1177,7 @@ class TestMoments(Base):
 
         tm.assert_almost_equal(q1, q2)
 
-    @pytest.mark.skipif(NumpyVersion(np.__version__) < '1.12.0',
+    @pytest.mark.skipif(_np_version_under1p12,
                         reason='numpy midpoint interpolation is broken')
     @pytest.mark.parametrize('quantile', [0.0, 0.1, 0.45, 0.5, 1])
     @pytest.mark.parametrize('interpolation', ['linear', 'lower', 'higher',
