b1b97e059a5064e01d7d1e0c6a0ad658ceec28a4
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2018-04-23 20:35:28 -0500

    [CI]: Use conda-env for CI environments (#20767)

--- ci/requirements-2.7.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source activate pandas
-
-echo "install 27"
-
-conda install -n pandas -c conda-forge feather-format pyarrow=0.4.1 jemalloc=4.5.0.post fastparquet
--- ci/requirements-3.6.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source activate pandas
-
-echo "[install 3.6 downstream deps]"
-
-conda install -n pandas -c conda-forge pandas-datareader xarray geopandas seaborn statsmodels scikit-learn dask
