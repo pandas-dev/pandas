eb43fa4159c2affaf57ad686c260b2d1899eeacd
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-04-26 20:47:35 -0500

    Really truly fix it hopefully.

--- pandas/tests/extension/json/test_json.py
+++ pandas/tests/extension/json/test_json.py
@@ -21,6 +21,17 @@ def dtype():
 @pytest.fixture
 def data():
     """Length-100 PeriodArray for semantics test."""
+    data = make_data()
+
+    # Why the while loop? NumPy is unable to construct an ndarray from
+    # equal-length ndarrays. Many of our operations involve coercing the
+    # EA to an ndarray of objects. To avoid random test failures, we ensure
+    # that our data is coercable to an ndarray. Several tests deal with only
+    # the first two elements, so that's what we'll check.
+
+    while len(data[0]) == len(data[1]):
+        data = make_data()
+
     return JSONArray(make_data())
 
 
