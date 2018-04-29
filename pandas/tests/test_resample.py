00c93d94770663833a57897a9803eae64ec26c8d
John Cant <a.johncant@gmail.com>
2018-04-29 15:04:34 +0100

    Fix #20744 . loffset ignored in upsample

--- pandas/tests/test_resample.py
+++ pandas/tests/test_resample.py
@@ -1153,22 +1153,33 @@ class TestDatetimeIndex(Base):
         rng = date_range('1/1/2000 00:00:00', '1/1/2000 00:13:00', freq='min')
         s = Series(np.random.randn(14), index=rng)
 
-        result = s.resample('5min', closed='right', label='right',
-                            loffset=timedelta(minutes=1)).mean()
         idx = date_range('1/1/2000', periods=4, freq='5min')
-        expected = Series([s[0], s[1:6].mean(), s[6:11].mean(), s[11:].mean()],
-                          index=idx + timedelta(minutes=1))
-        assert_series_equal(result, expected)
+        expected_downsample = Series([s[0], s[1:6].mean(),
+                                      s[6:11].mean(), s[11:].mean()],
+                                     index=idx + timedelta(minutes=1))
+        # GH 20744
+        expected_upsample = Series([s[0], s[5], s[10], s[-1]],
+                                   index=idx + timedelta(minutes=1))
 
-        expected = s.resample(
+        # loffset should work for upsample and downsample
+        result_downsample = s.resample('5min', closed='right', label='right',
+                                       loffset=timedelta(minutes=1)).mean()
+        result_upsample = s.resample('5min', closed='right', label='right',
+                                     loffset=timedelta(minutes=1)).ffill()
+
+        assert_series_equal(result_downsample, expected_downsample)
+        assert_series_equal(result_upsample, expected_upsample)
+
+        # loffset should allow various types
+        result = s.resample(
             '5min', closed='right', label='right',
             loffset='1min').mean()
-        assert_series_equal(result, expected)
+        assert_series_equal(result, expected_downsample)
 
-        expected = s.resample(
+        result = s.resample(
             '5min', closed='right', label='right',
             loffset=Minute(1)).mean()
-        assert_series_equal(result, expected)
+        assert_series_equal(result, expected_downsample)
 
         assert result.index.freq == Minute(5)
 
