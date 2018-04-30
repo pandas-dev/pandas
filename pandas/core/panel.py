7e589989bd7c6e8c8bf2beb9ce995fd2c8f4d5f6
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-04-30 13:14:24 -0500

    Make both tests happy

--- pandas/core/panel.py
+++ pandas/core/panel.py
@@ -1499,8 +1499,11 @@ class Panel(NDFrame):
                 raw_lengths.append(v.shape[axis])
 
         if have_frames:
+            # we want the "old" behavior here, of sorting only
+            # 1. we're doing a union (intersect=False)
+            # 2. the indices are not aligned.
             index = _get_objs_combined_axis(data.values(), axis=axis,
-                                            intersect=intersect, sort=True)
+                                            intersect=intersect, sort=None)
 
         if have_raw_arrays:
             lengths = list(set(raw_lengths))
