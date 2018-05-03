ce4ab828d882a0c50f2f63921621ccae0d14b5ae
jschendel <jschendel@users.noreply.github.com>
2018-05-02 18:33:28 -0600

    CLN: Fix typo "strudes" --> "strides" (#20929)

--- pandas/core/base.py
+++ pandas/core/base.py
@@ -758,7 +758,7 @@ class IndexOpsMixin(object):
     @property
     def strides(self):
         """ return the strides of the underlying data """
-        warnings.warn("{obj}.strudes is deprecated and will be removed "
+        warnings.warn("{obj}.strides is deprecated and will be removed "
                       "in a future version".format(obj=type(self).__name__),
                       FutureWarning, stacklevel=2)
         return self._ndarray_values.strides
