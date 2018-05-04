1e83e6066d03d42cc8020595a99bfdcdd3a11c1d
Will Ayd <william.ayd@icloud.com>
2018-05-04 08:40:25 -0700

    Reverted none to nan conversion in unique

--- pandas/tests/series/test_analytics.py
+++ pandas/tests/series/test_analytics.py
@@ -1015,10 +1015,11 @@ class TestSeriesAnalytics(TestData):
         tm.assert_categorical_equal(s.unique(), pd.Categorical([np.nan]),
                                     check_dtype=False)
 
-    def test_unique_obj_na_preservation(self, nulls_fixture):
+    def test_unique_obj_none_preservation(self):
         # GH 20866
-        s = pd.Series(['foo', nulls_fixture])
-        assert s.iloc[1] is nulls_fixture
+        s = pd.Series(['foo', None])
+        result = s.unique()
+        assert result[1] is None
 
     @pytest.mark.parametrize(
         "tc1, tc2",
