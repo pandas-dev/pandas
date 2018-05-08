375c656d1ef2667eb52d27a4a4e5fa7e72076387
Jeff Reback <jeff@reback.net>
2018-05-07 20:11:23 -0400

    test clean

--- pandas/tests/indexing/test_loc.py
+++ pandas/tests/indexing/test_loc.py
@@ -788,28 +788,18 @@ Region_1,Site_2,3977723089,A,5/20/2015 8:33,5/20/2015 9:09,Yes,No"""
     def test_loc_uint64(self):
         # GH20722
         # Test whether loc accept uint64 max value as index.
-        s = pd.Series(
-            [1, 2],
-            index=[
-                np.iinfo('uint64').max - 1,
-                np.iinfo('uint64').max
-            ]
-        )
-        exp1 = pd.Series(
-            [1],
-            index=[
-                18446744073709551614
-            ]
-        )
-        exp2 = pd.Series(
-            [2],
-            index=[
-                18446744073709551615
-            ]
-        )
+        s = pd.Series([1, 2],
+                      index=[np.iinfo('uint64').max - 1,
+                             np.iinfo('uint64').max])
 
-        tm.assert_series_equal(s.loc[[np.iinfo('uint64').max - 1]], exp1)
-        assert 1 == s.loc[np.iinfo('uint64').max - 1]
+        result = s.loc[np.iinfo('uint64').max - 1]
+        expected = s.iloc[0]
+        assert result == expected
 
-        tm.assert_series_equal(s.loc[[np.iinfo('uint64').max]], exp2)
-        assert 2 == s.loc[np.iinfo('uint64').max]
+        result = s.loc[[np.iinfo('uint64').max - 1]]
+        expected = s.iloc[[0]]
+        tm.assert_series_equal(result, expected)
+
+        result = s.loc[[np.iinfo('uint64').max - 1,
+                       np.iinfo('uint64').max]]
+        tm.assert_series_equal(result, s)
