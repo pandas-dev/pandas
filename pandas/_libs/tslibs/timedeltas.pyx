2c91847d9de14615dc4458589a24137218e65065
Chalmer Lowe <chalmer.lowe@gmail.com>
2018-05-18 07:17:12 -1000

    DOC: Improve docstring of Timedelta.resolution

--- pandas/_libs/tslibs/timedeltas.pyx
+++ pandas/_libs/tslibs/timedeltas.pyx
@@ -780,22 +780,14 @@ cdef class _Timedelta(timedelta):
         ============ ============
         Resolution   Return value
         ============ ============ 
-        Days         'D'
-        Hours        'H'
-        Minutes      'T'
-        Seconds      'S'
-        Milliseconds 'L' 
-        Microseconds 'U'
-        Nanoseconds  'N'
+        Days         ``'D'``
+        Hours        ``'H'``
+        Minutes      ``'T'``
+        Seconds      ``'S'``
+        Milliseconds ``'L'`` 
+        Microseconds ``'U'``
+        Nanoseconds  ``'N'``
         ============ ============
-
-          - Days:         'D'
-          - Hours:        'H'
-          - Minutes:      'T'
-          - Seconds:      'S'
-          - Milliseconds: 'L'
-          - Microseconds: 'U'
-          - Nanoseconds:  'N'
 
         Returns
         -------
