ab0a692ffbe754ebad6c7991e58c1c15fe2f9ba9
Matt Roeschke <emailformattr@gmail.com>
2018-05-23 17:38:30 -0700

    Small cleanup

--- pandas/_libs/tslibs/strptime.pyx
+++ pandas/_libs/tslibs/strptime.pyx
@@ -82,14 +82,13 @@ def array_strptime(ndarray[object] values, object fmt,
         ndarray[int64_t] iresult
         ndarray[object] result_timezone
         int year, month, day, minute, hour, second, weekday, julian
-        int week_of_year, week_of_year_start, parse_code
+        int week_of_year, week_of_year_start, parse_code, ordinal
         int64_t us, ns
         object val, group_key, ampm, found, timezone
         dict found_key
         bint is_raise = errors=='raise'
         bint is_ignore = errors=='ignore'
         bint is_coerce = errors=='coerce'
-        int ordinal
 
     assert is_raise or is_ignore or is_coerce
 
