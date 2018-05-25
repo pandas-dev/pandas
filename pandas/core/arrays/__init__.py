d1faeb69359bd50b7bc6d5288904d06c870e4ea1
Brock Mendel <jbrockmendel@gmail.com>
2018-05-25 11:10:41 -0700

    fixups imports

--- pandas/core/arrays/__init__.py
+++ pandas/core/arrays/__init__.py
@@ -1,5 +1,5 @@
 from .base import ExtensionArray  # noqa
 from .categorical import Categorical  # noqa
-from .datetimes import DatetimeArray  # noqa
-from .periods import PeriodArray  # noqa
-from .timedeltas import TimedeltaArray  # noqa
+from .datetimes import DatetimeArrayMixin  # noqa
+from .periods import PeriodArrayMixin  # noqa
+from .timedeltas import TimedeltaArrayMixin  # noqa
