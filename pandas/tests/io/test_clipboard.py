f698ed6bfcf6518570b070bef17c66cfc77047fa
david-liu-brattle-1 <david.liu@brattle.com>
2018-06-05 22:26:22 -0400

    Rebase

--- pandas/tests/io/test_clipboard.py
+++ pandas/tests/io/test_clipboard.py
@@ -12,7 +12,7 @@ from pandas import get_option
 from pandas.util import testing as tm
 from pandas.util.testing import makeCustomDataframe as mkdf
 from pandas.io.clipboard.exceptions import PyperclipException
-from pandas.io.clipboard import clipboard_set, clipboard_get
+from pandas.io.clipboard import clipboard_set
 
 
 try:
@@ -60,9 +60,6 @@ class TestClipboard(object):
         # unicode round trip test for GH 13747, GH 12529
         cls.data['utf8'] = pd.DataFrame({'a': ['µasd', 'Ωœ∑´'],
                                          'b': ['øπ∆˚¬', 'œ∑´®']})
-        # Test for quotes and common delimiters in text
-        cls.data['delim_symbols'] = pd.DataFrame({'a': ['"a,\t"b|c', 'd\tef´'],
-                                                  'b': ['hi\'j', 'k\'\'lm']})
         cls.data_types = list(cls.data.keys())
 
     @classmethod
@@ -72,36 +69,18 @@ class TestClipboard(object):
     def check_round_trip_frame(self, data_type, excel=None, sep=None,
                                encoding=None):
         data = self.data[data_type]
-        if excel in [None, True] and sep is not None and len(sep) > 1:
-            with tm.assert_produces_warning():
-                data.to_clipboard(excel=excel, sep=sep, encoding=encoding)
+        data.to_clipboard(excel=excel, sep=sep, encoding=encoding)
+        if sep is not None:
+            result = read_clipboard(sep=sep, index_col=0, encoding=encoding)
         else:
-            data.to_clipboard(excel=excel, sep=sep, encoding=encoding)
-
-            if excel in [None, True] and sep is not None:
-                # Expect Excel
-                result = read_clipboard(sep=sep, index_col=0,
-                                        encoding=encoding)
-            elif excel in [None, True] and sep is None:
-                # Expect Excel with tabs
-                result = read_clipboard(sep='\t', index_col=0,
-                                        encoding=encoding)
-            else:
-                # Expect df.__repr__ format
-                result = read_clipboard(encoding=encoding)
-
-            if excel in [None, True]:
-                tm.assert_frame_equal(data, result, check_dtype=False)
-            else:
-                assert data.to_string() == result.to_string()
-                assert data.shape == result.shape
+            result = read_clipboard(encoding=encoding)
+        tm.assert_frame_equal(data, result, check_dtype=False)
 
     def test_round_trip_frame_sep(self):
         for dt in self.data_types:
             self.check_round_trip_frame(dt, sep=',')
             self.check_round_trip_frame(dt, sep=r'\s+')
             self.check_round_trip_frame(dt, sep='|')
-            self.check_round_trip_frame(dt, sep='\t')
 
     def test_round_trip_frame_string(self):
         for dt in self.data_types:
@@ -144,34 +123,6 @@ class TestClipboard(object):
         exp = pd.read_clipboard(**clip_kwargs)
 
         tm.assert_frame_equal(res, exp)
-
-    def test_excel_clipboard_tabs(self):
-        for dt in self.data_types:
-            for sep in ['\t', None, 'default', ',', '|']:
-                for excel in [True, None, 'default', False]:
-                    # Function default should be to to produce tab delimited
-                    kwargs = {}
-                    if excel != 'default':
-                        kwargs['excel'] = excel
-                    if sep != 'default':
-                        kwargs['sep'] = sep
-                    data = self.data[dt]
-                    data.to_clipboard(**kwargs)
-                    if sep in ['\t', None, 'default'] and excel is not False:
-                        # Expect tab delimited
-                        result = read_clipboard(sep='\t', index_col=0)
-                        tm.assert_frame_equal(data, result, check_dtype=False)
-                        assert clipboard_get().count('\t') > 0
-                    elif excel is False:
-                        # Expect spaces (ie. df.__repr__() default)
-                        result = read_clipboard(sep=r'\s+')
-                        assert result.to_string() == data.to_string()
-                        assert data.shape == result.shape
-                    else:
-                        # Expect other delimited ',' and '|'
-                        result = read_clipboard(sep=sep, index_col=0)
-                        tm.assert_frame_equal(data, result, check_dtype=False)
-                        assert clipboard_get().count(sep) > 0
 
     def test_invalid_encoding(self):
         # test case for testing invalid encoding
