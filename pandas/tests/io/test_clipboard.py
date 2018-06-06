bfba15c678e4870e9bc91734fe98e7f775ecf2b9
david-liu-brattle-1 <david.liu@brattle.com>
2018-06-05 22:13:35 -0400

    Refactored tests

--- pandas/tests/io/test_clipboard.py
+++ pandas/tests/io/test_clipboard.py
@@ -22,131 +22,118 @@ except (PyperclipException, RuntimeError):
     _DEPS_INSTALLED = 0
 
 
+def build_kwargs(sep, excel):
+    kwargs = {}
+    if excel != 'default':
+        kwargs['excel'] = excel
+    if sep != 'default':
+        kwargs['sep'] = sep
+    return kwargs
+
+
+def gen_df(data_type):
+    if data_type == 'delims':
+        return pd.DataFrame({'a': ['"a,\t"b|c', 'd\tef´'],
+                             'b': ['hi\'j', 'k\'\'lm']})
+    elif data_type == 'utf8':
+        return pd.DataFrame({'a': ['µasd', 'Ωœ∑´'],
+                             'b': ['øπ∆˚¬', 'œ∑´®']})
+    elif data_type == 'string':
+        return mkdf(5, 3, c_idx_type='s', r_idx_type='i',
+                    c_idx_names=[None], r_idx_names=[None])
+    elif data_type == 'long':
+        max_rows = get_option('display.max_rows')
+        return mkdf(max_rows + 1, 3,
+                    data_gen_f=lambda *args: randint(2),
+                    c_idx_type='s', r_idx_type='i',
+                    c_idx_names=[None], r_idx_names=[None])
+    elif data_type == 'nonascii':
+        return pd.DataFrame({'en': 'in English'.split(),
+                             'es': 'en español'.split()})
+    elif data_type == 'colwidth':
+        _cw = get_option('display.max_colwidth') + 1
+        return mkdf(5, 3, data_gen_f=lambda *args: 'x' * _cw,
+                    c_idx_type='s', r_idx_type='i',
+                    c_idx_names=[None], r_idx_names=[None])
+    elif data_type == 'mixed':
+        return DataFrame({'a': np.arange(1.0, 6.0) + 0.01,
+                          'b': np.arange(1, 6),
+                          'c': list('abcde')})
+    elif data_type == 'float':
+        return mkdf(5, 3, data_gen_f=lambda r, c: float(r) + 0.01,
+                    c_idx_type='s', r_idx_type='i',
+                    c_idx_names=[None], r_idx_names=[None])
+
+    elif data_type == 'int':
+        return mkdf(5, 3, data_gen_f=lambda *args: randint(2),
+                    c_idx_type='s', r_idx_type='i',
+                    c_idx_names=[None], r_idx_names=[None])
+
+
+@pytest.fixture(params=['delims', 'utf8', 'string', 'long', 'nonascii',
+                        'colwidth', 'mixed', 'float', 'int'])
+def df(request):
+    return gen_df(request.param)
+
+
 @pytest.mark.single
 @pytest.mark.skipif(not _DEPS_INSTALLED,
                     reason="clipboard primitives not installed")
 class TestClipboard(object):
 
-    @classmethod
-    def setup_class(cls):
-        cls.data = {}
-        cls.data['string'] = mkdf(5, 3, c_idx_type='s', r_idx_type='i',
-                                  c_idx_names=[None], r_idx_names=[None])
-        cls.data['int'] = mkdf(5, 3, data_gen_f=lambda *args: randint(2),
-                               c_idx_type='s', r_idx_type='i',
-                               c_idx_names=[None], r_idx_names=[None])
-        cls.data['float'] = mkdf(5, 3,
-                                 data_gen_f=lambda r, c: float(r) + 0.01,
-                                 c_idx_type='s', r_idx_type='i',
-                                 c_idx_names=[None], r_idx_names=[None])
-        cls.data['mixed'] = DataFrame({'a': np.arange(1.0, 6.0) + 0.01,
-                                       'b': np.arange(1, 6),
-                                       'c': list('abcde')})
-
-        # Test columns exceeding "max_colwidth" (GH8305)
-        _cw = get_option('display.max_colwidth') + 1
-        cls.data['colwidth'] = mkdf(5, 3, data_gen_f=lambda *args: 'x' * _cw,
-                                    c_idx_type='s', r_idx_type='i',
-                                    c_idx_names=[None], r_idx_names=[None])
-        # Test GH-5346
-        max_rows = get_option('display.max_rows')
-        cls.data['longdf'] = mkdf(max_rows + 1, 3,
-                                  data_gen_f=lambda *args: randint(2),
-                                  c_idx_type='s', r_idx_type='i',
-                                  c_idx_names=[None], r_idx_names=[None])
-        # Test for non-ascii text: GH9263
-        cls.data['nonascii'] = pd.DataFrame({'en': 'in English'.split(),
-                                             'es': 'en español'.split()})
-        # unicode round trip test for GH 13747, GH 12529
-        cls.data['utf8'] = pd.DataFrame({'a': ['µasd', 'Ωœ∑´'],
-                                         'b': ['øπ∆˚¬', 'œ∑´®']})
-        # Test for quotes and common delimiters in text
-        cls.data['delim_symbols'] = pd.DataFrame({'a': ['"a,\t"b|c', 'd\tef´'],
-                                                  'b': ['hi\'j', 'k\'\'lm']})
-        cls.data_types = list(cls.data.keys())
-
-    @classmethod
-    def teardown_class(cls):
-        del cls.data_types, cls.data
-
-    def check_round_trip_frame(self, data_type, excel=None, sep=None,
+    def check_round_trip_frame(self, data, excel=None, sep=None,
                                encoding=None):
-        data = self.data[data_type]
         data.to_clipboard(excel=excel, sep=sep, encoding=encoding)
         result = read_clipboard(sep=sep or '\t', index_col=0,
                                 encoding=encoding)
         tm.assert_frame_equal(data, result, check_dtype=False)
 
-    def test_round_trip_frame(self):
-        for dt in self.data_types:
-            self.check_round_trip_frame(dt)
+    @pytest.mark.xfail
+    def test_round_trip_frame(self, df):
+        self.check_round_trip_frame(df)
 
-    def test_round_trip_frame_sep(self):
-        for dt in self.data_types:
-            self.check_round_trip_frame(dt, sep=',')
-            self.check_round_trip_frame(dt, sep='|')
-            self.check_round_trip_frame(dt, sep='\t')
+    def test_round_trip_frame_sep(self, df):
+        self.check_round_trip_frame(df, sep=',')
+        self.check_round_trip_frame(df, sep='|')
+        self.check_round_trip_frame(df, sep='\t')
 
-    def test_round_trip_frame_string(self):
-        for dt in self.data_types:
-            data = self.data[dt]
-            data.to_clipboard(excel=False, sep=None)
-            result = read_clipboard()
-            assert data.to_string() == result.to_string()
-            assert data.shape == result.shape
+    @pytest.mark.xfail
+    def test_round_trip_frame_string(self, df):
+        df.to_clipboard(excel=False, sep=None)
+        result = read_clipboard()
+        assert df.to_string() == result.to_string()
+        assert df.shape == result.shape
 
+    @pytest.mark.xfail
     def test_excel_sep_warning(self):
         with tm.assert_produces_warning():
-            self.data['string'].to_clipboard(excel=True, sep=r'\t')
+            gen_df('string').to_clipboard(excel=True, sep=r'\t')
 
-    def build_kwargs(self, sep, excel):
-        kwargs = {}
-        if excel != 'default':
-            kwargs['excel'] = excel
-        if sep != 'default':
-            kwargs['sep'] = sep
-        return kwargs
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('sep', ['\t', None, 'default'])
+    @pytest.mark.parametrize('excel', [True, None, 'default'])
+    def test_clipboard_copy_tabs_default(self, sep, excel, df):
+        kwargs = build_kwargs(sep, excel)
+        df.to_clipboard(**kwargs)
+        assert clipboard_get() == df.to_csv(sep='\t')
 
-    @pytest.mark.parametrize('sep, excel', [
-        ('\t', True),
-        (None, True),
-        ('default', True),
-        ('\t', None),
-        (None, None),
-        ('\t', 'default'),
-        (None, 'default')
-    ])
-    def test_clipboard_copy_tabs_default(self, sep, excel):
-        for dt in self.data_types:
-            data = self.data[dt]
-            kwargs = self.build_kwargs(sep, excel)
-            data.to_clipboard(**kwargs)
-            assert clipboard_get() == data.to_csv(sep='\t')
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('sep', [',', '|'])
+    @pytest.mark.parametrize('excel', [True])
+    def test_clipboard_copy_delim(self, sep, excel, df):
+        kwargs = build_kwargs(sep, excel)
+        df.to_clipboard(**kwargs)
+        assert clipboard_get() == df.to_csv(sep=sep)
 
-    @pytest.mark.parametrize('sep, excel', [
-        (',', True),
-        ('|', True)
-    ])
-    def test_clipboard_copy_delim(self, sep, excel):
-        for dt in self.data_types:
-            data = self.data[dt]
-            kwargs = self.build_kwargs(sep, excel)
-            data.to_clipboard(**kwargs)
-            assert clipboard_get() == data.to_csv(sep=sep)
-
-    @pytest.mark.parametrize('sep, excel', [
-        ('\t', False),
-        (None, False),
-        ('default', False)
-    ])
-    def test_clipboard_copy_strings(self, sep, excel):
-        for dt in self.data_types:
-            data = self.data[dt]
-            kwargs = self.build_kwargs(sep, excel)
-            data.to_clipboard(**kwargs)
-            result = read_clipboard(sep=r'\s+')
-            assert result.to_string() == data.to_string()
-            assert data.shape == result.shape
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('sep', ['\t', None, 'default'])
+    @pytest.mark.parametrize('excel', [False])
+    def test_clipboard_copy_strings(self, sep, excel, df):
+        kwargs = build_kwargs(sep, excel)
+        df.to_clipboard(**kwargs)
+        result = read_clipboard(sep=r'\s+')
+        assert result.to_string() == df.to_string()
+        assert df.shape == result.shape
 
     def test_read_clipboard_infer_excel(self):
         # gh-19010: avoid warnings
@@ -184,13 +171,13 @@ class TestClipboard(object):
 
     def test_invalid_encoding(self):
         # test case for testing invalid encoding
-        data = self.data['string']
+        df = gen_df('string')
         with pytest.raises(ValueError):
-            data.to_clipboard(encoding='ascii')
+            df.to_clipboard(encoding='ascii')
         with pytest.raises(NotImplementedError):
             pd.read_clipboard(encoding='ascii')
 
-    def test_round_trip_valid_encodings(self):
-        for enc in ['UTF-8', 'utf-8', 'utf8']:
-            for dt in self.data_types:
-                self.check_round_trip_frame(dt, encoding=enc)
+    @pytest.mark.xfail
+    @pytest.mark.parametrize('enc', ['UTF-8', 'utf-8', 'utf8'])
+    def test_round_trip_valid_encodings(self, enc, df):
+        self.check_round_trip_frame(df, encoding=enc)
