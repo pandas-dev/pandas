55060215aabdf3e57d751944bdfa03356b88b26b
tmnhat2001 <tmnhat2001@gmail.com>
2018-06-07 05:39:24 -0400

    BUG: Using DatetimeIndex.date with timezone returns incorrect date (#21281)
    * BUG: Using DatetimeIndex.date with timezone returns incorrect date #21230
    * Fix bug where DTI.time returns a tz-aware Time instead of tz-naive #21267
    
    (cherry picked from commit a363e1a920d93d41bc87cb70afe35d030cc6bf9a)

--- pandas/tests/indexes/datetimes/test_timezones.py
+++ pandas/tests/indexes/datetimes/test_timezones.py
@@ -2,7 +2,7 @@
 """
 Tests for DatetimeIndex timezone-related methods
 """
-from datetime import datetime, timedelta, tzinfo
+from datetime import datetime, timedelta, tzinfo, date, time
 from distutils.version import LooseVersion
 
 import pytest
@@ -705,6 +705,32 @@ class TestDatetimeIndexTimezones(object):
         result = left.join(right[:-5], how=join_type)
         assert isinstance(result, DatetimeIndex)
         assert result.tz.zone == 'UTC'
+
+    @pytest.mark.parametrize("dtype", [
+        None, 'datetime64[ns, CET]',
+        'datetime64[ns, EST]', 'datetime64[ns, UTC]'
+    ])
+    def test_date_accessor(self, dtype):
+        # Regression test for GH#21230
+        expected = np.array([date(2018, 6, 4), pd.NaT])
+
+        index = DatetimeIndex(['2018-06-04 10:00:00', pd.NaT], dtype=dtype)
+        result = index.date
+
+        tm.assert_numpy_array_equal(result, expected)
+
+    @pytest.mark.parametrize("dtype", [
+        None, 'datetime64[ns, CET]',
+        'datetime64[ns, EST]', 'datetime64[ns, UTC]'
+    ])
+    def test_time_accessor(self, dtype):
+        # Regression test for GH#21267
+        expected = np.array([time(10, 20, 30), pd.NaT])
+
+        index = DatetimeIndex(['2018-06-04 10:20:30', pd.NaT], dtype=dtype)
+        result = index.time
+
+        tm.assert_numpy_array_equal(result, expected)
 
     def test_dti_drop_dont_lose_tz(self):
         # GH#2621
