085313c4515666a40b792a4133efd292acfb34b4
Eduardo Schettino <schettino72@gmail.com>
2018-06-11 11:08:32 +0800

    ENH: to_sql() add parameter "method". Fix unit-test (#8953)

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -372,7 +372,7 @@ class PandasSQLTest(object):
         iris_frame = self.pandasSQL.read_query(query, params=params)
         self._check_iris_loaded_frame(iris_frame)
 
-    def _to_sql(self, method=None):
+    def _to_sql(self, method='default'):
         self.drop_table('test_frame1')
 
         self.pandasSQL.to_sql(self.test_frame1, 'test_frame1', method=method)
