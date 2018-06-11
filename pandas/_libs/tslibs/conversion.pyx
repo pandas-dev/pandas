795cc39a65394e8f2a8d8cc5b28fd6ac7a3c22d4
Matt Roeschke <emailformattr@gmail.com>
2018-06-10 21:47:28 -0700

    Use pydatetime_checkexact

--- pandas/_libs/tslibs/conversion.pyx
+++ pandas/_libs/tslibs/conversion.pyx
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 # cython: profile=False
 
-from warnings import catch_warnings
-
 cimport cython
 from cython cimport Py_ssize_t
 
@@ -353,10 +351,9 @@ cdef _TSObject convert_datetime_to_tsobject(datetime ts, object tz,
             if hasattr(tz, 'normalize') and hasattr(ts.tzinfo, '_utcoffset'):
                 # tz.localize does not correctly localize Timestamps near DST
                 # but correctly localizes datetimes
-                if hasattr(ts, 'to_pydatetime'):
+                if not PyDateTime_CheckExact(ts):
                     nanos += ts.nanosecond
-                    with catch_warnings(record=True):
-                        ts = ts.to_pydatetime()
+                    ts = ts.to_pydatetime()
                 ts = tz.normalize(ts)
                 obj.value = pydatetime_to_dt64(ts, &obj.dts)
                 obj.tzinfo = ts.tzinfo
