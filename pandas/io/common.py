f2c95407717bfba24eedc3d64c56ab45e3f67677
Filipe Fernandes <ocefpaf@gmail.com>
2018-06-18 10:01:37 -0300

    add compression docstring

--- pandas/io/common.py
+++ pandas/io/common.py
@@ -215,6 +215,14 @@ def get_filepath_or_buffer(filepath_or_buffer, encoding=None,
                          or buffer
     encoding : the encoding to use to decode py3 bytes, default is 'utf-8'
     mode : str, optional
+    compression : {'infer', 'gzip', 'bz2', 'zip', 'xz', None}, default 'infer'
+        For on-the-fly decompression of on-disk data. If 'infer' and
+        `filepath_or_buffer` is path-like, then detect compression from the
+        following extensions: '.gz', '.bz2', '.zip', or '.xz' (otherwise no
+        decompression). If using 'zip', the ZIP file must contain only one data
+        file to be read in. Set to None for no decompression.
+
+    .. versionadded:: 0.18.1 support for 'zip' and 'xz' compression.
 
     Returns
     -------
