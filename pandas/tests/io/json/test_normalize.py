b0d3a869f9c84543430e26f095d5978994df35ad
Vu Le <levu@microsoft.com>
2018-06-20 08:00:55 +0700

    Remove unnecessary 1reindex_like1

--- pandas/tests/io/json/test_normalize.py
+++ pandas/tests/io/json/test_normalize.py
@@ -127,7 +127,7 @@ class TestJSONNormalize(object):
         # GH 21536
         result = json_normalize({'A': [1, 2]}, 'A', record_prefix='Prefix.')
         expected = DataFrame([[1], [2]], columns=['Prefix.0'])
-        tm.assert_frame_equal(result.reindex_like(expected), expected)
+        tm.assert_frame_equal(result, expected)
 
     def test_more_deeply_nested(self, deep_nested):
 
