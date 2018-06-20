b37cb752073c2255edfd7f6220ae1c78f2cab466
Jay Offerdahl <jaysanco@gmail.com>
2018-06-20 08:33:12 -0500

    Fixup tests to add more scenarios. Code review changes.

--- pandas/tests/io/formats/test_console.py
+++ pandas/tests/io/formats/test_console.py
@@ -7,28 +7,39 @@ from pandas.io.formats.console import detect_console_encoding, locale
 def mock_raises_exception(error=Exception):
     raise error
 
+@pytest.mark.parametrize('empty,filled', [
+    ['stdin', 'stdout'],
+    ['stdout', 'stdin']
+])
+def test_detect_console_encoding_from_stdout_stdin(monkeypatch, empty, filled):
+    # Ensure that when sys.stdout.encoding or sys.stdin.encoding is used when
+    # they have values filled.
+    monkeypatch.setattr('sys.{}.encoding'.format(empty), '')
+    monkeypatch.setattr('sys.{}.encoding'.format(filled), filled)
+    assert detect_console_encoding() == filled
 
-def test_detect_console_encoding_stdout(monkeypatch):
-    monkeypatch.setattr('sys.stdin.encoding', '')
-    monkeypatch.setattr('sys.stdout.encoding', 'foo')
-    assert detect_console_encoding() == 'foo'
 
-
-def test_detect_console_encoding_stdin(monkeypatch):
-    monkeypatch.setattr('sys.stdout.encoding', '')
-    monkeypatch.setattr('sys.stdin.encoding', 'foo')
-    assert detect_console_encoding() == 'foo'
-
-
-@pytest.mark.parametrize('error', [AttributeError, IOError])
-def test_detect_console_encoding_stdout_error_uses_locale(monkeypatch, error):
+@pytest.mark.parametrize('stdEncoding', [
+    partial(mock_raises_exception, AttributeError),
+    partial(mock_raises_exception, IOError),
+    lambda: 'ascii'
+])
+def test_detect_console_encoding_fallback_to_locale(monkeypatch, stdEncoding):
     monkeypatch.setattr('locale.getpreferredencoding', lambda: 'foo')
-    monkeypatch.setattr('sys.stdout', partial(mock_raises_exception, error))
+    monkeypatch.setattr('sys.stdout', stdEncoding)
     assert detect_console_encoding() == 'foo'
 
 
-def test_detect_console_encoding_sys_default_encoding(monkeypatch):
-    monkeypatch.setattr('locale.getpreferredencoding', mock_raises_exception)
-    monkeypatch.setattr('sys.stdout', mock_raises_exception)
+@pytest.mark.parametrize('std,locale', [
+    ['ascii', 'ascii'],
+    ['ascii', mock_raises_exception],
+    [mock_raises_exception, 'ascii'],
+    [mock_raises_exception, mock_raises_exception]
+])
+def test_detect_console_encoding_fallback_to_default(monkeypatch, std, locale):
+    # When both the stdout/stdin encoding and locale preferred encoding checks
+    # fail (or return 'ascii', we should default to the sys default encoding.
+    monkeypatch.setattr('sys.stdout', std)
+    monkeypatch.setattr('locale.getpreferredencoding', locale)
     monkeypatch.setattr('sys.getdefaultencoding', lambda: 'sysDefaultEncoding')
     assert detect_console_encoding() == 'sysDefaultEncoding'
