95d66f0e17c12a1ad661ad68c4fb49eadcf4b578
Ming Li <ming.george.li@gmail.com>
2018-06-21 20:39:13 +0100

    Ax = (x.T * A.T).T = (x * A.T).T

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -2066,7 +2066,7 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
 
     def __rmatmul__(self, other):
         """ Matrix multiplication using binary `@` operator in Python>=3.5 """
-        return self.dot(other)
+        return self.dot(np.transpose(other)).T
 
     @Substitution(klass='Series')
     @Appender(base._shared_docs['searchsorted'])
