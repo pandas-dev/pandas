3db45a9cf671589e668f8a47618f4c6e248d6168
Ming Li <ming.george.li@gmail.com>
2018-06-22 13:12:43 +0100

    update tests for both lists and numpy array 1D and 2D

--- pandas/tests/series/test_analytics.py
+++ pandas/tests/series/test_analytics.py
@@ -849,14 +849,27 @@ class TestSeriesAnalytics(TestData):
         expected = np.dot(a.values, a.values)
         assert_almost_equal(result, expected)
 
-        # np.array @ Series (__rmatmul__)
+        # GH 21530
+        # vector (1D np.array) @ Series (__rmatmul__)
         result = operator.matmul(a.values, a)
         expected = np.dot(a.values, a.values)
         assert_almost_equal(result, expected)
 
         # GH 21530
-        # np.array (matrix) @ Series (__rmatmul__)
+        # vector (1D list) @ Series (__rmatmul__)
+        result = operator.matmul(a.tolist(), a)
+        expected = np.dot(a.values, a.values)
+        assert_almost_equal(result, expected)
+
+        # GH 21530
+        # matrix (2D np.array) @ Series (__rmatmul__)
         result = operator.matmul(b.T.values, a)
+        expected = np.dot(b.T.values, a.values)
+        assert_almost_equal(result, expected)
+
+        # GH 21530
+        # matrix (2D nested lists) @ Series (__rmatmul__)
+        result = operator.matmul(b.T.tolist(), a)
         expected = np.dot(b.T.values, a.values)
         assert_almost_equal(result, expected)
 
