cbc51c1013369e90e084b652105ea4752285eb9a
LeakedMemory <elmq0022@umn.edu>
2018-06-27 06:23:22 -0500

    TST: Use absolute path for datapath (#21647)

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -153,8 +153,10 @@ def datapath(request):
     ValueError
         If the path doesn't exist and the --strict-data-files option is set.
     """
+    BASE_PATH = os.path.join(os.path.dirname(__file__), 'tests')
+
     def deco(*args):
-        path = os.path.join('pandas', 'tests', *args)
+        path = os.path.join(BASE_PATH, *args)
         if not os.path.exists(path):
             if request.config.getoption("--strict-data-files"):
                 msg = "Could not find file {} and --strict-data-files is set."
