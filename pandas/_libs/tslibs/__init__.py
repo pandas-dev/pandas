6a9afa4c6bac4047d069169473973267d6ea3829
Brock Mendel <jbrockmendel@gmail.com>
2018-07-04 17:48:14 -0700

    make imports explicit

--- pandas/_libs/tslibs/__init__.py
+++ pandas/_libs/tslibs/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 # flake8: noqa
 
-from conversion import normalize_date, localize_pydatetime, tz_convert_single
-from nattype import NaT, iNaT
-from np_datetime import OutOfBoundsDatetime
-from timestamps import Timestamp
-from timedeltas import delta_to_nanoseconds, ints_to_pytimedelta, Timedelta
+from .conversion import normalize_date, localize_pydatetime, tz_convert_single
+from .nattype import NaT, iNaT
+from .np_datetime import OutOfBoundsDatetime
+from .timestamps import Timestamp
+from .timedeltas import delta_to_nanoseconds, ints_to_pytimedelta, Timedelta
