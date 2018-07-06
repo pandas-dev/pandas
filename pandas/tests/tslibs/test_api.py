687ee7f5032c52a625197211d2a7a9dfd3904933
Jeff Reback <jeff@reback.net>
2018-07-06 11:17:02 -0500

    separate api

--- pandas/tests/tslibs/test_api.py
+++ pandas/tests/tslibs/test_api.py
@@ -6,31 +6,32 @@ from pandas._libs import tslibs
 
 def test_namespace():
 
-    expected = [
-        'ccalendar',
-        'conversion',
-        'fields',
-        'frequencies',
-        'nattype',
-        'np_datetime',
-        'offsets',
-        'parsing',
-        'period',
-        'resolution',
-        'strptime',
-        'timedeltas',
-        'timestamps',
-        'timezones',
+    submodules = ['ccalendar',
+                  'conversion',
+                  'fields',
+                  'frequencies',
+                  'nattype',
+                  'np_datetime',
+                  'offsets',
+                  'parsing',
+                  'period',
+                  'resolution',
+                  'strptime',
+                  'timedeltas',
+                  'timestamps',
+                  'timezones']
 
-        'NaT',
-        'iNaT',
-        'OutOfBoundsDatetime',
-        'Timedelta',
-        'Timestamp',
-        'delta_to_nanoseconds',
-        'ints_to_pytimedelta',
-        'localize_pydatetime',
-        'normalize_date',
-        'tz_convert_single']
+    api = ['NaT',
+           'iNaT',
+           'OutOfBoundsDatetime',
+           'Timedelta',
+           'Timestamp',
+           'delta_to_nanoseconds',
+           'ints_to_pytimedelta',
+           'localize_pydatetime',
+           'normalize_date',
+           'tz_convert_single']
+
+    expected = set(submodules + api)
     names = [x for x in dir(tslibs) if not x.startswith('__')]
     assert set(names) == expected
