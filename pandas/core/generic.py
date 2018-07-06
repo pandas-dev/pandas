cede49640aca5b70f02399dd3dd12c742ffd8066
Kalyan Gokhale <4734245+KalyanGokhale@users.noreply.github.com>
2018-07-06 08:42:45 +0530

    Update generic.py
    2nd update removed duplicated deprecation comment

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -8883,9 +8883,6 @@ class NDFrame(PandasObject, SelectionMixin):
         cls.ptp = _make_stat_function(
             cls, 'ptp', name, name2, axis_descr,
             """
-            .. deprecated:: 0.24.0
-                Use numpy.ptp instead
-
             Returns the difference between the maximum value and the
             minimum value in the object. This is the equivalent of the
             ``numpy.ndarray`` method ``ptp``.
