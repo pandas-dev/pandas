c64f6cbf6838b118c66bc61f11ceb68cf636cef4
cxl923cc <38508191+cxl923cc@users.noreply.github.com>
2018-07-07 23:42:03 +0100

    Doc: Added docstring to Groupby mean (#20910)

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -1297,9 +1297,48 @@ class GroupBy(_GroupBy):
     @Appender(_doc_template)
     def mean(self, *args, **kwargs):
         """
-        Compute mean of groups, excluding missing values
+        Compute mean of groups, excluding missing values.
 
-        For multiple groupings, the result index will be a MultiIndex
+        Returns
+        -------
+        pandas.Series or pandas.DataFrame
+
+        Examples
+        --------
+        >>> df = pd.DataFrame({'A': [1, 1, 2, 1, 2],
+        ...                    'B': [np.nan, 2, 3, 4, 5],
+        ...                    'C': [1, 2, 1, 1, 2]}, columns=['A', 'B', 'C'])
+
+        Groupby one column and return the mean of the remaining columns in
+        each group.
+
+        >>> df.groupby('A').mean()
+        >>>
+             B         C
+        A
+        1  3.0  1.333333
+        2  4.0  1.500000
+
+        Groupby two columns and return the mean of the remaining column.
+
+        >>> df.groupby(['A', 'B']).mean()
+        >>>
+               C
+        A B
+        1 2.0  2
+          4.0  1
+        2 3.0  1
+          5.0  2
+
+        Groupby one column and return the mean of only particular column in
+        the group.
+
+        >>> df.groupby('A')['B'].mean()
+        >>>
+        A
+        1    3.0
+        2    4.0
+        Name: B, dtype: float64
         """
         nv.validate_groupby_func('mean', args, kwargs, ['numeric_only'])
         try:
