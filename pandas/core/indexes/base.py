bdaa807788c58aa19dafd9ed6b98c907b3c579e8
Tamas Nagy <tamas.nagy@balabit.com>
2018-07-08 17:07:18 +0200

    DOC: update the Index.get_level_values docstring (#20210)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3090,26 +3090,41 @@ class Index(IndexOpsMixin, PandasObject):
 
     def _get_level_values(self, level):
         """
-        Return an Index of values for requested level, equal to the length
-        of the index.
+        Return an Index of values for requested level.
+
+        This is primarily useful to get an individual level of values from a
+        MultiIndex, but is provided on Index as well for compatability.
 
         Parameters
         ----------
         level : int or str
-            ``level`` is either the integer position of the level in the
-            MultiIndex, or the name of the level.
+            It is either the integer position or the name of the level.
 
         Returns
         -------
         values : Index
-            ``self``, as there is only one level in the Index.
+            Calling object, as there is only one level in the Index.
 
         See also
-        ---------
-        pandas.MultiIndex.get_level_values : get values for a level of a
-                                             MultiIndex
-        """
+        --------
+        MultiIndex.get_level_values : get values for a level of a MultiIndex
 
+        Notes
+        -----
+        For Index, level should be 0, since there are no multiple levels.
+
+        Examples
+        --------
+
+        >>> idx = pd.Index(list('abc'))
+        >>> idx
+        Index(['a', 'b', 'c'], dtype='object')
+
+        Get level values by supplying `level` as integer:
+
+        >>> idx.get_level_values(0)
+        Index(['a', 'b', 'c'], dtype='object')
+        """
         self._validate_index_level(level)
         return self
 
