59e0993dbeeb0a200e0b87e14a1ee36aa11d634a
Jay Offerdahl <jaysanco@gmail.com>
2018-07-12 21:57:07 -0500

    Use context manager when monkeypatching standard library items.

--- pandas/tests/io/formats/test_console.py
+++ pandas/tests/io/formats/test_console.py
@@ -20,9 +20,10 @@ def test_detect_console_encoding_from_stdout_stdin(monkeypatch, empty, filled):
             super(MockEncoding, self).__init__()
             self.encoding = encoding
 
-    monkeypatch.setattr('sys.{}'.format(empty), MockEncoding(''))
-    monkeypatch.setattr('sys.{}'.format(filled), MockEncoding(filled))
-    assert detect_console_encoding() == filled
+    with monkeypatch.context() as context:
+        context.setattr('sys.{}'.format(empty), MockEncoding(''))
+        context.setattr('sys.{}'.format(filled), MockEncoding(filled))
+        assert detect_console_encoding() == filled
 
 
 @pytest.mark.parametrize('stdEncoding', [
@@ -32,8 +33,9 @@ def test_detect_console_encoding_from_stdout_stdin(monkeypatch, empty, filled):
 ])
 def test_detect_console_encoding_fallback_to_locale(monkeypatch, stdEncoding):
     monkeypatch.setattr('locale.getpreferredencoding', lambda: 'foo')
-    monkeypatch.setattr('sys.stdout', stdEncoding)
-    assert detect_console_encoding() == 'foo'
+    with monkeypatch.context() as context:
+        context.setattr('sys.stdout', stdEncoding)
+        assert detect_console_encoding() == 'foo'
 
 
 @pytest.mark.parametrize('std,locale', [
@@ -45,7 +47,8 @@ def test_detect_console_encoding_fallback_to_locale(monkeypatch, stdEncoding):
 def test_detect_console_encoding_fallback_to_default(monkeypatch, std, locale):
     # When both the stdout/stdin encoding and locale preferred encoding checks
     # fail (or return 'ascii', we should default to the sys default encoding.
-    monkeypatch.setattr('sys.stdout', std)
     monkeypatch.setattr('locale.getpreferredencoding', locale)
-    monkeypatch.setattr('sys.getdefaultencoding', lambda: 'sysDefaultEncoding')
-    assert detect_console_encoding() == 'sysDefaultEncoding'
+    with monkeypatch.context() as context:
+        context.setattr('sys.stdout', std)
+        context.setattr('sys.getdefaultencoding', lambda: 'sysDefaultEncoding')
+        assert detect_console_encoding() == 'sysDefaultEncoding'
