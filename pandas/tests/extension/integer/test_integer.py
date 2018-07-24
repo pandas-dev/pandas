1c40fd7b4cd3b9c9fc668745453a6b9a1414db53
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-07-24 11:17:45 +0200

    fix py2

--- pandas/tests/extension/integer/test_integer.py
+++ pandas/tests/extension/integer/test_integer.py
@@ -143,7 +143,10 @@ class TestArithmeticOps(BaseInteger, base.BaseArithmeticOpsTests):
             result = op(s, other)
             expected = s.combine(other, op)
 
-            if op_name in ('__rtruediv__', '__truediv__'):
+            if op_name == '__rdiv__':
+                # combine is not giving the correct result for this case
+                pytest.skip("skipping reverse div in python 2")
+            elif op_name in ('__rtruediv__', '__truediv__', '__div__'):
                 expected = expected.astype(float)
                 if op_name == '__rtruediv__':
                     # TODO reverse operators result in object dtype
