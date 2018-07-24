ab4fd0d387a13bb23d5aed671ef8672e4cad19aa
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-07-24 08:20:11 -0500

    More database

--- ci/install_db_circle.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "installing dbs"
-# mysql -e 'create database pandas_nosetest;'
-psql -d $POSTGRESQL_URL -c 'create database pandas_nosetest;' -U postgres || exit 1
-
-echo "done"
-exit 0
