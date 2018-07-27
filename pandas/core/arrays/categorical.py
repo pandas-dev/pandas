9aa50ecd53e4ab7dad95cf3faa9f9687306bcf90
Pietro Battiston <me@pietrobattiston.it>
2018-07-27 12:36:32 +0200

    DOC: clarify default behavior for categories ordering

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -241,10 +241,14 @@ class Categorical(ExtensionArray, PandasObject):
         categories will be replaced with NaN.
     categories : Index-like (unique), optional
         The unique categories for this categorical. If not given, the
-        categories are assumed to be the unique values of values.
+        categories are assumed to be the unique values of values (sorted, if
+        possible, otherwise in the order in which they appear).
     ordered : boolean, (default False)
         Whether or not this categorical is treated as a ordered categorical.
-        If not given, the resulting categorical will not be ordered.
+        If True, the resulting categorical will be ordered.
+        An ordered categorical respects, when sorted, the order of its
+        'categories' attribute (which in turn is the 'categories' argument, if
+        provided).
     dtype : CategoricalDtype
         An instance of ``CategoricalDtype`` to use for this categorical
 
