78adc53221acfe9d0600cdd9145e629749a5472c
Ming Li <14131823+minggli@users.noreply.github.com>
2018-07-28 14:16:25 +0100

    consistent docstring (#22066)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -1999,7 +1999,7 @@ class NDFrame(PandasObject, SelectionMixin):
 
             .. versionadded:: 0.19.0
 
-        compression : {'infer', 'gzip', 'bz2', 'xz', None}, default None
+        compression : {'infer', 'gzip', 'bz2', 'zip', 'xz', None}, default None
             A string representing the compression to use in the output file,
             only used when the first argument is a filename.
 
