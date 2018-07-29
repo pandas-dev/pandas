c6a656079a9e342b00fc8e7fe735473a41602de1
Dean Langsam <DeanLa@users.noreply.github.com>
2018-07-29 20:00:00 +0300

    DOC: assert_frame_equal index_like wording
    rows -> index

--- pandas/util/testing.py
+++ pandas/util/testing.py
@@ -1348,8 +1348,8 @@ def assert_frame_equal(left, right, check_dtype=True,
     check_categorical : bool, default True
         Whether to compare internal Categorical exactly.
     check_like : bool, default False
-        If True, ignore the order of rows & columns.
-        Note: Index (row) labels must match their respective rows
+        If True, ignore the order of index & columns.
+        Note: row labels must match their respective rows
         (same as in columns) - same labels must be with the same data
     obj : str, default 'DataFrame'
         Specify object name being compared, internally used to show appropriate
