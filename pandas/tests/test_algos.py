0f950dea1d7d12604b26e13ab85b72c57f89672c
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2018-07-29 22:30:20 +0200

    CLN: Remove special handling of nans in the float64-case of isin
    It is no longer needed because the hash-table handles the nans correctly out of the box (see GH21866)
    
    Not having to scan the values via isna(...).any()  will improve the perfomance.

--- pandas/tests/test_algos.py
+++ pandas/tests/test_algos.py
@@ -632,6 +632,27 @@ class TestIsin(object):
         result = algos.isin(comps, vals)
         tm.assert_numpy_array_equal(expected, result)
 
+    def test_different_nans_as_float64(self):
+        # create different nans from bit-patterns,
+        # these nans will land in different buckets in the hash-table
+        # if no special care is taken
+        NAN1 = struct.unpack("d", struct.pack("=Q", 0x7ff8000000000000))[0]
+        NAN2 = struct.unpack("d", struct.pack("=Q", 0x7ff8000000000001))[0]
+        assert NAN1 != NAN1
+        assert NAN2 != NAN2
+
+        # check that NAN1 and NAN2 are equivalent:
+        arr = np.array([NAN1, NAN2], dtype=np.float64)
+        lookup1 = np.array([NAN1], dtype=np.float64)
+        result = algos.isin(arr, lookup1)
+        expected = np.array([True, True])
+        tm.assert_numpy_array_equal(result, expected)
+
+        lookup2 = np.array([NAN2], dtype=np.float64)
+        result = algos.isin(arr, lookup2)
+        expected = np.array([True, True])
+        tm.assert_numpy_array_equal(result, expected)
+
 
 class TestValueCounts(object):
 
