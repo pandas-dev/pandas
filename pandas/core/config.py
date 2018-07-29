685e82c1e5e7541557193e66eb3e2b27052c0cc1
Paul van Mulbregt <pvanmulbregt@users.noreply.github.com>
2018-07-29 18:04:20 -0400

    DOC: Fix spaces and brackets in ValueError message for option_context.

--- pandas/core/config.py
+++ pandas/core/config.py
@@ -390,8 +390,8 @@ class option_context(object):
 
     def __init__(self, *args):
         if not (len(args) % 2 == 0 and len(args) >= 2):
-            raise ValueError('Need to invoke as'
-                             'option_context(pat, val, [(pat, val), ...)).')
+            raise ValueError('Need to invoke as '
+                             'option_context(pat, val, [(pat, val), ...]).')
 
         self.ops = list(zip(args[::2], args[1::2]))
 
