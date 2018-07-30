3e3e5c9281e6ab32fd8f1a50482a8eab5a99f68c
jschendel <jschendel@users.noreply.github.com>
2018-07-30 09:44:45 -0600

    DEPR: deprecate IntervalIndex.itemsize and remove IntervalArray.itemsize

--- pandas/core/indexes/interval.py
+++ pandas/core/indexes/interval.py
@@ -369,8 +369,14 @@ class IntervalIndex(IntervalMixin, Index):
 
     @property
     def itemsize(self):
-        # Avoid materializing ndarray[Interval]
-        return self._data.itemsize
+        msg = ('IntervalIndex.itemsize is deprecated and will be removed in '
+               'a future version')
+        warnings.warn(msg, FutureWarning, stacklevel=2)
+
+        # supress the warning from the underlying left/right itemsize
+        with warnings.catch_warnings():
+            warnings.simplefilter('ignore')
+            return self.left.itemsize + self.right.itemsize
 
     def __len__(self):
         return len(self.left)
