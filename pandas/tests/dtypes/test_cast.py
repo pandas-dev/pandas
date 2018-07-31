21c0520904e87b2f174fbc7c11258a1225b6ed80
Sergey Solovev <solovev@ws-finance.com>
2018-07-31 11:43:14 +0300

    fix test_astype_nansafe

--- pandas/tests/dtypes/test_cast.py
+++ pandas/tests/dtypes/test_cast.py
@@ -460,12 +460,9 @@ def test_construct_1d_ndarray_preserving_na(values, dtype, expected):
 
 
 @pytest.mark.parametrize('arr, dtype, expected', [
-    (np.array(['0:0:1'], dtype='object'),
-     'timedelta64[ns]', 'timedelta64[ns]'),
-    (np.array(['2000'], dtype='object'),
-     'datetime64[ns]', 'datetime64[ns]'),
-    (np.array(['2000'], dtype='object'),
-     'datetime64', 'datetime64[ns]'),
+    (np.array(['0:0:1'], dtype='O'), 'm8[ns]', 'm8[ns]'),
+    (np.array(['2000'], dtype='O'), 'M8[ns]', 'M8[ns]'),
+    (np.array(['2000'], dtype='O'), 'M8', 'M8[ns]'),
 ])
 def test_astype_nansafe(arr, dtype, expected):
     # GH #22100
