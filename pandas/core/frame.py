f5bb3b64c42a8e81ecbb2f9805e13f3cbf25ff6f
Brock Mendel <jbrockmendel@gmail.com>
2018-08-01 17:57:21 -0700

    dummy commit to force CI

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -4898,7 +4898,7 @@ class DataFrame(NDFrame):
         new_index, new_columns = this.index, this.columns
 
         def _arith_op(left, right):
-            # for the mixed_type case where we iterate over columns,
+            # For the mixed_type case where we iterate over columns,
             # _arith_op(left, right) is equivalent to
             # left._binop(right, func, fill_value=fill_value)
             left, right = ops.fill_binop(left, right, fill_value)
