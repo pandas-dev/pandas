2e6513e4e013ab0b22ea6167d8ec686a2c72e585
Shantanu Oak <shantanu.oak@gmail.com>
2018-08-03 20:09:51 +0000

    added a function
    to be used as a decorator to validate keyword arguments

--- pandas/util/_decorators.py
+++ pandas/util/_decorators.py
@@ -5,6 +5,21 @@ import warnings
 from textwrap import dedent, wrap
 from functools import wraps, update_wrapper, WRAPPER_ASSIGNMENTS
 
+def validate_kwarg():
+    def _validate_kwarg(func):
+        @wraps(func)
+        def wrapper(*args, **kwargs):
+            expected_keys=['io', 'sheet_name','header', 'names', 'index_col', 'usecols', 'squeeze', 'dtype', 'engine', 
+               'converters', 'true_values',  'false_values', 'skiprows', 'nrows', 'na_values', 'verbose', 'parse_dates',
+               'date_parser', 'thousands', 'comment', 'skipfooter', 'convert_float']
+
+            if set(kwargs.keys()).difference(set(expected_keys)): 
+                raise ValueError('invalid parameter found')
+
+            return func(*args, **kwargs)
+        return wrapper
+    return _validate_kwarg
+
 
 def deprecate(name, alternative, version, alt_name=None,
               klass=None, stacklevel=2, msg=None):
