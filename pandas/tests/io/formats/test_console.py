40b3588e9f56b78e665a506183ddc87cf9621dac
Jay Offerdahl <jaysanco@gmail.com>
2018-08-03 20:52:17 -0500

    Move raise_or_return to MockEncoding; clean up parameters.

--- pandas/tests/io/formats/test_console.py
+++ pandas/tests/io/formats/test_console.py
@@ -15,14 +15,14 @@ class MockEncoding(object):  # TODO(py27): replace with mock
 
     @property
     def encoding(self):
-        return raise_or_return(self.val)
+        return self.raise_or_return(self.val)
 
-
-def raise_or_return(val):
-    if isinstance(val, str):
-        return val
-    else:
-        raise val
+    @staticmethod
+    def raise_or_return(val):
+        if isinstance(val, str):
+            return val
+        else:
+            raise val
 
 
 @pytest.mark.parametrize('empty,filled', [
@@ -40,32 +40,35 @@ def test_detect_console_encoding_from_stdout_stdin(monkeypatch, empty, filled):
 
 
 @pytest.mark.parametrize('encoding', [
-    MockEncoding(AttributeError),
-    MockEncoding(IOError),
-    MockEncoding('ascii')
+    AttributeError,
+    IOError,
+    'ascii'
 ])
 def test_detect_console_encoding_fallback_to_locale(monkeypatch, encoding):
     # GH 21552
     with monkeypatch.context() as context:
         context.setattr('locale.getpreferredencoding', lambda: 'foo')
-        context.setattr('sys.stdout', encoding)
+        context.setattr('sys.stdout', MockEncoding(encoding))
         assert detect_console_encoding() == 'foo'
 
 
 @pytest.mark.parametrize('std,locale', [
-    [MockEncoding('ascii'), lambda: 'ascii'],
-    [MockEncoding('ascii'), lambda: raise_or_return(Exception)],
-    [MockEncoding(AttributeError), lambda: 'ascii'],
-    [MockEncoding(AttributeError), lambda: raise_or_return(Exception)],
-    [MockEncoding(IOError), lambda: 'ascii'],
-    [MockEncoding(IOError), lambda: raise_or_return(Exception)]
+    ['ascii', 'ascii'],
+    ['ascii', Exception],
+    [AttributeError, 'ascii'],
+    [AttributeError, Exception],
+    [IOError, 'ascii'],
+    [IOError, Exception]
 ])
 def test_detect_console_encoding_fallback_to_default(monkeypatch, std, locale):
     # When both the stdout/stdin encoding and locale preferred encoding checks
     # fail (or return 'ascii', we should default to the sys default encoding.
     # GH 21552
     with monkeypatch.context() as context:
-        context.setattr('locale.getpreferredencoding', locale)
-        context.setattr('sys.stdout', std)
+        context.setattr(
+            'locale.getpreferredencoding',
+            lambda: MockEncoding.raise_or_return(locale)
+        )
+        context.setattr('sys.stdout', MockEncoding(std))
         context.setattr('sys.getdefaultencoding', lambda: 'sysDefaultEncoding')
         assert detect_console_encoding() == 'sysDefaultEncoding'
