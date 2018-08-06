b9259b8561933530b1e56b11897a9edf7a43a11a
Jeff Reback <jeff@reback.net>
2018-08-06 06:32:16 -0400

    rebase

--- pandas/tests/frame/test_asof.py
+++ pandas/tests/frame/test_asof.py
@@ -108,8 +108,6 @@ class TestFrameAsof(TestData):
         expected = Series(np.nan, index=['A', 'B'], name=3)
         tm.assert_series_equal(result, expected)
 
-    # Testing awareness of DataFrame index considering different
-    # UTC and timezone
     @pytest.mark.parametrize(
         "stamp,expected",
         [(Timestamp('2018-01-01 23:22:43.325+00:00'),
@@ -120,6 +118,8 @@ class TestFrameAsof(TestData):
     )
     def test_time_zone_aware_index(self, stamp, expected):
         # GH21194
+        # Testing awareness of DataFrame index considering different
+        # UTC and timezone
         df = DataFrame(data=[1, 2],
                        index=[Timestamp('2018-01-01 21:00:05.001+00:00'),
                               Timestamp('2018-01-01 22:35:10.550+00:00')])
