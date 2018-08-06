f395314e72c41bd72b841578fdcb4c8cbb99bca5
Cecilia Isven <cecilia.isven@gmail.com>
2018-08-06 17:54:48 +0100

    Change series definition structure

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -2801,7 +2801,7 @@ class StringMethods(NoNewAttributesMixin):
         return self._wrap_result(result)
 
     _shared_docs['len'] = ("""
-    Compute the length of each element in the Series/Index. The element may be
+    Computes the length of each element in the Series/Index. The element may be
     a sequence (such as a string, tuple or list) or a collection
     (such as a dictionary).
 
@@ -2818,10 +2818,15 @@ class StringMethods(NoNewAttributesMixin):
 
     Examples
     --------
-    Returning the length (number of characters) in a string. Returning the
+    Returns the length (number of characters) in a string. Returns the
     number of entries for dictionaries, lists or tuples.
 
-    >>> s = pd.Series(['dog', '', 5, {'foo' : 'bar'}, [2, 3, 5, 7], ('one', 'two', 'three')])
+    >>> s = pd.Series(['dog',
+    ...                 '',
+    ...                 5,
+    ...                 {'foo' : 'bar'},
+    ...                 [2, 3, 5, 7],
+    ...                 ('one', 'two', 'three')])
     >>> s
     0                  dog
     1
