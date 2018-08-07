f22bbe9710f419402bc86e246dcb10bc91e4eb4f
Brock Mendel <jbrockmendel@gmail.com>
2018-08-07 13:51:54 -0700

    de-duplicate helper.h

--- pandas/_libs/src/skiplist.h
+++ pandas/_libs/src/skiplist.h
@@ -20,18 +20,7 @@ Python recipe (http://rhettinger.wordpress.com/2010/02/06/lost-knowledge/)
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-
-#ifndef PANDAS_INLINE
-#if defined(__GNUC__)
-#define PANDAS_INLINE static __inline__
-#elif defined(_MSC_VER)
-#define PANDAS_INLINE static __inline
-#elif defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L
-#define PANDAS_INLINE static inline
-#else
-#define PANDAS_INLINE
-#endif
-#endif
+#include "helper.h"
 
 PANDAS_INLINE float __skiplist_nanf(void) {
     const union {
