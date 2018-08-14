f3ddae0111c9b504fa15787193b03072c68b098a
Diego Argueta <diego@goodrx.com>
2018-08-14 12:16:36 -0700

    Test bugfixes and PR feedback.

--- pandas/tests/io/test_parquet.py
+++ pandas/tests/io/test_parquet.py
@@ -373,34 +373,34 @@ class TestBasic(Base):
         # Ensure index=False omits the index from the written Parquet file.
         df = pd.DataFrame({'a': [1, 2, 3], 'b': ['q', 'r', 's']})
 
+        write_kwargs = {
+            'compression': None,
+            'index': False,
+        }
+
         # Because we're dropping the index, we expect the loaded dataframe to
         # have the default integer index.
         expected = df.reset_index(drop=True)
 
-        check_round_trip(df, engine, write_kwargs={'index': False},
+        check_round_trip(df, engine, write_kwargs=write_kwargs,
                          expected=expected)
 
-    def test_write_ignoring_custom_index(self, engine):
-        # ENH 20768
-        # Ensure index=False omits the index from the written Parquet file,
-        # even if we're using a custom one.s
+        # Ignore custom index
         df = pd.DataFrame({'a': [1, 2, 3], 'b': ['q', 'r', 's']},
                           index=['zyx', 'wvu', 'tsr'])
 
         expected = df.reset_index(drop=True)
-        check_round_trip(df, engine, write_kwargs={'index': False},
+        check_round_trip(df, engine, write_kwargs=write_kwargs,
                          expected=expected)
 
-    def test_write_ignoring_multiindex(self, engine):
-        # ENH 20768
-        # Ensure index=False omits multiindexes as well.
+        # Ignore multi-indexes as well.
         arrays = [['bar', 'bar', 'baz', 'baz', 'foo', 'foo', 'qux', 'qux'],
                   ['one', 'two', 'one', 'two', 'one', 'two', 'one', 'two']]
         df = pd.DataFrame({'one': [i for i in range(8)],
                            'two': [-i for i in range(8)]}, index=arrays)
 
         expected = df.reset_index(drop=True)
-        check_round_trip(df, engine, write_kwargs={'index': False},
+        check_round_trip(df, engine, write_kwargs=write_kwargs,
                          expected=expected)
 
 
