d3d5a50d62197bf2908e81a936ac48440772bc43
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-08-16 12:18:40 +0200

    clean-up

--- ci/doctests.sh
+++ ci/doctests.sh
@@ -12,32 +12,14 @@ if [ "$DOCTEST" ]; then
 
     echo "Running doctests"
 
-    # pytest --doctest-modules --ignore=pandas/tests -v  pandas/core/frame.py
+    # running all doctests is not yet working
+    # pytest --doctest-modules --ignore=pandas/tests -v  pandas
 
     # if [ $? -ne "0" ]; then
     #     RET=1
     # fi
 
-
-    # # top-level reshaping functions
-    # pytest --doctest-modules -v \
-    #     pandas/core/reshape/concat.py \
-    #     pandas/core/reshape/pivot.py \
-    #     pandas/core/reshape/reshape.py \
-    #     pandas/core/reshape/tile.py
-
-    # if [ $? -ne "0" ]; then
-    #     RET=1
-    # fi
-
-    # # top-level io functionality
-    # pytest --doctest-modules -v pandas/io/* -k "read_"
-
-    # if [ $? -ne "0" ]; then
-    #     RET=1
-    # fi
-
-    # DataFrame docstrings
+    # DataFrame / Series docstrings
     pytest --doctest-modules -v pandas/core/frame.py \
         -k"-assign -axes -combine -isin -itertuples -join -nlargest -nsmallest -nunique -pivot_table -quantile -query -reindex -reindex_axis -replace -round -set_index -stack -to_dict -to_records -to_stata -transform"
 
@@ -45,7 +27,6 @@ if [ "$DOCTEST" ]; then
         RET=1
     fi
 
-    # Series docstrings
     pytest --doctest-modules -v pandas/core/series.py \
         -k"-agg -map -nlargest -nonzero -nsmallest -reindex -searchsorted -to_dict"
 
@@ -55,6 +36,18 @@ if [ "$DOCTEST" ]; then
 
     pytest --doctest-modules -v pandas/core/generic.py \
         -k"-_set_axis_name -_xs -droplevel -groupby -interpolate -pct_change -pipe -reindex -reindex_axis -resample -sample -to_json -to_xarray -transform -transpose -values -xs"
+
+    if [ $? -ne "0" ]; then
+        RET=1
+    fi
+
+    # top-level reshaping functions
+    pytest --doctest-modules -v \
+        pandas/core/reshape/concat.py \
+        pandas/core/reshape/pivot.py \
+        pandas/core/reshape/reshape.py \
+        pandas/core/reshape/tile.py \
+        -k"-crosstab -pivot_table -cut"
 
     if [ $? -ne "0" ]; then
         RET=1
