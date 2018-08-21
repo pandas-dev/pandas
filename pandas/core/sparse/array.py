2213b8397b5962d6fc0d20863658973fde82564c
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-08-20 21:36:43 -0500

    Fixed pickle

--- pandas/core/sparse/array.py
+++ pandas/core/sparse/array.py
@@ -875,13 +875,13 @@ class SparseArray(PandasObject, ExtensionArray, ExtensionOpsMixin):
         """Necessary for making this object picklable"""
         if isinstance(state, tuple):
             # Compat for pandas < 0.24.0
-            nd_state, own_state = state
+            nd_state, (fill_value, sp_index) = state
             sparse_values = np.array([])
             sparse_values.__setstate__(nd_state)
 
             self._sparse_values = sparse_values
-            self.fill_value, self._sparse_index = own_state[:2]
-            self._dtype = SparseDtype(sparse_values.dtype)
+            self._sparse_index = sp_index
+            self._dtype = SparseDtype(sparse_values.dtype, fill_value)
         else:
             self.__dict__.update(state)
 
