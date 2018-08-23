ca8f101ad7b51c512b19928d825ddedf35f52fe4
peterpanmj <lixx0880@gmail.com>
2018-08-23 12:57:11 +0800

    BUG: series of extension array add extension array values cause runtime exception #22478

--- pandas/core/ops.py
+++ pandas/core/ops.py
@@ -1182,7 +1182,7 @@ def dispatch_to_extension_op(op, left, right):
                 new_right = [new_right]
             new_right = list(new_right)
         elif is_extension_array_dtype(right) and type(left) != type(right):
-            new_right = list(new_right)
+            new_right = list(right)
         else:
             new_right = right
 
