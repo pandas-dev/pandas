9e99d90b60e70a39e912d1793572a4c053f28103
Alex Rychyk <odiscort@gmail.com>
2018-08-29 15:32:42 +0300

    TEST: added test for agg with list-like func (#22467)

--- pandas/tests/groupby/aggregate/test_other.py
+++ pandas/tests/groupby/aggregate/test_other.py
@@ -512,3 +512,14 @@ def test_agg_category_nansum(observed):
     if observed:
         expected = expected[expected != 0]
     tm.assert_series_equal(result, expected)
+
+
+def test_agg_list_like_func():
+    # GH 18473
+    df = pd.DataFrame({'A': [str(x) for x in range(3)],
+                       'B': [str(x) for x in range(3)]})
+    grouped = df.groupby('A', as_index=False, sort=False)
+    result = grouped.agg({'B': lambda x: list(x)})
+    expected = pd.DataFrame({'A': [str(x) for x in range(3)],
+                             'B': [[str(x)] for x in range(3)]})
+    tm.assert_frame_equal(result, expected)
