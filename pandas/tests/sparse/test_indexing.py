73e76262808d292500822c6fee2d0b9db75ee4e6
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-08-30 22:31:18 -0500

    32-bit compat

--- pandas/tests/sparse/test_indexing.py
+++ pandas/tests/sparse/test_indexing.py
@@ -972,7 +972,7 @@ class TestSparseDataFrameIndexing(object):
                              [0, 0, 0],
                              [0, 0, 0]],
                             index=list('ABCD'), columns=list('xyz'),
-                            dtype=np.intp)
+                            dtype=np.int)
         sparse = orig.to_sparse(fill_value=0)
 
         res = sparse.reindex(['A', 'C', 'B'])
