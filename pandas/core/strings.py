3f92a6941919ad33f618dfab81dd2183edbc00e0
Luca Donini <lucadonini96@gmail.com>
2018-09-01 14:05:27 +0200

    Corrected the formatting in the return value and added a period at the end of the parameter descriptions. Can also clarify descriptions if useful.

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -942,19 +942,20 @@ def str_extractall(arr, pat, flags=0):
 
     Parameters
     ----------
-    pat : string
-        Regular expression pattern with capturing groups
+    pat : str
+        Regular expression pattern with capturing groups.
     flags : int, default 0 (no flags)
-        re module flags, e.g. re.IGNORECASE
+        re module flags, e.g. re.IGNORECASE.
 
     Returns
     -------
-    A DataFrame with one row for each match, and one column for each
-    group. Its rows have a MultiIndex with first levels that come from
-    the subject Series. The last level is named 'match' and indicates
-    the order in the subject. Any capture group names in regular
-    expression pat will be used for column names; otherwise capture
-    group numbers will be used.
+    DataFrame
+        A DataFrame with one row for each match, and one column for each
+        group. Its rows have a MultiIndex with first levels that come from
+        the subject Series. The last level is named 'match' and indexes the
+        matches in each item of the Series. Any capture group names in regular
+        expression pat will be used for column names; otherwise capture
+        group numbers will be used.
 
     See Also
     --------
