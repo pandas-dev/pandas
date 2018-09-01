c96440708fc3ef176cc014f1a16a0d4c7e2e31b2
Luca Donini <lucadonini96@gmail.com>
2018-09-01 16:15:38 +0200

    pep8

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -946,8 +946,8 @@ def str_extractall(arr, pat, flags=0):
         Regular expression pattern with capturing groups.
     flags : int, default 0 (no flags)
         A ``re`` module flag, for example ``re.IGNORECASE``. These allow
-        to modify regular expression matching for things like case, spaces, etc.
-        Multiple flags can be combined with the bitwise OR operator,
+        to modify regular expression matching for things like case, spaces,
+        etc. Multiple flags can be combined with the bitwise OR operator,
         for example ``re.IGNORECASE | re.MULTILINE``.
 
     Returns
@@ -956,8 +956,8 @@ def str_extractall(arr, pat, flags=0):
         A ``DataFrame`` with one row for each match, and one column for each
         group. Its rows have a ``MultiIndex`` with first levels that come from
         the subject ``Series``. The last level is named 'match' and indexes the
-        matches in each item of the ``Series``. Any capture group names in regular
-        expression pat will be used for column names; otherwise capture
+        matches in each item of the ``Series``. Any capture group names in
+        regular expression pat will be used for column names; otherwise capture
         group numbers will be used.
 
     See Also
