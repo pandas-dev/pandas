a725108eefc584b03c4c556cb902054209b95617
Simon Hawkins <simonjayhawkins@gmail.com>
2018-09-10 17:51:29 +0100

    move regression test

--- pandas/tests/io/formats/test_to_html.py
+++ pandas/tests/io/formats/test_to_html.py
@@ -2243,12 +2243,51 @@ class TestToHTML(object):
         assert result == expected
 
     def test_to_html_index_name_multi_index_both(self):
-        # multi_index = MultiIndex.from_product([['a'], ['b', 'c']])
+        index_multi_index = MultiIndex.from_product(
+            [['a'], ['b', 'c']], names=['index.name.0', 'index.name.1'])
+        columns_multi_index = MultiIndex.from_product(
+            [['a'], ['b', 'c']], names=['columns.name.0', 'columns.name.1'])
+        df = DataFrame(np.zeros((2, 2), dtype=int),
+                       index=index_multi_index, columns=columns_multi_index)
+        result = df.to_html()
+        expected = dedent("""\
+        <table border="1" class="dataframe">
+          <thead>
+            <tr>
+              <th></th>
+              <th>columns.name.0</th>
+              <th colspan="2" halign="left">a</th>
+            </tr>
+            <tr>
+              <th></th>
+              <th>columns.name.1</th>
+              <th>b</th>
+              <th>c</th>
+            </tr>
+            <tr>
+              <th>index.name.0</th>
+              <th>index.name.1</th>
+              <th></th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th rowspan="2" valign="top">a</th>
+              <th>b</th>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+            <tr>
+              <th>c</th>
+              <td>0</td>
+              <td>0</td>
+            </tr>
+          </tbody>
+        </table>""")
+        assert result == expected
 
-        # df = DataFrame(np.zeros((2, 2), dtype=int), multi_index, multi_index)
-        # df.index.name = 'index.name'
-        # df.columns.name = 'columns.name'
-        # result = df.to_html()
+        # multi_index = MultiIndex.from_product([['a'], ['b', 'c']])
 
         # df = DataFrame(np.zeros((2, 2), dtype=int), multi_index, multi_index)
         # df.columns.name = 'columns.name'
@@ -2406,51 +2445,6 @@ class TestToHTML(object):
               <th>columns.name</th>
               <th>0</th>
               <th>1</th>
-            </tr>
-            <tr>
-              <th>index.name.0</th>
-              <th>index.name.1</th>
-              <th></th>
-              <th></th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th rowspan="2" valign="top">a</th>
-              <th>b</th>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-            <tr>
-              <th>c</th>
-              <td>0</td>
-              <td>0</td>
-            </tr>
-          </tbody>
-        </table>""")
-        assert result == expected
-
-    def test_to_html_index_names_multi_index_both(self):
-        index_multi_index = MultiIndex.from_product(
-            [['a'], ['b', 'c']], names=['index.name.0', 'index.name.1'])
-        columns_multi_index = MultiIndex.from_product(
-            [['a'], ['b', 'c']], names=['columns.name.0', 'columns.name.1'])
-        df = DataFrame(np.zeros((2, 2), dtype=int),
-                       index=index_multi_index, columns=columns_multi_index)
-        result = df.to_html()
-        expected = dedent("""\
-        <table border="1" class="dataframe">
-          <thead>
-            <tr>
-              <th></th>
-              <th>columns.name.0</th>
-              <th colspan="2" halign="left">a</th>
-            </tr>
-            <tr>
-              <th></th>
-              <th>columns.name.1</th>
-              <th>b</th>
-              <th>c</th>
             </tr>
             <tr>
               <th>index.name.0</th>
