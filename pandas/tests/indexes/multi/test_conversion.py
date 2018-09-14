ad713388d9f0f873af7849203652b39326e17c00
henriqueribeiro <henriqueribeiro@users.noreply.github.com>
2018-09-14 05:45:16 +0100

    API: Add 'name' as argument for index 'to_frame' method (#22580)

--- pandas/tests/indexes/multi/test_conversion.py
+++ pandas/tests/indexes/multi/test_conversion.py
@@ -37,6 +37,27 @@ def test_to_frame():
     expected.index = index
     tm.assert_frame_equal(result, expected)
 
+    # See GH-22580
+    index = MultiIndex.from_tuples(tuples)
+    result = index.to_frame(index=False, name=['first', 'second'])
+    expected = DataFrame(tuples)
+    expected.columns = ['first', 'second']
+    tm.assert_frame_equal(result, expected)
+
+    result = index.to_frame(name=['first', 'second'])
+    expected.index = index
+    expected.columns = ['first', 'second']
+    tm.assert_frame_equal(result, expected)
+
+    msg = "'name' must be a list / sequence of column names."
+    with tm.assert_raises_regex(TypeError, msg):
+        index.to_frame(name='first')
+
+    msg = "'name' should have same length as number of levels on index."
+    with tm.assert_raises_regex(ValueError, msg):
+        index.to_frame(name=['first'])
+
+    # Tests for datetime index
     index = MultiIndex.from_product([range(5),
                                      pd.date_range('20130101', periods=3)])
     result = index.to_frame(index=False)
@@ -45,9 +66,18 @@ def test_to_frame():
             1: np.tile(pd.date_range('20130101', periods=3), 5)})
     tm.assert_frame_equal(result, expected)
 
-    index = MultiIndex.from_product([range(5),
-                                     pd.date_range('20130101', periods=3)])
     result = index.to_frame()
+    expected.index = index
+    tm.assert_frame_equal(result, expected)
+
+    # See GH-22580
+    result = index.to_frame(index=False, name=['first', 'second'])
+    expected = DataFrame(
+        {'first': np.repeat(np.arange(5, dtype='int64'), 3),
+         'second': np.tile(pd.date_range('20130101', periods=3), 5)})
+    tm.assert_frame_equal(result, expected)
+
+    result = index.to_frame(name=['first', 'second'])
     expected.index = index
     tm.assert_frame_equal(result, expected)
 
