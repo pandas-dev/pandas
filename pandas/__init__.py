a6e2d6425140cbe58965a514887c87cccbd4b513
alimcmaster1 <alimcmaster1@gmail.com>
2018-09-18 00:31:22 +0100

    Add git version attr

--- pandas/__init__.py
+++ pandas/__init__.py
@@ -83,6 +83,7 @@ tslib = _DeprecatedModule(deprmod='pandas.tslib',
 from ._version import get_versions
 v = get_versions()
 __version__ = v.get('closest-tag', v['version'])
+__git_version__ = v.get('full-revisionid')
 del get_versions, v
 
 # module level doc-string
