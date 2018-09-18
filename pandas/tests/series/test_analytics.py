3678a62e75bc6b83265195450f4f1283c732c605
Anjali Singhal <anjali.singhal@booking.com>
2018-09-19 01:23:38 +0200

    Fixing PEP8 issues

--- pandas/tests/series/test_analytics.py
+++ pandas/tests/series/test_analytics.py
@@ -395,8 +395,8 @@ class TestSeriesAnalytics():
 
     def test_cummin(self, datetime_series):
         tm.assert_numpy_array_equal(datetime_series.cummin().values,
-                                    np.minimum.accumulate(
-                                                          np.array(datetime_series)))
+                                    np.minimum
+                                    .accumulate(np.array(datetime_series)))
         ts = datetime_series.copy()
         ts[::2] = np.NaN
         result = ts.cummin()[1::2]
@@ -406,8 +406,8 @@ class TestSeriesAnalytics():
 
     def test_cummax(self, datetime_series):
         tm.assert_numpy_array_equal(datetime_series.cummax().values,
-                                    np.maximum.accumulate(
-                                                          np.array(datetime_series)))
+                                    np.maximum
+                                    .accumulate(np.array(datetime_series)))
         ts = datetime_series.copy()
         ts[::2] = np.NaN
         result = ts.cummax()[1::2]
