b65bb539c30a59bdf764dcb2b663dd36bf57f878
Simon Hawkins <simonjayhawkins@gmail.com>
2018-09-19 23:18:34 +0100

    remove unapproved tests

--- pandas/tests/io/formats/test_to_html.py
+++ pandas/tests/io/formats/test_to_html.py
@@ -12,7 +12,6 @@ from pandas import compat, DataFrame, MultiIndex, option_context, Index
 from pandas.compat import u, lrange, StringIO
 from pandas.util import testing as tm
 import pandas.io.formats.format as fmt
-import pandas.core.common as com
 
 div_style = ''
 try:
@@ -21,113 +20,6 @@ try:
         div_style = ' style="max-width:1500px;overflow:auto;"'
 except (ImportError, AttributeError):
     pass
-
-
-def _has_names(index):
-    if index.nlevels > 1:
-        return com._any_not_none(*index.names)
-    else:
-        return index.name is not None
-
-
-def _index_init_params(index):
-    return (index.nlevels > 1,
-            _has_names(index))
-
-
-@pytest.fixture
-def expected_html(read_file):
-    def _expected_html(name):
-        filename = '.'.join([name, 'html'])
-        html = read_file(filename)
-        return html.rstrip()
-    return _expected_html
-
-
-@pytest.fixture(params=[True, False], ids=['index_true', 'index_false'])
-def index(request):
-    # to_html() parameter values for index
-    # whether to print index (row) labels, default True
-    return request.param
-
-
-@pytest.fixture(params=[True, False], ids=['header_true', 'header_false'])
-def header(request):
-    # to_html() parameter values for header
-    # whether to print column labels, default True
-    return request.param
-
-
-@pytest.fixture(params=[True, False], ids=['index_names_true',
-                                           'index_names_false'])
-def index_names(request):
-    # to_html() parameter values for index_names
-    # prints the names of the indexes, default True
-    return request.param
-
-
-@pytest.fixture(params=[
-    Index([0, 1]),
-    Index([0, 1], name='index.name'),
-    MultiIndex.from_product([['a'], ['b', 'c']]),
-    MultiIndex.from_product([['a'], ['b', 'c']], names=[
-        'index.name.0', 'index.name.1'])
-],
-    ids=['index_unnamed_standard',
-         'index_named_standard',
-         'index_unnamed_multi',
-         'index_named_multi'])
-def idx_type(request):
-    # standard and multiIndex index, named and unnamed
-    # used for basic table alignment tests
-    return request.param
-
-
-@pytest.fixture(
-    params=[
-        Index([0, 1]),
-        Index([0, 1], name='columns.name'),
-        MultiIndex.from_product([['a'], ['b', 'c']]),
-        MultiIndex.from_product([['a'], ['b', 'c']], names=[
-            'columns.name.0', 'columns.name.1'])
-    ],
-    ids=['columns_unnamed_standard',
-         'columns_named_standard',
-         'columns_unnamed_multi',
-         'columns_named_multi'])
-def col_idx_type(request):
-    # standard and multiIndex columns index, named and unnamed
-    # used for basic table alignment tests
-    return request.param
-
-
-@pytest.fixture
-def expected_output(expected_html):
-    def _expected_output(idx_type, col_idx_type,
-                         index=True, header=True, index_names=True):
-
-        def _expected_output_name(idx_type, index, index_names):
-            is_multi, is_named = _index_init_params(idx_type)
-            if index is False:
-                return 'none'
-
-            idx_type_ids = {
-                (False, False): 'unnamed_standard',
-                (True, False): 'named_standard',
-                (False, True): 'unnamed_multi',
-                (True, True): 'named_multi'}
-
-            return idx_type_ids[(is_named and index_names, is_multi)]
-
-        return expected_html(
-            '_'.join([
-                'index',
-                _expected_output_name(idx_type, index, index_names),
-                'columns',
-                _expected_output_name(col_idx_type, header, index_names)
-            ])
-        )
-    return _expected_output
 
 
 class TestToHTML(object):
@@ -2012,20 +1904,6 @@ class TestToHTML(object):
           </tbody>
         </table>""")
         assert result == expected
-
-    @pytest.mark.parametrize('header', [True])
-    @pytest.mark.parametrize('index', [True])
-    @pytest.mark.parametrize('index_names', [True])
-    def test_to_html_index_names(self, expected_output,
-                                 idx_type, col_idx_type, index, header,
-                                 index_names):
-        df = DataFrame(np.zeros((2, 2), dtype=int),
-                       index=idx_type, columns=col_idx_type)
-        result = df.to_html(index=index, header=header,
-                            index_names=index_names)
-        assert result == expected_output(idx_type, col_idx_type,
-                                         index=index, header=header,
-                                         index_names=index_names)
 
     def test_to_html_notebook_has_style(self):
         df = pd.DataFrame({"A": [1, 2, 3]})
