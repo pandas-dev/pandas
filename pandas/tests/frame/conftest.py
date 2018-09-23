77bcbcee65076f5a7189fdc77cc908673616c81a
H. Vetinari <h.vetinari@gmx.com>
2018-09-23 15:24:36 +0200

    Review (jreback)

--- pandas/tests/frame/conftest.py
+++ pandas/tests/frame/conftest.py
@@ -41,18 +41,6 @@ def int_frame():
 
 
 @pytest.fixture
-def int_frame_const_col():
-    """
-    Fixture for DataFrame of ints which are constant per column
-
-    Columns are ['A', 'B', 'C'], with values (per column): [1, 2, 3]
-    """
-    df = DataFrame(np.tile(np.arange(3, dtype='int64'), 6).reshape(6, -1) + 1,
-                   columns=['A', 'B', 'C'])
-    return df
-
-
-@pytest.fixture
 def datetime_frame():
     """
     Fixture for DataFrame of floats with DatetimeIndex
--- pandas/tests/frame/test_apply.py
+++ pandas/tests/frame/test_apply.py
@@ -25,6 +25,18 @@ import pandas.util.testing as tm
 from pandas.conftest import _get_cython_table_params
 
 
+@pytest.fixture
+def int_frame_const_col():
+    """
+    Fixture for DataFrame of ints which are constant per column
+
+    Columns are ['A', 'B', 'C'], with values (per column): [1, 2, 3]
+    """
+    df = DataFrame(np.tile(np.arange(3, dtype='int64'), 6).reshape(6, -1) + 1,
+                   columns=['A', 'B', 'C'])
+    return df
+
+
 class TestDataFrameApply():
 
     def test_apply(self, float_frame):
