988c40c9a44f5a951aa69318abc4c7a2a54e93bb
JustinZhengBC <justinzhengbc@gmail.com>
2018-09-30 09:35:15 -0700

    DOC GH22897 Fix other formatting issues in pandas/core/frame.py

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -6440,7 +6440,7 @@ class DataFrame(NDFrame):
     def join(self, other, on=None, how='left', lsuffix='', rsuffix='',
              sort=False):
         """
-        Append columns of another DataFrame.
+        Join columns of another DataFrame.
 
         Join columns with other DataFrame either on index or on a key
         column. Efficiently Join multiple DataFrame objects by index at once by
@@ -6448,11 +6448,11 @@ class DataFrame(NDFrame):
 
         Parameters
         ----------
-        other : DataFrame, Series with name field set, or list of DataFrame
+        other : DataFrame, Series, or list of DataFrame
             Index should be similar to one of the columns in this one. If a
             Series is passed, its name attribute must be set, and that will be
             used as the column name in the resulting joined DataFrame.
-        on : name, tuple/list of names, or array-like
+        on : str, list of str, or array-like
             Column or index level name(s) in the caller to join on the index
             in `other`, otherwise joins index-on-index. If multiple
             values given, the `other` DataFrame must have a MultiIndex. Can
@@ -6469,11 +6469,11 @@ class DataFrame(NDFrame):
             * inner: form intersection of calling frame's index (or column if
               on is specified) with other frame's index, preserving the order
               of the calling's one.
-        lsuffix : string
+        lsuffix : str
             Suffix to use from left frame's overlapping columns.
-        rsuffix : string
+        rsuffix : str
             Suffix to use from right frame's overlapping columns.
-        sort : boolean, default False
+        sort : bool, default False
             Order result DataFrame lexicographically by the join key. If False,
             the order of the join key depends on the join type (how keyword).
 
@@ -6485,14 +6485,17 @@ class DataFrame(NDFrame):
         Support for specifying index levels as the `on` parameter was added
         in version 0.23.0
 
+        See Also
+        --------
+        DataFrame.merge : For column(s)-on-columns(s) operations.
+
         Examples
         --------
-        >>> import pandas as pd
 
-        >>> caller = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K4', 'K5'],
+        >>> df = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K4', 'K5'],
         ...                        'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})
 
-        >>> caller
+        >>> df
           key   A
         0  K0  A0
         1  K1  A1
@@ -6512,7 +6515,7 @@ class DataFrame(NDFrame):
 
         Join DataFrames using their indexes.
 
-        >>> caller.join(other, lsuffix='_caller', rsuffix='_other')
+        >>> df.join(other, lsuffix='_caller', rsuffix='_other')
           key_caller   A key_other    B
         0         K0  A0        K0   B0
         1         K1  A1        K1   B1
@@ -6522,10 +6525,10 @@ class DataFrame(NDFrame):
         5         K5  A5       NaN  NaN
 
         If we want to join using the key columns, we need to set key to be
-        the index in both caller and other. The joined DataFrame will have
+        the index in both df and other. The joined DataFrame will have
         key as its index.
 
-        >>> caller.set_index('key').join(other.set_index('key'))
+        >>> df.set_index('key').join(other.set_index('key'))
               A    B
         key
         K0   A0   B0
@@ -6537,10 +6540,10 @@ class DataFrame(NDFrame):
 
         Another option to join using the key columns is to use the on
         parameter. DataFrame.join always uses other's index but we can use any
-        column in the caller. This method preserves the original caller's
+        column in df. This method preserves the original DataFrame's
         index in the result.
 
-        >>> caller.join(other.set_index('key'), on='key')
+        >>> df.join(other.set_index('key'), on='key')
           key   A    B
         0  K0  A0   B0
         1  K1  A1   B1
@@ -6548,10 +6551,6 @@ class DataFrame(NDFrame):
         3  K3  A3  NaN
         4  K4  A4  NaN
         5  K5  A5  NaN
-
-        See also
-        --------
-        DataFrame.merge : For column(s)-on-columns(s) operations
 
         Returns
         -------
