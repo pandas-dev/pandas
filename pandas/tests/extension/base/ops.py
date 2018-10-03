ba13d90e30f4c0e714a73fe254d1da04d6504ee1
Wenhuan <lixx0880@gmail.com>
2018-10-03 15:28:07 +0800

    BUG: fix Series(extension array) + extension array values addition (#22479)

--- pandas/tests/extension/base/ops.py
+++ pandas/tests/extension/base/ops.py
@@ -77,6 +77,12 @@ class BaseArithmeticOpsTests(BaseOpsUtil):
         self._check_divmod_op(s, divmod, 1, exc=TypeError)
         self._check_divmod_op(1, ops.rdivmod, s, exc=TypeError)
 
+    def test_add_series_with_extension_array(self, data):
+        s = pd.Series(data)
+        result = s + data
+        expected = pd.Series(data + data)
+        self.assert_series_equal(result, expected)
+
     def test_error(self, data, all_arithmetic_operators):
         # invalid ops
         op_name = all_arithmetic_operators
