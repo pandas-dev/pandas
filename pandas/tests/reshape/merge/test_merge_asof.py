322f965a43e312bc0a8df5c2fa46c7d0ad89f4d4
Sven <svenharris@hotmail.co.uk>
2018-10-03 20:04:57 +0100

    BUG: GH22981 allow tolerance in merge_asof with float type

--- pandas/tests/reshape/merge/test_merge_asof.py
+++ pandas/tests/reshape/merge/test_merge_asof.py
@@ -642,6 +642,20 @@ class TestAsOfMerge(object):
              'value2': list("BCDEE")})
         assert_frame_equal(result, expected)
 
+    def test_tolerance_float(self):
+        left = pd.DataFrame({'a': [1.1, 3.5, 10.9],
+                             'left_val': ['a', 'b', 'c']})
+        right = pd.DataFrame({'a': [1.0, 2.5, 3.3, 7.5, 11.5],
+                              'right_val': [1.0, 2.5, 3.3, 7.5, 11.5]})
+
+        expected = pd.DataFrame({'a': [1.1, 3.5, 10.9],
+                                 'left_val': ['a', 'b', 'c'],
+                                 'right_val': [1, 3.3, np.nan]})
+
+        result = pd.merge_asof(left, right, on='a', direction='nearest',
+                               tolerance=0.5)
+        assert_frame_equal(result, expected)
+
     def test_index_tolerance(self):
         # GH 15135
         expected = self.tolerance.set_index('time')
