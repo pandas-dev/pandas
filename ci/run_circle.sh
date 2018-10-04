0c255787fdd4fbe7811bad03a50a2048c1a0d52e
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-10-04 09:43:26 -0500

    circle -> azure

--- ci/run_circle.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-echo "[running tests]"
-export PATH="$MINICONDA_DIR/bin:$PATH"
-
-source activate pandas
-
-echo "pytest --strict --junitxml=$CIRCLE_TEST_REPORTS/reports/junit.xml $@ pandas"
-pytest --strict --color=no --junitxml=$CIRCLE_TEST_REPORTS/reports/junit.xml $@ pandas
