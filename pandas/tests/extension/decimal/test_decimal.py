ec814db9acea380be0b9e82474e7365bf2a20536
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-10-05 06:51:09 -0500

    Move

--- pandas/tests/extension/decimal/test_decimal.py
+++ pandas/tests/extension/decimal/test_decimal.py
@@ -240,23 +240,6 @@ class TestArithmeticOps(BaseDecimal, base.BaseArithmeticOpsTests):
         context.traps[decimal.DivisionByZero] = divbyzerotrap
         context.traps[decimal.InvalidOperation] = invalidoptrap
 
-    @pytest.mark.parametrize("reverse, expected_div, expected_mod", [
-        (False, [0, 1, 1, 2], [1, 0, 1, 0]),
-        (True, [2, 1, 0, 0], [0, 0, 2, 2]),
-    ])
-    def test_divmod_array(self, reverse, expected_div, expected_mod):
-        # https://github.com/pandas-dev/pandas/issues/22930
-        arr = to_decimal([1, 2, 3, 4])
-        if reverse:
-            div, mod = divmod(2, arr)
-        else:
-            div, mod = divmod(arr, 2)
-        expected_div = to_decimal(expected_div)
-        expected_mod = to_decimal(expected_mod)
-
-        tm.assert_extension_array_equal(div, expected_div)
-        tm.assert_extension_array_equal(mod, expected_mod)
-
     def _check_divmod_op(self, s, op, other, exc=NotImplementedError):
         # We implement divmod
         super(TestArithmeticOps, self)._check_divmod_op(
@@ -334,3 +317,21 @@ def test_scalar_ops_from_sequence_raises(class_):
     expected = np.array([decimal.Decimal("2.0"), decimal.Decimal("4.0")],
                         dtype="object")
     tm.assert_numpy_array_equal(result, expected)
+
+
+@pytest.mark.parametrize("reverse, expected_div, expected_mod", [
+    (False, [0, 1, 1, 2], [1, 0, 1, 0]),
+    (True, [2, 1, 0, 0], [0, 0, 2, 2]),
+])
+def test_divmod_array(reverse, expected_div, expected_mod):
+    # https://github.com/pandas-dev/pandas/issues/22930
+    arr = to_decimal([1, 2, 3, 4])
+    if reverse:
+        div, mod = divmod(2, arr)
+    else:
+        div, mod = divmod(arr, 2)
+    expected_div = to_decimal(expected_div)
+    expected_mod = to_decimal(expected_mod)
+
+    tm.assert_extension_array_equal(div, expected_div)
+    tm.assert_extension_array_equal(mod, expected_mod)
