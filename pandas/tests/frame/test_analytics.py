5530b9a4428f87a8dee15e3c55fd1fa4156f35e0
Kristian Holsheimer <krholshe@microsoft.com>
2018-10-11 11:52:39 +1100

    Updated test: Added github issue id and used 'tm' instead of 'pd.util.testing'

--- pandas/tests/frame/test_analytics.py
+++ pandas/tests/frame/test_analytics.py
@@ -2261,12 +2261,18 @@ class TestNLargestNSmallest(object):
         tm.assert_frame_equal(result, expected)
 
     def test_multiindex_column_lookup(self):
+        # Check whether tuples are correctly treated as multi-level lookups.
+        # GH 23033
         df = pd.DataFrame(
             columns=pd.MultiIndex.from_product([['x'], ['a', 'b']]),
             data=[[0.33, 0.13], [0.86, 0.25], [0.25, 0.70], [0.85, 0.91]])
-        pd.util.testing.assert_frame_equal(
-            df.nsmallest(3, ('x', 'a')),
-            df.iloc[[2, 0, 3]])
-        pd.util.testing.assert_frame_equal(
-            df.nlargest(3, ('x', 'b')),
-            df.iloc[[3, 2, 1]])
+
+        # nsmallest
+        result = df.nsmallest(3, ('x', 'a'))
+        expected = df.iloc[[2, 0, 3]]
+        tm.assert_frame_equal(result, expected)
+
+        # nlargest
+        result = df.nlargest(3, ('x', 'b'))
+        expected = df.iloc[[3, 2, 1]]
+        tm.assert_frame_equal(result, expected)
