a267d4a2e872a48986b35c771814fdf8617b0792
H. Vetinari <h.vetinari@gmx.com>
2018-10-11 21:58:33 +0200

    Review (jorisvandenbossche)

--- pandas/tests/test_algos.py
+++ pandas/tests/test_algos.py
@@ -1322,13 +1322,63 @@ class TestHashTable(object):
             s.loc[500:502] = [np.nan, None, pd.NaT]
 
         # create duplicated selection
-        s_duplicated = s.sample(frac=3, replace=True)
+        s_duplicated = s.sample(frac=3, replace=True).reset_index(drop=True)
 
         # drop_duplicates has own cython code (hash_table_func_helper.pxi)
-        #  and is tested separately; keeps first occurrence like ht.unique()
+        # and is tested separately; keeps first occurrence like ht.unique()
         expected_unique = s_duplicated.drop_duplicates(keep='first').values
         result_unique = htable().unique(s_duplicated.values)
         tm.assert_numpy_array_equal(result_unique, expected_unique)
+
+    @pytest.mark.parametrize('na_sentinel', [-1])
+    @pytest.mark.parametrize('htable, tm_dtype', [
+        (ht.PyObjectHashTable, 'String'),
+        (ht.StringHashTable, 'String'),
+        (ht.Float64HashTable, 'Float'),
+        (ht.Int64HashTable, 'Int'),
+        (ht.UInt64HashTable, 'UInt')])
+    def test_hashtable_factorize(self, htable, tm_dtype, na_sentinel):
+        # output of maker has guaranteed unique elements
+        maker = getattr(tm, 'make' + tm_dtype + 'Index')
+        s = Series(maker(1000))
+        if htable == ht.Float64HashTable:
+            # add NaN for float column
+            s.loc[500] = np.nan
+        elif htable == ht.PyObjectHashTable:
+            # use different NaN types for object column
+            s.loc[500:502] = [np.nan, None, pd.NaT]
+
+        # create duplicated selection
+        idx_duplicated = pd.Series(s.index).sample(frac=3, replace=True)
+        s_duplicated = s[idx_duplicated.values].reset_index(drop=True)
+        na_mask = s_duplicated.isna().values
+
+        result_inverse, result_unique = htable().factorize(s_duplicated.values)
+
+        # drop_duplicates has own cython code (hash_table_func_helper.pxi)
+        # and is tested separately; keeps first occurrence like ht.unique()
+        expected_unique = s_duplicated.dropna().drop_duplicates(keep='first')
+        expected_unique = expected_unique.values
+        tm.assert_numpy_array_equal(result_unique, expected_unique)
+
+        # ignore NaNs for calculating inverse
+        _, values2unique, unique2values = np.unique(idx_duplicated[~na_mask],
+                                                    return_inverse=True,
+                                                    return_index=True)
+        expected_inverse = np.ones(s_duplicated.shape,
+                                   dtype=np.intp) * na_sentinel
+
+        # np.unique yields a __SORTED__ list of uniques, and values2unique
+        # resp. unique2values are relative to this order. To restore the
+        # original order, we argsort values2unique, because values2unique would
+        # be ordered if np.unique had not sorted implicitly. The first argsort
+        # gives the permutation from values2unique to its sorted form, but we
+        # need the inverse permutation (the map from the unsorted uniques to
+        # values2unique, from which we can continue with unique2values).
+        # This inversion (as a permutation) is achieved by the second argsort.
+        inverse_no_na = np.argsort(np.argsort(values2unique))[unique2values]
+        expected_inverse[~na_mask] = inverse_no_na
+        tm.assert_numpy_array_equal(result_inverse, expected_inverse)
 
 
 def test_quantile():
