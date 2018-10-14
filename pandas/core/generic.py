ad2a14f4bec8a004b2972c12f12ed3e4ce37ff52
jbrockmendel <jbrockmendel@gmail.com>
2018-10-14 09:48:40 -0700

    BUG: Fix ndarray + DataFrame ops (#23114)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -1795,6 +1795,10 @@ class NDFrame(PandasObject, SelectionMixin):
     # ----------------------------------------------------------------------
     # Array Interface
 
+    # This is also set in IndexOpsMixin
+    # GH#23114 Ensure ndarray.__op__(DataFrame) returns NotImplemented
+    __array_priority__ = 1000
+
     def __array__(self, dtype=None):
         return com.values_from_object(self)
 
