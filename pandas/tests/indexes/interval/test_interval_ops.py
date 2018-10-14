69024a2b2974835b6773b8825c475b329c72e42c
Jeremy Schendel <jschendel@users.noreply.github.com>
2018-10-14 15:09:31 -0600

    mixin class for tests

--- pandas/tests/indexes/interval/test_interval_ops.py
+++ pandas/tests/indexes/interval/test_interval_ops.py
@@ -1,8 +1,8 @@
 """Tests for Interval-Interval operations, such as overlaps, contains, etc."""
 import pytest
 
-from pandas.core.arrays import IntervalArray
-from .interval_ops import BaseOverlaps
+from pandas import IntervalIndex
+from ...arrays.interval.interval_ops import BaseOverlaps
 
 
 class TestOverlaps(BaseOverlaps):
@@ -10,6 +10,6 @@ class TestOverlaps(BaseOverlaps):
     @pytest.fixture
     def constructor(self):
         """
-        Fixture for IntervalArray class constructor (used by parent class)
+        Fixture for IntervalIndex class constructor (used by parent class)
         """
-        return IntervalArray
+        return IntervalIndex
