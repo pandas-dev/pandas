3ee676c4fb2ad6e7c57a34e41e57e4000a6389d5
Artin Sarraf <rtnsrrf@gmail.com>
2018-10-15 23:16:56 -0400

    ENH - raise TypeError on inappropriate input

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -1421,6 +1421,8 @@ class MultiIndex(Index):
         """
         # just let column level names be the tuple of the meta df columns
         # since they're not required to be strings
+        if not isinstance(df, pd.DataFrame):
+            raise TypeError("Input must be a DataFrame")
         columns = list(df)
         mi = cls.from_tuples(list(df.values), names=columns)
         if squeeze:
