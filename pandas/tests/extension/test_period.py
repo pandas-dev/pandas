f88d6f70cda0c82302fcdc131378ef4d85ec04ee
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-10-18 21:21:17 -0500

    escape

--- pandas/tests/extension/test_period.py
+++ pandas/tests/extension/test_period.py
@@ -113,8 +113,8 @@ class TestArithmeticOps(BasePeriodTests, base.BaseArithmeticOpsTests):
     def test_add_series_with_extension_array(self, data):
         # we don't implement + for Period
         s = pd.Series(data)
-        msg = ("unsupported operand type\(s\) for \+: "
-               "\'PeriodArray\' and \'PeriodArray\'")
+        msg = (r"unsupported operand type\(s\) for \+: "
+               r"\'PeriodArray\' and \'PeriodArray\'")
         with tm.assert_raises_regex(TypeError, msg):
             s + data
 
