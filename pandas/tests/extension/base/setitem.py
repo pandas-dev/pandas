c2e5895487e356ff4c283d1c31d6abc26b9d5662
Kaiqi Dong <kaiqi@kth.se>
2018-10-23 21:07:16 +0200

    add box to be able to directly test setitem

--- pandas/tests/extension/base/setitem.py
+++ pandas/tests/extension/base/setitem.py
@@ -9,18 +9,20 @@ from .base import BaseExtensionTests
 
 
 class BaseSetitemTests(BaseExtensionTests):
-    def test_setitem_scalar_series(self, data):
-        arr = pd.Series(data)
-        arr[0] = data[1]
-        assert arr[0] == data[1]
+    def test_setitem_scalar_series(self, data, box):
+        if box:
+            data = pd.Series(data)
+        data[0] = data[1]
+        assert data[0] == data[1]
 
-    def test_setitem_sequence(self, data):
-        arr = pd.Series(data)
+    def test_setitem_sequence(self, data, box):
+        if box:
+            data = pd.Series(data)
         original = data.copy()
 
-        arr[[0, 1]] = [data[1], data[0]]
-        assert arr[0] == original[1]
-        assert arr[1] == original[0]
+        data[[0, 1]] = [data[1], data[0]]
+        assert data[0] == original[1]
+        assert data[1] == original[0]
 
     @pytest.mark.parametrize('as_array', [True, False])
     def test_setitem_sequence_mismatched_length_raises(self, data, as_array):
@@ -36,25 +38,27 @@ class BaseSetitemTests(BaseExtensionTests):
         with tm.assert_raises_regex(ValueError, xpr.format('slice')):
             ser[slice(3)] = value
 
-    def test_setitem_empty_indxer(self, data):
-        ser = pd.Series(data)
-        original = ser.copy()
-        ser[[]] = []
-        self.assert_series_equal(ser, original)
+    def test_setitem_empty_indxer(self, data, box):
+        if box:
+            data = pd.Series(data)
+        original = data.copy()
+        data[[]] = []
+        self.assert_series_equal(data, original)
 
-    def test_setitem_sequence_broadcasts(self, data):
-        arr = pd.Series(data)
-
-        arr[[0, 1]] = data[2]
-        assert arr[0] == data[2]
-        assert arr[1] == data[2]
+    def test_setitem_sequence_broadcasts(self, data, box):
+        if box:
+            data = pd.Series(data)
+        data[[0, 1]] = data[2]
+        assert data[0] == data[2]
+        assert data[1] == data[2]
 
     @pytest.mark.parametrize('setter', ['loc', 'iloc'])
-    def test_setitem_scalar(self, data, setter):
-        arr = pd.Series(data)
-        setter = getattr(arr, setter)
+    def test_setitem_scalar(self, data, box, setter):
+        if box:
+            data = pd.Series(data)
+        setter = getattr(data, setter)
         operator.setitem(setter, 0, data[1])
-        assert arr[0] == data[1]
+        assert data[0] == data[1]
 
     def test_setitem_loc_scalar_mixed(self, data):
         df = pd.DataFrame({"A": np.arange(len(data)), "B": data})
