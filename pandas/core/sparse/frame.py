23fcf74da49c120f8df0b7945b32fc2e4eb26f23
Brock Mendel <jbrockmendel@gmail.com>
2018-10-24 09:30:31 -0700

    remove unused errors kwarg

--- pandas/core/sparse/frame.py
+++ pandas/core/sparse/frame.py
@@ -642,7 +642,7 @@ class SparseDataFrame(DataFrame):
             new_data, index=self.index, columns=union,
             default_fill_value=self.default_fill_value).__finalize__(self)
 
-    def _combine_const(self, other, func, errors='raise'):
+    def _combine_const(self, other, func):
         return self._apply_columns(lambda x: func(x, other))
 
     def _reindex_index(self, index, method, copy, level, fill_value=np.nan,
