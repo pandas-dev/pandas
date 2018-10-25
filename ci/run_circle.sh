74f4dccafb1e7296fa92ab8a4deff66d1ab98142
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-10-24 21:25:43 -0500

    Restore one

---
+++ ci/run_circle.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+echo "[running tests]"
+export PATH="$MINICONDA_DIR/bin:$PATH"
+
+source activate pandas
+
+echo "pytest --strict --junitxml=$CIRCLE_TEST_REPORTS/reports/junit.xml $@ pandas"
+pytest --strict --color=no --junitxml=$CIRCLE_TEST_REPORTS/reports/junit.xml $@ pandas
