8ed7c73d27707f97ecfc44bba154f2059027f9d8
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-10-26 07:42:15 -0500

    unxfail test

--- pandas/tests/frame/test_reshape.py
+++ pandas/tests/frame/test_reshape.py
@@ -277,8 +277,6 @@ class TestDataFrameReshape(TestData):
                              index=['x', 'y', 'z'])
         assert_frame_equal(result, expected)
 
-    @pytest.mark.xfail(reason="GH-23077",
-                       strict=True)
     def test_unstack_fill_frame_period(self):
 
         # Test unstacking with period
