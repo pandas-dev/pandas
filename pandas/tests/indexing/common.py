3fc44999198ece58ce293449041b972f9849d786
Alexander Ponomaroff <a.ponomaroff@gmail.com>
2018-10-27 12:52:26 -0400

    Line 217 still problematic

--- pandas/tests/indexing/common.py
+++ pandas/tests/indexing/common.py
@@ -214,7 +214,7 @@ class Base(object):
 
                 try:
                     xp = self.get_result(obj, method2, k2, a)
-                except (KeyError, IndexError, ValueError, TypeError, AssertionError):
+                except Exception:
                     result = 'no comp'
                     _print(result)
                     return
