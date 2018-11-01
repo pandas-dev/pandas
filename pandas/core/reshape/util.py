91962b4221e18bd71dae7fb3ab11b4f8b93fa3ff
Charles David <streeck@users.noreply.github.com>
2018-11-01 02:10:12 +0100

    style: fix import format at pandas/core/reshape (#23387)

--- pandas/core/reshape/util.py
+++ pandas/core/reshape/util.py
@@ -1,8 +1,9 @@
 import numpy as np
 
+from pandas.compat import reduce
+
 from pandas.core.dtypes.common import is_list_like
 
-from pandas.compat import reduce
 from pandas.core import common as com
 
 
