362125e589a640a368bf87a8feb5b1731e1d78ec
Marc Garcia <garcia.marc@gmail.com>
2018-11-04 07:49:11 +0000

    Fixing indent in examples

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -6531,9 +6531,8 @@ class DataFrame(NDFrame):
 
         Examples
         --------
-
         >>> df = pd.DataFrame({'key': ['K0', 'K1', 'K2', 'K3', 'K4', 'K5'],
-        ...                        'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})
+        ...                    'A': ['A0', 'A1', 'A2', 'A3', 'A4', 'A5']})
 
         >>> df
           key   A
