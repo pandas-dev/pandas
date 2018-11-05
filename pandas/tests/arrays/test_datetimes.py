caedb95f5b756ef0b208f4de1dd398a117ec9f70
Brock Mendel <jbrockmendel@gmail.com>
2018-11-04 16:47:19 -0800

    parametrize test

--- pandas/tests/arrays/test_datetimes.py
+++ pandas/tests/arrays/test_datetimes.py
@@ -14,8 +14,11 @@ class TestDatetimeArrayComparisons(object):
     # TODO: merge this into tests/arithmetic/test_datetime64 once it is
     #  sufficiently robust
 
-    def test_cmp_dt64_arraylike_tznaive(self):
+    def test_cmp_dt64_arraylike_tznaive(self, all_compare_operators):
         # arbitrary tz-naive DatetimeIndex
+        opname = all_compare_operators.strip('_')
+        op = getattr(operator, opname)
+
         dti = pd.date_range('2016-01-1', freq='MS', periods=9, tz=None)
         arr = DatetimeArray(dti)
         assert arr.freq == dti.freq
@@ -24,31 +27,17 @@ class TestDatetimeArrayComparisons(object):
         right = dti
 
         expected = np.ones(len(arr), dtype=bool)
+        if opname in ['ne', 'gt', 'lt']:
+            # for these the comparisons should be all-False
+            expected = ~expected
 
-        for op in [operator.eq, operator.le, operator.ge]:
-            result = op(arr, arr)
+        result = op(arr, arr)
+        tm.assert_numpy_array_equal(result, expected)
+        for other in [right, np.array(right)]:
+            # TODO: add list and tuple, and object-dtype once those
+            #  are fixed in the constructor
+            result = op(arr, other)
             tm.assert_numpy_array_equal(result, expected)
-            for other in [right, np.array(right)]:
-                # TODO: add list and tuple, and object-dtype once those
-                #  are fixed in the constructor
-                result = op(arr, other)
-                tm.assert_numpy_array_equal(result, expected)
 
-                result = op(other, arr)
-                tm.assert_numpy_array_equal(result, expected)
-
-        # !=, <, >
-        expected = np.zeros(len(dti), dtype=bool)
-        tm.assert_numpy_array_equal(arr != arr, expected)
-
-        for op in [operator.ne, operator.lt, operator.gt]:
-            result = op(arr, arr)
+            result = op(other, arr)
             tm.assert_numpy_array_equal(result, expected)
-            for other in [right, np.array(right)]:
-                # TODO: add list and tuple, and object-dtype once those
-                #  are fixed in the constructor
-                result = op(arr, other)
-                tm.assert_numpy_array_equal(result, expected)
-
-                result = op(other, arr)
-                tm.assert_numpy_array_equal(result, expected)
