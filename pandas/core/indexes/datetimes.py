3a680577c1096aec4928767f8046f6f4242352a4
Markus Meier <meiermark13@gmail.com>
2018-11-09 15:27:13 +0100

    DOC: Remove incorrect periods at the end of parameter types (#23597)

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -501,7 +501,7 @@ class DatetimeIndex(DatetimeArrayMixin, DatelikeOps, TimelikeOps,
 
         Parameters
         ----------
-        keep_tz : optional, defaults False.
+        keep_tz : optional, defaults False
             return the data keeping the timezone.
 
             If keep_tz is True:
