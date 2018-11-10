23344b2823bf3c45f5ba88af50b1d8308d7d4f64
Brock Mendel <jbrockmendel@gmail.com>
2018-11-10 08:36:00 -0800

    remove unliked comment

--- pandas/_libs/algos.pyx
+++ pandas/_libs/algos.pyx
@@ -195,7 +195,6 @@ def groupsort_indexer(ndarray[int64_t] index, Py_ssize_t ngroups):
     return result, counts
 
 
-# TODO: redundant with groupby.kth_smallest_c
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def kth_smallest(numeric[:] a, Py_ssize_t k) -> numeric:
