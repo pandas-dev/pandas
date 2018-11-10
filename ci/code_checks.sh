b869ec303f98fb807a14a542192742cb936a2b28
Jose Rivera-Rubio <jmrr@users.noreply.github.com>
2018-11-10 22:47:24 +0100

    DOC: Updating Series.resample and DataFrame.resample docstrings (#23197)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -151,7 +151,7 @@ if [[ -z "$CHECK" || "$CHECK" == "doctests" ]]; then
 
     MSG='Doctests generic.py' ; echo $MSG
     pytest -q --doctest-modules pandas/core/generic.py \
-        -k"-_set_axis_name -_xs -describe -droplevel -groupby -interpolate -pct_change -pipe -reindex -reindex_axis -resample -to_json -transpose -values -xs"
+        -k"-_set_axis_name -_xs -describe -droplevel -groupby -interpolate -pct_change -pipe -reindex -reindex_axis -to_json -transpose -values -xs"
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
     MSG='Doctests top-level reshaping functions' ; echo $MSG
