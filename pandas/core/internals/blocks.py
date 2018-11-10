d8e7ba475b358877d3fcd5c93a7ba607de4cc2af
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-11-09 21:08:41 -0600

    py2 compat

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -1955,7 +1955,7 @@ class ExtensionBlock(NonConsolidatableMixIn, Block):
         # Do the warning here, it's only user in pandas, since we
         # have to check if the subclass overrode it.
         fv = getattr(type(self.values), '_formatting_values', None)
-        if fv is not ExtensionArray._formatting_values:
+        if fv and fv != ExtensionArray._formatting_values:
             msg = (
                 "'ExtensionArray._formatting_values' is deprecated. "
                 "Specify 'ExtensionArray._formatter' instead."
