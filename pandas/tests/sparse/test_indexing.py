ccb0cfb980f926d4a9d8e4a9f27440f3be7e7872
Martin Babka <babkys@gmail.com>
2018-11-11 09:27:05 +0100

    Split tests
    As required in PR

--- pandas/tests/sparse/test_indexing.py
+++ pandas/tests/sparse/test_indexing.py
@@ -455,25 +455,6 @@ class TestSparseSeriesIndexing(object):
             s.iloc[indexer]
 
 
-class TestSparseArray(object):
-
-    def test_nonzero(self):
-        sa = pd.SparseArray([
-            float('nan'),
-            float('nan'),
-            1, 0, 0,
-            2, 0, 0, 0,
-            3, 0, 0
-        ])
-        tm.assert_numpy_array_equal(np.array([2, 5, 9], dtype=np.int32),
-                                    sa.nonzero()[0])
-
-        sa = pd.SparseArray(
-            [0, 0, 1, 0, 0, 2, 0, 0, 0, 3, 0, 0])
-        tm.assert_numpy_array_equal(np.array([2, 5, 9], dtype=np.int32),
-                                    sa.nonzero()[0])
-
-
 class TestSparseSeriesMultiIndexing(TestSparseSeriesIndexing):
 
     def setup_method(self, method):
