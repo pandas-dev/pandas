8f0ace3011cf49c745bd32d0c7f60ace8214a127
Artin Sarraf <rtnsrrf@gmail.com>
2018-11-10 22:13:19 -0500

    TST - add tests for categrorical index, and compatible inconsistent pairs

--- pandas/tests/indexes/test_setops.py
+++ pandas/tests/indexes/test_setops.py
@@ -49,7 +49,7 @@ def test_union_same_types(idxType):
 
 
 @pytest.mark.parametrize('idxType1,idxType2', 
-    list(it.combinations([x for x in INDEXES if x != 'catIndex'], 2))
+    list(it.combinations(INDEXES, 2))
 )
 def test_union_different_types(idxType1, idxType2):
     if tuple(sorted([idxType1, idxType2])) in COMPATIBLE_INCONSISTENT_PAIRS:
@@ -57,5 +57,27 @@ def test_union_different_types(idxType1, idxType2):
 
     idx1 = INDEXES[idxType1](10)
     idx2 = INDEXES[idxType2](20)
+
+    # A union with a CategoricalIndex (even as dtype('O')) and a 
+    # non-CategoricalIndex can only be made if both indices are monotonic.
+    # This is true before this PR as well.
+    if idxType1 == 'catIndex' or idxType2 == 'catIndex':
+        idx1 = idx1.sort_values()
+        idx2 = idx2.sort_values()
+
     assert idx1.union(idx2).dtype == np.dtype('O')
     assert idx2.union(idx1).dtype == np.dtype('O')
+
+
+@pytest.mark.parametrize('idxType1,idxType2',
+    COMPATIBLE_INCONSISTENT_PAIRS
+)
+def test_compatible_inconsistent_pairs(idxType1, idxType2):
+    idx1 = INDEXES[idxType1](10)
+    idx2 = INDEXES[idxType2](20)
+
+    res1 = idx1.union(idx2)
+    res2 = idx2.union(idx1)
+
+    assert res1.dtype in (idx1.dtype, idx2.dtype)
+    assert res2.dtype in (idx1.dtype, idx2.dtype)
