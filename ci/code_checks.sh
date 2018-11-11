1fea3a452cc4ae0ac7efcf791a39cc9b9686f6c0
Marc Garcia <garcia.marc@gmail.com>
2018-11-11 23:00:53 +0000

    Excluding .pyc files from the new check

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -120,7 +120,7 @@ if [[ -z "$CHECK" || "$CHECK" == "patterns" ]]; then
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
     MSG='Check that the deprecated `assert_raises_regex` is not used (`pytest.raises(match=pattern)` should be used instead)' ; echo $MSG
-    ! grep -R --exclude=testing.py --exclude=test_testing.py assert_raises_regex pandas
+    ! grep -R --exclude=*.pyc --exclude=testing.py --exclude=test_testing.py assert_raises_regex pandas
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
     MSG='Check for modules that pandas should not import' ; echo $MSG
