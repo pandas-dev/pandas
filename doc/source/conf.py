ce1c134e6fe9c7e9906a047aa9c4e2113f13403f
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-11-12 10:24:51 -0600

    Minor fixups

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -313,6 +313,7 @@ common_imports = """\
    randn = np.random.randn
    np.set_printoptions(precision=4, suppress=True)
    options.display.max_rows = 15
+   from pandas.compat import StringIO
 """
 
 
