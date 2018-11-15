1c9fb1a5096941697f32ce7771831518b7892ca5
Brock Mendel <jbrockmendel@gmail.com>
2018-11-15 09:58:41 -0800

    test fixups

--- pandas/tests/arrays/test_timedeltas.py
+++ pandas/tests/arrays/test_timedeltas.py
@@ -10,20 +10,20 @@ import pandas.util.testing as tm
 class TestTimedeltaArray(object):
 
     def test_abs(self):
-        vals = np.array([-3600, 'NaT', 7200], dtype='M8[s]')
+        vals = np.array([-3600 * 10**9, 'NaT', 7200 * 10**9], dtype='m8[ns]')
         arr = TimedeltaArray(vals)
 
-        evals = np.array([3600, 'NaT', 7200], dtype='M8[s]')
+        evals = np.array([3600 * 10**9, 'NaT', 7200 * 10**9], dtype='m8[ns]')
         expected = TimedeltaArray(evals)
 
         result = abs(arr)
         tm.assert_timedelta_array_equal(result, expected)
 
     def test_neg(self):
-        vals = np.array([-3600, 'NaT', 7200], dtype='M8[s]')
+        vals = np.array([-3600 * 10**9, 'NaT', 7200 * 10**9], dtype='m8[ns]')
         arr = TimedeltaArray(vals)
 
-        evals = np.array([3600, 'NaT', -7200], dtype='M8[s]')
+        evals = np.array([3600 * 10**9, 'NaT', -7200 * 10**9], dtype='m8[ns]')
         expected = TimedeltaArray(evals)
 
         result = -arr
@@ -31,9 +31,9 @@ class TestTimedeltaArray(object):
 
     def test_neg_freq(self):
         tdi = pd.timedelta_range('2 Days', periods=4, freq='H')
-        arr = TimedeltaArray(tdi)
+        arr = TimedeltaArray(tdi, freq=tdi.freq)
 
-        expected = TimedeltaArray(-tdi._data, freq=-dti.freq)
+        expected = TimedeltaArray(-tdi._data, freq=-tdi.freq)
 
         result = -arr
         tm.assert_timedelta_array_equal(result, expected)
