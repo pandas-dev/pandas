4ad288a64418568a4e750d22ae872ff164bb63f8
Martin Babka <babky@users.noreply.github.com>
2018-11-17 23:15:57 +0100

    Fix nonzero of a SparseArray (#21175)

--- pandas/tests/arrays/sparse/test_array.py
+++ pandas/tests/arrays/sparse/test_array.py
@@ -784,6 +784,23 @@ class TestSparseArray(object):
         exp = SparseArray([1, 3, 3, 3, 3], fill_value=0, dtype=np.float64)
         tm.assert_sp_array_equal(res, exp)
 
+    def test_nonzero(self):
+        # Tests regression #21172.
+        sa = pd.SparseArray([
+            float('nan'),
+            float('nan'),
+            1, 0, 0,
+            2, 0, 0, 0,
+            3, 0, 0
+        ])
+        expected = np.array([2, 5, 9], dtype=np.int32)
+        result, = sa.nonzero()
+        tm.assert_numpy_array_equal(expected, result)
+
+        sa = pd.SparseArray([0, 0, 1, 0, 0, 2, 0, 0, 0, 3, 0, 0])
+        result, = sa.nonzero()
+        tm.assert_numpy_array_equal(expected, result)
+
 
 class TestSparseArrayAnalytics(object):
 
