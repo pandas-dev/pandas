5bf17093d0a79aae26df3a36dc91b4c0b6a6f089
Marc Garcia <garcia.marc@gmail.com>
2018-11-20 19:50:13 +0000

    Fixing build errors (typo in dependencies, missing cpplint, code checks before build), and clean up of travis dependencies

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -17,7 +17,7 @@
 #   $ ./ci/code_checks.sh docstrings    # validate docstring errors
 #   $ ./ci/code_checks.sh dependencies  # check that dependencies are consistent
 
-[[ -z "$1" || "$1" == "lint" || "$1" == "patterns" || "$1" == "code" || "$1" == "doctests" || "$1" == "docstrings" || "$1" == "dependecies" ]] || \
+[[ -z "$1" || "$1" == "lint" || "$1" == "patterns" || "$1" == "code" || "$1" == "doctests" || "$1" == "docstrings" || "$1" == "dependencies" ]] || \
     { echo "Unknown command $1. Usage: $0 [lint|patterns|code|doctests|docstrings|dependencies]"; exit 9999; }
 
 source activate pandas
