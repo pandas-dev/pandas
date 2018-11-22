29a78a6de457aab76fc4a6f2a0d4c97583b64ac8
Srinivas Reddy Thatiparthy (శ్రీనివాస్  రెడ్డి తాటిపర్తి) <thatiparthysreenivas@gmail.com>
2018-11-22 09:24:58 +0530

    Fix doc string of make_signature() function (#23768)

--- pandas/api/extensions/__init__.py
+++ pandas/api/extensions/__init__.py
@@ -1,4 +1,4 @@
-"""Public API for extending panadas objects."""
+"""Public API for extending pandas objects."""
 from pandas.core.accessor import (register_dataframe_accessor,  # noqa
                                   register_index_accessor,
                                   register_series_accessor)
