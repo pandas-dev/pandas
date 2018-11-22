19047a40d8e9bfb995303ce5a424f8302a0219f8
Srinivas Reddy Thatiparthy <thatiparthysreenivas@gmail.com>
2018-11-22 19:18:49 +0530

    Add test case for non existent file in user's home directory

--- pandas/tests/io/test_common.py
+++ pandas/tests/io/test_common.py
@@ -150,6 +150,26 @@ bar2,12,13,14,15
         with pytest.raises(error_class):
             reader(path)
 
+    @pytest.mark.parametrize('reader, module, error_class, fn_ext', [
+        (pd.read_csv, 'os', FileNotFoundError, 'csv'),
+        (pd.read_fwf, 'os', FileNotFoundError, 'txt'),
+        (pd.read_excel, 'xlrd', FileNotFoundError, 'xlsx'),
+        (pd.read_feather, 'feather', Exception, 'feather'),
+        (pd.read_hdf, 'tables', FileNotFoundError, 'h5'),
+        (pd.read_stata, 'os', FileNotFoundError, 'dta'),
+        (pd.read_sas, 'os', FileNotFoundError, 'sas7bdat'),
+        (pd.read_json, 'os', ValueError, 'json'),
+        (pd.read_msgpack, 'os', ValueError, 'mp'),
+        (pd.read_pickle, 'os', FileNotFoundError, 'pickle'),
+    ])
+    def test_read_non_existant_user_home_dir(self, reader, module,
+                                             error_class, fn_ext):
+        pytest.importorskip(module)
+
+        path = os.path.join('~', 'does_not_exist.' + fn_ext)
+        with pytest.raises(error_class):
+            reader(path)
+
     def test_read_non_existant_read_table(self):
         path = os.path.join(HERE, 'data', 'does_not_exist.' + 'csv')
         with pytest.raises(FileNotFoundError):
