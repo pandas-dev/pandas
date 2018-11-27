a6451e2929e0e269bed8667bb2a64181f048d2cd
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2018-11-27 18:15:28 +0100

    DOC: fix double Returns section in set_index docstring (#23948)

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -3971,10 +3971,6 @@ class DataFrame(NDFrame):
         DataFrame.reindex : Change to new indices or expand indices.
         DataFrame.reindex_like : Change to same indices as other DataFrame.
 
-        Returns
-        -------
-        DataFrame
-
         Examples
         --------
         >>> df = pd.DataFrame({'month': [1, 4, 7, 10],
