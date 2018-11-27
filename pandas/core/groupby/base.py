8c64854b68e0810d94f936c39cc7e652609ba877
Will Ayd <william.ayd@icloud.com>
2018-11-27 08:39:22 -0500

    Removed unneeded whitelist entries

--- pandas/core/groupby/base.py
+++ pandas/core/groupby/base.py
@@ -66,35 +66,22 @@ class GroupByMixin(object):
 
 # special case to prevent duplicate plots when catching exceptions when
 # forwarding methods from NDFrames
-plotting_methods = frozenset(['plot', 'boxplot', 'hist'])
+plotting_methods = frozenset(['hist'])
 
 common_apply_whitelist = frozenset([
-    'last', 'first',
-    'head', 'tail', 'median',
-    'mean', 'sum', 'min', 'max',
-    'cumcount', 'ngroup',
-    'resample',
-    'rank', 'quantile',
-    'fillna',
-    'mad',
-    'any', 'all',
-    'take',
-    'idxmax', 'idxmin',
-    'shift', 'tshift',
-    'ffill', 'bfill',
-    'pct_change', 'skew',
-    'corr', 'cov', 'diff',
+    'quantile', 'fillna', 'mad', 'take',
+    'idxmax', 'idxmin', 'tshift',
+    'skew', 'corr', 'cov', 'diff'
 ]) | plotting_methods
 
 series_apply_whitelist = ((common_apply_whitelist |
                            {'nlargest', 'nsmallest',
                             'is_monotonic_increasing',
-                            'is_monotonic_decreasing'}) -
-                          {'boxplot'}) | frozenset(['dtype', 'unique'])
+                            'is_monotonic_decreasing'})
+                          ) | frozenset(['dtype', 'unique'])
 
 dataframe_apply_whitelist = ((common_apply_whitelist |
-                              frozenset(['dtypes', 'corrwith'])) -
-                             {'boxplot'})
+                              frozenset(['dtypes', 'corrwith'])))
 
 cython_transforms = frozenset(['cumprod', 'cumsum', 'shift',
                                'cummin', 'cummax'])
