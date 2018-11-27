d650151b74a62cc67ae7db06f91cd30348c4996f
Christopher Whelan <topherwhelan@gmail.com>
2018-11-27 13:40:39 -0800

    PERF: add asv benchmarks for crosstab()

--- asv_bench/benchmarks/reshape.py
+++ asv_bench/benchmarks/reshape.py
@@ -140,6 +140,30 @@ class PivotTable(object):
                             margins=True)
 
 
+class Crosstab(object):
+
+    def setup(self):
+        N = 100000
+        fac1 = np.array(['A', 'B', 'C'], dtype='O')
+        fac2 = np.array(['one', 'two'], dtype='O')
+        self.ind1 = np.random.randint(0, 3, size=N)
+        self.ind2 = np.random.randint(0, 2, size=N)
+        self.vec1 = fac1.take(self.ind1)
+        self.vec2 = fac2.take(self.ind2)
+
+    def time_crosstab(self):
+        pd.crosstab(self.vec1, self.vec2)
+
+    def time_crosstab_values(self):
+        pd.crosstab(self.vec1, self.vec2, values=self.ind1, aggfunc='sum')
+
+    def time_crosstab_normalize(self):
+        pd.crosstab(self.vec1, self.vec2, normalize=True)
+
+    def time_crosstab_normalize_margins(self):
+        pd.crosstab(self.vec1, self.vec2, normalize=True, margins=True)
+
+
 class GetDummies(object):
     def setup(self):
         categories = list(string.ascii_letters[:12])
