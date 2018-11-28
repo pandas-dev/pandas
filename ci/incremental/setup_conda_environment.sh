7f48ac201878bd18b3b3e3425d20e1dc93e8b5df
Marc Garcia <garcia.marc@gmail.com>
2018-11-28 17:38:35 +0000

    adding more debug information

--- ci/incremental/setup_conda_environment.sh
+++ ci/incremental/setup_conda_environment.sh
@@ -8,6 +8,7 @@ PIP_INSTALL="pip install -q"
 
 # Deactivate any environment
 echo "PATH inside setup_conda_environment.sh: $PATH"
+ls $HOME/miniconda3/bin/
 source deactivate
 # Display root environment (for debugging)
 conda list
