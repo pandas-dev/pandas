c97de72fecb37c1e0648377d054a346a912dd00e
Simon Hawkins <simonjayhawkins@gmail.com>
2018-12-01 23:44:58 +0000

    remove test classes and replace catch_warnings

--- pandas/tests/indexing/multiindex/test_ix.py
+++ pandas/tests/indexing/multiindex/test_ix.py
@@ -1,27 +1,21 @@
-from warnings import catch_warnings, simplefilter
-
 import pytest
 
 from pandas.compat import lrange
 
 
 @pytest.mark.filterwarnings("ignore:\\n.ix:DeprecationWarning")
-class TestMultiIndexIx(object):
+def test_frame_setitem_ix(multiindex_dataframe_random_data):
+    frame = multiindex_dataframe_random_data
+    frame.loc[('bar', 'two'), 'B'] = 5
+    assert frame.loc[('bar', 'two'), 'B'] == 5
 
-    def test_frame_setitem_ix(self, multiindex_dataframe_random_data):
-        frame = multiindex_dataframe_random_data
-        frame.loc[('bar', 'two'), 'B'] = 5
-        assert frame.loc[('bar', 'two'), 'B'] == 5
+    # with integer labels
+    df = frame.copy()
+    df.columns = lrange(3)
+    df.loc[('bar', 'two'), 1] = 7
+    assert df.loc[('bar', 'two'), 1] == 7
 
-        # with integer labels
-        df = frame.copy()
-        df.columns = lrange(3)
-        df.loc[('bar', 'two'), 1] = 7
-        assert df.loc[('bar', 'two'), 1] == 7
-
-        with catch_warnings(record=True):
-            simplefilter("ignore", DeprecationWarning)
-            df = frame.copy()
-            df.columns = lrange(3)
-            df.ix[('bar', 'two'), 1] = 7
-        assert df.loc[('bar', 'two'), 1] == 7
+    df = frame.copy()
+    df.columns = lrange(3)
+    df.ix[('bar', 'two'), 1] = 7
+    assert df.loc[('bar', 'two'), 1] == 7
