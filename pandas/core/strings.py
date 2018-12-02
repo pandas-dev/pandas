2e0d03a9a104d5ea0579225339002512b694834b
H. Vetinari <h.vetinari@gmx.com>
2018-12-02 13:05:13 +0100

    PERF: Add if branch for empty sep in str.cat

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -59,6 +59,8 @@ def cat_core(list_of_columns: List, sep: str):
     nd.array
         The concatenation of list_of_columns with sep
     """
+    if sep == '':
+        return np.sum(list_of_columns, axis=0)
     list_with_sep = [sep] * (2 * len(list_of_columns) - 1)
     list_with_sep[::2] = list_of_columns
     return np.sum(list_with_sep, axis=0)
