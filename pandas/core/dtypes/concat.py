583ce4958aef67094e9cefeac50e2acd97f20963
Evangeline Liu <hsiyinliu@gmail.com>
2018-12-02 11:31:24 -0500

    tests failed so changing it back to when travis ci succeeded

--- pandas/core/dtypes/concat.py
+++ pandas/core/dtypes/concat.py
@@ -211,7 +211,7 @@ def _concat_categorical(to_concat, axis=0):
         # when all categories are identical
         first = to_concat[0]
         if all(first.is_dtype_equal(other) for other in to_concat[1:]):
-            return _concat_compat(categoricals)
+            return union_categoricals(categoricals)
 
     return _concat_asobject(to_concat)
 
