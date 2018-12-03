857c6beeeabd1f16e68de6f81875341fed5d5d25
Evangeline Liu <hsiyinliu@gmail.com>
2018-12-03 15:48:35 -0500

    Restored blank lines

--- pandas/core/dtypes/concat.py
+++ pandas/core/dtypes/concat.py
@@ -23,6 +23,7 @@ def get_dtype_kinds(l):
     Parameters
     ----------
     l : list of arrays
+
     Returns
     -------
     a set of kinds that exist in this list of arrays
@@ -106,10 +107,12 @@ def _concat_compat(to_concat, axis=0):
     'normalized' dtypes (in that for example, if it's object, then it is a
     non-datetimelike and provide a combined dtype for the resulting array that
     preserves the overall dtype if possible)
+
     Parameters
     ----------
     to_concat : array of arrays
     axis : axis to provide concatenation
+
     Returns
     -------
     a single array, preserving the combined dtypes
@@ -174,12 +177,14 @@ def _concat_compat(to_concat, axis=0):
 def _concat_categorical(to_concat, axis=0):
     """Concatenate an object/categorical array of arrays, each of which is a
     single dtype
+
     Parameters
     ----------
     to_concat : array of arrays
     axis : int
         Axis to provide concatenation in the current implementation this is
         always 0, e.g. we only have 1D categoricals
+
     Returns
     -------
     Categorical
@@ -217,7 +222,9 @@ def union_categoricals(to_union, sort_categories=False, ignore_order=False):
     """
     Combine list-like of Categorical-like, unioning categories. All
     categories must have the same dtype.
+
     .. versionadded:: 0.19.0
+
     Parameters
     ----------
     to_union : list-like of Categorical, CategoricalIndex,
@@ -228,10 +235,13 @@ def union_categoricals(to_union, sort_categories=False, ignore_order=False):
     ignore_order : boolean, default False
         If true, the ordered attribute of the Categoricals will be ignored.
         Results in an unordered categorical.
+
         .. versionadded:: 0.20.0
+
     Returns
     -------
     result : Categorical
+
     Raises
     ------
     TypeError
@@ -241,52 +251,69 @@ def union_categoricals(to_union, sort_categories=False, ignore_order=False):
         - sort_categories=True and Categoricals are ordered
     ValueError
         Empty list of categoricals passed
+
     Notes
     -----
+
     To learn more about categories, see `link
     <http://pandas.pydata.org/pandas-docs/stable/categorical.html#unioning>`__
+    
     Examples
     --------
+
     >>> from pandas.api.types import union_categoricals
+
     If you want to combine categoricals that do not necessarily have
     the same categories, `union_categoricals` will combine a list-like
     of categoricals. The new categories will be the union of the
     categories being combined.
+
     >>> a = pd.Categorical(["b", "c"])
     >>> b = pd.Categorical(["a", "b"])
     >>> union_categoricals([a, b])
     [b, c, a, b]
     Categories (3, object): [b, c, a]
+
     By default, the resulting categories will be ordered as they appear
     in the `categories` of the data. If you want the categories to be
     lexsorted, use `sort_categories=True` argument.
+
     >>> union_categoricals([a, b], sort_categories=True)
     [b, c, a, b]
     Categories (3, object): [a, b, c]
+
     `union_categoricals` also works with the case of combining two
     categoricals of the same categories and order information (e.g. what
     you could also `append` for).
+
     >>> a = pd.Categorical(["a", "b"], ordered=True)
     >>> b = pd.Categorical(["a", "b", "a"], ordered=True)
     >>> union_categoricals([a, b])
     [a, b, a, b, a]
     Categories (2, object): [a < b]
+
     Raises `TypeError` because the categories are ordered and not identical.
+
     >>> a = pd.Categorical(["a", "b"], ordered=True)
     >>> b = pd.Categorical(["a", "b", "c"], ordered=True)
     >>> union_categoricals([a, b])
     TypeError: to union ordered Categoricals, all categories must be the same
+
     New in version 0.20.0
+
     Ordered categoricals with different categories or orderings can be
     combined by using the `ignore_ordered=True` argument.
+
     >>> a = pd.Categorical(["a", "b", "c"], ordered=True)
     >>> b = pd.Categorical(["c", "b", "a"], ordered=True)
     >>> union_categoricals([a, b], ignore_order=True)
     [a, b, c, c, b, a]
     Categories (3, object): [a, b, c]
+
     `union_categoricals` also works with a `CategoricalIndex`, or `Series`
     containing categorical data, but note that the resulting array will
     always be a plain `Categorical`
+
     >>> a = pd.Series(["b", "c"], dtype='category')
     >>> b = pd.Series(["a", "b"], dtype='category')
     >>> union_categoricals([a, b])
@@ -377,11 +404,13 @@ def _concat_datetime(to_concat, axis=0, typs=None):
     """
     provide concatenation of an datetimelike array of arrays each of which is a
     single M8[ns], datetimet64[ns, tz] or m8[ns] dtype
+
     Parameters
     ----------
     to_concat : array of arrays
     axis : axis to provide concatenation
     typs : set of to_concat dtypes
+
     Returns
     -------
     a single array, preserving the combined dtypes
@@ -481,11 +510,13 @@ def _concat_sparse(to_concat, axis=0, typs=None):
     """
     provide concatenation of an sparse/dense array of arrays each of which is a
     single dtype
+
     Parameters
     ----------
     to_concat : array of arrays
     axis : axis to provide concatenation
     typs : set of to_concat dtypes
+    
     Returns
     -------
     a single array, preserving the combined dtypes
