4fcd2c4617081c137708c4e5230cda589c885339
Andy <andyshen2882@gmail.com>
2018-12-04 11:22:35 -0500

    Fix linter issues

--- pandas/tests/arrays/test_datetimes.py
+++ pandas/tests/arrays/test_datetimes.py
@@ -15,23 +15,27 @@ class TestDatetimeArrayComparisons(object):
     # TODO: merge this into tests/arithmetic/test_datetime64 once it is
     #  sufficiently robust
     def test_box(self):
-        df = pd.DataFrame({'year': [2015, 2016], 'month': [2, 3],'day': [4, 5]})
+        df = pd.DataFrame({'year': [2015, 2016], 
+            'month': [2, 3], 'day': [4, 5]})
         res = pd.to_datetime(df, box=False)
-        assert isinstance(res, np.ndarray) == True
+        assert isinstance(res, np.ndarray) is True
         res = pd.to_datetime(df, box=True)
-        assert isinstance(res, np.ndarray) == False
+        assert isinstance(res, np.ndarray) is False
 
     def test_utc(self):
-        df = pd.DataFrame({'year': [2015, 2016], 'month': [2, 3],'day': [4, 5]})
+        df = pd.DataFrame({'year': [2015, 2016], 
+            'month': [2, 3], 'day': [4, 5]})
         res = pd.to_datetime(df, utc=True)
         assert str(res[0].tz) == 'UTC'
         res = pd.to_datetime(df, utc=False)
         assert str(res[0].tz) != 'UTC'
 
     def test_box_to_datetime_box_False(self):
-        df = pd.DataFrame({'year': [2015, 2016], 'month': [2, 3], 'day': [4, 5]})
+        df = pd.DataFrame({'year': [2015, 2016], 
+            'month': [2, 3], 'day': [4, 5]})
         res = pd.to_datetime(df, box=False)
-        arr = array(['2015-02-04T00:00:00.000000000', '2016-03-05T00:00:00.000000000'],dtype='datetime64[ns]')
+        arr = array(['2015-02-04T00:00:00.000000000', 
+            '2016-03-05T00:00:00.000000000'], dtype='datetime64[ns]')
         assert res == arr
 
     def test_cmp_dt64_arraylike_tznaive(self, all_compare_operators):
