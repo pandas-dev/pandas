378515a88687143b01916b12b3831c3f0a814b74
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-12-04 14:07:49 -0600

    more hacky workarounds

--- pandas/tests/io/conftest.py
+++ pandas/tests/io/conftest.py
@@ -38,7 +38,10 @@ def s3_resource(tips_file, jsonl_file):
     pytest.importorskip('s3fs')
     boto3 = pytest.importorskip('boto3')
     # GH-24092. See if boto.plugin skips the test or fails.
-    pytest.importorskip("boto.plugin")
+    try:
+        pytest.importorskip("boto.plugin")
+    except AttributeError:
+        raise pytest.skip(reason="moto/moto error")
     moto = pytest.importorskip('moto')
 
     test_s3_files = [
