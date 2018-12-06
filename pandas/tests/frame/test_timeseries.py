34612719b635587494587f9ad142f1fb7857fbd6
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-12-06 13:48:12 +0200

    !U add test for data_frame

--- pandas/tests/frame/test_timeseries.py
+++ pandas/tests/frame/test_timeseries.py
@@ -320,6 +320,19 @@ class TestDataFrameTimeSeriesMethods(TestData):
         xp = DataFrame({'one': s1.shift(1), 'two': s2.shift(1)})
         assert_frame_equal(rs, xp)
 
+    def test_shift_fill_value(self):
+        df = DataFrame(np.random.randnint(5), index=date_range('1/1/2000',
+                                                            periods=5,
+                                                            freq='H'))
+        result = df.shift(1, fill_value=0)
+        tm.assert_equal(result.iloc[0, 0].value, 0)
+        tm.assert_equal(result.iloc[1, 0].value, df.iloc[0, 0].value)
+
+        result = df.shift(2, fill_value=0)
+        tm.assert_equal(result.iloc[0, 0].value, 0)
+        tm.assert_equal(result.iloc[1, 0].value, 0)
+        tm.assert_equal(result.iloc[2, 0].value, df.iloc[0, 0].value)
+
     def test_shift_empty(self):
         # Regression test for #8019
         df = DataFrame({'foo': []})
