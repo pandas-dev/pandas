939dfeb4eace1095cc486bb5c9b38cdef701616f
LJArendse <lindsayjohnarendse@gmail.com>
2018-12-09 21:54:02 +0200

    DOC: Fix Line 968:80: E501 line too long (81 > 79 characters)

--- pandas/core/window.py
+++ pandas/core/window.py
@@ -965,7 +965,8 @@ class _Rolling_and_Expanding(_Rolling):
 
         .. versionadded:: 0.23.0
     *args, **kwargs :
-        Passed to the function for compatibility with other %(name)s methods.""")
+        Passed to the function for compatibility with other %(name)s
+		methods.""")
 
     def apply(self, func, raw=None, args=(), kwargs={}):
         from pandas import Series
