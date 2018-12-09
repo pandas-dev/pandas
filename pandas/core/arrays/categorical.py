ce721ae9174aa22e3a4ad327a9dd17d1da2979c4
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-12-09 21:44:31 +0200

    !U remove the -1 implementation detail

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -1265,7 +1265,6 @@ class Categorical(ExtensionArray, PandasObject):
             Number of periods to move, can be positive or negative
         fill_value : object, optional
             The scalar value to use for newly introduced missing values.
-            The default is ``-1``.
 
             .. versionadded:: 0.24.0
 
