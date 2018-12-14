8ec63eb9e25d0fb54e7a84997bb3510498e762d8
Brock Mendel <jbrockmendel@gmail.com>
2018-12-14 14:24:20 -0800

    revert not-yet-ready

--- pandas/core/arrays/datetimes.py
+++ pandas/core/arrays/datetimes.py
@@ -505,6 +505,8 @@ class DatetimeArrayMixin(dtl.DatetimeLikeArrayMixin,
 
     def _sub_datetime_arraylike(self, other):
         """subtract DatetimeArray/Index or ndarray[datetime64]"""
+        if len(self) != len(other):
+            raise ValueError("cannot add indices of unequal length")
 
         if isinstance(other, np.ndarray):
             assert is_datetime64_dtype(other)
