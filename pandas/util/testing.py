8189fac3bf99a63272685bd551a5055bf85a7a47
h-vetinari <33685575+h-vetinari@users.noreply.github.com>
2018-12-15 19:04:23 +0100

    CI for boto: fix errors; add coverage; add skip for uncatchable ResourceWarning (#23731)

--- pandas/util/testing.py
+++ pandas/util/testing.py
@@ -782,6 +782,22 @@ def ensure_clean_dir():
             pass
 
 
+@contextmanager
+def ensure_safe_environment_variables():
+    """
+    Get a context manager to safely set environment variables
+
+    All changes will be undone on close, hence environment variables set
+    within this contextmanager will neither persist nor change global state.
+    """
+    saved_environ = dict(os.environ)
+    try:
+        yield
+    finally:
+        os.environ.clear()
+        os.environ.update(saved_environ)
+
+
 # -----------------------------------------------------------------------------
 # Comparators
 
