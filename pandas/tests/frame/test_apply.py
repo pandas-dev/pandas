83ce9464a533e40eaef0f4deea39cbc6451842db
alimcmaster1 <alimcmaster1@gmail.com>
2018-12-16 22:27:59 +0000

    Add issue number

--- pandas/tests/frame/test_apply.py
+++ pandas/tests/frame/test_apply.py
@@ -1140,6 +1140,7 @@ class TestDataFrameAggregate():
 
     @pytest.mark.parametrize("num_cols", [2, 3, 5])
     def test_frequency_is_original(self, num_cols):
+        # GH 22150
         index = pd.DatetimeIndex(["1950-06-30", "1952-10-24", "1953-05-29"])
         original = index.copy()
         df = DataFrame(1, index=index, columns=range(num_cols))
