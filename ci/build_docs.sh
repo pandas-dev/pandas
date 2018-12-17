9d33201694743508e88d7a0d66f8f651ba33a5df
Marc Garcia <garcia.marc@gmail.com>
2018-12-17 00:52:13 +0000

    DOC: Not pushing the docs to github pages in PRs

--- ci/build_docs.sh
+++ ci/build_docs.sh
@@ -25,28 +25,30 @@ if [ "$DOC" ]; then
     echo # Create and send docs #
     echo ########################
 
-    cd build/html
-    git config --global user.email "pandas-docs-bot@localhost.foo"
-    git config --global user.name "pandas-docs-bot"
+    if ["${TRAVIS_PULL_REQUEST}" != "false"]; then
+        cd build/html
+        git config --global user.email "pandas-docs-bot@localhost.foo"
+        git config --global user.name "pandas-docs-bot"
 
-    # create the repo
-    git init
+        # create the repo
+        git init
 
-    touch README
-    git add README
-    git commit -m "Initial commit" --allow-empty
-    git branch gh-pages
-    git checkout gh-pages
-    touch .nojekyll
-    git add --all .
-    git commit -m "Version" --allow-empty
+        touch README
+        git add README
+        git commit -m "Initial commit" --allow-empty
+        git branch gh-pages
+        git checkout gh-pages
+        touch .nojekyll
+        git add --all .
+        git commit -m "Version" --allow-empty
 
-    git remote remove origin
-    git remote add origin "https://${PANDAS_GH_TOKEN}@github.com/pandas-dev/pandas-docs-travis.git"
-    git fetch origin
-    git remote -v
+        git remote remove origin
+        git remote add origin "https://${PANDAS_GH_TOKEN}@github.com/pandas-dev/pandas-docs-travis.git"
+        git fetch origin
+        git remote -v
 
-    git push origin gh-pages -f
+        git push origin gh-pages -f
+    fi
 fi
 
 exit 0
