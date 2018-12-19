d800f2ba420d06ce7d31ea7c596dde8c03fd2731
Brock Mendel <jbrockmendel@gmail.com>
2018-12-19 15:07:11 -0800

    parametrize test over tz

--- pandas/tests/indexes/datetimes/test_tools.py
+++ pandas/tests/indexes/datetimes/test_tools.py
@@ -247,9 +247,10 @@ class TestTimeConversionFormats(object):
 
 
 class TestToDatetime(object):
-    def test_to_datetime_dtarr(self):
+    @pytest.mark.parametrize('tz', [None, 'US/Central'])
+    def test_to_datetime_dtarr(self, tz):
         # DatetimeArray
-        dti = date_range('1965-04-03', periods=19, freq='2W')
+        dti = date_range('1965-04-03', periods=19, freq='2W', tz=tz)
         arr = DatetimeArray(dti)
 
         result = to_datetime(arr)
