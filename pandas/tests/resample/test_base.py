6da825212dadcb68cef264c551666f65debcc6ef
Simon Hawkins <simonjayhawkins@gmail.com>
2018-12-23 18:24:03 +0000

    add code comment

--- pandas/tests/resample/test_base.py
+++ pandas/tests/resample/test_base.py
@@ -17,6 +17,11 @@ from pandas.util.testing import (
     assert_almost_equal, assert_frame_equal, assert_index_equal,
     assert_series_equal)
 
+# a fixture value can be overridden by the test parameter value. Note that the
+# value of the fixture can be overridden this way even if the test doesn’t use
+# it directly (doesn’t mention it in the function prototype).
+# see https://docs.pytest.org/en/latest/fixture.html#override-a-fixture-with-direct-test-parametrization  # noqa
+# in this module we override the fixture values defined in conftest.py
 # tuples of '_index_factory,_series_name,_index_start,_index_end'
 DATE_RANGE = (date_range, 'dti', datetime(2005, 1, 1), datetime(2005, 1, 10))
 PERIOD_RANGE = (
