0632a4a6ec32c8f1de242c7c4d3fb258a17ce993
Simon Hawkins <simonjayhawkins@gmail.com>
2018-12-24 17:31:00 +0000

    move tests from test_chaining_and_caching.py

---
+++ pandas/tests/indexing/multiindex/test_chaining_and_caching.py
@@ -0,0 +1,47 @@
+import numpy as np
+import pytest
+
+from pandas import DataFrame, MultiIndex
+from pandas.core import common as com
+
+
+class TestChaining(object):
+
+    def test_detect_chained_assignment(self):
+        # Inplace ops, originally from:
+        # http://stackoverflow.com/questions/20508968/series-fillna-in-a-multiindex-dataframe-does-not-fill-is-this-a-bug
+        a = [12, 23]
+        b = [123, None]
+        c = [1234, 2345]
+        d = [12345, 23456]
+        tuples = [('eyes', 'left'), ('eyes', 'right'), ('ears', 'left'),
+                  ('ears', 'right')]
+        events = {('eyes', 'left'): a,
+                  ('eyes', 'right'): b,
+                  ('ears', 'left'): c,
+                  ('ears', 'right'): d}
+        multiind = MultiIndex.from_tuples(tuples, names=['part', 'side'])
+        zed = DataFrame(events, index=['a', 'b'], columns=multiind)
+
+        with pytest.raises(com.SettingWithCopyError):
+            zed['eyes']['right'].fillna(value=555, inplace=True)
+
+    def test_cache_updating(self):
+        # 5216
+        # make sure that we don't try to set a dead cache
+        a = np.random.rand(10, 3)
+        df = DataFrame(a, columns=['x', 'y', 'z'])
+        tuples = [(i, j) for i in range(5) for j in range(2)]
+        index = MultiIndex.from_tuples(tuples)
+        df.index = index
+
+        # setting via chained assignment
+        # but actually works, since everything is a view
+        df.loc[0]['z'].iloc[0] = 1.
+        result = df.loc[(0, 0), 'z']
+        assert result == 1
+
+        # correct setting
+        df.loc[(0, 0), 'z'] = 2
+        result = df.loc[(0, 0), 'z']
+        assert result == 2
