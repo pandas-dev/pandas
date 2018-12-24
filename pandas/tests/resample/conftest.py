df56265649aa39aad411664a3190a66026d31fcd
Simon Hawkins <simonjayhawkins@gmail.com>
2018-12-24 21:27:39 +0000

    add doc-strings to fixtures

--- pandas/tests/resample/conftest.py
+++ pandas/tests/resample/conftest.py
@@ -57,64 +57,85 @@ def simple_period_range_series():
 
 @pytest.fixture
 def _index_start():
+    """Fixture for parametrization of index, series and frame."""
     return datetime(2005, 1, 1)
 
 
 @pytest.fixture
 def _index_end():
+    """Fixture for parametrization of index, series and frame."""
     return datetime(2005, 1, 10)
 
 
 @pytest.fixture
 def _index_freq():
+    """Fixture for parametrization of index, series and frame."""
     return 'D'
 
 
 @pytest.fixture
 def _index_name():
+    """Fixture for parametrization of index, series and frame."""
     return None
 
 
 @pytest.fixture
 def index(_index_factory, _index_start, _index_end, _index_freq, _index_name):
+    """Fixture for parametrization of date_range, period_range and
+    timedelta_range indexes"""
     return _index_factory(
         _index_start, _index_end, freq=_index_freq, name=_index_name)
 
 
 @pytest.fixture
 def _static_values(index):
+    """Fixture for parametrization of values used in parametrization of
+    Series and DataFrames with date_range, period_range and
+    timedelta_range indexes"""
     return np.arange(len(index))
 
 
 @pytest.fixture
 def _series_name():
+    """Fixture for parametrization of Series name for Series used with
+    date_range, period_range and timedelta_range indexes"""
     return None
 
 
 @pytest.fixture
 def series(index, _series_name, _static_values):
+    """Fixture for parametrization of Series with date_range, period_range and
+    timedelta_range indexes"""
     return Series(_static_values, index=index, name=_series_name)
 
 
 @pytest.fixture
 def empty_series(series):
+    """Fixture for parametrization of empty Series with date_range,
+    period_range and timedelta_range indexes"""
     return series[:0]
 
 
 @pytest.fixture
 def frame(index, _series_name, _static_values):
+    """Fixture for parametrization of DataFrame with date_range, period_range and
+    timedelta_range indexes"""
     # _series_name is intentionally unused
     return DataFrame({'value': _static_values}, index=index)
 
 
 @pytest.fixture
 def empty_frame(series):
+    """Fixture for parametrization of empty DataFrame with date_range,
+    period_range and timedelta_range indexes"""
     index = series.index[:0]
     return DataFrame(index=index)
 
 
 @pytest.fixture(params=[Series, DataFrame])
 def series_and_frame(request, series, frame):
+    """Fixture for parametrization of Series and DataFrame with date_range,
+    period_range and timedelta_range indexes"""
     if request.param == Series:
         return series
     if request.param == DataFrame:
