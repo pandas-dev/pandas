3503e86f4baeaf5134e6474e6d62818bc9f04fad
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2018-12-25 22:20:51 +0200

    fix imports sorting

--- pandas/tests/arrays/sparse/test_array.py
+++ pandas/tests/arrays/sparse/test_array.py
@@ -9,9 +9,10 @@ from pandas._libs.sparse import IntIndex
 from pandas.compat import range
 import pandas.util._test_decorators as td
 
+from pandas.core.dtypes.missing import isna
+
 import pandas as pd
 from pandas.core.sparse.api import SparseArray, SparseDtype, SparseSeries
-from pandas.core.dtypes.missing import isna
 import pandas.util.testing as tm
 from pandas.util.testing import assert_almost_equal
 
