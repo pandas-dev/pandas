336d06113f2ca08f21af4fec4272a9793c7b79de
Simon Hawkins <simonjayhawkins@gmail.com>
2018-12-25 19:52:08 +0000

    refactor import

--- pandas/tests/indexing/multiindex/test_indexing_slow.py
+++ pandas/tests/indexing/multiindex/test_indexing_slow.py
@@ -6,7 +6,7 @@ import numpy as np
 import pytest
 
 import pandas as pd
-from pandas.core.api import DataFrame, MultiIndex, Series
+from pandas import DataFrame, MultiIndex, Series
 import pandas.util.testing as tm
 
 
