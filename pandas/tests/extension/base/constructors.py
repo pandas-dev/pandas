1f3bb508dae496f3044f2516d8b137c6779c0ec4
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-12-28 14:52:58 -0600

    fixed test expected

--- pandas/tests/extension/base/constructors.py
+++ pandas/tests/extension/base/constructors.py
@@ -64,5 +64,6 @@ class BaseConstructorsTests(BaseExtensionTests):
 
     def test_pandas_array_dtype(self, data):
         # ... but specifying dtype will override idempotency
-        result = pd.array(data, dtype=object)
-        self.assert_equal(result, np.asarray(data, dtype=object))
+        result = pd.array(data, dtype=np.dtype(object))
+        expected = pd.arrays.PandasArray(np.asarray(data, dtype=object))
+        self.assert_equal(result, expected)
