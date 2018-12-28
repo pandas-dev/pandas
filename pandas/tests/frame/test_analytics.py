2ddb60f58bd5b9dbf0992b3deb2b1b935619bed2
cgangwar11 <chandan.gangwar0411@gmail.com>
2018-12-29 03:52:30 +0530

    Added test cases for lower cliping as well as combined lower and upper cliping

--- pandas/tests/frame/test_analytics.py
+++ pandas/tests/frame/test_analytics.py
@@ -1975,9 +1975,17 @@ class TestDataFrameAnalytics():
         # GH 20911
         df1 = DataFrame(np.random.randn(1000, 4), columns=['A', 'B', 'C', 'D'])
         df2 = DataFrame(np.random.randn(1000, 4), columns=['D', 'A', 'B', 'C'])
-        result = df1.clip(lower=0, upper=df2)
-        expected = df1.clip(lower=0, upper=df2[df1.columns])
-        tm.assert_frame_equal(result, expected)
+        df3 = DataFrame(df2.values - 1, columns=['B', 'D', 'C', 'A'])
+        result_upper = df1.clip(lower=0, upper=df2)
+        expected_upper = df1.clip(lower=0, upper=df2[df1.columns])
+        result_lower = df1.clip(lower=df3, upper=3)
+        expected_lower = df1.clip(lower=df3[df1.columns], upper=3)
+        result_lower_upper = df1.clip(lower=df3, upper=df2)
+        expected_lower_upper = df1.clip(lower=df3[df1.columns],
+                                        upper=df2[df1.columns])
+        tm.assert_frame_equal(result_upper, expected_upper)
+        tm.assert_frame_equal(result_lower, expected_lower)
+        tm.assert_frame_equal(result_lower_upper, expected_lower_upper)
 
     def test_clip_with_na_args(self, float_frame):
         """Should process np.nan argument as None """
