5fa32e9848406fc2f9f5a1608a7e3545df70b06c
Tom Augspurger <tom.w.augspurger@gmail.com>
2018-12-28 10:40:46 -0600

    Handle uint in astype tests

--- pandas/tests/arrays/test_datetimes.py
+++ pandas/tests/arrays/test_datetimes.py
@@ -53,6 +53,23 @@ class TestDatetimeArray(object):
         result = arr.astype(DatetimeTZDtype(tz="US/Central"), copy=False)
         assert result is arr
 
+    @pytest.mark.parametrize("dtype", [
+        int, np.int32, np.int64, 'uint32', 'uint64',
+    ])
+    def test_astype_int(self, dtype):
+        arr = DatetimeArray._from_sequence([pd.Timestamp('2000'),
+                                            pd.Timestamp('2001')])
+        result = arr.astype(dtype)
+
+        if np.dtype(dtype).kind == 'u':
+            expected_dtype = np.dtype('uint64')
+        else:
+            expected_dtype = np.dtype('int64')
+        expected = arr.astype(expected_dtype)
+
+        assert result.dtype == expected_dtype
+        tm.assert_numpy_array_equal(result, expected)
+
     def test_tz_setter_raises(self):
         arr = DatetimeArray._from_sequence(['2000'], tz='US/Central')
         with pytest.raises(AttributeError, match='tz_localize'):
