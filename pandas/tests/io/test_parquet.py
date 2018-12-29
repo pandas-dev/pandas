41427a34aabaed4c1ae3bb07a99f87e4b3f1243f
Ruijing Li <ruijing.li@berkeley.edu>
2018-12-29 12:01:10 -0800

    removing xfail completely

--- pandas/tests/io/test_parquet.py
+++ pandas/tests/io/test_parquet.py
@@ -8,7 +8,7 @@ from warnings import catch_warnings
 
 import numpy as np
 import pandas as pd
-from pandas.compat import PY3, is_platform_windows
+from pandas.compat import PY3
 from pandas.io.parquet import (to_parquet, read_parquet, get_engine,
                                PyArrowImpl, FastParquetImpl)
 from pandas.util import testing as tm
@@ -200,8 +200,6 @@ def test_options_get_engine(fp, pa):
         assert isinstance(get_engine('fastparquet'), FastParquetImpl)
 
 
-@pytest.mark.xfail(is_platform_windows(),
-                   reason="reading pa metadata failing on Windows")
 def test_cross_engine_pa_fp(df_cross_compat, pa, fp):
     # cross-compat with differing reading/writing engines
 
