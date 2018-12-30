07c6a1359f34b8093aac5deb6cdedb4907997f85
Marc Garcia <garcia.marc@gmail.com>
2018-12-30 21:37:49 +0000

    DOC: Fix building of a single API document (#24506)

--- doc/make.py
+++ doc/make.py
@@ -50,7 +50,7 @@ class DocBuilder:
         if single_doc and single_doc.endswith('.rst'):
             self.single_doc_html = os.path.splitext(single_doc)[0] + '.html'
         elif single_doc:
-            self.single_doc_html = 'generated/pandas.{}.html'.format(
+            self.single_doc_html = 'api/generated/pandas.{}.html'.format(
                 single_doc)
 
     def _process_single_doc(self, single_doc):
