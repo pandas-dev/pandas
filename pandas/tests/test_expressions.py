390f93e4f06056cd2bebec91219af68efc77f7f8
baidoosik <qoentlr37@naver.com>
2018-12-30 10:47:07 +0900

    reflect review

--- pandas/tests/test_expressions.py
+++ pandas/tests/test_expressions.py
@@ -446,19 +446,16 @@ class TestExpressions(object):
 
     @pytest.mark.parametrize("test_input,expected", [
         (DataFrame([[0, 1, 2, 'aa'], [0, 1, 2, 'aa']],
-                   columns=['a', 'b', 'c', 'dtype']).loc[:, ['a', 'dtype']].
-         ne(DataFrame([[0, 1, 2, 'aa'], [0, 1, 2, 'aa']],
-                      columns=['a', 'b', 'c', 'dtype']).
-            loc[:, ['a', 'dtype']]),
-         DataFrame([[False, False], [False, False]], columns=['a', 'dtype'])),
+                   columns=['a', 'b', 'c', 'dtype']),
+         DataFrame([[False, False], [False, False]],
+                   columns=['a', 'dtype'])),
         (DataFrame([[0, 3, 2, 'aa'], [0, 4, 2, 'aa'], [0, 1, 1, 'bb']],
-                   columns=['a', 'b', 'c', 'dtype']).loc[:, ['a', 'dtype']].
-         ne(DataFrame([[0, 3, 2, 'aa'], [0, 4, 2, 'aa'], [0, 1, 1, 'bb']],
-                      columns=['a', 'b', 'c', 'dtype'])
-            .loc[:, ['a', 'dtype']]),
+                   columns=['a', 'b', 'c', 'dtype']),
          DataFrame([[False, False], [False, False],
                    [False, False]], columns=['a', 'dtype'])),
     ])
     def test_bool_ops_column_name_dtype(self, test_input, expected):
         # GH 22383 - .ne fails if columns containing column name 'dtype'
-        assert_frame_equal(test_input, expected)
+        result = test_input.loc[:, ['a', 'dtype']].\
+            ne(test_input.loc[:, ['a', 'dtype']])
+        assert_frame_equal(result, expected)
