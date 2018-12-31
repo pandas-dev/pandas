9f6e7a48154e0a9a30d16f5934e057dd1bf60317
Jorge López <jorge.lopez@neosentec.com>
2018-12-31 11:13:29 +0100

    Fixed linting issues

--- pandas/tests/plotting/test_misc.py
+++ pandas/tests/plotting/test_misc.py
@@ -322,14 +322,17 @@ class TestDataFramePlots(TestPlotBase):
         assert len(color3) == 20
 
     def test_plot_single_color(self):
-        # Example from #20585. All 3 bars in the plot should have the same color
-        df = DataFrame({'account-start': ['2017-02-03', '2017-03-03', '2017-01-01'],
-                        'client': ['Alice Anders', 'Bob Baker', 'Charlie Chaplin'],
+        # Example from #20585. All 3 bars should have the same color
+        df = DataFrame({'account-start': ['2017-02-03', '2017-03-03',
+                                          '2017-01-01'],
+                        'client': ['Alice Anders', 'Bob Baker',
+                                   'Charlie Chaplin'],
                         'balance': [-1432.32, 10.43, 30000.00],
                         'db-id': [1234, 2424, 251],
                         'proxy-id': [525, 1525, 2542],
                         'rank': [52, 525, 32],
                         })
         ax = df.client.value_counts().plot.bar()
-        colors = lmap(lambda rect: rect.get_facecolor(), ax.get_children()[0:3])
+        colors = lmap(lambda rect: rect.get_facecolor(),
+                      ax.get_children()[0:3])
         assert all(color == colors[0] for color in colors)
