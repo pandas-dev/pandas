5a1da48dcb3beba1534158de316cdd0be1d05bf6
Jorge López <jorge.lopez@neosentec.com>
2019-01-01 19:19:07 +0100

    Added comment in boxplot._get_colors

--- pandas/plotting/_core.py
+++ pandas/plotting/_core.py
@@ -2172,6 +2172,8 @@ def boxplot(data, column=None, by=None, ax=None, fontsize=None,
         column = 'x'
 
     def _get_colors():
+        #  num_colors=3 is required as method maybe_color_bp takes the colors
+        #  in positions 0 and 2.
         return _get_standard_colors(color=kwds.get('color'), num_colors=3)
 
     def maybe_color_bp(bp):
