eb7ce62ee7dd4e14dbe39edcaec83874ee82babf
Brock Mendel <jbrockmendel@gmail.com>
2019-01-04 09:07:15 -0800

    isort fixup

--- pandas/core/dtypes/missing.py
+++ pandas/core/dtypes/missing.py
@@ -10,9 +10,9 @@ from .common import (
     _NS_DTYPE, _TD_DTYPE, ensure_object, is_bool_dtype, is_complex_dtype,
     is_datetime64_dtype, is_datetime64tz_dtype, is_datetimelike,
     is_datetimelike_v_numeric, is_dtype_equal, is_extension_array_dtype,
-    is_float_dtype, is_integer_dtype, is_object_dtype,
-    is_period_dtype, is_scalar, is_string_dtype, is_string_like_dtype,
-    is_timedelta64_dtype, needs_i8_conversion, pandas_dtype)
+    is_float_dtype, is_integer_dtype, is_object_dtype, is_period_dtype,
+    is_scalar, is_string_dtype, is_string_like_dtype, is_timedelta64_dtype,
+    needs_i8_conversion, pandas_dtype)
 from .generic import (
     ABCDatetimeArray, ABCExtensionArray, ABCGeneric, ABCIndexClass,
     ABCMultiIndex, ABCSeries, ABCTimedeltaArray)
