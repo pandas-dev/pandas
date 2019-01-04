81cfd82b8a13e4cf181f890130ea46502363e9c9
H. Vetinari <h.vetinari@gmx.com>
2019-01-05 00:57:43 +0100

    Review (WillAyd)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -655,7 +655,7 @@ class NDFrame(PandasObject, SelectionMixin):
 
         See Also
         --------
-        %(other_klass)s.set_index: Method adapted for %(other_klass)s.
+        %(other_klass)s.set_index : Method adapted for %(other_klass)s.
         %(klass)s.reset_index : Opposite of set_index.
         %(klass)s.reindex : Change to new indices or expand indices.
         %(klass)s.reindex_like : Change to same indices as other %(klass)s.
