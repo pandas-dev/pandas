ad8ab5f542626e7c018236c2c62515af623958e8
Matt Roeschke <mroeschke@housecanary.com>
2019-01-04 23:54:04 -0800

    test merge_asof with by_col tz-aware

--- pandas/tests/reshape/merge/test_merge_asof.py
+++ pandas/tests/reshape/merge/test_merge_asof.py
@@ -1022,3 +1022,19 @@ class TestAsOfMerge(object):
                 merge_asof(df_null, df, on='a')
             else:
                 merge_asof(df, df_null, on='a')
+
+    def test_merge_by_col_tz_aware(self):
+        # GH 21184
+        left = pd.DataFrame(
+            {'by_col': pd.DatetimeIndex(['2018-01-01']).tz_localize('UTC'),
+             'on_col': [2], 'values': ['a']})
+        right = pd.DataFrame(
+            {'by_col': pd.DatetimeIndex(['2018-01-01']).tz_localize('UTC'),
+             'on_col': [1], 'values': ['b']})
+        result = pd.merge_asof(left, right, by='by_col', on='on_col')
+        expected = pd.DataFrame([
+            [pd.Timestamp('2018-01-01', tz='UTC'), 2, 'a', 'b']
+        ],
+                                 columns=['by_col', 'on_col', 'values_x',
+                                          'values_y'])
+        assert_frame_equal(result, expected)
