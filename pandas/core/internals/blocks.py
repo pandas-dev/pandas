401abd8d287a4452f6e87f1b7b53b356b7126583
Brock Mendel <jbrockmendel@gmail.com>
2019-01-05 15:38:58 -0800

    best guess at feather fix

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -3078,7 +3078,7 @@ def make_block(values, placement, klass=None, ndim=None, dtype=None,
     elif klass is DatetimeTZBlock and not is_datetime64tz_dtype(values):
         # TODO: This is no longer hit internally; does it need to be retained
         #  for e.g. pyarrow?
-        values = DatetimeArray(values, dtype)
+        values = DatetimeArray(values.view('i8'), dtype)
 
     return klass(values, ndim=ndim, placement=placement)
 
