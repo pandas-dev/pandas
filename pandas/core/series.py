959f0e2a1f81eb621f7b5b77729c0c6d8ab31114
makbigc <makszechun@gmail.com>
2019-01-05 17:52:04 +0800

    Update deprecation msg in nonzero

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -575,7 +575,7 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
         """
         msg = ("Series.nonzero() is deprecated "
                "and will be removed in a future version."
-               "Use Series.to_numpy().non_zero() instead")
+               "Use Series.to_numpy().nonzero() instead")
         warnings.warn(msg, FutureWarning, stacklevel=2)
         return self._values.nonzero()
 
