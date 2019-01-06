d5e478ca3c60635b0327b63713f1bcb61f5a93d8
Matthew Roeschke <emailformattr@gmail.com>
2019-01-06 07:53:48 -0800

    TST: Fixed timezone issues post DatetimeArray refactor (#24634)

--- pandas/tests/indexes/datetimes/test_astype.py
+++ pandas/tests/indexes/datetimes/test_astype.py
@@ -293,6 +293,15 @@ class TestToPeriod(object):
 
         tm.assert_index_equal(result, expected)
 
+    @pytest.mark.parametrize('tz', ['Etc/GMT-1', 'Etc/GMT+1'])
+    def test_to_period_tz_utc_offset_consistency(self, tz):
+        # GH 22905
+        ts = pd.date_range('1/1/2000', '2/1/2000', tz='Etc/GMT-1')
+        with tm.assert_produces_warning(UserWarning):
+            result = ts.to_period()[0]
+            expected = ts[0].to_period()
+            assert result == expected
+
     def test_to_period_nofreq(self):
         idx = DatetimeIndex(['2000-01-01', '2000-01-02', '2000-01-04'])
         with pytest.raises(ValueError):
