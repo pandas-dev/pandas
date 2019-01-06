abaf652b3064e522492203bffa3bbb24bfd9af54
Saurav Chakravorty <sauravchakravorty@gmail.com>
2019-01-06 13:02:51 +0530

    further cleanup

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -1,7 +1,6 @@
-#!/bin/bash
+#!/bin/bash -e
 
 
-set -v -e
 # edit the locale file if needed
 function edit_init()
 {
@@ -13,6 +12,7 @@ function edit_init()
         echo "[head -4 pandas/__init__.py]"
         head -4 pandas/__init__.py
         echo
+        sudo locale-gen "$LOCALE_OVERRIDE"
     fi
 }
 
@@ -20,86 +20,88 @@ edit_init
 
 home_dir=$(pwd)
 echo
-echo "[home_dir]: $home_dir"
+echo "home_dir: $home_dir"
 
 # install miniconda
 MINICONDA_DIR="$HOME/miniconda3"
 
 echo
-echo "[Using clean Miniconda install]"
+echo "Using clean Miniconda install"
 
 if [ -d "$MINICONDA_DIR" ]; then
     rm -rf "$MINICONDA_DIR"
 fi
 
-
-# Install Miniconda
+echo "Install Miniconda"
 unamestr=`uname`
 if [[ "$unamestr" == 'Linux' ]]; then
     if [[ "$BITS32" == "yes" ]]; then
-        wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86.sh -O miniconda.sh
+        conda_dist = "Linux-x86.sh"
     else
-        wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+        conda_dist = "Linux-x86_64.sh"
     fi
 elif [[ "$unamestr" == 'Darwin' ]]; then
-    wget -q https://repo.continuum.io/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O miniconda.sh
+    conda_dist = "MacOSX-x86_64.sh"
 else
-  echo Error
+  echo "OS $unamestr not supported"
+  exit 1
 fi
+
+wget -q "https://repo.continuum.io/miniconda/Miniconda3-latest-$conda_dist" -O miniconda.sh
 chmod +x miniconda.sh
 ./miniconda.sh -b
 
-
 echo
-echo "[show conda]"
+echo "Show conda"
 which conda
 
 echo
-echo "[update conda]"
-conda config --set ssl_verify false || exit 1
-conda config --set quiet true --set always_yes true --set changeps1 false || exit 1
+echo "Update conda"
+conda config --set ssl_verify false
+conda config --set quiet true --set always_yes true --set changeps1 false
 conda update -q conda
 
 # Useful for debugging any issues with conda
-conda info -a || exit 1
+conda info -a
 
-# set the compiler cache to work
 echo
+echo "set the compiler cache to work"
 if [ -z "$NOCACHE" ] && [ "${TRAVIS_OS_NAME}" == "linux" ]; then
-    echo "[Using ccache]"
+    echo "Using ccache"
     export PATH=/usr/lib/ccache:/usr/lib64/ccache:$PATH
     gcc=$(which gcc)
-    echo "[gcc]: $gcc"
+    echo "gcc: $gcc"
     ccache=$(which ccache)
-    echo "[ccache]: $ccache"
+    echo "ccache: $ccache"
     export CC='ccache gcc'
 elif [ -z "$NOCACHE" ] && [ "${TRAVIS_OS_NAME}" == "osx" ]; then
-    echo "[Install ccache]"
+    echo "Install ccache"
     brew install ccache > /dev/null 2>&1
-    echo "[Using ccache]"
+    echo "Using ccache"
     export PATH=/usr/local/opt/ccache/libexec:$PATH
     gcc=$(which gcc)
-    echo "[gcc]: $gcc"
+    echo "gcc: $gcc"
     ccache=$(which ccache)
-    echo "[ccache]: $ccache"
+    echo "ccache: $ccache"
 else
-    echo "[Not using ccache]"
+    echo "Not using ccache"
 fi
 
-# Deactivate any environment
+echo "Deactivate any environment"
 source deactivate
-# Display root environment (for debugging)
+
+echo "Display root environment (for debugging)"
 conda list
+
 # Clean up any left-over from a previous build
 # (note workaround for https://github.com/conda/conda/issues/2679:
 #  `conda env remove` issue)
 conda remove --all -q -y -n pandas-dev
 
-echo
-echo "[create env]"
-
 # create our environment
-time conda env create -q --file="${ENV_FILE}" || exit 1
+echo
+echo "Create environment"
+time conda env create -q --file="${ENV_FILE}"
 
 set +v
 source activate pandas-dev
@@ -108,45 +110,40 @@ set -v
 # remove any installed pandas package
 # w/o removing anything else
 echo
-echo "[removing installed pandas]"
+echo "Removing installed pandas"
 conda remove pandas -y --force || true
 pip uninstall -y pandas || true
 
 echo
-echo "[no installed pandas]"
+echo "No installed pandas"
 conda list pandas
 
-
-if [ -n "$LOCALE_OVERRIDE" ]; then
-    sudo locale-gen "$LOCALE_OVERRIDE"
-fi
-
-#!/bin/bash
-
 # Make sure any error below is reported as such
-set -v -e
 
-echo "[building extensions]"
+echo "Build extensions and install pandas"
 python setup.py build_ext -q --inplace
 python -m pip install -e .
 
 echo
-echo "[show environment]"
+echo "Show environment"
 conda list
 
-echo
-echo "[done]"
-
-#!/bin/bash
-
+# Install DB for Linux
 if [ "${TRAVIS_OS_NAME}" != "linux" ]; then
    echo "not using dbs on non-linux"
-   exit 0
+else
+   echo "installing dbs"
+   mysql -e 'create database pandas_nosetest;'
+   psql -c 'create database pandas_nosetest;' -U postgres
 fi
 
-echo "installing dbs"
-mysql -e 'create database pandas_nosetest;'
-psql -c 'create database pandas_nosetest;' -U postgres
+echo
+echo "Setup virtual framebuffer"
+export DISPLAY=":99.
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+   sh -e /etc/init.d/xvfb start
+   sleep 3
+fi
 
 echo "done"
 exit 0
