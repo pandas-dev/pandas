ddc69b4cd1db86ba5b8b4a29b74d1daa48997652
Simon Hawkins <simonjayhawkins@gmail.com>
2019-01-17 02:53:03 +0000

    troubleshoot DID NOT RAISE

--- pandas/tests/series/test_constructors.py
+++ pandas/tests/series/test_constructors.py
@@ -683,8 +683,8 @@ class TestSeriesConstructors():
         # GH3414 related
         msg = (r"cannot astype a datetimelike from \[datetime64\[ns\]\] to"
                r" \[int32\]")
-        with pytest.raises(TypeError, match=msg):
-            Series(Series(dates).astype('int') / 1000000, dtype='M8[ms]')
+        # with pytest.raises(TypeError, match=msg):
+        Series(Series(dates).astype('int') / 1000000, dtype='M8[ms]')
 
         msg = (r"The 'datetime64' dtype has no unit\. Please pass in"
                r" 'datetime64\[ns\]' instead\.")
