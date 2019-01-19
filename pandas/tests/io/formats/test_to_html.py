bfad29ef882ac2de975cefa0abac27549fd6229b
Simon Hawkins <simonjayhawkins@gmail.com>
2019-01-19 21:22:08 +0000

    remove trailing whitespace

--- pandas/tests/io/formats/test_to_html.py
+++ pandas/tests/io/formats/test_to_html.py
@@ -15,7 +15,7 @@ from pandas.util import testing as tm
 
 import pandas.io.formats.format as fmt
 
-lorum_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  # noqa    
+lorum_ipsum = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."  # noqa
 
 
 def expected_html(datapath, name):
