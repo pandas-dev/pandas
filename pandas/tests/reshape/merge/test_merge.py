e22a6c848637a807c415936c7ae8a7bd53758985
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-01-28 15:01:32 +0100

    Revert "Backport PR #24916: BUG-24212 fix regression in #24897 (#24951)"
    This reverts commit 84056c52e3f20ab44921b86a8e0f05275bf8ddb4.

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -939,22 +939,25 @@ class TestMerge(object):
         with np.errstate(divide='raise'):
             merge(a, a, on=('a', 'b'))
 
-    @pytest.mark.parametrize('how', ['right', 'outer'])
+    @pytest.mark.parametrize('how', ['left', 'outer'])
+    @pytest.mark.xfail(reason="GH-24897")
     def test_merge_on_index_with_more_values(self, how):
         # GH 24212
-        # pd.merge gets [0, 1, 2, -1, -1, -1] as left_indexer, ensure that
-        # -1 is interpreted as a missing value instead of the last element
-        df1 = pd.DataFrame({'a': [1, 2, 3], 'key': [0, 2, 2]})
-        df2 = pd.DataFrame({'b': [1, 2, 3, 4, 5]})
-        result = df1.merge(df2, left_on='key', right_index=True, how=how)
-        expected = pd.DataFrame([[1.0, 0, 1],
-                                 [2.0, 2, 3],
-                                 [3.0, 2, 3],
-                                 [np.nan, 1, 2],
-                                 [np.nan, 3, 4],
-                                 [np.nan, 4, 5]],
-                                columns=['a', 'key', 'b'])
-        expected.set_index(Int64Index([0, 1, 2, 1, 3, 4]), inplace=True)
+        # pd.merge gets [-1, -1, 0, 1] as right_indexer, ensure that -1 is
+        # interpreted as a missing value instead of the last element
+        df1 = pd.DataFrame([[1, 2], [2, 4], [3, 6], [4, 8]],
+                           columns=['a', 'b'])
+        df2 = pd.DataFrame([[3, 30], [4, 40]],
+                           columns=['a', 'c'])
+        df1.set_index('a', drop=False, inplace=True)
+        df2.set_index('a', inplace=True)
+        result = pd.merge(df1, df2, left_index=True, right_on='a', how=how)
+        expected = pd.DataFrame([[1, 2, np.nan],
+                                 [2, 4, np.nan],
+                                 [3, 6, 30.0],
+                                 [4, 8, 40.0]],
+                                columns=['a', 'b', 'c'])
+        expected.set_index('a', drop=False, inplace=True)
         assert_frame_equal(result, expected)
 
     def test_merge_right_index_right(self):
