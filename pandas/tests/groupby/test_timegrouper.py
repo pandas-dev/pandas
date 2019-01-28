5c07d25f6ffb0d766b9e5f21c5f8977a2898b0f9
Alexander Buchkovsky <olex.buchkovsky@gmail.com>
2019-01-29 01:12:58 +0200

    move tests

--- pandas/tests/groupby/test_timegrouper.py
+++ pandas/tests/groupby/test_timegrouper.py
@@ -650,17 +650,3 @@ class TestGroupBy(object):
         expected = grouped.count()
 
         assert_frame_equal(result, expected)
-
-    def test_groupby_with_dst_time_change(self):
-        # GH 24972
-        index = pd.DatetimeIndex([1478064900001000000, 1480037118776792000],
-                                 tz='UTC').tz_convert('America/Chicago')
-
-        df = pd.DataFrame([1, 2], index=index)
-        result = df.groupby(pd.Grouper(freq='1d')).last()
-        expected_index_values = pd.date_range('2016-11-02', '2016-11-24',
-                                              freq='d', tz='America/Chicago')
-
-        index = pd.DatetimeIndex(expected_index_values)
-        expected = pd.DataFrame([1.0] + ([np.nan] * 21) + [2.0], index=index)
-        assert_frame_equal(result, expected)
