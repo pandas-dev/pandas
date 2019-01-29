642b01a760622ad780a07d87e31376e26510610c
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-01-29 07:19:41 -0600

    py2 compat

--- pandas/tests/extension/numpy_/conftest.py
+++ pandas/tests/extension/numpy_/conftest.py
@@ -3,41 +3,42 @@ import pytest
 # A set of Base EA tests that are know to not work for
 # the object-dtype PandasArray holding nested data.
 skips = {
-    'BaseCastingTests.test_astype_str',
-    'BaseConstructorsTests.test_array_from_scalars',
+    'TestCasting.test_astype_str',
+    'TestConstructors.test_array_from_scalars',
     # tuple isn't instance of np.object
-    'BaseGetitemTests.test_getitem_scalar',
+    'TestGetitem.test_getitem_scalar',
     # Can't pass tuples to _from_sequence
-    'BaseGetitemTests.test_take_series',
+    'TestGetitem.test_take_series',
     # np.array shape inference
-    'BaseInterfaceTests.test_array_interface',
+    'TestInterface.test_array_interface',
     # Can't construct expected.
-    'BaseMethodsTests.test_unique',
-    'BaseMethodsTests.test_combine_add',
-    'BaseMethodsTests.test_shift_fill_value',
-    'BaseMethodsTests.test_where_series',
-    'BaseMethodsTests.test_repeat',
+    'TestMethods.test_unique',
+    'TestMethods.test_combine_add',
+    'TestMethods.test_shift_fill_value',
+    'TestMethods.test_where_series',
+    'TestMethods.test_repeat',
     # Can't hash ndarray[tuple]
-    'BaseMethodsTests.test_hash_pandas_object_works',
+    'TestMethods.test_hash_pandas_object_works',
     # Can't construct expected.
-    'BaseReshapingTests.test_merge',
-    'BaseReshapingTests.test_merge_on_extension_array',
-    'BaseReshapingTests.test_merge_on_extension_array_duplicates',
+    'TestReshaping.test_merge',
+    'TestReshaping.test_merge_on_extension_array',
+    'TestReshaping.test_merge_on_extension_array_duplicates',
 
     # ndarray setting
-    'BaseSetitemTests.test_setitem_scalar_series',
-    'BaseSetitemTests.test_setitem_sequence',
-    'BaseSetitemTests.test_setitem_sequence_mismatched_length_raises',
-    'BaseSetitemTests.test_setitem_sequence_broadcasts',
-    'BaseSetitemTests.test_setitem_sequence_broadcasts',
-    'BaseSetitemTests.test_setitem_loc_scalar_mixed',
-    'BaseSetitemTests.test_setitem_iloc_scalar_mixed',
-    'BaseSetitemTests.test_setitem_loc_scalar_multiple_homogoneous',
-    'BaseSetitemTests.test_setitem_iloc_scalar_multiple_homogoneous',
-    'BaseSetitemTests.test_setitem_mask_broadcast',
-    'BaseSetitemTests.test_setitem_scalar_key_sequence_raise',
+    'TestSetitem.test_setitem_scalar_series',
+    'TestSetitem.test_setitem_sequence',
+    'TestSetitem.test_setitem_sequence_mismatched_length_raises',
+    'TestSetitem.test_setitem_sequence_broadcasts',
+    'TestSetitem.test_setitem_sequence_broadcasts',
+    'TestSetitem.test_setitem_loc_scalar_mixed',
+    'TestSetitem.test_setitem_iloc_scalar_mixed',
+    'TestSetitem.test_setitem_loc_scalar_multiple_homogoneous',
+    'TestSetitem.test_setitem_iloc_scalar_multiple_homogoneous',
+    'TestSetitem.test_setitem_mask_broadcast',
+    'TestSetitem.test_setitem_scalar_key_sequence_raise',
 
-    'BaseParsingTests.test_EA_types',
+    # parsing differs.
+    'TestParsing.test_EA_types',
 }
 
 
@@ -48,6 +49,7 @@ def pytest_collection_modifyitems(config, items):
         # supplied to a fixture. Right now .keywords gets things
         # like 'object' or 'data-object'.
         parts = item.name.split("[")
+        qualname = item.parent.obj.__class__.__name__ + '.' + item.obj.__name__
         if (len(parts) > 1 and 'object' in item.name.split('[')[1]
-                and item.obj.__qualname__ in skips):
+                and qualname in skips):
             item.add_marker(skip)
