d35ec842ae7308a6fba31bb296d8e5c049bb5aed
Daniel Costa <danielcosta@Daniels-MacBook-Air.local>
2019-01-29 16:55:22 -0200

    only look for return commands after docstring

--- scripts/validate_docstrings.py
+++ scripts/validate_docstrings.py
@@ -691,9 +691,10 @@ def get_validation_data(doc):
 
     if doc.is_function_or_method:
         if not doc.returns:
-            if re.search('\n[ \t\f\v]*return' +
+            if re.search('\"\"\".*\"\"\".*' +
+                         '\n[ \t\f\v]*return' +
                          '(?![ \t\f\v]*(None)?[ \t\f\v]*[\n#])',
-                         doc.method_source):
+                         doc.method_source, flags=re.DOTALL):
                 errs.append(error('RT01'))
         else:
             if len(doc.returns) == 1 and doc.returns[0][1]:
