f44e0cac0874cc6616a857ab264dbc7e876eabbb
Brock Mendel <jbrockmendel@gmail.com>
2019-01-30 10:10:15 -0800

    revert

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -2072,7 +2072,17 @@ class DatetimeLikeBlockMixin(object):
         return object dtype as boxed values, such as Timestamps/Timedelta
         """
         if is_object_dtype(dtype):
-            return self.values.astype(object)
+            values = self.values
+
+            if self.ndim > 1:
+                values = values.ravel()
+
+            values = lib.map_infer(values, self._box_func)
+
+            if self.ndim > 1:
+                values = values.reshape(self.values.shape)
+
+            return values
         return self.values
 
 
