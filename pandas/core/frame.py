7cc4c372c300ce168ccb2937453b9caa88fe3a2d
Samuel <samuelsinayoko@bmlltech.com>
2019-02-02 15:56:02 +0000

    revert initial bugfix
    Write tests first

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -4615,7 +4615,8 @@ class DataFrame(NDFrame):
                 else:
                     raise TypeError('must specify how or thresh')
 
-            result = self._take(mask.to_numpy().nonzero()[0], axis=axis)
+            #result = self._take(mask.to_numpy().nonzero()[0], axis=axis)
+            result = self.loc(axis=axis)[mask]
 
         if inplace:
             self._update_inplace(result)
