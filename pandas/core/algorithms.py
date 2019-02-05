6e658f84568fd0da342b172b6879432b5e470a39
thoo <theinhtay@gmail.com>
2019-02-04 19:50:16 -0500

    fix bullets

--- pandas/core/algorithms.py
+++ pandas/core/algorithms.py
@@ -289,10 +289,9 @@ def unique(values):
     Returns
     -------
     unique values.
-        The return can be:
-        - If the input is an Index, the return is an Index.
-        - If the input is a Categorical dtype, the return is a Categorical.
-        - If the input is a Series/ndarray, the return will be an ndarray.
+        If the input is an Index, the return is an Index.\n
+        If the input is a Categorical dtype, the return is a Categorical.\n
+        If the input is a Series/ndarray, the return will be an ndarray.
 
     See Also
     --------
