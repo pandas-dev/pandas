eb0b08e366b0f69b72fcfde6008fdc65a9126a65
Gioia Ballin <gioia.ballin@made.com>
2019-02-08 18:51:02 +0000

    Add feedback after further code review

--- pandas/tests/frame/test_apply.py
+++ pandas/tests/frame/test_apply.py
@@ -319,9 +319,11 @@ class TestDataFrameApply():
         assert_series_equal(result, expected)
 
     def test_apply_reduce_rows_to_dict(self):
+        # GH 25196
         data = pd.DataFrame([[1, 2], [3, 4]])
         expected = pd.Series([{0: 1, 1: 3}, {0: 2, 1: 4}])
-        assert_series_equal(data.apply(dict), expected)
+        result = data.apply(dict)
+        assert_series_equal(result, expected)
 
     def test_apply_differently_indexed(self):
         df = DataFrame(np.random.randn(20, 10))
