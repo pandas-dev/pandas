ab34a5ce6a79d11560a91bd0b5cfd8cadf99dc06
Matt Roeschke <mroeschke@housecanary.com>
2019-02-11 13:48:12 -0800

    Add more tests

--- pandas/tests/groupby/test_transform.py
+++ pandas/tests/groupby/test_transform.py
@@ -836,11 +836,12 @@ def test_groupby_transform_rename():
     tm.assert_series_equal(result_single, expected['value'])
 
 
-def test_groupby_transform_timezone_column():
+@pytest.mark.parametrize('func', [min, max, np.min, np.max])
+def test_groupby_transform_timezone_column(func):
     # GH 24198
     ts = pd.to_datetime('now', utc=True).tz_convert('Asia/Singapore')
     result = pd.DataFrame({'end_time': [ts], 'id': [1]})
-    result['max_end_time'] = result.groupby('id').end_time.transform(max)
+    result['max_end_time'] = result.groupby('id').end_time.transform(func)
     expected = pd.DataFrame([[ts, 1, ts]], columns=['end_time', 'id',
                                                     'max_end_time'])
     tm.assert_frame_equal(result, expected)
