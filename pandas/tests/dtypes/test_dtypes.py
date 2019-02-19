989942e3148bf9963091ef0153e6be1dcb8f5559
zangell44 <42625717+zangell44@users.noreply.github.com>
2019-02-19 11:04:33 -0500

    error message formatting fix for PEP8

--- pandas/tests/dtypes/test_dtypes.py
+++ pandas/tests/dtypes/test_dtypes.py
@@ -537,7 +537,7 @@ class TestIntervalDtype(Base):
     def test_construction_from_string_error_subtype(self, string):
         # this is an invalid subtype
         msg = ("Incorrectly formatted string passed to constructor. "
-               "Valid formats include Interval or Inverval\[dtype\] "
+               r"Valid formats include Interval or Inverval\[dtype\] "
                "where dtype is numeric, datetime, or timedelta")
 
         with pytest.raises(TypeError, match=msg):
