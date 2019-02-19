54bcf76e5922fe19c27dbb743161b80e56d233ef
zangell44 <42625717+zangell44@users.noreply.github.com>
2019-02-18 20:12:45 -0500

    construct from string formatting fixes

--- pandas/core/dtypes/dtypes.py
+++ pandas/core/dtypes/dtypes.py
@@ -933,7 +933,7 @@ class IntervalDtype(PandasExtensionDtype, ExtensionDtype):
         """
         if isinstance(string, compat.string_types):
             if (string.lower() == 'interval' or
-                cls._match.search(string) is not None):
+               cls._match.search(string) is not None):
                     return cls(string)
             else:
                 msg = ("Incorrectly formatted string passed to constructor. "
