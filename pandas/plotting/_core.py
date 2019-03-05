bbdd8be0b5d6f43d2e090b3952a6cf19d835c963
Bharat123Rox <bharatr@symphonyai.com>
2019-03-05 12:17:19 +0530

    BUG: Fix #25481 by fixing the error message in TypeError

--- pandas/plotting/_core.py
+++ pandas/plotting/_core.py
@@ -360,8 +360,7 @@ class MPLPlot(object):
 
         # no empty frames or series allowed
         if is_empty:
-            raise TypeError('Empty {0!r}: no numeric data to '
-                            'plot'.format(numeric_data.__class__.__name__))
+            raise TypeError('no numeric data to plot')
 
         self.data = numeric_data
 
