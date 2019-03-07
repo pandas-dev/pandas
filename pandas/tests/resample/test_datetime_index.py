c972dbcffeb5356a7f7a51b857efa4b79ae3acf9
HE, Tao <sighingnow@gmail.com>
2019-03-07 15:51:36 +0800

    Add IntegerArray.size, fix #25580.
    Signed-off-by: HE, Tao <sighingnow@gmail.com>

--- pandas/tests/resample/test_datetime_index.py
+++ pandas/tests/resample/test_datetime_index.py
@@ -101,6 +101,18 @@ def test_resample_basic(series, closed, expected):
     assert_series_equal(result, expected)
 
 
+def test_resample_integerarray():
+    # resample on IntegerArray
+    ts = pd.Series(range(9),
+                   index=pd.date_range('1/1/2000', periods=9, freq='T'),
+                   dtype='Int64')
+    result = ts.resample('3T').sum()
+    expected = Series([3, 12, 21],
+                      index=pd.date_range('1/1/2000', periods=3, freq='3T'),
+                      dtype="Int64")
+    assert_series_equal(result, expected)
+
+
 def test_resample_basic_grouper(series):
     s = series
     result = s.resample('5Min').last()
