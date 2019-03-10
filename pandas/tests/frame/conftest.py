b84dc2f4447cbea4180795d420fb2ce7f2af3df1
H. Vetinari <h.vetinari@gmx.com>
2019-03-10 15:10:35 +0100

    Fixturize tests/frame/test_asof.py

--- pandas/tests/frame/conftest.py
+++ pandas/tests/frame/conftest.py
@@ -68,6 +68,18 @@ def datetime_frame():
 
 
 @pytest.fixture
+def date_range_frame():
+    """
+    Fixture for DataFrame of ints with date_range index
+
+    Columns are ['A', 'B'].
+    """
+    N = 50
+    rng = date_range('1/1/1990', periods=N, freq='53s')
+    return DataFrame({'A': np.arange(N), 'B': np.arange(N)}, index=rng)
+
+
+@pytest.fixture
 def float_string_frame():
     """
     Fixture for DataFrame of floats and strings with index of unique strings
