f70f2b6a9dea950ca5b2f6f83dd0c58efcc3aaa1
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2019-03-11 08:55:46 -0500

    Backport PR #25629: Suppress incorrect warning in nargsort for timezone-aware DatetimeIndex (#25657)

--- pandas/tests/test_sorting.py
+++ pandas/tests/test_sorting.py
@@ -7,7 +7,8 @@ import numpy as np
 from numpy import nan
 import pytest
 
-from pandas import DataFrame, MultiIndex, Series, compat, concat, merge
+from pandas import (
+    DataFrame, MultiIndex, Series, compat, concat, merge, to_datetime)
 from pandas.core import common as com
 from pandas.core.sorting import (
     decons_group_index, get_group_index, is_int64_overflow_possible,
@@ -180,6 +181,13 @@ class TestSorting(object):
                           na_position='first')
         exp = list(range(5)) + list(range(105, 110)) + list(range(104, 4, -1))
         tm.assert_numpy_array_equal(result, np.array(exp), check_dtype=False)
+
+    def test_nargsort_datetimearray_warning(self):
+        # https://github.com/pandas-dev/pandas/issues/25439
+        # can be removed once the FutureWarning for np.array(DTA) is removed
+        data = to_datetime([0, 2, 0, 1]).tz_localize('Europe/Brussels')
+        with tm.assert_produces_warning(None):
+            nargsort(data)
 
 
 class TestMerge(object):
