3005aed6b9877217a3a45785aa246c2a8b67fd22
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-03-14 06:26:32 -0500

    isort?

--- pandas/tests/arrays/sparse/test_accessor.py
+++ pandas/tests/arrays/sparse/test_accessor.py
@@ -3,9 +3,10 @@ import string
 import numpy as np
 import pytest
 
+import pandas.util._test_decorators as td
+
 import pandas as pd
 import pandas.util.testing as tm
-import pandas.util._test_decorators as td
 
 
 class TestSeriesAccessor(object):
