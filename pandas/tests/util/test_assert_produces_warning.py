c05306d6162231220a4eeb17c08f028c08bcb47c
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-03-14 06:19:30 -0500

    take 2

--- pandas/tests/util/test_assert_produces_warning.py
+++ pandas/tests/util/test_assert_produces_warning.py
@@ -5,12 +5,19 @@ import pytest
 import pandas.util.testing as tm
 
 
-def f():
-    warnings.warn('f1', FutureWarning)
-    warnings.warn('f2', RuntimeWarning)
+def f(a=FutureWarning, b=RuntimeWarning):
+    warnings.warn('f1', a)
+    warnings.warn('f2', b)
 
 
 @pytest.mark.filterwarnings('ignore:f1:FutureWarning')
+@pytest.mark.filterwarnings('ignore:f2:RuntimeWarning')
 def test_assert_produces_warning_honors_filter():
-    with tm.assert_produces_warning(RuntimeWarning, filter_level=None):
+    with tm.assert_produces_warning(RuntimeWarning):
         f()
+
+
+@pytest.mark.filterwarnings('ignore:f1:FutureWarning')
+def test_assert_produces_warning_message():
+    with tm.assert_produces_warning(FutureWarning, message='f2'):
+        f(FutureWarning, FutureWarning)
