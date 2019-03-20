7164f61069085315fb198bf441782b88b3f9d303
Brock Mendel <jbrockmendel@gmail.com>
2019-03-19 21:07:46 -0700

    Change to absolute import

--- pandas/_config/__init__.py
+++ pandas/_config/__init__.py
@@ -3,4 +3,4 @@ pandas._config is considered explicitly upstream of everything else in pandas,
 should have no intra-pandas dependencies.
 """
 __all__ = ["config"]
-from . import config
+from pandas._config import config
