d82c15d91a66d7c2c0411c076c6575cfd02f4dde
Brock Mendel <jbrockmendel@gmail.com>
2019-03-22 10:47:32 -0700

    fixup import

--- pandas/_config/__init__.py
+++ pandas/_config/__init__.py
@@ -3,4 +3,5 @@ pandas._config is considered explicitly upstream of everything else in pandas,
 should have no intra-pandas dependencies.
 """
 __all__ = ["config", "get_option"]
-from pandas._config import config, get_option
+from pandas._config import config
+from pandas._config.config import get_option
