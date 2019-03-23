c565d7d7e9f596f72143b2f717beed6768145008
Will Ayd <william.ayd@icloud.com>
2019-03-23 14:27:31 -0700

    Changed code check to intentionally fail

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -264,7 +264,7 @@ if [[ -z "$CHECK" || "$CHECK" == "typing" ]]; then
     mypy --version
 
     MSG='Performing static analysis on items in mypy_whitelist.txt' ; echo $MSG
-    mypy @mypy_whitelist.txt
+    mypy pandas
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 fi
 
