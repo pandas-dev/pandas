c093df1f7d3247225a8496dce2563ac4a0acf65d
Brock Mendel <jbrockmendel@gmail.com>
2019-03-24 12:16:58 -0700

    update imports

--- pandas/_config/__init__.py
+++ pandas/_config/__init__.py
@@ -2,9 +2,12 @@
 pandas._config is considered explicitly upstream of everything else in pandas,
 should have no intra-pandas dependencies.
 """
-__all__ = ["config", "get_option", "set_option", "reset_option",
-           "describe_option", "option_context", "options"]
+__all__ = ["config", "detect_console_encoding", "get_option", "set_option",
+           "reset_option", "describe_option", "option_context", "options"]
 from pandas._config import config
 from pandas._config.config import (
     describe_option, get_option, option_context, options, reset_option,
     set_option)
+
+# importing .display causes display.encoding and other keys to be initialized
+from pandas._config.display import detect_console_encoding
