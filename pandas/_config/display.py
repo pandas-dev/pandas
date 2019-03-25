e9f59621045afa660aee0f0ddce6f514f2ed33d3
Brock Mendel <jbrockmendel@gmail.com>
2019-03-24 19:36:00 -0700

    implement dates.py, isort fixups

--- pandas/_config/display.py
+++ pandas/_config/display.py
@@ -50,23 +50,6 @@ pc_encoding_doc = """
     these are generally strings meant to be displayed on the console.
 """
 
-pc_date_dayfirst_doc = """
-: boolean
-    When True, prints and parses dates with the day first, eg 20/01/2005
-"""
-
-pc_date_yearfirst_doc = """
-: boolean
-    When True, prints and parses dates with the year first, eg 2005/01/20
-"""
-
-
 with cf.config_prefix('display'):
     cf.register_option('encoding', detect_console_encoding(), pc_encoding_doc,
                        validator=cf.is_text)
-
-    # Needed upstream of `_libs` because these are used in tslibs.parsing
-    cf.register_option('date_dayfirst', False, pc_date_dayfirst_doc,
-                       validator=cf.is_bool)
-    cf.register_option('date_yearfirst', False, pc_date_yearfirst_doc,
-                       validator=cf.is_bool)
