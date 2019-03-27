b621f325d6297a6104534379e6d8d440942dca09
Will Ayd <william.ayd@icloud.com>
2019-03-27 09:57:43 -0700

    Removed DateTimeLike types

--- pandas/typing.py
+++ pandas/typing.py
@@ -1,14 +1,5 @@
-from datetime import datetime, timedelta
 from pathlib import Path
 from typing import AnyStr, IO, Union
-
-from pandas._libs.tslibs import Period, NaT, Timedelta, Timestamp
-
-
-DateTimeLike = Union[datetime, timedelta, Period, Timedelta, Timestamp]
-
-
-NullableDateTimeLike = Union[NaT, DateTimeLike]
 
 
 FilePathOrBuffer = Union[str, Path, IO[AnyStr]]
