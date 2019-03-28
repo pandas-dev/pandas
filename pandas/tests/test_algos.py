445a9a2c3607330b1abe422213a6f9fc8c4dbb4f
H. Vetinari <h.vetinari@gmx.com>
2019-03-28 07:48:30 +0100

    Simplify regex

--- pandas/tests/test_algos.py
+++ pandas/tests/test_algos.py
@@ -228,11 +228,9 @@ class TestFactorize(object):
         # gh 12666 - check no segfault
         x17 = np.array([complex(i) for i in range(17)], dtype=object)
 
-        msg = ("unorderable types: {0} [<>] {0}".format(r"complex\(\)")
-               + "|" +
-               "'[<>]' not supported between instances of {0} and {0}".format(
-                   "'complex'")
-               )
+        msg = ("unorderable types: .* [<>] .*"
+               "|"  # the above case happens for numpy < 1.14
+               "'[<>]' not supported between instances of .*")
         with pytest.raises(TypeError, match=msg):
             algos.factorize(x17[::-1], sort=True)
 
--- pandas/tests/test_sorting.py
+++ pandas/tests/test_sorting.py
@@ -413,13 +413,9 @@ class TestSafeSort(object):
     def test_unsortable(self):
         # GH 13714
         arr = np.array([1, 2, datetime.now(), 0, 3], dtype=object)
-        msg = ("unorderable types: ({0} [<>] {1}|{1} [<>] {0})".format(
-                   r"int\(\)", r"datetime\.datetime\(\)")  # noqa: E126
-               + "|" +
-               ("'[<>]' not supported between instances of "
-                "({0} and {1}|{1} and {0})").format(
-                    "'int'", r"'datetime\.datetime'")
-               )
+        msg = ("unorderable types: .* [<>] .*"
+               "|"  # the above case happens for numpy < 1.14
+               "'[<>]' not supported between instances of .*")
         with pytest.raises(TypeError, match=msg):
             safe_sort(arr)
 
