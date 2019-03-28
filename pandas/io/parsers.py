a42fba3aed67f834252373dd833af5168d121c82
Enrico Rotundo <enrico.rotundo@hal24k.com>
2019-03-28 19:54:03 +0100

    updated filepath_or_buffer param type docstring to pandas.read_csv

--- pandas/io/parsers.py
+++ pandas/io/parsers.py
@@ -61,7 +61,7 @@ Additional help can be found in the online docs for
 
 Parameters
 ----------
-filepath_or_buffer : str, path object, or file-like object
+filepath_or_buffer : str, path object, URL or file-like object
     Any valid string path is acceptable. The string could be a URL. Valid
     URL schemes include http, ftp, s3, and file. For file URLs, a host is
     expected. A local file could be: file://localhost/path/to/table.csv.
@@ -739,7 +739,7 @@ def read_fwf(filepath_or_buffer, colspecs='infer', widths=None,
 
     Parameters
     ----------
-    filepath_or_buffer : str, path object, or file-like object
+    filepath_or_buffer : str, path object, URL or file-like object
         Any valid string path is acceptable. The string could be a URL. Valid
         URL schemes include http, ftp, s3, and file. For file URLs, a host is
         expected. A local file could be: file://localhost/path/to/table.csv.
