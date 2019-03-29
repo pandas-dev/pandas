f055be9d769935789aa270a5d550a5bc70f516d4
ArtificialQualia <Kendall.Masse@gmail.com>
2019-03-29 18:30:58 -0400

    BUG: Fix memory leak in Rolling.min and Rolling.max (#25893)

--- pandas/_libs/window.pyx
+++ pandas/_libs/window.pyx
@@ -1251,7 +1251,7 @@ cdef _roll_min_max(ndarray[numeric] values, int64_t win, int64_t minp,
     ignoring NaNs.
     """
     cdef:
-        ndarray[int64_t] starti, endi
+        int64_t[:] starti, endi
         int64_t N
         bint is_variable
 
@@ -1267,8 +1267,8 @@ cdef _roll_min_max(ndarray[numeric] values, int64_t win, int64_t minp,
 
 
 cdef _roll_min_max_variable(ndarray[numeric] values,
-                            ndarray[int64_t] starti,
-                            ndarray[int64_t] endi,
+                            int64_t[:] starti,
+                            int64_t[:] endi,
                             int64_t N,
                             int64_t win,
                             int64_t minp,
@@ -1349,8 +1349,8 @@ cdef _roll_min_max_variable(ndarray[numeric] values,
 
 
 cdef _roll_min_max_fixed(ndarray[numeric] values,
-                         ndarray[int64_t] starti,
-                         ndarray[int64_t] endi,
+                         int64_t[:] starti,
+                         int64_t[:] endi,
                          int64_t N,
                          int64_t win,
                          int64_t minp,
