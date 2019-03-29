31f66cc4c1fdf5754da90081c86fe37d6ae67df4
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-03-29 18:29:02 +0530

    leave the previous comments there

--- pandas/_config/config.py
+++ pandas/_config/config.py
@@ -58,8 +58,11 @@ DeprecatedOption = namedtuple('DeprecatedOption', 'key msg rkey removal_ver')
 RegisteredOption = namedtuple('RegisteredOption',
                               'key defval doc validator cb')
 
+# holds deprecated option metdata
 _deprecated_options = {}  # type: Dict[str, DeprecatedOption]
+# holds registered option metdata
 _registered_options = {}  # type: Dict[str, RegisteredOption]
+# holds the current values for registered options
 _global_config = {}  # type: Dict[str, str]
 _reserved_keys = ['all']  # keys which have a special meaning
 
