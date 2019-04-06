a81f3746c4a58429b570a6a3d014af9cdae3f90e
dlnp2 <hideta7.dev@gmail.com>
2019-04-06 14:27:17 +0900

    restrict ops to In or NotIn

--- pandas/core/computation/expr.py
+++ pandas/core/computation/expr.py
@@ -712,20 +712,27 @@ class BaseExprVisitor(ast.NodeVisitor):
 
         # base case: we have something like a CMP b
         if len(comps) == 1:
-            # partial string match:
-            # when the left node is `Term` with a str value and
-            # right `Term` with a value of Series containing str,
+            op = self.translate_In(ops[0])
+
+            # partial string match (case sensitive):
+            # when the left node is `Term` with a str value,
+            # right `Term` with a value of Series containing str
+            # and `op` is `In` or `NotIn`,
             # we skip ordinary binary ops and apply `str_contains`.
             left = self.visit(node.left)
             right = self.visit(comps[0])
+            op_cls_name = op.__class__.__name__
             if (is_term(left) and is_term(right) and
+                    op_cls_name in ['In', 'NotIn'] and
                     _do_partial_str_match(left, right)):
                 from pandas.core.strings import str_contains
+
                 _res = str_contains(right.value, left.value, regex=False)
+                if op_cls_name == "NotIn":
+                    _res = ~_res
                 name = self.env.add_tmp(_res)
                 return self.term_type(name, env=self.env)
 
-            op = self.translate_In(ops[0])
             binop = ast.BinOp(op=op, left=node.left, right=comps[0])
             return self.visit(binop)
 
