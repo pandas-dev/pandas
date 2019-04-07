7aa943b41a8e036c5a10b2638481de6342fcce40
Ryan Nazareth <ryankarlos@gmail.com>
2019-04-07 23:18:44 +0100

    Adding pandas.io and adding missing pandas core imports

--- pandas/__init__.py
+++ pandas/__init__.py
@@ -42,37 +42,42 @@ from pandas._config import (get_option, set_option, reset_option,
 # let init-time option registration happen
 import pandas.core.config_init
 
-
 from pandas.core.api import (
-    DataFrame, Series, array, Panel, Int64Index, Interval,
-    IntervalIndex, Float64Index, Index, DatetimeIndex, Int64Index,
-    MultiIndex, PeriodIndex, RangeIndex, TimedeltaIndex,
-    CategoricalIndex, Categorical, to_datetime, to_timedelta,
-    to_numeric, Timedelta, Timestamp, NaT, Period, isna,
-    CategoricalDtype, PeriodDtype, DatetimeTZDtype, IntervalDtype,
-    DateOffset, factorize, unique, value_counts, interval_range,
-    Grouper)
+    Int8Dtype, Int16Dtype, Int32Dtype, Int64Dtype, UInt8Dtype,
+    UInt16Dtype, UInt32Dtype, UInt64Dtype, factorize, unique,
+    value_counts, isna, isnull, notna, notnull, CategoricalDtype,
+    PeriodDtype, IntervalDtype, DatetimeTZDtype, Categorical,
+    array, Grouper, set_eng_float_format, Index, CategoricalIndex,
+    Int64Index, UInt64Index, RangeIndex, Float64Index, MultiIndex,
+    IntervalIndex, TimedeltaIndex, DatetimeIndex, PeriodIndex,
+    NaT, Period, period_range, Timedelta, timedelta_range,
+    Timestamp, date_range, bdate_range, Interval, interval_range,
+    Series, DataFrame, Panel, IndexSlice, to_numeric, DateOffset,
+    to_datetime, to_timedelta, np, TimeGrouper)
+
+from pandas.core.sparse.api import (
+    SparseArray, SparseDataFrame, SparseSeries, SparseDtype)
 
 from pandas.tseries.api import infer_freq
 from pandas.tseries import offsets
+
+from pandas.core.computation.api import eval
 
 from pandas.core.reshape.api import (
     concat, lreshape, melt, wide_to_long, merge, merge_asof,
     merge_ordered, crosstab, pivot, pivot_table, get_dummies,
     cut, qcut)
 
-from pandas.core.computation.api import eval
-
-from pandas.core.sparse.api import (
-    SparseArray, SparseDataFrame, SparseSeries, SparseDtype)
-
-from pandas.core.internals import BlockManager, _safe_reshape, make_block
-from pandas.core.arrays import DatetimeArray, IntervalArray, PeriodArray
-from pandas.core.arrays.sparse import BlockIndex, IntIndex
-
-from pandas._libs import join
-
 from pandas.util._print_versions import show_versions
+
+from pandas.io.api import (
+    read_clipboard, ExcelFile, ExcelWriter, read_excel,
+    read_feather, read_gbq, read_html, read_json,
+    read_msgpack, to_msgpack, read_parquet, read_csv,
+    read_fwf, read_table, read_pickle, to_pickle,
+    HDFStore, read_hdf, read_sas, read_sql, read_sql_query,
+    read_sql_table, read_stata)
+
 from pandas.util._tester import test
 import pandas.testing
 import pandas.arrays
