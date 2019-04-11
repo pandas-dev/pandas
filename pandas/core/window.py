73208dc245fe9251bd9775ab9093c73e0bdc88e9
yhaque <yhaque@umich.edu>
2019-04-11 16:21:03 -0400

    Added axis to constructor call in count

--- pandas/core/window.py
+++ pandas/core/window.py
@@ -939,6 +939,7 @@ class _Rolling_and_Expanding(_Rolling):
             result = self._constructor(result, window=window, min_periods=0,
                                        axis=self.axis,
                                        center=self.center,
+                                       axis=self.axis,
                                        closed=self.closed).sum()
             results.append(result)
 
