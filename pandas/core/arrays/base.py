190a69e3d6bc3f106ef635ae18ff0fb8fdfe85c0
Jeremy Schendel <jschendel@users.noreply.github.com>
2019-04-13 15:11:20 -0600

    CLN: Remove PY2/PY3 flags from pandas.compat (#26047)
    * CLN: Remove PY2/PY3 flags from pandas.compat
    
    * lint
    
    * no longer mention py2

--- pandas/core/arrays/base.py
+++ pandas/core/arrays/base.py
@@ -10,7 +10,7 @@ from typing import Any, Callable, Optional, Sequence, Tuple, Union
 
 import numpy as np
 
-from pandas.compat import PY3, set_function_name
+from pandas.compat import set_function_name
 from pandas.compat.numpy import function as nv
 from pandas.errors import AbstractMethodError
 from pandas.util._decorators import Appender, Substitution
@@ -991,10 +991,6 @@ class ExtensionOpsMixin(object):
         cls.__rfloordiv__ = cls._create_arithmetic_method(ops.rfloordiv)
         cls.__truediv__ = cls._create_arithmetic_method(operator.truediv)
         cls.__rtruediv__ = cls._create_arithmetic_method(ops.rtruediv)
-        if not PY3:
-            cls.__div__ = cls._create_arithmetic_method(operator.div)
-            cls.__rdiv__ = cls._create_arithmetic_method(ops.rdiv)
-
         cls.__divmod__ = cls._create_arithmetic_method(divmod)
         cls.__rdivmod__ = cls._create_arithmetic_method(ops.rdivmod)
 
