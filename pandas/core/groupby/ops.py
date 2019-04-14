ac433a471d39bf9c5aca35595c60e1c38b78a27f
Will Ayd <william.ayd@icloud.com>
2019-04-14 17:40:35 -0500

    Fixed typing issues in pandas.core.groupby

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -10,7 +10,9 @@ import collections
 
 import numpy as np
 
-from pandas._libs import NaT, groupby as libgroupby, iNaT, lib, reduction
+import pandas._libs.groupby as libgroupby
+import pandas._libs.reduction as reduction
+from pandas._libs import NaT, iNaT, lib
 from pandas.compat import lzip
 from pandas.errors import AbstractMethodError
 from pandas.util._decorators import cache_readonly
