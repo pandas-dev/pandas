f0ce12cf553a49565b7d2879472a8eae88706cfa
Adam Hooper <adam@adamhooper.com>
2019-04-16 17:48:13 -0400

    BUG: GroupBy.size with all-null data raises ValueError

--- pandas/tests/groupby/test_function.py
+++ pandas/tests/groupby/test_function.py
@@ -1089,6 +1089,12 @@ def test_size(df):
     out = Series(dtype='int64', index=Index([], name='A'))
     tm.assert_series_equal(df.groupby('A').size(), out)
 
+    # GH23050
+    # Assert no 'Value Error : Length of passed values is 2, index implies 0'
+    df = DataFrame({'A': [None, None]})  # all-null groups
+    out = Series(dtype='int64', index=Index([], name='A'))
+    tm.assert_series_equal(df.groupby('A').size(), out)
+
 
 # quantile
 # --------------------------------
