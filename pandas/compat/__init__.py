68b1da7f53f043b21e3f722e8872e19941a10250
topper-123 <contribute@tensortable.com>
2019-04-16 18:17:16 +0100

    CLN: remove compat.lfilter (#26110)

--- pandas/compat/__init__.py
+++ pandas/compat/__init__.py
@@ -5,7 +5,7 @@ compat
 Cross-compatible functions for different versions of Python.
 
 Key items to import for compatible code:
-* lists: lrange(), lmap(), lzip(), lfilter()
+* lists: lrange(), lmap(), lzip()
 * add_metaclass(metaclass) - class decorator that recreates class with with the
   given metaclass instead (and avoids intermediary class creation)
 
@@ -33,10 +33,6 @@ def lzip(*args, **kwargs):
 
 def lmap(*args, **kwargs):
     return list(map(*args, **kwargs))
-
-
-def lfilter(*args, **kwargs):
-    return list(filter(*args, **kwargs))
 
 
 # ----------------------------------------------------------------------------
