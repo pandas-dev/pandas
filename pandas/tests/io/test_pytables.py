2ecc05e3cda08f4bd9e28085ee83b347d4e332b0
Peter Tillmann <Peter.tillmann@helmholtz-berlin.de>
2019-04-18 17:13:26 +0200

    Linting, cleanup and replaced string_types with str

--- pandas/tests/io/test_pytables.py
+++ pandas/tests/io/test_pytables.py
@@ -5152,7 +5152,7 @@ class TestTimezones(Base):
     def test_wide_table_format(self):
         # test storing wide dataframes with in table format
 
-        df = DataFrame(np.random.random((10,10000)))
+        df = DataFrame(np.random.random((10, 10000)))
 
         with ensure_clean_path(self.path) as path:
             df.to_hdf(path, 'df', format='table')
@@ -5162,12 +5162,11 @@ class TestTimezones(Base):
     def test_append_wide_table_format(self):
         # test append to hdf with wide dataframe
 
-        df1 = DataFrame(np.random.random((10,10000)))
-        df2 = DataFrame(np.random.random((10,10000)))
+        df1 = DataFrame(np.random.random((10, 10000)))
+        df2 = DataFrame(np.random.random((10, 10000)))
 
         with ensure_clean_path(self.path) as path:
             df1.to_hdf(path, 'df', format='table')
             df2.to_hdf(path, 'df', append=True)
             reread = read_hdf(path)
             assert_frame_equal(pd.concat([df1, df2]), reread)
-
