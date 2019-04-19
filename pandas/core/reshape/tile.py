6a81c2d0918ed7ba7059e14e3c9f120b22bcb120
Batalex <Batalex@users.noreply.github.com>
2019-04-19 03:03:54 +0200

    BUG: prevent overflowing diffs raising error in cut (#26045) (#26063)

--- pandas/core/reshape/tile.py
+++ pandas/core/reshape/tile.py
@@ -230,7 +230,9 @@ def cut(x, bins, right=True, labels=None, retbins=False, precision=3,
         else:
             bins = np.asarray(bins)
         bins = _convert_bin_to_numeric_type(bins, dtype)
-        if (np.diff(bins) < 0).any():
+
+        # GH 26045: cast to float64 to avoid an overflow
+        if (np.diff(bins.astype('float64')) < 0).any():
             raise ValueError('bins must increase monotonically.')
 
     fac, bins = _bins_to_cuts(x, bins, right=right, labels=labels,
