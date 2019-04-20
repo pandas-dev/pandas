c3db771296617b588ce707635732384ad585e404
Peter Tillmann <Peter.tillmann@fu-berlin.de>
2019-04-20 15:07:48 +0200

    Linting

--- pandas/tests/io/test_pytables.py
+++ pandas/tests/io/test_pytables.py
@@ -4728,7 +4728,7 @@ class TestHDFStore(Base):
                 mode='r') as store:
             result = store['p']
             assert_frame_equal(result, expected)
-            
+
     def test_wide_table_format(self):
         # GH 26135
         # test storing wide dataframes with in table format
@@ -4752,27 +4752,27 @@ class TestHDFStore(Base):
             df2.to_hdf(path, 'df', append=True)
             reread = read_hdf(path)
             assert_frame_equal(pd.concat([df1, df2]), reread)
-            
+
     @xfail_non_writeable
     def test_legacy_table_table_format_read(self, datapath):
         # GH 26135
-        # test read of legacy table with table format and column 
+        # test read of legacy table with table format and column
         # saved as pytables metadata
-        
+
         column_numeric = [1, 2, 3, 4]
         column_str_1 = ['A', 'B', 'C', 'D']
         column_str_2 = ['Ä', 'Ö', 'Â', 'é']
         column_dt = pd.date_range('19700101', '19700104')
         column_multi_1 = pd.MultiIndex.from_tuples(
-                zip(column_numeric, column_str_1))
+            zip(column_numeric, column_str_1))
         column_multi_2 = pd.MultiIndex.from_tuples(
-                zip(column_str_2, column_dt))
-        
+            zip(column_str_2, column_dt))
+
         columns = [column_numeric, column_str_1, column_str_2, column_dt,
                    column_multi_1, column_multi_2]
-        
-        data = np.arange(0, 16).reshape(4,4)
-        
+
+        data = np.arange(0, 16).reshape(4, 4)
+
         with ensure_clean_store(
                 datapath('io', 'data', 'legacy_hdf',
                          'legacy_table_table_format.h5'),
@@ -4781,7 +4781,8 @@ class TestHDFStore(Base):
                 table_name = 'table_{}'.format(i)
                 df = pd.DataFrame(data, columns=column)
                 tm.assert_frame_equal(store[table_name], df)
-        
+
+
 class TestHDFComplexValues(Base):
     # GH10447
 
