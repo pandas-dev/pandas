a3bcf1a85653b153ba9d0004b42319808223a507
Benjamin Rowell <brrowell@gmail.com>
2019-04-21 22:09:11 +0100

    Setting categorical datatype after calc on expected.

--- pandas/tests/reshape/test_pivot.py
+++ pandas/tests/reshape/test_pivot.py
@@ -70,12 +70,15 @@ class TestPivotTable:
                            'col2': list('fghij'),
                            'col3': [1, 2, 3, 4, 5]})
 
-        df.col1 = df.col1.astype('category')
-        df.col2 = df.col1.astype('category')
-
         expected = df.pivot_table(index='col1', values='col3',
                                   columns='col2', aggfunc=np.sum,
                                   fill_value=0)
+
+        expected.index = expected.index.astype('category')
+        expected.columns = expected.columns.astype('category')
+
+        df.col1 = df.col1.astype('category')
+        df.col2 = df.col2.astype('category')
 
         result = df.pivot_table(index='col1', values='col3',
                                 columns='col2', aggfunc=np.sum,
