679d897218aa6af0abbfe161713d93367ba70a70
Mukund Manikantan <mukundm@umich.edu>
2019-04-21 04:00:33 -0400

    ENH: adding functionality to support skipdatarows flag in read_csv to skip first n rows excluding comments

--- pandas/tests/io/parser/test_skipdatarows.py
+++ pandas/tests/io/parser/test_skipdatarows.py
@@ -8,12 +8,8 @@
 from io import StringIO
 
 import numpy as np
-import pytest
 
-from pandas.compat import lrange
-from pandas.errors import EmptyDataError
-
-from pandas import DataFrame, Index
+from pandas import DataFrame
 import pandas.util.testing as tm
 
 
@@ -25,11 +21,11 @@ row1,row2,row3
 !,?,$
 4,5,6
 7,8,9"""
-    expected = DataFrame([["!", "?", "$"],
-                        ["4", "5", "6"],["7", "8", "9"]],
-                        columns=["1.", "2", "3."])
+    expected = DataFrame([["!", "?", "$"], ["4", "5", "6"], ["7", "8", "9"]],
+                         columns=["1.", "2", "3."])
     result = parser.read_csv(StringIO(data), skipdatarows=2, delimiter=",")
     tm.assert_frame_equal(result, expected)
+
 
 def test_comment_skipdatarows(all_parsers):
     parser = all_parsers
@@ -46,10 +42,10 @@ last,data,row"""
                              delimiter=",")
 
     expected = DataFrame([["A", "B", "C"], ["D", "E", "F"],
-                            ["last", "data", "row"]],
-                                                  columns=["X", "Y", "Z"])
+                         ["last", "data", "row"]], columns=["X", "Y", "Z"])
 
     tm.assert_frame_equal(result, expected)
+
 
 def test_skiprows_skipdatarows(all_parsers):
     parser = all_parsers
@@ -65,12 +61,11 @@ e,f,g
                              delimiter=",")
 
     expected = DataFrame([["row1", "row2", "row3"], ["A", "B", "C"],
-                            ["1", "2.", "4.0"],
-                            ["b", "c", "d"],
-                            ["e", "f", "g"],
-                            ["5", np.nan, "10.0"]],
-                            columns=["X", "Y", "Z"])
+                         ["1", "2.", "4.0"], ["b", "c", "d"],
+                         ["e", "f", "g"], ["5", np.nan, "10.0"]],
+                         columns=["X", "Y", "Z"])
     tm.assert_frame_equal(result, expected)
+
 
 def test_comment_skiprows_skipdatarows(all_parsers):
 
@@ -85,8 +80,8 @@ D,E,F
 G,H,I
 """
     result = parser.read_csv(StringIO(data), comment="#", skiprows=2,
-                            skipdatarows=3, delimiter=",")
+                             skipdatarows=3, delimiter=",")
 
     expected = DataFrame([["D", "E", "F"], ["G", "H", "I"]],
-                            columns=["X", "Y", "Z"])
+                         columns=["X", "Y", "Z"])
     tm.assert_frame_equal(result, expected)
