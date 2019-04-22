b1cf14055a6abe20345cd414ace285ccdc6d94c3
Anatoly Myachev <anatoly.myachev@intel.com>
2019-04-22 22:08:05 +0300

    changed wildcard import in '/pandas/core/index.py' to explicit import

--- pandas/core/index.py
+++ pandas/core/index.py
@@ -1,3 +1,12 @@
 # flake8: noqa
-from pandas.core.indexes.api import *
+from pandas.core.indexes.api import (
+    Index, CategoricalIndex, Int64Index, UInt64Index, RangeIndex, Float64Index,
+    MultiIndex, IntervalIndex, TimedeltaIndex, DatetimeIndex, PeriodIndex,
+    NumericIndex, InvalidIndexError, ensure_index, ensure_index_from_sequences,
+    NaT,
+
+    # private methods
+    _new_Index, _get_combined_index, _get_objs_combined_axis, _union_indexes,
+    _get_consensus_names, _all_indexes_same
+)
 from pandas.core.indexes.multi import _sparsify
