7cea50ae6de4b359d9df57c8819296b977ba7d40
nicolas.rebena <nicolas.rebena@irt-systemx.fr>
2019-04-23 09:42:14 +0200

    TST: Add test for #7612 #22334 #15912

--- pandas/tests/plotting/test_frame.py
+++ pandas/tests/plotting/test_frame.py
@@ -2995,6 +2995,23 @@ class TestDataFramePlots(TestPlotBase):
         self._check_ticks_props(axes=ax.right_ax,
                                 ylabelsize=fontsize)
 
+    @pytest.mark.slow
+    def test_x_string_values_ticks(self):
+        # GH: 7612
+        # GH: 22334
+        # GH: 15912
+        df = pd.DataFrame({'sales': [3, 2, 3],
+                   'visits': [20, 42, 28],
+                   'day': ['Monday', 'Tuesday', 'Wednesday']})
+        ax = df.plot.area(x='day')
+        ax.set_xlim(-1, 3)
+        xticklabels = [t.get_text() for t in ax.get_xticklabels()]
+        labels_position = dict(zip(xticklabels, ax.get_xticks()))
+        # Testing if the label stayed at the right position
+        assert labels_position['Monday'] == 0.0
+        assert labels_position['Tuesday'] == 1.0
+        assert labels_position['Wednesday'] == 2.0
+
 
 def _generate_4_axes_via_gridspec():
     import matplotlib.pyplot as plt
