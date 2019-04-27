ac4e339a8a01dfc6b031ec6abc6f5351dc664806
ihsan <ihsansecer@gmail.com>
2019-04-28 00:16:33 +0300

    DOC: Fix validation error RT01 in pandas/util (#25356)

--- pandas/util/testing.py
+++ pandas/util/testing.py
@@ -1007,6 +1007,10 @@ def assert_series_equal(left, right, check_dtype=True,
     obj : str, default 'Series'
         Specify object name being compared, internally used to show appropriate
         assertion message.
+
+    Returns
+    -------
+    None
     """
     __tracebackhide__ = True
 
