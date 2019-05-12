2f6a0c94e91a85c530e60bbfd846c8522804b228
topper-123 <contribute@tensortable.com>
2019-05-12 14:35:53 +0000

    BUG: constructing a DataFrame using range doesn't work (#26343)

--- pandas/core/dtypes/cast.py
+++ pandas/core/dtypes/cast.py
@@ -35,8 +35,8 @@ _int64_max = np.iinfo(np.int64).max
 def maybe_convert_platform(values):
     """ try to do platform conversion, allow ndarray or list here """
 
-    if isinstance(values, (list, tuple)):
-        values = construct_1d_object_array_from_listlike(list(values))
+    if isinstance(values, (list, tuple, range)):
+        values = construct_1d_object_array_from_listlike(values)
     if getattr(values, 'dtype', None) == np.object_:
         if hasattr(values, '_values'):
             values = values._values
