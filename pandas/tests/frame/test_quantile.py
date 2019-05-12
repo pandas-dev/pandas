f3c01e3b31c0c29407b4cd4c6823b33e519079a1
Daniel Saxton <daniel.saxton@gmail.com>
2019-05-11 21:05:52 -0500

    Remove a test

--- pandas/tests/frame/test_quantile.py
+++ pandas/tests/frame/test_quantile.py
@@ -214,12 +214,6 @@ class TestDataFrameQuantile(TestData):
         # result = df[['a', 'c']].quantile(.5)
         # result = df[['a', 'c']].quantile([.5])
 
-    def test_quantile_invalid(self):
-        msg = 'percentiles should all be in the interval \\[0, 1\\]'
-        for invalid in [-1, 2, [0.5, -1], [0.5, 2]]:
-            with pytest.raises(ValueError, match=msg):
-                self.tsframe.quantile(invalid)
-
     def test_quantile_box(self):
         df = DataFrame({'A': [pd.Timestamp('2011-01-01'),
                               pd.Timestamp('2011-01-02'),
