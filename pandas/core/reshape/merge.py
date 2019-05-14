663abf48033f590aebf70df99ea68bdcc357a2a0
Matthew Roeschke <emailformattr@gmail.com>
2019-05-14 09:03:59 -0700

    BUG: DataFrame.join on tz aware index and column (#26362)

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -1674,8 +1674,8 @@ _join_functions = {
 def _factorize_keys(lk, rk, sort=True):
     # Some pre-processing for non-ndarray lk / rk
     if is_datetime64tz_dtype(lk) and is_datetime64tz_dtype(rk):
-        lk = lk._data
-        rk = rk._data
+        lk = getattr(lk, '_values', lk)._data
+        rk = getattr(rk, '_values', rk)._data
 
     elif (is_categorical_dtype(lk) and
             is_categorical_dtype(rk) and
