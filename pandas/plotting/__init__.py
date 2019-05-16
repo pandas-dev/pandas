965abc9c29f3d153772fbeae456b3c0c9ec238f8
Marc Garcia <marc.garcia2@tesco.com>
2019-05-16 17:24:46 +0100

    Removing boxplot import from __init__.py

--- pandas/plotting/__init__.py
+++ pandas/plotting/__init__.py
@@ -8,7 +8,6 @@ from pandas.plotting._misc import (scatter_matrix, radviz,
                                    andrews_curves, bootstrap_plot,
                                    parallel_coordinates, lag_plot,
                                    autocorrelation_plot)
-from pandas.plotting._core import boxplot
 from pandas.plotting._style import plot_params
 from pandas.plotting._tools import table
 try:
