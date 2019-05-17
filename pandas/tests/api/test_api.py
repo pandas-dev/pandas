386cca1402b42bc1eaa4a4571cae2190b97d0001
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-05-17 10:41:30 -0500

    fix api test

--- pandas/tests/api/test_api.py
+++ pandas/tests/api/test_api.py
@@ -47,7 +47,7 @@ class TestPDApi(Base):
                'DatetimeTZDtype',
                'Int8Dtype', 'Int16Dtype', 'Int32Dtype', 'Int64Dtype',
                'UInt8Dtype', 'UInt16Dtype', 'UInt32Dtype', 'UInt64Dtype',
-               'Agg',
+               'KeywordAgg',
                ]
 
     # these are already deprecated; awaiting removal
