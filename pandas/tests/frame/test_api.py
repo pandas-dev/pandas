ffbbc496476563b5a6cb57ebd8ddb9381a6df055
Simon Hawkins <simonjayhawkins@gmail.com>
2019-05-18 15:16:54 +0100

    TST: Iterrows making incorrect assumptions about datetime (#26441)

--- pandas/tests/frame/test_api.py
+++ pandas/tests/frame/test_api.py
@@ -1,4 +1,5 @@
 from copy import deepcopy
+import datetime
 import pydoc
 
 import numpy as np
@@ -221,6 +222,17 @@ class SharedWithSparse:
         for k, v in s.iterrows():
             exp = s.loc[k]
             self._assert_series_equal(v, exp)
+
+    def test_iterrows_corner(self):
+        # gh-12222
+        df = DataFrame(
+            {'a': [datetime.datetime(2015, 1, 1)], 'b': [None], 'c': [None],
+             'd': [''], 'e': [[]], 'f': [set()], 'g': [{}]})
+        expected = Series(
+            [datetime.datetime(2015, 1, 1), None, None, '', [], set(), {}],
+            index=list('abcdefg'), name=0, dtype='object')
+        _, result = next(df.iterrows())
+        tm.assert_series_equal(result, expected)
 
     def test_itertuples(self, float_frame):
         for i, tup in enumerate(float_frame.itertuples()):
