0e9589d034823682a9cbfb63dbe9002293dda40b
Gregory Rome <gwrome@gmail.com>
2019-05-17 19:34:31 -0500

    Misc Type Annotation fixes (#26372)
    * Add type annotations to io.pytables
    
    * Start correcting mypy errors in core.window
    
    * Complete misc type fixes
    
    * Fix import sort on test_groupby.py
    
    * Update after code reviews
    
    * Whatsnew, second rev indexing, window and pytables
    
    * Annotate Set in core.window
    
    * Define attributes as List[str]

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -7,7 +7,8 @@ import warnings
 
 import numpy as np
 
-from pandas._libs import internals as libinternals, lib, tslib, tslibs
+from pandas._libs import lib, tslib, tslibs
+import pandas._libs.internals as libinternals
 from pandas._libs.tslibs import Timedelta, conversion, is_null_datetimelike
 from pandas.util._validators import validate_bool_kwarg
 
@@ -2050,11 +2051,14 @@ class DatetimeLikeBlockMixin:
 class DatetimeBlock(DatetimeLikeBlockMixin, Block):
     __slots__ = ()
     is_datetime = True
-    _can_hold_na = True
 
     def __init__(self, values, placement, ndim=None):
         values = self._maybe_coerce_values(values)
         super().__init__(values, placement=placement, ndim=ndim)
+
+    @property
+    def _can_hold_na(self):
+        return True
 
     def _maybe_coerce_values(self, values):
         """Input validation for values passed to __init__. Ensure that
