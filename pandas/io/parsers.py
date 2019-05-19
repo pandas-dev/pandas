9907cbc977776c7d315a75ffef2f0af31e03237f
Shantanu Gontia <gontia.shantanu@gmail.com>
2019-05-20 00:59:53 +0530

    removed check for readline attribute

--- pandas/io/parsers.py
+++ pandas/io/parsers.py
@@ -3604,14 +3604,9 @@ class FixedWidthReader(BaseIterator):
                 line = next(self.buffer)
             except StopIteration:
                 self.buffer = None
-                line = next(self.f)
-        else:
-            # If f is a file like object, use readline
-            if hasattr(self.f, 'readline'):
                 line = self.f.readline()
-            # otherwise iterate on it
-            else:
-                line = next(self.f)
+        else:
+            line = self.f.readline()
         # Note: 'colspecs' is a sequence of half-open intervals.
         return [line[fromm:to].strip(self.delimiter)
                 for (fromm, to) in self.colspecs]
