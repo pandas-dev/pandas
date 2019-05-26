01313a93d77febcefb19f280c6136a36449c996c
Mathew George <mathewgeorge@Mathews-MacBook-Pro.local>
2019-05-26 20:13:50 +0530

    GL01 GL02 fix on read_html

--- pandas/io/html.py
+++ pandas/io/html.py
@@ -924,7 +924,8 @@ def read_html(io, match='.+', flavor=None, header=None, index_col=None,
               tupleize_cols=None, thousands=',', encoding=None,
               decimal='.', converters=None, na_values=None,
               keep_default_na=True, displayed_only=True):
-    r"""Read HTML tables into a ``list`` of ``DataFrame`` objects.
+    """
+    Read HTML tables into a ``list`` of ``DataFrame`` objects.
 
     Parameters
     ----------
