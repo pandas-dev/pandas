cfc6147fcae9adc5278c2abbca73931a9c778ffd
Simon Hawkins <simonjayhawkins@gmail.com>
2019-05-28 14:55:23 +0100

    CI: unary operator expected error in log files

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -118,7 +118,7 @@ echo "conda list"
 conda list
 
 # Install DB for Linux
-if [ ${TRAVIS_OS_NAME} == "linux" ]; then
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
   echo "installing dbs"
   mysql -e 'create database pandas_nosetest;'
   psql -c 'create database pandas_nosetest;' -U postgres
