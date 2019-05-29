558a527963fd0be5efac62583407810aa0d2bd4b
killerontherun1 <mathewcgeorge212@gmail.com>
2019-05-29 12:08:52 +0530

    reverting changes outside pandas.core

--- pandas/plotting/_misc.py
+++ pandas/plotting/_misc.py
@@ -568,8 +568,7 @@ def parallel_coordinates(frame, class_column, cols=None, ax=None, color=None,
 
 
 def lag_plot(series, lag=1, ax=None, **kwds):
-    """
-    Lag plot for time series.
+    """Lag plot for time series.
 
     Parameters
     ----------
