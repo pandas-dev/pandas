d9db853a05a817d4e7709ce0d1169e0ec2f3c12a
Jiang Yue <jyue@wup-sgpubu02.us.drwholdings.com>
2019-05-29 18:23:58 +0800

    add test for verify _integrity=False

--- pandas/tests/indexes/multi/test_constructor.py
+++ pandas/tests/indexes/multi/test_constructor.py
@@ -84,6 +84,11 @@ def test_constructor_mismatched_codes_levels(idx):
     with pytest.raises(ValueError, match=label_error):
         idx.copy().set_codes([[0, 0, 0, 0], [0, 0]])
 
+    # test set_codes with verify_integrity=False
+    # the setting should not raise any value error
+    idx.copy().set_codes(codes=[[0, 0, 0, 0], [0, 0]],
+                         verify_integrity=False)
+
     # code value smaller than -1
     with pytest.raises(ValueError, match=code_value_error):
         MultiIndex(levels=[['a'], ['b']], codes=[[0, -2], [0, 0]])
