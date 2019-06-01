b2b923977c39ac7252edcc4a5b574e594efee594
Frank Hoang <fhoang7@berkeley.edu>
2019-06-01 09:33:14 -0500

    - Update what's new - Simplify scope of except Block - comment on non-regression test

--- pandas/core/sparse/scipy_sparse.py
+++ pandas/core/sparse/scipy_sparse.py
@@ -129,7 +129,10 @@ def _coo_to_sparse_series(A, dense_index: bool = False,
 
     Returns
     -------
-    Series or SparseSeries on success
+    Series or SparseSeries
+
+    Raises
+    -------
     TypeError if A is not a coo_matrix
 
     """
@@ -137,21 +140,22 @@ def _coo_to_sparse_series(A, dense_index: bool = False,
 
     try:
         s = Series(A.data, MultiIndex.from_arrays((A.row, A.col)))
-        s = s.sort_index()
-        if sparse_series:
-            # TODO(SparseSeries): remove this and the sparse_series keyword.
-            # This is just here to avoid a DeprecationWarning when
-            # _coo_to_sparse_series is called via Series.sparse.from_coo
-            s = s.to_sparse()  # TODO: specify kind?
-        else:
-            s = s.astype(SparseDtype(s.dtype))
-        if dense_index:
-            # is there a better constructor method to use here?
-            i = range(A.shape[0])
-            j = range(A.shape[1])
-            ind = MultiIndex.from_product([i, j])
-            s = s.reindex(ind)
-        return s
     except AttributeError:
         raise TypeError('Expected coo_matrix. Got {} instead.'
                         .format(type(A).__name__))
+    s = s.sort_index()
+    if sparse_series:
+        # TODO(SparseSeries): remove this and the sparse_series keyword.
+        # This is just here to avoid a DeprecationWarning when
+        # _coo_to_sparse_series is called via Series.sparse.from_coo
+        s = s.to_sparse()  # TODO: specify kind?
+    else:
+        s = s.astype(SparseDtype(s.dtype))
+    if dense_index:
+        # is there a better constructor method to use here?
+        i = range(A.shape[0])
+        j = range(A.shape[1])
+        ind = MultiIndex.from_product([i, j])
+        s = s.reindex(ind)
+    return s
+
