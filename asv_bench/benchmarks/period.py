f548d01310c7c6831f31c452ea65b99d757fa862
ApolllonDev <oleksii.polovyi@gmail.com>
2019-06-01 21:52:52 -0400

    add day_of_week to params

--- asv_bench/benchmarks/period.py
+++ asv_bench/benchmarks/period.py
@@ -8,7 +8,7 @@ class PeriodProperties:
     params = (['M', 'min'],
               ['year', 'month', 'day', 'hour', 'minute', 'second',
                'is_leap_year', 'quarter', 'qyear', 'week', 'daysinmonth',
-               'dayofweek', 'dayofyear', 'day_of_year', 'start_time', 'end_time'])
+               'dayofweek', 'day_of_week', 'dayofyear', 'day_of_year', 'start_time', 'end_time'])
     param_names = ['freq', 'attr']
 
     def setup(self, freq, attr):
