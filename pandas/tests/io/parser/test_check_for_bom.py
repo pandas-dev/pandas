0df0744e2914efd403f6f3e4bd668b5ccc9841b3
Belavin Denis <belavin.d.andreevich@yandex.ru>
2019-06-03 17:43:00 +0300

    #26545 fix: same .tsv file, get different data-frame structure using engine 'python' and 'c'

---
+++ pandas/tests/io/parser/test_check_for_bom.py
@@ -0,0 +1,8 @@
+import pytest
+
+from pandas import read_csv
+
+
+def test_first_row_bom():
+    assert read_csv('data/bom_first_line.txt', delimiter='\t', engine='python').shape == (8, 22)
+    assert read_csv('data/bom_first_line.txt', delimiter='\t').shape == (8, 22)
