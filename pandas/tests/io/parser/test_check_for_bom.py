0816572d5ca6a4fe4116fdc6d1cce74d7f5e5207
Belavin Denis <belavin.d.andreevich@yandex.ru>
2019-06-03 18:47:51 +0300

    test-fix

--- pandas/tests/io/parser/test_check_for_bom.py
+++ pandas/tests/io/parser/test_check_for_bom.py
@@ -1,11 +1,20 @@
+from pandas import read_csv
+import os
 import pytest
 
-from pandas import read_csv
 
+class TestFirstRowBom:
 
-def test_first_row_bom():
-    assert read_csv('data/bom_first_line.txt',
-                    delimiter='\t',
-                    engine='python').shape == (8, 22)
-    assert read_csv('data/bom_first_line.txt',
-                    delimiter='\t').shape == (8, 22)
+    @pytest.fixture(autouse=True)
+    def setup_method(self, datapath):
+        self.dirpath = datapath('io', 'parser', 'data')
+        self.file_path = os.path.join(self.dirpath, 'bom_first_line.txt')
+
+    def test_first_row_bom_python(self):
+        assert read_csv(self.file_path,
+                        delimiter='\t',
+                        engine='python').shape == (8, 22)
+
+    def test_first_row_bom_c(self):
+        assert read_csv(self.file_path,
+                        delimiter='\t').shape == (8, 22)
