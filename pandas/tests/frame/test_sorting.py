0ef1357e5bc4e7e817cbb05648d4d7f63fcf4aab
Matias Heikkilä <42970828+mahepe@users.noreply.github.com>
2019-06-05 15:54:34 +0300

    TST: Test sorting levels not aligned with index (#25775) (#26492)

--- pandas/tests/frame/test_sorting.py
+++ pandas/tests/frame/test_sorting.py
@@ -227,6 +227,18 @@ class TestDataFrameSorting(TestData):
                                    kind='mergesort')
         assert_frame_equal(sorted_df, expected)
 
+    def test_sort_multi_index(self):
+        # GH 25775, testing that sorting by index works with a multi-index.
+        df = DataFrame({'a': [3, 1, 2], 'b': [0, 0, 0],
+                        'c': [0, 1, 2], 'd': list('abc')})
+        result = df.set_index(list('abc')).sort_index(level=list('ba'))
+
+        expected = DataFrame({'a': [1, 2, 3], 'b': [0, 0, 0],
+                              'c': [1, 2, 0], 'd': list('bca')})
+        expected = expected.set_index(list('abc'))
+
+        tm.assert_frame_equal(result, expected)
+
     def test_stable_categorial(self):
         # GH 16793
         df = DataFrame({
