a4acf6f5e3f3c4ddf46a16c116eb71b90ba0ec5d
jbrockmendel <jbrockmendel@gmail.com>
2019-06-07 10:14:34 -0500

    BUG: TDI/PI comparison with zero-dim array (#26707)

--- pandas/core/arrays/period.py
+++ pandas/core/arrays/period.py
@@ -4,6 +4,7 @@ from typing import Any, Callable, List, Optional, Sequence, Union
 
 import numpy as np
 
+from pandas._libs import lib
 from pandas._libs.tslibs import (
     NaT, NaTType, frequencies as libfrequencies, iNaT, period as libperiod)
 from pandas._libs.tslibs.fields import isleapyear_arr
@@ -51,6 +52,7 @@ def _period_array_cmp(cls, op):
     def wrapper(self, other):
         op = getattr(self.asi8, opname)
 
+        other = lib.item_from_zerodim(other)
         if isinstance(other, (ABCDataFrame, ABCSeries, ABCIndexClass)):
             return NotImplemented
 
