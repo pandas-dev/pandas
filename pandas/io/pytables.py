e55f060d95f2693dce74dc7d8daa48b281fe0ee0
BeforeFlight <m07sss@gmail.com>
2019-06-08 04:07:21 +0300

    TST: issue #26610. Tests and fix.

--- pandas/io/pytables.py
+++ pandas/io/pytables.py
@@ -95,10 +95,10 @@ def _ensure_term(where, scope_level):
                 wlist.append(w)
             else:
                 wlist.append(Term(w, scope_level=level))
-        where = wlist
+        where = wlist if wlist else None
     elif maybe_expression(where):
         where = Term(where, scope_level=level)
-    return where
+    return where if where != "" else None
 
 
 class PossibleDataLossError(Exception):
--- pandas/tests/io/test_pytables.py
+++ pandas/tests/io/test_pytables.py
@@ -4731,6 +4731,34 @@ class TestHDFStore(Base):
             result = store['p']
             assert_frame_equal(result, expected)
 
+    @pytest.mark.parametrize("call", [
+        "read_hdf", "select", "select_as_coordinates", "select_as_multiple"
+    ])
+    @pytest.mark.parametrize("where", ["", (), (None, ), [], [None]])
+    def test_select_empty_where(self, call, where):
+        # GH26610
+
+        # Using keyword `where` as '' or (), or [None], etc
+        # while reading from HDF store raises
+        # "SyntaxError: only a single expression is allowed"
+
+        df = pd.DataFrame([[1, 2], [1, 2]], columns=list("ab"))
+        with ensure_clean_path("empty_where.h5") as path:
+            with pd.HDFStore(path) as store:
+                key1 = "df1"
+                key2 = "df2"
+                store.put("df1", df[["a"]], "t")
+                store.put("df2", df[["b"]], "t")
+                if call == "read_hdf":
+                    pd.read_hdf(store, key1, where=where)
+                elif call == "select":
+                    store.select(key1, where=where)
+                elif call == "select_as_coordinates":
+                    store.select_as_coordinates(key1, where=where)
+                elif call == "select_as_multiple":
+                    store.select_as_multiple(
+                        [key1, key2], where=where, selector=key1)
+
 
 class TestHDFComplexValues(Base):
     # GH10447
