595f0ecb5dd5890c5082c35c8b225c6b0c3a689a
jbrockmendel <jbrockmendel@gmail.com>
2019-06-10 10:19:47 -0700

    remove message from preliminary scalar analogue test

--- pandas/tests/arithmetic/test_timedelta64.py
+++ pandas/tests/arithmetic/test_timedelta64.py
@@ -480,10 +480,10 @@ class TestAddSubNaTMasking:
 
     def test_tdi_add_overflow(self):
         # See GH#14068
-        msg = "Cannot convert"
-        with pytest.raises(OutOfBoundsDatetime, match=msg):
+        # preliminary test scalar analogue of vectorized tests below
+        with pytest.raises(OutOfBoundsDatetime):
             pd.to_timedelta(106580, 'D') + Timestamp('2000')
-        with pytest.raises(OutOfBoundsDatetime, match=msg):
+        with pytest.raises(OutOfBoundsDatetime):
             Timestamp('2000') + pd.to_timedelta(106580, 'D')
 
         _NaT = int(pd.NaT) + 1
