6b797a266ae3b46515f6a58c864e6b7bb1067148
alimcmaster1 <alimcmaster1@gmail.com>
2019-06-10 21:14:40 +0100

    isort files

--- pandas/core/internals/__init__.py
+++ pandas/core/internals/__init__.py
@@ -1,14 +1,12 @@
-from .blocks import (
-    Block, BoolBlock, CategoricalBlock, ComplexBlock, DatetimeBlock,
-    DatetimeTZBlock, ExtensionBlock, FloatBlock, IntBlock, ObjectBlock,
-    TimeDeltaBlock)
-from .blocks import _safe_reshape  # io.packers
-from .blocks import make_block  # io.pytables, io.packers
-from .managers import (
-    BlockManager, SingleBlockManager, create_block_manager_from_arrays,
-    create_block_manager_from_blocks)
-from .managers import \
-    concatenate_block_managers  # noqa:F401; reshape.concat, reshape.merge
-from .managers import items_overlap_with_suffix  # reshape.merge
-
-from .blocks import _block_shape  # noqa:F401; io.pytables
+from .blocks import (  # noqa:F401
+    _block_shape,   # io.pytables
+    _safe_reshape,  # io.packers
+    make_block,     # io.pytables, io.packers
+    FloatBlock, IntBlock, ComplexBlock, BoolBlock, ObjectBlock,
+    TimeDeltaBlock, DatetimeBlock, DatetimeTZBlock,
+    CategoricalBlock, ExtensionBlock, Block)
+from .managers import (  # noqa:F401
+    BlockManager, SingleBlockManager,
+    create_block_manager_from_arrays, create_block_manager_from_blocks,
+    items_overlap_with_suffix,  # reshape.merge
+    concatenate_block_managers)  # reshape.concat, reshape.merge
