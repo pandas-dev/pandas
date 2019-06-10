bd5b433504795c29437cbe408dd32e87708c2508
Christopher Whelan <topherwhelan@gmail.com>
2019-06-10 11:16:56 -0700

    BENCH: fix noisy asv benchmarks that were running on exhausted generators

--- asv_bench/benchmarks/frame_ctor.py
+++ asv_bench/benchmarks/frame_ctor.py
@@ -72,6 +72,10 @@ class FromRecords:
     params = [None, 1000]
     param_names = ['nrows']
 
+    # Generators get exhausted on use, so run setup before every call
+    number = 1
+    repeat = (3, 250, 10)
+
     def setup(self, nrows):
         N = 100000
         self.gen = ((x, (x * 20), (x * 100)) for x in range(N))
