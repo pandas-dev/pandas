b6430f1d39d3ed1ef1a3432e010aedced5c8cb86
Will Ayd <william.ayd@icloud.com>
2019-06-10 14:56:40 -0400

    Removed old np13 check

--- pandas/tests/series/test_arithmetic.py
+++ pandas/tests/series/test_arithmetic.py
@@ -233,13 +233,9 @@ class TestSeriesUnary(object):
             exp = Series(operator.inv(ser.values), index=ser.index)
             tm.assert_series_equal(result, exp)
 
-            if pd.compat.numpy._np_version_under1p13:
-                # older NumPy support this
-                tm.assert_numpy_array_equal(~ser.values, op(ser.values))
-            else:
-                # inconsistent with NumPy
-                with pytest.raises(TypeError):
-                    op(ser.values)
+            # inconsistent with NumPy
+            with pytest.raises(TypeError):
+                op(ser.values)
         else:
             result = op(ser)
             exp = Series(op(ser.values), index=ser.index)
