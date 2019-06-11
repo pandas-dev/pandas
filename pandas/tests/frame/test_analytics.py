16e8ea8a878c2e08af9b89f08d049000b99e8d8c
jkovacevic <jk.simunic@gmail.com>
2019-06-11 02:16:57 +0200

    [26660] compare int with double by subtracting during describe percen… (#26768)

--- pandas/tests/frame/test_analytics.py
+++ pandas/tests/frame/test_analytics.py
@@ -704,6 +704,17 @@ class TestDataFrameAnalytics:
         result = df.describe(include='all')
         tm.assert_frame_equal(result, expected)
 
+    def test_describe_percentiles_integer_idx(self):
+        # Issue 26660
+        df = pd.DataFrame({'x': [1]})
+        pct = np.linspace(0, 1, 10 + 1)
+        result = df.describe(percentiles=pct)
+
+        expected = DataFrame(
+            {'x': [1.0, 1.0, np.NaN, 1.0, *[1.0 for _ in pct], 1.0]},
+            index=['count', 'mean', 'std', 'min', '0%', '10%', '20%', '30%',
+                   '40%', '50%', '60%', '70%', '80%', '90%', '100%', 'max'])
+        tm.assert_frame_equal(result, expected)
     # ---------------------------------------------------------------------
     # Reductions
 
