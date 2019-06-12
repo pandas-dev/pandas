02f6429662ea83b481a2156549bff482c3fcf24a
H. Vetinari <h.vetinari@gmx.com>
2019-06-12 22:54:30 +0200

    Review (jreback & simonjayhawkins)

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -58,19 +58,33 @@ def cat_safe(list_of_columns, sep):
     Auxiliary function for :meth:`str.cat`.
 
     Same signature as cat_core, but handles TypeErrors in concatenation, which
-    happen if the Series in list_of columns have the wrong dtypes or content.
+    happen if the arrays in list_of columns have the wrong dtypes or content.
+
+    Parameters
+    ----------
+    list_of_columns : list of numpy arrays
+        List of arrays to be concatenated with sep;
+        these arrays may not contain NaNs!
+    sep : string
+        The separator string for concatenating the columns
+
+    Returns
+    -------
+    nd.array
+        The concatenation of list_of_columns with sep
     """
-    # if there are any non-string values (wrong dtype or hidden behind object
-    # dtype), np.sum will fail; catch error and return with better message
     try:
         result = cat_core(list_of_columns, sep)
     except TypeError:
-        dtypes = [lib.infer_dtype(x, skipna=True) for x in list_of_columns]
-        illegal = [x not in ('string', 'empty') for x in dtypes]
-        first_offender = [x for x, y in zip(list_of_columns, illegal) if y][0]
-        raise TypeError('Concatenation requires list-likes containing only '
-                        'strings (or missing values). Offending values found '
-                        'in column {}'.format(first_offender)) from None
+        # if there are any non-string values (wrong dtype or hidden behind
+        # object dtype), np.sum will fail; catch and return with better message
+        for column in list_of_columns:
+            dtype = lib.infer_dtype(column, skipna=True)
+            if dtype not in ['string', 'empty']:
+                raise TypeError(
+                    'Concatenation requires list-likes containing only '
+                    'strings (or missing values). Offending values found in '
+                    'column {}'.format(dtype)) from None
     return result
 
 
