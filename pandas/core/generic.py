d7990f285734b2378b26706c70dc64c21ec11332
Oğuzhan Öğreden <oguzhan@oguzhanogreden.com>
2019-06-13 21:57:34 +0200

    Update pandas/core/generic.py
    Co-Authored-By: William Ayd <william.ayd@icloud.com>

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -2497,7 +2497,7 @@ class NDFrame(PandasObject, SelectionMixin):
             Specifying the datatype for columns. If a dictionary is used, the
             keys should be the column names and the values should be the
             SQLAlchemy types or strings for the sqlite3 legacy mode. If all
-            columns are of the same type, one single value can be used.
+            If a scalar is provided it will be applied to all columns.
         method : {None, 'multi', callable}, default None
             Controls the SQL insertion clause used:
 
