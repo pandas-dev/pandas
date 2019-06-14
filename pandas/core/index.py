12f9853bac77b353ba8c3ad80937428c5d25563b
Anatoly Myachev <anatoly.myachev@intel.com>
2019-06-14 15:59:36 +0300

    replaced '# noqa' statement for flake8 linter

--- pandas/core/index.py
+++ pandas/core/index.py
@@ -1,8 +1,7 @@
-# flake8: noqa
-from pandas.core.indexes.api import (
+from pandas.core.indexes.api import (  # noqa
     CategoricalIndex, DatetimeIndex, Float64Index, Index, Int64Index,
     IntervalIndex, InvalidIndexError, MultiIndex, NaT, NumericIndex,
     PeriodIndex, RangeIndex, TimedeltaIndex, UInt64Index, _all_indexes_same,
     _get_combined_index, _get_consensus_names, _get_objs_combined_axis,
     _new_Index, _union_indexes, ensure_index, ensure_index_from_sequences)
-from pandas.core.indexes.multi import _sparsify
+from pandas.core.indexes.multi import _sparsify  # noqa
