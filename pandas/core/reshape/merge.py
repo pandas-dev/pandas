a0680e09f0e71e25750cbb67af06feaa08760ee5
Harshit Saxena <connect.with.harshit@gmail.com>
2019-06-17 21:20:25 +0530

    Trying original patch

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -1089,7 +1089,9 @@ class _MergeOperation:
                     raise ValueError('len(left_on) must equal the number '
                                      'of levels in the index of "right"')
                 self.right_on = [None] * n
-            self.right_on = self.right_on or [None] * (n + 1)
+            if not self.right_on:
+                raise ValueError('both "left_on" and "right_on" '
+                                 'should be passed')
         elif self.right_on is not None:
             n = len(self.right_on)
             if self.left_index:
@@ -1097,7 +1099,9 @@ class _MergeOperation:
                     raise ValueError('len(right_on) must equal the number '
                                      'of levels in the index of "left"')
                 self.left_on = [None] * n
-            self.left_on = self.left_on or [None] * (n + 1)
+            if not self.left_on:
+                raise ValueError('both "left_on" and "right_on" '
+                                 'should be passed')
         if len(self.right_on) != len(self.left_on):
             raise ValueError("len(right_on) must equal len(left_on)")
 
