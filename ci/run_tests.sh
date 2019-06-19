8bae12976e92c8619178b43643723e3c8ecc75c8
Marc Garcia <garcia.marc@gmail.com>
2019-06-19 20:02:59 +0100

    Restoring python hash seed

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -2,6 +2,10 @@
 
 set -e
 
+# Workaround for pytest-xdist (it collects different tests in the workers if PYTHONHASHSEED is not set)
+# https://github.com/pytest-dev/pytest/issues/920
+# https://github.com/pytest-dev/pytest/issues/1075
+export PYTHONHASHSEED=$(python -c 'import random; print(random.randint(1, 4294967295))')
 export http_proxy=http://1.2.3.4 https_proxy=http://1.2.3.4;
 
 if [ -n "$LOCALE_OVERRIDE" ]; then
