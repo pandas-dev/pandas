BUG: avoid overflow in Bday generate_range, closes #24252 (#26651)

pandas/tseries/offsets.py
@@ -97,6 +97,8 @@ def apply_wraps(func):
             if tz is not None and result.tzinfo is None:
                 result = conversion.localize_pydatetime(result, tz)
 
+            result = Timestamp(result)
+
         return result
     return wrapper
 
@@ -2330,7 +2332,7 @@ class Tick(liboffsets._Tick, SingleConstructorOffset):
             # an exception, when we call using the + operator,
             # we directly call the known method
             result = other.__add__(self)
-            if result == NotImplemented:
+            if result is NotImplemented:
                 raise OverflowError
             return result
         elif isinstance(other, (datetime, np.datetime64, date)):
@@ -2467,6 +2469,11 @@ def generate_range(start=None, end=None, periods=None, offset=BDay()):
         while cur <= end:
             yield cur
 
+            if cur == end:
+                # GH#24252 avoid overflows by not performing the addition
+                # in offset.apply unless we have to
+                break
+
             # faster than cur + offset
             next_date = offset.apply(cur)
             if next_date <= cur:
@@ -2476,6 +2483,11 @@ def generate_range(start=None, end=None, periods=None, offset=BDay()):
     else:
         while cur >= end:
             yield cur
+
+            if cur == end:
+                # GH#24252 avoid overflows by not performing the addition
+                # in offset.apply unless we have to
+                break
 
             # faster than cur + offset
             next_date = offset.apply(cur)