d9ab7b0ab8113b9e3ecb237d38f8ffeb4deae490
William Ayd <william.ayd@gmail.com>
2019-06-24 18:06:03 -0400

    Removed DatetimeLikeArray annotations

--- pandas/core/indexes/datetimelike.py
+++ pandas/core/indexes/datetimelike.py
@@ -18,7 +18,6 @@ from pandas.core.dtypes.common import (
     is_period_dtype, is_scalar)
 from pandas.core.dtypes.generic import ABCIndex, ABCIndexClass, ABCSeries
 
-from pandas._typing import DatetimeLikeArray
 from pandas.core import algorithms, ops
 from pandas.core.accessor import PandasDelegate
 from pandas.core.arrays import ExtensionOpsMixin
@@ -59,7 +58,7 @@ class DatetimeIndexOpsMixin(ExtensionOpsMixin):
     """
     common ops mixin to support a unified interface datetimelike Index
     """
-    _data = None  # type: DatetimeLikeArray
+    _data = None
 
     # DatetimeLikeArrayMixin assumes subclasses are mutable, so these are
     # properties there.  They can be made into cache_readonly for Index
