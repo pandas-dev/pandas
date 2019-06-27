4d5eab12012186d2724326f7d2387089c45c0cf5
Ryan Joyce <ryan.joyce.88@gmail.com>
2019-06-27 01:24:40 -0400

    TST: added merged cell custom object xlsxwriter test

--- pandas/tests/io/excel/test_xlsxwriter.py
+++ pandas/tests/io/excel/test_xlsxwriter.py
@@ -5,6 +5,10 @@ import pytest
 from pandas import DataFrame
 from pandas.util.testing import ensure_clean
 
+from pandas.core.indexes.multi import MultiIndex
+from pandas.core.indexes.period import Period, PeriodIndex
+import numpy as np
+
 from pandas.io.excel import ExcelWriter
 
 xlsxwriter = pytest.importorskip("xlsxwriter")
@@ -63,3 +67,28 @@ def test_write_append_mode_raises(ext):
     with ensure_clean(ext) as f:
         with pytest.raises(ValueError, match=msg):
             ExcelWriter(f, engine='xlsxwriter', mode='a')
+
+def test_merged_cell_custom_objects(ext):
+    # Test that custom object types residing within merged (grouped) 
+    # cells are converted to python data types before being passed to
+    # the xlsxwriter package. Test for issue #27006
+
+    #create a custom object type, and place it in a grouped dataframe
+    pixy = PeriodIndex(['2018', '2018', '2018', '2018',
+                        '2019', '2019', '2019', '2019'], freq='Y')
+    pixq = PeriodIndex(['2018Q1', '2018Q2', '2018Q3', '2018Q4',
+                        '2019Q1', '2019Q2', '2019Q3', '2019Q4'], freq='Q')
+    pixarr = [pixy, pixq]
+    mipix = MultiIndex.from_arrays(pixarr, names=['year', 'quarter'])
+    df = DataFrame(np.random.rand(2, len(mipix)), columns=mipix)
+
+    #write the dataframe to excel
+    try:
+        with ensure_clean(ext) as path:
+            writer = ExcelWriter(path)
+            df.to_excel(writer, sheet_name='test')
+            passed = True
+    except TypeError:
+        passed = False
+
+    assert passed
