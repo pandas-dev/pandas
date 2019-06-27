7ab00fa071c10654aabba6a4158d6c6685eb1713
Jeff Reback <jeff@reback.net>
2019-06-27 10:02:45 -0500

    lint

--- pandas/tests/sparse/test_pivot.py
+++ pandas/tests/sparse/test_pivot.py
@@ -2,8 +2,8 @@ import numpy as np
 import pytest
 
 import pandas as pd
-import pandas.util.testing as tm
 from pandas import _np_version_under1p17
+import pandas.util.testing as tm
 
 
 @pytest.mark.filterwarnings("ignore:Sparse:FutureWarning")
