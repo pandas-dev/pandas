41e11e130347466f7d0c9d4dd5aba390282b85bb
Jeff Reback <jeff@reback.net>
2019-06-27 13:24:07 -0500

    use strict=False

--- pandas/tests/sparse/test_pivot.py
+++ pandas/tests/sparse/test_pivot.py
@@ -50,7 +50,8 @@ class TestPivotTable:
         # tm.assert_frame_equal(res_sparse, res_dense)
 
     @pytest.mark.xfail(not _np_version_under1p17,
-                       reason="failing occasionally on numpy > 1.17")
+                       reason="failing occasionally on numpy > 1.17",
+                       strict=False)
     def test_pivot_table_multi(self):
         res_sparse = pd.pivot_table(self.sparse, index='A', columns='B',
                                     values=['D', 'E'])
