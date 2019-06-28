3bf08f8ec2703bb5e50395b4f009b24123df7311
Jeff Reback <jeff@reback.net>
2019-06-28 07:31:01 -0500

    flake8

--- pandas/__init__.py
+++ pandas/__init__.py
@@ -46,7 +46,7 @@ from pandas.tseries.api import infer_freq
 # use the closest tagged version if possible
 from ._version import get_versions
 
-from pandas.core.api import (  # dtype; missing; indexes; tseries; conversion; misc
+from pandas.core.api import (  # dtype; missing; indexes; tseries; conversion
     Categorical, CategoricalDtype, CategoricalIndex, DataFrame, DateOffset,
     DatetimeIndex, DatetimeTZDtype, Float64Index, Grouper, Index, IndexSlice,
     Int8Dtype, Int16Dtype, Int32Dtype, Int64Dtype, Int64Index, Interval,
@@ -57,7 +57,7 @@ from pandas.core.api import (  # dtype; missing; indexes; tseries; conversion; m
     notna, notnull, np, period_range, set_eng_float_format, timedelta_range,
     to_datetime, to_numeric, to_timedelta, unique, value_counts)
 
-from pandas.io.api import (  # excel; packers; parsers; pickle; pytables; sql; misc
+from pandas.io.api import (  # excel; packers; parsers; pickle; pytables; sql
     ExcelFile, ExcelWriter, HDFStore, read_clipboard, read_csv, read_excel,
     read_feather, read_fwf, read_gbq, read_hdf, read_html, read_json,
     read_msgpack, read_parquet, read_pickle, read_sas, read_spss, read_sql,
