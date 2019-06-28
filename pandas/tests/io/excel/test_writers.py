c3133dbebb8442773ab5a6453d78dfbde4742219
Ryan Joyce <ryan.joyce.88@gmail.com>
2019-06-28 08:38:31 -0400

    BUG: XlsxWriter ignoring formats on numpy types if merged cells (#27006)

--- pandas/tests/io/excel/test_writers.py
+++ pandas/tests/io/excel/test_writers.py
@@ -1162,6 +1162,22 @@ class TestExcelWriter(_WriterBase):
                                        path="foo.{ext}".format(ext=ext))
         tm.assert_frame_equal(result, df)
 
+    def test_merged_cell_custom_objects(self, engine, merge_cells, ext):
+        # see GH-27006
+        mi = MultiIndex.from_tuples([(pd.Period('2018'), pd.Period('2018Q1')),
+                                     (pd.Period('2018'), pd.Period('2018Q2'))])
+        expected = DataFrame(np.ones((2, 2)), columns=mi)
+        expected.to_excel(self.path)
+        result = pd.read_excel(self.path, header=[0, 1],
+                               index_col=0, convert_float=False)
+        # need to convert PeriodIndexes to standard Indexes for assert equal
+        expected.columns.set_levels([[str(i) for i in mi.levels[0]],
+                                     [str(i) for i in mi.levels[1]]],
+                                    level=[0, 1],
+                                    inplace=True)
+        expected.index = expected.index.astype(np.float64)
+        tm.assert_frame_equal(expected, result)
+
 
 class TestExcelWriterEngineTests:
 
