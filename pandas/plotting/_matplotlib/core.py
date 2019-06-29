7004ef0803628aaab245222e3fc42a4f5934a8f4
andrewgsavage <andrewgsavage@gmail.com>
2019-06-29 13:43:52 +0100

    lint

--- pandas/plotting/_matplotlib/core.py
+++ pandas/plotting/_matplotlib/core.py
@@ -334,9 +334,9 @@ class MPLPlot:
         data = data._convert(datetime=True, timedelta=True)
         # Numeric categorical data gets caught by including np.number
         # in select_dtypes
-        numeric_dtypes = [dtype for dtype in set(data.dtypes) 
-                          if hasattr(dtype, "_is_numeric") and 
-                          dtype._is_numeric and 
+        numeric_dtypes = [dtype for dtype in set(data.dtypes)
+                          if hasattr(dtype, "_is_numeric") and
+                          dtype._is_numeric and
                           dtype.name is not "category"]
         numeric_data = data.select_dtypes(include=[np.number] + numeric_dtypes)
         try:
