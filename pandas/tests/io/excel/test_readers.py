6360c07f4a1ae4e6182355fbb68e67093c996059
Will Ayd <william.ayd@icloud.com>
2019-06-29 23:36:57 -0500

    Skip missing file on master

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -441,6 +441,9 @@ class TestReaders:
 
     @tm.network
     def test_read_from_http_url(self, read_ext):
+        if read_ext == '.ods':  # TODO: remove once on master
+            pytest.skip()
+
         url = ('https://raw.github.com/pandas-dev/pandas/master/'
                'pandas/tests/io/data/test1' + read_ext)
         url_table = pd.read_excel(url)
