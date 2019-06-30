a961e65c059fc7ffa61cfc692069364829776f46
alimcmaster1 <alimcmaster1@gmail.com>
2019-06-30 20:45:52 +0100

    Add back noqa and add __all__

--- pandas/io/json/__init__.py
+++ pandas/io/json/__init__.py
@@ -1,6 +1,6 @@
-from pandas.io.json._json import dumps, loads, read_json, to_json # noqa
-from pandas.io.json._normalize import json_normalize # noqa
-from pandas.io.json._table_schema import build_table_schema # noqa
+from pandas.io.json._json import dumps, loads, read_json, to_json  # noqa
+from pandas.io.json._normalize import json_normalize  # noqa
+from pandas.io.json._table_schema import build_table_schema  # noqa
 
 __all__ = [
     'dumps',
