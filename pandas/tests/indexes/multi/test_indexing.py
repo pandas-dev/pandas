7458cd82e63f46877ba296658e0e18061334c29b
Michael Slater <michael.slater@gmail.com>
2019-07-01 23:36:48 +0800

    BUG: GH26944
    Fixes GH26944 AttributeError on partial multiindex timestamp slice and includes test and PEP8 corrections w293, w391.  Simplified test code. E501. changed assertion method.

--- pandas/tests/indexes/multi/test_indexing.py
+++ pandas/tests/indexes/multi/test_indexing.py
@@ -404,8 +404,17 @@ def test_timestamp_multiindex_indexer():
     idx = pd.MultiIndex.from_product([
         pd.date_range("2019-01-01T00:15:33", periods=100, freq="H",
                       name="date"),
-        ['x'],
-        [3]
-    ])
+            ['x'],
+            [3]
+        ])
     df = pd.DataFrame({'foo': np.arange(len(idx))}, idx)
-    assert df.loc[pd.IndexSlice['2019-1-2':, "x", :], 'foo'][-1] == 98
+    result = df.loc[pd.IndexSlice['2019-1-2':, "x", :], 'foo']
+    qidx = pd.MultiIndex.from_product([
+        pd.date_range(start="2019-01-02T00:15:33", end='2019-01-05T02:15:33',
+                  freq="H", name="date"),
+            ['x'],
+            [3]
+        ])
+    should_be = pd.Series(data=np.arange(24,len(qidx)+24), index=qidx,
+                          name="foo")
+    tm.assert_series_equal(result, should_be)
