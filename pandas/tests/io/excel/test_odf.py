8ce45b45c4fedeeef872705e3cc12a6f465f9743
Will Ayd <william.ayd@icloud.com>
2019-07-01 06:36:45 -0500

    Removed one-off tests

--- pandas/tests/io/excel/test_odf.py
+++ pandas/tests/io/excel/test_odf.py
@@ -21,20 +21,7 @@ def test_read_invalid_types_raises():
     # of the included content.xml file
     with pytest.raises(ValueError,
                        match="Unrecognized type awesome_new_type"):
-        pd.read_excel("invalid_value_type.ods", header=None)
-
-
-def test_read_lower_diagonal():
-    # Make sure we can parse:
-    # 1
-    # 2 3
-    # 4 5 6
-    # 7 8 9 10
-
-    sheet = pd.read_excel("lowerdiagonal.ods", 'Sheet1',
-                          index_col=None, header=None)
-
-    assert sheet.shape == (4, 4)
+        pd.read_excel("invalid_value_type.ods")
 
 
 def test_read_writer_table():
@@ -50,24 +37,6 @@ def test_read_writer_table():
     result = pd.read_excel("writertable.odt", 'Table1', index_col=0)
 
     tm.assert_frame_equal(result, expected)
-
-
-def test_blank_row_repeat():
-    table = pd.read_excel("blank-row-repeat.ods", 'Value')
-
-    assert table.shape == (14, 2)
-    assert table['value'][7] == 9.0
-    assert pd.isnull(table['value'][8])
-    assert not pd.isnull(table['value'][11])
-
-
-def test_runlengthencoding():
-    sheet = pd.read_excel("runlengthencoding.ods", 'Sheet1', header=None)
-    assert sheet.shape == (5, 3)
-    # check by column, not by row.
-    assert list(sheet[0]) == [1.0, 1.0, 2.0, 2.0, 2.0]
-    assert list(sheet[1]) == [1.0, 2.0, 2.0, 2.0, 2.0]
-    assert list(sheet[2]) == [1.0, 2.0, 2.0, 2.0, 2.0]
 
 
 def test_raises_repeated_rows_not_in_col_0():
