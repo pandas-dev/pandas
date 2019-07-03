e4b2144862d8ddd717dd361b368074c3ce2fcf97
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-07-03 08:52:42 -0400

    simplify

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -182,22 +182,20 @@ class SeriesFormatter:
         from pandas.core.reshape.concat import concat
         min_rows = self.min_rows
         max_rows = self.max_rows
-        # if min_rows is None, follow value of max_rows
-        if max_rows and not min_rows:
-            min_rows = max_rows
-        # if both are set, min_rows is minimum of both
-        if min_rows and max_rows:
-            min_rows = min(min_rows, max_rows)
         # truncation determined by max_rows, actual truncated number of rows
         # used below by min_rows
         truncate_v = max_rows and (len(self.series) > max_rows)
         series = self.series
         if truncate_v:
-            if min_rows == 1:
-                row_num = min_rows
-                series = series.iloc[:min_rows]
+            if min_rows:
+                # if min_rows is set (not None or 0), set max_rows to minimum
+                # of both
+                max_rows = min(min_rows, max_rows)
+            if max_rows == 1:
+                row_num = max_rows
+                series = series.iloc[:max_rows]
             else:
-                row_num = min_rows // 2
+                row_num = max_rows // 2
                 series = concat((series.iloc[:row_num],
                                  series.iloc[-row_num:]))
             self.tr_row_num = row_num
