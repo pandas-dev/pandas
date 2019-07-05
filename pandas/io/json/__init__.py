3a744b494ccdd856db82d45f84a987de889a0ac7
alimcmaster1 <alimcmaster1@gmail.com>
2019-07-05 01:25:06 +0100

    black formatting

--- pandas/io/json/__init__.py
+++ pandas/io/json/__init__.py
@@ -3,10 +3,10 @@ from pandas.io.json._normalize import json_normalize
 from pandas.io.json._table_schema import build_table_schema
 
 __all__ = [
-    'dumps',
-    'loads',
-    'read_json',
-    'to_json',
-    'json_normalize',
-    'build_table_schema'
+    "dumps",
+    "loads",
+    "read_json",
+    "to_json",
+    "json_normalize",
+    "build_table_schema",
 ]
