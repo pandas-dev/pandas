72e4c9e914a750f57a884e1b6727d487c4a0169a
Matthew Roeschke <emailformattr@gmail.com>
2019-07-05 07:29:34 -0700

    BUG: merge_asof with multiple by columns with tz (#27243)

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -1686,6 +1686,9 @@ class _AsOfMerge(_OrderedMerge):
 
         def flip(xs):
             """ unlike np.transpose, this returns an array of tuples """
+            xs = [
+                x if not is_extension_array_dtype(x) else x._ndarray_values for x in xs
+            ]
             labels = list(string.ascii_lowercase[: len(xs)])
             dtypes = [x.dtype for x in xs]
             labeled_dtypes = list(zip(labels, dtypes))
