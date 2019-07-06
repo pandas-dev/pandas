790f06ecacebd1b8e4746b47c3110116eace4922
Jeff Reback <jeff@reback.net>
2019-07-06 15:51:18 -0400

    move is_list_like to cython and share impl

--- pandas/_libs/lib.pyx
+++ pandas/_libs/lib.pyx
@@ -1,3 +1,4 @@
+from collections import abc
 from decimal import Decimal
 from fractions import Fraction
 from numbers import Number
@@ -886,20 +887,58 @@ def is_period(val: object) -> bool:
     return util.is_period_object(val)
 
 
-cpdef inline bint is_list_like(object obj):
+cpdef is_list_like(obj: object, allow_sets: bool = True):
     """
+    Check if the object is list-like.
+
+    Objects that are considered list-like are for example Python
+    lists, tuples, sets, NumPy arrays, and Pandas Series.
+
+    Strings and datetime objects, however, are not considered list-like.
+
     Parameters
     ----------
-    obj : object
+    obj : The object to check
+    allow_sets : boolean, default True
+        If this parameter is False, sets will not be considered list-like
+
+        .. versionadded:: 0.24.0
 
     Returns
     -------
-    bool : if this is a list-like object
+    is_list_like : bool
+        Whether `obj` has list-like properties.
+
+    Examples
+    --------
+    >>> is_list_like([1, 2, 3])
+    True
+    >>> is_list_like({1, 2, 3})
+    True
+    >>> is_list_like(datetime(2017, 1, 1))
+    False
+    >>> is_list_like("foo")
+    False
+    >>> is_list_like(1)
+    False
+    >>> is_list_like(np.array([2]))
+    True
+    >>> is_list_like(np.array(2)))
+    False
     """
-    return (hasattr(obj, '__iter__') and
-            not isinstance(obj, (str, bytes))
-            # exclude zero-dimensional numpy arrays, effectively scalars
-            and not (isinstance(obj, ndarray) and obj.ndim == 0))
+
+    return (
+        isinstance(obj, abc.Iterable)
+        and
+        # we do not count strings/unicode/bytes as list-like
+        not isinstance(obj, (str, bytes))
+        and
+        # exclude zero-dimensional numpy arrays, effectively scalars
+        not (isinstance(obj, np.ndarray) and obj.ndim == 0)
+        and
+        # exclude sets if allow_sets is False
+        not (allow_sets is False and isinstance(obj, abc.Set))
+    )
 
 
 _TYPE_MAP = {
