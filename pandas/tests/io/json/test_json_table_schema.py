845d4b89e98d5d1678acb2007211fcf687001d12
alimcmaster1 <alimcmaster1@gmail.com>
2019-07-08 02:36:04 +0100

    Typing: Support New Mypy Semantic Analyzer (#27070)

--- pandas/tests/io/json/test_json_table_schema.py
+++ pandas/tests/io/json/test_json_table_schema.py
@@ -11,7 +11,7 @@ import pandas as pd
 from pandas import DataFrame
 import pandas.util.testing as tm
 
-from pandas.io.json.table_schema import (
+from pandas.io.json._table_schema import (
     as_json_table_type,
     build_table_schema,
     convert_json_field_to_pandas_type,
