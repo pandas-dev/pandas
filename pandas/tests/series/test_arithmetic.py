16057a4539be718bea9f2e20934a9560a27697b1
jbrockmendel <jbrockmendel@gmail.com>
2019-07-08 17:52:07 -0700

    revert

--- pandas/tests/series/test_arithmetic.py
+++ pandas/tests/series/test_arithmetic.py
@@ -24,7 +24,7 @@ class TestSeriesFlexArithmetic:
         ],
     )
     @pytest.mark.parametrize(
-        "opname", ["add", "sub", "mul", "floordiv", "truediv", "pow"]
+        "opname", ["add", "sub", "mul", "floordiv", "truediv", "div", "pow"]
     )
     def test_flex_method_equivalence(self, opname, ts):
         # check that Series.{opname} behaves like Series.__{opname}__,
@@ -34,8 +34,15 @@ class TestSeriesFlexArithmetic:
         other = ts[1](tser)
         check_reverse = ts[2]
 
+        if opname == "div":
+            pytest.skip("div test only for Py3")
+
         op = getattr(Series, opname)
-        alt = getattr(operator, opname)
+
+        if op == "div":
+            alt = operator.truediv
+        else:
+            alt = getattr(operator, opname)
 
         result = op(series, other)
         expected = alt(series, other)
