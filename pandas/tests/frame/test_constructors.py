9cb436284d95198a001d9ae33a796fe1751bf4ff
pilkibun <pilkibun@users.noreply.github.com>
2019-07-11 15:18:29 -0500

    clean

--- pandas/tests/frame/test_constructors.py
+++ pandas/tests/frame/test_constructors.py
@@ -1358,7 +1358,6 @@ class TestDataFrameConstructors:
             }
         )
         result = DataFrame(data)
-        assert set(result.columns) == set(expected.columns)
         tm.assert_frame_equal(result, expected, check_like=PY36)
 
     def test_constructor_orient(self, float_string_frame):
