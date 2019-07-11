5bb0d93e32a9ea29f631cc57de4d9cccf042836a
jbrockmendel <jbrockmendel@gmail.com>
2019-07-10 19:05:47 -0700

    remove no-longer-users expand_axes

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -565,18 +565,6 @@ class NDFrame(PandasObject, SelectionMixin):
         """ internal compat with SelectionMixin """
         return self
 
-    def _expand_axes(self, key):
-        new_axes = []
-        for k, ax in zip(key, self.axes):
-            if k not in ax:
-                if type(k) != ax.dtype.type:
-                    ax = ax.astype("O")
-                new_axes.append(ax.insert(len(ax), k))
-            else:
-                new_axes.append(ax)
-
-        return new_axes
-
     def set_axis(self, labels, axis=0, inplace=None):
         """
         Assign desired index to given axis.
