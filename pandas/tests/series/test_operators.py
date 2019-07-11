245df6e49b88c721e6451d0f0c9a3270c64306a6
jbrockmendel <jbrockmendel@gmail.com>
2019-07-10 21:07:16 -0700

    blackify

--- pandas/tests/series/test_operators.py
+++ pandas/tests/series/test_operators.py
@@ -692,10 +692,7 @@ class TestSeriesOperators(TestData):
         )
         tm.assert_series_equal(added[:-5], expected)
 
-    pairings = [
-        (Series.div, operator.truediv, 1),
-        (Series.rdiv, ops.rtruediv, 1),
-    ]
+    pairings = [(Series.div, operator.truediv, 1), (Series.rdiv, ops.rtruediv, 1)]
     for op in ["add", "sub", "mul", "pow", "truediv", "floordiv"]:
         fv = 0
         lop = getattr(Series, op)
