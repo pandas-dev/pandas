d2f9874fce5575a58e69019406764dd01db743c9
leeyspaul <t.leeyspaul@gmail.com>
2019-07-11 21:04:37 -0700

    change isort formatting
    fix isort formatting for awk
    
    fix isort format output
    
    test awk change
    
    fix isort output for azure
    
    remove comment
    
    fix deliberate import errors
    
    fix whitespace
    
    whitespace fix

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -45,10 +45,8 @@ function invgrep {
 
 if [[ "$AZURE" == "true" ]]; then
     FLAKE8_FORMAT="##vso[task.logissue type=error;sourcepath=%(path)s;linenumber=%(row)s;columnnumber=%(col)s;code=%(code)s;]%(text)s"
-    ISORT_FORMAT="##vso[task.logissue type=error;sourcepath=%(path)s]%(text)s"
 else
     FLAKE8_FORMAT="default"
-    ISORT_FORMAT="default"
 fi
 
 ### LINTING ###
@@ -111,9 +109,12 @@ if [[ -z "$CHECK" || "$CHECK" == "lint" ]]; then
 
     # Imports - Check formatting using isort see setup.cfg for settings
     MSG='Check import format using isort ' ; echo $MSG
-    isort --recursive --check-only pandas asv_bench | awk -F ":" $ISORT_FORMAT
+    if [[ "$AZURE" == "true" ]]; then
+        isort --recursive --check-only pandas asv_bench | awk -F ':' '{print $0"\n" "##vso[task.logissue type=error;sourcepath=%(path)s]%(text)s"}'
+    else
+        isort --recursive --check-only pandas asv_bench | awk -F ':' '{print}'
+    fi
     RET=$(($RET + $?)) ; echo $MSG "DONE"
-
 fi
 
 ### PATTERNS ###
