9c6e6168bdfce5ab04f2f6456441a5a2e4de0abb
Will Ayd <william.ayd@icloud.com>
2019-07-16 13:27:45 -0700

    Removed ABCs from pandas._typing

--- pandas/_typing.py
+++ pandas/_typing.py
@@ -1,33 +1,35 @@
 from pathlib import Path
-from typing import IO, AnyStr, TypeVar, Union
+from typing import IO, TYPE_CHECKING, AnyStr, TypeVar, Union
 
 import numpy as np
 
-from pandas._libs import Timestamp
-from pandas._libs.tslibs.period import Period
-from pandas._libs.tslibs.timedeltas import Timedelta
+if TYPE_CHECKING:  # Use for any internal imports
+    from pandas._libs import Timestamp
+    from pandas._libs.tslibs.period import Period
+    from pandas._libs.tslibs.timedeltas import Timedelta
 
-from pandas.core.dtypes.dtypes import ExtensionDtype
-from pandas.core.dtypes.generic import (
-    ABCDataFrame,
-    ABCExtensionArray,
-    ABCIndexClass,
-    ABCSeries,
-    ABCSparseSeries,
-)
+    from pandas.core.arrays.base import ExtensionArray
+    from pandas.core.dtypes.dtypes import ExtensionDtype
+    from pandas.core.dtypes.generic import (
+        ABCDataFrame,
+        ABCExtensionArray,
+        ABCIndexClass,
+        ABCSeries,
+        ABCSparseSeries,
+    )
+    from pandas.core.indexes.base import Index
+    from pandas.core.frame import DataFrame
+    from pandas.core.series import Series
+    from pandas.core.sparse.series import SparseSeries
+
 
 AnyArrayLike = TypeVar(
-    "AnyArrayLike",
-    ABCExtensionArray,
-    ABCIndexClass,
-    ABCSeries,
-    ABCSparseSeries,
-    np.ndarray,
+    "AnyArrayLike", "ExtensionArray", "Index", "Series", "SparseSeries", np.ndarray
 )
-ArrayLike = TypeVar("ArrayLike", ABCExtensionArray, np.ndarray)
-DatetimeLikeScalar = TypeVar("DatetimeLikeScalar", Period, Timestamp, Timedelta)
-Dtype = Union[str, np.dtype, ExtensionDtype]
+ArrayLike = TypeVar("ArrayLike", "ExtensionArray", np.ndarray)
+DatetimeLikeScalar = TypeVar("DatetimeLikeScalar", "Period", "Timestamp", "Timedelta")
+Dtype = Union[str, np.dtype, "ExtensionDtype"]
 FilePathOrBuffer = Union[str, Path, IO[AnyStr]]
 
-FrameOrSeries = TypeVar("FrameOrSeries", ABCSeries, ABCDataFrame)
+FrameOrSeries = TypeVar("FrameOrSeries", "Series", "DataFrame")
 Scalar = Union[str, int, float]
