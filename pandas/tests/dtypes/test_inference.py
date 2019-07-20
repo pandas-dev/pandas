d416771b0594e4f811eef9207529266259db9c07
BeforeFlight <m07sss@gmail.com>
2019-07-21 00:52:30 +0300

    Linter fixes.

--- pandas/tests/dtypes/test_inference.py
+++ pandas/tests/dtypes/test_inference.py
@@ -533,24 +533,21 @@ class TestInference:
 
     def test_maybe_convert_objects_datetime(self):
         # GH27438
-        arr = np.array([np.datetime64('2000-01-01'),
-                        np.timedelta64(1, 's')], dtype=object)
+        arr = np.array(
+            [np.datetime64("2000-01-01"), np.timedelta64(1, "s")], dtype=object
+        )
         exp = arr.copy()
-        out = lib.maybe_convert_objects(arr, convert_datetime=1,
-                                        convert_timedelta=1)
+        out = lib.maybe_convert_objects(arr, convert_datetime=1, convert_timedelta=1)
         tm.assert_numpy_array_equal(out, exp)
 
-        arr = np.array([pd.NaT, np.timedelta64(1, 's')], dtype=object)
-        exp = np.array([np.timedelta64('NaT'), np.timedelta64(1, 's')],
-                       dtype='m8[ns]')
-        out = lib.maybe_convert_objects(arr, convert_datetime=1,
-                                        convert_timedelta=1)
+        arr = np.array([pd.NaT, np.timedelta64(1, "s")], dtype=object)
+        exp = np.array([np.timedelta64("NaT"), np.timedelta64(1, "s")], dtype="m8[ns]")
+        out = lib.maybe_convert_objects(arr, convert_datetime=1, convert_timedelta=1)
         tm.assert_numpy_array_equal(out, exp)
 
-        arr = np.array([np.timedelta64(1, 's'), np.nan], dtype=object)
+        arr = np.array([np.timedelta64(1, "s"), np.nan], dtype=object)
         exp = arr.copy()
-        out = lib.maybe_convert_objects(arr, convert_datetime=1,
-                                        convert_timedelta=1)
+        out = lib.maybe_convert_objects(arr, convert_datetime=1, convert_timedelta=1)
         tm.assert_numpy_array_equal(out, exp)
 
     def test_mixed_dtypes_remain_object_array(self):
