a04fec672ed3708ae770058f398fccb1baa6a63a
Tony Lorenzo <atlorenzo@email.arizona.edu>
2019-07-23 15:35:08 -0700

    parametrize test with tz_naive_fixture

--- pandas/tests/series/test_missing.py
+++ pandas/tests/series/test_missing.py
@@ -1522,19 +1522,14 @@ class TestSeriesInterpolateData:
 
     @td.skip_if_no_scipy
     @pytest.mark.parametrize("method", ["nearest", "pad"])
-    def test_interp_datetime64(self, method):
-        df = Series([1, np.nan, 3], index=date_range("1/1/2000", periods=3))
-        result = df.interpolate(method=method)
-        expected = Series([1.0, 1.0, 3.0], index=date_range("1/1/2000", periods=3))
-        assert_series_equal(result, expected)
-
-    @td.skip_if_no_scipy
-    @pytest.mark.parametrize("method", ["nearest", "pad"])
-    def test_interp_datetime64_tz(self, method):
-        df = Series([1, np.nan, 3], index=date_range("1/1/2000", periods=3, tz="UTC"))
+    def test_interp_datetime64(self, method, tz_naive_fixture):
+        df = Series(
+            [1, np.nan, 3], index=date_range("1/1/2000", periods=3, tz=tz_naive_fixture)
+        )
         result = df.interpolate(method=method)
         expected = Series(
-            [1.0, 1.0, 3.0], index=date_range("1/1/2000", periods=3, tz="UTC")
+            [1.0, 1.0, 3.0],
+            index=date_range("1/1/2000", periods=3, tz=tz_naive_fixture),
         )
         assert_series_equal(result, expected)
 
