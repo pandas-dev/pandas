a55b5dfe0bfceb07ea138b74c7eb5a85355f7530
Big Head <yanglinlee@gmail.com>
2019-07-23 18:06:15 -0400

    TST: Add test for operations on DataFrame with Interval CategoricalIndex (#27443)

--- pandas/tests/frame/test_arithmetic.py
+++ pandas/tests/frame/test_arithmetic.py
@@ -642,3 +642,14 @@ class TestFrameArithmetic:
         val3 = np.random.rand(*df.shape)
         added = pd.DataFrame(df.values + val3, index=df.index, columns=df.columns)
         tm.assert_frame_equal(df.add(val3), added)
+
+    def test_operations_with_interval_categories_index(self, all_arithmetic_operators):
+        # GH#27415
+        op = all_arithmetic_operators
+        ind = pd.CategoricalIndex(pd.interval_range(start=0.0, end=2.0))
+        data = [1, 2]
+        df = pd.DataFrame([data], columns=ind)
+        num = 10
+        result = getattr(df, op)(num)
+        expected = pd.DataFrame([[getattr(n, op)(num) for n in data]], columns=ind)
+        tm.assert_frame_equal(result, expected)
