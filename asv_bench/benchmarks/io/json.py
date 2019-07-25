7c6def85971745acf1468afacce2cfbb209d9379
Will Ayd <william.ayd@icloud.com>
2019-07-25 15:03:44 -0700

    Broke out lines tests

--- asv_bench/benchmarks/io/json.py
+++ asv_bench/benchmarks/io/json.py
@@ -126,19 +126,69 @@ class ToJSON(BaseIO):
     def time_float_int_str(self, orient):
         self.df_int_float_str.to_json(self.fname, orient=orient)
 
-    def time_floats_with_int_idex_lines(self, orient):
+
+class ToJSON(BaseIO):
+
+    fname = "__test__.json"
+
+    def setup(self):
+        N = 10 ** 5
+        ncols = 5
+        index = date_range("20000101", periods=N, freq="H")
+        timedeltas = timedelta_range(start=1, periods=N, freq="s")
+        datetimes = date_range(start=1, periods=N, freq="s")
+        ints = np.random.randint(100000000, size=N)
+        floats = np.random.randn(N)
+        strings = tm.makeStringIndex(N)
+        self.df = DataFrame(np.random.randn(N, ncols), index=np.arange(N))
+        self.df_date_idx = DataFrame(np.random.randn(N, ncols), index=index)
+        self.df_td_int_ts = DataFrame(
+            {
+                "td_1": timedeltas,
+                "td_2": timedeltas,
+                "int_1": ints,
+                "int_2": ints,
+                "ts_1": datetimes,
+                "ts_2": datetimes,
+            },
+            index=index,
+        )
+        self.df_int_floats = DataFrame(
+            {
+                "int_1": ints,
+                "int_2": ints,
+                "int_3": ints,
+                "float_1": floats,
+                "float_2": floats,
+                "float_3": floats,
+            },
+            index=index,
+        )
+        self.df_int_float_str = DataFrame(
+            {
+                "int_1": ints,
+                "int_2": ints,
+                "float_1": floats,
+                "float_2": floats,
+                "str_1": strings,
+                "str_2": strings,
+            },
+            index=index,
+        )
+
+    def time_floats_with_int_idex_lines(self):
         self.df.to_json(self.fname, orient="records", lines=True)
 
-    def time_floats_with_dt_index_lines(self, orient):
+    def time_floats_with_dt_index_lines(self):
         self.df_date_idx.to_json(self.fname, orient="records", lines=True)
 
-    def time_delta_int_tstamp_lines(self, orient):
+    def time_delta_int_tstamp_lines(self):
         self.df_td_int_ts.to_json(self.fname, orient="records", lines=True)
 
-    def time_float_int_lines(self, orient):
+    def time_float_int_lines(self):
         self.df_int_floats.to_json(self.fname, orient="records", lines=True)
 
-    def time_float_int_str_lines(self, orient):
+    def time_float_int_str_lines(self):
         self.df_int_float_str.to_json(self.fname, orient="records", lines=True)
 
 
