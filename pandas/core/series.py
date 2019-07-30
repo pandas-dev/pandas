be1a43cc341fc6ff01fe12cad3884af5ab3965b9
h-vetinari <33685575+h-vetinari@users.noreply.github.com>
2019-07-30 11:10:48 +0200

    DOC: improve warnings for Series.{real,imag} (#27651)

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -958,7 +958,9 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
         .. deprecated 0.25.0
         """
         warnings.warn(
-            "`real` has be deprecated and will be removed in a " "future verison",
+            "`real` is deprecated and will be removed in a future version. "
+            "To eliminate this warning for a Series `ser`, use "
+            "`np.real(ser.to_numpy())` or `ser.to_numpy().real`.",
             FutureWarning,
             stacklevel=2,
         )
@@ -976,7 +978,9 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
         .. deprecated 0.25.0
         """
         warnings.warn(
-            "`imag` has be deprecated and will be removed in a " "future verison",
+            "`imag` is deprecated and will be removed in a future version. "
+            "To eliminate this warning for a Series `ser`, use "
+            "`np.imag(ser.to_numpy())` or `ser.to_numpy().imag`.",
             FutureWarning,
             stacklevel=2,
         )
