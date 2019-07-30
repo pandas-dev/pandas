1125126d74906fc67a7bb46d0633e8627bf308c3
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2019-07-30 04:21:48 -0700

    Backport PR #27631: BUG: raise when wrong level name is passed to "unstack" (#27640)

--- pandas/tests/series/test_alter_axes.py
+++ pandas/tests/series/test_alter_axes.py
@@ -322,9 +322,9 @@ class TestSeriesAlterAxes:
 
         # KeyError raised for series index when passed level name is missing
         s = Series(range(4))
-        with pytest.raises(KeyError, match="must be same as name"):
+        with pytest.raises(KeyError, match="does not match index name"):
             s.reset_index("wrong", drop=True)
-        with pytest.raises(KeyError, match="must be same as name"):
+        with pytest.raises(KeyError, match="does not match index name"):
             s.reset_index("wrong")
 
         # KeyError raised for series when level to be dropped is missing
