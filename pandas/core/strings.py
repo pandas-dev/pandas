637686b941f6020158892a748ab7b6c68da1ee1f
H. Vetinari <h.vetinari@gmx.com>
2019-07-30 09:53:43 +0200

    add comment

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -60,6 +60,7 @@ def cat_core(list_of_columns: List, sep: str):
         The concatenation of list_of_columns with sep
     """
     if sep == "":
+        # no need to interleave sep if it is empty
         return np.sum(list_of_columns, axis=0)
     list_with_sep = [sep] * (2 * len(list_of_columns) - 1)
     list_with_sep[::2] = list_of_columns
