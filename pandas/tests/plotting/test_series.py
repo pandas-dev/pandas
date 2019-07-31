951ede40fc649f0e660e8a6560bc7e3cacc338ac
charlesdong1991 <kaiqi@kth.se>
2019-07-31 21:29:17 +0200

    Fix linting error

--- pandas/tests/plotting/test_series.py
+++ pandas/tests/plotting/test_series.py
@@ -172,7 +172,7 @@ class TestSeriesPlots(TestPlotBase):
         s = Series([False, False, True])
         _check_plot_works(s.plot, include_bool=True)
 
-        msg = 'no numeric data to plot'
+        msg = "no numeric data to plot"
         with pytest.raises(TypeError, match=msg):
             _check_plot_works(s.plot)
 
