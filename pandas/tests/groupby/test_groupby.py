1413929c06e5f02126d4e09459784f4bfeb6402e
charlesdong1991 <kaiqi@kth.se>
2019-08-02 09:11:32 +0200

    Add test case for MI columns

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -1874,3 +1874,11 @@ def test_groupby_axis_1(group_name):
     results = df.groupby(group_name, axis=1).sum()
     expected = df.T.groupby(group_name).sum().T
     assert_frame_equal(results, expected)
+
+    # test on MI column
+    iterables = [["bar", "baz", "foo"], ["one", "two"]]
+    mi = pd.MultiIndex.from_product(iterables=iterables, names=["x", "x1"])
+    df = pd.DataFrame(np.arange(18).reshape(3, 6), index=[0, 1, 0], columns=mi)
+    results = df.groupby(group_name, axis=1).sum()
+    expected = df.T.groupby(group_name).sum().T
+    assert_frame_equal(results, expected)
