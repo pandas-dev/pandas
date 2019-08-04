868d2a22e1337a78052620b9573cb2732dcdb1e1
jbrockmendel <jbrockmendel@gmail.com>
2019-08-04 09:07:26 -0700

    remove dispatch_to_index_op

--- pandas/core/ops/__init__.py
+++ pandas/core/ops/__init__.py
@@ -602,42 +602,6 @@ def dispatch_to_series(left, right, func, str_rep=None, axis=None):
     return result
 
 
-def dispatch_to_index_op(op, left, right, index_class):
-    """
-    Wrap Series left in the given index_class to delegate the operation op
-    to the index implementation.  DatetimeIndex and TimedeltaIndex perform
-    type checking, timezone handling, overflow checks, etc.
-
-    Parameters
-    ----------
-    op : binary operator (operator.add, operator.sub, ...)
-    left : Series
-    right : object
-    index_class : DatetimeIndex or TimedeltaIndex
-
-    Returns
-    -------
-    result : object, usually DatetimeIndex, TimedeltaIndex, or Series
-    """
-    left_idx = index_class(left)
-
-    # avoid accidentally allowing integer add/sub.  For datetime64[tz] dtypes,
-    # left_idx may inherit a freq from a cached DatetimeIndex.
-    # See discussion in GH#19147.
-    if getattr(left_idx, "freq", None) is not None:
-        left_idx = left_idx._shallow_copy(freq=None)
-    try:
-        result = op(left_idx, right)
-    except NullFrequencyError:
-        # DatetimeIndex and TimedeltaIndex with freq == None raise ValueError
-        # on add/sub of integers (or int-like).  We re-raise as a TypeError.
-        raise TypeError(
-            "incompatible type for a datetime/timedelta "
-            "operation [{name}]".format(name=op.__name__)
-        )
-    return result
-
-
 def dispatch_to_extension_op(op, left, right):
     """
     Assume that left or right is a Series backed by an ExtensionArray,
@@ -1010,7 +974,7 @@ def _arith_method_SERIES(cls, op, special):
         elif is_timedelta64_dtype(right):
             # We should only get here with non-scalar or timedelta64('NaT')
             #  values for right
-            # Note: we cannot use dispatch_to_index_op because
+            # Note: we cannot use dispatch_to_extension_op because
             #  that may incorrectly raise TypeError when we
             #  should get NullFrequencyError
             orig_right = right
