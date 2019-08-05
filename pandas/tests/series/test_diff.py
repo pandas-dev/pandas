dd8ff9839fa515b8e04dd56b55ed40081127d62a
Unprocessable <Unprocessable@users.noreply.github.com>
2019-08-05 17:04:48 +0200

    Using tm to assert series differences

--- pandas/tests/series/test_diff.py
+++ pandas/tests/series/test_diff.py
@@ -1,24 +1,23 @@
-from pandas import (
-    Series
-)
-
 from numpy import nan
+from pandas import Series
+import pandas.util.testing as tm
 
 
 def test_diff():
+    '''
+    Tests the pd.Series diff function on boolean series.
+    '''
     data = Series(
-        [0, -1, -2, -3, -4, -3, -2, -1, 0, -1, -1, 0, -1, -2, -3, -2, 0]
+        [0, -1, -2, -3, -4, -3, -2, -1, 0]
     )
     filtered = data.between(-2, 0, inclusive=True)
     diff_boolean = filtered.diff()
     expected_boolean = Series(
-        [nan, False, False, True, False, False, True, False, False,
-         False, False, False, False, False, True, True, False]
+        [nan, False, False, True, False, False, True, False, False]
     )
-    assert diff_boolean.equals(expected_boolean)
+    tm.assert_series_equal(diff_boolean, expected_boolean)
     diff_data = data.diff()
     expected_data = Series(
-        [nan, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
-         -1.0, 0.0, 1.0, -1.0, -1.0, -1.0, 1.0, 2.0]
+        [nan, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0]
     )
-    assert diff_data.equals(expected_data)
+    tm.assert_series_equal(diff_data, expected_data)
