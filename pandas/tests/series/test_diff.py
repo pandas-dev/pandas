ecf1c04a27e73514ec7e6fc4e6518c6e78a69530
Unprocessable <Unprocessable@users.noreply.github.com>
2019-08-05 16:06:48 +0200

    Update test_diff.py

--- pandas/tests/series/test_diff.py
+++ pandas/tests/series/test_diff.py
@@ -12,13 +12,13 @@ def test_diff():
     filtered = data.between(-2, 0, inclusive=True)
     diff_boolean = filtered.diff()
     expected_boolean = Series(
-        [nan, False, False, True, False, False, True, False, False, \
+        [nan, False, False, True, False, False, True, False, False,
          False, False, False, False, False, True, True, False]
     )
     assert diff_boolean.equals(expected_boolean)
     diff_data = data.diff()
     expected_data = Series(
-        [nan, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, \
+        [nan, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0,
          -1.0, 0.0, 1.0, -1.0, -1.0, -1.0, 1.0, 2.0]
     )
     assert diff_data.equals(expected_data)
