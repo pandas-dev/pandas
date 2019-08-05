6c3875e5b640e2eee9139737864171bcac7a10b6
Unprocessable <Unprocessable@users.noreply.github.com>
2019-08-05 16:04:56 +0200

    Changing style to match PEP

--- pandas/tests/series/test_diff.py
+++ pandas/tests/series/test_diff.py
@@ -4,12 +4,21 @@ from pandas import (
 
 from numpy import nan
 
+
 def test_diff():
-    data = Series([0,-1,-2,-3,-4,-3,-2,-1,0,-1,-1,0,-1,-2,-3,-2,0])
-    filtered = data.between(-2,0, inclusive = True)
+    data = Series(
+        [0, -1, -2, -3, -4, -3, -2, -1, 0, -1, -1, 0, -1, -2, -3, -2, 0]
+    )
+    filtered = data.between(-2, 0, inclusive=True)
     diff_boolean = filtered.diff()
-    expected_boolean = Series([nan, False, False, True, False, False, True, False, False, False, False, False, False, False, True, True, False])
+    expected_boolean = Series(
+        [nan, False, False, True, False, False, True, False, False, \
+         False, False, False, False, False, True, True, False]
+    )
     assert diff_boolean.equals(expected_boolean)
     diff_data = data.diff()
-    expected_data = Series([nan, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, -1.0, 0.0, 1.0, -1.0, -1.0, -1.0, 1.0, 2.0])
+    expected_data = Series(
+        [nan, -1.0, -1.0, -1.0, -1.0, 1.0, 1.0, 1.0, 1.0, \
+         -1.0, 0.0, 1.0, -1.0, -1.0, -1.0, 1.0, 2.0]
+    )
     assert diff_data.equals(expected_data)
