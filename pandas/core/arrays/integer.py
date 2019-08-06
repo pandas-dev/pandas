a4e15bc9f612927596ce683be1ea557dda42a4d2
jbrockmendel <jbrockmendel@gmail.com>
2019-08-06 10:47:03 -0700

    remove unused imports

--- pandas/core/arrays/integer.py
+++ pandas/core/arrays/integer.py
@@ -21,12 +21,7 @@ from pandas.core.dtypes.common import (
     is_scalar,
 )
 from pandas.core.dtypes.dtypes import register_extension_dtype
-from pandas.core.dtypes.generic import (
-    ABCDatetimeArray,
-    ABCIndexClass,
-    ABCSeries,
-    ABCTimedeltaArray,
-)
+from pandas.core.dtypes.generic import ABCIndexClass, ABCSeries
 from pandas.core.dtypes.missing import isna, notna
 
 from pandas.core import nanops, ops
