18d28c88b4046bd04c74c0550b607777e6af4642
Will Ayd <william.ayd@icloud.com>
2019-08-07 09:35:38 -0700

    Parametrized frame test

--- pandas/tests/io/json/test_pandas.py
+++ pandas/tests/io/json/test_pandas.py
@@ -1012,29 +1012,31 @@ class TestPandasContainer:
             result = read_json(dumps(data))[["id", infer_word]]
             assert_frame_equal(result, expected)
 
-    def test_date_format_frame(self):
+    @pytest.mark.parametrize("date,date_unit", [
+        ("20130101 20:43:42.123", None),
+        ("20130101 20:43:42", "s"),
+        ("20130101 20:43:42.123", "ms"),
+        ("20130101 20:43:42.123456", "us"),
+        ("20130101 20:43:42.123456789", "ns")
+    ])            
+    def test_date_format_frame(self, date, date_unit):
         df = self.tsframe.copy()
 
-        def test_w_date(date, date_unit=None):
-            df["date"] = Timestamp(date)
-            df.iloc[1, df.columns.get_loc("date")] = pd.NaT
-            df.iloc[5, df.columns.get_loc("date")] = pd.NaT
-            if date_unit:
-                json = df.to_json(date_format="iso", date_unit=date_unit)
-            else:
-                json = df.to_json(date_format="iso")
-            result = read_json(json)
-            expected = df.copy()
-            expected.index = expected.index.tz_localize("UTC")
-            expected["date"] = expected["date"].dt.tz_localize("UTC")
-            assert_frame_equal(result, expected)
+        df["date"] = Timestamp(date)
+        df.iloc[1, df.columns.get_loc("date")] = pd.NaT
+        df.iloc[5, df.columns.get_loc("date")] = pd.NaT
+        if date_unit:
+            json = df.to_json(date_format="iso", date_unit=date_unit)
+        else:
+            json = df.to_json(date_format="iso")
+        result = read_json(json)
+        expected = df.copy()
+        expected.index = expected.index.tz_localize("UTC")
+        expected["date"] = expected["date"].dt.tz_localize("UTC")
+        assert_frame_equal(result, expected)
 
-        test_w_date("20130101 20:43:42.123")
-        test_w_date("20130101 20:43:42", date_unit="s")
-        test_w_date("20130101 20:43:42.123", date_unit="ms")
-        test_w_date("20130101 20:43:42.123456", date_unit="us")
-        test_w_date("20130101 20:43:42.123456789", date_unit="ns")
-
+    def test_date_format_frame_raises(self):
+        df = self.tsframe.copy()
         msg = "Invalid value 'foo' for option 'date_unit'"
         with pytest.raises(ValueError, match=msg):
             df.to_json(date_format="iso", date_unit="foo")
@@ -1043,7 +1045,7 @@ class TestPandasContainer:
         ("20130101 20:43:42.123", None),
         ("20130101 20:43:42", "s"),
         ("20130101 20:43:42.123", "ms"),
-        ("20130101 20:43:42.123456789", "us"),
+        ("20130101 20:43:42.123456", "us"),
         ("20130101 20:43:42.123456789", "ns")
     ])
     def test_date_format_series(self, date, date_unit):
