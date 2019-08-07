87ff677bf8d18b6399f282c49cc6c10a3445fcec
Christopher Whelan <topherwhelan@gmail.com>
2019-08-07 06:27:03 -0700

    [BLD] Add script that fails build if git tags do not exist (#27770)

--- pandas/tests/test_common.py
+++ pandas/tests/test_common.py
@@ -1,4 +1,5 @@
 import collections
+from distutils.version import LooseVersion
 from functools import partial
 import string
 
@@ -117,3 +118,13 @@ def test_git_version():
     git_version = pd.__git_version__
     assert len(git_version) == 40
     assert all(c in string.hexdigits for c in git_version)
+
+
+def test_version_tag():
+    version = pd.__version__
+    try:
+        version > LooseVersion("0.0.1")
+    except TypeError:
+        raise ValueError(
+            "No git tags exist, please sync tags between upstream and your repo"
+        )
