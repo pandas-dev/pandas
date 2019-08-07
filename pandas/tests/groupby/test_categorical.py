e63c2d831f31cc741197df5d0d23a23f03d86571
Kaiqi <kaiqi@kth.se>
2019-08-07 07:59:32 +0200

    Change name

--- pandas/tests/groupby/test_categorical.py
+++ pandas/tests/groupby/test_categorical.py
@@ -1171,7 +1171,7 @@ def test_seriesgroupby_observed_apply_dict(df_cat, observed, index, data):
 def test_groupby_categorical_axis_1(code):
     # GH 13420
     df = DataFrame({"a": [1, 2, 3, 4], "b": [-1, -2, -3, -4], "c": [5, 6, 7, 8]})
-    grp = pd.Categorical.from_codes(code, categories=list("abc"))
-    result = df.groupby(grp, axis=1).mean()
-    expected = df.T.groupby(grp, axis=0).mean().T
+    cat = pd.Categorical.from_codes(code, categories=list("abc"))
+    result = df.groupby(cat, axis=1).mean()
+    expected = df.T.groupby(cat, axis=0).mean().T
     assert_frame_equal(result, expected)
