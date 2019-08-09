3c27a50b74d2e5deb8cbeec1609913a9a7b08a9b
avelineg <50472542+avelineg@users.noreply.github.com>
2019-08-09 11:34:31 +0800

    Add example for Series.str.slice()
    Add example to show that negative values for start are allowed for Series.str.slice().

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -1442,11 +1442,11 @@ def str_slice(arr, start=None, stop=None, step=None):
     2    hameleon
     dtype: object
 
-	>>> s.str.slice(start=-1)
-	0           a
-	1           x
-	2           n
-	dtype: object
+    >>> s.str.slice(start=-1)
+    0           a
+    1           x
+    2           n
+    dtype: object
 
     >>> s.str.slice(stop=2)
     0    ko
