1569685478297046a914a46901041e5f1f26741f
Chris Stadler <chrisstadler@gmail.com>
2019-08-09 09:30:40 -0400

    Move comment to top of file

--- pandas/io/s3.py
+++ pandas/io/s3.py
@@ -1,11 +1,10 @@
+""" s3 support for remote file interactivity """
 from typing import IO, Any, Optional, Tuple
 from urllib.parse import urlparse as parse_url
 
 from pandas.compat._optional import import_optional_dependency
 
 from pandas._typing import FilePathOrBuffer
-
-""" s3 support for remote file interactivity """
 
 s3fs = import_optional_dependency(
     "s3fs", extra="The s3fs package is required to handle s3 files."
