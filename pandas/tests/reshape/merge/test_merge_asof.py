8f8aeb37df06a33b6791bbd731cbea9233d09237
ianzur <ian.zurutuza@gmail.com>
2019-08-09 10:41:07 -0500

    :bug: :wrench:

--- pandas/tests/reshape/merge/test_merge_asof.py
+++ pandas/tests/reshape/merge/test_merge_asof.py
@@ -1262,29 +1262,32 @@ class TestAsOfMerge:
         # GH 27642
 
         left = pd.DataFrame(
-            {
-                "time": pd.to_timedelta([0, 5, 10, 15, 20, 25], "ms"),
-                "left": [0, 1, 2, 3, 4, 5],
-            }
+            list(zip([0, 5, 10, 15, 20, 25],
+            [0, 1, 2, 3, 4, 5])),
+            columns=['time', 'left']
         )
+
+        left['time'] = pd.to_timedelta(left['time'], "ms")
 
         right = pd.DataFrame(
-            {
-                "time": pd.to_timedelta([0, 3, 9, 12, 15, 18], "ms"),
-                "right": [0, 1, 2, 3, 4, 5],
-            }
+                list(zip([0, 3, 9, 12, 15, 18],
+                [0, 1, 2, 3, 4, 5])), 
+                columns=['time', 'right']
         )
 
+        right['time'] = pd.to_timedelta(right['time'], "ms")
+
         expected = pd.DataFrame(
-            {
-                "time": pd.to_timedelta([0, 5, 10, 15, 20, 25], "ms"),
-                "left": [0, 1, 2, 3, 4, 5],
-                "right": [0, np.nan, 2, 4, np.nan, np.nan],
-            }
+            list(zip([0, 5, 10, 15, 20, 25],
+            [0, 1, 2, 3, 4, 5],
+            [0, np.nan, 2, 4, np.nan, np.nan])),
+            columns=['time', 'left', 'right']
         )
+
+        expected['time'] = pd.to_timedelta(expected['time'], "ms")
 
         result = pd.merge_asof(
             left, right, on="time", tolerance=Timedelta("1ms"), direction="nearest"
         )
 
-        assert_frame_equal(result, expected, check_names=False)
+        assert_frame_equal(result, expected)
