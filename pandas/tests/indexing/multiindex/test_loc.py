a5adb2e99a068578cdcc96b7cbdf1f064220f1c4
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-08-12 09:49:03 +0200

    add simpler test + whatsnew

--- pandas/tests/indexing/multiindex/test_loc.py
+++ pandas/tests/indexing/multiindex/test_loc.py
@@ -392,7 +392,7 @@ def test_loc_getitem_lowerdim_corner(multiindex_dataframe_random_data):
     assert result == expected
 
 
-def test_loc_setitem_object_array():
+def test_loc_setitem_single_column_slice():
     # case from https://github.com/pandas-dev/pandas/issues/27841
     df = DataFrame(
         "string",
@@ -402,3 +402,14 @@ def test_loc_setitem_object_array():
     df["labels"] = "a"
     df.loc[:, "labels"] = df.index
     tm.assert_numpy_array_equal(np.asarray(df["labels"]), np.asarray(df.index))
+
+    # test with non-object block
+    df = DataFrame(
+        np.nan,
+        index=range(4),
+        columns=MultiIndex.from_tuples([("A", "1"), ("A", "2"), ("B", "1")]),
+    )
+    expected = df.copy()
+    df.loc[:, "B"] = np.arange(4)
+    expected.iloc[:, 2] = np.arange(4)
+    tm.assert_frame_equal(df, expected)
