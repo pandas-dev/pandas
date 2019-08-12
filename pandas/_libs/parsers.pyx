2415e1f784f791f85c05bee4856aaa342d293659
Guilherme <gfs8@duke.edu>
2019-08-12 17:36:01 -0400

    Raise RuntimeError when calling a method of lzma when lzma is not available.

--- pandas/_libs/parsers.pyx
+++ pandas/_libs/parsers.pyx
@@ -646,6 +646,7 @@ cdef class TextReader:
                     raise ValueError('Multiple files found in compressed '
                                      'zip file %s', str(zip_names))
             elif self.compression == 'xz':
+                if lzma is None: raise RuntimeError("lzma module not available.")
                 if isinstance(source, str):
                     source = lzma.LZMAFile(source, 'rb')
                 else:
