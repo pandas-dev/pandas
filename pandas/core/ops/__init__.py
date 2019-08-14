b0cd06a562d2127150cbb89bca0d30d515025393
jbrockmendel <jbrockmendel@gmail.com>
2019-08-14 14:43:59 -0700

    remove incorrect check

--- pandas/core/ops/__init__.py
+++ pandas/core/ops/__init__.py
@@ -831,13 +831,6 @@ def _comp_method_SERIES(cls, op, special):
 
         if isinstance(other, ABCSeries) and not self._indexed_same(other):
             raise ValueError("Can only compare identically-labeled Series objects")
-        elif (
-            is_list_like(other)
-            and len(other) != len(self)
-            and not isinstance(other, (set, frozenset))
-        ):
-            raise ValueError("Lengths must match")
-
         elif isinstance(other, (np.ndarray, ABCIndexClass, ABCSeries)):
             # TODO: make this treatment consistent across ops and classes.
             #  We are not catching all listlikes here (e.g. frozenset, tuple)
