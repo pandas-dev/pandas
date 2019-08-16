2061a506b6345272eb342d7d1a55c2049f7124ac
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2019-08-16 12:22:19 +0100

    Backport PR #27932: BUG: Ensure that fill_na in Categorical only replaces null values

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -1864,8 +1864,8 @@ class Categorical(ExtensionArray, PandasObject):
                     raise ValueError("fill value must be in categories")
 
                 values_codes = _get_codes_for_values(value, self.categories)
-                indexer = np.where(values_codes != -1)
-                codes[indexer] = values_codes[values_codes != -1]
+                indexer = np.where(codes == -1)
+                codes[indexer] = values_codes[indexer]
 
             # If value is not a dict or Series it should be a scalar
             elif is_hashable(value):
