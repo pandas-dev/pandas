5cd0c83cf49f1e75e3e9cc4880043906397e2358
Kaiqi Dong <kaiqi@kth.se>
2019-08-16 13:38:46 +0200

    VIS: Fix DataFrame.plot() produces incorrect legend markers (#27808)

--- pandas/tests/plotting/common.py
+++ pandas/tests/plotting/common.py
@@ -103,6 +103,28 @@ class TestPlotBase:
             else:
                 assert ax.get_legend() is None
 
+    def _check_legend_marker(self, ax, expected_markers=None, visible=True):
+        """
+        Check ax has expected legend markers
+
+        Parameters
+        ----------
+        ax : matplotlib Axes object
+        expected_markers : list-like
+            expected legend markers
+        visible : bool
+            expected legend visibility. labels are checked only when visible is
+            True
+        """
+        if visible and (expected_markers is None):
+            raise ValueError("Markers must be specified when visible is True")
+        if visible:
+            handles, _ = ax.get_legend_handles_labels()
+            markers = [handle.get_marker() for handle in handles]
+            assert markers == expected_markers
+        else:
+            assert ax.get_legend() is None
+
     def _check_data(self, xp, rs):
         """
         Check each axes has identical lines
