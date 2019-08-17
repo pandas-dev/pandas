3d21f6697d9bfa6d8295cc1763be18db12e75064
Umang <umang.kantesaria@walmart.com>
2019-08-17 15:59:35 -0500

    corrected code review comments

--- pandas/tests/groupby/test_transform.py
+++ pandas/tests/groupby/test_transform.py
@@ -1075,32 +1075,33 @@ def test_transform_lambda_with_datetimetz():
     )
     assert_series_equal(result, expected)
 
+
 @pytest.mark.parametrize(
-   "inputDF, expectedDF",
-   [
-       (
-           DataFrame(
-               {
-                   "A": [1, 1, 1, 1, '', 1],
-                   "B": [2, 3, '', 5, 6, 7],
-                   "C": [3, '', 4, '', 3, 2]
-               }
-           ),
-           1,
-       ),
-       (
-           DataFrame(
-               {
-                   "A": [1, 1, 1, 1, 1, 1],
-                   "B": [2, 3, 4, 5, 6, 7],
-                   "C": [3, 3, 4, 3, 3, 2]
-               }
-           ),
-           1,
-       ),
-   ],
+    "inputDF, expectedDF",
+    [
+        (
+            DataFrame(
+                {
+                    "A": [1, 1, 1, 1, '', 1],
+                    "B": [2, 3, '', 5, 6, 7],
+                    "C": [3, '', 4, '', 3, 2]
+                }
+            ),
+            1,
+        ),
+        (
+            DataFrame(
+                {
+                    "A": [1, 1, 1, 1, 1, 1],
+                    "B": [2, 3, 4, 5, 6, 7],
+                    "C": [3, 3, 4, 3, 3, 2]
+                }
+            ),
+            1,
+        ),
+    ],
 )
 def test_transform_fillna(inputDF, expectedDF):
     # GH 27905 - Test fillna
-   input1 = inputDF.transform(lambda x: (sum(x)/len(x)))
-   assert input1 == expectedDF
+    input1 = inputDF.transform(lambda x: (sum(x) / len(x)))
+    assert input1 == expectedDF
