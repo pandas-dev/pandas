6f38159b199abc4dde415315f3059d69a4ead15d
pradeepb2505 <pradeep.b2505@gmail.com>
2019-08-18 00:14:59 -0500

    Solved issue #27482

--- pandas/core/dtypes/missing.py
+++ pandas/core/dtypes/missing.py
@@ -128,31 +128,35 @@ isnull = isna
 
 
 def _isna_new(obj):
-    if is_scalar(obj):
-        return libmissing.checknull(obj)
-    # hack (for now) because MI registers as ndarray
-    elif isinstance(obj, ABCMultiIndex):
-        raise NotImplementedError("isna is not defined for MultiIndex")
-    elif isinstance(
-        obj,
-        (
-            ABCSeries,
-            np.ndarray,
-            ABCIndexClass,
-            ABCExtensionArray,
-            ABCDatetimeArray,
-            ABCTimedeltaArray,
-        ),
-    ):
-        return _isna_ndarraylike(obj)
-    elif isinstance(obj, ABCGeneric):
-        return obj._constructor(obj._data.isna(func=isna))
-    elif isinstance(obj, list):
-        return _isna_ndarraylike(np.asarray(obj, dtype=object))
-    elif hasattr(obj, "__array__"):
-        return _isna_ndarraylike(np.asarray(obj))
-    else:
-        return obj is None
+    try:
+        if obj._data is None:
+            return False
+    except:
+        if is_scalar(obj):
+            return libmissing.checknull(obj)
+        # hack (for now) because MI registers as ndarray
+        elif isinstance(obj, ABCMultiIndex):
+            raise NotImplementedError("isna is not defined for MultiIndex")
+        elif isinstance(
+            obj,
+            (
+                ABCSeries,
+                np.ndarray,
+                ABCIndexClass,
+                ABCExtensionArray,
+                ABCDatetimeArray,
+                ABCTimedeltaArray,
+            ),
+        ):
+            return _isna_ndarraylike(obj)
+        elif isinstance(obj, ABCGeneric):
+            return obj._constructor(obj._data.isna(func=isna))
+        elif isinstance(obj, list):
+            return _isna_ndarraylike(np.asarray(obj, dtype=object))
+        elif hasattr(obj, "__array__"):
+            return _isna_ndarraylike(np.asarray(obj))
+        else:
+            return obj is None
 
 
 def _isna_old(obj):
