f35653baed1e89f0e1a51d384e2ab89aad73bd9f
Kaiqi <kaiqi@kth.se>
2019-08-18 14:37:56 +0200

    Add test for GH45160

--- pandas/tests/plotting/test_frame.py
+++ pandas/tests/plotting/test_frame.py
@@ -3213,6 +3213,22 @@ class TestDataFramePlots(TestPlotBase):
         xticklabels = [t.get_text() for t in ax.get_xticklabels()]
         assert xticklabels == indexes
 
+    def test_subplots_sharex_false(self):
+        # test when sharex is set to False, two plots should have different
+        # labels, GH 25160
+        df = pd.DataFrame(np.random.rand(10, 2))
+        df.iloc[5:, 1] = np.nan
+        df.iloc[:5, 0] = np.nan
+
+        figs, axs = self.plt.subplots(2, 1)
+        df.plot.line(ax=axs, subplots=True, sharex=False)
+
+        expected_ax1 = np.arange(4.5, 10, 0.5)
+        expected_ax2 = np.arange(-0.5, 5, 0.5)
+
+        tm.assert_numpy_array_equal(axs[0].get_xticks(), expected_ax1)
+        tm.assert_numpy_array_equal(axs[1].get_xticks(), expected_ax2)
+
 
 def _generate_4_axes_via_gridspec():
     import matplotlib.pyplot as plt
