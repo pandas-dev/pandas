ce31b20b9a6551a1f26f3e78205da3ed87b2c9be
jbrockmendel <jbrockmendel@gmail.com>
2019-08-19 18:03:12 -0700

    BUG: rfloordiv with fill_value, #27464

--- pandas/tests/arithmetic/test_numeric.py
+++ pandas/tests/arithmetic/test_numeric.py
@@ -1227,3 +1227,17 @@ class TestNumericArithmeticUnsorted:
         tm.assert_index_equal(index + index, 2 * index)
         tm.assert_index_equal(index - index, 0 * index)
         assert not (index - index).empty
+
+
+def test_fill_value_inf_masking():
+    # GH #27464 make sure we mask 0/1 with Inf and not NaN
+    df = pd.DataFrame({"A": [0, 1, 2], "B": [1.1, None, 1.1]})
+
+    other = pd.DataFrame({"A": [1.1, 1.2, 1.3]}, index=[0, 2, 3])
+
+    result = df.rfloordiv(other, fill_value=1)
+
+    expected = pd.DataFrame(
+        {"A": [np.inf, 1.0, 0.0, 1.0], "B": [0.0, np.nan, 0.0, np.nan]}
+    )
+    tm.assert_frame_equal(result, expected)
