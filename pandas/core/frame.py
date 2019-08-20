10461dfe61c3f7a7c25fbd18fb4586c97b470083
jbrockmendel <jbrockmendel@gmail.com>
2019-08-20 10:45:05 -0700

    Backport PR #28024: BUG: rfloordiv with fill_value, closes#27464

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -111,6 +111,7 @@ from pandas.core.internals.construction import (
     sanitize_index,
     to_arrays,
 )
+from pandas.core.ops.missing import dispatch_fill_zeros
 from pandas.core.series import Series
 
 from pandas.io.formats import console, format as fmt
@@ -5365,7 +5366,9 @@ class DataFrame(NDFrame):
             # iterate over columns
             return ops.dispatch_to_series(this, other, _arith_op)
         else:
-            result = _arith_op(this.values, other.values)
+            with np.errstate(all="ignore"):
+                result = _arith_op(this.values, other.values)
+            result = dispatch_fill_zeros(func, this.values, other.values, result)
             return self._constructor(
                 result, index=new_index, columns=new_columns, copy=False
             )
