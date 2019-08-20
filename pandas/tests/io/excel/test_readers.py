e93b25aa605c8d748ad5bae4966d415bc69acb59
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-08-20 14:22:57 -0500

    review comments

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -486,6 +486,7 @@ class TestReaders:
         tm.assert_frame_equal(df2, df_ref, check_names=False)
 
     def test_nonexistent_sheetname_raises(self, read_ext):
+        # GH-27676
         # Specifying a non-existent sheet_name parameter should throw an error
         # with the sheet name.
         with pytest.raises(ValueError, match="sheet xyz not found"):
