eeb434c04f3506d2030ca26db0c6a642d60534ba
jbrockmendel <jbrockmendel@gmail.com>
2019-08-20 10:03:59 -0700

    add test for 26793

--- pandas/tests/arithmetic/test_numeric.py
+++ pandas/tests/arithmetic/test_numeric.py
@@ -1241,3 +1241,22 @@ def test_fill_value_inf_masking():
         {"A": [np.inf, 1.0, 0.0, 1.0], "B": [0.0, np.nan, 0.0, np.nan]}
     )
     tm.assert_frame_equal(result, expected)
+
+
+def test_dataframe_div_silenced():
+    # GH#26793
+    pdf1 = pd.DataFrame(
+        {
+            "A": np.arange(10),
+            "B": [np.nan, 1, 2, 3, 4] * 2,
+            "C": [np.nan] * 10,
+            "D": np.arange(10),
+        },
+        index=list("abcdefghij"),
+        columns=list("ABCD"),
+    )
+    pdf2 = pd.DataFrame(
+        np.random.randn(10, 4), index=list("abcdefghjk"), columns=list("ABCX")
+    )
+    with tm.assert_produces_warning(None):
+        pdf1.div(pdf2, fill_value=0)
