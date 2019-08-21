3d24808145168d37a4cc6d5e498933cacf5a92b4
jbrockmendel <jbrockmendel@gmail.com>
2019-08-20 17:59:51 -0700

    BUG: timedelta64(NaT) incorrectly treated as datetime in some dataframe ops

--- pandas/core/ops/__init__.py
+++ pandas/core/ops/__init__.py
@@ -498,8 +498,17 @@ def dispatch_to_series(left, right, func, str_rep=None, axis=None):
         # in which case we specifically want to operate row-by-row
         assert right.index.equals(left.columns)
 
-        def column_op(a, b):
-            return {i: func(a.iloc[:, i], b.iloc[i]) for i in range(len(a.columns))}
+        if right.dtype == "timedelta64[ns]":
+            # ensure we treat NaT values as the correct dtype
+            right = np.asarray(right)
+
+            def column_op(a, b):
+                return {i: func(a.iloc[:, i], b[i]) for i in range(len(a.columns))}
+
+        else:
+
+            def column_op(a, b):
+                return {i: func(a.iloc[:, i], b.iloc[i]) for i in range(len(a.columns))}
 
     elif isinstance(right, ABCSeries):
         assert right.index.equals(left.index)  # Handle other cases later
