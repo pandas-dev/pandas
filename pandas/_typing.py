8830f54c04080b112ac54f8d9b7dd97d5a105923
Vaibhav Vishal <vaibhav.hrt@gmail.com>
2019-08-26 16:26:25 +0530

    don't change anything in _typing

--- pandas/_typing.py
+++ pandas/_typing.py
@@ -23,6 +23,7 @@ ArrayLike = TypeVar("ArrayLike", "ExtensionArray", np.ndarray)
 DatetimeLikeScalar = TypeVar("DatetimeLikeScalar", "Period", "Timestamp", "Timedelta")
 Dtype = Union[str, np.dtype, "ExtensionDtype"]
 FilePathOrBuffer = Union[str, Path, IO[AnyStr]]
+
 FrameOrSeries = TypeVar("FrameOrSeries", "Series", "DataFrame")
 Scalar = Union[str, int, float]
 Axis = Union[str, int]
