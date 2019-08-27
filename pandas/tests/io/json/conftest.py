853f8cfaaa52c306bdab05d65f815f6cb02a5a84
Will Ayd <william.ayd@icloud.com>
2019-08-27 14:47:03 -0700

    Fixed conftest docstring

--- pandas/tests/io/json/conftest.py
+++ pandas/tests/io/json/conftest.py
@@ -4,6 +4,6 @@ import pytest
 @pytest.fixture(params=["split", "records", "index", "columns", "values"])
 def orient(request):
     """
-     Fixture for orients excluding the table format.
-     """
+    Fixture for orients excluding the table format.
+    """
     return request.param
