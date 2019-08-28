059808a0fd45d948f65f3b5e4ef43fd8c0447a8b
Sergei Ivko <s.ivko@karazin.ua>
2019-08-28 21:03:14 +0300

    Add missing files

--- pandas/tests/io/parser/test_common.py
+++ pandas/tests/io/parser/test_common.py
@@ -1875,8 +1875,7 @@ b,-Infinity
 c,+Infinity
 """
     expected = DataFrame(
-        {"A": [float("inf"), float("-inf"), float("inf")]},
-        index=["a", "b", "c"],
+        {"A": [float("inf"), float("-inf"), float("inf")]}, index=["a", "b", "c"]
     )
     result = parser.read_csv(StringIO(data), index_col=0, na_filter=na_filter)
     tm.assert_frame_equal(result, expected)
