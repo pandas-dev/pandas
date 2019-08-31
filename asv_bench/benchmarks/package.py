ed992bc908e4d098cdf6715fc2cb04792b64b239
jbrockmendel <jbrockmendel@gmail.com>
2019-08-30 18:10:08 -0700

    benchmark for import

---
+++ asv_bench/benchmarks/package.py
@@ -0,0 +1,23 @@
+"""
+Benchmarks for pandas at the package-level.
+"""
+import subprocess
+import sys
+
+from pandas.compat import PY37
+
+class TimeImport:
+    def time_import(self):
+        if PY37:
+            cmd = [sys.executable, "-X", "importtime", "-c", 'import pandas as pd']
+            p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+            _, stderr = p.communicate()
+
+            line = stderr.splitlines()[-1]
+            field = line.split(b'|')[-2].strip()
+            total = int(field)  # microseconds
+            return total
+
+        cmd = [sys.executable, "-c", 'import pandas as pd']
+        p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        p.communicate()
