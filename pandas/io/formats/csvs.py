505ca5c536e38622dc55f086bd968bb4a49d9d50
Simon Hawkins <simonjayhawkins@gmail.com>
2019-09-01 18:53:00 +0100

    check_untyped_defs io.formats.csvs

--- pandas/io/formats/csvs.py
+++ pandas/io/formats/csvs.py
@@ -5,6 +5,7 @@ Module for formatting output data into CSV files.
 import csv as csvlib
 from io import StringIO
 import os
+from typing import List
 import warnings
 from zipfile import ZipFile
 
@@ -234,7 +235,7 @@ class CSVFormatter:
         cols = self.cols
         has_mi_columns = self.has_mi_columns
         header = self.header
-        encoded_labels = []
+        encoded_labels = []  # type: List[str]
 
         has_aliases = isinstance(header, (tuple, list, np.ndarray, ABCIndexClass))
         if not (has_aliases or self.header):
