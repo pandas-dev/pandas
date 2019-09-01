a1f653fb77245788c4a0f796576d64bdb16fc124
Simon Hawkins <simonjayhawkins@gmail.com>
2019-09-01 18:45:46 +0100

    check_untyped_defs io.formats.console

--- pandas/io/formats/console.py
+++ pandas/io/formats/console.py
@@ -66,7 +66,8 @@ def in_interactive_session():
         return not hasattr(main, "__file__") or get_option("mode.sim_interactive")
 
     try:
-        return __IPYTHON__ or check_main()  # noqa
+        # error: Name '__IPYTHON__' is not defined
+        return __IPYTHON__ or check_main()  # type:ignore # noqa
     except NameError:
         return check_main()
 
@@ -76,7 +77,8 @@ def in_ipython_frontend():
     check if we're inside an an IPython zmq frontend
     """
     try:
-        ip = get_ipython()  # noqa
+        # error: Name 'get_ipython' is not defined
+        ip = get_ipython()  # type: ignore # noqa
         return "zmq" in str(type(ip)).lower()
     except NameError:
         pass
