720eeb0ef55ef0ee85a196757921d9fdc4980732
jbrockmendel <jbrockmendel@gmail.com>
2019-09-03 12:05:32 -0700

    black

--- pandas/_libs/tslibs/__init__.py
+++ pandas/_libs/tslibs/__init__.py
@@ -3,7 +3,7 @@
 from .conversion import localize_pydatetime, normalize_date
 from .nattype import NaT, NaTType, iNaT, is_null_datetimelike
 from .np_datetime import OutOfBoundsDatetime
-from .c_timestamp import NullFrequencyError   # Note: import fails if earlier
+from .c_timestamp import NullFrequencyError  # Note: import fails if earlier
 from .period import IncompatibleFrequency, Period
 from .timedeltas import Timedelta, delta_to_nanoseconds, ints_to_pytimedelta
 from .timestamps import Timestamp
