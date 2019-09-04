a5d9eb27cfbb785a0eee0d4c1ad4be36870320e8
jbrockmendel <jbrockmendel@gmail.com>
2019-09-04 09:58:29 -0700

    comment

--- asv_bench/benchmarks/package.py
+++ asv_bench/benchmarks/package.py
@@ -10,6 +10,9 @@ from pandas.compat import PY37
 class TimeImport:
     def time_import(self):
         if PY37:
+            # on py37+ we the "-X importtime" usage gives us a more precise
+            #  measurement of the import time we actually care about,
+            #  without the subprocess or interpreter overhead
             cmd = [sys.executable, "-X", "importtime", "-c", "import pandas as pd"]
             p = subprocess.run(cmd, stderr=subprocess.PIPE)
 
@@ -19,4 +22,4 @@ class TimeImport:
             return total
 
         cmd = [sys.executable, "-c", "import pandas as pd"]
-        p = subprocess.run(cmd, stderr=subprocess.PIPE)
+        subprocess.run(cmd, stderr=subprocess.PIPE)
