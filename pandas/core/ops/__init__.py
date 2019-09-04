ad806e99badf4636119060fd61cef890aedd9001
jbrockmendel <jbrockmendel@gmail.com>
2019-09-03 19:11:09 -0700

    remove unnecessary axis kwarg

--- pandas/core/ops/__init__.py
+++ pandas/core/ops/__init__.py
@@ -698,10 +698,7 @@ def _comp_method_SERIES(cls, op, special):
 
         return result
 
-    def wrapper(self, other, axis=None):
-        # Validate the axis parameter
-        if axis is not None:
-            self._get_axis_number(axis)
+    def wrapper(self, other):
 
         res_name = get_op_result_name(self, other)
         other = lib.item_from_zerodim(other)
