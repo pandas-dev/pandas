03529301810a38fe9ad74522c1aea9f8db98ee54
Mabel Villalba <mabelvj@gmail.com>
2019-09-05 17:33:34 +0200

    Fixes #24893: added comment in pivot

--- pandas/core/reshape/pivot.py
+++ pandas/core/reshape/pivot.py
@@ -252,6 +252,7 @@ def _add_margins(
 
     row_names = result.index.names
     try:
+        # check the result column and leave floats
         for dtype in set(result.dtypes):
             cols = result.select_dtypes([dtype]).columns
             margin_dummy[cols] = margin_dummy[cols].apply(
