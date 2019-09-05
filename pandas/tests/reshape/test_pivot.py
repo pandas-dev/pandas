45ffc77b5af813b973e97a4e3f0a5186eae0db15
Mabel Villalba <mabelvj@gmail.com>
2019-09-05 22:02:55 +0200

    Fixes #24893: re-added test

--- pandas/tests/reshape/test_pivot.py
+++ pandas/tests/reshape/test_pivot.py
@@ -1630,6 +1630,25 @@ class TestPivotTable:
         table = df.pivot_table("x", "y", "z", dropna=observed, margins=True)
         tm.assert_frame_equal(table, expected)
 
+    def test_margins_casted_to_float(self, observed):
+        # GH #24893
+        df = pd.DataFrame(
+            {
+                "A": [2, 4, 6, 8],
+                "B": [1, 4, 5, 8],
+                "C": [1, 3, 4, 6],
+                "D": ["X", "X", "Y", "Y"],
+            }
+        )
+
+        result = pd.pivot_table(df, index="D", margins=True)
+        expected = pd.DataFrame(
+            {"A": [3, 7, 5], "B": [2.5, 6.5, 4.5], "C": [2, 5, 3.5]},
+            index=pd.Index(["X", "Y", "All"], name="D"),
+        )
+        table = result
+        tm.assert_frame_equal(result, expected)
+
     def test_categorical_aggfunc(self, observed):
         # GH 9534
         df = pd.DataFrame(
