01cb456bc11e982453e2718de83526ea256532d7
Daniel Saxton <>
2019-09-05 19:02:55 -0500

    Raise for Kendall benchmark

--- asv_bench/benchmarks/stat_ops.py
+++ asv_bench/benchmarks/stat_ops.py
@@ -122,9 +122,15 @@ class Correlation:
         self.df.corr(method=method)
 
     def time_corr_wide(self, method, use_bottleneck):
+        # Raise here until benchmark can pass
+        if method == "kendall":
+            raise NotImplementedError
         self.df_wide.corr(method=method)
 
     def time_corr_wide_nans(self, method, use_bottleneck):
+        # Raise here until benchmark can pass
+        if method == "kendall":
+            raise NotImplementedError
         self.df_wide_nans.corr(method=method)
 
     def peakmem_corr_wide(self, method, use_bottleneck):
