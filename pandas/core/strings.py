0b801a91f0dc58f94323836ef7bf057799da2e4d
boomsquared <paichana5@gmail.com>
2019-09-06 22:59:06 +0700

    FIX: add additional condition to check invalid type(#28277)

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -2107,7 +2107,7 @@ class StringMethods(NoNewAttributesMixin):
         elif isinstance(others, np.ndarray) and others.ndim == 2:
             others = DataFrame(others, index=idx)
             return [others[x] for x in others]
-        elif is_list_like(others, allow_sets=False):
+        elif is_list_like(others, allow_sets=False) and not isinstance(others,type(self)):
             others = list(others)  # ensure iterators do not get read twice etc
 
             # in case of list-like `others`, all elements must be
