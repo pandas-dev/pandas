8bb2c3d61f278573b0885c4b41309745f517b40c
Simon Hawkins <simonjayhawkins@gmail.com>
2019-09-07 05:23:51 +0100

    check_untyped_defs pandas.util._exceptions

--- pandas/util/_exceptions.py
+++ pandas/util/_exceptions.py
@@ -1,15 +1,16 @@
 import contextlib
+from typing import Tuple
 
 
 @contextlib.contextmanager
-def rewrite_exception(old_name, new_name):
+def rewrite_exception(old_name: str, new_name: str):
     """Rewrite the message of an exception."""
     try:
         yield
     except Exception as e:
         msg = e.args[0]
         msg = msg.replace(old_name, new_name)
-        args = (msg,)
+        args: Tuple[str, ...] = (msg,)
         if len(e.args) > 1:
             args = args + e.args[1:]
         e.args = args
