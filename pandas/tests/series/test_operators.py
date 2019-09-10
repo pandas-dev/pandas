48edcdce9f26b3af631df9da9183beccffa3ef02
jbrockmendel <jbrockmendel@gmail.com>
2019-09-10 04:42:06 -0700

    BUG: same behavior for Series/Index vs ndarray/list in Series logical ops (#28364)
    * treat listlikes symmetrically in logical ops
    
    * add test

--- pandas/tests/series/test_operators.py
+++ pandas/tests/series/test_operators.py
@@ -159,6 +159,41 @@ class TestSeriesLogicalOps:
         expected = s_tft
         assert_series_equal(res, expected)
 
+    def test_logical_ops_bool_dtype_with_ndarray(self):
+        # make sure we operate on ndarray the same as Series
+        left = pd.Series([True, True, True, False, True])
+        right = [True, False, None, True, np.nan]
+
+        expected = pd.Series([True, False, False, False, False])
+        result = left & right
+        tm.assert_series_equal(result, expected)
+        result = left & np.array(right)
+        tm.assert_series_equal(result, expected)
+        result = left & pd.Index(right)
+        tm.assert_series_equal(result, expected)
+        result = left & pd.Series(right)
+        tm.assert_series_equal(result, expected)
+
+        expected = pd.Series([True, True, True, True, True])
+        result = left | right
+        tm.assert_series_equal(result, expected)
+        result = left | np.array(right)
+        tm.assert_series_equal(result, expected)
+        result = left | pd.Index(right)
+        tm.assert_series_equal(result, expected)
+        result = left | pd.Series(right)
+        tm.assert_series_equal(result, expected)
+
+        expected = pd.Series([False, True, True, True, True])
+        result = left ^ right
+        tm.assert_series_equal(result, expected)
+        result = left ^ np.array(right)
+        tm.assert_series_equal(result, expected)
+        result = left ^ pd.Index(right)
+        tm.assert_series_equal(result, expected)
+        result = left ^ pd.Series(right)
+        tm.assert_series_equal(result, expected)
+
     def test_logical_operators_int_dtype_with_bool_dtype_and_reindex(self):
         # GH#9016: support bitwise op for integer types
 
