d66633494638536c39156fc4460d32afc7a5f976
Kaiqi <kaiqi@kth.se>
2019-09-11 11:42:43 +0200

    fix import failure

--- pandas/plotting/_core.py
+++ pandas/plotting/_core.py
@@ -9,7 +9,7 @@ from pandas.util._decorators import Appender
 from pandas.core.dtypes.common import is_integer, is_list_like
 from pandas.core.dtypes.generic import ABCDataFrame, ABCSeries
 
-from pandas import DataFrame
+from pandas.core.frame import DataFrame
 from pandas.core.base import PandasObject
 
 # Trigger matplotlib import, which implicitly registers our
