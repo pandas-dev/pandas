d4e1c3c76458a6448298844bfbdf9805163b31a0
Chris Zimmerman <chris-zimmerman@live.com>
2019-09-12 20:05:31 -0500

    BUG: Fix Series.append raises TypeError with tuple of Series (#28412)

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -2730,7 +2730,8 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
         from pandas.core.reshape.concat import concat
 
         if isinstance(to_append, (list, tuple)):
-            to_concat = [self] + to_append
+            to_concat = [self]
+            to_concat.extend(to_append)
         else:
             to_concat = [self, to_append]
         return concat(
