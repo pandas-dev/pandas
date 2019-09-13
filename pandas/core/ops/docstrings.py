2e78276088a083feaadcfd753cd668345f2d8c67
Simon Hawkins <simonjayhawkins@gmail.com>
2019-09-13 23:48:22 +0100

    ci fixups

--- pandas/core/ops/docstrings.py
+++ pandas/core/ops/docstrings.py
@@ -1,7 +1,7 @@
 """
 Templating for ops docstrings
 """
-from typing import Dict, Optional, TYPE_CHECKING
+from typing import TYPE_CHECKING, Dict, Optional
 
 if TYPE_CHECKING:
     from mypy_extensions import TypedDict
