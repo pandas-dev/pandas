255518e17594895f48f937f305be38a037f607c8
Christian Chwala <christian.chwala@kit.edu>
2019-09-17 21:25:59 +0200

    fix typo

--- pandas/tests/series/test_missing.py
+++ pandas/tests/series/test_missing.py
@@ -1495,23 +1495,23 @@ class TestSeriesInterpolateData:
     def test_interp_max_gap(self):
         s = Series([nan, 1.0, nan, 2.0, nan, nan, 5.0, nan, nan, nan, -1.0, nan, nan])
 
-        excpected = Series(
+        expected = Series(
             [1.0, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0, nan, nan, nan, -1.0, -1, -1]
         )
         result = s.interpolate(method="linear", max_gap=2)
-        assert_series_equal(result, excpected)
+        assert_series_equal(result, expected)
 
-        excpected = Series(
+        expected = Series(
             [nan, 1.0, 1.5, 2.0, 3.0, 4.0, 5.0, nan, nan, nan, -1.0, nan, nan]
         )
         result = s.interpolate(method="linear", max_gap=2, limit_area="inside")
-        assert_series_equal(result, excpected)
+        assert_series_equal(result, expected)
 
-        excpected = Series(
+        expected = Series(
             [nan, 1.0, 1, 2.0, 2.0, 2.0, 5.0, nan, nan, nan, -1.0, nan, nan]
         )
         result = s.interpolate(method="pad", max_gap=2, limit_area="inside")
-        assert_series_equal(result, excpected)
+        assert_series_equal(result, expected)
 
     def test_interp_max_gap_nat(self):
         series = Series([0, 1, 2, iNaT], dtype="M8[ns]")
