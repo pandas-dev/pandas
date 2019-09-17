79fde270c2334052843705c69bc9dbb71d0c156a
Marc Garcia <marc.garcia2@tesco.com>
2019-09-17 13:51:14 +0100

    Black

--- ci/print_skipped.py
+++ ci/print_skipped.py
@@ -17,19 +17,24 @@ def main(filename):
             current_class = cn
             if old_class != current_class:
                 yield None
-            yield {'class_name': current_class,
-                   'test_name': el.attrib["name"],
-                   'message': sk.attrib["message"]}
+            yield {
+                "class_name": current_class,
+                "test_name": el.attrib["name"],
+                "message": sk.attrib["message"],
+            }
 
 
 if __name__ == "__main__":
     print("SKIPPED TESTS:")
     i = 1
-    for file_type in ('-single', '-multi', ''):
+    for file_type in ("-single", "-multi", ""):
         for test_data in main("test-data{}.xml".format(file_type)):
             if test_data is None:
-                print('-' * 80)
+                print("-" * 80)
             else:
-                print('#{i} {class_name}.{test_name}: {message}'.format(
-                    **dict(test_data, i=i)))
+                print(
+                    "#{i} {class_name}.{test_name}: {message}".format(
+                        **dict(test_data, i=i)
+                    )
+                )
                 i += 1
