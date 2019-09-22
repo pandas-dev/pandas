db741d36d1c75c309c867ed46a2512dbc9a66c75
Matt Roeschke <mroeschke@housecanary.com>
2019-09-21 20:17:13 -0700

    BENCH: Add rolling apply benchmarks

--- asv_bench/benchmarks/rolling.py
+++ asv_bench/benchmarks/rolling.py
@@ -25,6 +25,25 @@ class Methods:
         getattr(self.roll, method)()
 
 
+class Apply:
+    params = (
+        ["DataFrame", "Series"],
+        [10, 1000],
+        ["int", "float"],
+        [sum, np.sum, lambda x: np.sum(x) + 5],
+        [True, False]
+    )
+    param_names = ["contructor", "window", "dtype", "function", "raw"]
+
+    def setup(self, constructor, window, dtype, function, raw):
+        N = 10 ** 5
+        arr = (100 * np.random.random(N)).astype(dtype)
+        self.roll = getattr(pd, constructor)(arr).rolling(window)
+
+    def time_rolling(self, constructor, window, dtype, function, raw):
+        self.roll.apply(function, raw=raw)
+
+
 class ExpandingMethods:
 
     params = (
