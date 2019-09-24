082d67479039faf7a60005415b6079ccd0a3b0eb
Richard Stanton <stanton@haas.berkeley.edu>
2019-09-24 10:26:48 -0700

    Add extra blank line

--- pandas/tests/tseries/holiday/test_calendar.py
+++ pandas/tests/tseries/holiday/test_calendar.py
@@ -83,6 +83,7 @@ def test_rule_from_name():
     us_fed_cal = get_calendar("USFederalHolidayCalendar")
     assert us_fed_cal.rule_from_name("Thanksgiving") == USThanksgivingDay
 
+    
 def test_calendar_2031():
     # See gh-27790
     #
