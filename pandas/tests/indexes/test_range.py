793c93c060c3dde8494c554643c4ac00c6ac7f7a
Kaiqi <kaiqi@kth.se>
2019-09-30 09:18:20 +0200

    better pandas

--- pandas/tests/indexes/test_range.py
+++ pandas/tests/indexes/test_range.py
@@ -416,6 +416,14 @@ class TestRangeIndex(Numeric):
         expected = np.array([0, 1, 1, 2, 2, 3, 3, 4, 4, 5], dtype=np.intp)
         tm.assert_numpy_array_equal(indexer, expected)
 
+    def test_get_indexer_limit(self):
+        # GH 28631
+        idx = RangeIndex(4)
+        target = RangeIndex(6)
+        result = idx.get_indexer(target, method="pad", limit=1)
+        expected = np.array([0, 1, 2, 3, 3, -1], dtype=np.intp)
+        tm.assert_numpy_array_equal(result, expected)
+
     def test_join_outer(self):
         # join with Int64Index
         other = Int64Index(np.arange(25, 14, -1))
@@ -1053,11 +1061,3 @@ class TestRangeIndex(Numeric):
         result = df.reindex([0, 1, 2, 3, 4, 5], method="ffill", limit=1)
         expected = pd.DataFrame(exp_data)
         tm.assert_frame_equal(result, expected)
-
-    def test_get_indexer_limit(self):
-        # GH 28631
-        idx = RangeIndex(0, 4)
-        target = RangeIndex(0, 6)
-        result = idx.get_indexer(target, method="pad", limit=1)
-        expected = np.array([0, 1, 2, 3, 3, -1])
-        tm.assert_numpy_array_equal(result, expected, check_dtype=False)
