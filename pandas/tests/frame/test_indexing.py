d34b48bb74bd074f184915d408f87c1361d5790f
Kaiqi Dong <kaiqi@kth.se>
2019-10-01 14:00:37 +0200

    Backport PR #28671: BUG: restore limit in RangeIndex.get_indexer

--- pandas/tests/frame/test_indexing.py
+++ pandas/tests/frame/test_indexing.py
@@ -2242,6 +2242,22 @@ class TestDataFrameIndexing(TestData):
         assert mask[-5:].all()
         assert not mask[:-5].any()
 
+    def test_reindex_limit(self):
+        # GH 28631
+        data = [["A", "A", "A"], ["B", "B", "B"], ["C", "C", "C"], ["D", "D", "D"]]
+        exp_data = [
+            ["A", "A", "A"],
+            ["B", "B", "B"],
+            ["C", "C", "C"],
+            ["D", "D", "D"],
+            ["D", "D", "D"],
+            [np.nan, np.nan, np.nan],
+        ]
+        df = DataFrame(data)
+        result = df.reindex([0, 1, 2, 3, 4, 5], method="ffill", limit=1)
+        expected = DataFrame(exp_data)
+        tm.assert_frame_equal(result, expected)
+
     def test_set_dataframe_column_ns_dtype(self):
         x = DataFrame([datetime.now(), datetime.now()])
         assert x[0].dtype == np.dtype("M8[ns]")
