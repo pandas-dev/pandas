5acd7866021aacf791ed8ecf8d71fddd02095359
jbrockmendel <jbrockmendel@gmail.com>
2019-10-01 09:51:32 -0700

    CLN: Exception in io/plotting (#28720)

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -539,13 +539,16 @@ class PandasSQLTest:
         with self.pandasSQL.run_transaction() as trans:
             trans.execute("CREATE TABLE test_trans (A INT, B TEXT)")
 
+        class DummyException(Exception):
+            pass
+
         # Make sure when transaction is rolled back, no rows get inserted
         ins_sql = "INSERT INTO test_trans (A,B) VALUES (1, 'blah')"
         try:
             with self.pandasSQL.run_transaction() as trans:
                 trans.execute(ins_sql)
-                raise Exception("error")
-        except Exception:
+                raise DummyException("error")
+        except DummyException:
             # ignore raised exception
             pass
         res = self.pandasSQL.read_query("SELECT * FROM test_trans")
