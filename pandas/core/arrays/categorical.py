34db4a36587e8180a9e46f923b626d7130deb7e5
Tola Alade <adetola.alade@gmail.com>
2019-10-03 17:22:45 +0100

    updating categorical.from codes args with type string to str and boolean to bool

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -634,7 +634,7 @@ class Categorical(ExtensionArray, PandasObject):
 
         Parameters
         ----------
-        codes : array-like, integers
+        codes : array-like, int
             An integer array, where each integer points to a category in
             categories or dtype.categories, or else is -1 for NaN.
         categories : index-like, optional
@@ -645,7 +645,7 @@ class Categorical(ExtensionArray, PandasObject):
             Whether or not this categorical is treated as an ordered
             categorical. If not given here or in `dtype`, the resulting
             categorical will be unordered.
-        dtype : CategoricalDtype or the string "category", optional
+        dtype : CategoricalDtype or the str "category", optional
             If :class:`CategoricalDtype`, cannot be used together with
             `categories` or `ordered`.
 
