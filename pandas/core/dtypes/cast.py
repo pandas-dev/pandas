35c4dcec089512849a0db49c99a5310121cfd4b5
jbrockmendel <jbrockmendel@gmail.com>
2019-10-06 14:38:22 -0700

    TST: Fix maybe_promote xfails (#28754)

--- pandas/core/dtypes/cast.py
+++ pandas/core/dtypes/cast.py
@@ -348,12 +348,22 @@ def maybe_promote(dtype, fill_value=np.nan):
                 dtype = np.dtype(np.object_)
             fill_value = np.nan
 
+        if dtype == np.object_ or dtype.kind in ["U", "S"]:
+            # We treat string-like dtypes as object, and _always_ fill
+            #  with np.nan
+            fill_value = np.nan
+            dtype = np.dtype(np.object_)
+
     # returns tuple of (dtype, fill_value)
     if issubclass(dtype.type, np.datetime64):
-        try:
-            fill_value = tslibs.Timestamp(fill_value).to_datetime64()
-        except (TypeError, ValueError):
+        if isinstance(fill_value, datetime) and fill_value.tzinfo is not None:
+            # Trying to insert tzaware into tznaive, have to cast to object
             dtype = np.dtype(np.object_)
+        else:
+            try:
+                fill_value = tslibs.Timestamp(fill_value).to_datetime64()
+            except (TypeError, ValueError):
+                dtype = np.dtype(np.object_)
     elif issubclass(dtype.type, np.timedelta64):
         try:
             fv = tslibs.Timedelta(fill_value)
@@ -416,8 +426,6 @@ def maybe_promote(dtype, fill_value=np.nan):
 
     # in case we have a string that looked like a number
     if is_extension_array_dtype(dtype):
-        pass
-    elif is_datetime64tz_dtype(dtype):
         pass
     elif issubclass(np.dtype(dtype).type, (bytes, str)):
         dtype = np.object_
