39602e7d7c5b663696f5a6eca9e66e65483bc868
bravech <cgp246@gmail.com>
2019-10-08 08:53:56 -0400

    TST: Dataframe KeyError when getting nonexistent category (#28799) (#28809)

--- pandas/tests/groupby/test_categorical.py
+++ pandas/tests/groupby/test_categorical.py
@@ -1210,3 +1210,14 @@ def test_groupby_cat_preserves_structure(observed, ordered_fixture):
     )
 
     assert_frame_equal(result, expected)
+
+
+def test_get_nonexistent_category():
+    # Accessing a Category that is not in the dataframe
+    df = pd.DataFrame({"var": ["a", "a", "b", "b"], "val": range(4)})
+    with pytest.raises(KeyError, match="'vau'"):
+        df.groupby("var").apply(
+            lambda rows: pd.DataFrame(
+                {"var": [rows.iloc[-1]["var"]], "val": [rows.iloc[-1]["vau"]]}
+            )
+        )
