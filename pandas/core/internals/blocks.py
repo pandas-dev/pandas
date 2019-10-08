890fe820920c28f08d808dc70fd5fd570c5911e6
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-10-08 16:02:05 +0200

    fallback to conversion to numpy array (eg for sparse array)

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -1784,15 +1784,24 @@ class ExtensionBlock(NonConsolidatableMixIn, Block):
 
     def to_native_types(self, slicer=None, na_rep="nan", quoting=None, **kwargs):
         """override to use ExtensionArray astype for the conversion"""
-        values = self.values
-        if slicer is not None:
-            values = values[slicer]
-        mask = isna(values)
-        values = values.astype(str)
-        values[mask] = na_rep
-
-        # we are expected to return a 2-d ndarray
-        return values.reshape(1, len(values))
+        try:
+            values = self.values
+            if slicer is not None:
+                values = values[slicer]
+            mask = isna(values)
+            values = values.astype(str)
+            values[mask] = na_rep
+            # we are expected to return a 2-d ndarray
+            return values.reshape(1, len(values))
+        except Exception:
+            # eg SparseArray does not support setitem, needs to be converted to ndarray
+            values = self.get_values()
+            if slicer is not None:
+                values = values[:, slicer]
+            mask = isna(values)
+            values = values.astype(str)
+            values[mask] = na_rep
+            return values
 
     def take_nd(self, indexer, axis=0, new_mgr_locs=None, fill_tuple=None):
         """
