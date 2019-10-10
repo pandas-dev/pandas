pandas-dev/pandas

commit 07415f80a0a4ea0b145a0e589be452137303470b

CLN: dont catch on groupby.mean (#28878)

pandas/core/groupby/generic.py
@@ -971,6 +971,18 @@ class DataFrameGroupBy(GroupBy):
                 if result is not no_result:
                     # see if we can cast the block back to the original dtype
                     result = maybe_downcast_numeric(result, block.dtype)
+
+                    if result.ndim == 1 and isinstance(result, np.ndarray):
+                        # e.g. block.values was an IntegerArray
+                        try:
+                            # Cast back if feasible
+                            result = type(block.values)._from_sequence(
+                                result, dtype=block.values.dtype
+                            )
+                        except ValueError:
+                            # reshape to be valid for non-Extension Block
+                            result = result.reshape(1, -1)
+
                     newb = block.make_block(result)
 
             new_items.append(locs)

pandas/core/groupby/groupby.py
@@ -1217,16 +1217,9 @@ class GroupBy(_GroupBy):
         Name: B, dtype: float64
         """
         nv.validate_groupby_func("mean", args, kwargs, ["numeric_only"])
-        try:
-            return self._cython_agg_general(
-                "mean", alt=lambda x, axis: Series(x).mean(**kwargs), **kwargs
-            )
-        except GroupByError:
-            raise
-        except Exception:
-            with _group_selection_context(self):
-                f = lambda x: x.mean(axis=self.axis, **kwargs)
-                return self._python_agg_general(f)
+        return self._cython_agg_general(
+            "mean", alt=lambda x, axis: Series(x).mean(**kwargs), **kwargs
+        )
 
     @Substitution(name="groupby")
     @Appender(_common_see_also)

commit bb457d8f762df319685d9e8c9926dab529700e05

silence error introduced in #28878

pandas/core/groupby/generic.py
@@ -982,7 +982,10 @@ class DataFrameGroupBy(GroupBy):
                     # see if we can cast the block back to the original dtype
                     result = maybe_downcast_numeric(result, block.dtype)
 
-                    if result.ndim == 1 and isinstance(result, np.ndarray):
+                    # error: "object" has no attribute "ndim"  [attr-defined]
+                    if result.ndim == 1 and isinstance(  # type: ignore[attr-defined]
+                        result, np.ndarray
+                    ):
                         # e.g. block.values was an IntegerArray
                         try:
                             # Cast back if feasible