26c8f9e6581e12d62f9e389f2196bf849ab134ff
Jeroen Kant <45035434+jjlkant@users.noreply.github.com>
2019-10-11 14:09:33 +0200

    BUG: Allow all int types for merge (GH28870) (#28875)

--- pandas/tests/reshape/merge/test_merge_asof.py
+++ pandas/tests/reshape/merge/test_merge_asof.py
@@ -1287,3 +1287,19 @@ class TestAsOfMerge:
         )
 
         assert_frame_equal(result, expected)
+
+    def test_int_type_tolerance(self, any_int_dtype):
+        # GH #28870
+
+        left = pd.DataFrame({"a": [0, 10, 20], "left_val": [1, 2, 3]})
+        right = pd.DataFrame({"a": [5, 15, 25], "right_val": [1, 2, 3]})
+        left["a"] = left["a"].astype(any_int_dtype)
+        right["a"] = right["a"].astype(any_int_dtype)
+
+        expected = pd.DataFrame(
+            {"a": [0, 10, 20], "left_val": [1, 2, 3], "right_val": [np.nan, 1.0, 2.0]}
+        )
+        expected["a"] = expected["a"].astype(any_int_dtype)
+
+        result = pd.merge_asof(left, right, on="a", tolerance=10)
+        assert_frame_equal(result, expected)
