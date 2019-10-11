8b93a95f93ad57859e90cf8fc3e16af08eeeeb29
yogendra <yogendra.kumar@dolcera.com>
2019-10-12 03:02:12 +0530

    fix #28926 pandas\api\test_api.py mypy errors

--- pandas/tests/api/test_api.py
+++ pandas/tests/api/test_api.py
@@ -1,6 +1,7 @@
 import pandas as pd
 from pandas import api, compat
 from pandas.util import testing as tm
+from typing import List
 
 
 class Base:
@@ -41,7 +42,8 @@ class TestPDApi(Base):
     ]
 
     # these are already deprecated; awaiting removal
-    deprecated_modules = []
+    # GH #28926
+    deprecated_modules: List[str] = []
 
     # misc
     misc = ["IndexSlice", "NaT"]
@@ -92,10 +94,12 @@ class TestPDApi(Base):
         classes.extend(["Panel", "SparseSeries", "SparseDataFrame"])
 
     # these are already deprecated; awaiting removal
-    deprecated_classes = []
+    # GH #28926
+    deprecated_classes: List[str] = []
 
     # these should be deprecated in the future
-    deprecated_classes_in_future = []
+    # GH #28926
+    deprecated_classes_in_future: List[str] = []
 
     # external modules exposed in pandas namespace
     modules = ["np", "datetime"]
@@ -171,10 +175,12 @@ class TestPDApi(Base):
     funcs_to = ["to_datetime", "to_msgpack", "to_numeric", "to_pickle", "to_timedelta"]
 
     # top-level to deprecate in the future
-    deprecated_funcs_in_future = []
+    # GH #28926
+    deprecated_funcs_in_future: List[str] = []
 
     # these are already deprecated; awaiting removal
-    deprecated_funcs = []
+    # GH #28926
+    deprecated_funcs: List[str] = []
 
     # private modules in pandas namespace
     private_modules = [
