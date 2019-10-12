4cf387e1fcbab3b16724231bae7ae2d730911865
Tola Alade <adetola.alade@gmail.com>
2019-10-12 22:00:21 +0100

    docstring fix for nunique method

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -1644,7 +1644,7 @@ class DataFrameGroupBy(GroupBy):
 
         Parameters
         ----------
-        dropna : boolean, default True
+        dropna : bool, default True
             Don't include NaN in the counts.
 
         Returns
