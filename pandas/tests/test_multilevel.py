0b934a28c9c29b48651f6aabef3f486e4b83f4e2
Rohit Sanjay <sanjay.rohit2@gmail.com>
2019-10-13 22:14:42 +0530

    TST: add test_series_any_timedelta for GH17667 (#28942)

--- pandas/tests/test_multilevel.py
+++ pandas/tests/test_multilevel.py
@@ -1065,6 +1065,23 @@ Thur,Lunch,Yes,51.51,17"""
         ex = DataFrame({"data": [False, False]}, index=["one", "two"])
         tm.assert_frame_equal(result, ex)
 
+    def test_series_any_timedelta(self):
+        # GH 17667
+        df = DataFrame(
+            {
+                "a": Series([0, 0]),
+                "t": Series([pd.to_timedelta(0, "s"), pd.to_timedelta(1, "ms")]),
+            }
+        )
+
+        result = df.any(axis=0)
+        expected = Series(data=[False, True], index=["a", "t"])
+        tm.assert_series_equal(result, expected)
+
+        result = df.any(axis=1)
+        expected = Series(data=[False, True])
+        tm.assert_series_equal(result, expected)
+
     def test_std_var_pass_ddof(self):
         index = MultiIndex.from_arrays(
             [np.arange(5).repeat(10), np.tile(np.arange(10), 5)]
