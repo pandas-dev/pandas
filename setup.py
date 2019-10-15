aebba0d1c2af43da056e24bad6a43f6c999a0a91
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2019-10-15 06:50:32 -0700

    Backport PR #28982 on branch 0.25.x (Document 3.8 compatibility) (#28999)
    * Backport PR #28982: Document 3.8 compatibility
    
    * CI failing Linux py36_32bit (#28830)
    
    (cherry picked from commit 558f754e1787625d8b18cd4960fa059d3533faae)

--- setup.py
+++ setup.py
@@ -230,6 +230,7 @@ CLASSIFIERS = [
     "Programming Language :: Python :: 3.5",
     "Programming Language :: Python :: 3.6",
     "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
     "Programming Language :: Cython",
     "Topic :: Scientific/Engineering",
 ]
