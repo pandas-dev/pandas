524ce17f1187d97d3bb550319a305386ecd0a338
Lukas Bentkamp <lukas.bentkamp@mailbox.org>
2019-10-15 11:23:33 +0200

    reformatting

--- pandas/tests/extension/base/base.py
+++ pandas/tests/extension/base/base.py
@@ -1,9 +1,11 @@
+from typing import Any
+
 import pandas.util.testing as tm
 
 
 class BaseExtensionTests:
 
     assert_equal = staticmethod(tm.assert_equal)
-    assert_series_equal = staticmethod(tm.assert_series_equal)
-    assert_frame_equal = staticmethod(tm.assert_frame_equal)
+    assert_series_equal = staticmethod(tm.assert_series_equal)  # type: Any
+    assert_frame_equal = staticmethod(tm.assert_frame_equal)  # type: Any
     assert_extension_array_equal = staticmethod(tm.assert_extension_array_equal)
