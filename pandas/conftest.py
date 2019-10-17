64083884e27783296a70160a9a11a5f414171696
Angela Ambroz <angelaambroz@users.noreply.github.com>
2019-10-16 21:26:35 -0400

    Adding Dtype

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -11,6 +11,7 @@ import pytest
 from pytz import FixedOffset, utc  # type: ignore
 
 import pandas.util._test_decorators as td
+from pandas._typing import Dtype
 
 import pandas as pd
 from pandas import DataFrame
@@ -481,9 +482,9 @@ tz_aware_fixture2 = tz_aware_fixture
 
 UNSIGNED_INT_DTYPES = ["uint8", "uint16", "uint32", "uint64"]
 UNSIGNED_EA_INT_DTYPES = ["UInt8", "UInt16", "UInt32", "UInt64"]
-SIGNED_INT_DTYPES = [int, "int8", "int16", "int32", "int64"]
+SIGNED_INT_DTYPES = [int, "int8", "int16", "int32", "int64"] # type: Dtype
 SIGNED_EA_INT_DTYPES = ["Int8", "Int16", "Int32", "Int64"]
-ALL_INT_DTYPES = UNSIGNED_INT_DTYPES + SIGNED_INT_DTYPES  # type: ignore
+ALL_INT_DTYPES = UNSIGNED_INT_DTYPES + SIGNED_INT_DTYPES
 ALL_EA_INT_DTYPES = UNSIGNED_EA_INT_DTYPES + SIGNED_EA_INT_DTYPES
 
 FLOAT_DTYPES = [float, "float32", "float64"]
@@ -497,13 +498,13 @@ BOOL_DTYPES = [bool, "bool"]
 BYTES_DTYPES = [bytes, "bytes"]
 OBJECT_DTYPES = [object, "object"]
 
-ALL_REAL_DTYPES = FLOAT_DTYPES + ALL_INT_DTYPES  # type: ignore
+ALL_REAL_DTYPES = FLOAT_DTYPES + ALL_INT_DTYPES
 ALL_NUMPY_DTYPES = (
     ALL_REAL_DTYPES
     + COMPLEX_DTYPES
     + STRING_DTYPES
-    + DATETIME64_DTYPES  # type: ignore
-    + TIMEDELTA64_DTYPES  # type: ignore
+    + DATETIME64_DTYPES
+    + TIMEDELTA64_DTYPES
     + BOOL_DTYPES
     + OBJECT_DTYPES
     + BYTES_DTYPES
