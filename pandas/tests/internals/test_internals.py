750ad232fc93c06f0e45d306308e5f6d962fa870
jbrockmendel <jbrockmendel@gmail.com>
2019-10-18 19:18:56 -0700

    TST: _can_hold_element is equivalent to __setitem__ being allowed

--- pandas/tests/internals/test_internals.py
+++ pandas/tests/internals/test_internals.py
@@ -330,16 +330,24 @@ class TestDatetimeBlock:
     def test_can_hold_element(self):
         block = create_block("datetime", [0])
 
+        # We will check that block._can_hold_element iff arr.__setitem__ works
+        arr = pd.array(block.values.ravel())
+
         # coerce None
         assert block._can_hold_element(None)
+        arr[0] = None
+        assert arr[0] is pd.NaT
 
         # coerce different types of datetime objects
         vals = [np.datetime64("2010-10-10"), datetime(2010, 10, 10)]
         for val in vals:
             assert block._can_hold_element(val)
+            arr[0] = val
 
         val = date(2010, 10, 10)
         assert not block._can_hold_element(val)
+        with pytest.raises(TypeError):
+            arr[0] = val
 
 
 class TestBlockManager:
