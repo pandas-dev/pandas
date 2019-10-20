c742f47358065e524a68e58ca04c2334013c4302
cvonsteg <cvonstegmann@gmail.com>
2019-10-20 20:10:12 +0100

    scratch functions added

---
+++ pandas/io/sql_scratch.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Table, select
+from sqlalchemy.engine.base import Connection
+
+
+def get_pkey(table: Table):
+    return [pkey.name for pkey in table.primary_key.columns.values()]
+
+
+def get_pkey_values(table: Table, conn: Connection):
+    pkeys = get_pkey(table)
+    statement = select([table.c[name] for name in pkeys])
+    return [row for row in conn.execute(statement)]
