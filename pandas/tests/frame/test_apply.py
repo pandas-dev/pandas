45eb2efafdd7a10a833d8ded9421821c517f2930
Grigorios Giannakopoulos <grigoriosgiann@gmail.com>
2019-10-20 16:49:54 +0300

    Add a regression test for the timezone issue (#29097)

--- pandas/tests/frame/test_apply.py
+++ pandas/tests/frame/test_apply.py
@@ -1346,3 +1346,17 @@ class TestDataFrameAggregate:
         df = DataFrame(1, index=index, columns=range(num_cols))
         df.apply(lambda x: x)
         assert index.freq == original.freq
+
+    def test_apply_datetime_tz_issue(self):
+        # GH 29052
+
+        timestamps = [
+            pd.Timestamp("2019-03-15 12:34:31.909000+0000", tz="UTC"),
+            pd.Timestamp("2019-03-15 12:34:34.359000+0000", tz="UTC"),
+            pd.Timestamp("2019-03-15 12:34:34.660000+0000", tz="UTC"),
+        ]
+        df = DataFrame(data=[0, 1, 2], index=timestamps)
+        result = df.apply(lambda x: x.name, axis=1)
+        expected = pd.Series(index=timestamps, data=timestamps)
+
+        tm.assert_series_equal(result, expected)
