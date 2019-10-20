0d6eeef82a757c4bb6507b47e01f40af1d92f592
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2019-10-20 10:06:55 +0200

    Format code according to black

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -1439,11 +1439,13 @@ class TestMergeDtypes:
 
     def test_merge_key_dtype_cast(self):
         # GH 17044
-        df1 = DataFrame({'key': [1., 2.], 'v1': [10, 20]}, columns=['key', 'v1'])
-        df2 = DataFrame({'key': [2], 'v2': [200]}, columns=['key', 'v2'])
-        result = df1.merge(df2, on='key', how='left')
-        expected = DataFrame({'key': [1.0, 2.0], 'v1': [10, 20], 'v2': [np.nan, 200.0]},
-                             columns=['key', 'v1', 'v2'])
+        df1 = DataFrame({"key": [1.0, 2.0], "v1": [10, 20]}, columns=["key", "v1"])
+        df2 = DataFrame({"key": [2], "v2": [200]}, columns=["key", "v2"])
+        result = df1.merge(df2, on="key", how="left")
+        expected = DataFrame(
+            {"key": [1.0, 2.0], "v1": [10, 20], "v2": [np.nan, 200.0]},
+            columns=["key", "v1", "v2"],
+        )
         tm.assert_frame_equal(result, expected)
 
     def test_merge_on_ints_floats_warning(self):
