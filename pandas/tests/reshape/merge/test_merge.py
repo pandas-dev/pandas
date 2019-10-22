6b3666b1d2238a465684d20560d84fb20c28b9f6
Albert Villanova del Moral <8515462+albertvillanova@users.noreply.github.com>
2019-10-22 02:34:37 +0200

    TST: Test key dtype cast after merge (#29030)

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -1437,6 +1437,17 @@ class TestMergeDtypes:
         result = B.merge(A, left_on="Y", right_on="X")
         assert_frame_equal(result, expected[["Y", "X"]])
 
+    def test_merge_key_dtype_cast(self):
+        # GH 17044
+        df1 = DataFrame({"key": [1.0, 2.0], "v1": [10, 20]}, columns=["key", "v1"])
+        df2 = DataFrame({"key": [2], "v2": [200]}, columns=["key", "v2"])
+        result = df1.merge(df2, on="key", how="left")
+        expected = DataFrame(
+            {"key": [1.0, 2.0], "v1": [10, 20], "v2": [np.nan, 200.0]},
+            columns=["key", "v1", "v2"],
+        )
+        tm.assert_frame_equal(result, expected)
+
     def test_merge_on_ints_floats_warning(self):
         # GH 16572
         # merge will produce a warning when merging on int and
