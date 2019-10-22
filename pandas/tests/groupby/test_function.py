69ca2b490eb644c69761b330d23b437656066740
Will Ayd <william.ayd@icloud.com>
2019-10-22 15:37:41 -0700

    Black

--- pandas/tests/groupby/test_function.py
+++ pandas/tests/groupby/test_function.py
@@ -1375,7 +1375,7 @@ def test_quantile_out_of_bounds_q_raises():
 
 def test_quantile_missing_group_values_no_segfaults():
     # GH 28662
-    data = np.array([1., np.nan, 1.])
+    data = np.array([1.0, np.nan, 1.0])
     df = pd.DataFrame(dict(key=data, val=range(3)))
 
     # Random segfaults; would have been guaranteed in loop
@@ -1386,11 +1386,13 @@ def test_quantile_missing_group_values_no_segfaults():
 
 def test_quantile_missing_group_values_correct_results():
     # GH 28662
-    data = np.array([1., np.nan, 3., np.nan])
+    data = np.array([1.0, np.nan, 3.0, np.nan])
     df = pd.DataFrame(dict(key=data, val=range(4)))
 
     result = df.groupby("key").quantile()
-    expected = pd.DataFrame([1., 3.], index=pd.Index([1., 3.], name="key"), columns=["val"])
+    expected = pd.DataFrame(
+        [1.0, 3.0], index=pd.Index([1.0, 3.0], name="key"), columns=["val"]
+    )
     tm.assert_frame_equal(result, expected)
 
 
