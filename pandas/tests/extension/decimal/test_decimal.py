83535c744532d1fd857f94cd143a2137aa4aebb0
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-10-23 16:54:00 +0200

    additional tests with multiple keys/columns

--- pandas/tests/extension/decimal/test_decimal.py
+++ pandas/tests/extension/decimal/test_decimal.py
@@ -433,13 +433,31 @@ def test_groupby_agg():
     # https://github.com/pandas-dev/pandas/issues/29141
 
     data = make_data()[:5]
-    df = pd.DataFrame({"id": [0, 0, 0, 1, 1], "decimals": DecimalArray(data)})
-    expected = pd.Series(to_decimal([data[0], data[3]]))
+    df = pd.DataFrame(
+        {"id1": [0, 0, 0, 1, 1], "id2": [0, 1, 0, 1, 1], "decimals": DecimalArray(data)}
+    )
 
-    result = df.groupby("id")["decimals"].agg(lambda x: x.iloc[0])
+    # single key, selected column
+    expected = pd.Series(to_decimal([data[0], data[3]]))
+    result = df.groupby("id1")["decimals"].agg(lambda x: x.iloc[0])
     tm.assert_series_equal(result, expected, check_names=False)
-    result = df["decimals"].groupby(df["id"]).agg(lambda x: x.iloc[0])
+    result = df["decimals"].groupby(df["id1"]).agg(lambda x: x.iloc[0])
     tm.assert_series_equal(result, expected, check_names=False)
+
+    # multiple keys, selected column
+    expected = pd.Series(
+        to_decimal([data[0], data[1], data[3]]),
+        index=pd.MultiIndex.from_tuples([(0, 0), (0, 1), (1, 1)]),
+    )
+    result = df.groupby(["id1", "id2"])["decimals"].agg(lambda x: x.iloc[0])
+    tm.assert_series_equal(result, expected, check_names=False)
+    result = df["decimals"].groupby([df["id1"], df["id2"]]).agg(lambda x: x.iloc[0])
+    tm.assert_series_equal(result, expected, check_names=False)
+
+    # multiple columns
+    expected = pd.DataFrame({"id2": [0, 1], "decimals": to_decimal([data[0], data[3]])})
+    result = df.groupby("id1").agg(lambda x: x.iloc[0])
+    tm.assert_frame_equal(result, expected, check_names=False)
 
 
 def test_groupby_agg_ea_method(monkeypatch):
