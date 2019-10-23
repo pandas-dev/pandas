cfd604cd725d34467a4cdd5c555160bae2b4c34d
Lukas Bentkamp <lukas.bentkamp@mailbox.org>
2019-10-23 19:10:36 +0200

    Removed returns

--- pandas/tests/extension/base/base.py
+++ pandas/tests/extension/base/base.py
@@ -4,16 +4,16 @@ import pandas.util.testing as tm
 class BaseExtensionTests:
     @staticmethod
     def assert_equal(left, right, **kwargs):
-        return tm.assert_equal(left, right, **kwargs)
+        tm.assert_equal(left, right, **kwargs)
 
     @staticmethod
     def assert_series_equal(left, right, **kwargs):
-        return tm.assert_series_equal(left, right, **kwargs)
+        tm.assert_series_equal(left, right, **kwargs)
 
     @staticmethod
     def assert_frame_equal(left, right, **kwargs):
-        return tm.assert_frame_equal(left, right, **kwargs)
+        tm.assert_frame_equal(left, right, **kwargs)
 
     @staticmethod
     def assert_extension_array_equal(left, right, **kwargs):
-        return tm.assert_extension_array_equal(left, right, **kwargs)
+        tm.assert_extension_array_equal(left, right, **kwargs)
