a6d1036a7883e7884524aaf916bff9c0b648519e
Pedro Cunial <pedro.campos@quintoandar.com.br>
2019-10-24 08:18:25 -0300

    change unused loop variable to _

--- pandas/_version.py
+++ pandas/_version.py
@@ -455,7 +455,7 @@ def get_versions():
         # versionfile_source is the relative path from the top of the source
         # tree (where the .git directory might live) to this file. Invert
         # this to find the root from __file__.
-        for i in cfg.versionfile_source.split("/"):
+        for _ in cfg.versionfile_source.split("/"):
             root = os.path.dirname(root)
     except NameError:
         return {
