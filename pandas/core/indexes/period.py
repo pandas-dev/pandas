98cdb672536d3078286a356e75a4fbf5656620ce
hughkelley <hghklly@gmail.com>
2019-10-25 15:31:00 +0100

    fix line length

--- pandas/core/indexes/period.py
+++ pandas/core/indexes/period.py
@@ -80,8 +80,8 @@ class PeriodIndex(DatetimeIndexOpsMixin, Int64Index, PeriodDelegateMixin):
     Holds ordinal time values.
 
     An immutable ndarray. Indicates regular periods in time such as particular years,
-    quarters, months, etc. Index keys are boxed to Period objects which carries the metadata (eg,
-    frequency information).
+    quarters, months, etc. Index keys are boxed to Period objects which carries the 
+    metadata (eg, frequency information).
 
     Parameters
     ----------
