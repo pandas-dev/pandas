c1a4d7e699e6041f5a348d8d37005179938680e3
gfyoung <gfyoung17+GitHub@gmail.com>
2019-10-25 09:54:34 -0700

    TST: Add test for nested JSON normalization (#29225)

--- pandas/tests/io/json/test_normalize.py
+++ pandas/tests/io/json/test_normalize.py
@@ -457,6 +457,14 @@ class TestJSONNormalize:
         expected_df = DataFrame(data=expected, columns=result.columns.values)
         tm.assert_equal(expected_df, result)
 
+    def test_nested_flattening_consistent(self):
+        # see gh-21537
+        df1 = json_normalize([{"A": {"B": 1}}])
+        df2 = json_normalize({"dummy": [{"A": {"B": 1}}]}, "dummy")
+
+        # They should be the same.
+        tm.assert_frame_equal(df1, df2)
+
 
 class TestNestedToRecord:
     def test_flat_stays_flat(self):
