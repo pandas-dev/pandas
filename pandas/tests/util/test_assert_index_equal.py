efe93cd263e7484285dc4f7dcd6cea385c3e0fe2
vijayant <vijayant@poshmark.com>
2019-10-26 07:02:34 -0700

    formatted file using black

--- pandas/tests/util/test_assert_index_equal.py
+++ pandas/tests/util/test_assert_index_equal.py
@@ -175,6 +175,6 @@ ordered=False\\)"""
 
 def test_index_equal_empty_iterable():
     # #16844
-    a = MultiIndex(levels=[[], []], codes=[[], []], names=['a', 'b'])
-    b = MultiIndex.from_arrays(arrays=[[], []], names=['a', 'b'])
+    a = MultiIndex(levels=[[], []], codes=[[], []], names=["a", "b"])
+    b = MultiIndex.from_arrays(arrays=[[], []], names=["a", "b"])
     assert_index_equal(a, b)
