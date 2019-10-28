bfcb20e2e4028353573e3bedeb0271c84de23aef
Asish Mahapatra <asishkm@gmail.com>
2019-10-28 11:51:40 -0400

    add check for new object

--- pandas/tests/frame/test_combine_concat.py
+++ pandas/tests/frame/test_combine_concat.py
@@ -130,6 +130,7 @@ class TestDataFrameConcatCommon:
         assert_frame_equal(results, expected)
 
     def test_append_empty_list(self):
+        # GH 28769
         df = DataFrame()
         result = df.append([])
         expected = df.copy()
@@ -139,6 +140,7 @@ class TestDataFrameConcatCommon:
         result = df.append([])
         expected = df.copy()
         assert_frame_equal(result, expected)
+        assert result is not df # .append() should return a new object
 
     def test_append_series_dict(self):
         df = DataFrame(np.random.randn(5, 4), columns=["foo", "bar", "baz", "qux"])
