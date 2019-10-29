9c8de15092e6f2b1ea545c18ed6ac6709c9cfda4
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-10-29 10:00:39 +0100

    DOC: use new logo in the sphinx theme as well (#29259)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -218,7 +218,7 @@ html_theme = "pandas_sphinx_theme"
 
 # The name of an image file (relative to this directory) to place at the top
 # of the sidebar.
-# html_logo = None
+html_logo = "../../web/pandas/static/img/pandas.svg"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
