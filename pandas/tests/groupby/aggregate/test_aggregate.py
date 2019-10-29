b63f866dea225d6c944edc87678f29314ca6039a
gfyoung <gfyoung17+GitHub@gmail.com>
2019-10-29 06:09:05 -0700

    TST: Test named aggregations with functions (#29262)

--- pandas/tests/groupby/aggregate/test_aggregate.py
+++ pandas/tests/groupby/aggregate/test_aggregate.py
@@ -495,6 +495,33 @@ class TestNamedAggregationDataFrame:
         tm.assert_frame_equal(result, expected)
 
 
+def myfunc(s):
+    return np.percentile(s, q=0.90)
+
+
+@pytest.mark.parametrize("func", [lambda s: np.percentile(s, q=0.90), myfunc])
+def test_lambda_named_agg(func):
+    # see gh-28467
+    animals = DataFrame(
+        {
+            "kind": ["cat", "dog", "cat", "dog"],
+            "height": [9.1, 6.0, 9.5, 34.0],
+            "weight": [7.9, 7.5, 9.9, 198.0],
+        }
+    )
+
+    result = animals.groupby("kind").agg(
+        mean_height=("height", "mean"), perc90=("height", func)
+    )
+    expected = DataFrame(
+        [[9.3, 9.1036], [20.0, 6.252]],
+        columns=["mean_height", "perc90"],
+        index=Index(["cat", "dog"], name="kind"),
+    )
+
+    tm.assert_frame_equal(result, expected)
+
+
 class TestLambdaMangling:
     def test_maybe_mangle_lambdas_passthrough(self):
         assert _maybe_mangle_lambdas("mean") == "mean"
