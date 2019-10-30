166b4bb4843688555ef6dded522df01decebb533
jbrockmendel <jbrockmendel@gmail.com>
2019-10-30 11:38:20 -0700

    Backport PR #29294 for 0.25.3 whatsnew

--- pandas/tests/io/parser/test_common.py
+++ pandas/tests/io/parser/test_common.py
@@ -1898,10 +1898,7 @@ def test_null_byte_char(all_parsers):
         out = parser.read_csv(StringIO(data), names=names)
         tm.assert_frame_equal(out, expected)
     else:
-        if compat.PY38:
-            msg = "line contains NUL"
-        else:
-            msg = "NULL byte detected"
+        msg = "NULL byte detected"
         with pytest.raises(ParserError, match=msg):
             parser.read_csv(StringIO(data), names=names)
 
