d82930918b65334cbab92599fe2cf59b16d6aab5
Marco Gorelli <m.gorelli@ws-1808.seri.co.uk>
2019-10-30 08:11:55 +0000

    Update test so there is no np.asarray, but rather the original return types (datetimearray and np.array) are used

--- pandas/tests/extension/test_categorical.py
+++ pandas/tests/extension/test_categorical.py
@@ -224,25 +224,23 @@ class TestCasting(base.BaseCastingTests):
         tm.assert_series_equal(result, expected)
 
     @pytest.mark.parametrize(
-        "data, dtype, expected",
+        "dtype, expected",
         [
             (
-                "2015-01-01",
                 "datetime64[ns]",
                 np.array(["2015-01-01T00:00:00.000000000"], dtype="datetime64[ns]"),
             ),
             (
-                "2015-01-01",
                 "datetime64[ns, MET]",
-                np.array(
-                    [Timestamp("2015-01-01 00:00:00+0100", tz="MET")], dtype=object
+                pd.arrays.DatetimeArray(
+                    pd.array([pd.Timestamp("2015-01-01 00:00:00+0100", tz="MET")])
                 ),
             ),
         ],
     )
-    def test_consistent_casting(self, data, dtype, expected):
+    def test_consistent_casting(self, dtype, expected):
         # GH 28448
-        result = np.asarray(pd.Categorical(data).astype(dtype))
+        result = pd.Categorical("2015-01-01").astype(dtype)
         assert result == expected
 
 
