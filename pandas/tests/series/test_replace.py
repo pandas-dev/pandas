521227de2c7a533a3aa9113990abf421e573d9b1
pv8493013j <pbvriend@gmail.com>
2019-11-02 18:33:52 +0000

    tidied tests

--- pandas/tests/series/test_replace.py
+++ pandas/tests/series/test_replace.py
@@ -318,7 +318,8 @@ class TestSeriesReplace:
         # DataFrame.replace() overwrites when values are non-numeric
 
         series = pd.Series(ser)
-        expected = pd.Series(exp)
 
+        expected = pd.Series(exp)
         result = series.replace(to_replace)
+
         tm.assert_series_equal(result, expected)
