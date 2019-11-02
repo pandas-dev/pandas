cadbdc38666865f0d61528a3a9ca0822a3b717d5
ganevgv <ganevgv@gmail.com>
2019-11-02 15:01:19 +0000

    change input data

--- pandas/tests/test_multilevel.py
+++ pandas/tests/test_multilevel.py
@@ -1934,11 +1934,66 @@ Thur,Lunch,Yes,51.51,17"""
 
     def test_subsets_multiindex_dtype(self):
         # GH 20757
-        cols = [(1, 2), (3, 4), (5, 6), (7, 8)]
-        data = ["a", "b", "c", "d"]
-        result = pd.DataFrame(data, columns=MultiIndex.from_tuples(cols)).dtypes.a.d.i
-        expected = pd.DataFrame(data, columns=MultiIndex.from_tuples(cols)).a.d.i.dtypes
-        tm.assert_frame_equal(result, expected)
+        data = [
+            [
+                "n",
+                1,
+                0,
+                False,
+                2,
+                1,
+                False,
+                0,
+                0,
+                False,
+                2,
+                0,
+                False,
+                0,
+                1,
+                False,
+                1,
+                1,
+                False,
+                "o",
+                1521734085.289453,
+                "p",
+                3233,
+                1521734085.289494,
+            ]
+        ]
+
+        columns = [
+            ("a", "d", "i", np.nan, np.nan),
+            ("a", "d", "j", 0.0, "k"),
+            ("a", "d", "j", 0.0, "l"),
+            ("a", "d", "j", 0.0, "m"),
+            ("a", "d", "j", 1.0, "k"),
+            ("a", "d", "j", 1.0, "l"),
+            ("a", "d", "j", 1.0, "m"),
+            ("a", "d", "j", 2.0, "k"),
+            ("a", "d", "j", 2.0, "l"),
+            ("a", "d", "j", 2.0, "m"),
+            ("a", "d", "j", 3.0, "k"),
+            ("a", "d", "j", 3.0, "l"),
+            ("a", "d", "j", 3.0, "m"),
+            ("a", "d", "j", 4.0, "k"),
+            ("a", "d", "j", 4.0, "l"),
+            ("a", "d", "j", 4.0, "m"),
+            ("a", "d", "j", 5.0, "k"),
+            ("a", "d", "j", 5.0, "l"),
+            ("a", "d", "j", 5.0, "m"),
+            ("b", "f", np.nan, np.nan, np.nan),
+            ("b", "h", np.nan, np.nan, np.nan),
+            ("c", "e", np.nan, np.nan, np.nan),
+            ("c", "g", np.nan, np.nan, np.nan),
+            ("c", "h", np.nan, np.nan, np.nan),
+        ]
+
+        columns = pd.MultiIndex.from_tuples(columns)
+        df = pd.DataFrame(data, columns=columns)
+
+        tm.assert_equal(df.dtypes.a.d.i, df.a.d.i.dtypes)
 
 
 class TestSorted(Base):
