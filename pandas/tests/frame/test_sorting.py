7f495006d3195ca7f3eae6b2b701e515450f1f67
pv8493013j <pbvriend@gmail.com>
2019-11-03 12:26:02 +0000

    specifying index for expected df

--- pandas/tests/frame/test_sorting.py
+++ pandas/tests/frame/test_sorting.py
@@ -747,13 +747,14 @@ class TestDataFrameSortIndexKinds:
             }
         )
         df["a"] = df["a"].astype(np.uint64)
-        result = df.sort_values(["a", "b"]).reset_index(drop=True)
+        result = df.sort_values(["a", "b"])
 
         expected = pd.DataFrame(
             {
-                "a": pd.Series([1162265347240853609, 18446637057563306014]),
-                "b": pd.Series([2, 1]),
-            }
+                "a": pd.Series([18446637057563306014, 1162265347240853609]),
+                "b": pd.Series([1, 2]),
+            },
+            index=pd.Index([1, 0]),
         )
 
         tm.assert_frame_equal(result, expected)
