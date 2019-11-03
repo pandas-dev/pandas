792c14518ac0035219b0f149afa43b7125205427
pv8493013j <pbvriend@gmail.com>
2019-11-03 12:18:09 +0000

    formatting

--- pandas/tests/frame/test_analytics.py
+++ pandas/tests/frame/test_analytics.py
@@ -1081,16 +1081,16 @@ class TestDataFrameAnalytics:
         # possible bug when calculating mean of DataFrame?
 
         d = [
-            {"A": 2, "B": None, "C": Decimal(628.00)},
-            {"A": 1, "B": None, "C": Decimal(383.00)},
-            {"A": 3, "B": None, "C": Decimal(651.00)},
-            {"A": 2, "B": None, "C": Decimal(575.00)},
-            {"A": 4, "B": None, "C": Decimal(1114.00)},
-            {"A": 1, "B": "TEST", "C": Decimal(241.00)},
-            {"A": 2, "B": None, "C": Decimal(572.00)},
-            {"A": 4, "B": None, "C": Decimal(609.00)},
-            {"A": 3, "B": None, "C": Decimal(820.00)},
-            {"A": 5, "B": None, "C": Decimal(1223.00)},
+            {"A": 2, "B": None, "C": Decimal("628.00")},
+            {"A": 1, "B": None, "C": Decimal("383.00")},
+            {"A": 3, "B": None, "C": Decimal("651.00")},
+            {"A": 2, "B": None, "C": Decimal("575.00")},
+            {"A": 4, "B": None, "C": Decimal("1114.00")},
+            {"A": 1, "B": "TEST", "C": Decimal("241.00")},
+            {"A": 2, "B": None, "C": Decimal("572.00")},
+            {"A": 4, "B": None, "C": Decimal("609.00")},
+            {"A": 3, "B": None, "C": Decimal("820.00")},
+            {"A": 5, "B": None, "C": Decimal("1223.00")},
         ]
 
         df = pd.DataFrame(d)
