20f5f8bf938d1422640989fde10f24bd46871861
pv8493013j <pbvriend@gmail.com>
2019-11-03 12:14:22 +0000

    changed float to decimal for test

--- pandas/tests/frame/test_analytics.py
+++ pandas/tests/frame/test_analytics.py
@@ -2,6 +2,7 @@ from datetime import timedelta
 import operator
 from string import ascii_lowercase
 import warnings
+from decimal import Decimal
 
 import numpy as np
 import pytest
@@ -1075,21 +1076,21 @@ class TestDataFrameAnalytics:
         expected = pd.Series()
         tm.assert_series_equal(result, expected)
 
-    def test_mean_mixed_string_float(self):
+    def test_mean_mixed_string_decimal(self):
         # GH 11670
         # possible bug when calculating mean of DataFrame?
 
         d = [
-            {"A": 2, "B": None, "C": 628.00},
-            {"A": 1, "B": None, "C": 383.00},
-            {"A": 3, "B": None, "C": 651.00},
-            {"A": 2, "B": None, "C": 575.00},
-            {"A": 4, "B": None, "C": 1114.00},
-            {"A": 1, "B": "TEST", "C": 241.00},
-            {"A": 2, "B": None, "C": 572.00},
-            {"A": 4, "B": None, "C": 609.00},
-            {"A": 3, "B": None, "C": 820.00},
-            {"A": 5, "B": None, "C": 1223.00},
+            {"A": 2, "B": None, "C": Decimal(628.00)},
+            {"A": 1, "B": None, "C": Decimal(383.00)},
+            {"A": 3, "B": None, "C": Decimal(651.00)},
+            {"A": 2, "B": None, "C": Decimal(575.00)},
+            {"A": 4, "B": None, "C": Decimal(1114.00)},
+            {"A": 1, "B": "TEST", "C": Decimal(241.00)},
+            {"A": 2, "B": None, "C": Decimal(572.00)},
+            {"A": 4, "B": None, "C": Decimal(609.00)},
+            {"A": 3, "B": None, "C": Decimal(820.00)},
+            {"A": 5, "B": None, "C": Decimal(1223.00)},
         ]
 
         df = pd.DataFrame(d)
