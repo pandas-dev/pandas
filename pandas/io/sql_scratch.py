a7b8e8eb47862f0d20b547a1af1f7047cb13ee2e
cvonsteg <cvonstegmann@gmail.com>
2019-11-05 19:16:27 +0000

    sql.py - methods to get pkey columns, pkey iterator, and almost finished upsert ignore method

--- pandas/io/sql_scratch.py
+++ pandas/io/sql_scratch.py
@@ -35,14 +35,24 @@ def pkey_generator2(table, engine):
     statement = select([table.c[name] for name in pkeys])
     with engine.connect() as conn:
         result = conn.execute(statement)
-    for row in result:
-        yield result.fetchone()
+    try:
+        for row in result:
+            yield result.fetchone()
+    finally:
+        result.close()
+
 
 # replace table with self
 def get_pkey_values(table: SQLTable):
     pkeys = [pkey.name for pkey in table.table.primary_key.columns.values()]
     statement = select([table.table.c[name] for name in pkeys])
     table.pd_sql.execute(statement)
+
+def generate_mask(df, dictionary):
+    return [df[key] == value for key, value in dictionary.items()]
+
+def generate_mask_of_masks(list_of_masks):
+    return pd.concat([mask for mask in list_of_masks], axis=1).all(1)
 
 
 ### REPRODUCIBLE SQLTable Creation:table
@@ -67,19 +77,25 @@ table_name = 'charterers'
 df = pd.read_sql_table(table_name, engine_v)
 df_test = df.head().copy()
 df_test['name'] = df_test['name'].apply(lambda x: x + '_TEST')
-engine.execute("create table charterers(id varchar primary key, name text, energy integer)")
+engine.execute("create table charterers(id text primary key, name text, energy integer)")
 df.to_sql(table_name, index=False, if_exists='append', con=engine)
 
 db = SQLDatabase(engine, schema=None, meta=None)
 new_data = SQLTable(table_name, db, frame=df_test, index=False)
+
+
 
 from sqlalchemy.sql import tuple_
 
 
 def delete_matching_keys(sql_table, key_columns, value_iter):
     delete_expression = sql_table.table.delete().where(
-        tuple_(*(table.table.c[col] for col in key_columns)).in_(value_iter)
+        tuple_(
+            *(table.table.c[col] for col in key_columns)
+        ).in_(
+            list(zip(value_iter))
+        )
     )
-    with sql_table.connectable.connect() as conn:
+    with sql_table.pd_sql.run_transaction() as conn:
         conn.execute(delete_expression)
 
