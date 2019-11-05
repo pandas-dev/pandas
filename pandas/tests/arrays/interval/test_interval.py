f325ff1c5c53ed1c7d14acae06d0c20e08cc72c9
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2019-11-05 16:30:33 +0100

    add tests for missing values with IntervalArray

--- pandas/tests/arrays/interval/test_interval.py
+++ pandas/tests/arrays/interval/test_interval.py
@@ -169,3 +169,33 @@ def test_arrow_array():
 
     with pytest.raises(TypeError, match="different 'subtype'"):
         pa.array(intervals, type=ArrowIntervalType(pa.float64(), "left"))
+
+
+@pyarrow_skip
+def test_arrow_array_missing():
+    import pyarrow as pa
+    from pandas.core.arrays.interval import ArrowIntervalType
+
+    arr = pd.arrays.IntervalArray.from_breaks([0, 1, 2, 3])
+    arr[1] = None
+
+    result = pa.array(arr)
+    assert isinstance(result.type, ArrowIntervalType)
+    assert result.type.closed == arr.closed
+    assert result.type.subtype == pa.float64()
+
+    # fields have missing values (not NaN)
+    left = pa.array([0.0, None, 2.0], type="float64")
+    right = pa.array([1.0, None, 3.0], type="float64")
+    assert result.storage.field("left").equals(left)
+    assert result.storage.field("right").equals(right)
+
+    # TODO implement setting the missing values bitmap on the array level
+    # # structarray itself also has missing values on the array level
+    # vals = [
+    #     {"left": 0.0, "right": 1.0},
+    #     {"left": None, "right": None},
+    #     {"left": 2.0, "right": 3.0},
+    # ]
+    # expected = pa.StructArray.from_pandas(vals, mask=np.array([False, True, False]))
+    # assert result.storage.equals(expected)
