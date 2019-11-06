58148f1b1e87fdec782e2a8ab39e507568dab598
Alasdair Dalgarno <alasdair.dalgarno@babylonhealth.com>
2019-11-06 16:19:56 -0500

    docs: add example to one fixture

--- pandas/tests/arithmetic/conftest.py
+++ pandas/tests/arithmetic/conftest.py
@@ -24,6 +24,22 @@ def one(request):
     """
     Several variants of integer value 1. The zero-dim integer array
     behaves like an integer.
+
+    This fixture is used to check that datetimelike indexes handle
+    addition and subtraction of integers and zero-dimensional arrays
+    of integers.
+
+    Examples
+    --------
+
+    >>> dti = pd.date_range('2016-01-01', periods=2, freq='H')
+    >>> dti
+    DatetimeIndex(['2016-01-01 00:00:00', '2016-01-01 01:00:00'],
+    dtype='datetime64[ns]', freq='H')
+    >>> dti + one
+    DatetimeIndex(['2016-01-01 01:00:00', '2016-01-01 02:00:00'],
+    dtype='datetime64[ns]', freq='H')
+
     """
     return request.param
 
