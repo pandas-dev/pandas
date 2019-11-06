961a72cc78a2c5c43d68e67a55ecbff2b59b4629
donghojung <andrew@buzzni.com>
2019-11-06 10:13:25 +0900

    add a comment under recons_labels

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -752,6 +752,7 @@ class BinGrouper(BaseGrouper):
 
     @cache_readonly
     def recons_labels(self):
+        # get unique result indices, and prepend 0 as groupby starts from the first
         return [np.r_[0, np.flatnonzero(self.bins[1:] != self.bins[:-1]) + 1]]
 
     @cache_readonly
