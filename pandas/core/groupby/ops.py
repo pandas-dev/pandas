368311c9adbaf2bef9764fd001a8e3f3340701e1
0xF4D3C0D3 <dongho971220@gmail.com>
2019-11-07 23:17:53 +0900

    rename from recons_labels to recons_codes

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -768,7 +768,7 @@ class BinGrouper(BaseGrouper):
         )
 
     @cache_readonly
-    def recons_labels(self):
+    def recons_codes(self):
         # get unique result indices, and prepend 0 as groupby starts from the first
         return [np.r_[0, np.flatnonzero(self.bins[1:] != self.bins[:-1]) + 1]]
 
