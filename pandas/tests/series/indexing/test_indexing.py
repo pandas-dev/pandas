497677e3800afafa3c612b64a1a5d3f4eafe367e
ganevgv <ganevgv@users.noreply.github.com>
2019-11-07 00:09:19 +0000

    change name test
    Co-Authored-By: Simon Hawkins <simonjayhawkins@gmail.com>

--- pandas/tests/series/indexing/test_indexing.py
+++ pandas/tests/series/indexing/test_indexing.py
@@ -391,7 +391,7 @@ def test_setslice(datetime_series):
     assert sl.index.is_unique is True
 
 
-def test_2d_to_1d_assignment_error_raised():
+def test_2d_to_1d_assignment_raises():
     x = np.random.randn(2, 2)
     y = pd.Series(range(2))
 
