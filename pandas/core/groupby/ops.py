8efc717e4652e1e4bfbc4455da1d40eb676eed91
Jung Dong Ho <dongho971220@gmail.com>
2019-11-08 06:19:57 +0900

    BUG: value_counts can handle the case even with empty groups (#28479) (#28634)

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -768,6 +768,11 @@ class BinGrouper(BaseGrouper):
         )
 
     @cache_readonly
+    def recons_codes(self):
+        # get unique result indices, and prepend 0 as groupby starts from the first
+        return [np.r_[0, np.flatnonzero(self.bins[1:] != self.bins[:-1]) + 1]]
+
+    @cache_readonly
     def result_index(self):
         if len(self.binlabels) != 0 and isna(self.binlabels[0]):
             return self.binlabels[1:]
