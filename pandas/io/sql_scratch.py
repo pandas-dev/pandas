6bcd6c2e23cab3821735967a74f3a3a1c5de8b03
cvonsteg <cvonstegmann@gmail.com>
2019-11-09 16:49:35 +0000

    tidy up repo

--- pandas/io/sql_scratch.py
+++ pandas/io/sql_scratch.py
@@ -1,5 +1,13 @@
-from sqlalchemy import Table, select
+### REPRODUCIBLE SQLTable Creation:table
+import sqlalchemy
+from sqlalchemy import Table, create_engine, select
 from sqlalchemy.engine.base import Connection
+from sqlalchemy.sql import tuple_
+
+import pandas as pd
+from vortexa_utils.database import ProdFactory
+
+from pandas.io.sql import SQLDatabase, SQLTable
 
 
 def get_pkey(table: Table):
@@ -20,6 +28,7 @@ def pkey_generator(table, engine):
     with engine.connect() as conn:
         for row in conn.execute(statement):
             yield row
+
 
 # Leaves connection open
 def pkey_results_proxy(table, engine):
@@ -48,54 +57,41 @@ def get_pkey_values(table: SQLTable):
     statement = select([table.table.c[name] for name in pkeys])
     table.pd_sql.execute(statement)
 
+
 def generate_mask(df, dictionary):
     return [df[key] == value for key, value in dictionary.items()]
+
 
 def generate_mask_of_masks(list_of_masks):
     return pd.concat([mask for mask in list_of_masks], axis=1).all(1)
 
 
-### REPRODUCIBLE SQLTable Creation:table
-import sqlalchemy
-
-engine = sqlalchemy.create_engine('enter string here')
+engine = sqlalchemy.create_engine("enter string here")
 meta = MetaData(engine)
-table_name = 'charterers' # or wtv
+table_name = "charterers"  # or wtv
 meta.reflect(only=[table_name], views=True)
 db = SQLDatabase(engine, meta=meta)
 table = SQLTable(table_name, db, index=None, schema=None)
 
 
-from vortexa_utils.database import ProdFactory
-from sqlalchemy import create_engine
-import pandas as pd
-from pandas.io.sql import SQLTable, SQLDatabase
-
 engine_v = ProdFactory().engine()
-engine = create_engine('sqlite:///:memory:')
-table_name = 'charterers'
+engine = create_engine("sqlite:///:memory:")
+table_name = "charterers"
 df = pd.read_sql_table(table_name, engine_v)
 df_test = df.head().copy()
-df_test['name'] = df_test['name'].apply(lambda x: x + '_TEST')
-engine.execute("create table charterers(id text primary key, name text, energy integer)")
-df.to_sql(table_name, index=False, if_exists='append', con=engine)
+df_test["name"] = df_test["name"].apply(lambda x: x + "_TEST")
+engine.execute(
+    "create table charterers(id text primary key, name text, energy integer)"
+)
+df.to_sql(table_name, index=False, if_exists="append", con=engine)
 
 db = SQLDatabase(engine, schema=None, meta=None)
 new_data = SQLTable(table_name, db, frame=df_test, index=False)
 
 
-
-from sqlalchemy.sql import tuple_
-
-
 def delete_matching_keys(sql_table, key_columns, value_iter):
     delete_expression = sql_table.table.delete().where(
-        tuple_(
-            *(table.table.c[col] for col in key_columns)
-        ).in_(
-            list(zip(value_iter))
-        )
+        tuple_(*(table.table.c[col] for col in key_columns)).in_(list(zip(value_iter)))
     )
     with sql_table.pd_sql.run_transaction() as conn:
         conn.execute(delete_expression)
-
