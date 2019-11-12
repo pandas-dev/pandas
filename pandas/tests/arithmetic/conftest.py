f7460eac668d70371f3b286565720befd4e99c30
Alasdair Dalgarno <alasdair.dalgarno@babylonhealth.com>
2019-11-12 21:41:54 +0000

    style: remove whitespace for lint

--- pandas/tests/arithmetic/conftest.py
+++ pandas/tests/arithmetic/conftest.py
@@ -58,11 +58,11 @@ zeros.extend([0, 0.0, -0.0])
 @pytest.fixture(params=zeros)
 def zero(request):
     """
-    Several types of scalar zeros and length 5 vectors of zeros. 
-    
+    Several types of scalar zeros and length 5 vectors of zeros.
+ 
     This fixture can be used to check that numeric-dtype indexes handle
     division by any zero numeric-dtype.
-    
+
     Uses vector of length 5 for broadcasting with `numeric_idx` fixture,
     which creates numeric-dtype vectors also of length 5.
 
