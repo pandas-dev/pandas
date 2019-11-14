a8f15c302d3836188c123bd058e2d7961f8c2cc0
Marc Garcia <garcia.marc@gmail.com>
2019-11-14 18:28:29 +0000

    Running xvfb only in travis

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -26,10 +26,13 @@ if [ "$COVERAGE" ]; then
 fi
 
 PYTEST_CMD="pytest -m \"$PATTERN\" -n auto --dist=loadfile -s --strict --durations=10 --junitxml=test-data.xml $TEST_ARGS $COVERAGE pandas"
-if [[ "$AGENT_OS" == "Linux" || "$TRAVIS_OS_NAME" == "linux" ]]; then
+
+# Travis does not have xvfb active
+if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     DISPLAY=DISPLAY=:99.0
-    PYTEST_CMD="$PYTEST_CMD"
+    PYTEST_CMD="xvfb-run $PYTEST_CMD"
 fi
+
 echo $PYTEST_CMD
 sh -c "$PYTEST_CMD"
 
