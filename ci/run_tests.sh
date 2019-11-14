0c54fa0f75d94151d29adb8c23b423531d7e73bd
Marc Garcia <garcia.marc@gmail.com>
2019-11-14 20:19:18 +0000

    Adding error information for xvfb

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -30,7 +30,7 @@ PYTEST_CMD="pytest -m \"$PATTERN\" -n auto --dist=loadfile -s --strict --duratio
 # Travis does not have xvfb active
 if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
     DISPLAY=DISPLAY=:99.0
-    PYTEST_CMD="xvfb-run $PYTEST_CMD"
+    PYTEST_CMD="xvfb-run -e /dev/stdout $PYTEST_CMD"
 fi
 
 echo $PYTEST_CMD
