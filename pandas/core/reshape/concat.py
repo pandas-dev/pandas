51276e311115e2d3f8d8b02063508b585869ee5d
jbrockmendel <jbrockmendel@gmail.com>
2019-11-14 13:11:40 -0800

    typings

--- pandas/core/reshape/concat.py
+++ pandas/core/reshape/concat.py
@@ -1,7 +1,6 @@
 """
 concat routines
 """
-
 import warnings
 
 import numpy as np
@@ -520,15 +519,11 @@ class _Concatenator:
         new_axes[self.axis] = self._get_concat_axis()
         return new_axes
 
-    def _get_comb_axis(self, i: int):
+    def _get_comb_axis(self, i: int) -> Index:
         data_axis = self.objs[0]._get_block_manager_axis(i)
-        try:
-            return _get_objs_combined_axis(
-                self.objs, axis=data_axis, intersect=self.intersect, sort=self.sort
-            )
-        except IndexError:
-            types = [type(x).__name__ for x in self.objs]
-            raise TypeError("Cannot concatenate list of {types}".format(types=types))
+        return _get_objs_combined_axis(
+            self.objs, axis=data_axis, intersect=self.intersect, sort=self.sort
+        )
 
     def _get_concat_axis(self) -> Index:
         """
