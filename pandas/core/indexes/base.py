83abe99e13ca6828e10f9f4b0eed88bbd3d594a3
jbrockmendel <jbrockmendel@gmail.com>
2019-11-15 20:12:00 -0800

    black version fixup

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -1868,7 +1868,7 @@ class Index(IndexOpsMixin, PandasObject):
     @cache_readonly
     def _nan_idxs(self):
         if self._can_hold_na:
-            (w,) = self._isnan.nonzero()
+            w = self._isnan.nonzero()[0]
             return w
         else:
             return np.array([], dtype=np.int64)
