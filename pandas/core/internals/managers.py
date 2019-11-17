6d38c1bf039196ccc4587a35c363bb0f254fb06d
Samuel <samuelsinayoko@bmlltech.com>
2019-11-17 23:40:59 +0000

    black

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -1868,7 +1868,7 @@ def _stack_arrays(tuples, dtype):
 
 
 def _interleaved_dtype(
-    blocks: List[Block]
+    blocks: List[Block],
 ) -> Optional[Union[np.dtype, ExtensionDtype]]:
     """Find the common dtype for `blocks`.
 
