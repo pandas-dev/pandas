44313877ecd3ef2a890158b837d45f0fa4de97fa
jbrockmendel <jbrockmendel@gmail.com>
2019-11-17 16:20:37 -0800

    CLN: parts of #29667 (#29677)

--- pandas/core/computation/pytables.py
+++ pandas/core/computation/pytables.py
@@ -13,7 +13,7 @@ from pandas.core.dtypes.common import is_list_like
 
 import pandas as pd
 import pandas.core.common as com
-from pandas.core.computation import expr, ops
+from pandas.core.computation import expr, ops, scope as _scope
 from pandas.core.computation.common import _ensure_decoded
 from pandas.core.computation.expr import BaseExprVisitor
 from pandas.core.computation.ops import UndefinedVariableError, is_term
@@ -21,10 +21,10 @@ from pandas.core.computation.ops import UndefinedVariableError, is_term
 from pandas.io.formats.printing import pprint_thing, pprint_thing_encoded
 
 
-class Scope(expr.Scope):
+class Scope(_scope.Scope):
     __slots__ = ("queryables",)
 
-    def __init__(self, level, global_dict=None, local_dict=None, queryables=None):
+    def __init__(self, level: int, global_dict=None, local_dict=None, queryables=None):
         super().__init__(level + 1, global_dict=global_dict, local_dict=local_dict)
         self.queryables = queryables or dict()
 
@@ -40,6 +40,7 @@ class Term(ops.Term):
     def _resolve_name(self):
         # must be a queryables
         if self.side == "left":
+            # Note: The behavior of __new__ ensures that self.name is a str here
             if self.name not in self.env.queryables:
                 raise NameError("name {name!r} is not defined".format(name=self.name))
             return self.name
