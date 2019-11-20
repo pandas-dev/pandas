6b23b79df2f578134f0bb8ef1e1e6d9b154eb242
jbrockmendel <jbrockmendel@gmail.com>
2019-11-19 19:20:38 -0800

    DEPR: change DTI.to_series keep_tz default to True

--- pandas/tests/frame/test_constructors.py
+++ pandas/tests/frame/test_constructors.py
@@ -1795,7 +1795,7 @@ class TestDataFrameConstructors:
         # preserver an index with a tz on dict construction
         i = date_range("1/1/2011", periods=5, freq="10s", tz="US/Eastern")
 
-        expected = DataFrame({"a": i.to_series(keep_tz=True).reset_index(drop=True)})
+        expected = DataFrame({"a": i.to_series().reset_index(drop=True)})
         df = DataFrame()
         df["a"] = i
         tm.assert_frame_equal(df, expected)
@@ -1807,7 +1807,7 @@ class TestDataFrameConstructors:
         i_no_tz = date_range("1/1/2011", periods=5, freq="10s")
         df = DataFrame({"a": i, "b": i_no_tz})
         expected = DataFrame(
-            {"a": i.to_series(keep_tz=True).reset_index(drop=True), "b": i_no_tz}
+            {"a": i.to_series().reset_index(drop=True), "b": i_no_tz}
         )
         tm.assert_frame_equal(df, expected)
 
