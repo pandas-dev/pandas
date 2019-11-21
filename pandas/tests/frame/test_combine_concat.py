7307e1bb5960de18d78267eff12bdd4e90a80655
iamrajhans <rajhans.jadhao@gmail.com>
2019-11-21 10:15:26 +0530

    fix test case

--- pandas/tests/frame/test_combine_concat.py
+++ pandas/tests/frame/test_combine_concat.py
@@ -945,10 +945,9 @@ class TestDataFrameCombineFirst:
         exp = pd.DataFrame(columns=["a", "b"])
         res = df1.combine_first(df2)
 
-        exp_rev = pd.DataFrame(columns=["b", "a"])
         res_rev = df2.combine_first(df1)
         tm.assert_frame_equal(res, exp)
-        tm.assert_frame_equal(res_rev, exp_rev)
+        tm.assert_frame_equal(res_rev, exp)
 
 
 class TestDataFrameUpdate:
