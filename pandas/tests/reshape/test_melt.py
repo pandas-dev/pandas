212dc7f8b6e4a59230cd44393f14ac968bcfe481
Jeremy Schendel <jschendel@users.noreply.github.com>
2019-11-22 09:20:09 -0700

    unnecessary

--- pandas/tests/reshape/test_melt.py
+++ pandas/tests/reshape/test_melt.py
@@ -289,36 +289,33 @@ class TestMelt:
         df = pd.DataFrame(np.random.randn(5, 4), columns=list("abcd"))
 
         # Try to melt with missing `value_vars` column name
-        msg = r"The following '{var}' are not present in the DataFrame: \[{col}\]"
-        with pytest.raises(KeyError, match=msg.format(var="value_vars", col="'C'")):
+        msg = "The following '{Var}' are not present in the DataFrame: {Col}"
+        with pytest.raises(
+            KeyError, match=msg.format(Var="value_vars", Col="\\['C'\\]")
+        ):
             df.melt(["a", "b"], ["C", "d"])
 
         # Try to melt with missing `id_vars` column name
-        with pytest.raises(KeyError, match=msg.format(var="id_vars", col="'A'")):
+        with pytest.raises(KeyError, match=msg.format(Var="id_vars", Col="\\['A'\\]")):
             df.melt(["A", "b"], ["c", "d"])
 
         # Multiple missing
         with pytest.raises(
-            KeyError, match=msg.format(var="id_vars", col="'not_here', 'or_there'"),
+            KeyError,
+            match=msg.format(Var="id_vars", Col="\\['not_here', 'or_there'\\]"),
         ):
             df.melt(["a", "b", "not_here", "or_there"], ["c", "d"])
 
         # Multiindex melt fails if column is missing from multilevel melt
         multi = df.copy()
         multi.columns = [list("ABCD"), list("abcd")]
-        with pytest.raises(KeyError, match=msg.format(var="id_vars", col="'E'")):
+        with pytest.raises(KeyError, match=msg.format(Var="id_vars", Col="\\['E'\\]")):
             multi.melt([("E", "a")], [("B", "b")])
         # Multiindex fails if column is missing from single level melt
-        with pytest.raises(KeyError, match=msg.format(var="value_vars", col="'F'")):
+        with pytest.raises(
+            KeyError, match=msg.format(Var="value_vars", Col="\\['F'\\]")
+        ):
             multi.melt(["A"], ["F"], col_level=0)
-
-        # GH 29718: mixed int/str
-        df_mixed = DataFrame(columns=[0, "a"])
-        with pytest.raises(KeyError, match=msg.format(var="id_vars", col="'0'")):
-            df_mixed.melt(id_vars=["0", "a"])
-
-        with pytest.raises(KeyError, match=msg.format(var="value_vars", col="'0'")):
-            df_mixed.melt(value_vars=["0", "a"])
 
     def test_melt_mixed_int_str_id_vars(self):
         # GH 29718
