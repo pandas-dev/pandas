7eb0db32182f7026292188eac8154bbf715746a6
jbrockmendel <jbrockmendel@gmail.com>
2019-11-25 15:45:27 -0800

    BUG: Index.get_loc raising incorrect error, closes #29189 (#29700)

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -1942,6 +1942,16 @@ def test_groupby_only_none_group():
     tm.assert_series_equal(actual, expected)
 
 
+def test_groupby_duplicate_index():
+    # GH#29189 the groupby call here used to raise
+    ser = pd.Series([2, 5, 6, 8], index=[2.0, 4.0, 4.0, 5.0])
+    gb = ser.groupby(level=0)
+
+    result = gb.mean()
+    expected = pd.Series([2, 5.5, 8], index=[2.0, 4.0, 5.0])
+    tm.assert_series_equal(result, expected)
+
+
 @pytest.mark.parametrize("bool_agg_func", ["any", "all"])
 def test_bool_aggs_dup_column_labels(bool_agg_func):
     # 21668
