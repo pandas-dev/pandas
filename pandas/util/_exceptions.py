f70d997296f39e6eac73be7b6ff59a4ba3c68cfd
Simon Hawkins <simonjayhawkins@gmail.com>
2019-11-27 14:39:14 +0000

    TYP: some types for pandas/util/_exceptions.py (#29881)

--- pandas/util/_exceptions.py
+++ pandas/util/_exceptions.py
@@ -1,15 +1,16 @@
 import contextlib
+from typing import Tuple
 
 
 @contextlib.contextmanager
-def rewrite_exception(old_name, new_name):
+def rewrite_exception(old_name: str, new_name: str):
     """Rewrite the message of an exception."""
     try:
         yield
     except Exception as err:
         msg = err.args[0]
         msg = msg.replace(old_name, new_name)
-        args = (msg,)
+        args: Tuple[str, ...] = (msg,)
         if len(err.args) > 1:
             args = args + err.args[1:]
         err.args = args
