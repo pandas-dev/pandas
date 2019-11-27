cffa9a31dd245a695545e66369ed4d91f20ef5ff
MomIsBestFriend <50263213+MomIsBestFriend@users.noreply.github.com>
2019-11-28 01:30:22 +0200

    Fixes for @jbrockmendel review

--- pandas/io/clipboard/__init__.py
+++ pandas/io/clipboard/__init__.py
@@ -95,7 +95,7 @@ def _stringifyText(text) -> str:
     acceptedTypes = (str, int, float, bool)
     if not isinstance(text, acceptedTypes):
         raise PyperclipException(
-            f"only str, int, float, and bool values"
+            f"only str, int, float, and bool values "
             f"can be copied to the clipboard, not {type(text).__name__}"
         )
     return str(text)
