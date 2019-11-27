97b4a6158b81e8c76cfa0863d8bc7ab5efa2b896
jbrockmendel <jbrockmendel@gmail.com>
2019-11-27 11:02:19 -0800

    CLN: follow-up to 29725 (#29890)

--- pandas/tests/test_strings.py
+++ pandas/tests/test_strings.py
@@ -666,7 +666,7 @@ class TestStringMethods:
 
     index_or_series2 = [Series, Index]  # type: ignore
     # List item 0 has incompatible type "Type[Series]"; expected "Type[PandasObject]"
-    # See GH#>????
+    # See GH#29725
 
     @pytest.mark.parametrize("other", index_or_series2)
     def test_str_cat_all_na(self, index_or_series, other):
