d338b94fbb3f347a2e37baf3553d29646e5b1862
jbrockmendel <jbrockmendel@gmail.com>
2019-11-27 10:14:18 -0800

    mypy fix (#29891)

--- pandas/io/pytables.py
+++ pandas/io/pytables.py
@@ -2641,7 +2641,9 @@ class Fixed:
             "cannot write on an abstract storer: sublcasses should implement"
         )
 
-    def delete(self, where=None, start=None, stop=None, **kwargs):
+    def delete(
+        self, where=None, start: Optional[int] = None, stop: Optional[int] = None
+    ):
         """
         support fully deleting the node in its entirety (only) - where
         specification must be None
