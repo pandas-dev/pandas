36b171b2ff1f01563367c13313572d257d7d67fc
Tom Augspurger <tom.w.augspurger@gmail.com>
2019-11-27 09:47:06 -0600

    added tests for empty

--- pandas/tests/arrays/test_boolean.py
+++ pandas/tests/arrays/test_boolean.py
@@ -360,6 +360,19 @@ class TestLogicalOps(BaseOpsUtil):
 
         return op
 
+    def test_empty_ok(self, all_logical_operators):
+        a = pd.array([], dtype="boolean")
+        op_name = all_logical_operators
+        result = getattr(a, op_name)(True)
+        tm.assert_extension_array_equal(a, result)
+
+        result = getattr(a, op_name)(False)
+        tm.assert_extension_array_equal(a, result)
+
+        # TODO: pd.NA
+        # result = getattr(a, op_name)(pd.NA)
+        # tm.assert_extension_array_equal(a, result)
+
     def test_logical_length_mismatch_raises(self, all_logical_operators):
         op_name = all_logical_operators
         a = pd.array([True, False, None], dtype="boolean")
