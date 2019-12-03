5bbccc7d98909466cb6d8e151270119c27e1e5a8
Max Chen <immaxchen@gmail.com>
2019-12-04 06:36:36 +0800

    BUG: fix Series.interpolate(method='index') with unsorted index (#29943)
    * fix series interpolate bug with unsorted index GH21037

--- pandas/tests/series/test_missing.py
+++ pandas/tests/series/test_missing.py
@@ -1649,3 +1649,14 @@ class TestSeriesInterpolateData:
             pytest.skip(
                 "This interpolation method is not supported for Timedelta Index yet."
             )
+
+    @pytest.mark.parametrize(
+        "ascending, expected_values",
+        [(True, [1, 2, 3, 9, 10]), (False, [10, 9, 3, 2, 1])],
+    )
+    def test_interpolate_unsorted_index(self, ascending, expected_values):
+        # GH 21037
+        ts = pd.Series(data=[10, 9, np.nan, 2, 1], index=[10, 9, 3, 2, 1])
+        result = ts.sort_index(ascending=ascending).interpolate(method="index")
+        expected = pd.Series(data=expected_values, index=expected_values, dtype=float)
+        tm.assert_series_equal(result, expected)
