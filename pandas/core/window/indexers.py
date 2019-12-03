5d8819f00d6cd85a6aae42299ad5722c31fd6489
Matt Roeschke <mroeschke@housecanary.com>
2019-12-02 23:09:24 -0800

    Change self.__dict__(kwargs) to more explicit setattr

--- pandas/core/window/indexers.py
+++ pandas/core/window/indexers.py
@@ -41,11 +41,13 @@ class BaseIndexer:
         Parameters
         ----------
         **kwargs :
-            keyword argument that will be available when get_window_bounds is called
+            keyword arguments that will be available when get_window_bounds is called
         """
         self.index_array = index_array
         self.window_size = window_size
-        self.__dict__.update(kwargs)
+        # Set user defined kwargs as attributes that can be used in get_window_bounds
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     @Appender(get_window_bounds_doc)
     def get_window_bounds(
