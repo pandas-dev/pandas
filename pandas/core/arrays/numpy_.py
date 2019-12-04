5d55a253beaba98c38ff2ceab7e4e429ac7dd336
Simon Hawkins <simonjayhawkins@gmail.com>
2019-12-04 20:06:56 +0000

    remove variable annotations from pandas/core/arrays/numpy_.py

--- pandas/core/arrays/numpy_.py
+++ pandas/core/arrays/numpy_.py
@@ -139,8 +139,8 @@ class PandasArray(ExtensionArray, ExtensionOpsMixin, NDArrayOperatorsMixin):
         if copy:
             values = values.copy()
 
-        self._ndarray: np.ndarray = values
-        self._dtype: ExtensionDtype = PandasDtype(values.dtype)
+        self._ndarray = values
+        self._dtype = PandasDtype(values.dtype)
 
     @classmethod
     def _from_sequence(cls, scalars, dtype=None, copy=False):
