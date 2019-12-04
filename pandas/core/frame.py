5e7d91557bb980781708e03ce093757d56fd3035
Michael Kakehashi <mkakehashi@icsecurity.com>
2019-12-03 21:06:41 -0800

    more typing syntax fixes

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -4114,7 +4114,7 @@ class DataFrame(NDFrame):
                     found = col in self.columns
                 except TypeError:
                     raise TypeError(
-                       f"{err_msg} Received column of type {type(col)}"
+                        f"{err_msg} Received column of type {type(col)}"
                         "array, or a list containing only valid column keys and "
                         f"one-dimensional arrays. Received column of type {type(col)}"
                     )
@@ -4592,7 +4592,7 @@ class DataFrame(NDFrame):
 
     def drop_duplicates(
         self,
-        subset: Optional[Union[Hashable, Sequence[Hashable]] = None,
+        subset: Optional[Union[Hashable, Sequence[Hashable]]] = None,
         keep: Union[str, bool] = "first",
         inplace: bool = False,
     ) -> "DataFrame":
@@ -4634,7 +4634,7 @@ class DataFrame(NDFrame):
 
     def duplicated(
         self,
-        subset: Optional[Union[Hashable, Sequence[Hashable]] = None,
+        subset: Optional[Union[Hashable, Sequence[Hashable]]] = None,
         keep: Union[str, bool] = "first",
     ) -> "Series":
         """
