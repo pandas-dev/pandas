2620764f0314ac89aa35f937ab2a0b7a7e87ee47
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2019-12-04 10:41:34 -0600

    CI: disable numpydev job (#30049)
    * CI: disable numpydev job
    
    xref https://github.com/pandas-dev/pandas/issues/30043.
    Will take some time to fix all these cases.
    
    * mypy fixup

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -9133,7 +9133,7 @@ class NDFrame(PandasObject, SelectionMixin):
             if freq == orig_freq:
                 new_data = self._data.copy()
                 new_data.axes[block_axis] = index.shift(periods)
-            else:
+            elif orig_freq is not None:
                 msg = (
                     f"Given freq {freq.rule_code} does not match"
                     f" PeriodIndex freq {orig_freq.rule_code}"
