997a2e33ed695493bc3d5f32537f245a73cc423c
Michael Kakehashi <mkakehashi@icsecurity.com>
2019-12-04 09:27:34 -0800

    fixed fstring with err_msg

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -4114,9 +4114,7 @@ class DataFrame(NDFrame):
                     found = col in self.columns
                 except TypeError:
                     raise TypeError(
-                        f"{err_msg} Received column of type {type(col)}"
-                        "array, or a list containing only valid column keys and "
-                        f"one-dimensional arrays. Received column of type {type(col)}"
+                        f"{err_msg}. Received column of type {type(col)}"
                     )
                 else:
                     if not found:
