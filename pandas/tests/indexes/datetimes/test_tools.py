4d6e0527ea4b9f8c02a54d38d7a324e7da983851
maheshbapatu <maheshreddy.bapatu@nutanix.com>
2019-12-05 16:29:02 +0000

    This avoids failure to_datetime when value passed is None. This fix is for pandas versions > 0.24 Closes #30011

--- pandas/tests/indexes/datetimes/test_tools.py
+++ pandas/tests/indexes/datetimes/test_tools.py
@@ -102,6 +102,24 @@ class TestTimeConversionFormats:
         tm.assert_series_equal(result, expected)
 
     @pytest.mark.parametrize(
+        "input_s",
+        [
+            # None with Strings
+            Series(["19801222", None, "20010112"]),
+            # None with Integers
+            Series([19801222, None, 20010112])
+        ],
+    )
+    def test_to_datetime_format_YYYYMMDD_with_none(self,input_s):
+        # GH 30011
+        # format='%Y%m%d'
+        # with None
+        expected = Series([Timestamp("19801222"), Timestamp(None),Timestamp("20010112")])
+
+        result = pd.to_datetime(input_s, format="%Y%m%d")
+        tm.assert_series_equal(result, expected)
+
+    @pytest.mark.parametrize(
         "input_s, expected",
         [
             # NaN before strings with invalid date values
