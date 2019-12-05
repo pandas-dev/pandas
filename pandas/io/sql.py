248133af343801e845f1687d28e17da8f3c8bc8b
Blake Hawkins <hawkinsba@hendrix.edu>
2019-12-04 20:51:29 -0600

    Fixing PR

--- pandas/io/sql.py
+++ pandas/io/sql.py
@@ -359,17 +359,9 @@ def read_sql(
         SQL query to be executed or a table name.
     con : SQLAlchemy connectable (engine/connection) or database str URI
         or DBAPI2 connection (fallback mode)
-
         Using SQLAlchemy makes it possible to use any DB supported by that
-<<<<<<< HEAD
         library. If a DBAPI2 object, only sqlite3 is supported.
     index_col : str or list of strings, optional, default: None
-=======
-        library. If a DBAPI2 object, only sqlite3 is supported. The user is responsible
-        for engine disposal and connection closure for the SQLAlchemy connectable. See
-        `here <https://docs.sqlalchemy.org/en/13/core/connections.html>`_
-    index_col : string or list of strings, optional, default: None
->>>>>>> upstream/master
         Column(s) to set as index(MultiIndex).
     coerce_float : bool, default True
         Attempts to convert values of non-string, non-numeric objects (like
