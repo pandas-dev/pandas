a42ba49efe59eb58c269abb30903b64241e581a8
jvpy <jvmartinellis@gmail.com>
2019-12-08 00:41:33 -0300

    Readded repr(foo) to parsers.py

--- pandas/io/parsers.py
+++ pandas/io/parsers.py
@@ -913,8 +913,8 @@ class TextFileReader(BaseIterator):
                         pass
                     else:
                         raise ValueError(
-                            f"The {argname} option is not supported with the"
-                            f" {engine} engine"
+                            f"The {repr(argname)} option is not supported with the"
+                            f" {repr(engine)} engine"
                         )
             else:
                 value = _deprecated_defaults.get(argname, default)
