c9649ed46f5b364b839b4b982135504ae40bee5a
Nicolas Lepleux <nlepleux@srv004260.fr.cfm.fr>
2019-12-13 09:23:59 +0100

    Suggested changes for pull request. I still need to merge with master for CI issues

--- pandas/core/groupby/grouper.py
+++ pandas/core/groupby/grouper.py
@@ -491,10 +491,9 @@ def get_grouper(
                     raise ValueError("multiple levels only valid with MultiIndex")
 
             if isinstance(level, str):
-                if obj._get_axis(axis) != level:
-                if getattr(obj, axis_name).name != level:
+                if obj._get_axis(axis).name != level:
                     raise ValueError(
-                        f"level name {level} is not the name of the {axis_name}"
+                        f"level name {level} is not the name of the {obj._get_axis_name(axis)}"
                     )
             elif level > 0 or level < -1:
                 raise ValueError("level > 0 or level < -1 only valid with MultiIndex")
--- pandas/tests/groupby/test_grouping.py
+++ pandas/tests/groupby/test_grouping.py
@@ -501,13 +501,12 @@ class TestGrouping:
         with pytest.raises(ValueError, match=msg):
             df.groupby(level=1)
 
-    @pytest.mark.parametrize("axis", [0, 1])
     def test_groupby_level_index_names(self, axis):
         # GH4014 this used to raise ValueError since 'exp'>1 (in py2)
         df = DataFrame({"exp": ["A"] * 3 + ["B"] * 3, "var1": range(6)}).set_index(
             "exp"
         )
-        if axis:
+        if axis in (1, "columns"):
             df = df.T
         df.groupby(level="exp", axis=axis)
         msg = f"level name foo is not the name of the {df._get_axis_name(axis)}"
