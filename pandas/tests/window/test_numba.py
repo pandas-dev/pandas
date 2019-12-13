6da8199f446ea0590c4596f746983e7a1da97277
Matt Roeschke <mroeschke@housecanary.com>
2019-12-12 22:47:39 -0800

    Add numba/cython comparison test

---
+++ pandas/tests/window/test_numba.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pytest
+
+import pandas.util._test_decorators as td
+
+from pandas import Series
+import pandas.util.testing as tm
+
+
+@td.skip_if_no("numba", "0.46.0")
+class TestApply:
+    @pytest.mark.parametrize("nogil", [True, False])
+    @pytest.mark.parametrize("parallel", [True, False])
+    @pytest.mark.parametrize("nopython", [True, False])
+    def test_numba_vs_cython(self, nogil, parallel, nopython):
+        def f(x, *args):
+            return np.sqrt(x) + np.sum(args) + 1
+
+        engine_kwargs = {"nogil": nogil, "parallel": parallel, "nopython": nopython}
+        args = (2,)
+
+        s = Series(range(10))
+        result = s.rolling(2).apply(
+            f, args=args, engine="numba", engine_kwargs=engine_kwargs, raw=True
+        )
+        expected = s.rolling(2).apply(f, engine="cython", args=args, raw=True)
+        tm.assert_series_equal(result, expected)
