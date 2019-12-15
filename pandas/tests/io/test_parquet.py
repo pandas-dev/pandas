0c3f6518a88eac122571bc6ec810993a5c5030a1
Ian Eaves <ian.k.eaves@gmail.com>
2019-12-14 19:58:50 -0600

    black

--- pandas/tests/io/test_parquet.py
+++ pandas/tests/io/test_parquet.py
@@ -506,10 +506,11 @@ class TestParquetPyArrow(Base):
 
     def test_write_with_schema(self, pa):
         import pyarrow
-        df = pd.DataFrame({'x': [0, 1]})
-        schema = pyarrow.schema([pyarrow.field('x', type=pyarrow.bool_())])
+
+        df = pd.DataFrame({"x": [0, 1]})
+        schema = pyarrow.schema([pyarrow.field("x", type=pyarrow.bool_())])
         out_df = df.astype(bool)
-        check_round_trip(df, pa, write_kwargs={'schema': schema}, expected=out_df)
+        check_round_trip(df, pa, write_kwargs={"schema": schema}, expected=out_df)
 
     @pytest.mark.skip(reason="broken test")
     @td.skip_if_no("pyarrow", min_version="0.15.0")
