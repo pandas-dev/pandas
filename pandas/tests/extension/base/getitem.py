158523b0435d190bda3c39cf68c232fefc5abf21
jbrockmendel <jbrockmendel@gmail.com>
2019-12-18 11:31:18 -0800

    TST: follow-up ot #30318 (#30326)

--- pandas/tests/extension/base/getitem.py
+++ pandas/tests/extension/base/getitem.py
@@ -272,3 +272,10 @@ class BaseGetitemTests(BaseExtensionTests):
         s = pd.Series(data)
         result = s[:1].item()
         assert result == data[0]
+
+        msg = "can only convert an array of size 1 to a Python scalar"
+        with pytest.raises(ValueError, match=msg):
+            s[:0].item()
+
+        with pytest.raises(ValueError, match=msg):
+            s.item()
