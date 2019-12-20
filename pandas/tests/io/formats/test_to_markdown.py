ee07c6832c886fff9786ee56d95cfe8036c96ef0
Marco Gorelli <m.gorelli@ws-1808.seri.co.uk>
2019-12-20 10:40:12 +0000

    :white_check_mark: update tests so they work with **kwargs, set skip_if_no_tabulate to whole class

--- pandas/tests/io/formats/test_to_markdown.py
+++ pandas/tests/io/formats/test_to_markdown.py
@@ -4,9 +4,16 @@ import pandas as pd
 
 
 @td.skip_if_no_tabulate
-def test_to_markdown():
-    df = pd.DataFrame([1, 2, 3])
-    result = df.to_markdown()
-    assert (
-        result == "|    |   0 |\n|---:|----:|\n|  0 |   1 |\n|  1 |   2 |\n|  2 |   3 |"
-    )
+class TestToMarkdown:
+    def test_to_markdown(self):
+        df = pd.DataFrame([1, 2, 3])
+        result = df.to_markdown()
+        assert (
+            result
+            == "|    |   0 |\n|---:|----:|\n|  0 |   1 |\n|  1 |   2 |\n|  2 |   3 |"
+        )
+
+    def test_to_markdown_other_tablefmt(self):
+        df = pd.DataFrame([1, 2, 3])
+        result = df.to_markdown(tablefmt="jira")
+        assert result == "||    ||   0 ||\n|  0 |   1 |\n|  1 |   2 |\n|  2 |   3 |"
