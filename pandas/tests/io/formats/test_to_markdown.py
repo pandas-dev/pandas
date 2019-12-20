68e84d6dc56c261d8b17f8b72a3124b36da03311
Marco Gorelli <m.gorelli@ws-1808.seri.co.uk>
2019-12-20 15:20:04 +0000

    :white_check_mark: update tests so they use buf

--- pandas/tests/io/formats/test_to_markdown.py
+++ pandas/tests/io/formats/test_to_markdown.py
@@ -1,35 +1,54 @@
-import pandas.util._test_decorators as td
+from io import StringIO
+
+import pytest
 
 import pandas as pd
 
+pytest.importorskip("tabulate")
 
-@td.skip_if_no_tabulate
-class TestToMarkdown:
-    def test_simple(self):
-        df = pd.DataFrame([1, 2, 3])
-        result = df.to_markdown()
-        assert (
-            result
-            == "|    |   0 |\n|---:|----:|\n|  0 |   1 |\n|  1 |   2 |\n|  2 |   3 |"
-        )
 
-    def test_other_tablefmt(self):
-        df = pd.DataFrame([1, 2, 3])
-        result = df.to_markdown(tablefmt="jira")
-        assert result == "||    ||   0 ||\n|  0 |   1 |\n|  1 |   2 |\n|  2 |   3 |"
+def test_simple():
+    buf = StringIO()
+    df = pd.DataFrame([1, 2, 3])
+    df.to_markdown(buf=buf)
+    result = buf.getvalue()
+    assert (
+        result == "|    |   0 |\n|---:|----:|\n|  0 |   1 |\n|  1 |   2 |\n|  2 |   3 |"
+    )
 
-    def test_other_headers(self):
-        df = pd.DataFrame([1, 2, 3])
-        result = df.to_markdown(headers=["foo", "bar"])
-        assert result == (
-            "|   foo |   bar |\n|------:|------:|\n|     0 "
-            "|     1 |\n|     1 |     2 |\n|     2 |     3 |"
-        )
 
-    def test_series(self):
-        s = pd.Series([1, 2, 3], name="foo")
-        result = s.to_markdown()
-        assert result == (
-            "|    |   foo |\n|---:|------:|\n|  0 |     1 "
-            "|\n|  1 |     2 |\n|  2 |     3 |"
-        )
+def test_other_tablefmt():
+    buf = StringIO()
+    df = pd.DataFrame([1, 2, 3])
+    df.to_markdown(buf=buf, tablefmt="jira")
+    result = buf.getvalue()
+    assert result == "||    ||   0 ||\n|  0 |   1 |\n|  1 |   2 |\n|  2 |   3 |"
+
+
+def test_other_headers():
+    buf = StringIO()
+    df = pd.DataFrame([1, 2, 3])
+    df.to_markdown(buf=buf, headers=["foo", "bar"])
+    result = buf.getvalue()
+    assert result == (
+        "|   foo |   bar |\n|------:|------:|\n|     0 "
+        "|     1 |\n|     1 |     2 |\n|     2 |     3 |"
+    )
+
+
+def test_series():
+    buf = StringIO()
+    s = pd.Series([1, 2, 3], name="foo")
+    s.to_markdown(buf=buf)
+    result = buf.getvalue()
+    assert result == (
+        "|    |   foo |\n|---:|------:|\n|  0 |     1 "
+        "|\n|  1 |     2 |\n|  2 |     3 |"
+    )
+
+
+def test_no_buf(capsys):
+    df = pd.DataFrame([1, 2, 3])
+    df.to_markdown()
+    out, _ = capsys.readouterr()
+    assert out == "|    |   0 |\n|---:|----:|\n|  0 |   1 |\n|  1 |   2 |\n|  2 |   3 |"
