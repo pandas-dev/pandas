b4f46a366d0079099c290b41d8d083d564249d51
jbrockmendel <jbrockmendel@gmail.com>
2019-12-23 00:49:26 -0800

    CI: transition powershell-> bash (#30393)

--- ci/incremental/build.cmd
+++
@@ -1,9 +0,0 @@
-@rem https://github.com/numba/numba/blob/master/buildscripts/incremental/build.cmd
-
-@rem Build extensions
-python setup.py build_ext -q -i
-
-@rem Install pandas
-python -m pip install --no-build-isolation -e .
-
-if %errorlevel% neq 0 exit /b %errorlevel%
