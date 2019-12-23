4668351c19788584c4b0cfba0e5fc7f6d7482703
jbrockmendel <jbrockmendel@gmail.com>
2019-12-23 10:06:13 -0800

    move misplaced sort_index test

--- pandas/tests/series/methods/test_sort_index.py
+++ pandas/tests/series/methods/test_sort_index.py
@@ -62,6 +62,23 @@ class TestSeriesSortIndex:
         assert result is None
         tm.assert_series_equal(random_order, datetime_series)
 
+    def test_sort_index_level(self):
+        mi = MultiIndex.from_tuples([[1, 1, 3], [1, 1, 1]], names=list("ABC"))
+        s = Series([1, 2], mi)
+        backwards = s.iloc[[1, 0]]
+
+        res = s.sort_index(level="A")
+        tm.assert_series_equal(backwards, res)
+
+        res = s.sort_index(level=["A", "B"])
+        tm.assert_series_equal(backwards, res)
+
+        res = s.sort_index(level="A", sort_remaining=False)
+        tm.assert_series_equal(s, res)
+
+        res = s.sort_index(level=["A", "B"], sort_remaining=False)
+        tm.assert_series_equal(s, res)
+
     @pytest.mark.parametrize("level", ["A", 0])  # GH#21052
     def test_sort_index_multiindex(self, level):
 
--- pandas/tests/series/test_analytics.py
+++ pandas/tests/series/test_analytics.py
@@ -478,23 +478,6 @@ class TestSeriesAnalytics:
         assert s.is_monotonic is False
         assert s.is_monotonic_decreasing is True
 
-    def test_sort_index_level(self):
-        mi = MultiIndex.from_tuples([[1, 1, 3], [1, 1, 1]], names=list("ABC"))
-        s = Series([1, 2], mi)
-        backwards = s.iloc[[1, 0]]
-
-        res = s.sort_index(level="A")
-        tm.assert_series_equal(backwards, res)
-
-        res = s.sort_index(level=["A", "B"])
-        tm.assert_series_equal(backwards, res)
-
-        res = s.sort_index(level="A", sort_remaining=False)
-        tm.assert_series_equal(s, res)
-
-        res = s.sort_index(level=["A", "B"], sort_remaining=False)
-        tm.assert_series_equal(s, res)
-
     def test_apply_categorical(self):
         values = pd.Categorical(list("ABBABCD"), categories=list("DCBA"), ordered=True)
         s = pd.Series(values, name="XX", index=list("abcdefg"))
