5d8d35f9a918a5a37e6f035201b242887a407748
hasnain2808 <hasnain2808@gmail.com>
2019-12-24 13:21:51 +0530

    use numpy.array_equal instead of ==

--- pandas/tests/series/test_analytics.py
+++ pandas/tests/series/test_analytics.py
@@ -859,7 +859,6 @@ class TestSeriesAnalytics:
         ser = Series(arr)
         ptp_np = np.ptp(arr)
         ptp_pd = np.ptp(ser)
-
         assert np.array_equal(ptp_pd, ptp_np)
 
     def test_repeat(self):
