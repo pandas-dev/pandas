7ab73a93c94e6557e5d77b2966549955e5c51cbc
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2019-12-27 16:58:05 +0000

    [ENH] Add to_markdown method (#30350)

--- pandas/compat/_optional.py
+++ pandas/compat/_optional.py
@@ -23,6 +23,7 @@ VERSIONS = {
     "scipy": "0.19.0",
     "sqlalchemy": "1.1.4",
     "tables": "3.4.2",
+    "tabulate": "0.8.3",
     "xarray": "0.8.2",
     "xlrd": "1.1.0",
     "xlwt": "1.2.0",
