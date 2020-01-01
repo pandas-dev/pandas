eb9a6f71ca2eca0ce0df3bbbccf632ea581c17d0
Kaiqi <kaiqi@kth.se>
2020-01-01 12:05:14 +0100

    Add level example for series groupby

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -2105,10 +2105,31 @@ def test_groupby_dropna_normal_index_dataframe(dropna, idx, outputs):
         ),
     ],
 )
-def test_groupby_dropna_series(dropna, idx, expected):
+def test_groupby_dropna_series_level(dropna, idx, expected):
     ser = pd.Series([1, 2, 3, 3], index=idx)
 
     result = ser.groupby(level=0, dropna=dropna).sum()
+    tm.assert_series_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "dropna, expected",
+    [
+        (True, pd.Series([210.0, 350.0], index=["a", "b"], name="Max Speed")),
+        (
+            False,
+            pd.Series([210.0, 350.0, 20.0], index=["a", "b", np.nan], name="Max Speed"),
+        ),
+    ],
+)
+def test_groupby_dropna_series_by(dropna, expected):
+    ser = pd.Series(
+        [390.0, 350.0, 30.0, 20.0],
+        index=["Falcon", "Falcon", "Parrot", "Parrot"],
+        name="Max Speed",
+    )
+
+    result = ser.groupby(["a", "b", "a", np.nan], dropna=dropna).mean()
     tm.assert_series_equal(result, expected)
 
 
