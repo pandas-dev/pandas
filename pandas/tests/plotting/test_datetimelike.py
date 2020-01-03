1a9b188097e3e094af306fc73621f71ad650207f
Nico Schlömer <nico.schloemer@gmail.com>
2020-01-03 09:28:50 +0100

    ignore warning

--- pandas/tests/plotting/test_datetimelike.py
+++ pandas/tests/plotting/test_datetimelike.py
@@ -1,12 +1,10 @@
 """ Test cases for time series specific (freq conversion, etc) """
-from datetime import date, datetime, time, timedelta, timezone
+from datetime import date, datetime, time, timedelta
 import pickle
 import sys
 
-import dateutil
 import numpy as np
 import pytest
-import pytz
 
 import pandas.util._test_decorators as td
 
@@ -45,21 +43,19 @@ class TestTSPlot(TestPlotBase):
     def teardown_method(self, method):
         tm.close()
 
+    # Ignore warning
+    # ```
+    # Converting to PeriodArray/Index representation will drop timezone information.
+    # ```
+    # which occurs for UTC-like timezones.
     @pytest.mark.slow
+    @pytest.mark.filterwarnings("ignore:msg:UserWarning")
     def test_ts_plot_with_tz(self, tz_aware_fixture):
         # GH2877, GH17173
         tz = tz_aware_fixture
         index = date_range("1/1/2011", periods=2, freq="H", tz=tz)
         ts = Series([188.5, 328.25], index=index)
-        if tz in ["UTC", pytz.UTC, timezone.utc] or isinstance(
-            tz, dateutil.tz.tz.tzutc
-        ):
-            # Converting to PeriodArray/Index representation will drop timezone
-            # information.
-            with tm.assert_produces_warning(UserWarning):
-                _check_plot_works(ts.plot)
-        else:
-            _check_plot_works(ts.plot)
+        _check_plot_works(ts.plot)
 
     def test_fontsize_set_correctly(self):
         # For issue #8765
