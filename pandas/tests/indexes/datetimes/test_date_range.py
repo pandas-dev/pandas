9a6959fdc5bf5d100613de69a1162be0b0f151b4
fujiaxiang <fujiaxiang1995@gmail.com>
2020-01-04 13:59:50 +0800

    BUG: bug in date_range with custom business hours and given periods (GH30593)

--- pandas/tests/indexes/datetimes/test_date_range.py
+++ pandas/tests/indexes/datetimes/test_date_range.py
@@ -945,3 +945,19 @@ class TestCustomDateRange:
         result = pd.date_range(start=start, end=end, periods=2, closed="left")
         expected = DatetimeIndex([start])
         tm.assert_index_equal(result, expected)
+
+
+def test_date_range_with_custom_holidays():
+    # GH 30593
+    freq = pd.offsets.CustomBusinessHour(start="15:00", holidays=["2020-11-26"])
+    result = pd.date_range(start="2020-11-25 15:00", periods=4, freq=freq)
+    expected = pd.DatetimeIndex(
+        [
+            "2020-11-25 15:00:00",
+            "2020-11-25 16:00:00",
+            "2020-11-27 15:00:00",
+            "2020-11-27 16:00:00",
+        ],
+        freq=freq,
+    )
+    tm.assert_index_equal(result, expected)
