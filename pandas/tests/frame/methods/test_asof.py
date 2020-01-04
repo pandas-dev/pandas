7c533a1734731d6f72dbb6c44d1fb464a2243e0c
Ryan Nazareth <ryankarlos@gmail.com>
2020-01-04 05:10:03 +0000

    fix merge conflict and linting

--- pandas/tests/frame/methods/test_asof.py
+++ pandas/tests/frame/methods/test_asof.py
@@ -1,15 +1,10 @@
+import warnings
+
 import numpy as np
 import pytest
 
 from pandas import DataFrame, Series, Timestamp, date_range, to_datetime
-
 import pandas._testing as tm
-
-import pandas.util.testing as tm
-import warnings
-
-warnings.simplefilter("error")
-
 
 
 @pytest.fixture
