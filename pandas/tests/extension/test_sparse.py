627251b7c21d10d2cf9a2d3c50f9f8de115ddb77
Irv Lustig <irv@princeton.com>
2020-01-04 14:08:30 -0500

    fix extension/test_sparse.py

--- pandas/tests/extension/test_sparse.py
+++ pandas/tests/extension/test_sparse.py
@@ -232,7 +232,7 @@ class TestMethods(BaseSparseTests, base.BaseMethodsTests):
         s2 = pd.Series(orig_data2)
         result = s1.combine(s2, lambda x1, x2: x1 <= x2)
         expected = pd.Series(
-            pd.arrays.SparseArray(
+            SparseArray(
                 [a <= b for (a, b) in zip(list(orig_data1), list(orig_data2))],
                 fill_value=False,
             )
@@ -242,9 +242,7 @@ class TestMethods(BaseSparseTests, base.BaseMethodsTests):
         val = s1.iloc[0]
         result = s1.combine(val, lambda x1, x2: x1 <= x2)
         expected = pd.Series(
-            pd.arrays.SparseArray(
-                [a <= val for a in list(orig_data1)], fill_value=False
-            )
+            SparseArray([a <= val for a in list(orig_data1)], fill_value=False)
         )
         self.assert_series_equal(result, expected)
 
@@ -349,7 +347,7 @@ class TestComparisonOps(BaseSparseTests, base.BaseComparisonOpsTests):
 
         with np.errstate(all="ignore"):
             expected = pd.Series(
-                pd.arrays.SparseArray(
+                SparseArray(
                     op(np.asarray(data), np.asarray(other)),
                     fill_value=result.values.fill_value,
                 )
