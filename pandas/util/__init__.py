cc9e18df220349e8bd296e5102b098e92794c40b
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-01-06 13:27:16 -0600

    revert

--- pandas/util/__init__.py
+++ pandas/util/__init__.py
@@ -1,11 +1,3 @@
-from pandas.util._decorators import Appender, Substitution, cache_readonly
+from pandas.util._decorators import Appender, Substitution, cache_readonly  # noqa
 
-from pandas.core.util.hashing import hash_array, hash_pandas_object
-
-__all__ = [
-    "Appender",
-    "Substitution",
-    "cache_readonly",
-    "hash_array",
-    "hash_pandas_object",
-]
+from pandas.core.util.hashing import hash_array, hash_pandas_object  # noqa
