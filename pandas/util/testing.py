bbcd6e28323d59fea80a95f06b0681bb07fdb743
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-01-06 13:26:19 -0600

    simplify

--- pandas/util/testing.py
+++ pandas/util/testing.py
@@ -2,4 +2,11 @@ import warnings
 
 from pandas._testing import *  # noqa
 
-warnings.warn("pandas.util.testing is deprecated", FutureWarning)
+warnings.warn(
+    (
+        "pandas.util.testing is deprecated. Use the functions in the "
+        "public API at pandas.testing instead."
+    ),
+    FutureWarning,
+    stacklevel=2,
+)
