cea68530e453d4b98e14ff506550feaa77ec718d
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-01-07 22:27:06 +0100

    use pandas.util.testing for back compat (#30779)

--- asv_bench/benchmarks/reindex.py
+++ asv_bench/benchmarks/reindex.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 from pandas import DataFrame, Index, MultiIndex, Series, date_range, period_range
-import pandas._testing as tm
+import pandas.util.testing as tm
 
 from .pandas_vb_common import lib
 
