2e6f53b63bbf79d66a3162529c5f910ed401474e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-01-07 17:26:02 +0100

    ENH: add ExtensionArray.to_numpy to have control over conversion to numpy array (#30322)
    Co-authored-by: Tom Augspurger <TomAugspurger@users.noreply.github.com>

--- pandas/core/arrays/numpy_.py
+++ pandas/core/arrays/numpy_.py
@@ -421,26 +421,14 @@ class PandasArray(ExtensionArray, ExtensionOpsMixin, NDArrayOperatorsMixin):
 
     # ------------------------------------------------------------------------
     # Additional Methods
-    def to_numpy(self, dtype=None, copy=False):
-        """
-        Convert the PandasArray to a :class:`numpy.ndarray`.
-
-        By default, this requires no coercion or copying of data.
-
-        Parameters
-        ----------
-        dtype : numpy.dtype
-            The NumPy dtype to pass to :func:`numpy.asarray`.
-        copy : bool, default False
-            Whether to copy the underlying data.
-
-        Returns
-        -------
-        ndarray
-        """
+    def to_numpy(self, dtype=None, copy=False, na_value=lib._no_default):
         result = np.asarray(self._ndarray, dtype=dtype)
-        if copy and result is self._ndarray:
+
+        if (copy or na_value is not lib._no_default) and result is self._ndarray:
             result = result.copy()
+
+        if na_value is not lib._no_default:
+            result[self.isna()] = na_value
 
         return result
 
