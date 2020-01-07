090957c669333bb2bbcdb15ab17bd6b10e9757e8
Simon-Martin Schröder <martin.schroeder@nerdluecht.de>
2020-01-07 08:05:18 +0100

    ENH: Show column name in assert_frame_equal (#29218)
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- pandas/_testing.py
+++ pandas/_testing.py
@@ -1287,7 +1287,7 @@ def assert_frame_equal(
     >>> assert_frame_equal(df1, df2)
     Traceback (most recent call last):
     ...
-    AssertionError: Attributes of DataFrame.iloc[:, 1] are different
+    AssertionError: Attributes of DataFrame.iloc[:, 1] (column name="b") are different
 
     Attribute "dtype" are different
     [left]:  int64
@@ -1366,7 +1366,7 @@ def assert_frame_equal(
                 check_names=check_names,
                 check_datetimelike_compat=check_datetimelike_compat,
                 check_categorical=check_categorical,
-                obj=f"{obj}.iloc[:, {i}]",
+                obj=f'{obj}.iloc[:, {i}] (column name="{col}")',
             )
 
 
