d37df39ea1a4f5745cdfd6966bfa252f772c879c
Trevor Bye <trevor.bye@microsoft.com>
2020-01-08 11:55:58 -0800

    initial test case

---
+++ pandas/tests/frame/test_set_index_preserve_dtype.py
@@ -0,0 +1,14 @@
+import pytest
+import numpy as np
+import pandas as pd
+import pandas._testing as tm
+
+
+@pytest.mark.parametrize('test_dtype', [object, 'int8', 'int16', 'int32', 'int64'])
+def test_dtypes(test_dtype):
+    df = pd.DataFrame({'A': pd.Series([1, 2, 3], dtype=test_dtype), 'B': [1, 2, 3]})
+    expected = df.dtypes.values[0].type
+
+    df.set_index('A')
+    result = df.index.dtype.type
+    tm.assert_equal(result, expected)
