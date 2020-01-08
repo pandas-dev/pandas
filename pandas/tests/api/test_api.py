8320cc6d7bf42f151f8cc4684352860ab9e8bdd4
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-01-08 13:33:30 -0600

    API: Limit assert_*_equal functions in public API

--- pandas/tests/api/test_api.py
+++ pandas/tests/api/test_api.py
@@ -271,16 +271,7 @@ class TestTesting(Base):
         "assert_frame_equal",
         "assert_series_equal",
         "assert_index_equal",
-        "assert_equal",
-        "assert_almost_equal",
-        "assert_categorical_equal",
-        "assert_datetime_array_equal",
         "assert_extension_array_equal",
-        "assert_interval_array_equal",
-        "assert_numpy_array_equal",
-        "assert_period_array_equal",
-        "assert_sp_array_equal",
-        "assert_timedelta_array_equal",
     ]
 
     def test_testing(self):
