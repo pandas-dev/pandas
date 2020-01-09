a780a1842737af125bb94457cbc13c81ce2237d7
Tanmay <tanmay.daripa@stylumia.com>
2020-01-09 16:36:43 +0530

    BUG: fill_value ignored for multindex unstack (#30740)

--- pandas/tests/reshape/test_reshape.py
+++ pandas/tests/reshape/test_reshape.py
@@ -651,3 +651,22 @@ class TestMakeAxisDummies:
 
             result = make_axis_dummies(df, transform=lambda x: x)
             tm.assert_frame_equal(result, expected)
+
+
+class TestMultiIndexReshape:
+    def test_unstacking_multi_index_df(self):
+        # BUG: 30740
+        df = pd.DataFrame(
+            {
+                "name": ["Alice", "Bob"],
+                "score": [9.5, 8],
+                "employed": [False, True],
+                "kids": [0, 0],
+                "gender": ["female", "male"],
+            }
+        )
+        df = df.set_index(["name", "employed", "kids", "gender"])
+        df = df.unstack(["gender"], fill_value=0)
+        expected = df.unstack("employed", fill_value=0).unstack("kids", fill_value=0)
+        result = df.unstack(["employed", "kids"], fill_value=0)
+        tm.assert_frame_equal(result, expected)
