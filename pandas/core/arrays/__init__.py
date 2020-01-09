d59c761c9cfaf43045759110f256986fbd99652c
MomIsBestFriend <50263213+MomIsBestFriend@users.noreply.github.com>
2020-01-09 02:06:58 +0200

    STY: absolute imports (#30828)

--- pandas/core/arrays/__init__.py
+++ pandas/core/arrays/__init__.py
@@ -1,16 +1,16 @@
-from .base import (  # noqa: F401
+from pandas.core.arrays.base import (  # noqa: F401
     ExtensionArray,
     ExtensionOpsMixin,
     ExtensionScalarOpsMixin,
     try_cast_to_ea,
 )
-from .boolean import BooleanArray  # noqa: F401
-from .categorical import Categorical  # noqa: F401
-from .datetimes import DatetimeArray  # noqa: F401
-from .integer import IntegerArray, integer_array  # noqa: F401
-from .interval import IntervalArray  # noqa: F401
-from .numpy_ import PandasArray, PandasDtype  # noqa: F401
-from .period import PeriodArray, period_array  # noqa: F401
-from .sparse import SparseArray  # noqa: F401
-from .string_ import StringArray  # noqa: F401
-from .timedeltas import TimedeltaArray  # noqa: F401
+from pandas.core.arrays.boolean import BooleanArray  # noqa: F401
+from pandas.core.arrays.categorical import Categorical  # noqa: F401
+from pandas.core.arrays.datetimes import DatetimeArray  # noqa: F401
+from pandas.core.arrays.integer import IntegerArray, integer_array  # noqa: F401
+from pandas.core.arrays.interval import IntervalArray  # noqa: F401
+from pandas.core.arrays.numpy_ import PandasArray, PandasDtype  # noqa: F401
+from pandas.core.arrays.period import PeriodArray, period_array  # noqa: F401
+from pandas.core.arrays.sparse import SparseArray  # noqa: F401
+from pandas.core.arrays.string_ import StringArray  # noqa: F401
+from pandas.core.arrays.timedeltas import TimedeltaArray  # noqa: F401
