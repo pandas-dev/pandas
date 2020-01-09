c51c5d7304d82b511ca581c7712e6501f9900cb2
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-01-09 08:21:52 -0600

    fixup

--- pandas/tests/arrays/test_boolean.py
+++ pandas/tests/arrays/test_boolean.py
@@ -251,6 +251,18 @@ def test_coerce_to_numpy_array():
         np.array(arr, dtype="bool")
 
 
+def test_repr():
+    df = pd.DataFrame({"A": pd.array([True, False, None], dtype="boolean")})
+    expected = "       A\n0   True\n1  False\n2   <NA>"
+    assert repr(df) == expected
+
+    expected = "0     True\n1    False\n2     <NA>\nName: A, dtype: boolean"
+    assert repr(df.A) == expected
+
+    expected = "<BooleanArray>\n[True, False, <NA>]\nLength: 3, dtype: boolean"
+    assert repr(df.A.array) == expected
+
+
 @pytest.mark.parametrize("box", [True, False], ids=["series", "array"])
 def test_to_numpy(box):
     con = pd.Series if box else pd.array
