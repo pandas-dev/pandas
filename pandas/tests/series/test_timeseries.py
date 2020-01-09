2baf788819cd1073d9c15444ebcefcfab8e4b9c8
MomIsBestFriend <50263213+MomIsBestFriend@users.noreply.github.com>
2020-01-09 17:58:02 +0200

    STY: Spaces in wrong place (#30781)
    * STY: Spaces in wrong place

--- pandas/tests/series/test_timeseries.py
+++ pandas/tests/series/test_timeseries.py
@@ -501,10 +501,7 @@ class TestTimeSeries:
     def test_between_time_types(self):
         # GH11818
         rng = date_range("1/1/2000", "1/5/2000", freq="5min")
-        msg = (
-            r"Cannot convert arg \[datetime\.datetime\(2010, 1, 2, 1, 0\)\]"
-            " to a time"
-        )
+        msg = r"Cannot convert arg \[datetime\.datetime\(2010, 1, 2, 1, 0\)\] to a time"
         with pytest.raises(ValueError, match=msg):
             rng.indexer_between_time(datetime(2010, 1, 2, 1), datetime(2010, 1, 2, 5))
 
