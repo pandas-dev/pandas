2c4df98b547c3c4459d2b403c04a62113a91f0ff
MomIsBestFriend <50263213+MomIsBestFriend@users.noreply.github.com>
2020-01-09 04:39:50 +0200

    Removed "# noqa: F401" comments (#30832)
    Implemented __all__ for each changed file

--- pandas/core/arrays/__init__.py
+++ pandas/core/arrays/__init__.py
@@ -1,16 +1,36 @@
-from pandas.core.arrays.base import (  # noqa: F401
+from pandas.core.arrays.base import (
     ExtensionArray,
     ExtensionOpsMixin,
     ExtensionScalarOpsMixin,
     try_cast_to_ea,
 )
-from pandas.core.arrays.boolean import BooleanArray  # noqa: F401
-from pandas.core.arrays.categorical import Categorical  # noqa: F401
-from pandas.core.arrays.datetimes import DatetimeArray  # noqa: F401
-from pandas.core.arrays.integer import IntegerArray, integer_array  # noqa: F401
-from pandas.core.arrays.interval import IntervalArray  # noqa: F401
-from pandas.core.arrays.numpy_ import PandasArray, PandasDtype  # noqa: F401
-from pandas.core.arrays.period import PeriodArray, period_array  # noqa: F401
-from pandas.core.arrays.sparse import SparseArray  # noqa: F401
-from pandas.core.arrays.string_ import StringArray  # noqa: F401
-from pandas.core.arrays.timedeltas import TimedeltaArray  # noqa: F401
+from pandas.core.arrays.boolean import BooleanArray
+from pandas.core.arrays.categorical import Categorical
+from pandas.core.arrays.datetimes import DatetimeArray
+from pandas.core.arrays.integer import IntegerArray, integer_array
+from pandas.core.arrays.interval import IntervalArray
+from pandas.core.arrays.numpy_ import PandasArray, PandasDtype
+from pandas.core.arrays.period import PeriodArray, period_array
+from pandas.core.arrays.sparse import SparseArray
+from pandas.core.arrays.string_ import StringArray
+from pandas.core.arrays.timedeltas import TimedeltaArray
+
+__all__ = [
+    "ExtensionArray",
+    "ExtensionOpsMixin",
+    "ExtensionScalarOpsMixin",
+    "try_cast_to_ea",
+    "BooleanArray",
+    "Categorical",
+    "DatetimeArray",
+    "IntegerArray",
+    "integer_array",
+    "IntervalArray",
+    "PandasArray",
+    "PandasDtype",
+    "PeriodArray",
+    "period_array",
+    "SparseArray",
+    "StringArray",
+    "TimedeltaArray",
+]
