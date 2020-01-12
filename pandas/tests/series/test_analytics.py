0c39036fe328990ab9c0a124aed58923bb5ba376
Kaiqi <kaiqi@kth.se>
2020-01-12 20:37:56 +0100

    update whatsnew and fix pep8

--- pandas/tests/series/test_analytics.py
+++ pandas/tests/series/test_analytics.py
@@ -240,25 +240,25 @@ class TestSeriesAnalytics:
         "unstack_idx, expected_values, expected_index, expected_columns",
         [
             (
-                    ("A", "a"),
-                    [[1, 1], [1, 1], [1, 1], [1, 1]],
-                    pd.MultiIndex.from_tuples(
-                        [(1, 3), (1, 4), (2, 3), (2, 4)], names=["B", "C"]
-                    ),
-                    pd.MultiIndex.from_tuples([("a",), ("b",)], names=[("A", "a")]),
+                ("A", "a"),
+                [[1, 1], [1, 1], [1, 1], [1, 1]],
+                pd.MultiIndex.from_tuples(
+                    [(1, 3), (1, 4), (2, 3), (2, 4)], names=["B", "C"]
+                ),
+                pd.MultiIndex.from_tuples([("a",), ("b",)], names=[("A", "a")]),
             ),
             (
-                    (("A", "a"), "B"),
-                    [[1, 1, 1, 1], [1, 1, 1, 1]],
-                    pd.Index([3, 4], name="C"),
-                    pd.MultiIndex.from_tuples(
-                        [("a", 1), ("a", 2), ("b", 1), ("b", 2)], names=[("A", "a"), "B"]
-                    ),
+                (("A", "a"), "B"),
+                [[1, 1, 1, 1], [1, 1, 1, 1]],
+                pd.Index([3, 4], name="C"),
+                pd.MultiIndex.from_tuples(
+                    [("a", 1), ("a", 2), ("b", 1), ("b", 2)], names=[("A", "a"), "B"]
+                ),
             ),
         ],
     )
     def test_unstack_mixed_type_name_in_multiindex(
-            self, unstack_idx, expected_values, expected_index, expected_columns
+        self, unstack_idx, expected_values, expected_index, expected_columns
     ):
         # GH 19966
         idx = pd.MultiIndex.from_product(
