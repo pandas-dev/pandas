48762f9e2756c16d8ab24dd24f971df684fce6b8
MomIsBestFriend <>
2020-01-12 12:01:43 +0200

    Fix tests case for 'strings_with_wrong_placed_space'

--- scripts/validate_string_concatenation.py
+++ scripts/validate_string_concatenation.py
@@ -132,7 +132,7 @@ def strings_with_wrong_placed_space(
             (not first_line.endswith(" "))
             and second_line.startswith(" ")
             and (not second_line.startswith("  "))
-            and (not first_line.endswith("\n"))
+            and (not first_line.endswith("\\n"))
         ) or (first_line.endswith(" ") and second_line.startswith(" "))
 
     with open(source_path, "r") as file_name:
