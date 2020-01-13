78e59ef35fa422ad845491bea58dbefa8a3c8fee
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-01-13 08:17:14 -0600

    ref

--- pandas/tests/util/test_hashing.py
+++ pandas/tests/util/test_hashing.py
@@ -377,6 +377,7 @@ def test_hash_with_tuple():
 
 
 def test_hash_object_none_key():
+    # https://github.com/pandas-dev/pandas/issues/30887
     result = pd.util.hash_pandas_object(pd.Series(["a", "b"]), hash_key=None)
     expected = pd.Series([4578374827886788867, 17338122309987883691], dtype="uint64")
     tm.assert_series_equal(result, expected)
