9f45b89307a669689122140164115922c2214e8b
MomIsBestFriend <>
2020-01-13 16:13:09 +0200

    Reverted error message

--- pandas/_libs/window/aggregations.pyx
+++ pandas/_libs/window/aggregations.pyx
@@ -1491,7 +1491,8 @@ cdef ndarray[float64_t] _roll_weighted_sum_mean(float64_t[:] values,
         tot_wgt = np.zeros(in_n, dtype=np.float64)
 
     if minp > win_n:
-        raise ValueError(f"min_periods {minp} must be <= window {win_n}")
+        raise ValueError(f"min_periods (minp) must be <= "
+                         f"window (win)")
     elif minp > in_n:
         minp = in_n + 1
     elif minp < 0:
