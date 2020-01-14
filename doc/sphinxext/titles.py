f089c0c2ba935ced41eb26cd49fdbc0cf4488afe
awu42 <awu42@jhu.edu>
2020-01-13 21:58:00 -0500

    Starting builder

--- doc/sphinxext/titles.py
+++ doc/sphinxext/titles.py
@@ -11,17 +11,33 @@ This will be replaced with nothing (or hello world haha)
 
 from docutils import nodes
 from docutils.parsers.rst import Directive
+from sphinx.builders import Builder
+
+# Use spelling builder as an example!!!!!
 
 
-class HelloWorld(Directive):
+class HeadingCapitalizationValidator(Builder):
 
-    def run(self):
+    def init(self):
+        z = 0
+        if z == 0:
+            self.warning("Warning: capitalization not followed")
         paragraph_node = nodes.paragraph(text='Hello World!')
         return [paragraph_node]
 
+    def write_doc(self, docname, doctree):
+        for node in doctree.traverse(nodes.Text):
+            if (node.tagname == '#title'):
+                print("Bro")
+
+    def finish(self):
+        z = 1
+        return
+
 
 def setup(app):
-    app.add_directive("helloworld", HelloWorld)
+    app.info("Initializing capitalization validator")
+    app.
 
     return {
         'version': '0.1',
