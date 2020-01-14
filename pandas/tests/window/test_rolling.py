4e831aba52d75d071be4f1d9889dcd381f1cbfce
MomIsBestFriend <>
2020-01-14 20:49:38 +0200

    Seperated messages

--- pandas/tests/window/test_rolling.py
+++ pandas/tests/window/test_rolling.py
@@ -46,17 +46,19 @@ class TestRolling(Base):
             c(-1)
 
         # not valid
-        msg = (
-            "window must be an integer|"
-            "passed window foo is not compatible with a datetimelike index|"
-            "min_periods must be an integer|"
-            "center must be a boolean"
-        )
         for w in [2.0, "foo", np.array([2])]:
+            msg = (
+                "window must be an integer|"
+                "passed window foo is not compatible with a datetimelike index"
+            )
             with pytest.raises(ValueError, match=msg):
                 c(window=w)
+
+            msg = "min_periods must be an integer"
             with pytest.raises(ValueError, match=msg):
                 c(window=2, min_periods=w)
+
+            msg = "center must be a boolean"
             with pytest.raises(ValueError, match=msg):
                 c(window=2, min_periods=1, center=w)
 
