e5a61cdfac6758fa40574b991e3d34d839912062
MomIsBestFriend <>
2020-01-14 14:21:10 +0200

    Fix failing tests

--- pandas/tests/internals/test_internals.py
+++ pandas/tests/internals/test_internals.py
@@ -1239,11 +1239,11 @@ class TestCanHoldElement:
 
         if (op, dtype) in invalid:
             msg = (
-                "cannot perform __pow__ with this index type: DatetimeArray|"
-                "cannot perform __mod__ with this index type: DatetimeArray|"
-                "cannot perform __truediv__ with this index type: DatetimeArray|"
-                "cannot perform __mul__ with this index type: DatetimeArray|"
-                "cannot perform __pow__ with this index type: TimedeltaArray|"
+                r"cannot perform __pow__ with this index type: DatetimeArray|"
+                r"cannot perform __mod__ with this index type: DatetimeArray|"
+                r"cannot perform __truediv__ with this index type: DatetimeArray|"
+                r"cannot perform __mul__ with this index type: DatetimeArray|"
+                r"cannot perform __pow__ with this index type: TimedeltaArray|"
                 "ufunc 'multiply' cannot use operands with types dtype"
                 r"\('<m8\[ns\]'\) and dtype\('<m8\[ns\]'\)|"
                 "cannot add DatetimeArray and Timestamp"
