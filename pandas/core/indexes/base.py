466b8995d6583c3f940ac93ae1d3fd1805e11d1d
jbrockmendel <jbrockmendel@gmail.com>
2020-01-15 14:43:11 -0800

    remove extraneous check

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -460,12 +460,6 @@ class Index(IndexOpsMixin, PandasObject):
         """
         assert isinstance(values, np.ndarray), type(values)
 
-        if isinstance(values, (ABCSeries, ABCIndexClass)):
-            # Index._data must always be an ndarray.
-            # This is no-copy for when _values is an ndarray,
-            # which should be always at this point.
-            values = np.asarray(values._values)
-
         result = object.__new__(cls)
         result._data = values
         # _index_data is a (temporary?) fix to ensure that the direct data
