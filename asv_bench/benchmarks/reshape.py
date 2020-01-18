fe9e8bc563a2560f3495be720a8f28f8fcece40d
Kaiqi <kaiqi@kth.se>
2020-01-18 19:18:52 +0100

    fix pep8

--- asv_bench/benchmarks/reshape.py
+++ asv_bench/benchmarks/reshape.py
@@ -160,7 +160,7 @@ class PivotTable:
             fill_value=0,
             observed=True,
         )
-        
+
     def time_pivot_table_margins_only_column(self):
         self.df.pivot_table(columns=["key2", "key3"], margins=True, aggfunc=np.sum)
 
