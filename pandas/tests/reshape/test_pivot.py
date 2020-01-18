fc497e2750f670f6ee11e107344c40a703b483ca
Kaiqi Dong <kaiqi@kth.se>
2020-01-18 17:23:31 +0100

    BUG: correct wrong error message in df.pivot when columns=None (#30925)

--- pandas/tests/reshape/test_pivot.py
+++ pandas/tests/reshape/test_pivot.py
@@ -781,6 +781,15 @@ class TestPivotTable:
         expected = DataFrame(data=data, index=index, columns=columns, dtype="object")
         tm.assert_frame_equal(result, expected)
 
+    def test_pivot_columns_none_raise_error(self):
+        # GH 30924
+        df = pd.DataFrame(
+            {"col1": ["a", "b", "c"], "col2": [1, 2, 3], "col3": [1, 2, 3]}
+        )
+        msg = r"pivot\(\) missing 1 required argument: 'columns'"
+        with pytest.raises(TypeError, match=msg):
+            df.pivot(index="col1", values="col3")
+
     @pytest.mark.xfail(
         reason="MultiIndexed unstack with tuple names fails with KeyError GH#19966"
     )
