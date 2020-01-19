9aa9ead820b135f8683ab2ec58a44bbd8aacdd53
Rebecca N. Palmer <rebecca_palmer@zoho.com>
2020-01-19 16:24:33 +0000

    TST: Add thousands separator, blank header cell, MultiIndex read_html tests
    Regain test coverage after the previous commit's removals

--- pandas/tests/io/test_html.py
+++ pandas/tests/io/test_html.py
@@ -856,7 +856,18 @@ class TestReadHtml:
         assert os.path.isfile(data), f"{repr(data)} is not a file"
         assert os.path.getsize(data), f"{repr(data)} is an empty file"
         result = self.read_html(data, "Arizona", header=1)[0]
+        assert result.shape == (60, 12)
+        assert 'Unnamed' in result.columns[-1]
         assert result["sq mi"].dtype == np.dtype("float64")
+        assert np.allclose(result.loc[0, "sq mi"], 665384.04)
+
+    def test_wikipedia_states_multiindex(self, datapath):
+        data = datapath("io", "data", "html", "wikipedia_states.html")
+        result = self.read_html(data, "Arizona", index_col=0)[0]
+        assert result.shape == (60, 11)
+        assert 'Unnamed' in result.columns[-1][1]
+        assert result.columns.nlevels == 2
+        assert np.allclose(result.loc["Alaska", ("Total area[2]", "sq mi")], 665384.04)
 
     def test_parser_error_on_empty_header_row(self):
         msg = (
