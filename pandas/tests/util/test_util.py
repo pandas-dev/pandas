9e0972de7979d92cd1159fda6c7785a1e8bb8b14
MomIsBestFriend <>
2020-01-19 08:40:32 +0200

    Droped confusing test
    Ref: https://github.com/pandas-dev/pandas/pull/31130#discussion_r368266676

--- pandas/tests/util/test_util.py
+++ pandas/tests/util/test_util.py
@@ -79,13 +79,5 @@ def test_rng_context():
 
 
 def test_external_error_raised():
-    # Raising an external error message
     with tm.external_error_raised(TypeError):
         raise TypeError("Should not check this error message, so it will pass")
-
-    # Note: Should be the only place in the codebase with
-    # "pytest.raises(Exception, match=None)"
-    # (Except pandas._testing.external_error_raised)
-    with pytest.raises(TypeError, match=None):
-        with tm.external_error_raised(ValueError):
-            raise TypeError("Checking that we catch only the specified Exception")
