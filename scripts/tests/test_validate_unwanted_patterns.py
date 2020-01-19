da9d4f9c0596ac38d5ae9ecd4765c8f892f960a0
MomIsBestFriend <>
2020-01-19 02:44:04 +0200

    Refactored test to use "pytest.mark.parametrize"

--- scripts/tests/test_validate_unwanted_patterns.py
+++ scripts/tests/test_validate_unwanted_patterns.py
@@ -1,215 +1,390 @@
 import io
 
+import pytest
+
 # the "as" is just so I won't have to change this in many places later.
 import validate_string_concatenation as validate_unwanted_patterns
 
 
 class TestBarePytestRaises:
-    def test_bare_pytest_raises(self):
-        fd = io.StringIO(
-            """
-with pytest.raises(ValueError):
-    pass
-""".strip()
-        )
-        result = list(validate_unwanted_patterns.bare_pytest_raises(fd))
-        expected = [
+    @pytest.mark.parametrize(
+        "fd, expected",
+        [
             (
-                1,
-                (
-                    "Bare pytests raise have been found. "
-                    "Please pass in the argument 'match' as well the exception."
-                ),
-            )
-        ]
-        assert result == expected
-
-    def test_pytest_raises(self):
-        fd = io.StringIO(
-            """
+                io.StringIO(
+                    """
 with pytest.raises(ValueError, match="foo"):
     pass
 """.strip()
-        )
-        result = list(validate_unwanted_patterns.bare_pytest_raises(fd))
-        expected = []
-        assert result == expected
-
-    def test_bare_pytest_raises_as_comment(self):
-        fd = io.StringIO(
-            """
-# with pytest.raises(ValueError):
-#    pass
-""".strip()
-        )
-        result = list(validate_unwanted_patterns.bare_pytest_raises(fd))
-        expected = []
-        assert result == expected
-
-    def test_pytest_raises_as_comment(self):
-        fd = io.StringIO(
-            """
+                ),
+                [],
+            ),
+            (
+                io.StringIO(
+                    """
 # with pytest.raises(ValueError, match="foo"):
 #    pass
 """.strip()
-        )
+                ),
+                [],
+            ),
+            (
+                io.StringIO(
+                    """
+# with pytest.raises(ValueError):
+#    pass
+""".strip()
+                ),
+                [],
+            ),
+        ],
+    )
+    def test_good_pytest_raises(self, fd, expected):
         result = list(validate_unwanted_patterns.bare_pytest_raises(fd))
-        expected = []
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "fd, expected",
+        [
+            (
+                io.StringIO(
+                    """
+    with pytest.raises(ValueError):
+        pass
+    """.strip()
+                ),
+                [
+                    (
+                        1,
+                        (
+                            "Bare pytests raise have been found. "
+                            "Please pass in the argument 'match' "
+                            "as well the exception."
+                        ),
+                    ),
+                ],
+            ),
+            (
+                io.StringIO(
+                    """
+    with pytest.raises(ValueError, match="foo"):
+        with pytest.raises(ValueError):
+            pass
+        pass
+    """.strip()
+                ),
+                [
+                    (
+                        2,
+                        (
+                            "Bare pytests raise have been found. "
+                            "Please pass in the argument 'match' "
+                            "as well the exception."
+                        ),
+                    ),
+                ],
+            ),
+            (
+                io.StringIO(
+                    """
+    with pytest.raises(ValueError):
+        with pytest.raises(ValueError, match="foo"):
+            pass
+        pass
+    """.strip()
+                ),
+                [
+                    (
+                        1,
+                        (
+                            "Bare pytests raise have been found. "
+                            "Please pass in the argument 'match' "
+                            "as well the exception."
+                        ),
+                    ),
+                ],
+            ),
+        ],
+    )
+    def test_bad_pytest_raises(self, fd, expected):
+        result = list(validate_unwanted_patterns.bare_pytest_raises(fd))
         assert result == expected
 
 
 class TestStringsToConcatenate:
-    def test_strings_to_concatenate(self):
-        fd = io.StringIO(
-            """
-msg = ("bar " "baz")
-""".strip()
-        )
-        result = list(validate_unwanted_patterns.strings_to_concatenate(fd))
-        expected = [
+    @pytest.mark.parametrize(
+        "fd, expected",
+        [
             (
-                1,
-                (
-                    "String unnecessarily split in two by black. "
-                    "Please merge them manually."
-                ),
-            )
-        ]
-        assert result == expected
-
-    def test_strings_to_concatenate_two_occurrences_same_line(self):
-        fd = io.StringIO(
-            """
-msg = ("foo " "bar " "baz")
-""".strip()
-        )
-        result = list(validate_unwanted_patterns.strings_to_concatenate(fd))
-        expected = [
-            (
-                1,
-                (
-                    "String unnecessarily split in two by black. "
-                    "Please merge them manually."
-                ),
+                io.StringIO('msg = ("bar " "baz")'),
+                [
+                    (
+                        1,
+                        (
+                            "String unnecessarily split in two by black. "
+                            "Please merge them manually."
+                        ),
+                    )
+                ],
             ),
             (
-                1,
-                (
-                    "String unnecessarily split in two by black. "
-                    "Please merge them manually."
-                ),
+                io.StringIO('msg = ("foo " "bar " "baz")'),
+                [
+                    (
+                        1,
+                        (
+                            "String unnecessarily split in two by black. "
+                            "Please merge them manually."
+                        ),
+                    ),
+                    (
+                        1,
+                        (
+                            "String unnecessarily split in two by black. "
+                            "Please merge them manually."
+                        ),
+                    ),
+                ],
             ),
-        ]
+        ],
+    )
+    def test_strings_to_concatenate(self, fd, expected):
+        result = list(validate_unwanted_patterns.strings_to_concatenate(fd))
         assert result == expected
 
 
 class TestStringsWithWrongPlacedWhitespace:
-    def test_strings_with_wrong_placed_whitespace(self):
-        fd = io.StringIO(
-            """
-msg = (
-    "foo"
-    " bar"
-)
-""".strip()
-        )
-        result = list(
-            validate_unwanted_patterns.strings_with_wrong_placed_whitespace(fd)
-        )
-        expected = [
+    @pytest.mark.parametrize(
+        "fd, expected",
+        [
             (
-                3,
-                (
-                    "String has a space at the beginning instead "
-                    "of the end of the previous string."
-                ),
-            ),
-        ]
-        assert result == expected
-
-    def test_strings_with_wrong_placed_whitespace_ends_new_line(self):
-        fd = io.StringIO(
-            """
+                io.StringIO(
+                    """
 msg = (
-    "foo\n"
+    "foo"
     " bar"
 )
 """.strip()
-        )
-        result = list(
-            validate_unwanted_patterns.strings_with_wrong_placed_whitespace(fd)
-        )
-        expected = []
-        assert result == expected
-
-    def test_strings_with_wrong_placed_whitespace_first_line_starts_double_space(self):
-        fd = io.StringIO(
-            """
-msg = (
-    "  foo"
-    "bar"
-)
-""".strip()
-        )
-        result = list(
-            validate_unwanted_patterns.strings_with_wrong_placed_whitespace(fd)
-        )
-        expected = []
-        assert result == expected
-
-    def test_strings_with_wrong_placed_whitespace_second_line_starts_double_sapce(self):
-        fd = io.StringIO(
-            """
-msg = (
-    "foo"
-    "  bar"
-)
-""".strip()
-        )
-        result = list(
-            validate_unwanted_patterns.strings_with_wrong_placed_whitespace(fd)
-        )
-        expected = []
-        assert result == expected
-
-    def test_strings_with_wrong_placed_whitespace_first_line_string_literal_prefix(
-        self,
-    ):
-        fd = io.StringIO(
-            """
+                ),
+                [
+                    (
+                        3,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    )
+                ],
+            ),
+            (
+                io.StringIO(
+                    """
 msg = (
     f"foo"
-    "  bar"
+    " bar"
 )
 """.strip()
-        )
-        result = list(
-            validate_unwanted_patterns.strings_with_wrong_placed_whitespace(fd)
-        )
-        expected = []
-        assert result == expected
-
-    def test_strings_with_wrong_placed_whitespace_second_line_string_literal_prefix(
-        self,
-    ):
-        fd = io.StringIO(
-            """
+                ),
+                [
+                    (
+                        3,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    )
+                ],
+            ),
+            (
+                io.StringIO(
+                    """
 msg = (
     "foo"
     f" bar"
 )
 """.strip()
-        )
+                ),
+                [
+                    (
+                        3,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    )
+                ],
+            ),
+            (
+                io.StringIO(
+                    """
+msg = (
+    f"foo"
+    f" bar"
+)
+""".strip()
+                ),
+                [
+                    (
+                        3,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    )
+                ],
+            ),
+            (
+                io.StringIO(
+                    """
+msg = (
+    "foo"
+    rf" bar"
+    " baz"
+)
+""".strip()
+                ),
+                [
+                    (
+                        3,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    ),
+                    (
+                        4,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    ),
+                ],
+            ),
+            (
+                io.StringIO(
+                    """
+msg = (
+    "foo"
+    " bar"
+    rf" baz"
+)
+""".strip()
+                ),
+                [
+                    (
+                        3,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    ),
+                    (
+                        4,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    ),
+                ],
+            ),
+            (
+                io.StringIO(
+                    """
+msg = (
+    "foo"
+    rf" bar"
+    rf" baz"
+)
+""".strip()
+                ),
+                [
+                    (
+                        3,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    ),
+                    (
+                        4,
+                        (
+                            "String has a space at the beginning instead "
+                            "of the end of the previous string."
+                        ),
+                    ),
+                ],
+            ),
+        ],
+    )
+    def test_strings_with_wrong_placed_whitespace(self, fd, expected):
         result = list(
             validate_unwanted_patterns.strings_with_wrong_placed_whitespace(fd)
         )
-        expected = [
+        assert result == expected
+
+    @pytest.mark.parametrize(
+        "fd, expected",
+        [
             (
-                3,
-                (
-                    "String has a space at the beginning instead "
-                    "of the end of the previous string."
+                io.StringIO(
+                    """
+msg = (
+    "foo\n"
+    " bar"
+)
+""".strip()
                 ),
-            )
-        ]
+                [],
+            ),
+            (
+                io.StringIO(
+                    """
+msg = (
+    "foo"
+    "  bar"
+    "baz"
+)
+""".strip()
+                ),
+                [],
+            ),
+            (
+                io.StringIO(
+                    """
+msg = (
+    f"foo"
+    "  bar"
+)
+""".strip()
+                ),
+                [],
+            ),
+            (
+                io.StringIO(
+                    """
+msg = (
+    "foo"
+    f"  bar"
+)
+""".strip()
+                ),
+                [],
+            ),
+            (
+                io.StringIO(
+                    """
+msg = (
+    "foo"
+    rf"  bar"
+)
+""".strip()
+                ),
+                [],
+            ),
+        ],
+    )
+    def test_excluded_strings_with_wrong_placed_whitespace(self, fd, expected):
+        result = list(
+            validate_unwanted_patterns.strings_with_wrong_placed_whitespace(fd)
+        )
         assert result == expected
