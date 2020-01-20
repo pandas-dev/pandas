f7fdf458357a9ef95f0fb25cf008d17ecccd7cb8
DANIEL SAXTON <danielsaxton@DANIELs-MacBook-Pro.local>
2020-01-20 12:06:16 -0600

    Add test

--- pandas/tests/arrays/test_boolean.py
+++ pandas/tests/arrays/test_boolean.py
@@ -251,6 +251,13 @@ def test_coerce_to_numpy_array():
         np.array(arr, dtype="bool")
 
 
+def test_to_boolean_array_from_strings():
+    result = BooleanArray._from_sequence_of_strings(["True", "False"])
+    expected = BooleanArray(np.array([True, False]), np.array([False, False]))
+
+    tm.assert_extension_array_equal(result, expected)
+
+
 def test_repr():
     df = pd.DataFrame({"A": pd.array([True, False, None], dtype="boolean")})
     expected = "       A\n0   True\n1  False\n2   <NA>"
