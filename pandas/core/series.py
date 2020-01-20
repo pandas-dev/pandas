cdf92ae4cdeb59a1bcff107c2c0a2de343ca2488
Harsha <hvardhan20@gmail.com>
2020-01-20 15:16:38 -0600

    GH 30975-Fix for Series.append(DataFrame) 0.25.3 behaviour return typing fix

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -23,7 +23,7 @@ import numpy as np
 from pandas._config import get_option
 
 from pandas._libs import index as libindex, lib, reshape, tslibs
-from pandas._typing import FrameOrSeries, Label
+from pandas._typing import FrameOrSeriesUnion, Label
 from pandas.compat.numpy import function as nv
 from pandas.util._decorators import Appender, Substitution
 from pandas.util._validators import validate_bool_kwarg, validate_percentile
@@ -2464,7 +2464,7 @@ Name: Max Speed, dtype: float64
 
     def append(
         self, to_append, ignore_index=False, verify_integrity=False
-    ) -> FrameOrSeries:
+    ) -> FrameOrSeriesUnion:
         """
         Concatenate two or more Series.
 
