c79f8b6aefdba0a2c16526705007655de5c15336
Matt Roeschke <mroeschke@housecanary.com>
2020-01-20 18:26:03 -0800

    TST: Add more regression tests for fixed issues

--- pandas/tests/indexes/interval/test_base.py
+++ pandas/tests/indexes/interval/test_base.py
@@ -86,3 +86,15 @@ class TestBase(Base):
         with pytest.raises(ValueError, match="multi-dimensional indexing not allowed"):
             with tm.assert_produces_warning(DeprecationWarning, check_stacklevel=False):
                 idx[:, None]
+
+    def test_get_indexer_non_monotonic(self):
+        # GH 16410
+        idx1 = IntervalIndex.from_tuples([(2, 3), (4, 5), (0, 1)])
+        idx2 = IntervalIndex.from_tuples([(0, 1), (2, 3), (6, 7), (8, 9)])
+        result = idx1.get_indexer(idx2)
+        expected = np.array([2, 0, -1, -1])
+        tm.assert_numpy_array_equal(result, expected)
+
+        result = idx1.get_indexer(idx1[1:])
+        expected = np.array([1, 2])
+        tm.assert_numpy_array_equal(result, expected)
