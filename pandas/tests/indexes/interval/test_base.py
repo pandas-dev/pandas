74fcb223090bc0485ec5c21a11f845d3295eeb55
Matt Roeschke <mroeschke@housecanary.com>
2020-01-20 19:56:55 -0800

    Using intp

--- pandas/tests/indexes/interval/test_base.py
+++ pandas/tests/indexes/interval/test_base.py
@@ -92,9 +92,9 @@ class TestBase(Base):
         idx1 = IntervalIndex.from_tuples([(2, 3), (4, 5), (0, 1)])
         idx2 = IntervalIndex.from_tuples([(0, 1), (2, 3), (6, 7), (8, 9)])
         result = idx1.get_indexer(idx2)
-        expected = np.array([2, 0, -1, -1], dtype=np.int64)
+        expected = np.array([2, 0, -1, -1], dtype=np.intp)
         tm.assert_numpy_array_equal(result, expected)
 
         result = idx1.get_indexer(idx1[1:])
-        expected = np.array([1, 2], dtype=np.int64)
+        expected = np.array([1, 2], dtype=np.intp)
         tm.assert_numpy_array_equal(result, expected)
