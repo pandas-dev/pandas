47bc9e3bea22ad8945d6427ef9f22074eea2f02c
Daniel Saxton <2658661+dsaxton@users.noreply.github.com>
2020-01-23 14:56:00 -0600

    ENH: Implement _from_sequence_of_strings for BooleanArray (#31159)

--- pandas/core/arrays/boolean.py
+++ pandas/core/arrays/boolean.py
@@ -1,5 +1,5 @@
 import numbers
-from typing import TYPE_CHECKING, Any, Tuple, Type
+from typing import TYPE_CHECKING, Any, List, Tuple, Type
 import warnings
 
 import numpy as np
@@ -285,6 +285,23 @@ class BooleanArray(BaseMaskedArray):
             assert dtype == "boolean"
         values, mask = coerce_to_array(scalars, copy=copy)
         return BooleanArray(values, mask)
+
+    @classmethod
+    def _from_sequence_of_strings(
+        cls, strings: List[str], dtype=None, copy: bool = False
+    ):
+        def map_string(s):
+            if isna(s):
+                return s
+            elif s in ["True", "TRUE", "true"]:
+                return True
+            elif s in ["False", "FALSE", "false"]:
+                return False
+            else:
+                raise ValueError(f"{s} cannot be cast to bool")
+
+        scalars = [map_string(x) for x in strings]
+        return cls._from_sequence(scalars, dtype, copy)
 
     def _values_for_factorize(self) -> Tuple[np.ndarray, Any]:
         data = self._data.astype("int8")
