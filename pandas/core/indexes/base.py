f0254569e9f70a4ee2c74bb66ebb3fe5fec81243
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-01-24 01:00:15 +0100

    PERF: improve access of .array (#31037)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3640,6 +3640,16 @@ class Index(IndexOpsMixin, PandasObject):
         """
         return self._data.view(np.ndarray)
 
+    @cache_readonly
+    @Appender(IndexOpsMixin.array.__doc__)  # type: ignore
+    def array(self) -> ExtensionArray:
+        array = self._data
+        if isinstance(array, np.ndarray):
+            from pandas.core.arrays.numpy_ import PandasArray
+
+            array = PandasArray(array)
+        return array
+
     @property
     def _values(self) -> Union[ExtensionArray, ABCIndexClass, np.ndarray]:
         # TODO(EA): remove index types as they become extension arrays
