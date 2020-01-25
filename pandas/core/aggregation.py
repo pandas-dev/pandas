381a69749d7378974e2320d3a230a0ddeb2b322f
Kaiqi <kaiqi@kth.se>
2020-01-25 21:02:17 +0100

    fixup

--- pandas/core/aggregation.py
+++ pandas/core/aggregation.py
@@ -32,19 +32,21 @@ def reconstruct_func(func, *args, **kwargs):
     columns: Optional[List[str]] = None
     order: Optional[List[int]] = None
 
+    if not relabeling:
+        if isinstance(func, list) and len(func) > len(set(func)):
+
+            # GH 28426 will raise error if duplicated function names are used and
+            # there is no reassigned name
+            raise SpecificationError(
+                "Function names must be unique if there is no new column names"
+                " assigned"
+            )
+        elif func is None:
+            # nicer error message
+            raise TypeError("Must provide 'func' or tuples of '(column, aggfunc).")
+
     if relabeling:
         func, columns, order = normalize_keyword_aggregation(kwargs)
-
-    elif isinstance(func, list) and len(func) > len(set(func)):
-
-        # GH 28426 will raise error if duplicated function names are used and
-        # there is no reassigned name
-        raise SpecificationError(
-            "Function names must be unique if there is no new column names assigned"
-        )
-    elif func is None:
-        # nicer error message
-        raise TypeError("Must provide 'func' or tuples of '(column, aggfunc).")
 
     func = maybe_mangle_lambdas(func)
 
