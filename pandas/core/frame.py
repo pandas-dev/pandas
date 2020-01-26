b9cf60a2cee2298f256a830f9730147ba387f9bf
emiliano.jordan <Loaf-Spit-Toast-69>
2020-01-26 12:29:46 -0700

    Subclassed type support in DataFrame.idxmax()

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -8148,7 +8148,7 @@ Wild         185.0
         indices = nanops.nanargmax(self.values, axis=axis, skipna=skipna)
         index = self._get_axis(axis)
         result = [index[i] if i >= 0 else np.nan for i in indices]
-        return Series(result, index=self._get_agg_axis(axis))
+        return self._constructor_sliced(result, index=self._get_agg_axis(axis))
 
     def _get_agg_axis(self, axis_num):
         """
--- pandas/tests/frame/test_subclass.py
+++ pandas/tests/frame/test_subclass.py
@@ -627,3 +627,9 @@ class TestDataFrameSubclassing:
         df = tm.SubclassedDataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
         result = df.idxmin()
         assert isinstance(result, tm.SubclassedSeries)
+
+    def test_idxmax(self):
+
+        df = tm.SubclassedDataFrame({"A": [1, 2, 3], "B": [4, 5, 6], "C": [7, 8, 9]})
+        result = df.idxmax()
+        assert isinstance(result, tm.SubclassedSeries)
