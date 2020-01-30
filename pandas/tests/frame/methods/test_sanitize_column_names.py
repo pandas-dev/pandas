7de676651647514baace6e32f194b989a945f138
Zahash Baig <zahash.z@gmail.com>
2020-01-30 11:24:47 +0000

    minor bug fixing in test code

--- pandas/tests/frame/methods/test_sanitize_column_names.py
+++ pandas/tests/frame/methods/test_sanitize_column_names.py
@@ -5,11 +5,13 @@ import pandas._testing as tm
 def test_sanitize_column_names():
     df = DataFrame(
         {
-            "A  ": [1],
-            "b": [1],
-            "  485 5(468a)44 44   4 ?  $@e3   *   C cc    c D  ": [1],
+            "A  ": (1, 1),
+            "b": (1, 1),
+            "  485 5(468a)44 44   4 ?  $@e3   *   C cc    c D  ": (1, 1),
         }
     )
     df.sanitize_column_names()
-    expected_df = DataFrame({"a": [1], "b": [1], "485_5468a44_44_4_e3_c_cc_c_d": [1]})
+    expected_df = DataFrame(
+        {"a": (1, 1), "b": (1, 1), "485_5468a44_44_4_e3_c_cc_c_d": (1, 1)}
+    )
     tm.assert_frame_equal(df, expected_df)
