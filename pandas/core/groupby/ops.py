a8bc3a82bd7b33b6f99e8943492adef22f3ef6b1
Daniel Saxton <danielsaxton@DANIELs-MacBook-Pro.local>
2020-01-30 19:56:34 -0600

    Check for period dtype

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -31,6 +31,7 @@ from pandas.core.dtypes.common import (
     is_extension_array_dtype,
     is_integer_dtype,
     is_numeric_dtype,
+    is_period_dtype,
     is_sparse,
     is_timedelta64_dtype,
     needs_i8_conversion,
@@ -567,7 +568,9 @@ class BaseGrouper:
         if swapped:
             result = result.swapaxes(0, axis)
 
-        if is_datetime64tz_dtype(orig_values.dtype):
+        if is_datetime64tz_dtype(orig_values.dtype) or is_period_dtype(
+            orig_values.dtype
+        ):
             result = type(orig_values)(result.astype(np.int64), dtype=orig_values.dtype)
         elif is_datetimelike and kind == "aggregate":
             result = result.astype(orig_values.dtype)
