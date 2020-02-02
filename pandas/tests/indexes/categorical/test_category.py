22e060fedef69f56788d6db56e0b55f730e8b78f
jbrockmendel <jbrockmendel@gmail.com>
2020-02-01 18:45:38 -0800

    Separate out get_loc

--- pandas/tests/indexes/categorical/test_category.py
+++ pandas/tests/indexes/categorical/test_category.py
@@ -423,53 +423,6 @@ class TestCategoricalIndex(Base):
         with pytest.raises(NotImplementedError, match=msg):
             idx2.get_indexer(idx1, method="nearest")
 
-    def test_get_loc(self):
-        # GH 12531
-        cidx1 = CategoricalIndex(list("abcde"), categories=list("edabc"))
-        idx1 = Index(list("abcde"))
-        assert cidx1.get_loc("a") == idx1.get_loc("a")
-        assert cidx1.get_loc("e") == idx1.get_loc("e")
-
-        for i in [cidx1, idx1]:
-            with pytest.raises(KeyError, match="'NOT-EXIST'"):
-                i.get_loc("NOT-EXIST")
-
-        # non-unique
-        cidx2 = CategoricalIndex(list("aacded"), categories=list("edabc"))
-        idx2 = Index(list("aacded"))
-
-        # results in bool array
-        res = cidx2.get_loc("d")
-        tm.assert_numpy_array_equal(res, idx2.get_loc("d"))
-        tm.assert_numpy_array_equal(
-            res, np.array([False, False, False, True, False, True])
-        )
-        # unique element results in scalar
-        res = cidx2.get_loc("e")
-        assert res == idx2.get_loc("e")
-        assert res == 4
-
-        for i in [cidx2, idx2]:
-            with pytest.raises(KeyError, match="'NOT-EXIST'"):
-                i.get_loc("NOT-EXIST")
-
-        # non-unique, sliceable
-        cidx3 = CategoricalIndex(list("aabbb"), categories=list("abc"))
-        idx3 = Index(list("aabbb"))
-
-        # results in slice
-        res = cidx3.get_loc("a")
-        assert res == idx3.get_loc("a")
-        assert res == slice(0, 2, None)
-
-        res = cidx3.get_loc("b")
-        assert res == idx3.get_loc("b")
-        assert res == slice(2, 5, None)
-
-        for i in [cidx3, idx3]:
-            with pytest.raises(KeyError, match="'c'"):
-                i.get_loc("c")
-
     def test_repr_roundtrip(self):
 
         ci = CategoricalIndex(["a", "b"], categories=["a", "b"], ordered=True)
--- pandas/tests/indexes/categorical/test_indexing.py
+++ pandas/tests/indexes/categorical/test_indexing.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 import pandas as pd
+from pandas import CategoricalIndex, Index
 import pandas._testing as tm
 
 
@@ -121,3 +122,52 @@ class TestTake:
         msg = "the 'mode' parameter is not supported"
         with pytest.raises(ValueError, match=msg):
             idx.take(indices, mode="clip")
+
+
+class TestGetLoc:
+    def test_get_loc(self):
+        # GH 12531
+        cidx1 = CategoricalIndex(list("abcde"), categories=list("edabc"))
+        idx1 = Index(list("abcde"))
+        assert cidx1.get_loc("a") == idx1.get_loc("a")
+        assert cidx1.get_loc("e") == idx1.get_loc("e")
+
+        for i in [cidx1, idx1]:
+            with pytest.raises(KeyError, match="'NOT-EXIST'"):
+                i.get_loc("NOT-EXIST")
+
+        # non-unique
+        cidx2 = CategoricalIndex(list("aacded"), categories=list("edabc"))
+        idx2 = Index(list("aacded"))
+
+        # results in bool array
+        res = cidx2.get_loc("d")
+        tm.assert_numpy_array_equal(res, idx2.get_loc("d"))
+        tm.assert_numpy_array_equal(
+            res, np.array([False, False, False, True, False, True])
+        )
+        # unique element results in scalar
+        res = cidx2.get_loc("e")
+        assert res == idx2.get_loc("e")
+        assert res == 4
+
+        for i in [cidx2, idx2]:
+            with pytest.raises(KeyError, match="'NOT-EXIST'"):
+                i.get_loc("NOT-EXIST")
+
+        # non-unique, sliceable
+        cidx3 = CategoricalIndex(list("aabbb"), categories=list("abc"))
+        idx3 = Index(list("aabbb"))
+
+        # results in slice
+        res = cidx3.get_loc("a")
+        assert res == idx3.get_loc("a")
+        assert res == slice(0, 2, None)
+
+        res = cidx3.get_loc("b")
+        assert res == idx3.get_loc("b")
+        assert res == slice(2, 5, None)
+
+        for i in [cidx3, idx3]:
+            with pytest.raises(KeyError, match="'c'"):
+                i.get_loc("c")
