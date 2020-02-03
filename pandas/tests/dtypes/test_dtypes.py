cc5f3f440c99e20a0ae67e400b272e17af3c9013
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-02-03 13:22:38 +0100

    Backport PR #31594: TST: troubleshoot npdev build (#31610)
    * TST: troubleshoot npdev build (#31594)
    
    * fixup
    
    Co-authored-by: jbrockmendel <jbrockmendel@gmail.com>

--- pandas/tests/dtypes/test_dtypes.py
+++ pandas/tests/dtypes/test_dtypes.py
@@ -43,7 +43,12 @@ class Base:
         assert not is_dtype_equal(self.dtype, np.int64)
 
     def test_numpy_informed(self):
-        with pytest.raises(TypeError, match="data type not understood"):
+        # npdev 2020-02-02 changed from "data type not understood" to
+        #  "Cannot interpret 'foo' as a data type"
+        msg = "|".join(
+            ["data type not understood", "Cannot interpret '.*' as a data type"]
+        )
+        with pytest.raises(TypeError, match=msg):
             np.dtype(self.dtype)
 
         assert not self.dtype == np.str_
