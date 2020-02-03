543ef9ab262e1004e967f112d4a37927f10ebe7f
Daniel Saxton <danielsaxton@DANIELs-MacBook-Pro.local>
2020-02-02 18:11:12 -0600

    Add frame test

--- pandas/tests/arrays/test_boolean.py
+++ pandas/tests/arrays/test_boolean.py
@@ -931,7 +931,7 @@ def test_diff():
     tm.assert_series_equal(result, expected)
 
 
-def test_nullable_boolean_index_series():
+def test_nullable_boolean_mask_series():
     s = pd.Series([1, 2, 3])
     mask = pd.array([True, True, None], dtype="boolean")
 
@@ -941,7 +941,7 @@ def test_nullable_boolean_index_series():
     tm.assert_series_equal(result, expected)
 
 
-def test_nullable_boolean_index_frame():
+def test_nullable_boolean_mask_frame():
     df = pd.DataFrame({"a": [1, 2, 3]})
     mask = pd.array([True, True, None], dtype="boolean")
 
