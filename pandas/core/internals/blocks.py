bf3ca08e39304aa3452499c1528e503dde8cbfe8
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-02-05 10:47:30 +0100

    BUG: Block.iget not wrapping timedelta64/datetime64 (#31666) (#31688)
    Co-authored-by: jbrockmendel <jbrockmendel@gmail.com>

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -7,7 +7,7 @@ import warnings
 
 import numpy as np
 
-from pandas._libs import NaT, algos as libalgos, lib, tslib, writers
+from pandas._libs import NaT, Timestamp, algos as libalgos, lib, tslib, writers
 from pandas._libs.index import convert_scalar
 import pandas._libs.internals as libinternals
 from pandas._libs.tslibs import Timedelta, conversion
@@ -2147,6 +2147,16 @@ class DatetimeLikeBlockMixin:
             result = self._holder(values).astype(object)
             return result.reshape(self.values.shape)
         return self.values
+
+    def iget(self, key):
+        # GH#31649 we need to wrap scalars in Timestamp/Timedelta
+        # TODO: this can be removed if we ever have 2D EA
+        result = super().iget(key)
+        if isinstance(result, np.datetime64):
+            result = Timestamp(result)
+        elif isinstance(result, np.timedelta64):
+            result = Timedelta(result)
+        return result
 
 
 class DatetimeBlock(DatetimeLikeBlockMixin, Block):
