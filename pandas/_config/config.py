42f7181cbe8953273e13ef980acfcf07dd5c6edc
David Sternlicht <d1618033@gmail.com>
2020-02-06 16:15:01 +0200

    Revert incorrect ordering of imports

--- pandas/_config/config.py
+++ pandas/_config/config.py
@@ -49,6 +49,7 @@ Implementation
 """
 
 from collections import namedtuple
+from contextlib import contextmanager
 import re
 from typing import (
     Any,
@@ -63,7 +64,6 @@ from typing import (
     cast,
 )
 import warnings
-from contextlib import contextmanager
 
 DeprecatedOption = namedtuple("DeprecatedOption", "key msg rkey removal_ver")
 RegisteredOption = namedtuple("RegisteredOption", "key defval doc validator cb")
