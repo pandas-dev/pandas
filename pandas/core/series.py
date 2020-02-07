1b71ab996cf1637ac3fd605002623c8d7e632bf4
jbrockmendel <jbrockmendel@gmail.com>
2020-02-06 18:46:27 -0800

    CLN: prune unreachable

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -987,8 +987,6 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
 
         try:
             self._set_with_engine(key, value)
-        except com.SettingWithCopyError:
-            raise
         except (KeyError, ValueError):
             values = self._values
             if is_integer(key) and not self.index.inferred_type == "integer":
@@ -997,9 +995,6 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
                 self[:] = value
             else:
                 self.loc[key] = value
-        except InvalidIndexError:
-            # e.g. slice
-            self._set_with(key, value)
 
         except TypeError as e:
             if isinstance(key, tuple) and not isinstance(self.index, MultiIndex):
@@ -1070,7 +1065,7 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
 
     def _set_labels(self, key, value):
         key = com.asarray_tuplesafe(key)
-        indexer = self.index.get_indexer(key)
+        indexer: np.ndarray = self.index.get_indexer(key)
         mask = indexer == -1
         if mask.any():
             raise ValueError(f"{key[mask]} not contained in the index")
@@ -1096,12 +1091,6 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
         value : object
             Scalar value.
         takeable : interpret the index as indexers, default False
-
-        Returns
-        -------
-        Series
-            If label is contained, will be reference to calling Series,
-            otherwise a new object.
         """
         try:
             if takeable:
@@ -1114,8 +1103,6 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
 
             # set using a non-recursive method
             self.loc[label] = value
-
-        return self
 
     # ----------------------------------------------------------------------
     # Unsorted
