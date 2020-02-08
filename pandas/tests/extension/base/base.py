a764fd22adbc4939704b714b80a61e4ac9a70c2a
Martin Winkel <martin.winkel.pps@gmail.com>
2020-02-08 15:33:16 +0100

    using classmethods now

--- pandas/tests/extension/base/base.py
+++ pandas/tests/extension/base/base.py
@@ -2,15 +2,20 @@ import pandas._testing as tm
 
 
 class BaseExtensionTests:
-    # can't use staticmethod() as this confuses mypy
-    def assert_equal(self, left, right, **kwargs):
+    # classmethod and different signature is needed
+    # to make inheritance compliant with mypy
+    @classmethod
+    def assert_equal(cls, left, right, **kwargs):
         return tm.assert_equal(left, right, **kwargs)
 
-    def assert_series_equal(self, left, right, *args, **kwargs):
+    @classmethod
+    def assert_series_equal(cls, left, right, *args, **kwargs):
         return tm.assert_series_equal(left, right, *args, **kwargs)
 
-    def assert_frame_equal(self, left, right, *args, **kwargs):
+    @classmethod
+    def assert_frame_equal(cls, left, right, *args, **kwargs):
         return tm.assert_frame_equal(left, right, *args, **kwargs)
 
-    def assert_extension_array_equal(self, left, right, *args, **kwargs):
+    @classmethod
+    def assert_extension_array_equal(cls, left, right, *args, **kwargs):
         return tm.assert_extension_array_equal(left, right, *args, **kwargs)
