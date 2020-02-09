ab14f6981601b738ff32ea08aae28a1ecbb65647
Martin Winkel <martin.winkel.pps@gmail.com>
2020-02-09 18:48:37 +0100

    fixed mypy errors in mypy-pandas.tests.extension.decimal.test_decimal (#31730)

--- pandas/tests/extension/base/base.py
+++ pandas/tests/extension/base/base.py
@@ -2,8 +2,20 @@ import pandas._testing as tm
 
 
 class BaseExtensionTests:
+    # classmethod and different signature is needed
+    # to make inheritance compliant with mypy
+    @classmethod
+    def assert_equal(cls, left, right, **kwargs):
+        return tm.assert_equal(left, right, **kwargs)
 
-    assert_equal = staticmethod(tm.assert_equal)
-    assert_series_equal = staticmethod(tm.assert_series_equal)
-    assert_frame_equal = staticmethod(tm.assert_frame_equal)
-    assert_extension_array_equal = staticmethod(tm.assert_extension_array_equal)
+    @classmethod
+    def assert_series_equal(cls, left, right, *args, **kwargs):
+        return tm.assert_series_equal(left, right, *args, **kwargs)
+
+    @classmethod
+    def assert_frame_equal(cls, left, right, *args, **kwargs):
+        return tm.assert_frame_equal(left, right, *args, **kwargs)
+
+    @classmethod
+    def assert_extension_array_equal(cls, left, right, *args, **kwargs):
+        return tm.assert_extension_array_equal(left, right, *args, **kwargs)
