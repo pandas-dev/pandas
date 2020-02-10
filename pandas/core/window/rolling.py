8c5911ec181d6c80666069c773373ffc553cd56f
Matthew Roeschke <emailformattr@gmail.com>
2020-02-10 04:50:31 -0800

    BUG: Fix raw parameter not being respected in groupby.rolling.apply (#31842)

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -1296,13 +1296,14 @@ class _Rolling_and_Expanding(_Rolling):
             raise ValueError("engine must be either 'numba' or 'cython'")
 
         # TODO: Why do we always pass center=False?
-        # name=func for WindowGroupByMixin._apply
+        # name=func & raw=raw for WindowGroupByMixin._apply
         return self._apply(
             apply_func,
             center=False,
             floor=0,
             name=func,
             use_numba_cache=engine == "numba",
+            raw=raw,
         )
 
     def _generate_cython_apply_func(self, args, kwargs, raw, offset, func):
