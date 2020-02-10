0be92b76925e9fcda66edfe347b730fc4881865c
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-02-10 11:42:53 +0100

    fix numpy test

--- pandas/tests/extension/test_numpy.py
+++ pandas/tests/extension/test_numpy.py
@@ -430,12 +430,17 @@ class TestSetitem(BaseNumPyTests, base.BaseSetitemTests):
 
     @skip_nested
     @pytest.mark.parametrize(
-        "idx",
-        [[0, 1, 2, pd.NA], pd.array([0, 1, 2, pd.NA], dtype="Int64")],
-        ids=["list", "integer-array"],
+        "idx, box_in_series",
+        [
+            ([0, 1, 2, pd.NA], False),
+            pytest.param([0, 1, 2, pd.NA], True, marks=pytest.mark.xfail),
+            (pd.array([0, 1, 2, pd.NA], dtype="Int64"), False),
+            (pd.array([0, 1, 2, pd.NA], dtype="Int64"), False),
+        ],
+        ids=["list-False", "list-True", "integer-array-False", "integer-array-True"],
     )
-    def test_setitem_integer_with_missing_raises(self, data, idx):
-        super().test_setitem_integer_with_missing_raises(data, idx)
+    def test_setitem_integer_with_missing_raises(self, data, idx, box_in_series):
+        super().test_setitem_integer_with_missing_raises(data, idx, box_in_series)
 
 
 @skip_nested
