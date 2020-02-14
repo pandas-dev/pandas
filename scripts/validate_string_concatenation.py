b1568f1a25145711916a9104f9c07407510e4265
MomIsBestFriend <>
2020-02-14 13:05:50 +0200

    Reverted a2a12fc39417721c5722e0fb9fbe17aff698e556

--- scripts/validate_string_concatenation.py
+++ scripts/validate_string_concatenation.py
@@ -17,7 +17,7 @@ import token
 import tokenize
 from typing import IO, Callable, Iterable, List, Tuple
 
-FILE_EXTENSIONS_TO_CHECK: Tuple[str, ...] = (".py", ".pyx", ".pxi.in", ".pxd")
+FILE_EXTENSIONS_TO_CHECK: Tuple[str, ...] = (".py", ".pyx", ".pxi.ini", ".pxd")
 
 
 def _get_literal_string_prefix_len(token_string: str) -> int:
