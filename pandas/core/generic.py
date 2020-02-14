144caa850bca154202b35bb69ab57e59a5ca05ec
MomIsBestFriend <>
2020-02-14 12:22:13 +0200

    Reverted change from fixing merge conflicts

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -2182,6 +2182,12 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
 
         Examples
         --------
+        >>> df = pd.DataFrame(
+        ...     [["a", "b"], ["c", "d"]],
+        ...     index=["row 1", "row 2"],
+        ...     columns=["col 1", "col 2"],
+        ... )
+
         >>> df.to_json(orient='split')
         '{"columns":["col 1","col 2"],\
 "index":["row 1","row 2"],\
