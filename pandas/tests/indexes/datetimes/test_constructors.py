29107203b61849d53e4aecc39e5096c9be33de25
Alexander Kirko <alexander.kirko@gmail.com>
2020-02-15 09:40:11 +0300

    TST: fix pytz fold conflict test

--- pandas/tests/indexes/datetimes/test_constructors.py
+++ pandas/tests/indexes/datetimes/test_constructors.py
@@ -966,6 +966,9 @@ def test_timestamp_constructor_pytz_fold_raise():
     # pytz doesn't support fold. Check that we raise
     # if fold is passed with pytz
     msg = "pytz timezones do not support fold. Please use dateutil " "timezones."
+    tz = pytz.timezone("Europe/London")
+    with pytest.raises(ValueError, match=msg):
+        Timestamp(datetime(2019, 10, 27, 0, 30, 0, 0), tz=tz, fold=0)
 
 
 @pytest.mark.parametrize("fold", [0, 1])
