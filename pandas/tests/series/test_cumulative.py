a9fc2bce5448fa0521236edd0c6bd3fb1ef860d8
Simon Hawkins <simonjayhawkins@gmail.com>
2020-02-16 17:43:56 +0000

    CI: change np-dev xfails to not strict (#32031)

--- pandas/tests/series/test_cumulative.py
+++ pandas/tests/series/test_cumulative.py
@@ -39,7 +39,9 @@ class TestSeriesCumulativeOps:
         _check_accum_op("cumprod", datetime_series)
 
     @pytest.mark.xfail(
-        _is_numpy_dev, reason="https://github.com/pandas-dev/pandas/issues/31992"
+        _is_numpy_dev,
+        reason="https://github.com/pandas-dev/pandas/issues/31992",
+        strict=False,
     )
     def test_cummin(self, datetime_series):
         tm.assert_numpy_array_equal(
@@ -54,7 +56,9 @@ class TestSeriesCumulativeOps:
         tm.assert_series_equal(result, expected)
 
     @pytest.mark.xfail(
-        _is_numpy_dev, reason="https://github.com/pandas-dev/pandas/issues/31992"
+        _is_numpy_dev,
+        reason="https://github.com/pandas-dev/pandas/issues/31992",
+        strict=False,
     )
     def test_cummax(self, datetime_series):
         tm.assert_numpy_array_equal(
