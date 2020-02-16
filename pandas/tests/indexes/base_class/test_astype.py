3959813b9947fd560b25fd41cb97f7b3dbddc243
Daniel Saxton <danielsaxton@DANIELs-MacBook-Pro.local>
2020-02-16 17:14:54 -0600

    Add some tests

--- pandas/tests/indexes/base_class/test_astype.py
+++ pandas/tests/indexes/base_class/test_astype.py
@@ -4,8 +4,9 @@ from pandas import Index
 
 
 @pytest.mark.parametrize(
-    "from_type, to_type", [("float64", "int64"), ("int64", "float64")]
+    "from_type", ["int64", "uint64", "float64"],
 )
+@pytest.mark.parametrize("to_type", ["int64", "uint64", "float64"])
 def test_astype_preserves_name(from_type, to_type):
     idx = Index([1, 2], name="abc", dtype=from_type).astype(to_type)
 
@@ -13,8 +14,9 @@ def test_astype_preserves_name(from_type, to_type):
 
 
 @pytest.mark.parametrize(
-    "from_type, to_type", [("float64", "int64"), ("int64", "float64")]
+    "from_type", ["int64", "uint64", "float64"],
 )
+@pytest.mark.parametrize("to_type", ["int64", "uint64", "float64"])
 def test_copy_with_astype_preserves_name(from_type, to_type):
     idx = Index([1, 2], name="abc", dtype=from_type).copy(dtype=to_type)
 
