9ca140160ad092648a46c1854c44e3c0873f3533
Daniel Saxton <danielsaxton@DANIELs-MacBook-Pro.local>
2020-02-15 21:12:29 -0600

    Change name

--- pandas/tests/indexes/base_class/test_astype.py
+++ pandas/tests/indexes/base_class/test_astype.py
@@ -7,15 +7,15 @@ from pandas import Index
     "from_type, to_type", [("float64", "int64"), ("int64", "float64")]
 )
 def test_astype_preserves_name(from_type, to_type):
-    idx = Index([1, 2], name="name", dtype=from_type).astype(to_type)
+    idx = Index([1, 2], name="abc", dtype=from_type).astype(to_type)
 
-    assert idx.name == "name"
+    assert idx.name == "abc"
 
 
 @pytest.mark.parametrize(
     "from_type, to_type", [("float64", "int64"), ("int64", "float64")]
 )
 def test_copy_with_astype_preserves_name(from_type, to_type):
-    idx = Index([1, 2], name="name", dtype=from_type).copy(dtype=to_type)
+    idx = Index([1, 2], name="abc", dtype=from_type).copy(dtype=to_type)
 
-    assert idx.name == "name"
+    assert idx.name == "abc"
