5209ebcc761124f64fbf05726c73a58924936675
Daniel Saxton <danielsaxton@DANIELs-MacBook-Pro.local>
2020-02-15 21:04:23 -0600

    Add Index astype tests

---
+++ pandas/tests/indexes/base_class/test_astype.py
@@ -0,0 +1,21 @@
+import pytest
+
+from pandas import Index
+
+
+@pytest.mark.parametrize(
+    "from_type, to_type", [("float64", "int64"), ("int64", "float64")]
+)
+def test_astype_preserves_name(from_type, to_type):
+    idx = Index([1, 2], name="name", dtype=from_type).astype(to_type)
+
+    assert idx.name == "name"
+
+
+@pytest.mark.parametrize(
+    "from_type, to_type", [("float64", "int64"), ("int64", "float64")]
+)
+def test_copy_with_astype_preserves_name(from_type, to_type):
+    idx = Index([1, 2], name="name", dtype=from_type).copy(dtype=to_type)
+
+    assert idx.name == "name"
