000df3658969da153a31d973d011f0638296d113
Will Ayd <william.ayd@icloud.com>
2020-02-16 23:55:58 -0500

    cleanups

--- pandas/tests/frame/test_native.py
+++ pandas/tests/frame/test_native.py
@@ -1,7 +1,9 @@
 import ctypes
 
+import pandas as pd
+
 
 def test_func():
-    libc = ctypes.CDLL('pandas/_libs/ndframe_data.cpython-37m-darwin.so')
+    libc = ctypes.PyDLL('pandas/_libs/ndframe_data.cpython-37m-darwin.so')
     df = pd.DataFrame([[1, "2", 3., 4., 5], [6, "7", 8., 9, 10]])
     libc.PdFrameIter_New(ctypes.py_object(df), ctypes.c_int(0))
