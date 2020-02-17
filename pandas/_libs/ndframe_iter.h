c7404ff455db91c845cc7d4cb1fe340156db3e61
Will Ayd <william.ayd@icloud.com>
2020-02-16 17:34:51 -0800

    cleaned up header

--- pandas/_libs/ndframe_iter.h
+++ pandas/_libs/ndframe_iter.h
@@ -1,4 +1,17 @@
-// Given a dataframe and axis, returns an iterator
-// to go through each
+// TODO: could use better naming than "PdBlockIter"
+// showing that this is really "deconstructed" block data
+// in native form
 
+// Struct containing a pointer to len # of NDArrays
+// The order of each item in data should match the
+// order specified by the BlockManager
+typedef struct {
+  Py_ssize_t len;
+  PyArrayObject **data;
+} PdBlocksIter;
+
+// Provided a DataFrame and axis deconstructs the block
+// data to match the order represented by the BlockManager
+// Returns NULL on error
 PdIter *PdFrameIter_New(PyObject *df, int axis);
+
