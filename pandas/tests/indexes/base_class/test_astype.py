01b326c2c00c03d7b49c0dde05339f3276378e7a
Daniel Saxton <danielsaxton@DANIELs-MacBook-Pro.local>
2020-02-17 20:18:29 -0600

    Update tests

--- pandas/tests/indexes/base_class/test_astype.py
+++ pandas/tests/indexes/base_class/test_astype.py
@@ -4,20 +4,20 @@ from pandas import Index
 
 
 @pytest.mark.parametrize(
-    "from_type", ["int64", "uint64", "float64"],
+    "from_type", ["int64", "uint64", "float64", "category"],
 )
-@pytest.mark.parametrize("to_type", ["int64", "uint64", "float64"])
+@pytest.mark.parametrize("to_type", ["int64", "uint64", "float64", "category"])
 def test_astype_preserves_name(from_type, to_type):
-    idx = Index([1, 2], name="abc", dtype=from_type).astype(to_type)
+    idx = Index([], name="idx", dtype=from_type).astype(to_type)
 
-    assert idx.name == "abc"
+    assert idx.name == "idx"
 
 
 @pytest.mark.parametrize(
-    "from_type", ["int64", "uint64", "float64"],
+    "from_type", ["int64", "uint64", "float64", "category"],
 )
-@pytest.mark.parametrize("to_type", ["int64", "uint64", "float64"])
+@pytest.mark.parametrize("to_type", ["int64", "uint64", "float64", "category"])
 def test_copy_with_astype_preserves_name(from_type, to_type):
-    idx = Index([1, 2], name="abc", dtype=from_type).copy(dtype=to_type)
+    idx = Index([], name="idx", dtype=from_type).copy(dtype=to_type)
 
-    assert idx.name == "abc"
+    assert idx.name == "idx"
