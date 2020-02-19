0c107bdf930780b8adca31f64d8c4648dd2cc1d4
Daniel Saxton <2658661+dsaxton@users.noreply.github.com>
2020-02-18 19:30:08 -0600

    DOC: Update sort_index docs (#31898)

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -4732,8 +4732,9 @@ class DataFrame(NDFrame):
             and 1 identifies the columns.
         level : int or level name or list of ints or list of level names
             If not None, sort on values in specified index level(s).
-        ascending : bool, default True
-            Sort ascending vs. descending.
+        ascending : bool or list of bools, default True
+            Sort ascending vs. descending. When the index is a MultiIndex the
+            sort direction can be controlled for each level individually.
         inplace : bool, default False
             If True, perform operation in-place.
         kind : {'quicksort', 'mergesort', 'heapsort'}, default 'quicksort'
