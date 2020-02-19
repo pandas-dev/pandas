4651bd76e77631dedcd0e71d5e7caa38c48e29b0
Marco Gorelli <m.gorelli@ws-1808.seri.co.uk>
2020-02-19 11:55:29 +0000

    add test

--- pandas/tests/io/formats/test_info.py
+++ pandas/tests/io/formats/test_info.py
@@ -159,6 +159,25 @@ def test_info_memory():
     )
     assert result == expected
 
+    s = Series([1, 2], dtype="i8")
+    buf = StringIO()
+    s.info(buf=buf)
+    result = buf.getvalue()
+    bytes = float(s.memory_usage())
+    expected = textwrap.dedent(
+        f"""\
+    <class 'pandas.core.series.Series'>
+    RangeIndex: 2 entries, 0 to 1
+    Series name: None
+     #   Non-Null Count  Dtype
+    ---  --------------  -----
+     0   2 non-null      int64
+    dtypes: int64(1)
+    memory usage: {bytes} bytes
+    """
+    )
+    assert result == expected
+
 
 def test_info_wide():
     io = StringIO()
