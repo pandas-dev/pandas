de24c54f392aeff638974b75a1197e05d7624b97
jbrockmendel <jbrockmendel@gmail.com>
2020-02-18 18:23:42 -0800

    TST: parametrize

--- pandas/tests/arithmetic/test_timedelta64.py
+++ pandas/tests/arithmetic/test_timedelta64.py
@@ -522,19 +522,35 @@ class TestTimedelta64ArithmeticUnsorted:
     # -------------------------------------------------------------
     # Binary operations TimedeltaIndex and timedelta-like
 
-    def test_tdi_iadd_timedeltalike(self, two_hours):
+    def test_tdi_iadd_timedeltalike(self, two_hours, box_with_array):
         # only test adding/sub offsets as + is now numeric
         rng = timedelta_range("1 days", "10 days")
         expected = timedelta_range("1 days 02:00:00", "10 days 02:00:00", freq="D")
-        rng += two_hours
-        tm.assert_index_equal(rng, expected)
 
-    def test_tdi_isub_timedeltalike(self, two_hours):
+        rng = tm.box_expected(rng, box_with_array)
+        expected = tm.box_expected(expected, box_with_array)
+
+        orig_rng = rng
+        rng += two_hours
+        tm.assert_equal(rng, expected)
+        if box_with_array is not pd.Index:
+            # Check that operation is actually inplace
+            tm.assert_equal(orig_rng, expected)
+
+    def test_tdi_isub_timedeltalike(self, two_hours, box_with_array):
         # only test adding/sub offsets as - is now numeric
         rng = timedelta_range("1 days", "10 days")
         expected = timedelta_range("0 days 22:00:00", "9 days 22:00:00")
+
+        rng = tm.box_expected(rng, box_with_array)
+        expected = tm.box_expected(expected, box_with_array)
+
+        orig_rng = rng
         rng -= two_hours
-        tm.assert_index_equal(rng, expected)
+        tm.assert_equal(rng, expected)
+        if box_with_array is not pd.Index:
+            # Check that operation is actually inplace
+            tm.assert_equal(orig_rng, expected)
 
     # -------------------------------------------------------------
 
