d2230fd6d3c106ba5c672071c0d671931ddd70e3
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-02-20 16:51:28 +0100

    add object to empty reduction test case

--- pandas/tests/reductions/test_reductions.py
+++ pandas/tests/reductions/test_reductions.py
@@ -533,7 +533,7 @@ class TestSeriesReductions:
         res = nanops.nansum(arr, axis=1)
         assert np.isinf(res).all()
 
-    @pytest.mark.parametrize("dtype", ["float64", "Int64", "boolean"])
+    @pytest.mark.parametrize("dtype", ["float64", "Int64", "boolean", "object"])
     @pytest.mark.parametrize("use_bottleneck", [True, False])
     @pytest.mark.parametrize("method, unit", [("sum", 0.0), ("prod", 1.0)])
     def test_empty(self, method, unit, use_bottleneck, dtype):
