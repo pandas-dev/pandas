4b2ab34be2fb87e5886a1368e19752978068bc76
jbrockmendel <jbrockmendel@gmail.com>
2020-02-20 14:19:14 -0800

    TST: show_versions test with unmerged commits

--- pandas/tests/util/test_show_versions.py
+++ pandas/tests/util/test_show_versions.py
@@ -1,8 +1,26 @@
 import re
 
+import pytest
+
 import pandas as pd
 
 
+@pytest.mark.filterwarnings(
+    # openpyxl
+    "ignore:defusedxml.lxml is no longer supported:DeprecationWarning"
+)
+@pytest.mark.filterwarnings(
+    # html5lib
+    "ignore:Using or importing the ABCs from:DeprecationWarning"
+)
+@pytest.mark.filterwarnings(
+    # fastparquet
+    "ignore:pandas.core.index is deprecated:FutureWarning"
+)
+@pytest.mark.filterwarnings(
+    # pandas_datareader
+    "ignore:pandas.util.testing is deprecated:FutureWarning"
+)
 def test_show_versions(capsys):
     # gh-32041
     pd.show_versions()
@@ -13,7 +31,11 @@ def test_show_versions(capsys):
     assert "INSTALLED VERSIONS" in result
 
     # check full commit hash
-    assert re.search(r"commit\s*:\s[0-9a-f]{40}\n", result)
+    if not re.search(r"commit\s*:\s[0-9a-f]{40}\n", result):
+        # GH#32120  If test is being run in a branch that has uncommited
+        #  changes, then we will not see the full commit hash, but this
+        #  should show up in the pandas version number.
+        assert re.search(r"pandas\s*: .*\.dirty\n", result)
 
     # check required dependency
     assert re.search(r"numpy\s*:\s([0-9\.\+a-f]|dev)+\n", result)
