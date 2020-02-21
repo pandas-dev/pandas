f96bbb5b885eeaa8bed106534d89c27f7609410a
Simon Hawkins <simonjayhawkins@gmail.com>
2020-02-21 14:31:17 +0000

    TST: add test for get_loc on tz-aware DatetimeIndex

--- pandas/tests/indexes/datetimes/test_indexing.py
+++ pandas/tests/indexes/datetimes/test_indexing.py
@@ -452,6 +452,17 @@ class TestGetLoc:
         with pytest.raises(KeyError, match="2000"):
             index.get_loc("1/1/2000")
 
+    def test_get_loc_tz_aware(self):
+        # https://github.com/pandas-dev/pandas/issues/32140
+        dti = pd.date_range(
+            pd.Timestamp("2019-12-12 00:00:00", tz="US/Eastern"),
+            pd.Timestamp("2019-12-13 00:00:00", tz="US/Eastern"),
+            freq="5s",
+        )
+        key = pd.Timestamp("2019-12-12 10:19:25", tz="US/Eastern")
+        result = dti.get_loc(key, method="nearest")
+        assert result == 7433
+
 
 class TestDatetimeIndex:
     @pytest.mark.parametrize(
