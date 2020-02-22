628dfba239865adc09c94108b288bcb60c619950
jbrockmendel <jbrockmendel@gmail.com>
2020-02-22 08:08:23 -0800

    BUG: Fix incorrect _is_scalar_access check in iloc (#32085)

--- pandas/core/indexing.py
+++ pandas/core/indexing.py
@@ -1414,10 +1414,6 @@ class _iLocIndexer(_LocationIndexer):
             if not is_integer(k):
                 return False
 
-            ax = self.obj.axes[i]
-            if not ax.is_unique:
-                return False
-
         return True
 
     def _validate_integer(self, key: int, axis: int) -> None:
