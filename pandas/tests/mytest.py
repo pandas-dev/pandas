02326fb552d0eec3d7d6fcbbb1f62c8c274991d0
MarcoGorelli <m.e.gorelli@gmail.com>
2020-02-22 11:29:05 +0000

    remove file

--- pandas/tests/mytest.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import pytest
-
-@pytest.mark.xfail(strict=True)
-def test():
-    index = pd.period_range(start='2018-01', periods=24, freq='M')
-    periodSerie = pd.Series(range(24),index=index)
-    periodSerie.index.name = 'Month'
-    periodSerie.groupby(periodSerie.index.month).sum()
