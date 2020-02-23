1ee1b10829b45cdbfeee694c56b8be015b6dfd0f
Daniel Saxton <dsaxton@pm.me>
2020-02-23 09:05:43 -0600

    Set index name

--- pandas/tests/indexes/base_class/test_astype.py
+++ pandas/tests/indexes/base_class/test_astype.py
@@ -3,6 +3,7 @@ import pytest
 
 @pytest.mark.parametrize("dtype", ["int64", "uint64", "float64", "category"])
 def test_astype_preserves_name(indices, dtype):
+    indices.name = "idx"
     try:
         result = indices.astype(dtype)
     except (ValueError, TypeError, NotImplementedError):
@@ -13,6 +14,7 @@ def test_astype_preserves_name(indices, dtype):
 
 @pytest.mark.parametrize("dtype", ["int64", "uint64", "float64", "category"])
 def test_astype_with_copy_preserves_name(indices, dtype):
+    indices.name = "idx"
     try:
         result = indices.copy(dtype=dtype)
     except (ValueError, TypeError, NotImplementedError):
