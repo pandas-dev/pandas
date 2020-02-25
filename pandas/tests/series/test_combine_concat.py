e99e77c7559d5b891bc9f7136b4976b35aae0565
jbrockmendel <jbrockmendel@gmail.com>
2020-02-24 16:02:35 -0800

    method-specific files for combine, update

--- pandas/tests/series/test_combine_concat.py
+++ pandas/tests/series/test_combine_concat.py
@@ -1,76 +1,10 @@
 import numpy as np
-import pytest
 
 import pandas as pd
-from pandas import DataFrame, Series
-import pandas._testing as tm
+from pandas import Series
 
 
 class TestSeriesCombine:
-    def test_combine_scalar(self):
-        # GH 21248
-        # Note - combine() with another Series is tested elsewhere because
-        # it is used when testing operators
-        s = pd.Series([i * 10 for i in range(5)])
-        result = s.combine(3, lambda x, y: x + y)
-        expected = pd.Series([i * 10 + 3 for i in range(5)])
-        tm.assert_series_equal(result, expected)
-
-        result = s.combine(22, lambda x, y: min(x, y))
-        expected = pd.Series([min(i * 10, 22) for i in range(5)])
-        tm.assert_series_equal(result, expected)
-
-    def test_update(self):
-        s = Series([1.5, np.nan, 3.0, 4.0, np.nan])
-        s2 = Series([np.nan, 3.5, np.nan, 5.0])
-        s.update(s2)
-
-        expected = Series([1.5, 3.5, 3.0, 5.0, np.nan])
-        tm.assert_series_equal(s, expected)
-
-        # GH 3217
-        df = DataFrame([{"a": 1}, {"a": 3, "b": 2}])
-        df["c"] = np.nan
-
-        df["c"].update(Series(["foo"], index=[0]))
-        expected = DataFrame(
-            [[1, np.nan, "foo"], [3, 2.0, np.nan]], columns=["a", "b", "c"]
-        )
-        tm.assert_frame_equal(df, expected)
-
-    @pytest.mark.parametrize(
-        "other, dtype, expected",
-        [
-            # other is int
-            ([61, 63], "int32", pd.Series([10, 61, 12], dtype="int32")),
-            ([61, 63], "int64", pd.Series([10, 61, 12])),
-            ([61, 63], float, pd.Series([10.0, 61.0, 12.0])),
-            ([61, 63], object, pd.Series([10, 61, 12], dtype=object)),
-            # other is float, but can be cast to int
-            ([61.0, 63.0], "int32", pd.Series([10, 61, 12], dtype="int32")),
-            ([61.0, 63.0], "int64", pd.Series([10, 61, 12])),
-            ([61.0, 63.0], float, pd.Series([10.0, 61.0, 12.0])),
-            ([61.0, 63.0], object, pd.Series([10, 61.0, 12], dtype=object)),
-            # others is float, cannot be cast to int
-            ([61.1, 63.1], "int32", pd.Series([10.0, 61.1, 12.0])),
-            ([61.1, 63.1], "int64", pd.Series([10.0, 61.1, 12.0])),
-            ([61.1, 63.1], float, pd.Series([10.0, 61.1, 12.0])),
-            ([61.1, 63.1], object, pd.Series([10, 61.1, 12], dtype=object)),
-            # other is object, cannot be cast
-            ([(61,), (63,)], "int32", pd.Series([10, (61,), 12])),
-            ([(61,), (63,)], "int64", pd.Series([10, (61,), 12])),
-            ([(61,), (63,)], float, pd.Series([10.0, (61,), 12.0])),
-            ([(61,), (63,)], object, pd.Series([10, (61,), 12])),
-        ],
-    )
-    def test_update_dtypes(self, other, dtype, expected):
-
-        s = Series([10, 11, 12], dtype=dtype)
-        other = Series(other, index=[1, 3])
-        s.update(other)
-
-        tm.assert_series_equal(s, expected)
-
     def test_concat_empty_series_dtypes_roundtrips(self):
 
         # round-tripping with self & like self
