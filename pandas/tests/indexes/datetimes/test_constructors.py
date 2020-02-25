42566426977bbcfb697f6e59c19d471d72fa2d16
Alexander Kirko <alexander.kirko@gmail.com>
2020-02-25 09:52:05 +0300

    TST: add test for invalid fold raise

--- pandas/tests/indexes/datetimes/test_constructors.py
+++ pandas/tests/indexes/datetimes/test_constructors.py
@@ -961,6 +961,14 @@ class TestTimeSeries:
         tm.assert_numpy_array_equal(idx.values, expected.values)
 
 
+def test_timestamp_constructor_invalid_fold_raise():
+    # Test for #25057
+    # Valid fold values are only [None, 0, 1]
+    msg = "Valid values for the fold argument are None, 0, or 1."
+    with pytest.raises(ValueError, match=msg):
+        Timestamp(123, fold=2)
+
+
 def test_timestamp_constructor_pytz_fold_raise():
     # Test for #25057
     # pytz doesn't support fold. Check that we raise
