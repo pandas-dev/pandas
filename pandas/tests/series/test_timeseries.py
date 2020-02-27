4d9315d09c54b34f583d5dd62673ee6c57a2e123
jbrockmendel <jbrockmendel@gmail.com>
2020-02-27 09:46:40 -0800

    Move misplaced get_level_values test

--- pandas/tests/series/test_timeseries.py
+++ pandas/tests/series/test_timeseries.py
@@ -290,17 +290,6 @@ class TestTimeSeries:
         assert isinstance(s[0], Timestamp)
         assert s[0] == dates[0][0]
 
-    def test_get_level_values_box(self):
-        from pandas import MultiIndex
-
-        dates = date_range("1/1/2000", periods=4)
-        levels = [dates, [0, 1]]
-        codes = [[0, 0, 1, 1, 2, 2, 3, 3], [0, 1, 0, 1, 0, 1, 0, 1]]
-
-        index = MultiIndex(levels=levels, codes=codes)
-
-        assert isinstance(index.get_level_values(0)[0], Timestamp)
-
     def test_view_tz(self):
         # GH#24024
         ser = pd.Series(pd.date_range("2000", periods=4, tz="US/Central"))
