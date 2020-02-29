c37ba71da6d3d6c8227acb6b930445a7b1b7d181
Hori75 <williamhori75@gmail.com>
2020-02-29 13:07:44 +0700

    DOC: fix PR01 error of pandas.DatetimeIndex

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -94,6 +94,7 @@ class DatetimeIndex(DatetimeTimedeltaMixin):
         'infer' can be passed in order to set the frequency of the index as the
         inferred frequency upon creation.
     tz : pytz.timezone or dateutil.tz.tzfile
+        Timezone of the datetime.
     ambiguous : 'infer', bool-ndarray, 'NaT', default 'raise'
         When clocks moved backward due to DST, ambiguous times may arise.
         For example in Central European Time (UTC+01), when going from 03:00
