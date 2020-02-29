7275ccd4a48b116f6c3d69f2791a79fee9ddf105
Dipta Laksmana <dipta004@gmail.com>
2020-02-29 04:52:20 +0000

    Fix PR06 and PR01 in pandas.core.groupby.GroupBy.pipe

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -212,14 +212,14 @@ which is much more readable.
 
 Parameters
 ----------
-func : callable or tuple of (callable, string)
+func : callable or tuple of (callable, str)
     Function to apply to this %(klass)s object or, alternatively,
     a `(callable, data_keyword)` tuple where `data_keyword` is a
     string indicating the keyword of `callable` that expects the
     %(klass)s object.
-args : iterable, optional
+*args : iterable, optional
        Positional arguments passed into `func`.
-kwargs : dict, optional
+**kwargs : dict, optional
          A dictionary of keyword arguments passed into `func`.
 
 Returns
