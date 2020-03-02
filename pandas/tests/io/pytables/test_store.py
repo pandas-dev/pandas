22808a0c2e3cc4b3689f81889a4294193992962f
Robert de Vries <rhdv@xs4all.nl>
2020-03-02 23:53:51 +0100

    HDFStore: Fix empty result of keys() method on non-pandas hdf5 file
    An additional kind parameter has been added that defaults to pandas
    original behavior, but with 'tables' value gives you the list of
    non-pandas tables in the file

--- pandas/tests/io/pytables/test_store.py
+++ pandas/tests/io/pytables/test_store.py
@@ -341,6 +341,29 @@ class TestHDFStore:
         # checksums are NOT same if track_time = True
         assert checksum_0_tt_true != checksum_1_tt_true
 
+    def test_non_pandas_keys(self, setup_path):
+
+        class Table1(tables.IsDescription):
+            value1 = tables.Float32Col()
+
+        class Table2(tables.IsDescription):
+            value2 = tables.Float32Col()
+
+        class Table3(tables.IsDescription):
+            value3 = tables.Float32Col()
+
+        with ensure_clean_path(setup_path) as path:
+            with tables.open_file(path, mode="w") as h5file:
+                group = h5file.create_group("/", "group")
+                h5file.create_table(group, "table1", Table1, "Table 1")
+                h5file.create_table(group, "table2", Table2, "Table 2")
+                h5file.create_table(group, "table3", Table3, "Table 3")
+            with HDFStore(path) as store:
+                assert len(store.keys(kind="tables")) == 3
+                expected = {"/group/table1", "/group/table2", "/group/table3"}
+                assert set(store.keys(kind="tables")) == expected
+                assert set(store) == set()
+
     def test_keys_ignore_hdf_softlink(self, setup_path):
 
         # GH 20523
