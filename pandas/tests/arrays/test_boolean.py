80c0f5760a5c75995608f680d0bbc21bdb0fd806
ShilpaSugan <51447420+ShilpaSugan@users.noreply.github.com>
2020-03-03 12:31:30 +0000

    TST: add message check to pytest.raises (tests/arrays/test_boolean.py) (#32397)

--- pandas/tests/arrays/test_boolean.py
+++ pandas/tests/arrays/test_boolean.py
@@ -248,7 +248,11 @@ def test_coerce_to_numpy_array():
     tm.assert_numpy_array_equal(result, expected)
     # with missing values will raise error
     arr = pd.array([True, False, None], dtype="boolean")
-    with pytest.raises(ValueError):
+    msg = (
+        "cannot convert to 'bool'-dtype NumPy array with missing values. "
+        "Specify an appropriate 'na_value' for this dtype."
+    )
+    with pytest.raises(ValueError, match=msg):
         np.array(arr, dtype="bool")
 
 
