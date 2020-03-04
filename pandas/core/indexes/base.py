609b8da3506dd9793a150f59bb8691453d12c947
Rafif <rafif.elfazri@gmail.com>
2020-03-04 22:41:31 +0700

    Fix PR08, RT02, RT03, and SA01 on pandas.Index.fillna (#32355)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -2129,13 +2129,18 @@ class Index(IndexOpsMixin, PandasObject):
             Scalar value to use to fill holes (e.g. 0).
             This value cannot be a list-likes.
         downcast : dict, default is None
-            a dict of item->dtype of what to downcast if possible,
+            A dict of item->dtype of what to downcast if possible,
             or the string 'infer' which will try to downcast to an appropriate
             equal type (e.g. float64 to int64 if possible).
 
         Returns
         -------
-        filled : Index
+        Index
+
+        See Also
+        --------
+        DataFrame.fillna : Fill NaN values of a DataFrame.
+        Series.fillna : Fill NaN Values of a Series.
         """
         self._assert_can_do_op(value)
         if self.hasnans:
