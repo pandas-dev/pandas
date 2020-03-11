5d73f6031bc16f2d2a60f45a05f2cf8206886308
gfyoung <gfyoung17@gmail.com>
2020-03-10 19:22:01 -0700

    REG: Restore read_csv function for some file-likes (#32577)
    Restores behavior down to the fact that the
    Python engine cannot handle NamedTemporaryFile.
    
    Closes https://github.com/pandas-dev/pandas/issues/31819
    
    Credit to @sasanquaneuf for originating idea.

--- pandas/io/parsers.py
+++ pandas/io/parsers.py
@@ -5,7 +5,7 @@ Module contains tools for processing files into DataFrames or other objects
 from collections import abc, defaultdict
 import csv
 import datetime
-from io import BufferedIOBase, RawIOBase, StringIO, TextIOWrapper
+from io import StringIO, TextIOWrapper
 import re
 import sys
 from textwrap import fill
@@ -1870,7 +1870,7 @@ class CParserWrapper(ParserBase):
 
             # Handle the file object with universal line mode enabled.
             # We will handle the newline character ourselves later on.
-            if isinstance(src, (BufferedIOBase, RawIOBase)):
+            if hasattr(src, "read") and not hasattr(src, "encoding"):
                 src = TextIOWrapper(src, encoding=encoding, newline="")
 
             kwds["encoding"] = "utf-8"
