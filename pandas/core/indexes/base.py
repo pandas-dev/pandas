bbdbac2cbb6033b31df96d17f582931ba63cdd2c
jbrockmendel <jbrockmendel@gmail.com>
2020-03-10 19:40:13 -0700

    REF: Remove BlockManager.rename_axis (#32349)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -4753,6 +4753,27 @@ class Index(IndexOpsMixin, PandasObject):
 
         return Index(new_values, **attributes)
 
+    # TODO: De-duplicate with map, xref GH#32349
+    def _transform_index(self, func, level=None) -> "Index":
+        """
+        Apply function to all values found in index.
+
+        This includes transforming multiindex entries separately.
+        Only apply function to one level of the MultiIndex if level is specified.
+        """
+        if isinstance(self, ABCMultiIndex):
+            if level is not None:
+                items = [
+                    tuple(func(y) if i == level else y for i, y in enumerate(x))
+                    for x in self
+                ]
+            else:
+                items = [tuple(func(y) for y in x) for x in self]
+            return type(self).from_tuples(items, names=self.names)
+        else:
+            items = [func(x) for x in self]
+            return Index(items, name=self.name, tupleize_cols=False)
+
     def isin(self, values, level=None):
         """
         Return a boolean array where the index values are in `values`.
