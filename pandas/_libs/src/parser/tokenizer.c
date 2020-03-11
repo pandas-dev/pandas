0f793ef8e560e534b20cdea95a95604923d8d29a
Robert de Vries <rhdv@xs4all.nl>
2020-03-11 18:15:33 +0100

    Skip the remainder of stream if there are no word_deletions

--- pandas/_libs/src/parser/tokenizer.c
+++ pandas/_libs/src/parser/tokenizer.c
@@ -1193,7 +1193,7 @@ int parser_consume_rows(parser_t *self, size_t nrows) {
         char_count = (self->word_starts[word_deletions - 1] +
                       strlen(self->words[word_deletions - 1]) + 1);
     } else {
-        char_count = 1;
+        char_count = self->stream_len;
     }
 
     TRACE(("parser_consume_rows: Deleting %d words, %d chars\n", word_deletions,
