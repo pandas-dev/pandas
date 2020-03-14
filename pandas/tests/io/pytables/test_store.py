8a374de828a3088dcf95a7433f7ed12ac1497fae
Radek Benes <radek.benes@gmail.com>
2020-03-14 15:19:02 +0100

    Unify

--- pandas/tests/io/pytables/test_store.py
+++ pandas/tests/io/pytables/test_store.py
@@ -306,7 +306,7 @@ class TestHDFStore:
 
         def checksum(filename, hash_factory=hashlib.md5, chunk_num_blocks=128):
             h = hash_factory()
-            with open(filename, 'rb') as f:
+            with open(filename, "rb") as f:
                 for chunk in iter(lambda: f.read(chunk_num_blocks * h.block_size), b''):
                     h.update(chunk)
             return h.digest()
@@ -319,7 +319,7 @@ class TestHDFStore:
                 hdf.put(
                     "table",
                     df,
-                    format='table',
+                    format="table",
                     data_columns=True,
                     index=None,
                     track_times=track_times,
