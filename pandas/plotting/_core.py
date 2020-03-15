2bfbe78149008cd509703cf4f0fa565a6f9559f3
Kaiqi <kaiqi@kth.se>
2020-03-15 17:23:57 +0100

    change figsize

--- pandas/plotting/_core.py
+++ pandas/plotting/_core.py
@@ -1220,7 +1220,7 @@ class PlotAccessor(PandasObject):
 
             >>> age_list = [8, 10, 12, 14, 72, 74, 76, 78, 20, 25, 30, 35, 60, 85]
             >>> df = pd.DataFrame({"gender": list("MMMMMMMMFFFFFF"), "age": age_list})
-            >>> ax = df.plot.hist(column=["age"], by="gender")
+            >>> ax = df.plot.hist(column=["age"], by="gender", figsize=(10, 8))
         """
         return self(kind="hist", by=by, bins=bins, **kwargs)
 
