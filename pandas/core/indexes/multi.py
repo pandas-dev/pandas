e957a3709aaa1e74bf15e38a1a0693e4765d7fff
Raisa Dzhamtyrova <raya@MacBook-Pro.home>
2020-03-15 10:32:18 +0000

    put the explanation above the examples

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -1628,15 +1628,16 @@ class MultiIndex(Index):
 
         Examples
         --------
+        In the below examples, the first level of the MultiIndex is sorted because
+        a<b<c, so there is no need to look at the next level.
+
         >>> pd.MultiIndex.from_arrays([['a', 'b', 'c'], ['d', 'e', 'f']]).is_lexsorted()
         True
         >>> pd.MultiIndex.from_arrays([['a', 'b', 'c'], ['d', 'f', 'e']]).is_lexsorted()
         True
 
-        In the above examples, the first level of MultiIndex is sorted because a<b<c,
-        so there is not need to look at the next level.
         In case there is a tie, the lexicographical sorting looks
-        at the next level of MultiIndex.
+        at the next level of the MultiIndex.
 
         >>> pd.MultiIndex.from_arrays([[0, 1, 1], ['a', 'b', 'c']]).is_lexsorted()
         True
