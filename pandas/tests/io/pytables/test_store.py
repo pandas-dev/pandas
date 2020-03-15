2f611401e52a9e737e4e14800351518b0c660901
Radek Benes <radek.benes@gmail.com>
2020-03-15 13:45:51 +0100

    Black

--- pandas/tests/io/pytables/test_store.py
+++ pandas/tests/io/pytables/test_store.py
@@ -307,7 +307,7 @@ class TestHDFStore:
         def checksum(filename, hash_factory=hashlib.md5, chunk_num_blocks=128):
             h = hash_factory()
             with open(filename, "rb") as f:
-                for chunk in iter(lambda: f.read(chunk_num_blocks * h.block_size), b''):
+                for chunk in iter(lambda: f.read(chunk_num_blocks * h.block_size), b""):
                     h.update(chunk)
             return h.digest()
 
