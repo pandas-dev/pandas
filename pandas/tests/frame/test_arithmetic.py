7a8217395b9141834209876f3266c72f318005f9
jbrockmendel <jbrockmendel@gmail.com>
2020-03-16 10:11:21 -0700

    update tests

--- pandas/tests/frame/test_arithmetic.py
+++ pandas/tests/frame/test_arithmetic.py
@@ -833,3 +833,17 @@ def test_pow_with_realignment():
     result = left ** right
     expected = pd.DataFrame({"A": [np.nan, 1.0, np.nan]})
     tm.assert_frame_equal(result, expected)
+
+
+# TODO: move to tests.arithmetic and parametrize
+def test_pow_nan_with_zero():
+    left = pd.DataFrame({"A": [np.nan, np.nan, np.nan]})
+    right = pd.DataFrame({"A": [0, 0, 0]})
+
+    expected = pd.DataFrame({"A": [1.0, 1.0, 1.0]})
+
+    result = left ** right
+    tm.assert_frame_equal(result, expected)
+
+    result = left["A"] ** right["A"]
+    tm.assert_series_equal(result, expected["A"])
