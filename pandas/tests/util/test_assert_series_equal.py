f6a29a8efe36a4223eb9ab26d88e7c993a2e9969
Daniel Saxton <2658661+dsaxton@users.noreply.github.com>
2020-03-17 14:33:39 -0500

    BUG: Respect check_dtype in assert_series_equal (#32757)

--- pandas/tests/util/test_assert_series_equal.py
+++ pandas/tests/util/test_assert_series_equal.py
@@ -1,5 +1,6 @@
 import pytest
 
+import pandas as pd
 from pandas import Categorical, DataFrame, Series
 import pandas._testing as tm
 
@@ -194,6 +195,40 @@ ordered=False\\)"""
             tm.assert_series_equal(s1, s2, check_categorical=check_categorical)
     else:
         _assert_series_equal_both(s1, s2, check_categorical=check_categorical)
+
+
+def test_assert_series_equal_extension_dtype_mismatch():
+    # https://github.com/pandas-dev/pandas/issues/32747
+    left = Series(pd.array([1, 2, 3], dtype="Int64"))
+    right = left.astype(int)
+
+    msg = """Attributes of Series are different
+
+Attribute "dtype" are different
+\\[left\\]:  Int64
+\\[right\\]: int[32|64]"""
+
+    tm.assert_series_equal(left, right, check_dtype=False)
+
+    with pytest.raises(AssertionError, match=msg):
+        tm.assert_series_equal(left, right, check_dtype=True)
+
+
+def test_assert_series_equal_interval_dtype_mismatch():
+    # https://github.com/pandas-dev/pandas/issues/32747
+    left = Series([pd.Interval(0, 1)], dtype="interval")
+    right = left.astype(object)
+
+    msg = """Attributes of Series are different
+
+Attribute "dtype" are different
+\\[left\\]:  interval\\[int64\\]
+\\[right\\]: object"""
+
+    tm.assert_series_equal(left, right, check_dtype=False)
+
+    with pytest.raises(AssertionError, match=msg):
+        tm.assert_series_equal(left, right, check_dtype=True)
 
 
 def test_series_equal_series_type():
