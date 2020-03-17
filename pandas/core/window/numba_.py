b722731813f8339ebbec36808b515be9a68ff23c
Matt Roeschke <mroeschke@housecanary.com>
2020-03-17 09:17:22 -0700

    isort

--- pandas/core/window/numba_.py
+++ pandas/core/window/numba_.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from pandas._typing import Scalar
 from pandas.compat._optional import import_optional_dependency
+
 from pandas.core.numba_ import (
     check_kwargs_and_nopython,
     get_jit_arguments,
