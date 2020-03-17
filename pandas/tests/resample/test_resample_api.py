ef2ebc3202766a59c5c0c09847619d240272e3eb
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2020-03-17 00:57:53 -0700

    Backport PR #32758: BUG: resample.agg with read-only data (#32765)
    Co-authored-by: jbrockmendel <jbrockmendel@gmail.com>

--- pandas/tests/resample/test_resample_api.py
+++ pandas/tests/resample/test_resample_api.py
@@ -580,3 +580,27 @@ def test_agg_with_datetime_index_list_agg_func(col_name):
         columns=pd.MultiIndex(levels=[[col_name], ["mean"]], codes=[[0], [0]]),
     )
     tm.assert_frame_equal(result, expected)
+
+
+def test_resample_agg_readonly():
+    # GH#31710 cython needs to allow readonly data
+    index = pd.date_range("2020-01-01", "2020-01-02", freq="1h")
+    arr = np.zeros_like(index)
+    arr.setflags(write=False)
+
+    ser = pd.Series(arr, index=index)
+    rs = ser.resample("1D")
+
+    expected = pd.Series([pd.Timestamp(0), pd.Timestamp(0)], index=index[::24])
+
+    result = rs.agg("last")
+    tm.assert_series_equal(result, expected)
+
+    result = rs.agg("first")
+    tm.assert_series_equal(result, expected)
+
+    result = rs.agg("max")
+    tm.assert_series_equal(result, expected)
+
+    result = rs.agg("min")
+    tm.assert_series_equal(result, expected)
