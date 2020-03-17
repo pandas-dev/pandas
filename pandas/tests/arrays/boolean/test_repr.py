189d53dd3fbe72412ba275184aa38126221f289b
Daniel Saxton <dsaxton@pm.me>
2020-03-17 14:46:21 -0500

    Add new test files

---
+++ pandas/tests/arrays/boolean/test_repr.py
@@ -0,0 +1,17 @@
+import numpy as np
+import pytest
+
+import pandas as pd
+import pandas._testing as tm
+
+
+def test_repr():
+    df = pd.DataFrame({"A": pd.array([True, False, None], dtype="boolean")})
+    expected = "       A\n0   True\n1  False\n2   <NA>"
+    assert repr(df) == expected
+
+    expected = "0     True\n1    False\n2     <NA>\nName: A, dtype: boolean"
+    assert repr(df.A) == expected
+
+    expected = "<BooleanArray>\n[True, False, <NA>]\nLength: 3, dtype: boolean"
+    assert repr(df.A.array) == expected
