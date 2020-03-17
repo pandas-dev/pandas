bb003fe77cf90ed58690916b83c59b01352d7b11
Daniel Saxton <dsaxton@pm.me>
2020-03-17 14:56:32 -0500

    Don't import unused

--- pandas/tests/arrays/boolean/test_repr.py
+++ pandas/tests/arrays/boolean/test_repr.py
@@ -1,8 +1,4 @@
-import numpy as np
-import pytest
-
 import pandas as pd
-import pandas._testing as tm
 
 
 def test_repr():
--- pandas/tests/arrays/boolean/test_unary_ops.py
+++ pandas/tests/arrays/boolean/test_unary_ops.py
@@ -1,6 +1,3 @@
-import numpy as np
-import pytest
-
 import pandas as pd
 import pandas._testing as tm
 
