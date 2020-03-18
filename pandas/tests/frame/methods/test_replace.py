617f03c704d5431b29d8c1bc32615f28e7086eb4
Derek McCammond <dmm545@drexel.edu>
2020-03-17 21:51:57 -0400

    Fixed bare pytest raises in test_replace.py

--- pandas/tests/frame/methods/test_replace.py
+++ pandas/tests/frame/methods/test_replace.py
@@ -1308,7 +1308,11 @@ class TestDataFrameReplace:
         expected["b"] = expected["b"].cat.set_categories([1, 2, 3])
         result = df.replace(replace_dict, 3)
         tm.assert_frame_equal(result, expected)
-        with pytest.raises(AssertionError):
+        msg = (
+            'Attributes of DataFrame.iloc[:, 0] '
+            '(column name="a") are different'
+        )
+        with pytest.raises(AssertionError, match=re.escape(msg)):
             # ensure non-inplace call does not affect original
             tm.assert_frame_equal(df, expected)
         df.replace(replace_dict, 3, inplace=True)
