a93e56d92146af9b2a38dff02260767644838563
MomIsBestFriend <50263213+MomIsBestFriend@users.noreply.github.com>
2020-03-18 04:35:09 +0200

    BLD: Suppressing warnings when compiling pandas/_libs/writers (#32795)

--- pandas/_libs/writers.pyx
+++ pandas/_libs/writers.pyx
@@ -36,7 +36,7 @@ def write_csv_rows(
     """
     # In crude testing, N>100 yields little marginal improvement
     cdef:
-        Py_ssize_t i, j, k = len(data_index), N = 100, ncols = len(cols)
+        Py_ssize_t i, j = 0, k = len(data_index), N = 100, ncols = len(cols)
         list rows
 
     # pre-allocate rows
