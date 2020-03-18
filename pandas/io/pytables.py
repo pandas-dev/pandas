f1ce70913aa9fe45362be77e73e24a497627aaf8
Robert de Vries <rhdv@xs4all.nl>
2020-03-18 20:50:35 +0100

    Fix versionadded tag 3rd try

--- pandas/io/pytables.py
+++ pandas/io/pytables.py
@@ -587,12 +587,13 @@ class HDFStore:
         Parameters
         ----------
 
-        .. versionadded:: 1.1.0
 
         include : str, default 'pandas'
                 When kind equals 'pandas' return pandas objects
                 When kind equals 'native' return native HDF5 Table objects
                 Otherwise fail with a ValueError
+
+                .. versionadded:: 1.1.0
 
         Returns
         -------
