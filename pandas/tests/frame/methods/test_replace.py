5546949d3e9f3fe32357782d935506a1aad6695f
Derek McCammond <dmm545@drexel.edu>
2020-03-19 00:00:15 -0400

    Fixed the issues with Black that were brought up by the linter test.

--- pandas/tests/frame/methods/test_replace.py
+++ pandas/tests/frame/methods/test_replace.py
@@ -1308,7 +1308,7 @@ class TestDataFrameReplace:
         expected["b"] = expected["b"].cat.set_categories([1, 2, 3])
         result = df.replace(replace_dict, 3)
         tm.assert_frame_equal(result, expected)
-        msg = "Attributes of DataFrame.iloc[:, 0] " '(column name="a") are different'
+        msg = 'Attributes of DataFrame.iloc[:, 0] (column name="a") are different'
         with pytest.raises(AssertionError, match=re.escape(msg)):
             # ensure non-inplace call does not affect original
             tm.assert_frame_equal(df, expected)
