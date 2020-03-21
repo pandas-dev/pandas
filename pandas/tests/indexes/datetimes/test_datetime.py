a8a0f42728959bf21c2d6b2251d76c06f10f64cc
JDkuba <34447984+JDkuba@users.noreply.github.com>
2020-03-21 21:19:04 +0100

    TST: add test for non UTC datetime split #14042 (#32866)

--- pandas/tests/indexes/datetimes/test_datetime.py
+++ pandas/tests/indexes/datetimes/test_datetime.py
@@ -425,3 +425,11 @@ class TestDatetimeIndex:
             ((2018,), range(1, 7)), names=[name, name]
         )
         tm.assert_index_equal(index, exp_index)
+
+    def test_split_non_utc(self):
+        # GH 14042
+        indices = pd.date_range("2016-01-01 00:00:00+0200", freq="S", periods=10)
+        result = np.split(indices, indices_or_sections=[])[0]
+        expected = indices.copy()
+        expected._set_freq(None)
+        tm.assert_index_equal(result, expected)
