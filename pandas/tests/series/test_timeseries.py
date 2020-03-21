b436cc11bd88a322d71dc5478c1e8f84d2699416
jbrockmendel <jbrockmendel@gmail.com>
2020-03-21 09:22:33 -0700

    parametrize

--- pandas/tests/series/test_timeseries.py
+++ pandas/tests/series/test_timeseries.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import pandas as pd
 from pandas import DataFrame, DatetimeIndex, Series, date_range, timedelta_range
@@ -130,18 +131,25 @@ class TestTimeSeries:
         )
         tm.assert_series_equal(result, expected)
 
+    @pytest.mark.parametrize("tz", [None, "US/Central"])
+    def test_asarray_object_dt64(self, tz):
+        ser = pd.Series(pd.date_range("2000", periods=2, tz=tz))
+
+        with tm.assert_produces_warning(None):
+            # Future behavior (for tzaware case) with no warning
+            result = np.asarray(ser, dtype=object)
+
+        expected = np.array(
+            [pd.Timestamp("2000-01-01", tz=tz), pd.Timestamp("2000-01-02", tz=tz)]
+        )
+        tm.assert_numpy_array_equal(result, expected)
+
     def test_asarray_tz_naive(self):
         # This shouldn't produce a warning.
         ser = pd.Series(pd.date_range("2000", periods=2))
         expected = np.array(["2000-01-01", "2000-01-02"], dtype="M8[ns]")
         result = np.asarray(ser)
 
-        tm.assert_numpy_array_equal(result, expected)
-
-        # optionally, object
-        result = np.asarray(ser, dtype=object)
-
-        expected = np.array([pd.Timestamp("2000-01-01"), pd.Timestamp("2000-01-02")])
         tm.assert_numpy_array_equal(result, expected)
 
     def test_asarray_tz_aware(self):
@@ -154,13 +162,5 @@ class TestTimeSeries:
 
         # Old behavior with no warning
         result = np.asarray(ser, dtype="M8[ns]")
-
-        tm.assert_numpy_array_equal(result, expected)
-
-        # Future behavior with no warning
-        expected = np.array(
-            [pd.Timestamp("2000-01-01", tz=tz), pd.Timestamp("2000-01-02", tz=tz)]
-        )
-        result = np.asarray(ser, dtype=object)
 
         tm.assert_numpy_array_equal(result, expected)
