d70eaac14895bd677b62132f4ea4b7777b43ea9d
Daniel Saxton <dsaxton@pm.me>
2020-03-22 13:58:10 -0500

    Fix test_arithmetic

--- pandas/tests/arrays/integer/test_arithmetic.py
+++ pandas/tests/arrays/integer/test_arithmetic.py
@@ -5,41 +5,7 @@ import pandas as pd
 import pandas._testing as tm
 from pandas.api.types import is_float, is_float_dtype, is_scalar
 from pandas.core.arrays import IntegerArray, integer_array
-from pandas.core.arrays.integer import (
-    Int8Dtype,
-    Int16Dtype,
-    Int32Dtype,
-    Int64Dtype,
-    UInt8Dtype,
-    UInt16Dtype,
-    UInt32Dtype,
-    UInt64Dtype,
-)
 from pandas.tests.extension.base import BaseOpsUtil
-
-
-@pytest.fixture(
-    params=[
-        Int8Dtype,
-        Int16Dtype,
-        Int32Dtype,
-        Int64Dtype,
-        UInt8Dtype,
-        UInt16Dtype,
-        UInt32Dtype,
-        UInt64Dtype,
-    ]
-)
-def dtype(request):
-    return request.param()
-
-
-@pytest.fixture
-def data(dtype):
-    return integer_array(
-        list(range(8)) + [np.nan] + list(range(10, 98)) + [np.nan] + [99, 100],
-        dtype=dtype,
-    )
 
 
 class TestArithmeticOps(BaseOpsUtil):
