161905eb8ea9fab3e3cc8fc915dbef7182108fe9
jbrockmendel <jbrockmendel@gmail.com>
2020-03-21 17:16:45 -0700

    CLN: Period tests (#32875)

--- pandas/tests/scalar/test_nat.py
+++ pandas/tests/scalar/test_nat.py
@@ -20,6 +20,7 @@ from pandas import (
     TimedeltaIndex,
     Timestamp,
     isna,
+    offsets,
 )
 import pandas._testing as tm
 from pandas.core.arrays import DatetimeArray, PeriodArray, TimedeltaArray
@@ -508,3 +509,38 @@ def test_nat_comparisons(compare_operators_no_eq_ne, other):
     # GH 26039
     assert getattr(NaT, compare_operators_no_eq_ne)(other) is False
     assert getattr(other, compare_operators_no_eq_ne)(NaT) is False
+
+
+@pytest.mark.parametrize(
+    "obj",
+    [
+        offsets.YearEnd(2),
+        offsets.YearBegin(2),
+        offsets.MonthBegin(1),
+        offsets.MonthEnd(2),
+        offsets.MonthEnd(12),
+        offsets.Day(2),
+        offsets.Day(5),
+        offsets.Hour(24),
+        offsets.Hour(3),
+        offsets.Minute(),
+        np.timedelta64(3, "h"),
+        np.timedelta64(4, "h"),
+        np.timedelta64(3200, "s"),
+        np.timedelta64(3600, "s"),
+        np.timedelta64(3600 * 24, "s"),
+        np.timedelta64(2, "D"),
+        np.timedelta64(365, "D"),
+        timedelta(-2),
+        timedelta(365),
+        timedelta(minutes=120),
+        timedelta(days=4, minutes=180),
+        timedelta(hours=23),
+        timedelta(hours=23, minutes=30),
+        timedelta(hours=48),
+    ],
+)
+def test_nat_addsub_tdlike_scalar(obj):
+    assert NaT + obj is NaT
+    assert obj + NaT is NaT
+    assert NaT - obj is NaT
