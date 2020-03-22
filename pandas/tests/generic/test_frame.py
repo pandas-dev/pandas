0722e6ae734ed5c731aa9c459672e1ccf22b7fec
jbrockmendel <jbrockmendel@gmail.com>
2020-03-22 13:31:04 -0700

    REF: collect to_xarray tests (#32877)
    * implement test_to_xarray
    
    * dummy commit to force CI

--- pandas/tests/generic/test_frame.py
+++ pandas/tests/generic/test_frame.py
@@ -1,26 +1,14 @@
 from copy import deepcopy
-from distutils.version import LooseVersion
 from operator import methodcaller
 
 import numpy as np
 import pytest
-
-import pandas.util._test_decorators as td
-
-from pandas.core.dtypes.generic import ABCMultiIndex
 
 import pandas as pd
 from pandas import DataFrame, MultiIndex, Series, date_range
 import pandas._testing as tm
 
 from .test_generic import Generic
-
-try:
-    import xarray
-
-    _XARRAY_INSTALLED = True
-except ImportError:
-    _XARRAY_INSTALLED = False
 
 
 class TestDataFrame(Generic):
@@ -238,91 +226,3 @@ class TestDataFrame2:
 
         with pytest.raises(TypeError, match=msg):
             ts.fillna(0, in_place=True)
-
-
-class TestToXArray:
-    @pytest.mark.skipif(
-        not _XARRAY_INSTALLED
-        or _XARRAY_INSTALLED
-        and LooseVersion(xarray.__version__) < LooseVersion("0.10.0"),
-        reason="xarray >= 0.10.0 required",
-    )
-    def test_to_xarray_index_types(self, indices):
-        if isinstance(indices, ABCMultiIndex):
-            pytest.skip("MultiIndex is tested separately")
-        if len(indices) == 0:
-            pytest.skip("Test doesn't make sense for empty index")
-
-        from xarray import Dataset
-
-        df = DataFrame(
-            {
-                "a": list("abc"),
-                "b": list(range(1, 4)),
-                "c": np.arange(3, 6).astype("u1"),
-                "d": np.arange(4.0, 7.0, dtype="float64"),
-                "e": [True, False, True],
-                "f": pd.Categorical(list("abc")),
-                "g": pd.date_range("20130101", periods=3),
-                "h": pd.date_range("20130101", periods=3, tz="US/Eastern"),
-            }
-        )
-
-        df.index = indices[:3]
-        df.index.name = "foo"
-        df.columns.name = "bar"
-        result = df.to_xarray()
-        assert result.dims["foo"] == 3
-        assert len(result.coords) == 1
-        assert len(result.data_vars) == 8
-        tm.assert_almost_equal(list(result.coords.keys()), ["foo"])
-        assert isinstance(result, Dataset)
-
-        # idempotency
-        # datetimes w/tz are preserved
-        # column names are lost
-        expected = df.copy()
-        expected["f"] = expected["f"].astype(object)
-        expected.columns.name = None
-        tm.assert_frame_equal(
-            result.to_dataframe(), expected,
-        )
-
-    @td.skip_if_no("xarray", min_version="0.7.0")
-    def test_to_xarray(self):
-        from xarray import Dataset
-
-        df = DataFrame(
-            {
-                "a": list("abc"),
-                "b": list(range(1, 4)),
-                "c": np.arange(3, 6).astype("u1"),
-                "d": np.arange(4.0, 7.0, dtype="float64"),
-                "e": [True, False, True],
-                "f": pd.Categorical(list("abc")),
-                "g": pd.date_range("20130101", periods=3),
-                "h": pd.date_range("20130101", periods=3, tz="US/Eastern"),
-            }
-        )
-
-        df.index.name = "foo"
-        result = df[0:0].to_xarray()
-        assert result.dims["foo"] == 0
-        assert isinstance(result, Dataset)
-
-        # available in 0.7.1
-        # MultiIndex
-        df.index = pd.MultiIndex.from_product([["a"], range(3)], names=["one", "two"])
-        result = df.to_xarray()
-        assert result.dims["one"] == 1
-        assert result.dims["two"] == 3
-        assert len(result.coords) == 2
-        assert len(result.data_vars) == 8
-        tm.assert_almost_equal(list(result.coords.keys()), ["one", "two"])
-        assert isinstance(result, Dataset)
-
-        result = result.to_dataframe()
-        expected = df.copy()
-        expected["f"] = expected["f"].astype(object)
-        expected.columns.name = None
-        tm.assert_frame_equal(result, expected, check_index_type=False)
