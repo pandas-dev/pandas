4c1bc8881a7505d46123568dbde2221158b0f0f0
Derek McCammond <dmm545@drexel.edu>
2020-03-22 09:56:10 -0400

    Parameterized tests after making error message more human readable.

--- pandas/tests/indexes/ranges/test_constructors.py
+++ pandas/tests/indexes/ranges/test_constructors.py
@@ -37,22 +37,6 @@ class TestRangeIndexConstructors:
         with pytest.raises(TypeError, match=msg):
             RangeIndex(name="Foo")
 
-        # invalid args
-        for i in [
-            Index(["a", "b"]),
-            Series(["a", "b"]),
-            np.array(["a", "b"]),
-            [],
-            "foo",
-            datetime(2000, 1, 1, 0, 0),
-            np.arange(0, 10),
-            np.array([1]),
-            [1],
-        ]:
-            msg = f"Value needs to be a scalar value, was type {type(i).__name__}"
-            with pytest.raises(TypeError, match=msg):
-                RangeIndex(i)
-
         # we don't allow on a bare Index
         msg = (
             r"Index\(\.\.\.\) must be called with a collection of some "
@@ -60,6 +44,38 @@ class TestRangeIndexConstructors:
         )
         with pytest.raises(TypeError, match=msg):
             Index(0, 1000)
+
+    @pytest.mark.parametrize(
+        "args",
+        [
+            Index(["a", "b"]),
+            Series(["a", "b"]),
+            np.array(["a", "b"]),
+            [],
+            np.arange(0, 10),
+            np.array([1]),
+            [1],
+        ]
+    )
+    def test_constructor_additional_invalid_args(self, args):
+        msg = (
+            "Value needs to be a scalar value, "
+            f"was type {type(args).__name__}"
+        )
+        with pytest.raises(TypeError, match=msg):
+            RangeIndex(args)
+
+    @pytest.mark.parametrize(
+        "args",
+        [
+            "foo",
+            datetime(2000, 1, 1, 0, 0),
+        ]
+    )
+    def test_constructor_invalid_args_wrong_type(self, args):
+        msg = f"Wrong type {type(args)} for value {args}"
+        with pytest.raises(TypeError, match=msg):
+            RangeIndex(args)
 
     def test_constructor_same(self):
 
