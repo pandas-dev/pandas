69c95131904d4e44170e0563b6b2208edfba53e6
Marco Gorelli <m.e.gorelli@gmail.com>
2020-03-22 21:48:07 +0000

    remove blank lines

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -1454,9 +1454,7 @@ class DataFrameGroupBy(GroupBy):
         # for each col, reshape to to size of original frame
         # by take operation
         ids, _, ngroup = self.grouper.group_info
-
         result = result.reindex(self.grouper.result_index)
-
         output = []
         for i, _ in enumerate(result.columns):
             res = algorithms.take_1d(result.iloc[:, i].values, ids)
