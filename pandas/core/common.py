1d6bf54edff805cab4c898e7288dc20dc6964679
jbrockmendel <jbrockmendel@gmail.com>
2020-03-23 10:33:36 -0700

    merge fixup

--- pandas/core/common.py
+++ pandas/core/common.py
@@ -213,7 +213,7 @@ def asarray_tuplesafe(values, dtype=None):
     if not (isinstance(values, (list, tuple)) or hasattr(values, "__array__")):
         values = list(values)
     elif isinstance(values, ABCIndexClass):
-        return values._values  # TODO: extract_array?
+        return values._values
 
     if isinstance(values, list) and dtype in [np.object_, object]:
         return construct_1d_object_array_from_listlike(values)
