393096306976346f69e6a567ebcb75f4a56d31f0
Matt Roeschke <mroeschke@housecanary.com>
2020-03-26 22:03:58 -0700

    black

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -395,9 +395,7 @@ class _Window(PandasObject, ShallowMixin, SelectionMixin):
             return self._get_roll_func(f"{func}_variable")
         return partial(self._get_roll_func(f"{func}_fixed"), win=self._get_window())
 
-    def _get_window_indexer(
-        self, window: int, func_name: Optional[str]
-    ) -> BaseIndexer:
+    def _get_window_indexer(self, window: int, func_name: Optional[str]) -> BaseIndexer:
         """
         Return an indexer class that will compute the window start and end bounds
         """
