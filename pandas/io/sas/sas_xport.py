f31b6b2d470ecc97c09b9a7c3c65b2e19c15d8c2
Tim Swast <swast@google.com>
2020-03-27 10:03:50 -0500

    BUG: AttributeError when read_sas used with GCS
    With GCSFS (and possibly other connectors), the output from they file
    read is `bytes`, not a Unicode `str`. The `encode` call is unnecessary
    in this case.

--- pandas/io/sas/sas_xport.py
+++ pandas/io/sas/sas_xport.py
@@ -265,10 +265,12 @@ class XportReader(abc.Iterator):
         else:
             # Copy to BytesIO, and ensure no encoding
             contents = filepath_or_buffer.read()
-            try:
-                contents = contents.encode(self._encoding)
-            except UnicodeEncodeError:
-                pass
+            # Convert string output to bytes, if needed
+            if hasattr(contents, "encode"):
+                try:
+                    contents = contents.encode(self._encoding)
+                except UnicodeEncodeError:
+                    pass
             self.filepath_or_buffer = BytesIO(contents)
 
         self._read_header()
