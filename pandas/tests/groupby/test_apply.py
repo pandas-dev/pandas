0b7caf79775be7aa7da5f912f8013e1be1f130d6
phofl <patrick_hoefler@gmx.net>
2020-03-29 20:07:50 +0200

    BUG: Parametrize unittest

--- pandas/tests/groupby/test_apply.py
+++ pandas/tests/groupby/test_apply.py
@@ -868,14 +868,15 @@ def test_apply_function_returns_numpy_array():
     tm.assert_series_equal(result, expected)
 
 
-def test_apply_function_index_return():
+@pytest.mark.parametrize(
+    "function", [lambda gr: gr.index, lambda gr: gr.index + 1 - 1],
+)
+def test_apply_function_index_return(function):
     # GH: 22541
     df = pd.DataFrame([1, 2, 2, 2, 1, 2, 3, 1, 3, 1], columns=["id"])
-    result = df.groupby("id").apply(lambda gr: gr.index)
-    result_2 = df.groupby("id").apply(lambda gr: gr.index + 1 - 1)
+    result = df.groupby("id").apply(function)
     expected = pd.Series(
         [pd.Index([0, 4, 7, 9]), pd.Index([1, 2, 3, 5]), pd.Index([6, 8])],
         index=pd.Index([1, 2, 3], name="id"),
     )
     tm.assert_series_equal(result, expected)
-    tm.assert_series_equal(result, result_2)
