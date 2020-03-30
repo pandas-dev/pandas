96b87352311590cc3222bfa7d3f1fbfa39d2a080
benabel <dev.abel@free.fr>
2020-03-30 05:52:34 +0200

    DOC iris.csv file has moved (#33099)

--- pandas/plotting/_misc.py
+++ pandas/plotting/_misc.py
@@ -359,7 +359,7 @@ def parallel_coordinates(
     --------
     >>> from matplotlib import pyplot as plt
     >>> df = pd.read_csv('https://raw.github.com/pandas-dev/pandas/master'
-                        '/pandas/tests/data/csv/iris.csv')
+                        '/pandas/tests/data/iris.csv')
     >>> pd.plotting.parallel_coordinates(
             df, 'Name',
             color=('#556270', '#4ECDC4', '#C7F464'))
