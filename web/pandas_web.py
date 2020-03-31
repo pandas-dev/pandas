6e964b19ad0365358ae55158d0259747f1ec54dd
Marc Garcia <garcia.marc@gmail.com>
2020-03-31 13:27:57 +0100

    WEB: Moving pandas blog to the website

--- web/pandas_web.py
+++ web/pandas_web.py
@@ -78,6 +78,37 @@ class Preprocessors:
         """
         tag_expr = re.compile("<.*?>")
         posts = []
+        # posts from the file system
+        if context["blog"]["posts_path"]:
+            posts_path = os.path.join(context["source_path"],
+                                      *context["blog"]["posts_path"].split("/"))
+            for fname in os.listdir(posts_path):
+                if fname.startswith("index."):
+                    continue
+                link = (f"/{context['blog']['posts_path']}"
+                        f"/{os.path.splitext(fname)[0]}.html")
+                md = markdown.Markdown(extensions=context["main"]["markdown_extensions"])
+                with open(os.path.join(posts_path, fname)) as f:
+                    html = md.convert(f.read())
+                title = md.Meta["title"][0]
+                summary = re.sub(tag_expr, "", html)
+                try:
+                    body_position = summary.index(title) + len(title)
+                except ValueError:
+                    raise ValueError(f'Blog post {fname} should have a markdown header corresponding to its "Title" element "{title}"')
+                summary = " ".join(summary[body_position:].split(" ")[:30])
+                posts.append(
+                    {
+                        "title": title,
+                        "author": context["blog"]["author"],
+                        "published": datetime.datetime.strptime(md.Meta["date"][0], "%Y-%m-%d"),
+                        "feed": context["blog"]["feed_name"],
+                        "link": link,
+                        "description": summary,
+                        "summary": summary,
+                    }
+                )
+        # posts from rss feeds
         for feed_url in context["blog"]["feed"]:
             feed_data = feedparser.parse(feed_url)
             for entry in feed_data.entries:
@@ -180,6 +211,7 @@ def get_context(config_fname: str, ignore_io_errors: bool, **kwargs):
     with open(config_fname) as f:
         context = yaml.safe_load(f)
 
+    context["source_path"] = os.path.dirname(config_fname)
     context["ignore_io_errors"] = ignore_io_errors
     context.update(kwargs)
 
