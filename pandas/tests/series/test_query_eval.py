7d3b90920ef6132f4354b42d8a930ef0748a6ffa
harri471 <55150103+harri471@users.noreply.github.com>
2020-03-31 06:08:12 -0400

    Added test cases for query

--- pandas/tests/series/test_query_eval.py
+++ pandas/tests/series/test_query_eval.py
@@ -102,3 +102,67 @@ class TestSeriesEvalWithSeries:
 
         with pytest.raises(TypeError, match=msg):
             series.eval(f"a {op} b", engine=engine, parser=parser)
+            
+    
+class TestSeriesQueryByIndex:
+    def setup_method(self, method):
+        self.series = Series(np.random.randn(10), index=[x for x in range(10)])
+        self.frame = self.series.to_frame()
+    
+    def teardown_method(self, method):
+        del self.series
+        del self.frame
+
+    # test the boolean operands
+    @pytest.mark.parametrize("op", ["<", "<=", ">", ">=", "==", "!="])
+    def test_bool_operands(self, op):
+        test = f"index {op} 5"
+        result = self.series.query(test)
+        expected = self.frame.query(test)[0]
+        tm.assert_series_equal(result, expected)
+
+    # test the operands that can join queries
+    def test_and_bitwise_operator(self, op):
+        test = f"(index > 2) & (index < 8)"
+        result = self.series.query(test)
+        expected = self.frame.query(test)[0]
+        tm.assert_series_equal(result, expected)
+
+    def test_or_bitwise_operator(self, op):
+        test = f"(index < 3) | (index > 7)"
+        result = self.series.query(test)
+        expected = self.frame.query(test)[0]
+        tm.assert_series_equal(result, expected)
+
+class TestSeriesQueryByMultiIndex:
+    def setup_method(self, method):
+        self.series = Series(np.random.randn(10),
+             index=[["a"]*5 + ["b"]*5, [x for x in range(10)]])
+        self.frame = self.series.to_frame()
+    
+    def teardown_method(self, method):
+        del self.series
+        del self.frame
+
+    # check against first level
+    def test_query_first_level(self):
+        test = "ilevel_0 == 'b'"
+        result = self.series.query(test)
+        expected = self.frame.query(test)[0]
+        tm.assert_series_equal(result, expected)
+
+    # check against not first level
+    def test_query_not_first_level(self):
+        test = "ilevel_1 > 4"
+        result = self.series.query(test)
+        expected = self.frame.query(test)[0]
+        tm.assert_series_equal(result, expected)
+
+    @pytest.mark.parametrize("op", ["&", "|"])
+    def test_both_levels(self, op):
+        test = f"(ilevel_0 == 'b') {op} ((ilevel_1 % 2) == 0)"
+        result = self.series.query(test)
+        expected = self.frame.query(test)[0]
+        tm.assert_series_equal(result, expected)
+
+
