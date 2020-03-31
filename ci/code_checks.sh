702dc0fe0636bb51c91acc28a20cda41c0eadadb
MomIsBestFriend <>
2020-03-31 14:29:57 +0300

    Removed empty checks
    When running `pytest --doctest-modules` and no tests are found
    the return status code is 5.
    Meaning that `$RET` is going to be larger than 0.

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -283,10 +283,6 @@ if [[ -z "$CHECK" || "$CHECK" == "doctests" ]]; then
     pytest -q --doctest-modules pandas/core/tools/
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
-    MSG='Doctests util' ; echo $MSG
-    pytest -q --doctest-modules pandas/core/util/
-    RET=$(($RET + $?)) ; echo $MSG "DONE"
-
     MSG='Doctests reshaping functions' ; echo $MSG
     pytest -q --doctest-modules pandas/core/reshape/
     RET=$(($RET + $?)) ; echo $MSG "DONE"
@@ -326,14 +322,6 @@ if [[ -z "$CHECK" || "$CHECK" == "doctests" ]]; then
 
     MSG='Doctests tseries' ; echo $MSG
     pytest -q --doctest-modules pandas/tseries/
-    RET=$(($RET + $?)) ; echo $MSG "DONE"
-
-    MSG='Doctests sparse' ; echo $MSG
-    pytest -q --doctest-modules pandas/core/sparse/
-    RET=$(($RET + $?)) ; echo $MSG "DONE"
-
-    MSG='Doctests array_algos' ; echo $MSG
-    pytest -q --doctest-modules pandas/core/array_algos/
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
     MSG='Doctests computation' ; echo $MSG
