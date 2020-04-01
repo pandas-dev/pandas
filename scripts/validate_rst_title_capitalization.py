940109db7e71a79dd9ee53a6a8202479a2dbc90a
clement <leconte.clement@orange.fr>
2020-04-01 15:14:08 +0100

    Remove the 'ExtensionDtype' and 'ExtensionArray' exceptions from 'CAPITALIZATION_EXCEPTIONS' that are present in title with special encoding, as it is not necessary anymore

--- scripts/validate_rst_title_capitalization.py
+++ scripts/validate_rst_title_capitalization.py
@@ -100,8 +100,6 @@ CAPITALIZATION_EXCEPTIONS = {
     "BusinessHour",
     "BusinessDay",
     "DateOffset",
-    "ExtensionDtype",
-    "ExtensionArray",
 }
 
 CAP_EXCEPTIONS_DICT = {word.lower(): word for word in CAPITALIZATION_EXCEPTIONS}
