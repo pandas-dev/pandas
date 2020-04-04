6065e54caafa4040e5b0e2cea4f1d4c72690a5f1
tv3141 <tv3141@users.noreply.github.com>
2020-04-04 01:01:46 +0100

    BUG: Fix segfault in GroupBy.count and DataFrame.count (#32842)

--- pandas/tests/groupby/test_counting.py
+++ pandas/tests/groupby/test_counting.py
@@ -3,7 +3,7 @@ from itertools import product
 import numpy as np
 import pytest
 
-from pandas import DataFrame, MultiIndex, Period, Series, Timedelta, Timestamp
+from pandas import DataFrame, Index, MultiIndex, Period, Series, Timedelta, Timestamp
 import pandas._testing as tm
 
 
@@ -220,3 +220,12 @@ class TestCounting:
         mi = MultiIndex(levels=[[], ["a", "b"]], codes=[[], []], names=["A", "B"])
         expected = Series([], index=mi, dtype=np.int64, name="C")
         tm.assert_series_equal(result, expected, check_index_type=False)
+
+    def test_count_groupby_column_with_nan_in_groupby_column(self):
+        # https://github.com/pandas-dev/pandas/issues/32841
+        df = DataFrame({"A": [1, 1, 1, 1, 1], "B": [5, 4, np.NaN, 3, 0]})
+        res = df.groupby(["B"]).count()
+        expected = DataFrame(
+            index=Index([0.0, 3.0, 4.0, 5.0], name="B"), data={"A": [1, 1, 1, 1]}
+        )
+        tm.assert_frame_equal(expected, res)
