06651276a71917b47bf683c435d28184a5e9f5d1
Jesse Farnham <jessefarnham1@gmail.com>
2020-04-07 19:39:47 -0400

    After discussion, go back to original approach

--- pandas/io/json/_json.py
+++ pandas/io/json/_json.py
@@ -978,15 +978,11 @@ class Parser:
             if not in_range.all():
                 return data, False
 
-        if new_data.dtype == "bool":
-            # GH#33373 ignore bool
-            return data, False
-
         date_units = (self.date_unit,) if self.date_unit else self._STAMP_UNITS
         for date_unit in date_units:
             try:
                 new_data = to_datetime(new_data, errors="raise", unit=date_unit)
-            except (ValueError, OverflowError):
+            except (ValueError, OverflowError, TypeError):
                 continue
             return new_data, True
         return data, False
