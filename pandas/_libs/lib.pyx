61f59f5a1ba1004777158c687ea69a990699fecb
MomIsBestFriend <>
2020-04-07 13:40:39 +0300

    Revert some changes
    XREF:
    https://github.com/pandas-dev/pandas/pull/33329#discussion_r404261189

--- pandas/_libs/lib.pyx
+++ pandas/_libs/lib.pyx
@@ -103,10 +103,12 @@ def memory_usage_of_objects(arr: object[:]) -> int64_t:
 
     Does not include the actual bytes of the pointers
     """
-    cdef:
-        Py_ssize_t i, n = len(arr)
-        int64_t size = 0
+    i: Py_ssize_t
+    n: Py_ssize_t
+    size: int64_t
 
+    size = 0
+    n = len(arr)
     for i in range(n):
         size += arr[i].__sizeof__()
     return size
