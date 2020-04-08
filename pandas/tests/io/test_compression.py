28eecf262586df6519b701a8ccf9db6a811f23fd
Jesse Farnham <jessefarnham1@gmail.com>
2020-04-08 12:55:03 -0400

    Add test for bz2; mypy ignore non-typesafe use of kwargs

--- pandas/tests/io/test_compression.py
+++ pandas/tests/io/test_compression.py
@@ -156,13 +156,33 @@ def test_with_missing_lzma_runtime():
     ],
 )
 @pytest.mark.parametrize("method", ["to_pickle", "to_json", "to_csv"])
-def test_gzip_compression_level_path(obj, method):
-    """GH#33398 Ideally this test should be repeated for bz2 as well,
-    but that is not practical because a file size of >100k is needed to see any
-    size difference between bz2 compression settings."""
+def test_gzip_compression_level(obj, method):
+    #GH33196
     with tm.ensure_clean() as path:
         getattr(obj, method)(path, compression="gzip")
         compressed_size_default = os.path.getsize(path)
-        getattr(obj, method)(path, compression={"method": "gzip", "compresslevel": 1})
+        getattr(obj, method)(
+            path, compression={ "method": "gzip", "compresslevel": 1})
         compressed_size_fast = os.path.getsize(path)
         assert compressed_size_default < compressed_size_fast
+
+
+@pytest.mark.parametrize(
+    "obj",
+    [
+        pd.DataFrame(
+            100 * [[0.123456, 0.234567, 0.567567], [12.32112, 123123.2, 321321.2]],
+            columns=["X", "Y", "Z"],
+        ),
+        pd.Series(100 * [0.123456, 0.234567, 0.567567], name="X"),
+    ],
+)
+@pytest.mark.parametrize("method", ["to_pickle", "to_json", "to_csv"])
+def test_bzip_compression_level(obj, method):
+    """GH33196 bzip needs file size > 100k to show a size difference between
+    compression levels, so here we just check if the call works when
+    compression is passed as a dict.
+    """
+    with tm.ensure_clean() as path:
+        getattr(obj, method)(
+            path, compression={ "method": "bz2", "compresslevel": 1})
