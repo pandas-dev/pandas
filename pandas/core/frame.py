71b8e05b6a9ba6d6499fc10736201e2ed06fe7db
Farhan Reynaldo <farhanreynaldo@gmail.com>
2020-04-08 09:06:17 +0700

    Remove index name

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -4681,39 +4681,38 @@ class DataFrame(NDFrame):
         >>> brand = ['Yum Yum', 'Yum Yum', 'Indomie', 'Indomie', 'Indomie']
         >>> df = pd.DataFrame({'brand': brand,
         ... 'style': ['cup', 'cup', 'cup', 'pack', 'pack'],
-        ... 'rating': [4, 4, 3.5, 15, 5]},
-        ...  index=['TH', 'TH', 'ID', 'ID', 'ID'])
+        ... 'rating': [4, 4, 3.5, 15, 5]})
         >>> df
             brand style  rating
-        TH  Yum Yum   cup     4.0
-        TH  Yum Yum   cup     4.0
-        ID  Indomie   cup     3.5
-        ID  Indomie  pack    15.0
-        ID  Indomie  pack     5.0
+        0  Yum Yum   cup     4.0
+        1  Yum Yum   cup     4.0
+        2  Indomie   cup     3.5
+        3  Indomie  pack    15.0
+        4  Indomie  pack     5.0
 
         By default, it removes duplicate rows based on all columns
 
         >>> df.drop_duplicates()
             brand style  rating
-        TH  Yum Yum   cup     4.0
-        ID  Indomie   cup     3.5
-        ID  Indomie  pack    15.0
-        ID  Indomie  pack     5.0
+        0  Yum Yum   cup     4.0
+        2  Indomie   cup     3.5
+        3  Indomie  pack    15.0
+        4  Indomie  pack     5.0
 
         To remove duplicates on specific column(s), use ``subset``
 
         >>> df.drop_duplicates(subset=['brand'])
             brand style  rating
-        TH  Yum Yum   cup     4.0
-        ID  Indomie   cup     3.5
+        0  Yum Yum   cup     4.0
+        2  Indomie   cup     3.5
 
         To remove duplicates and keep last occurences, use ``keep``
 
         >>> df.drop_duplicates(subset=['brand', 'style'], keep='last')
             brand style  rating
-        TH  Yum Yum   cup     4.0
-        ID  Indomie   cup     3.5
-        ID  Indomie  pack     5.0
+        1  Yum Yum   cup     4.0
+        2  Indomie   cup     3.5
+        4  Indomie  pack     5.0
         """
         if self.empty:
             return self.copy()
