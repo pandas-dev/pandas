5cb07c8864ff98b4f2c6f5bc57a5ea586ed19f1d
MomIsBestFriend <>
2020-04-08 22:57:09 +0300

    BUG: to_period() freq was not infered

--- pandas/core/arrays/datetimes.py
+++ pandas/core/arrays/datetimes.py
@@ -1090,14 +1090,13 @@ default 'raise'
             )
 
         if freq is None:
-            freq = self.freqstr or self.inferred_freq
+            freq = self.inferred_freq or self.freqstr
 
-            if freq is None:
-                raise ValueError(
-                    "You must pass a freq argument as current index has none."
-                )
-
+        if freq is None:
             freq = get_period_alias(freq)
+
+        if freq is None:
+            raise ValueError("You must pass a freq argument as current index has none.")
 
         return PeriodArray._from_datetime64(self._data, freq, tz=self.tz)
 
