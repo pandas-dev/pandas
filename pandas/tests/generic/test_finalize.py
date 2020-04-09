99a4cc26251de6a0bd2118567e384a50d52f2aad
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-04-09 10:32:00 -0500

    add categorical

--- pandas/tests/generic/test_finalize.py
+++ pandas/tests/generic/test_finalize.py
@@ -666,3 +666,25 @@ def test_timedelta_methods(method):
     s.attrs = {"a": 1}
     result = method(s.dt)
     assert result.attrs == {"a": 1}
+
+
+@pytest.mark.parametrize(
+    "method",
+    [
+        operator.methodcaller("add_categories", ["c"]),
+        operator.methodcaller("as_ordered"),
+        operator.methodcaller("as_unordered"),
+        lambda x: getattr(x, "codes"),
+        operator.methodcaller("remove_categories", "a"),
+        operator.methodcaller("remove_unused_categories"),
+        operator.methodcaller("rename_categories", {"a": "A", "b": "B"}),
+        operator.methodcaller("reorder_categories", ["b", "a"]),
+        operator.methodcaller("set_categories", ["A", "B"]),
+    ],
+)
+# @not_implemented_mark
+def test_categorical_accessor(method):
+    s = pd.Series(["a", "b"], dtype="category")
+    s.attrs = {"a": 1}
+    result = method(s.cat)
+    assert result.attrs == {"a": 1}
