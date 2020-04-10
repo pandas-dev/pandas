93be1c4ac3f8a0b0eb48878e47d53001f0ee2c7b
Suraj Harjani <sharjani@umich.edu>
2020-04-10 12:43:14 -0400

    Added in assert statement

--- pandas/tests/dtypes/test_concat.py
+++ pandas/tests/dtypes/test_concat.py
@@ -90,10 +90,13 @@ def test_concat_mismatched_categoricals_with_empty():
     tm.assert_categorical_equal(result, expected)
 
 
-# This will raise a ValueError issue if it fails
-# Regression test for issue 25257
+
 def test_no_tz_concat_without_copy():
-    df = pd.DataFrame({
-        'timestamp': [pd.Timestamp('2020-04-08 09:00:00.709949+0000', tz='UTC')],
-    })
-    pd.concat([df], copy=False)
+	# This will raise a ValueError issue if it fails
+	# Regression test for issue 25257
+	df = pd.DataFrame({
+	    'timestamp': [pd.Timestamp('2020-04-08 09:00:00.709949+0000', tz='UTC')],
+	})
+	result = pd.concat([df], copy=False)
+	expected = df
+	tm.assert_frame_equal(result, expected)
