26920c2cbd39a895c5c0c4ce390e0bb35bf0994d
Kaiqi Dong <kaiqi@kth.se>
2020-04-10 19:00:48 +0200

    BUG/PLT: Multiple plots with different cmap, colorbars legends use first cmap (#33392)

--- pandas/tests/plotting/test_frame.py
+++ pandas/tests/plotting/test_frame.py
@@ -1332,6 +1332,20 @@ class TestDataFramePlots(TestPlotBase):
             np.array([1, 1, 1, 1], dtype=np.float64),
         )
 
+    def test_scatter_colorbar_different_cmap(self):
+        # GH 33389
+        import matplotlib.pyplot as plt
+
+        df = pd.DataFrame({"x": [1, 2, 3], "y": [1, 3, 2], "c": [1, 2, 3]})
+        df["x2"] = df["x"] + 1
+
+        fig, ax = plt.subplots()
+        df.plot("x", "y", c="c", kind="scatter", cmap="cividis", ax=ax)
+        df.plot("x2", "y", c="c", kind="scatter", cmap="magma", ax=ax)
+
+        assert ax.collections[0].cmap.name == "cividis"
+        assert ax.collections[1].cmap.name == "magma"
+
     @pytest.mark.slow
     def test_plot_bar(self):
         df = DataFrame(
