6e33152c59ecf9a7a2ba3ba686170cd8faeccbce
Suraj Harjani <sharjani@umich.edu>
2020-04-10 16:15:15 -0400

    fixed spacing

--- pandas/tests/dtypes/test_concat.py
+++ pandas/tests/dtypes/test_concat.py
@@ -94,7 +94,7 @@ def test_no_tz_concat_without_copy():
     # This will raise a ValueError issue if it fails
     # Regression test for issue 25257
     df = pd.DataFrame({'timestamp': [pd.Timestamp('2020-04-08 09:00:00.709949+0000',
-        tz='UTC')], })
+                tz='UTC')], })
     result = pd.concat([df], copy=False)
     expected = df
     tm.assert_frame_equal(result, expected)
