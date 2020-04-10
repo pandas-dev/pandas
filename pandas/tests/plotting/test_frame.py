b19bf6ac365ed1ed1743f789ba84152560c54eca
Kaiqi <kaiqi@kth.se>
2020-04-10 10:45:12 +0200

    add test

--- pandas/tests/plotting/test_frame.py
+++ pandas/tests/plotting/test_frame.py
@@ -1688,14 +1688,18 @@ class TestDataFramePlots(TestPlotBase):
     def test_hist_weights(self, weights):
         # GH 33173
         np.random.seed(0)
-        df1 = pd.DataFrame(dict(zip(["A", "B"], np.random.randn(2, 100,))))
+        df = pd.DataFrame(dict(zip(["A", "B"], np.random.randn(2, 100,))))
 
-        ax1 = _check_plot_works(df1.plot, kind="hist", weights=weights)
+        ax1 = _check_plot_works(df.plot, kind="hist", weights=weights)
+        ax2 = _check_plot_works(df.plot, kind="hist")
 
-        df2 = pd.DataFrame(dict(zip(["A", "B"], 0.1 * np.random.randn(2, 100,))))
-        ax2 = _check_plot_works(df2.plot, kind="hist")
+        patch_height_with_weights = [patch.get_height() for patch in ax1.patches]
 
-        self._check_data(ax1, ax2)
+        # original heights with no weights, and we manually multiply with example
+        # weights, so after multiplication, they should be almost same
+        expected_patch_height = [0.1 * patch.get_height() for patch in ax2.patches]
+
+        tm.assert_almost_equal(patch_height_with_weights, expected_patch_height)
 
     def _check_box_coord(
         self,
