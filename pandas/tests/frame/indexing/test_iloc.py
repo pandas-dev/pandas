4ea9871c1f89154cc3ab15c64f4201a4647b044b
MomIsBestFriend <>
2020-04-13 22:38:40 +0300

    Added imports to the test case

--- pandas/tests/frame/indexing/test_iloc.py
+++ pandas/tests/frame/indexing/test_iloc.py
@@ -1,8 +1,16 @@
+import numpy as np
+
+import pandas as pd
+import pandas._testing as tm
+
+
 def test_unsure_about_the_name_and_location():
     # https://github.com/pandas-dev/pandas/issues/33198
     arr = np.random.randn(10 ** 6).reshape(500, 2000).astype(np.float64)
     df = pd.DataFrame(arr)
     df.iloc[:, 1000:] = df.iloc[:, 1000:].astype(np.float32)
-    expected = pd.Series({np.float32: 1000, np.float64: 1000}, dtype="int64")
+    expected = pd.Series(
+        {np.dtype("float32"): 1000, np.dtype("float64"): 1000}, dtype="int64"
+    )
     result = df.dtypes.value_counts()
     tm.assert_series_equal(result, expected)
