18d727fa4de473d9053c703704857ccfb8daf862
Edoardo Abati <abati.edoardo@gmail.com>
2020-04-16 00:28:35 +0100

    Changed assert to equal utc

--- pandas/tests/frame/indexing/test_at.py
+++ pandas/tests/frame/indexing/test_at.py
@@ -6,4 +6,4 @@ def test_at_timezone():
     # https://github.com/pandas-dev/pandas/issues/33544
     df = pd.DataFrame({"foo": [datetime(2000, 1, 1)]})
     df.at[0, "foo"] = datetime(2000, 1, 2, tzinfo=timezone.utc)
-    assert df.at[0, "foo"].tzinfo is not None
+    assert df.at[0, "foo"].tzinfo == timezone.utc
