c1006c886d2cdc060a3e60c9eb37fb7a84f566a7
jbrockmendel <jbrockmendel@gmail.com>
2020-04-15 12:21:57 -0700

    CLN: remove BlockManager.get (#33052)
    * CLN: remove BlockManager.get
    
    * Remove assertions

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -935,35 +935,6 @@ class BlockManager(PandasObject):
             self._known_consolidated = True
             self._rebuild_blknos_and_blklocs()
 
-    def get(self, item):
-        """
-        Return values for selected item (ndarray or BlockManager).
-        """
-        if self.items.is_unique:
-
-            if not isna(item):
-                loc = self.items.get_loc(item)
-            else:
-                indexer = np.arange(len(self.items))[isna(self.items)]
-
-                # allow a single nan location indexer
-                if not is_scalar(indexer):
-                    if len(indexer) == 1:
-                        loc = indexer.item()
-                    else:
-                        raise ValueError("cannot label index with a null key")
-
-            return self.iget(loc)
-        else:
-
-            if isna(item):
-                raise TypeError("cannot label index with a null key")
-
-            indexer = self.items.get_indexer_for([item])
-            return self.reindex_indexer(
-                new_axis=self.items[indexer], indexer=indexer, axis=0, allow_dups=True
-            )
-
     def iget(self, i: int) -> "SingleBlockManager":
         """
         Return the data as a SingleBlockManager.
