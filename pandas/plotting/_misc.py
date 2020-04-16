9f88a29b51e044e8d75b0dfeccdbfca7efd433d4
Da Xin <da.xin@wsu.edu>
2020-04-15 23:26:37 -0700

    issue 33428

--- pandas/plotting/_misc.py
+++ pandas/plotting/_misc.py
@@ -359,8 +359,7 @@ def parallel_coordinates(
     Examples
     --------
     >>> from matplotlib import pyplot as plt
-    >>> df = pd.read_csv('https://raw.github.com/pandas-dev/pandas/master'
-                        '/pandas/tests/data/iris.csv')
+    >>> df = pd.read_csv('https://raw.github.com/pandas-dev/pandas/master/pandas/tests/data/iris.csv')
     >>> pd.plotting.parallel_coordinates(
             df, 'Name',
             color=('#556270', '#4ECDC4', '#C7F464'))
