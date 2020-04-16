fbb9c70fcb94f55cfbd69f6b0728bfadadd0e2f8
Devjeet Roy <devjeetrr@gmail.com>
2020-04-15 22:41:29 -0700

    Revert "issue 30999"
    This reverts commit 6a6fde1ae092bf3f08809a6ea9b3774f3acdc029.

--- pandas/tests/arrays/boolean/test_arithmetic.py
+++ pandas/tests/arrays/boolean/test_arithmetic.py
@@ -17,25 +17,26 @@ class TestArithmeticOps(BaseOpsUtil):
     def test_error(self, data, all_arithmetic_operators):
         # invalid ops
 
-        msg="invalid scalars"
-        with pytest.raises(TypeError, match=msg):
+        op = all_arithmetic_operators
+        s = pd.Series(data)
+        ops = getattr(s, op)
+        opa = getattr(data, op)
+
+        # invalid scalars
+        with pytest.raises(TypeError):
             ops("foo")
-        with pytest.raises(TypeError, match=msg)
+        with pytest.raises(TypeError):
             ops(pd.Timestamp("20180101"))
 
-
         # invalid array-likes
-       if op not in ("__mul__", "__rmul__"):
-            # TODO(extension) numpy's mul with object array sees booleans as numbers	            
-            msg="invalid array-likes,numpy's mul with object array sees booleans as numbers"
-            with pytest.raises(TypeError, match=msg): 
-                ops(pd.Series("foo", index=s.index))	                
-
+        if op not in ("__mul__", "__rmul__"):
+            # TODO(extension) numpy's mul with object array sees booleans as numbers
+            with pytest.raises(TypeError):
+                ops(pd.Series("foo", index=s.index))
 
         # 2d
         result = opa(pd.DataFrame({"A": s}))
         assert result is NotImplemented
 
-        msg="invalid array-likes"
-        with pytest.raises(NotImplementedError, match=msg):
+        with pytest.raises(NotImplementedError):
             opa(np.arange(len(s)).reshape(-1, len(s)))
