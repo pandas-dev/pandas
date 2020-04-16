27a882dd4bc55e2c86b6ffb298522f8e37478f2b
Da Xin <da.xin@wsu.edu>
2020-04-15 22:59:51 -0700

    issue 30999

--- pandas/tests/arrays/boolean/test_arithmetic.py
+++ pandas/tests/arrays/boolean/test_arithmetic.py
@@ -23,20 +23,25 @@ class TestArithmeticOps(BaseOpsUtil):
         opa = getattr(data, op)
 
         # invalid scalars
-        with pytest.raises(TypeError):
+         msg="invalid scalars"
+        with pytest.raises(TypeError, match=msg):
             ops("foo")
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError, match=msg):
             ops(pd.Timestamp("20180101"))
 
+
         # invalid array-likes
-        if op not in ("__mul__", "__rmul__"):
-            # TODO(extension) numpy's mul with object array sees booleans as numbers
-            with pytest.raises(TypeError):
-                ops(pd.Series("foo", index=s.index))
+       if op not in ("__mul__", "__rmul__"):
+            # TODO(extension) numpy's mul with object array sees booleans as numbers	            
+            msg="invalid array-likes,numpy's mul with object array sees booleans as numbers"
+            with pytest.raises(TypeError, match=msg): 
+                ops(pd.Series("foo", index=s.index))	                
+
 
         # 2d
         result = opa(pd.DataFrame({"A": s}))
         assert result is NotImplemented
 
-        with pytest.raises(NotImplementedError):
+        msg="invalid array-likes"
+        with pytest.raises(NotImplementedError, match=msg):
             opa(np.arange(len(s)).reshape(-1, len(s)))
