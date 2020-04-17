0bda2e81499235ce536b0303125e8d78b541aa8a
Michael Marino <michael.marino@tado.com>
2020-04-17 07:42:56 +0200

    Refactor test to improve readability

--- pandas/tests/groupby/transform/test_transform.py
+++ pandas/tests/groupby/transform/test_transform.py
@@ -1022,9 +1022,7 @@ def test_groupby_transform_with_datetimes(func, values):
     dates = pd.date_range("1/1/2011", periods=10, freq="D")
 
     stocks = pd.DataFrame({"price": np.arange(10.0)}, index=dates)
-    stocks["week_id"] = (
-        pd.to_datetime(stocks.index).isocalendar().set_index(stocks.index).week
-    )
+    stocks["week_id"] = dates.isocalendar().set_index(dates).week
 
     result = stocks.groupby(stocks["week_id"])["price"].transform(func)
 
