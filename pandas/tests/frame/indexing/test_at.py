ac97198817dd8167f8d548af413fdec6de15667f
Edoardo Abati <abati.edoardo@gmail.com>
2020-04-17 12:42:29 +0100

    switched to assert_frame_equal

--- pandas/tests/frame/indexing/test_at.py
+++ pandas/tests/frame/indexing/test_at.py
@@ -1,10 +1,12 @@
 from datetime import datetime, timezone
 
 import pandas as pd
+import pandas._testing as tm
 
 
 def test_at_timezone():
     # https://github.com/pandas-dev/pandas/issues/33544
-    df = pd.DataFrame({"foo": [datetime(2000, 1, 1)]})
-    df.at[0, "foo"] = datetime(2000, 1, 2, tzinfo=timezone.utc)
-    assert df.at[0, "foo"].tzinfo == timezone.utc
+    result = pd.DataFrame({"foo": [datetime(2000, 1, 1)]})
+    result.at[0, "foo"] = datetime(2000, 1, 2, tzinfo=timezone.utc)
+    expected = pd.DataFrame({"foo": [datetime(2000, 1, 2, tzinfo=timezone.utc)]})
+    tm.assert_frame_equal(result, expected)
