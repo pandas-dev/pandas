af1ea2a440649074cc38fdd1280e5807e1d9678e
Suraj Harjani <sharjani@umich.edu>
2020-04-17 16:20:00 -0400

    Revert "Paramterized Copy-False"
    This reverts commit de1373e50c8edfc9db2ef59d86a5afe476cdd0d7.

--- pandas/tests/dtypes/test_concat.py
+++ pandas/tests/dtypes/test_concat.py
@@ -90,12 +90,11 @@ def test_concat_mismatched_categoricals_with_empty():
     tm.assert_categorical_equal(result, expected)
 
 
-@pytest.mark.parametrize("_COPY_", [True, False])
-def test_no_tz_concat_without_copy(_COPY_):
+def test_no_tz_concat_without_copy():
     # This will raise a ValueError issue if it fails
     # Regression test for issue 25257
     df = pd.DataFrame({'timestamp': [pd.Timestamp('2020-04-08 09:00:00.709949+0000',
                       tz='UTC')], })
-    result = pd.concat([df], copy=_COPY_)
+    result = pd.concat([df], copy=False)
     expected = df
     tm.assert_frame_equal(result, expected)
