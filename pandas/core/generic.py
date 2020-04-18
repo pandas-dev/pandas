150b28207520d6ec0a6b273b825fc482ebc6dd2b
David Mrva <david.mrva@gmail.com>
2020-04-18 12:49:18 +0100

    revert to Appender in dynamically generated functions

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -10656,6 +10656,60 @@ Returns
 %(examples)s
 """
 
+_num_ddof_doc = """	
+%(desc)s	
+Parameters	
+----------	
+axis : %(axis_descr)s	
+skipna : bool, default True	
+    Exclude NA/null values. If an entire row/column is NA, the result	
+    will be NA.	
+level : int or level name, default None	
+    If the axis is a MultiIndex (hierarchical), count along a	
+    particular level, collapsing into a %(name1)s.	
+ddof : int, default 1	
+    Delta Degrees of Freedom. The divisor used in calculations is N - ddof,	
+    where N represents the number of elements.	
+numeric_only : bool, default None	
+    Include only float, int, boolean columns. If None, will attempt to use	
+    everything, then use only numeric data. Not implemented for Series.	
+Returns	
+-------	
+%(name1)s or %(name2)s (if level specified)\n"""
+
+_bool_doc = """	
+%(desc)s	
+Parameters	
+----------	
+axis : {0 or 'index', 1 or 'columns', None}, default 0	
+    Indicate which axis or axes should be reduced.	
+    * 0 / 'index' : reduce the index, return a Series whose index is the	
+      original column labels.	
+    * 1 / 'columns' : reduce the columns, return a Series whose index is the	
+      original index.	
+    * None : reduce all axes, return a scalar.	
+bool_only : bool, default None	
+    Include only boolean columns. If None, will attempt to use everything,	
+    then use only boolean data. Not implemented for Series.	
+skipna : bool, default True	
+    Exclude NA/null values. If the entire row/column is NA and skipna is	
+    True, then the result will be %(empty_value)s, as for an empty row/column.	
+    If skipna is False, then NA are treated as True, because these are not	
+    equal to zero.	
+level : int or level name, default None	
+    If the axis is a MultiIndex (hierarchical), count along a	
+    particular level, collapsing into a %(name1)s.	
+**kwargs : any, default None	
+    Additional keywords have no effect but might be accepted for	
+    compatibility with NumPy.	
+Returns	
+-------	
+%(name1)s or %(name2)s	
+    If level is specified, then, %(name2)s is returned; otherwise, %(name1)s	
+    is returned.	
+%(see_also)s	
+%(examples)s"""
+
 _all_desc = """\
 Return whether all elements are True, potentially over an axis.
 
@@ -10715,6 +10769,36 @@ See Also
 Series.all : Return True if all elements are True.
 DataFrame.any : Return True if one (or more) elements are True.
 """
+
+_cnum_doc = """	
+Return cumulative %(desc)s over a DataFrame or Series axis.	
+Returns a DataFrame or Series of the same size containing the cumulative	
+%(desc)s.	
+Parameters	
+----------	
+axis : {0 or 'index', 1 or 'columns'}, default 0	
+    The index or the name of the axis. 0 is equivalent to None or 'index'.	
+skipna : bool, default True	
+    Exclude NA/null values. If an entire row/column is NA, the result	
+    will be NA.	
+*args, **kwargs	
+    Additional keywords have no effect but might be accepted for	
+    compatibility with NumPy.	
+Returns	
+-------	
+%(name1)s or %(name2)s	
+    Return cumulative %(desc)s of %(name1)s or %(name2)s.	
+See Also	
+--------	
+core.window.Expanding.%(accum_func_name)s : Similar functionality	
+    but ignores ``NaN`` values.	
+%(name2)s.%(accum_func_name)s : Return the %(desc)s over	
+    %(name2)s axis.	
+%(name2)s.cummax : Return cumulative maximum over %(name2)s axis.	
+%(name2)s.cummin : Return cumulative minimum over %(name2)s axis.	
+%(name2)s.cumsum : Return cumulative sum over %(name2)s axis.	
+%(name2)s.cumprod : Return cumulative product over %(name2)s axis.	
+%(examples)s"""
 
 _cummin_examples = """\
 Examples
@@ -11284,32 +11368,11 @@ def _make_stat_function(
 def _make_stat_function_ddof(
     cls, name: str, name1: str, name2: str, axis_descr: str, desc: str, func: Callable
 ) -> Callable:
-    @doc(desc=desc, name1=name1, name2=name2, axis_descr=axis_descr)
+    @Substitution(desc=desc, name1=name1, name2=name2, axis_descr=axis_descr)
+    @Appender(_num_ddof_doc)
     def stat_func(
         self, axis=None, skipna=None, level=None, ddof=1, numeric_only=None, **kwargs
     ):
-        """
-        {desc}
-
-        Parameters
-        ----------
-        axis : {axis_descr}
-        skipna : bool, default True
-            Exclude NA/null values. If an entire row/column is NA, the result
-            will be NA.
-        level : int or level name, default None
-            If the axis is a MultiIndex (hierarchical), count along a
-            particular level, collapsing into a %(name1)s.
-        ddof : int, default 1
-            Delta Degrees of Freedom. The divisor used in calculations is N - ddof,
-            where N represents the number of elements.
-        numeric_only : bool, default None
-            Include only float, int, boolean columns. If None, will attempt to use
-            everything, then use only numeric data. Not implemented for Series.
-
-        Returns
-        -------
-        {name1} or {name2} (if level specified)\n"""
         nv.validate_stat_ddof_func(tuple(), kwargs, fname=name)
         if skipna is None:
             skipna = True
@@ -11337,48 +11400,16 @@ def _make_cum_function(
     accum_func_name: str,
     examples: str,
 ) -> Callable:
-    @doc(
+    @Substitution(
         desc=desc,
         name1=name1,
         name2=name2,
+        axis_descr=axis_descr,
         accum_func_name=accum_func_name,
         examples=examples,
     )
+    @Appender(_cnum_doc)
     def cum_func(self, axis=None, skipna=True, *args, **kwargs):
-        """
-        Return cumulative {desc} over a DataFrame or Series axis.
-
-        Returns a DataFrame or Series of the same size containing the cumulative
-        {desc}.
-
-        Parameters
-        ----------
-        axis : {{0 or 'index', 1 or 'columns'}}, default 0
-            The index or the name of the axis. 0 is equivalent to None or 'index'.
-        skipna : bool, default True
-            Exclude NA/null values. If an entire row/column is NA, the result
-            will be NA.
-        *args, **kwargs
-            Additional keywords have no effect but might be accepted for
-            compatibility with NumPy.
-
-        Returns
-        -------
-        {name1} or {name2}
-            Return cumulative {desc} of {name1} or {name2}.
-
-        See Also
-        --------
-        core.window.Expanding.{accum_func_name} : Similar functionality
-            but ignores ``NaN`` values.
-        {name2}.{accum_func_name} : Return the {desc} over
-            {name2} axis.
-        {name2}.cummax : Return cumulative maximum over {name2} axis.
-        {name2}.cummin : Return cumulative minimum over {name2} axis.
-        {name2}.cumsum : Return cumulative sum over {name2} axis.
-        {name2}.cumprod : Return cumulative product over {name2} axis.
-
-        {examples}"""
         skipna = nv.validate_cum_func_with_skipna(skipna, args, kwargs, name)
         if axis is None:
             axis = self._stat_axis_number
@@ -11415,52 +11446,17 @@ def _make_logical_function(
     examples: str,
     empty_value: bool,
 ) -> Callable:
-    @doc(
+    @Substitution(
         desc=desc,
         name1=name1,
         name2=name2,
+        axis_descr=axis_descr,
         see_also=see_also,
         examples=examples,
-        empty_value=str(empty_value),
+        empty_value=empty_value,
     )
+    @Appender(_bool_doc)
     def logical_func(self, axis=0, bool_only=None, skipna=True, level=None, **kwargs):
-        """
-        {desc}
-
-        Parameters
-        ----------
-        axis : {{0 or 'index', 1 or 'columns', None}}, default 0
-           Indicate which axis or axes should be reduced.
-
-           * 0 / 'index' : reduce the index, return a Series whose index is the
-             original column labels.
-           * 1 / 'columns' : reduce the columns, return a Series whose index is the
-             original index.
-           * None : reduce all axes, return a scalar.
-
-        bool_only : bool, default None
-           Include only boolean columns. If None, will attempt to use everything,
-           then use only boolean data. Not implemented for Series.
-        skipna : bool, default True
-           Exclude NA/null values. If the entire row/column is NA and skipna is
-           True, then the result will be {empty_value}, as for an empty row/column.
-           If skipna is False, then NA are treated as True, because these are not
-           equal to zero.
-        level : int or level name, default None
-           If the axis is a MultiIndex (hierarchical), count along a
-           particular level, collapsing into a {name1}.
-        **kwargs : any, default None
-           Additional keywords have no effect but might be accepted for
-           compatibility with NumPy.
-
-        Returns
-        -------
-        {name1} or {name2}
-           If level is specified, then, {name2} is returned; otherwise, {name1}
-           is returned.
-
-        {see_also}
-        {examples}"""
         nv.validate_logical_func(tuple(), kwargs, fname=name)
         if level is not None:
             if bool_only is not None:
