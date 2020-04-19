7aa8d559bb472b56e542dd053ab5a6b75f743d03
phofl <patrick_hoefler@gmx.net>
2020-04-19 23:59:54 +0200

    Add previous/new section and relocate tests

--- pandas/tests/groupby/aggregate/test_aggregate.py
+++ pandas/tests/groupby/aggregate/test_aggregate.py
@@ -717,6 +717,41 @@ def test_agg_relabel_multiindex_duplicates():
     tm.assert_frame_equal(result, expected)
 
 
+def test_grouby_agg_loses_results_with_as_index_false_relabel():
+    # GH 32240: When the aggregate function relabels column names and
+    # as_index=False is specified, the results are dropped.
+
+    df = pd.DataFrame(
+        {"key": ["x", "y", "z", "x", "y", "z"], "val": [1.0, 0.8, 2.0, 3.0, 3.6, 0.75]}
+    )
+
+    grouped = df.groupby("key", as_index=False)
+    result = grouped.agg(min_val=pd.NamedAgg(column="val", aggfunc="min"))
+    expected = pd.DataFrame({"key": ["x", "y", "z"], "min_val": [1.0, 0.8, 0.75]})
+    tm.assert_frame_equal(result, expected)
+
+
+def test_grouby_agg_loses_results_with_as_index_false_relabel_multiindex():
+    # GH 32240: When the aggregate function relabels column names and
+    # as_index=False is specified, the results are dropped. Check if
+    # multiindex is returned in the right order
+
+    df = pd.DataFrame(
+        {
+            "key": ["x", "y", "x", "y", "x", "x"],
+            "key1": ["a", "b", "c", "b", "a", "c"],
+            "val": [1.0, 0.8, 2.0, 3.0, 3.6, 0.75],
+        }
+    )
+
+    grouped = df.groupby(["key", "key1"], as_index=False)
+    result = grouped.agg(min_val=pd.NamedAgg(column="val", aggfunc="min"))
+    expected = pd.DataFrame(
+        {"key": ["x", "x", "y"], "key1": ["a", "c", "b"], "min_val": [1.0, 0.75, 0.8]}
+    )
+    tm.assert_frame_equal(result, expected)
+
+
 @pytest.mark.parametrize(
     "func", [lambda s: s.mean(), lambda s: np.mean(s), lambda s: np.nanmean(s)]
 )
@@ -819,41 +854,6 @@ def test_groupby_aggregate_period_frame(func):
     idx = pd.Int64Index([1, 2], name="a")
     expected = pd.DataFrame({"b": periods}, index=idx)
 
-    tm.assert_frame_equal(result, expected)
-
-
-def test_grouby_agg_loses_results_with_as_index_false_relabel():
-    # GH 32240: When the aggregate function relabels column names and
-    # as_index=False is specified, the results are dropped.
-
-    df = pd.DataFrame(
-        {"key": ["x", "y", "z", "x", "y", "z"], "val": [1.0, 0.8, 2.0, 3.0, 3.6, 0.75]}
-    )
-
-    grouped = df.groupby("key", as_index=False)
-    result = grouped.agg(min_val=pd.NamedAgg(column="val", aggfunc="min"))
-    expected = pd.DataFrame({"key": ["x", "y", "z"], "min_val": [1.0, 0.8, 0.75]})
-    tm.assert_frame_equal(result, expected)
-
-
-def test_grouby_agg_loses_results_with_as_index_false_relabel_multiindex():
-    # GH 32240: When the aggregate function relabels column names and
-    # as_index=False is specified, the results are dropped. Check if
-    # multiindex is returned in the right order
-
-    df = pd.DataFrame(
-        {
-            "key": ["x", "y", "x", "y", "x", "x"],
-            "key1": ["a", "b", "c", "b", "a", "c"],
-            "val": [1.0, 0.8, 2.0, 3.0, 3.6, 0.75],
-        }
-    )
-
-    grouped = df.groupby(["key", "key1"], as_index=False)
-    result = grouped.agg(min_val=pd.NamedAgg(column="val", aggfunc="min"))
-    expected = pd.DataFrame(
-        {"key": ["x", "x", "y"], "key1": ["a", "c", "b"], "min_val": [1.0, 0.75, 0.8]}
-    )
     tm.assert_frame_equal(result, expected)
 
 
