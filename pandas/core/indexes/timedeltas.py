ad54dcca8a217145355b9d52762a427fe02204fd
jbrockmendel <jbrockmendel@gmail.com>
2020-04-20 15:14:16 -0700

    CLN: Remove redundant mixin to TimedeltaIndex (#33679)

--- pandas/core/indexes/timedeltas.py
+++ pandas/core/indexes/timedeltas.py
@@ -51,7 +51,7 @@ from pandas.tseries.frequencies import to_offset
     ],
     TimedeltaArray,
 )
-class TimedeltaIndex(DatetimeTimedeltaMixin, dtl.TimelikeOps):
+class TimedeltaIndex(DatetimeTimedeltaMixin):
     """
     Immutable ndarray of timedelta64 data, represented internally as int64, and
     which can be boxed to timedelta objects.
