5cdf0b59b840c8348658d3bd6eb5a1347169a92c
jbrockmendel <jbrockmendel@gmail.com>
2020-04-21 13:47:07 -0700

    troubleshoot asvs

--- asv_bench/benchmarks/stat_ops.py
+++ asv_bench/benchmarks/stat_ops.py
@@ -11,7 +11,7 @@ class FrameOps:
     param_names = ["op", "dtype", "axis"]
 
     def setup(self, op, dtype, axis):
-        if op == "mad" and dtype == "Int64" and axis == 0:
+        if op == "mad" and dtype == "Int64":
             # GH-33036, GH#33600
             raise NotImplementedError
         values = np.random.randn(100000, 4)
