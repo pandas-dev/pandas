939530e4c8b191cff82b1c7614c83faa314ce9bd
Simon Hawkins <simonjayhawkins@gmail.com>
2020-04-23 04:09:10 +0100

    CLN: misc cleanups from LGTM (#33724)

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -275,7 +275,6 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
                         "Cannot construct a Series from an ndarray with "
                         "compound dtype.  Use DataFrame instead."
                     )
-                pass
             elif isinstance(data, ABCSeries):
                 if index is None:
                     index = data.index
