221835d7f49feb04eacb289b0fa62ccc84f82a9b
jbrockmendel <jbrockmendel@gmail.com>
2020-04-22 17:49:16 -0700

    split_parametrize

--- pandas/tests/indexes/numeric/test_astype.py
+++ pandas/tests/indexes/numeric/test_astype.py
@@ -10,68 +10,70 @@ import pandas._testing as tm
 
 
 class TestAstype:
-    def test_astype(self):
-
+    def test_astype_float64_to_object(self):
         float_index = Float64Index([0.0, 2.5, 5.0, 7.5, 10.0])
         result = float_index.astype(object)
         assert result.equals(float_index)
         assert float_index.equals(result)
         assert isinstance(result, Index) and not isinstance(result, Float64Index)
 
+    def test_astype_float64_mixed_to_object(self):
         # mixed int-float
-        i = Float64Index([1.5, 2, 3, 4, 5])
-        i.name = "foo"
-        result = i.astype(object)
-        assert result.equals(i)
-        assert i.equals(result)
+        idx = Float64Index([1.5, 2, 3, 4, 5])
+        idx.name = "foo"
+        result = idx.astype(object)
+        assert result.equals(idx)
+        assert idx.equals(result)
         assert isinstance(result, Index) and not isinstance(result, Float64Index)
 
+    @pytest.mark.parametrize("dtype", ["int16", "int32", "int64"])
+    def test_astype_float64_to_int_dtype(self, dtype):
         # GH#12881
         # a float astype int
-        for dtype in ["int16", "int32", "int64"]:
-            i = Float64Index([0, 1, 2])
-            result = i.astype(dtype)
-            expected = Int64Index([0, 1, 2])
-            tm.assert_index_equal(result, expected)
+        idx = Float64Index([0, 1, 2])
+        result = idx.astype(dtype)
+        expected = Int64Index([0, 1, 2])
+        tm.assert_index_equal(result, expected)
 
-            i = Float64Index([0, 1.1, 2])
-            result = i.astype(dtype)
-            expected = Int64Index([0, 1, 2])
-            tm.assert_index_equal(result, expected)
+        idx = Float64Index([0, 1.1, 2])
+        result = idx.astype(dtype)
+        expected = Int64Index([0, 1, 2])
+        tm.assert_index_equal(result, expected)
 
-        for dtype in ["float32", "float64"]:
-            i = Float64Index([0, 1, 2])
-            result = i.astype(dtype)
-            expected = i
-            tm.assert_index_equal(result, expected)
+    @pytest.mark.parametrize("dtype", ["float32", "float64"])
+    def test_astype_float64_to_float_dtype(self, dtype):
+        # GH#12881
+        # a float astype int
+        idx = Float64Index([0, 1, 2])
+        result = idx.astype(dtype)
+        expected = idx
+        tm.assert_index_equal(result, expected)
 
-            i = Float64Index([0, 1.1, 2])
-            result = i.astype(dtype)
-            expected = Index(i.values.astype(dtype))
-            tm.assert_index_equal(result, expected)
+        idx = Float64Index([0, 1.1, 2])
+        result = idx.astype(dtype)
+        expected = Index(idx.values.astype(dtype))
+        tm.assert_index_equal(result, expected)
 
-        # invalid
-        for dtype in ["M8[ns]", "m8[ns]"]:
-            msg = (
-                f"Cannot convert Float64Index to dtype {pandas_dtype(dtype)}; "
-                f"integer values are required for conversion"
-            )
-            with pytest.raises(TypeError, match=re.escape(msg)):
-                i.astype(dtype)
+    @pytest.mark.parametrize("dtype", ["M8[ns]", "m8[ns]"])
+    def test_cannot_cast_to_datetimelike(self, dtype):
+        idx = Float64Index([0, 1.1, 2])
 
+        msg = (
+            f"Cannot convert Float64Index to dtype {pandas_dtype(dtype)}; "
+            f"integer values are required for conversion"
+        )
+        with pytest.raises(TypeError, match=re.escape(msg)):
+            idx.astype(dtype)
+
+    @pytest.mark.parametrize("dtype", [int, "int16", "int32", "int64"])
+    @pytest.mark.parametrize("non_finite", [np.inf, np.nan])
+    def test_cannot_cast_inf_to_int(self, non_finite, dtype):
         # GH#13149
-        for dtype in ["int16", "int32", "int64"]:
-            i = Float64Index([0, 1.1, np.NAN])
-            msg = r"Cannot convert non-finite values \(NA or inf\) to integer"
-            with pytest.raises(ValueError, match=msg):
-                i.astype(dtype)
-
-    def test_cannot_cast_inf_to_int(self):
-        idx = Float64Index([1, 2, np.inf])
+        idx = Float64Index([1, 2, non_finite])
 
         msg = r"Cannot convert non-finite values \(NA or inf\) to integer"
         with pytest.raises(ValueError, match=msg):
-            idx.astype(int)
+            idx.astype(dtype)
 
     def test_astype_from_object(self):
         index = Index([1.0, np.nan, 0.2], dtype="object")
