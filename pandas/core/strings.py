d99d1c99ff8dd23622754fae5c3917e9673f95fd
jbrockmendel <jbrockmendel@gmail.com>
2020-04-24 11:44:14 -0700

    revert

--- pandas/core/strings.py
+++ pandas/core/strings.py
@@ -2134,8 +2134,7 @@ class StringMethods(NoNewAttributesMixin):
         # see _libs/lib.pyx for list of inferred types
         allowed_types = ["string", "empty", "bytes", "mixed", "mixed-integer"]
 
-        values = getattr(data, "values", data)  # Series / Index  # without this we segfault in tests.io.excel.test_readers
-        #values = extract_array(data, extract_numpy=True)  # breaks tests.extension.test_numpy tests bc infer_dtype breaks on PandasArray/PandasDtype
+        values = getattr(data, "values", data)  # Series / Index
         values = getattr(values, "categories", values)  # categorical / normal
 
         # explicitly allow StringDtype
