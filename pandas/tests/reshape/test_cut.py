777e13e75877763b95f458ce2031f385bb946e2a
Mabel Villalba <mabelvj@gmail.com>
2020-04-26 18:10:54 +0200

    BUG: Added GH issue to test. Updated docstrings (#33141)

--- pandas/tests/reshape/test_cut.py
+++ pandas/tests/reshape/test_cut.py
@@ -660,6 +660,7 @@ def test_cut_unordered_labels(data, bins, labels, expected_codes, expected_label
 
 
 def test_cut_unordered_with_missing_labels_raises_error():
+    # GH 33141
     msg = "'labels' must be provided if 'ordered = False'"
     with pytest.raises(ValueError, match=msg):
         cut([0.5, 3], bins=[0, 1, 2], ordered=False)
