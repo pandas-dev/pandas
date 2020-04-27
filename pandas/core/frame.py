c2c059b78b98a21e2eed55094c149ce401cc4d83
Daniel Saxton <dsaxton@pm.me>
2020-04-27 16:36:16 -0500

    Don't copy

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -7870,7 +7870,7 @@ Wild         185.0
         numeric_df = self._get_numeric_data()
         cols = numeric_df.columns
         idx = cols.copy()
-        mat = numeric_df.astype(float).to_numpy()
+        mat = numeric_df.astype(float, copy=False).to_numpy()
 
         if method == "pearson":
             correl = libalgos.nancorr(mat, minp=min_periods)
@@ -8005,7 +8005,7 @@ Wild         185.0
         numeric_df = self._get_numeric_data()
         cols = numeric_df.columns
         idx = cols.copy()
-        mat = numeric_df.astype(float).to_numpy()
+        mat = numeric_df.astype(float, copy=False).to_numpy()
 
         if notna(mat).all():
             if min_periods is not None and min_periods > len(mat):
