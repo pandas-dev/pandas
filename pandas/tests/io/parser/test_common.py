5ebc88564d7604f96809bb63a7e08aaf0e303266
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2020-04-28 17:54:20 -0500

    CI: Skip permissions test when running as sudo (#33847)

--- pandas/tests/io/parser/test_common.py
+++ pandas/tests/io/parser/test_common.py
@@ -974,6 +974,15 @@ def test_no_permission(all_parsers):
     msg = r"\[Errno 13\]"
     with tm.ensure_clean() as path:
         os.chmod(path, 0)  # make file unreadable
+
+        # verify that this process cannot open the file (not running as sudo)
+        try:
+            with open(path):
+                pass
+            pytest.skip("Running as sudo.")
+        except PermissionError:
+            pass
+
         with pytest.raises(PermissionError, match=msg) as e:
             parser.read_csv(path)
         assert path == e.value.filename
