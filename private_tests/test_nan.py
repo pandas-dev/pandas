6e14ff833d515d334972125ee40689827af1d814
Tobias Pitters <tobias.pitters@gmail.com>
2020-05-01 17:56:32 +0200

    removed git tracking of test file

--- private_tests/test_nan.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-
-if __name__ == '__main__':
-    df = pd.DataFrame(dict(x=[np.nan, np.nan]))
-    import pdb; pdb.set_trace()
-    res = df.max()
-    print(res)
