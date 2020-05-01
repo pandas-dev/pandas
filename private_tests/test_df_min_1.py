0d9a754eef893a9cfb69f8ca127b5f766741cf45
Tobias Pitters <tobias.pitters@gmail.com>
2020-05-01 17:55:53 +0200

    removed git tracking of test file

--- private_tests/test_df_min_1.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-if __name__ == '__main__':
-    df = pd.DataFrame(dict(x=pd.to_datetime([])))
-    series = pd.Series(pd.to_datetime([]))
-    res = df.min(1)
-    print(res)
-
