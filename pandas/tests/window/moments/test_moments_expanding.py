6bf8c7f21cb3808614801149dcee3a078153a341
Kaiqi <kaiqi@kth.se>
2020-05-03 09:43:17 +0200

    remove slow for exapnding

--- pandas/tests/window/moments/test_moments_expanding.py
+++ pandas/tests/window/moments/test_moments_expanding.py
@@ -219,7 +219,6 @@ def test_expanding_consistency_std(consistency_data, min_periods):
     )
 
 
-@pytest.mark.slow
 @pytest.mark.parametrize("min_periods", [0, 1, 2, 3, 4])
 def test_expanding_consistency_cov(consistency_data, min_periods):
     x, is_constant, no_nans = consistency_data
