9c87e0031f4893fb9acdc0f50e7b6d17eecede47
Daniel Saxton <dsaxton@pm.me>
2020-05-04 07:21:12 -0500

    side effect test

--- pandas/tests/base/test_conversion.py
+++ pandas/tests/base/test_conversion.py
@@ -444,3 +444,11 @@ def test_to_numpy_dataframe_single_block(data, expected):
     df = pd.DataFrame(data)
     result = df.to_numpy(dtype=float, na_value=np.nan)
     tm.assert_numpy_array_equal(result, expected)
+
+
+def test_to_numpy_dataframe_single_block_no_mutate():
+    # https://github.com/pandas-dev/pandas/issues/33820
+    result = pd.DataFrame(np.array([1.0, 2.0, np.nan]))
+    expected = pd.DataFrame(np.array([1.0, 2.0, np.nan]))
+    result.to_numpy(dtype=float, na_value=0.0)
+    tm.assert_frame_equal(result, expected)
