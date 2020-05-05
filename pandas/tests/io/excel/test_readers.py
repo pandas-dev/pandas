aa8cfe9a90af53f7fb7c1a366b732e37c3cb70a5
mproszewska <magdalena.proszewska@gmail.com>
2020-05-05 03:05:42 +0200

    Fix lint

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -1085,7 +1085,7 @@ class TestExcelFileRead:
         actual = pd.read_excel("high_surrogate.xlsx")
         tm.assert_frame_equal(expected, actual)
 
-    @pytest.mark.parametrize("header, skiprows", [(1, 2), (0, 3)]) 
+    @pytest.mark.parametrize("header, skiprows", [(1, 2), (0, 3)])
     def test_header_skiprows_nrows(self, engine, read_ext, header, skiprows):
         data = pd.read_excel("test1" + read_ext, engine=engine)
         expected = (
