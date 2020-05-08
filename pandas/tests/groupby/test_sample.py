8c3dfd8936cad465f7e08d7c3d9bc8782e43131b
Daniel Saxton <dsaxton@pm.me>
2020-05-08 16:20:12 -0500

    Edit tests

--- pandas/tests/groupby/test_sample.py
+++ pandas/tests/groupby/test_sample.py
@@ -6,31 +6,35 @@ import pandas._testing as tm
 
 @pytest.mark.parametrize("n, frac", [(2, None), (None, 0.2)])
 def test_groupby_sample_balanced_groups_shape(n, frac):
-    df = DataFrame({"a": [1] * 10 + [2] * 10, "b": [1] * 20})
+    values = [1] * 10 + [2] * 10
+    df = DataFrame({"a": values, "b": values})
 
     result = df.groupby("a").sample(n=n, frac=frac)
-    expected = DataFrame({"a": [1] * 2 + [2] * 2, "b": [1] * 4}, index=result.index)
+    values = [1] * 2 + [2] * 2
+    expected = DataFrame({"a": values, "b": values}, index=result.index)
     tm.assert_frame_equal(result, expected)
 
     result = df.groupby("a")["b"].sample(n=n, frac=frac)
-    expected = Series([1] * 4, name="b", index=result.index)
+    expected = Series(values, name="b", index=result.index)
     tm.assert_series_equal(result, expected)
 
 
 def test_groupby_sample_unbalanced_groups_shape():
-    df = DataFrame({"a": [1] * 10 + [2] * 20, "b": [1] * 30})
+    values = [1] * 10 + [2] * 20
+    df = DataFrame({"a": values, "b": values})
 
     result = df.groupby("a").sample(n=5)
-    expected = DataFrame({"a": [1] * 5 + [2] * 5, "b": [1] * 10}, index=result.index)
+    values = [1] * 5 + [2] * 5
+    expected = DataFrame({"a": values, "b": values}, index=result.index)
     tm.assert_frame_equal(result, expected)
 
     result = df.groupby("a")["b"].sample(n=5)
-    expected = Series([1] * 10, name="b", index=result.index)
+    expected = Series(values, name="b", index=result.index)
     tm.assert_series_equal(result, expected)
 
 
 def test_groupby_sample_n_and_frac_raises():
-    df = DataFrame({"a": [1] * 10 + [2] * 10, "b": [1] * 20})
+    df = DataFrame({"a": [1, 2], "b": [1, 2]})
     msg = "Please enter a value for `frac` OR `n`, not both"
 
     with pytest.raises(ValueError, match=msg):
@@ -41,7 +45,7 @@ def test_groupby_sample_n_and_frac_raises():
 
 
 def test_groupby_sample_frac_gt_one_without_replacement_raises():
-    df = DataFrame({"a": [1] * 10 + [2] * 10, "b": [1] * 20})
+    df = DataFrame({"a": [1, 2], "b": [1, 2]})
     msg = "Replace has to be set to `True` when upsampling the population `frac` > 1."
 
     with pytest.raises(ValueError, match=msg):
@@ -53,7 +57,7 @@ def test_groupby_sample_frac_gt_one_without_replacement_raises():
 
 @pytest.mark.parametrize("n", [-1, 1.5])
 def test_groupby_sample_invalid_n_raises(n):
-    df = DataFrame({"a": [1] * 10 + [2] * 10, "b": [1] * 20})
+    df = DataFrame({"a": [1, 2], "b": [1, 2]})
 
     if n < 0:
         msg = "Please provide positive value"
@@ -68,38 +72,40 @@ def test_groupby_sample_invalid_n_raises(n):
 
 
 def test_groupby_sample_oversample():
-    df = DataFrame({"a": [1] * 10 + [2] * 10, "b": [1] * 20})
+    values = [1] * 10 + [2] * 10
+    df = DataFrame({"a": values, "b": values})
 
     result = df.groupby("a").sample(frac=2.0, replace=True)
-    expected = DataFrame({"a": [1] * 20 + [2] * 20, "b": [1] * 40}, index=result.index)
+    values = [1] * 20 + [2] * 20
+    expected = DataFrame({"a": values, "b": values}, index=result.index)
     tm.assert_frame_equal(result, expected)
 
     result = df.groupby("a")["b"].sample(frac=2.0, replace=True)
-    expected = Series([1] * 40, name="b", index=result.index)
+    expected = Series(values, name="b", index=result.index)
     tm.assert_series_equal(result, expected)
 
 
 def test_groupby_sample_without_n_or_frac():
-    df = DataFrame({"a": [1] * 10 + [2] * 10, "b": [1] * 20})
+    values = [1] * 10 + [2] * 10
+    df = DataFrame({"a": values, "b": values})
 
     result = df.groupby("a").sample(n=None, frac=None)
-    expected = DataFrame({"a": [1, 2], "b": [1, 1]}, index=result.index)
+    expected = DataFrame({"a": [1, 2], "b": [1, 2]}, index=result.index)
     tm.assert_frame_equal(result, expected)
 
     result = df.groupby("a")["b"].sample(n=None, frac=None)
-    expected = Series([1, 1], name="b", index=result.index)
+    expected = Series([1, 2], name="b", index=result.index)
     tm.assert_series_equal(result, expected)
 
 
 def test_groupby_sample_with_weights():
-    df = DataFrame({"a": [1] * 2 + [2] * 2, "b": [1] * 4}, index=Index([0, 1, 2, 3]))
+    values = [1] * 2 + [2] * 2
+    df = DataFrame({"a": values, "b": values}, index=Index([0, 1, 2, 3]))
 
     result = df.groupby("a").sample(n=2, replace=True, weights=[1, 0, 1, 0])
-    expected = DataFrame(
-        {"a": [1] * 2 + [2] * 2, "b": [1] * 4}, index=Index([0, 0, 2, 2])
-    )
+    expected = DataFrame({"a": values, "b": values}, index=Index([0, 0, 2, 2]))
     tm.assert_frame_equal(result, expected)
 
     result = df.groupby("a")["b"].sample(n=2, replace=True, weights=[1, 0, 1, 0])
-    expected = Series([1, 1, 1, 1], name="b", index=Index([0, 0, 2, 2]))
+    expected = Series(values, name="b", index=Index([0, 0, 2, 2]))
     tm.assert_series_equal(result, expected)
