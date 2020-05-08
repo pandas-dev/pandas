0482a4a44085662f8b9ce56a6d49e24fc9537294
Clément Robert <cr52@protonmail.com>
2020-05-08 16:38:52 +0200

    doc: clarify boxplot whisker's extension

--- pandas/plotting/_core.py
+++ pandas/plotting/_core.py
@@ -216,8 +216,9 @@ by some other columns. A box plot is a method for graphically depicting
 groups of numerical data through their quartiles.
 The box extends from the Q1 to Q3 quartile values of the data,
 with a line at the median (Q2). The whiskers extend from the edges
-of box to show the range of the data. The position of the whiskers
-is set by default to `1.5 * IQR (IQR = Q3 - Q1)` from the edges of the box.
+of box to show the range of the data. By default, they extend up to
+`1.5 * IQR (IQR = Q3 - Q1)` from the edges of the box, or to the farther
+data point if it lies within this interval.
 Outlier points are those past the end of the whiskers.
 
 For further details see
