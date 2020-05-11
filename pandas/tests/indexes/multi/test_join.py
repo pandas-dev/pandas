e72eebc2706846f42c6a240bf9aa7fdf20fc8e13
evert <evert.cuylen@student.kuleuven.be>
2020-05-11 17:56:00 +0200

    BUG: updated multijoin parameter test

--- pandas/tests/indexes/multi/test_join.py
+++ pandas/tests/indexes/multi/test_join.py
@@ -111,4 +111,5 @@ def test_join_multi_return_indexers():
     midx1 = pd.MultiIndex.from_product([[1, 2], [3, 4], [5, 6]], names=["a", "b", "c"])
     midx2 = pd.MultiIndex.from_product([[1, 2], [3, 4]], names=["a", "b"])
 
-    assert type(midx1.join(midx2, return_indexers=False)) != tuple
+    result = midx1.join(midx2, return_indexers=False)
+    tm.assert_index_equal(result, midx1)
