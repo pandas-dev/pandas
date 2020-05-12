049e8abdd76b81f624ca5d7ca1a350213ff74dd8
jbrockmendel <jbrockmendel@gmail.com>
2020-05-12 06:14:38 -0700

    ENH: make Tick comparisons match Timedelta behavior (#34088)

--- pandas/tests/tseries/offsets/test_ticks.py
+++ pandas/tests/tseries/offsets/test_ticks.py
@@ -320,3 +320,22 @@ def test_compare_ticks_to_strs(cls):
             left > right
         with pytest.raises(TypeError):
             left >= right
+
+
+@pytest.mark.parametrize("cls", tick_classes)
+def test_compare_ticks_to_timedeltalike(cls):
+    off = cls(19)
+
+    td = off.delta
+
+    others = [td, td.to_timedelta64()]
+    if cls is not Nano:
+        others.append(td.to_pytimedelta())
+
+    for other in others:
+        assert off == other
+        assert not off != other
+        assert not off < other
+        assert not off > other
+        assert off <= other
+        assert off >= other
