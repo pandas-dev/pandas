f112b7c96d432dc35fed7bfaede14216e6d41f61
mproszewska <magdalena.proszewska@gmail.com>
2020-05-15 13:30:00 +0200

    Add requested changes

--- pandas/tests/window/test_apply.py
+++ pandas/tests/window/test_apply.py
@@ -141,7 +141,8 @@ def test_invalid_kwargs_nopython():
         )
 
 
-def test_rolling_apply_args_kwargs():
+@pytest.mark.parametrize("args_kwargs", [[None, {"par": 10}], [(10,), None]])
+def test_rolling_apply_args_kwargs(args_kwargs):
     # GH 33433
     def foo(x, par):
         return np.sum(x + par)
@@ -151,19 +152,15 @@ def test_rolling_apply_args_kwargs():
     idx = Index(["gr", "a"])
     expected = DataFrame([[11.0, 11.0], [11.0, 12.0]], columns=idx)
 
-    result = df.rolling(1).apply(foo, kwargs={"par": 10})
+    result = df.rolling(1).apply(foo, args=args_kwargs[0], kwargs=args_kwargs[1])
     tm.assert_frame_equal(result, expected)
 
     result = df.rolling(1).apply(foo, args=(10,))
-    tm.assert_frame_equal(result, expected)
 
     midx = MultiIndex.from_tuples([(1, 0), (1, 1)], names=["gr", None])
     expected = Series([11.0, 12.0], index=midx, name="a")
 
     gb_rolling = df.groupby("gr")["a"].rolling(1)
 
-    result = gb_rolling.apply(foo, kwargs={"par": 10})
-    tm.assert_series_equal(result, expected)
-
-    result = gb_rolling.apply(foo, args=(10,))
+    result = gb_rolling.apply(foo, args=args_kwargs[0], kwargs=args_kwargs[1])
     tm.assert_series_equal(result, expected)
