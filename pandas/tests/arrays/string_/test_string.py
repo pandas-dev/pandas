1f90a62964ca4dd3326aa5917c07efe7e7aaf275
Marco Gorelli <m.e.gorelli@gmail.com>
2020-05-17 12:37:43 +0100

    wip

--- pandas/tests/arrays/string_/test_string.py
+++ pandas/tests/arrays/string_/test_string.py
@@ -11,10 +11,10 @@ import pandas._testing as tm
 
 def test_repr():
     df = pd.DataFrame({"A": pd.array(["a", pd.NA, "b"], dtype="string")})
-    expected = "      A\n0     a\n1  <NA>\n2     b"
+    expected = "      A\n0   'a'\n1  <NA>\n2   'b'"
     assert repr(df) == expected
 
-    expected = "0       a\n1    <NA>\n2       b\nName: A, dtype: string"
+    expected = "0     'a'\n1    <NA>\n2     'b'\nName: A, dtype: string"
     assert repr(df.A) == expected
 
     expected = "<StringArray>\n['a', <NA>, 'b']\nLength: 3, dtype: string"
