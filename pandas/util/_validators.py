727fea6be4992f8f518a173cc9f3409462f0c941
matteosantama <matteosantama@gmail.com>
2020-05-26 22:32:13 +0000

    Black formatting

--- pandas/util/_validators.py
+++ pandas/util/_validators.py
@@ -295,13 +295,13 @@ def validate_axis_style_args(data, args, kwargs, arg_name, method_name):
             raise TypeError(msg)
 
         msg = (
-            f"Interpreting call\n\t'.{method_name}(a, b)' as " +
-            f"\n\t'.{method_name}(index=a, columns=b)'.\nUse named " +
-            f"arguments to remove any ambiguity. In the future, using " +
-            f"positional arguments for 'index' or 'columns' will raise " +
-            f"a 'TypeError'."
+            f"Interpreting call\n\t'.{method_name}(a, b)' as "
+            + f"\n\t'.{method_name}(index=a, columns=b)'.\nUse named "
+            + f"arguments to remove any ambiguity. In the future, using "
+            + f"positional arguments for 'index' or 'columns' will raise "
+            + f"a 'TypeError'."
         )
-        warnings.warn(msg.format(msg, FutureWarning, stacklevel=4)
+        warnings.warn(msg, FutureWarning, stacklevel=4)
         out[data._get_axis_name(0)] = args[0]
         out[data._get_axis_name(1)] = args[1]
     else:
@@ -371,7 +371,10 @@ def validate_percentile(q: Union[float, Iterable[float]]) -> np.ndarray:
     ValueError if percentiles are not in given interval([0, 1]).
     """
     q_arr = np.asarray(q)
-    msg = f"percentiles should all be in the interval [0, 1]. Try {q_arr / 100.0} instead."
+    msg = (
+        f"percentiles should all be in the interval [0, 1]. "
+        f"Try {q_arr / 100.0} instead."
+    )
     if q_arr.ndim == 0:
         if not 0 <= q_arr <= 1:
             raise ValueError(msg)
