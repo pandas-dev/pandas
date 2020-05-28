7233382de9a4f84d1cc49c4fee1eb801ebd96165
guru kiran <jagatagurukiran.07@gmail.com>
2020-05-28 23:22:51 +0530

    'BUG: #GH34422 raises TypeError when parameters to named aggregation are tuple, list or NamedAgg'

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -240,7 +240,7 @@ class SeriesGroupBy(GroupBy[Series]):
             func = []
             for col in columns:
                 if isinstance(kwargs[col],(list,NamedAgg,tuple)):
-                    raise TypeError(tuple_given_message.format(type(kwargs[col])))
+                    raise TypeError(tuple_given_message.format(type(kwargs[col]).__name__))
                 func.append(kwargs[col])
             
             kwargs = {}
