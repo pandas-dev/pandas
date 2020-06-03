b398ccdc79003012353f1244c26256e7e2dbe392
mproszewska <magdalena.proszewska@gmail.com>
2020-06-03 02:05:36 +0200

    Run tests

--- pandas/core/sorting.py
+++ pandas/core/sorting.py
@@ -387,6 +387,7 @@ def ensure_key_mapped(values, key: Optional[Callable], levels=None):
     if not key:
         return values
 
+
     if isinstance(values, ABCMultiIndex):
         return ensure_key_mapped_multiindex(values, key, level=levels)
 
