81371a4f0b8025bb789f686fefb7e53507b9569c
alimcmaster1 <alimcmaster1@gmail.com>
2020-06-09 08:36:50 +0100

    Revert backport of #33632: Parquet & s3 I/O changes (#34632)
    Also skip for it_IT locale

--- pandas/io/gcs.py
+++ pandas/io/gcs.py
@@ -6,10 +6,6 @@ gcsfs = import_optional_dependency(
 )
 
 
-def get_fs():
-    return gcsfs.GCSFileSystem()
-
-
 def get_filepath_or_buffer(
     filepath_or_buffer, encoding=None, compression=None, mode=None
 ):
@@ -17,6 +13,6 @@ def get_filepath_or_buffer(
     if mode is None:
         mode = "rb"
 
-    fs = get_fs()
+    fs = gcsfs.GCSFileSystem()
     filepath_or_buffer = fs.open(filepath_or_buffer, mode)
     return filepath_or_buffer, None, compression, True
