2e06a8b3c274b1470c3b4720fac084279bd21554
arw2019 <andrew.r.wieteska@gmail.com>
2020-06-10 21:24:34 +0000

    Update pandas/_libs/src/ujson/python/objToJSON.c

--- pandas/_libs/src/ujson/python/objToJSON.c
+++ pandas/_libs/src/ujson/python/objToJSON.c
@@ -2129,9 +2129,7 @@ double Object_getDoubleValue(JSOBJ Py_UNUSED(obj), JSONTypeContext *tc) {
 const char *Object_getBigNumStringValue(JSOBJ obj, JSONTypeContext *tc, 
                                     size_t *_outLen) {
     PyObject* repr = PyObject_Str(obj);
-    Py_ssize_t* _py_ssize_t_outLen;
     const char *str = PyUnicode_AsUTF8AndSize(repr, <Py_ssize_t> *_outLen);
-    // need to cast _py_ssize_t_outLen as C size_t type (_outLen)
     char* bytes = malloc(*_outLen);
     memcpy(bytes, str, *_outLen);
     GET_TC(tc)->cStr = bytes;
