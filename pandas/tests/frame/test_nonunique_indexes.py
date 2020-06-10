ba0cedf8bccf71db618d1c3a42ee2fcd87a4783a
OlivierLuG <olivierlu.63@free.fr>
2020-06-10 20:48:02 +0200

    TST #20627 added tests, and take review into account

--- pandas/tests/frame/test_nonunique_indexes.py
+++ pandas/tests/frame/test_nonunique_indexes.py
@@ -513,23 +513,3 @@ class TestDataFrameNonuniqueIndexes:
 
         df.iloc[:, 0] = 3
         tm.assert_series_equal(df.iloc[:, 1], expected)
-
-
-def test_iloc_with_boolean_operation():
-    # GH 20627
-    result = DataFrame([[0, 1], [2, 3], [4, 5], [6, np.nan]])
-    result.iloc[result.index <= 2] *= 2
-    expected = DataFrame([[0, 2], [4, 6], [8, 10], [6, np.nan]])
-    tm.assert_frame_equal(result, expected)
-
-    result.iloc[result.index > 2] *= 2
-    expected = DataFrame([[0, 2], [4, 6], [8, 10], [12, np.nan]])
-    tm.assert_frame_equal(result, expected)
-
-    result.iloc[[True, True, False, False]] *= 2
-    expected = DataFrame([[0, 4], [8, 12], [8, 10], [12, np.nan]])
-    tm.assert_frame_equal(result, expected)
-
-    result.iloc[[False, False, True, True]] /= 2
-    expected = DataFrame([[0.0, 4.0], [8.0, 12.0], [4.0, 5.0], [6.0, np.nan]])
-    tm.assert_frame_equal(result, expected)
