a296e95fead8f4ef343d4db561342df8cbea1c91
jbrockmendel <jbrockmendel@gmail.com>
2020-06-14 07:13:00 -0700

    BUG: DataFrame.unstack with non-consolidated (#34709)

--- pandas/tests/frame/test_reshape.py
+++ pandas/tests/frame/test_reshape.py
@@ -140,6 +140,17 @@ class TestDataFrameReshape:
         expected = expected[["a", "b"]]
         tm.assert_frame_equal(result, expected)
 
+    def test_unstack_not_consolidated(self):
+        # Gh#34708
+        df = pd.DataFrame({"x": [1, 2, np.NaN], "y": [3.0, 4, np.NaN]})
+        df2 = df[["x"]]
+        df2["y"] = df["y"]
+        assert len(df2._mgr.blocks) == 2
+
+        res = df2.unstack()
+        expected = df.unstack()
+        tm.assert_series_equal(res, expected)
+
     def test_unstack_fill(self):
 
         # GH #9746: fill_value keyword argument for Series
