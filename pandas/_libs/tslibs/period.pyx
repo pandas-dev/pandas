95d740e5fd9e2be58b6004baf22ab9e154a2678e
OlivierLuG <olivierlu.63@free.fr>
2020-06-15 21:58:33 +0200

    #34621 review taken into account

--- pandas/_libs/tslibs/period.pyx
+++ pandas/_libs/tslibs/period.pyx
@@ -2407,11 +2407,11 @@ class Period(_Period):
             value = value.upper()
             dt, reso = parse_time_string(value, freq)
             try:
-                ts = Timestamp(value, freq=freq)
+                ts = Timestamp(value)
                 nanosecond = ts.nanosecond
                 if nanosecond != 0:
                     reso = 'nanosecond'
-            except (ValueError, OutOfBoundsDatetime):
+            except ValueError:
                 nanosecond = 0
             if dt is NaT:
                 ordinal = NPY_NAT
@@ -2498,7 +2498,7 @@ def quarter_to_myear(year: int, quarter: int, freq):
     mnum = c_MONTH_NUMBERS[get_rule_month(freq)] + 1
     month = (mnum + (quarter - 1) * 3) % 12 + 1
     if month > mnum:
-        year -= 1
+        year -= 1ough to only use ValueError, as that would already cover the OutOfBoundsDatet
 
     return year, month
     # TODO: This whole func is really similar to parsing.pyx L434-L450
