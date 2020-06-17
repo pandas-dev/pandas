52e6486bfd82f75fc8d672f86db65767051c12ec
datajanko <jan.koch@tu-dortmund.de>
2020-06-17 21:47:12 +0200

    adds todo for runtime warning

--- pandas/tests/extension/base/accumulate.py
+++ pandas/tests/extension/base/accumulate.py
@@ -38,6 +38,8 @@ class BaseNumericAccumulateTests(BaseAccumulateTests):
         op_name = all_numeric_accumulations
         s = pd.Series(data)
 
+        # TODO: check if needed, copied from reduce
+        # min/max with empty produce numpy warnings
         with warnings.catch_warnings():
             warnings.simplefilter("ignore", RuntimeWarning)
             self.check_accumulate(s, op_name, skipna)
