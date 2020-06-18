645905da0e7c80bdc7cebcab91f44da4a90a48e0
Marco Gorelli <m.e.gorelli@gmail.com>
2020-06-18 15:08:19 +0100

    regroup tests

--- pandas/tests/plotting/test_datetimelike.py
+++ pandas/tests/plotting/test_datetimelike.py
@@ -1493,30 +1493,28 @@ class TestTSPlot(TestPlotBase):
 
     def test_check_xticks_rot(self):
         # https://github.com/pandas-dev/pandas/issues/29460
+        # regular time series
+        x = to_datetime(["2020-05-01", "2020-05-02", "2020-05-03"])
+        df = DataFrame({"x": x, "y": [1, 2, 3]})
+        axes = df.plot(x="x", y="y")
+        self._check_ticks_props(axes, xrot=0)
+
         # irregular time series
         x = to_datetime(["2020-05-01", "2020-05-02", "2020-05-04"])
         df = DataFrame({"x": x, "y": [1, 2, 3]})
         axes = df.plot(x="x", y="y")
         self._check_ticks_props(axes, xrot=30)
 
-        # use timeseries index
+        # use timeseries index or not
         axes = df.set_index("x").plot(y="y", use_index=True)
         self._check_ticks_props(axes, xrot=30)
+        axes = df.set_index("x").plot(y="y", use_index=False)
+        self._check_ticks_props(axes, xrot=0)
 
         # separate subplots
         axes = df.plot(x="x", y="y", subplots=True, sharex=True)
         self._check_ticks_props(axes, xrot=30)
         axes = df.plot(x="x", y="y", subplots=True, sharex=False)
-        self._check_ticks_props(axes, xrot=0)
-
-        # index of dates but don't use index
-        axes = df.set_index("x").plot(y="y", use_index=False)
-        self._check_ticks_props(axes, xrot=0)
-
-        # regular time series
-        x = to_datetime(["2020-05-01", "2020-05-02", "2020-05-03"])
-        df = DataFrame({"x": x, "y": [1, 2, 3]})
-        axes = df.plot(x="x", y="y")
         self._check_ticks_props(axes, xrot=0)
 
 
