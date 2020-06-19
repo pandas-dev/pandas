90e2a288ed2378f289f7cfea8c0a0b8a4025b5e9
Matt Roeschke <mroeschke@housecanary.com>
2020-06-19 11:04:57 -0700

    black file

--- pandas/tests/window/test_ewm.py
+++ pandas/tests/window/test_ewm.py
@@ -86,9 +86,7 @@ def test_ewma_halflife_not_correct_type():
 
 
 def test_ewma_halflife_without_times(halflife_with_times):
-    msg = (
-        "halflife can only be a timedelta convertible argument if times is not None."
-    )
+    msg = "halflife can only be a timedelta convertible argument if times is not None."
     with pytest.raises(ValueError, match=msg):
         Series(range(5)).ewm(halflife=halflife_with_times)
 
