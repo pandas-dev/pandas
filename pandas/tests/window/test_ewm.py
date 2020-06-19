37962fb9344fd0f9099192ded34834c14e377852
Matt Roeschke <mroeschke@housecanary.com>
2020-06-19 09:10:55 -0700

    Merge split string

--- pandas/tests/window/test_ewm.py
+++ pandas/tests/window/test_ewm.py
@@ -87,7 +87,7 @@ def test_ewma_halflife_not_correct_type():
 
 def test_ewma_halflife_without_times(halflife_with_times):
     msg = (
-        "halflife can only be a timedelta " "convertible argument if times is not None."
+        "halflife can only be a timedelta convertible argument if times is not None."
     )
     with pytest.raises(ValueError, match=msg):
         Series(range(5)).ewm(halflife=halflife_with_times)
