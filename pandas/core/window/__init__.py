70498be93d1ff20ce94f8546c03d34e94c024532
KrishnaSai2020 <krishnasai.chivukula@gmal.com>
2020-06-20 19:22:06 +0100

    fixed pep8 issue

--- pandas/core/window/__init__.py
+++ pandas/core/window/__init__.py
@@ -1,3 +1,3 @@
-from pandas.core.window.exponentialmovingwindow import ExponentialMovingWindow  # noqa:F401
+from pandas.core.window.exponentialmoving import ExponentialMovingWindow  # noqa:F401
 from pandas.core.window.expanding import Expanding, ExpandingGroupby  # noqa:F401
 from pandas.core.window.rolling import Rolling, RollingGroupby, Window  # noqa:F401
