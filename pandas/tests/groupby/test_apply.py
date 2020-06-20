0d9ce0368a9e50721a97b0d5e743e7165cb92dc3
Rohith295 <rangaraju963@gmail.com>
2020-06-20 15:28:50 +0200

    :white_check_mark:

--- pandas/tests/groupby/test_apply.py
+++ pandas/tests/groupby/test_apply.py
@@ -979,16 +979,15 @@ def test_apply_function_with_indexing_return_column():
 def test_apply_function_called_count(capsys):
     # GH: 31111
     # groupby-apply need to execute len(set(group_by_columns)) times
-    # `https://github.com/pandas-dev/pandas/issues/31111`
-    
 
-    function_called_count = 2 # Number of times `apply` should call a function for the current test
-    
+    expected = 2  # Number of times `apply` should call a function for the current test
+
     df = pd.DataFrame({"group_by_column": [0, 0, 0, 0, 1, 1, 1, 1],
-                    "test_column": ["0", "2", "4", "6", "8", "10", "12", "14"]},
-                   index=["0", "2", "4",  "6", "8", "10", "12", "14"])
+                       "test_column": ["0", "2", "4", "6", "8", "10", "12", "14"]},
+                      index=["0", "2", "4", "6", "8", "10", "12", "14"])
 
-    df.groupby('group_by_column').apply(lambda df:print("function_called"))
+    df.groupby('group_by_column').apply(lambda df: print("function_called"))
 
+    result = capsys.readouterr().out.count("function_called")
     # If `groupby` behaves unexpectedly, this test will break
-    assert capsys.readouterr().out.count("function_called") == function_called_count
+    assert result == expected
