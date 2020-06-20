669f84a576320e1433421e74bfaeea851077f821
Dan Basson <danbas87@gmail.com>
2020-06-20 16:21:45 +0300

    CLN: GH29547 format with f-strings

--- pandas/tests/series/indexing/test_numeric.py
+++ pandas/tests/series/indexing/test_numeric.py
@@ -83,19 +83,19 @@ def test_setitem_float_labels():
 
 
 def test_slice_float_get_set(datetime_series):
-    msg = lambda key: (
+    msg = (
         "cannot do slice indexing on DatetimeIndex with these indexers "
-        f"{key} of type float"
+        r"\[{key}\] of type float"
     )
-    with pytest.raises(TypeError, match=msg(key=r"4\.0")):
+    with pytest.raises(TypeError, match=msg.format(key=r"4\.0")):
         datetime_series[4.0:10.0]
 
-    with pytest.raises(TypeError, match=msg(key=r"4\.0")):
+    with pytest.raises(TypeError, match=msg.format(key=r"4\.0")):
         datetime_series[4.0:10.0] = 0
 
-    with pytest.raises(TypeError, match=msg(key=r"4\.5")):
+    with pytest.raises(TypeError, match=msg.format(key=r"4\.5")):
         datetime_series[4.5:10.0]
-    with pytest.raises(TypeError, match=msg(key=r"4\.5")):
+    with pytest.raises(TypeError, match=msg.format(key=r"4\.5")):
         datetime_series[4.5:10.0] = 0
 
 
