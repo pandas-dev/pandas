59580b7d989adf577671ad69910ce305d91a45bd
Krishna Chivukula <63070026+KrishnaSai2020@users.noreply.github.com>
2020-06-20 18:42:11 +0100

    Update pandas/core/generic.py
    Co-authored-by: Kaiqi Dong <kaiqidong1991@gmail.com>

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -10460,8 +10460,7 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
         Add the series or dataframe only operations to the cls; evaluate
         the doc strings again.
         """
-        from pandas.core.window import ExponentialMovingWindow, Expanding, Rolling
-        from pandas.core.window import Window
+        from pandas.core.window import Expanding, ExponentialMovingWindow, Rolling, Window
 
         @doc(Rolling)
         def rolling(
