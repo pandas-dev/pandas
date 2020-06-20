b10bc720e41171844c23695c4a73fc35b1d8b353
Krishna Chivukula <63070026+KrishnaSai2020@users.noreply.github.com>
2020-06-20 15:00:35 +0100

    updated init.py

--- pandas/core/window/__init__.py
+++ pandas/core/window/__init__.py
@@ -1,3 +1,3 @@
-from pandas.core.window.ewf import EWF  # noqa:F401
+from pandas.core.window.ewf import exponentialmoving   # noqa:F401
 from pandas.core.window.expanding import Expanding, ExpandingGroupby  # noqa:F401
 from pandas.core.window.rolling import Rolling, RollingGroupby, Window  # noqa:F401
