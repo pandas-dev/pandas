7e71d06a064025181ae5d9776ce1cf6e9db2d6f9
alimcmaster1 <alimcmaster1@gmail.com>
2020-06-20 10:43:17 +0100

    Arrow skip

--- pandas/tests/io/test_feather.py
+++ pandas/tests/io/test_feather.py
@@ -115,6 +115,7 @@ class TestFeather:
         columns = ["col1", "col3"]
         self.check_round_trip(df, expected=df[columns], columns=columns)
 
+    @td.skip_if_no("pyarrow", min_version="0.17.1")
     def read_columns_different_order(self):
         # GH 33878
         df = pd.DataFrame({"A": [1, 2], "B": ["x", "y"], "C": [True, False]})
