dcdc223a09987dee2956e3b8be4c0c2f802809cb
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-06-22 12:56:12 -0500

    PERF: Fixed cut regression, improve Categorical
    This has two changes to address a performance regression in
    cut / qcut.
    
    1. Avoid an unnecessary `set` conversion in cut.
    2. Aviod a costly conversion to object in the Categoriacl constructor
       for dtypes we don't have a hashtable for.
    
    ```python
    In [2]: idx = pd.interval_range(0, 1, periods=10000)
    In [3]: %timeit pd.Categorical(idx, idx)
    ```
    
    ```
    10.4 ms ± 351 µs per loop (mean ± std. dev. of 7 runs, 100 loops each)
    
    256 ms ± 5.85 ms per loop (mean ± std. dev. of 7 runs, 1 loop each)
    
    53.2 µs ± 1.26 µs per loop (mean ± std. dev. of 7 runs, 10000 loops each)
    ```
    
    And for the qcut ASV
    
    ```
    58.5 ms ± 3.13 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
    
    134 ms ± 9.5 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
    
    29.8 ms ± 1.24 ms per loop (mean ± std. dev. of 7 runs, 10 loops each)
    ```
    
    Closes https://github.com/pandas-dev/pandas/issues/33921

--- pandas/core/reshape/tile.py
+++ pandas/core/reshape/tile.py
@@ -413,6 +413,7 @@ def _bins_to_cuts(
 
     na_mask = isna(x) | (ids == len(bins)) | (ids == 0)
     has_nas = na_mask.any()
+    labels_are_unique = None
 
     if labels is not False:
         if not (labels is None or is_list_like(labels)):
@@ -425,6 +426,7 @@ def _bins_to_cuts(
             labels = _format_labels(
                 bins, precision, right=right, include_lowest=include_lowest, dtype=dtype
             )
+            labels_are_unique = True
         elif ordered and len(set(labels)) != len(labels):
             raise ValueError(
                 "labels must be unique if ordered=True; pass ordered=False for duplicate labels"  # noqa
@@ -435,11 +437,14 @@ def _bins_to_cuts(
                     "Bin labels must be one fewer than the number of bin edges"
                 )
         if not is_categorical_dtype(labels):
-            labels = Categorical(
-                labels,
-                categories=labels if len(set(labels)) == len(labels) else None,
-                ordered=ordered,
-            )
+            if labels_are_unique:
+                categories = labels
+            elif labels_are_unique is None:
+                categories = labels if len(set(labels)) == len(labels) else None
+            else:
+                categories = None
+
+            labels = Categorical(labels, categories=categories, ordered=ordered)
         # TODO: handle mismatch between categorical label order and pandas.cut order.
         np.putmask(ids, na_mask, 0)
         result = algos.take_nd(labels, ids - 1)
