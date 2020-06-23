510bce602f639e2bab8f71b97f45a11aebd09e39
Rohith295 <rangaraju963@gmail.com>
2020-06-23 13:15:27 +0200

    Fixing the failing type annotation checks and also the code comments

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -355,12 +355,12 @@ class Series(base.IndexOpsMixin, generic.NDFrame):
         # raises KeyError), so we iterate the entire dict, and align
         if data:
             # GH:34717
-            # Using the below way to generate tuple of keys and values
+            # Using the below way for generating keys and values
             # increasing the performance by 50%, instead of zip
             keys = tuple(data.keys())
-            values = (
-                *[data[key] for key in keys],
-            )  # Generating tuple of values- faster way
+            values = [
+                data[key] for key in keys
+            ]  # Generating list of values- faster way
 
         elif index is not None:
             # fastpath for Series(data=None). Just use broadcasting a scalar
