bab926afdcb444084aa896aeef5cc3a9657bc7bf
Ketan <6256964+ketanarlulkar@users.noreply.github.com>
2020-06-24 21:30:49 +0530

    BUG: indexing regression with datetime index (#34917)

--- pandas/tests/indexing/test_partial.py
+++ pandas/tests/indexing/test_partial.py
@@ -650,3 +650,13 @@ class TestPartialSetting:
             s[labels]
         with pytest.raises(KeyError, match=msg):
             df.loc[labels]
+
+    def test_indexing_timeseries_regression(self):
+        # Issue 34860
+        arr = date_range("1/1/2008", "1/1/2009")
+        result = arr.to_series()["2008"]
+
+        rng = date_range(start="2008-01-01", end="2008-12-31")
+        expected = Series(rng, index=rng)
+
+        tm.assert_series_equal(result, expected)
