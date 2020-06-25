BUG: exponential moving window covariance fails for multiIndexed DataFrame (#34943)
* added test for df.ewm.cov with multiindex

* BUG: fixed _flex_binary_moment for multiindex

* added reference to GH issue

* DOC: updated whatnew

* DOC: moved note to rolling section of whatsnew

* changed df to fixed seed, linearly spaced ints

* removed extraneous comment

* TST: hardcoded expected df for test_multiindex_cov

* TST: cleaned up comment + blank line

* TST: clean up index definition

pandas/tests/window/test_pairwise.py
@@ -3,7 +3,7 @@ import warnings
 import numpy as np
 import pytest
 
-from pandas import DataFrame, Series, date_range
+from pandas import DataFrame, MultiIndex, Series, date_range
 import pandas._testing as tm
 from pandas.core.algorithms import safe_sort
 
@@ -189,3 +189,28 @@ class TestPairwise:
         result = s.rolling("12H").corr(s)
         expected = Series([np.nan] * 5, index=date_range("2020", periods=5))
         tm.assert_series_equal(result, expected)
+
+    def test_cov_mulittindex(self):
+        # GH 34440
+
+        columns = MultiIndex.from_product([list("ab"), list("xy"), list("AB")])
+        index = range(3)
+        df = DataFrame(np.arange(24).reshape(3, 8), index=index, columns=columns,)
+
+        result = df.ewm(alpha=0.1).cov()
+
+        index = MultiIndex.from_product([range(3), list("ab"), list("xy"), list("AB")])
+        columns = MultiIndex.from_product([list("ab"), list("xy"), list("AB")])
+        expected = DataFrame(
+            np.vstack(
+                (
+                    np.full((8, 8), np.NaN),
+                    np.full((8, 8), 32.000000),
+                    np.full((8, 8), 63.881919),
+                )
+            ),
+            index=index,
+            columns=columns,
+        )
+
+        tm.assert_frame_equal(result, expected)