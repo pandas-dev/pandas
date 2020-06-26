bf1919bad057b477d39fe00b4f9264f326b46385
Daniel Saxton <2658661+dsaxton@users.noreply.github.com>
2020-06-26 15:23:04 -0500

    ERR: Fix to_timedelta error message (#34981)

--- pandas/core/indexes/timedeltas.py
+++ pandas/core/indexes/timedeltas.py
@@ -136,7 +136,7 @@ class TimedeltaIndex(DatetimeTimedeltaMixin):
 
         if unit in {"Y", "y", "M"}:
             raise ValueError(
-                "Units 'M' and 'Y' are no longer supported, as they do not "
+                "Units 'M', 'Y', and 'y' are no longer supported, as they do not "
                 "represent unambiguous timedelta values durations."
             )
 
--- pandas/tests/indexes/timedeltas/test_constructors.py
+++ pandas/tests/indexes/timedeltas/test_constructors.py
@@ -12,7 +12,7 @@ from pandas.core.arrays import TimedeltaArray
 class TestTimedeltaIndex:
     @pytest.mark.parametrize("unit", ["Y", "y", "M"])
     def test_unit_m_y_raises(self, unit):
-        msg = "Units 'M' and 'Y' are no longer supported"
+        msg = "Units 'M', 'Y', and 'y' are no longer supported"
         with pytest.raises(ValueError, match=msg):
             TimedeltaIndex([1, 3, 7], unit)
 
