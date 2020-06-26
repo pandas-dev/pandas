fdf5e739993e0158532f50c8a33d456509223ecf
Erfan Nariman <34067903+erfannariman@users.noreply.github.com>
2020-06-26 22:22:23 +0200

    ENH: add ignore_index option in DataFrame.explode (#34933)

--- pandas/tests/frame/methods/test_explode.py
+++ pandas/tests/frame/methods/test_explode.py
@@ -162,3 +162,13 @@ def test_duplicate_index(input_dict, input_index, expected_dict, expected_index)
     result = df.explode("col1")
     expected = pd.DataFrame(expected_dict, index=expected_index, dtype=object)
     tm.assert_frame_equal(result, expected)
+
+
+def test_ignore_index():
+    # GH 34932
+    df = pd.DataFrame({"id": range(0, 20, 10), "values": [list("ab"), list("cd")]})
+    result = df.explode("values", ignore_index=True)
+    expected = pd.DataFrame(
+        {"id": [0, 0, 10, 10], "values": list("abcd")}, index=[0, 1, 2, 3]
+    )
+    tm.assert_frame_equal(result, expected)
