a8e86e3aab36a2f5cae8cebf2194cbf13094a8e1
Matt Roeschke <mroeschke@housecanary.com>
2020-06-27 16:22:05 -0700

    Test timezones

--- pandas/tests/window/test_ewm.py
+++ pandas/tests/window/test_ewm.py
@@ -116,11 +116,14 @@ def test_ewma_with_times_equal_spacing(halflife_with_times, times, min_periods):
     tm.assert_frame_equal(result, expected)
 
 
-def test_ewma_with_times_variable_spacing():
+def test_ewma_with_times_variable_spacing(tz_aware_fixture):
+    tz = tz_aware_fixture
     halflife = "23 days"
-    times = DatetimeIndex(["2020-01-01", "2020-01-10T00:04:05", "2020-03-23T05:00:23"])
+    times = DatetimeIndex(
+        ["2020-01-01", "2020-01-10T00:04:05", "2020-02-23T05:00:23"]
+    ).tz_localize(tz)
     data = np.arange(3)
     df = DataFrame(data)
     result = df.ewm(halflife=halflife, times=times).mean()
-    expected = DataFrame([0.0, 0.567416, 1.767163])
+    expected = DataFrame([0.0, 0.5674161888241773, 1.545239952073459])
     tm.assert_frame_equal(result, expected)
