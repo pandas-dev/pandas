cf67f903456e8742381b0f83eaa312529602708d
arw2019 <andrew.r.wieteska@gmail.com>
2020-06-27 05:35:09 +0000

    remove unnecessary styling changes

--- pandas/_libs/tslib.pyx
+++ pandas/_libs/tslib.pyx
@@ -416,6 +416,7 @@ def array_with_unit_to_datetime(
     m = cast_from_unit(None, unit)
 
     if is_raise:
+
         # try a quick conversion to i8
         # if we have nulls that are not type-compat
         # then need to iterate
@@ -439,6 +440,7 @@ def array_with_unit_to_datetime(
 
         # check the bounds
         if not need_to_iterate:
+
             if ((fvalues < Timestamp.min.value).any()
                     or (fvalues > Timestamp.max.value).any()):
                 raise OutOfBoundsDatetime(f"cannot convert input with unit '{unit}'")
@@ -606,6 +608,7 @@ cpdef array_to_datetime(
         float offset_seconds, tz_offset
         set out_tzoffset_vals = set()
         bint string_to_dts_failed
+        
     # specify error conditions
     assert is_raise or is_ignore or is_coerce
 
