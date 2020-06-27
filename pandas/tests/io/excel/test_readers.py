bf0612c595fed0090a18419560a92cc47853e26f
rjfs <rafajfsantos@gmail.com>
2020-06-27 20:20:29 +0100

    BUG: fix read_excel error for header=None and index_col as list GH31783

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -1131,6 +1131,20 @@ class TestExcelFileRead:
         actual = pd.read_excel("high_surrogate.xlsx")
         tm.assert_frame_equal(expected, actual)
 
+    @pytest.mark.new
+    def test_no_header_with_list_index_col(self, engine):
+        # GH 31783
+        file_name = "testmultiindex.xlsx"
+        data = [("B", "B"), ("key", "val"), (3, 4), (3, 4)]
+        idx = pd.MultiIndex.from_tuples(
+            [("A", "A"), ("key", "val"), (1, 2), (1, 2)], names=(0, 1)
+        )
+        expected = pd.DataFrame(data, index=idx, columns=(2, 3))
+        result = pd.read_excel(
+            file_name, sheet_name="index_col_none", index_col=[0, 1], header=None
+        )
+        tm.assert_frame_equal(expected, result)
+
     @pytest.mark.parametrize("filename", ["df_empty.xlsx", "df_equals.xlsx"])
     def test_header_with_index_col(self, engine, filename):
         # GH 33476
