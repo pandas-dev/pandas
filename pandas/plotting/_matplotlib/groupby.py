b90303d930be5f389f395f3bfbd259e0eb4bcf69
Kaiqi <kaiqi@kth.se>
2020-06-27 22:10:10 +0200

    improve docstring

--- pandas/plotting/_matplotlib/groupby.py
+++ pandas/plotting/_matplotlib/groupby.py
@@ -105,8 +105,8 @@ def reformat_hist_y_given_by(
 ) -> Union[Series, np.array]:
     """Internal function to reformat y given `by` is applied or not for hist plot.
 
-    If by is None, input y is 1-d array; and if by is not None, groupby will take
-    place and input y is multi-dimensional array.
+    If by is None, input y is 1-d with NaN removed; and if by is not None, groupby
+    will take place and input y is multi-dimensional array.
     """
     if by is not None and len(y.shape) > 1:
         notna = [col[~isna(col)] for col in y.T]
