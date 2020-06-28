46259b77469e7a956c10cd15ce2ee7229a9fc4b3
biddwan <biddwan@yellowmessenger.com>
2020-06-28 20:29:44 +0600

    fix linting

--- pandas/tests/groupby/aggregate/test_aggregate.py
+++ pandas/tests/groupby/aggregate/test_aggregate.py
@@ -998,18 +998,26 @@ def test_groupby_get_by_index():
 
 def test_aggregate_categorical_with_isnan():
     # GH 29837
-    df = pd.DataFrame({'A': [1, 1, 1, 1],
-                       'B': [1, 2, 1, 2],
-                       'numerical_col': [.1, .2, np.nan, .3],
-                       'object_col': ['foo', 'bar', 'foo', 'fee'],
-                       'categorical_col': ['foo', 'bar', 'foo', 'fee']
-                       })
+    df = pd.DataFrame(
+        {
+            "A": [1, 1, 1, 1],
+            "B": [1, 2, 1, 2],
+            "numerical_col": [0.1, 0.2, np.nan, 0.3],
+            "object_col": ["foo", "bar", "foo", "fee"],
+            "categorical_col": ["foo", "bar", "foo", "fee"],
+        }
+    )
 
-    df = df.astype({'categorical_col': 'category'})
+    df = df.astype({"categorical_col": "category"})
 
-    result = df.groupby(['A', 'B']).agg(lambda df: df.isna().sum())
-    index = pd.MultiIndex.from_arrays([[1, 1], [1, 2]], names=('A', 'B'))
-    expected = pd.DataFrame(data={'numerical_col': [1.0, 0.0],
-                            'object_col': [0, 0], 'categorical_col': [0, 0]},
-                            index=index)
+    result = df.groupby(["A", "B"]).agg(lambda df: df.isna().sum())
+    index = pd.MultiIndex.from_arrays([[1, 1], [1, 2]], names=("A", "B"))
+    expected = pd.DataFrame(
+        data={
+            "numerical_col": [1.0, 0.0],
+            "object_col": [0, 0],
+            "categorical_col": [0, 0],
+        },
+        index=index,
+    )
     pd.testing.assert_frame_equal(result, expected)
