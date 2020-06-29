8b3e07b9d953392dd3fd5c01f18750463b4f3080
Santhosh18 <santhosh.balakrishnan@datakalp.com>
2020-06-29 18:28:09 +0530

    Fixed Linting issues

--- pandas/tests/series/methods/test_explode.py
+++ pandas/tests/series/methods/test_explode.py
@@ -9,7 +9,8 @@ def test_basic():
     s = pd.Series([[0, 1, 2], np.nan, [], (3, 4)], index=list("abcd"), name="foo")
     result = s.explode()
     expected = pd.Series(
-        [0, 1, 2, np.nan, np.nan, 3, 4], index=list("aaabcdd"), dtype=np.float64, name="foo"
+        [0, 1, 2, np.nan, np.nan, 3, 4], index=list("aaabcdd"),
+        dtype=np.float64, name="foo"
     )
     tm.assert_series_equal(result, expected)
 
