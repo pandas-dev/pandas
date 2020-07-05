81116d47a9fd1b5ea82f0bbfc1055178b5013afb
Alexander Kirko <alexander.kirko@gmail.com>
2020-07-05 12:40:34 +0300

    test without multi, range, datetime in exceptions

--- pandas/core/indexes/api.py
+++ pandas/core/indexes/api.py
@@ -210,7 +210,7 @@ def union_indexes(indexes, sort=True) -> Index:
     # setting doesn't make sense
     ind_types = [type(index) for index in indexes]
     if any(
-        ind_type in [MultiIndex, RangeIndex]
+        ind_type in [CategoricalIndex]
         for ind_type in ind_types
     ):
         ignore_sort = True
