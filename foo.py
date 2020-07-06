ca29cfd2594fe65ad03c7c7ac5b4e5bf4e6600d9
Will Ayd <william.ayd@icloud.com>
2020-07-06 16:42:28 -0700

    deleted errant file

--- foo.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DataFrame.to_json.html
-#df = pd.DataFrame([['a', 'b'], ['c', 'd']],index=['row 1', 'row 2'],columns=['col 1', 'col 2'])
-
-df = pd.DataFrame([["a", "b"], ["c", "d"]], columns=["x", "x"])
-print(df.to_json(orient="split"))
-
-
