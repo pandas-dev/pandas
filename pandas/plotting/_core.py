dd0a1fe3a48f6be4052fffea2be3af73370e06d8
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2020-07-06 09:53:33 -0500

    Update pandas/plotting/_core.py

--- pandas/plotting/_core.py
+++ pandas/plotting/_core.py
@@ -553,7 +553,6 @@ def boxplot_frame_groupby(
         :context: close-figs
 
         >>> import itertools
-        >>> from pandas.plotting import boxplot_frame_groupby
         >>> tuples = [t for t in itertools.product(range(1000), range(4))]
         >>> index = pd.MultiIndex.from_tuples(tuples, names=['lvl0', 'lvl1'])
         >>> data = np.random.randn(len(index),4)
