eae32df7d2b48672b4423b60fd4b4753a069154a
Fangchen Li <fangchen.li@outlook.com>
2020-07-06 20:05:33 -0500

    CI: install sql for Azure Pipeline

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -164,9 +164,13 @@ if [[ -n ${SQL:0} ]]; then
   if [[ -n ${SQL_SETUP:0} ]]; then
     echo "setting up SQL dbs on Azure Pipelines"
     sudo apt-get install -y mysql-server
+
+    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+    sudo apt-get update
     sudo apt-get install -y postgres
   else
-    echo  ""
+    echo  "SQL dbs not installed"
   fi
 
   echo "installing dbs"
