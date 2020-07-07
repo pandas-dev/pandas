ed338e1d12faee6a833db20444bca7e2544d9c0a
arw2019 <andrew.r.wieteska@gmail.com>
2020-07-07 06:36:42 +0000

    _transform_general: add a check that we don't accidentally upcast

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -39,6 +39,7 @@ from pandas.core.dtypes.cast import (
     maybe_cast_result_dtype,
     maybe_convert_objects,
     maybe_downcast_numeric,
+    maybe_downcast_to_dtype,
 )
 from pandas.core.dtypes.common import (
     ensure_int64,
@@ -46,6 +47,7 @@ from pandas.core.dtypes.common import (
     is_bool,
     is_integer_dtype,
     is_interval_dtype,
+    is_numeric_dtype,
     is_object_dtype,
     is_scalar,
     needs_i8_conversion,
@@ -544,6 +546,15 @@ class SeriesGroupBy(GroupBy[Series]):
             result = concat(results).sort_index()
         else:
             result = self.obj._constructor(dtype=np.float64)
+
+        # we will only try to coerce the result type if
+        # we have a numeric dtype, as these are *always* user-defined funcs
+        # the cython take a different path (and casting)
+        # make sure we don't accidentally upcast (GH35014)
+        types = ["bool", "int64", "float64"]
+        dtype = self._selected_obj.dtype
+        if is_numeric_dtype(dtype) and types.index(dtype) < types.index(result.dtype):
+            result = maybe_downcast_to_dtype(result, dtype)
 
         result.name = self._selected_obj.name
         result.index = self._selected_obj.index
