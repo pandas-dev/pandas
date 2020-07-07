064bb49ea9dcb13188c7c5a9431a177b3112c8ee
Fangchen Li <fangchen.li@outlook.com>
2020-07-06 20:44:03 -0500

    CI: fix sql script

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -162,13 +162,9 @@ conda list
 if [[ -n ${SQL:0} ]]; then
 
   if [[ -n ${SQL_SETUP:0} ]]; then
-    echo "setting up SQL dbs on Azure Pipelines"
-    sudo apt-get install -y mysql-server
-
-    sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-    wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-    sudo apt-get update
-    sudo apt-get install -y postgresql
+    echo "starting up SQL dbs"
+    sudo services mysql start
+    sudo service postgresql start
   else
     echo  "SQL dbs not installed"
   fi
