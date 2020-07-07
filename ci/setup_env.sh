dc7d704274fe0f2e0f0778257cf7968a0ea28666
Fangchen Li <fangchen.li@outlook.com>
2020-07-06 20:27:01 -0500

    CI: fix sql script for Azure Pipeline

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -168,7 +168,7 @@ if [[ -n ${SQL:0} ]]; then
     sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
     wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
     sudo apt-get update
-    sudo apt-get install -y postgres
+    sudo apt-get install -y postgresql
   else
     echo  "SQL dbs not installed"
   fi
