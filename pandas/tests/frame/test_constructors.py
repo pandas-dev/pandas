6f3fb51ea1f809e1d9ebc11de445003103aa8007
Justin Essert <justinkessert@gmail.com>
2020-07-07 16:15:07 -0400

    pylint fixes

--- pandas/tests/frame/test_constructors.py
+++ pandas/tests/frame/test_constructors.py
@@ -732,7 +732,7 @@ class TestDataFrameConstructors:
         assert df["a"].dtype == PeriodDtype("M")
         assert df["b"].dtype == PeriodDtype("M")
 
-        arr = pd.array([data] * 2, dtype=PeriodDtype("M")
+        arr = pd.array([data] * 2, dtype=PeriodDtype("M"))
         expected = DataFrame({"a": arr, "b": arr})
 
         tm.assert_frame_equal(df, expected)
