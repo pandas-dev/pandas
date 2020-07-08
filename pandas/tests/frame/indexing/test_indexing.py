83bc94c63bc78347cb69d629ca99995e8d195828
Suvayu Ali <suvayu@users.noreply.github.com>
2020-07-08 11:47:08 +0000

    BUG: incorrect type when indexing sparse dataframe with iterable (#34908)
    * TST: regression tests for indexing sparse dataframe with iterable
    
    closes #34526

--- pandas/tests/frame/indexing/test_indexing.py
+++ pandas/tests/frame/indexing/test_indexing.py
@@ -21,7 +21,6 @@ from pandas import (
     notna,
 )
 import pandas._testing as tm
-from pandas.arrays import SparseArray
 import pandas.core.common as com
 from pandas.core.indexing import IndexingError
 
@@ -1906,20 +1905,6 @@ class TestDataFrameIndexing:
 
         expect = df.iloc[[1, -1], 0]
         tm.assert_series_equal(df.loc[0.2, "a"], expect)
-
-    def test_getitem_sparse_column(self):
-        # https://github.com/pandas-dev/pandas/issues/23559
-        data = SparseArray([0, 1])
-        df = pd.DataFrame({"A": data})
-        expected = pd.Series(data, name="A")
-        result = df["A"]
-        tm.assert_series_equal(result, expected)
-
-        result = df.iloc[:, 0]
-        tm.assert_series_equal(result, expected)
-
-        result = df.loc[:, "A"]
-        tm.assert_series_equal(result, expected)
 
     def test_setitem_with_unaligned_tz_aware_datetime_column(self):
         # GH 12981
