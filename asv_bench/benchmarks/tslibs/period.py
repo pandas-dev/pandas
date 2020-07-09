84079ffad0e1188c087398053010d67b15561187
jbrockmendel <jbrockmendel@gmail.com>
2020-07-09 15:08:16 -0700

    PERF: periodarr_to_dt64arr (#35171)

--- asv_bench/benchmarks/tslibs/period.py
+++ asv_bench/benchmarks/tslibs/period.py
@@ -2,9 +2,14 @@
 Period benchmarks that rely only on tslibs.  See benchmarks.period for
 Period benchmarks that rely on other parts fo pandas.
 """
-from pandas import Period
+
+import numpy as np
+
+from pandas._libs.tslibs.period import Period, periodarr_to_dt64arr
 
 from pandas.tseries.frequencies import to_offset
+
+from .tslib import _sizes
 
 
 class PeriodProperties:
@@ -68,3 +73,34 @@ class PeriodConstructor:
 
     def time_period_constructor(self, freq, is_offset):
         Period("2012-06-01", freq=freq)
+
+
+class TimePeriodArrToDT64Arr:
+    params = [
+        _sizes,
+        [
+            1000,
+            1011,  # Annual - November End
+            2000,
+            2011,  # Quarterly - November End
+            3000,
+            4000,
+            4006,  # Weekly - Saturday End
+            5000,
+            6000,
+            7000,
+            8000,
+            9000,
+            10000,
+            11000,
+            12000,
+        ],
+    ]
+    param_names = ["size", "freq"]
+
+    def setup(self, size, freq):
+        arr = np.arange(10, dtype="i8").repeat(size // 10)
+        self.i8values = arr
+
+    def time_periodarray_to_dt64arr(self, size, freq):
+        periodarr_to_dt64arr(self.i8values, freq)
