52fa1796a5aadd5ff14d698d70e6e17b9fe7caa4
Gim Seng Ng <nggimseng@gmail.com>
2020-07-10 14:41:22 +0100

    fix split string lint issue

--- pandas/tests/io/formats/test_format.py
+++ pandas/tests/io/formats/test_format.py
@@ -2916,7 +2916,7 @@ class TestFloatArrayFormatter:
         # Happens when display precision is set to zero
         with pd.option_context("display.precision", 0):
             s = pd.Series([840.0, 4200.0])
-            expected_output = "0     840\n" "1    4200\n" "dtype: float64"
+            expected_output = "0     840\n1    4200\ndtype: float64"
             assert str(s) == expected_output
 
     def test_output_significant_digits(self):
