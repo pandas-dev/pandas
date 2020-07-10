db7631bfef5553756e8acf78d4313bea161e9acd
Gim Seng <26968986+gimseng@users.noreply.github.com>
2020-07-10 20:33:13 +0100

    Fix strange behavior when precision display option is zero (#20359) (#35212)

--- pandas/tests/io/formats/test_format.py
+++ pandas/tests/io/formats/test_format.py
@@ -2910,6 +2910,15 @@ class TestFloatArrayFormatter:
         assert result[0] == " 12.0"
         assert result[1] == "  0.0"
 
+    def test_output_display_precision_trailing_zeroes(self):
+        # Issue #20359: trimming zeros while there is no decimal point
+
+        # Happens when display precision is set to zero
+        with pd.option_context("display.precision", 0):
+            s = pd.Series([840.0, 4200.0])
+            expected_output = "0     840\n1    4200\ndtype: float64"
+            assert str(s) == expected_output
+
     def test_output_significant_digits(self):
         # Issue #9764
 
