a061a968736122b81bf8af643116eaf3ea7dfeaa
Avinash Pancham <avinashpancham@outlook.com>
2020-07-12 13:17:06 +0200

    Remove try except and add 'always' testcase

--- pandas/tests/test_import.py
+++ pandas/tests/test_import.py
@@ -1,14 +1,10 @@
-def test_import_error():
+import pytest
+
+
+@pytest.mark.parametrize("action", ["error", "always"])
+def test_import_error(action):
     # GH#26367 Verify whether Pandas import succeeds when setting filterwarnings
     import warnings
 
-    warnings.filterwarnings("error")
-
-    try:
-        import pandas as pd  # noqa: F401
-    except ImportWarning:
-        assert False, (
-            "Pandas import breaks when settings filterwarnings, see GH#26367. "
-            "Please share your issue at "
-            "https://github.com/pandas-dev/pandas/issues/26367"
-        )
+    warnings.filterwarnings(action)
+    import pandas as pd  # noqa: F401
