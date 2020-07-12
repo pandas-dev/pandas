5305f1d764eac667a46962a40e9ae4e57ce3b9ec
Avinash Pancham <avinashpancham@outlook.com>
2020-07-12 13:02:15 +0200

    CLN: inline test data

--- pandas/tests/series/methods/test_equals.py
+++ pandas/tests/series/methods/test_equals.py
@@ -1,13 +1,10 @@
 import numpy as np
-import pytest
 
 from pandas import Series
 
 
-@pytest.mark.parametrize(
-    "input_data", [[np.array([1, 2]), np.array([1, 2])]],
-)
-def test_equals_list_array(input_data):
+def test_equals_list_array():
     # GH20676 Verify equals operator for list of Numpy arrays
-    ser = Series(input_data)
+    arr = np.array([1, 2])
+    ser = Series([arr, arr])
     assert ser.equals(ser)
