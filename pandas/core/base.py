39774245adb96bb91e087aaad2857437ac968798
Matthew Moye <mattemoye@gmail.com>
2020-07-14 00:35:16 -0400

    fixed doctests error in argmax/argmin

--- pandas/core/base.py
+++ pandas/core/base.py
@@ -964,9 +964,9 @@ class IndexOpsMixin:
         ...                'E': 120.0, 'F': 100.0}})
 
         >>> s2.argmax(keep='all')
-        array([2, 4])
+        [2, 4]
         >>> s2.argmin(keep='all')
-        array([0, 5])
+        [0, 5]
 
         >>> s2.argmax(keep='last')
         4
