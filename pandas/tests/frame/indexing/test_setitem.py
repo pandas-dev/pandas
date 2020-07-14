957313d4bec08b81ac168229aa01ed053f0295e4
Lewis Cowles <lewiscowles@me.com>
2020-07-14 18:05:58 +0100

    Improved error message for invalid construction (#35190)

--- pandas/tests/frame/indexing/test_setitem.py
+++ pandas/tests/frame/indexing/test_setitem.py
@@ -117,7 +117,10 @@ class TestDataFrameSetItem:
         cat = Categorical.from_codes([0, 1, 1, 0, 1, 2], ["a", "b", "c"])
         df = DataFrame(range(10), columns=["bar"])
 
-        msg = "Length of values does not match length of index"
+        msg = (
+            rf"Length of values \({len(cat)}\) "
+            rf"does not match length of index \({len(df)}\)"
+        )
         with pytest.raises(ValueError, match=msg):
             df["foo"] = cat
 
