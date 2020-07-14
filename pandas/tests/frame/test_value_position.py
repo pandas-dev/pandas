9672b6627ece574bbcf9d1bdb5db657ee4660215
Matthew Moye <mattemoye@gmail.com>
2020-07-14 01:09:35 -0400

    add test case for idxmin

--- pandas/tests/frame/test_value_position.py
+++ pandas/tests/frame/test_value_position.py
@@ -10,7 +10,7 @@ def test_idxmax():
             "b": [9, 4, 3, 4, 2, 2, 4, 9],
             "c": [1, 8, 2, 4, 1, 5, 8, 3],
         },
-        index=["A", "B", "C", "D", "E", "F", "G", "H"],
+        index=["A", "B", "C", "D", "E", "F", "G", "H"]
     )
     assert all(
         df.idxmax(keep="last") == pd.Series(["F", "H", "G"], index=["a", "b", "c"])
@@ -21,4 +21,25 @@ def test_idxmax():
     )
     assert all(
         df.idxmax(keep="first") == pd.Series(["B", "A", "B"], index=["a", "b", "c"])
+    )
+
+def test_idxmin():
+    # GH#35257
+    df = pd.DataFrame(
+        {
+            "a": [1, 5, 2, 4, 3, 5, 4, 2],
+            "b": [9, 4, 3, 4, 2, 2, 4, 9],
+            "c": [1, 8, 2, 4, 1, 5, 8, 3],
+        },
+        index=["A", "B", "C", "D", "E", "F", "G", "H"]
+    )
+    assert all(
+        df.idxmin(keep="last") == pd.Series(["A", "F", "E"], index=["a", "b", "c"])
+    )
+    tm.assert_series_equal(
+        df.idxmin(keep="all"),
+        pd.Series([["A"], ["E", "F"], ["A", "E"]], index=["a", "b", "c"]),
+    )
+    assert all(
+        df.idxmin(keep="first") == pd.Series(["A", "E", "A"], index=["a", "b", "c"])
     )
