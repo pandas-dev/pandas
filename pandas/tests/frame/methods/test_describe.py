cbe7f5b31e282f1c5c5fd3417475e3169f08497c
lvs1 <lvs1@sfu.ca>
2020-07-14 01:24:49 -0700

    TST: Update linting with black pandas formatting

--- pandas/tests/frame/methods/test_describe.py
+++ pandas/tests/frame/methods/test_describe.py
@@ -364,12 +364,9 @@ class TestDataFrameDescribe:
 
     def test_describe_does_not_raise_error(self):
         # GH#32409
-        df = pd.DataFrame(
-            [{"test": {"a": "1"}}, {"test": {"a": "2"}}]
-        )
+        df = pd.DataFrame([{"test": {"a": "1"}}, {"test": {"a": "2"}}])
         expected = DataFrame(
-            {"test": [2, 2, {'a': '1'}, 1]},
-            index=["count", "unique", "top", "freq"]
+            {"test": [2, 2, {"a": "1"}, 1]}, index=["count", "unique", "top", "freq"]
         )
         try:
             result = df.describe()
@@ -383,4 +380,4 @@ class TestDataFrameDescribe:
             )
             assert repr(result) == exp_repr
         except TypeError as e:
-            pytest.fail(f'TypeError was raised {e}')
+            pytest.fail(f"TypeError was raised {e}")
