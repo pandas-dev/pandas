e551fea60a8c3b96365f0afae0842973b158ee9f
Tom Augspurger <TomAugspurger@users.noreply.github.com>
2020-07-15 07:24:39 -0500

    Revert "BUG: fix union_indexes not supporting sort=False for Index subclasses (#35098)" (#35277)
    This reverts commit c21be0562a33d149b62735fc82aff80e4d5942f5.

--- pandas/tests/frame/test_constructors.py
+++ pandas/tests/frame/test_constructors.py
@@ -2578,13 +2578,11 @@ class TestDataFrameConstructors:
             index=pd.CategoricalIndex(["f", "female", "m", "male", "unknown"]),
         )
         result = DataFrame([s1, s2])
-        # GH 35092. Extra s2 columns are now appended to s1 columns
-        # in original order
         expected = DataFrame(
             np.array(
-                [[39.0, 6.0, 4.0, np.nan, np.nan], [152.0, 242.0, 150.0, 2.0, 2.0]]
+                [[np.nan, 39.0, np.nan, 6.0, 4.0], [2.0, 152.0, 2.0, 242.0, 150.0]]
             ),
-            columns=["female", "male", "unknown", "f", "m"],
+            columns=["f", "female", "m", "male", "unknown"],
         )
         tm.assert_frame_equal(result, expected)
 
