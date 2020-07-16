4170ca6969fa9d766597495cb6de279e7c26ec76
smithto1 <thomassmith0304@gmail.com>
2020-07-17 00:11:08 +0100

    restored test

--- pandas/tests/groupby/test_categorical.py
+++ pandas/tests/groupby/test_categorical.py
@@ -1287,7 +1287,7 @@ def test_get_nonexistent_category():
     # Accessing a Category that is not in the dataframe
     df = pd.DataFrame({"var": ["a", "a", "b", "b"], "val": range(4)})
     with pytest.raises(KeyError, match="'vau'"):
-        df.groupby("var", as_index=False).apply(
+        df.groupby("var").apply(
             lambda rows: pd.DataFrame(
                 {"var": [rows.iloc[-1]["var"]], "val": [rows.iloc[-1]["vau"]]}
             )
