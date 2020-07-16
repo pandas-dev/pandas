6a65e2f4eb64e9f2811d4d2742f0724596c7b658
smithto1 <thomassmith0304@gmail.com>
2020-07-17 00:03:29 +0100

    restore if-stat in test_transform

--- pandas/tests/groupby/transform/test_transform.py
+++ pandas/tests/groupby/transform/test_transform.py
@@ -728,6 +728,11 @@ def test_cython_transform_frame(op, args, targop):
             # dict(by=['int','string'])]:
 
             gb = df.groupby(**gb_target)
+            # allowlisted methods set the selection before applying
+            # bit a of hack to make sure the cythonized shift
+            # is equivalent to pre 0.17.1 behavior
+            if op == "shift":
+                gb._set_group_selection()
 
             if op != "shift" and "int" not in gb_target:
                 # numeric apply fastpath promotes dtype so have
