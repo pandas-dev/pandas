0fd04dc33ad5a475d2195151ab84fdc65381be12
arw2019 <andrew.r.wieteska@gmail.com>
2020-07-16 16:34:32 +0000

    added a test for end

--- pandas/tests/indexes/datetimes/test_constructors.py
+++ pandas/tests/indexes/datetimes/test_constructors.py
@@ -790,18 +790,27 @@ class TestDatetimeIndex:
     def test_constructor_with_ambiguous_keyword_arg(self):
         # GH 35297
 
-        timezone = "America/New_York"
-        start = pd.Timestamp(year=2020, month=11, day=1, hour=1).tz_localize(
-            timezone, ambiguous=False
-        )
-        result = pd.date_range(start, periods=2, ambiguous=False)
-
         expected = DatetimeIndex(
             ["2020-11-01 01:00:00", "2020-11-02 01:00:00"],
             dtype="datetime64[ns, America/New_York]",
             freq="D",
             ambiguous=False,
         )
+
+        # ambiguous keyword in start
+        timezone = "America/New_York"
+        start = pd.Timestamp(year=2020, month=11, day=1, hour=1).tz_localize(
+            timezone, ambiguous=False
+        )
+        result = pd.date_range(start=start, periods=2, ambiguous=False)
+        tm.assert_index_equal(result, expected)
+
+        # ambiguous keyword in end
+        timezone = "America/New_York"
+        end = pd.Timestamp(year=2020, month=11, day=2, hour=1).tz_localize(
+            timezone, ambiguous=False
+        )
+        result = pd.date_range(end=end, periods=2, ambiguous=False)
         tm.assert_index_equal(result, expected)
 
     def test_constructor_no_precision_raises(self):
