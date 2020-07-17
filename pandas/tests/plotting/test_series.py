7355360a8f4696fae77734d0193ce25d6a9712f1
Steffen Schmitz <steffenschmitz@hotmail.de>
2020-07-17 18:26:46 +0200

    BUG: asymmetric error bars for series (GH9536) (#34514)

--- pandas/tests/plotting/test_series.py
+++ pandas/tests/plotting/test_series.py
@@ -729,6 +729,26 @@ class TestSeriesPlots(TestPlotBase):
         s = Series(values, index=index)
         _check_plot_works(s.plot)
 
+    def test_errorbar_asymmetrical(self):
+        # GH9536
+        s = Series(np.arange(10), name="x")
+        err = np.random.rand(2, 10)
+
+        ax = s.plot(yerr=err, xerr=err)
+
+        result = np.vstack([i.vertices[:, 1] for i in ax.collections[1].get_paths()])
+        expected = (err.T * np.array([-1, 1])) + s.to_numpy().reshape(-1, 1)
+        tm.assert_numpy_array_equal(result, expected)
+
+        msg = (
+            "Asymmetrical error bars should be provided "
+            f"with the shape \\(2, {len(s)}\\)"
+        )
+        with pytest.raises(ValueError, match=msg):
+            s.plot(yerr=np.random.rand(2, 11))
+
+        tm.close()
+
     @pytest.mark.slow
     def test_errorbar_plot(self):
 
