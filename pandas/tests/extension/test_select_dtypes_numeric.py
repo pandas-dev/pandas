39323916a15ef68aebfe33568151819707168496
andrewgsavage <andrewgsavage@gmail.com>
2020-07-19 12:14:49 +0100

    flake8

--- pandas/tests/extension/test_select_dtypes_numeric.py
+++ pandas/tests/extension/test_select_dtypes_numeric.py
@@ -5,9 +5,10 @@ import pytest
 import pandas as pd
 import numpy as np
 
+
 class DummyDtype(ExtensionDtype):
     type = int
-   
+
     @property
     def name(self):
         return 'Dummy'
@@ -17,9 +18,10 @@ class DummyDtype(ExtensionDtype):
         # type: () -> bool
         return self._numeric
 
+
 class DummyArray(ExtensionArray):
     _dtype = DummyDtype()
-    
+
     def __init__(self, data):
         self.data = data
 
@@ -29,18 +31,17 @@ class DummyArray(ExtensionArray):
     @property
     def dtype(self):
         return self._dtype
-    
+
     def __len__(self) -> int:
         return len(self.data)
-    
+
     def __getitem__(self, item):
         pass
 
+
 @pytest.mark.parametrize("numeric", [True, False])
 def test_select_dtypes_numeric(numeric):
-    da = DummyArray([1,2])
+    da = DummyArray([1, 2])
     da._dtype._numeric = numeric
     df = pd.DataFrame(da)
     assert df.select_dtypes(np.number).shape == df.shape
-    
-    
