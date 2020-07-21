88137808d26938c6df968951b6a51ae372ba4e9a
guru kiran <47276342+gurukiran07@users.noreply.github.com>
2020-07-21 17:00:09 +0530

    Trailing whitespace removed

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -5075,7 +5075,7 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
         Notes
         -----
         `agg` is an alias for `aggregate`. Use the alias.
-        
+
         Pandas operations generally exclude NaNs. For example, `agg(np.nanmedian)`,
         `agg(np.median)`, and `agg('median') will return the same result.
 
