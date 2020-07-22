0927b61baf0eefff9d123c4f7622af74b098d9be
guru kiran <47276342+gurukiran07@users.noreply.github.com>
2020-07-22 12:31:17 +0530

    Update generic.py

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -5076,8 +5076,8 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
         -----
         `agg` is an alias for `aggregate`. Use the alias.
 
-        Pandas operations generally exclude NaNs. For example, `agg(np.nanmedian)`,
-        `agg(np.median)`, and `agg('median')` will return the same result.
+        Pandas operations generally exclude NaNs. For example, ``agg(np.nanmedian)``,
+        ``agg(np.median)``, and ``agg('median')`` will return the same result.
 
         A passed user-defined-function will be passed a Series for evaluation.
         {examples}"""
