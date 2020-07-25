cea28d85b7043374ac0f4d0507cb4aa268d2969a
arw2019 <andrew.r.wieteska@gmail.com>
2020-07-25 04:25:59 +0000

    TST: remove failing test - read_csv takes kwargs now

--- pandas/tests/io/parser/test_common.py
+++ pandas/tests/io/parser/test_common.py
@@ -2077,8 +2077,6 @@ def test_unexpected_keyword_parameter_exception(all_parsers):
     parser = all_parsers
 
     msg = "{}\\(\\) got an unexpected keyword argument 'foo'"
-    with pytest.raises(TypeError, match=msg.format("read_csv")):
-        parser.read_csv("foo.csv", foo=1)
     with pytest.raises(TypeError, match=msg.format("read_table")):
         parser.read_table("foo.tsv", foo=1)
 
