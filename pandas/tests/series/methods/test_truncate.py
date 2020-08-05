2e85aef7caa2db9647109b9e7c1744fda3ad5232
gabicca <hodosan.gabriella@gmail.com>
2020-08-05 08:16:12 +0100

    BUG: series.truncate fix for one-element series (#35544) - PR review updates

--- pandas/tests/series/methods/test_truncate.py
+++ pandas/tests/series/methods/test_truncate.py
@@ -148,4 +148,7 @@ class TestTruncate:
         before = pd.Timestamp("2020-08-02")
         after = pd.Timestamp("2020-08-04")
 
-        assert series.truncate(before=before, after=after).equals(series)
+        result = series.truncate(before=before, after=after)
+
+        # the input Series and the expected Series are the same
+        tm.assert_series_equal(result, series)
