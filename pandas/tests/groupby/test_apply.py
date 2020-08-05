5122d1502c4e108c93058715bfdeef197a9f5064
smithto1 <thomassmith0304@gmail.com>
2020-08-05 18:45:42 +0100

    restart tests

--- pandas/tests/groupby/test_apply.py
+++ pandas/tests/groupby/test_apply.py
@@ -1040,7 +1040,7 @@ def test_apply_with_date_in_multiindex_does_not_convert_to_timestamp():
 
 
 def test_apply_by_cols_equals_apply_by_rows_transposed():
-    # GH #16646
+    # GH 16646
     # Operating on the columns, or transposing and operating on the rows
     # should give the same result. There was previously a bug where the
     # by_rows operation would work fine, but by_cols would throw a ValueError
