47b24a3cfab9a7e4dd5adcb53d0ae328d6713f7b
Matthew Roeschke <emailformattr@gmail.com>
2020-08-06 14:51:05 -0700

    CLN: get_flattened_iterator (#35515)

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -50,7 +50,7 @@ from pandas.core.series import Series
 from pandas.core.sorting import (
     compress_group_index,
     decons_obs_group_ids,
-    get_flattened_iterator,
+    get_flattened_list,
     get_group_index,
     get_group_index_sorter,
     get_indexer_dict,
@@ -153,7 +153,7 @@ class BaseGrouper:
             comp_ids, _, ngroups = self.group_info
 
             # provide "flattened" iterator for multi-group setting
-            return get_flattened_iterator(comp_ids, ngroups, self.levels, self.codes)
+            return get_flattened_list(comp_ids, ngroups, self.levels, self.codes)
 
     def apply(self, f: F, data: FrameOrSeries, axis: int = 0):
         mutated = self.mutated
