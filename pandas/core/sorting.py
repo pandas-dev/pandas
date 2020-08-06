7793b9d9fb069ae1277043643cc46bebc5a5431f
Matt Roeschke <mroeschke@housecanary.com>
2020-08-06 09:21:49 -0700

    use iterable instead of list

--- pandas/core/sorting.py
+++ pandas/core/sorting.py
@@ -1,6 +1,6 @@
 """ miscellaneous sorting / groupby utilities """
 from collections import defaultdict
-from typing import TYPE_CHECKING, Callable, DefaultDict, List, Optional, Tuple
+from typing import TYPE_CHECKING, Callable, DefaultDict, Iterable, List, Optional, Tuple
 
 import numpy as np
 
@@ -445,7 +445,10 @@ def ensure_key_mapped(values, key: Optional[Callable], levels=None):
 
 
 def get_flattened_list(
-    comp_ids: np.ndarray, ngroups: int, levels: List["Index"], labels: List[np.ndarray]
+    comp_ids: np.ndarray,
+    ngroups: int,
+    levels: Iterable["Index"],
+    labels: Iterable[np.ndarray],
 ) -> List[Tuple]:
     """Map compressed group id -> key tuple."""
     comp_ids = comp_ids.astype(np.int64, copy=False)
