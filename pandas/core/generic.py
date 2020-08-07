9843926e39dee88f689afc51cbf0f2fdc232dcd3
Andrew Wieteska <48889395+arw2019@users.noreply.github.com>
2020-08-07 12:58:23 -0400

    CLN: clarify TypeError for IndexSlice argument to pd.xs (#35411)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -3492,7 +3492,10 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
 
         index = self.index
         if isinstance(index, MultiIndex):
-            loc, new_index = self.index.get_loc_level(key, drop_level=drop_level)
+            try:
+                loc, new_index = self.index.get_loc_level(key, drop_level=drop_level)
+            except TypeError as e:
+                raise TypeError(f"Expected label or tuple of labels, got {key}") from e
         else:
             loc = self.index.get_loc(key)
 
