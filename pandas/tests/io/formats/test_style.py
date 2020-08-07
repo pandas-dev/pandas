b0cab2de419d0f364533e8cf61444f7b59be4897
attack68 <24256554+attack68@users.noreply.github.com>
2020-08-07 23:35:21 +0200

    BUG: fix styler cell_ids arg so that blank style is ignored on False (#35588)

--- pandas/tests/io/formats/test_style.py
+++ pandas/tests/io/formats/test_style.py
@@ -1682,6 +1682,12 @@ class TestStyler:
         result = styler.pipe((f, "styler"), a=1, b=2)
         assert result == (1, 2, styler)
 
+    def test_no_cell_ids(self):
+        # GH 35588
+        df = pd.DataFrame(data=[[0]])
+        s = Styler(df, uuid="_", cell_ids=False).render()
+        assert s.find('<td  class="data row0 col0" >') != -1
+
 
 @td.skip_if_no_mpl
 class TestStylerMatplotlibDep:
