e646c1654d1ec070aae9dcf3c4801b9b9bcde176
Martin Durant <martin.durant@utoronto.ca>
2020-08-10 12:52:30 -0400

    Add flask where there is moto
    Apparently moto in server mode needs flask
    https://github.com/spulec/moto/blob/master/setup.py#L88

--- pandas/tests/io/conftest.py
+++ pandas/tests/io/conftest.py
@@ -56,6 +56,7 @@ def s3_resource(tips_file, jsonl_file, feather_file):
         os.environ.setdefault("AWS_SECRET_ACCESS_KEY", "foobar_secret")
 
         pytest.importorskip("moto")
+        pytest.importorskip("flask")  # server mode needs flask too
 
         test_s3_files = [
             ("tips#1.csv", tips_file),
