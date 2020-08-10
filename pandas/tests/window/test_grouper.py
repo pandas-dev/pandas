18627e1c95771b0d7d87ac699b3c64ec9bf3c62a
Matthew Roeschke <emailformattr@gmail.com>
2020-08-10 06:13:30 -0700

    BUG: RollingGroupby with closed and column selection no longer raises ValueError (#35639)

--- pandas/tests/window/test_grouper.py
+++ pandas/tests/window/test_grouper.py
@@ -304,3 +304,54 @@ class TestGrouperGrouping:
             name="b",
         )
         tm.assert_series_equal(result, expected)
+
+    def test_groupby_rolling_subset_with_closed(self):
+        # GH 35549
+        df = pd.DataFrame(
+            {
+                "column1": range(6),
+                "column2": range(6),
+                "group": 3 * ["A", "B"],
+                "date": [pd.Timestamp("2019-01-01")] * 6,
+            }
+        )
+        result = (
+            df.groupby("group").rolling("1D", on="date", closed="left")["column1"].sum()
+        )
+        expected = Series(
+            [np.nan, 0.0, 2.0, np.nan, 1.0, 4.0],
+            index=pd.MultiIndex.from_tuples(
+                [("A", pd.Timestamp("2019-01-01"))] * 3
+                + [("B", pd.Timestamp("2019-01-01"))] * 3,
+                names=["group", "date"],
+            ),
+            name="column1",
+        )
+        tm.assert_series_equal(result, expected)
+
+    def test_groupby_subset_rolling_subset_with_closed(self):
+        # GH 35549
+        df = pd.DataFrame(
+            {
+                "column1": range(6),
+                "column2": range(6),
+                "group": 3 * ["A", "B"],
+                "date": [pd.Timestamp("2019-01-01")] * 6,
+            }
+        )
+
+        result = (
+            df.groupby("group")[["column1", "date"]]
+            .rolling("1D", on="date", closed="left")["column1"]
+            .sum()
+        )
+        expected = Series(
+            [np.nan, 0.0, 2.0, np.nan, 1.0, 4.0],
+            index=pd.MultiIndex.from_tuples(
+                [("A", pd.Timestamp("2019-01-01"))] * 3
+                + [("B", pd.Timestamp("2019-01-01"))] * 3,
+                names=["group", "date"],
+            ),
+            name="column1",
+        )
+        tm.assert_series_equal(result, expected)
