0588ce450875d93b6247fa67ce96c7028a1cf929
jbrockmendel <jbrockmendel@gmail.com>
2020-08-10 17:01:57 -0700

    BUG: DataFrame.apply with func altering row in-place (#35633)

--- pandas/tests/frame/apply/test_frame_apply.py
+++ pandas/tests/frame/apply/test_frame_apply.py
@@ -1522,3 +1522,22 @@ class TestDataFrameAggregate:
         expected = df.dtypes
 
         tm.assert_series_equal(result, expected)
+
+
+def test_apply_mutating():
+    # GH#35462 case where applied func pins a new BlockManager to a row
+    df = pd.DataFrame({"a": range(100), "b": range(100, 200)})
+
+    def func(row):
+        mgr = row._mgr
+        row.loc["a"] += 1
+        assert row._mgr is not mgr
+        return row
+
+    expected = df.copy()
+    expected["a"] += 1
+
+    result = df.apply(func, axis=1)
+
+    tm.assert_frame_equal(result, expected)
+    tm.assert_frame_equal(df, result)
