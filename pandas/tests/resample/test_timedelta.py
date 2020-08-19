d7a392d8c9cb01af02bcf9502b6b32a7e585aa74
tkmz-n <60312218+tkmz-n@users.noreply.github.com>
2020-08-19 07:52:29 +0000

    TST: resample does not yield empty groups (#10603)

--- pandas/tests/resample/test_timedelta.py
+++ pandas/tests/resample/test_timedelta.py
@@ -150,3 +150,15 @@ def test_resample_timedelta_edge_case(start, end, freq, resample_freq):
     tm.assert_index_equal(result.index, expected_index)
     assert result.index.freq == expected_index.freq
     assert not np.isnan(result[-1])
+
+
+def test_resample_timedelta_no_empty_groups():
+    # GH 10603
+    # check that resample does not yield empty groups
+    df = pd.DataFrame(np.random.normal(size=(10000, 4)))
+    df.index = pd.timedelta_range(start="0s", periods=10000, freq="3906250n")
+    result = df.loc["1s":, :].resample("3s").apply(lambda x: len(x))
+
+    expected = pd.DataFrame([[768.0] * 4] * 12 + [[528.0] * 4])
+    expected.index = pd.timedelta_range(start="1s", periods=13, freq="3s")
+    tm.assert_frame_equal(result, expected)
