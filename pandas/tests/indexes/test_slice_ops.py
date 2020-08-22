460284d0aa1c4a93113d05dda3db366b121c1588
Matt Roeschke <mroeschke@housecanary.com>
2020-08-21 23:41:21 -0700

    Move file and add tests for numeric and object types

--- pandas/tests/indexes/test_slice_ops.py
+++ pandas/tests/indexes/test_slice_ops.py
@@ -2,7 +2,7 @@ from datetime import date, datetime
 
 import pytest
 
-from pandas import DatetimeIndex, Timestamp, bdate_range
+from pandas import DatetimeIndex, Index, Timestamp, bdate_range
 
 
 def test_get_slice_bounds_invalid_kind():
@@ -17,10 +17,36 @@ def test_get_slice_bounds_invalid_side():
         )
 
 
+@pytest.mark.parametrize("kind", ["getitem", "loc", None])
+@pytest.mark.parametrize("side, expected", [("left", 4), ("right", 5)])
+@pytest.mark.parametrize("data, bound", [(range(6), 4), (list("abcdef"), "e")])
+def test_get_slice_bounds_within(kind, side, expected, data, bound):
+    index = Index(data)
+    result = index.get_slice_bound(bound, kind=kind, side=side)
+    assert result == expected
+
+
+@pytest.mark.parametrize("kind", ["getitem", "loc", None])
+@pytest.mark.parametrize("side", ["left", "right"])
+@pytest.mark.parametrize(
+    "data, bound, expected",
+    [
+        (range(6), -1, 0),
+        (range(6), 10, 6),
+        (list("abcdef"), "x", 6),
+        (list("bcdefg"), "a", 0),
+    ],
+)
+def test_get_slice_bounds_outside(kind, side, expected, data, bound):
+    index = Index(data)
+    result = index.get_slice_bound(bound, kind=kind, side=side)
+    assert result == expected
+
+
 @pytest.mark.parametrize("box", [date, datetime, Timestamp])
 @pytest.mark.parametrize("kind", ["getitem", "loc", None])
 @pytest.mark.parametrize("side, expected", [("left", 4), ("right", 5)])
-def test_get_slice_bounds_within(box, kind, side, expected, tz_aware_fixture):
+def test_get_slice_bounds_datetime_within(box, kind, side, expected, tz_aware_fixture):
     # GH 35690
     index = bdate_range("2000-01-03", "2000-02-11").tz_localize(tz_aware_fixture)
     result = index.get_slice_bound(box(year=2000, month=1, day=7), kind=kind, side=side)
@@ -31,7 +57,9 @@ def test_get_slice_bounds_within(box, kind, side, expected, tz_aware_fixture):
 @pytest.mark.parametrize("kind", ["getitem", "loc", None])
 @pytest.mark.parametrize("side", ["left", "right"])
 @pytest.mark.parametrize("year, expected", [(1999, 0), (2020, 30)])
-def test_get_slice_bounds_outside(box, kind, side, year, expected, tz_aware_fixture):
+def test_get_slice_bounds_datetime_outside(
+    box, kind, side, year, expected, tz_aware_fixture
+):
     # GH 35690
     index = bdate_range("2000-01-03", "2000-02-11").tz_localize(tz_aware_fixture)
     result = index.get_slice_bound(box(year=year, month=1, day=7), kind=kind, side=side)
@@ -40,7 +68,7 @@ def test_get_slice_bounds_outside(box, kind, side, year, expected, tz_aware_fixt
 
 @pytest.mark.parametrize("box", [date, datetime, Timestamp])
 @pytest.mark.parametrize("kind", ["getitem", "loc", None])
-def test_slice_locs(box, kind, tz_aware_fixture):
+def test_slice_datetime_locs(box, kind, tz_aware_fixture):
     # GH 34077
     index = DatetimeIndex(["2010-01-01", "2010-01-03"]).tz_localize(tz_aware_fixture)
     result = index.slice_locs(box(2010, 1, 1), box(2010, 1, 2))
