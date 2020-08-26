f86b129c81a1172f645fcecbadfd9750561ea5d6
Simon Hawkins <simonjayhawkins@gmail.com>
2020-08-26 15:40:21 +0100

    Backport PR #35712: PERF: RangeIndex.format performance (#35904)
    Co-authored-by: Terji Petersen <contribute@tensortable.com>

--- pandas/core/indexes/range.py
+++ pandas/core/indexes/range.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 import operator
 from sys import getsizeof
-from typing import Any, Optional
+from typing import Any, List, Optional
 import warnings
 
 import numpy as np
@@ -194,6 +194,15 @@ class RangeIndex(Int64Index):
     def _format_data(self, name=None):
         # we are formatting thru the attributes
         return None
+
+    def _format_with_header(self, header: List[str], na_rep: str = "NaN") -> List[str]:
+        if not len(self._range):
+            return header
+        first_val_str = str(self._range[0])
+        last_val_str = str(self._range[-1])
+        max_length = max(len(first_val_str), len(last_val_str))
+
+        return header + [f"{x:<{max_length}}" for x in self._range]
 
     # --------------------------------------------------------------------
     _deprecation_message = (
