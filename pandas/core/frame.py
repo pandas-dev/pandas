5a9ee74d4ec68acfd4cb8403fe22e285b7dd694f
Simon Hawkins <simonjayhawkins@gmail.com>
2020-08-30 18:50:37 +0100

    TYP: misc typing cleanups for #29116 (#35953)

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -7415,6 +7415,12 @@ NaN 12.3   33.0
         if relabeling:
             # This is to keep the order to columns occurrence unchanged, and also
             # keep the order of new columns occurrence unchanged
+
+            # For the return values of reconstruct_func, if relabeling is
+            # False, columns and order will be None.
+            assert columns is not None
+            assert order is not None
+
             result_in_dict = relabel_result(result, func, columns, order)
             result = DataFrame(result_in_dict, index=columns)
 
