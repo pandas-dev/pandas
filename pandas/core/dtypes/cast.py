73d0ee051d08d4f9ef0cc94cadf32ddff6119c40
Daniel Saxton <dsaxton@pm.me>
2020-08-29 21:06:51 -0500

    Nit

--- pandas/core/dtypes/cast.py
+++ pandas/core/dtypes/cast.py
@@ -595,7 +595,7 @@ def maybe_promote(dtype, fill_value=np.nan):
     return dtype, fill_value
 
 
-def maybe_astype(values, dtype, copy, errors="raise"):
+def maybe_astype(values, dtype, copy, errors):
     try:
         result = np.array(values, dtype=dtype, copy=copy)
     except (ValueError, TypeError):
