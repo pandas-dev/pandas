ad29fff2997969370962f12b90a3ba5f80e2d3d3
Alexander Kirko <alexander.kirko@gmail.com>
2020-08-31 13:57:26 +0300

    Revert "TST: add prints to debug numpy dev test fails"
    This reverts commit c7e824049d82df9370126f3c6bf5737f3c8c194a.

--- pandas/tests/indexes/test_setops.py
+++ pandas/tests/indexes/test_setops.py
@@ -48,12 +48,8 @@ def test_union_different_types(index, index_fixture2):
 
     # Union with a non-unique, non-monotonic index raises error
     # This applies to the boolean index
-    print(idx1)
-    print(idx2)
     idx1 = idx1.sort_values()
     idx2 = idx2.sort_values()
-    print(idx1)
-    print(idx2)
 
     assert idx1.union(idx2).dtype == np.dtype("O")
     assert idx2.union(idx1).dtype == np.dtype("O")
