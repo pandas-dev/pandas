f9f413f117cf5d89231eb35d3719d6bc09fa2b38
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2020-09-01 00:16:53 +0200

    black fix

--- pandas/core/arrays/datetimes.py
+++ pandas/core/arrays/datetimes.py
@@ -1954,9 +1954,7 @@ def sequence_to_dt64ns(
             except TypeError as e:
                 #  TODO: make generic conversion instead of only from UTC
                 if inferred_tz.zone == "UTC":
-                    data = tzconversion.tz_convert_from_utc(
-                        data.view("i8"), tz
-                    )
+                    data = tzconversion.tz_convert_from_utc(data.view("i8"), tz)
                     data = data.view(DT64NS_DTYPE)
                 else:
                     raise e
