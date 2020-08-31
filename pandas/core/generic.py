ba5b55a2f155b713142ec746fe515ab1f0aec8df
tim <43515959+timhunderwood@users.noreply.github.com>
2020-08-31 11:19:29 +0100

    Make wording simpler and remove reference to normalization.

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -10863,9 +10863,8 @@ Returns
 
 Notes
 -----
-Note that the default divisor (normalization) is different to numpy,
-which by default normalizes by N (equivalent to ddof=0). To have the same
-behaviour as numpy, use ddof=0.\n"""
+To have the same behaviour as `numpy.std`, use `ddof=0` (instead of the
+default `ddof=1`)\n"""
 
 _bool_doc = """
 %(desc)s
