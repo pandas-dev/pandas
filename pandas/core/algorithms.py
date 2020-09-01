c9454579c61561c8f3c7aeb036b9e31b7ad2b2d9
Kaiqi <kaiqi@kth.se>
2020-09-01 21:16:03 +0200

    one more try

--- pandas/core/algorithms.py
+++ pandas/core/algorithms.py
@@ -631,7 +631,7 @@ def factorize(
     >>> codes
     array([ 0,  1,  0, -1])
     >>> uniques
-    array([ 1.,  2.])
+    array([1., 2.])
     >>> codes, uniques = pd.factorize(values, na_sentinel=None)
     >>> codes
     array([0, 1, 0, 2])
