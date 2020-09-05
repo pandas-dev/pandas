5d63c46d44ba2074289cff20c5524e6ed732ddab
Marco Gorelli <m.e.gorelli@gmail.com>
2020-09-05 11:36:53 +0100

    :art:

--- pandas/tests/plotting/test_frame.py
+++ pandas/tests/plotting/test_frame.py
@@ -216,11 +216,9 @@ class TestDataFramePlots(TestPlotBase):
         # GH 21003
         df = DataFrame(np.random.random((7, 4)))
         ax = df.plot(color=color, style="d--")
-
         # check colors
         result = [i.get_color() for i in ax.lines]
         assert result == expected
-
         # check markers and linestyles
         assert all(i.get_linestyle() == "--" for i in ax.lines)
         assert all(i.get_marker() == "d" for i in ax.lines)
