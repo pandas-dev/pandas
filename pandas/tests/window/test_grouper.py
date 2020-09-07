b9c3d7f131fc4edc4a764e03e839d5b0531c2814
patrick <61934744+phofl@users.noreply.github.com>
2020-09-07 23:06:29 +0200

    Fix compressed multiindex for output of groupby.rolling (#36152)

--- pandas/tests/window/test_grouper.py
+++ pandas/tests/window/test_grouper.py
@@ -372,3 +372,24 @@ class TestGrouperGrouping:
             name="column1",
         )
         tm.assert_series_equal(result, expected)
+
+    @pytest.mark.parametrize("func", ["max", "min"])
+    def test_groupby_rolling_index_changed(self, func):
+        # GH: #36018 nlevels of MultiIndex changed
+        ds = Series(
+            [1, 2, 2],
+            index=pd.MultiIndex.from_tuples(
+                [("a", "x"), ("a", "y"), ("c", "z")], names=["1", "2"]
+            ),
+            name="a",
+        )
+
+        result = getattr(ds.groupby(ds).rolling(2), func)()
+        expected = Series(
+            [np.nan, np.nan, 2.0],
+            index=pd.MultiIndex.from_tuples(
+                [(1, "a", "x"), (2, "a", "y"), (2, "c", "z")], names=["a", "1", "2"]
+            ),
+            name="a",
+        )
+        tm.assert_series_equal(result, expected)
