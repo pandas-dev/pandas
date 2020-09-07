f35be342257af56a4745e0abbe63bdd715570e88
phofl <patrick_hoefler@gmx.net>
2020-09-07 21:47:31 +0200

    Add gh issue number

--- pandas/tests/frame/methods/test_replace.py
+++ pandas/tests/frame/methods/test_replace.py
@@ -1601,6 +1601,7 @@ class TestDataFrameReplace:
         tm.assert_frame_equal(result, expected)
 
     def test_replace_unicode(self):
+        # GH: 16784
         columns_values_map = {"positive": {"正面": 1, "中立": 1, "负面": 0}}
         df1 = pd.DataFrame({"positive": np.ones(3)})
         result = df1.replace(columns_values_map)
