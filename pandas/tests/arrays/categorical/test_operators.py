b20f3f0f65424cc3ab798dfc7518aaf3404d9f9a
jbrockmendel <jbrockmendel@gmail.com>
2020-09-08 15:53:03 -0700

    CLN: re-use invalid_comparison in Categorical comparisons (#36229)

--- pandas/tests/arrays/categorical/test_operators.py
+++ pandas/tests/arrays/categorical/test_operators.py
@@ -171,17 +171,14 @@ class TestCategoricalOps:
         # for unequal comps, but not for equal/not equal
         cat = Categorical([1, 2, 3], ordered=True)
 
-        msg = (
-            "Cannot compare a Categorical for op __{}__ with a scalar, "
-            "which is not a category"
-        )
-        with pytest.raises(TypeError, match=msg.format("lt")):
+        msg = "Invalid comparison between dtype=category and int"
+        with pytest.raises(TypeError, match=msg):
             cat < 4
-        with pytest.raises(TypeError, match=msg.format("gt")):
+        with pytest.raises(TypeError, match=msg):
             cat > 4
-        with pytest.raises(TypeError, match=msg.format("gt")):
+        with pytest.raises(TypeError, match=msg):
             4 < cat
-        with pytest.raises(TypeError, match=msg.format("lt")):
+        with pytest.raises(TypeError, match=msg):
             4 > cat
 
         tm.assert_numpy_array_equal(cat == 4, np.array([False, False, False]))
