d5095d4be1a8b7a124bb8b595716de1c442d47cb
Tomasz Sakrejda <tomasz@atsu.io>
2020-09-08 16:09:50 -0700

    Fix style in `sparse/array.py` to match other changes

--- pandas/core/arrays/sparse/array.py
+++ pandas/core/arrays/sparse/array.py
@@ -1063,10 +1063,11 @@ class SparseArray(PandasObject, ExtensionArray, ExtensionOpsMixin):
         IntIndex
         Indices: array([2, 3], dtype=int32)
         """
-        if dtype == self.dtype and copy:
-            return self.copy()
-        elif dtype == self.dtype and not copy:
-            return self
+        if dtype == self._dtype:
+            if not copy:
+                return self
+            elif copy:
+                return self.copy()
         dtype = self.dtype.update_dtype(dtype)
         subtype = dtype._subtype_with_str
         # TODO copy=False is broken for astype_nansafe with int -> float, so cannot
