29b8cda2dbd22b8c23a2211b0964b0f2959ca639
Asish Mahapatra <asishkm@gmail.com>
2020-09-08 19:23:31 -0400

    revert monkeypatch and change impl

--- pandas/io/excel/_odfreader.py
+++ pandas/io/excel/_odfreader.py
@@ -28,27 +28,7 @@ class _ODFReader(BaseExcelReader):
         storage_options: StorageOptions = None,
     ):
         import_optional_dependency("odf")
-        self._monkeypatch_odf_element_str()
         super().__init__(filepath_or_buffer, storage_options=storage_options)
-
-    def _monkeypatch_odf_element_str(self):
-        from odf.element import Element
-        from odf.namespaces import TEXTNS
-        from odf.text import S
-
-        text_s = S().qname
-
-        def element_str(self):
-            value = []
-            for c in self.childNodes:
-                if isinstance(c, Element) and c.qname == text_s:
-                    spaces = int(c.attributes.get((TEXTNS, "c"), 1))
-                    value.append(" " * spaces)
-                else:
-                    value.append(str(c))
-            return "".join(value)
-
-        Element.__str__ = element_str
 
     @property
     def _workbook_class(self):
@@ -198,7 +178,7 @@ class _ODFReader(BaseExcelReader):
             cell_value = cell.attributes.get((OFFICENS, "value"))
             return float(cell_value)
         elif cell_type == "string":
-            return str(cell)
+            return self._get_cell_string_value(cell)
         elif cell_type == "currency":
             cell_value = cell.attributes.get((OFFICENS, "value"))
             return float(cell_value)
@@ -211,3 +191,27 @@ class _ODFReader(BaseExcelReader):
             return result.time()
         else:
             raise ValueError(f"Unrecognized type {cell_type}")
+
+    def _get_cell_string_value(self, cell) -> str:
+        """
+        Find and decode OpenDocument text:s tags that represent
+        a run length encoded sequence of space characters.
+        """
+        from odf.element import Element
+        from odf.namespaces import TEXTNS
+        from odf.text import S
+
+        text_s = S().qname
+
+        value = []
+
+        for fragment in cell.childNodes:
+            if isinstance(fragment, Element):
+                if fragment.qname == text_s:
+                    spaces = int(fragment.attributes.get((TEXTNS, "c"), 1))
+                    value.append(" " * spaces)
+                else:
+                    value.append(self._get_cell_string_value(fragment))
+            else:
+                value.append(str(fragment))
+        return "".join(value)
