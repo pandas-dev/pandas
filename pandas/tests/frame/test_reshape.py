047ae40f50499d1054ac0370210b01baf3115cf6
steveya <steveya@gmail.com>
2020-09-09 17:44:39 +0800

    add a comma to pass black lint

--- pandas/tests/frame/test_reshape.py
+++ pandas/tests/frame/test_reshape.py
@@ -1276,7 +1276,7 @@ def test_stack_timezone_aware_values():
 def test_stack_empty_frame():
     tm.assert_series_equal(
         DataFrame().stack(),
-        Series(index=MultiIndex([[], []], [[], []]), dtype=np.float64)
+        Series(index=MultiIndex([[], []], [[], []]), dtype=np.float64),
     )
     tm.assert_series_equal(
         DataFrame().stack(dropna=True),
