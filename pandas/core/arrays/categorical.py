cbee215fc5a40228eec9f5c9ce5f15832ba05143
Brock <jbrockmendel@gmail.com>
2020-09-08 18:43:54 -0700

    restore Categorical.argsort docstring

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -1418,6 +1418,59 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject):
                 "Categorical to an ordered one\n"
             )
 
+    def argsort(self, ascending=True, kind="quicksort", **kwargs):
+        """
+        Return the indices that would sort the Categorical.
+
+        .. versionchanged:: 0.25.0
+
+           Changed to sort missing values at the end.
+
+        Parameters
+        ----------
+        ascending : bool, default True
+            Whether the indices should result in an ascending
+            or descending sort.
+        kind : {'quicksort', 'mergesort', 'heapsort'}, optional
+            Sorting algorithm.
+        **kwargs:
+            passed through to :func:`numpy.argsort`.
+
+        Returns
+        -------
+        numpy.array
+
+        See Also
+        --------
+        numpy.ndarray.argsort
+
+        Notes
+        -----
+        While an ordering is applied to the category values, arg-sorting
+        in this context refers more to organizing and grouping together
+        based on matching category values. Thus, this function can be
+        called on an unordered Categorical instance unlike the functions
+        'Categorical.min' and 'Categorical.max'.
+
+        Examples
+        --------
+        >>> pd.Categorical(['b', 'b', 'a', 'c']).argsort()
+        array([2, 0, 1, 3])
+
+        >>> cat = pd.Categorical(['b', 'b', 'a', 'c'],
+        ...                      categories=['c', 'b', 'a'],
+        ...                      ordered=True)
+        >>> cat.argsort()
+        array([3, 0, 1, 2])
+
+        Missing values are placed at the end
+
+        >>> cat = pd.Categorical([2, None, 1])
+        >>> cat.argsort()
+        array([2, 0, 1])
+        """
+        return super().argsort(ascending=ascending, kind=kind, **kwargs)
+
     def sort_values(
         self, inplace: bool = False, ascending: bool = True, na_position: str = "last"
     ):
