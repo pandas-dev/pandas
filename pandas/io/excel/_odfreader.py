21cc458d21ab57c3afbee1b1a06dafa5b9db9357
Asish Mahapatra <asishkm@gmail.com>
2020-09-09 09:41:15 -0400

    add comment linking discussion + add whatsnew

--- pandas/io/excel/_odfreader.py
+++ pandas/io/excel/_odfreader.py
@@ -211,6 +211,9 @@ class _ODFReader(BaseExcelReader):
                     spaces = int(fragment.attributes.get((TEXTNS, "c"), 1))
                     value.append(" " * spaces)
                 else:
+                    # recursive impl needed in case of nested fragments
+                    # with multiple spaces
+                    # https://github.com/pandas-dev/pandas/pull/36175#discussion_r484639704
                     value.append(self._get_cell_string_value(fragment))
             else:
                 value.append(str(fragment))
