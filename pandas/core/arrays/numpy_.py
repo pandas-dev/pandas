7f4537011c7431a7e98f86463be8a817220230a2
Tomasz Sakrejda <tomasz@atsu.io>
2020-09-09 12:18:52 -0700

    Add 'if copy' check to base ExtensionArray astype class
    - Remove corresponding check from numpy_.py
    - Note that all tests now pass

--- pandas/core/arrays/numpy_.py
+++ pandas/core/arrays/numpy_.py
@@ -13,6 +13,7 @@ from pandas.util._validators import validate_fillna_kwargs
 from pandas.core.dtypes.dtypes import ExtensionDtype
 from pandas.core.dtypes.inference import is_array_like
 from pandas.core.dtypes.missing import isna
+from pandas.core.dtypes.common import is_dtype_equal
 
 from pandas import compat
 from pandas.core import nanops, ops
@@ -23,6 +24,7 @@ from pandas.core.arrays.base import ExtensionArray, ExtensionOpsMixin
 from pandas.core.construction import extract_array
 from pandas.core.indexers import check_array_indexer
 from pandas.core.missing import backfill_1d, pad_1d
+
 
 
 class PandasDtype(ExtensionDtype):
@@ -277,13 +279,7 @@ class PandasArray(
         self._ndarray[key] = value
 
     def astype(self, dtype, copy: bool = True) -> ArrayLike:
-        if dtype == self.dtype:
-            if not copy:
-                return self
-            elif copy:
-                return self.copy()
-        else:
-            return super().astype(dtype, copy)
+        return super().astype(dtype, copy)
 
     def isna(self) -> np.ndarray:
         return isna(self._ndarray)
