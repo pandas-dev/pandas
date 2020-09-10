c5030d02b344aaa196102943d450a0688ad1d224
Corey Levinson <thecoreylevinson@gmail.com>
2020-09-10 17:43:52 -0400

    added tests for peek

--- pandas/tests/generic/test_generic.py
+++ pandas/tests/generic/test_generic.py
@@ -251,7 +251,7 @@ class Generic:
             self.check_metadata(v1 & v2)
             self.check_metadata(v1 | v2)
 
-    def test_head_tail(self, index):
+    def test_head_tail_peek(self, index):
         # GH5370
 
         o = self._construct(shape=len(index))
@@ -263,18 +263,25 @@ class Generic:
 
         self._compare(o.head(), o.iloc[:5])
         self._compare(o.tail(), o.iloc[-5:])
+        if o.shape[0]>=10:
+            self._compare(o.peek(), pd.concat([o.iloc[:5], o.iloc[-5:]], axis=0))
+        else:
+            self._compare(o.peek(), o)
 
         # 0-len
         self._compare(o.head(0), o.iloc[0:0])
         self._compare(o.tail(0), o.iloc[0:0])
+        self._compare(o.peek(0), o.iloc[0:0])
 
         # bounded
         self._compare(o.head(len(o) + 1), o)
         self._compare(o.tail(len(o) + 1), o)
+        self._compare(o.peek(len(o)//2 + 1), o)
 
         # neg index
         self._compare(o.head(-3), o.head(len(index) - 3))
         self._compare(o.tail(-3), o.tail(len(index) - 3))
+        self._compare(o.peek(-3), o.iloc[3:-3])
 
     def test_sample(self):
         # Fixes issue: 2419
