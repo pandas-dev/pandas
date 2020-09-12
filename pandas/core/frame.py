9258622846fde91ad7a9f15b6b12589e435f9e7b
Sam Ezebunandu <sam.ezebunandu@gmail.com>
2020-09-12 15:31:58 -0600

    DOC: Fix DataFrame.query contradiction on use of Python keywords as identifiers (#36311)

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -3238,11 +3238,12 @@ class DataFrame(NDFrame):
             in the environment by prefixing them with an '@' character like
             ``@a + b``.
 
-            You can refer to column names that contain spaces or operators by
-            surrounding them in backticks. This way you can also escape
-            names that start with a digit, or those that  are a Python keyword.
-            Basically when it is not valid Python identifier. See notes down
-            for more details.
+            You can refer to column names that are not valid Python variable names
+            by surrounding them in backticks. Thus, column names containing spaces
+            or punctuations (besides underscores) or starting with digits must be
+            surrounded by backticks. (For example, a column named "Area (cm^2) would
+            be referenced as `Area (cm^2)`). Column names which are Python keywords
+            (like "list", "for", "import", etc) cannot be used.
 
             For example, if one of your columns is called ``a a`` and you want
             to sum it with ``b``, your query should be ```a a` + b``.
