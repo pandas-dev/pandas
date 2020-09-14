57fb70bd405520b70338c0e5b304120f0102c5dd
Kumar Shivam <1996.kumar.shivam@gmail.com>
2020-09-14 23:06:57 +0530

    Update parsers.py
    DOC: Added docstring for storage_options GH36361

--- pandas/io/parsers.py
+++ pandas/io/parsers.py
@@ -347,7 +347,9 @@ storage_options : dict, optional
     be parsed by ``fsspec``, e.g., starting "s3://", "gcs://". An error
     will be raised if providing this argument with a local path or
     a file-like buffer. See the fsspec and backend storage implementation
-    docs for the set of allowed keys and values. 
+    docs for the set of allowed keys and values.
+    
+    .. versionadded:: 1.2.0
 
 Returns
 -------
