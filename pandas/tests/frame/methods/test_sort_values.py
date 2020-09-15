d45ae2dcb20258164bfff17d3a17472bfc2992e2
Daniel Saxton <dsaxton@pm.me>
2020-09-15 11:08:51 -0500

    Edit test

--- pandas/tests/frame/methods/test_sort_values.py
+++ pandas/tests/frame/methods/test_sort_values.py
@@ -695,7 +695,7 @@ class TestDataFrameSortKey:  # test key sorting (issue 27237)
     def test_sort_values_key_casts_to_categorical(self):
         # https://github.com/pandas-dev/pandas/issues/36383
         categories = ["a", "b", "c"]
-        df = pd.DataFrame({"x": [3, 2, 1], "y": ["c", "b", "a"]})
+        df = pd.DataFrame({"x": [1, 1, 1], "y": ["c", "b", "a"]})
 
         def sorter(key):
             if key.name == "y":
@@ -706,7 +706,7 @@ class TestDataFrameSortKey:  # test key sorting (issue 27237)
 
         result = df.sort_values(by=["x", "y"], key=sorter)
         expected = pd.DataFrame(
-            {"x": [1, 2, 3], "y": ["a", "b", "c"]}, index=pd.Index([2, 1, 0])
+            {"x": [1, 1, 1], "y": ["a", "b", "c"]}, index=pd.Index([2, 1, 0])
         )
 
         tm.assert_frame_equal(result, expected)
