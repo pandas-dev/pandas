9bf1a55ccba1dd5f15fc4659e188b6b0d01cdf0e
Fangchen Li <fangchen.li@outlook.com>
2020-09-16 08:03:29 -0500

    BLD/CI bump cython version, add doc

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ def is_platform_mac():
 
 
 min_numpy_ver = "1.16.5"
-min_cython_ver = "0.29.16"  # note: sync with pyproject.toml
+min_cython_ver = "0.29.21"  # note: sync with pyproject.toml
 
 try:
     import Cython
