0cd4fe6d7bf37a7fa1ad4e6a839be9382c5a5428
jbrockmendel <jbrockmendel@gmail.com>
2020-09-17 09:38:34 -0700

    BUG: Categorical.sort_values inplace breaking views (#36404)

--- pandas/tests/arrays/categorical/test_sorting.py
+++ pandas/tests/arrays/categorical/test_sorting.py
@@ -66,7 +66,9 @@ class TestCategoricalSort:
 
         # sort (inplace order)
         cat1 = cat.copy()
+        orig_codes = cat1._codes
         cat1.sort_values(inplace=True)
+        assert cat1._codes is orig_codes
         exp = np.array(["a", "b", "c", "d"], dtype=object)
         tm.assert_numpy_array_equal(cat1.__array__(), exp)
         tm.assert_index_equal(res.categories, cat.categories)
