c331d2c6845418af401af06ce3371e2b2b23fa2e
Terji Petersen <contribute@tensortable.com>
2020-09-17 17:50:59 +0100

    PERF: StringArray construction (#36325)

--- pandas/core/arrays/string_.py
+++ pandas/core/arrays/string_.py
@@ -199,13 +199,18 @@ class StringArray(PandasArray):
             assert dtype == "string"
 
         result = np.asarray(scalars, dtype="object")
-
         # convert non-na-likes to str, and nan-likes to StringDtype.na_value
         result = lib.ensure_string_array(
             result, na_value=StringDtype.na_value, copy=copy
         )
 
-        return cls(result)
+        # Manually creating new array avoids the validation step in the __init__, so is
+        # faster. Refactor need for validation?
+        new_string_array = object.__new__(cls)
+        new_string_array._dtype = StringDtype()
+        new_string_array._ndarray = result
+
+        return new_string_array
 
     @classmethod
     def _from_sequence_of_strings(cls, strings, dtype=None, copy=False):
