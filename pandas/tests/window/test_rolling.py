58b9fed9d8986ab785f3e42fca6884b158085622
phofl <patrick_hoefler@gmx.net>
2020-09-18 22:16:25 +0200

    Run black pandas

--- pandas/tests/window/test_rolling.py
+++ pandas/tests/window/test_rolling.py
@@ -790,11 +790,19 @@ def test_rolling_mixed_dtypes_axis_1(func, value):
 
 def test_rolling_axis_one_with_nan():
     # GH: 35596
-    df = pd.DataFrame([[0, 1, 2, 4, np.nan, np.nan, np.nan],
-                       [0, 1, 2, np.nan, np.nan, np.nan, np.nan],
-                       [0, 2, 2, np.nan, 2, np.nan, 1]])
-    result = df.rolling(window=7, min_periods=1, axis='columns').sum()
-    expected = pd.DataFrame([[0.0, 1.0, 3.0, 7.0, 7.0, 7.0, 7.0],
-                             [0.0, 1.0, 3.0, 3.0, 3.0, 3.0, 3.0],
-                             [0.0, 2.0, 4.0, 4.0, 6.0, 6.0, 7.0]])
+    df = pd.DataFrame(
+        [
+            [0, 1, 2, 4, np.nan, np.nan, np.nan],
+            [0, 1, 2, np.nan, np.nan, np.nan, np.nan],
+            [0, 2, 2, np.nan, 2, np.nan, 1],
+        ]
+    )
+    result = df.rolling(window=7, min_periods=1, axis="columns").sum()
+    expected = pd.DataFrame(
+        [
+            [0.0, 1.0, 3.0, 7.0, 7.0, 7.0, 7.0],
+            [0.0, 1.0, 3.0, 3.0, 3.0, 3.0, 3.0],
+            [0.0, 2.0, 4.0, 4.0, 6.0, 6.0, 7.0],
+        ]
+    )
     tm.assert_frame_equal(result, expected)
