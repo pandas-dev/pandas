3dcef566a1cd22903da4de1820796bce6580380c
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2020-09-19 21:53:46 +0200

    add comment

--- pandas/tests/extension/base/casting.py
+++ pandas/tests/extension/base/casting.py
@@ -60,6 +60,8 @@ class BaseCastingTests(BaseExtensionTests):
 
     @pytest.mark.parametrize("copy", [True, False])
     def test_astype_own_type(self, data, copy):
+        # ensure that astype returns the original object for equal dtype and copy=False
+        # https://github.com/pandas-dev/pandas/issues/28488
         result = data.astype(data.dtype, copy=copy)
         assert (result is data) is (not copy)
         self.assert_extension_array_equal(result, data)
