27e285e363b27f2fc3e0529bdc5051120d2996b8
Marco Gorelli <m.e.gorelli@gmail.com>
2020-09-19 08:24:56 +0100

    add flake8-rst back to code_checks

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -48,6 +48,13 @@ fi
 ### LINTING ###
 if [[ -z "$CHECK" || "$CHECK" == "lint" ]]; then
 
+    echo "flake8-rst --version"
+    flake8-rst --version
+
+    MSG='Linting code-blocks in .rst documentation' ; echo $MSG
+    flake8-rst doc/source --filename=*.rst --format="$FLAKE8_FORMAT"
+    RET=$(($RET + $?)) ; echo $MSG "DONE"
+
     # Check that cython casting is of the form `<type>obj` as opposed to `<type> obj`;
     # it doesn't make a difference, but we want to be internally consistent.
     # Note: this grep pattern is (intended to be) equivalent to the python
