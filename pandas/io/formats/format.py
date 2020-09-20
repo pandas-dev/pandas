bd5cb87f8b520571635ba977823d6026007176a4
Maxim Ivanov <ivanovmg@gmail.com>
2020-09-20 19:56:41 +0700

    REF: move _join_multiline to ConsoleFormatter

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -922,44 +922,6 @@ class DataFrameFormatter:
         strcols = self._to_str_columns()
         return self.adj.adjoin(1, *strcols)
 
-    def _join_multiline(self, *args) -> str:
-        lwidth = self.line_width
-        adjoin_width = 1
-        strcols = list(args)
-        if self.index:
-            idx = strcols.pop(0)
-            lwidth -= np.array([self.adj.len(x) for x in idx]).max() + adjoin_width
-
-        col_widths = [
-            np.array([self.adj.len(x) for x in col]).max() if len(col) > 0 else 0
-            for col in strcols
-        ]
-
-        assert lwidth is not None
-        col_bins = _binify(col_widths, lwidth)
-        nbins = len(col_bins)
-
-        if self.is_truncated_vertically:
-            assert self.max_rows_fitted is not None
-            nrows = self.max_rows_fitted + 1
-        else:
-            nrows = len(self.frame)
-
-        str_lst = []
-        start = 0
-        for i, end in enumerate(col_bins):
-            row = strcols[start:end]
-            if self.index:
-                row.insert(0, idx)
-            if nbins > 1:
-                if end <= len(strcols) and i < nbins - 1:
-                    row.append([" \\"] + ["  "] * (nrows - 1))
-                else:
-                    row.append([" "] * nrows)
-            str_lst.append(self.adj.adjoin(adjoin_width, *row))
-            start = end
-        return "\n\n".join(str_lst)
-
     def to_latex(
         self,
         buf: Optional[FilePathOrBuffer[str]] = None,
@@ -1186,6 +1148,44 @@ class ConsoleFormatter(DataFrameFormatter):
 
         # max_cols == 0. Try to fit frame to terminal
         return self._fit_strcols_to_terminal_width(strcols)
+
+    def _join_multiline(self, *args) -> str:
+        lwidth = self.line_width
+        adjoin_width = 1
+        strcols = list(args)
+        if self.index:
+            idx = strcols.pop(0)
+            lwidth -= np.array([self.adj.len(x) for x in idx]).max() + adjoin_width
+
+        col_widths = [
+            np.array([self.adj.len(x) for x in col]).max() if len(col) > 0 else 0
+            for col in strcols
+        ]
+
+        assert lwidth is not None
+        col_bins = _binify(col_widths, lwidth)
+        nbins = len(col_bins)
+
+        if self.is_truncated_vertically:
+            assert self.max_rows_fitted is not None
+            nrows = self.max_rows_fitted + 1
+        else:
+            nrows = len(self.frame)
+
+        str_lst = []
+        start = 0
+        for i, end in enumerate(col_bins):
+            row = strcols[start:end]
+            if self.index:
+                row.insert(0, idx)
+            if nbins > 1:
+                if end <= len(strcols) and i < nbins - 1:
+                    row.append([" \\"] + ["  "] * (nrows - 1))
+                else:
+                    row.append([" "] * nrows)
+            str_lst.append(self.adj.adjoin(adjoin_width, *row))
+            start = end
+        return "\n\n".join(str_lst)
 
 
 # ----------------------------------------------------------------------
