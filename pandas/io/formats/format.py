2a408d47b404020efbbf07a2994b4a5ea70217d7
Marco Gorelli <m.e.gorelli@gmail.com>
2020-09-22 14:37:58 +0100

    CI: Update version of 'black' (#36493)

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -1384,10 +1384,6 @@ class GenericArrayFormatter:
 
 
 class FloatArrayFormatter(GenericArrayFormatter):
-    """
-
-    """
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
