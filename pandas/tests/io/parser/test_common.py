df8794bfa29cfd78cccec77cfbeb2f22ad585d54
phofl <patrick_hoefler@gmx.net>
2020-09-23 20:55:44 +0200

    Add validation and add test

--- pandas/tests/io/parser/test_common.py
+++ pandas/tests/io/parser/test_common.py
@@ -2209,3 +2209,13 @@ def test_read_table_delim_whitespace_default_sep(all_parsers):
     result = parser.read_table(f, delim_whitespace=True)
     expected = DataFrame({"a": [1, 4], "b": [-2, 5], "c": [-3, 6]})
     tm.assert_frame_equal(result, expected)
+
+
+def test_read_table_delim_whitespace_non_default_sep(all_parsers):
+    # GH: 35958
+    f = StringIO("a  b  c\n1 -2 -3\n4  5   6")
+    parser = all_parsers
+    msg = "Specified a delimiter with both sep and " \
+          "delim_whitespace=True; you can only specify one."
+    with pytest.raises(ValueError, match=msg):
+        parser.read_table(f, delim_whitespace=True, sep=",")
