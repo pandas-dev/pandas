7dde2fd1ec464903e0c1036b31463af3a9493e8b
Dmitry Perepelkin <dimithras@gmail.com>
2020-09-24 21:04:11 +0300

    Comply with black

--- pandas/tests/groupby/test_grouping.py
+++ pandas/tests/groupby/test_grouping.py
@@ -129,11 +129,15 @@ class TestSelection:
         df = pd.DataFrame(
             {"Tuples": ((x, y) for x in [0, 1] for y in np.random.randint(3, 5, 5))}
         )
+
         gb = df.groupby("Tuples")
         gb_lambda = df.groupby(lambda x: df.iloc[x, 0])
+
         expected = gb.indices
         result = gb_lambda.indices
+
         tm.assert_dict_equal(result, expected)
+
 
 # grouping
 # --------------------------------
@@ -790,10 +794,13 @@ class TestGetGroup:
         df = pd.DataFrame(
             {"Tuples": ((x, y) for x in [0, 1] for y in np.random.randint(3, 5, 5))}
         )
+
         gb = df.groupby("Tuples")
         gb_lambda = df.groupby(lambda x: df.iloc[x, 0])
+
         expected = gb.get_group(list(gb.groups.keys())[0])
         result = gb_lambda.get_group(list(gb_lambda.groups.keys())[0])
+
         tm.assert_frame_equal(result, expected)
 
     def test_groupby_with_empty(self):
