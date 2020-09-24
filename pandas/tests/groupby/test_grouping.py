fa82e079d7488e9f1251c2ba6435006728458f19
Dmitry Perepelkin <dimithras@gmail.com>
2020-09-24 20:33:20 +0300

    Aligned with [pre-commit] test

--- pandas/tests/groupby/test_grouping.py
+++ pandas/tests/groupby/test_grouping.py
@@ -129,7 +129,7 @@ class TestSelection:
         df = pd.DataFrame(
             {"Tuples": ((x, y) for x in [0, 1] for y in np.random.randint(3, 5, 5))}
         )
-        gb = df.groupby('Tuples')
+        gb = df.groupby("Tuples")
         gb_lambda = df.groupby(lambda x: df.iloc[x, 0])
         expected = gb.indices
         result = gb_lambda.indices
@@ -790,7 +790,7 @@ class TestGetGroup:
         df = pd.DataFrame(
             {"Tuples": ((x, y) for x in [0, 1] for y in np.random.randint(3, 5, 5))}
         )
-        gb = df.groupby('Tuples')
+        gb = df.groupby("Tuples")
         gb_lambda = df.groupby(lambda x: df.iloc[x, 0])
         expected = gb.get_group(list(gb.groups.keys())[0])
         result = gb_lambda.get_group(list(gb_lambda.groups.keys())[0])
