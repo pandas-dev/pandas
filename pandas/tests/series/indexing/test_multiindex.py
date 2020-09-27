b00d2a1afbca5566b8afb8679ab90e6156e13606
ssche <sven.schellenberg@paradynsystems.com>
2020-09-27 16:03:43 +1000

    black changes

--- pandas/tests/series/indexing/test_multiindex.py
+++ pandas/tests/series/indexing/test_multiindex.py
@@ -24,16 +24,23 @@ def test_access_none_value_in_multiindex():
     assert result == 1
 
 
-@pytest.mark.parametrize('ix_data, exp_data', [(
-    [(pd.NaT, 1), (pd.NaT, 2)],
-    {"a": [pd.NaT, pd.NaT], "b": [1, 2], "x": [11, 12]}
-), (
-    [(pd.NaT, 1), (pd.Timestamp("2020-01-01"), 2)],
-    {"a": [pd.NaT, pd.Timestamp("2020-01-01")], "b": [1, 2], "x": [11, 12]}
-), (
-    [(pd.NaT, 1), (pd.Timedelta(123, 'd'), 2)],
-    {"a": [pd.NaT, pd.Timedelta(123, 'd')], "b": [1, 2], "x": [11, 12]}
-)])
+@pytest.mark.parametrize(
+    "ix_data, exp_data",
+    [
+        (
+            [(pd.NaT, 1), (pd.NaT, 2)],
+            {"a": [pd.NaT, pd.NaT], "b": [1, 2], "x": [11, 12]},
+        ),
+        (
+            [(pd.NaT, 1), (pd.Timestamp("2020-01-01"), 2)],
+            {"a": [pd.NaT, pd.Timestamp("2020-01-01")], "b": [1, 2], "x": [11, 12]},
+        ),
+        (
+            [(pd.NaT, 1), (pd.Timedelta(123, "d"), 2)],
+            {"a": [pd.NaT, pd.Timedelta(123, "d")], "b": [1, 2], "x": [11, 12]},
+        ),
+    ],
+)
 def test_nat_multi_index(ix_data, exp_data):
     # GH36541: that reset_index() does not raise ValueError
     ix = pd.MultiIndex.from_tuples(ix_data, names=["a", "b"])
