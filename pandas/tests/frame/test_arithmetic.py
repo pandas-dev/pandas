f34f51190e03e28cd3f120cfc58c8efb5845b774
jbrockmendel <jbrockmendel@gmail.com>
2020-09-28 14:36:32 -0700

    Backport PR #36706: CI: npdev new exception message

--- pandas/tests/frame/test_arithmetic.py
+++ pandas/tests/frame/test_arithmetic.py
@@ -53,6 +53,11 @@ class TestFrameComparisons:
                 msgs = [
                     r"Invalid comparison between dtype=datetime64\[ns\] and ndarray",
                     "invalid type promotion",
+                    (
+                        # npdev 1.20.0
+                        r"The DTypes <class 'numpy.dtype\[.*\]'> and "
+                        r"<class 'numpy.dtype\[.*\]'> do not have a common DType."
+                    ),
                 ]
                 msg = "|".join(msgs)
                 with pytest.raises(TypeError, match=msg):
