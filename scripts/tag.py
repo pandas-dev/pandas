6c4119368737df72485f129c6532496e13a6ebec
Simon Hawkins <simonjayhawkins@gmail.com>
2020-09-28 16:46:29 +0100

    Github action for release

---
+++ scripts/tag.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python3
+"""
+Tag a pandas release.
+
+This does not push the tag.
+"""
+import argparse
+import subprocess
+import sys
+
+from packaging import version
+
+
+def check_tag(tag):
+    assert tag.startswith("v"), "Invalid tag '{}', must " "start with 'v'".format(tag)
+    ver = version.parse(tag.lstrip("v"))
+    assert isinstance(ver, version.Version), "Invalid tag '{}'".format(tag)
+    if "rc" in tag:
+        assert ".rc" not in tag, "RC tags should be formatted like '.0rcX' "
+    return tag
+
+
+def checkout(tag):
+    if tag[-1] == "0" or "rc" in tag:
+        # off master
+        base = "master"
+    else:
+        base = ".".join([tag[1:].rsplit(".", 1)[0], "x"])
+
+    subprocess.check_call(["git", "checkout", base])
+    # subprocess.check_call(["git", "pull", "--ff-only", "upstream", base])
+
+
+def commit(tag):
+    subprocess.check_call(["git", "clean", "-xdf"])
+    print("Creating tag {}".format(tag))
+    subprocess.check_call(
+        [
+            "git",
+            "commit",
+            "--allow-empty",
+            "-m",
+            "RLS: {}".format(tag[1:]),
+        ]
+    )
+    subprocess.check_call(["git", "tag", "-a", tag, "-m", "Version {}".format(tag[1:])])
+
+
+def parse_args(args=None):
+    parser = argparse.ArgumentParser(__name__, usage=__doc__)
+    parser.add_argument("tag", type=check_tag)
+
+    return parser.parse_args(args)
+
+
+def main(args=None):
+    args = parse_args(args)
+    checkout(args.tag)
+    commit(args.tag)
+
+
+if __name__ == "__main__":
+    sys.exit(main())
