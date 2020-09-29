17be3649120c1bd5ad09878ec47a9f8c5870615d
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-09-29 19:46:20 +0300

    add re, un-parametrize

--- pandas/tests/io/formats/test_format.py
+++ pandas/tests/io/formats/test_format.py
@@ -2846,18 +2846,12 @@ class TestSeriesFormatting:
         exp = "    r1 r2\na b      \n0 1  2  3"
         assert res == exp
 
-    @pytest.mark.parametrize(
-        "data, expected",
-        (
-            ["", "Hello", "World", "", "", "Mooooo", "", ""],
-            "      \n Hello\n World\n      \n      \nMooooo\n      \n      ",
-        ),
-    )
-    def test_to_string_empty_col(self, data, expected):
+    def test_to_string_empty_col(self):
         # GH 13653
-        s = pd.Series(data)
+        s = pd.Series(["", "Hello", "World", "", "", "Mooooo", "", ""])
         res = s.to_string(index=False)
-        assert res == expected
+        exp = "      \n Hello\n World\n      \n      \nMooooo\n      \n      "
+        assert re.match(exp, res)
 
 
 class TestGenericArrayFormatter:
