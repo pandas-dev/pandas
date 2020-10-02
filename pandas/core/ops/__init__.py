8b68ecbc022d095d1a7b63af375a7f81e813f759
jbrockmendel <jbrockmendel@gmail.com>
2020-10-02 14:29:24 -0700

    BUG: DTI/TDI.equals with i8 (#36744)

--- pandas/core/ops/__init__.py
+++ pandas/core/ops/__init__.py
@@ -539,7 +539,9 @@ def _should_reindex_frame_op(
     if fill_value is None and level is None and axis is default_axis:
         # TODO: any other cases we should handle here?
         cols = left.columns.intersection(right.columns)
-        if not (cols.equals(left.columns) and cols.equals(right.columns)):
+
+        if len(cols) and not (cols.equals(left.columns) and cols.equals(right.columns)):
+            # TODO: is there a shortcut available when len(cols) == 0?
             return True
 
     return False
