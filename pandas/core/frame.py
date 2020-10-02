c65a025bbd54f4ec0ae25328728847798f0a974c
jbrockmendel <jbrockmendel@gmail.com>
2020-10-02 14:12:35 -0700

    ERR: error handling in DataFrame.__rmatmul__ (#36792)

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -1216,7 +1216,14 @@ class DataFrame(NDFrame):
         """
         Matrix multiplication using binary `@` operator in Python>=3.5.
         """
-        return self.T.dot(np.transpose(other)).T
+        try:
+            return self.T.dot(np.transpose(other)).T
+        except ValueError as err:
+            if "shape mismatch" not in str(err):
+                raise
+            # GH#21581 give exception message for original shapes
+            msg = f"shapes {np.shape(other)} and {self.shape} not aligned"
+            raise ValueError(msg) from err
 
     # ----------------------------------------------------------------------
     # IO methods (to / from other formats)
