2684e52031421bd83ad09b9b4949b81227507cd6
Simon Hawkins <simonjayhawkins@gmail.com>
2020-10-04 14:12:10 +0100

    [mypy-pandas.core.computation.expressions]

--- pandas/core/computation/expressions.py
+++ pandas/core/computation/expressions.py
@@ -248,7 +248,12 @@ def where(cond, a, b, use_numexpr=True):
     use_numexpr : bool, default True
         Whether to try to use numexpr.
     """
-    return _where(cond, a, b) if use_numexpr else _where_standard(cond, a, b)
+    # pandas\core\computation\expressions.py:251: error: "None" not callable  [misc]
+    return (
+        _where(cond, a, b)  # type: ignore[misc]
+        if use_numexpr
+        else _where_standard(cond, a, b)
+    )
 
 
 def set_test_mode(v: bool = True) -> None:
