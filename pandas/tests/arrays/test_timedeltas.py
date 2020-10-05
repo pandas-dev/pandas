fbecbe3607708b6a11ab6571b51256642c18fd15
Brock <jbrockmendel@gmail.com>
2020-10-05 15:30:07 -0700

    REF: collect unary ops tests

--- pandas/tests/arrays/test_timedeltas.py
+++ pandas/tests/arrays/test_timedeltas.py
@@ -77,35 +77,6 @@ class TestTimedeltaArray:
         with pytest.raises(ValueError, match=msg):
             TimedeltaArray._from_sequence([], dtype=object)
 
-    def test_abs(self):
-        vals = np.array([-3600 * 10 ** 9, "NaT", 7200 * 10 ** 9], dtype="m8[ns]")
-        arr = TimedeltaArray(vals)
-
-        evals = np.array([3600 * 10 ** 9, "NaT", 7200 * 10 ** 9], dtype="m8[ns]")
-        expected = TimedeltaArray(evals)
-
-        result = abs(arr)
-        tm.assert_timedelta_array_equal(result, expected)
-
-    def test_neg(self):
-        vals = np.array([-3600 * 10 ** 9, "NaT", 7200 * 10 ** 9], dtype="m8[ns]")
-        arr = TimedeltaArray(vals)
-
-        evals = np.array([3600 * 10 ** 9, "NaT", -7200 * 10 ** 9], dtype="m8[ns]")
-        expected = TimedeltaArray(evals)
-
-        result = -arr
-        tm.assert_timedelta_array_equal(result, expected)
-
-    def test_neg_freq(self):
-        tdi = pd.timedelta_range("2 Days", periods=4, freq="H")
-        arr = TimedeltaArray(tdi, freq=tdi.freq)
-
-        expected = TimedeltaArray(-tdi._data, freq=-tdi.freq)
-
-        result = -arr
-        tm.assert_timedelta_array_equal(result, expected)
-
     @pytest.mark.parametrize("dtype", [int, np.int32, np.int64, "uint32", "uint64"])
     def test_astype_int(self, dtype):
         arr = TimedeltaArray._from_sequence([pd.Timedelta("1H"), pd.Timedelta("2H")])
@@ -170,6 +141,37 @@ class TestTimedeltaArray:
         )
         with pytest.raises(TypeError, match=msg):
             arr.searchsorted(other)
+
+
+class TestUnaryOps:
+    def test_abs(self):
+        vals = np.array([-3600 * 10 ** 9, "NaT", 7200 * 10 ** 9], dtype="m8[ns]")
+        arr = TimedeltaArray(vals)
+
+        evals = np.array([3600 * 10 ** 9, "NaT", 7200 * 10 ** 9], dtype="m8[ns]")
+        expected = TimedeltaArray(evals)
+
+        result = abs(arr)
+        tm.assert_timedelta_array_equal(result, expected)
+
+    def test_neg(self):
+        vals = np.array([-3600 * 10 ** 9, "NaT", 7200 * 10 ** 9], dtype="m8[ns]")
+        arr = TimedeltaArray(vals)
+
+        evals = np.array([3600 * 10 ** 9, "NaT", -7200 * 10 ** 9], dtype="m8[ns]")
+        expected = TimedeltaArray(evals)
+
+        result = -arr
+        tm.assert_timedelta_array_equal(result, expected)
+
+    def test_neg_freq(self):
+        tdi = pd.timedelta_range("2 Days", periods=4, freq="H")
+        arr = TimedeltaArray(tdi, freq=tdi.freq)
+
+        expected = TimedeltaArray(-tdi._data, freq=-tdi.freq)
+
+        result = -arr
+        tm.assert_timedelta_array_equal(result, expected)
 
 
 class TestReductions:
