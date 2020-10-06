1c73099a6f41fb8104b6ea88a7bd46a2beb9ebae
jbrockmendel <jbrockmendel@gmail.com>
2020-10-06 15:52:57 -0700

    REF: IntervalIndex.equals defer to IntervalArray.equals (#36871)

--- pandas/core/indexes/interval.py
+++ pandas/core/indexes/interval.py
@@ -997,19 +997,10 @@ class IntervalIndex(IntervalMixin, ExtensionIndex):
         if self.is_(other):
             return True
 
-        # if we can coerce to an IntervalIndex then we can compare
         if not isinstance(other, IntervalIndex):
-            if not is_interval_dtype(other):
-                return False
-            other = Index(other)
-            if not isinstance(other, IntervalIndex):
-                return False
+            return False
 
-        return (
-            self.left.equals(other.left)
-            and self.right.equals(other.right)
-            and self.closed == other.closed
-        )
+        return self._data.equals(other._data)
 
     # --------------------------------------------------------------------
     # Set Operations
