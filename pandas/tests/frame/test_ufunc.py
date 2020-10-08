2260c836455247a9abc5c09cf3edd13b7d96af78
Tom Augspurger <tom.w.augspurger@gmail.com>
2020-10-08 08:59:47 -0500

    fixup

--- pandas/tests/frame/test_ufunc.py
+++ pandas/tests/frame/test_ufunc.py
@@ -70,8 +70,8 @@ def test_binary_input_aligns_columns(dtype_a, dtype_b):
         dtype_b["C"] = dtype_b.pop("B")
 
     df2 = pd.DataFrame({"A": [1, 2], "C": [3, 4]}).astype(dtype_b)
-    result = np.logaddexp(df1, df2)
-    expected = np.logaddexp(
+    result = np.heaviside(df1, df2)
+    expected = np.heaviside(
         np.array([[1, 3, np.nan], [2, 4, np.nan]]),
         np.array([[1, np.nan, 3], [2, np.nan, 4]]),
     )
@@ -85,8 +85,8 @@ def test_binary_input_aligns_index(dtype):
         pytest.xfail(reason="Extension / mixed with multiple inputs not implemented.")
     df1 = pd.DataFrame({"A": [1, 2], "B": [3, 4]}, index=["a", "b"]).astype(dtype)
     df2 = pd.DataFrame({"A": [1, 2], "B": [3, 4]}, index=["a", "c"]).astype(dtype)
-    result = np.logaddexp(df1, df2)
-    expected = np.logaddexp(
+    result = np.heaviside(df1, df2)
+    expected = np.heaviside(
         np.array([[1, 3], [3, 4], [np.nan, np.nan]]),
         np.array([[1, 3], [np.nan, np.nan], [3, 4]]),
     )
