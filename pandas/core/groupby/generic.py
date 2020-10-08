10dff12d35605dd37d3d89a4fe46f31bad9fe0ad
arw2019 <andrew.r.wieteska@gmail.com>
2020-10-08 07:02:06 +0000

    drop the if-else clause

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -1645,9 +1645,8 @@ class DataFrameGroupBy(GroupBy[DataFrame]):
         DataFrame
         """
         indexed_output = {key.position: val for key, val in output.items()}
-
         agg_axis = self._obj_with_exclusions._get_axis(1 - self.axis)
-        columns = Index([key.label for key in output], names=agg_axis.names)
+        columns = Index([key.label for key in output], name=agg_axis.names)
 
         result = self.obj._constructor(indexed_output)
         result.columns = columns
