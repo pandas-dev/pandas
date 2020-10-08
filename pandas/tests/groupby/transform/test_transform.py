84abae0c43b8ebea3c1584a71a2e6a7ae9fd8e79
smithto1 <thomassmith0304@gmail.com>
2020-10-08 10:41:13 +0100

    move test to test_missing,py

--- pandas/tests/groupby/transform/test_transform.py
+++ pandas/tests/groupby/transform/test_transform.py
@@ -977,40 +977,6 @@ def test_ffill_bfill_non_unique_multilevel(func, expected_status):
     tm.assert_series_equal(result, expected)
 
 
-@pytest.mark.parametrize("method", ["ffill", "bfill"])
-@pytest.mark.parametrize("dropna", [True, False])
-@pytest.mark.parametrize("has_nan_group", [True, False])
-def test_ffill_handles_nan_groups(dropna, method, has_nan_group):
-    # GH 34725
-
-    df_without_nan_rows = pd.DataFrame([(1, 0.1), (2, 0.2)])
-
-    ridx = [-1, 0, -1, -1, 1, -1]
-    df = df_without_nan_rows.reindex(ridx).reset_index(drop=True)
-
-    group_b = np.nan if has_nan_group else "b"
-    df["group_col"] = pd.Series(["a"] * 3 + [group_b] * 3)
-
-    grouped = df.groupby(by="group_col", dropna=dropna)
-    result = getattr(grouped, method)(limit=None)
-
-    expected_rows = {
-        ("ffill", True, True): [-1, 0, 0, -1, -1, -1],
-        ("ffill", True, False): [-1, 0, 0, -1, 1, 1],
-        ("ffill", False, True): [-1, 0, 0, -1, 1, 1],
-        ("ffill", False, False): [-1, 0, 0, -1, 1, 1],
-        ("bfill", True, True): [0, 0, -1, -1, -1, -1],
-        ("bfill", True, False): [0, 0, -1, 1, 1, -1],
-        ("bfill", False, True): [0, 0, -1, 1, 1, -1],
-        ("bfill", False, False): [0, 0, -1, 1, 1, -1],
-    }
-
-    ridx = expected_rows.get((method, dropna, has_nan_group))
-    expected = df_without_nan_rows.reindex(ridx).reset_index(drop=True)
-
-    tm.assert_frame_equal(result, expected)
-
-
 @pytest.mark.parametrize("func", [np.any, np.all])
 def test_any_all_np_func(func):
     # GH 20653
