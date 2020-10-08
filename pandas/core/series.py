d7c84912bcb2736a35795dc2b9c8c7d0c5fb7ab2
jbrockmendel <jbrockmendel@gmail.com>
2020-10-08 13:46:37 -0700

    REF: IndexOpsMixin wrapping (#36848)

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -2053,7 +2053,9 @@ Name: Max Speed, dtype: float64
         4     True
         dtype: bool
         """
-        return super().duplicated(keep=keep)
+        res = base.IndexOpsMixin.duplicated(self, keep=keep)
+        result = self._constructor(res, index=self.index)
+        return result.__finalize__(self, method="duplicated")
 
     def idxmin(self, axis=0, skipna=True, *args, **kwargs):
         """
@@ -4776,7 +4778,7 @@ Keep all original rows and also all original values
 
     @doc(NDFrame.isna, klass=_shared_doc_kwargs["klass"])
     def isna(self) -> "Series":
-        return super().isna()
+        return generic.NDFrame.isna(self)
 
     @doc(NDFrame.isna, klass=_shared_doc_kwargs["klass"])
     def isnull(self) -> "Series":
