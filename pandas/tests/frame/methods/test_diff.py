2dccd37c6290f74f9144ab57ae9aae7bb173727d
Brock <jbrockmendel@gmail.com>
2020-10-09 16:01:19 -0700

    tests for td64 case

--- pandas/tests/frame/methods/test_diff.py
+++ pandas/tests/frame/methods/test_diff.py
@@ -39,6 +39,28 @@ class TestDataFrameDiff:
         expected = pd.DataFrame({"x": np.nan, "y": pd.Series(1), "z": pd.Series(1)})
         tm.assert_frame_equal(result, expected)
 
+    def test_diff_timedelta64_with_nat(self):
+        # GH#32441
+        arr = np.arange(6).reshape(3, 2).astype("timedelta64[ns]")
+        arr[:, 0] = "NaT"
+
+        df = pd.DataFrame(arr)
+        result = df.diff(1, axis=0)
+
+        expected = pd.DataFrame(
+            {0: df[0], 1: [pd.NaT, pd.Timedelta(2), pd.Timedelta(2)]}
+        )
+        tm.assert_equal(result, expected)
+
+        result = df.diff(0)
+        expected = df - df
+        assert expected[0].isna().all()
+        tm.assert_equal(result, expected)
+
+        result = df.diff(-1, axis=1)
+        expected = df * np.nan
+        tm.assert_equal(result, expected)
+
     @pytest.mark.parametrize("tz", [None, "UTC"])
     def test_diff_datetime_axis0_with_nat(self, tz):
         # GH#32441
