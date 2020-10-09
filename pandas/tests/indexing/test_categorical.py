17e73a52c5bf5b4478872492dd63673d1e4422c0
arw2019 <andrew.r.wieteska@gmail.com>
2020-10-09 05:00:46 +0000

    move test to frame/test_alter_axes.py

--- pandas/tests/indexing/test_categorical.py
+++ pandas/tests/indexing/test_categorical.py
@@ -650,33 +650,6 @@ class TestCategoricalIndex:
         expected = self.df.iloc[[2, 3, 4]]
         tm.assert_frame_equal(result, expected)
 
-    @pytest.mark.parametrize(
-        "codes", ([[0, 0, 1, 1], [0, 1, 0, 1]], [[0, 0, -1, 1], [0, 1, 0, 1]])
-    )
-    def test_reindexing_with_missing_values(self, codes):
-        # GH 24206
-
-        index = pd.MultiIndex(
-            [pd.CategoricalIndex(["A", "B"]), pd.CategoricalIndex(["a", "b"])], codes
-        )
-        data = {"col": range(len(index))}
-        df = DataFrame(data=data, index=index)
-
-        expected = pd.DataFrame(
-            {
-                "level_0": pd.Categorical.from_codes(codes[0], categories=["A", "B"]),
-                "level_1": pd.Categorical.from_codes(codes[1], categories=["a", "b"]),
-                "col": range(4),
-            }
-        )
-
-        res = df.reset_index()
-        tm.assert_frame_equal(res, expected)
-
-        # roundtrip
-        res = expected.set_index(["level_0", "level_1"]).reset_index()
-        tm.assert_frame_equal(res, expected)
-
     def test_loc_and_at_with_categorical_index(self):
         # GH 20629
         s = Series([1, 2, 3], index=pd.CategoricalIndex(["A", "B", "C"]))
