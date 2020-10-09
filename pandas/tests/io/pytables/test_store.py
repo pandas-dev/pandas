42a6bda8552452f1e3e95eef8e5496cdec4f36fa
jnecus <joenecus@gmail.com>
2020-10-09 15:00:24 +0100

    Update test_store.py

--- pandas/tests/io/pytables/test_store.py
+++ pandas/tests/io/pytables/test_store.py
@@ -4216,7 +4216,9 @@ class TestHDFStore:
                         import tempfile
 
                         fd, new_f = tempfile.mkstemp()
-                        tstore = store.copy(new_f, keys=keys, propindexes=propindexes, **kwargs)
+                        tstore = store.copy(
+                            new_f, keys=keys, propindexes=propindexes, **kwargs
+                        )
 
                     # check keys
                     if keys is None:
