2d981a14fd3df2a5aec4d8c5480774a42fca2591
Eric Leung <eric@erictleung.com>
2020-10-10 15:13:32 -0700

    DOC: update fillna info to contrast interpolate
    In the "See Also" section of interpolate, the fillna method sounds very
    similar, but it actually is more limited. So this commit makes this
    distinction more clear.

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -6916,7 +6916,7 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
 
         See Also
         --------
-        fillna : Fill missing values using different methods.
+        fillna : Fill missing values using a set value or existing values.
         scipy.interpolate.Akima1DInterpolator : Piecewise cubic polynomials
             (Akima interpolator).
         scipy.interpolate.BPoly.from_derivatives : Piecewise polynomial in the
