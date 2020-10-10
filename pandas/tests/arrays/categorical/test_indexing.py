96e4318d0d1702fd0cadb1e7387fe890be337f3b
chrispe92 <ch.petrop@gmail.com>
2020-10-10 13:54:19 +0300

    Fix import ordering/format

--- pandas/tests/arrays/categorical/test_indexing.py
+++ pandas/tests/arrays/categorical/test_indexing.py
@@ -1,7 +1,6 @@
 import numpy as np
 import pytest
 
-
 import pandas as pd
 from pandas import Categorical, CategoricalIndex, Index, PeriodIndex, Series
 import pandas._testing as tm
--- pandas/tests/series/test_categorical.py
+++ pandas/tests/series/test_categorical.py
@@ -1,8 +1,8 @@
 import pytest
-import pandas as pd
-import pandas._testing as tm
 
+import pandas as pd
 from pandas import Categorical, Index
+import pandas._testing as tm
 
 
 class TestCategoricalSeries:
