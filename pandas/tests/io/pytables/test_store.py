740a1da516ad3d60dac9c708b0727cff5955d070
jnecus <joenecus@gmail.com>
2020-10-11 10:46:15 +0100

    Update test_store.py

--- pandas/tests/io/pytables/test_store.py
+++ pandas/tests/io/pytables/test_store.py
@@ -88,8 +88,10 @@ class TestHDFStore:
                     raise ValueError("blah")
             except ValueError:
                 pass
+        with tm.ensure_clean(setup_path) as path:
             with HDFStore(path) as tbl:
                 tbl["a"] = tm.makeDataFrame()
+        with tm.ensure_clean(setup_path) as path:
             with HDFStore(path) as tbl:
                 assert len(tbl) == 1
                 assert type(tbl["a"]) == DataFrame
