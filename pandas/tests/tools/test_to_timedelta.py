ad38ad349ea4fd500118782a6af2f99bfbb4e2d8
Avinash Pancham <avinashpancham@outlook.com>
2020-10-12 09:03:11 +0200

    Add description to unittest

--- pandas/tests/tools/test_to_timedelta.py
+++ pandas/tests/tools/test_to_timedelta.py
@@ -137,6 +137,8 @@ class TestTimedeltas:
         ],
     )
     def test_unambiguous_timedelta_values(self, val, warning):
+        # GH36666 Deprecate use of strings denoting units with 'M', 'Y', 'm' or 'y'
+        # in pd.to_timedelta
         with tm.assert_produces_warning(warning):
             to_timedelta(val)
 
