8bc61db736e77480e3d4bfa9d6dda89aeadbd661
smithto1 <thomassmith0304@gmail.com>
2020-10-15 23:26:25 +0100

    #36757 fix for speed issue

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -1191,7 +1191,7 @@ b  2""",
             # when the ax has duplicates
             # so we resort to this
             # GH 14776, 30667
-            if ax.has_duplicates:
+            if ax.has_duplicates and not result.axes[self.axis].equals(ax):
                 indexer, _ = result.index.get_indexer_non_unique(ax.values)
                 indexer = algorithms.unique1d(indexer)
                 result = result.take(indexer, axis=self.axis)
