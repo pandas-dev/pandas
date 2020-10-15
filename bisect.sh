80b0103c3b2db15bee54b04c074e7db19d7d9a03
Daniel Saxton <dsaxton@pm.me>
2020-10-14 20:26:32 -0500

    Oops

--- bisect.sh
+++
@@ -1,4 +0,0 @@
-git bisect start ${1} ${2}
-git bisect run bash -c "python setup.py build_ext --inplace -j 4; /
-    python -m pip install -e . --no-build-isolation --no-use-pep517; /
-    python ${3}"
--- sample.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df = pd.DataFrame(
-    {"A": ["x", "x", "y"], "B": ["a", "b", "b"], "C": [1, 1, 1]}
-).set_index(["A", "B"])
-df["D"] = 2
-result = df.unstack("B").fillna(0)
-assert result.notna().all().all()
