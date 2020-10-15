cce1fc5c8bcf080e951d948dfe23018b95ff445c
arw2019 <andrew.r.wieteska@gmail.com>
2020-10-15 04:04:50 +0000

    TYP: use Shape alias in core/indexes/multi.py

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -20,7 +20,7 @@ from pandas._config import get_option
 
 from pandas._libs import algos as libalgos, index as libindex, lib
 from pandas._libs.hashtable import duplicated_int64
-from pandas._typing import AnyArrayLike, Label, Scalar
+from pandas._typing import AnyArrayLike, Label, Scalar, Shape
 from pandas.compat.numpy import function as nv
 from pandas.errors import InvalidIndexError, PerformanceWarning, UnsortedIndexError
 from pandas.util._decorators import Appender, cache_readonly, doc
@@ -702,7 +702,7 @@ class MultiIndex(Index):
         )
 
     @property
-    def shape(self):
+    def shape(self) -> Shape:
         """
         Return a tuple of the shape of the underlying data.
         """
