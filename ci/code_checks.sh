b2a1b22374b92bce55a2fda5e8ab65cfeaa382de
jnecus <joenecus@gmail.com>
2020-10-16 02:30:55 +0100

    Update code_checks.sh to check for instances of os.remove (#37097)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -248,6 +248,10 @@ if [[ -z "$CHECK" || "$CHECK" == "patterns" ]]; then
     invgrep -RI --exclude=\*.{svg,c,cpp,html,js} --exclude-dir=env "\s$" *
     RET=$(($RET + $?)) ; echo $MSG "DONE"
     unset INVGREP_APPEND
+
+    MSG='Check code for instances of os.remove' ; echo $MSG
+    invgrep -R --include="*.py*" --exclude "common.py" --exclude "test_writers.py" --exclude "test_store.py" -E "os\.remove" pandas/tests/
+    RET=$(($RET + $?)) ; echo $MSG "DONE"
 fi
 
 ### CODE ###
