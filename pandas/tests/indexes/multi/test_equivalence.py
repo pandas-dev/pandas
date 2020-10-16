0f558aaca2189ccf8dfc3a086267a72dc4203002
Brock <jbrockmendel@gmail.com>
2020-10-16 13:51:57 -0700

    TST: MultiIndex comparison with tuple #21517

--- pandas/tests/indexes/multi/test_equivalence.py
+++ pandas/tests/indexes/multi/test_equivalence.py
@@ -84,6 +84,32 @@ def test_equals_op(idx):
         tm.assert_series_equal(series_a == item, Series(expected3))
 
 
+def test_compare_tuple():
+    # GH#21517
+    mi = MultiIndex.from_product([[1, 2]] * 2)
+
+    all_false = np.array([False, False, False, False])
+
+    result = mi == mi[0]
+    expected = np.array([True, False, False, False])
+    tm.assert_numpy_array_equal(result, expected)
+
+    result = mi != mi[0]
+    tm.assert_numpy_array_equal(result, ~expected)
+
+    result = mi < mi[0]
+    tm.assert_numpy_array_equal(result, all_false)
+
+    result = mi <= mi[0]
+    tm.assert_numpy_array_equal(result, expected)
+
+    result = mi > mi[0]
+    tm.assert_numpy_array_equal(result, ~expected)
+
+    result = mi >= mi[0]
+    tm.assert_numpy_array_equal(result, ~all_false)
+
+
 def test_equals_multi(idx):
     assert idx.equals(idx)
     assert not idx.equals(idx.values)
