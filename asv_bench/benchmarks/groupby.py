19ddf0d6d29c6a7b6c2d0342ba61eaf31d33dff6
smithto1 <thomassmith0304@gmail.com>
2020-10-17 00:13:39 +0100

    added asv FillNA

--- asv_bench/benchmarks/groupby.py
+++ asv_bench/benchmarks/groupby.py
@@ -358,6 +358,21 @@ class Size:
         self.draws.groupby(self.cats).size()
 
 
+class FillNA:
+    def setup(self):
+        N = 1500
+        self.df = pd.DataFrame({
+                    'group' : [1] * N + [2] * N,
+                    'value' : [np.nan, 1.0] * N
+                }).set_index('group')
+    
+    def time_df_ffill(self):
+        self.df.groupby('group').fillna(method='ffill')
+    
+    def time_srs_ffill(self):
+        self.df.groupby('group')['value'].fillna(method='ffill')
+
+
 class GroupByMethods:
 
     param_names = ["dtype", "method", "application"]
