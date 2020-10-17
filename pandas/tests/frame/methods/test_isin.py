2ffa9748522739e4371f34102888b48960424ba8
Daniel Saxton <2658661+dsaxton@users.noreply.github.com>
2020-10-17 12:04:01 -0500

    BUG: Fix isin with read-only target (#37181)

--- pandas/tests/frame/methods/test_isin.py
+++ pandas/tests/frame/methods/test_isin.py
@@ -204,3 +204,12 @@ class TestDataFrameIsIn:
 
         result = df.isin(values)
         tm.assert_frame_equal(result, expected)
+
+    def test_isin_read_only(self):
+        # https://github.com/pandas-dev/pandas/issues/37174
+        arr = np.array([1, 2, 3])
+        arr.setflags(write=False)
+        df = DataFrame([1, 2, 3])
+        result = df.isin(arr)
+        expected = DataFrame([True, True, True])
+        tm.assert_frame_equal(result, expected)
