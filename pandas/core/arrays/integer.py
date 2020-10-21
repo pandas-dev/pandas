d21675a7b98058276f497a4998d6fb4c67db2f29
Nagesh Kumar C <65619177+nagesh-chowdaiah@users.noreply.github.com>
2020-10-21 14:30:47 +0530

    Update pandas/core/arrays/integer.py
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- pandas/core/arrays/integer.py
+++ pandas/core/arrays/integer.py
@@ -484,7 +484,7 @@ class IntegerArray(BaseMaskedArray):
 
         See Also
         --------
-        ExtensionArray.argsort: Return the indices that would sort this array.
+        ExtensionArray.argsort : Return the indices that would sort this array.
         """
         data = self._data.copy()
         if self._mask.any():
