24cd07d97cd2a507084bafa38f9c5c4fa6d0b538
Brock <jbrockmendel@gmail.com>
2020-10-21 10:50:34 -0700

    TST/REF: collect indexing tests by method

--- pandas/tests/frame/methods/test_reindex.py
+++ pandas/tests/frame/methods/test_reindex.py
@@ -12,6 +12,21 @@ class TestDataFrameSelectReindex:
     # These are specific reindex-based tests; other indexing tests should go in
     # test_indexing
 
+    def test_reindex_sparse(self):
+        # https://github.com/pandas-dev/pandas/issues/35286
+        df = DataFrame(
+            {"A": [0, 1], "B": pd.array([0, 1], dtype=pd.SparseDtype("int64", 0))}
+        )
+        result = df.reindex([0, 2])
+        expected = DataFrame(
+            {
+                "A": [0.0, np.nan],
+                "B": pd.array([0.0, np.nan], dtype=pd.SparseDtype("float64", 0.0)),
+            },
+            index=[0, 2],
+        )
+        tm.assert_frame_equal(result, expected)
+
     def test_reindex(self, float_frame):
         datetime_series = tm.makeTimeSeries(nper=30)
 
