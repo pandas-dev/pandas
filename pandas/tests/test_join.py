69ddcc84340e18174d478c8f586bfc722b040e88
Brock <jbrockmendel@gmail.com>
2020-10-21 15:37:19 -0700

    TST/REF: implement tests.libs

--- pandas/tests/test_join.py
+++ pandas/tests/test_join.py
@@ -3,7 +3,7 @@ import pytest
 
 from pandas._libs import join as libjoin
 
-from pandas import Categorical, DataFrame, Index, merge
+from pandas import Index
 import pandas._testing as tm
 
 
@@ -288,59 +288,3 @@ def test_inner_join_indexer2():
 
     exp_ridx = np.array([0, 1, 2, 3], dtype=np.int64)
     tm.assert_almost_equal(ridx, exp_ridx)
-
-
-def test_merge_join_categorical_multiindex():
-    # From issue 16627
-    a = {
-        "Cat1": Categorical(["a", "b", "a", "c", "a", "b"], ["a", "b", "c"]),
-        "Int1": [0, 1, 0, 1, 0, 0],
-    }
-    a = DataFrame(a)
-
-    b = {
-        "Cat": Categorical(["a", "b", "c", "a", "b", "c"], ["a", "b", "c"]),
-        "Int": [0, 0, 0, 1, 1, 1],
-        "Factor": [1.1, 1.2, 1.3, 1.4, 1.5, 1.6],
-    }
-    b = DataFrame(b).set_index(["Cat", "Int"])["Factor"]
-
-    expected = merge(
-        a,
-        b.reset_index(),
-        left_on=["Cat1", "Int1"],
-        right_on=["Cat", "Int"],
-        how="left",
-    )
-    result = a.join(b, on=["Cat1", "Int1"])
-    expected = expected.drop(["Cat", "Int"], axis=1)
-    tm.assert_frame_equal(expected, result)
-
-    # Same test, but with ordered categorical
-    a = {
-        "Cat1": Categorical(
-            ["a", "b", "a", "c", "a", "b"], ["b", "a", "c"], ordered=True
-        ),
-        "Int1": [0, 1, 0, 1, 0, 0],
-    }
-    a = DataFrame(a)
-
-    b = {
-        "Cat": Categorical(
-            ["a", "b", "c", "a", "b", "c"], ["b", "a", "c"], ordered=True
-        ),
-        "Int": [0, 0, 0, 1, 1, 1],
-        "Factor": [1.1, 1.2, 1.3, 1.4, 1.5, 1.6],
-    }
-    b = DataFrame(b).set_index(["Cat", "Int"])["Factor"]
-
-    expected = merge(
-        a,
-        b.reset_index(),
-        left_on=["Cat1", "Int1"],
-        right_on=["Cat", "Int"],
-        how="left",
-    )
-    result = a.join(b, on=["Cat1", "Int1"])
-    expected = expected.drop(["Cat", "Int"], axis=1)
-    tm.assert_frame_equal(expected, result)
