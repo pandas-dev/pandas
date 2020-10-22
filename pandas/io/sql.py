0b6c1efd27ad35495e8765021f1bb52567606743
pilotjoe <36243295+pilotjoe@users.noreply.github.com>
2020-10-22 16:24:11 -0400

    Resolve multi-line if formatting

--- pandas/io/sql.py
+++ pandas/io/sql.py
@@ -1011,9 +1011,11 @@ class SQLTable(PandasObject):
 
                 elif len(df_col) == df_col.count():
                     # No NA values, can convert ints and bools
-                    if col_type is np.dtype("int64") \
-                        or col_type is np.dtype("int32") \
-                        or col_type is bool:
+                    if (
+                        col_type is np.dtype("int64")
+                        or col_type is np.dtype("int32")
+                        or col_type is bool
+                    ):
                         self.frame[col_name] = df_col.astype(col_type, copy=False)
             except KeyError:
                 pass  # this column not in results
