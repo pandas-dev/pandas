7b7d6f8d9d34fdbd1d6cccfbe43bac07dd06e0b3
ssche <sven.schellenberg@paradynsystems.com>
2020-10-22 14:26:36 +1100

    Removed pd.xyz

--- pandas/tests/indexing/multiindex/test_multiindex.py
+++ pandas/tests/indexing/multiindex/test_multiindex.py
@@ -94,23 +94,23 @@ class TestMultiIndexBasic:
 
 
 def test_combine_first_with_nan_multiindex():
-    mi1 = pd.MultiIndex.from_arrays(
+    mi1 = MultiIndex.from_arrays(
         [["b", "b", "c", "a", "b", np.nan], [1, 2, 3, 4, 5, 6]], names=["a", "b"]
     )
-    df = pd.DataFrame({"c": [1, 1, 1, 1, 1, 1]}, index=mi1)
-    mi2 = pd.MultiIndex.from_arrays(
+    df = DataFrame({"c": [1, 1, 1, 1, 1, 1]}, index=mi1)
+    mi2 = MultiIndex.from_arrays(
         [["a", "b", "c", "a", "b", "d"], [1, 1, 1, 1, 1, 1]], names=["a", "b"]
     )
-    s = pd.Series([1, 2, 3, 4, 5, 6], index=mi2)
+    s = Series([1, 2, 3, 4, 5, 6], index=mi2)
     res = df.combine_first(pd.DataFrame({"d": s}))
-    mi_expected = pd.MultiIndex.from_arrays(
+    mi_expected = MultiIndex.from_arrays(
         [
             ["a", "a", "a", "b", "b", "b", "b", "c", "c", "d", np.nan],
             [1, 1, 4, 1, 1, 2, 5, 1, 3, 1, 6],
         ],
         names=["a", "b"],
     )
-    expected = pd.DataFrame(
+    expected = DataFrame(
         {
             "c": [np.nan, np.nan, 1, 1, 1, 1, 1, np.nan, 1, np.nan, 1],
             "d": [1.0, 4.0, np.nan, 2.0, 5.0, np.nan, np.nan, 3.0, np.nan, 6.0, np.nan],
