6b50e32f95bf0c2684443b170e42f84681e3f338
Daniel Saxton <dsaxton@pm.me>
2020-10-22 19:55:59 -0500

    CI/CLN: Add more test file linting

--- pandas/tests/reshape/merge/test_join.py
+++ pandas/tests/reshape/merge/test_join.py
@@ -505,7 +505,7 @@ class TestJoin:
 
         # smoke test
         joined = left.join(right, on="key", sort=False)
-        tm.assert_index_equal(joined.index, pd.Index(list(range(4))))
+        tm.assert_index_equal(joined.index, Index(list(range(4))))
 
     def test_join_mixed_non_unique_index(self):
         # GH 12814, unorderable types in py3 with a non-unique index
