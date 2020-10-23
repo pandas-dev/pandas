f41596dda1d1c6af1827d487a33424f9223bebde
Maxim Ivanov <ivanovmg@gmail.com>
2020-10-23 21:57:14 +0700

    CLN: merge two lines

--- pandas/tests/io/formats/test_series_info.py
+++ pandas/tests/io/formats/test_series_info.py
@@ -90,7 +90,7 @@ def test_info_memory():
 
 def test_info_wide():
     s = Series(np.random.randn(101))
-    msg = "Argument `max_cols` can only be passed in DataFrame.info, " "not Series.info"
+    msg = "Argument `max_cols` can only be passed in DataFrame.info, not Series.info"
     with pytest.raises(ValueError, match=msg):
         s.info(max_cols=1)
 
