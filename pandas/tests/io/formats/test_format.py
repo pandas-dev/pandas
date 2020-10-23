1b252d8abe896e4e02f99e5fb3f961fbd2bbaf70
Maxim Ivanov <ivanovmg@gmail.com>
2020-10-23 18:07:14 +0700

    TST: add failing test

--- pandas/tests/io/formats/test_format.py
+++ pandas/tests/io/formats/test_format.py
@@ -2029,6 +2029,31 @@ c  10  11  12  13  14\
         )
         assert str(df) == exp
 
+    @pytest.mark.parametrize(
+        "length, max_rows, min_rows, expected",
+        [
+            (10, 10, 10, 10),
+            (20, 30, 10, 30),
+            (50, 30, 10, 10),
+            (100, 60, 10, 10),
+        ],
+    )
+    def test_max_rows_fitted(self, length, min_rows, max_rows, expected):
+        """Check that display logic is correct.
+
+        GH #37359
+
+        See description here:
+        https://pandas.pydata.org/docs/dev/user_guide/options.html#frequently-used-options
+        """
+        formatter = fmt.DataFrameFormatter(
+            pd.DataFrame(np.random.rand(length, 3)),
+            max_rows=max_rows,
+            min_rows=min_rows,
+        )
+        result = formatter.max_rows_fitted
+        assert result == expected
+
 
 def gen_series_formatting():
     s1 = Series(["a"] * 100)
