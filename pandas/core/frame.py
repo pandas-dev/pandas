7d31a25dc3d9efa101d6a30616fcebf774cedd9b
S.TAKENO <takegue@gmail.com>
2020-10-24 16:31:01 +0900

    DOC: Remove confusing description from `core.DataFrame.iterrows` (#37331)

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -974,9 +974,6 @@ class DataFrame(NDFrame, OpsMixin):
         data : Series
             The data of the row as a Series.
 
-        it : generator
-            A generator that iterates over the rows of the frame.
-
         See Also
         --------
         DataFrame.itertuples : Iterate over DataFrame rows as namedtuples of the values.
