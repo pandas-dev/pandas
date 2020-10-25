50c8c6900b984dba7eff8594279613a445bb5c72
Yury Mikhaylov <44315225+Mikhaylov-yv@users.noreply.github.com>
2020-10-25 21:43:00 +0300

    DOC: add searchsorted examples #36411 (#37370)
    * DOC: add searchsorted examples
    
    * Update base.py
    
    * corrected values
    
    * Update base.py

--- pandas/core/base.py
+++ pandas/core/base.py
@@ -1201,6 +1201,16 @@ class IndexOpsMixin(OpsMixin):
         >>> ser.searchsorted([1, 3], side='right')
         array([1, 3])
 
+        >>> ser = pd.Series(pd.to_datetime(['3/11/2000', '3/12/2000', '3/13/2000']))
+        >>> ser
+        0   2000-03-11
+        1   2000-03-12
+        2   2000-03-13
+        dtype: datetime64[ns]
+
+        >>> ser.searchsorted('3/14/2000')
+        3
+
         >>> ser = pd.Categorical(
         ...     ['apple', 'bread', 'bread', 'cheese', 'milk'], ordered=True
         ... )
