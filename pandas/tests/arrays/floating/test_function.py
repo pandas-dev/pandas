ff1c510436c703d2eb53efbc46b6799bbc1b4dcb
Fangchen Li <fangchen.li@outlook.com>
2020-10-25 18:19:59 -0500

    TST/CI: xfail 32-bit tests #36579, #32709

--- pandas/tests/arrays/floating/test_function.py
+++ pandas/tests/arrays/floating/test_function.py
@@ -73,6 +73,7 @@ def test_ufunc_reduce_raises(values):
         np.add.reduce(a)
 
 
+@pytest.mark.xfail(not IS64, reason="GH 36579: fail on 32-bit system")
 @pytest.mark.parametrize(
     "pandasmethname, kwargs",
     [
@@ -84,15 +85,10 @@ def test_ufunc_reduce_raises(values):
     ],
 )
 def test_stat_method(pandasmethname, kwargs):
-    s = pd.Series(
-        data=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, np.nan, np.nan],
-        dtype="Float64" if IS64 else "Float32",
-    )
+    s = pd.Series(data=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6, np.nan, np.nan], dtype="Float64")
     pandasmeth = getattr(s, pandasmethname)
     result = pandasmeth(**kwargs)
-    s2 = pd.Series(
-        data=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6], dtype="float64" if IS64 else "float32"
-    )
+    s2 = pd.Series(data=[0.1, 0.2, 0.3, 0.4, 0.5, 0.6], dtype="float64")
     pandasmeth = getattr(s2, pandasmethname)
     expected = pandasmeth(**kwargs)
     assert expected == result
