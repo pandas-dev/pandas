f95ea070ec1ba5ab55f0d1f830852ca83e40d661
Brock <jbrockmendel@gmail.com>
2020-10-25 09:25:28 -0700

    TST/REF: collect tests by method

--- pandas/tests/indexes/categorical/test_map.py
+++ pandas/tests/indexes/categorical/test_map.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 
 import pandas as pd
-from pandas import CategoricalIndex, Index
+from pandas import CategoricalIndex, Index, Series
 import pandas._testing as tm
 
 
@@ -93,3 +93,19 @@ class TestMap:
         else:
             expected = Index([False, False, np.nan])
             tm.assert_index_equal(result, expected)
+
+    def test_map_with_dict_or_series(self):
+        orig_values = ["a", "B", 1, "a"]
+        new_values = ["one", 2, 3.0, "one"]
+        cur_index = CategoricalIndex(orig_values, name="XXX")
+        expected = CategoricalIndex(new_values, name="XXX", categories=[3.0, 2, "one"])
+
+        mapper = Series(new_values[:-1], index=orig_values[:-1])
+        result = cur_index.map(mapper)
+        # Order of categories in result can be different
+        tm.assert_index_equal(result, expected)
+
+        mapper = {o: n for o, n in zip(orig_values[:-1], new_values[:-1])}
+        result = cur_index.map(mapper)
+        # Order of categories in result can be different
+        tm.assert_index_equal(result, expected)
