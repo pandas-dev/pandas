b858aace1fbc8e990b8618ce1ea4a6ffc105fc41
Brock <jbrockmendel@gmail.com>
2020-10-26 11:21:52 -0700

    TST/REF: collect indexing tests by method

--- pandas/tests/series/indexing/test_getitem.py
+++ pandas/tests/series/indexing/test_getitem.py
@@ -228,6 +228,22 @@ class TestGetitemBooleanMask:
         tm.assert_series_equal(sel, exp)
 
 
+class TestGetitemCallable:
+    def test_getitem_callable(self):
+        # GH#12533
+        ser = Series(4, index=list("ABCD"))
+        result = ser[lambda x: "A"]
+        assert result == ser.loc["A"]
+
+        result = ser[lambda x: ["A", "B"]]
+        expected = ser.loc[["A", "B"]]
+        tm.assert_series_equal(result, expected)
+
+        result = ser[lambda x: [True, False, True, True]]
+        expected = ser.iloc[[0, 2, 3]]
+        tm.assert_series_equal(result, expected)
+
+
 def test_getitem_generator(string_series):
     gen = (x > 0 for x in string_series)
     result = string_series[gen]
