02ae223eb204ab5ca9a1854b212193381e935bce
Marco Gorelli <m.e.gorelli@gmail.com>
2020-10-26 12:55:49 +0000

    Update pandas/tests/indexing/test_partial.py

--- pandas/tests/indexing/test_partial.py
+++ pandas/tests/indexing/test_partial.py
@@ -685,7 +685,7 @@ class TestPartialSetting:
     def test_slice_irregular_datetime_index_with_nan(self):
         # GH36953
         index = pd.to_datetime(["2012-01-01", "2012-01-02", "2012-01-03", None])
-        df = pd.DataFrame(range(len(index)), index=index)
+        df = DataFrame(range(len(index)), index=index)
         expected = DataFrame(range(len(index[:3])), index=index[:3])
         result = df["2012-01-01":"2012-01-04"]
         tm.assert_frame_equal(result, expected)
