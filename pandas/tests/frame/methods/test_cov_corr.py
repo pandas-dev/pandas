daaaabdb990835760206ad412993e8af9be69532
phofl <patrick_hoefler@gmx.net>
2020-10-27 23:46:44 +0100

    Fix pattern

--- pandas/tests/frame/methods/test_cov_corr.py
+++ pandas/tests/frame/methods/test_cov_corr.py
@@ -213,7 +213,7 @@ class TestDataFrameCorr:
         # GH: 37448
         df = DataFrame(length * [[0.4, 0.1]], columns=["A", "B"])
         result = df.corr()
-        expected = pd.DataFrame(
+        expected = DataFrame(
             {"A": [np.nan, np.nan], "B": [np.nan, np.nan]}, index=["A", "B"]
         )
         tm.assert_frame_equal(result, expected)
