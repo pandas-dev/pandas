cded237a3c77c56b0c1ad9f5a011c5a1ffaf3b55
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2020-10-27 07:30:47 -0700

    Backport PR #37221: Fix regression for is_monotonic_increasing with nan in MultiIndex (#37446)
    Co-authored-by: patrick <61934744+phofl@users.noreply.github.com>

--- pandas/tests/indexes/multi/test_monotonic.py
+++ pandas/tests/indexes/multi/test_monotonic.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 import pandas as pd
 from pandas import Index, MultiIndex
@@ -174,3 +175,14 @@ def test_is_strictly_monotonic_decreasing():
     )
     assert idx.is_monotonic_decreasing is True
     assert idx._is_strictly_monotonic_decreasing is False
+
+
+@pytest.mark.parametrize("attr", ["is_monotonic_increasing", "is_monotonic_decreasing"])
+@pytest.mark.parametrize(
+    "values",
+    [[(np.nan,), (1,), (2,)], [(1,), (np.nan,), (2,)], [(1,), (2,), (np.nan,)]],
+)
+def test_is_monotonic_with_nans(values, attr):
+    # GH: 37220
+    idx = pd.MultiIndex.from_tuples(values, names=["test"])
+    assert getattr(idx, attr) is False
