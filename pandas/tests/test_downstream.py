afe9721b23b03f7d2ae41d1e9370e256b4458ced
Simon Hawkins <simonjayhawkins@gmail.com>
2020-10-27 12:49:44 +0000

    Backport PR #37428: Failing test_missing_required_dependency in pandas-wheels

--- pandas/tests/test_downstream.py
+++ pandas/tests/test_downstream.py
@@ -150,6 +150,18 @@ def test_missing_required_dependency():
     # https://github.com/MacPython/pandas-wheels/pull/50
 
     pyexe = sys.executable.replace("\\", "/")
+
+    # We skip this test if pandas is installed as a site package. We first
+    # import the package normally and check the path to the module before
+    # executing the test which imports pandas with site packages disabled.
+    call = [pyexe, "-c", "import pandas;print(pandas.__file__)"]
+    output = subprocess.check_output(call).decode()
+    if "site-packages" in output:
+        pytest.skip("pandas installed as site package")
+
+    # This test will fail if pandas is installed as a site package. The flags
+    # prevent pandas being imported and the test will report Failed: DID NOT
+    # RAISE <class 'subprocess.CalledProcessError'>
     call = [pyexe, "-sSE", "-c", "import pandas"]
 
     msg = (
