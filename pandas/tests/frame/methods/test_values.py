6faca3591ff54a4ee5017264d0798a4e6faf0e8d
jbrockmendel <jbrockmendel@gmail.com>
2020-10-26 19:43:16 -0700

    TST/REF: collect tests by method (#37434)

--- pandas/tests/frame/methods/test_values.py
+++ pandas/tests/frame/methods/test_values.py
@@ -5,6 +5,35 @@ import pandas._testing as tm
 
 
 class TestDataFrameValues:
+    def test_values(self, float_frame):
+        float_frame.values[:, 0] = 5.0
+        assert (float_frame.values[:, 0] == 5).all()
+
+    def test_more_values(self, float_string_frame):
+        values = float_string_frame.values
+        assert values.shape[1] == len(float_string_frame.columns)
+
+    def test_values_mixed_dtypes(self, float_frame, float_string_frame):
+        frame = float_frame
+        arr = frame.values
+
+        frame_cols = frame.columns
+        for i, row in enumerate(arr):
+            for j, value in enumerate(row):
+                col = frame_cols[j]
+                if np.isnan(value):
+                    assert np.isnan(frame[col][i])
+                else:
+                    assert value == frame[col][i]
+
+        # mixed type
+        arr = float_string_frame[["foo", "A"]].values
+        assert arr[0, 0] == "bar"
+
+        df = DataFrame({"complex": [1j, 2j, 3j], "real": [1, 2, 3]})
+        arr = df.values
+        assert arr[0, 0] == 1j
+
     def test_values_duplicates(self):
         df = DataFrame(
             [[1, 2, "a", "b"], [1, 2, "a", "b"]], columns=["one", "one", "two", "two"]
