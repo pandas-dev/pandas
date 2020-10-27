9a25cc11ef7a74c51a54162f11d32f72b8169e66
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2020-10-27 07:49:08 -0700

    Backport PR #37433 on branch 1.1.x (REGR: fix groupby std() with nullable dtypes) (#37444)
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- pandas/tests/groupby/aggregate/test_cython.py
+++ pandas/tests/groupby/aggregate/test_cython.py
@@ -277,3 +277,27 @@ def test_read_only_buffer_source_agg(agg):
     expected = df.copy().groupby(["species"]).agg({"sepal_length": agg})
 
     tm.assert_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "op_name",
+    ["count", "sum", "std", "var", "sem", "mean", "median", "prod", "min", "max"],
+)
+def test_cython_agg_nullable_int(op_name):
+    # ensure that the cython-based aggregations don't fail for nullable dtype
+    # (eg https://github.com/pandas-dev/pandas/issues/37415)
+    df = DataFrame(
+        {
+            "A": ["A", "B"] * 5,
+            "B": pd.array([1, 2, 3, 4, 5, 6, 7, 8, 9, pd.NA], dtype="Int64"),
+        }
+    )
+    result = getattr(df.groupby("A")["B"], op_name)()
+    df2 = df.assign(B=df["B"].astype("float64"))
+    expected = getattr(df2.groupby("A")["B"], op_name)()
+
+    if op_name != "count":
+        # the result is not yet consistently using Int64/Float64 dtype,
+        # so for now just checking the values by casting to float
+        result = result.astype("float64")
+    tm.assert_series_equal(result, expected)
