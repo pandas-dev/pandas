d265783b84d2a4b644caf7d0a46cde980bc89856
Brock <jbrockmendel@gmail.com>
2020-10-27 19:13:02 -0700

    move TestAsArray to test_conversion

--- pandas/tests/series/test_npfuncs.py
+++ pandas/tests/series/test_npfuncs.py
@@ -3,46 +3,8 @@ Tests for np.foo applied to Series, not necessarily ufuncs.
 """
 
 import numpy as np
-import pytest
 
-from pandas import Series, Timestamp, date_range
-import pandas._testing as tm
-
-
-class TestAsArray:
-    @pytest.mark.parametrize("tz", [None, "US/Central"])
-    def test_asarray_object_dt64(self, tz):
-        ser = Series(date_range("2000", periods=2, tz=tz))
-
-        with tm.assert_produces_warning(None):
-            # Future behavior (for tzaware case) with no warning
-            result = np.asarray(ser, dtype=object)
-
-        expected = np.array(
-            [Timestamp("2000-01-01", tz=tz), Timestamp("2000-01-02", tz=tz)]
-        )
-        tm.assert_numpy_array_equal(result, expected)
-
-    def test_asarray_tz_naive(self):
-        # This shouldn't produce a warning.
-        ser = Series(date_range("2000", periods=2))
-        expected = np.array(["2000-01-01", "2000-01-02"], dtype="M8[ns]")
-        result = np.asarray(ser)
-
-        tm.assert_numpy_array_equal(result, expected)
-
-    def test_asarray_tz_aware(self):
-        tz = "US/Central"
-        ser = Series(date_range("2000", periods=2, tz=tz))
-        expected = np.array(["2000-01-01T06", "2000-01-02T06"], dtype="M8[ns]")
-        result = np.asarray(ser, dtype="datetime64[ns]")
-
-        tm.assert_numpy_array_equal(result, expected)
-
-        # Old behavior with no warning
-        result = np.asarray(ser, dtype="M8[ns]")
-
-        tm.assert_numpy_array_equal(result, expected)
+from pandas import Series
 
 
 class TestPtp:
