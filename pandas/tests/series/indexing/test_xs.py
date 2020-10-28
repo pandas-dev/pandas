46d217469267fe5153ca81cb6d77c00076e58c21
Brock <jbrockmendel@gmail.com>
2020-10-27 17:56:09 -0700

    lint fixup

--- pandas/tests/series/indexing/test_xs.py
+++ pandas/tests/series/indexing/test_xs.py
@@ -1,15 +1,14 @@
 import numpy as np
 
-import pandas as pd
 from pandas import MultiIndex, Series, date_range
 import pandas._testing as tm
 
 
 def test_xs_datetimelike_wrapping():
     # GH#31630 a case where we shouldn't wrap datetime64 in Timestamp
-    arr = pd.date_range("2016-01-01", periods=3)._data._data
+    arr = date_range("2016-01-01", periods=3)._data._data
 
-    ser = pd.Series(arr, dtype=object)
+    ser = Series(arr, dtype=object)
     for i in range(len(ser)):
         ser.iloc[i] = arr[i]
     assert ser.dtype == object
