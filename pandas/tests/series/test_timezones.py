7b9f34aec6389310b32c2386436f04aadd10269d
Brock <jbrockmendel@gmail.com>
2020-10-29 13:50:00 -0700

    TST: fix tzlocalize/tz_convert test asserting wrong thing

--- pandas/tests/series/test_timezones.py
+++ pandas/tests/series/test_timezones.py
@@ -4,12 +4,8 @@ Tests for Series timezone-related methods
 from datetime import datetime
 
 from dateutil.tz import tzoffset
-import numpy as np
-import pytest
 
 from pandas import Series
-import pandas._testing as tm
-from pandas.core.indexes.datetimes import date_range
 
 
 class TestSeriesTimezones:
@@ -26,18 +22,3 @@ class TestSeriesTimezones:
 
         # it works! #2443
         repr(series.index[0])
-
-    @pytest.mark.parametrize("copy", [True, False])
-    @pytest.mark.parametrize(
-        "method, tz", [["tz_localize", None], ["tz_convert", "Europe/Berlin"]]
-    )
-    def test_tz_localize_convert_copy_inplace_mutate(self, copy, method, tz):
-        # GH 6326
-        result = Series(
-            np.arange(0, 5), index=date_range("20131027", periods=5, freq="1H", tz=tz)
-        )
-        getattr(result, method)("UTC", copy=copy)
-        expected = Series(
-            np.arange(0, 5), index=date_range("20131027", periods=5, freq="1H", tz=tz)
-        )
-        tm.assert_series_equal(result, expected)
