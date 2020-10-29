13cf8f9032607ae6176bb6c4610686fac9164cd4
Terji Petersen <contribute@tensortable.com>
2020-10-29 01:17:54 +0000

    PERF: faster dir() calls (#37450)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -5439,12 +5439,10 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
         add the string-like attributes from the info_axis.
         If info_axis is a MultiIndex, it's first level values are used.
         """
-        additions = {
-            c
-            for c in self._info_axis.unique(level=0)[:100]
-            if isinstance(c, str) and c.isidentifier()
-        }
-        return super()._dir_additions().union(additions)
+        additions = super()._dir_additions()
+        if self._info_axis._can_hold_strings:
+            additions.update(self._info_axis._dir_additions_for_owner)
+        return additions
 
     # ----------------------------------------------------------------------
     # Consolidation of internals
