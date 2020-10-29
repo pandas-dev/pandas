13cae2b4267607181b84b7237375a067f87d14fe
jbrockmendel <jbrockmendel@gmail.com>
2020-10-29 14:46:28 -0700

    CI: 32 bit maybe_indices_to_slice (#37473)

--- pandas/_libs/lib.pyx
+++ pandas/_libs/lib.pyx
@@ -36,6 +36,7 @@ from numpy cimport (
     float32_t,
     float64_t,
     int64_t,
+    intp_t,
     ndarray,
     uint8_t,
     uint64_t,
@@ -490,7 +491,7 @@ def has_infs_f8(const float64_t[:] arr) -> bool:
     return False
 
 
-def maybe_indices_to_slice(ndarray[int64_t] indices, int max_len):
+def maybe_indices_to_slice(ndarray[intp_t] indices, int max_len):
     cdef:
         Py_ssize_t i, n = len(indices)
         int k, vstart, vlast, v
