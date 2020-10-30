9e6a1302e03c71b939ff451be7b587262f3340db
arw2019 <andrew.r.wieteska@gmail.com>
2020-10-30 14:13:02 +0000

    BUG: fix merge mistake

--- pandas/tests/groupby/test_grouping.py
+++ pandas/tests/groupby/test_grouping.py
@@ -615,7 +615,7 @@ class TestGrouping:
         [
             (
                 "transform",
-                Series(name=2, dtype=np.float64, index=pd.RangeIndex(0, 0, 1)),
+                Series(name=2, dtype=np.float64, index=pd.Index([])),
             ),
             (
                 "agg",
