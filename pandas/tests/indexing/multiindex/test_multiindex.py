66c30c74fab35f25aa8c6279fc8bf7b4e194cd8a
ssche <sven.schellenberg@paradynsystems.com>
2020-11-01 07:44:07 +1100

    Address review comments * factor out inner functions * relocated test case * simplified try/except

--- pandas/tests/indexing/multiindex/test_multiindex.py
+++ pandas/tests/indexing/multiindex/test_multiindex.py
@@ -83,32 +83,3 @@ class TestMultiIndexBasic:
         df3 = df.copy(deep=True)
         df3.loc[[(dti[0], "a")], "c2"] = 1.0
         tm.assert_frame_equal(df3, expected)
-
-
-def test_combine_first_with_nan_multiindex():
-    # GH#36562
-
-    mi1 = MultiIndex.from_arrays(
-        [["b", "b", "c", "a", "b", np.nan], [1, 2, 3, 4, 5, 6]], names=["a", "b"]
-    )
-    df = DataFrame({"c": [1, 1, 1, 1, 1, 1]}, index=mi1)
-    mi2 = MultiIndex.from_arrays(
-        [["a", "b", "c", "a", "b", "d"], [1, 1, 1, 1, 1, 1]], names=["a", "b"]
-    )
-    s = Series([1, 2, 3, 4, 5, 6], index=mi2)
-    res = df.combine_first(DataFrame({"d": s}))
-    mi_expected = MultiIndex.from_arrays(
-        [
-            ["a", "a", "a", "b", "b", "b", "b", "c", "c", "d", np.nan],
-            [1, 1, 4, 1, 1, 2, 5, 1, 3, 1, 6],
-        ],
-        names=["a", "b"],
-    )
-    expected = DataFrame(
-        {
-            "c": [np.nan, np.nan, 1, 1, 1, 1, 1, np.nan, 1, np.nan, 1],
-            "d": [1.0, 4.0, np.nan, 2.0, 5.0, np.nan, np.nan, 3.0, np.nan, 6.0, np.nan],
-        },
-        index=mi_expected,
-    )
-    tm.assert_frame_equal(res, expected)
