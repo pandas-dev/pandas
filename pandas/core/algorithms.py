d9dcd22a269cbab2313cfc7ffae251a189815a03
ssche <sven.schellenberg@paradynsystems.com>
2020-10-31 17:51:11 +1100

    sort import

--- pandas/core/algorithms.py
+++ pandas/core/algorithms.py
@@ -2075,8 +2075,8 @@ def safe_sort(
         # we need to keep the columns separately as they contain different
         # types and nans (can't use `np.sort` as it may fail when str and nan
         # are mixed in a column as types cannot be compared).
-        from pandas.core.sorting import lexsort_indexer
         from pandas.core.internals.construction import to_arrays
+        from pandas.core.sorting import lexsort_indexer
 
         arrays, _ = to_arrays(values, None)
         indexer = lexsort_indexer(arrays, orders=True)
