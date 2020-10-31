d6dcbe4447e2fe609c2a14ff58d93956c0fa3ea3
jbrockmendel <jbrockmendel@gmail.com>
2020-10-31 08:28:18 -0700

    TST/REF: collect tests from test_api into method-specific files (#37525)

--- pandas/tests/frame/test_block_internals.py
+++ pandas/tests/frame/test_block_internals.py
@@ -254,15 +254,6 @@ class TestDataFrameBlockInternals:
         if not compat.is_platform_windows():
             f("M8[ns]")
 
-    def test_copy(self, float_frame, float_string_frame):
-        cop = float_frame.copy()
-        cop["E"] = cop["A"]
-        assert "E" not in float_frame
-
-        # copy objects
-        copy = float_string_frame.copy()
-        assert copy._mgr is not float_string_frame._mgr
-
     def test_pickle(self, float_string_frame, timezone_frame):
         empty_frame = DataFrame()
 
