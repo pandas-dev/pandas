84729d4eee6df6d28a322916bd0710425a6c6831
Brock <jbrockmendel@gmail.com>
2020-11-01 10:33:01 -0800

    move misplaced arithmetic test

--- pandas/tests/series/test_arithmetic.py
+++ pandas/tests/series/test_arithmetic.py
@@ -8,7 +8,7 @@ import pytz
 from pandas._libs.tslibs import IncompatibleFrequency
 
 import pandas as pd
-from pandas import Categorical, Index, Series, bdate_range, date_range, isna
+from pandas import Categorical, Index, Series, Timedelta, bdate_range, date_range, isna
 import pandas._testing as tm
 from pandas.core import nanops, ops
 
@@ -266,6 +266,25 @@ class TestSeriesArithmetic:
 
         assert ser.index is dti
         assert ser_utc.index is dti_utc
+
+    def test_arithmetic_with_duplicate_index(self):
+
+        # GH#8363
+        # integer ops with a non-unique index
+        index = [2, 2, 3, 3, 4]
+        ser = Series(np.arange(1, 6, dtype="int64"), index=index)
+        other = Series(np.arange(5, dtype="int64"), index=index)
+        result = ser - other
+        expected = Series(1, index=[2, 2, 3, 3, 4])
+        tm.assert_series_equal(result, expected)
+
+        # GH#8363
+        # datetime ops with a non-unique index
+        ser = Series(date_range("20130101 09:00:00", periods=5), index=index)
+        other = Series(date_range("20130101", periods=5), index=index)
+        result = ser - other
+        expected = Series(Timedelta("9 hours"), index=[2, 2, 3, 3, 4])
+        tm.assert_series_equal(result, expected)
 
 
 # ------------------------------------------------------------------
