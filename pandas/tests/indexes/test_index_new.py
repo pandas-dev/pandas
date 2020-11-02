c08372d96530cae89dc94813a20c086864e0278a
jbrockmendel <jbrockmendel@gmail.com>
2020-11-01 16:28:18 -0800

    TST: avoid/suppress DeprecationWarnings (#37570)

--- pandas/tests/indexes/test_index_new.py
+++ pandas/tests/indexes/test_index_new.py
@@ -90,16 +90,25 @@ class TestIndexConstructorInference:
         data = [ctor]
         data.insert(pos, nulls_fixture)
 
+        warn = None
         if nulls_fixture is NA:
             expected = Index([NA, NaT])
             mark = pytest.mark.xfail(reason="Broken with np.NaT ctor; see GH 31884")
             request.node.add_marker(mark)
+            # GH#35942 numpy will emit a DeprecationWarning within the
+            #  assert_index_equal calls.  Since we can't do anything
+            #  about it until GH#31884 is fixed, we suppress that warning.
+            warn = DeprecationWarning
 
         result = Index(data)
-        tm.assert_index_equal(result, expected)
+
+        with tm.assert_produces_warning(warn):
+            tm.assert_index_equal(result, expected)
 
         result = Index(np.array(data, dtype=object))
-        tm.assert_index_equal(result, expected)
+
+        with tm.assert_produces_warning(warn):
+            tm.assert_index_equal(result, expected)
 
     @pytest.mark.parametrize("swap_objs", [True, False])
     def test_constructor_mixed_nat_objs_infers_object(self, swap_objs):
