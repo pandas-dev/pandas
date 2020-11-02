462b21dda2ce5ac93eeea14c403d8285e31b9e75
patrick <61934744+phofl@users.noreply.github.com>
2020-11-02 01:42:21 +0100

    Fix bug in combine first with string dtype and NA only in one level of index (#37568)

--- pandas/tests/frame/methods/test_combine_first.py
+++ pandas/tests/frame/methods/test_combine_first.py
@@ -353,3 +353,15 @@ class TestDataFrameCombineFirst:
         exp = DataFrame({"isBool": [True], "isNum": [val]})
 
         tm.assert_frame_equal(res, exp)
+
+    def test_combine_first_string_dtype_only_na(self):
+        # GH: 37519
+        df = DataFrame({"a": ["962", "85"], "b": [pd.NA] * 2}, dtype="string")
+        df2 = DataFrame({"a": ["85"], "b": [pd.NA]}, dtype="string")
+        df.set_index(["a", "b"], inplace=True)
+        df2.set_index(["a", "b"], inplace=True)
+        result = df.combine_first(df2)
+        expected = DataFrame(
+            {"a": ["962", "85"], "b": [pd.NA] * 2}, dtype="string"
+        ).set_index(["a", "b"])
+        tm.assert_frame_equal(result, expected)
