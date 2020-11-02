4af8ae9f3b9b46f45d935ca8e981d2d9b30042be
jbrockmendel <jbrockmendel@gmail.com>
2020-11-01 16:23:34 -0800

    TST/REF: collect base tests by method (#37549)

--- pandas/tests/series/test_arithmetic.py
+++ pandas/tests/series/test_arithmetic.py
@@ -7,8 +7,18 @@ import pytz
 
 from pandas._libs.tslibs import IncompatibleFrequency
 
+from pandas.core.dtypes.common import is_datetime64_dtype, is_datetime64tz_dtype
+
 import pandas as pd
-from pandas import Categorical, Index, Series, bdate_range, date_range, isna
+from pandas import (
+    Categorical,
+    Index,
+    IntervalIndex,
+    Series,
+    bdate_range,
+    date_range,
+    isna,
+)
 import pandas._testing as tm
 from pandas.core import nanops, ops
 
@@ -779,3 +789,51 @@ class TestNamePreservation:
     def test_scalarop_preserve_name(self, datetime_series):
         result = datetime_series * 2
         assert result.name == datetime_series.name
+
+
+def test_none_comparison(series_with_simple_index):
+    series = series_with_simple_index
+    if isinstance(series.index, IntervalIndex):
+        # IntervalIndex breaks on "series[0] = np.nan" below
+        pytest.skip("IntervalIndex doesn't support assignment")
+    if len(series) < 1:
+        pytest.skip("Test doesn't make sense on empty data")
+
+    # bug brought up by #1079
+    # changed from TypeError in 0.17.0
+    series[0] = np.nan
+
+    # noinspection PyComparisonWithNone
+    result = series == None  # noqa
+    assert not result.iat[0]
+    assert not result.iat[1]
+
+    # noinspection PyComparisonWithNone
+    result = series != None  # noqa
+    assert result.iat[0]
+    assert result.iat[1]
+
+    result = None == series  # noqa
+    assert not result.iat[0]
+    assert not result.iat[1]
+
+    result = None != series  # noqa
+    assert result.iat[0]
+    assert result.iat[1]
+
+    if is_datetime64_dtype(series.dtype) or is_datetime64tz_dtype(series.dtype):
+        # Following DatetimeIndex (and Timestamp) convention,
+        # inequality comparisons with Series[datetime64] raise
+        msg = "Invalid comparison"
+        with pytest.raises(TypeError, match=msg):
+            None > series
+        with pytest.raises(TypeError, match=msg):
+            series > None
+    else:
+        result = None > series
+        assert not result.iat[0]
+        assert not result.iat[1]
+
+        result = series < None
+        assert not result.iat[0]
+        assert not result.iat[1]
