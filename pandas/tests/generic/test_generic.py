93ed6fb049318f1cbef70be656b60017bf380ba2
Erfan Nariman <erfan.nariman@briccanalytics.nl>
2020-11-03 13:35:03 +0100

    added test for depr

--- pandas/tests/generic/test_generic.py
+++ pandas/tests/generic/test_generic.py
@@ -561,3 +561,10 @@ class TestNDFrame:
         assert s.flags is s.flags
         s2 = s.copy()
         assert s2.flags is not s.flags
+
+    def test_slice_shift_deprecated(self):
+        # GH 37601
+        df = pd.DataFrame({'A': [1, 2, 3, 4]})
+
+        with tm.assert_produces_warning(FutureWarning):
+            df['A'].slice_shift()
