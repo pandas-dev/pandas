4966d9444a61a953d644859d1866244dac633065
jbrockmendel <jbrockmendel@gmail.com>
2020-11-03 17:50:29 -0800

    TST/REF: tests.generic (#37618)

--- pandas/tests/frame/methods/test_equals.py
+++ pandas/tests/frame/methods/test_equals.py
@@ -1,4 +1,6 @@
-from pandas import DataFrame
+import numpy as np
+
+from pandas import DataFrame, date_range
 import pandas._testing as tm
 
 
@@ -21,3 +23,56 @@ class TestEquals:
         tm.assert_frame_equal(df0, df1)
         assert df0.equals(df1)
         assert df1.equals(df0)
+
+    def test_equals(self):
+        # Add object dtype column with nans
+        index = np.random.random(10)
+        df1 = DataFrame(np.random.random(10), index=index, columns=["floats"])
+        df1["text"] = "the sky is so blue. we could use more chocolate.".split()
+        df1["start"] = date_range("2000-1-1", periods=10, freq="T")
+        df1["end"] = date_range("2000-1-1", periods=10, freq="D")
+        df1["diff"] = df1["end"] - df1["start"]
+        df1["bool"] = np.arange(10) % 3 == 0
+        df1.loc[::2] = np.nan
+        df2 = df1.copy()
+        assert df1["text"].equals(df2["text"])
+        assert df1["start"].equals(df2["start"])
+        assert df1["end"].equals(df2["end"])
+        assert df1["diff"].equals(df2["diff"])
+        assert df1["bool"].equals(df2["bool"])
+        assert df1.equals(df2)
+        assert not df1.equals(object)
+
+        # different dtype
+        different = df1.copy()
+        different["floats"] = different["floats"].astype("float32")
+        assert not df1.equals(different)
+
+        # different index
+        different_index = -index
+        different = df2.set_index(different_index)
+        assert not df1.equals(different)
+
+        # different columns
+        different = df2.copy()
+        different.columns = df2.columns[::-1]
+        assert not df1.equals(different)
+
+        # DatetimeIndex
+        index = date_range("2000-1-1", periods=10, freq="T")
+        df1 = df1.set_index(index)
+        df2 = df1.copy()
+        assert df1.equals(df2)
+
+        # MultiIndex
+        df3 = df1.set_index(["text"], append=True)
+        df2 = df1.set_index(["text"], append=True)
+        assert df3.equals(df2)
+
+        df2 = df1.set_index(["floats"], append=True)
+        assert not df3.equals(df2)
+
+        # NaN in index
+        df3 = df1.set_index(["floats"], append=True)
+        df2 = df1.set_index(["floats"], append=True)
+        assert df3.equals(df2)
--- pandas/tests/generic/methods/test_pipe.py
+++ pandas/tests/generic/methods/test_pipe.py
@@ -5,11 +5,10 @@ import pandas._testing as tm
 
 
 class TestPipe:
-    @pytest.mark.parametrize("klass", [Series, DataFrame])
-    def test_pipe(self, klass):
+    def test_pipe(self, frame_or_series):
         obj = DataFrame({"A": [1, 2, 3]})
         expected = DataFrame({"A": [1, 4, 9]})
-        if klass is Series:
+        if frame_or_series is Series:
             obj = obj["A"]
             expected = expected["A"]
 
@@ -17,20 +16,18 @@ class TestPipe:
         result = obj.pipe(f, 2)
         tm.assert_equal(result, expected)
 
-    @pytest.mark.parametrize("klass", [Series, DataFrame])
-    def test_pipe_tuple(self, klass):
+    def test_pipe_tuple(self, frame_or_series):
         obj = DataFrame({"A": [1, 2, 3]})
-        if klass is Series:
+        if frame_or_series is Series:
             obj = obj["A"]
 
         f = lambda x, y: y
         result = obj.pipe((f, "y"), 0)
         tm.assert_equal(result, obj)
 
-    @pytest.mark.parametrize("klass", [Series, DataFrame])
-    def test_pipe_tuple_error(self, klass):
+    def test_pipe_tuple_error(self, frame_or_series):
         obj = DataFrame({"A": [1, 2, 3]})
-        if klass is Series:
+        if frame_or_series is Series:
             obj = obj["A"]
 
         f = lambda x, y: y
