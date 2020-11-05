26c419e3a9650b4d0cd4c9a5b1b084d0146b1046
Marco Gorelli <m.e.gorelli@gmail.com>
2020-11-05 07:53:29 +0000

    keep asi8.min() inside try-except

--- pandas/core/indexes/datetimelike.py
+++ pandas/core/indexes/datetimelike.py
@@ -241,17 +241,17 @@ class DatetimeIndexOpsMixin(NDArrayBackedExtensionIndex):
         # quick check
         if self.is_monotonic and i8[0] != iNaT:
             return self._data._box_func(i8[0])
-        if self.hasnans:
-            if skipna:
+        try:
+            if self.hasnans and skipna:
                 min_stamp = self[~self._isnan].asi8.min()
+            elif not self.hasnans:
+                min_stamp = i8.min()
             else:
                 return self._na_value
-        else:
-            min_stamp = i8.min()
-        try:
-            return self._data._box_func(min_stamp)
         except ValueError:
             return self._na_value
+        else:
+            return self._data._box_func(min_stamp)
 
     def argmin(self, axis=None, skipna=True, *args, **kwargs):
         """
