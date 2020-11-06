ada4d94a615c4a9eea131065edc470a6e7d6763e
Alexander Kirko <alexander.kirko@gmail.com>
2020-11-06 10:15:38 +0300

    TST: tzinfo instead of tz in the test

--- pandas/tests/scalar/timestamp/test_unary_ops.py
+++ pandas/tests/scalar/timestamp/test_unary_ops.py
@@ -431,7 +431,7 @@ def test_replace_preserves_fold(fold):
     # GH 37610. Check that replace preserves Timestamp fold property
     tz = gettz("Europe/Moscow")
 
-    ts = Timestamp(year=2009, month=10, day=25, hour=2, minute=30, fold=fold, tz=tz)
+    ts = Timestamp(year=2009, month=10, day=25, hour=2, minute=30, fold=fold, tzinfo=tz)
     ts_replaced = ts.replace(second=1)
 
     assert ts_replaced.fold == fold
