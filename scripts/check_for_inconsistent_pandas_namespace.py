e2aa1d67d471cba47ddff8d750db2eda00cd0d5c
Marco Gorelli <m.e.gorelli@gmail.com>
2020-11-06 08:06:53 +0000

    check for inconsistent namespace usage

---
+++ scripts/check_for_inconsistent_pandas_namespace.py
@@ -0,0 +1,32 @@
+import argparse
+import re
+
+PATTERN = r"(?<!pd\.)(?<!\w){class_name}\(.*pd\.{class_name}\("
+CLASS_NAMES = (
+    "Series",
+    "DataFrame",
+    "Index",
+    "MultiIndex",
+    "Timestamp",
+    "Timedelta",
+    "TimedeltaIndex",
+    "DatetimeIndex",
+    "Categorical",
+)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("paths", nargs="*")
+    args = parser.parse_args()
+
+    for class_name in CLASS_NAMES:
+        pattern = re.compile(
+            PATTERN.format(class_name=class_name).encode(), re.MULTILINE | re.DOTALL
+        )
+        for path in args.paths:
+            with open(path, "rb") as f:
+                contents = f.read()
+            match = pattern.search(contents)
+            assert (
+                match is None
+            ), f"Found both `pd.{class_name}` and `{class_name}` in {path}"
