cb81aa565e67d8405a8d4ea60f90421ef4ddbc23
Marco Gorelli <m.e.gorelli@gmail.com>
2020-11-07 08:37:35 +0000

    don't use subprocess

--- scripts/tests/test_inconsistent_namespace_check.py
+++ scripts/tests/test_inconsistent_namespace_check.py
@@ -1,7 +1,8 @@
 from pathlib import Path
-import subprocess
 
 import pytest
+
+from scripts.check_for_inconsistent_pandas_namespace import main
 
 BAD_FILE_0 = "cat_0 = Categorical()\ncat_1 = pd.Categorical()"
 BAD_FILE_1 = "cat_0 = pd.Categorical()\ncat_1 = Categorical()"
@@ -14,20 +15,9 @@ def test_inconsistent_usage(tmpdir, content: str) -> None:
     tmpfile = Path(tmpdir / "tmpfile.py")
     tmpfile.touch()
     tmpfile.write_text(content)
-    output = subprocess.run(
-        ["python", "scripts/check_for_inconsistent_pandas_namespace.py", str(tmpfile)],
-        stderr=subprocess.PIPE,
-    )
-
-    # check stderr
-    result = output.stderr.decode()
-    expected = "Found both `pd.Categorical` and `Categorical` in"
-    assert expected in result
-
-    # check return code
-    result = output.returncode
-    expected = 1
-    assert result == expected
+    msg = fr"Found both `pd\.Categorical` and `Categorical` in {str(tmpfile)}"
+    with pytest.raises(AssertionError, match=msg):
+        main((str(tmpfile),))
 
 
 @pytest.mark.parametrize("content", [GOOD_FILE_0, GOOD_FILE_1])
@@ -35,17 +25,4 @@ def test_consistent_usage(tmpdir, content: str) -> None:
     tmpfile = Path(tmpdir / "tmpfile.py")
     tmpfile.touch()
     tmpfile.write_text(content)
-    output = subprocess.run(
-        ["python", "scripts/check_for_inconsistent_pandas_namespace.py", str(tmpfile)],
-        stderr=subprocess.PIPE,
-    )
-
-    # check stderr
-    result = output.stderr.decode()
-    expected = ""
-    assert expected == result
-
-    # check return code
-    result = output.returncode
-    expected = 0
-    assert result == expected
+    main((str(tmpfile),))  # Should not raise.
