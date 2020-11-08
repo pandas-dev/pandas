f5a163563796372397b71b0c32c25b9e010b5b15
Andrew Wieteska <andrew.r.wieteska@gmail.com>
2020-11-08 07:56:17 +0000

    REF (feedback): _set_result_index_ordered

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -716,28 +716,21 @@ class BaseGroupBy(PandasObject, SelectionMixin, Generic[FrameOrSeries]):
         # set the result index on the passed values object and
         # return the new object, xref 8046
 
-        # the values/counts are repeated according to the group index
-        # shortcut if we have an already ordered grouper
         if self.grouper.is_monotonic:
+            # shortcut if we have an already ordered grouper
             result.set_axis(self.obj._get_axis(self.axis), axis=self.axis, inplace=True)
-            return result
+        else:
+            # row order is scrambled => sort the rows by position in original index
+            original_positions = Index(
+                np.concatenate(self._get_indices(self.grouper.result_index))
+            )
+            result.set_axis(original_positions, axis=self.axis, inplace=True)
+            result = result.sort_index(axis=self.axis)
 
-        # row order is scrambled => sort the rows by position in original index
-        original_positions = Index(
-            np.concatenate(self._get_indices(self.grouper.result_index))
-        )
-        result.set_axis(original_positions, axis=self.axis, inplace=True)
-        result = result.sort_index(axis=self.axis)
-
-        rows_dropped = len(result.index) < len(self._selected_obj)
-
-        # get index by slicing original index according to original positions
-        # slice drops attrs => use set_axis when no rows were dropped
-        if rows_dropped:
+            # get index by slicing original index according to original positions
+            # slice drops attrs => use set_axis when no rows were dropped
             sorted_indexer = result.index
             result.index = self._selected_obj.index[sorted_indexer]
-        else:
-            result.set_axis(self.obj._get_axis(self.axis), axis=self.axis, inplace=True)
 
         return result
 
