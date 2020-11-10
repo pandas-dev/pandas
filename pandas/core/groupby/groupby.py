a789b6a0b7f7b4fc0d19c32015a7d35c6e99cda2
Andrew Wieteska <andrew.r.wieteska@gmail.com>
2020-11-10 07:41:20 +0000

    REF: restore casing on rows_dropped

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -719,18 +719,24 @@ class BaseGroupBy(PandasObject, SelectionMixin, Generic[FrameOrSeries]):
         if self.grouper.is_monotonic:
             # shortcut if we have an already ordered grouper
             result.set_axis(self.obj._get_axis(self.axis), axis=self.axis, inplace=True)
-        else:
-            # row order is scrambled => sort the rows by position in original index
-            original_positions = Index(
-                np.concatenate(self._get_indices(self.grouper.result_index))
-            )
-            result.set_axis(original_positions, axis=self.axis, inplace=True)
-            result = result.sort_index(axis=self.axis)
+            return result
 
+        # row order is scrambled => sort the rows by position in original index
+        original_positions = Index(
+            np.concatenate(self._get_indices(self.grouper.result_index))
+        )
+        result.set_axis(original_positions, axis=self.axis, inplace=True)
+        result = result.sort_index(axis=self.axis)
+
+        dropped_rows = len(result.index) < len(self.obj.index)
+
+        if dropped_rows:
             # get index by slicing original index according to original positions
             # slice drops attrs => use set_axis when no rows were dropped
             sorted_indexer = result.index
             result.index = self._selected_obj.index[sorted_indexer]
+        else:
+            result.set_axis(self.obj._get_axis(self.axis), axis=self.axis, inplace=True)
 
         return result
 
