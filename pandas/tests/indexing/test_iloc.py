c55cd626cecad577c30e265d16543758e5365c37
phofl <patrick_hoefler@gmx.net>
2020-11-10 20:40:06 +0100

    Add test

--- pandas/tests/indexing/test_iloc.py
+++ pandas/tests/indexing/test_iloc.py
@@ -768,6 +768,13 @@ class TestiLoc2:
         expected = Series([1]).astype(CategoricalDtype([1, 2, 3]))
         tm.assert_series_equal(result, expected)
 
+    def test_iloc_assign_series_to_df_cell(self):
+        # GH 37593
+        df = DataFrame(columns=["a"], index=[0])
+        df.iloc[0, 0] = Series([1, 2, 3])
+        expected = DataFrame({"a": [Series([1, 2, 3])]}, columns=["a"], index=[0])
+        tm.assert_frame_equal(df, expected)
+
 
 class TestILocSetItemDuplicateColumns:
     def test_iloc_setitem_scalar_duplicate_columns(self):
