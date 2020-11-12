bcf61e6e62aae95ea790b31f2124e6dc63a50686
Sam Cohen <samc1213@gmail.com>
2020-11-12 01:18:55 +0000

    Add back series regex test

--- pandas/tests/series/methods/test_replace.py
+++ pandas/tests/series/methods/test_replace.py
@@ -442,6 +442,14 @@ class TestSeriesReplace:
         ser = pd.Series(pd.array([1, 2, 3], dtype="Int64"))
         ser.replace("", "")  # no exception
 
+    def test_replace_with_compiled_regex(self):
+        # https://github.com/pandas-dev/pandas/issues/35680
+        s = pd.Series(["a", "b", "c"])
+        regex = re.compile("^a$")
+        result = s.replace({regex: "z"}, regex=True)
+        expected = pd.Series(["z", "b", "c"])
+        tm.assert_series_equal(result, expected)
+
     @pytest.mark.parametrize("pattern", ["^.$", "."])
     def test_str_replace_regex_default_raises_warning(self, pattern):
         # https://github.com/pandas-dev/pandas/pull/24809
