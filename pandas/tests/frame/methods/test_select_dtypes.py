810bbab51536e056df34ec2310f8814729fb8e4b
Oleh Kozynets <ok7mailbox@gmail.com>
2020-11-12 20:42:30 +0100

    Add a separate unit test.

--- pandas/tests/frame/methods/test_select_dtypes.py
+++ pandas/tests/frame/methods/test_select_dtypes.py
@@ -82,16 +82,33 @@ class TestSelectDtypes:
         e = df[["b", "c", "e"]]
         tm.assert_frame_equal(r, e)
 
-        exclude = (np.datetime64,)
-        include = np.bool_, "int"
-        r = df.select_dtypes(include=include, exclude=exclude)
-        e = df[["b", "e"]]
-        tm.assert_frame_equal(r, e)
-
         exclude = ("datetime",)
         include = "bool", "int64", "int32"
         r = df.select_dtypes(include=include, exclude=exclude)
         e = df[["b", "e"]]
+        tm.assert_frame_equal(r, e)
+
+    def test_select_dtypes_exclude_include_int(self):
+        df = DataFrame(
+            {
+                "a": list("abc"),
+                "b": list(range(1, 4)),
+                "c": np.arange(3, 6).astype("int32"),
+                "d": np.arange(4.0, 7.0, dtype="float64"),
+                "e": [True, False, True],
+                "f": pd.date_range("now", periods=3).values,
+            }
+        )
+        exclude = (np.datetime64,)
+        include = np.bool_, "int"
+        r = df.select_dtypes(include=include, exclude=exclude)
+        e = df[["b", "c", "e"]]
+        tm.assert_frame_equal(r, e)
+
+        exclude = ("datetime",)
+        include = "bool", int
+        r = df.select_dtypes(include=include, exclude=exclude)
+        e = df[["b", "c", "e"]]
         tm.assert_frame_equal(r, e)
 
     def test_select_dtypes_include_using_scalars(self):
