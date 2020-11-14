74688649de86d09b0547d6ebb0db14acd1a6f969
Simon Hawkins <simonjayhawkins@gmail.com>
2020-11-14 10:30:19 +0000

    Backport PR #37801 on branch 1.1.x: REGR: SeriesGroupBy where index has a tuple name fails (#37829)
    Co-authored-by: Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -2055,3 +2055,13 @@ def test_groups_repr_truncates(max_seq_items, expected):
 
         result = df.groupby(np.array(df.a)).groups.__repr__()
         assert result == expected
+
+
+def test_groupby_series_with_tuple_name():
+    # GH 37755
+    ser = Series([1, 2, 3, 4], index=[1, 1, 2, 2], name=("a", "a"))
+    ser.index.name = ("b", "b")
+    result = ser.groupby(level=0).last()
+    expected = Series([2, 4], index=[1, 2], name=("a", "a"))
+    expected.index.name = ("b", "b")
+    tm.assert_series_equal(result, expected)
