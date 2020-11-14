65cb6fc128d038ce442923d9473f9bbf2f41e337
Margret `Pax` Williams <pax.margret@gmail.com>
2020-11-14 20:26:30 +0000

    CLN: Parametrize mask constructor instead of mask values (#37840)

--- pandas/tests/series/indexing/test_setitem.py
+++ pandas/tests/series/indexing/test_setitem.py
@@ -126,17 +126,11 @@ class TestSetitemBooleanMask:
 
         tm.assert_series_equal(copy, expected)
 
-    @pytest.mark.parametrize(
-        "mask",
-        [
-            [True, False, True],
-            np.array([True, False, True]),
-            Series([True, False, True]),
-        ],
-    )
-    def test_setitem_boolean_python_list(self, mask):
+    @pytest.mark.parametrize("func", [list, np.array, Series])
+    def test_setitem_boolean_python_list(self, func):
         # GH19406
         ser = Series([None, "b", None])
+        mask = func([True, False, True])
         ser[mask] = ["a", "c"]
         expected = Series(["a", "b", "c"])
         tm.assert_series_equal(ser, expected)
