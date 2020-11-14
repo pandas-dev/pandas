0fc43f6c64075e53739e877b77cc1d763274d916
rhshadrach <rhshadrach@gmail.com>
2020-11-13 23:51:10 -0500

    int32 fix

--- pandas/tests/groupby/transform/test_transform.py
+++ pandas/tests/groupby/transform/test_transform.py
@@ -170,7 +170,7 @@ def test_transform_axis_1(transformation_func):
 
     if transformation_func == "diff":
         # Result contains nans, so transpose coerces to float
-        expected["b"] = expected["b"].astype(np.intp)
+        expected["b"] = expected["b"].astype("int64")
 
     # cumcount returns Series; the rest are DataFrame
     tm.assert_equal(result, expected)
