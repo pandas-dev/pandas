2dc5744da803b53ea7933616792185a04531bc1f
rhshadrach <rhshadrach@gmail.com>
2020-11-13 20:47:34 -0500

    s -> ser in test

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -2150,9 +2150,9 @@ def test_groupby_duplicate_columns():
 
 def test_groupby_series_with_tuple_name():
     # GH 37755
-    s = Series([1, 2, 3, 4], index=[1, 1, 2, 2], name=("a", "a"))
-    s.index.name = ("b", "b")
-    result = s.groupby(level=0).last()
+    ser = Series([1, 2, 3, 4], index=[1, 1, 2, 2], name=("a", "a"))
+    ser.index.name = ("b", "b")
+    result = ser.groupby(level=0).last()
     expected = Series([2, 4], index=[1, 2], name=("a", "a"))
     expected.index.name = ("b", "b")
     tm.assert_series_equal(result, expected)
