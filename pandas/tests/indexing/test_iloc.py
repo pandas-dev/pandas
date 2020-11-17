3bef35a999534dff26a6ed8fb33a2275c831708a
phofl <patrick_hoefler@gmx.net>
2020-11-17 23:25:21 +0100

    Add test

--- pandas/tests/indexing/test_iloc.py
+++ pandas/tests/indexing/test_iloc.py
@@ -810,18 +810,19 @@ class TestiLoc2:
 
     @pytest.mark.parametrize("klass", [list, np.array])
     def test_iloc_setitem_bool_indexer(self, klass):
-        # GH: 36741
+        # GH#36741
         df = DataFrame({"flag": ["x", "y", "z"], "value": [1, 3, 4]})
         indexer = klass([True, False, False])
         df.iloc[indexer, 1] = df.iloc[indexer, 1] * 2
         expected = DataFrame({"flag": ["x", "y", "z"], "value": [2, 3, 4]})
         tm.assert_frame_equal(df, expected)
 
-    def test_setitem_iloc_pure_position_based(self):
-        # GH: 22046
+    @pytest.mark.parametrize("indexer", [[1], slice(1, 2)])
+    def test_setitem_iloc_pure_position_based(self, indexer):
+        # GH#22046
         df1 = DataFrame({"a2": [11, 12, 13], "b2": [14, 15, 16]})
         df2 = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]})
-        df2.iloc[:, [1]] = df1.iloc[:, [0]]
+        df2.iloc[:, indexer] = df1.iloc[:, [0]]
         expected = DataFrame({"a": [1, 2, 3], "b": [11, 12, 13], "c": [7, 8, 9]})
         tm.assert_frame_equal(df2, expected)
 
@@ -992,7 +993,7 @@ class TestILocSeries:
         assert ser.iloc[2] == 2
 
     def test_setitem_iloc_pure_position_based(self):
-        # GH: 22046
+        # GH#22046
         ser1 = Series([1, 2, 3])
         ser2 = Series([4, 5, 6], index=[1, 0, 2])
         ser1.iloc[1:3] = ser2.iloc[1:3]
