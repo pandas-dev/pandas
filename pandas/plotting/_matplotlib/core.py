876278896a6469324a11d72b5b12670f5b1a3bcf
leo <leonidrusnac4@gmail.com>
2020-11-18 19:02:12 +0100

    remove trailing whitespace

--- pandas/plotting/_matplotlib/core.py
+++ pandas/plotting/_matplotlib/core.py
@@ -1340,7 +1340,7 @@ class AreaPlot(LinePlot):
 
         is_shared_y = len(list(ax.get_shared_y_axes())) > 0
         # do not override the default axis behaviour in case of shared y axes
-        if self.ylim is None and not is_shared_y: 
+        if self.ylim is None and not is_shared_y:
             if (data >= 0).all().all():
                 ax.set_ylim(0, None)
             elif (data <= 0).all().all():
