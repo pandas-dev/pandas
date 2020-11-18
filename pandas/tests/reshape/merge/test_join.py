d7c1156971d90249d2e48225915e98767860f7ea
phofl <patrick_hoefler@gmx.net>
2020-11-19 00:28:36 +0100

    Move import

--- pandas/tests/reshape/merge/test_join.py
+++ pandas/tests/reshape/merge/test_join.py
@@ -1,10 +1,11 @@
 import numpy as np
 import pytest
 
+from pandas.errors import MergeError
+
 import pandas as pd
 from pandas import DataFrame, Index, MultiIndex, Series, concat, merge
 import pandas._testing as tm
-from pandas.errors import MergeError
 from pandas.tests.reshape.merge.test_merge import NGROUPS, N, get_test_data
 
 a_ = np.array
