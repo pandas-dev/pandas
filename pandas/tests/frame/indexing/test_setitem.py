c8868b28d8e9241babdcac3c7a11c49425dd43c7
patrick <61934744+phofl@users.noreply.github.com>
2020-11-19 01:22:06 +0100

    Bug in iloc aligned objects (#37728)

--- pandas/tests/frame/indexing/test_setitem.py
+++ pandas/tests/frame/indexing/test_setitem.py
@@ -289,15 +289,6 @@ class TestDataFrameSetItem:
         assert isinstance(rs.index, PeriodIndex)
         tm.assert_index_equal(rs.index, rng)
 
-    @pytest.mark.parametrize("klass", [list, np.array])
-    def test_iloc_setitem_bool_indexer(self, klass):
-        # GH: 36741
-        df = DataFrame({"flag": ["x", "y", "z"], "value": [1, 3, 4]})
-        indexer = klass([True, False, False])
-        df.iloc[indexer, 1] = df.iloc[indexer, 1] * 2
-        expected = DataFrame({"flag": ["x", "y", "z"], "value": [2, 3, 4]})
-        tm.assert_frame_equal(df, expected)
-
 
 class TestDataFrameSetItemSlicing:
     def test_setitem_slice_position(self):
