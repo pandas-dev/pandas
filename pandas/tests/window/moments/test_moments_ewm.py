914be70e6e7e989e8029c32501ce0152736dc3b4
Matthew Roeschke <emailformattr@gmail.com>
2020-11-19 07:40:38 -0800

    CLN: test_ewm_rolling_consistency.py (#37951)

--- pandas/tests/window/moments/test_moments_ewm.py
+++ pandas/tests/window/moments/test_moments_ewm.py
@@ -226,8 +226,12 @@ def test_ewma_halflife_arg(series):
         series.ewm()
 
 
-def test_ewm_alpha(arr):
+def test_ewm_alpha():
     # GH 10789
+    arr = np.random.randn(100)
+    locs = np.arange(20, 40)
+    arr[locs] = np.NaN
+
     s = Series(arr)
     a = s.ewm(alpha=0.61722699889169674).mean()
     b = s.ewm(com=0.62014947789973052).mean()
@@ -254,8 +258,12 @@ def test_ewm_alpha_arg(series):
         s.ewm(halflife=10.0, alpha=0.5)
 
 
-def test_ewm_domain_checks(arr):
+def test_ewm_domain_checks():
     # GH 12492
+    arr = np.random.randn(100)
+    locs = np.arange(20, 40)
+    arr[locs] = np.NaN
+
     s = Series(arr)
     msg = "comass must satisfy: comass >= 0"
     with pytest.raises(ValueError, match=msg):
