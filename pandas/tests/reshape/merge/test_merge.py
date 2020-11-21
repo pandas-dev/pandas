0ff78fc89fc41ad1b2e2e57ecc8b047ce421b0b2
phofl <patrick_hoefler@gmx.net>
2020-11-21 02:49:01 +0100

    Check unmodified inputs

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -2346,9 +2346,13 @@ def test_merge_cross(input_col, output_cols):
     # GH#5401
     left = DataFrame({"a": [1, 3]})
     right = DataFrame({input_col: [3, 4]})
+    left_copy = left.copy()
+    right_copy = right.copy()
     result = merge(left, right, how="cross")
     expected = DataFrame({output_cols[0]: [1, 1, 3, 3], output_cols[1]: [3, 4, 3, 4]})
     tm.assert_frame_equal(result, expected)
+    tm.assert_frame_equal(left, left_copy)
+    tm.assert_frame_equal(right, right_copy)
 
 
 @pytest.mark.parametrize(
