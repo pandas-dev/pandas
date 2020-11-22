383b936aabfc59d0bb93c70acfde517d18398eb2
arw2019 <andrew.r.wieteska@gmail.com>
2020-11-22 04:46:04 +0000

    feedback: move test to test_arithmetic

--- pandas/tests/series/test_arithmetic.py
+++ pandas/tests/series/test_arithmetic.py
@@ -832,6 +832,40 @@ class TestNamePreservation:
         assert result.name == datetime_series.name
 
 
+class TestInplaceOperations:
+    @pytest.mark.parametrize(
+        "dtype1, dtype2, dtype_expected, dtype_mul",
+        (
+            ["Int64"] * 4,
+            ["float"] * 4,
+            ["Int64"] + ["float"] * 3,
+            pytest.param(
+                "Int64",
+                "Float64",
+                "Float64",
+                "Float64",
+                marks=pytest.mark.xfail(reason="Not implemented yet"),
+            ),
+        ),
+    )
+    def test_series_inplace_ops(self, dtype1, dtype2, dtype_expected, dtype_mul):
+        # GH 37910
+
+        ser1 = Series([1], dtype=dtype1)
+        ser2 = Series([2], dtype=dtype2)
+        ser1 += ser2
+        expected = Series([3], dtype=dtype_expected)
+        tm.assert_series_equal(ser1, expected)
+
+        ser1 -= ser2
+        expected = Series([1], dtype=dtype_expected)
+        tm.assert_series_equal(ser1, expected)
+
+        ser1 *= ser2
+        expected = Series([2], dtype=dtype_mul)
+        tm.assert_series_equal(ser1, expected)
+
+
 def test_none_comparison(series_with_simple_index):
     series = series_with_simple_index
     if isinstance(series.index, IntervalIndex):
