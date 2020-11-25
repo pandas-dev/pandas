63164691e6dff33881f8579c8943ff5dd6d8eebd
phofl <patrick_hoefler@gmx.net>
2020-11-25 21:08:12 +0100

    Remove not

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -5508,7 +5508,7 @@ class Index(IndexOpsMixin, PandasObject):
         """
         arr_dtype = "object" if self.dtype == "object" else None
         labels = com.index_labels_to_array(labels, dtype=arr_dtype)
-        if not self.is_unique:
+        if self.is_unique:
             indexer = self.get_indexer(labels)
         else:
             indexer, _ = self.get_indexer_non_unique(labels)
