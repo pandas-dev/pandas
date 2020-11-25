8c9659d5721f1eb3032f0ac06973b4f672023ffd
phofl <patrick_hoefler@gmx.net>
2020-11-25 21:38:43 +0100

    Catch warning

--- pandas/tests/indexes/multi/test_drop.py
+++ pandas/tests/indexes/multi/test_drop.py
@@ -1,3 +1,5 @@
+import warnings
+
 import numpy as np
 import pytest
 
@@ -152,6 +154,8 @@ def test_drop_with_nan_in_index(nulls_fixture):
 def test_drop_with_non_monotonic_duplicates():
     # GH#33494
     mi = MultiIndex.from_tuples([(1, 2), (2, 3), (1, 2)])
-    result = mi.drop((1, 2))
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore", PerformanceWarning)
+        result = mi.drop((1, 2))
     expected = MultiIndex.from_tuples([(2, 3)])
     tm.assert_index_equal(result, expected)
