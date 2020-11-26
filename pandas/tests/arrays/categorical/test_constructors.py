bfd05717deec710093c2ddccb1ba6f8344bec5cb
jbrockmendel <jbrockmendel@gmail.com>
2020-11-26 14:24:31 -0800

    ENH: preserve RangeIndex in factorize (#38034)
    * ENH: preserve RangeIndex in factorize
    
    * dedoc
    
    * ensure np.intp
    
    * 32bit compat
    
    * if->elif

--- pandas/tests/arrays/categorical/test_constructors.py
+++ pandas/tests/arrays/categorical/test_constructors.py
@@ -290,6 +290,16 @@ class TestCategoricalConstructors:
         cat = Categorical([0, 1, 2], categories=range(3))
         tm.assert_categorical_equal(cat, exp)
 
+    def test_constructor_with_rangeindex(self):
+        # RangeIndex is preserved in Categories
+        rng = Index(range(3))
+
+        cat = Categorical(rng)
+        tm.assert_index_equal(cat.categories, rng, exact=True)
+
+        cat = Categorical([1, 2, 0], categories=rng)
+        tm.assert_index_equal(cat.categories, rng, exact=True)
+
     @pytest.mark.parametrize(
         "dtl",
         [
