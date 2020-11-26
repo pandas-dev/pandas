bbe864ab37d5d05248ebefef5bf36bec16c50586
GYHHAHA <1801214626@qq.com>
2020-11-26 16:47:42 +0800

    add an edge case

--- pandas/tests/indexes/multi/test_drop.py
+++ pandas/tests/indexes/multi/test_drop.py
@@ -161,6 +161,8 @@ def test_single_level_drop():
     msg = r"labels \[nan\] not found in level"
     with pytest.raises(KeyError, match=msg):
         mi.drop([np.nan], level=0)
+    with pytest.raises(KeyError, match=msg):
+        mi.drop([np.nan, 1, 2, 3], level=0)
 
     mi = MultiIndex.from_tuples([(np.nan, 1), (1, 2)])
     msg = r"labels \['a'\] not found in level"
