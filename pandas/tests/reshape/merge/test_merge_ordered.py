6551c188f60e3c3e3afc831e3341ce3134c000d5
GYHHAHA <1801214626@qq.com>
2020-11-26 23:04:30 +0800

    merge_ordered fails with list-like left_by

--- pandas/tests/reshape/merge/test_merge_ordered.py
+++ pandas/tests/reshape/merge/test_merge_ordered.py
@@ -115,3 +115,33 @@ class TestMergeOrdered:
         )
 
         tm.assert_frame_equal(result, expected)
+
+    def test_list_type_by(self):
+        # GH 35269
+        left = DataFrame(
+            {
+                "G": ["g", "g"],
+                "H": ["h", "h"],
+                "T": [1, 3]
+            }
+        )
+
+        right = DataFrame(
+            {
+                "T": [2],
+                "E": [1]
+            }
+        )
+
+        result = merge_ordered(left, right, on=["T"], left_by=["G", "H"])
+
+        expected = DataFrame(
+            {
+                "G": ["g", "g", "g"],
+                "H": ["h", "h", "h"],
+                "T": [1, 2, 3],
+                "E": [np.nan, 1.0, np.nan]
+            }
+        )
+
+        tm.assert_frame_equal(result, expected)
