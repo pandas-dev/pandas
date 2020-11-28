00295baed3d8769b52d49fa6ff8da6fdabfc869a
GYHHAHA <1801214626@qq.com>
2020-11-28 20:00:36 +0800

    Update test_sorting.py

--- pandas/tests/indexes/multi/test_sorting.py
+++ pandas/tests/indexes/multi/test_sorting.py
@@ -288,14 +288,11 @@ def test_remove_unused_levels_with_missing():
     df1.index = df1.index.set_levels(levels=new_levels, level="L1")
     df1.index = df1.index.set_levels(levels=new_levels, level="L2")
 
-    result = df1.unstack("L3")[("x", 1)].sort_index()
-    expected_index = (
-        Index(
-            [("n1", "n3"), ("n2", np.nan), ("n3", "n1"), (np.nan, "n2")],
-            names=["L1", "L2"],
-        )
-        .set_levels(levels=new_levels, level="L1")
-        .set_levels(levels=new_levels, level="L2")
+    result = df1.unstack("L3")[("x", 1)].sort_index().index
+    expected = MultiIndex(
+        levels=[["n1", "n2", "n3", None], ["n1", "n2", "n3", None]],
+        codes=[[0, 1, 2, 3], [2, 3, 0, 1]],
+        names=["L1", "L2"]
     )
 
-    tm.assert_index_equal(result.index, expected_index)
+    tm.assert_index_equal(result.index, expected)
