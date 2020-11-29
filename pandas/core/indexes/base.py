e99e5ab32c4e831e7bbac0346189f4d6d86a6225
patrick <61934744+phofl@users.noreply.github.com>
2020-11-29 18:21:52 +0100

    BUG: Fix duplicates in intersection of multiindexes (#36927)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -2822,7 +2822,7 @@ class Index(IndexOpsMixin, PandasObject):
         self._assert_can_do_setop(other)
         other = ensure_index(other)
 
-        if self.equals(other):
+        if self.equals(other) and not self.has_duplicates:
             return self._get_reconciled_name_object(other)
 
         if not is_dtype_equal(self.dtype, other.dtype):
@@ -2847,7 +2847,7 @@ class Index(IndexOpsMixin, PandasObject):
             except TypeError:
                 pass
             else:
-                return result
+                return algos.unique1d(result)
 
         try:
             indexer = Index(rvals).get_indexer(lvals)
@@ -2858,10 +2858,13 @@ class Index(IndexOpsMixin, PandasObject):
             indexer = algos.unique1d(Index(rvals).get_indexer_non_unique(lvals)[0])
             indexer = indexer[indexer != -1]
 
-        result = other.take(indexer)._values
+        result = other.take(indexer).unique()._values
 
         if sort is None:
             result = algos.safe_sort(result)
+
+        # Intersection has to be unique
+        assert algos.unique(result).shape == result.shape
 
         return result
 
