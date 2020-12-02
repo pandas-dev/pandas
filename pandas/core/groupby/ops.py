d80d74d315c5e209283f9281d727668b768e6778
Brock <jbrockmendel@gmail.com>
2020-12-01 20:19:28 -0800

    simplify

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -627,12 +627,8 @@ class BaseGrouper:
         if is_datetimelike and kind == "aggregate":
             result = result.astype(orig_values.dtype)
 
-        if kind == "transform":
-            filled_series = self.size().fillna(0)
-            if filled_series.gt(0).any() and how not in base.cython_cast_blocklist:
-                dtype = maybe_cast_result_dtype(orig_values.dtype, how)
-                result = maybe_downcast_to_dtype(result, dtype)
-        else:
+        if how not in base.cython_cast_blocklist:
+            # "rank" is the only member of cython_cast_blocklist we get here
             dtype = maybe_cast_result_dtype(orig_values.dtype, how)
             result = maybe_downcast_to_dtype(result, dtype)
 
