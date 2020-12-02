7c3337ae84c03fb9090927127ebd9c11d4d59fb7
Mark Graham <mark.graham93@btinternet.com>
2020-12-02 10:49:04 +0000

    match message using the generate_error_message method

--- pandas/tests/extension/base/reduce.py
+++ pandas/tests/extension/base/reduce.py
@@ -2,10 +2,52 @@ import warnings
 
 import pytest
 
+import re
+
 import pandas as pd
 import pandas._testing as tm
 
 from .base import BaseExtensionTests
+
+
+def generate_error_message(dtype, op_name):
+    """
+    Generate an error message returned by the following
+
+        getattr(s, op_name)(skipna=skipna)
+
+    where `s` is an object with datatype `dtype`, `op_name` is an operation
+    to be performed on `s`, and `skipna` is a boolean flag.
+
+    Parameters
+    ----------
+    dtype : str
+        Datatype of the object `s`
+    op_name : str
+        Name of operation to perform on `s`
+
+    Returns
+    -------
+    str
+        Error message caused by performing the operation
+    """
+    if dtype == "category":
+        if op_name in ["min", "max"]:
+            return re.escape(
+                f"Categorical is not ordered for operation {op_name}\n"
+                "you can use .as_ordered() to change the Categorical to an "
+                "ordered one\n"
+            )
+        else:
+            return f"'Categorical' does not implement reduction '{op_name}'"
+    elif dtype in ["string", "arrow_string"]:
+        return f"Cannot perform reduction '{op_name}' with string dtype"
+    elif dtype == "interval":
+        return rf"cannot perform {op_name} with type interval\[float64\]"
+    elif dtype == "json":
+        return f"cannot perform {op_name} with type json"
+    elif dtype == "arrow_bool":
+        return ""
 
 
 class BaseReduceTests(BaseExtensionTests):
@@ -28,7 +70,9 @@ class BaseNoReduceTests(BaseReduceTests):
         op_name = all_numeric_reductions
         s = pd.Series(data)
 
-        with pytest.raises(TypeError):
+        msg = generate_error_message(s.dtype.name, op_name)
+
+        with pytest.raises(TypeError, match=msg):
             getattr(s, op_name)(skipna=skipna)
 
     @pytest.mark.parametrize("skipna", [True, False])
@@ -36,7 +80,9 @@ class BaseNoReduceTests(BaseReduceTests):
         op_name = all_boolean_reductions
         s = pd.Series(data)
 
-        with pytest.raises(TypeError):
+        msg = generate_error_message(s.dtype.name, op_name)
+
+        with pytest.raises(TypeError, match=msg):
             getattr(s, op_name)(skipna=skipna)
 
 
