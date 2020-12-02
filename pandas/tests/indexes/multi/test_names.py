dd49505b233c6ad367008539c76c43e1a60fda6e
phofl <patrick_hoefler@gmx.net>
2020-12-02 23:30:06 +0100

    Add tests

--- pandas/tests/indexes/multi/test_names.py
+++ pandas/tests/indexes/multi/test_names.py
@@ -170,6 +170,24 @@ def test_name_mi_with_dict_like(func, rename_dict, exp_names):
     tm.assert_index_equal(result, expected)
 
 
+@pytest.mark.parametrize("func", ["rename", "set_names"])
+@pytest.mark.parametrize(
+    "rename_dict, exp_names",
+    [
+        ({"x": "z"}, ["z", "y"]),
+        ({"x": "z", "y": "x"}, ["z", "x"]),
+        ({"a": "z"}, ["x", "y"]),
+        ({}, ["x", "y"]),
+    ],
+)
+def test_name_mi_with_dict_like(func, rename_dict, exp_names):
+    # GH#20421
+    mi = MultiIndex.from_arrays([[1, 2], [3, 4]], names=["x", "y"])
+    result = getattr(mi, func)(rename_dict)
+    expected = MultiIndex.from_arrays([[1, 2], [3, 4]], names=exp_names)
+    tm.assert_index_equal(result, expected)
+
+
 def test_name_with_dict_like_raising():
     # GH#20421
     ix = pd.Index([1, 2])
