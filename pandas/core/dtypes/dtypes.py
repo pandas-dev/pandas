1c7cbba663f2ad181fb39fc6f40eba19efb15564
jbrockmendel <jbrockmendel@gmail.com>
2020-12-03 07:55:03 -0800

    BUG: CategoricalDtype.__repr__ with RangeIndex categories (#38245)
    * BUG: CategoricalDtype.__repr__ with RangeIndex categories
    
    * fix broken tests
    
    * fixup

--- pandas/core/dtypes/dtypes.py
+++ pandas/core/dtypes/dtypes.py
@@ -399,10 +399,14 @@ class CategoricalDtype(PandasExtensionDtype, ExtensionDtype):
 
     def __repr__(self) -> str_type:
         if self.categories is None:
-            data = "None, "
+            data = "None"
         else:
             data = self.categories._format_data(name=type(self).__name__)
-        return f"CategoricalDtype(categories={data}ordered={self.ordered})"
+            if data is None:
+                # self.categories is RangeIndex
+                data = str(self.categories._range)
+            data = data.rstrip(", ")
+        return f"CategoricalDtype(categories={data}, ordered={self.ordered})"
 
     @staticmethod
     def _hash_categories(categories, ordered: Ordered = True) -> int:
