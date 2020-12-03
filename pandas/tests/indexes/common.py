de4a8dc30509021f4f4ef056c3e54cfb586dbc17
ZhihuiChen0903 <57449793+ZhihuiChen0903@users.noreply.github.com>
2020-12-03 19:31:18 +0800

    Update common.py

--- pandas/tests/indexes/common.py
+++ pandas/tests/indexes/common.py
@@ -73,8 +73,8 @@ class Base:
 
         # GH8083 test the base class for shift
         idx = self.create_index()
-        msg = f"This method is only implemented for DatetimeIndex, "
-        "PeriodIndex and TimedeltaIndex; Got type {type(idx).__name__}"
+        msg = f"This method is only implemented for DatetimeIndex, PeriodIndex and "
+              f"TimedeltaIndex; Got type {type(idx).__name__}"
         with pytest.raises(NotImplementedError, match=msg):
             idx.shift(1)
         with pytest.raises(NotImplementedError, match=msg):
