108a8ad290632ae7ffd3f1c26928340aa8264f31
ZhihuiChen0903 <57449793+ZhihuiChen0903@users.noreply.github.com>
2020-12-03 19:28:36 +0800

    Update base.py

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -4735,9 +4735,8 @@ class Index(IndexOpsMixin, PandasObject):
                        '2012-03-01'],
                       dtype='datetime64[ns]', freq='MS')
         """
-        raise NotImplementedError(
-            f"This method is only implemented for DatetimeIndex, PeriodIndex and "
-            f"TimedeltaIndex; Got type {type(self).__name__}")
+        raise NotImplementedError(f"This method is only implemented for DatetimeIndex, PeriodIndex and "
+                                  f"TimedeltaIndex; Got type {type(self).__name__}")
 
     def argsort(self, *args, **kwargs) -> np.ndarray:
         """
