efd0e919c0fcc00c44d24720ec38b7ab1dae5d62
Brock <jbrockmendel@gmail.com>
2020-12-04 18:29:32 -0800

    docstring

--- pandas/tests/frame/apply/test_frame_transform.py
+++ pandas/tests/frame/apply/test_frame_transform.py
@@ -18,6 +18,10 @@ frame_kernels = [
 
 
 def unpack_obj(obj, klass, axis):
+    """
+    Helper to ensure we have the right type of object for a test parametrized
+    over frame_or_series.
+    """
     if klass is not DataFrame:
         obj = obj["A"]
         if axis != 0:
