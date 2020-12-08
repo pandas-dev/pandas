32bebdbcd2c8d68ad6dc9dab7cab2149c4861883
JoseNavy <72418396+JoseNavy@users.noreply.github.com>
2020-12-08 14:31:50 -0700

    TST: add message matches to pytest.raises in various tests GH30999 (#38350)

--- pandas/tests/io/test_clipboard.py
+++ pandas/tests/io/test_clipboard.py
@@ -238,10 +238,11 @@ class TestClipboard:
         tm.assert_frame_equal(res, exp)
 
     def test_invalid_encoding(self, df):
+        msg = "clipboard only supports utf-8 encoding"
         # test case for testing invalid encoding
-        with pytest.raises(ValueError):
+        with pytest.raises(ValueError, match=msg):
             df.to_clipboard(encoding="ascii")
-        with pytest.raises(NotImplementedError):
+        with pytest.raises(NotImplementedError, match=msg):
             pd.read_clipboard(encoding="ascii")
 
     @pytest.mark.parametrize("enc", ["UTF-8", "utf-8", "utf8"])
