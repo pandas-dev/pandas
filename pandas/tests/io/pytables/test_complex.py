9992ce2ca514a7776299000f18c6308b3eaa5805
JoseNavy <72418396+JoseNavy@users.noreply.github.com>
2020-12-08 14:31:50 -0700

    TST: add message matches to pytest.raises in various tests GH30999 (#38350)

--- pandas/tests/io/pytables/test_complex.py
+++ pandas/tests/io/pytables/test_complex.py
@@ -149,8 +149,17 @@ def test_complex_indexing_error(setup_path):
         {"A": [1, 2, 3, 4], "B": ["a", "b", "c", "d"], "C": complex128},
         index=list("abcd"),
     )
+
+    msg = (
+        "Columns containing complex values can be stored "
+        "but cannot be indexed when using table format. "
+        "Either use fixed format, set index=False, "
+        "or do not include the columns containing complex "
+        "values to data_columns when initializing the table."
+    )
+
     with ensure_clean_store(setup_path) as store:
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError, match=msg):
             store.append("df", df, data_columns=["C"])
 
 
@@ -158,8 +167,16 @@ def test_complex_series_error(setup_path):
     complex128 = np.array([1.0 + 1.0j, 1.0 + 1.0j, 1.0 + 1.0j, 1.0 + 1.0j])
     s = Series(complex128, index=list("abcd"))
 
+    msg = (
+        "Columns containing complex values can be stored "
+        "but cannot be indexed when using table format. "
+        "Either use fixed format, set index=False, "
+        "or do not include the columns containing complex "
+        "values to data_columns when initializing the table."
+    )
+
     with ensure_clean_path(setup_path) as path:
-        with pytest.raises(TypeError):
+        with pytest.raises(TypeError, match=msg):
             s.to_hdf(path, "obj", format="t")
 
     with ensure_clean_path(setup_path) as path:
