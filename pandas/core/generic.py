1da3f6e3da9efa5cfacf342575804d769ab4b675
phofl <patrick_hoefler@gmx.net>
2020-12-12 01:57:42 +0100

    Add gh refernce

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -8427,6 +8427,7 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
 
         offset = to_offset(offset)
         if not isinstance(offset, Tick) and offset.is_on_offset(self.index[0]):
+            # GH#29623 if first value is end of period
             end_date = end = self.index[0]
         else:
             end_date = end = self.index[0] + offset
