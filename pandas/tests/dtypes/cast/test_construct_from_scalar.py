cc258727b081eecf6e07aae079d312464027d692
jbrockmendel <jbrockmendel@gmail.com>
2020-12-12 15:23:54 -0800

    BUG: Series/DataFrame construction from scalars (#38405)

--- pandas/tests/dtypes/cast/test_construct_from_scalar.py
+++ pandas/tests/dtypes/cast/test_construct_from_scalar.py
@@ -1,7 +1,9 @@
+import numpy as np
+
 from pandas.core.dtypes.cast import construct_1d_arraylike_from_scalar
 from pandas.core.dtypes.dtypes import CategoricalDtype
 
-from pandas import Categorical
+from pandas import Categorical, Timedelta, Timestamp
 import pandas._testing as tm
 
 
@@ -16,3 +18,17 @@ def test_cast_1d_array_like_from_scalar_categorical():
 
     result = construct_1d_arraylike_from_scalar("a", len(expected), cat_type)
     tm.assert_categorical_equal(result, expected)
+
+
+def test_cast_1d_array_like_from_timestamp():
+    # check we dont lose nanoseconds
+    ts = Timestamp.now() + Timedelta(1)
+    res = construct_1d_arraylike_from_scalar(ts, 2, np.dtype("M8[ns]"))
+    assert res[0] == ts
+
+
+def test_cast_1d_array_like_from_timedelta():
+    # check we dont lose nanoseconds
+    td = Timedelta(1)
+    res = construct_1d_arraylike_from_scalar(td, 2, np.dtype("m8[ns]"))
+    assert res[0] == td
