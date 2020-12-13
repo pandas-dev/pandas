3c8e082312777b2e4137ddf463005bb761a4629b
patrick <61934744+phofl@users.noreply.github.com>
2020-12-14 00:44:22 +0100

    BUG: read_csv interpreting NA value as comment when NA contains comment string (#38392)
    * BUG: read_csv not converting to float for python engine with decimal sep, usecols and parse_dates
    
    * Fix comment issues for python parser
    
    * Add test
    
    * Add whatsnew
    
    * Revert "BUG: read_csv not converting to float for python engine with decimal sep, usecols and parse_dates"
    
    This reverts commit 8c2e1caa
    
    * Commit merge conflict
    
    * Improve test
    
    * Remove import
    
    * Add c tests
    
    * Remove function input
    
    * Improve note
    
    Co-authored-by: Jeff Reback <jeff@reback.net>

--- pandas/io/parsers.py
+++ pandas/io/parsers.py
@@ -2983,7 +2983,11 @@ class PythonParser(ParserBase):
         for line in lines:
             rl = []
             for x in line:
-                if not isinstance(x, str) or self.comment not in x:
+                if (
+                    not isinstance(x, str)
+                    or self.comment not in x
+                    or x in self.na_values
+                ):
                     rl.append(x)
                 else:
                     x = x[: x.find(self.comment)]
