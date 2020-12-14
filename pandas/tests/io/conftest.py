307ba15b9267e3f21af9a1e40a4ad991405190ff
Matt Roeschke <mroeschke@housecanary.com>
2020-12-14 15:53:04 -0800

    CI: Supress moto server logs in tests

--- pandas/tests/io/conftest.py
+++ pandas/tests/io/conftest.py
@@ -1,4 +1,3 @@
-import logging
 import os
 import shlex
 import subprocess
@@ -50,8 +49,6 @@ def s3_base(worker_id):
     pytest.importorskip("s3fs")
     pytest.importorskip("boto3")
     requests = pytest.importorskip("requests")
-    # GH 38090: Suppress http logs in tests by moto_server
-    logging.getLogger("werkzeug").disabled = True
 
     with tm.ensure_safe_environment_variables():
         # temporary workaround as moto fails for botocore >= 1.11 otherwise,
@@ -71,7 +68,9 @@ def s3_base(worker_id):
 
         # pipe to null to avoid logging in terminal
         proc = subprocess.Popen(
-            shlex.split(f"moto_server s3 -p {endpoint_port}"), stdout=subprocess.DEVNULL
+            shlex.split(f"moto_server s3 -p {endpoint_port}"),
+            stdout=subprocess.DEVNULL,
+            stderr=subprocess.DEVNULL,
         )
 
         timeout = 5
