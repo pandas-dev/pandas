3c07dcc6b6336e6c75ed4d8f4f72ef27030357a4
Brock <jbrockmendel@gmail.com>
2020-12-15 15:45:08 -0800

    test with periods=2, remove whatsnew

--- pandas/tests/frame/methods/test_shift.py
+++ pandas/tests/frame/methods/test_shift.py
@@ -295,9 +295,22 @@ class TestDataFrameShift:
 
     def test_shift_axis1_categorical_columns(self):
         # GH#38434
-        ci = CategoricalIndex(["a", "b"])
-        df = DataFrame([[1, 2], [3, 4]], index=ci, columns=ci)
+        ci = CategoricalIndex(["a", "b", "c"])
+        df = DataFrame(
+            {"a": [1, 3], "b": [2, 4], "c": [5, 6]}, index=ci[:-1], columns=ci
+        )
         result = df.shift(axis=1)
 
-        expected = DataFrame({"a": [np.nan, np.nan], "b": [1, 3]}, index=ci, columns=ci)
+        expected = DataFrame(
+            {"a": [np.nan, np.nan], "b": [1, 3], "c": [2, 4]}, index=ci[:-1], columns=ci
+        )
+        tm.assert_frame_equal(result, expected)
+
+        # periods != 1
+        result = df.shift(2, axis=1)
+        expected = DataFrame(
+            {"a": [np.nan, np.nan], "b": [np.nan, np.nan], "c": [1, 3]},
+            index=ci[:-1],
+            columns=ci,
+        )
         tm.assert_frame_equal(result, expected)
