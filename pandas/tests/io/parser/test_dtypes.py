384c114fd6935001cfa43b4eee0f1870a8b32b03
phofl <patrick_hoefler@gmx.net>
2020-12-15 21:32:27 +0100

    Move test

--- pandas/tests/io/parser/test_dtypes.py
+++ pandas/tests/io/parser/test_dtypes.py
@@ -603,3 +603,19 @@ def test_boolean_dtype(all_parsers):
     )
 
     tm.assert_frame_equal(result, expected)
+
+
+def test_delimiter_with_usecols_and_parse_dates(all_parsers):
+    # GH#35873
+    result = all_parsers.read_csv(
+        StringIO('"dump","-9,1","-9,1",20101010'),
+        engine="python",
+        names=["col", "col1", "col2", "col3"],
+        usecols=["col1", "col2", "col3"],
+        parse_dates=["col3"],
+        decimal=",",
+    )
+    expected = DataFrame(
+        {"col1": [-9.1], "col2": [-9.1], "col3": [Timestamp("2010-10-10")]}
+    )
+    tm.assert_frame_equal(result, expected)
