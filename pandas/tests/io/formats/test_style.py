8bfffc6dd27007415efbb8279762a7f0b73d0091
JoseNavy <josenavs22@gmail.com>
2020-12-16 17:22:33 -0700

    TST: Add test Styler.render() in rowspan attribute

--- pandas/tests/io/formats/test_style.py
+++ pandas/tests/io/formats/test_style.py
@@ -1740,6 +1740,11 @@ class TestStyler:
         s = Styler(df, uuid="_", cell_ids=False)
         assert '<th class="col_heading level0 col0" colspan="2">l0</th>' in s.render()
 
+    def test_rowspan_w3(self):
+        df = DataFrame(data=[[1, 2]], index=[["l0", "l0"], ["l1a", "l1b"]])
+        s = Styler(df, uuid="_", cell_ids=False)
+        assert '<th id="T___level0_row0" class="row_heading level0 row0" rowspan="2">l0</th>' in s.render()
+
     @pytest.mark.parametrize("len_", [1, 5, 32, 33, 100])
     def test_uuid_len(self, len_):
         # GH 36345
