4c2ba8d9578672f27e5c633df9b254c65e1a9f16
aflah02 <72096386+aflah02@users.noreply.github.com>
2020-12-17 11:28:31 +0530

    Update generic.py
    Fixed PEP 8  Issues

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -8442,7 +8442,7 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
         When having a DataFrame with dates as index, this function can
         select the last few rows based on a date offset.
 
-        Note that for the last() method to work, the DatetimeIndex 
+        Note that for the last() method to work, the DatetimeIndex
         must be sorted otherwise it will not work as intended.
 
         Parameters
