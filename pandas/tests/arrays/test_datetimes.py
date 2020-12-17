baeacad24a417fbf880f11fac578f9cb5216711d
jbrockmendel <jbrockmendel@gmail.com>
2020-12-17 05:52:43 -0800

    ENH: support 2D in DatetimeArray._from_sequence (#38021)

--- pandas/tests/arrays/test_datetimes.py
+++ pandas/tests/arrays/test_datetimes.py
@@ -465,6 +465,24 @@ class TestSequenceToDT64NS:
         result, _, _ = sequence_to_dt64ns(arr, dtype=DatetimeTZDtype(tz="US/Central"))
         tm.assert_numpy_array_equal(arr._data, result)
 
+    @pytest.mark.parametrize("order", ["F", "C"])
+    def test_2d(self, order):
+        dti = pd.date_range("2016-01-01", periods=6, tz="US/Pacific")
+        arr = np.array(dti, dtype=object).reshape(3, 2)
+        if order == "F":
+            arr = arr.T
+
+        res = sequence_to_dt64ns(arr)
+        expected = sequence_to_dt64ns(arr.ravel())
+
+        tm.assert_numpy_array_equal(res[0].ravel(), expected[0])
+        assert res[1] == expected[1]
+        assert res[2] == expected[2]
+
+        res = DatetimeArray._from_sequence(arr)
+        expected = DatetimeArray._from_sequence(arr.ravel()).reshape(arr.shape)
+        tm.assert_datetime_array_equal(res, expected)
+
 
 class TestReductions:
     @pytest.fixture
