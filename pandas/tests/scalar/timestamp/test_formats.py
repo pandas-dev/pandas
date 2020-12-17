972ce2bb33d5d8c9bccae39e7d0bc5a129d6de89
Marius <marius.mather@sydney.edu.au>
2020-12-18 09:41:02 +1100

    Reformat with black

--- pandas/tests/scalar/timestamp/test_formats.py
+++ pandas/tests/scalar/timestamp/test_formats.py
@@ -4,9 +4,8 @@ from pandas import Timestamp
 
 
 def test_isoformat():
-    ts = Timestamp(year=2019, month=5, day=18, hour=15, minute=17,
-                   second=8, microsecond=132263)
-    assert ts.isoformat() == '2019-05-18T15:17:08.132263'
-    assert ts.isoformat(timespec='seconds') == '2019-05-18T15:17:08'
-
-
+    ts = Timestamp(
+        year=2019, month=5, day=18, hour=15, minute=17, second=8, microsecond=132263
+    )
+    assert ts.isoformat() == "2019-05-18T15:17:08.132263"
+    assert ts.isoformat(timespec="seconds") == "2019-05-18T15:17:08"
