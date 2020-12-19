a4efa9f63665a644d6f0ac512b1bc2e3d6fb04ac
patrick <61934744+phofl@users.noreply.github.com>
2020-12-19 03:14:08 +0100

    BUG: first("2M") returning incorrect results (#38446)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -8424,7 +8424,12 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
             return self
 
         offset = to_offset(offset)
-        end_date = end = self.index[0] + offset
+        if not isinstance(offset, Tick) and offset.is_on_offset(self.index[0]):
+            # GH#29623 if first value is end of period, remove offset with n = 1
+            #  before adding the real offset
+            end_date = end = self.index[0] - offset.base + offset
+        else:
+            end_date = end = self.index[0] + offset
 
         # Tick-like, e.g. 3 weeks
         if isinstance(offset, Tick):
