6c5a74f367fe72d606fed37f259770f15d65aa25
Mohammad Jafar Mashhadi <MJafarMashhadi@users.noreply.github.com>
2020-12-20 14:37:32 -0700

    TST: Bare pytest raises (#38576)
    * test_assert_series_equal.py
    
    * test_setitem_slice_mismatch_length_raises
    
    * test_setitem_scalar_key_sequence_raise
    
    * test_sub
    
    * test_arithmetic
    
    * black pandas on test_arithmetic.py
    
    * black pandas on setitem.py
    
    * BaseComparisonOpsTests
    
    * extension/base/ops.py
    
    * Break too long lines
    
    * The numpy error was an external one
    
    * Update RegExs to match more tests
    
    * Added new error messages
    
    * Removed redundant parenthesis in regexps

--- pandas/tests/arrays/boolean/test_arithmetic.py
+++ pandas/tests/arrays/boolean/test_arithmetic.py
@@ -46,7 +46,7 @@ def test_add_mul(left_array, right_array, opname, exp):
 
 
 def test_sub(left_array, right_array):
-    with pytest.raises(TypeError):
+    with tm.external_error_raised(TypeError):
         # numpy points to ^ operator or logical_xor function instead
         left_array - right_array
 
@@ -92,13 +92,29 @@ def test_error_invalid_values(data, all_arithmetic_operators):
     ops = getattr(s, op)
 
     # invalid scalars
-    with pytest.raises(TypeError):
+    msg = (
+        "ufunc '\\w+' did not contain a loop with signature matching types|"
+        "ufunc '\\w+' not supported for the input types, and the inputs could "
+        "not be safely coerced to any supported types|"
+        "\\w+ cannot perform the operation \\w+"
+    )
+    with pytest.raises(TypeError, match=msg):
         ops("foo")
-    with pytest.raises(TypeError):
+
+    msg = (
+        "unsupported operand type\\(s\\) for|"
+        "Concatenation operation is not implemented for NumPy arrays"
+    )
+    with pytest.raises(TypeError, match=msg):
         ops(pd.Timestamp("20180101"))
 
     # invalid array-likes
     if op not in ("__mul__", "__rmul__"):
         # TODO(extension) numpy's mul with object array sees booleans as numbers
-        with pytest.raises(TypeError):
+        msg = (
+            "unsupported operand type\\(s\\) for|"
+            'can only concatenate str \\(not "bool"\\) to str|'
+            "not all arguments converted during string formatting"
+        )
+        with pytest.raises(TypeError, match=msg):
             ops(pd.Series("foo", index=s.index))
