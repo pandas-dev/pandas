1ef1065535fa95b46a13dafb7641dc3b5b45c61a
VirosaLi <2EkF8qUgpNkj>
2020-12-21 11:02:40 -0600

    CI: move arm64 build to azure #38617

---
+++ ci/docker_ci.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+IMAGE = $1
+PATTERN = $2
+
+docker pull quay.io/pypa/$IMAGE
+docker run -v $(pwd):/pandas quay.io/pypa/$IMAGE \
+/bin/bash -xc "cd pandas && \
+/opt/python/cp37-cp37m/bin/python -m venv ~/virtualenvs/pandas-dev && \
+. ~/virtualenvs/pandas-dev/bin/activate && \
+python -m pip install --no-deps -U pip wheel setuptools && \
+pip install cython numpy python-dateutil pytz pytest pytest-xdist hypothesis pytest-azurepipelines && \
+python setup.py build_ext -q -j2 && \
+python -m pip install --no-build-isolation -e . && \
+pytest -m '$PATTERN' pandas --junitxml=test-data.xml"
