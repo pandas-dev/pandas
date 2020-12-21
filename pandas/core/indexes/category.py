9c9a897f603a3fe75f7a493b7cba433bb2e6a8fb
Brock <jbrockmendel@gmail.com>
2020-12-21 07:45:14 -0800

    BUG: disallow scalar in CategoricalIndex construtor

--- pandas/core/indexes/category.py
+++ pandas/core/indexes/category.py
@@ -188,13 +188,8 @@ class CategoricalIndex(NDArrayBackedExtensionIndex, accessor.PandasDelegate):
 
         name = maybe_extract_name(name, data, cls)
 
-        if not is_categorical_dtype(data):
-            # don't allow scalars
-            # if data is None, then categories must be provided
-            if is_scalar(data):
-                if data is not None or categories is None:
-                    raise cls._scalar_data_error(data)
-                data = []
+        if is_scalar(data):
+            raise cls._scalar_data_error(data)
 
         assert isinstance(dtype, CategoricalDtype), dtype
         data = extract_array(data, extract_numpy=True)
