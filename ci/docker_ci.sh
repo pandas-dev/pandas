b53d7730ac0a2d4e99c9e38afb430fc7cf82eac0
VirosaLi <2EkF8qUgpNkj>
2020-12-21 23:57:13 -0600

    qemu for aarch64

--- ci/docker_ci.sh
+++ ci/docker_ci.sh
@@ -1,5 +1,10 @@
 #!/bin/bash -e
 
+if [[ $PATTERN == "manylinux2014_aarch64" ]]; then
+    docker pull multiarch/qemu-user-static
+    docker run --privileged multiarch/qemu-user-static --reset -p yes
+fi
+
 docker pull quay.io/pypa/$IMAGE
 docker run -v $(pwd):/pandas quay.io/pypa/$IMAGE \
 /bin/bash -xc "cd pandas && \
