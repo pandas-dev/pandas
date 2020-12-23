2a1e1a581d5110bda04eb45a5bab834b40120173
jbrockmendel <jbrockmendel@gmail.com>
2020-12-23 06:36:41 -0800

    REF: use _should_compare to short-circuit set operations (#38643)

--- pandas/core/indexes/datetimelike.py
+++ pandas/core/indexes/datetimelike.py
@@ -683,12 +683,16 @@ class DatetimeTimedeltaMixin(DatetimeIndexOpsMixin, Int64Index):
         """
         self._validate_sort_keyword(sort)
         self._assert_can_do_setop(other)
-        other, _ = self._convert_can_do_setop(other)
+        other, result_name = self._convert_can_do_setop(other)
 
         if self.equals(other):
             if self.has_duplicates:
                 return self.unique()._get_reconciled_name_object(other)
             return self._get_reconciled_name_object(other)
+
+        elif not self._should_compare(other):
+            # We can infer that the intersection is empty.
+            return Index([], name=result_name)
 
         return self._intersection(other, sort=sort)
 
