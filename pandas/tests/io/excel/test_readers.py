05a6f0963b0516200523ee2b05722f97301b70d0
rhshadrach <rhshadrach@gmail.com>
2020-12-23 15:37:10 -0500

    xfail instead

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -1182,15 +1182,11 @@ class TestExcelFileRead:
     def test_excel_read_binary_via_read_excel(self, read_ext, engine):
         # GH 38424
         if read_ext == ".xlsb" and engine == "pyxlsb":
-            # GH 38667 - should default to pyxlsb but doesn't
-            with pytest.raises(IOError, match="File contains no valid workbook part"):
-                with open("test1" + read_ext, "rb") as f:
-                    pd.read_excel(f)
-        else:
-            with open("test1" + read_ext, "rb") as f:
-                result = pd.read_excel(f)
-            expected = pd.read_excel("test1" + read_ext, engine=engine)
-            tm.assert_frame_equal(result, expected)
+            pytest.xfail("GH 38667 - should default to pyxlsb but doesn't")
+        with open("test1" + read_ext, "rb") as f:
+            result = pd.read_excel(f)
+        expected = pd.read_excel("test1" + read_ext, engine=engine)
+        tm.assert_frame_equal(result, expected)
 
     @pytest.mark.skipif(
         xlrd_version is not None and xlrd_version >= "2",
