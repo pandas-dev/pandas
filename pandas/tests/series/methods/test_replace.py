778392cf3c29c83c3237664778b5cf0f3f5af332
ftrihardjo <felixtrihardjo@gmail.com>
2020-12-28 08:59:28 +0700

    pandas-dev issue #38267

--- pandas/tests/series/methods/test_replace.py
+++ pandas/tests/series/methods/test_replace.py
@@ -213,11 +213,8 @@ class TestSeriesReplace:
         result = pd.Series([0, None], dtype=any_nullable_int_dtype).replace(0, pd.NA)
         expected = pd.Series([pd.NA, pd.NA], dtype=any_nullable_int_dtype)
         tm.assert_series_equal(result, expected)
-        result = (
-+            pd.Series([0, 1], dtype=any_nullable_int_dtype)
-+            .replace(0, pd.NA)
-+            .replace(1, pd.NA)
-+        )
+        result = pd.Series([0, 1], dtype=any_nullable_int_dtype).replace(0, pd.NA)
+        result.replace(1, pd.NA, inplace=True)
         tm.assert_series_equal(result, expected)
 
     def test_replace2(self):
