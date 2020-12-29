a4eb82cf5d9da9757f88cbdf9e8d6c50ec8afc0b
jbrockmendel <jbrockmendel@gmail.com>
2020-12-29 10:09:24 -0800

    BUG: Series.where casting dt64 to int64 (#38073)
    * ENH: support 2D in DatetimeArray._from_sequence
    
    * BUG: Series.where casting dt64 to int64
    
    * whatsnew
    
    * move whatsnew
    
    * use fixture, remove unnecessary check

--- pandas/core/arrays/numpy_.py
+++ pandas/core/arrays/numpy_.py
@@ -161,7 +161,8 @@ class PandasArray(
                 f"'values' must be a NumPy array, not {type(values).__name__}"
             )
 
-        if values.ndim != 1:
+        if values.ndim == 0:
+            # Technically we support 2, but do not advertise that fact.
             raise ValueError("PandasArray must be 1-dimensional.")
 
         if copy:
