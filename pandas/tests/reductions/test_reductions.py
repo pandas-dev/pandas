73b94125c92af9abb3546fa311e1593438e9e005
jbrockmendel <jbrockmendel@gmail.com>
2020-12-30 05:23:24 -0800

    BUG: inconsistency between frame.any/all with dt64 vs dt64tz (#38723)

--- pandas/tests/reductions/test_reductions.py
+++ pandas/tests/reductions/test_reductions.py
@@ -17,6 +17,7 @@ from pandas import (
     Timedelta,
     TimedeltaIndex,
     Timestamp,
+    date_range,
     isna,
     timedelta_range,
     to_timedelta,
@@ -922,6 +923,48 @@ class TestSeriesReductions:
         result = df.any(axis=1, bool_only=True)
         expected = Series([True, False])
         tm.assert_series_equal(result, expected)
+
+    def test_any_all_datetimelike(self):
+        # GH#38723 these may not be the desired long-term behavior (GH#34479)
+        #  but in the interim should be internally consistent
+        dta = date_range("1995-01-02", periods=3)._data
+        ser = Series(dta)
+        df = DataFrame(ser)
+
+        assert dta.all()
+        assert dta.any()
+
+        assert ser.all()
+        assert ser.any()
+
+        assert df.any().all()
+        assert df.all().all()
+
+        dta = dta.tz_localize("UTC")
+        ser = Series(dta)
+        df = DataFrame(ser)
+
+        assert dta.all()
+        assert dta.any()
+
+        assert ser.all()
+        assert ser.any()
+
+        assert df.any().all()
+        assert df.all().all()
+
+        tda = dta - dta[0]
+        ser = Series(tda)
+        df = DataFrame(ser)
+
+        assert tda.any()
+        assert not tda.all()
+
+        assert ser.any()
+        assert not ser.all()
+
+        assert df.any().all()
+        assert not df.all().any()
 
     def test_timedelta64_analytics(self):
 
