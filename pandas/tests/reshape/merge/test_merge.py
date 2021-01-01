efc6eb13acdc8a384008a33e63765fbdb746c7fc
phofl <patrick_hoefler@gmx.net>
2021-01-02 00:58:12 +0100

    Remove pd

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -2379,11 +2379,11 @@ def test_merge_right_left_index():
 
 def test_merge_empty_right_index_left_on():
     # GH#33814
-    df1 = pd.DataFrame({"a": [1], "b": [2]}).set_index(["a", "b"])
-    df2 = pd.DataFrame({"b": [1]}).set_index(["b"])
-    expected = pd.DataFrame({"a": [], "b": []}, dtype=np.int64).set_index(["a", "b"])
-    result = pd.merge(df1, df2, left_on=["b"], right_index=True)
+    df1 = DataFrame({"a": [1], "b": [2]}).set_index(["a", "b"])
+    df2 = DataFrame({"b": [1]}).set_index(["b"])
+    expected = DataFrame({"a": [], "b": []}, dtype=np.int64).set_index(["a", "b"])
+    result = merge(df1, df2, left_on=["b"], right_index=True)
     tm.assert_frame_equal(result, expected)
 
-    result = pd.merge(df2, df1, left_index=True, right_on=["b"])
+    result = merge(df2, df1, left_index=True, right_on=["b"])
     tm.assert_frame_equal(result, expected)
