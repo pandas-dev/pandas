fff7297806572af4e806979d52ab94e60afad1be
Marco Gorelli <m.e.gorelli@gmail.com>
2021-01-02 12:36:36 +0000

    clarify docstring

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -177,7 +177,7 @@ def names_compat(meth):
 
 
 def _lexsort_depth(codes: List[np.ndarray], nlevels: int) -> int:
-    """Count depth with which codes are lexsorted, up to nlevels."""
+    """Count depth (up to a maximum of `nlevels`) with which codes are lexsorted."""
     int64_codes = [ensure_int64(level_codes) for level_codes in codes]
     for k in range(nlevels, 0, -1):
         if libalgos.is_lexsorted(int64_codes[:k]):
