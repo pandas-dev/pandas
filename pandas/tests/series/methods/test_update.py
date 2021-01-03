dfc92bbfe1071f3562c384f7c341559eba6de830
ftrihardjo <felixtrihardjo@gmail.com>
2021-01-03 09:03:11 +0700

    pandas-dev issue #25744

--- pandas/tests/series/methods/test_update.py
+++ pandas/tests/series/methods/test_update.py
@@ -114,6 +114,7 @@ class TestUpdate:
         dtype = CategoricalDtype(["a", "b", "c", "d"])
         s1 = Series(["a", "b", "c"], index=[1, 2, 3], dtype=dtype)
         s2 = Series(["b", "a"], index=[1, 2], dtype=dtype)
-        result = s1.update(s2)
+        s1.update(s2)
+        result = s1
         expected = Series(["b", "a", "c"], index=[1, 2, 3], dtype=dtype)
         tm.assert_series_equal(result, expected)
