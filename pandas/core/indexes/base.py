bd063c863084ffeb92e039fa578dc32267ca6e35
phofl <patrick_hoefler@gmx.net>
2021-01-03 19:17:00 +0100

    Remove len self

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -1672,7 +1672,7 @@ class Index(IndexOpsMixin, PandasObject):
         Drop MultiIndex levels by level _number_, not name.
         """
 
-        if not levnums and (len(self) > 1 or not isinstance(self, ABCMultiIndex)):
+        if not levnums and not isinstance(self, ABCMultiIndex):
             return self
         if len(levnums) >= self.nlevels:
             raise ValueError(
