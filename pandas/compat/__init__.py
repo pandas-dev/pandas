7941a15ed65920598444a71016a8b166e73a0be8
Fangchen Li <fangchen.li@outlook.com>
2021-01-03 15:29:49 -0600

    fix arch check

--- pandas/compat/__init__.py
+++ pandas/compat/__init__.py
@@ -17,7 +17,7 @@ PY38 = sys.version_info >= (3, 8)
 PY39 = sys.version_info >= (3, 9)
 PYPY = platform.python_implementation() == "PyPy"
 IS64 = sys.maxsize > 2 ** 32
-ARM64 = platform.machine() == "arm64"
+ARM64 = platform.machine() == "arm64" or platform.machine() == "aarch64"
 
 
 def set_function_name(f: F, name: str, cls) -> F:
