2b6beb32f97d5849bf890a45c5b140a2a2d7eb74
phofl <patrick_hoefler@gmx.net>
2021-01-03 22:50:40 +0100

    Remove pd for Index after merge

--- pandas/tests/indexing/test_loc.py
+++ pandas/tests/indexing/test_loc.py
@@ -1689,7 +1689,7 @@ class TestLabelSlicing:
         # GH: 20975
         df = DataFrame({"test": 1, 1: 2, 2: 3}, index=[0])
         expected = DataFrame(
-            data=[[2, 3]], index=[0], columns=pd.Index([1, 2], dtype=object)
+            data=[[2, 3]], index=[0], columns=Index([1, 2], dtype=object)
         )
         tm.assert_frame_equal(df.loc[:, 1:], expected)
 
