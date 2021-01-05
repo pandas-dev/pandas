f15bac53b8205ccb4a9ec05a7022a13954d7e0e7
phofl <patrick_hoefler@gmx.net>
2021-01-05 20:44:08 +0100

    Reorder imports

--- pandas/tests/indexes/multi/test_setops.py
+++ pandas/tests/indexes/multi/test_setops.py
@@ -2,7 +2,7 @@ import numpy as np
 import pytest
 
 import pandas as pd
-from pandas import Index, MultiIndex, Series, IntervalIndex, CategoricalIndex
+from pandas import CategoricalIndex, Index, IntervalIndex, MultiIndex, Series
 import pandas._testing as tm
 
 
