BUG: Timedelta(td64_out_of_bounds) silently overflowing (#38965)

pandas/core/dtypes/cast.py
@@ -28,6 +28,7 @@ from pandas._libs import lib, missing as libmissing, tslib
 from pandas._libs.tslibs import (
     NaT,
     OutOfBoundsDatetime,
+    OutOfBoundsTimedelta,
     Period,
     Timedelta,
     Timestamp,
@@ -743,8 +744,12 @@ def infer_dtype_from_scalar(val, pandas_dtype: bool = False) -> Tuple[DtypeObj, 
         val = val.value
 
     elif isinstance(val, (np.timedelta64, timedelta)):
-        val = Timedelta(val).value
-        dtype = np.dtype("m8[ns]")
+        try:
+            val = Timedelta(val).value
+        except (OutOfBoundsTimedelta, OverflowError):
+            dtype = np.dtype(object)
+        else:
+            dtype = np.dtype("m8[ns]")
 
     elif is_bool(val):
         dtype = np.dtype(np.bool_)
@@ -1386,7 +1391,7 @@ def maybe_infer_to_datetimelike(
 
         try:
             td_values = to_timedelta(v)
-        except ValueError:
+        except (ValueError, OverflowError):
             return v.reshape(shape)
         else:
             return np.asarray(td_values).reshape(shape)
@@ -1618,8 +1623,16 @@ def construct_2d_arraylike_from_scalar(
     value: Scalar, length: int, width: int, dtype: np.dtype, copy: bool
 ) -> np.ndarray:
 
+    shape = (length, width)
+
     if dtype.kind in ["m", "M"]:
         value = maybe_unbox_datetimelike(value, dtype)
+    elif dtype == object:
+        if isinstance(value, (np.timedelta64, np.datetime64)):
+            # calling np.array below would cast to pytimedelta/pydatetime
+            out = np.empty(shape, dtype=object)
+            out.fill(value)
+            return out
 
     # Attempt to coerce to a numpy array
     try:
@@ -1632,7 +1645,6 @@ def construct_2d_arraylike_from_scalar(
     if arr.ndim != 0:
         raise ValueError("DataFrame constructor not properly called!")
 
-    shape = (length, width)
     return np.full(shape, arr)
 
 

pandas/tests/frame/test_constructors.py
@@ -10,7 +10,7 @@ import numpy.ma.mrecords as mrecords
 import pytest
 import pytz
 
-from pandas.compat.numpy import _np_version_under1p19, _np_version_under1p20
+from pandas.compat.numpy import _np_version_under1p19
 
 from pandas.core.dtypes.common import is_integer_dtype
 from pandas.core.dtypes.dtypes import DatetimeTZDtype, IntervalDtype, PeriodDtype
@@ -2371,16 +2371,10 @@ class TestFromScalar:
     def test_from_timestamp_scalar_preserves_nanos(self, constructor):
         ts = Timestamp.now() + Timedelta(1)
 
-        obj = Series(ts, index=range(1), dtype="M8[ns]")
+        obj = constructor(ts, dtype="M8[ns]")
         assert get1(obj) == ts
 
-    def test_from_timedelta64_scalar_object(self, constructor, request):
-        if getattr(constructor, "func", None) is DataFrame and _np_version_under1p20:
-            # getattr check means we only xfail when box is None
-            mark = pytest.mark.xfail(
-                reason="np.array(td64, dtype=object) converts to int"
-            )
-            request.node.add_marker(mark)
+    def test_from_timedelta64_scalar_object(self, constructor):
 
         td = Timedelta(1)
         td64 = td.to_timedelta64()
@@ -2407,8 +2401,20 @@ class TestFromScalar:
         with pytest.raises(TypeError, match="Cannot cast"):
             constructor(scalar, dtype=dtype)
 
-    def test_from_out_of_bounds_datetime(self, constructor):
+    @pytest.mark.parametrize("cls", [datetime, np.datetime64])
+    def test_from_out_of_bounds_datetime(self, constructor, cls):
         scalar = datetime(9999, 1, 1)
+        if cls is np.datetime64:
+            scalar = np.datetime64(scalar, "D")
         result = constructor(scalar)
 
-        assert type(get1(result)) is datetime
+        assert type(get1(result)) is cls
+
+    @pytest.mark.parametrize("cls", [timedelta, np.timedelta64])
+    def test_from_out_of_bounds_timedelta(self, constructor, cls):
+        scalar = datetime(9999, 1, 1) - datetime(1970, 1, 1)
+        if cls is np.timedelta64:
+            scalar = np.timedelta64(scalar, "D")
+        result = constructor(scalar)
+
+        assert type(get1(result)) is cls