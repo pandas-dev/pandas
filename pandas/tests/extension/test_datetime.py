da043acfc71102c5eed9597bd9c95ac27fcbc55f
ftrihardjo <felixtrihardjo@gmail.com>
2021-01-06 13:56:35 +0700

    pandas-dev issue #30699

--- pandas/tests/extension/test_datetime.py
+++ pandas/tests/extension/test_datetime.py
@@ -165,16 +165,16 @@ class TestComparisonOps(BaseDatetimeTests, base.BaseComparisonOpsTests):
     def test_compare_with_Categorical(self):
         result = pd.date_range("2020", periods=3)
         expected = pd.Categorical(result)
-        assert (result == expected).all()
+        assert all(result == expected)
         result = pd.date_range("2020", periods=3, tz="UTC")
         expected = pd.Categorical(result)
-        assert (result == expected).all()
+        assert all(result == expected)
         result = pd.timedelta_range("0 days", periods=3)
         expected = pd.Categorical(result)
-        assert (result == expected).all()
+        assert all(result == expected)
         result = pd.period_range("2020Q1", periods=3, freq="Q")
         expected = pd.Categorical(result)
-        assert (result == expected).all()
+        assert all(result == expected)
 
 
 class TestMissing(BaseDatetimeTests, base.BaseMissingTests):
