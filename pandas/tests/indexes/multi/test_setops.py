dd921337c4cdc622cfa6f9b29a09cb994fdd902a
phofl <patrick_hoefler@gmx.net>
2021-01-06 01:30:38 +0100

    Add fixture and whatsnew

--- pandas/tests/indexes/multi/test_setops.py
+++ pandas/tests/indexes/multi/test_setops.py
@@ -485,10 +485,10 @@ def test_intersection_different_names():
     tm.assert_index_equal(result, mi2)
 
 
-def test_intersection_with_missing_values_on_both_sides():
+def test_intersection_with_missing_values_on_both_sides(nulls_fixture):
     # GH#38623
-    mi1 = MultiIndex.from_arrays([[3, np.nan, 4, np.nan], [1, 2, 4, 2]])
-    mi2 = MultiIndex.from_arrays([[3, np.nan, 3], [1, 2, 4]])
+    mi1 = MultiIndex.from_arrays([[3, nulls_fixture, 4, nulls_fixture], [1, 2, 4, 2]])
+    mi2 = MultiIndex.from_arrays([[3, nulls_fixture, 3], [1, 2, 4]])
     result = mi1.intersection(mi2)
-    expected = MultiIndex.from_arrays([[3.0, np.nan], [1, 2]])
+    expected = MultiIndex.from_arrays([[3.0, nulls_fixture], [1, 2]])
     tm.assert_index_equal(result, expected)
