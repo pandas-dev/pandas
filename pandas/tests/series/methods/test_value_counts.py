9962c3a1c0c2b38dff57b22044715ca6746f0cb2
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2021-01-08 22:20:15 +0100

    fixing test cases where the position of NaN has changed

--- pandas/tests/series/methods/test_value_counts.py
+++ pandas/tests/series/methods/test_value_counts.py
@@ -185,7 +185,7 @@ class TestSeriesValueCounts:
             (
                 Series([False, True, True, pd.NA]),
                 False,
-                Series([2, 1, 1], index=[True, pd.NA, False]),
+                Series([2, 1, 1], index=[True, False, pd.NA]),
             ),
             (
                 Series([False, True, True, pd.NA]),
@@ -195,7 +195,7 @@ class TestSeriesValueCounts:
             (
                 Series(range(3), index=[True, False, np.nan]).index,
                 False,
-                Series([1, 1, 1], index=[np.nan, True, False]),
+                Series([1, 1, 1], index=[True, False, np.nan]),
             ),
         ],
     )
