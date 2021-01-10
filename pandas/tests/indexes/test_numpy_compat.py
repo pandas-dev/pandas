629a3c29ff586801364727fbe1e91360771d9b78
phofl <patrick_hoefler@gmx.net>
2021-01-10 18:41:29 +0100

    Pull is_numpy_dev up

--- pandas/tests/indexes/test_numpy_compat.py
+++ pandas/tests/indexes/test_numpy_compat.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from pandas.compat.numpy import np_version_under1p17, np_version_under1p18
+from pandas.compat import np_version_under1p17, np_version_under1p18
 
 from pandas import (
     DatetimeIndex,
