62ebc782acc72f6502320d916f455e399772ad90
patrick <61934744+phofl@users.noreply.github.com>
2021-01-15 15:14:17 +0100

    Backport PR #39161: Fix regression in loc setitem raising KeyError when enlarging df with multiindex

--- pandas/tests/indexing/multiindex/test_loc.py
+++ pandas/tests/indexing/multiindex/test_loc.py
@@ -305,6 +305,21 @@ class TestMultiIndexLoc:
         expected = DataFrame([0, 2], index=mi)
         tm.assert_frame_equal(obj, expected)
 
+    @pytest.mark.parametrize(
+        "indexer, exp_value", [(slice(None), 1.0), ((1, 2), np.nan)]
+    )
+    def test_multiindex_setitem_columns_enlarging(self, indexer, exp_value):
+        # GH#39147
+        mi = MultiIndex.from_tuples([(1, 2), (3, 4)])
+        df = DataFrame([[1, 2], [3, 4]], index=mi, columns=["a", "b"])
+        df.loc[indexer, ["c", "d"]] = 1.0
+        expected = DataFrame(
+            [[1, 2, 1.0, 1.0], [3, 4, exp_value, exp_value]],
+            index=mi,
+            columns=["a", "b", "c", "d"],
+        )
+        tm.assert_frame_equal(df, expected)
+
 
 @pytest.mark.parametrize(
     "indexer, pos",
