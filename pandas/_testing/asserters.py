90f809b43c89e142e6d509b0cbd7f4a593480caf
patrick <61934744+phofl@users.noreply.github.com>
2021-01-16 15:21:17 +0100

    BUG: assert_frame_equal raising TypeError with check_like and mixed dtype in Index or columns (#39204)

--- pandas/_testing/asserters.py
+++ pandas/_testing/asserters.py
@@ -29,7 +29,7 @@ from pandas import (
     Series,
     TimedeltaIndex,
 )
-from pandas.core.algorithms import take_1d
+from pandas.core.algorithms import safe_sort, take_1d
 from pandas.core.arrays import (
     DatetimeArray,
     ExtensionArray,
@@ -344,8 +344,8 @@ def assert_index_equal(
 
     # If order doesn't matter then sort the index entries
     if not check_order:
-        left = left.sort_values()
-        right = right.sort_values()
+        left = Index(safe_sort(left))
+        right = Index(safe_sort(right))
 
     # MultiIndex special comparison for little-friendly error messages
     if left.nlevels > 1:
