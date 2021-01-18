d1d1ae3c6ce7abfb218858a370214fc889c63b1d
Matthew Roeschke <emailformattr@gmail.com>
2021-01-17 20:00:25 -0800

    More docstring validation fixes

--- pandas/core/window/doc.py
+++ pandas/core/window/doc.py
@@ -120,3 +120,29 @@ window_agg_numba_parameters = dedent(
           .. versionadded:: 1.3.0
     """
 )
+
+window_agg_numba_args_kwargs_parameters = dedent(
+    """
+    *args
+        For function compatibility and will not have an effect on the result.
+
+    engine : str, default None
+        * ``'cython'`` : Runs the operation through C-extensions from cython.
+        * ``'numba'`` : Runs the operation through JIT compiled code from numba.
+        * ``None`` : Defaults to ``'cython'`` or globally setting ``compute.use_numba``
+
+          .. versionadded:: 1.3.0
+
+    engine_kwargs : dict, default None
+        * For ``'cython'`` engine, there are no accepted ``engine_kwargs``
+        * For ``'numba'`` engine, the engine can accept ``nopython``, ``nogil``
+          and ``parallel`` dictionary keys. The values must either be ``True`` or
+          ``False``. The default ``engine_kwargs`` for the ``'numba'`` engine is
+          ``{'nopython': True, 'nogil': False, 'parallel': False}``
+
+          .. versionadded:: 1.3.0
+
+    **kwargs
+        For function compatibility and will not have an effect on the result.
+    """
+)
