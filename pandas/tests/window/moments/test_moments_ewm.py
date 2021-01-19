5bc2006a89973b811fdff1bf351d5a49da0ea73d
Matthew Roeschke <emailformattr@gmail.com>
2021-01-19 07:53:54 -0800

    DEPR: EWM.vol (#39220)

--- pandas/tests/window/moments/test_moments_ewm.py
+++ pandas/tests/window/moments/test_moments_ewm.py
@@ -5,13 +5,13 @@ from pandas import DataFrame, Series
 import pandas._testing as tm
 
 
-@pytest.mark.parametrize("name", ["var", "vol", "mean"])
+@pytest.mark.parametrize("name", ["var", "std", "mean"])
 def test_ewma_series(series, name):
     series_result = getattr(series.ewm(com=10), name)()
     assert isinstance(series_result, Series)
 
 
-@pytest.mark.parametrize("name", ["var", "vol", "mean"])
+@pytest.mark.parametrize("name", ["var", "std", "mean"])
 def test_ewma_frame(frame, name):
     frame_result = getattr(frame.ewm(com=10), name)()
     assert isinstance(frame_result, DataFrame)
@@ -300,7 +300,7 @@ def test_ewm_domain_checks():
         s.ewm(alpha=1.1)
 
 
-@pytest.mark.parametrize("method", ["mean", "vol", "var"])
+@pytest.mark.parametrize("method", ["mean", "std", "var"])
 def test_ew_empty_series(method):
     vals = Series([], dtype=np.float64)
 
@@ -310,7 +310,7 @@ def test_ew_empty_series(method):
 
 
 @pytest.mark.parametrize("min_periods", [0, 1])
-@pytest.mark.parametrize("name", ["mean", "var", "vol"])
+@pytest.mark.parametrize("name", ["mean", "var", "std"])
 def test_ew_min_periods(min_periods, name):
     # excluding NaNs correctly
     arr = np.random.randn(50)
@@ -329,7 +329,7 @@ def test_ew_min_periods(min_periods, name):
         assert result[:10].isna().all()
         assert not result[10:].isna().any()
     else:
-        # ewm.std, ewm.vol, ewm.var (with bias=False) require at least
+        # ewm.std, ewm.var (with bias=False) require at least
         # two values
         assert result[:11].isna().all()
         assert not result[11:].isna().any()
@@ -343,7 +343,7 @@ def test_ew_min_periods(min_periods, name):
     if name == "mean":
         tm.assert_series_equal(result, Series([1.0]))
     else:
-        # ewm.std, ewm.vol, ewm.var with bias=False require at least
+        # ewm.std, ewm.var with bias=False require at least
         # two values
         tm.assert_series_equal(result, Series([np.NaN]))
 
