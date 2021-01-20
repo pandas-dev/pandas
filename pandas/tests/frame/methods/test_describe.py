e2f09dfb53efeab5c8f75303770d1e7895e2fc36
Lucky Sivagurunathan <25218389+luckyvs1@users.noreply.github.com>
2021-01-20 10:57:32 -0800

    TST: Add test to ensure DF describe does not throw an error (#32409) (#38403)

--- pandas/tests/frame/methods/test_describe.py
+++ pandas/tests/frame/methods/test_describe.py
@@ -367,6 +367,15 @@ class TestDataFrameDescribe:
         )
         tm.assert_frame_equal(result, expected)
 
+    def test_describe_does_not_raise_error_for_dictlike_elements(self):
+        # GH#32409
+        df = DataFrame([{"test": {"a": "1"}}, {"test": {"a": "2"}}])
+        expected = DataFrame(
+            {"test": [2, 2, {"a": "2"}, 1]}, index=["count", "unique", "top", "freq"]
+        )
+        result = df.describe()
+        tm.assert_frame_equal(result, expected)
+
     @pytest.mark.parametrize("exclude", ["x", "y", ["x", "y"], ["x", "z"]])
     def test_describe_when_include_all_exclude_not_allowed(self, exclude):
         """
