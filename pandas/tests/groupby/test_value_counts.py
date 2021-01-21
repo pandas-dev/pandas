466a100eeb73f8085da1b96c495f173dce150ec9
Anna Daglis <ana.daglis@farfetch.com>
2021-01-21 19:05:08 +0000

    Fix bug

--- pandas/tests/groupby/test_value_counts.py
+++ pandas/tests/groupby/test_value_counts.py
@@ -59,39 +59,39 @@ for seed_nans in [True, False]:
             ids.append(f"{k}-{n}-{m}")
 
 
-# @pytest.mark.slow
-# @pytest.mark.parametrize("df, keys, bins, n, m", binned, ids=ids)
-# @pytest.mark.parametrize("isort", [True, False])
-# @pytest.mark.parametrize("normalize", [True, False])
-# @pytest.mark.parametrize("sort", [True, False])
-# @pytest.mark.parametrize("ascending", [True, False])
-# @pytest.mark.parametrize("dropna", [True, False])
-# def test_series_groupby_value_counts(
-#     df, keys, bins, n, m, isort, normalize, sort, ascending, dropna
-# ):
-#     def rebuild_index(df):
-#         arr = list(map(df.index.get_level_values, range(df.index.nlevels)))
-#         df.index = MultiIndex.from_arrays(arr, names=df.index.names)
-#         return df
-#
-#     kwargs = {
-#         "normalize": normalize,
-#         "sort": sort,
-#         "ascending": ascending,
-#         "dropna": dropna,
-#         "bins": bins,
-#     }
-#
-#     gr = df.groupby(keys, sort=isort)
-#     left = gr["3rd"].value_counts(**kwargs)
-#
-#     gr = df.groupby(keys, sort=isort)
-#     right = gr["3rd"].apply(Series.value_counts, **kwargs)
-#     right.index.names = right.index.names[:-1] + ["3rd"]
-#
-#     # have to sort on index because of unstable sort on values
-#     left, right = map(rebuild_index, (left, right))  # xref GH9212
-#     tm.assert_series_equal(left.sort_index(), right.sort_index())
+@pytest.mark.slow
+@pytest.mark.parametrize("df, keys, bins, n, m", binned, ids=ids)
+@pytest.mark.parametrize("isort", [True, False])
+@pytest.mark.parametrize("normalize", [True, False])
+@pytest.mark.parametrize("sort", [True, False])
+@pytest.mark.parametrize("ascending", [True, False])
+@pytest.mark.parametrize("dropna", [True, False])
+def test_series_groupby_value_counts(
+    df, keys, bins, n, m, isort, normalize, sort, ascending, dropna
+):
+    def rebuild_index(df):
+        arr = list(map(df.index.get_level_values, range(df.index.nlevels)))
+        df.index = MultiIndex.from_arrays(arr, names=df.index.names)
+        return df
+
+    kwargs = {
+        "normalize": normalize,
+        "sort": sort,
+        "ascending": ascending,
+        "dropna": dropna,
+        "bins": bins,
+    }
+
+    gr = df.groupby(keys, sort=isort)
+    left = gr["3rd"].value_counts(**kwargs)
+
+    gr = df.groupby(keys, sort=isort)
+    right = gr["3rd"].apply(Series.value_counts, **kwargs)
+    right.index.names = right.index.names[:-1] + ["3rd"]
+
+    # have to sort on index because of unstable sort on values
+    left, right = map(rebuild_index, (left, right))  # xref GH9212
+    tm.assert_series_equal(left.sort_index(), right.sort_index())
 
 
 def test_series_groupby_value_counts_with_grouper():
