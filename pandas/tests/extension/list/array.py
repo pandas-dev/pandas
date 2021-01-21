0d5a64442d2a0895dde2e58e9f5ea1f7f9f14321
Marco Gorelli <m.e.gorelli@gmail.com>
2021-01-21 11:21:07 +0000

    remove string type hints (#39299)

--- pandas/tests/extension/list/array.py
+++ pandas/tests/extension/list/array.py
@@ -3,6 +3,8 @@ Test extension array for storing nested data in a pandas container.
 
 The ListArray stores an ndarray of lists.
 """
+from __future__ import annotations
+
 import numbers
 import random
 import string
@@ -22,7 +24,7 @@ class ListDtype(ExtensionDtype):
     na_value = np.nan
 
     @classmethod
-    def construct_array_type(cls) -> Type["ListArray"]:
+    def construct_array_type(cls) -> Type[ListArray]:
         """
         Return the array type associated with this dtype.
 
