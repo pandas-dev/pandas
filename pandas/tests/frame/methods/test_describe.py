7e531e3dfabd22fdf6669ea3b4caaf8e3b57cdd8
realead <egor.dranischnikow@googlemail.com>
2021-01-22 22:42:33 +0100

    ENH: making value_counts stable/keeping original ordering (#39009)

--- pandas/tests/frame/methods/test_describe.py
+++ pandas/tests/frame/methods/test_describe.py
@@ -371,7 +371,7 @@ class TestDataFrameDescribe:
         # GH#32409
         df = DataFrame([{"test": {"a": "1"}}, {"test": {"a": "2"}}])
         expected = DataFrame(
-            {"test": [2, 2, {"a": "2"}, 1]}, index=["count", "unique", "top", "freq"]
+            {"test": [2, 2, {"a": "1"}, 1]}, index=["count", "unique", "top", "freq"]
         )
         result = df.describe()
         tm.assert_frame_equal(result, expected)
