ae5e9ba584cacf4b3284f3eb0e9f71fd701e2ab7
Brock <jbrockmendel@gmail.com>
2021-01-23 16:08:16 -0800

    troubleshoot windows and 32bit

--- pandas/tests/extension/test_sparse.py
+++ pandas/tests/extension/test_sparse.py
@@ -16,7 +16,7 @@ be added to the array-specific tests in `pandas/tests/arrays/`.
 import numpy as np
 import pytest
 
-from pandas.compat import is_platform_windows
+from pandas.compat import IS64, is_platform_windows
 from pandas.errors import PerformanceWarning
 
 from pandas.core.dtypes.common import is_object_dtype
@@ -405,7 +405,9 @@ class TestArithmeticOps(BaseSparseTests, base.BaseArithmeticOpsTests):
         super().test_arith_series_with_array(data, all_arithmetic_operators)
 
     def test_arith_frame_with_scalar(self, data, all_arithmetic_operators, request):
-        if data.dtype.fill_value == 0 and all_arithmetic_operators.strip("_") not in [
+        if data.dtype.fill_value != 0:
+            pass
+        elif all_arithmetic_operators.strip("_") not in [
             "mul",
             "rmul",
             "floordiv",
@@ -416,7 +418,7 @@ class TestArithmeticOps(BaseSparseTests, base.BaseArithmeticOpsTests):
         ]:
             mark = pytest.mark.xfail(reason="result dtype.fill_value mismatch")
             request.node.add_marker(mark)
-        elif is_platform_windows():
+        elif is_platform_windows() or not IS64:
             mark = pytest.mark.xfail(reason="results are int32, expected int64")
             request.node.add_marker(mark)
         super().test_arith_frame_with_scalar(data, all_arithmetic_operators)
