a1a2fa229340d3e981ff06559a18016c50ab3513
patrick <61934744+phofl@users.noreply.github.com>
2021-01-25 17:33:12 +0100

    BUG: Slicing DatetimeIndex with strings containing microseconds raising KeyError (#39378)

--- pandas/tests/indexing/test_datetime.py
+++ pandas/tests/indexing/test_datetime.py
@@ -231,3 +231,24 @@ class TestDatetimeIndex:
             dtype=object,
         )
         tm.assert_frame_equal(result, expected)
+
+    def test_getitem_millisecond_resolution(self, frame_or_series):
+        # GH#33589
+        obj = frame_or_series(
+            [1, 2, 3, 4],
+            index=[
+                Timestamp("2017-10-25T16:25:04.151"),
+                Timestamp("2017-10-25T16:25:04.252"),
+                Timestamp("2017-10-25T16:50:05.237"),
+                Timestamp("2017-10-25T16:50:05.238"),
+            ],
+        )
+        result = obj["2017-10-25T16:25:04.252":"2017-10-25T16:50:05.237"]
+        expected = frame_or_series(
+            [2, 3],
+            index=[
+                Timestamp("2017-10-25T16:25:04.252"),
+                Timestamp("2017-10-25T16:50:05.237"),
+            ],
+        )
+        tm.assert_equal(result, expected)
