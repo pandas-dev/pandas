150fd3b63130fd669615644def565beff66f321d
datajanko <jan.koch@tu-dortmund.de>
2021-01-26 21:05:43 +0100

    tests no numeric accumulations according to _accumulate interface

--- pandas/tests/extension/test_sparse.py
+++ pandas/tests/extension/test_sparse.py
@@ -444,3 +444,7 @@ class TestParsing(BaseSparseTests, base.BaseParsingTests):
         expected_msg = r".*must implement _from_sequence_of_strings.*"
         with pytest.raises(NotImplementedError, match=expected_msg):
             super().test_EA_types(engine, data)
+
+
+class TestNoNumericAccumulations(base.BaseNoAccumulateTests):
+    pass
