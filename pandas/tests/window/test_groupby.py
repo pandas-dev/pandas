92f89928c515403f61aa80fa06b73a082872b0c4
sevberg <s.ryberg@accure.net>
2021-02-02 23:25:40 +0100

    fix test

--- pandas/tests/window/test_groupby.py
+++ pandas/tests/window/test_groupby.py
@@ -522,17 +522,28 @@ class TestRolling:
         tm.assert_series_equal(result, expected)
 
     @pytest.mark.parametrize(
-        ("func", "kwargs"),
-        [("rolling", {"window": 2, "min_periods": 1}), ("expanding", {})],
+        ("func", "kwargs", "expected_data"),
+        [
+            (
+                "rolling",
+                {"window": 2, "min_periods": 1},
+                {"a": [np.nan] * 5, "b": [np.nan, 0.5, np.nan, 0.5, 0.5]},
+            ),
+            (
+                "expanding",
+                {},
+                {"a": [np.nan] * 5, "b": [np.nan, 0.5, np.nan, 0.5, 0.577350]},
+            ),
+        ],
     )
-    def test_groupby_rolling_sem(self, func, kwargs):
+    def test_groupby_rolling_sem(self, func, kwargs, expected_data):
         # GH: 26476
         df = DataFrame(
             [["a", 1], ["a", 2], ["b", 1], ["b", 2], ["b", 3]], columns=["a", "b"]
         )
         result = getattr(df.groupby("a"), func)(**kwargs).sem()
         expected = DataFrame(
-            {"a": [np.nan] * 5, "b": [np.nan, 0.70711, np.nan, 0.70711, 0.70711]},
+            expected_data,
             index=MultiIndex.from_tuples(
                 [("a", 0), ("a", 1), ("b", 2), ("b", 3), ("b", 4)], names=["a", None]
             ),
