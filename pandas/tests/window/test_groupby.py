278d33f0e18016064574402be6177b584bcf38a0
sevberg <s.ryberg@accure.net>
2021-02-03 14:23:17 +0100

    revert sem test

--- pandas/tests/window/test_groupby.py
+++ pandas/tests/window/test_groupby.py
@@ -522,28 +522,17 @@ class TestRolling:
         tm.assert_series_equal(result, expected)
 
     @pytest.mark.parametrize(
-        ("func", "kwargs", "expected_data"),
-        [
-            (
-                "rolling",
-                {"window": 2, "min_periods": 1},
-                {"a": [np.nan] * 5, "b": [np.nan, 0.5, np.nan, 0.5, 0.5]},
-            ),
-            (
-                "expanding",
-                {},
-                {"a": [np.nan] * 5, "b": [np.nan, 0.5, np.nan, 0.5, 0.577350]},
-            ),
-        ],
+        ("func", "kwargs"),
+        [("rolling", {"window": 2, "min_periods": 1}), ("expanding", {})],
     )
-    def test_groupby_rolling_sem(self, func, kwargs, expected_data):
+    def test_groupby_rolling_sem(self, func, kwargs):
         # GH: 26476
         df = DataFrame(
             [["a", 1], ["a", 2], ["b", 1], ["b", 2], ["b", 3]], columns=["a", "b"]
         )
         result = getattr(df.groupby("a"), func)(**kwargs).sem()
         expected = DataFrame(
-            expected_data,
+            {"a": [np.nan] * 5, "b": [np.nan, 0.70711, np.nan, 0.70711, 0.70711]},
             index=MultiIndex.from_tuples(
                 [("a", 0), ("a", 1), ("b", 2), ("b", 3), ("b", 4)], names=["a", None]
             ),
