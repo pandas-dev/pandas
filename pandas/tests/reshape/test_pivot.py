9f6d8e7af3d0ee66256929994bb0e21d9188f7fd
Brock <jbrockmendel@gmail.com>
2021-02-02 18:19:40 -0800

    troubleshoot 32bit

--- pandas/tests/reshape/test_pivot.py
+++ pandas/tests/reshape/test_pivot.py
@@ -962,7 +962,7 @@ class TestPivotTable:
         # GH 17013
 
         df = self.data.copy()
-        df[["D", "E", "F"]] = np.arange(len(df) * 3).reshape(len(df), 3)
+        df[["D", "E", "F"]] = np.arange(len(df) * 3).reshape(len(df), 3).astype("i8")
 
         mi_val = list(product(["bar", "foo"], ["one", "two"])) + [("All", "")]
         mi = MultiIndex.from_tuples(mi_val, names=("A", "B"))
@@ -970,8 +970,6 @@ class TestPivotTable:
             {"dull": [12, 21, 3, 9, 45], "shiny": [33, 0, 36, 51, 120]}, index=mi
         ).rename_axis("C", axis=1)
         expected["All"] = expected["dull"] + expected["shiny"]
-        expected["All"] = expected["All"].astype(np.intp)
-        assert expected.dtypes["All"] == np.intp
 
         result = df.pivot_table(
             values="D",
