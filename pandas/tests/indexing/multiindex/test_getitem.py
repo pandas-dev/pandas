548ca8a34052856e82add2bd09b56330c5236a53
Theodor Athanasiadis <theodorathanasiadis@gmail.com>
2021-02-05 01:59:22 +0200

    Reverting changes in test_series_getitem_indexing_errors

--- pandas/tests/indexing/multiindex/test_getitem.py
+++ pandas/tests/indexing/multiindex/test_getitem.py
@@ -86,8 +86,7 @@ def test_series_getitem_returns_scalar(
         (lambda s: s[(2000, 3, 4)], KeyError, r"^\(2000, 3, 4\)$"),
         (lambda s: s.loc[(2000, 3, 4)], KeyError, r"^\(2000, 3, 4\)$"),
         (lambda s: s.loc[(2000, 3, 4, 5)], IndexingError, "Too many indexers"),
-        (lambda s: s.__getitem__(len(s)), KeyError, ""),
-        # match should include len(s)
+        (lambda s: s.__getitem__(len(s)), KeyError, ""),  # match should include len(s)
         (lambda s: s[len(s)], KeyError, ""),  # match should include len(s)
         (
             lambda s: s.iloc[len(s)],
