1381eccfa8e8ca19fde8dd0c483c33ba7a43fd32
Richard Shadrach <rhshadrach@gmail.com>
2021-02-05 16:51:19 -0500

    Added comment

--- pandas/io/excel/_openpyxl.py
+++ pandas/io/excel/_openpyxl.py
@@ -533,6 +533,8 @@ class OpenpyxlReader(BaseExcelReader):
 
         version = LooseVersion(get_version(openpyxl))
 
+        # There is no good way of determining if a sheet is read-only
+        # https://foss.heptapod.net/openpyxl/openpyxl/-/issues/1605
         is_readonly = hasattr(sheet, "reset_dimensions")
 
         if version >= "3.0.0" and is_readonly:
