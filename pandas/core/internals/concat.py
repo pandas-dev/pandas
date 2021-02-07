512a50c4a88fa8fe43a32d76d26212e6cf09a4cf
Brock <jbrockmendel@gmail.com>
2021-02-07 09:23:42 -0800

    pretend_axi1 -> ea_compat_axis

--- pandas/core/internals/concat.py
+++ pandas/core/internals/concat.py
@@ -359,7 +359,7 @@ def _concatenate_join_units(
         # concatting with at least one EA means we are concatting a single column
         # the non-EA values are 2D arrays with shape (1, n)
         to_concat = [t if isinstance(t, ExtensionArray) else t[0, :] for t in to_concat]
-        concat_values = concat_compat(to_concat, axis=0, pretend_axis1=True)
+        concat_values = concat_compat(to_concat, axis=0, ea_compat_axis=True)
         if (
             not isinstance(concat_values, ExtensionArray)
             or (isinstance(concat_values, DatetimeArray) and concat_values.tz is None)
