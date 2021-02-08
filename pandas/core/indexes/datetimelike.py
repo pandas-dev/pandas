bf15bdcc08a9d94beceb86a7067c750cc27d7b7e
Brock <jbrockmendel@gmail.com>
2021-02-08 11:26:34 -0800

    REF: remove unncessary PeriodIndex._get_indexer

--- pandas/core/indexes/datetimelike.py
+++ pandas/core/indexes/datetimelike.py
@@ -230,10 +230,8 @@ class DatetimeIndexOpsMixin(NDArrayBackedExtensionIndex):
     """The expected NA value to use with this index."""
 
     def _convert_tolerance(self, tolerance, target):
+        tolerance = super()._convert_tolerance(tolerance, target)
         tolerance = np.asarray(to_timedelta(tolerance).to_numpy())
-
-        if target.size != tolerance.size and tolerance.size > 1:
-            raise ValueError("list-like tolerance size must match target index size")
         return tolerance
 
     def tolist(self) -> List:
