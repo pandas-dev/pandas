df248cffd71600bf6afe54094a411875fe5437b0
phofl <patrick_hoefler@gmx.net>
2021-02-13 22:05:48 +0100

    Regression in to_excel when setting duplicate column names

--- pandas/tests/io/excel/test_writers.py
+++ pandas/tests/io/excel/test_writers.py
@@ -1295,6 +1295,15 @@ class TestExcelWriter:
         with pytest.raises(ValueError, match="Excel does not support"):
             df.to_excel(path)
 
+    def test_excel_duplicate_columns_with_names(self, path):
+        # GH#39695
+        df = DataFrame({"A": [0, 1], "B": [10, 11]})
+        df.to_excel(path, columns=["A", "B", "A"], index=False)
+
+        result = pd.read_excel(path)
+        expected = DataFrame([[0, 10, 0], [1, 11, 1]], columns=["A", "B", "A.1"])
+        tm.assert_frame_equal(result, expected)
+
 
 class TestExcelWriterEngineTests:
     @pytest.mark.parametrize(
