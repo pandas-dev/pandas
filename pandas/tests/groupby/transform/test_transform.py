0b9a24304366c2d8373e5336dcdd8a5bb45677ca
ftrihardjo <felixtrihardjo@gmail.com>
2021-02-16 05:34:47 +0700

    API: transform behaves differently with 'ffill' on DataFrameGroupBy and SeriesGroupBy (#39638)

--- pandas/tests/groupby/transform/test_transform.py
+++ pandas/tests/groupby/transform/test_transform.py
@@ -769,6 +769,18 @@ def test_transform_numeric_ret(cols, exp, comp_func, agg_func, request):
     comp_func(result, exp)
 
 
+def test_transform_ffill():
+    # GH 24211
+    data = [["a", 0.0], ["a", float("nan")], ["b", 1.0], ["b", float("nan")]]
+    df = DataFrame(data, columns=["key", "values"])
+    result = df.groupby("key").transform("ffill")
+    expected = DataFrame({"values": [0.0, 0.0, 1.0, 1.0]})
+    tm.assert_frame_equal(result, expected)
+    result = df.groupby("key")["values"].transform("ffill")
+    expected = Series([0.0, 0.0, 1.0, 1.0], name="values")
+    tm.assert_series_equal(result, expected)
+
+
 @pytest.mark.parametrize("mix_groupings", [True, False])
 @pytest.mark.parametrize("as_series", [True, False])
 @pytest.mark.parametrize("val1,val2", [("foo", "bar"), (1, 2), (1.0, 2.0)])
