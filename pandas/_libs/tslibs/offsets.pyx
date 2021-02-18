53b09c19e9b77edc0c4bb98b51b356c966e15443
Pawel Kranzberg <pawel@kranzberg.co>
2021-02-18 01:10:29 +0100

    Update offsets.pyx

--- pandas/_libs/tslibs/offsets.pyx
+++ pandas/_libs/tslibs/offsets.pyx
@@ -1841,10 +1841,9 @@ cdef class YearOffset(SingleConstructorOffset):
     
     _attributes = tuple(["n", "normalize", "month"])
     _default_month = 1
-    cdef int _period_dtype_code
+    cdef public int _period_dtype_code
 
-    cdef readonly:
-        int month
+    cdef readonly int month
 
     def __init__(self, n=1, normalize=False, month=None):
         BaseOffset.__init__(self, n, normalize)
@@ -2030,10 +2029,9 @@ cdef class QuarterOffset(SingleConstructorOffset):
     
     _attributes = tuple(["n", "normalize", "startingMonth"])
     _default_month = 1
-    cdef int _period_dtype_code
+    cdef public int _period_dtype_code
 
-    cdef readonly:
-        int startingMonth
+    cdef readonly int startingMonth
 
     def __init__(self, n=1, normalize=False, startingMonth=None):
         BaseOffset.__init__(self, n, normalize)
