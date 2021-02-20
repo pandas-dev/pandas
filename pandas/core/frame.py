cbea56894526b60553b90ba9bd90736e300be740
phofl <patrick_hoefler@gmx.net>
2021-02-20 23:20:59 +0100

    Fix regression for setitem not aligning rhs with boolean indexer

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -3263,6 +3263,8 @@ class DataFrame(NDFrame, OpsMixin):
             key = check_bool_indexer(self.index, key)
             indexer = key.nonzero()[0]
             self._check_setitem_copy()
+            if isinstance(value, DataFrame):
+                value = value.reindex(self.index.take(indexer))
             self.iloc[indexer] = value
         else:
             if isinstance(value, DataFrame):
