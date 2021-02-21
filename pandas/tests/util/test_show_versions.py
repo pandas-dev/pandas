691b1a2924b913fda20215961dc3475838d6b630
alexprincel@gmail.com <alexprincel@gmail.com>
2021-02-20 20:25:32 -0500

    BUG: Separate multiple tests (39701)

--- pandas/tests/util/test_show_versions.py
+++ pandas/tests/util/test_show_versions.py
@@ -4,7 +4,10 @@ import re
 
 import pytest
 
-from pandas.util._print_versions import _get_dependency_info, _get_sys_info
+from pandas.util._print_versions import (
+    _get_dependency_info,
+    _get_sys_info,
+)
 
 import pandas as pd
 
@@ -30,56 +33,67 @@ import pandas as pd
     "ignore:Distutils:UserWarning"
 )
 @pytest.mark.filterwarnings("ignore:Setuptools is replacing distutils:UserWarning")
-@pytest.mark.parametrize("as_json", [True, False, "test_output.json"])
-def test_show_versions(capsys, as_json, tmpdir):
-    # gh-32041
-    if isinstance(as_json, str):
-        as_json = os.path.join(tmpdir, as_json)
+def test_show_versions(tmpdir):
+    as_json = os.path.join(tmpdir, "test_output.json")
 
     pd.show_versions(as_json=as_json)
+
+    # make sure that the file was created
+    assert os.path.exists(as_json)
+
+    with open(as_json) as fd:
+        contents = fd.readlines()
+        str_contents = "".join(contents)
+
+        # make sure that there was output to the file
+        assert str_contents
+
+        # check if file output is valid JSON, will raise an exception if not
+        dict_check = json.loads(str_contents)
+
+    # Basic check that each version element is found in output
+    version_elements = {
+        "system": _get_sys_info(),
+        "dependencies": _get_dependency_info(),
+    }
+
+    assert version_elements == dict_check
+
+
+def test_show_versions_console_json(capsys):
+    pd.show_versions(as_json=True)
     captured = capsys.readouterr()
     result = captured.out
 
-    # check header for non-JSON console output
-    if as_json is False:
-        assert "INSTALLED VERSIONS" in result
+    # check valid json is printed to the console if as_json is True
+    dict_check = json.loads(result)
 
-        # check full commit hash
-        assert re.search(r"commit\s*:\s[0-9a-f]{40}\n", result)
+    # Basic check that each version element is found in output
+    version_elements = {
+        "system": _get_sys_info(),
+        "dependencies": _get_dependency_info(),
+    }
 
-        # check required dependency
-        # 2020-12-09 npdev has "dirty" in the tag
-        assert re.search(r"numpy\s*:\s([0-9\.\+a-g\_]|dev)+(dirty)?\n", result)
+    assert version_elements == dict_check
 
-        # check optional dependency
-        assert re.search(r"pyarrow\s*:\s([0-9\.]+|None)\n", result)
 
-    # Dictionary-based asserts
-    else:
-        # check valid json is printed to the console if as_json is True
-        if as_json is True:
-            dict_check = json.loads(result)
-        elif isinstance(as_json, str):
-            # make sure that the file was created
-            assert os.path.exists(as_json)
+def test_show_versions_console(capsys):
+    # gh-32041
+    pd.show_versions(as_json=False)
+    captured = capsys.readouterr()
+    result = captured.out
 
-            with open(as_json) as fd:
-                contents = fd.readlines()
-                str_contents = "".join(contents)
+    assert "INSTALLED VERSIONS" in result
 
-                # make sure that there was output to the file
-                assert str_contents
+    # check full commit hash
+    assert re.search(r"commit\s*:\s[0-9a-f]{40}\n", result)
 
-                # check if file output is valid JSON
-                dict_check = json.loads(str_contents)
+    # check required dependency
+    # 2020-12-09 npdev has "dirty" in the tag
+    assert re.search(r"numpy\s*:\s([0-9\.\+a-g\_]|dev)+(dirty)?\n", result)
 
-        # Basic check that each version element is found in output
-        version_elements = {
-            "system": _get_sys_info(),
-            "dependencies": _get_dependency_info(),
-        }
-
-        assert version_elements == dict_check
+    # check optional dependency
+    assert re.search(r"pyarrow\s*:\s([0-9\.]+|None)\n", result)
 
 
 def test_json_output_match(capsys, tmpdir):
