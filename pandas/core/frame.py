3b05b3b8938c354c17cbd544b959c535444d130f
phofl <patrick_hoefler@gmx.net>
2021-02-22 20:52:56 +0100

    Add comment to code

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -3264,6 +3264,7 @@ class DataFrame(NDFrame, OpsMixin):
             indexer = key.nonzero()[0]
             self._check_setitem_copy()
             if isinstance(value, DataFrame):
+                # GHä39931 reindex since iloc does not align
                 value = value.reindex(self.index.take(indexer))
             self.iloc[indexer] = value
         else:
