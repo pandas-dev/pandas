5ffe49f672c5fea5f8cf698c2db6be5bc158af5f
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-02-23 07:55:03 +0100

    add tests to new modules.

--- pandas/tests/io/formats/style/test_highlight.py
+++ pandas/tests/io/formats/style/test_highlight.py
@@ -1,12 +1,20 @@
 import numpy as np
 import pytest
 
-from pandas import DataFrame
+from pandas import (
+    DataFrame,
+    IndexSlice,
+)
 
 pytest.importorskip("jinja2")
 
 
 class TestStylerHighlight:
+    def setup_method(self, method):
+        np.random.seed(24)
+        self.s = DataFrame({"A": np.random.permutation(range(6))})
+        self.df = DataFrame({"A": [0, 1], "B": np.random.randn(2)})
+
     def test_highlight_null(self):
         df = DataFrame({"A": [0, np.nan]})
         result = df.style.highlight_null()._compute().ctx
@@ -28,43 +36,105 @@ class TestStylerHighlight:
         }
         assert result == expected
 
-    def test_highlight_max(self):
-        df = DataFrame([[1, 2], [3, 4]], columns=["A", "B"])
-        css_seq = [("background-color", "yellow")]
-        # max(df) = min(-df)
-        for max_ in [True, False]:
-            if max_:
-                attr = "highlight_max"
-            else:
-                df = -df
-                attr = "highlight_min"
-            result = getattr(df.style, attr)()._compute().ctx
-            assert result[(1, 1)] == css_seq
+    @pytest.mark.parametrize("f", ["highlight_min", "highlight_max"])
+    def test_highlight_minmax_basic(self, f):
+        expected = {
+            (0, 0): [("background-color", "red")],
+            (1, 0): [("background-color", "red")],
+        }
+        if f == "highlight_min":
+            df = -self.df
+        else:
+            df = self.df
+        result = getattr(df.style, f)(axis=1, color="red")._compute().ctx
+        assert result == expected
 
-            result = getattr(df.style, attr)(color="green")._compute().ctx
-            assert result[(1, 1)] == [("background-color", "green")]
+    @pytest.mark.parametrize("f", ["highlight_min", "highlight_max"])
+    @pytest.mark.parametrize(
+        "kwargs",
+        [
+            {"axis": None, "color": "red"},  # test axis
+            {"axis": 0, "subset": ["A"], "color": "red"},  # test subset
+            {"axis": None, "props": "background-color: red"},  # test props
+        ],
+    )
+    def test_highlight_minmax_ext(self, f, kwargs):
+        expected = {(1, 0): [("background-color", "red")]}
+        if f == "highlight_min":
+            df = -self.df
+        else:
+            df = self.df
+        result = getattr(df.style, f)(**kwargs)._compute().ctx
+        assert result == expected
 
-            result = getattr(df.style, attr)(subset="A")._compute().ctx
-            assert result[(1, 0)] == css_seq
+    @pytest.mark.parametrize(
+        "kwargs",
+        [
+            {"start": 0, "stop": 1},  # test basic range
+            {"start": 0, "stop": 1, "props": "background-color: yellow"},  # test props
+            {"start": -9, "stop": 9, "subset": ["A"]},  # test subset effective
+            {"start": 0},  # test no stop
+            {"stop": 1, "subset": ["A"]},  # test no start
+            {"start": [0, 1], "axis": 0},  # test start as sequence
+            {"start": DataFrame([[0, 1], [1, 1]]), "axis": None},  # test axis with seq
+            {"start": 0, "stop": [0, 1], "axis": 0},  # test sequence stop
+        ],
+    )
+    def test_highlight_range(self, kwargs):
+        expected = {
+            (0, 0): [("background-color", "yellow")],
+            (1, 0): [("background-color", "yellow")],
+        }
+        result = self.df.style.highlight_range(**kwargs)._compute().ctx
+        assert result == expected
 
-            result = getattr(df.style, attr)(axis=0)._compute().ctx
-            expected = {
-                (1, 0): css_seq,
-                (1, 1): css_seq,
-            }
-            assert result == expected
+    @pytest.mark.parametrize(
+        "kwargs",
+        [
+            {"q_low": 0.5, "q_high": 1, "axis": 1},  # test basic range
+            {"q_low": 0.5, "q_high": 1, "axis": None},  # test axis
+            {"q_low": 0, "q_high": 1, "subset": ["A"]},  # test subset
+            {"q_low": 0.5, "axis": 1},  # test no high
+            {"q_high": 1, "subset": ["A"], "axis": 0},  # test no low
+            {"q_low": 0.5, "axis": 1, "props": "background-color: yellow"},  # tst props
+        ],
+    )
+    def test_highlight_quantile(self, kwargs):
+        expected = {
+            (0, 0): [("background-color", "yellow")],
+            (1, 0): [("background-color", "yellow")],
+        }
+        result = self.df.style.highlight_quantile(**kwargs)._compute().ctx
+        assert result == expected
 
-            result = getattr(df.style, attr)(axis=1)._compute().ctx
-            expected = {
-                (0, 1): css_seq,
-                (1, 1): css_seq,
-            }
-            assert result == expected
+    @pytest.mark.skipif(
+        np.__version__[:4] in ["1.16", "1.17"], reason="Numpy Issue #14831"
+    )
+    @pytest.mark.parametrize(
+        "f,kwargs",
+        [
+            ("highlight_min", {"axis": 1, "subset": IndexSlice[1, :]}),
+            ("highlight_max", {"axis": 0, "subset": [0]}),
+            ("highlight_quantile", {"axis": None, "q_low": 0.6, "q_high": 0.8}),
+            ("highlight_range", {"subset": [0]}),
+        ],
+    )
+    @pytest.mark.parametrize(
+        "df",
+        [
+            DataFrame([[0, 1], [2, 3]], dtype=int),
+            DataFrame([[0, 1], [2, 3]], dtype=float),
+            DataFrame([[0, 1], [2, 3]], dtype="datetime64[ns]"),
+            DataFrame([[0, 1], [2, 3]], dtype=str),
+            DataFrame([[0, 1], [2, 3]], dtype="timedelta64[ns]"),
+        ],
+    )
+    def test_all_highlight_dtypes(self, f, kwargs, df):
+        if f == "highlight_quantile" and isinstance(df.iloc[0, 0], str):
+            return None  # quantile incompatible with str
+        elif f == "highlight_range":
+            kwargs["start"] = df.iloc[1, 0]  # set the range low for testing
 
-        # separate since we can't negate the strs
-        df["C"] = ["a", "b"]
-        result = df.style.highlight_max()._compute().ctx
-        expected = {(1, 1): css_seq}
-
-        result = df.style.highlight_min()._compute().ctx
-        expected = {(0, 0): css_seq}
+        expected = {(1, 0): [("background-color", "yellow")]}
+        result = getattr(df.style, f)(**kwargs)._compute().ctx
+        assert result == expected
