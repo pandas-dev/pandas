9367b1b767748fb4957b5b2b66d91714ddaf4cbf
zitorelova <zitorelova@gmail.com>
2021-02-23 03:46:18 +0000

    Edit test to include all numeric dtypes

--- pandas/tests/series/test_unary.py
+++ pandas/tests/series/test_unary.py
@@ -22,17 +22,20 @@ class TestSeriesUnaryOps:
         [
             ([1, 2, 3], [-1, -2, -3], [1, 2, 3]),
             ([1, 2, None], [-1, -2, None], [1, 2, None]),
-            ([-1, 0, 1], [1, 0, -1], [1, 0, 1]),
         ],
     )
     def test_all_numeric_unary_operators(
-        self, any_signed_nullable_numeric_dtype, source, neg_target, abs_target
+        self, any_nullable_numeric_dtype, source, neg_target, abs_target
     ):
         # GH38794
-        dtype = any_signed_nullable_numeric_dtype
+        dtype = any_nullable_numeric_dtype
         ser = Series(source, dtype=dtype)
         neg_result, pos_result, abs_result = -ser, +ser, abs(ser)
-        neg_target = Series(neg_target, dtype=dtype)
+        if dtype.startswith('U'):
+            neg_target = -Series(source, dtype=dtype)
+        else:
+            neg_target = Series(neg_target, dtype=dtype)
+
         abs_target = Series(abs_target, dtype=dtype)
 
         tm.assert_series_equal(neg_result, neg_target)
