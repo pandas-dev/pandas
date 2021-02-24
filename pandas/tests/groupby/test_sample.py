6710a7d42ebdc04ef16d8116b7ea7f124064fbbe
Wenjun Si <swj0066@gmail.com>
2021-02-25 01:58:05 +0800

    BUG: Cannot sample on DataFrameGroupBy with weights when index is specified (#40015)

--- pandas/tests/groupby/test_sample.py
+++ pandas/tests/groupby/test_sample.py
@@ -116,14 +116,19 @@ def test_groupby_sample_without_n_or_frac():
     tm.assert_series_equal(result, expected)
 
 
-def test_groupby_sample_with_weights():
+@pytest.mark.parametrize(
+    "index, expected_index",
+    [(["w", "x", "y", "z"], ["w", "w", "y", "y"]), ([3, 4, 5, 6], [3, 3, 5, 5])],
+)
+def test_groupby_sample_with_weights(index, expected_index):
+    # GH 39927 - tests for integer index needed
     values = [1] * 2 + [2] * 2
-    df = DataFrame({"a": values, "b": values}, index=Index(["w", "x", "y", "z"]))
+    df = DataFrame({"a": values, "b": values}, index=Index(index))
 
     result = df.groupby("a").sample(n=2, replace=True, weights=[1, 0, 1, 0])
-    expected = DataFrame({"a": values, "b": values}, index=Index(["w", "w", "y", "y"]))
+    expected = DataFrame({"a": values, "b": values}, index=Index(expected_index))
     tm.assert_frame_equal(result, expected)
 
     result = df.groupby("a")["b"].sample(n=2, replace=True, weights=[1, 0, 1, 0])
-    expected = Series(values, name="b", index=Index(["w", "w", "y", "y"]))
+    expected = Series(values, name="b", index=Index(expected_index))
     tm.assert_series_equal(result, expected)
