83ff71592991e975a83976301434bf84460d824c
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-03-02 22:34:14 +0100

    PERF: don't sort data twice in groupby apply when not using libreduction fast_apply (#40176)

--- pandas/tests/groupby/test_apply.py
+++ pandas/tests/groupby/test_apply.py
@@ -113,7 +113,7 @@ def test_fast_apply():
 
     splitter = grouper._get_splitter(g._selected_obj, axis=g.axis)
     group_keys = grouper._get_group_keys()
-    sdata = splitter._get_sorted_data()
+    sdata = splitter.sorted_data
 
     values, mutated = splitter.fast_apply(f, sdata, group_keys)
 
