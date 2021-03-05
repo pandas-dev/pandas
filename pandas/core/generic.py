63f30f5cabaa31d94f091a6f17419c022427c5e2
Brock <jbrockmendel@gmail.com>
2021-03-04 16:24:17 -0800

    PERF: non-cython groupby.apply

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -276,6 +276,22 @@ class NDFrame(PandasObject, SelectionMixin, indexing.IndexingMixin):
                 mgr = mgr.astype(dtype=dtype)
         return mgr
 
+    @classmethod
+    def _from_mgr(cls, mgr: Manager):
+        """
+        Fastpath to create a new DataFrame/Series from just a BlockManager/ArrayManager.
+
+        Notes
+        -----
+        Skips setting `_flags` attribute; caller is responsible for doing so.
+        """
+        obj = cls.__new__(cls)
+        object.__setattr__(obj, "_is_copy", None)
+        object.__setattr__(obj, "_mgr", mgr)
+        object.__setattr__(obj, "_item_cache", {})
+        object.__setattr__(obj, "_attrs", {})
+        return obj
+
     # ----------------------------------------------------------------------
     # attrs and flags
 
