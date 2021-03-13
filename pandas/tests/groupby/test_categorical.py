f049151c6ddf0cddbf5214cdeb37fa8f0a134112
ftrihardjo <felixtrihardjo@gmail.com>
2021-03-13 10:43:53 +0700

    pandas-dev issue #24271

--- pandas/tests/groupby/test_categorical.py
+++ pandas/tests/groupby/test_categorical.py
@@ -256,7 +256,7 @@ def test_sorting_with_different_categoricals():
     index = ["low", "med", "high", "low", "med", "high"]
     index = Categorical(index, categories=["low", "med", "high"], ordered=True)
     index = [["A", "A", "A", "B", "B", "B"], CategoricalIndex(index)]
-    index = MultiIndex.from_arrays(index)
+    index = MultiIndex.from_arrays(index, names=["group"])
     expected = Series([2] * 6, index=index)
     tm.assert_series_equal(result, expected)
 
