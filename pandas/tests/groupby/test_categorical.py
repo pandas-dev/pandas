0f1f9da889709885ed687d56152ad36e11620b7a
ftrihardjo <felixtrihardjo@gmail.com>
2021-03-13 08:22:28 +0700

    pandas-dev issue #24271

--- pandas/tests/groupby/test_categorical.py
+++ pandas/tests/groupby/test_categorical.py
@@ -249,12 +249,12 @@ def test_sorting_with_different_categoricals():
         }
     )
 
-    df.dose = pd.Categorical(df.dose, categories=["low", "med", "high"], ordered=True)
+    df.dose = Categorical(df.dose, categories=["low", "med", "high"], ordered=True)
 
     result = df.groupby("group")["dose"].value_counts()
     result = result.sort_index(level=0, sort_remaining=True)
     index = ["low", "med", "high", "low", "med", "high"]
-    index = pd.Categorical(index, categories=["low", "med", "high"], ordered=True)
+    index = Categorical(index, categories=["low", "med", "high"], ordered=True)
     index = [["A", "A", "A", "B", "B", "B"], CategoricalIndex(index)]
     index = MultiIndex.from_arrays(index, names=["group", "dose"])
     expected = Series([2] * 6, index=index, name="dose")
