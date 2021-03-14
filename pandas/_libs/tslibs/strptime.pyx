889e65f44cea6550070a7593c28a59f5a1973cc2
Brock <jbrockmendel@gmail.com>
2021-03-14 11:15:58 -0700

    TYP: stubs for tslibs

--- pandas/_libs/tslibs/strptime.pyx
+++ pandas/_libs/tslibs/strptime.pyx
@@ -78,11 +78,12 @@ def array_strptime(ndarray[object] values, object fmt, bint exact=True, errors='
         int week_of_year, week_of_year_start, parse_code, ordinal
         int iso_week, iso_year
         int64_t us, ns
-        object val, group_key, ampm, found, timezone
+        object val, group_key, ampm, found
         dict found_key
         bint is_raise = errors=='raise'
         bint is_ignore = errors=='ignore'
         bint is_coerce = errors=='coerce'
+        tzinfo timezone
 
     assert is_raise or is_ignore or is_coerce
 
