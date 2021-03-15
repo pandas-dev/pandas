cf0e90ab69242aa9bc0dda4c3878b11bf04d0c25
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-03-15 14:16:50 +0100

    update typing error

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -685,7 +685,9 @@ class BaseGrouper:
             # e.g. if we are int64 and need to restore to datetime64/timedelta64
             # "rank" is the only member of cython_cast_blocklist we get here
             dtype = maybe_cast_result_dtype(orig_values.dtype, how)
-            result = maybe_downcast_to_dtype(result, dtype)
+            # error: Incompatible types in assignment (expression has type
+            # "Union[ExtensionArray, ndarray]", variable has type "ndarray")
+            result = maybe_downcast_to_dtype(result, dtype)  # type: ignore[assignment]
 
         return result
 
