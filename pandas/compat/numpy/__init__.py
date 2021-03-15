b8a71f33149755286561628e2d71206d7c1faeaa
jbrockmendel <jbrockmendel@gmail.com>
2021-03-15 04:35:46 -0700

    TYP: fix type ignores (#40434)

--- pandas/compat/numpy/__init__.py
+++ pandas/compat/numpy/__init__.py
@@ -27,41 +27,39 @@ if _nlv < _min_numpy_ver:
 _tz_regex = re.compile("[+-]0000$")
 
 
-def tz_replacer(s):
-    if isinstance(s, str):
-        if s.endswith("Z"):
-            s = s[:-1]
-        elif _tz_regex.search(s):
-            s = s[:-5]
-    return s
+def _tz_replacer(tstring):
+    if isinstance(tstring, str):
+        if tstring.endswith("Z"):
+            tstring = tstring[:-1]
+        elif _tz_regex.search(tstring):
+            tstring = tstring[:-5]
+    return tstring
 
 
-def np_datetime64_compat(s, *args, **kwargs):
+def np_datetime64_compat(tstring: str, unit: str = "ns"):
     """
     provide compat for construction of strings to numpy datetime64's with
     tz-changes in 1.11 that make '2015-01-01 09:00:00Z' show a deprecation
     warning, when need to pass '2015-01-01 09:00:00'
     """
-    s = tz_replacer(s)
-    # error: No overload variant of "datetime64" matches argument types "Any",
-    # "Tuple[Any, ...]", "Dict[str, Any]"
-    return np.datetime64(s, *args, **kwargs)  # type: ignore[call-overload]
+    tstring = _tz_replacer(tstring)
+    return np.datetime64(tstring, unit)
 
 
-def np_array_datetime64_compat(arr, *args, **kwargs):
+def np_array_datetime64_compat(arr, dtype="M8[ns]"):
     """
     provide compat for construction of an array of strings to a
     np.array(..., dtype=np.datetime64(..))
     tz-changes in 1.11 that make '2015-01-01 09:00:00Z' show a deprecation
     warning, when need to pass '2015-01-01 09:00:00'
     """
-    # is_list_like
+    # is_list_like; can't import as it would be circular
     if hasattr(arr, "__iter__") and not isinstance(arr, (str, bytes)):
-        arr = [tz_replacer(s) for s in arr]
+        arr = [_tz_replacer(s) for s in arr]
     else:
-        arr = tz_replacer(arr)
+        arr = _tz_replacer(arr)
 
-    return np.array(arr, *args, **kwargs)
+    return np.array(arr, dtype=dtype)
 
 
 __all__ = [
