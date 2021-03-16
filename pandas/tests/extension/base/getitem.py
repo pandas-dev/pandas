6f45917079e599b94fea177d89493fe598508d9d
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-03-16 09:14:00 +0100

    update test

--- pandas/tests/extension/base/getitem.py
+++ pandas/tests/extension/base/getitem.py
@@ -388,6 +388,8 @@ class BaseGetitemTests(BaseExtensionTests):
         # see GH-27785 take_nd with indexer of len 1 resulting in wrong ndim
         df = pd.DataFrame({"A": data})
         res = df.loc[[0], "A"]
+        assert res.ndim == 1
+        assert res._mgr.arrays[0].ndim == 1
         if hasattr(res._mgr, "blocks"):
             assert res._mgr._block.ndim == 1
 
