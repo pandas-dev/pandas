a2ab5ab251c7da4246ed54742cc17285b1f781ef
Brock <jbrockmendel@gmail.com>
2021-03-16 08:11:00 -0700

    update per suggestions

--- pandas/_libs/ops.pyi
+++ pandas/_libs/ops.pyi
@@ -5,39 +5,39 @@ from typing import (
 
 import numpy as np
 
-binop = Callable[[Any, Any], Any]
-bool_op = Callable[[Any, Any], bool]
+_BinOp = Callable[[Any, Any], Any]
+_BoolOp = Callable[[Any, Any], bool]
 
 
 def scalar_compare(
     values: np.ndarray,  # object[:]
     val: object,
-    op: bool_op,          # {operator.eq, operator.ne, ...}
+    op: _BoolOp,          # {operator.eq, operator.ne, ...}
 ) -> np.ndarray: ...     # np.ndarray[bool]
 
 def vec_compare(
     left: np.ndarray,   # np.ndarray[object]
     right: np.ndarray,  # np.ndarray[object]
-    op: bool_op,         # {operator.eq, operator.ne, ...}
+    op: _BoolOp,         # {operator.eq, operator.ne, ...}
 ) -> np.ndarray: ...    # np.ndarray[bool]
 
 
 def scalar_binop(
     values: np.ndarray,   # object[:]
     val: object,
-    op: binop,           # binary operator
+    op: _BinOp,           # binary operator
 ) -> np.ndarray: ...
 
 
 def vec_binop(
     left: np.ndarray,   # object[:]
     right: np.ndarray,  # object[:]
-    op: binop,         # binary operator
+    op: _BinOp,         # binary operator
 ) -> np.ndarray: ...
 
 
 def maybe_convert_bool(
     arr: np.ndarray,  # np.ndarray[object]
-    true_values=None,
-    false_values=None
+    true_values=...,
+    false_values=...
 ) -> np.ndarray: ...
