40d65d30ef2294731e11e0e8f0c6be7cdc133aa7
Andrew Hawyrluk <50434302+ahawryluk@users.noreply.github.com>
2021-03-16 14:31:40 -0600

    TST: csv parser segfaulting on malformed input. (#40458)

--- pandas/tests/io/parser/common/test_common_basic.py
+++ pandas/tests/io/parser/common/test_common_basic.py
@@ -753,3 +753,12 @@ def test_encoding_surrogatepass(all_parsers):
         tm.assert_frame_equal(df, expected)
         with pytest.raises(UnicodeDecodeError, match="'utf-8' codec can't decode byte"):
             parser.read_csv(path)
+
+
+def test_malformed_second_line(all_parsers):
+    # see GH14782
+    parser = all_parsers
+    data = "\na\nb\n"
+    result = parser.read_csv(StringIO(data), skip_blank_lines=False, header=1)
+    expected = DataFrame({"a": ["b"]})
+    tm.assert_frame_equal(result, expected)
