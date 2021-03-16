6db2ea315716dea120b259cd8e1856f93d139c00
Brock <jbrockmendel@gmail.com>
2021-03-16 12:02:13 -0700

    remove extraneous info

--- pandas/_libs/hashing.pyi
+++ pandas/_libs/hashing.pyi
@@ -3,5 +3,5 @@ import numpy as np
 def hash_object_array(
     arr: np.ndarray,  # np.ndarray[object]
     key: str,
-    encoding: str = "utf8",
+    encoding: str = ...,
 ) -> np.ndarray: ...  # np.ndarray[np.uint64]
--- pandas/_libs/ops_dispatch.pyi
+++ pandas/_libs/ops_dispatch.pyi
@@ -1,9 +1,5 @@
 import numpy as np
 
-REVERSED_NAMES: dict[str, str]
-UFUNC_ALIASES: dict[str, str]
-DISPATCHED_UFUNCS: set[str]
-
 def maybe_dispatch_ufunc_to_dunder_op(
     self, ufunc: np.ufunc, method: str, *inputs, **kwargs
 ): ...
