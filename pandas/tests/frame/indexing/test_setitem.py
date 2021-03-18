36b3302718d5aa52ccd28c7fadc66af57434ae92
Brock <jbrockmendel@gmail.com>
2021-03-18 13:42:03 -0700

    troublehsoot

--- pandas/tests/frame/indexing/test_setitem.py
+++ pandas/tests/frame/indexing/test_setitem.py
@@ -936,6 +936,7 @@ class TestDataFrameSetitemCopyViewSemantics:
         df[["B"]] = value
 
         expected = DataFrame([[0, 1], [0, 1]], columns=cols, dtype=np.intp)
+        assert (expected.dtypes == np.intp).all()  # troubleshoot 32bit builds
         tm.assert_frame_equal(df, expected)
         tm.assert_frame_equal(df_view, df_copy)
 
