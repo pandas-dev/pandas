04bc955dec412715931c67ccd96319838cacb562
Brock <jbrockmendel@gmail.com>
2021-03-17 18:38:25 -0700

    CLN: simplify take_nd and helpers

--- pandas/core/array_algos/take.py
+++ pandas/core/array_algos/take.py
@@ -112,8 +112,12 @@ def _take_nd_ndarray(
         dtype, fill_value = arr.dtype, arr.dtype.type()
     else:
         indexer = ensure_int64(indexer, copy=False)
-    indexer, dtype, fill_value, mask_info = _take_preprocess_indexer_and_fill_value(
-        arr, indexer, fill_value, allow_fill
+
+    if not allow_fill:
+        return arr.take(indexer, axis=axis)
+
+    dtype, fill_value, mask_info = _take_preprocess_indexer_and_fill_value(
+        arr, indexer, fill_value
     )
 
     flip_order = False
@@ -180,8 +184,8 @@ def take_1d(
     if not allow_fill:
         return arr.take(indexer)
 
-    indexer, dtype, fill_value, mask_info = _take_preprocess_indexer_and_fill_value(
-        arr, indexer, fill_value, allow_fill
+    dtype, fill_value, mask_info = _take_preprocess_indexer_and_fill_value(
+        arr, indexer, fill_value
     )
 
     # at this point, it's guaranteed that dtype can hold both the arr values
@@ -506,26 +510,21 @@ def _take_preprocess_indexer_and_fill_value(
     arr: np.ndarray,
     indexer: np.ndarray,
     fill_value,
-    allow_fill: bool,
 ):
     mask_info = None
 
-    if not allow_fill:
-        dtype, fill_value = arr.dtype, arr.dtype.type()
-        mask_info = None, False
-    else:
-        # check for promotion based on types only (do this first because
-        # it's faster than computing a mask)
-        dtype, fill_value = maybe_promote(arr.dtype, fill_value)
-        if dtype != arr.dtype:
-            # check if promotion is actually required based on indexer
-            mask = indexer == -1
-            needs_masking = mask.any()
-            mask_info = mask, needs_masking
-            if not needs_masking:
-                # if not, then depromote, set fill_value to dummy
-                # (it won't be used but we don't want the cython code
-                # to crash when trying to cast it to dtype)
-                dtype, fill_value = arr.dtype, arr.dtype.type()
+    # check for promotion based on types only (do this first because
+    # it's faster than computing a mask)
+    dtype, fill_value = maybe_promote(arr.dtype, fill_value)
+    if dtype != arr.dtype:
+        # check if promotion is actually required based on indexer
+        mask = indexer == -1
+        needs_masking = mask.any()
+        mask_info = mask, needs_masking
+        if not needs_masking:
+            # if not, then depromote, set fill_value to dummy
+            # (it won't be used but we don't want the cython code
+            # to crash when trying to cast it to dtype)
+            dtype, fill_value = arr.dtype, arr.dtype.type()
 
-    return indexer, dtype, fill_value, mask_info
+    return dtype, fill_value, mask_info
