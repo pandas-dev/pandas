48d50118323b12b92494d7db1d524a61774edc06
vladu <vovansim@gmail.com>
2021-03-18 15:25:48 -0500

    DOC: One more little fix to groupby.apply function doc, for consistency.

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -157,9 +157,9 @@ _apply_docs = {
 
     Notes
     -----
-    In the current implementation ``apply`` calls `func` twice on the
+    In the current implementation ``apply`` calls ``func`` twice on the
     first group to decide whether it can take a fast or slow code
-    path. This can lead to unexpected behavior if `func` has
+    path. This can lead to unexpected behavior if ``func`` has
     side-effects, as they will take effect twice for the first
     group.
 
