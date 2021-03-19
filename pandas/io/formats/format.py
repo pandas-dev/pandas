2a074d23d5f1462810eed2b0e694c16662c2fdf8
Brock <jbrockmendel@gmail.com>
2021-03-19 08:14:30 -0700

    ignore in get_format_timedelt64

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -1799,14 +1799,20 @@ def get_format_timedelta64(
 
     If box, then show the return in quotes
     """
-    values_int = np.asarray(values.view(np.int64))
+    values_int = values.view(np.int64)
 
     consider_values = values_int != iNaT
 
     one_day_nanos = 86400 * 10 ** 9
-    even_days = (
-        np.logical_and(consider_values, values_int % one_day_nanos != 0).sum() == 0
-    )
+    # error: Unsupported operand types for % ("ExtensionArray" and "int")
+    not_midnight = values_int % one_day_nanos != 0  # type: ignore[operator]
+    # error: Argument 1 to "__call__" of "ufunc" has incompatible type
+    # "Union[Any, ExtensionArray, ndarray]"; expected
+    # "Union[Union[int, float, complex, str, bytes, generic],
+    # Sequence[Union[int, float, complex, str, bytes, generic]],
+    # Sequence[Sequence[Any]], _SupportsArray]"
+    both = np.logical_and(consider_values, not_midnight)  # type: ignore[arg-type]
+    even_days = both.sum() == 0
 
     if even_days:
         format = None
