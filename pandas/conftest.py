a8e47a4110d8b57aa1fcee5a9164f70fa6f409ed
Pawel Kranzberg <pawel@kranzberg.co>
2021-03-19 14:21:49 +0100

    Rename startingMonth to month

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -162,7 +162,7 @@ for name in "QuarterBegin QuarterEnd BQuarterBegin BQuarterEnd".split():
             cls,
             n=st.integers(-24, 24),
             normalize=st.booleans(),
-            startingMonth=st.integers(min_value=1, max_value=12),
+            month=st.integers(min_value=1, max_value=12),
         ),
     )
 
