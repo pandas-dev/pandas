08f63684cfb749dd0e03558c42b03c57141f87dd
Brock <jbrockmendel@gmail.com>
2021-03-20 21:05:52 -0700

    TYP: internals.pyi

---
+++ pandas/_libs/internals.pyi
@@ -0,0 +1,48 @@
+from typing import (
+    Iterator,
+    Sequence,
+    overload,
+)
+
+import numpy as np
+
+def slice_len(slc: slice, objlen: int = ...) -> int: ...
+
+
+def get_blkno_indexers(
+    blknos: np.ndarray,  # int64_t[:]
+    group: bool = ...,
+) -> list[tuple[int, slice | np.ndarray]]: ...
+
+
+def get_blkno_placements(
+    blknos: np.ndarray,
+    group: bool = ...,
+) -> Iterator[tuple[int, BlockPlacement]]: ...
+
+
+class BlockPlacement:
+    def __init__(self, val: int | slice | np.ndarray): ...
+
+    @property
+    def indexer(self) -> np.ndarray | slice: ...
+
+    @property
+    def as_array(self) -> np.ndarray: ...
+
+    @property
+    def is_slice_like(self) -> bool: ...
+
+    @overload
+    def __getitem__(self, loc: slice | Sequence[int]) -> BlockPlacement: ...
+
+    @overload
+    def __getitem__(self, loc: int) -> int: ...
+
+    def __iter__(self) -> Iterator[int]: ...
+
+    def __len__(self) -> int: ...
+
+    def delete(self, loc) -> BlockPlacement: ...
+
+    def append(self, others: list[BlockPlacement]) -> BlockPlacement: ...
