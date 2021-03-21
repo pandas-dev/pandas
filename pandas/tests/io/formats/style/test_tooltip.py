39c774b3c5a263cd1c26021f03da3cd26d18c8b6
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-03-21 09:00:27 +0100

    test_tooltip convert to functional tests not class

--- pandas/tests/io/formats/style/test_tooltip.py
+++ pandas/tests/io/formats/style/test_tooltip.py
@@ -24,13 +24,13 @@ def s(df):
 @pytest.mark.parametrize(
     "ttips",
     [
-        DataFrame(  # Test basic and ignoring blank
+        DataFrame(  # Test basic reindex and ignoring blank
             data=[["Min", "Max"], [np.nan, ""]],
-            columns=["A", "B"],
+            columns=["A", "C"],
             index=["x", "y"],
         ),
-        DataFrame(  # Test non-referenced columns and reversed col names reindex
-            data=[["Max", "Min", "Bad-Col"]], columns=["B", "A", "D"], index=["x"]
+        DataFrame(  # Test non-referenced columns, reversed col names, short index
+            data=[["Max", "Min", "Bad-Col"]], columns=["C", "A", "D"], index=["x"]
         ),
     ],
 )
@@ -47,14 +47,14 @@ def test_tooltip_render(ttips, s):
     assert 'class="data row0 col0" >0<span class="pd-t"></span></td>' in result
 
     # test 'Max' tooltip added
-    assert "#T__ #T__row0_col1:hover .pd-t {\n  visibility: visible;\n}" in result
-    assert '#T__ #T__row0_col1 .pd-t::after {\n  content: "Max";\n}' in result
-    assert 'class="data row0 col1" >1<span class="pd-t"></span></td>' in result
+    assert "#T__ #T__row0_col2:hover .pd-t {\n  visibility: visible;\n}" in result
+    assert '#T__ #T__row0_col2 .pd-t::after {\n  content: "Max";\n}' in result
+    assert 'class="data row0 col2" >2<span class="pd-t"></span></td>' in result
 
     # test Nan, empty string and bad column ignored
     assert "#T__ #T__row1_col0:hover .pd-t {\n  visibility: visible;\n}" not in result
     assert "#T__ #T__row1_col1:hover .pd-t {\n  visibility: visible;\n}" not in result
-    assert "#T__ #T__row0_col2:hover .pd-t {\n  visibility: visible;\n}" not in result
+    assert "#T__ #T__row0_col1:hover .pd-t {\n  visibility: visible;\n}" not in result
     assert "#T__ #T__row1_col2:hover .pd-t {\n  visibility: visible;\n}" not in result
     assert "Bad-Col" not in result
 
