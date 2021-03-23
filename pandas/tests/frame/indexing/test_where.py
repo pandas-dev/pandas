b3ff39f5c39b7205e4e8dbcc54ec927ff8db1234
Matthew Zeitlin <mzeitlin@caltech.edu>
2021-03-23 11:52:28 -0400

    Add an additional test case

--- pandas/tests/frame/indexing/test_where.py
+++ pandas/tests/frame/indexing/test_where.py
@@ -695,11 +695,17 @@ def test_where_try_cast_deprecated(frame_or_series):
 
 
 def test_where_copies_with_noop(frame_or_series):
+    # GH-39595
     result = frame_or_series([1, 2, 3, 4])
     expected = result.copy()
     col = result[0] if frame_or_series is DataFrame else result
 
     where_res = result.where(col < 5)
+    where_res *= 2
+
+    tm.assert_equal(result, expected)
+
+    where_res = result.where(col > 5, [1, 2, 3, 4])
     where_res *= 2
 
     tm.assert_equal(result, expected)
