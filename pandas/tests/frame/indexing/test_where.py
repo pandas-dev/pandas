558490529cab99e717dba236068864d40100188f
Matthew Zeitlin <mzeitlin@caltech.edu>
2021-03-23 11:43:53 -0400

    Add test

--- pandas/tests/frame/indexing/test_where.py
+++ pandas/tests/frame/indexing/test_where.py
@@ -698,7 +698,7 @@ def test_where_copies_with_noop(frame_or_series):
     result = frame_or_series([1, 2, 3, 4])
     expected = result.copy()
     col = result[0] if frame_or_series is DataFrame else result
-    
+
     where_res = result.where(col < 5)
     where_res *= 2
     tm.assert_equal(result, expected)
