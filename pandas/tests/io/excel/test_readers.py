bb21aacc65249968fd55e3dd367b7db2a57fd083
Andrew Hawryluk <ahawryluk@gmail.com>
2021-03-26 12:01:02 -0600

    Linting fix

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -1196,7 +1196,7 @@ class TestReaders:
     def test_multiheader_two_blank_lines(self, read_ext):
         # GH 40442
         file_name = "testmultiindex" + read_ext
-        columns = pd.MultiIndex.from_tuples([("a", "A"), ("b", "B")])
+        columns = MultiIndex.from_tuples([("a", "A"), ("b", "B")])
         data = [[np.nan, np.nan], [np.nan, np.nan], [1, 3], [2, 4]]
         expected = DataFrame(data, columns=columns)
         result = pd.read_excel(
