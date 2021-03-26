114e0d92d57a17c81afa7944b3e18b2b52e2f8ec
Simon Hawkins <simonjayhawkins@gmail.com>
2021-03-26 13:04:24 +0000

    Backport PR #40604: REGR: replace with multivalued regex raising (#40648)
    Co-authored-by: Matthew Zeitlin <37011898+mzeitlin11@users.noreply.github.com>

--- pandas/tests/frame/methods/test_replace.py
+++ pandas/tests/frame/methods/test_replace.py
@@ -644,6 +644,28 @@ class TestDataFrameReplace:
         tm.assert_frame_equal(res, expec)
         assert res.a.dtype == np.object_
 
+    @pytest.mark.parametrize(
+        "to_replace", [{"": np.nan, ",": ""}, {",": "", "": np.nan}]
+    )
+    def test_joint_simple_replace_and_regex_replace(self, to_replace):
+        # GH-39338
+        df = DataFrame(
+            {
+                "col1": ["1,000", "a", "3"],
+                "col2": ["a", "", "b"],
+                "col3": ["a", "b", "c"],
+            }
+        )
+        result = df.replace(regex=to_replace)
+        expected = DataFrame(
+            {
+                "col1": ["1000", "a", "3"],
+                "col2": ["a", np.nan, "b"],
+                "col3": ["a", "b", "c"],
+            }
+        )
+        tm.assert_frame_equal(result, expected)
+
     @pytest.mark.parametrize("metachar", ["[]", "()", r"\d", r"\w", r"\s"])
     def test_replace_regex_metachar(self, metachar):
         df = DataFrame({"a": [metachar, "else"]})
