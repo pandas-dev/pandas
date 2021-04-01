c6cf9ee54b77e5b18e89f272e5cf4d129ea2d0fd
Matthew Zeitlin <mzeitlin@caltech.edu>
2021-04-01 12:30:32 -0400

    Shrink benchmark

--- asv_bench/benchmarks/groupby.py
+++ asv_bench/benchmarks/groupby.py
@@ -501,15 +501,21 @@ class CumminMax:
     ]
 
     def setup(self, dtype, method):
-        N = 1_000_000
-        vals = np.random.randint(0, 1000, (N, 10))
+        N = 500_000
+        rg = np.random.default_rng(seed=0)
+        vals = rg.integers(
+            0,
+            1000,
+            (N, 5),
+        )
         null_vals = vals.astype(float, copy=True)
         null_vals[::2, :] = np.nan
         null_vals[::3, :] = np.nan
-        df = DataFrame(vals, columns=list("abcdefghij"), dtype=dtype)
-        null_df = DataFrame(null_vals, columns=list("abcdefghij"), dtype=dtype)
-        df["key"] = np.random.randint(0, 100, size=N)
-        null_df["key"] = np.random.randint(0, 100, size=N)
+        df = DataFrame(vals, columns=list("abcde"), dtype=dtype)
+        null_df = DataFrame(null_vals, columns=list("abcde"), dtype=dtype)
+        keys = rg.integers(0, 100, size=N)
+        df["key"] = keys
+        null_df["key"] = keys
         self.df = df
         self.null_df = null_df
 
