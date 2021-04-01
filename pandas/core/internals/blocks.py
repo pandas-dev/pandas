9444137f54d4cbbc0c32a95dea4c638218c60e34
Brock <jbrockmendel@gmail.com>
2021-04-01 16:25:17 -0700

    mypy fixup

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -2190,6 +2190,7 @@ def ensure_block_shape(values: ArrayLike, ndim: int = 1) -> ArrayLike:
             # TODO(EA2D): https://github.com/pandas-dev/pandas/issues/23023
             # block.shape is incorrect for "2D" ExtensionArrays
             # We can't, and don't need to, reshape.
+            values = cast(Union[np.ndarray, DatetimeArray, TimedeltaArray], values)
             values = values.reshape(1, -1)
 
     return values
