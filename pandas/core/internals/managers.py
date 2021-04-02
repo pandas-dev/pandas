547f9dbb7e2bcd8686a9158afdf0de10fd0425bb
jbrockmendel <jbrockmendel@gmail.com>
2021-04-02 08:18:43 -0700

    REF: remove DatetimeBlock, TimeDeltaBlock (#40614)

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -35,7 +35,6 @@ from pandas.util._validators import validate_bool_kwarg
 
 from pandas.core.dtypes.cast import infer_dtype_from_scalar
 from pandas.core.dtypes.common import (
-    DT64NS_DTYPE,
     ensure_int64,
     is_dtype_equal,
     is_extension_array_dtype,
@@ -1639,7 +1638,7 @@ class SingleBlockManager(BlockManager, SingleDataManager):
 
     def internal_values(self):
         """The array that Series._values returns"""
-        return self._block.internal_values()
+        return self._block.values
 
     def array_values(self):
         """The array that Series.array returns"""
@@ -1794,17 +1793,11 @@ def _form_blocks(
         )
         blocks.extend(numeric_blocks)
 
-    if len(items_dict["TimeDeltaBlock"]):
-        timedelta_blocks = _multi_blockify(
-            items_dict["TimeDeltaBlock"], consolidate=consolidate
+    if len(items_dict["DatetimeLikeBlock"]):
+        dtlike_blocks = _multi_blockify(
+            items_dict["DatetimeLikeBlock"], consolidate=consolidate
         )
-        blocks.extend(timedelta_blocks)
-
-    if len(items_dict["DatetimeBlock"]):
-        datetime_blocks = _simple_blockify(
-            items_dict["DatetimeBlock"], DT64NS_DTYPE, consolidate=consolidate
-        )
-        blocks.extend(datetime_blocks)
+        blocks.extend(dtlike_blocks)
 
     if len(items_dict["DatetimeTZBlock"]):
         dttz_blocks = [
