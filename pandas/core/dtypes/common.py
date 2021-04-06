432ed7de630481918322feffd0328619edd2a6fc
DriesS <dries.schaumont@gmail.com>
2021-04-06 21:18:52 +0200

    BUG: Do not allow boolean values for indexing with .loc without a boolean index. (#40726)
    * Add boolean loc tests.
    
    * BUG: Raise TypeError when using boolean indexer with loc.
    
    * Fix styling issues.
    
    * Adjustments for review.
    
    * Add whatsnew entry.
    
    * Switch boolean statements.
    
    * Switch to KeyError.
    
    * Switch to KeyError pt.2
    
    * Remove stray dot in whatsnew
    
    * Fix DeprecationWarning.
    
    * Fix DeprecationWarning pt.2
    
    * Adjustments to comments.
    
    * Adjustments to comments pt. 2.

--- pandas/core/dtypes/common.py
+++ pandas/core/dtypes/common.py
@@ -1386,7 +1386,7 @@ def is_bool_dtype(arr_or_dtype) -> bool:
         # we don't have a boolean Index class
         # so its object, we need to infer to
         # guess this
-        return arr_or_dtype.is_object and arr_or_dtype.inferred_type == "boolean"
+        return arr_or_dtype.is_object() and arr_or_dtype.inferred_type == "boolean"
     elif is_extension_array_dtype(arr_or_dtype):
         return getattr(dtype, "_is_boolean", False)
 
