4f318374d17709f464696428de5a2607ad33d6b3
Dries Schaumont <dries.schaumont@gmail.com>
2021-04-09 10:30:25 +0200

    Check test expected result.

--- pandas/tests/groupby/aggregate/test_aggregate.py
+++ pandas/tests/groupby/aggregate/test_aggregate.py
@@ -1228,4 +1228,9 @@ def test_groupby_index_object_dtype():
     df = DataFrame({"c0": ["x", "x", "x"], "c1": ["x", "x", "y"], "p": [0, 1, 2]})
     df.index = df.index.astype("O")
     grouped = df.groupby(["c0", "c1"])
-    grouped.p.agg(lambda x: all(x > 0))
+    res = grouped.p.agg(lambda x: all(x > 0))
+    expected_index = MultiIndex.from_tuples(
+        [("x", "x"), ("x", "y")], names=("c0", "c1")
+    )
+    expected = Series([False, True], index=expected_index, name="p")
+    tm.assert_series_equal(res, expected)
