d821f037a1c2a367bf7a73b3669f74f49fe31026
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-04-09 08:53:56 +0200

    DOC: use the sphinx-panels extension for cards instead of raw html (#34065)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -65,6 +65,7 @@ extensions = [
     "sphinx.ext.ifconfig",
     "sphinx.ext.linkcode",
     "nbsphinx",
+    "sphinx_panels",
     "contributors",  # custom pandas extension
 ]
 
@@ -138,6 +139,10 @@ import pandas as pd"""
 
 # nbsphinx do not use requirejs (breaks bootstrap)
 nbsphinx_requirejs_path = ""
+
+# sphinx-panels shouldn't add bootstrap css since the pydata-sphinx-theme
+# already loads it
+panels_add_bootstrap_css = False
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["../_templates"]
