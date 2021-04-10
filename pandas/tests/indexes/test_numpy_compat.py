b5a83fc2bd4a227dcc8c913acb4772ce2ae75275
Fangchen Li <fangchen.li@outlook.com>
2021-04-10 09:35:19 -0500

    debug rng related compat check

--- pandas/tests/indexes/test_numpy_compat.py
+++ pandas/tests/indexes/test_numpy_compat.py
@@ -82,7 +82,10 @@ def test_numpy_ufuncs_other(index, func, request):
             isinstance(index, DatetimeIndex)
             and index.tz is not None
             and func in [np.isfinite, np.isnan, np.isinf]
-            and not np_version_under1p18
+            and (
+                not np_version_under1p18
+                or (np_version_under1p18 and func is np.isfinite)
+            )
         ):
             mark = pytest.mark.xfail(reason="__array_ufunc__ is not defined")
             request.node.add_marker(mark)
