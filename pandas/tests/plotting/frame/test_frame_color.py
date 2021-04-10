c1ae650fc176421f5d19962d418b400c7aeb8765
Yeshwanth N <yeshsurya@gmail.com>
2021-04-10 14:38:31 +0530

    TST : Fixed pre-commit errors for file - test_frame_color.py

--- pandas/tests/plotting/frame/test_frame_color.py
+++ pandas/tests/plotting/frame/test_frame_color.py
@@ -546,7 +546,13 @@ class TestDataFrameColor(TestPlotBase):
 
         df = DataFrame(np.random.randn(5, 5))
         bp = df.plot.box(return_type="dict")
-        _check_colors(bp, default_colors[0], default_colors[0], default_colors[2], default_colors[0])
+        _check_colors(
+            bp,
+            default_colors[0],
+            default_colors[0],
+            default_colors[2],
+            default_colors[0],
+        )
         tm.close()
 
         dict_colors = {
@@ -569,7 +575,7 @@ class TestDataFrameColor(TestPlotBase):
         # partial colors
         dict_colors = {"whiskers": "c", "medians": "m"}
         bp = df.plot.box(color=dict_colors, return_type="dict")
-        _check_colors(bp, default_colors[0], "c", "m",default_colors[0])
+        _check_colors(bp, default_colors[0], "c", "m", default_colors[0])
         tm.close()
 
         from matplotlib import cm
