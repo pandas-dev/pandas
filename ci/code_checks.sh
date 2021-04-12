65e4ff40831535a2582f05984f78663e40270a64
ShaharNaveh <>
2021-04-12 20:37:33 +0300

    Fix docs for nanops.py

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -166,6 +166,10 @@ if [[ -z "$CHECK" || "$CHECK" == "doctests" ]]; then
     pytest -q --doctest-modules pandas/core/generic.py
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
+    MSG='Doctests nanops.py' ; echo $MSG
+    pytest -q --doctest-modules pandas/core/nanops.py
+    RET=$(($RET + $?)) ; echo $MSG "DONE"
+
     MSG='Doctests series.py' ; echo $MSG
     pytest -q --doctest-modules pandas/core/series.py
     RET=$(($RET + $?)) ; echo $MSG "DONE"
