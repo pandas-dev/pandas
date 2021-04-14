5730e3bba21331c0f516500ee116fc91289f4848
Dries Schaumont <dries.schaumont@gmail.com>
2021-04-14 16:02:06 +0200

    Add test and attempted fix.

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -8983,7 +8983,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
                     join="left",
                     axis=axis,
                     level=level,
-                    fill_value=np.nan,
+                    fill_value=None,
                     copy=False,
                 )
 
