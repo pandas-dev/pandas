929cc37a962c1a2f34199e69cd6ad50a2c6dd8d3
tp <contribute@tensortable.com>
2021-04-24 10:36:42 +0100

    Add gh issue number

--- pandas/tests/test_algos.py
+++ pandas/tests/test_algos.py
@@ -247,6 +247,7 @@ class TestFactorize:
             algos.factorize(x17[::-1], sort=True)
 
     def test_numeric_dtype_factorize(self, any_real_dtype):
+        # GH41132
         dtype = any_real_dtype
         data = np.array([1, 2, 2, 1], dtype=dtype)
         expected_codes = np.array([0, 1, 1, 0], dtype=np.intp)
