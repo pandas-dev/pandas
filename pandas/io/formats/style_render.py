8fc9f0bff82ed50c86fa544f12495826ffb26eb6
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-04-24 21:08:21 +0200

    _parse_latex_table_wrapping doc and bool return (rhshadrach request)

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -1015,20 +1015,18 @@ class Tooltips:
 
 def _parse_latex_table_wrapping(table_styles: CSSStyles, caption: str | None) -> bool:
     """
-    Discovers whether \\begin{tabular}..\\end{tabular} should be wrapped within
-    \\begin{table}..\\end{table}
+    Indicate whether LaTeX {tabular} should be wrapped with a {table} environment.
 
     Parses the `table_styles` and detects any selectors which must be included outside
-    of {tabular}, i.e. indicating that wrapping must occur, and therefore return True.
+    of {tabular}, i.e. indicating that wrapping must occur, and therefore return True,
+    or if a caption exists and requires similar.
     """
     IGNORED_WRAPPERS = ["toprule", "midrule", "bottomrule", "column_format"]
     # ignored selectors are included with {tabular} so do not need wrapping
-    if (
+    return (
         table_styles is not None
         and any(d["selector"] not in IGNORED_WRAPPERS for d in table_styles)
-    ) or caption:
-        return True
-    return False
+    ) or caption
 
 
 def _parse_latex_table_styles(table_styles: CSSStyles, selector: str) -> str | None:
