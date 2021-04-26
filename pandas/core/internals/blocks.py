13165aa17422fd405839eb6cd137518eca037f54
kolaug <kolaug@umich.edu>
2021-04-26 19:42:25 -0400

    fixed style issues

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -2053,7 +2053,7 @@ def to_native_types(
     if isinstance(values, Categorical) and isinstance(values.categories[0], Timestamp):
         values = values.astype('datetime64[ns]')
         values = DatetimeArray(values)
-        
+
     if isinstance(values, (DatetimeArray, TimedeltaArray)):
         result = values._format_native_types(na_rep=na_rep, **kwargs)
         result = result.astype(object, copy=False)
@@ -2064,7 +2064,7 @@ def to_native_types(
         mask = isna(values)
         new_values = np.asarray(values.astype(object))
         new_values[mask] = na_rep
-        return new_values 
+        return new_values
 
 
     elif values.dtype.kind == "f":
