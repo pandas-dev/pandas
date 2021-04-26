f0c7b3afd3a437601ef2bb4113153f3fbcf74e30
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-04-26 11:34:19 +0200

    [ArrayManager] DataFrame constructor from ndarray (#40441)

--- pandas/tests/frame/methods/test_astype.py
+++ pandas/tests/frame/methods/test_astype.py
@@ -428,11 +428,27 @@ class TestAstype:
         other = f"m8[{unit}]"
 
         df = DataFrame(np.array([[1, 2, 3]], dtype=dtype))
-        msg = fr"Cannot cast DatetimeArray to dtype timedelta64\[{unit}\]"
+        msg = "|".join(
+            [
+                # BlockManager path
+                fr"Cannot cast DatetimeArray to dtype timedelta64\[{unit}\]",
+                # ArrayManager path
+                "cannot astype a datetimelike from "
+                fr"\[datetime64\[ns\]\] to \[timedelta64\[{unit}\]\]",
+            ]
+        )
         with pytest.raises(TypeError, match=msg):
             df.astype(other)
 
-        msg = fr"Cannot cast TimedeltaArray to dtype datetime64\[{unit}\]"
+        msg = "|".join(
+            [
+                # BlockManager path
+                fr"Cannot cast TimedeltaArray to dtype datetime64\[{unit}\]",
+                # ArrayManager path
+                "cannot astype a timedelta from "
+                fr"\[timedelta64\[ns\]\] to \[datetime64\[{unit}\]\]",
+            ]
+        )
         df = DataFrame(np.array([[1, 2, 3]], dtype=other))
         with pytest.raises(TypeError, match=msg):
             df.astype(dtype)
