e3750a06ab7ae085a3b19b07884f36c30f6571e3
Brock <jbrockmendel@gmail.com>
2021-04-26 11:01:47 -0700

    TYP: overload lib.maybe_convert_objects

--- pandas/core/tools/datetimes.py
+++ pandas/core/tools/datetimes.py
@@ -252,9 +252,7 @@ def _convert_and_box_cache(
     from pandas import Series
 
     result = Series(arg).map(cache_array)
-    # error: Argument 1 to "_box_as_indexlike" has incompatible type "Series"; expected
-    # "Union[ExtensionArray, ndarray]"
-    return _box_as_indexlike(result, utc=None, name=name)  # type: ignore[arg-type]
+    return _box_as_indexlike(result._values, utc=None, name=name)
 
 
 def _return_parsed_timezone_results(result: np.ndarray, timezones, tz, name) -> Index:
@@ -368,13 +366,11 @@ def _convert_listlike_datetimes(
         arg, _ = maybe_convert_dtype(arg, copy=False)
     except TypeError:
         if errors == "coerce":
-            result = np.array(["NaT"], dtype="datetime64[ns]").repeat(len(arg))
-            return DatetimeIndex(result, name=name)
+            npvalues = np.array(["NaT"], dtype="datetime64[ns]").repeat(len(arg))
+            return DatetimeIndex(npvalues, name=name)
         elif errors == "ignore":
-            # error: Incompatible types in assignment (expression has type
-            # "Index", variable has type "ExtensionArray")
-            result = Index(arg, name=name)  # type: ignore[assignment]
-            return result
+            idx = Index(arg, name=name)
+            return idx
         raise
 
     arg = ensure_object(arg)
@@ -393,18 +389,14 @@ def _convert_listlike_datetimes(
             require_iso8601 = not infer_datetime_format
             format = None
 
-    # error: Incompatible types in assignment (expression has type "None", variable has
-    # type "ExtensionArray")
-    result = None  # type: ignore[assignment]
+    result = None
 
     if format is not None:
-        # error: Incompatible types in assignment (expression has type
-        # "Optional[Index]", variable has type "ndarray")
-        result = _to_datetime_with_format(  # type: ignore[assignment]
+        res = _to_datetime_with_format(
             arg, orig_arg, name, tz, format, exact, errors, infer_datetime_format
         )
-        if result is not None:
-            return result
+        if res is not None:
+            return res
 
     if result is None:
         assert format is None or infer_datetime_format
@@ -509,13 +501,11 @@ def _to_datetime_with_format(
 
         # fallback
         if result is None:
-            # error: Incompatible types in assignment (expression has type
-            # "Optional[Index]", variable has type "Optional[ndarray]")
-            result = _array_strptime_with_fallback(  # type: ignore[assignment]
+            res = _array_strptime_with_fallback(
                 arg, name, tz, fmt, exact, errors, infer_datetime_format
             )
-            if result is not None:
-                return result
+            if res is not None:
+                return res
 
     except ValueError as e:
         # Fallback to try to convert datetime objects if timezone-aware
