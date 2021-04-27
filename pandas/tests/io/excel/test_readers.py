a0d98006bb2266c6c0d49ec54bc5bea2f26ca097
Andrew Hawryluk <ahawryluk@gmail.com>
2021-04-27 07:34:47 -0600

    Linting 2

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -433,7 +433,9 @@ class TestReaders:
         float_expected = expected.copy()
         float_expected["IntCol"] = float_expected["IntCol"].astype(float)
         float_expected.loc[float_expected.index[1], "Str2Col"] = 3.0
-        with tm.assert_produces_warning(FutureWarning, match="convert_float is deprecated"):
+        with tm.assert_produces_warning(
+            FutureWarning, match="convert_float is deprecated"
+        ):
             actual = pd.read_excel(
                 basename + read_ext, sheet_name="Sheet1", convert_float=False
             )
@@ -456,7 +458,9 @@ class TestReaders:
 
         no_convert_float = float_expected.copy()
         no_convert_float["StrCol"] = no_convert_float["StrCol"].apply(str)
-        with tm.assert_produces_warning(FutureWarning, match="convert_float is deprecated"):
+        with tm.assert_produces_warning(
+            FutureWarning, match="convert_float is deprecated"
+        ):
             actual = pd.read_excel(
                 basename + read_ext,
                 sheet_name="Sheet1",
