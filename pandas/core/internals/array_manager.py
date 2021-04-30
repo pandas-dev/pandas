446ddba6b9e99c70caf47ef888968097448b9609
jbrockmendel <jbrockmendel@gmail.com>
2021-04-30 11:50:01 -0700

    REF: hide ArrayManager implementation details from GroupBy (#41086)

--- pandas/core/internals/array_manager.py
+++ pandas/core/internals/array_manager.py
@@ -932,12 +932,20 @@ class ArrayManager(BaseArrayManager):
         result_indices: list[int] = []
 
         for i, arr in enumerate(self.arrays):
+            # grouped_reduce functions all expect 2D arrays
+            arr = ensure_block_shape(arr, ndim=2)
             try:
                 res = func(arr)
             except (TypeError, NotImplementedError):
                 if not ignore_failures:
                     raise
                 continue
+
+            if res.ndim == 2:
+                # reverse of ensure_block_shape
+                assert res.shape[0] == 1
+                res = res[0]
+
             result_arrays.append(res)
             result_indices.append(i)
 
