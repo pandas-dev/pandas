692272cb48ceb9011a1be6719796e8baa60896b5
phofl <patrick_hoefler@gmx.net>
2021-05-01 18:51:54 +0200

    Only dispatch up in certain cases

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -3568,7 +3568,14 @@ class MultiIndex(Index):
 
     def _union(self, other, sort) -> MultiIndex:
         other, result_names = self._convert_can_do_setop(other)
-        if self.hasnans or other.hasnans:
+        if (
+            any(-1 in code for code in self.codes)
+            and any(-1 in code for code in self.codes)
+            or self.has_duplicates
+            or other.has_duplicates
+        ):
+            # This is only necessary if both sides have nans or one has dups,
+            # fast_unique_multiple is faster
             result = super()._union(other, sort)
         else:
             rvals = other._values.astype(object, copy=False)
