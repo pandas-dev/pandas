c2ba234ce06896032407ad2776a9a2e411b4d97c
Brock <jbrockmendel@gmail.com>
2021-05-02 13:02:04 -0700

    revert everything but describe

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -1282,7 +1282,9 @@ class GroupBy(BaseGroupBy[FrameOrSeries]):
             except DataError:
                 pass
             except NotImplementedError as err:
-                if "function is not implemented for this dtype" in str(err):
+                if "function is not implemented for this dtype" in str(
+                    err
+                ) or "category dtype not supported" in str(err):
                     # raised in _get_cython_function, in some cases can
                     #  be trimmed by implementing cython funcs for more dtypes
                     pass
