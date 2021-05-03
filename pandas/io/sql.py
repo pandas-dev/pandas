36adf43ccc03f1c5395a49581102ede9a485d432
Josh Dimarsky <24758845+yehoshuadimarsky@users.noreply.github.com>
2021-05-02 22:10:40 -0400

    Update pandas/io/sql.py
    Co-authored-by: Thomas Li <47963215+lithomas1@users.noreply.github.com>

--- pandas/io/sql.py
+++ pandas/io/sql.py
@@ -1345,7 +1345,7 @@ class SQLAlchemyEngine(BaseEngine):
             # GH34431
             # https://stackoverflow.com/a/67358288/6067848
             msg = r"""(\(1054, "Unknown column 'inf(e0)?' in 'field list'"\))(?#
-            )|(ProgrammingError: inf can not be used with MySQL)"""
+            )|inf can not be used with MySQL"""
             err_text = str(err.orig)
             if re.search(msg, err_text):
                 raise ValueError("inf cannot be used with MySQL") from err
