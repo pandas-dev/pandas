7f2811142f52be5d70cb440037dd181dcc155eb6
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-05-03 08:46:12 +0200

    ENH: tests for hide_columns

--- pandas/tests/io/formats/style/test_non_unique.py
+++ pandas/tests/io/formats/style/test_non_unique.py
@@ -119,3 +119,20 @@ def test_set_td_classes_non_unique_raises(styler):
     classes = DataFrame([["1", "2"], ["3", "4"]], columns=["c", "d"], index=["a", "a"])
     with pytest.raises(KeyError, match="Classes render only if `classes` has unique"):
         styler.set_td_classes(classes=classes)
+
+
+def test_hide_columns_non_unique(styler):
+    ctx = styler.hide_columns(["d"])._translate()
+
+    assert ctx["head"][0][1]["display_value"] == "c"
+    assert ctx["head"][0][1]["is_visible"] is True
+
+    assert ctx["head"][0][2]["display_value"] == "d"
+    assert ctx["head"][0][2]["is_visible"] is False
+
+    assert ctx["head"][0][3]["display_value"] == "d"
+    assert ctx["head"][0][3]["is_visible"] is False
+
+    assert ctx["body"][0][1]["is_visible"] is True
+    assert ctx["body"][0][2]["is_visible"] is False
+    assert ctx["body"][0][3]["is_visible"] is False
