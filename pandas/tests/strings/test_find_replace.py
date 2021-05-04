4d656ac2e1687e0020dea3f073b470093e1632ec
Simon Hawkins <simonjayhawkins@gmail.com>
2021-05-04 12:44:42 +0100

    de-duplicate fixture

--- pandas/tests/strings/test_find_replace.py
+++ pandas/tests/strings/test_find_replace.py
@@ -4,35 +4,12 @@ import re
 import numpy as np
 import pytest
 
-import pandas.util._test_decorators as td
-
 import pandas as pd
 from pandas import (
     Index,
     Series,
     _testing as tm,
 )
-
-
-@pytest.fixture(
-    params=[
-        "object",
-        "string",
-        pytest.param(
-            "arrow_string", marks=td.skip_if_no("pyarrow", min_version="1.0.0")
-        ),
-    ]
-)
-def any_string_dtype(request):
-    """
-    Parametrized fixture for string dtypes.
-    * 'object'
-    * 'string'
-    * 'arrow_string'
-    """
-    from pandas.core.arrays.string_arrow import ArrowStringDtype  # noqa: F401
-
-    return request.param
 
 
 def test_contains(any_string_dtype):
