c62b020d08c3e35528f4cdc9663149e03e7d7a10
jbrockmendel <jbrockmendel@gmail.com>
2021-05-04 12:38:17 -0700

    REF: raise more selectively in libreduction (#41298)

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -1040,7 +1040,7 @@ class BaseGrouper:
 
             if not initialized:
                 # We only do this validation on the first iteration
-                libreduction.check_result_array(res)
+                libreduction.check_result_array(res, group.dtype)
                 initialized = True
 
             counts[i] = group.shape[0]
