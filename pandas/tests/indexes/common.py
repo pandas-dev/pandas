d6cb2497fad673e71b6e5e2b36261922bc7c3933
Terji Petersen <contribute@tensortable.com>
2021-05-05 13:39:13 +0100

    BUG: RangeIndex.astype('category') (#41263)

--- pandas/tests/indexes/common.py
+++ pandas/tests/indexes/common.py
@@ -672,20 +672,20 @@ class Base:
         # standard categories
         dtype = CategoricalDtype(ordered=ordered)
         result = idx.astype(dtype, copy=copy)
-        expected = CategoricalIndex(idx.values, name=name, ordered=ordered)
-        tm.assert_index_equal(result, expected)
+        expected = CategoricalIndex(idx, name=name, ordered=ordered)
+        tm.assert_index_equal(result, expected, exact=True)
 
         # non-standard categories
         dtype = CategoricalDtype(idx.unique().tolist()[:-1], ordered)
         result = idx.astype(dtype, copy=copy)
-        expected = CategoricalIndex(idx.values, name=name, dtype=dtype)
-        tm.assert_index_equal(result, expected)
+        expected = CategoricalIndex(idx, name=name, dtype=dtype)
+        tm.assert_index_equal(result, expected, exact=True)
 
         if ordered is False:
             # dtype='category' defaults to ordered=False, so only test once
             result = idx.astype("category", copy=copy)
-            expected = CategoricalIndex(idx.values, name=name)
-            tm.assert_index_equal(result, expected)
+            expected = CategoricalIndex(idx, name=name)
+            tm.assert_index_equal(result, expected, exact=True)
 
     def test_is_unique(self, simple_index):
         # initialize a unique index
