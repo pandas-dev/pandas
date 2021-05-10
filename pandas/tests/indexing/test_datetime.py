2b014984afe5b885ee12048d0a3df52a3fca2fd6
Mortada Mehyar <mortada@users.noreply.github.com>
2021-05-10 22:29:05 +0800

    BUG: fix TypeError when looking up a str subclass on a DataFrame with DatetimeIndex (#37366) (#41406)

--- pandas/tests/indexing/test_datetime.py
+++ pandas/tests/indexing/test_datetime.py
@@ -152,3 +152,16 @@ class TestDatetimeIndex:
             ],
         )
         tm.assert_equal(result, expected)
+
+    def test_str_subclass(self):
+        # GH 37366
+        class mystring(str):
+            pass
+
+        data = ["2020-10-22 01:21:00+00:00"]
+        index = pd.DatetimeIndex(data)
+        df = DataFrame({"a": [1]}, index=index)
+        df["b"] = 2
+        df[mystring("c")] = 3
+        expected = DataFrame({"a": [1], "b": [2], mystring("c"): [3]}, index=index)
+        tm.assert_equal(df, expected)
