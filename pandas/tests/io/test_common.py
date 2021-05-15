2fcdc61a7095d1b14529481520ed9b138ca0d0ce
Matthew Roeschke <emailformattr@gmail.com>
2021-05-15 00:03:27 -0700

    Shorten match

--- pandas/tests/io/test_common.py
+++ pandas/tests/io/test_common.py
@@ -524,9 +524,6 @@ def test_bad_encdoing_errors():
 
 def test_errno_attribute():
     # GH 13872
-    with pytest.raises(
-        FileNotFoundError,
-        match="\\[Errno 2\\] No such file or directory: 'doesnt_exist'",
-    ) as err:
+    with pytest.raises(FileNotFoundError, match="\\[Errno 2\\]") as err:
         pd.read_csv("doesnt_exist")
         assert err.errno == errno.ENOENT
