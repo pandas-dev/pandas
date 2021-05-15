92a06a4f6c842a0fa5c47a91ff7fbfee40b2e7da
Matthew Roeschke <emailformattr@gmail.com>
2021-05-15 15:13:30 -0700

    Address comments

--- pandas/tests/frame/test_stack_unstack.py
+++ pandas/tests/frame/test_stack_unstack.py
@@ -2019,17 +2019,17 @@ Thu,Lunch,Yes,51.51,17"""
         )
         tm.assert_frame_equal(result, expected)
 
-    def test_unstack_categorical(self):
+    def test_unstack_categorical_columns(self):
         # GH 14018
         idx = MultiIndex.from_product([["A"], [0, 1]])
         df = DataFrame({"cat": pd.Categorical(["a", "b"])}, index=idx)
         result = df.unstack()
         expected = DataFrame(
-            [
-                pd.Categorical(["a"], categories=["a", "b"]),
-                pd.Categorical(["b"], categories=["a", "b"]),
-            ],
+            {
+                0: pd.Categorical(["a"], categories=["a", "b"]),
+                1: pd.Categorical(["b"], categories=["a", "b"]),
+            },
             index=["A"],
-            columns=MultiIndex.from_tuples([("cat", 0), ("cat", 1)]),
         )
+        expected.columns = MultiIndex.from_tuples([("cat", 0), ("cat", 1)])
         tm.assert_frame_equal(result, expected)
