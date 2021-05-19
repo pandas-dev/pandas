1c883e0d50be2ba04dbb90312b4e651d7541a824
Simon Hawkins <simonjayhawkins@gmail.com>
2021-05-19 12:39:46 +0100

    Revert "use mask"
    This reverts commit 8e584a44de47620f4f1c7119af5255ec69cb97a8.

--- pandas/core/strings/accessor.py
+++ pandas/core/strings/accessor.py
@@ -277,13 +277,19 @@ class StringMethods(NoNewAttributesMixin):
             # required when expand=True is explicitly specified
             # not needed when inferred
 
-            mask = isna(result)
-            result = [[x] if mask[i] else x for i, x in enumerate(result)]
+            def cons_row(x):
+                if is_list_like(x):
+                    return x
+                else:
+                    return [x]
 
+            result = [cons_row(x) for x in result]
             if result and not self._is_string:
                 # propagate nan values to match longest sequence (GH 18450)
                 max_len = max(len(x) for x in result)
-                result = [x * max_len if mask[i] else x for i, x in enumerate(result)]
+                result = [
+                    x * max_len if len(x) == 0 or x[0] is np.nan else x for x in result
+                ]
 
         if not isinstance(expand, bool):
             raise ValueError("expand must be True or False")
