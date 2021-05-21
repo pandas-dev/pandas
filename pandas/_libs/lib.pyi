922436a78903dfa55cd1d54d4381477cad934af5
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2021-05-21 13:30:08 -0700

    Update lib.pyi

--- pandas/_libs/lib.pyi
+++ pandas/_libs/lib.pyi
@@ -146,7 +146,7 @@ def maybe_convert_numeric(
 def ensure_string_array(
     arr,
     na_value: object = np.nan,
-    coerce: str = "all,
+    coerce: str = "all",
     copy: bool = True,
     skipna: bool = True,
 ) -> np.ndarray: ...  # np.ndarray[object]
