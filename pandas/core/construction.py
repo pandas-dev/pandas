876dfc38730b19cc7e7fa7b0400beac6b5d235b3
jbrockmendel <jbrockmendel@gmail.com>
2021-05-21 18:24:07 -0700

    REF: _try_cast; go through fastpath more often (#41597)

--- pandas/core/construction.py
+++ pandas/core/construction.py
@@ -38,9 +38,9 @@ from pandas.core.dtypes.cast import (
     construct_1d_object_array_from_listlike,
     maybe_cast_to_datetime,
     maybe_cast_to_integer_array,
-    maybe_castable,
     maybe_convert_platform,
     maybe_upcast,
+    sanitize_to_nanoseconds,
 )
 from pandas.core.dtypes.common import (
     is_datetime64_ns_dtype,
@@ -656,33 +656,53 @@ def _try_cast(
     -------
     np.ndarray or ExtensionArray
     """
+    is_ndarray = isinstance(arr, np.ndarray)
+
     # perf shortcut as this is the most common case
+    # Item "List[Any]" of "Union[List[Any], ndarray]" has no attribute "dtype"
     if (
-        isinstance(arr, np.ndarray)
-        and maybe_castable(arr.dtype)
+        is_ndarray
+        and arr.dtype != object  # type: ignore[union-attr]
         and not copy
         and dtype is None
     ):
-        return arr
+        # Argument 1 to "sanitize_to_nanoseconds" has incompatible type
+        # "Union[List[Any], ndarray]"; expected "ndarray"
+        return sanitize_to_nanoseconds(arr)  # type: ignore[arg-type]
 
-    if isinstance(dtype, ExtensionDtype) and not isinstance(dtype, DatetimeTZDtype):
+    if isinstance(dtype, ExtensionDtype):
         # create an extension array from its dtype
         # DatetimeTZ case needs to go through maybe_cast_to_datetime but
         # SparseDtype does not
+        if isinstance(dtype, DatetimeTZDtype):
+            # We can't go through _from_sequence because it handles dt64naive
+            #  data differently; _from_sequence treats naive as wall times,
+            #  while maybe_cast_to_datetime treats it as UTC
+            #  see test_maybe_promote_any_numpy_dtype_with_datetimetz
+
+            # error: Incompatible return value type (got "Union[ExtensionArray,
+            # ndarray, List[Any]]", expected "Union[ExtensionArray, ndarray]")
+            return maybe_cast_to_datetime(arr, dtype)  # type: ignore[return-value]
+            # TODO: copy?
+
         array_type = dtype.construct_array_type()._from_sequence
         subarr = array_type(arr, dtype=dtype, copy=copy)
         return subarr
 
-    if is_object_dtype(dtype) and not isinstance(arr, np.ndarray):
-        subarr = construct_1d_object_array_from_listlike(arr)
-        return subarr
+    elif is_object_dtype(dtype):
+        if not is_ndarray:
+            subarr = construct_1d_object_array_from_listlike(arr)
+            return subarr
+        return ensure_wrapped_if_datetimelike(arr).astype(dtype, copy=copy)
 
-    if dtype is None and isinstance(arr, list):
+    elif dtype is None and not is_ndarray:
         # filter out cases that we _dont_ want to go through maybe_cast_to_datetime
         varr = np.array(arr, copy=False)
         if varr.dtype != object or varr.size == 0:
             return varr
-        arr = varr
+        # error: Incompatible return value type (got "Union[ExtensionArray,
+        # ndarray, List[Any]]", expected "Union[ExtensionArray, ndarray]")
+        return maybe_cast_to_datetime(varr, None)  # type: ignore[return-value]
 
     try:
         # GH#15832: Check if we are requesting a numeric dtype and
