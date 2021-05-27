72c07f8a421803d19b572d9fadb55314e0a1fd6a
Michael Hsieh <mdhsieh8@gmail.com>
2021-05-27 10:17:15 -0700

    tst: Remove parentheses to try fix linting issue.
    They should not be there.

--- pandas/tests/series/test_repr.py
+++ pandas/tests/series/test_repr.py
@@ -244,10 +244,7 @@ class TestSeriesRepr:
         # GH#35603
         # check float format when cast to object
         ser = Series([1.0]).astype(object)
-        expected = (
-            "0    1.0\n"
-            "dtype: object"
-        )
+        expected = "0    1.0\ndtype: object"
         assert repr(ser) == expected
 
 class TestCategoricalRepr:
