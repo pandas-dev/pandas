35f3b657ac52719dd6c79a1e8b167f6c9b423257
Matthew Roeschke <emailformattr@gmail.com>
2021-05-27 20:36:41 -0700

    Add test for GH 20951

--- pandas/tests/indexing/multiindex/test_loc.py
+++ pandas/tests/indexing/multiindex/test_loc.py
@@ -875,3 +875,18 @@ def test_loc_empty_single_selector_with_names():
     result = s2.loc["a"]
     expected = Series([np.nan, np.nan], index=Index(["A", "B"], name=0))
     tm.assert_series_equal(result, expected)
+
+
+def test_loc_keyerror_rightmost_key_missing():
+    # GH 20951
+
+    df = DataFrame(
+        {
+            "A": [100, 100, 200, 200, 300, 300],
+            "B": [10, 10, 20, 21, 31, 33],
+            "C": range(6),
+        }
+    )
+    df = df.set_index(["A", "B"])
+    with pytest.raises(KeyError, match="1"):
+        df.loc[(100, 1)]
