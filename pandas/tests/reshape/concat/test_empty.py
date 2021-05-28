32ce82ff7689f6ea79981c6cb1386c00705c8a39
weikhor <cheanwei.KCW@gmail.com>
2021-05-28 23:05:31 +0800

    change single quote to double quote

--- pandas/tests/reshape/concat/test_empty.py
+++ pandas/tests/reshape/concat/test_empty.py
@@ -255,12 +255,11 @@ class TestEmptyConcat:
         df1 = DataFrame(columns=["a", "b"])
         df2 = DataFrame(columns=["b", "c"])
         result = concat([df1, df2, df1])
-        expected = DataFrame(columns=['a', 'b', 'c'])
+        expected = DataFrame(columns=["a", "b", "c"])
         tm.assert_frame_equal(result, expected)
 
         df3 = DataFrame(columns=["a", "b"])
         df4 = DataFrame(columns=["b"])
         result = concat([df3, df4])
-        expected = DataFrame(columns=['a', 'b'])
-        tm.assert_frame_equal(result, expected)   
-
+        expected = DataFrame(columns=["a", "b"])
+        tm.assert_frame_equal(result, expected)
