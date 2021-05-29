cda0ced743c5fa29e058e3b16553b9d6894b0b0c
weikhor <cheanwei.KCW@gmail.com>
2021-05-29 17:53:09 +0800

    reverse automatic change the lambda function to def created from autopep8

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -1388,13 +1388,11 @@ class DataFrameGroupBy(GroupBy[DataFrame]):
     def _define_paths(self, func, *args, **kwargs):
         if isinstance(func, str):
             fast_path = lambda group: getattr(group, func)(*args, **kwargs)
-
             slow_path = lambda group: group.apply(
                 lambda x: getattr(x, func)(*args, **kwargs), axis=self.axis
             )
         else:
             fast_path = lambda group: func(group, *args, **kwargs)
-
             slow_path = lambda group: group.apply(
                 lambda x: func(x, *args, **kwargs), axis=self.axis
             )
