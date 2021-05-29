590396d6984f0b4d0794ddade636d60ab2521953
Michael Hsieh <mdhsieh8@gmail.com>
2021-05-29 10:33:20 -0700

    TST: Remove unused imports.

--- pandas/tests/frame/methods/test_map.py
+++ pandas/tests/frame/methods/test_map.py
@@ -1,6 +1,3 @@
-import numpy as np
-import pytest
-
 import pandas as pd
 import pandas._testing as tm
 
@@ -9,25 +6,19 @@ class TestMap:
     def test_map(self):
         # map test on StringDType, GH#40823
         df1 = pd.DataFrame(
-            {
-                "col1": [pd.NA, "foo", "bar"]
-            }, index=["id1", "id2", "id3"], dtype=pd.StringDtype()
+            {"col1": [pd.NA, "foo", "bar"]},
+            index=["id1", "id2", "id3"],
+            dtype=pd.StringDtype(),
         )
 
-        df2 = pd.DataFrame(
-            {
-                "id": ["id4", "id2", "id1"]
-            }, dtype=pd.StringDtype()
-        )
-        
+        df2 = pd.DataFrame({"id": ["id4", "id2", "id1"]}, dtype=pd.StringDtype())
+
         df2["col1"] = df2["id"].map(df1["col1"])
-        
+
         result = df2
         expected = pd.DataFrame(
-            {
-                "id": ["id4", "id2", "id1"], 
-                "col1": [pd.NA, "foo", pd.NA]
-            }, dtype=pd.StringDtype()
+            {"id": ["id4", "id2", "id1"], "col1": [pd.NA, "foo", pd.NA]},
+            dtype=pd.StringDtype(),
         )
-        
+
         tm.assert_frame_equal(result, expected)
