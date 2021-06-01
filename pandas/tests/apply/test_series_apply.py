078b1b64f9cf9872185abf0be3a9f568219db8bf
Michael Hsieh <mdhsieh8@gmail.com>
2021-06-01 16:36:55 -0700

    TST: Change StringDType to any string data type.
    Using suggested test fixture any_string_dtype.

--- pandas/tests/apply/test_series_apply.py
+++ pandas/tests/apply/test_series_apply.py
@@ -115,16 +115,16 @@ def test_series_map_box_timestamps():
     ser.apply(func)
 
 
-def test_series_map_stringdtype():
+def test_series_map_stringdtype(any_string_dtype):
     # map test on StringDType, GH#40823
     ser1 = Series(
         data=["cat", "dog", "rabbit"],
         index=["id1", "id2", "id3"],
-        dtype=pd.StringDtype(),
+        dtype=any_string_dtype,
     )
-    ser2 = Series(data=["id3", "id2", "id1", "id7000"], dtype=pd.StringDtype())
+    ser2 = Series(data=["id3", "id2", "id1", "id7000"], dtype=any_string_dtype)
     result = ser2.map(ser1)
-    expected = Series(data=["rabbit", "dog", "cat", pd.NA], dtype=pd.StringDtype())
+    expected = Series(data=["rabbit", "dog", "cat", pd.NA], dtype=any_string_dtype)
 
     tm.assert_series_equal(result, expected)
 
