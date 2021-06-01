b119a8be9dc762648187af1524c513bab4f56e9e
Marc Garcia <garcia.marc@gmail.com>
2021-06-01 16:29:32 -0600

    TST: Make ARM build work (not in the CI) (#41739)

--- pandas/tests/indexes/interval/test_astype.py
+++ pandas/tests/indexes/interval/test_astype.py
@@ -3,6 +3,8 @@ import re
 import numpy as np
 import pytest
 
+from pandas.compat import is_platform_arm
+
 from pandas.core.dtypes.dtypes import (
     CategoricalDtype,
     IntervalDtype,
@@ -168,6 +170,7 @@ class TestFloatSubtype(AstypeTests):
         )
         tm.assert_index_equal(result, expected)
 
+    @pytest.mark.xfail(is_platform_arm(), reason="GH 41740")
     def test_subtype_integer_errors(self):
         # float64 -> uint64 fails with negative values
         index = interval_range(-10.0, 10.0)
