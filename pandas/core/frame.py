7946226ed8c52ead9643785237aa1e3f2d358749
Khor Chean Wei <cheanwei.KCW@gmail.com>
2021-06-01 23:58:44 +0800

    Update frame.py

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -8544,8 +8544,6 @@ NaN 12.3   33.0
             result_in_dict = relabel_result(result, func, columns, order)
             result = DataFrame(result_in_dict, index=columns)
 
-        if isinstance(result, DataFrame):
-            result = result.squeeze()
             
         return result
 
