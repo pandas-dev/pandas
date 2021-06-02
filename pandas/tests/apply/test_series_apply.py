9d2ee9df6aa7da65486da24f13d6efae0c19cc50
Michael Hsieh <52055110+mdhsieh@users.noreply.github.com>
2021-06-02 06:24:14 -0700

    TST: Check map function works with StringDtype (#40823) (#41723)

--- pandas/tests/apply/test_series_apply.py
+++ pandas/tests/apply/test_series_apply.py
@@ -115,6 +115,20 @@ def test_series_map_box_timestamps():
     ser.apply(func)
 
 
+def test_series_map_stringdtype(any_string_dtype):
+    # map test on StringDType, GH#40823
+    ser1 = Series(
+        data=["cat", "dog", "rabbit"],
+        index=["id1", "id2", "id3"],
+        dtype=any_string_dtype,
+    )
+    ser2 = Series(data=["id3", "id2", "id1", "id7000"], dtype=any_string_dtype)
+    result = ser2.map(ser1)
+    expected = Series(data=["rabbit", "dog", "cat", pd.NA], dtype=any_string_dtype)
+
+    tm.assert_series_equal(result, expected)
+
+
 def test_apply_box():
     # ufunc will not be boxed. Same test cases as the test_map_box
     vals = [pd.Timestamp("2011-01-01"), pd.Timestamp("2011-01-02")]
