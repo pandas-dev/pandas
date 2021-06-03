29549509c55b56a5cb74b7c6877ed880468ce720
Anupam-USP <ianupam1509@gmail.com>
2021-06-03 11:39:27 +0530

    ENH: Deprecate non-keyword arguments for interpolate

--- pandas/tests/resample/test_deprecated.py
+++ pandas/tests/resample/test_deprecated.py
@@ -282,10 +282,8 @@ def test_resample_base_with_timedeltaindex():
 
 def test_interpolate_posargs_deprecation():
     # GH 41485
-    df = DataFrame({"ds": ["1992-08-27 07:46:48", "1992-08-27 07:46:59"], "y": [1, 4]})
-    s = Series(
-        df.iloc[:, 1].values.reshape(-1), index=pd.to_datetime(df.iloc[:, 0].values)
-    )
+    idx = pd.to_datetime(["1992-08-27 07:46:48", "1992-08-27 07:46:59"])
+    s = Series([1, 4], index=idx)
 
     msg = (
         r"In a future version of pandas all arguments of Resampler\.interpolate "
@@ -293,21 +291,17 @@ def test_interpolate_posargs_deprecation():
     )
 
     with tm.assert_produces_warning(FutureWarning, match=msg):
-        result = s.resample("3S").interpolate("linear", 0)
+        result = s.resample("3s").interpolate("linear", 0)
 
-    df = DataFrame(
-        {
-            "ds": [
-                "1992-08-27 07:46:48",
-                "1992-08-27 07:46:51",
-                "1992-08-27 07:46:54",
-                "1992-08-27 07:46:57",
-            ],
-            "y": [1.0, 1.0, 1.0, 1.0],
-        }
+    idx = pd.to_datetime(
+        [
+            "1992-08-27 07:46:48",
+            "1992-08-27 07:46:51",
+            "1992-08-27 07:46:54",
+            "1992-08-27 07:46:57",
+        ]
     )
-    expected = Series(
-        df.iloc[:, 1].values.reshape(-1), index=pd.to_datetime(df.iloc[:, 0].values)
-    )
+    expected = Series([1.0, 1.0, 1.0, 1.0], index=idx)
+
     expected.index.freq = "3s"
     tm.assert_series_equal(result, expected)
