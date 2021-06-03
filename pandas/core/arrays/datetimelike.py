0056f82b40724ab1bba2497ea2a0bc2d420fd02f
Kiley <42876297+hewittk@users.noreply.github.com>
2021-06-02 23:33:12 -0600

    Remove validate_periods

--- pandas/core/arrays/datetimelike.py
+++ pandas/core/arrays/datetimelike.py
@@ -1825,29 +1825,6 @@ def validate_periods(periods):
     return periods
 
 
-def validate_periods(periods):
-    """
-    If a `periods` argument is passed to the Datetime/Timedelta Array/Index
-    constructor, cast it to an integer.
-    Parameters
-    ----------
-    periods : None, float, int
-    Returns
-    -------
-    periods : None or int
-    Raises
-    ------
-    TypeError
-        if periods is None, float, or int
-    """
-    if periods is not None:
-        if lib.is_float(periods):
-            periods = int(periods)
-        elif not lib.is_integer(periods):
-            raise TypeError(f"periods must be a number, got {periods}")
-    return periods
-
-
 def validate_endpoints(closed):
     """
     Check that the `closed` argument is among [None, "left", "right"]
