c22eb72d5113e9edaee16e861c2ce0ceef904e21
Brock <jbrockmendel@gmail.com>
2021-06-03 11:55:04 -0700

    REF: remove Index._convert_list_indexer

--- pandas/tests/indexing/test_indexing.py
+++ pandas/tests/indexing/test_indexing.py
@@ -338,7 +338,7 @@ class TestFancy:
         # GH 10610
         df = DataFrame(np.random.random((10, 5)), columns=["a"] + [20, 21, 22, 23])
 
-        with pytest.raises(KeyError, match=re.escape("'[-8, 26] not in index'")):
+        with pytest.raises(KeyError, match=re.escape("'[26, -8] not in index'")):
             df[[22, 26, -8]]
         assert df[21].shape[0] == df.shape[0]
 
