1842637cb92e9dea303f265199f54d25a6a8f767
jbrockmendel <jbrockmendel@gmail.com>
2021-06-04 06:21:05 -0700

    CLN: datetimelike setops (#41750)

--- pandas/core/indexes/datetimelike.py
+++ pandas/core/indexes/datetimelike.py
@@ -631,10 +631,6 @@ class DatetimeTimedeltaMixin(DatetimeIndexOpsMixin):
     # --------------------------------------------------------------------
     # Set Operation Methods
 
-    def _difference(self, other, sort=None):
-        new_idx = super()._difference(other, sort=sort)._with_freq(None)
-        return new_idx
-
     def _intersection(self, other: Index, sort=False) -> Index:
         """
         intersection specialized to the case with matching dtypes.
@@ -780,13 +776,8 @@ class DatetimeTimedeltaMixin(DatetimeIndexOpsMixin):
 
         if self._can_fast_union(other):
             result = self._fast_union(other, sort=sort)
-            if sort is None:
-                # In the case where sort is None, _can_fast_union
-                #  implies that result.freq should match self.freq
-                assert result.freq == self.freq, (result.freq, self.freq)
-            elif result.freq is None:
-                # TODO: no tests rely on this; needed?
-                result = result._with_freq("infer")
+            # in the case with sort=None, the _can_fast_union check ensures
+            #  that result.freq == self.freq
             return result
         else:
             i8self = Int64Index._simple_new(self.asi8)
