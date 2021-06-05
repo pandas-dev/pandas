c911db741d8b3a2b0653d2601593d2cfe878ed5d
Brock <jbrockmendel@gmail.com>
2021-06-05 11:50:09 -0700

    TYP: datetimelike

--- pandas/core/arrays/datetimes.py
+++ pandas/core/arrays/datetimes.py
@@ -83,6 +83,7 @@ from pandas.tseries.offsets import (
 if TYPE_CHECKING:
     from typing import Literal
 
+    from pandas import DataFrame
     from pandas.core.arrays import (
         PeriodArray,
         TimedeltaArray,
@@ -1248,7 +1249,7 @@ default 'raise'
         return result
 
     @property
-    def time(self):
+    def time(self) -> np.ndarray:
         """
         Returns numpy array of datetime.time. The time part of the Timestamps.
         """
@@ -1260,7 +1261,7 @@ default 'raise'
         return ints_to_pydatetime(timestamps, box="time")
 
     @property
-    def timetz(self):
+    def timetz(self) -> np.ndarray:
         """
         Returns numpy array of datetime.time also containing timezone
         information. The time part of the Timestamps.
@@ -1268,7 +1269,7 @@ default 'raise'
         return ints_to_pydatetime(self.asi8, self.tz, box="time")
 
     @property
-    def date(self):
+    def date(self) -> np.ndarray:
         """
         Returns numpy array of python datetime.date objects (namely, the date
         part of Timestamps without timezone information).
@@ -1280,7 +1281,7 @@ default 'raise'
 
         return ints_to_pydatetime(timestamps, box="date")
 
-    def isocalendar(self):
+    def isocalendar(self) -> DataFrame:
         """
         Returns a DataFrame with the year, week, and day calculated according to
         the ISO 8601 standard.
@@ -1863,7 +1864,7 @@ default 'raise'
         """,
     )
 
-    def to_julian_date(self):
+    def to_julian_date(self) -> np.ndarray:
         """
         Convert Datetime Array to float64 ndarray of Julian Dates.
         0 Julian date is noon January 1, 4713 BC.
