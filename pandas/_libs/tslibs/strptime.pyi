21efd463d845f4e0b7b3f68ea4789d86c290dbc7
Fangchen Li <fangchen.li@outlook.com>
2021-06-06 16:08:29 -0500

    reformat

--- pandas/_libs/tslibs/strptime.pyi
+++ pandas/_libs/tslibs/strptime.pyi
@@ -4,6 +4,7 @@ def array_strptime(
     values: np.ndarray,  # np.ndarray[object]
     fmt: str | None,
     exact: bool = True,
-    errors: str = "raise"
+    errors: str = "raise",
 ) -> tuple[np.ndarray, np.ndarray]: ...
+
 # first  ndarray is M8[ns], second is object ndarray of tzinfo | None
