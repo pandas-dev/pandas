0b7e77325cbbf9f24b48e0da2e3111550eb87818
Matthew Roeschke <emailformattr@gmail.com>
2021-06-06 21:14:55 -0700

    Add test with invalid update

--- pandas/tests/window/test_online.py
+++ pandas/tests/window/test_online.py
@@ -13,7 +13,18 @@ import pandas._testing as tm
 @td.skip_if_no("numba", "0.46.0")
 @pytest.mark.filterwarnings("ignore:\\nThe keyword argument")
 class TestEWM:
-    def test_online_vs_non_online(self, nogil, parallel, nopython, adjust, ignore_na):
+    def test_invalid_update(self):
+        df = DataFrame({"a": range(5), "b": range(5)})
+        online_ewm = df.head(2).ewm(0.5).online()
+        with pytest.raises(
+            ValueError,
+            match="Must call mean with update=None first before passing update",
+        ):
+            online_ewm.mean(update=df.head(1))
+
+    def test_online_vs_non_online_mean(
+        self, nogil, parallel, nopython, adjust, ignore_na
+    ):
         df = DataFrame({"a": range(5), "b": range(5)})
         expected = df.ewm(0.5, adjust=adjust, ignore_na=ignore_na).mean()
         engine_kwargs = {"nogil": nogil, "parallel": parallel, "nopython": nopython}
@@ -24,7 +35,7 @@ class TestEWM:
             .online(engine_kwargs=engine_kwargs)
         )
         # Test resetting once
-        for i in range(2):
+        for _ in range(2):
             result = online_ewm.mean()
             tm.assert_frame_equal(result, expected.head(2))
 
@@ -33,7 +44,7 @@ class TestEWM:
 
             online_ewm.reset()
 
-    def test_update_times(self, nogil, parallel, nopython, adjust, ignore_na):
+    def test_update_times_mean(self, nogil, parallel, nopython, adjust, ignore_na):
         times = Series(
             np.array(
                 ["2020-01-01", "2020-01-02", "2020-01-04", "2020-01-17", "2020-01-21"],
@@ -50,7 +61,7 @@ class TestEWM:
             .online(engine_kwargs=engine_kwargs)
         )
         # Test resetting once
-        for i in range(2):
+        for _ in range(2):
             result = online_ewm.mean()
             tm.assert_frame_equal(result, expected.head(2))
 
