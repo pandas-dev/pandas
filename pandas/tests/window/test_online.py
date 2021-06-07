bab78ccf5e673b0f247400102f9e46a992067ea3
Matthew Roeschke <emailformattr@gmail.com>
2021-06-06 21:05:18 -0700

    Parameterize over adjust and ignore_na

--- pandas/tests/window/test_online.py
+++ pandas/tests/window/test_online.py
@@ -13,19 +13,23 @@ import pandas._testing as tm
 @td.skip_if_no("numba", "0.46.0")
 @pytest.mark.filterwarnings("ignore:\\nThe keyword argument")
 class TestEWM:
-    def test_online_vs_non_online(self, nogil, parallel, nopython):
+    def test_online_vs_non_online(self, nogil, parallel, nopython, adjust, ignore_na):
         df = DataFrame({"a": range(5), "b": range(5)})
-        expected = df.ewm(0.5).mean()
+        expected = df.ewm(0.5, adjust=adjust, ignore_na=ignore_na).mean()
         engine_kwargs = {"nogil": nogil, "parallel": parallel, "nopython": nopython}
 
-        online_ewm = df.head(2).ewm(0.5).online(engine_kwargs=engine_kwargs)
+        online_ewm = (
+            df.head(2)
+            .ewm(0.5, adjust=adjust, ignore_na=ignore_na)
+            .online(engine_kwargs=engine_kwargs)
+        )
         result = online_ewm.mean()
         tm.assert_frame_equal(result, expected.head(2))
 
         result = online_ewm.update(update=df.tail(3))
         tm.assert_frame_equal(result, expected.tail(3))
 
-    def test_update_times(self, nogil, parallel, nopython):
+    def test_update_times(self, nogil, parallel, nopython, adjust, ignore_na):
         times = Series(
             np.array(
                 ["2020-01-01", "2020-01-02", "2020-01-04", "2020-01-17", "2020-01-21"],
@@ -33,11 +37,13 @@ class TestEWM:
             )
         )
         df = DataFrame({"a": range(5), "b": range(5)})
-        expected = df.ewm(0.5, times=times).mean()
+        expected = df.ewm(0.5, adjust=adjust, ignore_na=ignore_na, times=times).mean()
 
         engine_kwargs = {"nogil": nogil, "parallel": parallel, "nopython": nopython}
         online_ewm = (
-            df.head(2).ewm(0.5, times=times.head(2)).online(engine_kwargs=engine_kwargs)
+            df.head(2)
+            .ewm(0.5, adjust=adjust, ignore_na=ignore_na, times=times.head(2))
+            .online(engine_kwargs=engine_kwargs)
         )
         result = online_ewm.mean()
         tm.assert_frame_equal(result, expected.head(2))
