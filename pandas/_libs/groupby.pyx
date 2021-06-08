208e6edcb07ae91f03d38c5b55a77d0a170d3121
Matthew Zeitlin <mzeitlin@caltech.edu>
2021-06-08 19:41:54 -0400

    Add kahan comment

--- pandas/_libs/groupby.pyx
+++ pandas/_libs/groupby.pyx
@@ -247,6 +247,8 @@ def group_cumsum(numeric[:, ::1] out,
             for j in range(K):
                 val = values[i, j]
 
+                # For floats, use Kahan summation to reduce floating-point
+                # error (https://en.wikipedia.org/wiki/Kahan_summation_algorithm)
                 if numeric == float32_t or numeric == float64_t:
                     if val == val:
                         y = val - compensation[lab, j]
