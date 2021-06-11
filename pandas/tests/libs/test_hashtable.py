1c10229db2965d50d28ba78b57e708ba09e455b0
Egor Dranischnikow <egor.dranischnikow@googlemail.com>
2021-06-11 21:54:23 +0200

    adding test for 5 == 5.0 == 5+0j

--- pandas/tests/libs/test_hashtable.py
+++ pandas/tests/libs/test_hashtable.py
@@ -520,3 +520,11 @@ def test_ismember_tuple_with_nans():
     result = isin(values, comps)
     expected = np.array([True, False], dtype=np.bool_)
     tm.assert_numpy_array_equal(result, expected)
+
+
+def test_float_complex_int_are_equal_as_objects():
+    values = ["a", 5, 5.0, 5.0 + 0j]
+    comps = list(range(129))
+    result = isin(values, comps)
+    expected = np.array([False, True, True, True], dtype=np.bool_)
+    tm.assert_numpy_array_equal(result, expected)
