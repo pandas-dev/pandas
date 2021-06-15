c67092a9a884331a12508795c81d1188f36304af
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-06-15 10:02:41 +0200

    add tests

--- pandas/tests/io/formats/style/test_style.py
+++ pandas/tests/io/formats/style/test_style.py
@@ -583,6 +583,25 @@ class TestStyler:
         result._compute()
         assert result.ctx == expected
 
+    @pytest.mark.parametrize("axis", [0, 1])
+    def test_apply_series_return(self, axis):
+        # GH 42014
+        df = DataFrame([[1, 2], [3, 4]], index=["X", "Y"], columns=["X", "Y"])
+
+        # test Series return where len(Series) < df.index or df.columns but labels OK
+        func = lambda s: pd.Series(["color: red;"], index=["Y"])
+        result = df.style.apply(func, axis=axis)._compute().ctx
+        assert result[(1, 1)] == [("color", "red")]
+        assert result[(1 - axis, axis)] == [("color", "red")]
+
+        # test Series return where labels align but different order
+        func = lambda s: pd.Series(["color: red;", "color: blue;"], index=["Y", "X"])
+        result = df.style.apply(func, axis=axis)._compute().ctx
+        assert result[(0, 0)] == [("color", "blue")]
+        assert result[(1, 1)] == [("color", "red")]
+        assert result[(1 - axis, axis)] == [("color", "red")]
+        assert result[(axis, 1 - axis)] == [("color", "blue")]
+
     @pytest.mark.parametrize(
         "slice_",
         [
