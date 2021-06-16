ac0f0a7cac032aac759086e4d54cb90dfa0861a8
jbrockmendel <jbrockmendel@gmail.com>
2021-06-15 17:03:29 -0700

    BUG: DataFrame.at with CategoricalIndex (#41846)

--- pandas/tests/indexing/test_at.py
+++ pandas/tests/indexing/test_at.py
@@ -8,6 +8,7 @@ import pytest
 
 from pandas import (
     CategoricalDtype,
+    CategoricalIndex,
     DataFrame,
     Series,
     Timestamp,
@@ -141,3 +142,16 @@ class TestAtErrors:
             ser.at[0]
         with pytest.raises(KeyError, match="^4$"):
             ser.at[4]
+
+    def test_at_categorical_integers(self):
+        # CategoricalIndex with integer categories that don't happen to match
+        #  the Categorical's codes
+        ci = CategoricalIndex([3, 4])
+
+        arr = np.arange(4).reshape(2, 2)
+        frame = DataFrame(arr, index=ci)
+
+        for df in [frame, frame.T]:
+            for key in [0, 1]:
+                with pytest.raises(KeyError, match=str(key)):
+                    df.at[key, key]
