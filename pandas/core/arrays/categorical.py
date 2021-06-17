5a101dcd62552733636fbecad4f8336378b23c77
Simon Hawkins <simonjayhawkins@gmail.com>
2021-06-17 20:18:29 +0100

    REGR: undocumented astype("category").astype(str) type inconsistency between pandas 1.1 & 1.2

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -523,6 +523,7 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
             try:
                 new_cats = np.asarray(self.categories)
                 new_cats = new_cats.astype(dtype=dtype, copy=copy)
+                fill_value = np.array(np.nan).astype(dtype).item()
             except (
                 TypeError,  # downstream error msg for CategoricalIndex is misleading
                 ValueError,
@@ -530,7 +531,9 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
                 msg = f"Cannot cast {self.categories.dtype} dtype to {dtype}"
                 raise ValueError(msg)
 
-            result = take_nd(new_cats, ensure_platform_int(self._codes))
+            result = take_nd(
+                new_cats, ensure_platform_int(self._codes), fill_value=fill_value
+            )
 
         return result
 
