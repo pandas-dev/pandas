fe98cd66ecb1c9b93ceced2dc04cbe1a777db7a4
Brock <jbrockmendel@gmail.com>
2021-06-18 14:53:21 -0700

    DEPR: Index.is_type_compatible

--- pandas/tests/indexes/test_any_index.py
+++ pandas/tests/indexes/test_any_index.py
@@ -66,6 +66,12 @@ def test_ravel_deprecation(index):
         index.ravel()
 
 
+def test_is_type_compatible_deprecation(index):
+    msg = "is_type_compatible is deprecated"
+    with tm.assert_produces_warning(FutureWarning, match=msg):
+        index.is_type_compatible(index.inferred_type)
+
+
 class TestConversion:
     def test_to_series(self, index):
         # assert that we are creating a copy of the index
