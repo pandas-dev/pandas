5aebd52eb653cd866f741f897e915388122efc9a
Calvin Storoschuk <calvinsomething@gmail.com>
2021-06-22 10:09:32 -0400

    reformat unmatched error message

--- pandas/tests/util/test_assert_produces_warning.py
+++ pandas/tests/util/test_assert_produces_warning.py
@@ -98,8 +98,8 @@ def test_fail_to_match_runtime_warning():
     category = RuntimeWarning
     match = "Did not see this warning"
     unmatched = (
-        r"Did not see warning 'RuntimeWarning' matching 'Did not see this "
-        r"warning'. The emitted warning messages are \[RuntimeWarning\('This is not a "
+        r"Did not see warning 'RuntimeWarning' matching 'Did not see this warning'. "
+        r"The emitted warning messages are \[RuntimeWarning\('This is not a "
         r"match.'\), RuntimeWarning\('Another unmatched warning.'\)\]"
     )
     with pytest.raises(AssertionError, match=unmatched):
@@ -113,8 +113,8 @@ def test_fail_to_match_future_warning():
     match = "Warning"
     unmatched = (
         r"Did not see warning 'FutureWarning' matching 'Warning'. "
-        r"The emitted warning messages are \[FutureWarning\('This is not a match.'\),"
-        r" FutureWarning\('Another unmatched warning.'\)\]"
+        r"The emitted warning messages are \[FutureWarning\('This is not a match.'\), "
+        r"FutureWarning\('Another unmatched warning.'\)\]"
     )
     with pytest.raises(AssertionError, match=unmatched):
         with tm.assert_produces_warning(category, match=match):
