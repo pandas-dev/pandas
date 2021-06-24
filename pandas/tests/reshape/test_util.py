52564f5bf9dcdc7dab724440b4f45aff34acce9b
Joshua Klein <jklein@gritstone.com>
2021-06-24 13:29:28 -0400

    style

--- pandas/tests/reshape/test_util.py
+++ pandas/tests/reshape/test_util.py
@@ -71,7 +71,7 @@ class TestCartesianProduct:
 
     def test_exceed_product_space(self):
         # GH31355: raise useful error when produce space is too large
-        msg = ("Product space too large to allocate arrays!")
+        msg = "Product space too large to allocate arrays!"
 
         with pytest.raises(ValueError, match=msg):
             dims = [np.arange(0, 22, dtype=np.int16) for i in range(12)] + [
