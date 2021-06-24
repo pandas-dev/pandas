265743df0eab109748fe5abe720763400f5de395
Joshua Klein <jklein@gritstone.com>
2021-06-24 13:56:12 -0400

    More style

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -2993,8 +2993,8 @@ class GroupBy(BaseGroupBy[FrameOrSeries]):
                 "Product space too large to allocate arrays! You're trying to "
                 "group over too complex a column cross-product to fit in memory. "
                 "If any of those columns are categorical, you may be able "
-                "to circumvent this issue by passing `observed=True`.") from err
-
+                "to circumvent this issue by passing `observed=True`."
+            ) from err
 
         if self.as_index:
             d = {
