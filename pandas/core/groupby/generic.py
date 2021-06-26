c3ec602f4626f8bac7a36ee267232340df5c70c0
Richard Shadrach <rhshadrach@gmail.com>
2021-06-26 13:18:31 -0400

    Comment

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -1676,6 +1676,7 @@ class DataFrameGroupBy(GroupBy[DataFrame]):
 
     def _wrap_agged_manager(self, mgr: Manager2D) -> DataFrame:
         if not self.as_index:
+            # GH 41998 - empty mgr always gets index of length 0
             rows = mgr.shape[1] if mgr.shape[0] > 0 else 0
             index = Index(range(rows))
             mgr.set_axis(1, index)
