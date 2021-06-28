4e781be8c00ca948b7da2612043022a85ddef34b
Brock <jbrockmendel@gmail.com>
2021-06-28 10:56:43 -0700

    port _maybe_cast_listlike_indexer

--- pandas/core/indexes/category.py
+++ pandas/core/indexes/category.py
@@ -487,6 +487,21 @@ class CategoricalIndex(NDArrayBackedExtensionIndex, accessor.PandasDelegate):
                 return -1
             raise
 
+    def _maybe_cast_listlike_indexer(self, values) -> CategoricalIndex:
+        if isinstance(values, CategoricalIndex):
+            values = values._data
+        if isinstance(values, Categorical):
+            # Indexing on codes is more efficient if categories are the same,
+            #  so we can apply some optimizations based on the degree of
+            #  dtype-matching.
+            cat = self._data._encode_with_my_categories(values)
+            codes = cat._codes
+        else:
+            codes = self.categories.get_indexer(values)
+            codes = codes.astype(self.codes.dtype, copy=False)
+            cat = self._data._from_backing_data(codes)
+        return type(self)._simple_new(cat)
+
     def _get_indexer(
         self,
         target: Index,
