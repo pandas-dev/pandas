ed47241e370e82b9463dce3d0a2af38a43397f42
P-Tillmann <Peter.tillmann@fu-berlin.de>
2021-06-28 15:29:03 +0200

    fixed flake8 issue

--- pandas/tests/reshape/concat/test_categorical.py
+++ pandas/tests/reshape/concat/test_categorical.py
@@ -208,18 +208,18 @@ class TestCategoricalConcat:
         # test upcasting to object when concatinating on categorical indexes
         # with non-identical categories
 
-        a = pd.DataFrame({"foo": [1, 2]}, index=Categorical(["foo", "bar"]))
-        b = pd.DataFrame({"foo": [4, 3]}, index=Categorical(["baz", "bar"]))
+        a = DataFrame({"foo": [1, 2]}, index=Categorical(["foo", "bar"]))
+        b = DataFrame({"foo": [4, 3]}, index=Categorical(["baz", "bar"]))
 
         res = pd.concat([a, b])
-        exp = pd.DataFrame({"foo": [1, 2, 4, 3]}, index=["foo", "bar", "baz", "bar"])
+        exp = DataFrame({"foo": [1, 2, 4, 3]}, index=["foo", "bar", "baz", "bar"])
 
         tm.assert_equal(res, exp)
 
-        a = pd.Series([1, 2], index=Categorical(["foo", "bar"]))
-        b = pd.Series([4, 3], index=Categorical(["baz", "bar"]))
+        a = Series([1, 2], index=Categorical(["foo", "bar"]))
+        b = Series([4, 3], index=Categorical(["baz", "bar"]))
 
         res = pd.concat([a, b])
-        exp = pd.Series([1, 2, 4, 3], index=["foo", "bar", "baz", "bar"])
+        exp = Series([1, 2, 4, 3], index=["foo", "bar", "baz", "bar"])
 
         tm.assert_equal(res, exp)
