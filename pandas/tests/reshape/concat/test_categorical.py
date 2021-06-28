8a45a2ef0743a2f9adc8a90dc1424c41660eff6f
P-Tillmann <Peter.tillmann@fu-berlin.de>
2021-06-28 14:58:52 +0200

    test upcasting to object when concatinating on categorical indexes with non-identical categories

--- pandas/tests/reshape/concat/test_categorical.py
+++ pandas/tests/reshape/concat/test_categorical.py
@@ -202,3 +202,24 @@ class TestCategoricalConcat:
 
         dfa = df1.append(df2)
         tm.assert_index_equal(df["grade"].cat.categories, dfa["grade"].cat.categories)
+
+    def test_categorical_index_upcast(self):
+        # GH 17629
+        # test upcasting to object when concatinating on categorical indexes
+        # with non-identical categories
+
+        a = pd.DataFrame({"foo": [1, 2]}, index=Categorical(["foo", "bar"]))
+        b = pd.DataFrame({"foo": [4, 3]}, index=Categorical(["baz", "bar"]))
+
+        res = pd.concat([a, b])
+        exp = pd.DataFrame({"foo": [1, 2, 4, 3]}, index=["foo", "bar", "baz", "bar"])
+
+        tm.assert_equal(res, exp)
+
+        a = pd.Series([1, 2], index=Categorical(["foo", "bar"]))
+        b = pd.Series([4, 3], index=Categorical(["baz", "bar"]))
+
+        res = pd.concat([a, b])
+        exp = pd.Series([1, 2, 4, 3], index=["foo", "bar", "baz", "bar"])
+
+        tm.assert_equal(res, exp)
