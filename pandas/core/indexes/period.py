cfc31ce49b88dbadfb44f6d4e7940be9f292fa85
jbrockmendel <jbrockmendel@gmail.com>
2021-06-29 05:31:39 -0700

    Backport PR #42293: PERF/REGR: IntervalIndex.intersection, PeriodIndex.get_loc

--- pandas/core/indexes/period.py
+++ pandas/core/indexes/period.py
@@ -447,10 +447,25 @@ class PeriodIndex(DatetimeIndexOpsMixin):
             else:
                 key = asdt
 
-        elif is_integer(key):
-            # Period constructor will cast to string, which we dont want
-            raise KeyError(key)
-        elif isinstance(key, Period) and key.freq != self.freq:
+        elif isinstance(key, Period):
+            sfreq = self.freq
+            kfreq = key.freq
+            if not (
+                sfreq.n == kfreq.n
+                and sfreq._period_dtype_code == kfreq._period_dtype_code
+            ):
+                # GH#42247 For the subset of DateOffsets that can be Period freqs,
+                #  checking these two attributes is sufficient to check equality,
+                #  and much more performant than `self.freq == key.freq`
+                raise KeyError(key)
+        elif isinstance(key, datetime):
+            try:
+                key = Period(key, freq=self.freq)
+            except ValueError as err:
+                # we cannot construct the Period
+                raise KeyError(orig_key) from err
+        else:
+            # in particular integer, which Period constructor would cast to string
             raise KeyError(key)
 
         try:
