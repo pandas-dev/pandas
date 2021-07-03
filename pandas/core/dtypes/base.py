4d6dbdbd026fe0c054a62f8a9c62b4c17b8ce6f9
Irv Lustig <irv@princeton.com>
2021-07-03 14:50:10 -0400

    cast to the union

--- pandas/core/dtypes/base.py
+++ pandas/core/dtypes/base.py
@@ -18,6 +18,7 @@ from pandas._libs.hashtable import object_hash
 from pandas._typing import (
     DtypeObj,
     NpDtype,
+    Union,
     type_t,
 )
 from pandas.errors import AbstractMethodError
@@ -465,8 +466,8 @@ class Registry:
                 dtype_type = dtype
             if issubclass(dtype_type, ExtensionDtype):
                 # cast needed here as mypy doesn't know we have figured
-                # out it is an ExtensionDtype
-                return cast(ExtensionDtype, dtype)
+                # out it is an ExtensionDtype or type_t[ExtensionDtype]
+                return cast(Union[ExtensionDtype, type_t[ExtensionDtype]], dtype)
 
             return None
 
