4b18c90dc93f97d4b5c551264c15be76f6fac817
jbrockmendel <jbrockmendel@gmail.com>
2021-07-04 14:35:13 -0700

    REF: avoid try/except in maybe_mi_droplevel (#42326)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -3767,8 +3767,10 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         if isinstance(index, MultiIndex):
             try:
                 loc, new_index = index._get_loc_level(key, level=0)
-            except TypeError as e:
-                raise TypeError(f"Expected label or tuple of labels, got {key}") from e
+            except TypeError as err:
+                raise TypeError(
+                    f"Expected label or tuple of labels, got {key}"
+                ) from err
             else:
                 if not drop_level:
                     if lib.is_integer(loc):
