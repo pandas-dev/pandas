pandas-dev/pandas

commit c355ed15cee71c396f81bfaa0dcbd00b2243b400

REF: simplify libreduction (#41262)

pandas/_libs/reduction.pyx
@@ -53,24 +53,24 @@ cdef class _BaseGrouper:
 
         return values, index
 
+    cdef _init_dummy_series_and_index(self, Slider islider, Slider vslider):
+        """
+        Create Series and Index objects that we will alter in-place while iterating.
+        """
+        cached_index = self.ityp(islider.buf, dtype=self.idtype)
+        cached_series = self.typ(
+            vslider.buf, dtype=vslider.buf.dtype, index=cached_index, name=self.name
+        )
+        return cached_index, cached_series
+
     cdef inline _update_cached_objs(self, object cached_typ, object cached_ityp,
                                     Slider islider, Slider vslider):
-        if cached_typ is None:
-            cached_ityp = self.ityp(islider.buf, dtype=self.idtype)
-            cached_typ = self.typ(
-                vslider.buf, dtype=vslider.buf.dtype, index=cached_ityp, name=self.name
-            )
-        else:
-            # See the comment in indexes/base.py about _index_data.
-            # We need this for EA-backed indexes that have a reference
-            # to a 1-d ndarray like datetime / timedelta / period.
-            object.__setattr__(cached_ityp, '_index_data', islider.buf)
-            cached_ityp._engine.clear_mapping()
-            cached_ityp._cache.clear()  # e.g. inferred_freq must go
-            cached_typ._mgr.set_values(vslider.buf)
-            object.__setattr__(cached_typ, '_index', cached_ityp)
-            object.__setattr__(cached_typ, 'name', self.name)
-        return cached_typ, cached_ityp
+        # See the comment in indexes/base.py about _index_data.
+        # We need this for EA-backed indexes that have a reference
+        # to a 1-d ndarray like datetime / timedelta / period.
+        cached_ityp._engine.clear_mapping()
+        cached_ityp._cache.clear()  # e.g. inferred_freq must go
+        cached_typ._mgr.set_values(vslider.buf)
 
     cdef inline object _apply_to_group(self,
                                        object cached_typ, object cached_ityp,
@@ -81,8 +81,8 @@ cdef class _BaseGrouper:
         cdef:
             object res
 
-        cached_ityp._engine.clear_mapping()
-        cached_ityp._cache.clear()  # e.g. inferred_freq must go
+        # NB: we assume that _update_cached_objs has already cleared cleared
+        #  the cache and engine mapping
         res = self.f(cached_typ)
         res = extract_result(res)
         if not initialized:
@@ -160,6 +160,8 @@ cdef class SeriesBinGrouper(_BaseGrouper):
 
         result = np.empty(self.ngroups, dtype='O')
 
+        cached_ityp, cached_typ = self._init_dummy_series_and_index(islider, vslider)
+
         start = 0
         try:
             for i in range(self.ngroups):
@@ -169,7 +171,7 @@ cdef class SeriesBinGrouper(_BaseGrouper):
                 islider.move(start, end)
                 vslider.move(start, end)
 
-                cached_typ, cached_ityp = self._update_cached_objs(
+                self._update_cached_objs(
                     cached_typ, cached_ityp, islider, vslider)
 
                 res, initialized = self._apply_to_group(cached_typ, cached_ityp,
@@ -246,6 +248,8 @@ cdef class SeriesGrouper(_BaseGrouper):
 
         result = np.empty(self.ngroups, dtype='O')
 
+        cached_ityp, cached_typ = self._init_dummy_series_and_index(islider, vslider)
+
         start = 0
         try:
             for i in range(n):
@@ -263,7 +267,7 @@ cdef class SeriesGrouper(_BaseGrouper):
                     islider.move(start, end)
                     vslider.move(start, end)
 
-                    cached_typ, cached_ityp = self._update_cached_objs(
+                    self._update_cached_objs(
                         cached_typ, cached_ityp, islider, vslider)
 
                     res, initialized = self._apply_to_group(cached_typ, cached_ityp,

commit 4fb825f0860e0c1cebac638883c4d67c9fbf2188

BUG: fix regression with SerieGrouper with Timestamp index (#42390)
This fixes a regression introduced in c355ed1 where cache is not
initialized with correct state of islider and vslider. The first call of
{v,i}slider.move() must be done before initializing the cache.

On Timestamp index this trigger a "ValueError Length of values does not match length of index"

Closes #42390

Signed-off-by: Philippe Pepiot <phil@lowatt.fr>

pandas/_libs/reduction.pyx
@@ -165,10 +165,6 @@ cdef class SeriesBinGrouper(_BaseGrouper):
 
         result = np.empty(self.ngroups, dtype='O')
 
-        cached_index, cached_series = self._init_dummy_series_and_index(
-            islider, vslider
-        )
-
         start = 0
         try:
             for i in range(self.ngroups):
@@ -177,6 +173,11 @@ cdef class SeriesBinGrouper(_BaseGrouper):
 
                 islider.move(start, end)
                 vslider.move(start, end)
+
+                if cached_index is None:
+                    cached_index, cached_series = self._init_dummy_series_and_index(
+                        islider, vslider
+                    )
 
                 self._update_cached_objs(
                     cached_series, cached_index, islider, vslider)
@@ -254,10 +255,6 @@ cdef class SeriesGrouper(_BaseGrouper):
 
         result = np.empty(self.ngroups, dtype='O')
 
-        cached_index, cached_series = self._init_dummy_series_and_index(
-            islider, vslider
-        )
-
         start = 0
         try:
             for i in range(n):
@@ -274,6 +271,11 @@ cdef class SeriesGrouper(_BaseGrouper):
                     end = start + group_size
                     islider.move(start, end)
                     vslider.move(start, end)
+
+                    if cached_index is None:
+                        cached_index, cached_series = self._init_dummy_series_and_index(
+                            islider, vslider
+                        )
 
                     self._update_cached_objs(
                         cached_series, cached_index, islider, vslider)

pandas/tests/groupby/test_bin_groupby.py
@@ -27,6 +27,23 @@ def test_series_grouper():
     tm.assert_almost_equal(counts, exp_counts)
 
 
+def test_series_grouper_timestamp():
+    # GH 42390
+    obj = Series([1], index=[pd.Timestamp("2018-01-16 00:00:00+00:00")], dtype=np.intp)
+    labels = np.array([0], dtype=np.intp)
+
+    def agg(series):
+        # this should not raise
+        if series.isna().values.all():
+            return None
+        return np.sum(series)
+
+    grouper = libreduction.SeriesGrouper(obj, agg, labels, 1)
+    result, counts = grouper.get_result()
+    tm.assert_numpy_array_equal(result, np.array([1], dtype=object))
+    tm.assert_numpy_array_equal(counts, np.array([1], dtype=np.int64))
+
+
 def test_series_grouper_result_length_difference():
     # GH 40014
     obj = Series(np.random.randn(10), dtype="float64")