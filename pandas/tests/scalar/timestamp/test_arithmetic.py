ceb24262b04bdf6ff259771c00584bcb5f9974c4
Anton Lodder <antonlodder@gmail.com>
2021-07-08 00:01:32 -0400

    TST: 31739 - add coverage of subtracting datetimes w/differing timezones

--- pandas/tests/scalar/timestamp/test_arithmetic.py
+++ pandas/tests/scalar/timestamp/test_arithmetic.py
@@ -141,6 +141,16 @@ class TestTimestampArithmetic:
         assert isinstance(result, Timedelta)
         assert result == Timedelta("-1 days +18:00:00")
 
+    def test_subtracting_different_timezones(self, tz_aware_fixture):
+        t_raw = Timestamp("20130101")
+        t_UTC = t_raw.tz_localize("UTC")
+        t_diff = t_UTC.tz_convert(tz_aware_fixture) + Timedelta("0 days 05:00:00")
+
+        result = t_diff - t_UTC
+
+        assert isinstance(result, Timedelta)
+        assert result == Timedelta("0 days 05:00:00")
+
     def test_addition_subtraction_types(self):
         # Assert on the types resulting from Timestamp +/- various date/time
         # objects
