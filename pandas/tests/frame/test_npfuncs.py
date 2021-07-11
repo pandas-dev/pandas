922cffe88687540f6c5eeab01e4573a46e36fd7b
usersblock <thomasly8991@gmail.com>
2021-07-11 01:18:34 -0400

    Update test_npfuncs.py

--- pandas/tests/frame/test_npfuncs.py
+++ pandas/tests/frame/test_npfuncs.py
@@ -27,7 +27,19 @@ class TestAsArray:
 
         tm.assert_frame_equal(result, float_frame.apply(np.sqrt))
 
-    def test_np_ravel(self):
+    def test_np_ravel_verone(self):
+        # GH#26247 np.ravel() fails on list of DataFrames with column names
+        x = np.zeros((10, 3))
+        df = [DataFrame(batch.reshape(1, 3)) for batch in x]
+        np.testing.assert_array_equal(np.ravel(df), np.zeros(30))
+
+    def test_np_ravel_vertwo(self):
+        # GH#26247 np.ravel() fails on list of DataFrames with column names
+        x = np.zeros((10, 3))
+        df = DataFrame(x[0].reshape(1, 3), columns=["1", "2", "3"])
+        np.testing.assert_array_equal(np.ravel(df), np.zeros(3))
+
+    def test_np_ravel_verthree(self):
         # GH#26247 np.ravel() fails on list of DataFrames with column names
         x = np.zeros((10, 3))
         df = [DataFrame(batch.reshape(1, 3), columns=["1", "2", "3"]) for batch in x]
