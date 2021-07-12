030766522fd3d8572941f59843f0b75fc305cdad
benHeid <benHeid@users.noreply.github.com>
2021-07-12 14:09:07 +0200

    Fix linting errors (Line to long)

--- pandas/tests/window/test_groupby.py
+++ pandas/tests/window/test_groupby.py
@@ -702,11 +702,14 @@ class TestRolling:
         ])
     def test_groupby_rolling_var(self, window, min_periods, closed, expected):
         df = DataFrame([1, 2, 3, 4, 5, 6, 7, 8])
-        result = df.groupby([1, 2, 1, 2, 1, 2, 1, 2]).rolling(window=window, min_periods=min_periods,
+        result = df.groupby([1, 2, 1, 2, 1, 2, 1, 2]).rolling(window=window,
+                                                              min_periods=min_periods,
                                                               closed=closed).var(0)
-        expected_result = DataFrame(np.array(expected, dtype="float64"), index=MultiIndex(levels=[[1, 2], [0, 1, 2, 3, 4, 5, 6, 7]],
-                                                               codes=[[0, 0, 0, 0, 1, 1, 1, 1],
-                                                                      [0, 2, 4, 6, 1, 3, 5, 7]]))
+        expected_result = DataFrame(np.array(expected, dtype="float64"),
+                                    index=MultiIndex(levels=[[1, 2],
+                                                             [0, 1, 2, 3, 4, 5, 6, 7]],
+                                                     codes=[[0, 0, 0, 0, 1, 1, 1, 1],
+                                                            [0, 2, 4, 6, 1, 3, 5, 7]]))
         tm.assert_frame_equal(result, expected_result)
 
     @pytest.mark.parametrize(
