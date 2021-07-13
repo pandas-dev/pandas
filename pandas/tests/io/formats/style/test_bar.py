b2188e723006cdb0b56aee0787a9831c55486c7e
attack68 <24256554+attack68@users.noreply.github.com>
2021-07-13 14:02:03 +0200

    ENH: Styler.bar height control (#42483)

--- pandas/tests/io/formats/style/test_bar.py
+++ pandas/tests/io/formats/style/test_bar.py
@@ -275,6 +275,22 @@ def test_colors_mixed(align, exp):
     assert result == {(0, 0): exp[0], (1, 0): exp[1]}
 
 
+def test_bar_align_height():
+    # test when keyword height is used 'no-repeat center' and 'background-size' present
+    data = DataFrame([[1], [2]])
+    result = data.style.bar(align="left", height=50)._compute().ctx
+    bg_s = "linear-gradient(90deg, #d65f5f 100.0%, transparent 100.0%) no-repeat center"
+    expected = {
+        (0, 0): [("width", "10em")],
+        (1, 0): [
+            ("width", "10em"),
+            ("background", bg_s),
+            ("background-size", "100% 50.0%"),
+        ],
+    }
+    assert result == expected
+
+
 def test_bar_bad_align_raises():
     df = DataFrame({"A": [-100, -60, -30, -20]})
     msg = "`align` should be in {'left', 'right', 'mid', 'mean', 'zero'} or"
