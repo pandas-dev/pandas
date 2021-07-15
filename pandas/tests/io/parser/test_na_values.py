3093041ca8a2a62d01b734228a1a7f151993779f
Justin McOmie <justin.mcomie@gmail.com>
2021-07-14 21:20:23 -0700

    BUG: Fix multi-index colname references in read_csv c engine. (#42519)

--- pandas/tests/io/parser/test_na_values.py
+++ pandas/tests/io/parser/test_na_values.py
@@ -570,3 +570,23 @@ foo,,bar
     )
 
     tm.assert_frame_equal(result, expected)
+
+
+def test_nan_multi_index(all_parsers):
+    # GH 42446
+    parser = all_parsers
+    data = "A,B,B\nX,Y,Z\n1,2,inf"
+
+    result = parser.read_csv(
+        StringIO(data), header=list(range(2)), na_values={("B", "Z"): "inf"}
+    )
+
+    expected = DataFrame(
+        {
+            ("A", "X"): [1],
+            ("B", "Y"): [2],
+            ("B", "Z"): [np.nan],
+        }
+    )
+
+    tm.assert_frame_equal(result, expected)
