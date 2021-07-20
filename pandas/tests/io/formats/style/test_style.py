f0991f094c7a8ae69bb2a138e3b07b2add693b4f
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-07-20 23:59:19 +0200

    whats new

--- pandas/tests/io/formats/style/test_style.py
+++ pandas/tests/io/formats/style/test_style.py
@@ -274,6 +274,12 @@ def test_hide_raises(mi_styler):
 def test_hide_level(mi_styler, levels):
     mi_styler.index.names, mi_styler.columns.names = ["zero", "one"], ["zero", "one"]
     ctx = mi_styler.hide_index(levels=levels)._translate(False, True)
+    assert len(ctx["head"][0]) == 3
+    assert len(ctx["head"][1]) == 3
+    assert len(ctx["head"][2]) == 4
+    assert ctx["head"][2][0]["is_visible"]
+    assert not ctx["head"][2][1]["is_visible"]
+
     assert ctx["body"][0][0]["is_visible"]
     assert not ctx["body"][0][1]["is_visible"]
     assert ctx["body"][1][0]["is_visible"]
