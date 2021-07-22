bd751ea1c60c78a3e1bec611b3ca0086a98af469
partev <petrosyan@gmail.com>
2021-07-22 00:23:40 -0400

    DOC: fix small issues (#42621)
    read_clipboard passes the text to read_csv not read_table. See here for example: https://pandas.pydata.org/docs/dev/reference/api/pandas.read_clipboard.html
    
    also replace "OS X" -> macOS

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -2988,7 +2988,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         --------
         DataFrame.to_csv : Write a DataFrame to a comma-separated values
             (csv) file.
-        read_clipboard : Read text from clipboard and pass to read_table.
+        read_clipboard : Read text from clipboard and pass to read_csv.
 
         Notes
         -----
@@ -2996,7 +2996,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
 
           - Linux : `xclip`, or `xsel` (with `PyQt4` modules)
           - Windows : none
-          - OS X : none
+          - macOS : none
 
         Examples
         --------
