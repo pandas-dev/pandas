2677e83670d4e324e690482ba085e84ac339e0d1
debnathshoham <debnathshoham@gmail.com>
2021-07-24 16:01:55 +0530

    added constant data in test

--- pandas/tests/window/test_ewm.py
+++ pandas/tests/window/test_ewm.py
@@ -187,8 +187,28 @@ def test_ewma_times_adjust_false_raises():
 @pytest.mark.parametrize("dtype", [np.float32, np.float16, np.float64, float, "float"])
 def test_float_dtype_ewma(dtype, func):
     # GH#42452
-    df = DataFrame(np.random.rand(20, 3), dtype=dtype)
+    expected_mean = DataFrame(
+        {
+            0: range(5),
+            1: range(4, 9),
+            2: [7.428571, 9, 10.571429, 12.142857, 13.714286],
+        },
+        dtype=float,
+    )
+    expected_std = DataFrame(
+        {
+            0: [np.nan] * 5,
+            1: [4.242641] * 5,
+            2: [4.6291, 5.196152, 5.781745, 6.380775, 6.989788],
+        }
+    )
+    expected_var = expected_std ** 2
+    df = DataFrame({0: range(5), 1: range(6, 11), 2: range(10, 20, 2)}, dtype=dtype)
     e = df.ewm(alpha=0.5, axis=1)
-    result = getattr(e, func)().shape
-    expected = (20, 3)
-    assert result == expected, f"Shape of ewm {func} must match dataframe"
+    result = getattr(e, func)()
+    expected = {
+        "expected_var": expected_var,
+        "expected_mean": expected_mean,
+        "expected_std": expected_std,
+    }
+    tm.assert_frame_equal(result, expected[f"expected_{func}"])
