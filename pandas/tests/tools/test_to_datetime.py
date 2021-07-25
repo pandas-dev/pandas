970ed06055ec703159a26af5895127d28f105bc3
usersblock <thomasly8991@gmail.com>
2021-07-25 00:21:41 -0400

    Update test_to_datetime.py

--- pandas/tests/tools/test_to_datetime.py
+++ pandas/tests/tools/test_to_datetime.py
@@ -2536,7 +2536,7 @@ def test_empty_string_datetime_coerce__unit():
 def test_to_datetime_monotonic_increasing_index(cache):
     # GH28238
     cstart = start_caching_at
-    times = date_range(Timestamp("1980"), periods=50, freq="YS")
+    times = date_range(Timestamp("1980"), periods=cstart, freq="YS")
     times = times.to_frame(index=False, name="DT").sample(n=cstart, random_state=1)
     times.index = times.index.to_series().astype(float) / 1000
     result = to_datetime(times.iloc[:, 0], cache=cache)
