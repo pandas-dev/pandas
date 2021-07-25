0c49cdc03240468a709233ae76f0a9461d89d545
KrishnaSai2020 <krishnasai.chivukula@gmal.com>
2021-07-25 10:49:54 +0100

    doctest fix for #42671 (pep 8 changes)

--- pandas/io/stata.py
+++ pandas/io/stata.py
@@ -162,12 +162,12 @@ Examples
 --------
 Read a Stata dta file:
 
->>> df = pd.read_stata('docs_example_files/read_stata_example.dta') 
+>>> df = pd.read_stata('docs_example_files/read_stata_example.dta')
 
 Read a Stata dta file in 10,000 line chunks:
 
->>> itr = pd.read_stata('docs_examples/read_stata_example.dta', chunksize=10000) 
->>> for chunk in itr: 
+>>> itr = pd.read_stata('docs_examples/read_stata_example.dta', chunksize=10000)
+>>> for chunk in itr:
 ...     pass # do_something(chunk)
 """
 
