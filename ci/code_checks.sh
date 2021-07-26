2ccbe84ce658f8035d8037ba87a5eda55d79b5db
aneesh98 <askthefactorcamera@gmail.com>
2021-07-27 04:09:08 +0530

    TST: Solution to doctest error in _parse_latex_header_span (#42700)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -121,7 +121,8 @@ if [[ -z "$CHECK" || "$CHECK" == "doctests" ]]; then
       pandas/io/parsers/ \
       pandas/io/sas/ \
       pandas/io/sql.py \
-      pandas/tseries/
+      pandas/tseries/ \
+      pandas/io/formats/style_render.py
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
 fi
