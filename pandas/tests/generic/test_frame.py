4c4bcf9741bb1ab389944fb96a2321a88f09e750
Shiv Gupta <18354771+shiv-io@users.noreply.github.com>
2021-07-26 19:54:34 +0530

    TST GH #26779

--- pandas/tests/generic/test_frame.py
+++ pandas/tests/generic/test_frame.py
@@ -10,6 +10,7 @@ from pandas import (
     MultiIndex,
     Series,
     date_range,
+    Timestamp,
 )
 import pandas._testing as tm
 from pandas.tests.generic.test_generic import Generic
@@ -86,6 +87,37 @@ class TestDataFrame(Generic):
         )
         result = df.resample("1T")
         self.check_metadata(df, result)
+
+    def test_column_types_consistent(self):
+        # GH 26779
+        df = DataFrame(
+            data={
+                "channel": [1, 2, 3],
+                "A": ["String 1", np.NaN, "String 2"],
+                "B": [
+                    Timestamp("2019-06-11 11:00:00"),
+                    np.NaN,
+                    Timestamp("2019-06-11 12:00:00"),
+                ],
+            }
+        )
+        df2 = pd.DataFrame(
+            data={"A": ["String 3"], "B": [Timestamp("2019-06-11 12:00:00")]}
+        )
+        # Change Columns A and B to df2.values wherever Column A is NaN
+        df.loc[df["A"].isna(), ["A", "B"]] = df2.values
+        expected = DataFrame(
+            data={
+                "channel": [1, 2, 3],
+                "A": ["String 1", "String 3", "String 2"],
+                "B": [
+                    Timestamp("2019-06-11 11:00:00"),
+                    Timestamp("2019-06-11 12:00:00"),
+                    Timestamp("2019-06-11 12:00:00"),
+                ],
+            }
+        )
+        tm.assert_frame_equal(df, expected)
 
     def test_metadata_propagation_indiv(self):
         # merging with override
