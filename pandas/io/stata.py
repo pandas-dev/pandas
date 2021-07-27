24a8861b0a76eb3ba0d4231aa001fb9bb1dd0c55
KrishnaSai2020 <krishnasai.chivukula@gmal.com>
2021-07-27 16:59:26 +0100

    moved comment to the side to allow doctest to pass

--- pandas/io/stata.py
+++ pandas/io/stata.py
@@ -178,8 +178,7 @@ Read a Stata dta file in 10,000 line chunks:
 
 >>> itr = pd.read_stata('filename.dta', chunksize=10000)
 >>> for chunk in itr:
-...    # Operate on a single chunk
-...    chunk.mean()
+...    chunk.mean() # Operate on a single chunk
 
 >>> import os
 >>> os.remove("./filename.dta")
