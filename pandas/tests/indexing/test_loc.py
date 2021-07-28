8fab0c51aff45330ec9a8aaa0ea0dd99a0681d58
Yvan Gatete <78673871+GYvan@users.noreply.github.com>
2021-07-28 08:39:50 +0200

    TST: Test Loc to set Multiple Items to multiple new columns (#42665)
    * Modified ecosystem.rst to include ibis
    
    * created a test for issue #25594
    
    * Test for issue #25594
    
    * reverted the changes
    
    * Test Loc to set Multiple Items to multiple new columns - Changes Made
    
    * Test Loc to set Multiple Items to multiple new columns - Changes made and linting addresssed
    
    * TST: Test Loc to set Multiple Items to multiple new columns - Changes Made and pre-commit
    
    * TST: test for issue #25594 - Changes made
    
    * removed the check_dtype

--- pandas/tests/indexing/test_loc.py
+++ pandas/tests/indexing/test_loc.py
@@ -2798,3 +2798,19 @@ class TestLocSeries:
             [[Timedelta(6, unit="s"), "foo"]], columns=["time", "value"], index=[1]
         )
         tm.assert_frame_equal(result, expected)
+
+    def test_loc_set_multiple_items_in_multiple_new_columns(self):
+        # GH 25594
+        df = DataFrame(index=[1, 2], columns=["a"])
+        df.loc[1, ["b", "c"]] = [6, 7]
+
+        expected = DataFrame(
+            {
+                "a": Series([np.nan, np.nan], dtype="object"),
+                "b": [6, np.nan],
+                "c": [7, np.nan],
+            },
+            index=[1, 2],
+        )
+
+        tm.assert_frame_equal(df, expected)
