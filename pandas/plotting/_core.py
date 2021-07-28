58cc3c9a776835e2165fbfe09b4e724b7fbdde14
Kaiqi Dong <kaiqidong1991@gmail.com>
2021-07-28 03:06:32 +0200

    DOC: Add user_guide examples and docstring example for df.plot.box and df.plot.hist (#42520)

--- pandas/plotting/_core.py
+++ pandas/plotting/_core.py
@@ -1267,6 +1267,18 @@ class PlotAccessor(PandasObject):
             >>> data = np.random.randn(25, 4)
             >>> df = pd.DataFrame(data, columns=list('ABCD'))
             >>> ax = df.plot.box()
+
+        You can also generate groupings if you specify the `by` parameter (which
+        can take a column name, or a list or tuple of column names):
+
+        .. versionchanged:: 1.4.0
+
+        .. plot::
+            :context: close-figs
+
+            >>> age_list = [8, 10, 12, 14, 72, 74, 76, 78, 20, 25, 30, 35, 60, 85]
+            >>> df = pd.DataFrame({"gender": list("MMMMMMMMFFFFFF"), "age": age_list})
+            >>> ax = df.plot.box(column="age", by="gender", figsize=(10, 8))
         """
         return self(kind="box", by=by, **kwargs)
 
