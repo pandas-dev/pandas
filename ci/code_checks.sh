266992b07bdfbf01bc30cf4cae758e86151cd27f
Leonardo Freua <leonardo.batista.freua@gmail.com>
2021-07-31 03:38:46 -0300

    TST: Fix doctests in ``style.py`` (#42783)
    * TST: Fix doctests for pandas.io.formats.style
    
    * Modified: pandas/io/formats/style.py
    
    * Added some expected results
    
    * Skipped some tests
    
    * TST: Add link to redirect to Table Visualization user guide
    
    * Modified style.py
    
    * Updated the doctest of the apply()
    
    * Updated the doctest of the applymap()
    
    * Updated the doctest of the set_table_styles()
    
    * Updated the doctest of the set_properties()
    
    * TST: Add image to pipe function result
    
    * Modified style.py
    
    * Updated the doctest of the pipe()
    
    * TST: Remove unnecessary outputs
    
    * Modified pandas/io/formats/style.py
    
    * Updated the doctests of the set_tooltips()
    
    * Updated the doctests of the to_latex()
    
    * Updated the doctests of the set_td_classes()
    
    * Updated the doctests of the set_table_attributes()
    
    * TST: Add the output to the Styler.format doctest in to_latex()
    
    * REG: DataFrame.agg where func returns lists and axis=1 (#42762)
    
    * Fix typing issues for CI (#42770)
    
    * BUG: groupby.shift returns different columns when fill_value is specified (#41858)
    
    * PERF: extract_array earlier in DataFrame construction (#42774)
    
    * ENH: `sparse_columns` and `sparse_index` added to `Styler.to_html`  (#41946)
    
    * TYP: Fix typing for searchsorted (#42788)
    
    * DOC GH42756 Update documentation for pandas.DataFrame.drop to clarify tuples. (#42789)
    
    * CI: Fix doctests (#42790)
    
    * REGR: nanosecond timestamp comparisons to OOB datetimes (#42796)
    
    * COMPAT: MPL 3.4.0 (#42803)
    
    * Delete duplicates and unused code from reshape tests (#42802)
    
    * REGR: ValueError raised when both prefix and names are set to None (#42690)
    
    * REGR: ValueError raised when both prefix and names are set to None
    
    * Update readers.py
    
    * whitespace
    
    * Update v1.3.1.rst
    
    * Update v1.3.2.rst
    
    * Update readers.py
    
    * Update readers.py
    
    Co-authored-by: Jeff Reback <jeff@reback.net>
    
    * TST: Add style.py to the doctest check
    
    * TST: fixed eng_formatter doctest for #42671 (#42705)
    
    * TST: Revert x and y position in some doctests
    
    * Updated the doctest of the hide_columns()
    
    Co-authored-by: Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
    Co-authored-by: Irv Lustig <irv@princeton.com>
    Co-authored-by: Thomas Smith <thomassmith0304@gmail.com>
    Co-authored-by: jbrockmendel <jbrockmendel@gmail.com>
    Co-authored-by: attack68 <24256554+attack68@users.noreply.github.com>
    Co-authored-by: Mike Phung <mikephung122@gmail.com>
    Co-authored-by: Matthew Zeitlin <37011898+mzeitlin11@users.noreply.github.com>
    Co-authored-by: Thomas Li <47963215+lithomas1@users.noreply.github.com>
    Co-authored-by: Patrick Hoefler <61934744+phofl@users.noreply.github.com>
    Co-authored-by: Jeff Reback <jeff@reback.net>
    Co-authored-by: Krishna Chivukula <63070026+KrishnaSai2020@users.noreply.github.com>

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -122,6 +122,7 @@ if [[ -z "$CHECK" || "$CHECK" == "doctests" ]]; then
       pandas/io/sas/ \
       pandas/io/sql.py \
       pandas/io/formats/format.py \
+      pandas/io/formats/style.py \
       pandas/tseries/
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
