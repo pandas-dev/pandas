cbab83fffdb2546a432e1ac0a37cd1c54e1ce93b
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-08-03 10:04:31 +0200

    whats new 1.3.2

--- pandas/tests/io/formats/style/test_highlight.py
+++ pandas/tests/io/formats/style/test_highlight.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from pandas import (
+    NA,
     DataFrame,
     IndexSlice,
 )
@@ -72,6 +73,26 @@ def test_highlight_minmax_ext(df, f, kwargs):
     if f == "highlight_min":
         df = -df
     result = getattr(df.style, f)(**kwargs)._compute().ctx
+    assert result == expected
+
+
+@pytest.mark.parametrize("f", ["highlight_min", "highlight_max"])
+@pytest.mark.parametrize("axis", [None, 0, 1])
+def test_highlight_minmax_nulls(f, axis):
+    # GH 42750
+    expected = {
+        (1, 0): [("background-color", "yellow")],
+        (1, 1): [("background-color", "yellow")],
+    }
+    if axis == 1:
+        expected.update({(2, 1): [("background-color", "yellow")]})
+
+    if f == "highlight_max":
+        df = DataFrame({"a": [NA, 1, None], "b": [np.nan, 1, -1]})
+    else:
+        df = DataFrame({"a": [NA, -1, None], "b": [np.nan, -1, 1]})
+
+    result = getattr(df.style, f)(axis=axis)._compute().ctx
     assert result == expected
 
 
