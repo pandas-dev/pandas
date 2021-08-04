242da5544981c6ac1ec06452170d71383c2815db
Shoham Debnath <debnathshoham@gmail.com>
2021-08-04 22:43:50 +0530

    Update test_insert.py

--- pandas/tests/frame/indexing/test_insert.py
+++ pandas/tests/frame/indexing/test_insert.py
@@ -95,7 +95,7 @@ class TestDataFrameInsert:
         df = DataFrame({"col1": [1, 2], "col2": [3, 4]})
         msg = (
             "Expected a 1D array, got an array with shape "
-            "\(2, 2\)|Wrong number of items passed 2, placement implies 1"
+           r"\(2, 2\)|Wrong number of items passed 2, placement implies 1"
         )
         with pytest.raises(ValueError, match=msg):
             df.insert(1, "newcol", df)
