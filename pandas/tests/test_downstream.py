0e62243cd8883413b7bab4776e2210a588eed3ba
GorAlexander <alexander@energydeck.com>
2021-08-04 12:53:38 +0300

    BUG: GH42866 DatetimeIndex de-serializing fails in PYTHONOPTIMIZE mode [fix pre-commit check]

--- pandas/tests/test_downstream.py
+++ pandas/tests/test_downstream.py
@@ -75,13 +75,17 @@ def test_oo_optimizable():
 
 def test_oo_optimized_datetime_index_unpickle():
     # GH 42866
-    subprocess.check_call([
-        sys.executable, "-OO", "-c",
-        (
-            "import pandas as pd, pickle; "
-            "pickle.loads(pickle.dumps(pd.date_range('2021-01-01', periods=1)))"
-        ),
-    ])
+    subprocess.check_call(
+        [
+            sys.executable,
+            "-OO",
+            "-c",
+            (
+                "import pandas as pd, pickle; "
+                "pickle.loads(pickle.dumps(pd.date_range('2021-01-01', periods=1)))"
+            ),
+        ]
+    )
 
 
 @tm.network
