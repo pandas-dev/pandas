d9805891efe3a398f444f6c875f91092e7ec12a1
debnathshoham <debnathshoham@gmail.com>
2021-08-05 12:35:57 +0530

    added whatsnew; uint tests

--- pandas/tests/window/test_rolling.py
+++ pandas/tests/window/test_rolling.py
@@ -1434,9 +1434,9 @@ def test_rolling_float_dtype(float_dtype):
     tm.assert_frame_equal(result, expected, check_dtype=False)
 
 
-def test_rolling_rows():
+def test_rolling_numeric_dtypes():
     # GH#41779
-    df = DataFrame(np.arange(24).reshape(4, 6), columns=list("abcdef")).astype(
+    df = DataFrame(np.arange(40).reshape(4, 10), columns=list("abcdefghij")).astype(
         {
             "a": "float16",
             "b": "float32",
@@ -1444,17 +1444,25 @@ def test_rolling_rows():
             "d": "int8",
             "e": "int16",
             "f": "int32",
+            "g": "uint8",
+            "h": "uint16",
+            "i": "uint32",
+            "j": "uint64",
         }
     )
     result = df.rolling(window=2, min_periods=1, axis=1).min()
     expected = DataFrame(
         {
-            "a": range(0, 20, 6),
-            "b": range(0, 20, 6),
-            "c": range(1, 20, 6),
-            "d": range(2, 25, 6),
-            "e": range(3, 25, 6),
-            "f": range(4, 25, 6),
+            "a": range(0, 40, 10),
+            "b": range(0, 40, 10),
+            "c": range(1, 40, 10),
+            "d": range(2, 40, 10),
+            "e": range(3, 40, 10),
+            "f": range(4, 40, 10),
+            "g": range(5, 40, 10),
+            "h": range(6, 40, 10),
+            "i": range(7, 40, 10),
+            "j": range(8, 40, 10),
         },
         dtype="float64",
     )
