fc0410ea8807fa717bf6d575e210925ebb168a41
attack68 <24256554+attack68@users.noreply.github.com>
2021-08-08 01:14:31 +0200

    BUG: `Styler.set_sticky` fix the names rows 2/2 (#42799)

--- pandas/io/formats/style.py
+++ pandas/io/formats/style.py
@@ -1534,24 +1534,24 @@ class Styler(StylerRenderer):
         may produce strange behaviour due to CSS controls with missing elements.
         """
         if axis in [0, "index"]:
-            axis, obj, tag, pos = 0, self.data.index, "tbody", "left"
+            axis, obj = 0, self.data.index
             pixel_size = 75 if not pixel_size else pixel_size
         elif axis in [1, "columns"]:
-            axis, obj, tag, pos = 1, self.data.columns, "thead", "top"
+            axis, obj = 1, self.data.columns
             pixel_size = 25 if not pixel_size else pixel_size
         else:
             raise ValueError("`axis` must be one of {0, 1, 'index', 'columns'}")
 
+        props = "position:sticky; background-color:white;"
         if not isinstance(obj, pd.MultiIndex):
             # handling MultiIndexes requires different CSS
-            props = "position:sticky; background-color:white;"
 
             if axis == 1:
                 # stick the first <tr> of <head> and, if index names, the second <tr>
                 # if self._hide_columns then no <thead><tr> here will exist: no conflict
                 styles: CSSStyles = [
                     {
-                        "selector": "thead tr:first-child",
+                        "selector": "thead tr:nth-child(1) th",
                         "props": props + "top:0px; z-index:2;",
                     }
                 ]
@@ -1561,7 +1561,7 @@ class Styler(StylerRenderer):
                     )
                     styles.append(
                         {
-                            "selector": "thead tr:nth-child(2)",
+                            "selector": "thead tr:nth-child(2) th",
                             "props": props
                             + f"top:{pixel_size}px; z-index:2; height:{pixel_size}px; ",
                         }
@@ -1572,34 +1572,67 @@ class Styler(StylerRenderer):
                 # but <th> will exist in <thead>: conflict with initial element
                 styles = [
                     {
-                        "selector": "tr th:first-child",
+                        "selector": "thead tr th:nth-child(1)",
+                        "props": props + "left:0px; z-index:3 !important;",
+                    },
+                    {
+                        "selector": "tbody tr th:nth-child(1)",
                         "props": props + "left:0px; z-index:1;",
-                    }
+                    },
                 ]
 
-            return self.set_table_styles(styles, overwrite=False)
-
         else:
+            # handle the MultiIndex case
             range_idx = list(range(obj.nlevels))
+            levels = sorted(levels) if levels else range_idx
 
-        levels = sorted(levels) if levels else range_idx
-        for i, level in enumerate(levels):
-            self.set_table_styles(
-                [
-                    {
-                        "selector": f"{tag} th.level{level}",
-                        "props": f"position: sticky; "
-                        f"{pos}: {i * pixel_size}px; "
-                        f"{f'height: {pixel_size}px; ' if axis == 1 else ''}"
-                        f"{f'min-width: {pixel_size}px; ' if axis == 0 else ''}"
-                        f"{f'max-width: {pixel_size}px; ' if axis == 0 else ''}"
-                        f"background-color: white;",
-                    }
-                ],
-                overwrite=False,
-            )
+            if axis == 1:
+                styles = []
+                for i, level in enumerate(levels):
+                    styles.append(
+                        {
+                            "selector": f"thead tr:nth-child({level+1}) th",
+                            "props": props
+                            + (
+                                f"top:{i * pixel_size}px; height:{pixel_size}px; "
+                                "z-index:2;"
+                            ),
+                        }
+                    )
+                if not all(name is None for name in self.index.names):
+                    styles.append(
+                        {
+                            "selector": f"thead tr:nth-child({obj.nlevels+1}) th",
+                            "props": props
+                            + (
+                                f"top:{(i+1) * pixel_size}px; height:{pixel_size}px; "
+                                "z-index:2;"
+                            ),
+                        }
+                    )
 
-        return self
+            else:
+                styles = []
+                for i, level in enumerate(levels):
+                    props_ = props + (
+                        f"left:{i * pixel_size}px; "
+                        f"min-width:{pixel_size}px; "
+                        f"max-width:{pixel_size}px; "
+                    )
+                    styles.extend(
+                        [
+                            {
+                                "selector": f"thead tr th:nth-child({level+1})",
+                                "props": props_ + "z-index:3 !important;",
+                            },
+                            {
+                                "selector": f"tbody tr th.level{level}",
+                                "props": props_ + "z-index:1;",
+                            },
+                        ]
+                    )
+
+        return self.set_table_styles(styles, overwrite=False)
 
     def set_table_styles(
         self,
