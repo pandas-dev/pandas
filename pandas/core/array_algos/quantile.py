3614159063e125654f0585a3f10c7e2bdcf19434
debnathshoham <debnathshoham@gmail.com>
2021-08-08 23:25:19 +0530

    BUG:Cannot calculate quantiles from Int64Dtype Series when results are floats

--- pandas/core/array_algos/quantile.py
+++ pandas/core/array_algos/quantile.py
@@ -181,5 +181,8 @@ def _quantile_ea_fallback(
     assert res.ndim == 2
     assert res.shape[0] == 1
     res = res[0]
-    out = type(values)._from_sequence(res, dtype=values.dtype)
+    try:
+        out = type(values)._from_sequence(res, dtype=values.dtype)
+    except TypeError:
+        out = np.atleast_2d(np.asarray(res))
     return out
