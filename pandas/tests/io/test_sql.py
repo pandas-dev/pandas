bdc717a0f0c6172cc65ce87df0f2c058d591412d
Fangchen Li <fangchen.li@outlook.com>
2021-08-10 22:35:24 -0500

    fix errors

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -268,14 +268,14 @@ def check_iris_frame(frame: DataFrame):
 def count_rows(conn, table_name: str):
     import sqlite3
 
-    from sqlalchemy import text
-    from sqlalchemy.engine import Engine
-
     stmt = f"SELECT count(*) AS count_1 FROM {table_name}"
     if isinstance(conn, sqlite3.Connection):
         cur = conn.cursor()
         result = cur.execute(stmt)
     else:
+        from sqlalchemy import text
+        from sqlalchemy.engine import Engine
+
         stmt = text(stmt)
         if isinstance(conn, Engine):
             with conn.connect() as conn:
