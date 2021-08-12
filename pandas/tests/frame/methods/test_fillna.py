ceef973a056b6bab70ce3a6d64fb321a8c76f532
GYvan <78673871+GYvan@users.noreply.github.com>
2021-08-12 16:57:36 +0200

    added another test

--- pandas/tests/frame/methods/test_fillna.py
+++ pandas/tests/frame/methods/test_fillna.py
@@ -438,22 +438,6 @@ class TestFillNA:
         df.fillna(method="ffill", inplace=True)
         tm.assert_frame_equal(df, expected)
 
-        df = DataFrame(
-            [
-                [np.nan, 2, np.nan, 0],
-                [3, 4, np.nan, 1],
-                [np.nan, np.nan, np.nan, 5],
-                [np.nan, 3, np.nan, 4],
-            ],
-            columns=list("ABCD"),
-        )
-
-        expected = df.fillna(axis=1, value=100, limit=1)
-        assert expected is not df
-
-        df.fillna(axis=1, value=100, limit=1, inplace=True)
-        tm.assert_frame_equal(df, expected)
-
     def test_fillna_dict_series(self):
         df = DataFrame(
             {
@@ -625,6 +609,24 @@ class TestFillNA:
 
         tm.assert_frame_equal(result, expected)
         tm.assert_frame_equal(result2, expected2)
+
+    def test_fillna_inplace_with_columns_limit_and_value(self):
+        # GH40989
+        df = DataFrame(
+            [
+                [np.nan, 2, np.nan, 0],
+                [3, 4, np.nan, 1],
+                [np.nan, np.nan, np.nan, 5],
+                [np.nan, 3, np.nan, 4],
+            ],
+            columns=list("ABCD"),
+        )
+
+        expected = df.fillna(axis=1, value=100, limit=1)
+        assert expected is not df
+
+        df.fillna(axis=1, value=100, limit=1, inplace=True)
+        tm.assert_frame_equal(df, expected)
 
 
 def test_fillna_nonconsolidated_frame():
