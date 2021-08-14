faaaa3b1446458ae3a5a0b1f9b390c2f51b096c7
Matthew Roeschke <emailformattr@gmail.com>
2021-08-13 17:04:33 -0700

    Use ast to check for the errors

--- scripts/pandas_errors_documented.py
+++ scripts/pandas_errors_documented.py
@@ -1,31 +1,50 @@
 """
-Check that doc/source/reference/general_utility_functions.rst documents all exceptions and
-warnings in  pandas/errors/__init__.py.
+Check that doc/source/reference/general_utility_functions.rst documents
+all exceptions and warnings in pandas/errors/__init__.py.
 
 This is meant to be run as a pre-commit hook - to run it manually, you can do:
 
     pre-commit run pandas-errors-documented --all-files
 """
-import inspect
+from __future__ import annotations
+
+import argparse
+import ast
 import sys
-
-from pandas import errors
-
+from typing import Sequence
 
 API_PATH = "doc/source/reference/general_utility_functions.rst"
 
 
-def main():
-    with open(API_PATH, "r") as f:
-        api_docs = f.read()
-        missing = []
-        for obj in errors.__dict__.values():
-            if inspect.isclass(obj) and issubclass(obj, (Exception, Warning)) and obj.__name__ not in api_docs:
-                missing.append(obj.__name__)
-        if missing:
-            sys.stdout.write(f"The follow exceptions and/or warnings are not documented in {API_PATH}: {missing}")
-            sys.exit(1)
-        sys.exit(0)
+def get_defined_errors(content: str) -> set[str]:
+    errors = set()
+    for node in ast.walk(ast.parse(content)):
+        if isinstance(node, ast.ClassDef):
+            errors.add(node.name)
+        elif isinstance(node, ast.ImportFrom):
+            for alias in node.names:
+                errors.add(alias.name)
+    return errors
+
+
+def main(argv: Sequence[str] | None = None) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path")
+    args = parser.parse_args(argv)
+    with open(args.path, encoding="utf-8") as f:
+        file_errors = get_defined_errors(f.read())
+    with open(API_PATH) as f:
+        doc_errors = {
+            line.split(".")[1].strip() for line in f.readlines() if "errors" in line
+        }
+    missing = file_errors.difference(doc_errors)
+    if missing:
+        sys.stdout.write(
+            f"The follow exceptions and/or warnings are not documented "
+            f"in {API_PATH}: {missing}"
+        )
+        sys.exit(1)
+    sys.exit(0)
 
 
 if __name__ == "__main__":
