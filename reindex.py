2bc90977dc6d1d20e1914583e875c5b259fd08d7
Harsh Kumar <harsh_201800494@smit.smu.edu.in>
2021-08-18 03:42:21 +0530

    Revert "#43090 Hope it helps!!"
    This reverts commit 01345d34ab09446b217c9cb0c034d5f55215b65a.

--- reindex.py
+++
@@ -1,5 +0,0 @@
-date_index = pd.date_range('1/1/2010', periods=6, freq='D')
-df2 = pd.DataFrame({"prices": [100, 101, np.nan, 100, 89, 88]},
-                   index=date_index)
-date_index2 = pd.date_range('12/29/2009', periods=10, freq='D')
-df2 = df2.reindex(date_index2)
