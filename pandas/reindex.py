4f687079dce519d6642bf2260c1086ce770c7572
Pragati Mehra <pragatimehra21@gmail.com>
2021-08-18 03:29:28 +0530

    Here is the solution to issue #43085 Basically, you have to make sure that the df2 after reindexing is stored again in df2

--- pandas/reindex.py
+++ pandas/reindex.py
@@ -4,3 +4,4 @@ df2 = pd.DataFrame({"prices": [100, 101, np.nan, 100, 89, 88]},
                    index=date_index)
 date_index2 = pd.date_range('12/29/2009', periods=10, freq='D')
 df2=df2.reindex(date_index2)
+
