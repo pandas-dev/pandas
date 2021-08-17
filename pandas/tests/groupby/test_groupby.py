d7e731668a490b2c5cddfe0fa1ab780bc22626a2
Horace Lai <44500643+horaceklai@users.noreply.github.com>
2021-08-17 02:20:31 +0200

    TST: GH34037 add test for type error when using parameter 'min_period' (#42940)

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -2407,3 +2407,15 @@ def test_datetime_categorical_multikey_groupby_indices():
         ("c", Timestamp("2018-03-01 00:00:00")): np.array([2]),
     }
     assert result == expected
+
+
+def test_rolling_wrong_param_min_period():
+    # GH34037
+    name_l = ["Alice"] * 5 + ["Bob"] * 5
+    val_l = [np.nan, np.nan, 1, 2, 3] + [np.nan, 1, 2, 3, 4]
+    test_df = DataFrame([name_l, val_l]).T
+    test_df.columns = ["name", "val"]
+
+    result_error_msg = r"__init__\(\) got an unexpected keyword argument 'min_period'"
+    with pytest.raises(TypeError, match=result_error_msg):
+        test_df.groupby("name")["val"].rolling(window=2, min_period=1).sum()
