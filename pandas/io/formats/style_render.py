c8cd6db61b1555ce7f805e75ec602b201b667d6c
attack68 <24256554+attack68@users.noreply.github.com>
2021-08-19 01:52:23 +0200

    BUG: also copy `uuid` in `Styler.copy` (#43044)

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -89,8 +89,8 @@ class StylerRenderer:
         self.columns: Index = data.columns
         if not isinstance(uuid_len, int) or not uuid_len >= 0:
             raise TypeError("``uuid_len`` must be an integer in range [0, 32].")
-        self.uuid_len = min(32, uuid_len)
-        self.uuid = uuid or uuid4().hex[: self.uuid_len]
+        self.uuid = uuid or uuid4().hex[: min(32, uuid_len)]
+        self.uuid_len = len(self.uuid)
         self.table_styles = table_styles
         self.table_attributes = table_attributes
         self.caption = caption
