a74d8a00f4ed45922d8c5bceaa9e33d165270abe
Horace Lai <horacelai@gmail.com>
2021-08-22 02:14:03 +0000

    TST: add test for getting array from series

--- pandas/tests/series/test_constructors.py
+++ pandas/tests/series/test_constructors.py
@@ -1827,3 +1827,16 @@ def test_constructor(rand_series_with_duplicate_datetimeindex):
     dups = rand_series_with_duplicate_datetimeindex
     assert isinstance(dups, Series)
     assert isinstance(dups.index, DatetimeIndex)
+
+
+@pytest.mark.parametrize(
+    "input_dict,expected",
+    [
+        ({0: 0}, np.array([[0]], dtype=int)),
+        ({"a": "a"}, np.array([["a"]], dtype=object)),
+        ({1: 1}, np.array([[1]], dtype=int)),
+    ],
+)
+def test_numpy_array(input_dict, expected):
+    result = np.array([Series(input_dict)])
+    tm.assert_numpy_array_equal(result, expected)
