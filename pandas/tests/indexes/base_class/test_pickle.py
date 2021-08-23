b581bfe906714d66441f449d729fae824606563b
Brock <jbrockmendel@gmail.com>
2021-08-23 15:44:11 -0700

    whatsnew

--- pandas/tests/indexes/base_class/test_pickle.py
+++ pandas/tests/indexes/base_class/test_pickle.py
@@ -3,7 +3,7 @@ import pandas._testing as tm
 
 
 def test_pickle_preserves_object_dtype():
-    # GH#43155 don't infer numeric dtype
+    # GH#43188, GH#43155 don't infer numeric dtype
     index = Index([1, 2, 3], dtype=object)
 
     result = tm.round_trip_pickle(index)
