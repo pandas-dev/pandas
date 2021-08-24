53c97dbd4f360f473870c984505dc052c90618fa
debnathshoham <debnathshoham@gmail.com>
2021-08-24 12:29:58 +0530

    simplified as suggested

--- pandas/tests/indexing/multiindex/test_multiindex.py
+++ pandas/tests/indexing/multiindex/test_multiindex.py
@@ -101,21 +101,20 @@ class TestMultiIndexBasic:
 
     def test_multiindex_complex(self):
         # GH#42145
-        complex_data = [1 + 2j, 4 - 3j, 10 - 1j, 4 + 5j, 5 - 8j]
-        non_complex_data1 = [3, 4, 5, 6, 7]
-        non_complex_data2 = [0.1, 0.2, 0.3, 0.4, 0.5]
+        complex_data = [1 + 2j, 4 - 3j, 10 - 1j]
+        non_complex_data = [3, 4, 5]
         result = DataFrame(
             {
                 "x": complex_data,
-                "y": non_complex_data1,
-                "z": non_complex_data2,
+                "y": non_complex_data,
+                "z": non_complex_data,
             }
         )
         result.set_index(["x", "y"], inplace=True)
         expected = DataFrame(
-            {"z": non_complex_data2},
+            {"z": non_complex_data},
             index=MultiIndex.from_arrays(
-                [complex_data, non_complex_data1],
+                [complex_data, non_complex_data],
                 names=("x", "y"),
             ),
         )
