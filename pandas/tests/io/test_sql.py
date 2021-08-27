de4e38061a98ce7ec8fa2ef27a3b428e2cc3c4aa
Fangchen Li <fangchen.li@outlook.com>
2021-08-27 11:43:03 -0500

    BUG: check is_named_tuple for objects that are not subclass of tuple #40682

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -2189,6 +2189,43 @@ class _TestSQLiteAlchemy:
         with tm.assert_produces_warning(None):
             sql.read_sql_table("test_bigintwarning", self.conn)
 
+    def test_row_object_is_named_tuple(self):
+        # GH 40682
+        # Test for the is_named_tuple() function
+        # Placed here due to its usage of sqlalchemy
+
+        from sqlalchemy import (
+            Column,
+            Integer,
+            String,
+        )
+        from sqlalchemy.orm import (
+            declarative_base,
+            sessionmaker,
+        )
+
+        BaseModel = declarative_base()
+
+        class Test(BaseModel):
+            __tablename__ = "test_Frame"
+            id = Column(Integer, primary_key=True)
+            foo = Column(String(50))
+
+        BaseModel.metadata.create_all(self.conn)
+        Session = sessionmaker(bind=self.conn)
+        session = Session()
+
+        df = pd.DataFrame({"id": [0, 1], "foo": ["hello", "world"]})
+        df.to_sql("test_Frame", con=self.conn, index=False, if_exists="replace")
+
+        session.commit()
+        foo = session.query(Test.id, Test.foo)
+        print(foo)
+        df = pd.DataFrame(foo)
+        session.close()
+
+        assert list(df.columns) == ["id", "foo"]
+
 
 class _TestMySQLAlchemy:
     """
