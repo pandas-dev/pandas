b5e4e2eb5dde3837a858619b572d5a6d785f7631
Vaibhav K <58251592+oovk@users.noreply.github.com>
2021-08-27 16:26:34 +0530

    Added example to Dataframe.get docs (#43233)
    * added example to Dataframe.get
    
    * added example for default param in dataframe.get doc
    
    * made suggested changes in dataframe.get example
    
    * more changes
    
    * fixup
    
    Co-authored-by: Marco Gorelli <marcogorelli@protonmail.com>

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -3991,6 +3991,38 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         Returns
         -------
         value : same type as items contained in object
+
+        Examples
+        --------
+        >>> df = pd.DataFrame(
+        ...     [
+        ...         [24.3, 75.7, "high"],
+        ...         [31, 87.8, "high"],
+        ...         [22, 71.6, "medium"],
+        ...         [35, 95, "medium"],
+        ...     ],
+        ...     columns=["temp_celsius", "temp_fahrenheit", "windspeed"],
+        ...     index=pd.date_range(start="2014-02-12", end="2014-02-15", freq="D"),
+        ... )
+
+        >>> df
+                    temp_celsius  temp_fahrenheit windspeed
+        2014-02-12          24.3             75.7      high
+        2014-02-13          31.0             87.8      high
+        2014-02-14          22.0             71.6    medium
+        2014-02-15          35.0             95.0    medium
+
+        >>> df.get(["temp_celsius", "windspeed"])
+                    temp_celsius windspeed
+        2014-02-12          24.3      high
+        2014-02-13          31.0      high
+        2014-02-14          22.0    medium
+        2014-02-15          35.0    medium
+
+        If the key isn't found, the default value will be used.
+
+        >>> df.get(["temp_celsius", "temp_kelvin"], default="default_value")
+        'default_value'
         """
         try:
             return self[key]
