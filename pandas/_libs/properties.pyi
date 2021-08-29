b74e9e21dca05d3840a2775e7ecd79331bfaed1e
Torsten Wörtwein <twoertwein@gmail.com>
2021-08-29 19:23:40 -0400

    need to expose everything from properties.pyx

--- pandas/_libs/properties.pyi
+++ pandas/_libs/properties.pyi
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from typing import (
+    Any,
     Callable,
     Generic,
     TypeVar,
@@ -10,10 +11,14 @@ from typing import (
 F = TypeVar("F")
 G = TypeVar("G")
 
-class cache_readonly(Generic[F, G]):
+class CachedProperty(Generic[F, G]):
     def __init__(self, func: Callable[[F], G]) -> None: ...
     @overload
     def __get__(self, obj: F, typ) -> G: ...
     @overload
     def __get__(self, obj: None, typ) -> cache_readonly[F, G]: ...
     def __set__(self, obj: F, value: G) -> None: ...
+
+cache_readonly = CachedProperty
+
+AxisProperty: Any
