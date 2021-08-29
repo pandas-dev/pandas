f7f831be3dc8a6fa8f4865ff22759bf53f0f31fa
Torsten Wörtwein <twoertwein@gmail.com>
2021-08-29 09:34:53 -0400

    type 'typ'

--- pandas/_libs/properties.pyi
+++ pandas/_libs/properties.pyi
@@ -1,6 +1,9 @@
+from __future__ import annotations
+
 from typing import (
     Callable,
     Protocol,
+    Type,
     TypeVar,
     overload,
 )
@@ -11,7 +14,7 @@ _G = TypeVar("_G")
 class cache_readonly(Protocol[_F, _G]):
     def __init__(self, func: Callable[[_F], _G]) -> None: ...
     @overload
-    def __get__(self, obj: _F, typ) -> _G: ...
+    def __get__(self, obj: _F, typ: Type[_F] | None) -> _G: ...
     @overload
-    def __get__(self, obj: None, typ) -> cache_readonly[_F, _G]: ...
+    def __get__(self, obj: None, typ: Type[_F] | None) -> cache_readonly[_F, _G]: ...
     def __set__(self, obj: _F, value: _G) -> None: ...
