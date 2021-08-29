47d0126aed01c643ad55ed97e014f021449f5850
Torsten Wörtwein <twoertwein@gmail.com>
2021-08-28 21:11:20 -0400

    overload for self

--- pandas/_libs/properties.pyi
+++ pandas/_libs/properties.pyi
@@ -2,12 +2,16 @@ from typing import (
     Callable,
     Protocol,
     TypeVar,
+    overload,
 )
 
-_F = TypeVar("_F", contravariant=True)
+_F = TypeVar("_F")
 _G = TypeVar("_G")
 
 class cache_readonly(Protocol[_F, _G]):
     def __init__(self, func: Callable[[_F], _G]) -> None: ...
+    @overload
     def __get__(self, obj: _F, typ) -> _G: ...
+    @overload
+    def __get__(self, obj: None, typ) -> cache_readonly[_F, _G]: ...
     def __set__(self, obj: _F, value: _G) -> None: ...
