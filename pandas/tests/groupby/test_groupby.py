46a29e05bae0b0a77d6cc617f27fb649be1adece
debnathshoham <debnathshoham@gmail.com>
2021-08-30 20:13:30 +0530

    reorganized tests

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -2422,17 +2422,23 @@ def test_rolling_wrong_param_min_period():
 
 
 @pytest.mark.parametrize(
-    "func, expected",
-    [("sum", [5, 7, 9]), ("std", [4.5 ** 0.5] * 3), ("var", [4.5] * 3)],
+    "func, expected, dtype, result_dtype",
+    [
+        ("sum", [5, 7, 9], int, int),
+        ("std", [4.5 ** 0.5] * 3, int, float),
+        ("var", [4.5] * 3, int, float),
+        ("sum", [5, 7, 9], "Int64", "Int64"),
+        # result_dtype should ideally be Float64
+        ("std", [4.5 ** 0.5] * 3, "Int64", float),
+        ("var", [4.5] * 3, "Int64", "Float64"),
+    ],
 )
-@pytest.mark.parametrize("dtype", [int, "Int64"])
-def test_multiindex_groupby(dtype, func, expected):
+def test_multiindex_groupby(func, expected, dtype, result_dtype):
     # GH#43209
     df = DataFrame(
         [[1, 2, 3, 4, 5, 6]] * 3,
         columns=MultiIndex.from_product([["a", "b"], ["i", "j", "k"]]),
     ).astype({("a", "j"): dtype, ("b", "j"): dtype})
     result = df.groupby(level=1, axis=1).agg(func)
-    out_dtype = dtype if func == "sum" else None
-    expected = DataFrame([expected] * 3, columns=["i", "j", "k"], dtype=out_dtype)
-    tm.assert_frame_equal(result, expected, check_dtype=False)
+    expected = DataFrame([expected] * 3, columns=["i", "j", "k"], dtype=result_dtype)
+    tm.assert_frame_equal(result, expected)
