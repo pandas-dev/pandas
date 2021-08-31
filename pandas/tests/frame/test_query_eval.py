37e9d0281df58ef2bea3eaa70ba6a9bb6ab5b0c4
Alexis Mignon <alexis.mignon@proabyes.com>
2021-08-31 09:43:12 +0200

    Passed black and removed 'pd' prefix as indicated by flake8

--- pandas/tests/frame/test_query_eval.py
+++ pandas/tests/frame/test_query_eval.py
@@ -734,16 +734,19 @@ class TestDataFrameQueryNumExprPandas:
     def test_method_calls_in_query(self):
         # https://github.com/pandas-dev/pandas/issues/22435
         n = 10
-        df = pd.DataFrame({"a": 2 * np.random.rand(n), "b": np.random.rand(n)})
+        df = DataFrame({"a": 2 * np.random.rand(n), "b": np.random.rand(n)})
         expected = df[df["a"].astype("int") == 0]
-        result = df.query("a.astype('int') == 0",
-                          engine=self.engine, parser=self.parser)
+        result = df.query(
+            "a.astype('int') == 0", engine=self.engine, parser=self.parser
+        )
         tm.assert_frame_equal(result, expected)
 
-        df = pd.DataFrame({
-            "a": np.where(np.random.rand(n) < 0.5, np.nan, np.random.randn(n)),
-            "b": np.random.randn(n)
-        })
+        df = DataFrame(
+            {
+                "a": np.where(np.random.rand(n) < 0.5, np.nan, np.random.randn(n)),
+                "b": np.random.randn(n),
+            }
+        )
         expected = df[df["a"].notnull()]
         result = df.query("a.notnull()", engine=self.engine, parser=self.parser)
         tm.assert_frame_equal(result, expected)
