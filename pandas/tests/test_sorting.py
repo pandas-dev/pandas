0aee999622d343e459137fd42a30e64612a5e477
DeepakPareta <82256264+DeepakPareta@users.noreply.github.com>
2021-08-31 16:49:57 +0530

    Update test_sorting.py

--- pandas/tests/test_sorting.py
+++ pandas/tests/test_sorting.py
@@ -48,14 +48,14 @@ class TestSorting:
         lg = df.groupby(["A", "B", "C", "D", "E", "F", "G", "H"])
         rg = df.groupby(["H", "G", "F", "E", "D", "C", "B", "A"])
 
-        left = lg.sum()["values"]
-        right = rg.sum()["values"]
+        left = rg.sum()["values"]
+        right = lg.sum()["values"]
 
         exp_index, _ = left.index.sortlevel()
-        tm.assert_index_equal(left.index, exp_index)
+        tm.assert_index_equal(right.index, exp_index)
 
         exp_index, _ = right.index.sortlevel(0)
-        tm.assert_index_equal(right.index, exp_index)
+        tm.assert_index_equal(left.index, exp_index)
 
         tups = list(map(tuple, df[["A", "B", "C", "D", "E", "F", "G", "H"]].values))
         tups = com.asarray_tuplesafe(tups)
