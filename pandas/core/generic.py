7dbc6c43d6b58464baed8544f17af76d346d1832
Suyash Gupta <suyashgupta0001@gmail.com>
2021-08-31 16:08:03 +0000

    updated docstring for CI error

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -7610,7 +7610,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         include_start=lib.no_default,
         include_end=lib.no_default,
         axis=None,
-        inclusive="both",
+        inclusive: str = "both",
     ) -> FrameOrSeries:
         """
         Select values between particular times of the day (e.g., 9:00-9:30 AM).
