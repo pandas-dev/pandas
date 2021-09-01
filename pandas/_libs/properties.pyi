e2024f79d742e05e68d0c9f4eaabc9a46e52b204
Torsten Wörtwein <twoertwein@gmail.com>
2021-09-01 09:50:38 -0400

    sub-classing

--- pandas/_libs/properties.pyi
+++ pandas/_libs/properties.pyi
@@ -9,6 +9,7 @@ from typing import (
     overload,
 )
 
+T = TypeVar("T")
 F = TypeVar("F", contravariant=True)
 G = TypeVar("G", covariant=True)
 
@@ -17,7 +18,7 @@ class CachedProperty(Generic[F, G]):
     @overload
     def __get__(self, obj: F, typ: Type[F]) -> G: ...
     @overload
-    def __get__(self, obj: None, typ: Type[F]) -> cache_readonly[F, G]: ...
+    def __get__(self: T, obj: None, typ: Type[F]) -> T: ...
     def __set__(self, obj: F, value: Any) -> None: ...
 
 cache_readonly = CachedProperty
