9345b0a07fe6691c95844e4a32ead1fc722c9df2
Torsten Wörtwein <twoertwein@gmail.com>
2021-09-01 13:58:41 -0400

    cache_readonly is almost a property

--- pandas/_libs/properties.pyi
+++ pandas/_libs/properties.pyi
@@ -1,26 +1,9 @@
-from __future__ import annotations
-
 from typing import (
     Any,
-    Callable,
-    Generic,
     Type,
-    TypeVar,
-    overload,
 )
 
-T = TypeVar("T")
-F = TypeVar("F", contravariant=True)
-G = TypeVar("G", covariant=True)
-
-class CachedProperty(Generic[F, G]):
-    def __init__(self, func: Callable[[F], G]) -> None: ...
-    @overload
-    def __get__(self, obj: F, typ: Type[F]) -> G: ...
-    @overload
-    def __get__(self: T, obj: None, typ: Type[F]) -> T: ...
-    def __set__(self, obj: F, value: Any) -> None: ...
-
+CachedProperty: Type[property]
 cache_readonly = CachedProperty
 
 class AxisProperty:
