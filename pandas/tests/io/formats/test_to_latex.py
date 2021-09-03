f7da49ed5d7a82697665ebc91ffe9621d9598fad
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-09-03 21:59:43 +0200

    fix tests

--- pandas/tests/io/formats/test_to_latex.py
+++ pandas/tests/io/formats/test_to_latex.py
@@ -695,35 +695,34 @@ class TestToLatexCaptionLabel:
     ):
         # GH 25436
         result = df_short.to_latex(
-            longtable=True,
+            environment="longtable",
+            hrules=True,
             caption=caption_longtable,
             label=label_longtable,
         )
         expected = _dedent(
             r"""
-            \begin{longtable}{lrl}
-            \caption{a table in a \texttt{longtable} environment}
-            \label{tab:longtable}\\
-            \toprule
-            {} & {a} & {b} \\
-            \midrule
-            \endfirsthead
-            \caption[]{a table in a \texttt{longtable} environment} \\
-            \toprule
-            {} & {a} & {b} \\
-            \midrule
-            \endhead
-            \midrule
-            \multicolumn{3}{r}{Continued on next page} \\
-            \midrule
-            \endfoot
-
-            \bottomrule
-            \endlastfoot
-            0 & 1 & b1 \\
-            1 & 2 & b2 \\
-            \end{longtable}
-            """
+        \begin{longtable}{lrl}
+        \caption{a table in a \texttt{longtable} environment} \label{tab:longtable} \\
+        \toprule
+        {} & {a} & {b} \\
+        \midrule
+        \endfirsthead
+        \caption[]{a table in a \texttt{longtable} environment} \\
+        \toprule
+        {} & {a} & {b} \\
+        \midrule
+        \endhead
+        \midrule
+        \multicolumn{3}{r}{Continued on next page} \\
+        \midrule
+        \endfoot
+        \bottomrule
+        \endlastfoot
+        0 & 1 & b1 \\
+        1 & 2 & b2 \\
+        \end{longtable}
+        """
         )
         assert result == expected
 
