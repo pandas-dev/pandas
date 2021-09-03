0c55981279d92ba2b5604a6bc6d97283c30af624
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-09-03 19:44:15 +0200

    fix tests

--- pandas/tests/io/formats/test_to_latex.py
+++ pandas/tests/io/formats/test_to_latex.py
@@ -1058,15 +1058,15 @@ class TestToLatexMultiindex:
         # GH 16718
         df = DataFrame({"a": [0], "b": [1], "c": [2], "d": [3]})
         df = df.set_index(["a", "b"])
-        observed = df.to_latex(header=["r1", "r2"])
+        observed = df.to_latex(header=["r1", "r2"], hrules=True)
         expected = _dedent(
             r"""
             \begin{tabular}{llrr}
             \toprule
-              &   & r1 & r2 \\
-            a & b &    &    \\
+            {} & {} & {r1} & {r2} \\
+            {a} & {b} & {} & {} \\
             \midrule
-            0 & 1 &  2 &  3 \\
+            0 & 1 & 2 & 3 \\
             \bottomrule
             \end{tabular}
             """
