7f314a0084d70710d3e08c168688b062405000e2
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-09-03 20:28:03 +0200

    fix tests

--- pandas/tests/io/formats/test_to_latex.py
+++ pandas/tests/io/formats/test_to_latex.py
@@ -1284,20 +1284,18 @@ class TestToLatexMultiindex:
         assert result == expected
 
     def test_to_latex_multirow_true(self, multicolumn_frame):
-        result = multicolumn_frame.T.to_latex(multirow=True)
+        result = multicolumn_frame.T.to_latex(hrules=True)
         expected = _dedent(
             r"""
             \begin{tabular}{llrrrrr}
             \toprule
-               &   &  0 &  1 &  2 &  3 &  4 \\
+            {} & {} & {0} & {1} & {2} & {3} & {4} \\
             \midrule
-            \multirow{2}{*}{c1} & 0 &  0 &  1 &  2 &  3 &  4 \\
-               & 1 &  5 &  6 &  7 &  8 &  9 \\
-            \cline{1-7}
-            \multirow{2}{*}{c2} & 0 &  0 &  1 &  2 &  3 &  4 \\
-               & 1 &  5 &  6 &  7 &  8 &  9 \\
-            \cline{1-7}
-            c3 & 0 &  0 &  1 &  2 &  3 &  4 \\
+            \multirow[c]{2}{*}{c1} & 0 & 0 & 1 & 2 & 3 & 4 \\
+             & 1 & 5 & 6 & 7 & 8 & 9 \\
+            \multirow[c]{2}{*}{c2} & 0 & 0 & 1 & 2 & 3 & 4 \\
+             & 1 & 5 & 6 & 7 & 8 & 9 \\
+            c3 & 0 & 0 & 1 & 2 & 3 & 4 \\
             \bottomrule
             \end{tabular}
             """
@@ -1307,24 +1305,21 @@ class TestToLatexMultiindex:
     def test_to_latex_multicolumnrow_with_multicol_format(self, multicolumn_frame):
         multicolumn_frame.index = multicolumn_frame.T.index
         result = multicolumn_frame.T.to_latex(
-            multirow=True,
-            multicolumn=True,
-            multicolumn_format="c",
+            multirow_align="t",
+            multicol_align="c",
         )
         expected = _dedent(
             r"""
             \begin{tabular}{llrrrrr}
             \toprule
-               &   & \multicolumn{2}{c}{c1} & \multicolumn{2}{c}{c2} & c3 \\
-               &   &  0 &  1 &  0 &  1 &  0 \\
+            {} & {} & \multicolumn{2}{c}{c1} & \multicolumn{2}{c}{c2} & c3 \\
+            {} & {} & {0} & {1} & {0} & {1} & {0} \\
             \midrule
-            \multirow{2}{*}{c1} & 0 &  0 &  1 &  2 &  3 &  4 \\
-               & 1 &  5 &  6 &  7 &  8 &  9 \\
-            \cline{1-7}
-            \multirow{2}{*}{c2} & 0 &  0 &  1 &  2 &  3 &  4 \\
-               & 1 &  5 &  6 &  7 &  8 &  9 \\
-            \cline{1-7}
-            c3 & 0 &  0 &  1 &  2 &  3 &  4 \\
+            \multirow[t]{2}{*}{c1} & 0 & 0 & 1 & 2 & 3 & 4 \\
+             & 1 & 5 & 6 & 7 & 8 & 9 \\
+            \multirow[t]{2}{*}{c2} & 0 & 0 & 1 & 2 & 3 & 4 \\
+             & 1 & 5 & 6 & 7 & 8 & 9 \\
+            c3 & 0 & 0 & 1 & 2 & 3 & 4 \\
             \bottomrule
             \end{tabular}
             """
