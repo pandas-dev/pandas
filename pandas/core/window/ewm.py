9a997c8e5f5db0ec42dc39c3bf7d97700e4b4a60
Matthew Roeschke <emailformattr@gmail.com>
2021-09-04 18:43:20 -0700

    DEPR: Passing in a string column label for DataFrame.ewm(times=...) (#43265)

--- pandas/core/window/ewm.py
+++ pandas/core/window/ewm.py
@@ -21,6 +21,7 @@ if TYPE_CHECKING:
 
 from pandas.compat.numpy import function as nv
 from pandas.util._decorators import doc
+from pandas.util._exceptions import find_stack_level
 
 from pandas.core.dtypes.common import is_datetime64_ns_dtype
 from pandas.core.dtypes.missing import isna
@@ -315,6 +316,15 @@ class ExponentialMovingWindow(BaseWindow):
             if not self.adjust:
                 raise NotImplementedError("times is not supported with adjust=False.")
             if isinstance(self.times, str):
+                warnings.warn(
+                    (
+                        "Specifying times as a string column label is deprecated "
+                        "and will be removed in a future version. Pass the column "
+                        "into times instead."
+                    ),
+                    FutureWarning,
+                    stacklevel=find_stack_level(),
+                )
                 self.times = self._selected_obj[self.times]
             if not is_datetime64_ns_dtype(self.times):
                 raise ValueError("times must be datetime64[ns] dtype.")
