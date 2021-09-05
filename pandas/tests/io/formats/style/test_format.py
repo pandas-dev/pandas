ecd01dd298378e872e63f9d25bd9565562318d65
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-09-05 10:38:47 +0200

    fix recent merged tests

--- pandas/tests/io/formats/style/test_format.py
+++ pandas/tests/io/formats/style/test_format.py
@@ -408,7 +408,7 @@ def test_1level_multiindex():
     midx = MultiIndex.from_product([[1, 2]], names=[""])
     df = DataFrame(-1, index=midx, columns=[0, 1])
     ctx = df.style._translate(True, True)
-    assert ctx["body"][0][0]["display_value"] == 1
+    assert ctx["body"][0][0]["display_value"] == "1"
     assert ctx["body"][0][0]["is_visible"] is True
-    assert ctx["body"][1][0]["display_value"] == 2
+    assert ctx["body"][1][0]["display_value"] == "2"
     assert ctx["body"][1][0]["is_visible"] is True
