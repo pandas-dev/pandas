6a683a2911bd82ea40c7e11b8135a0e952f55f2c
jbrockmendel <jbrockmendel@gmail.com>
2021-09-04 18:45:58 -0700

    PERF: MultiIndex.get_indexer (#43370)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3618,7 +3618,12 @@ class Index(IndexOpsMixin, PandasObject):
         elif method == "nearest":
             indexer = self._get_nearest_indexer(target, limit, tolerance)
         else:
-            indexer = self._engine.get_indexer(target._get_engine_target())
+            tgt_values = target._get_engine_target()
+            if target._is_multi and self._is_multi:
+                # error: Incompatible types in assignment (expression has type
+                # "Index", variable has type "ndarray[Any, Any]")
+                tgt_values = target  # type: ignore[assignment]
+            indexer = self._engine.get_indexer(tgt_values)
 
         return ensure_platform_int(indexer)
 
@@ -5459,6 +5464,8 @@ class Index(IndexOpsMixin, PandasObject):
         # Note: _maybe_promote ensures we never get here with MultiIndex
         #  self and non-Multi target
         tgt_values = target._get_engine_target()
+        if self._is_multi and target._is_multi:
+            tgt_values = target
 
         indexer, missing = self._engine.get_indexer_non_unique(tgt_values)
         return ensure_platform_int(indexer), ensure_platform_int(missing)
