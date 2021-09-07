63970356f041a826c0792ee91f6e805afa390c3c
Leonardus Chen <leonardus.chen@gmail.com>
2021-09-07 21:47:03 +0800

    Fix regex error in Windows

--- pandas/tests/io/test_common.py
+++ pandas/tests/io/test_common.py
@@ -247,11 +247,10 @@ Look,a snake,🐍"""
         dummy_frame = pd.DataFrame({"a": [1, 2, 3], "b": [2, 3, 4], "c": [3, 4, 5]})
 
         path = os.path.join(HERE, "data", "missing_folder", "does_not_exist." + fn_ext)
-        dirname = os.path.dirname(path)
 
         with pytest.raises(
             error_class,
-            match=fr"Cannot save file into a non-existent directory: '{dirname}'",
+            match=r"Cannot save file into a non-existent directory: .*missing_folder",
         ):
             method(dummy_frame, path)
 
