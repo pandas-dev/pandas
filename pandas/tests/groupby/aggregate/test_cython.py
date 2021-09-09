2d7d0355e820d7dbf5c101a2f2e222c379337f30
Prerana Chakraborty <40196782+kurchi1205@users.noreply.github.com>
2021-09-09 23:29:37 +0530

    Updating _resolve_numeric_only function of GroupBy (#43154)

--- pandas/tests/groupby/aggregate/test_cython.py
+++ pandas/tests/groupby/aggregate/test_cython.py
@@ -97,7 +97,8 @@ def test_cython_agg_nothing_to_agg():
 
     frame = DataFrame({"a": np.random.randint(0, 5, 50), "b": ["foo", "bar"] * 25})
 
-    result = frame[["b"]].groupby(frame["a"]).mean()
+    with tm.assert_produces_warning(FutureWarning, check_stacklevel=False):
+        result = frame[["b"]].groupby(frame["a"]).mean()
     expected = DataFrame([], index=frame["a"].sort_values().drop_duplicates())
     tm.assert_frame_equal(result, expected)
 
