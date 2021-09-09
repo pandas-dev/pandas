7d790cf7157b539417e7214dcf3521e94d42295d
Prerana Chakraborty <40196782+kurchi1205@users.noreply.github.com>
2021-09-09 23:29:37 +0530

    Updating _resolve_numeric_only function of GroupBy (#43154)

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -1195,6 +1195,14 @@ class GroupBy(BaseGroupBy[FrameOrSeries]):
             if self.obj.ndim == 2:
                 # i.e. DataFrameGroupBy
                 numeric_only = True
+                # GH#42395 GH#43108 GH#43154
+                # Regression from 1.2.5 to 1.3 caused object columns to be dropped
+                obj = self._obj_with_exclusions
+                check = obj._get_numeric_data()
+                if len(obj.columns) and not len(check.columns) and not obj.empty:
+                    numeric_only = False
+                    # TODO: v1.4+ Add FutureWarning
+
             else:
                 numeric_only = False
 
