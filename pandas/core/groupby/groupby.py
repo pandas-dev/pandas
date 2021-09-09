a55b74bb1adeea4ef11602d26397a153ccb4caf0
debnathshoham <debnathshoham@gmail.com>
2021-09-09 12:48:04 +0530

    suggested change

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -2440,7 +2440,7 @@ class GroupBy(BaseGroupBy[FrameOrSeries]):
                 inference = np.dtype("timedelta64[ns]")
                 out = np.asarray(vals).astype(float)
             elif isinstance(vals, ExtensionArray) and is_float_dtype(vals):
-                inference = np.dtype(object)
+                inference = np.dtype(np.float64)
                 out = vals.to_numpy(dtype=float, na_value=np.nan)
             else:
                 out = np.asarray(vals)
