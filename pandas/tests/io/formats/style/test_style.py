7723e5615ddae9e514c39096a731c3448b80b702
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-09-09 07:38:51 +0200

    add direct test

--- pandas/tests/io/formats/style/test_style.py
+++ pandas/tests/io/formats/style/test_style.py
@@ -1468,3 +1468,25 @@ def test_hiding_headers_over_axis_no_sparsify(axis):
     if axis == "index":  # test row headers
         for ix in [(0, 0), (0, 1), (1, 0), (1, 1)]:
             assert ctx["body"][ix[0]][ix[1]]["is_visible"] is False
+
+
+def test_get_level_lengths_mi_hidden():
+    # GH 43464
+    index = MultiIndex.from_arrays([[1, 1, 1, 2, 2, 2], ["a", "a", "b", "a", "a", "b"]])
+    expected = {
+        (0, 2): 1,
+        (0, 3): 1,
+        (0, 4): 1,
+        (0, 5): 1,
+        (1, 2): 1,
+        (1, 3): 1,
+        (1, 4): 1,
+        (1, 5): 1,
+    }
+    result = _get_level_lengths(
+        index,
+        sparsify=False,
+        max_index=100,
+        hidden_elements=[0, 1, 0, 1],  # hidden element can repeat if duplicated index
+    )
+    tm.assert_dict_equal(result, expected)
