0a2e0a9a74e5eb8aea3bf399f58cef220ae8f754
shubham11941140 <63910248+shubham11941140@users.noreply.github.com>
2021-09-11 10:53:20 +0530

    Remove build error

--- pandas/tests/series/methods/test_convert_dtypes.py
+++ pandas/tests/series/methods/test_convert_dtypes.py
@@ -228,7 +228,7 @@ class TestSeriesConvertDtypes:
         tm.assert_frame_equal(df, df.convert_dtypes())
 
     def test_convert_byte_string_dtype(self):
-        # GH-43183
+        # GH43183
         byte_str = b"binary-string"
 
         df = pd.DataFrame(data={"A": byte_str}, index=[0])
