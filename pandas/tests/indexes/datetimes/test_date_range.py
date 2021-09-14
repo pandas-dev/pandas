e80329f475b4def492af25837ff09bc22340fbf2
Zheyuan Chen <zheyuan09@gmail.com>
2021-09-15 02:08:24 +0800

    remove line

--- pandas/tests/indexes/datetimes/test_date_range.py
+++ pandas/tests/indexes/datetimes/test_date_range.py
@@ -756,7 +756,7 @@ class TestDateRanges:
         if inclusive_endpoints_fixture == "neither":
             expected = both_range[1:-1]
         elif inclusive_endpoints_fixture in ("left", "right", "both"):
-            expected = date_range(start=start, end=end, freq="D", inclusive="both")
+            expected = both_range[:]
 
         tm.assert_index_equal(result, expected)
 
