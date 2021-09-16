fe5ed50171e14dfef9154961fa3424a43306d47b
Jason Bian <jasonzb@umich.edu>
2021-09-15 21:38:30 -0700

    Update test_groupby_shift_emptydf.py

--- pandas/tests/groupby/test_groupby_shift_emptydf.py
+++ pandas/tests/groupby/test_groupby_shift_emptydf.py
@@ -10,7 +10,7 @@ import pandas._testing as tm
 def test_empty_shift_with_fill():
     #This test is designed to replicate the extra named indexes in 41264 caused by having a fill_value parameter
     
-    empty_df = pd.DataFrame(columns=["a", "b", "c"])
+    empty_df = DataFrame(columns=["a", "b", "c"])
     
     shifted = empty_df.groupby(['a']).shift(1)
     shifted_with_fill = empty_df.groupby(['a']).shift(1, fill_value=0)
@@ -21,7 +21,7 @@ def test_empty_shift_with_fill():
 def test_multindex_empty_shift_with_fill():
     #This test is designed to replicate the extra named indexes in 41264 caused by having a fill_value parameter for multindexes
     
-    empty_df = pd.DataFrame(columns=["a", "b", "c"])
+    empty_df = DataFrame(columns=["a", "b", "c"])
     
     shifted = empty_df.groupby(['a','b']).shift(1)
     shifted_with_fill = empty_df.groupby(['a','b']).shift(1, fill_value=0)
@@ -33,8 +33,8 @@ def test_multindex_shift_with_fill():
     #This test is designed to replicate the extra named indexes in 41264. 
     #Both numbered dataframes and empty dataframes should return FrozenList([None])
     
-    num_df = pd.DataFrame({'a':[1,2,3], 'b':[4,5,6], 'c':[7,8,9]})
-    empty_df = pd.DataFrame(columns=["a", "b", "c"])
+    num_df = DataFrame({'a':[1,2,3], 'b':[4,5,6], 'c':[7,8,9]})
+    empty_df = DataFrame(columns=["a", "b", "c"])
     
     shifted = empty_df.groupby(['a','b']).shift(1)
     shifted_with_fill = num_df.groupby(['a','b']).shift(1, fill_value=0)
