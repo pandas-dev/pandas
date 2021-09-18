c034ea09a616431a4c2157867859a60ed45a8376
Matthew Zeitlin <mzeitlin@caltech.edu>
2021-09-18 13:40:53 -0400

    Mark hypothesis slow

--- pandas/tests/io/parser/test_parse_dates.py
+++ pandas/tests/io/parser/test_parse_dates.py
@@ -1643,6 +1643,7 @@ def _helper_hypothesis_delimited_date(call, date_string, **kwargs):
     return msg, result
 
 
+@pytest.mark.slow
 @given(date_strategy)
 @settings(deadline=None)
 @pytest.mark.parametrize("delimiter", list(" -./"))
