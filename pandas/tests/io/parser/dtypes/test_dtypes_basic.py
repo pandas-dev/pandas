85c4560450498c46a802973abcede06e0e206584
Ales Erjavec <ales.erjavec@fri.uni-lj.si>
2021-09-23 11:04:18 +0200

    Test float parsers with leading and trailing spaces

--- pandas/tests/io/parser/dtypes/test_dtypes_basic.py
+++ pandas/tests/io/parser/dtypes/test_dtypes_basic.py
@@ -204,6 +204,11 @@ def test_1000_sep_decimal_float_precision(
     # test decimal and thousand sep handling in across 'float_precision'
     # parsers
     decimal_number_check(c_parser_only, numeric_decimal, thousands, float_precision)
+    text, value = numeric_decimal
+    text = " " + text + " "
+    if isinstance(value, str):  # the negative cases (parse as text)
+        value = " " + value + " "
+    decimal_number_check(c_parser_only, (text, value), thousands, float_precision)
 
 
 def decimal_number_check(parser, numeric_decimal, thousands, float_precision):
