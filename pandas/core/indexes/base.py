69354aaadd16aaa48013076abb78b5c7f2902620
DhruvBShetty <dhruvbshetty@gmail.com>
2021-09-24 23:05:17 +0530

    Added sentence to show that unmatched values return -1

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3348,6 +3348,9 @@ class Index(IndexOpsMixin, PandasObject):
         Compute indexer and mask for new index given the current index. The
         indexer should be then used as an input to ndarray.take to align the
         current data to the new index.
+        
+        Returns -1 for unmatched values, for further explanation see the example
+        below.
 
         Parameters
         ----------
