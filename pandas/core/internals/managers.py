764045d7884a24b0c805133f30cfbd04212ee71b
jbrockmendel <jbrockmendel@gmail.com>
2021-09-25 09:57:41 -0700

    REF: unwrap PandasArray earlier (#43728)

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -71,7 +71,6 @@ from pandas.core.internals.blocks import (
     ensure_block_shape,
     extend_blocks,
     get_block_type,
-    maybe_coerce_values,
     new_block,
 )
 from pandas.core.internals.ops import (
@@ -989,7 +988,6 @@ class BlockManager(libinternals.BlockManager, BaseBlockManager):
 
         # shortcut for select a single-dim from a 2-dim BM
         bp = BlockPlacement(slice(0, len(values)))
-        values = maybe_coerce_values(values)
         nb = type(block)(values, placement=bp, ndim=1)
         return SingleBlockManager(nb, self.axes[1])
 
