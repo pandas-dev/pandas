af9d567df4f6fe82edcd192ba98a9e3e2f18e2fa
Torsten Wörtwein <twoertwein@gmail.com>
2021-09-26 17:52:05 -0400

    also used in tests

--- pandas/plotting/_matplotlib/converter.py
+++ pandas/plotting/_matplotlib/converter.py
@@ -4,6 +4,7 @@ import contextlib
 import datetime as pydt
 from datetime import (
     datetime,
+    timedelta,
     tzinfo,
 )
 import functools
@@ -444,6 +445,28 @@ class MilliSecondLocator(dates.DateLocator):
         vmax = dates.date2num(dmax)
 
         return self.nonsingular(vmin, vmax)
+
+
+def _from_ordinal(x, tz: tzinfo | None = None) -> datetime:
+    ix = int(x)
+    dt = datetime.fromordinal(ix)
+    remainder = float(x) - ix
+    hour, remainder = divmod(24 * remainder, 1)
+    minute, remainder = divmod(60 * remainder, 1)
+    second, remainder = divmod(60 * remainder, 1)
+    microsecond = int(1_000_000 * remainder)
+    if microsecond < 10:
+        microsecond = 0  # compensate for rounding errors
+    dt = datetime(
+        dt.year, dt.month, dt.day, int(hour), int(minute), int(second), microsecond
+    )
+    if tz is not None:
+        dt = dt.astimezone(tz)
+
+    if microsecond > 999990:  # compensate for rounding errors
+        dt += timedelta(microseconds=1_000_000 - microsecond)
+
+    return dt
 
 
 # Fixed frequency dynamic tick locators and formatters
