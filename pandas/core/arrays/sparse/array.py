9a641afe5b769cbbd274a865828ff007d450ca72
Matthew Zeitlin <mzeitlin@caltech.edu>
2021-09-27 20:17:15 -0700

    WIP

--- pandas/core/arrays/sparse/array.py
+++ pandas/core/arrays/sparse/array.py
@@ -892,21 +892,39 @@ class SparseArray(OpsMixin, PandasObject, ExtensionArray):
         elif isinstance(key, tuple):
             data_slice = self.to_dense()[key]
         elif isinstance(key, slice):
-            start = 0 if key.start is None else key.start
-            if start < 0:
-                start += len(self)
 
-            end = len(self) if key.step is None else key.stop
-            if end < 0:
-                end += len(self)
+            # Avoid densifying when handling contiguous slices
+            if key.step is None or key.step == 1:
+                start = 0 if key.start is None else key.start
+                if start < 0:
+                    start += len(self)
 
-            indices = self.sp_index.to_int_index().indices
+                end = len(self) if key.stop is None else key.stop
+                if end < 0:
+                    end += len(self)
 
-            keep_inds = np.flatnonzero((indices >= start) & (indices <= end))
-            sp_vals = self.sp_values[keep_inds]
-            sp_index = indices[keep_inds]
-            new_sp_index = make_sparse_index(len(self), sp_index, self.kind)
-            return type(self)._simple_new(sp_vals, new_sp_index, self.dtype)
+                indices = self.sp_index.to_int_index().indices
+                keep_inds = np.flatnonzero((indices >= start) & (indices < end))
+                sp_vals = self.sp_values[keep_inds]
+
+                sp_index = indices[keep_inds].copy()
+
+                # If we've sliced to not include the start of the array, all our indices
+                # should be shifted. NB: here we are careful to also not shift by a
+                # negative value for a case like [0, 1][-100:] where the start index
+                # should be treated like 0
+                if start > 0:
+                    sp_index -= start
+
+                # Length of our result should match applying this slice to a range
+                # of the length of our original array
+                new_len = len(range(len(self))[key])
+                new_sp_index = make_sparse_index(new_len, sp_index, self.kind)
+                return type(self)._simple_new(sp_vals, new_sp_index, self.dtype)
+            else:
+                indices = np.arange(len(self), dtype=np.int32)[key]
+                return self.take(indices)
+
         else:
             # TODO: I think we can avoid densifying when masking a
             # boolean SparseArray with another. Need to look at the
