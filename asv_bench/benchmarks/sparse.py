BUG: to_coo silently converting non-zero fill values (#43763)
* BUG: to_coo silently converting non-zero fill values

* fix to_coo benchmark to have 0 fill value

asv_bench/benchmarks/sparse.py
@@ -95,11 +95,11 @@ class ToCooFrame:
     def setup(self):
         N = 10000
         k = 10
-        arr = np.full((N, k), np.nan)
+        arr = np.zeros((N, k), dtype=float)
         arr[0, 0] = 3.0
         arr[12, 7] = -1.0
         arr[0, 9] = 11.2
-        self.df = pd.DataFrame(arr, dtype=pd.SparseDtype("float"))
+        self.df = pd.DataFrame(arr, dtype=pd.SparseDtype("float", fill_value=0.0))
 
     def time_to_coo(self):
         self.df.sparse.to_coo()

pandas/core/arrays/sparse/accessor.py
@@ -340,10 +340,14 @@ class SparseFrameAccessor(BaseAccessor, PandasDelegate):
 
         cols, rows, data = [], [], []
         for col, (_, ser) in enumerate(self._parent.iteritems()):
-            row = ser.array.sp_index.to_int_index().indices
+            sp_arr = ser.array
+            if sp_arr.fill_value != 0:
+                raise ValueError("fill value must be 0 when converting to COO matrix")
+
+            row = sp_arr.sp_index.to_int_index().indices
             cols.append(np.repeat(col, len(row)))
             rows.append(row)
-            data.append(ser.array.sp_values.astype(dtype, copy=False))
+            data.append(sp_arr.sp_values.astype(dtype, copy=False))
 
         cols = np.concatenate(cols)
         rows = np.concatenate(rows)