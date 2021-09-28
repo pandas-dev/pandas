081b5553f37fac6fd1486f1a8e0a6205f58f916b
Varun Shrivastava <varunshrivastava2706@gmail.com>
2021-09-28 23:18:54 +0530

    Fixed Issue No. #43755

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -10563,7 +10563,7 @@ NaN 12.3   33.0
 
         When ``values`` is a Series or DataFrame the index and column must
         match. Note that 'falcon' does not match based on the number of legs
-        in df2.
+        in other.
 
         >>> other = pd.DataFrame({'num_legs': [8, 2], 'num_wings': [0, 2]},
         ...                      index=['spider', 'falcon'])
