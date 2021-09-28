c953389832d76ef77edbb89b1eac4a300494814e
Torsten Wörtwein <twoertwein@gmail.com>
2021-09-28 15:42:20 -0400

    dont reformat init

--- pandas/__init__.py
+++ pandas/__init__.py
@@ -22,11 +22,7 @@ del hard_dependencies, dependency, missing_dependencies
 from pandas.compat import is_numpy_dev as _is_numpy_dev
 
 try:
-    from pandas._libs import (
-        hashtable as _hashtable,
-        lib as _lib,
-        tslib as _tslib,
-    )
+    from pandas._libs import hashtable as _hashtable, lib as _lib, tslib as _tslib
 except ImportError as e:  # pragma: no cover
     module = e.name
     raise ImportError(
@@ -36,137 +32,147 @@ except ImportError as e:  # pragma: no cover
     ) from e
 
 from pandas._config import (
-    describe_option,
     get_option,
+    set_option,
+    reset_option,
+    describe_option,
     option_context,
     options,
-    reset_option,
-    set_option,
 )
 
-from pandas.util._print_versions import show_versions
-from pandas.util._tester import test
+# let init-time option registration happen
+import pandas.core.config_init
 
-from pandas import (
-    api,
-    arrays,
-    errors,
-    io,
-    plotting,
-    testing,
-    tseries,
-)
-
-# use the closest tagged version if possible
-from pandas._version import get_versions
-from pandas.core.api import (  # dtype; missing; indexes; tseries; conversion; misc
-    NA,
-    BooleanDtype,
-    Categorical,
-    CategoricalDtype,
-    CategoricalIndex,
-    DataFrame,
-    DateOffset,
-    DatetimeIndex,
-    DatetimeTZDtype,
-    Flags,
-    Float32Dtype,
-    Float64Dtype,
-    Grouper,
-    Index,
-    IndexSlice,
+from pandas.core.api import (
+    # dtype
     Int8Dtype,
     Int16Dtype,
     Int32Dtype,
     Int64Dtype,
-    Interval,
-    IntervalDtype,
-    IntervalIndex,
-    MultiIndex,
-    NamedAgg,
-    NaT,
-    NumericIndex,
-    Period,
-    PeriodDtype,
-    PeriodIndex,
-    RangeIndex,
-    Series,
-    StringDtype,
-    Timedelta,
-    TimedeltaIndex,
-    Timestamp,
     UInt8Dtype,
     UInt16Dtype,
     UInt32Dtype,
     UInt64Dtype,
-    array,
-    bdate_range,
-    date_range,
-    factorize,
-    interval_range,
+    Float32Dtype,
+    Float64Dtype,
+    CategoricalDtype,
+    PeriodDtype,
+    IntervalDtype,
+    DatetimeTZDtype,
+    StringDtype,
+    BooleanDtype,
+    # missing
+    NA,
     isna,
     isnull,
     notna,
     notnull,
+    # indexes
+    Index,
+    CategoricalIndex,
+    RangeIndex,
+    NumericIndex,
+    MultiIndex,
+    IntervalIndex,
+    TimedeltaIndex,
+    DatetimeIndex,
+    PeriodIndex,
+    IndexSlice,
+    # tseries
+    NaT,
+    Period,
     period_range,
-    set_eng_float_format,
+    Timedelta,
     timedelta_range,
-    to_datetime,
+    Timestamp,
+    date_range,
+    bdate_range,
+    Interval,
+    interval_range,
+    DateOffset,
+    # conversion
     to_numeric,
+    to_datetime,
     to_timedelta,
+    # misc
+    Flags,
+    Grouper,
+    factorize,
     unique,
     value_counts,
+    NamedAgg,
+    array,
+    Categorical,
+    set_eng_float_format,
+    Series,
+    DataFrame,
 )
+
 from pandas.core.arrays.sparse import SparseDtype
+
+from pandas.tseries.api import infer_freq
+from pandas.tseries import offsets
+
 from pandas.core.computation.api import eval
 
-# let init-time option registration happen
-import pandas.core.config_init
 from pandas.core.reshape.api import (
     concat,
-    crosstab,
-    cut,
-    get_dummies,
     lreshape,
     melt,
+    wide_to_long,
     merge,
     merge_asof,
     merge_ordered,
+    crosstab,
     pivot,
     pivot_table,
+    get_dummies,
+    cut,
     qcut,
-    wide_to_long,
 )
 
-from pandas.io.api import (  # excel; parsers; pickle; pytables; sql; misc
+from pandas import api, arrays, errors, io, plotting, testing, tseries
+from pandas.util._print_versions import show_versions
+
+from pandas.io.api import (
+    # excel
     ExcelFile,
     ExcelWriter,
-    HDFStore,
-    read_clipboard,
-    read_csv,
     read_excel,
-    read_feather,
+    # parsers
+    read_csv,
     read_fwf,
-    read_gbq,
-    read_hdf,
-    read_html,
-    read_json,
-    read_orc,
-    read_parquet,
+    read_table,
+    # pickle
     read_pickle,
-    read_sas,
-    read_spss,
+    to_pickle,
+    # pytables
+    HDFStore,
+    read_hdf,
+    # sql
     read_sql,
     read_sql_query,
     read_sql_table,
-    read_stata,
-    read_table,
+    # misc
+    read_clipboard,
+    read_parquet,
+    read_orc,
+    read_feather,
+    read_gbq,
+    read_html,
     read_xml,
-    to_pickle,
+    read_json,
+    read_stata,
+    read_sas,
+    read_spss,
 )
+
 from pandas.io.json import _json_normalize as json_normalize
-from pandas.tseries import offsets
-from pandas.tseries.api import infer_freq
+
+from pandas.util._tester import test
+
+# use the closest tagged version if possible
+from pandas._version import get_versions
 
 v = get_versions()
 __version__ = v.get("closest-tag", v["version"])
@@ -195,11 +201,7 @@ def __getattr__(name):
             FutureWarning,
             stacklevel=2,
         )
-        from pandas.core.api import (
-            Float64Index,
-            Int64Index,
-            UInt64Index,
-        )
+        from pandas.core.api import Float64Index, Int64Index, UInt64Index
 
         return {
             "Float64Index": Float64Index,
