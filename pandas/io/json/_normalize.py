04b04381c2c3eb36f1a2a718f8a45242a7b5ab57
debnathshoham <debnathshoham@gmail.com>
2021-10-02 20:08:22 +0530

    check if only separator at new_key start

--- pandas/io/json/_normalize.py
+++ pandas/io/json/_normalize.py
@@ -149,7 +149,9 @@ def _normalise_json(
                 data=value,
                 # to avoid adding the separator to the start of every key
                 # GH#43831 avoid adding key if key_string blank
-                key_string=new_key if key_string == "" else new_key[len(separator) :],
+                key_string=new_key
+                if new_key[: len(separator)] != separator
+                else new_key[len(separator) :],
                 normalized_dict=normalized_dict,
                 separator=separator,
             )
