be56abdba83f95638f06a31fef0831a6ff7912d9
saehuihwang <57047354+saehuihwang@users.noreply.github.com>
2021-10-01 17:03:48 -0700

    TST: test selection methods after assignment with at (#43843)

--- pandas/tests/indexing/test_at.py
+++ pandas/tests/indexing/test_at.py
@@ -26,6 +26,27 @@ def test_at_timezone():
     tm.assert_frame_equal(result, expected)
 
 
+def test_selection_methods_of_assigned_col():
+    # GH 29282
+    df = DataFrame(data={"a": [1, 2, 3], "b": [4, 5, 6]})
+    df2 = DataFrame(data={"c": [7, 8, 9]}, index=[2, 1, 0])
+    df["c"] = df2["c"]
+    df.at[1, "c"] = 11
+    result = df
+    expected = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [9, 11, 7]})
+    tm.assert_frame_equal(result, expected)
+    result = df.at[1, "c"]
+    assert result == 11
+
+    result = df["c"]
+    expected = Series([9, 11, 7], name="c")
+    tm.assert_series_equal(result, expected)
+
+    result = df[["c"]]
+    expected = DataFrame({"c": [9, 11, 7]})
+    tm.assert_frame_equal(result, expected)
+
+
 class TestAtSetItem:
     def test_at_setitem_mixed_index_assignment(self):
         # GH#19860
