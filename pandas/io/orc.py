fe849207cd177334f0d5533d163915b1ffe97eff
NickFillot <40593450+NickFillot@users.noreply.github.com>
2021-10-03 16:23:02 +0200

    [ENH] to_orc
    pandas.io.orc.to_orc method definition

--- pandas/io/orc.py
+++ pandas/io/orc.py
@@ -1,15 +1,19 @@
 """ orc compat """
 from __future__ import annotations
 
+import os
 from typing import TYPE_CHECKING
+from tempfile import gettempdir
 
 from pandas._typing import FilePathOrBuffer
 from pandas.compat._optional import import_optional_dependency
 
 from pandas.io.common import get_handle
 
-if TYPE_CHECKING:
-    from pandas import DataFrame
+from pandas.core import generic
+from pandas.util._decorators import doc
+
+from pandas import DataFrame
 
 
 def read_orc(
@@ -55,3 +59,78 @@ def read_orc(
     with get_handle(path, "rb", is_text=False) as handles:
         orc_file = orc.ORCFile(handles.handle)
         return orc_file.read(columns=columns, **kwargs).to_pandas()
+
+
+def to_orc(
+    df: DataFrame,
+    path: FilePathOrBuffer = None,
+    engine: str = 'pyarrow',
+    index: bool = None,
+    **kwargs
+) -> bytes:
+    """
+    Write a DataFrame to the orc/arrow format.
+    Parameters
+    ----------
+    df : DataFrame
+    path : str or file-like object, default None
+        If a string, it will be used as Root Directory path
+        when writing a partitioned dataset. By file-like object,
+        we refer to objects with a write() method, such as a file handle
+        (e.g. via builtin open function) or io.BytesIO. The engine
+        fastparquet does not accept file-like objects. If path is None,
+        a bytes object is returned.
+    engine : {{'pyarrow'}}, default 'pyarrow'
+        Parquet library to use, or library it self, checked with 'pyarrow' name
+        and version > 4.0.0
+    index : bool, default None
+        If ``True``, include the dataframe's index(es) in the file output. If
+        ``False``, they will not be written to the file.
+        If ``None``, similar to ``infer`` the dataframe's index(es)
+        will be saved. However, instead of being saved as values,
+        the RangeIndex will be stored as a range in the metadata so it
+        doesn't require much space and is faster. Other indexes will
+        be included as columns in the file output.
+    kwargs
+        Additional keyword arguments passed to the engine
+    Returns
+    -------
+    bytes if no path argument is provided else None
+    """
+    if index is None:
+        index = df.index.names[0] is not None
+    
+    if isinstance(engine, str):
+        engine = import_optional_dependency(engine, min_version='4.0.0')
+    else:
+        try:
+            assert engine.__name__ == 'pyarrow', "engine must be 'pyarrow' module"
+            assert hasattr(engine, 'orc'), "'pyarrow' module must have version > 4.0.0 with orc module"
+        except Exception as e:
+            raise ValueError("Wrong engine passed, %s" % (
+                e,
+            ))
+            
+    if path is None:
+        # to bytes: tmp path, pyarrow auto closes buffers
+        path = os.path.join(gettempdir(), os.urandom(12).hex())
+        try:
+            engine.orc.write_table(
+                engine.Table.from_pandas(df, preserve_index=index),
+                path, **kwargs
+            )
+            with open(path, 'rb') as path:
+                return path.read()
+        except BaseException as e:
+            raise e
+        finally:
+            try:
+                os.remove(path)
+            except Exception as e:
+                pass
+    else:
+        engine.orc.write_table(
+            engine.Table.from_pandas(df, preserve_index=index),
+            path, **kwargs
+        )
+    return
