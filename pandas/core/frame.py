6cc7030cb23511aeaf803a69fde89040e9fc6ae4
NickFillot <40593450+NickFillot@users.noreply.github.com>
2021-10-03 16:34:37 +0200

    pandas.DataFrame.to_orc
    set to_orc to pandas.DataFrame

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -2734,7 +2734,81 @@ class DataFrame(NDFrame, OpsMixin):
             storage_options=storage_options,
             **kwargs,
         )
+    
+    def to_orc(
+        self,
+        path: FilePathOrBuffer = None,
+        engine: str = 'pyarrow',
+        index: bool = None,
+        **kwargs
+    ) -> bytes:
+        """
+        Write a DataFrame to the orc/arrow format.
+        Parameters
+        ----------
+        df : DataFrame
+        path : str or file-like object, default None
+            If a string, it will be used as Root Directory path
+            when writing a partitioned dataset. By file-like object,
+            we refer to objects with a write() method, such as a file handle
+            (e.g. via builtin open function) or io.BytesIO. The engine
+            fastparquet does not accept file-like objects. If path is None,
+            a bytes object is returned.
+        engine : {{'pyarrow'}}, default 'pyarrow'
+            Parquet library to use, or library it self, checked with 'pyarrow' name
+            and version > 4.0.0
+        index : bool, default None
+            If ``True``, include the dataframe's index(es) in the file output. If
+            ``False``, they will not be written to the file.
+            If ``None``, similar to ``infer`` the dataframe's index(es)
+            will be saved. However, instead of being saved as values,
+            the RangeIndex will be stored as a range in the metadata so it
+            doesn't require much space and is faster. Other indexes will
+            be included as columns in the file output.
+        kwargs
+            Additional keyword arguments passed to the engine
+        Returns
+        -------
+        bytes if no path argument is provided else None
 
+        See Also
+        --------
+        read_orc : Read a ORC file.
+        DataFrame.to_parquet : Write a parquet file.
+        DataFrame.to_csv : Write a csv file.
+        DataFrame.to_sql : Write to a sql table.
+        DataFrame.to_hdf : Write to hdf.
+
+        Notes
+        -----
+        This function requires `pyarrow <https://arrow.apache.org/docs/python/>`_ library.
+
+        Examples
+        --------
+        >>> df = pd.DataFrame(data={{'col1': [1, 2], 'col2': [3, 4]}})
+        >>> df.to_orc('df.orc', compression='gzip')  # doctest: +SKIP
+        >>> pd.read_orc('df.orc')  # doctest: +SKIP
+           col1  col2
+        0     1     3
+        1     2     4
+
+        If you want to get a buffer to the orc content you can write it to io.BytesIO
+        >>> import io
+        >>> b = io.BytesIO(df.to_orc())
+        >>> b.seek(0)
+        0
+        >>> content = b.read()
+        """
+        from pandas.io.orc import to_orc
+
+        return to_orc(
+            self,
+            path,
+            engine,
+            index=index,
+            **kwargs
+        )
+    
     @Substitution(
         header_type="bool",
         header="Whether to print column labels, default True",
