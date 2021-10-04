ce9816bf43603a25c31eadbc2c69c027c0c4ceef
Thomas Kluyver <thomas@kluyver.me.uk>
2021-10-04 21:17:45 +0100

    Fix variable name in test

--- pandas/tests/test_optional_dependency.py
+++ pandas/tests/test_optional_dependency.py
@@ -16,7 +16,7 @@ def test_import_optional():
     with pytest.raises(ImportError, match=match) as exc_info:
         import_optional_dependency("notapackage")
     # The original exception should be there as context:
-    assert isinstance(excinfo.value.__context__, ImportError)
+    assert isinstance(exc_info.value.__context__, ImportError)
 
     result = import_optional_dependency("notapackage", errors="ignore")
     assert result is None
