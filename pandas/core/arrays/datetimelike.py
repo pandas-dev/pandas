7790aac139dc40e894e75c5e4a71ed1e55ff3dd9
Brock <jbrockmendel@gmail.com>
2021-10-04 21:07:05 -0700

    BUG: pd.array preserve PandasArray

--- pandas/core/arrays/datetimelike.py
+++ pandas/core/arrays/datetimelike.py
@@ -718,8 +718,9 @@ class DatetimeLikeArrayMixin(OpsMixin, NDArrayBackedExtensionArray):
                     msg = self._validation_error_message(value, True)
                     raise TypeError(msg)
 
-        # Do type inference if necessary up front
+        # Do type inference if necessary up front (after unpacking PandasArray)
         # e.g. we passed PeriodIndex.values and got an ndarray of Periods
+        value = extract_array(value, extract_numpy=True)
         value = pd_array(value)
         value = extract_array(value, extract_numpy=True)
 
