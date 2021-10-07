0d7ba7bc8e90bc53625ba748a30000ae8a75815d
jbrockmendel <jbrockmendel@gmail.com>
2021-10-07 15:02:09 -0700

    TST/REF: collect/de-dup index tests (#43914)

--- pandas/tests/indexes/period/test_partial_slicing.py
+++ pandas/tests/indexes/period/test_partial_slicing.py
@@ -3,6 +3,7 @@ import pytest
 
 from pandas import (
     DataFrame,
+    PeriodIndex,
     Series,
     date_range,
     period_range,
@@ -11,6 +12,31 @@ import pandas._testing as tm
 
 
 class TestPeriodIndex:
+    def test_getitem_periodindex_duplicates_string_slice(self):
+        # monotonic
+        idx = PeriodIndex([2000, 2007, 2007, 2009, 2009], freq="A-JUN")
+        ts = Series(np.random.randn(len(idx)), index=idx)
+
+        result = ts["2007"]
+        expected = ts[1:3]
+        tm.assert_series_equal(result, expected)
+        result[:] = 1
+        assert (ts[1:3] == 1).all()
+
+        # not monotonic
+        idx = PeriodIndex([2000, 2007, 2007, 2009, 2007], freq="A-JUN")
+        ts = Series(np.random.randn(len(idx)), index=idx)
+
+        result = ts["2007"]
+        expected = ts[idx == "2007"]
+        tm.assert_series_equal(result, expected)
+
+    def test_getitem_periodindex_quarter_string(self):
+        pi = PeriodIndex(["2Q05", "3Q05", "4Q05", "1Q06", "2Q06"], freq="Q")
+        ser = Series(np.random.rand(len(pi)), index=pi).cumsum()
+        # Todo: fix these accessors!
+        assert ser["05Q4"] == ser[2]
+
     def test_pindex_slice_index(self):
         pi = period_range(start="1/1/10", end="12/31/12", freq="M")
         s = Series(np.random.rand(len(pi)), index=pi)
--- pandas/tests/series/methods/test_reindex.py
+++ pandas/tests/series/methods/test_reindex.py
@@ -359,3 +359,11 @@ def test_reindex_empty_with_level(values):
         index=MultiIndex(levels=[["b"], values[1]], codes=[[], []]), dtype="object"
     )
     tm.assert_series_equal(result, expected)
+
+
+def test_reindex_missing_category():
+    # GH#18185
+    ser = Series([1, 2, 3, 1], dtype="category")
+    msg = r"Cannot setitem on a Categorical with a new category \(-1\)"
+    with pytest.raises(TypeError, match=msg):
+        ser.reindex([1, 2, 3, 4, 5], fill_value=-1)
