e7a97e7a9c09cf6a074451db5889099e63a396bc
tp <contribute@tensortable.com>
2021-10-11 23:31:45 +0100

    REF: extract params used in DataFrame.__repr__

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -994,26 +994,24 @@ class DataFrame(NDFrame, OpsMixin):
             self.info(buf=buf)
             return buf.getvalue()
 
-        max_rows = get_option("display.max_rows")
-        min_rows = get_option("display.min_rows")
-        max_cols = get_option("display.max_columns")
-        max_colwidth = get_option("display.max_colwidth")
-        show_dimensions = get_option("display.show_dimensions")
+        repr_params = self._get_repr_params()
+        self.to_string(buf=buf, **repr_params)
+
+        return buf.getvalue()
+
+    def _get_repr_params(self) -> dict[str, Any]:
         if get_option("display.expand_frame_repr"):
             width, _ = console.get_console_size()
         else:
             width = None
-        self.to_string(
-            buf=buf,
-            max_rows=max_rows,
-            min_rows=min_rows,
-            max_cols=max_cols,
-            line_width=width,
-            max_colwidth=max_colwidth,
-            show_dimensions=show_dimensions,
-        )
-
-        return buf.getvalue()
+        return {
+            "max_rows": get_option("display.max_rows"),
+            "min_rows": get_option("display.min_rows"),
+            "max_cols": get_option("display.max_columns"),
+            "max_colwidth": get_option("display.max_colwidth"),
+            "show_dimensions": get_option("display.show_dimensions"),
+            "width": width,
+        }
 
     def _repr_html_(self) -> str | None:
         """
