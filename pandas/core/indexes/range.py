58a4f33510df3efd5d488334a324d19144f27b2c
Brock <jbrockmendel@gmail.com>
2021-10-13 15:11:36 -0700

    BUG: RangeIndex.union

--- pandas/core/indexes/range.py
+++ pandas/core/indexes/range.py
@@ -635,10 +635,13 @@ class RangeIndex(NumericIndex):
                     return type(self)(start_r, end_r + step_s, step_s)
                 if (
                     (step_s % 2 == 0)
-                    and (abs(start_s - start_o) <= step_s / 2)
-                    and (abs(end_s - end_o) <= step_s / 2)
+                    and (abs(start_s - start_o) == step_s / 2)
+                    and (abs(end_s - end_o) == step_s / 2)
                 ):
+                    # e.g. range(0, 10, 2) and range(1, 11, 2)
+                    #  but not range(0, 20, 4) and range(1, 21, 4) GH#??
                     return type(self)(start_r, end_r + step_s / 2, step_s / 2)
+
             elif step_o % step_s == 0:
                 if (
                     (start_o - start_s) % step_s == 0
