4d2b24aa84246492753da955e13d025bcdac62a4
Terji Petersen <contribute@tensortable.com>
2021-10-15 00:33:23 +0100

    REF: extract params used in DataFrame.__repr__ (#43987)

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -483,6 +483,37 @@ def get_adjustment() -> TextAdjustment:
         return TextAdjustment()
 
 
+def get_dataframe_repr_params() -> dict[str, Any]:
+    """Get the parameters used to repr(dataFrame) calls using DataFrame.to_string.
+
+    Supplying these parameters to DataFrame.to_string is equivalent to calling
+    ``repr(DataFrame)``. This is useful if you want to adjust the repr output.
+
+    Example
+    -------
+    >>> import pandas as pd
+    >>>
+    >>> df = pd.DataFrame([[1, 2], [3, 4]])
+    >>> repr_params = pd.io.formats.format.get_dataframe_repr_params()
+    >>> repr(df) == df.to_string(**repr_params)
+    True
+    """
+    from pandas.io.formats import console
+
+    if get_option("display.expand_frame_repr"):
+        line_width, _ = console.get_console_size()
+    else:
+        line_width = None
+    return {
+        "max_rows": get_option("display.max_rows"),
+        "min_rows": get_option("display.min_rows"),
+        "max_cols": get_option("display.max_columns"),
+        "max_colwidth": get_option("display.max_colwidth"),
+        "show_dimensions": get_option("display.show_dimensions"),
+        "line_width": line_width,
+    }
+
+
 class DataFrameFormatter:
     """Class for processing dataframe formatting options and data."""
 
