1490968cbdbde885bb4c82955566e45e778a2bf8
Alex Lim <alexanderheionlim@gmail.com>
2021-10-14 16:13:22 -0400

    TST: ensure numpy doesn't downcast nats

--- pandas/tests/indexes/object/test_indexing.py
+++ pandas/tests/indexes/object/test_indexing.py
@@ -108,6 +108,7 @@ class TestGetIndexerNonUnique:
             index = Index(
                 np.array(
                     ["2021-10-02", np_nat_fixture.copy(), np_nat_fixture2.copy()],
+                    dtype=object,
                 ),
                 dtype=object,
             )
@@ -130,7 +131,8 @@ class TestGetIndexerNonUnique:
                         np_nat_fixture2,
                     ],
                     dtype=object,
-                )
+                ),
+                dtype=object,
             )
             # pass as index to prevent target from being casted to DatetimeIndex
             indexer, missing = index.get_indexer_non_unique(
