11bc77d684748fe7bcc80a4b7960a6dbc389b6a0
DSM <dsm054@gmail.com>
2021-10-16 16:26:51 -0400

    Backport PR #43291: BUG: Fixes to FixedForwardWindowIndexer and GroupbyIndexer (#43267)

--- pandas/core/window/expanding.py
+++ pandas/core/window/expanding.py
@@ -680,7 +680,7 @@ class ExpandingGroupby(BaseWindowGroupby, Expanding):
         GroupbyIndexer
         """
         window_indexer = GroupbyIndexer(
-            groupby_indicies=self._grouper.indices,
+            groupby_indices=self._grouper.indices,
             window_indexer=ExpandingIndexer,
         )
         return window_indexer
