61edd30010cea3cc0e6fc27919cbde207a4e9c8a
pckSF <pckSF@protonmail.com>
2021-10-16 22:48:24 +0200

    Add None category to mixed_cats_basic test

--- pandas/tests/reshape/test_from_dummies.py
+++ pandas/tests/reshape/test_from_dummies.py
@@ -59,9 +59,15 @@ def test_from_dummies_no_prefix_float_cats_basic():
 
 def test_from_dummies_no_prefix_mixed_cats_basic():
     dummies = DataFrame(
-        {1.23: [1, 0, 0, 0], "c": [0, 1, 0, 0], 2: [0, 0, 1, 0], False: [0, 0, 0, 1]}
+        {
+            1.23: [1, 0, 0, 0, 0],
+            "c": [0, 1, 0, 0, 0],
+            2: [0, 0, 1, 0, 0],
+            False: [0, 0, 0, 1, 0],
+            None: [0, 0, 0, 0, 1],
+        }
     )
-    expected = DataFrame({"categories": [1.23, "c", 2, False]}, dtype="object")
+    expected = DataFrame({"categories": [1.23, "c", 2, False, None]}, dtype="object")
     result = from_dummies(dummies)
     tm.assert_frame_equal(result, expected)
 
