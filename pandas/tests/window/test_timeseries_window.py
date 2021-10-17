d6dd13e072c8e6ed42c42e19268e2d2a544964e1
jamesholcombe <jholcombe@hotmail.co.uk>
2021-10-17 17:06:58 +0100

    amended tests

--- pandas/tests/window/test_timeseries_window.py
+++ pandas/tests/window/test_timeseries_window.py
@@ -648,6 +648,9 @@ class TestRollingTS:
         # GH 15130
         # we don't need to validate monotonicity when grouping
 
+        #GH 43909 we should raise an error here to match behaviour of non-groupby rolling.
+
+
         data = [
             ["David", "1/1/2015", 100],
             ["David", "1/5/2015", 500],
@@ -664,17 +667,17 @@ class TestRollingTS:
         df = DataFrame(data=data, columns=["name", "date", "amount"])
         df["date"] = to_datetime(df["date"])
 
-        expected = (
-            df.set_index("date")
-            .groupby("name")
-            .apply(lambda x: x.rolling("180D")["amount"].sum())
-        )
-        result = df.groupby("name").rolling("180D", on="date")["amount"].sum()
-        tm.assert_series_equal(result, expected)
+        
+        with pytest.raises(ValueError,match=r".* must be monotonic"):
+            df.groupby("name").rolling("180D", on="date")
+            
 
-    def test_non_monotonic(self):
+    def test_non_monotonic_raises(self):
         # GH 13966 (similar to #15130, closed by #15175)
 
+        #superceded by 43909
+
+        
         dates = date_range(start="2016-01-01 09:30:00", periods=20, freq="s")
         df = DataFrame(
             {
@@ -684,15 +687,10 @@ class TestRollingTS:
             }
         )
 
-        result = df.groupby("A").rolling("4s", on="B").C.mean()
-        expected = (
-            df.set_index("B").groupby("A").apply(lambda x: x.rolling("4s")["C"].mean())
-        )
-        tm.assert_series_equal(result, expected)
+        with pytest.raises(ValueError,match=r".* must be monotonic"):
+            df.groupby("A").rolling("4s", on="B").C.mean()
+        
 
-        df2 = df.sort_values("B")
-        result = df2.groupby("A").rolling("4s", on="B").C.mean()
-        tm.assert_series_equal(result, expected)
 
     def test_rolling_cov_offset(self):
         # GH16058
