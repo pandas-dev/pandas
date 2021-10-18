456c4ba4ba6b74d6b142c4eb8e1876e400a3174f
Torsten Wörtwein <twoertwein@gmail.com>
2021-10-18 17:51:25 -0400

    use type instead of typing.Type

--- pandas/_libs/properties.pyi
+++ pandas/_libs/properties.pyi
@@ -1,11 +1,10 @@
 from typing import (
-    Any,
-    Type,
+    Any
 )
 
 cache_readonly = property
 
 class AxisProperty:
     def __init__(self, axis: int = ..., doc: str = ...) -> None: ...
-    def __get__(self, obj: Any, typ: Type) -> Any: ...  # TODO
+    def __get__(self, obj: Any, typ: type) -> Any: ...  # TODO
     def __set__(self, obj: Any, value: Any) -> None: ...  # TODO
