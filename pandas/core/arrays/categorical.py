ff29fe01c8084c3e3f416b1d9499eb553653b6d9
jbrockmendel <jbrockmendel@gmail.com>
2021-10-24 07:47:02 -0700

    REF: simplify EA.astype (#44133)

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -110,7 +110,6 @@ from pandas.core.base import (
 )
 import pandas.core.common as com
 from pandas.core.construction import (
-    array as pd_array,
     extract_array,
     sanitize_array,
 )
@@ -527,7 +526,7 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
 
         # TODO: consolidate with ndarray case?
         elif isinstance(dtype, ExtensionDtype):
-            result = pd_array(self, dtype=dtype, copy=copy)
+            return super().astype(dtype, copy=copy)
 
         elif is_integer_dtype(dtype) and self.isna().any():
             raise ValueError("Cannot convert float NaN to integer")
