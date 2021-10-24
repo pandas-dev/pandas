3ac69f39e8200e12f74a4bea8cac277ef48cc805
jbrockmendel <jbrockmendel@gmail.com>
2021-10-24 07:41:57 -0700

    CLN/TST: address/annotate TODOs (#44154)

--- pandas/tests/arrays/sparse/test_array.py
+++ pandas/tests/arrays/sparse/test_array.py
@@ -1203,7 +1203,7 @@ class TestAccessor:
         row = [0, 3, 1, 0]
         col = [0, 3, 1, 2]
         data = [4, 5, 7, 9]
-        # TODO: Remove dtype when scipy is fixed
+        # TODO(scipy#13585): Remove dtype when scipy is fixed
         # https://github.com/scipy/scipy/issues/13585
         sp_array = scipy.sparse.coo_matrix((data, (row, col)), dtype="int")
         result = pd.Series.sparse.from_coo(sp_array)
