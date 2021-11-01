015541eabcd1eba2d79823cee872cfb90b516495
Terji Petersen <contribute@tensortable.com>
2021-11-01 17:16:10 +0000

    CLN: DataFrame.__repr__ (#44271)

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -988,15 +988,13 @@ class DataFrame(NDFrame, OpsMixin):
         """
         Return a string representation for a particular DataFrame.
         """
-        buf = StringIO("")
         if self._info_repr():
+            buf = StringIO("")
             self.info(buf=buf)
             return buf.getvalue()
 
         repr_params = fmt.get_dataframe_repr_params()
-        self.to_string(buf=buf, **repr_params)
-
-        return buf.getvalue()
+        return self.to_string(**repr_params)
 
     def _repr_html_(self) -> str | None:
         """
