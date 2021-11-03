ac74fde187818d763b0d5fd19210eb674d568833
Emiliano Jordan <emiliano.jordan+github@protonmail.com>
2021-11-02 22:26:46 -0600

    formatted

--- pandas/tests/util/test_assert_frame_equal.py
+++ pandas/tests/util/test_assert_frame_equal.py
@@ -341,7 +341,7 @@ def test_assert_frame_equal_attrs():
 
     right = DataFrame({"a": [1, 2], "b": [3, 4]})
 
-    msg = f"{expected_attrs} != {dict()}"
+    msg = f"{expected_attrs} != {{}}"
     with pytest.raises(AssertionError, match=msg):
         tm.assert_frame_equal(left, right)
 
