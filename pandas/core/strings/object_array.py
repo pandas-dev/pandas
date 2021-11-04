f79922d25bac9f8da9fbb6b72e1d33ef803182fc
saehuihwang <57047354+saehuihwang@users.noreply.github.com>
2021-11-03 17:40:27 -0700

    ENH: added regex argument to Series.str.split (#44185)

--- pandas/core/strings/object_array.py
+++ pandas/core/strings/object_array.py
@@ -308,21 +308,38 @@ class ObjectStringArrayMixin(BaseStringArrayMethods):
 
         return self._str_map(f)
 
-    def _str_split(self, pat=None, n=-1, expand=False):
+    def _str_split(
+        self,
+        pat: str | re.Pattern | None = None,
+        n=-1,
+        expand=False,
+        regex: bool | None = None,
+    ):
         if pat is None:
             if n is None or n == 0:
                 n = -1
             f = lambda x: x.split(pat, n)
         else:
-            if len(pat) == 1:
+            new_pat: str | re.Pattern
+            if regex is True or isinstance(pat, re.Pattern):
+                new_pat = re.compile(pat)
+            elif regex is False:
+                new_pat = pat
+            # regex is None so link to old behavior #43563
+            else:
+                if len(pat) == 1:
+                    new_pat = pat
+                else:
+                    new_pat = re.compile(pat)
+
+            if isinstance(new_pat, re.Pattern):
+                if n is None or n == -1:
+                    n = 0
+                f = lambda x: new_pat.split(x, maxsplit=n)
+            else:
                 if n is None or n == 0:
                     n = -1
                 f = lambda x: x.split(pat, n)
-            else:
-                if n is None or n == -1:
-                    n = 0
-                regex = re.compile(pat)
-                f = lambda x: regex.split(x, maxsplit=n)
         return self._str_map(f, dtype=object)
 
     def _str_rsplit(self, pat=None, n=-1):
