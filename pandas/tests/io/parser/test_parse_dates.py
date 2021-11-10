6961eecfb50118e116e50add06cc98c8f8141790
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-11-10 11:34:10 +0100

    fix out of bounds

--- pandas/tests/io/parser/test_parse_dates.py
+++ pandas/tests/io/parser/test_parse_dates.py
@@ -435,7 +435,7 @@ KORD,19990127 22:00:00, 21:56:00, -0.5900, 1.7100, 5.1000, 0.0000, 290.0000
     if parser.engine == "pyarrow" and not pa_version_under6p0:
         # https://github.com/pandas-dev/pandas/issues/44231
         # pyarrow 6.0 starts to infer time type
-        expected["X2"] = pd.to_datetime(expected["X2"]).time
+        expected["X2"] = pd.to_datetime("1970-01-01" + expected["X2"]).time
 
     tm.assert_frame_equal(result, expected)
 
