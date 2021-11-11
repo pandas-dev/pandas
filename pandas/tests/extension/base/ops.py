d101853e1f4c186b1f1069dd3517e6d53b2db33f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-11-11 21:10:23 +0100

    TST: use custom parametrization for consistency in base extension array tests (#44332)

--- pandas/tests/extension/base/ops.py
+++ pandas/tests/extension/base/ops.py
@@ -162,13 +162,12 @@ class BaseComparisonOpsTests(BaseOpsUtil):
         other = pd.Series([data[0]] * len(data))
         self._compare_other(ser, data, comparison_op, other)
 
-    def test_direct_arith_with_ndframe_returns_not_implemented(
-        self, data, frame_or_series
-    ):
+    @pytest.mark.parametrize("box", [pd.Series, pd.DataFrame])
+    def test_direct_arith_with_ndframe_returns_not_implemented(self, data, box):
         # EAs should return NotImplemented for ops with Series/DataFrame
         # Pandas takes care of unboxing the series and calling the EA's op.
         other = pd.Series(data)
-        if frame_or_series is pd.DataFrame:
+        if box is pd.DataFrame:
             other = other.to_frame()
 
         if hasattr(data, "__eq__"):
