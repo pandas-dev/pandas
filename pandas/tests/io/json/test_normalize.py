f9300d2f0a50bdcd9ff5d7828f3b3ea516a2b7db
Evgeny Naumov <qdbp@users.noreply.github.com>
2021-11-13 21:34:44 -0500

    BUG: closes #44312: fixes unwanted TypeError when a missing metadata field is missing (#44325)

--- pandas/tests/io/json/test_normalize.py
+++ pandas/tests/io/json/test_normalize.py
@@ -634,6 +634,33 @@ class TestNestedToRecord:
         expected = DataFrame(ex_data, columns=columns)
         tm.assert_frame_equal(result, expected)
 
+    def test_missing_nested_meta(self):
+        # GH44312
+        # If errors="ignore" and nested metadata is null, we should return nan
+        data = {"meta": "foo", "nested_meta": None, "value": [{"rec": 1}, {"rec": 2}]}
+        result = json_normalize(
+            data,
+            record_path="value",
+            meta=["meta", ["nested_meta", "leaf"]],
+            errors="ignore",
+        )
+        ex_data = [[1, "foo", np.nan], [2, "foo", np.nan]]
+        columns = ["rec", "meta", "nested_meta.leaf"]
+        expected = DataFrame(ex_data, columns=columns).astype(
+            {"nested_meta.leaf": object}
+        )
+        tm.assert_frame_equal(result, expected)
+
+        # If errors="raise" and nested metadata is null, we should raise with the
+        # key of the first missing level
+        with pytest.raises(KeyError, match="'leaf' not found"):
+            json_normalize(
+                data,
+                record_path="value",
+                meta=["meta", ["nested_meta", "leaf"]],
+                errors="raise",
+            )
+
     def test_missing_meta_multilevel_record_path_errors_raise(self, missing_metadata):
         # GH41876
         # Ensure errors='raise' works as intended even when a record_path of length
