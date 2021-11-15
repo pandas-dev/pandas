07d8e94c2edaa36c233af605bb56a4452c819b1d
Brock <jbrockmendel@gmail.com>
2021-11-15 10:35:54 -0800

    BUG: Tick + np.timedelta64

--- pandas/tests/tseries/offsets/test_business_day.py
+++ pandas/tests/tseries/offsets/test_business_day.py
@@ -7,6 +7,7 @@ from datetime import (
     timedelta,
 )
 
+import numpy as np
 import pytest
 
 from pandas._libs.tslibs.offsets import (
@@ -17,6 +18,7 @@ from pandas._libs.tslibs.offsets import (
 
 from pandas import (
     DatetimeIndex,
+    Timedelta,
     _testing as tm,
 )
 from pandas.tests.tseries.offsets.common import (
@@ -57,11 +59,30 @@ class TestBusinessDay(Base):
 
         assert (self.d + offset) == datetime(2008, 1, 2, 2)
 
-    def test_with_offset_index(self):
-        dti = DatetimeIndex([self.d])
-        result = dti + (self.offset + timedelta(hours=2))
+    @pytest.mark.parametrize("reverse", [True, False])
+    @pytest.mark.parametrize(
+        "td",
+        [
+            Timedelta(hours=2),
+            Timedelta(hours=2).to_pytimedelta(),
+            Timedelta(hours=2).to_timedelta64(),
+        ],
+        ids=lambda x: type(x),
+    )
+    def test_with_offset_index(self, reverse, td, request):
+        if reverse and isinstance(td, np.timedelta64):
+            mark = pytest.mark.xfail(
+                reason="need __array_priority__, but that causes other errors"
+            )
+            request.node.add_marker(mark)
 
+        dti = DatetimeIndex([self.d])
         expected = DatetimeIndex([datetime(2008, 1, 2, 2)])
+
+        if reverse:
+            result = dti + (td + self.offset)
+        else:
+            result = dti + (self.offset + td)
         tm.assert_index_equal(result, expected)
 
     def test_eq(self):
--- pandas/tests/tseries/offsets/test_custom_business_day.py
+++ pandas/tests/tseries/offsets/test_custom_business_day.py
@@ -19,6 +19,7 @@ from pandas.compat import np_datetime64_compat
 
 from pandas import (
     DatetimeIndex,
+    Timedelta,
     _testing as tm,
     read_pickle,
 )
@@ -62,11 +63,30 @@ class TestCustomBusinessDay(Base):
 
         assert (self.d + offset) == datetime(2008, 1, 2, 2)
 
-    def test_with_offset_index(self):
-        dti = DatetimeIndex([self.d])
-        result = dti + (self.offset + timedelta(hours=2))
+    @pytest.mark.parametrize("reverse", [True, False])
+    @pytest.mark.parametrize(
+        "td",
+        [
+            Timedelta(hours=2),
+            Timedelta(hours=2).to_pytimedelta(),
+            Timedelta(hours=2).to_timedelta64(),
+        ],
+        ids=lambda x: type(x),
+    )
+    def test_with_offset_index(self, reverse, td, request):
+        if reverse and isinstance(td, np.timedelta64):
+            mark = pytest.mark.xfail(
+                reason="need __array_priority__, but that causes other errors"
+            )
+            request.node.add_marker(mark)
 
+        dti = DatetimeIndex([self.d])
         expected = DatetimeIndex([datetime(2008, 1, 2, 2)])
+
+        if reverse:
+            result = dti + (td + self.offset)
+        else:
+            result = dti + (self.offset + td)
         tm.assert_index_equal(result, expected)
 
     def test_eq(self):
