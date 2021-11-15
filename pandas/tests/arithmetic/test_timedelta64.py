c7808ae7606f6afb220831bab593028fc31fbbfc
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-11-15 16:06:51 +0100

    BUG: fix timedelta floordiv with scalar float (#44466)

--- pandas/tests/arithmetic/test_timedelta64.py
+++ pandas/tests/arithmetic/test_timedelta64.py
@@ -1981,6 +1981,20 @@ class TestTimedeltaArraylikeMulDivOps:
         with pytest.raises(TypeError, match="Cannot divide"):
             two / tdser
 
+    @pytest.mark.parametrize("two", [2, 2.0, np.array(2), np.array(2.0)])
+    def test_td64arr_floordiv_numeric_scalar(self, box_with_array, two):
+        tdser = Series(["59 Days", "59 Days", "NaT"], dtype="m8[ns]")
+        expected = Series(["29.5D", "29.5D", "NaT"], dtype="timedelta64[ns]")
+
+        tdser = tm.box_expected(tdser, box_with_array)
+        expected = tm.box_expected(expected, box_with_array)
+
+        result = tdser // two
+        tm.assert_equal(result, expected)
+
+        with pytest.raises(TypeError, match="Cannot divide"):
+            two // tdser
+
     @pytest.mark.parametrize(
         "vector",
         [np.array([20, 30, 40]), pd.Index([20, 30, 40]), Series([20, 30, 40])],
