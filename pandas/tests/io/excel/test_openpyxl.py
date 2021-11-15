f8a8fae77c8601a01891bd1db2c9eeed7550e014
Joeperdefloep <joepgevaert@gmail.com>
2021-11-15 16:23:24 +0100

    BUG: GH43445 finalized tests

--- pandas/tests/io/excel/test_openpyxl.py
+++ pandas/tests/io/excel/test_openpyxl.py
@@ -109,31 +109,6 @@ def test_engine_kwargs_write(ext, iso_dates):
             DataFrame().to_excel(writer)
 
 
-def test_engine_kwargs_append(ext):
-    # GH 43445
-    with tm.ensure_clean(ext) as f:
-        DataFrame(["hello", "world"]).to_excel(f)
-        with ExcelWriter(
-            f, engine="openpyxl", mode="a", engine_kwargs={"keep_vba": True}
-        ) as writer:
-            DataFrame(["goodbye", "world"]).to_excel(writer, sheet_name="Sheet2")
-
-
-@pytest.mark.parametrize("data_only, expected", [(True, 0), (False, "=1+1")])
-def test_engine_kwargs_append_data_only(ext, data_only, expected):
-    # GH 43445
-    # tests whether the keep_links engine_kwarg actually works well for
-    # openpyxl's load_workbook
-    with tm.ensure_clean(ext) as f:
-        DataFrame(["=1+1"]).to_excel(f)
-        with ExcelWriter(
-            f, engine="openpyxl", mode="a", engine_kwargs={"data_only": data_only}
-        ) as writer:
-            assert writer.sheets["Sheet1"]["B2"].value == expected
-            # ExcelWriter needs us to writer something to close properly?
-            DataFrame().to_excel(writer, sheet_name="Sheet2")
-
-
 def test_engine_kwargs_append_invalid(ext):
     # GH 43445
     # test whether an invalid engine kwargs actually raises
@@ -148,8 +123,23 @@ def test_engine_kwargs_append_invalid(ext):
             with ExcelWriter(
                 f, engine="openpyxl", mode="a", engine_kwargs={"apple_banana": "fruit"}
             ) as writer:
-                # not sure if we have to do something here?
+                # ExcelWriter needs us to write something to close properly
                 DataFrame(["good"]).to_excel(writer, sheet_name="Sheet2")
+
+
+@pytest.mark.parametrize("data_only, expected", [(True, 0), (False, "=1+1")])
+def test_engine_kwargs_append_data_only(ext, data_only, expected):
+    # GH 43445
+    # tests whether the data_only engine_kwarg actually works well for
+    # openpyxl's load_workbook
+    with tm.ensure_clean(ext) as f:
+        DataFrame(["=1+1"]).to_excel(f)
+        with ExcelWriter(
+            f, engine="openpyxl", mode="a", engine_kwargs={"data_only": data_only}
+        ) as writer:
+            assert writer.sheets["Sheet1"]["B2"].value == expected
+            # ExcelWriter needs us to writer something to close properly?
+            DataFrame().to_excel(writer, sheet_name="Sheet2")
 
 
 @pytest.mark.parametrize(
