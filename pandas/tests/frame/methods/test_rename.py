6c44c005ec3d5413ed119f60eeda43c1c8a56aa9
DriesS <dries.schaumont@gmail.com>
2021-11-15 14:55:25 +0100

    TST: Add test for #4980 (#44462)

--- pandas/tests/frame/methods/test_rename.py
+++ pandas/tests/frame/methods/test_rename.py
@@ -406,3 +406,14 @@ class TestRename:
             ],
         ).set_index(["STK_ID", "RPT_Date"], drop=False)
         tm.assert_frame_equal(result, expected)
+
+    def test_rename_boolean_index(self):
+        df = DataFrame(np.arange(15).reshape(3, 5), columns=[False, True, 2, 3, 4])
+        mapper = {0: "foo", 1: "bar", 2: "bah"}
+        res = df.rename(index=mapper)
+        exp = DataFrame(
+            np.arange(15).reshape(3, 5),
+            columns=[False, True, 2, 3, 4],
+            index=["foo", "bar", "bah"],
+        )
+        tm.assert_frame_equal(res, exp)
