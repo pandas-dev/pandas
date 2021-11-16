d5fe414c428e09ff227cbd709b79448677d69bd6
Brock <jbrockmendel@gmail.com>
2021-11-16 10:17:42 -0800

    tigher xfail for pytz 2017.3

--- pandas/tests/tseries/offsets/test_dst.py
+++ pandas/tests/tseries/offsets/test_dst.py
@@ -180,12 +180,13 @@ class TestDST:
 @pytest.mark.parametrize(
     "original_dt, target_dt, offset, tz",
     [
-        (
+        pytest.param(
             Timestamp("1900-01-01"),
             Timestamp("1905-07-01"),
             MonthBegin(66),
             "Africa/Kinshasa",
-        ),  # GH41906
+            marks=pytest.mark.xfail(pytz.__version__ == "2017.3", reason="GH#41906"),
+        ),
         (
             Timestamp("2021-10-01 01:15"),
             Timestamp("2021-10-31 01:15"),
