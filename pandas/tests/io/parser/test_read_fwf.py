ea26f433c02b31642915e063584a88fad001e68f
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2021-11-16 01:55:58 +0100

    BUG: read_csv and read_fwf not skipping all defined rows when nrows is given (#44434)

--- pandas/tests/io/parser/test_read_fwf.py
+++ pandas/tests/io/parser/test_read_fwf.py
@@ -862,3 +862,18 @@ def test_colspecs_with_comment():
     )
     expected = DataFrame([[1, "K"]], columns=[0, 1])
     tm.assert_frame_equal(result, expected)
+
+
+def test_skip_rows_and_n_rows():
+    # GH#44021
+    data = """a\tb
+1\t a
+2\t b
+3\t c
+4\t d
+5\t e
+6\t f
+    """
+    result = read_fwf(StringIO(data), nrows=4, skiprows=[2, 4])
+    expected = DataFrame({"a": [1, 3, 5, 6], "b": ["a", "c", "e", "f"]})
+    tm.assert_frame_equal(result, expected)
