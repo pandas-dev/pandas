8e236a0753194d3293d82f87c7e98512013a5b2b
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2021-11-17 19:54:50 +0100

    basic tests

--- pandas/tests/io/formats/style/test_to_string.py
+++ pandas/tests/io/formats/style/test_to_string.py
@@ -1,12 +1,8 @@
 from textwrap import dedent
 
-import numpy as np
 import pytest
 
-from pandas import (
-    DataFrame,
-    MultiIndex,
-)
+from pandas import DataFrame
 
 pytest.importorskip("jinja2")
 from pandas.io.formats.style import Styler
@@ -34,17 +30,13 @@ def test_basic_string(styler):
     assert result == expected
 
 
-def test_comprehesive_string():
-    midx = MultiIndex.from_product(
-        [["A", "B"], ["a", "b"], ["X", "Y"]], names=["zero", "one", "two"]
+def test_string_delimiter(styler):
+    result = styler.to_string(delimiter=";")
+    expected = dedent(
+        """\
+    ;A;B;C
+    0;0;-0.61;ab
+    1;1;-1.22;cd
+    """
     )
-    cidx = MultiIndex.from_product(
-        [["a", "b"], ["C", "D"], ["V", "W"]], names=["zero", "one", "two"]
-    )
-    df = DataFrame(np.arange(64).reshape(8, 8), index=midx, columns=cidx)
-    styler = Styler(df)
-    styler.hide_index(level=0).hide_columns(level=1)
-    styler.hide_index([("A", "a", "X"), ("A", "b", "X")])
-    styler.hide_columns([("a", "C", "W"), ("a", "D", "V")])
-    result = styler.to_string()
-    assert result == 1
+    assert result == expected
