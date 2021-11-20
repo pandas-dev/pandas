c3845aa5e887c46cc12244b7ff3d0cae6aa2f4be
zhangxiaoxing <58493968+zhangxiaoxing@users.noreply.github.com>
2021-11-20 23:58:54 +0800

    BUG: AttributeError: 'BooleanArray' object has no attribute 'sum' while infer types #44079 (#44442)

--- pandas/tests/io/parser/test_index_col.py
+++ pandas/tests/io/parser/test_index_col.py
@@ -297,3 +297,27 @@ def test_multiindex_columns_index_col_with_data(all_parsers):
         index=Index(["data"]),
     )
     tm.assert_frame_equal(result, expected)
+
+
+@skip_pyarrow
+def test_infer_types_boolean_sum(all_parsers):
+    # GH#44079
+    parser = all_parsers
+    result = parser.read_csv(
+        StringIO("0,1"),
+        names=["a", "b"],
+        index_col=["a"],
+        dtype={"a": "UInt8"},
+    )
+    expected = DataFrame(
+        data={
+            "a": [
+                0,
+            ],
+            "b": [1],
+        }
+    ).set_index("a")
+    # Not checking index type now, because the C parser will return a
+    # index column of dtype 'object', and the Python parser will return a
+    # index column of dtype 'int64'.
+    tm.assert_frame_equal(result, expected, check_index_type=False)
