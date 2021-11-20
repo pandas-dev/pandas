b90f387f92dd32c2b7a90459b4c5eccab728b5da
jbrockmendel <jbrockmendel@gmail.com>
2021-11-20 10:14:49 -0800

    BUG: Period incorrectly allowing np.timedelta64('NaT') (#44507)

--- pandas/tests/arrays/period/test_constructors.py
+++ pandas/tests/arrays/period/test_constructors.py
@@ -96,3 +96,28 @@ def test_from_sequence_disallows_i8():
 
     with pytest.raises(TypeError, match=msg):
         PeriodArray._from_sequence(list(arr.asi8), dtype=arr.dtype)
+
+
+def test_from_td64nat_sequence_raises():
+    # GH#44507
+    td = pd.NaT.to_numpy("m8[ns]")
+
+    dtype = pd.period_range("2005-01-01", periods=3, freq="D").dtype
+
+    arr = np.array([None], dtype=object)
+    arr[0] = td
+
+    msg = "Value must be Period, string, integer, or datetime"
+    with pytest.raises(ValueError, match=msg):
+        PeriodArray._from_sequence(arr, dtype=dtype)
+
+    with pytest.raises(ValueError, match=msg):
+        pd.PeriodIndex(arr, dtype=dtype)
+    with pytest.raises(ValueError, match=msg):
+        pd.Index(arr, dtype=dtype)
+    with pytest.raises(ValueError, match=msg):
+        pd.array(arr, dtype=dtype)
+    with pytest.raises(ValueError, match=msg):
+        pd.Series(arr, dtype=dtype)
+    with pytest.raises(ValueError, match=msg):
+        pd.DataFrame(arr, dtype=dtype)
