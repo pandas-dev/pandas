37e7ce0b108a56047b937605750bb68861fd73f4
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-11-22 11:29:47 +0100

    limit changes to putmask for this PR

--- pandas/core/internals/array_manager.py
+++ pandas/core/internals/array_manager.py
@@ -55,10 +55,6 @@ from pandas.core.dtypes.missing import (
 )
 
 import pandas.core.algorithms as algos
-from pandas.core.array_algos.putmask import (
-    putmask_flexible_ea,
-    putmask_flexible_ndarray,
-)
 from pandas.core.array_algos.quantile import quantile_compat
 from pandas.core.array_algos.take import take_1d
 from pandas.core.arrays import (
@@ -382,36 +378,19 @@ class BaseArrayManager(DataManager):
             align_keys = ["mask"]
             new = extract_array(new, extract_numpy=True)
 
-        kwargs = {"mask": mask, "new": new}
-        aligned_kwargs = {k: kwargs[k] for k in align_keys}
-
         for i in range(len(self.arrays)):
-
             if not self._has_no_reference(i):
                 # if being referenced -> perform Copy-on-Write and clear the reference
                 self.arrays[i] = self.arrays[i].copy()
                 self._clear_reference(i)
 
-            for k, obj in aligned_kwargs.items():
-                if isinstance(obj, (ABCSeries, ABCDataFrame)):
-                    # The caller is responsible for ensuring that
-                    #  obj.axes[-1].equals(self.items)
-                    if obj.ndim == 1:
-                        kwargs[k] = obj._values
-                    else:
-                        kwargs[k] = obj.iloc[:, i]._values
-                else:
-                    # otherwise we have an ndarray
-                    if self.ndim == 2:
-                        kwargs[k] = obj[i]
-
-            arr = self.arrays[i]
-            if isinstance(arr, np.ndarray):
-                new = putmask_flexible_ndarray(arr, **kwargs)
-            else:
-                new = putmask_flexible_ea(arr, **kwargs)
-            self.arrays[i] = new
-
+        new_mgr = self.apply_with_block(
+            "putmask",
+            align_keys=align_keys,
+            mask=mask,
+            new=new,
+        )
+        self.arrays = new_mgr.arrays
         return self
 
     def diff(self: T, n: int, axis: int) -> T:
