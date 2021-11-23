56b6d49c8679aa00447fdb31325f561474db7557
phofl <patrick_hoefler@gmx.net>
2021-11-23 21:30:28 +0100

    Change check

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -10599,7 +10599,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         {see_also}\
         {examples}
         """
-        if not isinstance(skipna, bool):
+        if not is_bool(skipna):
             warnings.warn(
                 "Passing None for skipna is deprecated and will raise in a future"
                 "version. Pass True instead. Only boolean values will be allowed "
