5f1931ace5b091f52ddac9d7833d2bfc3fca8bef
Johannes Mueller <johannes.mueller4@de.bosch.com>
2021-11-25 21:49:48 +0100

    BUG: join MultiIndex with overlapping IntervalIndex level (#44096) (#44588)

--- pandas/tests/indexes/multi/test_join.py
+++ pandas/tests/indexes/multi/test_join.py
@@ -3,6 +3,7 @@ import pytest
 
 from pandas import (
     Index,
+    Interval,
     MultiIndex,
 )
 import pandas._testing as tm
@@ -115,3 +116,45 @@ def test_join_multi_return_indexers():
 
     result = midx1.join(midx2, return_indexers=False)
     tm.assert_index_equal(result, midx1)
+
+
+def test_join_overlapping_interval_level():
+    # GH 44096
+    idx_1 = MultiIndex.from_tuples(
+        [
+            (1, Interval(0.0, 1.0)),
+            (1, Interval(1.0, 2.0)),
+            (1, Interval(2.0, 5.0)),
+            (2, Interval(0.0, 1.0)),
+            (2, Interval(1.0, 3.0)),  # interval limit is here at 3.0, not at 2.0
+            (2, Interval(3.0, 5.0)),
+        ],
+        names=["num", "interval"],
+    )
+
+    idx_2 = MultiIndex.from_tuples(
+        [
+            (1, Interval(2.0, 5.0)),
+            (1, Interval(0.0, 1.0)),
+            (1, Interval(1.0, 2.0)),
+            (2, Interval(3.0, 5.0)),
+            (2, Interval(0.0, 1.0)),
+            (2, Interval(1.0, 3.0)),
+        ],
+        names=["num", "interval"],
+    )
+
+    expected = MultiIndex.from_tuples(
+        [
+            (1, Interval(0.0, 1.0)),
+            (1, Interval(1.0, 2.0)),
+            (1, Interval(2.0, 5.0)),
+            (2, Interval(0.0, 1.0)),
+            (2, Interval(1.0, 3.0)),
+            (2, Interval(3.0, 5.0)),
+        ],
+        names=["num", "interval"],
+    )
+    result = idx_1.join(idx_2, how="outer")
+
+    tm.assert_index_equal(result, expected)
