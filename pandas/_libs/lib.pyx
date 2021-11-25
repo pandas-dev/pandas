68cd5d058b0b4753deaedb76c4869ddc151a7976
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2021-11-25 18:24:42 +0100

    PERF: improve find_common_type perf for special case of all-equal dtypes (#44594)

--- pandas/_libs/lib.pyx
+++ pandas/_libs/lib.pyx
@@ -3038,3 +3038,25 @@ def is_bool_list(obj: list) -> bool:
 
     # Note: we return True for empty list
     return True
+
+
+def dtypes_all_equal(list types not None) -> bool:
+    """
+    Faster version for:
+
+    first = types[0]
+    all(is_dtype_equal(first, t) for t in types[1:])
+
+    And assuming all elements in the list are np.dtype/ExtensionDtype objects
+
+    See timings at https://github.com/pandas-dev/pandas/pull/44594
+    """
+    first = types[0]
+    for t in types[1:]:
+        try:
+            if not t == first:
+                return False
+        except (TypeError, AttributeError):
+            return False
+    else:
+        return True
