dcc66d2186a8197ede7f848ae0901fae9f66bf73
jbrockmendel <jbrockmendel@gmail.com>
2021-11-25 07:15:17 -0800

    BUG: Indexing on nullable column (#44551)

--- pandas/tests/indexing/test_iloc.py
+++ pandas/tests/indexing/test_iloc.py
@@ -1146,6 +1146,18 @@ class TestiLocBaseIndependent:
         expected = DataFrame({"a": [5, 1, 10]})
         tm.assert_frame_equal(df, expected)
 
+    def test_iloc_getitem_slice_negative_step_ea_block(self):
+        # GH#44551
+        df = DataFrame({"A": [1, 2, 3]}, dtype="Int64")
+
+        res = df.iloc[:, ::-1]
+        tm.assert_frame_equal(res, df)
+
+        df["B"] = "foo"
+        res = df.iloc[:, ::-1]
+        expected = DataFrame({"B": df["B"], "A": df["A"]})
+        tm.assert_frame_equal(res, expected)
+
 
 class TestILocErrors:
     # NB: this test should work for _any_ Series we can pass as
