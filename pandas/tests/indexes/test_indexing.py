a36486e2ff50022f9e63c512e5c7edeb69bc5914
Johannes Mueller <johannes.mueller4@de.bosch.com>
2021-11-26 16:39:38 +0100

    BUG: fix get_indexer_non_unique() with 'object' targets with NaNs (#44482) (#44483)

--- pandas/tests/indexes/test_indexing.py
+++ pandas/tests/indexes/test_indexing.py
@@ -332,3 +332,12 @@ def test_get_indexer_non_unique_multiple_nans(idx, target, expected):
     axis = Index(idx)
     actual = axis.get_indexer_for(target)
     tm.assert_numpy_array_equal(actual, expected)
+
+
+def test_get_indexer_non_unique_nans_in_object_dtype_target(nulls_fixture):
+    idx = Index([1.0, 2.0])
+    target = Index([1, nulls_fixture], dtype="object")
+
+    result_idx, result_missing = idx.get_indexer_non_unique(target)
+    tm.assert_numpy_array_equal(result_idx, np.array([0, -1], dtype=np.intp))
+    tm.assert_numpy_array_equal(result_missing, np.array([1], dtype=np.intp))
