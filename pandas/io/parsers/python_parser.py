377d0ad412279c306e78d158eb14c9d119232a3f
phofl <patrick_hoefler@gmx.net>
2021-11-28 03:10:46 +0100

    BUG: read_csv not recognizing bad lines with names given

--- pandas/io/parsers/python_parser.py
+++ pandas/io/parsers/python_parser.py
@@ -356,6 +356,7 @@ class PythonParser(ParserBase):
         num_original_columns = 0
         clear_buffer = True
         unnamed_cols: set[str | int | None] = set()
+        self._header_line = None
 
         if self.header is not None:
             header = self.header
@@ -496,6 +497,7 @@ class PythonParser(ParserBase):
 
                 line = names[:]
 
+            self._header_line = line
             ncols = len(line)
             num_original_columns = ncols
 
@@ -867,10 +869,13 @@ class PythonParser(ParserBase):
         orig_names = list(columns)
         columns = list(columns)
 
-        try:
-            line = self._next_line()
-        except StopIteration:
-            line = None
+        if self._header_line is not None:
+            line = self._header_line
+        else:
+            try:
+                line = self._next_line()
+            except StopIteration:
+                line = None
 
         try:
             next_line = self._next_line()
