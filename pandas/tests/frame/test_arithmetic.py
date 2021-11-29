fe0500e485a1512c56cfc95c62cc35218f916f74
Gabriel Di Pardi Arruda <gabrielpardi@outlook.com>
2021-11-28 21:59:12 -0300

    testing broadcast on multiindex (#44094)

--- pandas/tests/frame/test_arithmetic.py
+++ pandas/tests/frame/test_arithmetic.py
@@ -668,6 +668,21 @@ class TestFrameFlexArithmetic:
         str(result)
         result.dtypes
 
+    @pytest.mark.parametrize("level", [0, None])
+    def test_broadcast_multiindex(self, level):
+        # GH34388
+        df1 = DataFrame({"A": [0, 1, 2], "B": [1, 2, 3]})
+        df1.columns = df1.columns.set_names("L1")
+
+        df2 = DataFrame({("A", "C"): [0, 0, 0], ("A", "D"): [0, 0, 0]})
+        df2.columns = df2.columns.set_names(["L1", "L2"])
+
+        result = df1.add(df2, level=level)
+        expected = DataFrame({("A", "C"): [0, 1, 2], ("A", "D"): [0, 1, 2]})
+        expected.columns = expected.columns.set_names(["L1", "L2"])
+
+        tm.assert_frame_equal(result, expected)
+
 
 class TestFrameArithmetic:
     def test_td64_op_nat_casting(self):
