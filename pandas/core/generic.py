3c3c33cf41d1df596836b0eeefc21912e3c2aaa1
skwirskj <skwirskj@umich.edu>
2021-11-29 16:20:32 -0500

    BUG: GH#44414 Added comments to indicate which issue was fixed

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -5875,6 +5875,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         # GH 19920: retain column metadata after concat
         result = concat(results, axis=1, copy=False)
         result.columns = self.columns
+        # GH#44414: call finalize to retain metadata for the DataFrame or Series
         result = result.__finalize__(result, method="astype")
         # https://github.com/python/mypy/issues/8354
         return cast(NDFrameT, result)
