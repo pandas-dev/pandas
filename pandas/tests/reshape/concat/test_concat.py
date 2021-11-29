cfcc6d1ff43a43fa16e805a5d36dde5a62b0562a
xiki-tempula <zhiyi.wu@msdtc.ox.ac.uk>
2021-11-29 17:46:13 +0000

    update

--- pandas/tests/reshape/concat/test_concat.py
+++ pandas/tests/reshape/concat/test_concat.py
@@ -699,35 +699,28 @@ def test_concat_posargs_deprecation():
     expected = DataFrame([[1, 2, 3], [4, 5, 6]], index=["a", "b"])
     tm.assert_frame_equal(result, expected)
 
-
-def test_concat_retain_attrs_df():
+@pytest.mark.parametrize("data", [Series(data=[1, 2]),
+                                  DataFrame(data={"col1": [1, 2],}),
+                                  DataFrame(),
+                                  Series()])
+def test_concat_drop_attrs(data):
     # GH#41828
-    d = {"col1": [1, 2], "col2": [3, 4]}
-    df1 = DataFrame(data=d)
+    df1 = data.copy()
     df1.attrs = {1: 1}
-    df2 = DataFrame(data=d)
-    df2.attrs = {1: 1}
-    df = concat([df1, df2])
-    assert df.attrs[1] == 1
-
-
-def test_concat_retain_attrs_series():
-    # GH#41828
-    d = [1, 2]
-    df1 = Series(data=d)
-    df1.attrs = {1: 1}
-    df2 = Series(data=d)
-    df2.attrs = {1: 1}
-    df = concat([df1, df2])
-    assert df.attrs[1] == 1
-
-
-def test_concat_drop_attrs():
-    # GH#41828
-    d = {"col1": [1, 2], "col2": [3, 4]}
-    df1 = DataFrame(data=d)
-    df1.attrs = {1: 1}
-    df2 = DataFrame(data=d)
+    df2 = data.copy()
     df2.attrs = {1: 2}
     df = concat([df1, df2])
     assert len(df.attrs) == 0
+
+@pytest.mark.parametrize("data", [Series(data=[1, 2]),
+                                  DataFrame(data={"col1": [1, 2],}),
+                                  DataFrame(),
+                                  Series()])
+def test_concat_retain_attrs(data):
+    # GH#41828
+    df1 = data.copy()
+    df1.attrs = {1: 1}
+    df2 = data.copy()
+    df2.attrs = {1: 1}
+    df = concat([df1, df2])
+    assert df.attrs[1] == 1
