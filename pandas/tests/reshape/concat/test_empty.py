fd9ba2470234ecd79302569b588ea3aa3ff574c1
Brock <jbrockmendel@gmail.com>
2021-11-30 11:38:48 -0800

    remove non-actionable

--- pandas/tests/reshape/concat/test_empty.py
+++ pandas/tests/reshape/concat/test_empty.py
@@ -197,14 +197,12 @@ class TestEmptyConcat:
         result = concat(
             [Series(dtype="float64").astype("Sparse"), Series(dtype="float64")]
         )
-        # TODO(TomAugspurger#22325): release-note: concat sparse dtype
         expected = pd.SparseDtype(np.float64)
         assert result.dtype == expected
 
         result = concat(
             [Series(dtype="float64").astype("Sparse"), Series(dtype="object")]
         )
-        # TODO(TomAugspurger#22325): release-note: concat sparse dtype
         expected = pd.SparseDtype("object")
         assert result.dtype == expected
 
