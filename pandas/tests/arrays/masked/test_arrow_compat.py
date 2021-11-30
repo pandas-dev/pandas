31b0065ebf9be6ba38d52563b70de3a52f0c91a2
Tim Swast <swast@google.com>
2021-11-30 17:12:44 -0600

    simplify types_mapper tests

--- pandas/tests/arrays/masked/test_arrow_compat.py
+++ pandas/tests/arrays/masked/test_arrow_compat.py
@@ -3,7 +3,6 @@ import pytest
 
 import pandas as pd
 import pandas._testing as tm
-from pandas.api.types import pandas_dtype
 
 pa = pytest.importorskip("pyarrow", minversion="1.0.1")
 
@@ -37,24 +36,24 @@ def test_arrow_roundtrip(data):
     tm.assert_frame_equal(result, df)
 
 
-def test_dataframe_from_arrow_types_mapper(any_int_ea_dtype):
-    int_dtype = pandas_dtype(any_int_ea_dtype)
-
+def test_dataframe_from_arrow_types_mapper():
     def types_mapper(arrow_type):
         if pa.types.is_boolean(arrow_type):
             return pd.BooleanDtype()
         elif pa.types.is_integer(arrow_type):
-            return int_dtype
+            return pd.Int64Dtype()
 
     bools_array = pa.array([True, None, False], type=pa.bool_())
     ints_array = pa.array([1, None, 2], type=pa.int64())
+    small_ints_array = pa.array([-1, 0, 7], type=pa.int8())
     record_batch = pa.RecordBatch.from_arrays(
-        [bools_array, ints_array], ["bools", "ints"]
+        [bools_array, ints_array, small_ints_array], ["bools", "ints", "small_ints"]
     )
     result = record_batch.to_pandas(types_mapper=types_mapper)
     bools = pd.Series([True, None, False], dtype="boolean")
-    ints = pd.Series([1, None, 2], dtype=int_dtype.name)
-    expected = pd.DataFrame({"bools": bools, "ints": ints})
+    ints = pd.Series([1, None, 2], dtype="Int64")
+    small_ints = pd.Series([-1, 0, 7], dtype="Int64")
+    expected = pd.DataFrame({"bools": bools, "ints": ints, "small_ints": small_ints})
     tm.assert_frame_equal(result, expected)
 
 
