860a07256e09a65922b39e29c8ca236ea14faa69
Brock <jbrockmendel@gmail.com>
2021-12-03 15:43:21 -0800

    TST skip->xfail

--- pandas/tests/apply/test_frame_transform.py
+++ pandas/tests/apply/test_frame_transform.py
@@ -158,10 +158,8 @@ def test_transform_bad_dtype(op, frame_or_series, request):
 
 
 @pytest.mark.parametrize("op", frame_kernels_raise)
-def test_transform_partial_failure_typeerror(op):
+def test_transform_partial_failure_typeerror(op, request):
     # GH 35964
-    if op == "rank":
-        pytest.skip("GH 40418: rank does not raise a TypeError")
 
     # Using object makes most transform kernels fail
     df = DataFrame({"A": 3 * [object], "B": [1, 2, 3]})
