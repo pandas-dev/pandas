a1f0ca9186011c10b4d28b49d52091e6cc9ffdc2
John Zangwill <john@zangwill.net>
2021-12-04 16:01:16 +0000

    Update test_nth.py

--- pandas/tests/groupby/test_nth.py
+++ pandas/tests/groupby/test_nth.py
@@ -801,11 +801,11 @@ def test_groupby_nth_with_column_axis():
 def test_nth_slices_with_column_axis(
     start, stop, expected_values, expected_columns, method
 ):
-    df = pd.DataFrame([range(5)], columns=[list("ABCDE")])
+    df = DataFrame([range(5)], columns=[list("ABCDE")])
     gb = df.groupby([5, 5, 5, 6, 6], axis=1)
     result = {
         "call": lambda start, stop: gb.nth(slice(start, stop)),
         "index": lambda start, stop: gb.nth[start:stop],
     }[method](start, stop)
-    expected = pd.DataFrame([expected_values], columns=expected_columns)
+    expected = DataFrame([expected_values], columns=expected_columns)
     tm.assert_frame_equal(result, expected)
