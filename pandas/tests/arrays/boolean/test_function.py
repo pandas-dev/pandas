1b77e3eedc6f7e19b6a85aabd880acc0a30c08e1
jbrockmendel <jbrockmendel@gmail.com>
2021-12-04 15:51:30 -0800

    BUG: FloatingArray(float16data) (#44715)

--- pandas/tests/arrays/boolean/test_function.py
+++ pandas/tests/arrays/boolean/test_function.py
@@ -66,6 +66,17 @@ def test_ufuncs_unary(ufunc):
     tm.assert_series_equal(result, expected)
 
 
+def test_ufunc_numeric():
+    # np.sqrt on np.bool returns float16, which we upcast to Float32
+    #  bc we do not have Float16
+    arr = pd.array([True, False, None], dtype="boolean")
+
+    res = np.sqrt(arr)
+
+    expected = pd.array([1, 0, None], dtype="Float32")
+    tm.assert_extension_array_equal(res, expected)
+
+
 @pytest.mark.parametrize("values", [[True, False], [True, None]])
 def test_ufunc_reduce_raises(values):
     a = pd.array(values, dtype="boolean")
