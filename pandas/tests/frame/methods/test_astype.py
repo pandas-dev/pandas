e61aa827302f326c8254d35bbdf69bb2fa058645
skwirskj <skwirskj@umich.edu>
2021-12-05 15:56:17 -0500

    BUG: GH#44414 Added fixture use in test_astype.py
    Added fixture use in both test_astype_retain_attrs functions in the DataFrame and Series testing files.

--- pandas/tests/frame/methods/test_astype.py
+++ pandas/tests/frame/methods/test_astype.py
@@ -714,15 +714,12 @@ class TestAstype:
         expected = df.iloc[index_slice]
         tm.assert_frame_equal(result, expected, check_dtype=False)
 
-    @pytest.mark.parametrize(
-        "dtype", ["float", "int32", "int64", "string", "int16", "float32"]
-    )
-    def test_astype_retain_attrs(self, dtype):
+    def test_astype_retain_attrs(self, any_numpy_dtype):
         # GH#44414
         df = DataFrame({"a": [0, 1, 2], "b": [3, 4, 5]})
         df.attrs["Location"] = "Michigan"
 
-        result = df.astype({"a": dtype}).attrs
+        result = df.astype({"a": any_numpy_dtype}).attrs
         expected = df.attrs
 
         tm.assert_dict_equal(expected, result)
