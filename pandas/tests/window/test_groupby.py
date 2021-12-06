572baff96ab3e27377f1194faa782b4e469ff8d1
Matthew Roeschke <emailformattr@gmail.com>
2021-12-06 06:32:38 -0800

    TST: Add regression test for groupby rolling (#44774)

--- pandas/tests/window/test_groupby.py
+++ pandas/tests/window/test_groupby.py
@@ -838,6 +838,33 @@ class TestRolling:
         )
         tm.assert_frame_equal(result, expected)
 
+    def test_nan_and_zero_endpoints(self):
+        # https://github.com/twosigma/pandas/issues/53
+        size = 1000
+        idx = np.repeat(0, size)
+        idx[-1] = 1
+
+        val = 5e25
+        arr = np.repeat(val, size)
+        arr[0] = np.nan
+        arr[-1] = 0
+
+        df = DataFrame(
+            {
+                "index": idx,
+                "adl2": arr,
+            }
+        ).set_index("index")
+        result = df.groupby("index")["adl2"].rolling(window=10, min_periods=1).mean()
+        expected = Series(
+            arr,
+            name="adl2",
+            index=MultiIndex.from_arrays(
+                [[0] * 999 + [1], [0] * 999 + [1]], names=["index", "index"]
+            ),
+        )
+        tm.assert_series_equal(result, expected)
+
 
 class TestExpanding:
     def setup_method(self):
