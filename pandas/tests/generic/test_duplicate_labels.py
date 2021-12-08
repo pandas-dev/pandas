b673e4bfcc7dfbdb0452d64e920f42a609f1b1e1
Gesa Stupperich <gesa.stupperich@gmail.com>
2021-12-08 12:28:55 +0000

    Extract variable and improve variable name

--- pandas/tests/generic/test_duplicate_labels.py
+++ pandas/tests/generic/test_duplicate_labels.py
@@ -295,10 +295,11 @@ class TestRaises:
         assert data.flags.allows_duplicate_labels is True
 
     def test_series_raises(self):
-        s = pd.Series([0, 1], index=["a", "b"]).set_flags(allows_duplicate_labels=False)
+        a = pd.Series(0, index=["a", "b"])
+        b = pd.Series([0, 1], index=["a", "b"]).set_flags(allows_duplicate_labels=False)
         msg = "Index has duplicates."
         with pytest.raises(pd.errors.DuplicateLabelError, match=msg):
-            pd.concat([pd.Series(0, index=["a", "b"]), s])
+            pd.concat([a, b])
 
     @pytest.mark.parametrize(
         "getter, target",
--- pandas/tests/indexing/test_partial.py
+++ pandas/tests/indexing/test_partial.py
@@ -538,8 +538,8 @@ class TestPartialSetting:
         # allow object conversion here
         df = orig.copy()
         df.loc["a", :] = df.iloc[0]
-        s = Series(df.iloc[0], name="a")
-        exp = pd.concat([orig, DataFrame(s).T.infer_objects()])
+        ser = Series(df.iloc[0], name="a")
+        exp = pd.concat([orig, DataFrame(ser).T.infer_objects()])
         tm.assert_frame_equal(df, exp)
         tm.assert_index_equal(df.index, Index(orig.index.tolist() + ["a"]))
         assert df.index.dtype == "object"
