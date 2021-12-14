2cd3cf2c8588e5449d4b7bfdff372547942dd2f2
Anuj Koli <aanuj111@gmail.com>
2021-12-14 15:42:34 -0500

    Fixed issue with pre commit test

--- pandas/core/strings/accessor.py
+++ pandas/core/strings/accessor.py
@@ -1894,7 +1894,8 @@ class StringMethods(NoNewAttributesMixin):
 
     See Also
     --------
-    Series.str.strip : Remove leading and trailing characters in Series/Index, it also replaces numeric and bool values with NaN.
+    Series.str.strip : Remove leading and trailing characters in Series/Index,
+                        it also replaces numeric and bool values with NaN.
     Series.str.lstrip : Remove leading characters in Series/Index.
     Series.str.rstrip : Remove trailing characters in Series/Index.
 
@@ -1938,7 +1939,7 @@ class StringMethods(NoNewAttributesMixin):
 
     s = pd.Series(['hello ', ' goodbye', 1.0, 0.0, 1, 0, True, False, np.nan])
     >>> s
-    0      hello 
+    0      hello
     1     goodbye
     2         1.0
     3         0.0
