8d8ce93dd75280afd3014e40e14fcd3116d1ee46
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2021-12-16 22:27:43 +0100

    Bug in concat casting all na levels to float (#44902)

--- pandas/tests/reshape/concat/test_datetimes.py
+++ pandas/tests/reshape/concat/test_datetimes.py
@@ -528,3 +528,16 @@ def test_concat_timedelta64_block():
     result = concat([df, df])
     tm.assert_frame_equal(result.iloc[:10], df)
     tm.assert_frame_equal(result.iloc[10:], df)
+
+
+def test_concat_multiindex_datetime_nat():
+    # GH#44900
+    left = DataFrame({"a": 1}, index=MultiIndex.from_tuples([(1, pd.NaT)]))
+    right = DataFrame(
+        {"b": 2}, index=MultiIndex.from_tuples([(1, pd.NaT), (2, pd.NaT)])
+    )
+    result = concat([left, right], axis="columns")
+    expected = DataFrame(
+        {"a": [1.0, np.nan], "b": 2}, MultiIndex.from_tuples([(1, pd.NaT), (2, pd.NaT)])
+    )
+    tm.assert_frame_equal(result, expected)
