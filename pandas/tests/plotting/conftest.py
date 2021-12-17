b0cc40c5044e8d2d6e179ca73b75c806eaa27d14
Matthew Roeschke <emailformattr@gmail.com>
2021-12-16 19:34:36 -0800

    Mock FT2Font object that doesn't close file

---
+++ pandas/tests/plotting/conftest.py
@@ -0,0 +1,14 @@
+import matplotlib
+import pytest
+
+
+class FT2FontCloser(matplotlib.ft2font.FT2Font):
+    def __init__(self, path, *args, **kwargs):
+        with open(path, "rb") as f:
+            super().__init__(f, *args, **kwargs)
+
+
+@pytest.fixture(autouse=True)
+def no_font_loading(monkeypatch):
+    """Remove requests.sessions.Session.request for all tests."""
+    monkeypatch.setattr("matplotlib.ft2font.FT2Font", FT2FontCloser)
