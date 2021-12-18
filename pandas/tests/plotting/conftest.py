113c108acad07db37d2fb4ab9b359e0f793c3513
Matthew Roeschke <emailformattr@gmail.com>
2021-12-17 18:08:28 -0800

    Try patching without importing

--- pandas/tests/plotting/conftest.py
+++ pandas/tests/plotting/conftest.py
@@ -8,6 +8,12 @@ import pytest
 pat = re.compile(r"python3")
 
 
+def __init_closer__(self, path, *args, **kwargs):
+    with open(path, "rb") as f:
+        stream = io.BytesIO(f.read())
+    super().__init__(stream, *args, **kwargs)
+
+
 @pytest.fixture(autouse=True)
 def check_bufferedrandom_resourcewarning(request):
     yield
@@ -23,12 +29,12 @@ def check_bufferedrandom_resourcewarning(request):
 
 @pytest.fixture(autouse=True)
 def no_font_loading(monkeypatch):
-    import matplotlib
+    # import matplotlib
+    #
+    # class FT2FontCloser(matplotlib.ft2font.FT2Font):
+    #     def __init__(self, path, *args, **kwargs):
+    #         with open(path, "rb") as f:
+    #             stream = io.BytesIO(f.read())
+    #         super().__init__(stream, *args, **kwargs)
 
-    class FT2FontCloser(matplotlib.ft2font.FT2Font):
-        def __init__(self, path, *args, **kwargs):
-            with open(path, "rb") as f:
-                stream = io.BytesIO(f.read())
-            super().__init__(stream, *args, **kwargs)
-
-    monkeypatch.setattr("matplotlib.ft2font.FT2Font", FT2FontCloser)
+    monkeypatch.setattr("matplotlib.ft2font.FT2Font.__init__", __init_closer__)
