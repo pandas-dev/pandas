f04d05d668066ab9ef382360bcca92fcd0cfe3b1
shubham11941140 <shubhamgupta@iitbhilai.ac.in>
2021-12-18 23:03:30 +0530

    Changed to string

--- pandas/tests/frame/methods/test_replace.py
+++ pandas/tests/frame/methods/test_replace.py
@@ -1512,7 +1512,7 @@ class TestDataFrameReplaceRegex:
         result_df1 = df1.replace(to_replace="0", value=1, regex=regex)
         tm.assert_frame_equal(result_df1, expected_df1)
 
-        df2 = DataFrame({"A": ["0"], "B": [np.NaN]})
-        expected_df2 = DataFrame({"A": [1], "B": [np.NaN]})
+        df2 = DataFrame({"A": ["0"], "B": ["1"]})
+        expected_df2 = DataFrame({"A": [1], "B": ["1"]})
         result_df2 = df2.replace(to_replace="0", value=1, regex=regex)
         tm.assert_frame_equal(result_df2, expected_df2)
