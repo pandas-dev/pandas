de88be98bd06f17b12b0aa7f23ff18d402ef5a3d
jbrockmendel <jbrockmendel@gmail.com>
2021-12-23 09:12:42 -0800

    ENH: EA._hasnans, EADtype.empty (#45024)

--- pandas/tests/extension/base/constructors.py
+++ pandas/tests/extension/base/constructors.py
@@ -126,6 +126,12 @@ class BaseConstructorsTests(BaseExtensionTests):
     def test_empty(self, dtype):
         cls = dtype.construct_array_type()
         result = cls._empty((4,), dtype=dtype)
-
         assert isinstance(result, cls)
         assert result.dtype == dtype
+        assert result.shape == (4,)
+
+        # GH#19600 method on ExtensionDtype
+        result2 = dtype.empty((4,))
+        assert isinstance(result2, cls)
+        assert result2.dtype == dtype
+        assert result2.shape == (4,)
