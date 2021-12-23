814e5ff7cf80bf3053843e02455f8744b9ee5299
shubham11941140 <shubhamgupta@iitbhilai.ac.in>
2021-12-24 01:35:30 +0530

    precommit

--- pandas/core/dtypes/cast.py
+++ pandas/core/dtypes/cast.py
@@ -2096,7 +2096,7 @@ def maybe_cast_to_integer_array(
         )
         return casted
 
-    if lib.infer_dtype(casted) is "integer":
+    if lib.infer_dtype(casted) == "integer":
         return casted
 
     # No known cases that get here, but raising explicitly to cover our bases.
