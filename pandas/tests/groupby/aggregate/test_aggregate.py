4d4a02349a0320c4adcf5350292abca49b8c8999
aliraeisdanaei <mrrookie2@gmail.com>
2021-12-26 22:36:18 -0500

    Removed unnecessary lines from function

--- pandas/tests/groupby/aggregate/test_aggregate.py
+++ pandas/tests/groupby/aggregate/test_aggregate.py
@@ -65,9 +65,7 @@ def test_agg_ser_multi_key(df):
 def test_agg_different_partials():
     # for issue 28570
 
-    def add_top_column(df, top_col, inplace=False):
-        if not inplace:
-            df = df.copy()
+    def add_top_column(df, top_col):
         df.columns = pd.MultiIndex.from_product([[top_col], df.columns])
         return df
 
