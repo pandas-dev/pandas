6fbd724031c907982dea8fd765d4b27a7447fa10
Matthew Roeschke <emailformattr@gmail.com>
2021-12-27 14:02:40 -0800

    TST: Split and parameterize series/test_api.py (#45079)

--- pandas/tests/series/test_missing.py
+++ pandas/tests/series/test_missing.py
@@ -36,17 +36,19 @@ class TestSeriesMissingData:
         tm.assert_series_equal(r, e)
         tm.assert_series_equal(dr, de)
 
-    def test_isnull_for_inf_deprecated(self):
+    @pytest.mark.parametrize(
+        "method, expected",
+        [
+            ["isna", Series([False, True, True, False])],
+            ["dropna", Series(["a", 1.0], index=[0, 3])],
+        ],
+    )
+    def test_isnull_for_inf_deprecated(self, method, expected):
         # gh-17115
         s = Series(["a", np.inf, np.nan, 1.0])
         with pd.option_context("mode.use_inf_as_null", True):
-            r = s.isna()
-            dr = s.dropna()
-
-        e = Series([False, True, True, False])
-        de = Series(["a", 1.0], index=[0, 3])
-        tm.assert_series_equal(r, e)
-        tm.assert_series_equal(dr, de)
+            result = getattr(s, method)()
+        tm.assert_series_equal(result, expected)
 
     def test_timedelta64_nan(self):
 
