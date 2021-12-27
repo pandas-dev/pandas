81ca49772a0ccba1e5bec7d33555a37f4142000b
Matthew Roeschke <emailformattr@gmail.com>
2021-12-27 05:48:56 -0800

    DOC: Run doctests over pandas directory (#45056)

--- pandas/tests/arithmetic/conftest.py
+++ pandas/tests/arithmetic/conftest.py
@@ -24,7 +24,9 @@ def switch_numexpr_min_elements(request):
 
 # ------------------------------------------------------------------
 
-
+# doctest with +SKIP for one fixture fails during setup with
+# 'DoctestItem' object has no attribute 'callspec'
+# due to switch_numexpr_min_elements fixture
 @pytest.fixture(params=[1, np.array(1, dtype=np.int64)])
 def one(request):
     """
@@ -37,11 +39,11 @@ def one(request):
 
     Examples
     --------
-    >>> dti = pd.date_range('2016-01-01', periods=2, freq='H')
-    >>> dti
+    dti = pd.date_range('2016-01-01', periods=2, freq='H')
+    dti
     DatetimeIndex(['2016-01-01 00:00:00', '2016-01-01 01:00:00'],
     dtype='datetime64[ns]', freq='H')
-    >>> dti + one
+    dti + one
     DatetimeIndex(['2016-01-01 01:00:00', '2016-01-01 02:00:00'],
     dtype='datetime64[ns]', freq='H')
     """
@@ -61,6 +63,9 @@ zeros.extend([np.array(-0.0, dtype=np.float64)])
 zeros.extend([0, 0.0, -0.0])
 
 
+# doctest with +SKIP for zero fixture fails during setup with
+# 'DoctestItem' object has no attribute 'callspec'
+# due to switch_numexpr_min_elements fixture
 @pytest.fixture(params=zeros)
 def zero(request):
     """
@@ -74,8 +79,8 @@ def zero(request):
 
     Examples
     --------
-    >>> arr = RangeIndex(5)
-    >>> arr / zeros
+    arr = RangeIndex(5)
+    arr / zeros
     Float64Index([nan, inf, inf, inf, inf], dtype='float64')
     """
     return request.param
