1d502bb5be5066e1511e9981045c0e753353e137
phofl <patrick_hoefler@gmx.net>
2021-12-29 23:55:59 +0100

    Skip when no lxml

--- pandas/tests/io/xml/test_to_xml.py
+++ pandas/tests/io/xml/test_to_xml.py
@@ -1308,6 +1308,7 @@ def test_filename_and_suffix_comp(parser, compression_only):
     assert geom_xml == output.strip()
 
 
+@td.skip_if_no("lxml")
 def test_ea_dtypes(any_numeric_ea_dtype):
     # GH#43903
     expected = """<?xml version='1.0' encoding='utf-8'?>
