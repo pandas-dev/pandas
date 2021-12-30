8fdd3d2b044fad37a55f87ddd6a41cf2e29b7ddb
phofl <patrick_hoefler@gmx.net>
2021-12-30 16:06:46 +0100

    Revert "Remove typevar"
    This reverts commit ceb17c68d1a3460ea6c1ff9b4acd4fb52f2b9c66.

--- pandas/io/excel/_base.py
+++ pandas/io/excel/_base.py
@@ -28,6 +28,7 @@ from pandas._libs.parsers import STR_NA_VALUES
 from pandas._typing import (
     DtypeArg,
     FilePath,
+    IntStrT,
     ReadBuffer,
     StorageOptions,
     WriteExcelBuffer,
@@ -386,7 +387,7 @@ def read_excel(
 @overload
 def read_excel(
     io,
-    sheet_name: list[int],
+    sheet_name: list[IntStrT] | None,
     header: int | Sequence[int] | None = ...,
     names=...,
     index_col: int | Sequence[int] | None = ...,
@@ -412,40 +413,7 @@ def read_excel(
     convert_float: bool | None = ...,
     mangle_dupe_cols: bool = ...,
     storage_options: StorageOptions = ...,
-) -> dict[int, DataFrame]:
-    ...
-
-
-@overload
-def read_excel(
-    io,
-    sheet_name: list[str] | None,
-    header: int | Sequence[int] | None = ...,
-    names=...,
-    index_col: int | Sequence[int] | None = ...,
-    usecols=...,
-    squeeze: bool | None = ...,
-    dtype: DtypeArg | None = ...,
-    engine: Literal["xlrd", "openpyxl", "odf", "pyxlsb"] | None = ...,
-    converters=...,
-    true_values: Iterable[Hashable] | None = ...,
-    false_values: Iterable[Hashable] | None = ...,
-    skiprows: Sequence[int] | int | Callable[[int], object] | None = ...,
-    nrows: int | None = ...,
-    na_values=...,
-    keep_default_na: bool = ...,
-    na_filter: bool = ...,
-    verbose: bool = ...,
-    parse_dates=...,
-    date_parser=...,
-    thousands: str | None = ...,
-    decimal: str = ...,
-    comment: str | None = ...,
-    skipfooter: int = ...,
-    convert_float: bool | None = ...,
-    mangle_dupe_cols: bool = ...,
-    storage_options: StorageOptions = ...,
-) -> dict[str, DataFrame]:
+) -> dict[IntStrT, DataFrame]:
     ...
 
 
@@ -453,7 +421,7 @@ def read_excel(
 @Appender(_read_excel_doc)
 def read_excel(
     io,
-    sheet_name: str | int | list[int] | list[str] | None = 0,
+    sheet_name: str | int | list[IntStrT] | None = 0,
     header: int | Sequence[int] | None = 0,
     names=None,
     index_col: int | Sequence[int] | None = None,
@@ -479,7 +447,7 @@ def read_excel(
     convert_float: bool | None = None,
     mangle_dupe_cols: bool = True,
     storage_options: StorageOptions = None,
-) -> DataFrame | dict[int, DataFrame] | dict[str, DataFrame]:
+) -> DataFrame | dict[IntStrT, DataFrame]:
 
     should_close = False
     if not isinstance(io, ExcelFile):
