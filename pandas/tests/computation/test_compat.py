6911891b21032954f13c5552afc97d38767ab62a
Matthew Roeschke <emailformattr@gmail.com>
2021-12-31 17:20:14 -0800

    TST: Use pytest or decorator instead of checking ImportError

--- pandas/tests/computation/test_compat.py
+++ pandas/tests/computation/test_compat.py
@@ -13,16 +13,13 @@ def test_compat():
 
     from pandas.core.computation.check import NUMEXPR_INSTALLED
 
-    try:
-        import numexpr as ne
+    ne = pytest.importorskip("numexpr")
 
-        ver = ne.__version__
-        if Version(ver) < Version(VERSIONS["numexpr"]):
-            assert not NUMEXPR_INSTALLED
-        else:
-            assert NUMEXPR_INSTALLED
-    except ImportError:
-        pytest.skip("not testing numexpr version compat")
+    ver = ne.__version__
+    if Version(ver) < Version(VERSIONS["numexpr"]):
+        assert not NUMEXPR_INSTALLED
+    else:
+        assert NUMEXPR_INSTALLED
 
 
 @pytest.mark.parametrize("engine", ENGINES)
@@ -34,11 +31,5 @@ def test_invalid_numexpr_version(engine, parser):
         assert res == 3
 
     if engine == "numexpr":
-        try:
-            import numexpr as ne  # noqa:F401
-        except ImportError:
-            pytest.skip("no numexpr")
-        else:
-            testit()
-    else:
-        testit()
+        pytest.importorskip("numexpr")
+    testit()
