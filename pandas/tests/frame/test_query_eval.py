85db2cd75e8cfe4b1234f34dae9be0fc342cebc0
bubblingoak <gubbels.chris@gmail.com>
2022-01-04 06:15:13 -0800

    BUG: allow use of both default+input resolvers in df.eval, GH34966 (#45134)

--- pandas/tests/frame/test_query_eval.py
+++ pandas/tests/frame/test_query_eval.py
@@ -165,6 +165,17 @@ class TestDataFrameEval:
         assert df.eval("a + b", resolvers=[dict1, dict2]) == dict1["a"] + dict2["b"]
         assert pd.eval("a + b", resolvers=[dict1, dict2]) == dict1["a"] + dict2["b"]
 
+    def test_eval_resolvers_combined(self):
+        # GH 34966
+        df = DataFrame(np.random.randn(10, 2), columns=list("ab"))
+        dict1 = {"c": 2}
+
+        # Both input and default index/column resolvers should be usable
+        result = df.eval("a + b * c", resolvers=[dict1])
+
+        expected = df["a"] + df["b"] * dict1["c"]
+        tm.assert_series_equal(result, expected)
+
     def test_eval_object_dtype_binop(self):
         # GH#24883
         df = DataFrame({"a1": ["Y", "N"]})
