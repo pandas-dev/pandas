4f996f31319f37320a41ee315220c252501f4f3f
Matthew Roeschke <emailformattr@gmail.com>
2022-01-04 19:17:26 -0800

    Complete get_versions_from_optional

--- scripts/validate_min_versions_in_sync.py
+++ scripts/validate_min_versions_in_sync.py
@@ -10,6 +10,8 @@ This is meant to be run as a pre-commit hook - to run it manually, you can do:
 
     pre-commit run validate-min-versions-in-sync --all-files
 """
+from __future__ import annotations
+
 # import argparse
 import ast
 import pathlib
@@ -25,10 +27,22 @@ CODE_PATH = pathlib.Path("pandas/compat/_optional.py").resolve()
 
 
 def get_versions_from_optional(content: str) -> dict[str, str]:
+    num_dicts = 0
     for node in ast.walk(ast.parse(content)):
         if isinstance(node, ast.Dict):
-            # version_dict = node
-            break
+            if num_dicts == 0:
+                version_dict_ast = node
+                num_dicts += 1
+            elif num_dicts == 1:
+                install_map_ast = node
+                break
+    install_map = {
+        k.value: v.value for k, v in zip(install_map_ast.keys, install_map_ast.values)
+    }
+    return {
+        install_map.get(k.value, k.value): v.value
+        for k, v in zip(version_dict_ast.keys, version_dict_ast.values)
+    }
 
 
 if __name__ == "__main__":
