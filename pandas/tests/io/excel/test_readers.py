c15f9bdc173b8d8e1b24651245f46ef21d85a73d
Matthew Roeschke <emailformattr@gmail.com>
2022-01-10 13:25:50 -0800

    Adjust tests for it_IT locale

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -738,9 +738,11 @@ class TestReaders:
 
     def test_missing_file_raises(self, read_ext):
         bad_file = f"foo{read_ext}"
-        # CI tests with zh_CN.utf8, translates to "No such file or directory"
+        # CI tests with other languages, translates to "No such file or directory"
+        match = r"(No such file or directory|没有那个文件或目录|File o directory non esistente)"
         with pytest.raises(
-            FileNotFoundError, match=r"(No such file or directory|没有那个文件或目录)"
+            FileNotFoundError,
+            match=match,
         ):
             pd.read_excel(bad_file)
 
