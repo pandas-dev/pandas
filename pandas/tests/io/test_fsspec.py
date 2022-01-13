f74365e4ce75dab54224b6bd111e0cf43e336425
Matthew Roeschke <emailformattr@gmail.com>
2022-01-13 15:06:04 -0800

    TST: xfail test_arrowparquet_options for windows

--- pandas/tests/io/test_fsspec.py
+++ pandas/tests/io/test_fsspec.py
@@ -161,6 +161,7 @@ def test_to_parquet_new_file(monkeypatch, cleared_fs):
 
 
 @td.skip_if_no("pyarrow")
+@pytest.mark.xfail(td.is_platform_windows(), reason="GH 45344")
 def test_arrowparquet_options(fsspectest):
     """Regression test for writing to a not-yet-existent GCS Parquet file."""
     df = DataFrame({"a": [0]})
