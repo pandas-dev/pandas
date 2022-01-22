6677f9ecb2e019c0f49b59ecaf54d9024ce3ccb1
jbrockmendel <jbrockmendel@gmail.com>
2022-01-21 16:23:19 -0800

    PERF: MaskedArray._values_for_argsort (#45434)

--- pandas/core/arrays/base.py
+++ pandas/core/arrays/base.py
@@ -628,6 +628,16 @@ class ExtensionArray:
         See Also
         --------
         ExtensionArray.argsort : Return the indices that would sort this array.
+
+        Notes
+        -----
+        The caller is responsible for *not* modifying these values in-place, so
+        it is safe for implementors to give views on `self`.
+
+        Functions that use this (e.g. ExtensionArray.argsort) should ignore
+        entries with missing values in the original array (according to `self.isna()`).
+        This means that the corresponding entries in the returned array don't need to
+        be modified to sort correctly.
         """
         # Note: this is used in `ExtensionArray.argsort`.
         return np.array(self)
