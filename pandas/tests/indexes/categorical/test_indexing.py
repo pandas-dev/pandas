b6fcd444b069d15ef6f1b670a62798ba2a8e050a
Shashank Shet <shashankshet@gmail.com>
2022-01-24 21:55:15 +0530

    Re-aligned test cases

--- pandas/tests/indexes/categorical/test_indexing.py
+++ pandas/tests/indexes/categorical/test_indexing.py
@@ -302,12 +302,12 @@ class TestGetIndexer:
         # GH 45361
         ci = CategoricalIndex([1, 2, np.nan, 3])
         other1 = [2, 3, 4, np.nan]
-        other2 = [1, 4, 2, 3]
         res1 = ci.get_indexer(other1)
-        res2 = ci.get_indexer(other2)
         expected1 = np.array([1, 3, -1, 2], dtype=np.intp)
-        expected2 = np.array([0, -1, 1, 3], dtype=np.intp)
         tm.assert_numpy_array_equal(res1, expected1)
+        other2 = [1, 4, 2, 3]
+        res2 = ci.get_indexer(other2)
+        expected2 = np.array([0, -1, 1, 3], dtype=np.intp)
         tm.assert_numpy_array_equal(res2, expected2)
 
 
