2d416a37ad3e09cb7e2a7e2c9de51d96ec63ef83
Matthew Roeschke <emailformattr@gmail.com>
2022-01-23 17:23:03 -0800

    Fix method name

--- pandas/io/excel/_base.py
+++ pandas/io/excel/_base.py
@@ -541,10 +541,10 @@ class BaseExcelReader(metaclass=abc.ABCMeta):
                 # openpyxl: https://stackoverflow.com/questions/31416842/
                 #     openpyxl-does-not-close-excel-workbook-in-read-only-mode
                 self.book.close()
-            elif hasattr(self.book, "release_resource"):
+            elif hasattr(self.book, "release_resources"):
                 # xlrd
                 # https://github.com/python-excel/xlrd/blob/2.0.1/xlrd/book.py#L548
-                self.book.release_resource()
+                self.book.release_resources()
         self.handles.close()
 
     @property
