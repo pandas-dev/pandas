8e8b37cdcde9b30d6cdfa9394c8531d5916bd193
Pablo Osorio López <osoriopabl@gmail.com>
2022-01-26 11:10:34 -0500

    TST: Change test to correct categorical naming
    Value counts tend to preserve index names #45625
    Change test test_sorting_with_different_categoricals to comply
    to this change

--- pandas/tests/groupby/test_categorical.py
+++ pandas/tests/groupby/test_categorical.py
@@ -271,7 +271,7 @@ def test_sorting_with_different_categoricals():
     index = ["low", "med", "high", "low", "med", "high"]
     index = Categorical(index, categories=["low", "med", "high"], ordered=True)
     index = [["A", "A", "A", "B", "B", "B"], CategoricalIndex(index)]
-    index = MultiIndex.from_arrays(index, names=["group", None])
+    index = MultiIndex.from_arrays(index, names=["group", "dose"])
     expected = Series([2] * 6, index=index, name="dose")
     tm.assert_series_equal(result, expected)
 
