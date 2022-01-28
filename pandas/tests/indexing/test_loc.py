20ced9e4cd0131d6af2e8fa15798dad1b9dedc62
jbrockmendel <jbrockmendel@gmail.com>
2022-01-27 16:36:44 -0800

    BUG: parse_time_string with np.str_ obj (#45626)

--- pandas/tests/indexing/test_loc.py
+++ pandas/tests/indexing/test_loc.py
@@ -209,6 +209,13 @@ class TestLoc(Base):
 
 class TestLocBaseIndependent:
     # Tests for loc that do not depend on subclassing Base
+    def test_loc_npstr(self):
+        # GH#45580
+        df = DataFrame(index=date_range("2021", "2022"))
+        result = df.loc[np.array(["2021/6/1"])[0] :]
+        expected = df.iloc[151:]
+        tm.assert_frame_equal(result, expected)
+
     @pytest.mark.parametrize(
         "msg, key",
         [
