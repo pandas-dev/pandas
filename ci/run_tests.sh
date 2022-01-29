7e0662dc08ad2d87a55a189a35da004888b55a90
Matthew Roeschke <emailformattr@gmail.com>
2022-01-28 23:06:09 -0800

    Declare victory

--- ci/run_tests.sh
+++ ci/run_tests.sh
@@ -24,7 +24,7 @@ if [[ $(uname) == "Linux" && -z $DISPLAY ]]; then
     XVFB="xvfb-run "
 fi
 
-PYTEST_CMD="${XVFB}pytest -v -r fEs -m \"$PATTERN\" -n $PYTEST_WORKERS --dist=loadfile $TEST_ARGS $COVERAGE $PYTEST_TARGET"
+PYTEST_CMD="${XVFB}pytest -r fEs -m \"$PATTERN\" -n $PYTEST_WORKERS --dist=loadfile $TEST_ARGS $COVERAGE $PYTEST_TARGET"
 
 echo $PYTEST_CMD
 sh -c "$PYTEST_CMD"
