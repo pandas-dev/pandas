f0434a3a45f5199626147cd5a4d54da658c56b49
Compro Prasad <Compro-Prasad@users.noreply.github.com>
2022-01-31 22:01:36 +0530

    Fixes #45506 Catch overflow error when converting to datetime (#45532)

--- pandas/tests/series/methods/test_compare.py
+++ pandas/tests/series/methods/test_compare.py
@@ -114,3 +114,28 @@ def test_compare_unaligned_objects():
         ser1 = pd.Series([1, 2, 3])
         ser2 = pd.Series([1, 2, 3, 4])
         ser1.compare(ser2)
+
+
+def test_compare_datetime64_and_string():
+    # Issue https://github.com/pandas-dev/pandas/issues/45506
+    # Catch OverflowError when comparing datetime64 and string
+    data = [
+        {"a": "2015-07-01", "b": "08335394550"},
+        {"a": "2015-07-02", "b": "+49 (0) 0345 300033"},
+        {"a": "2015-07-03", "b": "+49(0)2598 04457"},
+        {"a": "2015-07-04", "b": "0741470003"},
+        {"a": "2015-07-05", "b": "04181 83668"},
+    ]
+    dtypes = {"a": "datetime64[ns]", "b": "string"}
+    df = pd.DataFrame(data=data).astype(dtypes)
+
+    result_eq1 = df["a"].eq(df["b"])
+    result_eq2 = df["a"] == df["b"]
+    result_neq = df["a"] != df["b"]
+
+    expected_eq = pd.Series([False] * 5)  # For .eq and ==
+    expected_neq = pd.Series([True] * 5)  # For !=
+
+    tm.assert_series_equal(result_eq1, expected_eq)
+    tm.assert_series_equal(result_eq2, expected_eq)
+    tm.assert_series_equal(result_neq, expected_neq)
