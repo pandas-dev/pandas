8eb356a219efeeb68cc47bd7d2f59efb553e3350
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2022-01-31 07:15:57 -0800

    Backport PR #45616: BUG: is_bool_dtype raises AttributeError when checking categorical Series (#45738)
    Co-authored-by: Robbie Palmer <8760191+Robbie-Palmer@users.noreply.github.com>

--- pandas/tests/dtypes/test_common.py
+++ pandas/tests/dtypes/test_common.py
@@ -562,12 +562,14 @@ def test_is_bool_dtype():
     assert not com.is_bool_dtype(int)
     assert not com.is_bool_dtype(str)
     assert not com.is_bool_dtype(pd.Series([1, 2]))
+    assert not com.is_bool_dtype(pd.Series(["a", "b"], dtype="category"))
     assert not com.is_bool_dtype(np.array(["a", "b"]))
     assert not com.is_bool_dtype(pd.Index(["a", "b"]))
     assert not com.is_bool_dtype("Int64")
 
     assert com.is_bool_dtype(bool)
     assert com.is_bool_dtype(np.bool_)
+    assert com.is_bool_dtype(pd.Series([True, False], dtype="category"))
     assert com.is_bool_dtype(np.array([True, False]))
     assert com.is_bool_dtype(pd.Index([True, False]))
 
