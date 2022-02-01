53405528052e1650e94b63f9c7d1a0540ec9fc04
roib20 <66280613+roib20@users.noreply.github.com>
2022-02-01 02:52:39 +0000

    Fixes from pre-commit [automated commit]

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -669,7 +669,7 @@ class Block(PandasObject):
         #  go through replace_list
 
         values = self.values
-        value = self._standardize_fill_value(value) # GH#45725
+        value = self._standardize_fill_value(value)  # GH#45725
 
         if isinstance(values, Categorical):
             # TODO: avoid special-casing
