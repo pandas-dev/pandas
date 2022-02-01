5c63b694e14d4a55d99245c09a65511e56a05812
ChrisAlbertsen <chrisdalbertsen@gmail.com>
2022-02-01 21:39:40 +0100

    TST; add test for compatibility with numpy datetime64 (#41617)

--- pandas/tests/tseries/frequencies/test_freq_code.py
+++ pandas/tests/tseries/frequencies/test_freq_code.py
@@ -80,19 +80,18 @@ def test_cat(args):
     with pytest.raises(ValueError, match=msg):
         to_offset(str(args[0]) + args[1])
 
+
 @pytest.mark.parametrize(
-    'freqstr,expected',
+    "freqstr,expected", 
     [   
-        ('1H','2021-01-01T09:00:00'),
-        ('1D','2021-01-01T08:00:00'),
-        ('1W','2021-01-03T08:00:00'),
-        ('1M','2021-01-31T08:00:00'),
-        ('1Y','2021-12-31T08:00:00')
+        ("1H","2021-01-01T09:00:00"), 
+        ("1D","2021-01-01T08:00:00"), 
+        ("1W","2021-01-03T08:00:00"), 
+        ("1M","2021-01-31T08:00:00"), 
+        ("1Y","2021-12-31T08:00:00")
     ]
 )
-def test_compatibility(freqstr,expected):
-    ts_np = datetime64('2021-01-01T08:00:00.00')
+def test_compatibility(freqstr,expected): 
+    ts_np = datetime64("2021-01-01T08:00:00.00")
     do = to_offset(freqstr)
-    
     assert ts_np + do == datetime64(expected)
-    
