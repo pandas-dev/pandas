615f032621138854bff649fd6a1af137a1c5e875
Simon Hawkins <simonjayhawkins@gmail.com>
2022-02-01 12:31:06 +0000

    Backport PR #45679: BUG: Do not error on other dbapi2 connections (#45754)
    Co-authored-by: Adam Bowden <23028875+abowden1989@users.noreply.github.com>

--- pandas/io/sql.py
+++ pandas/io/sql.py
@@ -747,12 +747,15 @@ def pandasSQL_builder(con, schema: str | None = None):
     if isinstance(con, sqlite3.Connection) or con is None:
         return SQLiteDatabase(con)
 
-    sqlalchemy = import_optional_dependency("sqlalchemy")
+    sqlalchemy = import_optional_dependency("sqlalchemy", errors="ignore")
 
     if isinstance(con, str):
-        con = sqlalchemy.create_engine(con)
+        if sqlalchemy is None:
+            raise ImportError("Using URI string without sqlalchemy installed.")
+        else:
+            con = sqlalchemy.create_engine(con)
 
-    if isinstance(con, sqlalchemy.engine.Connectable):
+    if sqlalchemy is not None and isinstance(con, sqlalchemy.engine.Connectable):
         return SQLDatabase(con, schema=schema)
 
     warnings.warn(
