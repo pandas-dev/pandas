99159d0055138ee2790c5b655746c44382d18cc2
unknown <cheanwei.KCW@gmail.com>
2022-02-03 20:53:53 +0800

    add

--- pandas/tests/series/methods/test_replace.py
+++ pandas/tests/series/methods/test_replace.py
@@ -654,12 +654,11 @@ class TestSeriesReplace:
 
     def test_replace_different_int_types(self):
         # GH#45311
-        labs = pd.Series([1, 1, 1, 0, 0, 2, 2, 2]).astype(np.int8)
+        labs = pd.Series([1, 1, 1, 0, 0, 2, 2, 2]).astype(np.int32)
 
-        maps = pd.Series([np.int8(0), np.int8(2), np.int8(1)])
+        maps = pd.Series([np.int64(0), np.int64(2), np.int64(1)])
         map_dict = {old: new for (old, new) in zip(maps.values, maps.index)}
 
         result = labs.replace(map_dict)
         expected = labs.replace({0: 0, 2: 1, 1: 2})
-
         tm.assert_series_equal(result, expected)
