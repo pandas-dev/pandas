7a9f0c6b4470fb54006577bbbbc088871a122ab9
jbrockmendel <jbrockmendel@gmail.com>
2022-02-02 18:57:15 -0800

    REF: array_to_timedelta64 handle 2D (#45788)

--- pandas/tests/tslibs/test_timedeltas.py
+++ pandas/tests/tslibs/test_timedeltas.py
@@ -3,7 +3,10 @@ import re
 import numpy as np
 import pytest
 
-from pandas._libs.tslibs.timedeltas import delta_to_nanoseconds
+from pandas._libs.tslibs.timedeltas import (
+    array_to_timedelta64,
+    delta_to_nanoseconds,
+)
 
 from pandas import (
     Timedelta,
@@ -63,3 +66,20 @@ def test_kwarg_assertion(kwargs):
 
     with pytest.raises(ValueError, match=re.escape(err_message)):
         Timedelta(**kwargs)
+
+
+class TestArrayToTimedelta64:
+    def test_array_to_timedelta64_string_with_unit_2d_raises(self):
+        # check the 'unit is not None and errors != "coerce"' path
+        #  in array_to_timedelta64 raises correctly with 2D values
+        values = np.array([["1", 2], [3, "4"]], dtype=object)
+        with pytest.raises(ValueError, match="unit must not be specified"):
+            array_to_timedelta64(values, unit="s")
+
+    def test_array_to_timedelta64_non_object_raises(self):
+        # check we raise, not segfault
+        values = np.arange(5)
+
+        msg = "'values' must have object dtype"
+        with pytest.raises(TypeError, match=msg):
+            array_to_timedelta64(values)
