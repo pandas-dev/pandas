e6b749ed7bcd013111b4590c218ad6219926577f
Khor Chean Wei <cheanwei.KCW@gmail.com>
2022-02-06 03:01:00 +0800

    Test case for concat inconsistent multiIndex (#45822)

--- pandas/tests/reshape/concat/test_index.py
+++ pandas/tests/reshape/concat/test_index.py
@@ -306,3 +306,20 @@ class TestMultiIndexConcat:
             result_df = concat((df1, df2), axis=1)
 
         tm.assert_frame_equal(expected_df, result_df)
+
+    def test_concat_multiindex_(self):
+        # GitHub #44786
+        df = DataFrame({"col": ["a", "b", "c"]}, index=["1", "2", "2"])
+        df = concat([df], keys=["X"])
+
+        iterables = [["X"], ["1", "2", "2"]]
+        result_index = df.index
+        expected_index = MultiIndex.from_product(iterables)
+
+        tm.assert_index_equal(result_index, expected_index)
+
+        result_df = df
+        expected_df = DataFrame(
+            {"col": ["a", "b", "c"]}, index=MultiIndex.from_product(iterables)
+        )
+        tm.assert_frame_equal(result_df, expected_df)
