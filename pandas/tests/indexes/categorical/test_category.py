7c944f4b91a6ce7130f79f3567e7029414f454fd
jbrockmendel <jbrockmendel@gmail.com>
2022-02-05 11:02:59 -0800

    BUG: Categorical.view not accepting integer dtypes GH#25464 (#45300)
    Co-authored-by: Jeff Reback <jeff@reback.net>

--- pandas/tests/indexes/categorical/test_category.py
+++ pandas/tests/indexes/categorical/test_category.py
@@ -290,6 +290,24 @@ class TestCategoricalIndex(Base):
 class TestCategoricalIndex2:
     # Tests that are not overriding a test in Base
 
+    def test_view_i8(self):
+        # GH#25464
+        ci = tm.makeCategoricalIndex(100)
+        msg = "When changing to a larger dtype, its size must be a divisor"
+        with pytest.raises(ValueError, match=msg):
+            ci.view("i8")
+        with pytest.raises(ValueError, match=msg):
+            ci._data.view("i8")
+
+        ci = ci[:-4]  # length divisible by 8
+
+        res = ci.view("i8")
+        expected = ci._data.codes.view("i8")
+        tm.assert_numpy_array_equal(res, expected)
+
+        cat = ci._data
+        tm.assert_numpy_array_equal(cat.view("i8"), expected)
+
     @pytest.mark.parametrize(
         "dtype, engine_type",
         [
