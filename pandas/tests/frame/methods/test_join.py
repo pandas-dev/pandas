1037cb9dcbb6d762b98ca4d309c7aef4f0c36461
Zhengfei Wang <wangzhengfei.chn@outlook.com>
2022-02-10 10:25:00 +0800

    Update test_join.py
    add msg for pytest.raises for Code Check

--- pandas/tests/frame/methods/test_join.py
+++ pandas/tests/frame/methods/test_join.py
@@ -375,5 +375,6 @@ class TestDataFrameJoin:
         # https://github.com/pandas-dev/pandas/issues/36263
         df1 = DataFrame(np.random.randn(5), index=[0, 1, 2, 3, 3], columns=["a"])
         df2 = DataFrame(np.random.randn(5), index=[0, 1, 2, 2, 4], columns=["b"])
-        with pytest.raises(InvalidIndexError):
+        msg = "Reindexing only valid with uniquely valued Index objects"
+        with pytest.raises(InvalidIndexError, match=msg):
             pd.concat([df1, df2], axis=1)
