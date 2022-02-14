d2be88bb06e77eb1c092a49750072c12844a0e3b
Khor Chean Wei <cheanwei.KCW@gmail.com>
2022-02-14 19:59:24 +0800

    Add test

--- pandas/tests/groupby/test_indexing.py
+++ pandas/tests/groupby/test_indexing.py
@@ -314,3 +314,17 @@ def test_groupby_duplicated_columns(func):
         [[1.5, 3.0, 1.5]], columns=["A", "B", "A"], index=pd.Index(["G"], name="C")
     )
     tm.assert_frame_equal(result, expected)
+    
+def test_dataframe_groupby_incorrect_with_multiindex_make_df_from_data():
+    # GH#32492
+    df = pd.DataFrame(
+        data={
+            "A": ["a1", "a2", None],
+            "B": ["b1", "b2", "b1"],
+            "val": [1, 2, 3],
+        }
+    )
+    grps = df.groupby(by=["A", "B"])
+
+    with pytest.raises(KeyError):
+        grps.get_group(("a2", "b1"))
