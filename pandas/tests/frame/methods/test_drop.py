36f7e3db720bcf0503ad1262fa24228f241ff6fa
phofl <patrick_hoefler@gmx.net>
2022-02-14 20:42:41 +0100

    Add NA

--- pandas/tests/frame/methods/test_drop.py
+++ pandas/tests/frame/methods/test_drop.py
@@ -542,10 +542,10 @@ class TestDataFrameDrop:
     def test_drop_index_ea_dtype(self, any_numeric_ea_dtype, idx, level):
         # GH#45860
         df = DataFrame(
-            {"a": [1, 2, 2], "b": 100}, dtype=any_numeric_ea_dtype
+            {"a": [1, 2, 2, pd.NA], "b": 100}, dtype=any_numeric_ea_dtype
         ).set_index(idx)
         result = df.drop(Index([2]), level=level)
         expected = DataFrame(
-            {"a": [1], "b": 100}, dtype=any_numeric_ea_dtype
+            {"a": [1, pd.NA], "b": 100}, dtype=any_numeric_ea_dtype
         ).set_index(idx)
         tm.assert_frame_equal(result, expected)
