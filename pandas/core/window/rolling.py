42a5cbc76d6841d543ee54e053cbaef4a75caee7
Matthew Roeschke <emailformattr@gmail.com>
2022-02-20 01:49:26 -0800

    Revert "BUG: groupby().rolling(freq) with monotonic dates within groups (#46065)" (#46078)
    This reverts commit 18b6b5aec0d00e67811a72067ef6f049ad3f9555.

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -837,7 +837,7 @@ class BaseWindowGroupby(BaseWindow):
             subset = self.obj.set_index(self._on)
         return super()._gotitem(key, ndim, subset=subset)
 
-    def _validate_datetimelike_monotonic(self):
+    def _validate_monotonic(self):
         """
         Validate that "on" is monotonic; already validated at a higher level.
         """
@@ -1687,7 +1687,7 @@ class Rolling(RollingAndExpandingMixin):
             or isinstance(self._on, (DatetimeIndex, TimedeltaIndex, PeriodIndex))
         ) and isinstance(self.window, (str, BaseOffset, timedelta)):
 
-            self._validate_datetimelike_monotonic()
+            self._validate_monotonic()
 
             # this will raise ValueError on non-fixed freqs
             try:
@@ -1712,13 +1712,18 @@ class Rolling(RollingAndExpandingMixin):
         elif not is_integer(self.window) or self.window < 0:
             raise ValueError("window must be an integer 0 or greater")
 
-    def _validate_datetimelike_monotonic(self):
+    def _validate_monotonic(self):
         """
         Validate monotonic (increasing or decreasing).
         """
         if not (self._on.is_monotonic_increasing or self._on.is_monotonic_decreasing):
-            on = "index" if self.on is None else self.on
-            raise ValueError(f"{on} must be monotonic.")
+            self._raise_monotonic_error()
+
+    def _raise_monotonic_error(self):
+        formatted = self.on
+        if self.on is None:
+            formatted = "index"
+        raise ValueError(f"{formatted} must be monotonic")
 
     @doc(
         _shared_docs["aggregate"],
@@ -2626,17 +2631,12 @@ class RollingGroupby(BaseWindowGroupby, Rolling):
         )
         return window_indexer
 
-    def _validate_datetimelike_monotonic(self):
+    def _validate_monotonic(self):
         """
-        Validate that each group in self._on is monotonic
+        Validate that on is monotonic;
         """
-        # GH 46061
-        on = "index" if self.on is None else self.on
-        if self._on.hasnans:
-            raise ValueError(f"{on} must not have any NaT values.")
-        for group_indices in self._grouper.indices.values():
-            group_on = self._on.take(group_indices)
-            if not (
-                group_on.is_monotonic_increasing or group_on.is_monotonic_decreasing
-            ):
-                raise ValueError(f"Each group within {on} must be monotonic.")
+        if (
+            not (self._on.is_monotonic_increasing or self._on.is_monotonic_decreasing)
+            or self._on.hasnans
+        ):
+            self._raise_monotonic_error()
