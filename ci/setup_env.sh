3c5bb13498697f2ab3c89dabfe60f6e578ee2676
Jonas Haag <jonas@lophus.org>
2022-02-21 22:40:21 +0100

    Pin setuptools < 60

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -52,7 +52,7 @@ echo "update conda"
 conda config --set ssl_verify false
 conda config --set quiet true --set always_yes true --set changeps1 false
 # TODO: GH#44980 https://github.com/pypa/setuptools/issues/2941
-conda install -y -c conda-forge -n base 'mamba>=0.21.2' pip 'setuptools<60'
+conda install -y -c conda-forge -n base 'mamba>=0.21.2' pip
 
 echo "conda info -a"
 conda info -a
@@ -65,9 +65,10 @@ echo
 mamba env remove -n pandas-dev
 echo "mamba env update --file=${ENV_FILE}"
 # See https://github.com/mamba-org/mamba/issues/633
-# TODO: GH#44980 https://github.com/pypa/setuptools/issues/2941
-mamba create -q -n pandas-dev 'setuptools<60'
+mamba create -q -n pandas-dev
 time mamba env update -n pandas-dev --file="${ENV_FILE}"
+# TODO: GH#44980 https://github.com/pypa/setuptools/issues/2941
+mamba install -n pandas-dev 'setuptools<60'
 
 echo "conda list -n pandas-dev"
 conda list -n pandas-dev
