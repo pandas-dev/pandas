5aeb8b8e7a2b4946033b1a9c0f2d568f75fd0c9c
JHM Darbyshire (iMac) <attack68@users.noreply.github.com>
2022-02-23 21:08:10 +0100

    html test

--- pandas/tests/io/formats/style/test_html.py
+++ pandas/tests/io/formats/style/test_html.py
@@ -804,3 +804,29 @@ def test_multiple_rendered_links():
     for link in links:
         assert href.format(link) in result
     assert href.format("text") not in result
+
+
+def test_concat(styler):
+    other = styler.data.agg(["mean"]).style
+    styler.concat(other).set_uuid("X")
+    result = styler.to_html()
+    expected = dedent(
+        """\
+          <tbody>
+            <tr>
+              <th id="T_X_level0_row0" class="row_heading level0 row0" >a</th>
+              <td id="T_X_row0_col0" class="data row0 col0" >2.610000</td>
+            </tr>
+            <tr>
+              <th id="T_X_level0_row1" class="row_heading level0 row1" >b</th>
+              <td id="T_X_row1_col0" class="data row1 col0" >2.690000</td>
+            </tr>
+            <tr>
+              <th id="T_X_level0_foot0" class="foot_heading level0 foot0" >mean</th>
+              <td id="T_X_foot0_col0" class="foot foot0 col0" >2.650000</td>
+            </tr>
+          </tbody>
+        </table>
+        """
+    )
+    assert expected in result
