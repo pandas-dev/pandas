9a6be6e8af0b609e9e84cba3442154a444bc5421
Matthew Roeschke <emailformattr@gmail.com>
2022-02-22 20:15:08 -0800

    Do it in setup_env.sh

--- ci/setup_env.sh
+++ ci/setup_env.sh
@@ -66,6 +66,14 @@ echo
 echo "mamba env create -q --file=${ENV_FILE}"
 time mamba env create -q --file="${ENV_FILE}"
 
+# From pyarrow on MacOS
+# ImportError: 2): Library not loaded: @rpath/libssl.1.1.dylib
+# Referenced from: /Users/runner/miniconda3/envs/pandas-dev/lib/libthrift.0.13.0.dylib
+# Reason: image not found
+if [[ "$(uname)" == 'Darwin' ]]; then
+    echo "Update libthrift for pyarrow on MacOS"
+    conda install -n pandas-dev -c conda-forge --no-update-deps libthrift=0.15.0
+fi
 
 if [[ "$BITS32" == "yes" ]]; then
     # activate 32-bit compiler
