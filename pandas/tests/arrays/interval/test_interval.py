fa8c3338239d57f9ba70128ff882a47b04326197
Irv Lustig <irv@princeton.com>
2022-02-25 15:22:59 -0500

    Adding tests for IntervalArray.unique() for DateTimeArrays with time zones

--- pandas/tests/arrays/interval/test_interval.py
+++ pandas/tests/arrays/interval/test_interval.py
@@ -376,3 +376,18 @@ def test_arrow_table_roundtrip_without_metadata(breaks):
     result = table.to_pandas()
     assert isinstance(result["a"].dtype, pd.IntervalDtype)
     tm.assert_frame_equal(result, df)
+
+
+def test_tz_unique():
+    # GH 46128
+    dti1 = date_range("2016-01-01", periods=3)
+    ii1 = IntervalIndex.from_breaks(dti1)
+    ser1 = pd.Series(ii1)
+    uni1 = ser1.unique()
+
+    dti2 = date_range("2016-01-01", periods=3, tz="US/Eastern")
+    ii2 = IntervalIndex.from_breaks(dti2)
+    ser2 = pd.Series(ii2)
+    uni2 = ser2.unique()
+
+    assert uni1.dtype != uni2.dtype
