a517e3e6f2dcd4f253c25056f8d703f7469fdad0
Irv Lustig <irv@princeton.com>
2022-02-25 21:02:49 -0500

    move tests to series/methods/test_unique

--- pandas/tests/series/methods/test_unique.py
+++ pandas/tests/series/methods/test_unique.py
@@ -2,7 +2,9 @@ import numpy as np
 
 from pandas import (
     Categorical,
+    IntervalIndex,
     Series,
+    date_range,
 )
 import pandas._testing as tm
 
@@ -50,3 +52,19 @@ class TestUnique:
         ser = Series(cat)
         result = ser.unique()
         tm.assert_categorical_equal(result, cat)
+
+    def test_tz_unique(self):
+        # GH 46128
+        dti1 = date_range("2016-01-01", periods=3)
+        ii1 = IntervalIndex.from_breaks(dti1)
+        ser1 = Series(ii1)
+        uni1 = ser1.unique()
+        tm.assert_interval_array_equal(ser1.array, uni1)
+
+        dti2 = date_range("2016-01-01", periods=3, tz="US/Eastern")
+        ii2 = IntervalIndex.from_breaks(dti2)
+        ser2 = Series(ii2)
+        uni2 = ser2.unique()
+        tm.assert_interval_array_equal(ser2.array, uni2)
+
+        assert uni1.dtype != uni2.dtype
