f2701a245e958681c844a2880825c443b977d249
JHM Darbyshire (MBP) <attack68@users.noreply.github.com>
2022-02-26 21:09:54 +0100

    mypy fix

--- pandas/io/formats/style_render.py
+++ pandas/io/formats/style_render.py
@@ -165,14 +165,13 @@ class StylerRenderer:
         if self.concatenated is not None:
             self.concatenated.hide_index_ = self.hide_index_
             self.concatenated.hidden_columns = self.hidden_columns
-            self.concatenated.set_table_styles(
-                css_class_names={
-                    "data": f"{self.css['foot']}_{self.css['data']}",
-                    "row_heading": f"{self.css['foot']}_{self.css['row_heading']}",
-                    "row": f"{self.css['foot']}_{self.css['row']}",
-                    "foot": self.css["foot"],
-                }
-            )
+            self.concatenated.css = {
+                **self.css,
+                "data": f"{self.css['foot']}_{self.css['data']}",
+                "row_heading": f"{self.css['foot']}_{self.css['row_heading']}",
+                "row": f"{self.css['foot']}_{self.css['row']}",
+                "foot": self.css["foot"],
+            }
             dx, _ = self.concatenated._render(
                 sparse_index, sparse_columns, max_rows, max_cols, blank
             )
