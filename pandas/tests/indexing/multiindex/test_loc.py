ff1fefc6652472f028c5d91bfed10e9da346d20a
Roger Murray <96553566+rogimus@users.noreply.github.com>
2022-03-01 00:02:53 -0500

    Fix spacing

--- pandas/tests/indexing/multiindex/test_loc.py
+++ pandas/tests/indexing/multiindex/test_loc.py
@@ -402,7 +402,8 @@ class TestMultiIndexLoc:
         result = df.loc["2011-01-01":"2011-01-02"]
         tm.assert_frame_equal(result, expected)
 
-    def test_loc_keep_second_level_index(self):
+        
+    def test_loc_no_second_level_index(self):
         # GH#43599
         df = DataFrame(
             index=MultiIndex.from_product(
@@ -416,9 +417,8 @@ class TestMultiIndexLoc:
             columns=['Val']
         )
         tm.assert_frame_equal(res, expected)
-
-
-
+        
+        
 @pytest.mark.parametrize(
     "indexer, pos",
     [
