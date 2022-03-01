6a23e70bfebb30d68c3417af64b2c1c8df0099f1
Roger Murray <96553566+rogimus@users.noreply.github.com>
2022-03-01 00:10:20 -0500

    editing spacing for PEP 8

--- pandas/tests/indexing/multiindex/test_loc.py
+++ pandas/tests/indexing/multiindex/test_loc.py
@@ -402,7 +402,7 @@ class TestMultiIndexLoc:
         result = df.loc["2011-01-01":"2011-01-02"]
         tm.assert_frame_equal(result, expected)
 
-        
+
     def test_loc_no_second_level_index(self):
         # GH#43599
         df = DataFrame(
@@ -410,15 +410,15 @@ class TestMultiIndexLoc:
                 [list('ab'), list('cd'), list('e')]),
             columns=['Val']
         )
-        res = df.loc[np.s_[:,'c',:]]
+        res = df.loc[np.s_[:, 'c', :]]
         expected = DataFrame(
             index=pd.MultiIndex.from_product(
                 [list('ab'), list('e')]),
             columns=['Val']
         )
         tm.assert_frame_equal(res, expected)
-        
-        
+
+
 @pytest.mark.parametrize(
     "indexer, pos",
     [
