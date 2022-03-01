b3a97451593a6972d8c9060302dc5636f720b3c5
Roger Murray <96553566+rogimus@users.noreply.github.com>
2022-03-01 00:35:17 -0500

    fixing precommit checks

--- pandas/tests/indexing/multiindex/test_loc.py
+++ pandas/tests/indexing/multiindex/test_loc.py
@@ -405,15 +405,12 @@ class TestMultiIndexLoc:
     def test_loc_no_second_level_index(self):
         # GH#43599
         df = DataFrame(
-            index=MultiIndex.from_product(
-                [list('ab'), list('cd'), list('e')]),
-            columns=['Val']
+            index=MultiIndex.from_product([list("ab"), list("cd"), list("e")]), 
+            columns=["Val"],
         )
-        res = df.loc[np.s_[:, 'c', :]]
+        res = df.loc[np.s_[:, "c", :]]
         expected = DataFrame(
-            index=pd.MultiIndex.from_product(
-                [list('ab'), list('e')]),
-            columns=['Val']
+            index=MultiIndex.from_product([list("ab"), list("e")]), columns=["Val"]
         )
         tm.assert_frame_equal(res, expected)
 
