24aec6f4a0913219a516300183a9e6ec69c224cd
Torsten Wörtwein <twoertwein@gmail.com>
2022-03-04 19:07:09 -0500

    comment for cast

--- pandas/core/arrays/boolean.py
+++ pandas/core/arrays/boolean.py
@@ -205,6 +205,8 @@ def coerce_to_array(
         if inferred_dtype not in ("boolean", "empty") + integer_like:
             raise TypeError("Need to pass bool-like values")
 
+        # mypy does not to narrow the type of mask_values to npt.NDArray[np.bool_]
+        # within this branch, it assumes it can also be None
         mask_values = cast("npt.NDArray[np.bool_]", isna(values_object))
         values = np.zeros(len(values), dtype=bool)
         values[~mask_values] = values_object[~mask_values].astype(bool)
