e64dee26d253b712ecc0c6e0c3adf4a2de1f8e62
Yuval <yuval,jacoby1@mail.huji.ac.il>
2022-03-07 22:52:20 +0200

    TST: added test for rolling with centering=True and axis=1 (#46135)

--- pandas/tests/window/test_rolling.py
+++ pandas/tests/window/test_rolling.py
@@ -1781,3 +1781,17 @@ def test_step_not_integer_raises():
 def test_step_not_positive_raises():
     with pytest.raises(ValueError, match="step must be >= 0"):
         DataFrame(range(2)).rolling(1, step=-1)
+
+
+def test_rolling_center_axis_1():
+    df = DataFrame({'a': [1, 1, 0, 0, 0, 1],
+                    'b': [1, 0, 0, 1, 0, 0],
+                    'c': [1, 0, 0, 1, 0, 1]})
+
+    result = df.rolling(window=3, axis=1, win_type="boxcar", center=True).sum()
+
+    expected = DataFrame({'a': [None, None, None, None, None, None],
+                          'b': [3, 1, 0, 2, 0, 2],
+                          'c': [None, None, None, None, None, None]})
+
+    tm.assert_frame_equal(result, expected, check_dtype=False)
