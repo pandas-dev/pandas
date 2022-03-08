39b28392ae715891a4efe7444c54479866eca04b
chean.wei.khor <cheanwei.KCW@gmail.com>
2022-03-08 22:23:22 +0800

    add

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -1076,6 +1076,7 @@ class Index(IndexOpsMixin, PandasObject):
         if isinstance(values, ExtensionArray):
             with rewrite_exception(type(values).__name__, type(self).__name__):
                 new_values = values.astype(dtype, copy=copy)
+
         elif is_float_dtype(self.dtype) and needs_i8_conversion(dtype):
             # NB: this must come before the ExtensionDtype check below
             # TODO: this differs from Series behavior; can/should we align them?
