ca3a6ccecad8523f30138093aa7abbb5ea7b7f18
Sylvain MARIE <sylvain.marie@se.com>
2022-03-10 16:46:53 +0100

    Fixed `CSVFormatter` (used in `to_csv`) so that `DatetimeIndex` or `PeriodIndex` are now formatted using the same routine than columns of the same type. In particular `strftime` is not used anymore when possible, which is therefore faster. Added two tests.

--- pandas/io/formats/csvs.py
+++ pandas/io/formats/csvs.py
@@ -182,14 +182,7 @@ class CSVFormatter:
     @cache_readonly
     def data_index(self) -> Index:
         data_index = self.obj.index
-        if (
-            isinstance(data_index, (ABCDatetimeIndex, ABCPeriodIndex))
-            and self.date_format is not None
-        ):
-            data_index = Index(
-                [x.strftime(self.date_format) if notna(x) else "" for x in data_index]
-            )
-        elif isinstance(data_index, ABCMultiIndex):
+        if isinstance(data_index, ABCMultiIndex):
             data_index = data_index.remove_unused_levels()
         return data_index
 
