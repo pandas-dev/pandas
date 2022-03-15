c1cbc72f5387bb0bd640eed794ebce4aa7302ee9
Brock <jbrockmendel@gmail.com>
2022-03-15 16:36:31 -0700

    GH ref

--- pandas/tests/groupby/test_function.py
+++ pandas/tests/groupby/test_function.py
@@ -832,7 +832,7 @@ def test_cummax(dtypes_for_minmax):
 
 def test_cummax_i8_at_implementation_bound():
     # the minimum value used to be treated as NPY_NAT+1 instead of NPY_NAT
-    #  for int64 dtype.
+    #  for int64 dtype GH#46382
     ser = Series([pd.NaT.value + n for n in range(5)])
     df = DataFrame({"A": 1, "B": ser, "C": ser.view("M8[ns]")})
     gb = df.groupby("A")
