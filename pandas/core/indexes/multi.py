8f2d3a51177a9e3191921416a4b45fbd0cbf858c
chean.wei.khor <cheanwei.KCW@gmail.com>
2022-03-16 01:00:54 +0800

    pre-commit

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -490,7 +490,7 @@ class MultiIndex(Index):
 
         if all(isinstance(e, tuple) for e in arrays):
             if not all(arrays):
-                codes = list(i for i in range(len(arrays)))
+                codes = [list(arrays)]
                 _dtype_obj = np.dtype("object")
                 subarr = com.asarray_tuplesafe(arrays, dtype=_dtype_obj)
                 levels = [Index(subarr)]
