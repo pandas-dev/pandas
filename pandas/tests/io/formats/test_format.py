6eecc1bf51d883ce24bd8a72659f03845f65a2aa
Sylvain MARIE <sylvain.marie@se.com>
2022-03-16 17:37:55 +0100

    Discarding single-letter variable, as per code review

--- pandas/tests/io/formats/test_format.py
+++ pandas/tests/io/formats/test_format.py
@@ -3215,14 +3215,14 @@ class TestPeriodIndexFormat:
         # Converting to a period looses the timezone information
         # Since tz is currently set as utc, we'll see 2012
         with tm.assert_produces_warning(UserWarning, match="will drop timezone"):
-            p = dt.to_period(freq="H")
-        assert p.format()[0] == "2012-12-31 23:00"
+            per = dt.to_period(freq="H")
+        assert per.format()[0] == "2012-12-31 23:00"
 
         # If tz is currently set as paris before conversion, we'll see 2013
         dt = dt.tz_convert("Europe/Paris")
         with tm.assert_produces_warning(UserWarning, match="will drop timezone"):
-            p = dt.to_period(freq="H")
-        assert p.format()[0] == "2013-01-01 00:00"
+            per = dt.to_period(freq="H")
+        assert per.format()[0] == "2013-01-01 00:00"
 
 
 class TestDatetimeIndexFormat:
