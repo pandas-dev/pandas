12d40190c8fffac7b41c88a04f0061abe5dbfc38
jbrockmendel <jbrockmendel@gmail.com>
2022-03-16 19:03:30 -0700

    BUG: Nullable _get_common_dtype match non-nullable behavior (#46379)
    * BUG: Nullable _get_common_dtype match non-nullable behavior
    
    * Whatsnew

--- pandas/core/arrays/boolean.py
+++ pandas/core/arrays/boolean.py
@@ -137,18 +137,6 @@ class BooleanDtype(BaseMaskedDtype):
         else:
             return BooleanArray._concat_same_type(results)
 
-    def _get_common_dtype(self, dtypes: list[DtypeObj]) -> DtypeObj | None:
-        # Handle only boolean + np.bool_ -> boolean, since other cases like
-        # Int64 + boolean -> Int64 will be handled by the other type
-        if all(
-            isinstance(t, BooleanDtype)
-            or (isinstance(t, np.dtype) and (np.issubdtype(t, np.bool_)))
-            for t in dtypes
-        ):
-            return BooleanDtype()
-        else:
-            return None
-
 
 def coerce_to_array(
     values, mask=None, copy: bool = False
