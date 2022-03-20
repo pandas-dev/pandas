db33bea9eed68c046bbc96cfbe3c635178ff8dd4
Luke Manley <lukemanley@gmail.com>
2022-03-20 09:43:06 -0400

    mypy

--- pandas/core/arrays/_mixins.py
+++ pandas/core/arrays/_mixins.py
@@ -675,7 +675,7 @@ class ArrowExtensionArray(ExtensionArray):
         if isinstance(key, slice):
             indices = np.arange(n)[key]
         elif is_integer(key):
-            indices = np.arange(n)[[key]]
+            indices = np.arange(n)[[key]]  # type: ignore[index]
         elif is_bool_dtype(key):
             key = np.asarray(key)
             if len(key) != n:
