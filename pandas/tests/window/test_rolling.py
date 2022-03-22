d627560b7acf0ba7c1ecd10ee1211cf61223f619
jyuv <45140931+jyuv@users.noreply.github.com>
2022-03-22 22:01:26 +0000

    Fixes from pre-commit [automated commit]

--- pandas/tests/window/test_rolling.py
+++ pandas/tests/window/test_rolling.py
@@ -1784,14 +1784,14 @@ def test_step_not_positive_raises():
 
 
 def test_rolling_center_axis_1():
-    df = DataFrame({'a': [1, 1, 0, 0, 0, 1],
-                    'b': [1, 0, 0, 1, 0, 0],
-                    'c': [1, 0, 0, 1, 0, 1]})
+    df = DataFrame(
+        {"a": [1, 1, 0, 0, 0, 1], "b": [1, 0, 0, 1, 0, 0], "c": [1, 0, 0, 1, 0, 1]}
+    )
 
     result = df.rolling(window=3, axis=1, win_type="boxcar", center=True).sum()
 
-    expected = DataFrame({'a': [np.nan] * 6,
-                          'b': [3.0, 1.0, 0.0, 2.0, 0.0, 2.0],
-                          'c': [np.nan] * 6})
+    expected = DataFrame(
+        {"a": [np.nan] * 6, "b": [3.0, 1.0, 0.0, 2.0, 0.0, 2.0], "c": [np.nan] * 6}
+    )
 
     tm.assert_frame_equal(result, expected, check_dtype=True)
