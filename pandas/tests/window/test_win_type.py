63e116e9124e3d341375fe43d87232d2a5674400
jyuv <45140931+jyuv@users.noreply.github.com>
2022-03-23 09:58:11 +0000

    Fixes from pre-commit [automated commit]

--- pandas/tests/window/test_win_type.py
+++ pandas/tests/window/test_win_type.py
@@ -680,14 +680,14 @@ def test_cmov_window_special_linear_range(win_types_special, step):
 
 @td.skip_if_no_scipy
 def test_rolling_center_axis_1():
-    df = DataFrame({'a': [1, 1, 0, 0, 0, 1],
-                    'b': [1, 0, 0, 1, 0, 0],
-                    'c': [1, 0, 0, 1, 0, 1]})
+    df = DataFrame(
+        {"a": [1, 1, 0, 0, 0, 1], "b": [1, 0, 0, 1, 0, 0], "c": [1, 0, 0, 1, 0, 1]}
+    )
 
     result = df.rolling(window=3, axis=1, win_type="boxcar", center=True).sum()
 
-    expected = DataFrame({'a': [np.nan] * 6,
-                          'b': [3.0, 1.0, 0.0, 2.0, 0.0, 2.0],
-                          'c': [np.nan] * 6})
+    expected = DataFrame(
+        {"a": [np.nan] * 6, "b": [3.0, 1.0, 0.0, 2.0, 0.0, 2.0], "c": [np.nan] * 6}
+    )
 
     tm.assert_frame_equal(result, expected, check_dtype=True)
