f7c2bdbf7e44340ee9b1066abbb7e493669b0b94
jbrockmendel <jbrockmendel@gmail.com>
2022-03-22 17:39:47 -0700

    DEPR: Timedelta.delta (#46476)

--- pandas/_libs/tslibs/timedeltas.pyx
+++ pandas/_libs/tslibs/timedeltas.pyx
@@ -1014,6 +1014,12 @@ cdef class _Timedelta(timedelta):
         >>> td.delta
         42
         """
+        # Deprecated GH#46476
+        warnings.warn(
+            "Timedelta.delta is deprecated and will be removed in a future version.",
+            FutureWarning,
+            stacklevel=1,
+        )
         return self.value
 
     @property
