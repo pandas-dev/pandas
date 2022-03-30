47814b3123cca237f1574e1b0f8c1f2896c0fb94
Brock <jbrockmendel@gmail.com>
2022-03-29 21:06:27 -0700

    revert change that broke some builds

--- pandas/_libs/tslibs/strptime.pyx
+++ pandas/_libs/tslibs/strptime.pyx
@@ -78,8 +78,7 @@ def array_strptime(ndarray[object] values, object fmt, bint exact=True, errors='
         int week_of_year, week_of_year_start, parse_code, ordinal
         int iso_week, iso_year
         int64_t us, ns
-        object val, group_key, ampm, found
-        tzinfo timezone
+        object val, group_key, ampm, found, timezone
         dict found_key
         bint is_raise = errors=='raise'
         bint is_ignore = errors=='ignore'
