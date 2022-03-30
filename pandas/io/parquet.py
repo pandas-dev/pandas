e85dc94c828996b27cddc69dee99e0ba415ad92c
Philipp A <flying-sheep@web.de>
2022-03-30 20:53:14 +0200

    Close FastParquet file even on error (#46556)

--- pandas/io/parquet.py
+++ pandas/io/parquet.py
@@ -349,13 +349,12 @@ class FastParquetImpl(BaseImpl):
             )
             path = handles.handle
 
-        parquet_file = self.api.ParquetFile(path, **parquet_kwargs)
-
-        result = parquet_file.to_pandas(columns=columns, **kwargs)
-
-        if handles is not None:
-            handles.close()
-        return result
+        try:
+            parquet_file = self.api.ParquetFile(path, **parquet_kwargs)
+            return parquet_file.to_pandas(columns=columns, **kwargs)
+        finally:
+            if handles is not None:
+                handles.close()
 
 
 @doc(storage_options=_shared_docs["storage_options"])
