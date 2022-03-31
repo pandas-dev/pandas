efdb7c9a494a98c11baf4db51683323c9bc53e6b
Matthew Roeschke <emailformattr@gmail.com>
2022-03-31 11:01:23 -0700

    BUG: groupby().rolling(freq) with monotonic dates within groups #46065  (#46567)

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -2680,3 +2680,21 @@ class RollingGroupby(BaseWindowGroupby, Rolling):
             indexer_kwargs=indexer_kwargs,
         )
         return window_indexer
+
+    def _validate_datetimelike_monotonic(self):
+        """
+        Validate that each group in self._on is monotonic
+        """
+        # GH 46061
+        if self._on.hasnans:
+            self._raise_monotonic_error("values must not have NaT")
+        for group_indices in self._grouper.indices.values():
+            group_on = self._on.take(group_indices)
+            if not (
+                group_on.is_monotonic_increasing or group_on.is_monotonic_decreasing
+            ):
+                on = "index" if self.on is None else self.on
+                raise ValueError(
+                    f"Each group within {on} must be monotonic. "
+                    f"Sort the values in {on} first."
+                )
