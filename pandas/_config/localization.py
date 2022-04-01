0fcea6c8039818deb90c1beea37d10503a5578e4
Sylvain MARIE <sylvain.marie@se.com>
2022-04-01 16:20:01 +0200

    Fixed `tm.set_locale` context manager, it could error and leak when category LC_ALL was used. Fixed #46595

--- pandas/_config/localization.py
+++ pandas/_config/localization.py
@@ -39,7 +39,8 @@ def set_locale(
     particular locale, without globally setting the locale. This probably isn't
     thread-safe.
     """
-    current_locale = locale.getlocale()
+    # getlocale is wrong, see GH#46595
+    current_locale = locale.setlocale(lc_var)
 
     try:
         locale.setlocale(lc_var, new_locale)
