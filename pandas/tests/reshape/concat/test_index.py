f2fe384ebf5e7a3c088cd4271320653a6fd3e02b
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-04-04 15:21:31 -0500

    add warning message check

--- pandas/tests/reshape/concat/test_index.py
+++ pandas/tests/reshape/concat/test_index.py
@@ -332,13 +332,20 @@ class TestMultiIndexConcat:
         df2 = DataFrame({"name": [2]})
         df3 = DataFrame({"name": [3]})
         df_a = concat([df1, df2, df3], keys=["x", "y", "x"])
-        with tm.assert_produces_warning(PerformanceWarning):
+        # the warning is caused by indexing unsorted multi-index
+        with tm.assert_produces_warning(
+            PerformanceWarning,
+            match="indexing past lexsort depth"
+        ):
             out_a = df_a.loc[("x", 0), :]
 
         df_b = DataFrame(
             {"name": [1, 2, 3]}, index=Index([("x", 0), ("y", 0), ("x", 0)])
         )
-        with tm.assert_produces_warning(PerformanceWarning):
+        with tm.assert_produces_warning(
+            PerformanceWarning,
+            match="indexing past lexsort depth"
+        ):
             out_b = df_b.loc[("x", 0)]
 
         tm.assert_frame_equal(out_a, out_b)
@@ -347,7 +354,10 @@ class TestMultiIndexConcat:
         df2 = DataFrame({"name": ["a", "b"]})
         df3 = DataFrame({"name": ["c", "d"]})
         df_a = concat([df1, df2, df3], keys=["x", "y", "x"])
-        with tm.assert_produces_warning(PerformanceWarning):
+        with tm.assert_produces_warning(
+            PerformanceWarning,
+            match="indexing past lexsort depth"
+        ):
             out_a = df_a.loc[("x", 0), :]
 
         df_b = DataFrame(
@@ -358,7 +368,10 @@ class TestMultiIndexConcat:
             }
         ).set_index(["a", "b"])
         df_b.index.names = [None, None]
-        with tm.assert_produces_warning(PerformanceWarning):
+        with tm.assert_produces_warning(
+            PerformanceWarning,
+            match="indexing past lexsort depth"
+        ):
             out_b = df_b.loc[("x", 0), :]
 
         tm.assert_frame_equal(out_a, out_b)
