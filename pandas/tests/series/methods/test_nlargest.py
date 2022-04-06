cabe5e0f1a78910fe61c85dc10c02676bf9d4cb5
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-04-06 01:31:30 -0500

    fix format

--- pandas/tests/series/methods/test_nlargest.py
+++ pandas/tests/series/methods/test_nlargest.py
@@ -236,7 +236,7 @@ class TestSeriesNLargestNSmallest:
         # GH#46589
         s = Series([1, 2, 3, 3, 3, None])
         result = s.nsmallest(3, keep="all")
-        expected = Series([1., 2., 3., 3., 3.])
+        expected = Series([1.0, 2.0, 3.0, 3.0, 3.0])
         tm.assert_series_equal(result, expected)
 
         s = Series([1, 2, None, None, None])
