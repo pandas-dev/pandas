a4f6375d6be4fd27f8fb923bb149a1efcfd2900f
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-04-09 01:28:12 +0200

    REGR: read_fwf raising ValueError when widths was specified with usecols (#46691)

--- pandas/tests/io/parser/test_read_fwf.py
+++ pandas/tests/io/parser/test_read_fwf.py
@@ -930,3 +930,26 @@ def test_names_and_infer_colspecs():
     result = read_fwf(StringIO(data), skiprows=1, usecols=[0, 2], names=["a", "b"])
     expected = DataFrame({"a": [959.0], "b": 22.2})
     tm.assert_frame_equal(result, expected)
+
+
+def test_widths_and_usecols():
+    # GH#46580
+    data = """0  1    n -0.4100.1
+0  2    p  0.2 90.1
+0  3    n -0.3140.4"""
+    result = read_fwf(
+        StringIO(data),
+        header=None,
+        usecols=(0, 1, 3),
+        widths=(3, 5, 1, 5, 5),
+        index_col=False,
+        names=("c0", "c1", "c3"),
+    )
+    expected = DataFrame(
+        {
+            "c0": 0,
+            "c1": [1, 2, 3],
+            "c3": [-0.4, 0.2, -0.3],
+        }
+    )
+    tm.assert_frame_equal(result, expected)
