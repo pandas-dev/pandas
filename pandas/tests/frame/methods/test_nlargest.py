44f501e1f9c577f118e417a963d74a6e5307607b
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-04-10 11:59:37 -0500

    BUG: df.nsmallest get wrong results when NaN in the sorting column (#46656)

--- pandas/tests/frame/methods/test_nlargest.py
+++ pandas/tests/frame/methods/test_nlargest.py
@@ -216,3 +216,24 @@ class TestNLargestNSmallest:
         result = df.nlargest(5, 0)
         expected = df.sort_values(0, ascending=False).head(5)
         tm.assert_frame_equal(result, expected)
+
+    def test_nsmallest_nan_after_n_element(self):
+        # GH#46589
+        df = pd.DataFrame(
+            {
+                "a": [1, 2, 3, 4, 5, None, 7],
+                "b": [7, 6, 5, 4, 3, 2, 1],
+                "c": [1, 1, 2, 2, 3, 3, 3],
+            },
+            index=range(7),
+        )
+        result = df.nsmallest(5, columns=["a", "b"])
+        expected = pd.DataFrame(
+            {
+                "a": [1, 2, 3, 4, 5],
+                "b": [7, 6, 5, 4, 3],
+                "c": [1, 1, 2, 2, 3],
+            },
+            index=range(5),
+        ).astype({"a": "float"})
+        tm.assert_frame_equal(result, expected)
