074e62c0e0c398cc795e57cee7b2921c92b3933a
David Brandon <dbrandon@gritstone.com>
2022-04-12 10:34:33 -0400

    Again fix formatting

--- pandas/io/common.py
+++ pandas/io/common.py
@@ -321,9 +321,7 @@ def _get_filepath_or_buffer(
     if "t" not in fsspec_mode and "b" not in fsspec_mode:
         fsspec_mode += "b"
 
-    if (
-        isinstance(filepath_or_buffer, str) and not is_fsspec_url(filepath_or_buffer)
-    ):
+    if isinstance(filepath_or_buffer, str) and not is_fsspec_url(filepath_or_buffer):
         # TODO: fsspec can also handle HTTP via requests, but leaving this
         # unchanged. using fsspec appears to break the ability to infer if the
         # server responded with gzipped data
