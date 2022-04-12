52f134bb4ced826d5d7e67b0708d381b6bf471c7
David Brandon <dbrandon@gritstone.com>
2022-04-12 10:33:36 -0400

    it turns out that is_fsspec_url is the correct support function to use

--- pandas/io/common.py
+++ pandas/io/common.py
@@ -322,8 +322,7 @@ def _get_filepath_or_buffer(
         fsspec_mode += "b"
 
     if (
-        isinstance(filepath_or_buffer, str)
-        and parse_url(filepath_or_buffer).scheme in ["http", "https"]
+        isinstance(filepath_or_buffer, str) and not is_fsspec_url(filepath_or_buffer)
     ):
         # TODO: fsspec can also handle HTTP via requests, but leaving this
         # unchanged. using fsspec appears to break the ability to infer if the
