f681d5e36e6736747735520d0b8fabd3b9a537fd
Matthew Roeschke <emailformattr@gmail.com>
2022-04-17 21:05:59 -0700

    Simplfy inheritance

--- pandas/core/arrays/arrow/base.py
+++ pandas/core/arrays/arrow/base.py
@@ -3,28 +3,87 @@ from __future__ import annotations
 import numpy as np
 import pyarrow as pa
 
+from pandas._typing import DtypeObj
 from pandas.util._decorators import cache_readonly
 
-from pandas.core.dtypes.dtypes import BaseMaskedDtype
+from pandas.core.dtypes.base import StorageExtensionDtype
+
+from pandas.core.arrays.arrow import ArrowExtensionArray
 
 
-class BaseArrowDtype(BaseMaskedDtype):
+class ArrowDtype(StorageExtensionDtype):
     """
     Base class for dtypes for BaseArrowArray subclasses.
+    Modeled after BaseMaskedDtype
     """
 
+    name: str
+    base = None
     type: pa.DataType
+
     na_value = pa.NA
+
+    def __init__(self, storage="pyarrow") -> None:
+        super().__init__(storage)
 
     @cache_readonly
     def numpy_dtype(self) -> np.dtype:
-        """Return an instance of our numpy dtype"""
+        """Return an instance of the related numpy dtype"""
         return self.type.to_pandas_dtype()
 
+    @cache_readonly
+    def kind(self) -> str:
+        return self.numpy_dtype.kind
+
+    @cache_readonly
+    def itemsize(self) -> int:
+        """Return the number of bytes in this dtype"""
+        return self.numpy_dtype.itemsize
+
     @classmethod
-    def from_numpy_dtype(cls, dtype: np.dtype) -> BaseArrowDtype:
+    def construct_array_type(cls):
+        """
+        Return the array type associated with this dtype.
+
+        Returns
+        -------
+        type
+        """
+        return ArrowExtensionArray
+
+    @classmethod
+    def from_numpy_dtype(cls, dtype: np.dtype) -> ArrowDtype:
         """
         Construct the ArrowDtype corresponding to the given numpy dtype.
         """
-        # TODO: Fill when the other ArrowDtyes are created
+        # TODO: This may be incomplete
+        pa_dtype = pa.from_numpy_dtype(dtype)
+        if pa_dtype is cls.type:
+            return cls()
         raise NotImplementedError(dtype)
+
+    def _get_common_dtype(self, dtypes: list[DtypeObj]) -> DtypeObj | None:
+        # We unwrap any masked dtypes, find the common dtype we would use
+        #  for that, then re-mask the result.
+        from pandas.core.dtypes.cast import find_common_type
+
+        new_dtype = find_common_type(
+            [
+                dtype.numpy_dtype if isinstance(dtype, ArrowDtype) else dtype
+                for dtype in dtypes
+            ]
+        )
+        if not isinstance(new_dtype, np.dtype):
+            # If we ever support e.g. Masked[DatetimeArray] then this will change
+            return None
+        try:
+            return type(self).from_numpy_dtype(new_dtype)
+        except (KeyError, NotImplementedError):
+            return None
+
+    def __from_arrow__(self, array: pa.Array | pa.ChunkedArray):
+        """
+        Construct IntegerArray/FloatingArray from pyarrow Array/ChunkedArray.
+        """
+        array_class = self.construct_array_type()
+        return array_class(array)
