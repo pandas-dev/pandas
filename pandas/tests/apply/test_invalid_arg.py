283ddc158dce1b39592457f92375bfc3956f322d
stanleycai95 <46463107+stanleycai95@users.noreply.github.com>
2022-04-24 19:53:39 -0400

    Add test for map with invalid na_action argument

--- pandas/tests/apply/test_invalid_arg.py
+++ pandas/tests/apply/test_invalid_arg.py
@@ -64,6 +64,18 @@ def test_map_with_invalid_na_action_raises():
     msg = "na_action must either be 'ignore' or None"
     with pytest.raises(ValueError, match=msg):
         s.map(lambda x: x, na_action="____")
+        
+
+def test_map_via_dict_with_invalid_na_action_raises():
+    # https://github.com/pandas-dev/pandas/issues/46588
+    s = Series([1, 2, 3])
+    na_action = "____"
+    msg = (
+              "na_action must either be 'ignore' or None, "
+              f"{na_action} was passed"
+          )
+    with pytest.raises(ValueError, match=msg):
+        s.map({1: 2}, na_action="____")
 
 
 def test_map_categorical_na_action():
