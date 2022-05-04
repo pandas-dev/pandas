4c5bc99215a00630ec4b6f1ba8d6096881eeeabf
Lance <46547065+Condielj@users.noreply.github.com>
2022-05-04 02:04:47 -0600

    added test for validate_bool_kwargs_from_keywords

--- pandas/tests/frame/test_validate.py
+++ pandas/tests/frame/test_validate.py
@@ -39,3 +39,10 @@ class TestDataFrameValidate:
 
         with pytest.raises(ValueError, match=msg):
             getattr(dataframe, func)(**kwargs)
+
+@pytest.mark.parametrize('keyword', ('nan_as_null', 'allow_copy', 'ignore_width', 'index', 'index_names', 'show_dimensions', 'copy', 'inplace', 'reauth', 'auth_local_webserver', 'progress_bar', 'verify_integrity', 'write_index', 'bold_rows', 'escape', 'notebook', 'render_links', 'deep', 'takeable', 'drop', 'append', 'ignore_index', 'sort_remaining', 'normalize', 'ascending', 'dropna', 'keep_shape', 'keep_equal', 'overwrite', 'as_index', 'observed', 'sort', 'raw', 'left_index', 'right_index', 'numeric_only', 'skipna'))
+def test_set_index_validation(dataframe, func, keyword):
+    msg = 'For argument "{}" expected type bool'.format(keyword)
+    kwargs = {keyword: 'hello'}
+    with pytest.raises(ValueError, match=msg):
+        getattr(dataframe, func)(**kwargs)
