8aebe521b49b98f558572e4d8f5636f6a21bb616
Matthew Roeschke <emailformattr@gmail.com>
2022-05-10 18:10:26 -0700

    CI/TST: Fix test for pyarrow 8.0 release (#46991)

--- pandas/compat/pyarrow.py
+++ pandas/compat/pyarrow.py
@@ -14,6 +14,7 @@ try:
     pa_version_under5p0 = _palv < Version("5.0.0")
     pa_version_under6p0 = _palv < Version("6.0.0")
     pa_version_under7p0 = _palv < Version("7.0.0")
+    pa_version_under8p0 = _palv < Version("8.0.0")
 except ImportError:
     pa_version_under1p01 = True
     pa_version_under2p0 = True
@@ -22,3 +23,4 @@ except ImportError:
     pa_version_under5p0 = True
     pa_version_under6p0 = True
     pa_version_under7p0 = True
+    pa_version_under8p0 = True
