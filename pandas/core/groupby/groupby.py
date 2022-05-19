806fabadc7c39ddcf0d4ba509802c15d6b80715b
Matthew Roeschke <emailformattr@gmail.com>
2022-05-19 16:03:25 -0700

    Backport PR #47057 on branch 1.4.x (BUG: groupby.transform/agg with engine='numba' and a MultiIndex) (#47062)
    BUG: groupby.transform/agg with engine='numba' and a MultiIndex (#47057)
    
    Co-authored-by: Jeff Reback <jeff@reback.net>
    (cherry picked from commit c4027ad704c452ffb0f5ba962b5b789decbd29bd)

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -1257,7 +1257,16 @@ class GroupBy(BaseGroupBy[NDFrameT]):
         sorted_ids = algorithms.take_nd(ids, sorted_index, allow_fill=False)
 
         sorted_data = data.take(sorted_index, axis=self.axis).to_numpy()
-        sorted_index_data = data.index.take(sorted_index).to_numpy()
+        if len(self.grouper.groupings) > 1:
+            raise NotImplementedError(
+                "More than 1 grouping labels are not supported with engine='numba'"
+            )
+        # GH 46867
+        index_data = data.index
+        if isinstance(index_data, MultiIndex):
+            group_key = self.grouper.groupings[0].name
+            index_data = index_data.get_level_values(group_key)
+        sorted_index_data = index_data.take(sorted_index).to_numpy()
 
         starts, ends = lib.generate_slices(sorted_ids, ngroups)
         return (
