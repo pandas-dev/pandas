c8c771680f9ef711cf0f46d848a6f8dc9ccc7607
gplatz <gplatz@phoenixcontact.com>
2022-06-03 21:51:15 +0200

    BUG: Cannot map MultiIndex to more levels 846dc78
    fixed formatting

--- pandas/tests/indexes/base_class/test_map.py
+++ pandas/tests/indexes/base_class/test_map.py
@@ -9,29 +9,26 @@ import pandas._testing as tm
 
 @pytest.fixture
 def mi():
-    return MultiIndex.from_tuples([
-        ('a', '1/2'), ('b', '3/4'), ('d', '5/6')
-    ])
+    return MultiIndex.from_tuples([("a", "1/2"), ("b", "3/4"), ("d", "5/6")])
+
 
 @pytest.mark.parametrize(
-    'f,exp',
+    "f,exp",
     [
-        (lambda x: '/'.join(x), Index(['a/1/2', 'b/3/4', 'd/5/6'])),
-
+        (lambda x: "/".join(x), Index(["a/1/2", "b/3/4", "d/5/6"])),
         (
-            lambda x: tuple(y.replace('/', '') for y in x), 
-            MultiIndex.from_tuples([('a', '12'), ('b', '34'), ('d', '56')])
+            lambda x: tuple(y.replace("/", "") for y in x),
+            MultiIndex.from_tuples([("a", "12"), ("b", "34"), ("d", "56")]),
         ),
-
         (
-            lambda x: (x[0], *x[1].split('/')), 
-            MultiIndex.from_tuples([('a', '1', '2'), ('b', '3', '4'), ('d', '5', '6')])
-        )
-    ]
+            lambda x: (x[0], *x[1].split("/")),
+            MultiIndex.from_tuples([("a", "1", "2"), ("b", "3", "4"), ("d", "5", "6")]),
+        ),
+    ],
 )
 def test_map_multiindex(mi, f, exp):
     # GH 47173
-    
+
     result = mi.map(f)
-    
+
     tm.assert_index_equal(result, exp)
