87e8f79ca37cad96f3fd50708b7d36903d0005bc
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-06-06 00:47:18 +0200

    REGR: concat not sorting columns for mixed column names (#47206)

--- pandas/tests/reshape/concat/test_dataframe.py
+++ pandas/tests/reshape/concat/test_dataframe.py
@@ -205,3 +205,26 @@ class TestDataFrameConcat:
         for arr in res._iter_column_arrays():
             for arr2 in df._iter_column_arrays():
                 assert not np.shares_memory(arr, arr2)
+
+    def test_outer_sort_columns(self):
+        # GH#47127
+        df1 = DataFrame({"A": [0], "B": [1], 0: 1})
+        df2 = DataFrame({"A": [100]})
+        result = concat([df1, df2], ignore_index=True, join="outer", sort=True)
+        expected = DataFrame({0: [1.0, np.nan], "A": [0, 100], "B": [1.0, np.nan]})
+        tm.assert_frame_equal(result, expected)
+
+    def test_inner_sort_columns(self):
+        # GH#47127
+        df1 = DataFrame({"A": [0], "B": [1], 0: 1})
+        df2 = DataFrame({"A": [100], 0: 2})
+        result = concat([df1, df2], ignore_index=True, join="inner", sort=True)
+        expected = DataFrame({0: [1, 2], "A": [0, 100]})
+        tm.assert_frame_equal(result, expected)
+
+    def test_sort_columns_one_df(self):
+        # GH#47127
+        df1 = DataFrame({"A": [100], 0: 2})
+        result = concat([df1], ignore_index=True, join="inner", sort=True)
+        expected = DataFrame({0: [2], "A": [100]})
+        tm.assert_frame_equal(result, expected)
