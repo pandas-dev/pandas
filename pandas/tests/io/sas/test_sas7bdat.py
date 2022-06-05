0ede847ab847722cf66fabb21780762d2f8b9323
Jonas Haag <jonas@lophus.org>
2022-06-06 00:55:37 +0200

    Properly propagate exceptions in sas.pyx (#47149)

--- pandas/tests/io/sas/test_sas7bdat.py
+++ pandas/tests/io/sas/test_sas7bdat.py
@@ -341,3 +341,36 @@ def test_null_date(datapath):
         },
     )
     tm.assert_frame_equal(df, expected)
+
+
+@pytest.mark.parametrize("test_file", ["test2.sas7bdat", "test3.sas7bdat"])
+def test_exception_propagation_rdc_rle_decompress(datapath, monkeypatch, test_file):
+    """Errors in RLE/RDC decompression should propagate the same error."""
+    orig_np_zeros = np.zeros
+
+    def _patched_zeros(size, dtype):
+        if isinstance(size, int):
+            # np.zeros() call in {rdc,rle}_decompress
+            raise Exception("Test exception")
+        else:
+            # Other calls to np.zeros
+            return orig_np_zeros(size, dtype)
+
+    monkeypatch.setattr(np, "zeros", _patched_zeros)
+
+    with pytest.raises(Exception, match="^Test exception$"):
+        pd.read_sas(datapath("io", "sas", "data", test_file))
+
+
+def test_exception_propagation_rle_decompress(tmp_path, datapath):
+    """Illegal control byte in RLE decompressor should raise the correct ValueError."""
+    with open(datapath("io", "sas", "data", "test2.sas7bdat"), "rb") as f:
+        data = bytearray(f.read())
+    invalid_control_byte = 0x10
+    page_offset = 0x10000
+    control_byte_pos = 55229
+    data[page_offset + control_byte_pos] = invalid_control_byte
+    tmp_file = tmp_path / "test2.sas7bdat"
+    tmp_file.write_bytes(data)
+    with pytest.raises(ValueError, match="unknown control byte"):
+        pd.read_sas(tmp_file)
