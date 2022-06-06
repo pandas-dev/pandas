1c38a6baaad37c1692630ccdc73046cd9be429cb
Janosh Riebesell <janosh.riebesell@gmail.com>
2022-06-06 12:05:57 +0100

    remove extra period in extension checking

--- pandas/io/json/_json.py
+++ pandas/io/json/_json.py
@@ -723,7 +723,7 @@ class JsonReader(abc.Iterator):
         elif (
             isinstance(filepath_or_buffer, str)
             and filepath_or_buffer.lower().endswith(
-                (".json",) + tuple(f".json.{c}" for c in _extension_to_compression)
+                (".json",) + tuple(f".json{c}" for c in _extension_to_compression)
             )
             and not file_exists(filepath_or_buffer)
         ):
