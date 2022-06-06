3fdfa663bf0efee1f58eb2aacc831caa62419fe1
Simon Hawkins <simonjayhawkins@gmail.com>
2022-06-06 12:26:18 +0100

    Backport PR #47143 on branch 1.4.x (REGR: setitem writing into RangeIndex instead of creating a copy) (#47256)
    Backport PR #47143: REGR: setitem writing into RangeIndex instead of creating a copy
    
    Co-authored-by: Patrick Hoefler <61934744+phofl@users.noreply.github.com>

--- pandas/tests/test_downstream.py
+++ pandas/tests/test_downstream.py
@@ -269,3 +269,27 @@ def test_missing_required_dependency():
     output = exc.value.stdout.decode()
     for name in ["numpy", "pytz", "dateutil"]:
         assert name in output
+
+
+def test_frame_setitem_dask_array_into_new_col():
+    # GH#47128
+
+    # dask sets "compute.use_numexpr" to False, so catch the current value
+    # and ensure to reset it afterwards to avoid impacting other tests
+    olduse = pd.get_option("compute.use_numexpr")
+
+    try:
+        dask = import_module("dask")  # noqa:F841
+
+        import dask.array as da
+
+        dda = da.array([1, 2])
+        df = DataFrame({"a": ["a", "b"]})
+        df["b"] = dda
+        df["c"] = dda
+        df.loc[[False, True], "b"] = 100
+        result = df.loc[[1], :]
+        expected = DataFrame({"a": ["b"], "b": [100], "c": [2]}, index=[1])
+        tm.assert_frame_equal(result, expected)
+    finally:
+        pd.set_option("compute.use_numexpr", olduse)
