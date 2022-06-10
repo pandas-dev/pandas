858b76537a566f56958b126bc2f296f49a52b04b
jackgoldsmith4 <jackgoldsmith4@gmail.com>
2022-06-09 20:30:32 -0400

    fix index type

--- pandas/tests/indexing/test_loc.py
+++ pandas/tests/indexing/test_loc.py
@@ -2874,7 +2874,7 @@ def test_loc_set_int_dtype():
     df = DataFrame([list("abc")])
     df.loc[:, "col1"] = 5
 
-    expected = DataFrame({'0': ['a'], '1': ['b'], '2': ['c'], 'col1': [5]})
+    expected = DataFrame({0: ["a"], 1: ["b"], 2: ["c"], "col1": [5]})
     tm.assert_frame_equal(df, expected)
 
 
