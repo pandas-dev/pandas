82b15b736a344f69bb511199d0d81a687f051fd0
Brock <jbrockmendel@gmail.com>
2022-06-13 11:05:35 -0700

    tzaware case

--- pandas/tests/tslibs/test_resolution.py
+++ pandas/tests/tslibs/test_resolution.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytz
 
 from pandas._libs.tslibs import (
     Resolution,
@@ -17,4 +18,7 @@ def test_get_resolution_nano():
 def test_get_resolution_non_nano_data():
     arr = np.array([1], dtype=np.int64)
     res = get_resolution(arr, None, NpyDatetimeUnit.NPY_FR_us.value)
+    assert res == Resolution.RESO_US
+
+    res = get_resolution(arr, pytz.UTC, NpyDatetimeUnit.NPY_FR_us.value)
     assert res == Resolution.RESO_US
