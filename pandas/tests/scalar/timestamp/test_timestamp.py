a1c7231bf17bae95f50bb1fbb62ab9260189683f
jbrockmendel <jbrockmendel@gmail.com>
2022-06-13 16:44:18 -0700

    ENH: Timestamp.tz_convert support non-nano (#47320)

--- pandas/tests/scalar/timestamp/test_timestamp.py
+++ pandas/tests/scalar/timestamp/test_timestamp.py
@@ -22,6 +22,7 @@ from pandas._libs.tslibs.dtypes import NpyDatetimeUnit
 from pandas._libs.tslibs.timezones import (
     dateutil_gettz as gettz,
     get_timezone,
+    tz_compare,
 )
 from pandas.errors import OutOfBoundsDatetime
 import pandas.util._test_decorators as td
@@ -762,6 +763,16 @@ class TestNonNano:
     def test_month_name(self, dt64, ts):
         alt = Timestamp(dt64)
         assert ts.month_name() == alt.month_name()
+
+    def test_tz_convert(self, ts):
+        ts = Timestamp._from_value_and_reso(ts.value, ts._reso, utc)
+
+        tz = pytz.timezone("US/Pacific")
+        result = ts.tz_convert(tz)
+
+        assert isinstance(result, Timestamp)
+        assert result._reso == ts._reso
+        assert tz_compare(result.tz, tz)
 
     def test_repr(self, dt64, ts):
         alt = Timestamp(dt64)
