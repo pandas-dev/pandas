d90219559550f24ebc575aa5bafb5d9f7d0d1034
jbrockmendel <jbrockmendel@gmail.com>
2022-06-13 10:17:20 -0700

    ENH: implement Timestamp _as_reso, _as_unit (#47333)

--- pandas/tests/scalar/timedelta/test_timedelta.py
+++ pandas/tests/scalar/timedelta/test_timedelta.py
@@ -13,6 +13,7 @@ from pandas._libs.tslibs import (
     NaT,
     iNaT,
 )
+from pandas._libs.tslibs.dtypes import NpyDatetimeUnit
 from pandas.errors import OutOfBoundsTimedelta
 
 import pandas as pd
@@ -33,7 +34,7 @@ class TestAsUnit:
 
         res = td._as_unit("us")
         assert res.value == td.value // 1000
-        assert res._reso == td._reso - 1
+        assert res._reso == NpyDatetimeUnit.NPY_FR_us.value
 
         rt = res._as_unit("ns")
         assert rt.value == td.value
@@ -41,7 +42,7 @@ class TestAsUnit:
 
         res = td._as_unit("ms")
         assert res.value == td.value // 1_000_000
-        assert res._reso == td._reso - 2
+        assert res._reso == NpyDatetimeUnit.NPY_FR_ms.value
 
         rt = res._as_unit("ns")
         assert rt.value == td.value
@@ -49,7 +50,7 @@ class TestAsUnit:
 
         res = td._as_unit("s")
         assert res.value == td.value // 1_000_000_000
-        assert res._reso == td._reso - 3
+        assert res._reso == NpyDatetimeUnit.NPY_FR_s.value
 
         rt = res._as_unit("ns")
         assert rt.value == td.value
@@ -58,7 +59,7 @@ class TestAsUnit:
     def test_as_unit_overflows(self):
         # microsecond that would be just out of bounds for nano
         us = 9223372800000000
-        td = Timedelta._from_value_and_reso(us, 9)
+        td = Timedelta._from_value_and_reso(us, NpyDatetimeUnit.NPY_FR_us.value)
 
         msg = "Cannot cast 106752 days 00:00:00 to unit='ns' without overflow"
         with pytest.raises(OutOfBoundsTimedelta, match=msg):
@@ -66,7 +67,7 @@ class TestAsUnit:
 
         res = td._as_unit("ms")
         assert res.value == us // 1000
-        assert res._reso == 8
+        assert res._reso == NpyDatetimeUnit.NPY_FR_ms.value
 
     def test_as_unit_rounding(self):
         td = Timedelta(microseconds=1500)
@@ -75,7 +76,7 @@ class TestAsUnit:
         expected = Timedelta(milliseconds=1)
         assert res == expected
 
-        assert res._reso == 8
+        assert res._reso == NpyDatetimeUnit.NPY_FR_ms.value
         assert res.value == 1
 
         with pytest.raises(ValueError, match="Cannot losslessly convert units"):
