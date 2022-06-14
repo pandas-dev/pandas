bdec4f433da2344d231315532f3381ab26b4b4ea
Brock <jbrockmendel@gmail.com>
2022-06-14 14:50:34 -0700

    mypy fixup

--- pandas/core/arrays/datetimelike.py
+++ pandas/core/arrays/datetimelike.py
@@ -424,13 +424,13 @@ class DatetimeLikeArrayMixin(OpsMixin, NDArrayBackedExtensionArray):
 
         if is_object_dtype(dtype):
             if self.dtype.kind == "M":
+                self = cast("DatetimeArray", self)
                 # *much* faster than self._box_values
                 #  for e.g. test_get_loc_tuple_monotonic_above_size_cutoff
                 i8data = self.asi8
                 converted = ints_to_pydatetime(
                     i8data,
-                    # error: "DatetimeLikeArrayMixin" has no attribute "tz"
-                    tz=self.tz,  # type: ignore[attr-defined]
+                    tz=self.tz,
                     freq=self.freq,
                     box="timestamp",
                     reso=self._reso,
