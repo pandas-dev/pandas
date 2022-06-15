616c23ee3808c1b5c5b57fa00fd2ce76d38a0fbb
jbrockmendel <jbrockmendel@gmail.com>
2022-06-15 11:47:37 -0700

    ENH: DTA to_pydatetime, time, timetz, date, iter support non-nano (#47307)
    * ENH: Timestamp +- timedeltalike scalar support non-nano
    
    * ENH: Timestamp.__sub__(datetime) with non-nano
    
    * better exception message
    
    * BUG: concat not sorting mixed column names when None is included (#47331)
    
    * REGR: concat not sorting columns for mixed column names
    
    * Fix none in columns
    
    * BUG: concat not sorting column names when None is included
    
    * Update doc/source/whatsnew/v1.5.0.rst
    
    Co-authored-by: Matthew Roeschke <emailformattr@gmail.com>
    
    * Add gh reference
    
    Co-authored-by: Matthew Roeschke <emailformattr@gmail.com>
    
    * Add run-tests action (#47292)
    
    * Add run-tests action
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Update macos-windows.yml
    
    * Update posix.yml
    
    * Update python-dev.yml
    
    * Update action.yml
    
    * Update macos-windows.yml
    
    * Update posix.yml
    
    * Update python-dev.yml
    
    * Update python-dev.yml
    
    * Update python-dev.yml
    
    * Update python-dev.yml
    
    * Update python-dev.yml
    
    * Update python-dev.yml
    
    * Update python-dev.yml
    
    * Update python-dev.yml
    
    * Update python-dev.yml
    
    * ENH: Timestamp pickle support non-nano tzaware (#47340)
    
    * ENH: DTA to_pydatetime, time, timetz, date, iter support non-nano
    
    * cast in liboffsets
    
    * mypy fixup
    
    Co-authored-by: Patrick Hoefler <61934744+phofl@users.noreply.github.com>
    Co-authored-by: Matthew Roeschke <emailformattr@gmail.com>
    Co-authored-by: Jonas Haag <jonas@lophus.org>

--- pandas/tests/arrays/test_datetimes.py
+++ pandas/tests/arrays/test_datetimes.py
@@ -126,6 +126,35 @@ class TestNonNano:
 
         tm.assert_extension_array_equal(result, expected)
 
+    def test_iter(self, dta):
+        res = next(iter(dta))
+        expected = dta[0]
+
+        assert type(res) is pd.Timestamp
+        assert res.value == expected.value
+        assert res._reso == expected._reso
+        assert res == expected
+
+    def test_astype_object(self, dta):
+        result = dta.astype(object)
+        assert all(x._reso == dta._reso for x in result)
+        assert all(x == y for x, y in zip(result, dta))
+
+    def test_to_pydatetime(self, dta_dti):
+        dta, dti = dta_dti
+
+        result = dta.to_pydatetime()
+        expected = dti.to_pydatetime()
+        tm.assert_numpy_array_equal(result, expected)
+
+    @pytest.mark.parametrize("meth", ["time", "timetz", "date"])
+    def test_time_date(self, dta_dti, meth):
+        dta, dti = dta_dti
+
+        result = getattr(dta, meth)
+        expected = getattr(dti, meth)
+        tm.assert_numpy_array_equal(result, expected)
+
 
 class TestDatetimeArrayComparisons:
     # TODO: merge this into tests/arithmetic/test_datetime64 once it is
