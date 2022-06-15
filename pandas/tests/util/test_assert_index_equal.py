5879fb422605c01cb0b8b5d3bb638cd83e8f1058
phofl <patrick_hoefler@gmx.net>
2022-06-15 09:31:14 +0200

    Remove fixture

--- pandas/tests/util/test_assert_index_equal.py
+++ pandas/tests/util/test_assert_index_equal.py
@@ -251,7 +251,7 @@ def test_assert_index_equal_ea_dtype_order_false(any_numeric_ea_dtype):
     tm.assert_index_equal(idx1, idx2, check_order=False)
 
 
-def test_assert_index_equal_object_ints_order_false(any_numeric_ea_dtype):
+def test_assert_index_equal_object_ints_order_false():
     # GH#47207
     idx1 = Index([1, 3], dtype="object")
     idx2 = Index([3, 1], dtype="object")
