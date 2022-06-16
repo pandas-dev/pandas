9dd728d0523ed8dce596bcca52d2135d5ed8808b
phofl <patrick_hoefler@gmx.net>
2022-06-16 23:06:02 +0200

    Add partial case

--- pandas/tests/frame/indexing/test_setitem.py
+++ pandas/tests/frame/indexing/test_setitem.py
@@ -708,7 +708,10 @@ class TestDataFrameSetItem:
         df = DataFrame({"a": [1, 2], "b": [3, 4], **vals})
         df.loc[:, "a"] = {1: 100, 0: 200}
         df.loc[:, "c"] = {0: 5, 1: 6}
-        expected = DataFrame({"a": [200, 100], "b": [3, 4], **vals, "c": [5, 6]})
+        df.loc[:, "e"] = {1: 5}
+        expected = DataFrame(
+            {"a": [200, 100], "b": [3, 4], **vals, "c": [5, 6], "e": [np.nan, 5]}
+        )
         tm.assert_frame_equal(df, expected)
 
 
