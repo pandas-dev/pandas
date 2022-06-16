9c619182b14c644e4cc13a4d6f19952d8981e86e
Jonas Haag <jonas@lophus.org>
2022-06-16 18:20:15 +0200

    Speedup

--- pandas/io/sas/sas.pyx
+++ pandas/io/sas/sas.pyx
@@ -426,8 +426,10 @@ cdef class Parser:
                 jb += 1
             elif column_types[j] == column_type_string:
                 # string
-                string_chunk[js, current_row] = np.array(source[start:(
-                    start + lngt)]).tobytes().rstrip(b"\x00 ")
+                # Skip trailing whitespace
+                while lngt > 0 and source[start+lngt-1] in b"\x00 ":
+                    lngt -= 1
+                string_chunk[js, current_row] = source[start:start+lngt]
                 js += 1
 
         self.current_row_on_page_index += 1
