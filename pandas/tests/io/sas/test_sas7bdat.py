91d868d57ba49a5eea711c199bc6df49f6a0b277
Jonas Haag <jonas@lophus.org>
2022-06-17 10:38:25 +0200

    Fix tests

--- pandas/tests/io/sas/test_sas7bdat.py
+++ pandas/tests/io/sas/test_sas7bdat.py
@@ -385,6 +385,6 @@ def test_exception_propagation_rle_decompress(tmp_path, datapath):
 
 def test_0x00_control_byte(datapath):
     # GH 47099
-    fname = datapath("io", "sas", "data", "0x00controlbyte.sas7bdat.gz")
+    fname = datapath("io", "sas", "data", "0x00controlbyte.sas7bdat.bz2")
     df = next(pd.read_sas(fname, chunksize=11_000))
-    assert df.shape == (1, 20)
+    assert df.shape == (11_000, 20)
