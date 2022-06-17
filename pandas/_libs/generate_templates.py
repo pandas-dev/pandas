9d2f2509d7cde8072f6f688c176351a4f2eae5bd
William Ayd <william.ayd@icloud.com>
2022-06-17 10:32:41 -0700

    generation script

---
+++ pandas/_libs/generate_templates.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import argparse
+import pathlib
+from Cython import Tempita
+from Cython.Build import cythonize
+
+
+if __name__ == "__main__":
+    for template in (
+            "algos_common_helper.pxi.in",
+            "algos_take_helper.pxi.in",
+            "hashtable_class_helper.pxi.in",
+            "hashtable_func_helper.pxi.in",
+            "index_class_helper.pxi.in",
+            "intervaltree.pxi.in",
+            "khash_for_primitive_helper.pxi.in",
+            "sparse_op_helper.pxi.in",
+    ):
+        pyxcontent = Tempita.sub(open(template).read())
+        with open(template.replace(".in", ""), "w") as outfile:
+            outfile.write(pyxcontent)
