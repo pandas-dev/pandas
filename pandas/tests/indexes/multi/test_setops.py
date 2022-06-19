9b70d1e58c48e0d40637b900ebbc5ca2dca6cdf9
jackgoldsmith4 <jackgoldsmith4@gmail.com>
2022-06-19 14:40:12 +0000

    Fixes from pre-commit [automated commit]

--- pandas/tests/indexes/multi/test_setops.py
+++ pandas/tests/indexes/multi/test_setops.py
@@ -549,9 +549,20 @@ def test_union_duplicates(index, request):
 @pytest.mark.parametrize(
     "levels1, levels2, codes1, codes2, names",
     [
-        ([['2018', 'a', 'c', 'e', 'num', 'oan', 'vol'], [1970, '']],
-            [['e', 'num', 'oan', 'vol', 'year'], ['']], [[3, 6, 4, ], [1, 1, 1]],
-            [[3, 1, 4, 1, 0], [0, 0, 0, 0, 0]], ['variable', 'cit_year']),
+        (
+            [["2018", "a", "c", "e", "num", "oan", "vol"], [1970, ""]],
+            [["e", "num", "oan", "vol", "year"], [""]],
+            [
+                [
+                    3,
+                    6,
+                    4,
+                ],
+                [1, 1, 1],
+            ],
+            [[3, 1, 4, 1, 0], [0, 0, 0, 0, 0]],
+            ["variable", "cit_year"],
+        ),
     ],
 )
 def test_intersection_lexsort_depth(levels1, levels2, codes1, codes2, names):
--- pandas/tests/indexing/test_loc.py
+++ pandas/tests/indexing/test_loc.py
@@ -2881,11 +2881,16 @@ def test_loc_set_int_dtype():
 
 def test_loc_periodindex_3_levels():
     # GH#24091
-    p_index = PeriodIndex(["20181101 1100", "20181101 1200", "20181102 1300",
-                          "20181102 1400"], name='datetime', freq="B")
-    mi_series = DataFrame([["A", "B", 1.0], ["A", "C", 2.0], ["Z", "Q", 3.0],
-                          ["W", "F", 4.0]], index=p_index,
-                          columns=["ONE", "TWO", "VALUES"])
+    p_index = PeriodIndex(
+        ["20181101 1100", "20181101 1200", "20181102 1300", "20181102 1400"],
+        name="datetime",
+        freq="B",
+    )
+    mi_series = DataFrame(
+        [["A", "B", 1.0], ["A", "C", 2.0], ["Z", "Q", 3.0], ["W", "F", 4.0]],
+        index=p_index,
+        columns=["ONE", "TWO", "VALUES"],
+    )
     mi_series = mi_series.set_index(["ONE", "TWO"], append=True)["VALUES"]
     assert mi_series.loc[(p_index[0], "A", "B")] == 1.0
 
