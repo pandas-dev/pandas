3070a6bf238940691a810f193b8556096f747955
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-06-21 20:09:33 +0200

    REGR: Fix fillna making a copy when dict was given as fill value and inplace is set (#47327)

--- pandas/tests/frame/methods/test_update.py
+++ pandas/tests/frame/methods/test_update.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pytest
 
+import pandas.util._test_decorators as td
+
 import pandas as pd
 from pandas import (
     DataFrame,
@@ -146,3 +148,14 @@ class TestDataFrameUpdate:
 
         expected = DataFrame({"a": [1, 3], "b": [np.nan, 2], "c": ["foo", np.nan]})
         tm.assert_frame_equal(df, expected)
+
+    @td.skip_array_manager_invalid_test
+    def test_update_modify_view(self):
+        # GH#47188
+        df = DataFrame({"A": ["1", np.nan], "B": ["100", np.nan]})
+        df2 = DataFrame({"A": ["a", "x"], "B": ["100", "200"]})
+        result_view = df2[:]
+        df2.update(df)
+        expected = DataFrame({"A": ["1", "x"], "B": ["100", "200"]})
+        tm.assert_frame_equal(df2, expected)
+        tm.assert_frame_equal(result_view, expected)
