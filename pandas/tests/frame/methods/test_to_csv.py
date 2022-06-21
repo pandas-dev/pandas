ee6c7fac48fcc9c6ddba34c721a3fe34ef44e600
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-06-21 22:09:36 +0200

    Clarify doc

--- pandas/tests/frame/methods/test_to_csv.py
+++ pandas/tests/frame/methods/test_to_csv.py
@@ -836,7 +836,11 @@ class TestDataFrameToCSV:
     def test_to_csv_float_format_over_decimal(self):
         # GH#47436
         df = DataFrame({"a": [0.5, 1.0]})
-        result = df.to_csv(decimal=",", float_format=lambda x: np.format_float_positional(x, trim="-"), index=False)
+        result = df.to_csv(
+            decimal=",",
+            float_format=lambda x: np.format_float_positional(x, trim="-"),
+            index=False,
+        )
         expected_rows = ["a", "0.5", "1"]
         expected = tm.convert_rows_list_to_csv_str(expected_rows)
         assert result == expected
