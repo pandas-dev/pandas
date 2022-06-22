9ea513aa09270165c8e5b9bf47a9881d11ac9afa
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-06-22 22:41:29 +0200

    REGR: revert behaviour change for concat with empty/all-NaN data (#47372)

--- pandas/tests/extension/base/setitem.py
+++ pandas/tests/extension/base/setitem.py
@@ -357,6 +357,20 @@ class BaseSetitemTests(BaseExtensionTests):
 
         self.assert_frame_equal(result, expected)
 
+    def test_setitem_with_expansion_row(self, data, na_value):
+        df = pd.DataFrame({"data": data[:1]})
+
+        df.loc[1, "data"] = data[1]
+        expected = pd.DataFrame({"data": data[:2]})
+        self.assert_frame_equal(df, expected)
+
+        # https://github.com/pandas-dev/pandas/issues/47284
+        df.loc[2, "data"] = na_value
+        expected = pd.DataFrame(
+            {"data": pd.Series([data[0], data[1], na_value], dtype=data.dtype)}
+        )
+        self.assert_frame_equal(df, expected)
+
     def test_setitem_series(self, data, full_indexer):
         # https://github.com/pandas-dev/pandas/issues/32395
         ser = pd.Series(data, name="data")
