758c3a50187dd9280d43483daf03a83e2a35de8f
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-06-24 20:14:26 +0200

    BUG: isin casting to float64 for unsigned int and list (#46693)

--- pandas/tests/test_algos.py
+++ pandas/tests/test_algos.py
@@ -1089,6 +1089,13 @@ class TestIsin:
         expected_false = DataFrame({"values": [False, False]})
         tm.assert_frame_equal(result, expected_false)
 
+    def test_isin_unsigned_dtype(self):
+        # GH#46485
+        ser = Series([1378774140726870442], dtype=np.uint64)
+        result = ser.isin([1378774140726870528])
+        expected = Series(False)
+        tm.assert_series_equal(result, expected)
+
 
 class TestValueCounts:
     def test_value_counts(self):
