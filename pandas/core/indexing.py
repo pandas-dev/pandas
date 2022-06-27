fc007b0f07907ffa95f62324058836f45a02f2f3
INDIG0N <hicksjordan1999@gmail.com>
2022-06-27 18:47:40 -0400

    changed refernce style for .loc docstring

--- pandas/core/indexing.py
+++ pandas/core/indexing.py
@@ -522,9 +522,8 @@ class IndexingMixin:
                    mark ii          1       4
         viper      mark ii          7       1
 
-        For more details and explanations on advanced indexing, 
-        please visit the following link:
-        https://pandas.pydata.org/docs/user_guide/advanced.html#advanced-indexing-with-hierarchical-index
+        Please see the :ref:`user guide<advanced.advanced_hierarchical>`
+        for more details and explanations of advanced indexing.
         """
         return _LocIndexer("loc", self)
 
