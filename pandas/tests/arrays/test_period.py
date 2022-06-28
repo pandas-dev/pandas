78d6f1640f25d9cbb0cf2919aac14776ee3d0f2a
jbrockmendel <jbrockmendel@gmail.com>
2022-06-28 16:22:04 -0700

    BUG: PeriodArray.__sub__(Period) overflows (#47538)
    * BUG: PeriodArray.__sub__(Period) overflows
    
    * GH refs

--- pandas/tests/arrays/test_period.py
+++ pandas/tests/arrays/test_period.py
@@ -115,6 +115,20 @@ def test_sub_period():
         arr - other
 
 
+def test_sub_period_overflow():
+    # GH#47538
+    dti = pd.date_range("1677-09-22", periods=2, freq="D")
+    pi = dti.to_period("ns")
+
+    per = pd.Period._from_ordinal(10**14, pi.freq)
+
+    with pytest.raises(OverflowError, match="Overflow in int64 addition"):
+        pi - per
+
+    with pytest.raises(OverflowError, match="Overflow in int64 addition"):
+        per - pi
+
+
 # ----------------------------------------------------------------------------
 # Methods
 
