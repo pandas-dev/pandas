5fc5b04082201f40e9402625e222225c3f9736d8
tobias.pitters <tobias.pitters@gmail.com>
2022-06-29 11:01:19 +0200

    fix regression

--- pandas/tests/frame/indexing/test_getitem.py
+++ pandas/tests/frame/indexing/test_getitem.py
@@ -16,6 +16,8 @@ from pandas import (
     concat,
     get_dummies,
     period_range,
+    date_range,
+    DateOffset
 )
 import pandas._testing as tm
 from pandas.core.arrays import SparseArray
@@ -172,6 +174,14 @@ class TestGetitemListLike:
         expected = Series([5, 6], name="b", index=[1, 2])
         tm.assert_series_equal(result, expected)
 
+    def test_getitem_iloc_dateoffset_days(self):
+        # GH 46671
+        df = DataFrame(list(range(10)), index=date_range('01-01-2022', periods=10))
+        df = df.asfreq(DateOffset(days=1))
+        result = df.loc['2022-01-01': '2022-01-03']
+        expected = DataFrame([0, 1, 2], index=DatetimeIndex(['2022-01-01', '2022-01-02', '2022-01-03'],
+                                              dtype='datetime64[ns]', freq=DateOffset(days=1)))
+        tm.assert_frame_equal(result, expected)
 
 class TestGetitemCallable:
     def test_getitem_callable(self, float_frame):
@@ -426,3 +436,4 @@ class TestGetitemDeprecatedIndexers:
         )
         with tm.assert_produces_warning(FutureWarning):
             df[key]
+
