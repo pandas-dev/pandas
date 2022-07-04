0e208c8215acd4bfacb53cfe1b43954b441659fd
Steven Rotondo <steven.rotondo75@gmail.com>
2022-07-04 15:36:54 -0700

    TST: Added more test cases #25581

--- pandas/tests/groupby/aggregate/test_aggregate.py
+++ pandas/tests/groupby/aggregate/test_aggregate.py
@@ -1415,12 +1415,23 @@ def test_multi_axis_1_raises(func):
         gb.agg(func)
 
 
-def test_agg_of_list():
+@pytest.mark.parametrize(
+    "test, constant",
+    [
+        ([[20, "A"], [20, "B"], [10, "C"]], {0: [10, 20], 1: ["C", ["A", "B"]]}),
+        ([[20, "A"], [20, "B"], [30, "C"]], {0: [20, 30], 1: [["A", "B"], "C"]}),
+        ([["a", 1], ["a", 1], ["b", 2], ["b", 3]], {0: ["a", "b"], 1: [1, [2, 3]]})
+        # This test case still fails
+        # ([["a", 1], ["a", 2], ["b", 3], ["b", 3]], {0: ["a", "b"], 1: [[1, 2], 3]})
+    ],
+)
+def test_agg_of_mode_list(test, constant):
     # GH#25581
-    df1 = DataFrame([[20, "A"], [20, "B"], [30, "C"]])
+    df1 = DataFrame(test)
     result = df1.groupby(0).agg(Series.mode)
+    # Mode usually only returns 1 value, but can return a list in the case of a tie.
 
-    expected = DataFrame({0: [20, 30], 1: [["A", "B"], "C"]})
+    expected = DataFrame(constant)
     expected = expected.set_index(0)
 
     tm.assert_frame_equal(result, expected)
