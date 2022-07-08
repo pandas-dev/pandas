2951fb14ef8c589f50b5a28e76878de410968b79
ahmedibrhm <81244897+ahmedibrhm@users.noreply.github.com>
2022-07-08 00:39:29 -0700

    DOC #45443 edited the documentation of where/mask functions

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -9614,8 +9614,8 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         The {name} method is an application of the if-then idiom. For each
         element in the calling DataFrame, if ``cond`` is ``{cond}`` the
         element is used; otherwise the corresponding element from the DataFrame
-        ``other`` is used. If `cond` {klass} is less in size than `other`, the default bool
-        for the missing value is {cond_rev}.
+        ``other`` is used. If `cond` {klass} is less in size than `other`, the
+        default bool for the missing value is {cond_rev}.
 
         The signature for :func:`DataFrame.where` differs from
         :func:`numpy.where`. Roughly ``df1.where(m, df2)`` is equivalent to
