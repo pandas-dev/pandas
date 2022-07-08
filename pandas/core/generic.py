4fd16d160b0738909d56fa5b7649bf61142885bc
Ahmed Ibrahim <81244897+ahmedibrhm@users.noreply.github.com>
2022-07-08 09:56:11 -0700

    Update generic.py

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -9614,8 +9614,9 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         The {name} method is an application of the if-then idiom. For each
         element in the calling DataFrame, if ``cond`` is ``{cond}`` the
         element is used; otherwise the corresponding element from the DataFrame
-        ``other`` is used. If `cond` {klass} is less in size than `other`, the default bool
-        for the missing value is {cond_rev}.
+        ``other`` is used. If the axis of ``other`` does not align with axis of 
+        ``cond`` {klass}, the misaligned index positions will be filled with 
+        {cond_rev}
 
         The signature for :func:`DataFrame.where` differs from
         :func:`numpy.where`. Roughly ``df1.where(m, df2)`` is equivalent to
