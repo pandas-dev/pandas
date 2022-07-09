e20cf659580949541607d3732fd3abfa04a51fb9
Jonas Haag <jonas@lophus.org>
2022-07-09 09:15:01 +0200

    Review feedback

--- pandas/io/sas/sas.pyx
+++ pandas/io/sas/sas.pyx
@@ -424,7 +424,8 @@ cdef class Parser:
                 jb += 1
             elif column_types[j] == column_type_string:
                 # string
-                # Skip trailing whitespace
+                # Skip trailing whitespace. This is equivalent to calling
+                # .rstrip(b"\x00 ") but without Python call overhead.
                 while lngt > 0 and source[start+lngt-1] in b"\x00 ":
                     lngt -= 1
                 string_chunk[js, current_row] = (&source[start])[:lngt]
