263aea64e5df62a8f45b36e826b0e7d41a6cf146
Jonas Haag <jonas@lophus.org>
2022-07-09 09:57:57 +0200

    Simplify condition

--- pandas/io/sas/sas.pyx
+++ pandas/io/sas/sas.pyx
@@ -415,7 +415,7 @@ cdef class Parser:
 
         cdef:
             Py_ssize_t j
-            int s, k, m, jb, js, current_row, rpos
+            int k, m, jb, js, current_row, rpos
             int64_t lngt, start, ct
             Buffer source, decompressed_source
             int64_t[:] column_types
@@ -444,7 +444,6 @@ cdef class Parser:
         offsets = self.offsets
         byte_chunk = self.byte_chunk
         string_chunk = self.string_chunk
-        s = 8 * self.current_row_in_chunk_index
         js = 0
         jb = 0
         for j in range(self.column_count):
@@ -455,10 +454,10 @@ cdef class Parser:
             ct = column_types[j]
             if ct == column_type_decimal:
                 # decimal
-                if self.is_little_endian:
-                    m = s + 8 - lngt
-                else:
-                    m = s
+                assert lngt in (4, 8)
+                m = 8 * self.current_row_in_chunk_index
+                if lngt == 4 and self.is_little_endian:
+                    m += 4
                 for k in range(lngt):
                     byte_chunk[jb, m + k] = buf_get(source, start + k)
                 jb += 1
