4d43e69b61d0ec6df931a69a50e70c1494624bbc
ahmedibrhm <81244897+ahmedibrhm@users.noreply.github.com>
2022-07-11 12:13:56 -0700

    Bug 43767 fix

--- pandas/core/resample.py
+++ pandas/core/resample.py
@@ -512,10 +512,10 @@ class Resampler(BaseGroupBy, PandasObject):
         """
         Potentially wrap any results.
         """
-        if isinstance(result, ABCSeries) and self._selection is not None:
+        if (isinstance(result, ABCSeries) or isinstance(result, ABCDataFrame)) and self._selection is not None:
             result.name = self._selection
 
-        if isinstance(result, ABCSeries) and result.empty:
+        if (isinstance(result, ABCSeries) or isinstance(result, ABCDataFrame)) and result.empty:
             obj = self.obj
             # When index is all NaT, result is empty but index is not
             result.index = _asfreq_compat(obj.index[:0], freq=self.freq)
