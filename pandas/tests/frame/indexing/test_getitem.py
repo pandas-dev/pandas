fb11c6641500d5bc5a5689bafba755dfa280eb31
tobias.pitters <tobias.pitters@gmail.com>
2022-07-11 21:49:00 +0200

    update test

--- pandas/tests/frame/indexing/test_getitem.py
+++ pandas/tests/frame/indexing/test_getitem.py
@@ -176,7 +176,10 @@ class TestGetitemListLike:
 
     def test_getitem_iloc_dateoffset_days(self):
         # GH 46671
-        df = DataFrame(list(range(10)), index=date_range("01-01-2022", periods=10, freq=DateOffset(days=1)))
+        df = DataFrame(
+            list(range(10)),
+            index=date_range("01-01-2022", periods=10, freq=DateOffset(days=1)),
+        )
         result = df.loc["2022-01-01":"2022-01-03"]
         expected = DataFrame(
             [0, 1, 2],
@@ -188,7 +191,12 @@ class TestGetitemListLike:
         )
         tm.assert_frame_equal(result, expected)
 
-        df = DataFrame(list(range(10)), index=date_range("01-01-2022", periods=10, freq=DateOffset(days=1, hours=2)))
+        df = DataFrame(
+            list(range(10)),
+            index=date_range(
+                "01-01-2022", periods=10, freq=DateOffset(days=1, hours=2)
+            ),
+        )
         result = df.loc["2022-01-01":"2022-01-03"]
         expected = DataFrame(
             [0, 1, 2],
@@ -200,16 +208,26 @@ class TestGetitemListLike:
         )
         tm.assert_frame_equal(result, expected)
 
-
-        df = DataFrame(list(range(10)), index=date_range("01-01-2022", periods=10, freq=DateOffset(minutes=3)))
+        df = DataFrame(
+            list(range(10)),
+            index=date_range("01-01-2022", periods=10, freq=DateOffset(minutes=3)),
+        )
         result = df.loc["2022-01-01":"2022-01-03"]
         expected = DataFrame(
             [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
             index=DatetimeIndex(
-                ["2022-01-01 00:00:00", "2022-01-01 00:03:00", "2022-01-01 00:06:00",
-                 "2022-01-01 00:09:00", "2022-01-01 00:12:00", "2022-01-01 00:15:00",
-                 "2022-01-01 00:18:00", "2022-01-01 00:21:00", "2022-01-01 00:24:00",
-                 "2022-01-01 00:27:00"],
+                [
+                    "2022-01-01 00:00:00",
+                    "2022-01-01 00:03:00",
+                    "2022-01-01 00:06:00",
+                    "2022-01-01 00:09:00",
+                    "2022-01-01 00:12:00",
+                    "2022-01-01 00:15:00",
+                    "2022-01-01 00:18:00",
+                    "2022-01-01 00:21:00",
+                    "2022-01-01 00:24:00",
+                    "2022-01-01 00:27:00",
+                ],
                 dtype="datetime64[ns]",
                 freq=DateOffset(minutes=3),
             ),
