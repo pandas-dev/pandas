e2df478e132739767082c0cd6c2f65d6bb523bf4
Matthew Roeschke <emailformattr@gmail.com>
2022-07-11 15:01:11 -0700

    Change to intp

--- pandas/_libs/arrays.pyx
+++ pandas/_libs/arrays.pyx
@@ -8,7 +8,7 @@ import numpy as np
 cimport numpy as cnp
 from cpython cimport PyErr_Clear
 from numpy cimport (
-    int64_t,
+    intp_t,
     ndarray,
 )
 
@@ -160,7 +160,7 @@ cdef class NDArrayBacked:
         return self._from_backing_data(res_values)
 
     # TODO: pass NPY_MAXDIMS equiv to axis=None?
-    def repeat(self, repeats, int64_t axis = 0):
+    def repeat(self, repeats, intp_t axis = 0):
         if axis is None:
             axis = 0
         res_values = cnp.PyArray_Repeat(self._ndarray, repeats, <int>axis)
