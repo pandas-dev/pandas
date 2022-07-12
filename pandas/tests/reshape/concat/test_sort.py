d1c47b6fb27b7d56cc2339e0fd2c151beac95b36
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-07-12 14:09:30 -0500

    Update test_sort.py

--- pandas/tests/reshape/concat/test_sort.py
+++ pandas/tests/reshape/concat/test_sort.py
@@ -92,7 +92,22 @@ class TestConcatSort:
         expected = DataFrame([[2, np.nan], [np.nan, 1]], index=[2, 1], columns=[2, 1])
 
         tm.assert_frame_equal(result, expected)
-
+        
+        # GH 37937
+        df1 = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, index=[1, 2, 3])
+        df2 = DataFrame({"c": [7, 8, 9], "d": [10, 11, 12]}, index=[3, 1, 6])
+        result = pd.concat([df2, df1], axis=1, sort=False)
+        expected = DataFrame(
+            [
+                [7.0, 10.0, 3.0, 6.0],
+                [8.0, 11.0, 1.0, 4.0],
+                [9.0, 12.0, np.nan, np.nan],
+                [np.nan, np.nan, 2.0, 5.0],
+            ],
+            index=[3, 1, 6, 2],
+            columns=["c", "d", "a", "b"],
+        )
+        
     def test_concat_sort_none_warning(self):
         # GH#41518
         df = DataFrame({1: [1, 2], "a": [3, 4]})
