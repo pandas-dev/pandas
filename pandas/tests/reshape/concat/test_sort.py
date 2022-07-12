46358b999598c1855d7c868c9ec43217ce9c58a8
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-07-12 16:40:08 -0500

    TST: avoid sort when concat int-index Dataframes with sort=False (#47685)
    * Update test_sort.py
    
    * Update test_sort.py
    
    * add assert

--- pandas/tests/reshape/concat/test_sort.py
+++ pandas/tests/reshape/concat/test_sort.py
@@ -93,6 +93,22 @@ class TestConcatSort:
 
         tm.assert_frame_equal(result, expected)
 
+        # GH 37937
+        df1 = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, index=[1, 2, 3])
+        df2 = DataFrame({"c": [7, 8, 9], "d": [10, 11, 12]}, index=[3, 1, 6])
+        result = pd.concat([df2, df1], axis=1, sort=False)
+        expected = DataFrame(
+            [
+                [7.0, 10.0, 3.0, 6.0],
+                [8.0, 11.0, 1.0, 4.0],
+                [9.0, 12.0, np.nan, np.nan],
+                [np.nan, np.nan, 2.0, 5.0],
+            ],
+            index=[3, 1, 6, 2],
+            columns=["c", "d", "a", "b"],
+        )
+        tm.assert_frame_equal(result, expected)
+
     def test_concat_sort_none_warning(self):
         # GH#41518
         df = DataFrame({1: [1, 2], "a": [3, 4]})
