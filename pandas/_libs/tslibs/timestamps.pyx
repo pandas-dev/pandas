37b449d90e8614e0eb02cdacb50901e9e4020581
Brock <jbrockmendel@gmail.com>
2022-07-12 15:16:10 -0700

    REF: move get_conversion_factor to np_datetime

--- pandas/_libs/tslibs/timestamps.pyx
+++ pandas/_libs/tslibs/timestamps.pyx
@@ -54,7 +54,6 @@ from pandas._libs.tslibs.conversion cimport (
     maybe_localize_tso,
 )
 from pandas._libs.tslibs.dtypes cimport (
-    get_conversion_factor,
     npy_unit_to_abbrev,
     periods_per_day,
     periods_per_second,
@@ -83,6 +82,7 @@ from pandas._libs.tslibs.np_datetime cimport (
     NPY_FR_ns,
     cmp_dtstructs,
     cmp_scalar,
+    get_conversion_factor,
     get_datetime64_unit,
     get_datetime64_value,
     get_unit_from_dtype,
