506d8b5a4b0448d434ba7ab7bedb2cca84005116
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-07-13 23:27:30 -0500

    Update test_fillna.py

--- pandas/tests/frame/methods/test_fillna.py
+++ pandas/tests/frame/methods/test_fillna.py
@@ -689,20 +689,20 @@ class TestFillNA:
         # GH 47713
         df = DataFrame(
             {
-                'col1': [5, 0, np.nan, 10, np.nan],
-                'col2': [7, np.nan, np.nan, 5, 3],
-                'col3': [12, np.nan, 1, 2, 0],
-                'col4': [np.nan, 1, 1, np.nan, 18],
+                "col1": [5, 0, np.nan, 10, np.nan],
+                "col2": [7, np.nan, np.nan, 5, 3],
+                "col3": [12, np.nan, 1, 2, 0],
+                "col4": [np.nan, 1, 1, np.nan, 18],
             }
         )
         result = df.fillna(50, limit=1, axis=1)
         expected = DataFrame(
             [
-                [5., 7., 12., 50.],
-                [0., 50., np.nan, 1.],
-                [50., np.nan, 1., 1.],
-                [10., 5., 2., 50.],
-                [50., 3., 0., 18.],
+                [5.0, 7.0, 12.0, 50.0],
+                [0.0, 50.0, np.nan, 1.0],
+                [50.0, np.nan, 1.0, 1.0],
+                [10.0, 5.0, 2.0, 50.0],
+                [50.0, 3.0, 0.0, 18.0],
             ],
             columns=["col1", "col2", "col3", "col4"],
         )
