dc4bec842cad96970c23c0fb5de4d848f5be2b70
Eli Dourado <elidourado@gmail.com>
2022-07-14 16:51:52 -0400

    Add Github discussion link as comment

--- pandas/plotting/_matplotlib/converter.py
+++ pandas/plotting/_matplotlib/converter.py
@@ -867,6 +867,7 @@ def _quarterly_finder(vmin, vmax, freq):
         info_fmt[year_start] = "%F"
 
     else:
+        # https://github.com/pandas-dev/pandas/pull/47602
         years = dates_[year_start] // 4 + 1970
         nyears = span / periodsperyear
         (min_anndef, maj_anndef) = _get_default_annual_spacing(nyears)
