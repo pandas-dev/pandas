5b39d0207e771c5cdcd407fdbaf35f4da0c8fb12
potato <93452403+Shadimrad@users.noreply.github.com>
2022-07-13 21:17:22 -0700

    TST

--- pandas/tests/frame/indexing/test_get_value.py
+++ pandas/tests/frame/indexing/test_get_value.py
@@ -1,5 +1,5 @@
 import pytest
-
+from enum import Enum
 from pandas import (
     DataFrame,
     MultiIndex,
@@ -20,3 +20,16 @@ class TestGetValue:
                 result = float_frame._get_value(idx, col)
                 expected = float_frame[col][idx]
                 assert result == expected
+
+def test_enum_value(self,):
+    Cols = Enum('Cols', 'col1 col2')
+
+    q1 = DataFrame({Cols.col1: [1, 2, 3]})
+    q2 = DataFrame({Cols.col1: [1, 2, 3]})
+
+    result=((q1[Cols.col1] == q2[Cols.col1]).all())
+
+    expected=True
+
+    assert result == expected
+
