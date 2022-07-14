007db1e973880a62f7ac36c1ec498c0517560a0b
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-07-14 15:41:37 -0500

    BUG: df.fillna ignores axis when df is single block (#47714)
    * Update generic.py
    
    * Update test_fillna.py
    
    * Update v1.5.0.rst
    
    * fix array format
    
    * fix what's new description
    
    * fix format
    
    * Update test_fillna.py

--- pandas/tests/frame/methods/test_fillna.py
+++ pandas/tests/frame/methods/test_fillna.py
@@ -685,6 +685,29 @@ class TestFillNA:
         tm.assert_frame_equal(df, expected)
         tm.assert_frame_equal(result_view, expected)
 
+    def test_single_block_df_with_horizontal_axis(self):
+        # GH 47713
+        df = DataFrame(
+            {
+                "col1": [5, 0, np.nan, 10, np.nan],
+                "col2": [7, np.nan, np.nan, 5, 3],
+                "col3": [12, np.nan, 1, 2, 0],
+                "col4": [np.nan, 1, 1, np.nan, 18],
+            }
+        )
+        result = df.fillna(50, limit=1, axis=1)
+        expected = DataFrame(
+            [
+                [5.0, 7.0, 12.0, 50.0],
+                [0.0, 50.0, np.nan, 1.0],
+                [50.0, np.nan, 1.0, 1.0],
+                [10.0, 5.0, 2.0, 50.0],
+                [50.0, 3.0, 0.0, 18.0],
+            ],
+            columns=["col1", "col2", "col3", "col4"],
+        )
+        tm.assert_frame_equal(result, expected)
+
 
 def test_fillna_nonconsolidated_frame():
     # https://github.com/pandas-dev/pandas/issues/36495
