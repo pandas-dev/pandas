9c4aaa5e92b84ac3b8c07304419bd2b921423c64
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-07-14 22:51:17 -0500

    fix flake

--- pandas/tests/groupby/test_function.py
+++ pandas/tests/groupby/test_function.py
@@ -1511,7 +1511,7 @@ def test_corrwith_with_1_axis():
     # GH 47723
     df = DataFrame({"a": [1, 1, 2], "b": [3, 7, 4]})
     result = df.groupby("a").corrwith(df, axis=1)
-    index = pd.Index(
+    index = Index(
         data=[(1, 0), (1, 1), (1, 2), (2, 2), (2, 0), (2, 1)],
         name=("a", None),
     )
