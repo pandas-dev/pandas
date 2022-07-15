63b88ac481ce0c156f4aff752adfda19c4edd5ae
Torsten Wörtwein <twoertwein@gmail.com>
2022-07-15 11:55:38 -0400

    line length

--- pandas/tests/__init__.py
+++ pandas/tests/__init__.py
@@ -1,6 +1,7 @@
 from warnings import warn
 
 warn(
-    "pandas.tests is considered to be private and will be renamed to pandas._tests in the future.",
+    "pandas.tests is considered to be private and will be renamed "
+    "to pandas._tests in the future.",
     DeprecationWarning,
 )
