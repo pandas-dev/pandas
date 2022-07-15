1cb253d8a5e4bff196edc0698b8db708b8db21de
ahmedibrhm <81244897+ahmedibrhm@users.noreply.github.com>
2022-07-15 08:00:13 -0700

    editing tests

--- pandas/tests/extension/test_string.py
+++ pandas/tests/extension/test_string.py
@@ -195,8 +195,8 @@ class TestPrinting(base.BasePrintingTests):
 
 
 class TestGroupBy(base.BaseGroupbyTests):
-    @pytest.mark.skip(reason="uses old format of groupby return GH #42795")
     def test_groupby_extension_transform(self, data_for_grouping, request):
+        pytest.skip("uses old format of groupby return GH #42795")
         super().test_groupby_extension_transform(data_for_grouping)
 
 
