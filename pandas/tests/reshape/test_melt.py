47d7c2e946468cd91f2f0f30ec385edb5fecd379
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-07-16 15:49:08 -0500

    Update test_melt.py

--- pandas/tests/reshape/test_melt.py
+++ pandas/tests/reshape/test_melt.py
@@ -1086,3 +1086,31 @@ class TestWideToLong:
         with tm.assert_produces_warning(FutureWarning):
             result = df.melt(id_vars="value")
             tm.assert_frame_equal(result, expected)
+
+    def test_missing_stubname(self):
+        # GH46044
+        df = DataFrame({"id": ["1", "2"], "a-1": [100, 200], "a-2": [300, 400]})
+        result = wide_to_long(
+            df,
+            stubnames=["a", "b"],
+            i="id",
+            j="num",
+            sep="-",
+        )
+        index = pd.Index(
+            [("1", 1), ("2", 1), ("1", 2), ("2", 2)],
+            name=("id", "num"),
+        )
+        expected = DataFrame(
+            {"a": [100, 200, 300, 400], "b": [np.nan] * 4},
+            index=index,
+        )
+        tm.assert_frame_equal(result, expected)
+        # test extension string array
+        df_str = df.astype({"id": "string"})
+        result_str = wide_to_long(
+            df_str, stubnames=["a", "b"], i="id", j="num", sep="-",
+        )
+        new_level = expected.index.levels[0].astype("string")
+        expected.index = expected.index.set_levels(new_level, level=0)
+        tm.assert_frame_equal(result_str, expected)
