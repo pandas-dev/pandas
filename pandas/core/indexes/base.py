a2304b8dd2fa51ce7ccad1e6386a269b077b7d6b
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-07-17 21:43:52 +0200

    REGR: fix pd.cut with datetime IntervalIndex as bins

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3981,8 +3981,11 @@ class Index(IndexOpsMixin, PandasObject):
         Should we attempt partial-matching indexing?
         """
         if is_interval_dtype(self.dtype):
+            if is_interval_dtype(target.dtype):
+                return False
             # "Index" has no attribute "left"
-            return self.left._should_compare(target)  # type: ignore[attr-defined]
+            # return self.left._should_compare(target)  # type: ignore[attr-defined]
+            return True
         return False
 
     @final
