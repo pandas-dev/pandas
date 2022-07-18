e36f60590e7456d442efb3056a56b509e78ebdfb
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-07-18 12:56:49 -0500

    Update sorting.py

--- pandas/core/sorting.py
+++ pandas/core/sorting.py
@@ -465,7 +465,7 @@ def nargminmax(values: ExtensionArray, method: str, axis: int = 0):
     assert method in {"argmax", "argmin"}
     func = np.argmax if method == "argmax" else np.argmin
 
-    mask = np.asarray(isna(values))
+    mask = values._mask_for_argsort()
     arr_values = values._values_for_argsort()
 
     if arr_values.ndim > 1:
