5dd83155d96862b84f4b17e7a77721ec20cdf868
ahmedibrhm <81244897+ahmedibrhm@users.noreply.github.com>
2022-07-18 07:58:57 -0700

    warning expected

--- pandas/tests/plotting/frame/test_hist_box_by.py
+++ pandas/tests/plotting/frame/test_hist_box_by.py
@@ -165,9 +165,10 @@ class TestHistWithBy(TestPlotBase):
         # GH 15079
         # _check_plot_works adds an ax so catch warning. see GH #13188
         with tm.assert_produces_warning(UserWarning):
-            axes = _check_plot_works(
-                hist_df.plot.hist, column=column, by=by, layout=layout
-            )
+            with tm.assert_produces_warning(FutureWarning):
+                axes = _check_plot_works(
+                    hist_df.plot.hist, column=column, by=by, layout=layout
+                )
         self._check_axes_shape(axes, axes_num=axes_num, layout=layout)
 
     @pytest.mark.parametrize(
