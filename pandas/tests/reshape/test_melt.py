3181525ed2ebfb0974e445e4806eb5ce691e56b4
Yuanhao Geng <41546976+GYHHAHA@users.noreply.github.com>
2022-07-18 13:25:28 -0500

    Update test_melt.py

--- pandas/tests/reshape/test_melt.py
+++ pandas/tests/reshape/test_melt.py
@@ -1087,9 +1087,11 @@ class TestWideToLong:
             result = df.melt(id_vars="value")
             tm.assert_frame_equal(result, expected)
 
-    def test_missing_stubname(self):
+    @pytest.mark.parametrize("dtype", ["O", "string"])
+    def test_missing_stubname(self, dtype):
         # GH46044
         df = DataFrame({"id": ["1", "2"], "a-1": [100, 200], "a-2": [300, 400]})
+        df = df.astype({"id": dtype})
         result = wide_to_long(
             df,
             stubnames=["a", "b"],
@@ -1105,16 +1107,6 @@ class TestWideToLong:
             {"a": [100, 200, 300, 400], "b": [np.nan] * 4},
             index=index,
         )
-        tm.assert_frame_equal(result, expected)
-        # test extension string array
-        df_str = df.astype({"id": "string"})
-        result_str = wide_to_long(
-            df_str,
-            stubnames=["a", "b"],
-            i="id",
-            j="num",
-            sep="-",
-        )
-        new_level = expected.index.levels[0].astype("string")
+        new_level = expected.index.levels[0].astype(dtype)
         expected.index = expected.index.set_levels(new_level, level=0)
-        tm.assert_frame_equal(result_str, expected)
+        tm.assert_frame_equal(result, expected)
