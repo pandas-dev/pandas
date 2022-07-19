941efd2a57de27ded47a74a5168c1f0760bef54e
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2022-07-19 09:10:02 -0500

    Backport PR #47763 on branch 1.4.x (BUG: fix regression in Series[string] setitem setting a scalar with a mask) (#47784)
    Backport PR #47763: BUG: fix regression in Series[string] setitem setting a scalar with a mask
    
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- pandas/tests/arrays/string_/test_string.py
+++ pandas/tests/arrays/string_/test_string.py
@@ -553,3 +553,23 @@ def test_isin(dtype, request, fixed_now_ts):
     result = s.isin(["a", fixed_now_ts])
     expected = pd.Series([True, False, False])
     tm.assert_series_equal(result, expected)
+
+
+def test_setitem_scalar_with_mask_validation(dtype):
+    # https://github.com/pandas-dev/pandas/issues/47628
+    # setting None with a boolean mask (through _putmaks) should still result
+    # in pd.NA values in the underlying array
+    ser = pd.Series(["a", "b", "c"], dtype=dtype)
+    mask = np.array([False, True, False])
+
+    ser[mask] = None
+    assert ser.array[1] is pd.NA
+
+    # for other non-string we should also raise an error
+    ser = pd.Series(["a", "b", "c"], dtype=dtype)
+    if type(ser.array) is pd.arrays.StringArray:
+        msg = "Cannot set non-string value"
+    else:
+        msg = "Scalar must be NA or str"
+    with pytest.raises(ValueError, match=msg):
+        ser[mask] = 1
