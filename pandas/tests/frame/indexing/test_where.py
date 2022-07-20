e92bb7c92d326753f954c3df2153fee903341677
Shaghayegh <93452403+Shadimrad@users.noreply.github.com>
2022-07-19 20:31:02 -0700

    Update test_where.py

--- pandas/tests/frame/indexing/test_where.py
+++ pandas/tests/frame/indexing/test_where.py
@@ -1055,4 +1055,5 @@ def test_consitency_inplace():
     df2 = pd.DataFrame({"M": [""]}, dtype="string")
     df2.where(df2 != "", np.nan, inplace=True)
     df = df.where(df != "", np.nan)
+    
     tm.assert_frame_equal(df,df2)
