4fe8b84c777e541bad7ba365540f959358608a8f
weikhor <cheanwei.KCW@gmail.com>
2022-07-26 15:21:42 +0800

    test

--- pandas/tests/computation/test_eval.py
+++ pandas/tests/computation/test_eval.py
@@ -1906,9 +1906,7 @@ def test_negate_lt_eq_le(engine, parser):
 def test_eval_no_support_column_name(request, column):
     # GH#44603
     if column in ["True", "False", "inf", "Inf"]:
-        request.node.add_marker(
-            pytest.mark.xfail(raises=ValueError, reason="ngroup not valid for NDFrame")
-        )
+        request.node.add_marker(pytest.mark.xfail(raises=ValueError))
 
     df = DataFrame(np.random.randint(0, 100, size=(10, 2)), columns=[column, "col1"])
     expected = df[df[column] > 6]
