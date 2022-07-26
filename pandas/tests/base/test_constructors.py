e27dc9a4ae61d1df4dc7cc1e0dd882aaa4f70e06
dannyi96 <danielbcbs2@gmail.com>
2022-07-26 11:52:57 +0530

    improving OutOfBoundsDatetime exception messages

--- pandas/tests/base/test_constructors.py
+++ pandas/tests/base/test_constructors.py
@@ -157,8 +157,7 @@ class TestConstruction:
 
         # Explicit dtype specified
         # Forced conversion fails for all -> all cases raise error
-        msg = "Out of bounds|" \
-            f"Cannot convert \"{a[0]}\" at position 0 to datetime"
+        msg = f'Out of bounds|Cannot convert "{a[0]}" at position 0 to datetime'
         with pytest.raises(pd.errors.OutOfBoundsDatetime, match=msg):
             klass(a, dtype="datetime64[ns]")
 
