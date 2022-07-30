4c7f5321e0dc9526f3b2680c13eed2c38f9c3404
Abdurrahmaan Iqbal <abdurrahmaaniqbal@hotmail.com>
2022-07-30 12:01:42 +0100

    Move unnecessary fixtures into test body

--- pandas/tests/io/test_html.py
+++ pandas/tests/io/test_html.py
@@ -111,58 +111,6 @@ class TestReadHtml:
     def banklist_data(self, datapath):
         return datapath("io", "data", "html", "banklist.html")
 
-    @pytest.fixture
-    def gh_13141_data(self):
-        return """
-          <table>
-            <tr>
-              <th>HTTP</th>
-              <th>FTP</th>
-              <th><a href="https://en.wiktionary.org/wiki/linkless">Linkless</a></th>
-            </tr>
-            <tr>
-              <td><a href="https://en.wikipedia.org/">Wikipedia</a></td>
-              <td>SURROUNDING <a href="ftp://ftp.us.debian.org/">Debian</a> TEXT</td>
-              <td>Linkless</td>
-            </tr>
-            <tfoot>
-              <tr>
-                <td><a href="https://en.wikipedia.org/wiki/Page_footer">Footer</a></td>
-                <td>
-                  Multiple <a href="1">links:</a> <a href="2">Only first captured.</a>
-                </td>
-              </tr>
-            </tfoot>
-          </table>
-          """
-
-    @pytest.fixture
-    def gh_13141_expected(self):
-        return {
-            "head_ignore": ["HTTP", "FTP", "Linkless"],
-            "head_extract": [
-                ("HTTP", None),
-                ("FTP", None),
-                ("Linkless", "https://en.wiktionary.org/wiki/linkless"),
-            ],
-            "body_ignore": ["Wikipedia", "SURROUNDING Debian TEXT", "Linkless"],
-            "body_extract": [
-                ("Wikipedia", "https://en.wikipedia.org/"),
-                ("SURROUNDING Debian TEXT", "ftp://ftp.us.debian.org/"),
-                ("Linkless", None),
-            ],
-            "footer_ignore": [
-                "Footer",
-                "Multiple links: Only first captured.",
-                None,
-            ],
-            "footer_extract": [
-                ("Footer", "https://en.wikipedia.org/wiki/Page_footer"),
-                ("Multiple links: Only first captured.", "1"),
-                None,
-            ],
-        }
-
     @pytest.fixture(autouse=True, scope="function")
     def set_defaults(self, flavor):
         self.read_html = partial(read_html, flavor=flavor)
@@ -1394,7 +1342,55 @@ class TestReadHtml:
         tm.assert_frame_equal(result, expected)
 
     @pytest.mark.parametrize("arg", ["all", "body", "header", "footer"])
-    def test_extract_links(self, gh_13141_data, gh_13141_expected, arg):
+    def test_extract_links(self, arg):
+        gh_13141_data = """
+          <table>
+            <tr>
+              <th>HTTP</th>
+              <th>FTP</th>
+              <th><a href="https://en.wiktionary.org/wiki/linkless">Linkless</a></th>
+            </tr>
+            <tr>
+              <td><a href="https://en.wikipedia.org/">Wikipedia</a></td>
+              <td>SURROUNDING <a href="ftp://ftp.us.debian.org/">Debian</a> TEXT</td>
+              <td>Linkless</td>
+            </tr>
+            <tfoot>
+              <tr>
+                <td><a href="https://en.wikipedia.org/wiki/Page_footer">Footer</a></td>
+                <td>
+                  Multiple <a href="1">links:</a> <a href="2">Only first captured.</a>
+                </td>
+              </tr>
+            </tfoot>
+          </table>
+          """
+
+        gh_13141_expected = {
+            "head_ignore": ["HTTP", "FTP", "Linkless"],
+            "head_extract": [
+                ("HTTP", None),
+                ("FTP", None),
+                ("Linkless", "https://en.wiktionary.org/wiki/linkless"),
+            ],
+            "body_ignore": ["Wikipedia", "SURROUNDING Debian TEXT", "Linkless"],
+            "body_extract": [
+                ("Wikipedia", "https://en.wikipedia.org/"),
+                ("SURROUNDING Debian TEXT", "ftp://ftp.us.debian.org/"),
+                ("Linkless", None),
+            ],
+            "footer_ignore": [
+                "Footer",
+                "Multiple links: Only first captured.",
+                None,
+            ],
+            "footer_extract": [
+                ("Footer", "https://en.wikipedia.org/wiki/Page_footer"),
+                ("Multiple links: Only first captured.", "1"),
+                None,
+            ],
+        }
+
         data_exp = gh_13141_expected["body_ignore"]
         foot_exp = gh_13141_expected["footer_ignore"]
         head_exp = gh_13141_expected["head_ignore"]
@@ -1413,10 +1409,10 @@ class TestReadHtml:
         expected = DataFrame([data_exp, foot_exp], columns=head_exp)
         tm.assert_frame_equal(result, expected)
 
-    def test_extract_links_bad(self, gh_13141_data):
+    def test_extract_links_bad(self, spam_data):
         msg = (
             "`extract_links` must be one of "
             '{None, "header", "footer", "body", "all"}, got "incorrect"'
         )
         with pytest.raises(ValueError, match=msg):
-            read_html(gh_13141_data, extract_links="incorrect")
+            read_html(spam_data, extract_links="incorrect")
