44068418f02691afe1b6f18074f534321d49acfe
shourya5 <shourya5@illinois.edu>
2022-08-02 04:30:34 -0500

    DOC : added some documentation regarding type annotations

--- pandas/_testing/_warnings.py
+++ pandas/_testing/_warnings.py
@@ -28,15 +28,15 @@ def assert_produces_warning(
 ):
     """
     Context manager for running code expected to either raise a specific
-    warning, or not raise any warnings. Verifies that the code raises the
-    expected warning, and that it does not raise any other unexpected
+    warning, multiple specific warnings , or not raise any warnings. Verifies that the code raises the
+    expected warnings, and that it does not raise any other unexpected
     warnings. It is basically a wrapper around ``warnings.catch_warnings``.
 
     Parameters
     ----------
-    expected_warning : {Warning, False, None}, default Warning
+    expected_warning : {Warning, False, Tuple[Warning, ...], None}, default Warning
         The type of Exception raised. ``exception.Warning`` is the base
-        class for all warnings. To check that no warning is returned,
+        class for all warnings. To raise multiple types of exceptions,pass them as a Tuple. To check that no warning is returned,
         specify ``False`` or ``None``.
     filter_level : str or None, default "always"
         Specifies whether warnings are ignored, displayed, or turned
