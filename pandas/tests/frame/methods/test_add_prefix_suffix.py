85459965a7e5054e8ba429d37404039cec9c66cd
Brock <jbrockmendel@gmail.com>
2022-08-02 16:11:23 -0700

    ENH: copy kwd for add_suffix/add_prefix

--- pandas/tests/frame/methods/test_add_prefix_suffix.py
+++ pandas/tests/frame/methods/test_add_prefix_suffix.py
@@ -18,3 +18,37 @@ def test_add_prefix_suffix(float_frame):
     with_pct_suffix = float_frame.add_suffix("%")
     expected = Index([f"{c}%" for c in float_frame.columns])
     tm.assert_index_equal(with_pct_suffix.columns, expected)
+
+
+def test_add_prefix_suffix_copy(float_frame):
+    with_prefix = float_frame.add_prefix("foo#", copy=True)
+    expected = Index([f"foo#{c}" for c in float_frame.columns])
+    tm.assert_index_equal(with_prefix.columns, expected)
+    assert not any(
+        tm.shares_memory(float_frame.iloc[:, i], with_prefix.iloc[:, i])
+        for i in range(float_frame.shape[1])
+    )
+
+    with_prefix = float_frame.add_prefix("foo#", copy=False)
+    expected = Index([f"foo#{c}" for c in float_frame.columns])
+    tm.assert_index_equal(with_prefix.columns, expected)
+    assert all(
+        tm.shares_memory(float_frame.iloc[:, i], with_prefix.iloc[:, i])
+        for i in range(float_frame.shape[1])
+    )
+
+    with_suffix = float_frame.add_suffix("#foo", copy=True)
+    expected = Index([f"{c}#foo" for c in float_frame.columns])
+    tm.assert_index_equal(with_suffix.columns, expected)
+    assert not any(
+        tm.shares_memory(float_frame.iloc[:, i], with_suffix.iloc[:, i])
+        for i in range(float_frame.shape[1])
+    )
+
+    with_suffix = float_frame.add_suffix("#foo", copy=False)
+    expected = Index([f"{c}#foo" for c in float_frame.columns])
+    tm.assert_index_equal(with_suffix.columns, expected)
+    assert all(
+        tm.shares_memory(float_frame.iloc[:, i], with_suffix.iloc[:, i])
+        for i in range(float_frame.shape[1])
+    )
