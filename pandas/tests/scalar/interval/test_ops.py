48d9aa452fe7a68a3245b442cc0cc1630f7ccc32
Kapil <90269125+kapiliyer@users.noreply.github.com>
2022-08-08 21:49:37 -0400

    ENH: Support For Interval __contains__ Other Interval (#46613)

--- pandas/tests/scalar/interval/test_ops.py
+++ pandas/tests/scalar/interval/test_ops.py
@@ -69,17 +69,13 @@ class TestOverlaps:
 
 
 class TestContains:
-    @pytest.mark.parametrize(
-        "closed",
-        ["neither", "left", "right"],
-    )
-    def test_contains_interval(self, closed):
+    def test_contains_interval(self, inclusive_endpoints_fixture):
         interval1 = Interval(0, 1, "both")
-        interval2 = Interval(0, 1, closed)
+        interval2 = Interval(0, 1, inclusive_endpoints_fixture)
         assert interval1 in interval1
         assert interval2 in interval2
         assert interval2 in interval1
-        assert interval1 not in interval2
+        assert interval1 not in interval2 or inclusive_endpoints_fixture == "both"
 
     def test_contains_infinite_length(self):
         interval1 = Interval(0, 1, "both")
