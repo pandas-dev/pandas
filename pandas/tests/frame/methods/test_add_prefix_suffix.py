609dcd387063e94d779a11c18ca3e0790dd2d1fd
Brock <jbrockmendel@gmail.com>
2022-08-09 08:47:52 -0700

    test series

--- pandas/tests/frame/methods/test_add_prefix_suffix.py
+++ pandas/tests/frame/methods/test_add_prefix_suffix.py
@@ -22,6 +22,8 @@ def test_add_prefix_suffix(float_frame):
 
 def test_add_prefix_suffix_copy(float_frame):
     # GH#47934
+    ser = float_frame.iloc[0]
+
     with_prefix = float_frame.add_prefix("foo#", copy=True)
     expected = Index([f"foo#{c}" for c in float_frame.columns])
     tm.assert_index_equal(with_prefix.columns, expected)
@@ -29,6 +31,10 @@ def test_add_prefix_suffix_copy(float_frame):
         tm.shares_memory(float_frame.iloc[:, i], with_prefix.iloc[:, i])
         for i in range(float_frame.shape[1])
     )
+
+    ser_with_prefix = ser.add_prefix("foo#", copy=True)
+    tm.assert_index_equal(ser_with_prefix.index, expected)
+    assert not tm.shares_memory(ser_with_prefix, ser)
 
     with_prefix = float_frame.add_prefix("foo#", copy=False)
     expected = Index([f"foo#{c}" for c in float_frame.columns])
@@ -38,6 +44,10 @@ def test_add_prefix_suffix_copy(float_frame):
         for i in range(float_frame.shape[1])
     )
 
+    ser_with_prefix = ser.add_prefix("foo#", copy=False)
+    tm.assert_index_equal(ser_with_prefix.index, expected)
+    assert tm.shares_memory(ser_with_prefix, ser)
+
     with_suffix = float_frame.add_suffix("#foo", copy=True)
     expected = Index([f"{c}#foo" for c in float_frame.columns])
     tm.assert_index_equal(with_suffix.columns, expected)
@@ -46,6 +56,10 @@ def test_add_prefix_suffix_copy(float_frame):
         for i in range(float_frame.shape[1])
     )
 
+    ser_with_suffix = ser.add_suffix("#foo", copy=True)
+    tm.assert_index_equal(ser_with_suffix.index, expected)
+    assert not tm.shares_memory(ser_with_suffix, ser)
+
     with_suffix = float_frame.add_suffix("#foo", copy=False)
     expected = Index([f"{c}#foo" for c in float_frame.columns])
     tm.assert_index_equal(with_suffix.columns, expected)
@@ -53,3 +67,7 @@ def test_add_prefix_suffix_copy(float_frame):
         tm.shares_memory(float_frame.iloc[:, i], with_suffix.iloc[:, i])
         for i in range(float_frame.shape[1])
     )
+
+    ser_with_suffix = ser.add_suffix("#foo", copy=False)
+    tm.assert_index_equal(ser_with_suffix.index, expected)
+    assert tm.shares_memory(ser_with_suffix, ser)
