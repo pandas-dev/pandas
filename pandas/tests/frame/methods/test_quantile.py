a5977dc16b5619d965776e919156aeff8dc7e844
Matthew Roeschke <emailformattr@gmail.com>
2022-08-10 15:00:43 -0700

    Improve assertin of test_quantile

--- pandas/tests/frame/methods/test_quantile.py
+++ pandas/tests/frame/methods/test_quantile.py
@@ -84,19 +84,35 @@ class TestDataFrameQuantile:
     def test_quantile(self, datetime_frame, interp_method):
         interpolation, method = interp_method
         df = datetime_frame
-        q = df.quantile(
+        result = df.quantile(
             0.1, axis=0, numeric_only=True, interpolation=interpolation, method=method
         )
-        if method == "single":
-            assert q["A"] == np.percentile(df["A"], 10)
-        tm.assert_index_equal(q.index, df.columns)
+        expected = Series(
+            [np.percentile(df[col], 10) for col in df.columns],
+            index=df.columns,
+            name=0.1,
+        )
+        if interpolation == "linear":
+            # np.percentile values only comparable to linear interpolation
+            tm.assert_series_equal(result, expected)
+        else:
+            tm.assert_index_equal(result.index, expected.index)
+            assert result.name == expected.name
 
-        q = df.quantile(
+        result = df.quantile(
             0.9, axis=1, numeric_only=True, interpolation=interpolation, method=method
         )
-        if method == "single":
-            assert q["2000-01-17"] == np.percentile(df.loc["2000-01-17"], 90)
-        tm.assert_index_equal(q.index, df.index)
+        expected = Series(
+            [np.percentile(df.loc[date], 90) for date in df.index],
+            index=df.index,
+            name=0.9,
+        )
+        if interpolation == "linear":
+            # np.percentile values only comparable to linear interpolation
+            tm.assert_series_equal(result, expected)
+        else:
+            tm.assert_index_equal(result.index, expected.index)
+            assert result.name == expected.name
 
     def test_empty(self, interp_method):
         interpolation, method = interp_method
