55c2443171a5191b4c4bc74975d07f5160ef91ec
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-08-11 19:23:52 +0200

    ENH: Support mask in groupby sum (#48018)
    * ENH: Support mask in groupby sum
    
    * ENH: Support mask in groupby sum
    
    * Fix mypy
    
    * Refactor if condition

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -155,6 +155,7 @@ class WrappedCythonOp:
         "last",
         "first",
         "rank",
+        "sum",
     }
 
     _cython_arity = {"ohlc": 4}  # OHLC
@@ -217,7 +218,7 @@ class WrappedCythonOp:
             values = ensure_float64(values)
 
         elif values.dtype.kind in ["i", "u"]:
-            if how in ["sum", "var", "prod", "mean", "ohlc"] or (
+            if how in ["var", "prod", "mean", "ohlc"] or (
                 self.kind == "transform" and self.has_dropped_na
             ):
                 # result may still include NaN, so we have to cast
@@ -578,6 +579,8 @@ class WrappedCythonOp:
                     counts=counts,
                     values=values,
                     labels=comp_ids,
+                    mask=mask,
+                    result_mask=result_mask,
                     min_count=min_count,
                     is_datetimelike=is_datetimelike,
                 )
@@ -613,7 +616,8 @@ class WrappedCythonOp:
             # need to have the result set to np.nan, which may require casting,
             # see GH#40767
             if is_integer_dtype(result.dtype) and not is_datetimelike:
-                cutoff = max(1, min_count)
+                # Neutral value for sum is 0, so don't fill empty groups with nan
+                cutoff = max(0 if self.how == "sum" else 1, min_count)
                 empty_groups = counts < cutoff
                 if empty_groups.any():
                     if result_mask is not None and self.uses_mask():
