fe7da63e6317ae0292d6b6f965b3b19be82a7c77
th3nn3ss <chuksmcdennis@yahoo.com>
2022-08-11 12:54:02 +0100

    fix PEP 8 issues 'line too long'

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -4873,7 +4873,8 @@ Keep all original rows and also all original values
         level : int or level name, default None
             In case of MultiIndex, only rename labels in the specified level.
         errors : {'ignore', 'raise'}, default 'ignore'
-            If 'raise', raise `KeyError` when a `dict-like mapper` or `index` contains labels that are not present in the index being transformed.
+            If 'raise', raise `KeyError` when a `dict-like mapper` or 
+            `index` contains labels that are not present in the index being transformed.
             If 'ignore', existing keys will be renamed and extra keys will be ignored.
 
         Returns
