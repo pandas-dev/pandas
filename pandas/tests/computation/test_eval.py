1aedbf95a5334de9126419950726c91ac73d1bb7
Khor Chean Wei <cheanwei.KCW@gmail.com>
2022-08-12 08:15:48 +0800

    Error raised message: DataFrame eval will not work with 'Timestamp' column name (#47273)

--- pandas/tests/computation/test_eval.py
+++ pandas/tests/computation/test_eval.py
@@ -49,6 +49,7 @@ from pandas.core.computation.ops import (
     _binary_ops_dict,
     _unary_math_ops,
 )
+from pandas.core.computation.scope import DEFAULT_GLOBALS
 
 
 @pytest.fixture(
@@ -1888,6 +1889,27 @@ def test_negate_lt_eq_le(engine, parser):
     else:
         result = df.query("not (cat > 0)", engine=engine, parser=parser)
         tm.assert_frame_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "column",
+    DEFAULT_GLOBALS.keys(),
+)
+def test_eval_no_support_column_name(request, column):
+    # GH 44603
+    if column in ["True", "False", "inf", "Inf"]:
+        request.node.add_marker(
+            pytest.mark.xfail(
+                raises=KeyError,
+                reason=f"GH 47859 DataFrame eval not supported with {column}",
+            )
+        )
+
+    df = DataFrame(np.random.randint(0, 100, size=(10, 2)), columns=[column, "col1"])
+    expected = df[df[column] > 6]
+    result = df.query(f"{column}>6")
+
+    tm.assert_frame_equal(result, expected)
 
 
 class TestValidate:
