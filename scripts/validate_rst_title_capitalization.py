e92d4a518e47185c7a90e12e803fc0695907e1d4
INDIG0N <hicksjordan1999@gmail.com>
2022-08-15 16:48:10 -0400

    main function now handles removing links to ensure they are not considered for fixing capitalization.

--- scripts/validate_rst_title_capitalization.py
+++ scripts/validate_rst_title_capitalization.py
@@ -185,12 +185,8 @@ def correct_title_capitalization(title: str) -> str:
     # first word character.
     correct_title: str = re.sub(r"^\W*", "", title).capitalize()
 
-    # Remove a URL from the title. We do this because words in a URL must
-    # stay lowercase, even if they are a capitalization exception.
-    removed_https_title = re.sub(r"<https?:\/\/.*[\r\n]*>", "", correct_title)
-
     # Split a title into a list using non-word character delimiters.
-    word_list = re.split(r"\W", removed_https_title)
+    word_list = re.split(r"\W", correct_title)
 
     for word in word_list:
         if word.lower() in CAP_EXCEPTIONS_DICT:
@@ -256,10 +252,11 @@ def main(source_paths: list[str]) -> int:
 
     for filename in source_paths:
         for title, line_number in find_titles(filename):
-            if title != correct_title_capitalization(title):
+            removed_https_title = re.sub(r"<https?:\/\/.*[\r\n]*>", "", title)
+            if removed_https_title != correct_title_capitalization(removed_https_title):
                 print(
-                    f"""{filename}:{line_number}:{err_msg} "{title}" to "{
-                    correct_title_capitalization(title)}" """
+                    f"""{filename}:{line_number}:{err_msg} "{removed_https_title.strip()}" to "{
+                    correct_title_capitalization(removed_https_title).strip()}" """
                 )
                 number_of_errors += 1
 
