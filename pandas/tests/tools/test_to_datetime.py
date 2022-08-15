c3bbb4f8daa897c3b0baf581fbeaeb47eeac0340
dannyi96 <danielbcbs2@gmail.com>
2022-08-15 23:56:15 +0530

    testcase updates

--- pandas/tests/tools/test_to_datetime.py
+++ pandas/tests/tools/test_to_datetime.py
@@ -2220,7 +2220,7 @@ class TestDaysInMonth:
 
     @pytest.mark.parametrize("arg", ["2015-02-29", "2015-02-32", "2015-04-31"])
     def test_day_not_in_month_raise_value(self, cache, arg):
-        msg = f"time data \"{arg}\" at position 0 doesn't match format specified"
+        msg = f'time data "{arg}" at position 0 doesn\'t match format specified'
         with pytest.raises(ValueError, match=msg):
             to_datetime(arg, errors="raise", format="%Y-%m-%d", cache=cache)
 
