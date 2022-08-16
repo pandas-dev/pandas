ca359fd439df02de45926b96d685037f2caa9b42
Brock <jbrockmendel@gmail.com>
2022-08-16 12:24:52 -0700

    DEPR: inplace kwarg in set_index

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -5776,7 +5776,7 @@ class DataFrame(NDFrame, OpsMixin):
         *,
         drop: bool = ...,
         append: bool = ...,
-        inplace: Literal[False] = ...,
+        inplace: Literal[False] | lib.NoDefault = ...,
         verify_integrity: bool = ...,
         copy: bool | lib.NoDefault = ...,
     ) -> DataFrame:
@@ -5801,7 +5801,7 @@ class DataFrame(NDFrame, OpsMixin):
         keys,
         drop: bool = True,
         append: bool = False,
-        inplace: bool = False,
+        inplace: bool | lib.NoDefault = lib.no_default,
         verify_integrity: bool = False,
         copy: bool | lib.NoDefault = lib.no_default,
     ) -> DataFrame | None:
@@ -5826,6 +5826,9 @@ class DataFrame(NDFrame, OpsMixin):
             Whether to append columns to existing index.
         inplace : bool, default False
             Whether to modify the DataFrame rather than creating a new one.
+
+            .. deprecated:: 1.5.0
+
         verify_integrity : bool, default False
             Check the new index for duplicates. Otherwise defer the check until
             necessary. Setting to False will improve the performance of this
@@ -5899,7 +5902,18 @@ class DataFrame(NDFrame, OpsMixin):
         3 9       7  2013    84
         4 16     10  2014    31
         """
-        inplace = validate_bool_kwarg(inplace, "inplace")
+        if inplace is not lib.no_default:
+            inplace = validate_bool_kwarg(inplace, "inplace")
+            warnings.warn(
+                "The 'inplace' keyword in DataFrame.set_index is deprecated "
+                "and will be removed in a future version. Use "
+                "`df = df.set_index(..., copy=False)` instead.",
+                FutureWarning,
+                stacklevel=find_stack_level(inspect.currentframe()),
+            )
+        else:
+            inplace = False
+
         if inplace:
             if copy is not lib.no_default:
                 raise ValueError("Cannot specify copy when inplace=True")
--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -742,9 +742,9 @@ class _MergeOperation:
         if self.indicator:
             result = self._indicator_post_merge(result)
 
-        self._maybe_add_join_keys(result, left_indexer, right_indexer)
+        result = self._maybe_add_join_keys(result, left_indexer, right_indexer)
 
-        self._maybe_restore_index_levels(result)
+        result = self._maybe_restore_index_levels(result)
 
         self._maybe_drop_cross_column(result, self._cross)
 
@@ -811,7 +811,7 @@ class _MergeOperation:
         result = result.drop(labels=["_left_indicator", "_right_indicator"], axis=1)
         return result
 
-    def _maybe_restore_index_levels(self, result: DataFrame) -> None:
+    def _maybe_restore_index_levels(self, result: DataFrame) -> DataFrame:
         """
         Restore index levels specified as `on` parameters
 
@@ -829,7 +829,7 @@ class _MergeOperation:
 
         Returns
         -------
-        None
+        DataFrame
         """
         names_to_restore = []
         for name, left_key, right_key in zip(
@@ -853,14 +853,15 @@ class _MergeOperation:
                 names_to_restore.append(name)
 
         if names_to_restore:
-            result.set_index(names_to_restore, inplace=True)
+            result = result.set_index(names_to_restore, copy=False)
+        return result
 
     def _maybe_add_join_keys(
         self,
         result: DataFrame,
         left_indexer: np.ndarray | None,
         right_indexer: np.ndarray | None,
-    ) -> None:
+    ) -> DataFrame:
 
         left_has_missing = None
         right_has_missing = None
@@ -956,11 +957,12 @@ class _MergeOperation:
                             for level_name in result.index.names
                         ]
 
-                        result.set_index(idx_list, inplace=True)
+                        result = result.set_index(idx_list, copy=False)
                     else:
                         result.index = Index(key_col, name=name)
                 else:
                     result.insert(i, name or f"key_{i}", key_col)
+        return result
 
     def _get_join_indexers(self) -> tuple[npt.NDArray[np.intp], npt.NDArray[np.intp]]:
         """return the join indexers"""
@@ -1738,7 +1740,7 @@ class _OrderedMerge(_MergeOperation):
         typ = self.left._constructor
         result = typ(result_data)
 
-        self._maybe_add_join_keys(result, left_indexer, right_indexer)
+        result = self._maybe_add_join_keys(result, left_indexer, right_indexer)
 
         return result
 
