b4d7346fe1d151780fc0506cc0678eb7637e0b70
Yasin Tatar <yasin.tatar@quantco.com>
2022-08-16 19:58:35 +0200

    rewrite test: no type hinting + context manager call

--- pandas/tests/io/sas/test_sas7bdat.py
+++ pandas/tests/io/sas/test_sas7bdat.py
@@ -137,19 +137,17 @@ def test_encoding_options(datapath):
 
 
 def test_encoding_infer(datapath):
-    from pandas.io.sas.sas7bdat import SAS7BDATReader
-
     fname = datapath("io", "sas", "data", "test1.sas7bdat")
 
-    # check if inferred correctly
-    df1_reader = pd.read_sas(fname, encoding="infer", iterator=True)
-    assert df1_reader.inferred_encoding == "cp1252"
+    with pd.read_sas(fname, encoding="infer", iterator=True) as df1_reader:
+        # check: is encoding inferred correctly from file
+        assert df1_reader.inferred_encoding == "cp1252"
+        df1 = df1_reader.read()
 
-    # check if the reader reads correctly with encoding
-    df1 = df1_reader.read()
-    df2_reader: SAS7BDATReader = pd.read_sas(fname, encoding="cp1252", iterator=True)
-    df2 = df2_reader.read()
+    with pd.read_sas(fname, encoding="cp1252", iterator=True) as df2_reader:
+        df2 = df2_reader.read()
 
+    # check: reader reads correct information
     tm.assert_frame_equal(df1, df2)
 
 
