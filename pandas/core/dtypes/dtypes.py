c9d4d3d474b70744cf25d36b0dbeaf356f828eaa
Matthew Roeschke <emailformattr@gmail.com>
2022-08-17 10:58:50 -0700

    Fix remaining tests

--- pandas/core/dtypes/dtypes.py
+++ pandas/core/dtypes/dtypes.py
@@ -1077,7 +1077,8 @@ class IntervalDtype(PandasExtensionDtype):
     )
 
     _match = re.compile(
-        r"(I|i)nterval\[(?P<subtype>[^,]+)(, (?P<closed>(right|left|both|neither)))?\]"
+        r"(I|i)nterval\[(?P<subtype>[^,]+(\[.+\])?)"
+        r"(, (?P<closed>(right|left|both|neither)))?\]"
     )
 
     _cache_dtypes: dict[str_type, PandasExtensionDtype] = {}
