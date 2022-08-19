25f944340662d381283d8a700a306a0e10bb2de0
Matthew Roeschke <emailformattr@gmail.com>
2022-08-19 11:41:28 -0700

    BUG/REGR: Fix subset for DataFrameGroupBy.value_counts

--- pandas/tests/groupby/test_frame_value_counts.py
+++ pandas/tests/groupby/test_frame_value_counts.py
@@ -738,3 +738,20 @@ def test_ambiguous_grouping():
     result = gb.value_counts()
     expected = Series([2], index=MultiIndex.from_tuples([[1, 1]], names=[None, "a"]))
     tm.assert_series_equal(result, expected)
+
+
+def test_subset_overlaps_gb_key_raises():
+    # GH 46383
+    df = DataFrame({"c1": ["a", "b", "c"], "c2": ["x", "y", "y"]}, index=[0, 1, 1])
+    with pytest.raises(ValueError, match="Keys {'c1'}"):
+        df.groupby("c1").value_counts(subset=["c1"])
+
+
+def test_subset():
+    # GH 46383
+    df = DataFrame({"c1": ["a", "b", "c"], "c2": ["x", "y", "y"]}, index=[0, 1, 1])
+    result = df.groupby(level=0).value_counts(subset=["c2"])
+    expected = Series(
+        [1, 2], index=MultiIndex.from_arrays([[0, 1], ["x", "y"]], names=[None, "c2"])
+    )
+    tm.assert_series_equal(result, expected)
