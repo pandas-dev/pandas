6a205dd9907b24da34f5efb8d01d16e1740a305a
Matthew Roeschke <emailformattr@gmail.com>
2022-08-19 14:17:14 -0700

    TST: Filter/test pyarrow PerformanceWarnings (#48093)

--- pandas/tests/base/test_value_counts.py
+++ pandas/tests/base/test_value_counts.py
@@ -4,6 +4,9 @@ from datetime import timedelta
 import numpy as np
 import pytest
 
+from pandas.compat import pa_version_under7p0
+from pandas.errors import PerformanceWarning
+
 import pandas as pd
 from pandas import (
     DatetimeIndex,
@@ -36,8 +39,16 @@ def test_value_counts(index_or_series_obj):
     # TODO(GH#32514): Order of entries with the same count is inconsistent
     #  on CI (gh-32449)
     if obj.duplicated().any():
-        result = result.sort_index()
-        expected = expected.sort_index()
+        with tm.maybe_produces_warning(
+            PerformanceWarning,
+            pa_version_under7p0 and getattr(obj.dtype, "storage", "") == "pyarrow",
+        ):
+            result = result.sort_index()
+        with tm.maybe_produces_warning(
+            PerformanceWarning,
+            pa_version_under7p0 and getattr(obj.dtype, "storage", "") == "pyarrow",
+        ):
+            expected = expected.sort_index()
     tm.assert_series_equal(result, expected)
 
 
@@ -70,8 +81,16 @@ def test_value_counts_null(null_obj, index_or_series_obj):
     if obj.duplicated().any():
         # TODO(GH#32514):
         #  Order of entries with the same count is inconsistent on CI (gh-32449)
-        expected = expected.sort_index()
-        result = result.sort_index()
+        with tm.maybe_produces_warning(
+            PerformanceWarning,
+            pa_version_under7p0 and getattr(obj.dtype, "storage", "") == "pyarrow",
+        ):
+            expected = expected.sort_index()
+        with tm.maybe_produces_warning(
+            PerformanceWarning,
+            pa_version_under7p0 and getattr(obj.dtype, "storage", "") == "pyarrow",
+        ):
+            result = result.sort_index()
 
     if not isinstance(result.dtype, np.dtype):
         # i.e IntegerDtype
@@ -84,8 +103,16 @@ def test_value_counts_null(null_obj, index_or_series_obj):
     if obj.duplicated().any():
         # TODO(GH#32514):
         #  Order of entries with the same count is inconsistent on CI (gh-32449)
-        expected = expected.sort_index()
-        result = result.sort_index()
+        with tm.maybe_produces_warning(
+            PerformanceWarning,
+            pa_version_under7p0 and getattr(obj.dtype, "storage", "") == "pyarrow",
+        ):
+            expected = expected.sort_index()
+        with tm.maybe_produces_warning(
+            PerformanceWarning,
+            pa_version_under7p0 and getattr(obj.dtype, "storage", "") == "pyarrow",
+        ):
+            result = result.sort_index()
     tm.assert_series_equal(result, expected)
 
 
