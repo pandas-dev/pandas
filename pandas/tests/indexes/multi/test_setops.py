3e8b0dab00f13d8a95964863d07a239938a25774
Matthew Roeschke <emailformattr@gmail.com>
2022-08-18 21:13:59 -0700

    Catch more test warnings

--- pandas/tests/indexes/multi/test_setops.py
+++ pandas/tests/indexes/multi/test_setops.py
@@ -571,5 +571,8 @@ def test_intersection_lexsort_depth(levels1, levels2, codes1, codes2, names):
     mi1 = MultiIndex(levels=levels1, codes=codes1, names=names)
     mi2 = MultiIndex(levels=levels2, codes=codes2, names=names)
     mi_int = mi1.intersection(mi2)
-
-    assert mi_int.lexsort_depth == 0
+    with tm.assert_produces_warning(
+        FutureWarning,
+        match="MultiIndex.lexsort_depth is deprecated as a public function",
+    ):
+        assert mi_int.lexsort_depth == 0
