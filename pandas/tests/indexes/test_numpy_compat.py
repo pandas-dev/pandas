8abd0586edc479de92380c9bcc3685fb1aecf736
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-08-22 21:55:00 +0200

    Backport PR #48149: REGR: fix calling numpy bitwise ufunc with Index objects

--- pandas/tests/indexes/test_numpy_compat.py
+++ pandas/tests/indexes/test_numpy_compat.py
@@ -146,3 +146,16 @@ def test_numpy_ufuncs_reductions(index, func, request):
         assert isna(expected)
     else:
         assert result == expected
+
+
+@pytest.mark.parametrize("func", [np.bitwise_and, np.bitwise_or, np.bitwise_xor])
+def test_numpy_ufuncs_bitwise(func):
+    # https://github.com/pandas-dev/pandas/issues/46769
+    idx1 = Index([1, 2, 3, 4], dtype="int64")
+    idx2 = Index([3, 4, 5, 6], dtype="int64")
+
+    with tm.assert_produces_warning(None):
+        result = func(idx1, idx2)
+
+    expected = Index(func(idx1.values, idx2.values))
+    tm.assert_index_equal(result, expected)
