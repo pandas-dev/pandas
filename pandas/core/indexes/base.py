2adbd92c3132adf5511e492ea034657506a0e975
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-08-22 21:46:20 +0200

    Backport PR #48058: REGR: fix reset_index (Index.insert) regression with custom Index subclasses

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -6614,9 +6614,12 @@ class Index(IndexOpsMixin, PandasObject):
             loc = loc if loc >= 0 else loc - 1
             new_values[loc] = item
 
-        # Use self._constructor instead of Index to retain NumericIndex GH#43921
-        # TODO(2.0) can use Index instead of self._constructor
-        return self._constructor._with_infer(new_values, name=self.name)
+        if self._typ == "numericindex":
+            # Use self._constructor instead of Index to retain NumericIndex GH#43921
+            # TODO(2.0) can use Index instead of self._constructor
+            return self._constructor._with_infer(new_values, name=self.name)
+        else:
+            return Index._with_infer(new_values, name=self.name)
 
     def drop(self, labels, errors: str_t = "raise") -> Index:
         """
