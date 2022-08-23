319338831edbcc173f79e75eeca5ef7fda5ccdd3
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-08-23 16:58:28 +0200

    Backport PR #48176: REGR: ensure DataFrame.select_dtypes() returns a copy

--- pandas/tests/frame/methods/test_select_dtypes.py
+++ pandas/tests/frame/methods/test_select_dtypes.py
@@ -456,3 +456,12 @@ class TestSelectDtypes:
         result = df.select_dtypes(include="number")
         expected = DataFrame({"a": [1, 2, 3]})
         tm.assert_frame_equal(result, expected)
+
+    def test_select_dtypes_no_view(self):
+        # https://github.com/pandas-dev/pandas/issues/48090
+        # result of this method is not a view on the original dataframe
+        df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+        df_orig = df.copy()
+        result = df.select_dtypes(include=["number"])
+        result.iloc[0, 0] = 0
+        tm.assert_frame_equal(df, df_orig)
