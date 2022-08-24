3e938c24f28b032f593aa47018360dad558dbf07
Simon Hawkins <simonjayhawkins@gmail.com>
2022-08-24 17:11:30 +0100

    Backport PR #48176 on branch 1.4.x (REGR: ensure DataFrame.select_dtypes() returns a copy) (#48219)

--- pandas/tests/frame/methods/test_select_dtypes.py
+++ pandas/tests/frame/methods/test_select_dtypes.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pytest
 
+import pandas.util._test_decorators as td
+
 from pandas.core.dtypes.dtypes import ExtensionDtype
 
 import pandas as pd
@@ -456,3 +458,13 @@ class TestSelectDtypes:
         result = df.select_dtypes(include="number")
         expected = DataFrame({"a": [1, 2, 3]})
         tm.assert_frame_equal(result, expected)
+
+    @td.skip_array_manager_invalid_test
+    def test_select_dtypes_no_view(self):
+        # https://github.com/pandas-dev/pandas/issues/48090
+        # result of this method is not a view on the original dataframe
+        df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+        df_orig = df.copy()
+        result = df.select_dtypes(include=["number"])
+        result.iloc[0, 0] = 0
+        tm.assert_frame_equal(df, df_orig)
