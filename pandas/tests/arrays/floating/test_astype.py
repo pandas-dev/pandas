7452030575a1321b722fa831afb21859c034436c
Steven Rotondo <97266896+srotondo@users.noreply.github.com>
2022-08-26 11:34:39 -0700

    TST: Wrote test for Float64 conversion #40729 (#48205)
    * TST: Wrote test for Float64 conversion #40729
    
    * TST: Moved test #40729
    
    Co-authored-by: Steven Rotondo <steven.rotondo75@gmail.com>

--- pandas/tests/arrays/floating/test_astype.py
+++ pandas/tests/arrays/floating/test_astype.py
@@ -116,3 +116,13 @@ def test_astype_object(dtype):
     # check exact element types
     assert isinstance(result[0], float)
     assert result[1] is pd.NA
+
+
+def test_Float64_conversion():
+    # GH#40729
+    testseries = pd.Series(["1", "2", "3", "4"], dtype="object")
+    result = testseries.astype(pd.Float64Dtype())
+
+    expected = pd.Series([1.0, 2.0, 3.0, 4.0], dtype=pd.Float64Dtype())
+
+    tm.assert_series_equal(result, expected)
