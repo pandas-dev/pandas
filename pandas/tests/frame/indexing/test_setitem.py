c40c48c2793b78e61911a574bd7627fc4fbbf824
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2022-08-26 16:11:25 +0200

    Backport PR #48215 on branch 1.4.x (REGR: properly update DataFrame cache in Series.__setitem__) (#48266)
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- pandas/tests/frame/indexing/test_setitem.py
+++ pandas/tests/frame/indexing/test_setitem.py
@@ -1174,3 +1174,17 @@ class TestDataFrameSetitemCopyViewSemantics:
         view = df[:]
         df[indexer] = set_value
         tm.assert_frame_equal(view, expected)
+
+    @td.skip_array_manager_invalid_test
+    def test_setitem_column_update_inplace(self):
+        # https://github.com/pandas-dev/pandas/issues/47172
+
+        labels = [f"c{i}" for i in range(10)]
+        df = DataFrame({col: np.zeros(len(labels)) for col in labels}, index=labels)
+        values = df._mgr.blocks[0].values
+
+        for label in df.columns:
+            df[label][label] = 1
+
+        # diagonal values all updated
+        assert np.all(values[np.arange(10), np.arange(10)] == 1)
