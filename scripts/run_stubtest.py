d5b48b8167eda7cf98bc84b486fa0cfd36d5599a
Ambuj Pawar <ambuj.pawar95@gmail.com>
2022-08-27 00:53:47 +0530

    resolve review comments

--- scripts/run_stubtest.py
+++ scripts/run_stubtest.py
@@ -23,9 +23,9 @@ if not pd_version:
 if "dev" not in pd_version:
     warnings.warn(
         f"stubtest may fail as {pd_version} is not a dev version. "
-        f"Please install a pandas dev version or see <https://github.com/pandas-dev"
-        f"/pandas/blob/main/doc/source/development/contributing_codebase.rst"
-        f"#validating-type-hints> on how to skip the stubtest"
+        f"Please install a pandas dev version or see https://pandas.pydata.org/"
+        f"pandas-docs/stable/development/contributing_codebase.html"
+        f"#validating-type-hints on how to skip the stubtest"
     )
 
 
