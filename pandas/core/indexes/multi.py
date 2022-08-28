0149a1c45041d31ccbee5afe7c117273e6881673
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-08-28 13:44:16 +0200

    BUG: MultiIndex.append not checking names for equality

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -2203,21 +2203,19 @@ class MultiIndex(Index):
         if all(
             (isinstance(o, MultiIndex) and o.nlevels >= self.nlevels) for o in other
         ):
-            arrays = []
+            arrays, names = [], []
             for i in range(self.nlevels):
                 label = self._get_level_values(i)
                 appended = [o._get_level_values(i) for o in other]
                 arrays.append(label.append(appended))
-            return MultiIndex.from_arrays(arrays, names=self.names)
+                level_names = {label.name}.union({x.name for x in appended})
+                names.append(None if len(level_names) > 1 else label.name)
+            return MultiIndex.from_arrays(arrays, names=names)
 
         to_concat = (self._values,) + tuple(k._values for k in other)
         new_tuples = np.concatenate(to_concat)
 
-        # if all(isinstance(x, MultiIndex) for x in other):
-        try:
-            return MultiIndex.from_tuples(new_tuples, names=self.names)
-        except (TypeError, IndexError):
-            return Index._with_infer(new_tuples)
+        return Index._with_infer(new_tuples)
 
     def argsort(self, *args, **kwargs) -> npt.NDArray[np.intp]:
         return self._values.argsort(*args, **kwargs)
