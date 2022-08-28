9853f0e709ec20f790feb81df40b311c1cb06761
Neal Muppidi <neal.muppidi@gmail.com>
2022-08-28 16:22:44 -0500

    fix pep8 mistakes

--- pandas/tests/indexing/test_at.py
+++ pandas/tests/indexing/test_at.py
@@ -99,10 +99,20 @@ class TestAtSetItem:
 
         tm.assert_frame_equal(df, expected)
 
-    @pytest.mark.parametrize("row", (Timestamp("2019-01-01"), to_datetime("2019-01-01"), "2019-01-01"))
+    @pytest.mark.parametrize("row",
+                             (Timestamp("2019-01-01"),
+                              to_datetime("2019-01-01"),
+                              "2019-01-01")
+                             )
     def test_at_datetime_index(self, row):
-        df = DataFrame(data=[[1] * 2], index=DatetimeIndex(data=["2019-01-01", "2019-01-02"]))
-        expected = DataFrame(data=[[0.5, 1], [1.0, 1]], index=DatetimeIndex(data=["2019-01-01", "2019-01-02"]))
+        df = DataFrame(
+            data=[[1] * 2],
+            index=DatetimeIndex(data=["2019-01-01", "2019-01-02"])
+        )
+        expected = DataFrame(
+            data=[[0.5, 1], [1.0, 1]],
+            index=DatetimeIndex(data=["2019-01-01", "2019-01-02"])
+        )
 
         df.at[row, 0] = 0.5
         tm.assert_frame_equal(df, expected)
