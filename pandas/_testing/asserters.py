BUG: Propagating precision to interval dtype #43913

pandas/_testing/asserters.py
@@ -580,7 +580,7 @@ def assert_categorical_equal(
 
 
 def assert_interval_array_equal(
-    left, right, exact="equiv", obj="IntervalArray"
+    left, right, exact="equiv", obj="IntervalArray", **kwargs
 ) -> None:
     """
     Test that two IntervalArrays are equivalent.
@@ -599,7 +599,7 @@ def assert_interval_array_equal(
     """
     _check_isinstance(left, right, IntervalArray)
 
-    kwargs = {}
+    kwargs = dict(kwargs)
     if left._left.dtype.kind in ["m", "M"]:
         # We have a DatetimeArray or TimedeltaArray
         kwargs["check_freq"] = False
@@ -686,6 +686,7 @@ def assert_numpy_array_equal(
     check_same=None,
     obj="numpy array",
     index_values=None,
+    **kwargs,
 ) -> None:
     """
     Check that 'np.ndarray' is equivalent.
@@ -749,7 +750,7 @@ def assert_numpy_array_equal(
         raise AssertionError(err_msg)
 
     # compare shape and values
-    if not array_equivalent(left, right, strict_nan=strict_nan):
+    if not array_equivalent(left, right, strict_nan=strict_nan, **kwargs):
         _raise(left, right, err_msg)
 
     if check_dtype:
@@ -1063,7 +1064,7 @@ def assert_series_equal(
             )
             raise AssertionError(msg)
     elif is_interval_dtype(left.dtype) and is_interval_dtype(right.dtype):
-        assert_interval_array_equal(left.array, right.array)
+        assert_interval_array_equal(left.array, right.array, rtol=rtol, atol=atol)
     elif isinstance(left.dtype, CategoricalDtype) or isinstance(
         right.dtype, CategoricalDtype
     ):

pandas/core/dtypes/missing.py
@@ -460,6 +460,8 @@ def isna_compat(arr, fill_value=np.nan) -> bool:
 def array_equivalent(
     left,
     right,
+    rtol: float = 1.0e-5,
+    atol: float = 1.0e-8,
     strict_nan: bool = False,
     dtype_equal: bool = False,
 ) -> bool:
@@ -530,7 +532,10 @@ def array_equivalent(
     if is_float_dtype(left.dtype) or is_complex_dtype(left.dtype):
         if not (left.size and right.size):
             return True
-        return ((left == right) | (isna(left) & isna(right))).all()
+        return (
+            (abs(left - right) <= (atol + rtol * abs(right)))
+            | (isna(left) & isna(right))
+        ).all()
 
     elif is_datetimelike_v_numeric(left, right):
         # GH#29553 avoid numpy deprecation warning