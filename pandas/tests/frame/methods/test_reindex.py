4897545fa4628f2f4e0ea49b29384a3e946ceb0b
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-08-31 19:43:20 +0200

    Use fixture

--- pandas/tests/frame/methods/test_reindex.py
+++ pandas/tests/frame/methods/test_reindex.py
@@ -772,13 +772,13 @@ class TestDataFrameSelectReindex:
         expected = df.reindex(range(15)).fillna(0)
         tm.assert_frame_equal(result, expected)
 
-    @pytest.mark.parametrize("dtype", ["uint8", "uint16", "uint32", "uint64"])
-    def test_reindex_uint_dtypes_fill_value(self, dtype):
+    def test_reindex_uint_dtypes_fill_value(self, any_unsigned_int_numpy_dtype):
         # GH#48184
-        df = DataFrame({"a": [1, 2], "b": [1, 2]}, dtype=dtype)
+        df = DataFrame({"a": [1, 2], "b": [1, 2]}, dtype=any_unsigned_int_numpy_dtype)
         result = df.reindex(columns=list("abcd"), index=[0, 1, 2, 3], fill_value=10)
         expected = DataFrame(
-            {"a": [1, 2, 10, 10], "b": [1, 2, 10, 10], "c": 10, "d": 10}, dtype=dtype
+            {"a": [1, 2, 10, 10], "b": [1, 2, 10, 10], "c": 10, "d": 10},
+            dtype=any_unsigned_int_numpy_dtype,
         )
         tm.assert_frame_equal(result, expected)
 
