db2c34a00b5ac454e872174f31719ef49a22cf64
weikhor <cheanwei.KCW@gmail.com>
2022-09-01 20:15:28 +0800

    add

--- pandas/tests/series/test_ufunc.py
+++ pandas/tests/series/test_ufunc.py
@@ -1,5 +1,4 @@
 from collections import deque
-import re
 import string
 
 import numpy as np
@@ -454,24 +453,3 @@ def test_np_matmul():
         expected_result,
         result,
     )
-
-
-def test_array_ufuncs_for_many_arguments():
-    # GH39853
-    def add3(x, y, z):
-        return x + y + z
-
-    ufunc = np.frompyfunc(add3, 3, 1)
-    ser = pd.Series([1, 2])
-
-    result = ufunc(ser, ser, 1)
-    expected = pd.Series([3, 5], dtype=object)
-    tm.assert_series_equal(result, expected)
-
-    df = pd.DataFrame([[1, 2]])
-    msg = (
-        "Cannot apply ufunc <ufunc 'add3 (vectorized)'> "
-        "to mixed DataFrame and Series inputs."
-    )
-    with pytest.raises(NotImplementedError, match=re.escape(msg)):
-        ufunc(ser, ser, df)
