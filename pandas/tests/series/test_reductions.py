af191dc99f00e9579e37bddd16d5a8094b22c8ef
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-09-03 21:13:13 +0200

    Move test

--- pandas/tests/series/test_reductions.py
+++ pandas/tests/series/test_reductions.py
@@ -141,16 +141,3 @@ def test_validate_stat_keepdims():
     )
     with pytest.raises(ValueError, match=msg):
         np.sum(ser, keepdims=True)
-
-
-def test_mean_masked_overflow():
-    # GH#48378
-    val = 100_000_000_000_000_000
-    n_elements = 100
-    na = np.array([val] * n_elements)
-    ser = Series([val] * n_elements, dtype="Int64")
-
-    result_numpy = np.mean(na)
-    result_masked = ser.mean()
-    assert result_masked - result_numpy == 0
-    assert result_masked == 1e17
