3895e2cfdc98948a0b276d21d445959e2f43f23c
Parfait Gasana <parfait.gasana@gmail.com>
2022-09-04 10:56:33 -0500

    CLN/DOC: Adjust xpath validation and error messaging in read_xml with IO tools doc note and example

--- pandas/tests/io/xml/test_xml.py
+++ pandas/tests/io/xml/test_xml.py
@@ -760,6 +760,30 @@ def test_elem_and_attrs_only(datapath, parser):
         read_xml(filename, elems_only=True, attrs_only=True, parser=parser)
 
 
+def test_empty_attrs_only(parser):
+    xml = """
+      <data>
+        <row>
+          <shape sides="4">square</shape>
+          <degrees>360</degrees>
+        </row>
+        <row>
+          <shape sides="0">circle</shape>
+          <degrees>360</degrees>
+        </row>
+        <row>
+          <shape sides="3">triangle</shape>
+          <degrees>180</degrees>
+        </row>
+      </data>"""
+
+    with pytest.raises(
+        ValueError,
+        match=("xpath does not return any nodes or attributes"),
+    ):
+        read_xml(xml, xpath="./row", attrs_only=True, parser=parser)
+
+
 @td.skip_if_no("lxml")
 def test_attribute_centric_xml():
     xml = """\
