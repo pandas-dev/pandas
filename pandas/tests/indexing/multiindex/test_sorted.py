bd50149511202aab272525b6b5bcfb6577b9ff31
Luke Manley <lukemanley@gmail.com>
2022-09-10 06:07:02 -0400

    add test

--- pandas/tests/indexing/multiindex/test_sorted.py
+++ pandas/tests/indexing/multiindex/test_sorted.py
@@ -2,9 +2,11 @@ import numpy as np
 import pytest
 
 from pandas import (
+    NA,
     DataFrame,
     MultiIndex,
     Series,
+    array,
 )
 import pandas._testing as tm
 
@@ -83,6 +85,25 @@ class TestMultiIndexSorted:
         tuples = zip(*arrays)
         index = MultiIndex.from_tuples(tuples)
         expected = DataFrame(range(8), index=index)
+
+        tm.assert_frame_equal(result, expected)
+
+    def test_sort_values_with_na(self):
+        arrays = [
+            array([2, 1, NA], dtype="Int64"),
+            array([1, 2, 3], dtype="Int64"),
+        ]
+        index = MultiIndex.from_arrays(arrays)
+        index = index.sort_values()
+        result = DataFrame(range(3), index=index)
+
+        arrays = [
+            array([1, 2, NA], dtype="Int64"),
+            array([2, 1, 3], dtype="Int64"),
+        ]
+        index = MultiIndex.from_arrays(arrays)
+        index = index.sort_values()
+        expected = DataFrame(range(3), index=index)
 
         tm.assert_frame_equal(result, expected)
 
