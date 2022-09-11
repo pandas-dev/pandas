41abe02d93707892d3d2c5c8056a02401dac246c
Jonas Haag <jonas@lophus.org>
2022-09-11 11:00:01 +0200

    Lint

--- pandas/tests/io/sas/test_byteswap.py
+++ pandas/tests/io/sas/test_byteswap.py
@@ -7,6 +7,8 @@ from hypothesis import (
 import numpy as np
 import pytest
 
+import pandas._testing as tm
+
 from pandas.io.sas._byteswap import (
     read_double_with_byteswap,
     read_float_with_byteswap,
@@ -35,18 +37,18 @@ def test_float_byteswap(read_offset, number, float_type, should_byteswap):
 
 
 def _test(number, number_type, read_offset, should_byteswap):
-    number = number_type([number])
+    number = number_type(number)
     data = np.random.default_rng().integers(0, 256, size=20, dtype="uint8")
-    data[read_offset : read_offset + number.itemsize] = number.view("uint8")
+    data[read_offset : read_offset + number.itemsize] = number[None].view("uint8")
     swap_func = {
         np.float32: read_float_with_byteswap,
         np.float64: read_double_with_byteswap,
         np.uint16: read_uint16_with_byteswap,
         np.uint32: read_uint32_with_byteswap,
         np.uint64: read_uint64_with_byteswap,
-    }[type(number[0])]
-    output_number = swap_func(bytes(data), read_offset, should_byteswap)
+    }[type(number)]
+    output_number = number_type(swap_func(bytes(data), read_offset, should_byteswap))
     if should_byteswap:
-        np.testing.assert_equal(output_number, number.byteswap())
+        tm.assert_equal(output_number, number.byteswap())
     else:
-        np.testing.assert_equal(output_number, number)
+        tm.assert_equal(output_number, number)
