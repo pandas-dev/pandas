01bf0cf9adae056f7355bd95d3ecbbacc3dc1a7e
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-09-12 20:30:57 +0200

    Parametrize test

--- pandas/tests/reductions/test_reductions.py
+++ pandas/tests/reductions/test_reductions.py
@@ -787,14 +787,15 @@ class TestSeriesReductions:
         assert result_masked - result_numpy == 0
         assert result_masked == 1e17
 
-    def test_var_masked_array(self):
+    @pytest.mark.parametrize("ddof, exp", [(1, 2.5), (0, 2.0)])
+    def test_var_masked_array(self, ddof, exp):
         # GH#48379
         ser = Series([1, 2, 3, 4, 5], dtype="Int64")
         ser_numpy_dtype = Series([1, 2, 3, 4, 5], dtype="int64")
-        result = ser.var()
-        result_numpy_dtype = ser_numpy_dtype.var()
+        result = ser.var(ddof=ddof)
+        result_numpy_dtype = ser_numpy_dtype.var(ddof=ddof)
         assert result == result_numpy_dtype
-        assert result == 2.5
+        assert result == exp
 
     @pytest.mark.parametrize("dtype", ("m8[ns]", "m8[ns]", "M8[ns]", "M8[ns, UTC]"))
     @pytest.mark.parametrize("skipna", [True, False])
