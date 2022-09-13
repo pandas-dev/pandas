20427a27726db6ca78cc3747f378c19f0c0daf34
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2022-09-13 15:58:37 -0700

    DOC: Add missing API item to reference docs (#48455)
    * DOC: Add missing API item to reference docs
    
    * Address review
    
    * Fix formatting

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -115,6 +115,31 @@ ScalarResult = TypeVar("ScalarResult")
 
 
 class NamedAgg(NamedTuple):
+    """
+    Helper for column specific aggregation with control over output column names.
+
+    Subclass of typing.NamedTuple.
+
+    Parameters
+    ----------
+    column : Hashable
+        Column label in the DataFrame to apply aggfunc.
+    aggfunc : function or str
+        Function to apply to the provided column. If string, the name of a built-in
+        pandas function.
+
+    Examples
+    --------
+    >>> df = pd.DataFrame({"key": [1, 1, 2], "a": [-1, 0, 1], 1: [10, 11, 12]})
+    >>> agg_a = pd.NamedAgg(column="a", aggfunc="min")
+    >>> agg_1 = pd.NamedAgg(column=1, aggfunc=np.mean)
+    >>> df.groupby("key").agg(result_a=agg_a, result_1=agg_1)
+         result_a  result_1
+    key
+    1          -1      10.5
+    2           1      12.0
+    """
+
     column: Hashable
     aggfunc: AggScalar
 
