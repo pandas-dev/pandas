042853436f08e28060157bf741c689403ff5454d
SFuller4 <steven.a.fuller@gmail.com>
2022-09-13 22:02:29 -0500

    Adding tests for append functionality, along with updated whatsnew, user_guide, and generic docstring.

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -2400,7 +2400,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         index: bool_t = True,
         indent: int | None = None,
         storage_options: StorageOptions = None,
-        mode: str = 'w',
+        mode: str = "w",
     ) -> str | None:
         """
         Convert the object to a JSON string.
@@ -2480,9 +2480,9 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
             .. versionadded:: 1.2.0
 
         mode : str, default 'w' (writing)
-            Specify the IO mode for output when supplying a path_or_buf. 
-            Accepted args are 'w' (writing) and 'a' (append) only. 
-            Supplying mode='a' is only supported when lines is True and orient is 'records'.
+            Specify the IO mode for output when supplying a path_or_buf.
+            Accepted args are 'w' (writing) and 'a' (append) only.
+            mode='a' is only supported when lines is True and orient is 'records'.
 
         Returns
         -------
