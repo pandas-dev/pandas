39402db64d8b445e0bf527e35964515d45bd3dfb
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-09-17 15:41:13 +0200

    Add test and whatsnew

--- pandas/tests/indexes/multi/test_setops.py
+++ pandas/tests/indexes/multi/test_setops.py
@@ -616,3 +616,17 @@ def test_intersection_lexsort_depth(levels1, levels2, codes1, codes2, names):
 
     with tm.assert_produces_warning(FutureWarning, match="MultiIndex.lexsort_depth"):
         assert mi_int.lexsort_depth == 2
+
+
+@pytest.mark.parametrize("val", [pd.NA, 100])
+def test_intersection_keep_ea_dtypes(val, any_numeric_ea_dtype):
+    # GH#
+    midx = MultiIndex.from_arrays(
+        [Series([1, 2], dtype=any_numeric_ea_dtype), [2, 1]], names=["a", None]
+    )
+    midx2 = MultiIndex.from_arrays(
+        [Series([1, 2, val], dtype=any_numeric_ea_dtype), [1, 1, 3]]
+    )
+    result = midx.intersection(midx2)
+    expected = MultiIndex.from_arrays([Series([2], dtype=any_numeric_ea_dtype), [1]])
+    tm.assert_index_equal(result, expected)
