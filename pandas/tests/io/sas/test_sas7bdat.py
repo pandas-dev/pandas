1f7dc15b1e4910a9c8f25a77b45a700416c40a4a
Yasin Tatar <ytatar9@googlemail.com>
2022-09-20 01:21:57 +0200

    ENH: allow user to infer SAS file encoding; add correct encoding names (#48050)
    * ENH: allow user to infer SAS file encoding; use detected encoding; add correct encoding names
    
    * remove typing annotation and assert error message
    
    * include initial release documentation
    
    * rewrite test: no type hinting + context manager call
    
    * add encoding constant for whole documentation -- incl. explicit not found references
    
    * moved release note to 1.6.0

--- pandas/tests/io/sas/test_sas7bdat.py
+++ pandas/tests/io/sas/test_sas7bdat.py
@@ -136,6 +136,21 @@ def test_encoding_options(datapath):
         assert x == y.decode()
 
 
+def test_encoding_infer(datapath):
+    fname = datapath("io", "sas", "data", "test1.sas7bdat")
+
+    with pd.read_sas(fname, encoding="infer", iterator=True) as df1_reader:
+        # check: is encoding inferred correctly from file
+        assert df1_reader.inferred_encoding == "cp1252"
+        df1 = df1_reader.read()
+
+    with pd.read_sas(fname, encoding="cp1252", iterator=True) as df2_reader:
+        df2 = df2_reader.read()
+
+    # check: reader reads correct information
+    tm.assert_frame_equal(df1, df2)
+
+
 def test_productsales(datapath):
     fname = datapath("io", "sas", "data", "productsales.sas7bdat")
     df = pd.read_sas(fname, encoding="utf-8")
