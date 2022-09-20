744b846557540a86286f3bdc534a6a70639dd902
dataxerik <dsshar@gmail.com>
2022-09-20 14:31:49 -0400

    ENH: move an exception and add a prehook to check for exception place… (#48088)
    * ENH: move an exception and add a prehook to check for exception placement
    
    * ENH: fix import
    
    * ENH: revert moving error
    
    * ENH: add docstring and fix import for test
    
    * ENH: re-design approach based on feedback
    
    * ENH: update whatsnew rst
    
    * ENH: apply feedback changes
    
    * ENH: refactor to remove exception_warning_list and ignore _version.py
    
    * ENH: remove NotThisMethod from tests and all

--- pandas/errors/__init__.py
+++ pandas/errors/__init__.py
@@ -12,6 +12,8 @@ from pandas._libs.tslibs import (
     OutOfBoundsTimedelta,
 )
 
+from pandas.util.version import InvalidVersion
+
 
 class IntCastingNaNError(ValueError):
     """
@@ -535,6 +537,24 @@ class CategoricalConversionWarning(Warning):
     """
 
 
+class LossySetitemError(Exception):
+    """
+    Raised when trying to do a __setitem__ on an np.ndarray that is not lossless.
+    """
+
+
+class NoBufferPresent(Exception):
+    """
+    Exception is raised in _get_data_buffer to signal that there is no requested buffer.
+    """
+
+
+class InvalidComparison(Exception):
+    """
+    Exception is raised by _validate_comparison_value to indicate an invalid comparison.
+    """
+
+
 __all__ = [
     "AbstractMethodError",
     "AccessorRegistrationWarning",
@@ -550,9 +570,13 @@ __all__ = [
     "IncompatibilityWarning",
     "IntCastingNaNError",
     "InvalidColumnName",
+    "InvalidComparison",
     "InvalidIndexError",
+    "InvalidVersion",
     "IndexingError",
+    "LossySetitemError",
     "MergeError",
+    "NoBufferPresent",
     "NullFrequencyError",
     "NumbaUtilError",
     "NumExprClobberingError",
