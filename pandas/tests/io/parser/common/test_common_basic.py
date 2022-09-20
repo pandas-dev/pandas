cfe3446e93711d7f17cffbb8a05c539e835dc5fe
Torsten Wörtwein <twoertwein@gmail.com>
2022-09-19 22:08:49 -0400

    pyupgrade

--- pandas/tests/io/parser/common/test_common_basic.py
+++ pandas/tests/io/parser/common/test_common_basic.py
@@ -937,7 +937,7 @@ def test_read_seek(all_parsers):
     content = "nkey,value\ntables,rectangular\n"
     with tm.ensure_clean() as path:
         Path(path).write_text(prefix + content)
-        with open(path, mode="r") as file:
+        with open(path, encoding="utf-8") as file:
             file.readline()
             actual = parser.read_csv(file)
         expected = parser.read_csv(StringIO(content))
