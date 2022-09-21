32b0937c260c86fa44808839af4c0b6870ee6b04
jbrockmendel <jbrockmendel@gmail.com>
2022-09-21 10:33:47 -0700

    ENH: DTA/TDA add datetimelike scalar with mismatched reso (#48669)
    * ENH: DTA/TDA add datetimelike scalar with mismatched reso
    
    * mypy fixup

--- pandas/tests/arrays/test_timedeltas.py
+++ pandas/tests/arrays/test_timedeltas.py
@@ -104,11 +104,18 @@ class TestNonNano:
     def test_add_datetimelike_scalar(self, tda, tz_naive_fixture):
         ts = pd.Timestamp("2016-01-01", tz=tz_naive_fixture)
 
-        msg = "with mis-matched resolutions"
-        with pytest.raises(NotImplementedError, match=msg):
+        expected = tda + ts._as_unit(tda._unit)
+        res = tda + ts
+        tm.assert_extension_array_equal(res, expected)
+        res = ts + tda
+        tm.assert_extension_array_equal(res, expected)
+
+        ts += Timedelta(1)  # so we can't cast losslessly
+        msg = "Cannot losslessly convert units"
+        with pytest.raises(ValueError, match=msg):
             # mismatched reso -> check that we don't give an incorrect result
             tda + ts
-        with pytest.raises(NotImplementedError, match=msg):
+        with pytest.raises(ValueError, match=msg):
             # mismatched reso -> check that we don't give an incorrect result
             ts + tda
 
@@ -179,13 +186,28 @@ class TestNonNano:
         tda_nano = TimedeltaArray(tda._ndarray.astype("m8[ns]"))
 
         msg = "mis-matched resolutions is not yet supported"
-        with pytest.raises(NotImplementedError, match=msg):
+        expected = tda * 2
+        res = tda_nano + tda
+        tm.assert_extension_array_equal(res, expected)
+        res = tda + tda_nano
+        tm.assert_extension_array_equal(res, expected)
+
+        expected = tda * 0
+        res = tda - tda_nano
+        tm.assert_extension_array_equal(res, expected)
+
+        res = tda_nano - tda
+        tm.assert_extension_array_equal(res, expected)
+
+        tda_nano[:] = np.timedelta64(1, "ns")  # can't round losslessly
+        msg = "Cannot losslessly cast '-?1 ns' to"
+        with pytest.raises(ValueError, match=msg):
             tda_nano + tda
-        with pytest.raises(NotImplementedError, match=msg):
+        with pytest.raises(ValueError, match=msg):
             tda + tda_nano
-        with pytest.raises(NotImplementedError, match=msg):
+        with pytest.raises(ValueError, match=msg):
             tda - tda_nano
-        with pytest.raises(NotImplementedError, match=msg):
+        with pytest.raises(ValueError, match=msg):
             tda_nano - tda
 
         result = tda_nano + tda_nano
