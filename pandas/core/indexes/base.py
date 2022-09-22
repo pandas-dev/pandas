359cc5ca015a296c51ab8e569d5a5915ea0aee4e
Torsten Wörtwein <twoertwein@users.noreply.github.com>
2022-09-22 12:05:19 -0400

    TYP: type all arguments with str default values (#48508)
    * TYP: type all arguments with str default values
    
    * na_rep: back to str
    
    * na(t)_rep is always a string
    
    * add float for some functions
    
    * and the same for the few float default arguments
    
    * define a few more literal constants
    
    * avoid itertools.cycle mypy error
    
    * revert mistake

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -50,6 +50,7 @@ from pandas._typing import (
     ArrayLike,
     Axes,
     AxisInt,
+    DropKeep,
     Dtype,
     DtypeObj,
     F,
@@ -1001,7 +1002,7 @@ class Index(IndexOpsMixin, PandasObject):
         return self._data.dtype
 
     @final
-    def ravel(self, order="C"):
+    def ravel(self, order: str_t = "C"):
         """
         Return an ndarray of the flattened values of the underlying data.
 
@@ -1538,7 +1539,7 @@ class Index(IndexOpsMixin, PandasObject):
         return values._format_native_types(**kwargs)
 
     def _format_native_types(
-        self, *, na_rep="", quoting=None, **kwargs
+        self, *, na_rep: str_t = "", quoting=None, **kwargs
     ) -> npt.NDArray[np.object_]:
         """
         Actually format specific types of the index.
@@ -3067,7 +3068,7 @@ class Index(IndexOpsMixin, PandasObject):
         return self._shallow_copy(result)
 
     @deprecate_nonkeyword_arguments(version=None, allowed_args=["self"])
-    def drop_duplicates(self: _IndexT, keep: str_t | bool = "first") -> _IndexT:
+    def drop_duplicates(self: _IndexT, keep: DropKeep = "first") -> _IndexT:
         """
         Return Index with duplicate values removed.
 
@@ -3118,9 +3119,7 @@ class Index(IndexOpsMixin, PandasObject):
 
         return super().drop_duplicates(keep=keep)
 
-    def duplicated(
-        self, keep: Literal["first", "last", False] = "first"
-    ) -> npt.NDArray[np.bool_]:
+    def duplicated(self, keep: DropKeep = "first") -> npt.NDArray[np.bool_]:
         """
         Indicate duplicate index values.
 
