503ef9ed4910159293770a5fb54172b56ff2efae
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-09-23 11:06:48 -0700

    BUG: MultiIndex.difference not keeping ea dtype (#48606)
    * BUG: MultiIndex.difference not keeping ea dtype
    
    * Add asv
    
    * Add whatsnew
    
    * Reduce asv
    
    * Ad ea asv
    
    * Fix mypy
    
    * Add whatsnew
    
    * Fix mypy

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3684,7 +3684,12 @@ class Index(IndexOpsMixin, PandasObject):
         indexer = indexer.take((indexer != -1).nonzero()[0])
 
         label_diff = np.setdiff1d(np.arange(this.size), indexer, assume_unique=True)
-        the_diff = this._values.take(label_diff)
+
+        the_diff: MultiIndex | ArrayLike
+        if isinstance(this, ABCMultiIndex):
+            the_diff = this.take(label_diff)
+        else:
+            the_diff = this._values.take(label_diff)
         the_diff = _maybe_try_sort(the_diff, sort)
 
         return the_diff
