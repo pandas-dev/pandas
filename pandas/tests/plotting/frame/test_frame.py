c6a3b66d03d95fe57ad0216eb4c4ec14e6c0b4ce
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-09-23 07:07:04 -0700

    Fix test

--- pandas/tests/plotting/frame/test_frame.py
+++ pandas/tests/plotting/frame/test_frame.py
@@ -1684,7 +1684,7 @@ class TestDataFramePlots(TestPlotBase):
     def test_scatter_unknown_colormap(self):
         # GH#48726
         df = DataFrame({"a": [1, 2, 3], "b": 4})
-        with pytest.raises(ValueError, match="'unknown' is not a valid value for name"):
+        with pytest.raises((ValueError, KeyError), match="'unknown' is not a"):
             df.plot(x="a", y="b", colormap="unknown", kind="scatter")
 
     def test_sharex_and_ax(self):
