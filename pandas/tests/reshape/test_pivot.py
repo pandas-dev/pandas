ef9c68231ed762008b2402846fd262462e2b8183
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-09-23 10:23:02 -0700

    Backport PR #48713: BUG: pivot_table raising Future Warning with datetime column as index

--- pandas/tests/reshape/test_pivot.py
+++ pandas/tests/reshape/test_pivot.py
@@ -2237,6 +2237,38 @@ class TestPivotTable:
         )
         tm.assert_frame_equal(result, expected)
 
+    def test_pivot_table_datetime_warning(self):
+        # GH#48683
+        df = DataFrame(
+            {
+                "a": "A",
+                "b": [1, 2],
+                "date": pd.Timestamp("2019-12-31"),
+                "sales": [10.0, 11],
+            }
+        )
+        with tm.assert_produces_warning(None):
+            result = df.pivot_table(
+                index=["b", "date"], columns="a", margins=True, aggfunc="sum"
+            )
+        expected = DataFrame(
+            [[10.0, 10.0], [11.0, 11.0], [21.0, 21.0]],
+            index=MultiIndex.from_arrays(
+                [
+                    Index([1, 2, "All"], name="b"),
+                    Index(
+                        [pd.Timestamp("2019-12-31"), pd.Timestamp("2019-12-31"), ""],
+                        dtype=object,
+                        name="date",
+                    ),
+                ]
+            ),
+            columns=MultiIndex.from_tuples(
+                [("sales", "A"), ("sales", "All")], names=[None, "a"]
+            ),
+        )
+        tm.assert_frame_equal(result, expected)
+
 
 class TestPivot:
     def test_pivot(self):
