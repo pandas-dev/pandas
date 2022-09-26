682d7f92865b082d1cec4a8cb69de02f43795620
hauntsaninja <hauntsaninja@gmail.com>
2022-09-26 16:25:45 -0700

    black

--- pandas/tests/groupby/test_any_all.py
+++ pandas/tests/groupby/test_any_all.py
@@ -87,7 +87,7 @@ def test_any_apply_keyword_non_zero_axis_regression():
     # https://github.com/pandas-dev/pandas/issues/48656
     df = DataFrame({"A": [1, 2], "B": [0, 2], "C": [0, 0]})
     expected = df.any(axis=1)
-    result = df.apply('any', axis=1)
+    result = df.apply("any", axis=1)
     tm.assert_series_equal(result, expected)
 
     msg = (
@@ -96,7 +96,7 @@ def test_any_apply_keyword_non_zero_axis_regression():
     )
     with tm.assert_produces_warning(FutureWarning, match=msg):
         expected = df.any(1)
-    result = df.apply('any', 1)
+    result = df.apply("any", 1)
     tm.assert_series_equal(result, expected)
 
 
