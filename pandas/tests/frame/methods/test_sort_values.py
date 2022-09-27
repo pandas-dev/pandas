b892ec0f34f850567f7cc9bdd0910d512dfd672d
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2022-09-27 11:05:19 +0100

    BUG: still emitting unnecessary FutureWarning in DataFrame.sort_values with sparse columns (#48785)
    * BUG: still emitting unnecessary FutureWarning in DataFrame.sort_values with sparse columns
    
    * make whatsnew consistent
    
    Co-authored-by: MarcoGorelli <>

--- pandas/tests/frame/methods/test_sort_values.py
+++ pandas/tests/frame/methods/test_sort_values.py
@@ -15,11 +15,12 @@ import pandas._testing as tm
 
 
 class TestDataFrameSortValues:
-    def test_sort_values_sparse_no_warning(self):
+    @pytest.mark.parametrize("dtype", [np.uint8, bool])
+    def test_sort_values_sparse_no_warning(self, dtype):
         # GH#45618
         # TODO(2.0): test will be unnecessary
         ser = pd.Series(Categorical(["a", "b", "a"], categories=["a", "b", "c"]))
-        df = pd.get_dummies(ser, sparse=True)
+        df = pd.get_dummies(ser, dtype=dtype, sparse=True)
 
         with tm.assert_produces_warning(None):
             # No warnings about constructing Index from SparseArray
