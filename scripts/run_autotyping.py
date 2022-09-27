6c70d8a1c7cd6b42983a64fc9fa01ad1e8856e08
MarcoGorelli <>
2022-09-27 17:06:47 +0100

    add run_autotyping script

---
+++ scripts/run_autotyping.py
@@ -0,0 +1,38 @@
+"""
+Script to run ``autotyping``, to get around the fact that
+pre-commit puts ``args`` before the list of files, whereas
+``autotyping`` wants the files to come after, see
+https://github.com/pandas-dev/pandas/issues/48808#issuecomment-1259711679.
+"""
+from __future__ import annotations
+
+import argparse
+import subprocess
+import sys
+from typing import Sequence
+
+
+def main(argv: Sequence[str] | None = None) -> None:
+    parser = argparse.ArgumentParser()
+    parser.add_argument("paths", nargs="*")
+    args = parser.parse_args(argv)
+    output = subprocess.run(
+        [
+            "python",
+            "-m",
+            "libcst.tool",
+            "codemod",
+            "autotyping.AutotypeCommand",
+            *args.paths,
+            "--aggressive",
+        ],
+        capture_output=True,
+        text=True,
+    )
+    sys.stdout.write(output.stdout)
+    sys.stdout.write(output.stderr)
+    sys.exit(output.returncode)
+
+
+if __name__ == "__main__":
+    main()
