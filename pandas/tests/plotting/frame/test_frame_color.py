f4b92763677138ccad7861579575d86f39058bd5
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-09-28 22:35:56 +0200

    Move import

--- pandas/tests/plotting/frame/test_frame_color.py
+++ pandas/tests/plotting/frame/test_frame_color.py
@@ -15,8 +15,6 @@ from pandas.tests.plotting.common import (
     _check_plot_works,
 )
 
-from pandas.plotting._matplotlib.compat import mpl_ge_3_6_0
-
 
 @td.skip_if_no_mpl
 class TestDataFrameColor(TestPlotBase):
@@ -201,6 +199,8 @@ class TestDataFrameColor(TestPlotBase):
     @pytest.mark.parametrize("kw", ["c", "color"])
     def test_scatter_with_c_column_name_with_colors(self, cmap, kw):
         # https://github.com/pandas-dev/pandas/issues/34316
+        from pandas.plotting._matplotlib.compat import mpl_ge_3_6_0
+
         df = DataFrame(
             [[5.1, 3.5], [4.9, 3.0], [7.0, 3.2], [6.4, 3.2], [5.9, 3.0]],
             columns=["length", "width"],
