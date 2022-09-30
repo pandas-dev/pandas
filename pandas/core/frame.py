8cc7970c0826713036f0f7d66dceac32dabf4dfc
Ambuj Pawar <your_email@abc.example>
2022-09-30 17:22:21 +0200

    ERR: Improve error message when assigning a complete row using 'at' method

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -222,6 +222,7 @@ from pandas.io.formats import (
     console,
     format as fmt,
 )
+from pandas.errors import InvalidIndexError
 from pandas.io.formats.info import (
     INFO_DOCSTRING,
     DataFrameInfo,
@@ -4238,6 +4239,11 @@ class DataFrame(NDFrame, OpsMixin):
             else:
                 self.loc[index, col] = value
             self._item_cache.pop(col, None)
+        
+        except InvalidIndexError:
+            # GH48729: Seems like you are trying to assign a value to a
+            # row when only scalar options are permitted
+            raise InvalidIndexError(f"You can only assign a scalar value not a {type(value)}")
 
     def _ensure_valid_index(self, value) -> None:
         """
