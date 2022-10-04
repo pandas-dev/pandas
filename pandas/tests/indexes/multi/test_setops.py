51bc2495b83ad66cf69620cc15fbf46b78eb4acc
Luke Manley <lukemanley@gmail.com>
2022-10-03 20:39:35 -0400

    add test for duplicate NA

--- pandas/tests/indexes/multi/test_setops.py
+++ pandas/tests/indexes/multi/test_setops.py
@@ -578,25 +578,26 @@ def test_union_keep_ea_dtype(any_numeric_ea_dtype, val):
     tm.assert_index_equal(result, expected)
 
 
-def test_union_with_duplicates_keep_ea_dtype(any_numeric_ea_dtype):
+@pytest.mark.parametrize("dupe_val", [3, pd.NA])
+def test_union_with_duplicates_keep_ea_dtype(dupe_val, any_numeric_ea_dtype):
     # GH48900
     mi1 = MultiIndex.from_arrays(
         [
-            Series([1, 3, 2], dtype=any_numeric_ea_dtype),
-            Series([1, 3, 2], dtype=any_numeric_ea_dtype),
+            Series([1, dupe_val, 2], dtype=any_numeric_ea_dtype),
+            Series([1, dupe_val, 2], dtype=any_numeric_ea_dtype),
         ]
     )
     mi2 = MultiIndex.from_arrays(
         [
-            Series([2, 3, 3], dtype=any_numeric_ea_dtype),
-            Series([2, 3, 3], dtype=any_numeric_ea_dtype),
+            Series([2, dupe_val, dupe_val], dtype=any_numeric_ea_dtype),
+            Series([2, dupe_val, dupe_val], dtype=any_numeric_ea_dtype),
         ]
     )
     result = mi1.union(mi2)
     expected = MultiIndex.from_arrays(
         [
-            Series([1, 2, 3, 3], dtype=any_numeric_ea_dtype),
-            Series([1, 2, 3, 3], dtype=any_numeric_ea_dtype),
+            Series([1, 2, dupe_val, dupe_val], dtype=any_numeric_ea_dtype),
+            Series([1, 2, dupe_val, dupe_val], dtype=any_numeric_ea_dtype),
         ]
     )
     tm.assert_index_equal(result, expected)
