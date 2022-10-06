c34da509497717308c97c4a211ad3ff9bab92d87
Luke Manley <lukemanley@gmail.com>
2022-10-06 04:00:30 -0400

    BUG/PERF: algos.union_with_duplicates losing EA dtypes (#48900)

--- pandas/tests/indexes/multi/test_setops.py
+++ pandas/tests/indexes/multi/test_setops.py
@@ -578,6 +578,31 @@ def test_union_keep_ea_dtype(any_numeric_ea_dtype, val):
     tm.assert_index_equal(result, expected)
 
 
+@pytest.mark.parametrize("dupe_val", [3, pd.NA])
+def test_union_with_duplicates_keep_ea_dtype(dupe_val, any_numeric_ea_dtype):
+    # GH48900
+    mi1 = MultiIndex.from_arrays(
+        [
+            Series([1, dupe_val, 2], dtype=any_numeric_ea_dtype),
+            Series([1, dupe_val, 2], dtype=any_numeric_ea_dtype),
+        ]
+    )
+    mi2 = MultiIndex.from_arrays(
+        [
+            Series([2, dupe_val, dupe_val], dtype=any_numeric_ea_dtype),
+            Series([2, dupe_val, dupe_val], dtype=any_numeric_ea_dtype),
+        ]
+    )
+    result = mi1.union(mi2)
+    expected = MultiIndex.from_arrays(
+        [
+            Series([1, 2, dupe_val, dupe_val], dtype=any_numeric_ea_dtype),
+            Series([1, 2, dupe_val, dupe_val], dtype=any_numeric_ea_dtype),
+        ]
+    )
+    tm.assert_index_equal(result, expected)
+
+
 def test_union_duplicates(index, request):
     # GH#38977
     if index.empty or isinstance(index, (IntervalIndex, CategoricalIndex)):
