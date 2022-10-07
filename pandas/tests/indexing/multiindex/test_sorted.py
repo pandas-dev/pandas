88fe6b44eca0895449c0dc6fbbc8e922562b5e91
Luke Manley <lukemanley@gmail.com>
2022-10-07 07:01:44 -0400

    BUG/PERF: use lexsort_indexer in MultiIndex.argsort (#48495)

--- pandas/tests/indexing/multiindex/test_sorted.py
+++ pandas/tests/indexing/multiindex/test_sorted.py
@@ -2,9 +2,11 @@ import numpy as np
 import pytest
 
 from pandas import (
+    NA,
     DataFrame,
     MultiIndex,
     Series,
+    array,
 )
 import pandas._testing as tm
 
@@ -83,6 +85,36 @@ class TestMultiIndexSorted:
         tuples = zip(*arrays)
         index = MultiIndex.from_tuples(tuples)
         expected = DataFrame(range(8), index=index)
+
+        tm.assert_frame_equal(result, expected)
+
+    def test_argsort_with_na(self):
+        # GH48495
+        arrays = [
+            array([2, NA, 1], dtype="Int64"),
+            array([1, 2, 3], dtype="Int64"),
+        ]
+        index = MultiIndex.from_arrays(arrays)
+        result = index.argsort()
+        expected = np.array([2, 0, 1], dtype=np.intp)
+        tm.assert_numpy_array_equal(result, expected)
+
+    def test_sort_values_with_na(self):
+        # GH48495
+        arrays = [
+            array([2, NA, 1], dtype="Int64"),
+            array([1, 2, 3], dtype="Int64"),
+        ]
+        index = MultiIndex.from_arrays(arrays)
+        index = index.sort_values()
+        result = DataFrame(range(3), index=index)
+
+        arrays = [
+            array([1, 2, NA], dtype="Int64"),
+            array([3, 1, 2], dtype="Int64"),
+        ]
+        index = MultiIndex.from_arrays(arrays)
+        expected = DataFrame(range(3), index=index)
 
         tm.assert_frame_equal(result, expected)
 
