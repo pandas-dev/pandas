5264283975920de8e85c7793be4f08741493d911
jbrockmendel <jbrockmendel@gmail.com>
2022-10-07 17:26:41 -0700

    API: Timedelta constructor pytimedelta, Tick preserve reso (#48918)
    * BUG: Timedelta.__new__
    
    * remove assertion
    
    * GH refs
    
    * API: Timedelta(td64_obj) retain resolution
    
    * API: Timedelta constructor pytimedelta, Tick preserve reso
    
    * remove debugging variable
    
    * remove duplicate

--- pandas/tests/dtypes/cast/test_promote.py
+++ pandas/tests/dtypes/cast/test_promote.py
@@ -480,6 +480,12 @@ def test_maybe_promote_any_with_timedelta64(
                 "Timedelta scalar"
             )
             request.node.add_marker(mark)
+        elif type(fill_value) is datetime.timedelta:
+            mark = pytest.mark.xfail(
+                reason="maybe_promote not yet updated to handle non-nano "
+                "Timedelta scalar"
+            )
+            request.node.add_marker(mark)
     else:
         expected_dtype = np.dtype(object)
         exp_val_for_scalar = fill_value
