5edd8d1855a507b86b32e63b42487ee704044119
Ambuj Pawar <your_email@abc.example>
2022-10-08 11:36:39 +0200

    Fix: precommit issues

--- pandas/tests/indexing/test_at.py
+++ pandas/tests/indexing/test_at.py
@@ -239,7 +239,9 @@ class TestAtErrors:
         # GH#48729 .at should raise InvalidIndexError when assigning rows
         df = DataFrame(index=["a"], columns=["col1", "col2"])
         new_row = [123, 15]
-        with pytest.raises(InvalidIndexError,
-                           match=f"You can only assign a scalar value not a \\{type(new_row)} with value \\{new_row}"
-                           ):
+        with pytest.raises(
+            InvalidIndexError,
+            match=f"You can only assign a scalar value not a \\{type(new_row)} "
+            f"with value \\{new_row}",
+        ):
             df.at["a"] = new_row
