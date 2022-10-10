daca2ba340eff9c7c7a0529dc2dac91460426a87
Jonathan <JMBurley@users.noreply.github.com>
2022-10-10 18:17:12 -0400

    remove unneeded comment

--- pandas/core/arrays/sparse/accessor.py
+++ pandas/core/arrays/sparse/accessor.py
@@ -369,8 +369,7 @@ class SparseFrameAccessor(BaseAccessor, PandasDelegate):
         Ratio of non-sparse points to total (dense) data points.
         """
         tmp = np.mean([column.array.density for _, column in self._parent.items()])
-        # error: Expression of type "floating" cannot be assigned to return type "float"
-        return tmp  # pyright: ignore[reportGeneralTypeIssues]
+        return tmp
 
     @staticmethod
     def _prep_index(data, index, columns):
