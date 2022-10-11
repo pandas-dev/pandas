58d3bbef65b35b2ea08b3b4753ac10ca9584d0e5
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2022-10-11 23:42:43 +0100

    BUG: guess_datetime_format doesn't guess 2018-01-01T00:00:00.000 (#49047)
    * guess nanoseconds
    
    * add gh reference number
    
    Co-authored-by: MarcoGorelli <>

--- pandas/core/tools/datetimes.py
+++ pandas/core/tools/datetimes.py
@@ -820,7 +820,7 @@ def to_datetime(
           to the day starting at noon on January 1, 4713 BC.
         - If Timestamp convertible (Timestamp, dt.datetime, np.datetimt64 or date
           string), origin is set to Timestamp identified by origin.
-        - If a float or integer, origin is the mullisecond difference
+        - If a float or integer, origin is the millisecond difference
           relative to 1970-01-01.
     cache : bool, default True
         If :const:`True`, use a cache of unique, converted dates to apply the
