4ff420833bfd228f6d21f937a279f64e53875ddb
Brock <jbrockmendel@gmail.com>
2022-10-10 19:22:38 -0700

    update test

--- pandas/tests/arrays/datetimes/test_constructors.py
+++ pandas/tests/arrays/datetimes/test_constructors.py
@@ -140,9 +140,7 @@ class TestSequenceToDT64NS:
 
     def test_tz_dtype_matches(self):
         dtype = DatetimeTZDtype(tz="US/Central")
-        arr = DatetimeArray._from_sequence(
-            ["2000"], dtype=dtype
-        )
+        arr = DatetimeArray._from_sequence(["2000"], dtype=dtype)
         result = DatetimeArray._from_sequence_not_strict(arr, dtype=dtype)
         tm.assert_equal(arr, result)
 
