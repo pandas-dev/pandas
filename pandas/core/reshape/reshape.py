75a7a7851141523d17aa8cc2405f601953e6ddcc
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-10-14 17:36:25 +0200

    BUG: unstack accessing wrong index level when midx has mixed names

--- pandas/core/reshape/reshape.py
+++ pandas/core/reshape/reshape.py
@@ -470,9 +470,8 @@ def unstack(obj: Series | DataFrame, level, fill_value=None):
         else:
             level = level[0]
 
-    # Prioritize integer interpretation (GH #21677):
     if not is_integer(level) and not level == "__placeholder__":
-        level = obj.index._get_level_number(level)
+        obj.index._get_level_number(level)
 
     if isinstance(obj, DataFrame):
         if isinstance(obj.index, MultiIndex):
