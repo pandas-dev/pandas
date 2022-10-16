2715677549748dc05a5eb282bdbb340c283ca397
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2022-10-16 13:05:25 +0100

    BUG: _guess_datetime_format_for_array doesn't guess if first element is '' or 'NaT' (#49120)

--- pandas/_libs/tslib.pyx
+++ pandas/_libs/tslib.pyx
@@ -421,6 +421,23 @@ def array_with_unit_to_datetime(
 
     return oresult, tz
 
+@cython.wraparound(False)
+@cython.boundscheck(False)
+def first_non_null(values: ndarray) -> int:
+    """Find position of first non-null value, return -1 if there isn't one."""
+    cdef:
+        Py_ssize_t n = len(values)
+        Py_ssize_t i
+        int result
+    for i in range(n):
+        val = values[i]
+        if checknull_with_nat_and_na(val):
+            continue
+        if isinstance(val, str) and (len(val) == 0 or val in nat_strings):
+            continue
+        return i
+    else:
+        return -1
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
