9c66ab9dfebabd4cd849aade484f00d600e9d75f
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2022-10-17 13:51:43 -0700

    Remove unused rand_bools

--- pandas/_testing/__init__.py
+++ pandas/_testing/__init__.py
@@ -60,7 +60,6 @@ from pandas._testing._io import (
     write_to_compressed,
 )
 from pandas._testing._random import (
-    randbool,
     rands,
     rands_array,
 )
@@ -1116,7 +1115,6 @@ __all__ = [
     "NULL_OBJECTS",
     "OBJECT_DTYPES",
     "raise_assert_detail",
-    "randbool",
     "rands",
     "reset_display_options",
     "RNGContext",
