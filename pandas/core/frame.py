053814de6dd61a17aa358263824c6f9339de429d
Ambuj Pawar <your_email@abc.example>
2022-10-17 21:07:20 +0200

    Raise InvalidIndex error from original error

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -4242,7 +4242,9 @@ class DataFrame(NDFrame, OpsMixin):
         except InvalidIndexError as ii_err:
             # GH48729: Seems like you are trying to assign a value to a
             # row when only scalar options are permitted
-            raise InvalidIndexError(ii_err)
+            raise InvalidIndexError(
+                f"You can only assign a scalar value not a {type(value)}"
+            ) from ii_err
 
     def _ensure_valid_index(self, value) -> None:
         """
