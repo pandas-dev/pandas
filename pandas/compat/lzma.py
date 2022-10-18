7604d48da0c72985ab4a6d98e4c77368ecca5f14
jakirkham <jakirkham@gmail.com>
2022-10-18 12:00:24 -0700

    Patch `BZ2File` & `LZMAFile` on Python pre-3.10
    This should limit the effects of this patch. Also should make it easier
    to remove this backport later once all supported Python versions have
    the fix.

--- pandas/compat/lzma.py
+++ pandas/compat/lzma.py
@@ -5,14 +5,21 @@ Patched ``LZMAFile`` to handle pickle protocol 5.
 from __future__ import annotations
 
 import lzma
+import sys
 
 from pandas.compat._utils import flatten_buffer
 
 
-class LZMAFile(lzma.LZMAFile):
-    def write(self, b) -> int:
-        # Workaround issue where `lzma.LZMAFile` expects `len`
-        # to return the number of bytes in `b` by converting
-        # `b` into something that meets that constraint with
-        # minimal copying.
-        return super().write(flatten_buffer(b))
+if sys.version_info < (3, 10):
+    class LZMAFile(lzma.LZMAFile):
+        def write(self, b) -> int:
+            # Workaround issue where `lzma.LZMAFile` expects `len`
+            # to return the number of bytes in `b` by converting
+            # `b` into something that meets that constraint with
+            # minimal copying.
+            #
+            # Note: This is fixed in Python 3.10.
+            return super().write(flatten_buffer(b))
+else:
+    class LZMAFile(lzma.LZMAFile):
+        pass
