1b111887cff577f22074c3327cf08087bdc3195c
jakirkham <jakirkham@gmail.com>
2022-10-18 18:49:24 -0700

    Assign `IS64` first

--- pandas/compat/_constants.py
+++ pandas/compat/_constants.py
@@ -10,8 +10,9 @@ from __future__ import annotations
 import platform
 import sys
 
+IS64 = sys.maxsize > 2**32
+
 PY39 = sys.version_info >= (3, 9)
 PY310 = sys.version_info >= (3, 10)
 PY311 = sys.version_info >= (3, 11)
 PYPY = platform.python_implementation() == "PyPy"
-IS64 = sys.maxsize > 2**32
