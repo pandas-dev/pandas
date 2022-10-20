366f645e6f0f71401619d432d15582393169d1ca
jakirkham <jakirkham@gmail.com>
2022-10-19 22:34:36 -0700

    Assign constants to themselves
    Should work around linter issues.

--- pandas/compat/__init__.py
+++ pandas/compat/__init__.py
@@ -16,11 +16,11 @@ import sys
 from pandas._typing import F
 import pandas.compat._compressors
 from pandas.compat._constants import (
-    IS64 as IS64,
-    PY39 as PY39,
-    PY310 as PY310,
-    PY311 as PY311,
-    PYPY as PYPY,
+    IS64,
+    PY39,
+    PY310,
+    PY311,
+    PYPY,
 )
 from pandas.compat.numpy import (
     is_numpy_dev,
@@ -32,6 +32,12 @@ from pandas.compat.pyarrow import (
     pa_version_under8p0,
     pa_version_under9p0,
 )
+
+IS64 = IS64
+PY39 = PY39
+PY310 = PY310
+PY311 = PY311
+PYPY = PYPY
 
 
 def set_function_name(f: F, name: str, cls) -> F:
