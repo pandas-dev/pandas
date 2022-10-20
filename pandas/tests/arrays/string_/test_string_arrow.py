a2946293ae49245585de15fa0760af423120e7da
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2022-10-20 09:19:48 -0700

    BUG: pickling subset of Arrow-backed data would serialize the entire data (#49078)
    * BUG: pickling subset of Arrow-backed data would serialize the entire data
    
    * Use data

--- pandas/tests/arrays/string_/test_string_arrow.py
+++ pandas/tests/arrays/string_/test_string_arrow.py
@@ -1,3 +1,4 @@
+import pickle
 import re
 
 import numpy as np
@@ -197,3 +198,20 @@ def test_setitem_invalid_indexer_raises():
 
     with pytest.raises(ValueError, match=None):
         arr[[0, 1]] = ["foo", "bar", "baz"]
+
+
+@skip_if_no_pyarrow
+def test_pickle_roundtrip():
+    # GH 42600
+    expected = pd.Series(range(10), dtype="string[pyarrow]")
+    expected_sliced = expected.head(2)
+    full_pickled = pickle.dumps(expected)
+    sliced_pickled = pickle.dumps(expected_sliced)
+
+    assert len(full_pickled) > len(sliced_pickled)
+
+    result = pickle.loads(full_pickled)
+    tm.assert_series_equal(result, expected)
+
+    result_sliced = pickle.loads(sliced_pickled)
+    tm.assert_series_equal(result_sliced, expected_sliced)
