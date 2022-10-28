8ea52bb32a5a0956467b3532e6c2ac704c8b90e2
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2022-10-28 16:16:55 -0700

    DEPR: Enforce disallowing loc with positionals (#49345)
    * DEPR: Enforce disallowing loc with positionals
    
    * Fix asv usage
    
    * Trigger CI

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -4189,12 +4189,9 @@ class Index(IndexOpsMixin, PandasObject):
         elif is_positional:
             if kind == "loc":
                 # GH#16121, GH#24612, GH#31810
-                warnings.warn(
-                    "Slicing a positional slice with .loc is not supported, "
-                    "and will raise TypeError in a future version.  "
+                raise TypeError(
+                    "Slicing a positional slice with .loc is not allowed, "
                     "Use .loc with labels or .iloc with positions instead.",
-                    FutureWarning,
-                    stacklevel=find_stack_level(),
                 )
             indexer = key
         else:
