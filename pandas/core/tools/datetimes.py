610ffbc856c3ed3bf4e940c1458ad16ecc61913c
sarvaSanjay <111774640+sarvaSanjay@users.noreply.github.com>
2022-10-28 09:19:13 -0400

    DOC: Removed line about %S parses all the way up to nanosecond even if no (#49349)
    * Removed line about %S parses all the way up to nanosecond even if no decimal place present from docstring
    
    * Deleted example code and changed documentation to though note that %f will parse all the way up to nanoseconds
    
    * Fixed CI error
    
    * Changed  to %f
    
    * Removed line between format and exact
    
    * removed whitespace at end of line
    
    * Re-added formatting to %f

--- pandas/core/tools/datetimes.py
+++ pandas/core/tools/datetimes.py
@@ -782,15 +782,7 @@ def to_datetime(
         `strftime documentation
         <https://docs.python.org/3/library/datetime.html
         #strftime-and-strptime-behavior>`_ for more information on choices, though
-        note the following differences:
-
-        - :const:`"%f"` will parse all the way
-          up to nanoseconds;
-
-        - :const:`"%S"` without :const:`"%f"` will capture all the way
-          up to nanoseconds if present as decimal places, and will also handle
-          the case where the number of seconds is an integer.
-
+        note that :const:`"%f"` will parse all the way up to nanoseconds.
     exact : bool, default True
         Control how `format` is used:
 
@@ -968,13 +960,6 @@ def to_datetime(
     >>> pd.to_datetime('2018-10-26 12:00:00.0000000011',
     ...                format='%Y-%m-%d %H:%M:%S.%f')
     Timestamp('2018-10-26 12:00:00.000000001')
-
-    :const:`"%S"` without :const:`"%f"` will capture all the way
-    up to nanoseconds if present as decimal places.
-
-    >>> pd.to_datetime('2017-03-22 15:16:45.433502912',
-    ...                format='%Y-%m-%d %H:%M:%S')
-    Timestamp('2017-03-22 15:16:45.433502912')
 
     **Non-convertible date/times**
 
