82617c381001a133a2ad561000ed904762783dd9
debnathshoham <debnathshoham@gmail.com>
2022-10-31 15:07:16 +0530

    mypy happy

--- pandas/core/computation/pytables.py
+++ pandas/core/computation/pytables.py
@@ -606,20 +606,21 @@ class PyTablesExpr(expr.Expr):
 
     def evaluate(self):
         """create and return the numexpr condition and filter"""
-        try:
-            self.condition = self.terms.prune(ConditionBinOp)
-        except AttributeError as err:
-            raise ValueError(
-                f"cannot process expression [{self.expr}], [{self}] "
-                "is not a valid condition"
-            ) from err
-        try:
-            self.filter = self.terms.prune(FilterBinOp)
-        except AttributeError as err:
-            raise ValueError(
-                f"cannot process expression [{self.expr}], [{self}] "
-                "is not a valid filter"
-            ) from err
+        if self.terms is not None:
+            try:
+                self.condition = self.terms.prune(ConditionBinOp)
+            except AttributeError as err:
+                raise ValueError(
+                    f"cannot process expression [{self.expr}], [{self}] "
+                    "is not a valid condition"
+                ) from err
+            try:
+                self.filter = self.terms.prune(FilterBinOp)
+            except AttributeError as err:
+                raise ValueError(
+                    f"cannot process expression [{self.expr}], [{self}] "
+                    "is not a valid filter"
+                ) from err
 
         return self.condition, self.filter
 
