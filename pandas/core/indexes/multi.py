77b4e0991d511bfe2dc2bf673e7ea4736d9b08db
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-11-01 14:35:01 +0100

    BUG: MultiIndex.get_indexer not matching nan values

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -1088,8 +1088,10 @@ class MultiIndex(Index):
     @cache_readonly
     def _engine(self):
         # Calculate the number of bits needed to represent labels in each
-        # level, as log2 of their sizes (including -1 for NaN):
-        sizes = np.ceil(np.log2([len(level) + 1 for level in self.levels]))
+        # level, as log2 of their sizes:
+        # NaN values are shifted to 1 and missing values in other while
+        # calculating the indexer are shifted to 0
+        sizes = np.ceil(np.log2([len(level) + 2 for level in self.levels]))
 
         # Sum bit counts, starting from the _right_....
         lev_bits = np.cumsum(sizes[::-1])[::-1]
