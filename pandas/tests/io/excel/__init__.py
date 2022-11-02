514c328e4705caab2b9f4981386849d438669344
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2022-11-01 20:42:58 -0400

    CLN: Remove xlrd < 2.0 code (#49376)
    * CLN: Remove xlrd < 2.0 code
    
    * Add test
    
    * Add warning
    
    * Fix type-hint
    
    * Update pandas/io/excel/_xlrd.py
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
    
    * Remove warning
    
    * Remove warnings in tests
    
    * Remove filter
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/tests/io/excel/__init__.py
+++ pandas/tests/io/excel/__init__.py
@@ -9,8 +9,4 @@ pytestmark = [
     pytest.mark.filterwarnings(
         "ignore:This method will be removed in future versions:DeprecationWarning"
     ),
-    # GH 38571
-    pytest.mark.filterwarnings(
-        "ignore:.*In xlrd >= 2.0, only the xls format is supported:FutureWarning"
-    ),
 ]
