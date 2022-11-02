fa3c521fe16ffae19f12bd0ac5bc08e08c9b3d01
Bill <35750915+roadswitcher@users.noreply.github.com>
2022-11-02 09:37:49 -0400

    Update pandas/tests/io/parser/test_c_parser_only.py
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- pandas/tests/io/parser/test_c_parser_only.py
+++ pandas/tests/io/parser/test_c_parser_only.py
@@ -176,7 +176,7 @@ def test_precise_conversion(c_parser_only):
     normal_errors = []
     precise_errors = []
 
-    def error(val: float, actual_val: Decimal) -> float:
+    def error(val: float, actual_val: Decimal) -> Decimal:
         return abs(Decimal(f"{val:.100}") - actual_val)
 
     # test numbers between 1 and 2
