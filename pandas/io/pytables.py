b77417832c76f0027723cad68ffd5654bbafe2a9
Bill <35750915+roadswitcher@users.noreply.github.com>
2022-11-03 12:32:05 -0400

    STYLE: enable pylint warnings for `cell-var-from-loop` (#49445)
    * STYLE: Refactored one loop, suppressed three false positives.
    
    * Oooh, didn't think of that.
    
    * I need to get better at spotting this stuff.
    
    * Yeah, let's not suppress those after all.
    
    * Accidentally removed a newline.
    
    * Helps to revert to correct version of file.
    
    * OK, backing slowly away from the git client now.
    
    * OK, backing slowly away from the git client now.
    
    * Resolve autotyper find
    
    * Added type hints per reviewer note.
    
    * Update pandas/core/strings/object_array.py
    
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
    
    * Update pandas/tests/io/parser/test_c_parser_only.py
    
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
    
    Co-authored-by: William Blum <william.blum@altamiracorp.com>
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- pandas/io/pytables.py
+++ pandas/io/pytables.py
@@ -4094,44 +4094,44 @@ class Table(Fixed):
         for axis, labels in self.non_index_axes:
             obj = _reindex_axis(obj, axis, labels, columns)
 
+            def process_filter(field, filt, op):
+
+                for axis_name in obj._AXIS_ORDERS:
+                    axis_number = obj._get_axis_number(axis_name)
+                    axis_values = obj._get_axis(axis_name)
+                    assert axis_number is not None
+
+                    # see if the field is the name of an axis
+                    if field == axis_name:
+
+                        # if we have a multi-index, then need to include
+                        # the levels
+                        if self.is_multi_index:
+                            filt = filt.union(Index(self.levels))
+
+                        takers = op(axis_values, filt)
+                        return obj.loc(axis=axis_number)[takers]
+
+                    # this might be the name of a file IN an axis
+                    elif field in axis_values:
+
+                        # we need to filter on this dimension
+                        values = ensure_index(getattr(obj, field).values)
+                        filt = ensure_index(filt)
+
+                        # hack until we support reversed dim flags
+                        if isinstance(obj, DataFrame):
+                            axis_number = 1 - axis_number
+
+                        takers = op(values, filt)
+                        return obj.loc(axis=axis_number)[takers]
+
+                raise ValueError(f"cannot find the field [{field}] for filtering!")
+
         # apply the selection filters (but keep in the same order)
         if selection.filter is not None:
             for field, op, filt in selection.filter.format():
-
-                def process_filter(field, filt):
-
-                    for axis_name in obj._AXIS_ORDERS:
-                        axis_number = obj._get_axis_number(axis_name)
-                        axis_values = obj._get_axis(axis_name)
-                        assert axis_number is not None
-
-                        # see if the field is the name of an axis
-                        if field == axis_name:
-
-                            # if we have a multi-index, then need to include
-                            # the levels
-                            if self.is_multi_index:
-                                filt = filt.union(Index(self.levels))
-
-                            takers = op(axis_values, filt)
-                            return obj.loc(axis=axis_number)[takers]
-
-                        # this might be the name of a file IN an axis
-                        elif field in axis_values:
-
-                            # we need to filter on this dimension
-                            values = ensure_index(getattr(obj, field).values)
-                            filt = ensure_index(filt)
-
-                            # hack until we support reversed dim flags
-                            if isinstance(obj, DataFrame):
-                                axis_number = 1 - axis_number
-                            takers = op(values, filt)
-                            return obj.loc(axis=axis_number)[takers]
-
-                    raise ValueError(f"cannot find the field [{field}] for filtering!")
-
-                obj = process_filter(field, filt)
+                obj = process_filter(field, filt, op)
 
         return obj
 
