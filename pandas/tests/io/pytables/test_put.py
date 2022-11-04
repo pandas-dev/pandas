891fe6c14328d1011d60bd51db759d1c7d278357
Terji Petersen <terjipetersen@Terjis-Air.fritz.box>
2022-11-04 14:36:59 +0000

    fix feather_format

--- pandas/tests/io/pytables/test_put.py
+++ pandas/tests/io/pytables/test_put.py
@@ -21,7 +21,6 @@ from pandas import (
     _testing as tm,
     concat,
 )
-from pandas.core.api import Int64Index
 from pandas.tests.io.pytables.common import (
     _maybe_remove,
     ensure_clean_store,
