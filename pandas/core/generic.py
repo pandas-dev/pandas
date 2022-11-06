2b5319793dcce8d7382142ff90b9aeca22e6d64b
ram vikram singh <ramvikrams243@gmail.com>
2022-11-06 15:05:10 +0530

    for #49508 changing Doc for DataFrame.astype
    added the series in input in the doc of DataFrame.astype

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -6004,11 +6004,12 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
 
         Parameters
         ----------
-        dtype : data type, or dict of column name -> data type
-            Use a numpy.dtype or Python type to cast entire pandas object to
-            the same type. Alternatively, use {col: dtype, ...}, where col is a
-            column label and dtype is a numpy.dtype or Python type to cast one
-            or more of the DataFrame's columns to column-specific types.
+        dtype : string, data type, Series or Mapping of column name -> data type
+            Use a data type like object (string, numpy.dtype, pandas.ExtensionDtype
+            or Python type) to cast entire pandas object to the same type. Alternatively,
+            use {col: dtype, ...}, where col is a column label and dtype is a 
+            numpy.dtype or Python type to cast one or more of the DataFrame's
+            columns to column-specific types.
         copy : bool, default True
             Return a copy when ``copy=True`` (be very careful setting
             ``copy=False`` as changes to values then may propagate to other
