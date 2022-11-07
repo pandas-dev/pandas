c6694a20e3b1515d6f5976d88bd649e9e45ed12b
Kostya Farber <73378227+kostyafarber@users.noreply.github.com>
2022-11-07 19:38:42 +0000

    ENH: Add pre-commit check for setup.cfg options.extras_require (#49261)
    * Add dependency check for setup.cfg
    
    * improve diff alert, add install mapping, remove test deps
    
    * align versions across min version files
    
    * fix version issues for boto3, s3fs and pyqt5. Small change to script
    
    * fix version equality, modify script for CI check
    
    * fix version typo in min version yaml and update min version script
    
    * bump fsspec, gcsfs to match in whats new
    
    * Revert "bump fsspec, gcsfs to match in whats new"
    
    This reverts commit 52efecb8c7c65e25640faeb8de5ea4bc7bdf40e5.
    
    * align fastparquet, lowercase PyQt5
    
    * ENH: align matplotlib versions
    
    * ENH: align matplotlib in setup.cfg
    
    * bump s3fs across min version files
    
    * Update scripts/validate_min_versions_in_sync.py
    
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
    
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- scripts/validate_min_versions_in_sync.py
+++ scripts/validate_min_versions_in_sync.py
@@ -4,6 +4,7 @@ Check pandas required and optional dependencies are synced across:
 
 ci/deps/actions-.*-minimum_versions.yaml
 pandas/compat/_optional.py
+setup.cfg
 
 TODO: doc/source/getting_started/install.rst
 
@@ -13,6 +14,7 @@ This is meant to be run as a pre-commit hook - to run it manually, you can do:
 """
 from __future__ import annotations
 
+import configparser
 import pathlib
 import sys
 
@@ -21,6 +23,7 @@ CI_PATH = next(
     pathlib.Path("ci/deps").absolute().glob("actions-*-minimum_versions.yaml")
 )
 CODE_PATH = pathlib.Path("pandas/compat/_optional.py").resolve()
+SETUP_PATH = pathlib.Path("setup.cfg").resolve()
 EXCLUDE_DEPS = {"tzdata"}
 # pandas package is not available
 # in pre-commit environment
@@ -57,29 +60,76 @@ def get_versions_from_ci(content: list[str]) -> tuple[dict[str, str], dict[str, 
             seen_required = True
         elif "# optional dependencies" in line:
             seen_optional = True
+        elif "- pip:" in line:
+            continue
         elif seen_required and line.strip():
-            package, version = line.strip().split("=")
+            if "==" in line:
+                package, version = line.strip().split("==")
+
+            else:
+                package, version = line.strip().split("=")
             package = package[2:]
             if package in EXCLUDE_DEPS:
                 continue
             if not seen_optional:
-                required_deps[package] = version
+                required_deps[package.casefold()] = version
             else:
-                optional_deps[package] = version
+                optional_deps[package.casefold()] = version
     return required_deps, optional_deps
+
+
+def get_versions_from_setup() -> dict[str, str]:
+    install_map = _optional.INSTALL_MAPPING
+    optional_dependencies = {}
+
+    parser = configparser.ConfigParser()
+    parser.read(SETUP_PATH)
+    setup_optional = parser["options.extras_require"]["all"]
+    dependencies = setup_optional[1:].split("\n")
+
+    # remove test dependencies
+    test = parser["options.extras_require"]["test"]
+    test_dependencies = set(test[1:].split("\n"))
+    dependencies = [
+        package for package in dependencies if package not in test_dependencies
+    ]
+
+    for dependency in dependencies:
+        package, version = dependency.strip().split(">=")
+        optional_dependencies[install_map.get(package, package).casefold()] = version
+
+    for item in EXCLUDE_DEPS:
+        optional_dependencies.pop(item)
+
+    return optional_dependencies
 
 
 def main():
     with open(CI_PATH, encoding="utf-8") as f:
         _, ci_optional = get_versions_from_ci(f.readlines())
     code_optional = get_versions_from_code()
-    diff = set(ci_optional.items()).symmetric_difference(code_optional.items())
+    setup_optional = get_versions_from_setup()
+
+    diff = (ci_optional.items() | code_optional.items() | setup_optional.items()) - (
+        ci_optional.items() & code_optional.items() & setup_optional.items()
+    )
+
     if diff:
-        sys.stdout.write(
+        packages = {package for package, _ in diff}
+        out = sys.stdout
+        out.write(
             f"The follow minimum version differences were found between  "
-            f"{CI_PATH} and {CODE_PATH}. Please ensure these are aligned: "
-            f"{diff}\n"
+            f"{CI_PATH}, {CODE_PATH} AND {SETUP_PATH}. "
+            f"Please ensure these are aligned: \n\n"
         )
+
+        for package in packages:
+            out.write(
+                f"{package}\n"
+                f"{CI_PATH}: {ci_optional.get(package, 'Not specified')}\n"
+                f"{CODE_PATH}: {code_optional.get(package, 'Not specified')}\n"
+                f"{SETUP_PATH}: {setup_optional.get(package, 'Not specified')}\n\n"
+            )
         sys.exit(1)
     sys.exit(0)
 
