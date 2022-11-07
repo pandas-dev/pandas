203928ebed690d33b2bc3abb7abfb34653f01150
jbrockmendel <jbrockmendel@gmail.com>
2022-11-07 10:13:54 -0800

    DEPR: maybe_promote with pydate (#49487)
    * DEPR: maybe_promote with pydate
    
    * fix whatsnew

--- pandas/tests/frame/methods/test_reindex.py
+++ pandas/tests/frame/methods/test_reindex.py
@@ -136,29 +136,31 @@ class TestDataFrameSelectReindex:
 
     @td.skip_array_manager_not_yet_implemented
     def test_reindex_date_fill_value(self):
-        # passing date to dt64 is deprecated
+        # passing date to dt64 is deprecated; enforced in 2.0 to cast to object
         arr = date_range("2016-01-01", periods=6).values.reshape(3, 2)
         df = DataFrame(arr, columns=["A", "B"], index=range(3))
 
         ts = df.iloc[0, 0]
         fv = ts.date()
 
-        with tm.assert_produces_warning(FutureWarning):
-            res = df.reindex(index=range(4), columns=["A", "B", "C"], fill_value=fv)
+        res = df.reindex(index=range(4), columns=["A", "B", "C"], fill_value=fv)
 
         expected = DataFrame(
-            {"A": df["A"].tolist() + [ts], "B": df["B"].tolist() + [ts], "C": [ts] * 4}
+            {"A": df["A"].tolist() + [fv], "B": df["B"].tolist() + [fv], "C": [fv] * 4},
+            dtype=object,
         )
         tm.assert_frame_equal(res, expected)
 
         # only reindexing rows
-        with tm.assert_produces_warning(FutureWarning):
-            res = df.reindex(index=range(4), fill_value=fv)
+        res = df.reindex(index=range(4), fill_value=fv)
         tm.assert_frame_equal(res, expected[["A", "B"]])
 
         # same with a datetime-castable str
         res = df.reindex(
             index=range(4), columns=["A", "B", "C"], fill_value="2016-01-01"
+        )
+        expected = DataFrame(
+            {"A": df["A"].tolist() + [ts], "B": df["B"].tolist() + [ts], "C": [ts] * 4},
         )
         tm.assert_frame_equal(res, expected)
 
