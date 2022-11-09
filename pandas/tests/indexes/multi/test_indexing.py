f9dc98b167458dc84a0baeb11e32245d819358b4
Luke Manley <lukemanley@gmail.com>
2022-11-09 16:01:54 -0500

    TST: MultiIndex.get_indexer with na/missing (#48877)

--- pandas/tests/indexes/multi/test_indexing.py
+++ pandas/tests/indexes/multi/test_indexing.py
@@ -933,3 +933,17 @@ def test_get_locs_reordering(keys, expected):
     result = idx.get_locs(keys)
     expected = np.array(expected, dtype=np.intp)
     tm.assert_numpy_array_equal(result, expected)
+
+
+def test_get_indexer_for_multiindex_with_nans(nulls_fixture):
+    # GH37222
+    idx1 = MultiIndex.from_product([["A"], [1.0, 2.0]], names=["id1", "id2"])
+    idx2 = MultiIndex.from_product([["A"], [nulls_fixture, 2.0]], names=["id1", "id2"])
+
+    result = idx2.get_indexer(idx1)
+    expected = np.array([-1, 1], dtype=np.intp)
+    tm.assert_numpy_array_equal(result, expected)
+
+    result = idx1.get_indexer(idx2)
+    expected = np.array([-1, 1], dtype=np.intp)
+    tm.assert_numpy_array_equal(result, expected)
