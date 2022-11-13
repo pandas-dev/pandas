92f57073490610aed667a517826cae2e0112f3f1
seljaks <33955366+seljaks@users.noreply.github.com>
2022-11-14 00:22:14 +0100

    ENH/TST: add CoW to df.drop and test for it GH49473

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -52,6 +52,24 @@ def test_copy_shallow(using_copy_on_write):
 # DataFrame methods returning new DataFrame using shallow copy
 
 
+def test_drop_on_column(using_copy_on_write):
+    df = DataFrame(
+        {"a": [1, 2, 3], "b": [4, 5, 6], "c": [0.1, 0.2, 0.3]}, index=[10, 11, 12]
+    )
+    df_orig = df.copy()
+    df2 = df.drop(columns="a")
+    df2._mgr._verify_integrity()
+
+    if using_copy_on_write:
+        assert np.shares_memory(get_array(df2, "b"), get_array(df, "b"))
+        assert np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
+    df2.iloc[0, 0] = 0
+    assert not np.shares_memory(get_array(df2, "b"), get_array(df, "b"))
+    if using_copy_on_write:
+        assert np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
+    tm.assert_frame_equal(df, df_orig)
+
+
 def test_reset_index(using_copy_on_write):
     # Case: resetting the index (i.e. adding a new column) + mutating the
     # resulting dataframe
@@ -61,7 +79,6 @@ def test_reset_index(using_copy_on_write):
     df_orig = df.copy()
     df2 = df.reset_index()
     df2._mgr._verify_integrity()
-
     if using_copy_on_write:
         # still shares memory (df2 is a shallow copy)
         assert np.shares_memory(get_array(df2, "b"), get_array(df, "b"))
