b32e60614ea62be68622723aadc596273879983c
seljaks <33955366+seljaks@users.noreply.github.com>
2022-11-15 20:55:55 +0100

    black

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -52,24 +52,6 @@ def test_copy_shallow(using_copy_on_write):
 # DataFrame methods returning new DataFrame using shallow copy
 
 
-def test_drop_on_column(using_copy_on_write):
-    df = DataFrame(
-        {"a": [1, 2, 3], "b": [4, 5, 6], "c": [0.1, 0.2, 0.3]}, index=[10, 11, 12]
-    )
-    df_orig = df.copy()
-    df2 = df.drop(columns="a")
-    df2._mgr._verify_integrity()
-
-    if using_copy_on_write:
-        assert np.shares_memory(get_array(df2, "b"), get_array(df, "b"))
-        assert np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
-    df2.iloc[0, 0] = 0
-    assert not np.shares_memory(get_array(df2, "b"), get_array(df, "b"))
-    if using_copy_on_write:
-        assert np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
-    tm.assert_frame_equal(df, df_orig)
-
-
 def test_reset_index(using_copy_on_write):
     # Case: resetting the index (i.e. adding a new column) + mutating the
     # resulting dataframe
@@ -145,6 +127,25 @@ def test_reindex_columns(using_copy_on_write):
     # mutating df2 triggers a copy-on-write for that column
     df2.iloc[0, 0] = 0
     assert not np.shares_memory(get_array(df2, "a"), get_array(df, "a"))
+    if using_copy_on_write:
+        assert np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
+    tm.assert_frame_equal(df, df_orig)
+
+
+def test_drop_on_column(using_copy_on_write):
+    df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [0.1, 0.2, 0.3]})
+    df_orig = df.copy()
+    df2 = df.drop(columns="a")
+    df2._mgr._verify_integrity()
+
+    if using_copy_on_write:
+        assert np.shares_memory(get_array(df2, "b"), get_array(df, "b"))
+        assert np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
+    else:
+        assert not np.shares_memory(get_array(df2, "b"), get_array(df, "b"))
+        assert not np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
+    df2.iloc[0, 0] = 0
+    assert not np.shares_memory(get_array(df2, "b"), get_array(df, "b"))
     if using_copy_on_write:
         assert np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
     tm.assert_frame_equal(df, df_orig)
