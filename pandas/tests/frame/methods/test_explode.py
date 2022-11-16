a6ed52eac6a48b127f731331bb98f06e7bbfaadc
debnathshoham <debnathshoham@gmail.com>
2022-11-16 22:22:13 +0530

    suggested changes

--- pandas/tests/frame/methods/test_explode.py
+++ pandas/tests/frame/methods/test_explode.py
@@ -282,7 +282,7 @@ def test_multi_columns(input_subset, expected_dict, expected_index):
     tm.assert_frame_equal(result, expected)
 
 
-def test_multi_columns2():
+def test_multi_columns_nan_empty():
     # GH 46084
     df = pd.DataFrame(
         {
@@ -294,10 +294,10 @@ def test_multi_columns2():
     result = df.explode(["A", "C"])
     expected = pd.DataFrame(
         {
-            "A": [0, 1, 5, np.nan, 2, 3],
+            "A": np.array([0, 1, 5, np.nan, 2, 3], dtype=object),
             "B": [9, 9, 8, 7, 6, 6],
-            "C": [1, 2, np.nan, np.nan, 3, 4],
+            "C": np.array([1, 2, np.nan, np.nan, 3, 4], dtype=object),
         },
         index=[0, 0, 1, 2, 3, 3],
-    ).astype({"A": object, "C": object})
+    )
     tm.assert_frame_equal(result, expected)
