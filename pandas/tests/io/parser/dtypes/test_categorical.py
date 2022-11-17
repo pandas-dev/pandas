963b62bf510b29f4a9f0aff2e011cf3a6b15d943
MarcoGorelli <>
2022-11-17 19:30:51 +0000

    fixup test

--- pandas/tests/io/parser/dtypes/test_categorical.py
+++ pandas/tests/io/parser/dtypes/test_categorical.py
@@ -262,7 +262,7 @@ def test_categorical_coerces_timestamp(all_parsers):
     parser = all_parsers
     dtype = {"b": CategoricalDtype([Timestamp("2014")])}
 
-    data = "b\n2014-01-01\n2014-01-01T00:00:00"
+    data = "b\n2014-01-01\n2014-01-01"
     expected = DataFrame({"b": Categorical([Timestamp("2014")] * 2)})
 
     result = parser.read_csv(StringIO(data), dtype=dtype)
