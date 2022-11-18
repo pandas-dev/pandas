5dbdb92c41d4b38ecc59d3fb7cf6abc6156a74a3
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2022-11-17 20:36:50 -0500

    BUG: groupby.describe with as_index=False incorrect (#49643)
    * BUG: groupby.describe with as_index=False incorrect
    
    * Add test for two groupings
    
    * Simplify logic

--- pandas/tests/groupby/test_apply.py
+++ pandas/tests/groupby/test_apply.py
@@ -974,15 +974,21 @@ def test_apply_function_index_return(function):
 
 
 def test_apply_function_with_indexing_return_column():
-    # GH#7002, GH#41480
+    # GH#7002, GH#41480, GH#49256
     df = DataFrame(
         {
             "foo1": ["one", "two", "two", "three", "one", "two"],
             "foo2": [1, 2, 4, 4, 5, 6],
         }
     )
-    with pytest.raises(TypeError, match="Could not convert"):
-        df.groupby("foo1", as_index=False).apply(lambda x: x.mean())
+    result = df.groupby("foo1", as_index=False).apply(lambda x: x.mean())
+    expected = DataFrame(
+        {
+            "foo1": ["one", "three", "two"],
+            "foo2": [3.0, 4.0, 4.0],
+        }
+    )
+    tm.assert_frame_equal(result, expected)
 
 
 @pytest.mark.parametrize(
