b21446b0a98e101c708c478ca3ddbe214a65fba0
Brock <jbrockmendel@gmail.com>
2022-11-21 13:07:18 -0800

    GH ref

--- pandas/tests/indexes/timedeltas/test_timedelta_range.py
+++ pandas/tests/indexes/timedeltas/test_timedelta_range.py
@@ -16,6 +16,7 @@ from pandas.tseries.offsets import (
 
 class TestTimedeltas:
     def test_timedelta_range_unit(self):
+        # GH#49824
         tdi = timedelta_range("0 Days", periods=10, freq="100000D", unit="s")
         exp_arr = (np.arange(10, dtype="i8") * 100_000).view("m8[D]").astype("m8[s]")
         tm.assert_numpy_array_equal(tdi.to_numpy(), exp_arr)
