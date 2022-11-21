2f8507569d4e24b1a8e5d57ed8bcbaca0e359cbe
4441only <94888665+yqyqyq-W@users.noreply.github.com>
2022-11-20 22:53:33 -0500

    Bug: Fix inconsistent behavior in searchsorted (#49620)

--- pandas/core/base.py
+++ pandas/core/base.py
@@ -1272,7 +1272,7 @@ class IndexOpsMixin(OpsMixin):
 
         if isinstance(value, ABCDataFrame):
             msg = (
-                "Value must be array-like or scalar, "
+                "Value must be 1-D array-like or scalar, "
                 f"{type(value).__name__} is not supported"
             )
             raise ValueError(msg)
