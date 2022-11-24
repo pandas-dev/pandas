0cc09c148a839647b0ce357cbbda61c1a33446f0
Luke Manley <lukemanley@gmail.com>
2022-11-24 09:15:10 -0500

    nullable tolist to return python scalars

---
+++ pandas/tests/series/methods/test_tolist.py
@@ -0,0 +1,34 @@
+import pytest
+
+from pandas import (
+    Interval,
+    Period,
+    Series,
+    Timedelta,
+    Timestamp,
+)
+
+
+@pytest.mark.parametrize(
+    "values, dtype, expected_dtype",
+    (
+        ([1], "int64", int),
+        ([1], "Int64", int),
+        ([1], "int64[pyarrow]", int),
+        ([1.0], "float64", float),
+        ([1.0], "Float64", float),
+        ([1.0], "float64[pyarrow]", float),
+        (["abc"], "object", str),
+        (["abc"], "string", str),
+        (["abc"], "string[pyarrow]", str),
+        ([Interval(1, 3)], "interval", Interval),
+        ([Period("2000-01-01", "D")], "period[D]", Period),
+        ([Timedelta(days=1)], "timedelta64[ns]", Timedelta),
+        ([Timestamp("2000-01-01")], "datetime64[ns]", Timestamp),
+    ),
+)
+def test_tolist_scalar_dtype(values, dtype, expected_dtype):
+    # GH #####
+    ser = Series(values, dtype=dtype)
+    result_dtype = type(ser.tolist()[0])
+    assert result_dtype == expected_dtype
