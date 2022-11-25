df6a108671e45a3e3b8a3d696c775576a24b3f75
MarcoGorelli <>
2022-11-25 14:19:42 +0000

    more fixup

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -1176,10 +1176,9 @@ class GroupBy(BaseGroupBy[NDFrameT]):
             values = reset_identity(values)
             result = concat(values, axis=self.axis)
 
-        if result.name is None:
-            name = self.obj.name if self.obj.ndim == 1 else self._selection
-            if isinstance(result, Series) and name is not None:
-                result.name = name
+        name = self.obj.name if self.obj.ndim == 1 else self._selection
+        if isinstance(result, Series) and name is not None:
+            result.name = name
 
         return result
 
