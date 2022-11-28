832ed522861ddf450fb636256ad00b0aeeb7b0c1
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-11-29 00:49:57 +0100

    Remove dtypes

--- asv_bench/benchmarks/join_merge.py
+++ asv_bench/benchmarks/join_merge.py
@@ -279,11 +279,9 @@ class MergeEA:
         "Int64",
         "Int32",
         "Int16",
-        "Int8",
         "UInt64",
         "UInt32",
         "UInt16",
-        "UInt8",
         "Float64",
         "Float32",
     ]
