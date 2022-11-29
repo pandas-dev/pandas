c3a257c17a1f3558811016c76de1e4cd69f71cb2
Marie K <lmknapp@gmail.com>
2022-11-29 11:06:21 -0800

    STYLE #49656: fixed redefined-outer-name linting issue to format.py  (#49937)
    * changed decimal module import, Decimal function call in EngFormatter class __call__ function
    
    * changed name of parameter for get_format_datetime64 to avoid redefined outer name conflict with is_dates_only function
    
    * change in argument name for call to get_format_datetime64 to match change in datetimes.py
    
    * removed import - justify function not used and creating redefined outer name conflict with justify functions defined for TextAdjustment and EastAsianTextAdjustment classes
    
    * replaced justify import with alias to avoid redefined-outer-name conflict
    
    * renamed imports from printing module
    
    * function renamed to is_dates_only_, returned argument for get_formate_datetime64 to is_dates_only
    
    * changes in datetimes.py to reflect function and argument names in format.py
    
    * argument and function names swapped
    
    * function name is is_dates_only, argument for get_format_datetime64 is is_dates_only_
    
    * Update .pre-commit-config.yaml
    
    removed format and datetimes files

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -384,7 +384,7 @@ class DatetimeIndex(DatetimeTimedeltaMixin):
     def _formatter_func(self):
         from pandas.io.formats.format import get_format_datetime64
 
-        formatter = get_format_datetime64(is_dates_only=self._is_dates_only)
+        formatter = get_format_datetime64(is_dates_only_=self._is_dates_only)
         return lambda x: f"'{formatter(x)}'"
 
     # --------------------------------------------------------------------
