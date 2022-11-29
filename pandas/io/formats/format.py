5c3863d1ed8dc6878f07661a33a425f0341fe276
bang128 <71242233+bang128@users.noreply.github.com>
2022-11-28 22:01:28 -0800

    Update format.py 9.99/10

--- pandas/io/formats/format.py
+++ pandas/io/formats/format.py
@@ -1794,12 +1794,12 @@ def _format_datetime64_dateonly(
 
 
 def get_format_datetime64(
-    dates_only: bool, nat_rep: str = "NaT", date_format: str | None = None
+    is_dates_only: bool, nat_rep: str = "NaT", date_format: str | None = None
 ) -> Callable:
     """Return a formatter callable taking a datetime64 as input and providing
     a string as output"""
 
-    if dates_only:
+    if is_dates_only:
         return lambda x: _format_datetime64_dateonly(
             x, nat_rep=nat_rep, date_format=date_format
         )
