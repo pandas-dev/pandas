c1be7c6fc358b7d639b05e8a80129f625fc9c366
Marko Pacak <info@markopacak.com>
2022-12-04 16:37:01 +0100

    CLN: remove return statement

--- pandas/_libs/tslibs/offsets.pyx
+++ pandas/_libs/tslibs/offsets.pyx
@@ -315,9 +315,8 @@ cdef _determine_offset(kwds):
     use_relativedelta = False
     if len(kwds_no_nanos) > 0:
         if "milliseconds" in kwds_no_nanos:
-            return timedelta(**kwds_no_nanos), use_relativedelta
-
-        if any(k in _kwds_use_relativedelta for k in kwds_no_nanos):
+            offset = timedelta(**kwds_no_nanos)
+        elif any(k in _kwds_use_relativedelta for k in kwds_no_nanos):
             if "millisecond" in kwds_no_nanos:
                 raise NotImplementedError(
                     "Using DateOffset to replace `millisecond` component in "
