c7b502bcef5632879917d55d05731cd705ef22c1
jbrockmendel <jbrockmendel@gmail.com>
2022-12-06 09:43:39 -0800

    ENH: Index.infer_objects (#50034)
    * ENH: Index.infer_objects
    
    * GH ref
    
    * mypy fixup
    
    * raise NotImplementedError for MultiIndex

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -6521,6 +6521,36 @@ class Index(IndexOpsMixin, PandasObject):
             indexer = indexer[~mask]
         return self.delete(indexer)
 
+    def infer_objects(self, copy: bool = True) -> Index:
+        """
+        If we have an object dtype, try to infer a non-object dtype.
+
+        Parameters
+        ----------
+        copy : bool, default True
+            Whether to make a copy in cases where no inference occurs.
+        """
+        if self._is_multi:
+            raise NotImplementedError(
+                "infer_objects is not implemented for MultiIndex. "
+                "Use index.to_frame().infer_objects() instead."
+            )
+        if self.dtype != object:
+            return self.copy() if copy else self
+
+        values = self._values
+        values = cast("npt.NDArray[np.object_]", values)
+        res_values = lib.maybe_convert_objects(
+            values,
+            convert_datetime=True,
+            convert_timedelta=True,
+            convert_period=True,
+            convert_interval=True,
+        )
+        if copy and res_values is values:
+            return self.copy()
+        return Index(res_values, name=self.name)
+
     # --------------------------------------------------------------------
     # Generated Arithmetic, Comparison, and Unary Methods
 
