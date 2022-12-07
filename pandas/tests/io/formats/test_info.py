5c9b48e043951742ff88d03fb5b044845f6a6cc1
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-12-07 22:44:10 +0100

    Fix

--- pandas/tests/io/formats/test_info.py
+++ pandas/tests/io/formats/test_info.py
@@ -499,5 +499,5 @@ def test_memory_usage_empty_no_warning():
     df = DataFrame(index=["a", "b"])
     with tm.assert_produces_warning(None):
         result = df.memory_usage()
-    expected = Series(16, index=["Index"])
+    expected = Series(16 if IS64 else 8, index=["Index"])
     tm.assert_series_equal(result, expected)
