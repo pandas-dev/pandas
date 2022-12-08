a083fad2d683f1f7c0d8384d9599d85d710cf63b
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2022-12-07 16:46:22 -0800

    Add unit test converting pandas nullable to pyarrow

--- pandas/tests/frame/methods/test_convert_dtypes.py
+++ pandas/tests/frame/methods/test_convert_dtypes.py
@@ -96,3 +96,27 @@ class TestConvertDtypes:
         with pd.option_context("mode.nullable_backend", "pyarrow"):
             result = expected.convert_dtypes()
         tm.assert_frame_equal(result, expected)
+
+    def test_pyarrow_nullable_backend_from_pandas_nullable(self):
+        pa = pytest.importorskip("pyarrow")
+        df = pd.DataFrame(
+            {
+                "a": pd.Series([1, 2, None], dtype="Int32"),
+                "b": pd.Series(["x", "y", None], dtype="string[python]"),
+                "c": pd.Series([True, False, None], dtype="boolean"),
+                "d": pd.Series([None, 100.5, 200], dtype="Float64"),
+            }
+        )
+        with pd.option_context("mode.nullable_backend", "pyarrow"):
+            result = df.convert_dtypes()
+        expected = pd.DataFrame(
+            {
+                "a": pd.arrays.ArrowExtensionArray(
+                    pa.array([1, 2, None], type=pa.int32())
+                ),
+                "b": pd.arrays.ArrowExtensionArray(pa.array(["x", "y", None])),
+                "c": pd.arrays.ArrowExtensionArray(pa.array([True, False, None])),
+                "d": pd.arrays.ArrowExtensionArray(pa.array([None, 100.5, 200.0])),
+            }
+        )
+        tm.assert_frame_equal(result, expected)
