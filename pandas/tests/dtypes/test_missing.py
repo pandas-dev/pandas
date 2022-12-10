ee533dd78a374d8fc781512952d0881358cbd231
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2022-12-10 17:02:53 +0000

    Backport PR #50129: CI: fix with new numpy nightly

--- pandas/tests/dtypes/test_missing.py
+++ pandas/tests/dtypes/test_missing.py
@@ -9,6 +9,7 @@ from pandas._config import config as cf
 
 from pandas._libs import missing as libmissing
 from pandas._libs.tslibs import iNaT
+from pandas.compat import is_numpy_dev
 
 from pandas.core.dtypes.common import (
     is_float,
@@ -461,7 +462,7 @@ def test_array_equivalent_series(val):
     cm = (
         # stacklevel is chosen to make sense when called from .equals
         tm.assert_produces_warning(FutureWarning, match=msg, check_stacklevel=False)
-        if isinstance(val, str)
+        if isinstance(val, str) and not is_numpy_dev
         else nullcontext()
     )
     with cm:
