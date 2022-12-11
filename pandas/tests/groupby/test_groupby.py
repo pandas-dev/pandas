a8dc3d671d8a14aae26ebc0b0ab8618893bc1652
pranavb <pranavb@umich.edu>
2022-12-11 18:13:45 -0500

    TST: Added index list inside of expected DataFrame constructor

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -2848,7 +2848,6 @@ def test_groupby_on_column_drops_na():
         {"a": [np.nan, "foo", "bar", np.nan, np.nan, "baz"], "b": [0, 1, 2, 3, 4, 5]}
     )
     result = df.groupby("a").head(1)
-    expected = DataFrame({"a": ["foo", "bar", "baz"], "b": [1, 2, 5]})
-    expected.index = [1, 2, 5]
+    expected = DataFrame({"a": ["foo", "bar", "baz"], "b": [1, 2, 5]}, index=[1, 2, 5])
 
     tm.assert_frame_equal(result, expected)
