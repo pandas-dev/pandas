6ffcb1cfe5aa25dfcda21be08fae3dacc38b320b
Terji Petersen <terjipetersen@Terjis-MacBook-Air.local>
2022-12-12 22:09:01 +0000

    fix precommit

--- pandas/tests/series/test_constructors.py
+++ pandas/tests/series/test_constructors.py
@@ -1214,7 +1214,7 @@ class TestSeriesConstructors:
         # construction from interval & array of intervals
         intervals = interval_constructor.from_breaks(np.arange(3), closed="right")
         result = Series(intervals)
-        assert result.dtype == f"interval[int64, right]"
+        assert result.dtype == "interval[int64, right]"
         tm.assert_index_equal(Index(result.values), Index(intervals))
 
     @pytest.mark.parametrize(
