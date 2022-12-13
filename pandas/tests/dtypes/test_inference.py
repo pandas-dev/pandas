74d6ec7a3500c50e566328dd9554dd96343e2f10
Irv Lustig <irv@princeton.com>
2022-12-12 20:44:54 -0500

    Fix: Treat Generic classes as not being is_list_like (#49736)
    * Fix: Treat Generic classes as not being is_list_like
    
    * tst: update test to test subclass of DataFrame
    
    * add is_list_like test for instance of generic dataframe
    
    * combine isinstance tests
    
    * update whatsnew with clearer message
    
    * Update whatsnew to reference DataFrame as a class
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
    
    * fix whatsnew blank line
    
    * fix whatsnew issue quotes
    
    * move whatsnew comment from 1.5.2 to 1.5.3
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
    (cherry picked from commit e37040a54b89ebb6a1c33dbde700b57900912ff4)

--- pandas/tests/dtypes/test_inference.py
+++ pandas/tests/dtypes/test_inference.py
@@ -18,6 +18,10 @@ import itertools
 from numbers import Number
 import re
 import sys
+from typing import (
+    Generic,
+    TypeVar,
+)
 
 import numpy as np
 import pytest
@@ -226,6 +230,22 @@ def test_is_list_like_iter_is_none():
         __iter__ = None
 
     assert not inference.is_list_like(NotListLike())
+
+
+def test_is_list_like_generic():
+    # GH 49649
+    # is_list_like was yielding false positives for Generic classes in python 3.11
+    T = TypeVar("T")
+
+    class MyDataFrame(DataFrame, Generic[T]):
+        ...
+
+    tstc = MyDataFrame[int]
+    tst = MyDataFrame[int]({"x": [1, 2, 3]})
+
+    assert not inference.is_list_like(tstc)
+    assert isinstance(tst, DataFrame)
+    assert inference.is_list_like(tst)
 
 
 def test_is_sequence():
