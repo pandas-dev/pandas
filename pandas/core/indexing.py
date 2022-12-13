5f6be083c92244b6ac8045994ce24afc8a3bf981
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-12-13 03:40:22 +0100

    BUG: iloc raising for ea series (#50171)
    * BUG: iloc raising for ea series
    
    * Move test
    
    * Add test

--- pandas/core/indexing.py
+++ pandas/core/indexing.py
@@ -1481,7 +1481,12 @@ class _iLocIndexer(_LocationIndexer):
             # so don't treat a tuple as a valid indexer
             raise IndexingError("Too many indexers")
         elif is_list_like_indexer(key):
-            arr = np.array(key)
+            if isinstance(key, ABCSeries):
+                arr = key._values
+            elif is_array_like(key):
+                arr = key
+            else:
+                arr = np.array(key)
             len_axis = len(self.obj._get_axis(axis))
 
             # check that the key has a numeric dtype
