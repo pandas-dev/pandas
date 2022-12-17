8409f5b43b6670e2b2413b3c2a1126090037463d
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-12-17 23:35:27 +0100

    BUG: loc for ea index raising with oob slice end (#50170)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -6158,6 +6158,11 @@ class Index(IndexOpsMixin, PandasObject):
         # We are a plain index here (sub-class override this method if they
         # wish to have special treatment for floats/ints, e.g. Float64Index and
         # datetimelike Indexes
+        # Special case numeric EA Indexes, since they are not handled by NumericIndex
+
+        if is_extension_array_dtype(self.dtype) and is_numeric_dtype(self.dtype):
+            return self._maybe_cast_indexer(label)
+
         # reject them, if index does not contain label
         if (is_float(label) or is_integer(label)) and label not in self:
             self._raise_invalid_indexer("slice", label)
