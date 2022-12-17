075947f0a29459d2df04bf08314909390d5031f1
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-12-17 22:01:06 +0100

    BUG: sql ignores dtype when chunksize is set and result is empty (#50288)

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -2362,6 +2362,21 @@ class _TestSQLAlchemy(SQLAlchemyMixIn, PandasSQLTest):
             }
         )
 
+    def test_chunksize_empty_dtypes(self):
+        # GH#50245
+        dtypes = {"a": "int64", "b": "object"}
+        df = DataFrame(columns=["a", "b"]).astype(dtypes)
+        expected = df.copy()
+        df.to_sql("test", self.conn, index=False, if_exists="replace")
+
+        for result in read_sql_query(
+            "SELECT * FROM test",
+            self.conn,
+            dtype=dtypes,
+            chunksize=1,
+        ):
+            tm.assert_frame_equal(result, expected)
+
 
 class TestSQLiteAlchemy(_TestSQLAlchemy):
     """
