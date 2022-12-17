0af8223268fa7d317657919eac895e5bac364dc2
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2022-12-17 13:10:11 -0800

    Remove commented out code

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -1258,10 +1258,6 @@ class ArrowExtensionArray(OpsMixin, ObjectStringArrayMixin, ExtensionArray):
     def _str_map(
         self, f, na_value=None, dtype: Dtype | None = None, convert: bool = True
     ):
-        # TODO: de-duplicate with StringArray method. This method is moreless copy and
-        # paste.
-        if dtype is None:
-            dtype = self.dtype
         if na_value is None:
             na_value = self.dtype.na_value
 
@@ -1275,52 +1271,6 @@ class ArrowExtensionArray(OpsMixin, ObjectStringArrayMixin, ExtensionArray):
             return type(self)(pa.array(np_result, mask=mask, from_pandas=True))
         except pa.ArrowInvalid:
             return np_result
-        # if is_integer_dtype(dtype) or is_bool_dtype(dtype):
-        #
-        #     from pandas.arrays import (
-        #         BooleanArray,
-        #         IntegerArray,
-        #     )
-        #
-        #     constructor: type[IntegerArray] | type[BooleanArray]
-        #     if is_integer_dtype(dtype):
-        #         constructor = IntegerArray
-        #     else:
-        #         constructor = BooleanArray
-        #
-        #     na_value_is_na = isna(na_value)
-        #     if na_value_is_na:
-        #         na_value = 1
-        #     result = lib.map_infer_mask(
-        #         arr,
-        #         f,
-        #         mask.view("uint8"),
-        #         convert=False,
-        #         na_value=na_value,
-        #         # error: Argument 1 to "dtype" has incompatible type
-        #         # "Union[ExtensionDtype, str, dtype[Any], Type[object]]"; expected
-        #         # "Type[object]"
-        #         dtype=np.dtype(dtype),  # type: ignore[arg-type]
-        #     )
-        #
-        #     if not na_value_is_na:
-        #         mask[:] = False
-        #
-        #     return constructor(result, mask)
-        #
-        # elif is_string_dtype(dtype) and not is_object_dtype(dtype):
-        #     # i.e. StringDtype
-        #     result = lib.map_infer_mask(
-        #         arr, f, mask.view("uint8"), convert=False, na_value=na_value
-        #     )
-        #     result = pa.array(result, mask=mask, type=pa.string(), from_pandas=True)
-        #     return type(self)(result)
-        # else:
-        #     # This is when the result type is object. We reach this when
-        #     # -> We know the result type is truly object (e.g. .encode returns bytes
-        #     #    or .findall returns a list).
-        #     # -> We don't know the result type. E.g. `.get` can return anything.
-        #     return lib.map_infer_mask(arr, f, mask.view("uint8"))
 
     def _str_contains(
         self, pat, case: bool = True, flags: int = 0, na=None, regex: bool = True
