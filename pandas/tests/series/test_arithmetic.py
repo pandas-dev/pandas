d56fb32766dbb829ebd4f2e21bfedce955b48a93
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-12-19 20:36:30 +0100

    BUG: arithmetic ops not propagating mask when combining mask arrays and numpy (#50322)
    * BUG: arithmetic ops not propagating mask when combining mask arrays and numpy
    
    * Fix mypy

--- pandas/tests/series/test_arithmetic.py
+++ pandas/tests/series/test_arithmetic.py
@@ -307,6 +307,25 @@ class TestSeriesArithmetic:
         expected = Series(Timedelta("9 hours"), index=[2, 2, 3, 3, 4])
         tm.assert_series_equal(result, expected)
 
+    def test_masked_and_non_masked_propagate_na(self):
+        # GH#45810
+        ser1 = Series([0, np.nan], dtype="float")
+        ser2 = Series([0, 1], dtype="Int64")
+        result = ser1 * ser2
+        expected = Series([0, pd.NA], dtype="Float64")
+        tm.assert_series_equal(result, expected)
+
+    def test_mask_div_propagate_na_for_non_na_dtype(self):
+        # GH#42630
+        ser1 = Series([15, pd.NA, 5, 4], dtype="Int64")
+        ser2 = Series([15, 5, np.nan, 4])
+        result = ser1 / ser2
+        expected = Series([1.0, pd.NA, pd.NA, 1.0], dtype="Float64")
+        tm.assert_series_equal(result, expected)
+
+        result = ser2 / ser1
+        tm.assert_series_equal(result, expected)
+
 
 # ------------------------------------------------------------------
 # Comparisons
