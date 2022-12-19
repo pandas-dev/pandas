a1c002bf0731ce2e499e41709052bacfeda8767c
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2022-12-19 13:07:24 -0800

    Undo test

--- pandas/util/_tester.py
+++ pandas/util/_tester.py
@@ -24,8 +24,7 @@ def test(extra_args: list[str] | None = None) -> None:
     """
     pytest = import_optional_dependency("pytest")
     import_optional_dependency("hypothesis")
-    # cmd = ["--skip-slow", "--skip-network", "--skip-db"]
-    cmd = ["-m slow"]
+    cmd = ["--skip-slow", "--skip-network", "--skip-db"]
     if extra_args:
         if not isinstance(extra_args, list):
             extra_args = [extra_args]
