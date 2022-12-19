d69e73e45822d79227b94dd3ea176c942cf652b3
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-12-19 19:55:46 +0100

    BUG: read_json raising with table orient and NA values (#50332)
    * BUG: read_json raising with table orient and NA values
    
    * Add test

--- pandas/tests/io/json/test_json_table_schema_ext_dtype.py
+++ pandas/tests/io/json/test_json_table_schema_ext_dtype.py
@@ -8,9 +8,13 @@ import json
 import pytest
 
 from pandas import (
+    NA,
     DataFrame,
+    Index,
     array,
+    read_json,
 )
+import pandas._testing as tm
 from pandas.core.arrays.integer import Int64Dtype
 from pandas.core.arrays.string_ import StringDtype
 from pandas.core.series import Series
@@ -273,3 +277,43 @@ class TestTableOrient:
         expected = OrderedDict([("schema", schema), ("data", data)])
 
         assert result == expected
+
+    def test_json_ext_dtype_reading_roundtrip(self):
+        # GH#40255
+        df = DataFrame(
+            {
+                "a": Series([2, NA], dtype="Int64"),
+                "b": Series([1.5, NA], dtype="Float64"),
+                "c": Series([True, NA], dtype="boolean"),
+            },
+            index=Index([1, NA], dtype="Int64"),
+        )
+        expected = df.copy()
+        data_json = df.to_json(orient="table", indent=4)
+        result = read_json(data_json, orient="table")
+        tm.assert_frame_equal(result, expected)
+
+    def test_json_ext_dtype_reading(self):
+        # GH#40255
+        data_json = """{
+            "schema":{
+                "fields":[
+                    {
+                        "name":"a",
+                        "type":"integer",
+                        "extDtype":"Int64"
+                    }
+                ],
+            },
+            "data":[
+                {
+                    "a":2
+                },
+                {
+                    "a":null
+                }
+            ]
+        }"""
+        result = read_json(data_json, orient="table")
+        expected = DataFrame({"a": Series([2, NA], dtype="Int64")})
+        tm.assert_frame_equal(result, expected)
