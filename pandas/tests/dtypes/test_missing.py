84f1abc92c7fb2eee3326cfe52327e6ce130f02f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2022-12-22 20:47:02 +0100

    add comment + extra test

--- pandas/tests/dtypes/test_missing.py
+++ pandas/tests/dtypes/test_missing.py
@@ -557,6 +557,13 @@ def test_array_equivalent_nested(strict_nan):
     assert array_equivalent(left, right, strict_nan=strict_nan)
     assert not array_equivalent(left, right[::-1], strict_nan=strict_nan)
 
+    left = np.empty(2, dtype=object)
+    left[:] = [np.array([50, 70, 90]), np.array([20, 30, 40])]
+    right = np.empty(2, dtype=object)
+    right[:] = [np.array([50, 70, 90]), np.array([20, 30, 40])]
+    assert array_equivalent(left, right, strict_nan=strict_nan)
+    assert not array_equivalent(left, right[::-1], strict_nan=strict_nan)
+
     left = np.array([np.array([50, 50, 50]), np.array([40, 40])], dtype=object)
     right = np.array([50, 40])
     assert not array_equivalent(left, right, strict_nan=strict_nan)
