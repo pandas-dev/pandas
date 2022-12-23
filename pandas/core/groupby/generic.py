63bcc5f9dea99a531798aa1ea9aa0253169e80e6
iofall <50991099+iofall@users.noreply.github.com>
2022-12-23 12:13:44 +0530

    Fix doctest

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -1928,8 +1928,8 @@ class DataFrameGroupBy(GroupBy[DataFrame]):
         --------
         Consider a dataset containing food consumption in Argentina.
 
-        >>> df = pd.DataFrame({{'consumption': [10.51, 103.11, 55.48],
-        ...                    'co2_emissions': [37.2, 19.66, 1712]}},
+        >>> df = pd.DataFrame({'consumption': [10.51, 103.11, 55.48],
+        ...                    'co2_emissions': [37.2, 19.66, 1712]},
         ...                    index=['Pork', 'Wheat Products', 'Beef'])
 
         >>> df
