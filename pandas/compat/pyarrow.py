157ce9468ad8f557a3c72f3947782937505dff60
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2022-12-24 20:49:35 +0100

    CI: Unpin pyarrow<10 (#50314)
    * CI: Unpin pyarrow<10
    
    * Skip test

--- pandas/compat/pyarrow.py
+++ pandas/compat/pyarrow.py
@@ -13,8 +13,10 @@ try:
     pa_version_under7p0 = _palv < Version("7.0.0")
     pa_version_under8p0 = _palv < Version("8.0.0")
     pa_version_under9p0 = _palv < Version("9.0.0")
+    pa_version_under10p0 = _palv < Version("10.0.0")
 except ImportError:
     pa_version_under6p0 = True
     pa_version_under7p0 = True
     pa_version_under8p0 = True
     pa_version_under9p0 = True
+    pa_version_under10p0 = True
