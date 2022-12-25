c6c140473155bcb18d4bc03ddb7915b3ee6555f1
Luke Manley <lukemanley@gmail.com>
2022-12-25 12:40:49 -0500

    BUG: Series(pyarrow-backed).round raising AttributeError

--- pandas/tests/extension/test_arrow.py
+++ pandas/tests/extension/test_arrow.py
@@ -1531,3 +1531,17 @@ def test_setitem_invalid_dtype(data):
         msg = "cannot be converted"
     with pytest.raises(err, match=msg):
         data[:] = fill_value
+
+
+def test_round():
+    dtype = "float64[pyarrow]"
+
+    ser = pd.Series([0.0, 1.23, 2.56, pd.NA], dtype=dtype)
+    result = ser.round(1)
+    expected = pd.Series([0.0, 1.2, 2.6, pd.NA], dtype=dtype)
+    tm.assert_series_equal(result, expected)
+
+    ser = pd.Series([123.4, pd.NA, 56.78], dtype=dtype)
+    result = ser.round(-1)
+    expected = pd.Series([120.0, pd.NA, 60.0], dtype=dtype)
+    tm.assert_series_equal(result, expected)
