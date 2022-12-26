6d30477ed2957537201b6ead3cea473371402243
Marc Garcia <garcia.marc@gmail.com>
2022-12-26 14:50:57 +0700

    Black

--- pandas/io/parsers/c_parser_wrapper.py
+++ pandas/io/parsers/c_parser_wrapper.py
@@ -288,9 +288,7 @@ class CParserWrapper(ParserBase):
             if self.usecols is not None:
                 names = self._filter_usecols(names)
 
-            names = dedup_names(
-                names, is_potential_multi_index(names, self.index_col)
-            )
+            names = dedup_names(names, is_potential_multi_index(names, self.index_col))
 
             # rename dict keys
             data_tups = sorted(data.items())
@@ -312,9 +310,7 @@ class CParserWrapper(ParserBase):
             # assert for mypy, orig_names is List or None, None would error in list(...)
             assert self.orig_names is not None
             names = list(self.orig_names)
-            names = dedup_names(
-                names, is_potential_multi_index(names, self.index_col)
-            )
+            names = dedup_names(names, is_potential_multi_index(names, self.index_col))
 
             if self.usecols is not None:
                 names = self._filter_usecols(names)
