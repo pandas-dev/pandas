c2e9a31bd6dfbcb194ff0bdb34a9045a132cd28a
Luke Manley <lukemanley@gmail.com>
2022-12-27 14:23:35 -0500

    BUG: Series(pyarrow-backed).round raising AttributeError (#50437)
    * BUG: Series(pyarrow-backed).round raising AttributeError
    
    * gh ref

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -661,6 +661,32 @@ class ArrowExtensionArray(OpsMixin, ExtensionArray):
             f"as backed by a 1D pyarrow.ChunkedArray."
         )
 
+    def round(
+        self: ArrowExtensionArrayT, decimals: int = 0, *args, **kwargs
+    ) -> ArrowExtensionArrayT:
+        """
+        Round each value in the array a to the given number of decimals.
+
+        Parameters
+        ----------
+        decimals : int, default 0
+            Number of decimal places to round to. If decimals is negative,
+            it specifies the number of positions to the left of the decimal point.
+        *args, **kwargs
+            Additional arguments and keywords have no effect.
+
+        Returns
+        -------
+        ArrowExtensionArray
+            Rounded values of the ArrowExtensionArray.
+
+        See Also
+        --------
+        DataFrame.round : Round values of a DataFrame.
+        Series.round : Round values of a Series.
+        """
+        return type(self)(pc.round(self._data, ndigits=decimals))
+
     def take(
         self,
         indices: TakeIndexer,
