02a9ae7c82393c7580c116f54d486110e1e7a319
Jaime Di Cristina <74217512+dicristina@users.noreply.github.com>
2022-12-27 15:39:55 -0400

    Expected result is manually constructed.

--- pandas/tests/resample/test_resampler_grouper.py
+++ pandas/tests/resample/test_resampler_grouper.py
@@ -517,13 +517,23 @@ def test_resample_empty_Dataframe(keys):
     tm.assert_frame_equal(result, expected)
 
 
-@pytest.mark.parametrize("freq", ["D", "M", "Q", "A"])
-def test_groupby_resample_size_all_index_same(freq):
+def test_groupby_resample_size_all_index_same():
     # GH 46826
     df = DataFrame(
         {"A": [1] * 3 + [2] * 3 + [1] * 3 + [2] * 3, "B": np.arange(12)},
         index=date_range("31/12/2000 18:00", freq="H", periods=12),
     )
-    result = df.groupby("A").resample(freq).size()
-    expected = df.groupby(["A", pd.Grouper(freq=freq)]).size()
+    result = df.groupby("A").resample("D").size()
+    expected = Series(
+        3,
+        index=pd.MultiIndex.from_tuples(
+            [
+                (1, Timestamp("2000-12-31")),
+                (1, Timestamp("2001-01-01")),
+                (2, Timestamp("2000-12-31")),
+                (2, Timestamp("2001-01-01")),
+            ],
+            names=["A", None],
+        ),
+    )
     tm.assert_series_equal(result, expected)
