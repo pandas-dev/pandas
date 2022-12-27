f008c847fb46039957553f2a0abe5d1ab7096ec6
Brock <jbrockmendel@gmail.com>
2022-12-27 15:23:28 -0800

    added test

--- pandas/tests/tools/test_to_datetime.py
+++ pandas/tests/tools/test_to_datetime.py
@@ -1497,14 +1497,23 @@ class TestToDatetimeUnit:
         msg = f"Conversion of non-round float with unit={unit} is ambiguous"
         with pytest.raises(ValueError, match=msg):
             to_datetime([1.5], unit=unit, errors="raise")
+        with pytest.raises(ValueError, match=msg):
+            to_datetime(["1.5"], unit=unit, errors="raise")
 
         # with errors="ignore" we also end up raising within the Timestamp
         #  constructor; this may not be ideal
         with pytest.raises(ValueError, match=msg):
             to_datetime([1.5], unit=unit, errors="ignore")
+        # TODO: we are NOT consistent with the Timestamp behavior in the
+        #  float-like string case
+        # with pytest.raises(ValueError, match=msg):
+        #    to_datetime(["1.5"], unit=unit, errors="ignore")
 
         res = to_datetime([1.5], unit=unit, errors="coerce")
         expected = Index([NaT], dtype="M8[ns]")
+        tm.assert_index_equal(res, expected)
+
+        res = to_datetime(["1.5"], unit=unit, errors="coerce")
         tm.assert_index_equal(res, expected)
 
         # round floats are OK
