a16f3ff91e14432fa6db6aec42379f4a70e24adc
Natalia Mokeeva <natalia.mokeeva.de@gmail.com>
2022-12-28 16:50:21 +0100

    fixup! add test to check crosstab supports Float64 formats

--- pandas/tests/reshape/test_crosstab.py
+++ pandas/tests/reshape/test_crosstab.py
@@ -799,9 +799,9 @@ class TestCrosstab:
         # GH 50313
         # use Float64 formats and function aggfunc with margins
         df = DataFrame(
-            {"A": [1, 2, 2, 1], "B": [3, 3, 4, 5], "C": [-1.0, 10.0, 1.0, 10.0]}
+            {"A": [1, 2, 2, 1], "B": [3, 3, 4, 5], "C": [-1.0, 10.0, 1.0, 10.0]},
+            dtype="Float64",
         )
-        df = df.astype({"C": "Float64"})
         result = crosstab(
             df["A"],
             df["B"],
@@ -815,11 +815,10 @@ class TestCrosstab:
                 [10.0, 1.0, pd.NA, 11.0],
                 [9.0, 1.0, 10.0, 20.0],
             ],
-            index=[1, 2, "All"],
+            index=Index([1.0, 2.0, "All"], dtype="object", name="A"),
+            columns=Index([3.0, 4.0, 5.0, "All"], dtype="object", name="B"),
             dtype="Float64",
         )
-        expected.columns = Index([3, 4, 5, "All"], dtype="object", name="B")
-        expected.index.name = "A"
         tm.assert_frame_equal(result, expected)
 
 
