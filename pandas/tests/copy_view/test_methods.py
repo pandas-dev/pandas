e631e1a1b72176ea85758475df3e36ab820cecbe
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-12-28 20:50:36 +0100

    ENH: Add lazy copy for series.reorder_levels

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -424,6 +424,25 @@ def test_reorder_levels(using_copy_on_write):
     tm.assert_frame_equal(df, df_orig)
 
 
+def test_series_reorder_levels(using_copy_on_write):
+    index = MultiIndex.from_tuples(
+        [(1, 1), (1, 2), (2, 1), (2, 2)], names=["one", "two"]
+    )
+    ser = Series([1, 2, 3, 4], index=index)
+    ser_orig = ser.copy()
+    ser2 = ser.reorder_levels(order=["two", "one"])
+
+    if using_copy_on_write:
+        assert np.shares_memory(ser2.values, ser.values)
+    else:
+        assert not np.shares_memory(ser2.values, ser.values)
+
+    ser2.iloc[0] = 0
+    if using_copy_on_write:
+        assert not np.shares_memory(ser2.values, ser.values)
+    tm.assert_series_equal(ser, ser_orig)
+
+
 def test_frame_set_axis(using_copy_on_write):
     # GH 49473
     df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [0.1, 0.2, 0.3]})
