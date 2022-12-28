83c2a5f7fcafa3134d2a8cb29b257be0146f1c6b
jbrockmendel <jbrockmendel@gmail.com>
2022-12-28 09:47:22 -0800

    BUG: to_datetime with M or Y unit and non-round float (#50301)
    * BUG: to_datetime with M or Y unit and non-round float
    
    * use is_integer
    
    * GH ref
    
    * added test

--- pandas/tests/tools/test_to_datetime.py
+++ pandas/tests/tools/test_to_datetime.py
@@ -1498,6 +1498,38 @@ class TestToDatetime:
 
 
 class TestToDatetimeUnit:
+    @pytest.mark.parametrize("unit", ["Y", "M"])
+    def test_to_datetime_month_or_year_unit_non_round_float(self, cache, unit):
+        # GH#50301
+        # Match Timestamp behavior in disallowing non-round floats with
+        #  Y or M unit
+        msg = f"Conversion of non-round float with unit={unit} is ambiguous"
+        with pytest.raises(ValueError, match=msg):
+            to_datetime([1.5], unit=unit, errors="raise")
+        with pytest.raises(ValueError, match=msg):
+            to_datetime(["1.5"], unit=unit, errors="raise")
+
+        # with errors="ignore" we also end up raising within the Timestamp
+        #  constructor; this may not be ideal
+        with pytest.raises(ValueError, match=msg):
+            to_datetime([1.5], unit=unit, errors="ignore")
+        # TODO: we are NOT consistent with the Timestamp behavior in the
+        #  float-like string case
+        # with pytest.raises(ValueError, match=msg):
+        #    to_datetime(["1.5"], unit=unit, errors="ignore")
+
+        res = to_datetime([1.5], unit=unit, errors="coerce")
+        expected = Index([NaT], dtype="M8[ns]")
+        tm.assert_index_equal(res, expected)
+
+        res = to_datetime(["1.5"], unit=unit, errors="coerce")
+        tm.assert_index_equal(res, expected)
+
+        # round floats are OK
+        res = to_datetime([1.0], unit=unit)
+        expected = to_datetime([1], unit=unit)
+        tm.assert_index_equal(res, expected)
+
     def test_unit(self, cache):
         # GH 11758
         # test proper behavior with errors
