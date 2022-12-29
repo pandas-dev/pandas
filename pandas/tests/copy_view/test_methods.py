83edb2926a7d34165236f544cfc4b32ec6157b59
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-12-29 21:25:00 +0100

    TST: Test cow for set_flags

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -456,3 +456,21 @@ def test_series_set_axis(using_copy_on_write):
     ser2.iloc[0] = 0
     assert not np.shares_memory(ser2, ser)
     tm.assert_series_equal(ser, ser_orig)
+
+
+def test_set_flags(using_copy_on_write):
+    ser = Series([1, 2, 3])
+    ser_orig = ser.copy()
+    ser2 = ser.set_flags(allows_duplicate_labels=False)
+
+    assert np.shares_memory(ser, ser2)
+
+    # mutating ser triggers a copy-on-write for the column / block
+    ser2.iloc[0] = 0
+    if using_copy_on_write:
+        assert not np.shares_memory(ser2, ser)
+        tm.assert_series_equal(ser, ser_orig)
+    else:
+        assert np.shares_memory(ser2, ser)
+        expected = Series([0, 2, 3])
+        tm.assert_series_equal(ser, expected)
