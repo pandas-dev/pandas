fc564f8b41f73384b724c47489369ddb30444ef7
Patrick Hoefler <patrick_hoefler@gmx.net>
2022-12-30 13:05:36 +0100

    Add series test

--- pandas/tests/copy_view/test_functions.py
+++ pandas/tests/copy_view/test_functions.py
@@ -2,6 +2,7 @@ import numpy as np
 
 from pandas import (
     DataFrame,
+    Series,
     concat,
 )
 import pandas._testing as tm
@@ -30,3 +31,27 @@ def test_concat_frames(using_copy_on_write):
     if using_copy_on_write:
         assert not np.shares_memory(get_array(result, "a"), get_array(df2, "a"))
     tm.assert_frame_equal(df, df_orig)
+
+
+def test_concat_series(using_copy_on_write):
+    ser = Series([1, 2], name="a")
+    ser2 = Series([3, 4], name="b")
+    ser_orig = ser.copy()
+    result = concat([ser, ser2], axis=1)
+
+    if using_copy_on_write:
+        assert np.shares_memory(get_array(result, "a"), ser.values)
+        assert np.shares_memory(get_array(result, "b"), ser2.values)
+    else:
+        assert not np.shares_memory(get_array(result, "a"), ser.values)
+        assert not np.shares_memory(get_array(result, "b"), ser2.values)
+
+    result.iloc[0, 0] = 100
+    if using_copy_on_write:
+        assert not np.shares_memory(get_array(result, "a"), ser.values)
+        assert np.shares_memory(get_array(result, "b"), ser2.values)
+
+    result.iloc[0, 1] = 1000
+    if using_copy_on_write:
+        assert not np.shares_memory(get_array(result, "b"), ser2.values)
+    tm.assert_series_equal(ser, ser_orig)
