1a056df275ea602976c1dd3f8ccbe8295c9bd042
MarcoGorelli <>
2022-12-31 17:56:19 +0000

    Revert "STYLE use pandas-dev-flaker (#40906)"
    This reverts commit 1f27ed0adc2fea2dd301d2dccab5215afaca0e6b.

--- pandas/tests/tools/test_to_numeric.py
+++ pandas/tests/tools/test_to_numeric.py
@@ -234,7 +234,9 @@ def test_type_check(errors):
     # see gh-11776
     df = DataFrame({"a": [1, -3.14, 7], "b": ["4", "5", "6"]})
     kwargs = {"errors": errors} if errors is not None else {}
-    with pytest.raises(TypeError, match="1-d array"):
+    error_ctx = pytest.raises(TypeError, match="1-d array")
+
+    with error_ctx:
         to_numeric(df, **kwargs)
 
 
