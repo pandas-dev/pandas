1f9447fc5b1a1a33d549f29eef85b7ae3802d328
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-01-02 16:33:54 +0100

    ENH: add lazy copy (CoW) mechanism to rename_axis (#50415)

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -3,6 +3,7 @@ import pytest
 
 from pandas import (
     DataFrame,
+    Index,
     MultiIndex,
     Series,
 )
@@ -456,3 +457,21 @@ def test_series_set_axis(using_copy_on_write):
     ser2.iloc[0] = 0
     assert not np.shares_memory(ser2, ser)
     tm.assert_series_equal(ser, ser_orig)
+
+
+@pytest.mark.parametrize("copy_kwargs", [{"copy": True}, {}])
+@pytest.mark.parametrize("kwargs", [{"mapper": "test"}, {"index": "test"}])
+def test_rename_axis(using_copy_on_write, kwargs, copy_kwargs):
+    df = DataFrame({"a": [1, 2, 3, 4]}, index=Index([1, 2, 3, 4], name="a"))
+    df_orig = df.copy()
+    df2 = df.rename_axis(**kwargs, **copy_kwargs)
+
+    if using_copy_on_write and not copy_kwargs:
+        assert np.shares_memory(get_array(df2, "a"), get_array(df, "a"))
+    else:
+        assert not np.shares_memory(get_array(df2, "a"), get_array(df, "a"))
+
+    df2.iloc[0, 0] = 0
+    if using_copy_on_write:
+        assert not np.shares_memory(get_array(df2, "a"), get_array(df, "a"))
+    tm.assert_frame_equal(df, df_orig)
