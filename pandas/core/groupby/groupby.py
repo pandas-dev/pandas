66ae02eb6ebd754f87b779f635058e02aa155f88
Nick Crews <nicholas.b.crews@gmail.com>
2023-01-01 20:41:41 -0900

    DOC: fixup: update order of labels
    Now I guess since the groups
    are lexicographically sorted,
    and we are using "red" and "blue" instead of "a" and "b",
    the ngroup labels have swapped order.
    
    I think therefore that this should be deterministic
    and not flaky.

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -3241,20 +3241,20 @@ class GroupBy(BaseGroupBy[NDFrameT]):
         4   blue
         5    red
         >>> df.groupby("color").ngroup()
-        0    0.0
+        0    1.0
         1    NaN
-        2    0.0
-        3    1.0
-        4    1.0
-        5    0.0
+        2    1.0
+        3    0.0
+        4    0.0
+        5    1.0
         dtype: float64
         >>> df.groupby("color", dropna=False).ngroup()
-        0    0
+        0    1
         1    2
-        2    0
-        3    1
-        4    1
-        5    0
+        2    1
+        3    0
+        4    0
+        5    1
         dtype: int64
         """
         with self._group_selection_context():
