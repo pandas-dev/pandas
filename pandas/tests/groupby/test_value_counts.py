fc707dde2e6adfa6c61eb07a5d33ccff47bc78a9
MarcoGorelli <>
2023-01-03 15:33:25 +0000

    explicit casts to float to avoid implicit casts

--- pandas/tests/groupby/test_value_counts.py
+++ pandas/tests/groupby/test_value_counts.py
@@ -54,6 +54,8 @@ def seed_df(seed_nans, n, m):
             "3rd": np.random.randint(1, m + 1, n),
         }
     )
+    # Explicitly cast to float to avoid implicit cast when setting nan
+    frame["3rd"] = frame["3rd"].astype("float")
 
     if seed_nans:
         frame.loc[1::11, "1st"] = np.nan
