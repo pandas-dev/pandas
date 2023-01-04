7cebad9cb9489467b463e33e46337419f03d82db
Terji Petersen <terji78@gmail.com>
2023-01-04 23:38:03 +0000

    fixes for comments

--- pandas/tests/util/test_assert_categorical_equal.py
+++ pandas/tests/util/test_assert_categorical_equal.py
@@ -22,7 +22,7 @@ def test_categorical_equal_order_mismatch(check_category_order):
         msg = """Categorical\\.categories are different
 
 Categorical\\.categories values are different \\(100\\.0 %\\)
-\\[left\\]:  NumericIndex\\(\\[1, 2, 3, 4]\\, dtype='int64'\\)
+\\[left\\]:  NumericIndex\\(\\[1, 2, 3, 4\\], dtype='int64'\\)
 \\[right\\]: NumericIndex\\(\\[4, 3, 2, 1\\], dtype='int64'\\)"""
         with pytest.raises(AssertionError, match=msg):
             tm.assert_categorical_equal(c1, c2, **kwargs)
