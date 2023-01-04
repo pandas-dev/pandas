b3961c8478982ea67879295e564af271a8b4bbee
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-01-03 18:34:59 -0800

    Ensure only hypothesis and pytest and checked aligned

--- pandas/compat/_optional.py
+++ pandas/compat/_optional.py
@@ -33,7 +33,7 @@ VERSIONS = {
     "pymysql": "1.0.2",
     "pyarrow": "6.0.0",
     "pyreadstat": "1.1.2",
-    "pytest": "6.0",
+    "pytest": "7.0.0",
     "pyxlsb": "1.0.8",
     "s3fs": "2021.08.0",
     "scipy": "1.7.1",
