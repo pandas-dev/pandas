f084b616ddbd148fb534c9a96b02d49cee48225c
Terji Petersen <terji78@gmail.com>
2023-01-04 07:37:38 +0000

    small fix

--- pandas/tests/indexing/multiindex/test_partial.py
+++ pandas/tests/indexing/multiindex/test_partial.py
@@ -156,7 +156,7 @@ class TestMultiIndexPartial:
         mi = ser.index
         assert isinstance(mi, MultiIndex)
         if dtype is int:
-            assert mi.levels[0].dtype == np.intpq
+            assert mi.levels[0].dtype == np.intp
         else:
             assert mi.levels[0].dtype == np.float64
 
