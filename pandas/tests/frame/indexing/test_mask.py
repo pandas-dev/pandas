38628322c0e3fb53296ed40de02a2c9e7eb2dc8c
luke396 <77875500+luke396@users.noreply.github.com>
2023-01-04 12:42:57 +0800

    Update pandas/tests/frame/indexing/test_mask.py
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/tests/frame/indexing/test_mask.py
+++ pandas/tests/frame/indexing/test_mask.py
@@ -135,7 +135,7 @@ def test_mask_where_dtype_timedelta():
 
 def test_mask_return_dtype():
     # GH#50488
-    ser = Series([0.0, 1.0, 2.0, 3.0], dtype=Float64Dtype)
+    ser = Series([0.0, 1.0, 2.0, 3.0], dtype=Float64Dtype())
     cond = ~ser.isna()
     other = Series([True, False, True, False])
     excepted = Series([1.0, 0.0, 1.0, 0.0], dtype=ser.dtype)
