ca1f768069dfb6c04e77c32339f76283d0ff9a2d
Jaime Di Cristina <74217512+dicristina@users.noreply.github.com>
2023-01-04 14:08:14 -0400

    BUG: Fix issue where df.groupby.resample.size returns wide DF instead of MultiIndex Series. (#50440)
    * Issue where df.groupby.resample.size returns DF.
    
    * Expected result is manually constructed.
    
    * Moved whatsnew entry to v2.0.0.rst.

--- pandas/tests/resample/test_resampler_grouper.py
+++ pandas/tests/resample/test_resampler_grouper.py
@@ -515,3 +515,25 @@ def test_resample_empty_Dataframe(keys):
         expected.index.name = keys[0]
 
     tm.assert_frame_equal(result, expected)
+
+
+def test_groupby_resample_size_all_index_same():
+    # GH 46826
+    df = DataFrame(
+        {"A": [1] * 3 + [2] * 3 + [1] * 3 + [2] * 3, "B": np.arange(12)},
+        index=date_range("31/12/2000 18:00", freq="H", periods=12),
+    )
+    result = df.groupby("A").resample("D").size()
+    expected = Series(
+        3,
+        index=pd.MultiIndex.from_tuples(
+            [
+                (1, Timestamp("2000-12-31")),
+                (1, Timestamp("2001-01-01")),
+                (2, Timestamp("2000-12-31")),
+                (2, Timestamp("2001-01-01")),
+            ],
+            names=["A", None],
+        ),
+    )
+    tm.assert_series_equal(result, expected)
