bb1e8d6eca13aa74e18e0627d15219bcf5e35cad
David Leon <davidleon123@gmail.com>
2023-01-05 11:34:21 +0100

    implemented PR comments

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -598,9 +598,8 @@ def test_tz_convert_localize(using_copy_on_write, func, tz):
 
 def test_droplevel(using_copy_on_write):
     # GH 49473
-    df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]}).set_index(
-        ["a", "b"]
-    )
+    index = MultiIndex.from_tuples([(1, 1), (1, 2), (2, 1)], names=["one", "two"])
+    df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]}, index=index)
     df_orig = df.copy()
     df2 = df.droplevel(0)
 
@@ -610,7 +609,7 @@ def test_droplevel(using_copy_on_write):
         assert not np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
 
     # mutating df2 triggers a copy-on-write for that column / block
-    df2.loc[4, "c"] = 1
+    df2.iloc[0, 0] = 0
 
     assert not np.shares_memory(get_array(df2, "c"), get_array(df, "c"))
     tm.assert_frame_equal(df, df_orig)
