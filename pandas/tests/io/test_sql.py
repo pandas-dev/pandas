1903bf6a8f91f35262d9404603d376e96723d421
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-01-06 12:04:53 +0100

    Change tests

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -1682,12 +1682,12 @@ class _TestSQLAlchemy(SQLAlchemyMixIn, PandasSQLTest):
         from sqlalchemy import inspect
 
         temp_conn = self.connect()
-        temp_conn.begin()
-        temp_frame = DataFrame(
-            {"one": [1.0, 2.0, 3.0, 4.0], "two": [4.0, 3.0, 2.0, 1.0]}
-        )
-        pandasSQL = sql.SQLDatabase(temp_conn)
-        assert pandasSQL.to_sql(temp_frame, "temp_frame") == 4
+        with temp_conn.begin():
+            temp_frame = DataFrame(
+                {"one": [1.0, 2.0, 3.0, 4.0], "two": [4.0, 3.0, 2.0, 1.0]}
+            )
+            pandasSQL = sql.SQLDatabase(temp_conn)
+            assert pandasSQL.to_sql(temp_frame, "temp_frame") == 4
 
         insp = inspect(temp_conn)
         assert insp.has_table("temp_frame")
@@ -1696,12 +1696,12 @@ class _TestSQLAlchemy(SQLAlchemyMixIn, PandasSQLTest):
         from sqlalchemy import inspect
 
         temp_conn = self.connect()
-        temp_conn.begin()
-        temp_frame = DataFrame(
-            {"one": [1.0, 2.0, 3.0, 4.0], "two": [4.0, 3.0, 2.0, 1.0]}
-        )
-        pandasSQL = sql.SQLDatabase(temp_conn)
-        assert pandasSQL.to_sql(temp_frame, "temp_frame") == 4
+        with temp_conn.begin():
+            temp_frame = DataFrame(
+                {"one": [1.0, 2.0, 3.0, 4.0], "two": [4.0, 3.0, 2.0, 1.0]}
+            )
+            pandasSQL = sql.SQLDatabase(temp_conn)
+            assert pandasSQL.to_sql(temp_frame, "temp_frame") == 4
 
         insp = inspect(temp_conn)
         assert insp.has_table("temp_frame")
