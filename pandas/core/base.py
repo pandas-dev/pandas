1e31efc840785d57d9d556a70f73fa1ccdc170c5
yqyqyq-W <94888665+yqyqyq-W@users.noreply.github.com>
2023-01-06 15:42:28 -0500

    Bug: Fix Inconsistent Behavior for Series.searchsorted (#49706)
    * Bug: Fix inconsistent behavior in searchsorted (#49620)
    
    Raise error when DataFrame is passed to searchsorted
    
    * Update v2.0.0.rst
    
    * Revert "Update v2.0.0.rst"
    
    This reverts commit 0b1b450d0993203b78a660c286b0bb568a620a7f.
    
    * Update whatsnew
    
    * Update v2.0.0.rst
    
    * Update base.py
    
    * Revert "Update base.py"
    
    This reverts commit 090bd7928369b1ae70aa7bd9532ae1f6b735b6d3.
    
    * Bug: Fix inconsistent behavior in searchsorted (#49620)
    
    Raise error when DataFrame is passed to searchsorted
    
    * Update whatsnew
    
    * Bug: Fix inconsistent behavior in searchsorted (#49620)
    
    Raise error when DataFrame is passed to searchsorted
    
    * Add test for failure on DataFrame
    
    * Add test for failure on DataFrame
    
    * Add test for failure on DataFrame
    
    * Add test for failure on DataFrame
    
    * Bug: Fix inconsistent behavior in searchsorted (#49620)
    
    * Add test for failure on DataFrame

--- pandas/core/base.py
+++ pandas/core/base.py
@@ -1292,6 +1292,13 @@ class IndexOpsMixin(OpsMixin):
         sorter: NumpySorter = None,
     ) -> npt.NDArray[np.intp] | np.intp:
 
+        if isinstance(value, ABCDataFrame):
+            msg = (
+                "Value must be 1-D array-like or scalar, "
+                f"{type(value).__name__} is not supported"
+            )
+            raise ValueError(msg)
+
         values = self._values
         if not isinstance(values, np.ndarray):
             # Going through EA.searchsorted directly improves performance GH#38083
