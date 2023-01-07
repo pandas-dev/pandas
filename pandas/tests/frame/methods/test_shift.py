df385731bcbbf401909ced26a5904af04f3b8549
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-01-07 17:50:07 +0000

    CLN avoid more test warnings (#50621)

--- pandas/tests/frame/methods/test_shift.py
+++ pandas/tests/frame/methods/test_shift.py
@@ -397,6 +397,13 @@ class TestDataFrameShift:
         result = df3.shift(2, axis=1)
 
         expected = df3.take([-1, -1, 0, 1, 2], axis=1)
+        # Explicit cast to float to avoid implicit cast when setting nan.
+        # Column names aren't unique, so directly calling `expected.astype` won't work.
+        expected = expected.pipe(
+            lambda df: df.set_axis(range(df.shape[1]), axis=1)
+            .astype({0: "float", 1: "float"})
+            .set_axis(df.columns, axis=1)
+        )
         expected.iloc[:, :2] = np.nan
         expected.columns = df3.columns
 
@@ -410,6 +417,13 @@ class TestDataFrameShift:
         result = df3.shift(-2, axis=1)
 
         expected = df3.take([2, 3, 4, -1, -1], axis=1)
+        # Explicit cast to float to avoid implicit cast when setting nan.
+        # Column names aren't unique, so directly calling `expected.astype` won't work.
+        expected = expected.pipe(
+            lambda df: df.set_axis(range(df.shape[1]), axis=1)
+            .astype({3: "float", 4: "float"})
+            .set_axis(df.columns, axis=1)
+        )
         expected.iloc[:, -2:] = np.nan
         expected.columns = df3.columns
 
