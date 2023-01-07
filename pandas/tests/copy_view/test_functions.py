3c4abd210c956f852f6f6cc057d63c39ea3495e6
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-01-07 12:46:38 +0100

    Add check

--- pandas/tests/copy_view/test_functions.py
+++ pandas/tests/copy_view/test_functions.py
@@ -37,6 +37,7 @@ def test_concat_series(using_copy_on_write):
     ser = Series([1, 2], name="a")
     ser2 = Series([3, 4], name="b")
     ser_orig = ser.copy()
+    ser2_orig = ser2.copy()
     result = concat([ser, ser2], axis=1)
 
     if using_copy_on_write:
@@ -55,3 +56,4 @@ def test_concat_series(using_copy_on_write):
     if using_copy_on_write:
         assert not np.shares_memory(get_array(result, "b"), ser2.values)
     tm.assert_series_equal(ser, ser_orig)
+    tm.assert_series_equal(ser2, ser2_orig)
