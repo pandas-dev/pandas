2b51129fa561adfe8d15a6aa5a79ef0e1b46c9b8
jbrockmendel <jbrockmendel@gmail.com>
2023-01-09 15:01:17 -0800

    REF: handle 2D in array_to_datetime (#50549)
    * REF: handle 2D in array_to_datetime
    
    * troubleshoot build failures
    
    * avoid build error

--- pandas/core/arrays/datetimes.py
+++ pandas/core/arrays/datetimes.py
@@ -9,7 +9,6 @@ from datetime import (
 from typing import (
     TYPE_CHECKING,
     Iterator,
-    Literal,
     cast,
 )
 import warnings
@@ -2149,16 +2148,13 @@ def objects_to_datetime64ns(
     # if str-dtype, convert
     data = np.array(data, copy=False, dtype=np.object_)
 
-    flags = data.flags
-    order: Literal["F", "C"] = "F" if flags.f_contiguous else "C"
     result, tz_parsed = tslib.array_to_datetime(
-        data.ravel("K"),
+        data,
         errors=errors,
         utc=utc,
         dayfirst=dayfirst,
         yearfirst=yearfirst,
     )
-    result = result.reshape(data.shape, order=order)
 
     if tz_parsed is not None:
         # We can take a shortcut since the datetime64 numpy array
