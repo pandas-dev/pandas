81c985803154afa2ea63eb056ae5d7d91161ea30
Pooja Subramaniam <poojas2086@gmail.com>
2023-01-10 19:29:27 +0000

    added test based on issue #25797 to check if changing dtype of series stays object

--- pandas/tests/series/methods/test_replace.py
+++ pandas/tests/series/methods/test_replace.py
@@ -675,3 +675,26 @@ class TestSeriesReplace:
         result = ser.replace(val, None)
         expected = pd.Series([1, None], dtype=object)
         tm.assert_series_equal(result, expected)
+
+    def test_replace_change_dtype_series(self):
+        # GH#25797
+        df = pd.DataFrame.from_dict({'Test': ['0.5', True, '0.6']})
+        print(df['Test'].dtype)
+        assert df['Test'].dtype == 'object'
+        df['Test'] = df['Test'].replace([True], [np.nan])
+        print(df['Test'].dtype)
+        assert df['Test'].dtype == 'object'
+
+        df = pd.DataFrame.from_dict({'Test': ['0.5', None, '0.6']})
+        print(df['Test'].dtype)
+        assert df['Test'].dtype == 'object'
+        df['Test'] = df['Test'].replace([None], [np.nan])
+        print(df['Test'].dtype)
+        assert df['Test'].dtype == 'object'
+
+        df = pd.DataFrame.from_dict({'Test': ['0.5', None, '0.6']})
+        print(df['Test'].dtype)
+        assert df['Test'].dtype == 'object'
+        df['Test'] = df['Test'].fillna(np.nan)
+        print(df['Test'].dtype)
+        assert df['Test'].dtype == 'object'
