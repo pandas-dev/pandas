c0f3338468b9d2a30f7cfbcd29311d7df27ec6b9
Luca Pizzini <lpizzini7@gmail.com>
2023-01-11 17:44:10 +0100

    TEST: added test case for issue 41800
    addresses #41800

--- pandas/tests/series/methods/test_combine_first.py
+++ pandas/tests/series/methods/test_combine_first.py
@@ -100,3 +100,11 @@ class TestCombineFirst:
         )
         exp = Series(exp_vals, name="ser1")
         tm.assert_series_equal(exp, result)
+
+    def test_combine_first_timezone_series_with_empty_series(self):
+        # GH 41800
+        time_index = pd.date_range(datetime(2021, 1, 1, 1), datetime(2021, 1, 1, 10), freq='H', tz='Europe/Rome')
+        s1 = Series(np.random.random(10), index=time_index)
+        s2 = Series()
+        result = s1.combine_first(s2)
+        tm.assert_equal(result.values, s1.values)
