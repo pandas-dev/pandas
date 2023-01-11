837bb96ce52caabdbf223a2aa66c1c13738a3099
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2023-01-10 21:21:56 -0500

    BUG/PERF: SeriesGroupBy.value_counts sorting bug and categorical performance (#50548)
    Co-authored-by: Simon Hawkins <simonjayhawkins@gmail.com>

--- pandas/tests/groupby/test_value_counts.py
+++ pandas/tests/groupby/test_value_counts.py
@@ -199,6 +199,26 @@ def test_series_groupby_value_counts_on_categorical():
     tm.assert_series_equal(result, expected)
 
 
+def test_series_groupby_value_counts_no_sort():
+    # GH#50482
+    df = DataFrame(
+        {
+            "gender": ["male", "male", "female", "male", "female", "male"],
+            "education": ["low", "medium", "high", "low", "high", "low"],
+            "country": ["US", "FR", "US", "FR", "FR", "FR"],
+        }
+    )
+    gb = df.groupby(["country", "gender"], sort=False)["education"]
+    result = gb.value_counts(sort=False)
+    index = MultiIndex(
+        levels=[["US", "FR"], ["male", "female"], ["low", "medium", "high"]],
+        codes=[[0, 1, 0, 1, 1], [0, 0, 1, 0, 1], [0, 1, 2, 0, 2]],
+        names=["country", "gender", "education"],
+    )
+    expected = Series([1, 1, 1, 2, 1], index=index, name="education")
+    tm.assert_series_equal(result, expected)
+
+
 @pytest.fixture
 def education_df():
     return DataFrame(
