04d8b7f71124116ad663bd66be67ae9ad54d93df
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2023-01-12 07:34:37 -0800

    Address review comments

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -765,19 +765,19 @@ def test_isetitem(using_copy_on_write):
     df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [7, 8, 9]})
     df_orig = df.copy()
     df2 = df.copy(deep=None)  # Trigger a CoW
-    df2.isetitem(1, np.array([-1, -2, -3]))
+    df2.isetitem(1, np.array([-1, -2, -3]))  # This is inplace
 
     if using_copy_on_write:
-        assert np.shares_memory(df["c"].values, df2["c"].values)
-        assert np.shares_memory(df["a"].values, df2["a"].values)
+        assert np.shares_memory(get_array(df, "c"), get_array(df2, "c"))
+        assert np.shares_memory(get_array(df, "a"), get_array(df2, "a"))
     else:
-        assert not np.shares_memory(df["c"].values, df2["c"].values)
-        assert not np.shares_memory(df["a"].values, df2["a"].values)
+        assert not np.shares_memory(get_array(df, "c"), get_array(df2, "c"))
+        assert not np.shares_memory(get_array(df, "a"), get_array(df2, "a"))
 
     df2.loc[0, "a"] = 0
-    tm.assert_series_equal(df["a"], df_orig["a"])  # Original is unchanged
+    tm.assert_frame_equal(df, df_orig)  # Original is unchanged
 
     if using_copy_on_write:
-        assert np.shares_memory(df["c"].values, df2["c"].values)
+        assert np.shares_memory(get_array(df, "c"), get_array(df2, "c"))
     else:
-        assert not np.shares_memory(df["c"].values, df2["c"].values)
+        assert not np.shares_memory(get_array(df, "c"), get_array(df2, "c"))
