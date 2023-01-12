74970c1333b4b920ab1a5e518810699a885510c9
Luca Pizzini <lpizzini7@gmail.com>
2023-01-12 19:50:51 +0100

    TEST: added test case for issue 41800 (#50677)
    * TEST: added test case for issue 41800
    
    addresses #41800
    
    * fix lint error
    
    * fix lint error
    
    * added assert_series_equal control and changed date_range call

--- pandas/tests/series/methods/test_combine_first.py
+++ pandas/tests/series/methods/test_combine_first.py
@@ -100,3 +100,16 @@ class TestCombineFirst:
         )
         exp = Series(exp_vals, name="ser1")
         tm.assert_series_equal(exp, result)
+
+    def test_combine_first_timezone_series_with_empty_series(self):
+        # GH 41800
+        time_index = date_range(
+            datetime(2021, 1, 1, 1),
+            datetime(2021, 1, 1, 10),
+            freq="H",
+            tz="Europe/Rome",
+        )
+        s1 = Series(range(10), index=time_index)
+        s2 = Series(index=time_index)
+        result = s1.combine_first(s2)
+        tm.assert_series_equal(result, s1)
