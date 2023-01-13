4bd8ea10bae3370ac9576aec445867a184ea91ab
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-01-13 16:49:10 +0100

    use helper

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -426,10 +426,7 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
         elif isinstance(data, Series):
             if index is None:
                 index = data.index
-                if (
-                    get_option("mode.copy_on_write")
-                    and get_option("mode.data_manager") == "block"
-                ):
+                if using_copy_on_write():
                     data = data._mgr.copy(deep=False)
                 else:
                     data = data._mgr
