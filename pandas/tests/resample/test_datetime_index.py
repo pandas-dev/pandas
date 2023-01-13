f6e85f14d35d416ec025825d9b79d16ae1b1b0ea
MarcoGorelli <>
2023-01-13 09:31:18 +0000

    some more fixups

--- pandas/tests/resample/test_datetime_index.py
+++ pandas/tests/resample/test_datetime_index.py
@@ -1114,21 +1114,17 @@ def test_monthly_resample_error(unit):
     ts.resample("M")
 
 
-def test_nanosecond_resample_error(unit):
+def test_nanosecond_resample_error():
     # GH 12307 - Values falls after last bin when
     # Resampling using pd.tseries.offsets.Nano as period
     start = 1443707890427
     exp_start = 1443707890400
-    indx = date_range(start=pd.to_datetime(start), periods=10, freq="100n").as_unit(
-        unit
-    )
+    indx = date_range(start=pd.to_datetime(start), periods=10, freq="100n")
     ts = Series(range(len(indx)), index=indx)
     r = ts.resample(pd.tseries.offsets.Nano(100))
     result = r.agg("mean")
 
-    exp_indx = date_range(
-        start=pd.to_datetime(exp_start), periods=10, freq="100n"
-    ).as_unit(unit)
+    exp_indx = date_range(start=pd.to_datetime(exp_start), periods=10, freq="100n")
     exp = Series(range(len(exp_indx)), index=exp_indx, dtype=float)
 
     tm.assert_series_equal(result, exp)
