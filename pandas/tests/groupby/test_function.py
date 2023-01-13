d010c4a8443b3f0dc0dc8bc9160f275d6c6f262b
Terji Petersen <contribute@tensortable.com>
2023-01-13 18:12:56 +0000

    DEPR: Remove int64 uint64 float64 index part 1 (#49560)
    * DEPR: don't make Index instantiate Int64/Uint64/Flaot64Index
    
    * fix precommit
    
    * fix various issues
    
    * np.int_ -> np.intp
    
    * update
    
    * float16 & groupby.nlargest/nsmallest tests
    
    * small fix
    
    * tests/window/test_groupby.py types
    
    * Revert "np.int_ -> np.intp"
    
    This reverts commit d759966d843e4bbf98f428e908dafc0690f4e898.
    
    * fixes for comments
    
    * fix intp->int_ error
    
    * fix interval
    
    * fix groupby value_counts
    
    * fix comments
    
    * fix more comments
    
    * fix stuff
    
    * fix
    
    Co-authored-by: Terji Petersen <terjipetersen@Terjis-Air.fritz.box>
    Co-authored-by: Terji Petersen <terjipetersen@Terjis-MacBook-Air.local>

--- pandas/tests/groupby/test_function.py
+++ pandas/tests/groupby/test_function.py
@@ -770,16 +770,20 @@ def test_nsmallest():
     "data, groups",
     [([0, 1, 2, 3], [0, 0, 1, 1]), ([0], [0])],
 )
+@pytest.mark.parametrize("dtype", [None, *tm.ALL_INT_NUMPY_DTYPES])
 @pytest.mark.parametrize("method", ["nlargest", "nsmallest"])
-def test_nlargest_and_smallest_noop(data, groups, method):
+def test_nlargest_and_smallest_noop(data, groups, dtype, method):
     # GH 15272, GH 16345, GH 29129
     # Test nlargest/smallest when it results in a noop,
     # i.e. input is sorted and group size <= n
+    if dtype is not None:
+        data = np.array(data, dtype=dtype)
     if method == "nlargest":
         data = list(reversed(data))
     ser = Series(data, name="a")
     result = getattr(ser.groupby(groups), method)(n=2)
-    expected = Series(data, index=MultiIndex.from_arrays([groups, ser.index]), name="a")
+    expidx = np.array(groups, dtype=np.int_) if isinstance(groups, list) else groups
+    expected = Series(data, index=MultiIndex.from_arrays([expidx, ser.index]), name="a")
     tm.assert_series_equal(result, expected)
 
 
