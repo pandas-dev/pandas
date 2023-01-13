25f0693b958d739852304287b6ce5f6f5fb6bac3
MarcoGorelli <>
2023-01-13 13:03:18 +0000

    fixup test_at

--- pandas/tests/indexing/test_at.py
+++ pandas/tests/indexing/test_at.py
@@ -122,7 +122,10 @@ class TestAtSetItem:
             index=DatetimeIndex(data=["2019-01-01", "2019-01-02"]),
         )
 
-        df.at[row, 0] = 0.5
+        with tm.assert_produces_warning(
+            FutureWarning, match="item of incompatible dtype"
+        ):
+            df.at[row, 0] = 0.5
         tm.assert_frame_equal(df, expected)
 
 
