9e84de41e44860325ab55630ebaccc4c5ba9d7ec
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-01-14 22:49:33 +0100

    Adjust test

--- pandas/tests/copy_view/test_internals.py
+++ pandas/tests/copy_view/test_internals.py
@@ -97,21 +97,22 @@ def test_switch_options():
 
 
 @td.skip_array_manager_invalid_test
+@pytest.mark.parametrize("dtype", [np.intp, np.int8])
 @pytest.mark.parametrize(
     "locs, arr",
     [
-        ([0], np.array([-1, -2, -3], dtype=np.intp)),
-        ([1], np.array([-1, -2, -3], dtype=np.intp)),
-        ([5], np.array([-1, -2, -3], dtype=np.intp)),
-        ([0, 1], np.array([-1, -2, -3], dtype=np.intp)),
-        ([0, 2], np.array([-1, -2, -3], dtype=np.intp)),
-        ([0, 1, 2], np.array([-1, -2, -3], dtype=np.intp)),
-        ([1, 2], np.array([-1, -2, -3], dtype=np.intp)),
-        ([1, 3], np.array([-1, -2, -3], dtype=np.intp)),
-        ([1, 3], np.array([[-1, -2, -3], [-4, -5, -6]], dtype=np.intp).T),
+        ([0], np.array([-1, -2, -3])),
+        ([1], np.array([-1, -2, -3])),
+        ([5], np.array([-1, -2, -3])),
+        ([0, 1], np.array([[-1, -2, -3], [-4, -5, -6]]).T),
+        ([0, 2], np.array([[-1, -2, -3], [-4, -5, -6]]).T),
+        ([0, 1, 2], np.array([[-1, -2, -3], [-4, -5, -6], [-4, -5, -6]]).T),
+        ([1, 2], np.array([[-1, -2, -3], [-4, -5, -6]]).T),
+        ([1, 3], np.array([[-1, -2, -3], [-4, -5, -6]]).T),
+        ([1, 3], np.array([[-1, -2, -3], [-4, -5, -6]]).T),
     ],
 )
-def test_iset_splits_blocks_inplace(using_copy_on_write, locs, arr):
+def test_iset_splits_blocks_inplace(using_copy_on_write, locs, arr, dtype):
     # Nothing currently calls iset with
     # more than 1 loc with inplace=True (only happens with inplace=False)
     # but ensure that it works
@@ -122,10 +123,10 @@ def test_iset_splits_blocks_inplace(using_copy_on_write, locs, arr):
             "c": [7, 8, 9],
             "d": [10, 11, 12],
             "e": [13, 14, 15],
+            "f": ["a", "b", "c"],
         },
-        dtype=np.intp,
     )
-    df["f"] = ["a", "b", "c"]
+    arr = arr.astype(dtype)
     df_orig = df.copy()
     df2 = df.copy(deep=None)  # Trigger a CoW (if enabled, otherwise makes copy)
     df2._mgr.iset(locs, arr, inplace=True)
