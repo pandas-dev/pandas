e484e762e4bac2b6397d1c2be447523431680cd3
luke <2736230899@qq.com>
2023-01-16 13:20:39 +0800

    instead format of f-string

--- pandas/tests/groupby/transform/test_transform.py
+++ pandas/tests/groupby/transform/test_transform.py
@@ -1050,7 +1050,7 @@ def test_groupby_transform_dtype():
     # GH 22243
     df = DataFrame({"a": [1], "val": [1.35]})
 
-    result = df["val"].transform(lambda x: x.map("+{}".format))
+    result = df["val"].transform(lambda x: x.map(lambda y: f"+{y}"))
     expected1 = Series(["+1.35"], name="val", dtype="object")
     tm.assert_series_equal(result, expected1)
 
