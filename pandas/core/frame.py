b33627949c300d0316437c6dee2661834b5195de
smij720 <smij720@gmail.com>
2023-01-15 17:13:42 -0800

    Add docstring for DataFrame.T property

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -3569,6 +3569,32 @@ class DataFrame(NDFrame, OpsMixin):
 
     @property
     def T(self) -> DataFrame:
+        """
+        The transpose of the DataFrame.
+
+        Returns
+        -------
+        DataFrame
+            The transposed DataFrame.
+
+        Examples
+        --------
+        >>> df = pd.DataFrame({'col1': [1, 2], 'col2': [3, 4]})
+        >>> df
+           col1  col2
+        0     1     3
+        1     2     4
+
+        >>> df.T
+              0  1
+        col1  1  2
+        col2  3  4
+
+
+        See Also
+        --------
+        DataFrame.transpose : Transpose index and columns.
+        """
         return self.transpose()
 
     # ----------------------------------------------------------------------
