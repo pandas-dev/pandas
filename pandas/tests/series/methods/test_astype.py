e7a127570421004eaef81ebe0088c069ed0c990b
Brock <jbrockmendel@gmail.com>
2023-01-16 08:10:24 -0800

    fix test

--- pandas/tests/series/methods/test_astype.py
+++ pandas/tests/series/methods/test_astype.py
@@ -454,7 +454,7 @@ class TestAstypeString:
     def test_astype_string_to_extension_dtype_roundtrip(
         self, data, dtype, request, nullable_string_dtype
     ):
-        if dtype == "boolean":
+        if dtype == "boolean" or (dtype == "timedelta64[ns]" and NaT in data):
             mark = pytest.mark.xfail(
                 reason="TODO StringArray.astype() with missing values #GH40566"
             )
