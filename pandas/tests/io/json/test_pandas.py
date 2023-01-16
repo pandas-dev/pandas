36c649d573e969f26bf92c194eb163fee392c561
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-01-16 20:22:57 +0100

    Add test

--- pandas/tests/io/json/test_pandas.py
+++ pandas/tests/io/json/test_pandas.py
@@ -1933,3 +1933,26 @@ class TestPandasContainer:
             expected.columns = list(range(0, 8))
 
         tm.assert_frame_equal(result, expected)
+
+    @pytest.mark.parametrize("dtype_backend", ["pandas", "pyarrow"])
+    @pytest.mark.parametrize("orient", ["split", "records", "index"])
+    def test_read_json_nullable_series(self, string_storage, dtype_backend, orient):
+        # GH#50750
+        pa = pytest.importorskip("pyarrow")
+        ser = Series([1, np.nan, 3], dtype="Int64")
+
+        out = ser.to_json(orient=orient)
+        with pd.option_context("mode.string_storage", string_storage):
+            with pd.option_context("mode.dtype_backend", dtype_backend):
+                result = read_json(
+                    out, use_nullable_dtypes=True, orient=orient, typ="series"
+                )
+
+        expected = Series([1, np.nan, 3], dtype="Int64")
+
+        if dtype_backend == "pyarrow":
+            from pandas.arrays import ArrowExtensionArray
+
+            expected = Series(ArrowExtensionArray(pa.array(expected, from_pandas=True)))
+
+        tm.assert_series_equal(result, expected)
