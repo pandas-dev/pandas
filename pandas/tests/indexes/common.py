11e29863784cb5fc283e6faffd37c645a7ea730d
ABCPAN-rank <1047068900@qq.com>
2023-01-17 10:56:54 +0800

    fix pre-commit error

--- pandas/tests/indexes/common.py
+++ pandas/tests/indexes/common.py
@@ -822,12 +822,12 @@ class Base:
         with tm.assert_produces_warning(FutureWarning, match=msg):
             idx.holds_integer()
 
-    def test_numeric_is_deprecated(self, simple_index):
+    def test_is_numeric_is_deprecated(self, simple_index):
         # GH50042
         idx = simple_index
         with tm.assert_produces_warning(FutureWarning):
             idx.is_numeric()
-            
+
     def test_is_categorical_is_deprecated(self, simple_index):
         # GH50042
         idx = simple_index
