2f0253995d49986f52413c1a7775fbc5a542e7ae
Zhengbo Wang <77875500+luke396@users.noreply.github.com>
2023-01-18 02:51:37 +0800

    TST: Determine groupby-transform not convert back type (#50754)
    * determine groupby-transform not convert back type
    
    * Replace format with f-string
    
    * assert whole DataFrame
    
    * add more cases
    
    * instead format of f-string

--- pandas/tests/groupby/transform/test_transform.py
+++ pandas/tests/groupby/transform/test_transform.py
@@ -1046,6 +1046,26 @@ def test_groupby_transform_with_datetimes(func, values):
     tm.assert_series_equal(result, expected)
 
 
+def test_groupby_transform_dtype():
+    # GH 22243
+    df = DataFrame({"a": [1], "val": [1.35]})
+
+    result = df["val"].transform(lambda x: x.map(lambda y: f"+{y}"))
+    expected1 = Series(["+1.35"], name="val", dtype="object")
+    tm.assert_series_equal(result, expected1)
+
+    result = df.groupby("a")["val"].transform(lambda x: x.map(lambda y: f"+{y}"))
+    tm.assert_series_equal(result, expected1)
+
+    result = df.groupby("a")["val"].transform(lambda x: x.map(lambda y: f"+({y})"))
+    expected2 = Series(["+(1.35)"], name="val", dtype="object")
+    tm.assert_series_equal(result, expected2)
+
+    df["val"] = df["val"].astype(object)
+    result = df.groupby("a")["val"].transform(lambda x: x.map(lambda y: f"+{y}"))
+    tm.assert_series_equal(result, expected1)
+
+
 @pytest.mark.parametrize("func", ["cumsum", "cumprod", "cummin", "cummax"])
 def test_transform_absent_categories(func):
     # GH 16771
