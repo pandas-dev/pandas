a786285d3ec7ff080bf1107018d6a425b54506f5
jbrockmendel <jbrockmendel@gmail.com>
2023-01-17 11:11:25 -0800

    BUG: stringdtype.astype(dt64_or_td64) (#50734)
    * BUG: stringdtype.astype(dt64_or_td64)
    
    * fix test

--- pandas/tests/series/methods/test_astype.py
+++ pandas/tests/series/methods/test_astype.py
@@ -454,9 +454,7 @@ class TestAstypeString:
     def test_astype_string_to_extension_dtype_roundtrip(
         self, data, dtype, request, nullable_string_dtype
     ):
-        if dtype == "boolean" or (
-            dtype in ("datetime64[ns]", "timedelta64[ns]") and NaT in data
-        ):
+        if dtype == "boolean" or (dtype == "timedelta64[ns]" and NaT in data):
             mark = pytest.mark.xfail(
                 reason="TODO StringArray.astype() with missing values #GH40566"
             )
