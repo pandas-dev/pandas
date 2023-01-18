8ebc9100b8ea59b0703850967accc49dca3b633c
Rebecca N. Palmer <rebecca_palmer@zoho.com>
2023-01-18 22:16:19 +0000

    TST: don't try to use non-integer years (see #50301)

--- pandas/tests/tools/test_to_datetime.py
+++ pandas/tests/tools/test_to_datetime.py
@@ -1936,24 +1936,24 @@ class TestToDatetimeUnit:
         tm.assert_index_equal(result, expected)
 
     def test_float_to_datetime_raise_near_bounds(self):
-        msg = "cannot convert input with unit 'Y'"
-        oneyear_in_ns = 1e9 * 60 * 60 * 24 * 365.2425
-        tsmax_in_years = 2**63 / oneyear_in_ns  # 2**63 ns, in years
+        msg = "cannot convert input with unit 'D'"
+        oneday_in_ns = 1e9 * 60 * 60 * 24
+        tsmax_in_days = 2**63 / oneday_in_ns  # 2**63 ns, in days
         # just in bounds
         should_succeed = Series(
-            [0, tsmax_in_years - 0.05, -tsmax_in_years + 0.05], dtype=float
+            [0, tsmax_in_days - 0.05, -tsmax_in_days + 0.05], dtype=float
         )
-        expected = (should_succeed * oneyear_in_ns).astype(np.int64)
+        expected = (should_succeed * oneday_in_ns).astype(np.int64)
         for error_mode in ["raise", "coerce", "ignore"]:
-            result1 = to_datetime(should_succeed, unit="Y", errors=error_mode)
+            result1 = to_datetime(should_succeed, unit="D", errors=error_mode)
             tm.assert_almost_equal(result1.astype(np.int64), expected, rtol=1e-10)
         # just out of bounds
-        should_fail1 = Series([0, tsmax_in_years + 0.05], dtype=float)
-        should_fail2 = Series([0, -tsmax_in_years - 0.05], dtype=float)
+        should_fail1 = Series([0, tsmax_in_days + 0.05], dtype=float)
+        should_fail2 = Series([0, -tsmax_in_days - 0.05], dtype=float)
         with pytest.raises(OutOfBoundsDatetime, match=msg):
-            to_datetime(should_fail1, unit="Y", errors="raise")
+            to_datetime(should_fail1, unit="D", errors="raise")
         with pytest.raises(OutOfBoundsDatetime, match=msg):
-            to_datetime(should_fail2, unit="Y", errors="raise")
+            to_datetime(should_fail2, unit="D", errors="raise")
 
 
 class TestToDatetimeDataFrame:
