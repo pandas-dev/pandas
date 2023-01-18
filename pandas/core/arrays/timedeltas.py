942bdc0c5eb3c19ce32e9fb5acd0298667ab36b0
Brock <jbrockmendel@gmail.com>
2023-01-18 10:32:57 -0800

    BUG: merge with non-nano

--- pandas/core/arrays/timedeltas.py
+++ pandas/core/arrays/timedeltas.py
@@ -316,7 +316,7 @@ class TimedeltaArray(dtl.TimelikeOps):
             raise ValueError("'value' should be a Timedelta.")
         self._check_compatible_with(value)
         if value is NaT:
-            return np.timedelta64(value.value, "ns")
+            return np.timedelta64(value.value, self.unit)
         else:
             return value.as_unit(self.unit).asm8
 
