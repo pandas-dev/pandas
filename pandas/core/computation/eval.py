5e2327ec45f5617d5c0df11ef9b314d5d4754a50
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-01-19 17:01:49 +0100

    Fix eval

--- pandas/core/computation/eval.py
+++ pandas/core/computation/eval.py
@@ -335,8 +335,13 @@ def eval(
 
         parsed_expr = Expr(expr, engine=engine, parser=parser, env=env)
 
-        if is_extension_array_dtype(parsed_expr.terms.return_type) or any(
-            is_extension_array_dtype(elem) for elem in parsed_expr.terms.operand_types
+        if (
+            is_extension_array_dtype(parsed_expr.terms.return_type)
+            or getattr(parsed_expr.terms, "operand_typesany") is not None
+            and (
+                is_extension_array_dtype(elem)
+                for elem in parsed_expr.terms.operand_types
+            )
         ):
             engine = "python"
 
