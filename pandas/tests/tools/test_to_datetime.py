0b04174115d156541552da07e2c220df613ae36f
rebecca-palmer <rebecca_palmer@zoho.com>
2023-01-20 17:25:47 +0000

    Improve to_datetime bounds checking (#50183)
    * add test for float to_datetime near overflow bounds
    
    * fix float to_datetime near overflow bounds
    
    * fix typo and formatting
    
    * fix formatting
    
    * fix test to not fail on rounding differences
    
    * don't use approximate comparison on datetimes, it doesn't work
    
    * also can't convert datetime to float
    
    * match dtypes
    
    * TST: don't try to use non-integer years (see #50301)
    
    * TST: don't cross an integer
    
    (tsmax_in_days happens to be close to an integer,
    and this is a test of rounding)
    
    * PERF: remove unnecessary copy
    
    * add whatsnew

--- pandas/tests/tools/test_to_datetime.py
+++ pandas/tests/tools/test_to_datetime.py
@@ -1935,6 +1935,27 @@ class TestToDatetimeUnit:
         result = to_datetime([1, 2, bad_val], unit="D", errors="coerce")
         tm.assert_index_equal(result, expected)
 
+    def test_float_to_datetime_raise_near_bounds(self):
+        # GH50183
+        msg = "cannot convert input with unit 'D'"
+        oneday_in_ns = 1e9 * 60 * 60 * 24
+        tsmax_in_days = 2**63 / oneday_in_ns  # 2**63 ns, in days
+        # just in bounds
+        should_succeed = Series(
+            [0, tsmax_in_days - 0.005, -tsmax_in_days + 0.005], dtype=float
+        )
+        expected = (should_succeed * oneday_in_ns).astype(np.int64)
+        for error_mode in ["raise", "coerce", "ignore"]:
+            result1 = to_datetime(should_succeed, unit="D", errors=error_mode)
+            tm.assert_almost_equal(result1.astype(np.int64), expected, rtol=1e-10)
+        # just out of bounds
+        should_fail1 = Series([0, tsmax_in_days + 0.005], dtype=float)
+        should_fail2 = Series([0, -tsmax_in_days - 0.005], dtype=float)
+        with pytest.raises(OutOfBoundsDatetime, match=msg):
+            to_datetime(should_fail1, unit="D", errors="raise")
+        with pytest.raises(OutOfBoundsDatetime, match=msg):
+            to_datetime(should_fail2, unit="D", errors="raise")
+
 
 class TestToDatetimeDataFrame:
     @pytest.fixture
