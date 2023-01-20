e7d2f0f0d297819a89005bc3f1fe69096f785758
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-01-20 16:04:58 +0100

    expand tests

--- pandas/tests/copy_view/test_constructors.py
+++ pandas/tests/copy_view/test_constructors.py
@@ -82,17 +82,22 @@ def test_series_from_series_with_reindex(using_copy_on_write):
 
 
 @pytest.mark.parametrize("dtype", [None, "int64"])
+@pytest.mark.parametrize("index", [None, [0, 1, 2]])
 @pytest.mark.parametrize("columns", [None, ["a", "b"], ["a", "b", "c"]])
-def test_dataframe_from_dict_of_series(using_copy_on_write, columns, dtype):
+def test_dataframe_from_dict_of_series(using_copy_on_write, columns, index, dtype):
     # Case: constructing a DataFrame from Series objects with copy=False
     # has to do a lazy following CoW rules
     # (the default for DataFrame(dict) is still to copy to ensure consolidation)
     s1 = Series([1, 2, 3])
     s2 = Series([4, 5, 6])
     s1_orig = s1.copy()
-    expected = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, dtype=dtype, columns=columns)
+    expected = DataFrame(
+        {"a": [1, 2, 3], "b": [4, 5, 6]}, index=index, columns=columns, dtype=dtype
+    )
 
-    result = DataFrame({"a": s1, "b": s2}, columns=columns, dtype=dtype, copy=False)
+    result = DataFrame(
+        {"a": s1, "b": s2}, index=index, columns=columns, dtype=dtype, copy=False
+    )
 
     # the shallow copy still shares memory
     assert np.shares_memory(get_array(result, "a"), s1.values)
@@ -106,10 +111,45 @@ def test_dataframe_from_dict_of_series(using_copy_on_write, columns, dtype):
         assert s1.iloc[0] == 10
 
     # the same when modifying the parent series
-    result = DataFrame({"a": s1, "b": s2}, columns=columns, dtype=dtype, copy=False)
+    result = DataFrame(
+        {"a": s1, "b": s2}, index=index, columns=columns, dtype=dtype, copy=False
+    )
     s1.iloc[0] = 10
     if using_copy_on_write:
         assert not np.shares_memory(get_array(result, "a"), s1.values)
         tm.assert_frame_equal(result, expected)
     else:
         assert result.iloc[0, 0] == 10
+
+
+@pytest.mark.parametrize("dtype", [None, "int64"])
+def test_dataframe_from_dict_of_series_with_reindex(dtype):
+    # Case: constructing a DataFrame from Series objects with copy=False
+    # and passing an index that requires an actual (no-view) reindex -> need
+    # to ensure the result doesn't have refs set up to unnecessarily trigger
+    # a copy on write
+    s1 = Series([1, 2, 3])
+    s2 = Series([4, 5, 6])
+    df = DataFrame({"a": s1, "b": s2}, index=[1, 2, 3], dtype=dtype, copy=False)
+
+    # df should own its memory, so mutating shouldn't trigger a copy
+    arr_before = get_array(df, "a")
+    df.iloc[0, 0] = 100
+    arr_after = get_array(df, "a")
+    assert np.shares_memory(arr_before, arr_after)
+
+
+@pytest.mark.parametrize("index", [None, [0, 1, 2]])
+def test_dataframe_from_dict_of_series_with_dtype(index):
+    # Variant of above, but now passing a dtype that causes a copy
+    # -> need to ensure the result doesn't have refs set up to unnecessarily
+    # trigger a copy on write
+    s1 = Series([1.0, 2.0, 3.0])
+    s2 = Series([4, 5, 6])
+    df = DataFrame({"a": s1, "b": s2}, index=index, dtype="int64", copy=False)
+
+    # df should own its memory, so mutating shouldn't trigger a copy
+    arr_before = get_array(df, "a")
+    df.iloc[0, 0] = 100
+    arr_after = get_array(df, "a")
+    assert np.shares_memory(arr_before, arr_after)
