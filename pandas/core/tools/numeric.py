933d0d4f54293c0716b1bd78c8a664fd21f1fdc8
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-01-21 17:12:15 -0800

    ENH: Add dtype_backend to to_numeric (#50910)

--- pandas/core/tools/numeric.py
+++ pandas/core/tools/numeric.py
@@ -4,6 +4,8 @@ from typing import Literal
 
 import numpy as np
 
+from pandas._config import get_option
+
 from pandas._libs import lib
 from pandas._typing import (
     DateTimeErrorChoices,
@@ -190,6 +192,9 @@ def to_numeric(
         values = values._data[~mask]
 
     values_dtype = getattr(values, "dtype", None)
+    if isinstance(values_dtype, pd.ArrowDtype):
+        mask = values.isna()
+        values = values.dropna().to_numpy()
     new_mask: np.ndarray | None = None
     if is_numeric_dtype(values_dtype):
         pass
@@ -258,6 +263,7 @@ def to_numeric(
         data[~mask] = values
 
         from pandas.core.arrays import (
+            ArrowExtensionArray,
             BooleanArray,
             FloatingArray,
             IntegerArray,
@@ -271,6 +277,11 @@ def to_numeric(
         else:
             klass = FloatingArray
         values = klass(data, mask)
+
+        if get_option("mode.dtype_backend") == "pyarrow" or isinstance(
+            values_dtype, pd.ArrowDtype
+        ):
+            values = ArrowExtensionArray(values.__arrow_array__())
 
     if is_series:
         return arg._constructor(values, index=arg.index, name=arg.name)
