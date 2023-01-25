3e77d99cc74efe22685c5aa170940398cad30ca4
Terji Petersen <terji78@gmail.com>
2023-01-25 20:51:44 +0000

    DEPR: move NumericIndex._convert_tolerance to Index (#50932)
    * DEPR: move NumericIndex._convert_tolerance to Index
    
    * fix error message

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3811,6 +3811,17 @@ class Index(IndexOpsMixin, PandasObject):
         tolerance = np.asarray(tolerance)
         if target.size != tolerance.size and tolerance.size > 1:
             raise ValueError("list-like tolerance size must match target index size")
+        elif is_numeric_dtype(self) and not np.issubdtype(tolerance.dtype, np.number):
+            if tolerance.ndim > 0:
+                raise ValueError(
+                    f"tolerance argument for {type(self).__name__} with dtype "
+                    f"{self.dtype} must contain numeric elements if it is list type"
+                )
+
+            raise ValueError(
+                f"tolerance argument for {type(self).__name__} with dtype {self.dtype} "
+                f"must be numeric if it is a scalar: {repr(tolerance)}"
+            )
         return tolerance
 
     @final
