08e28011bd4a9e6a03b34e184066de64cbcdd1ae
jbrockmendel <jbrockmendel@gmail.com>
2023-02-01 13:59:04 -0800

    PERF: avoid copy in _obj_with_exclusions, _selected_obj (#51090)

--- pandas/core/base.py
+++ pandas/core/base.py
@@ -213,11 +213,11 @@ class SelectionMixin(Generic[NDFrameT]):
     @final
     @cache_readonly
     def _obj_with_exclusions(self):
-        if self._selection is not None and isinstance(self.obj, ABCDataFrame):
-            return self.obj[self._selection_list]
-
         if isinstance(self.obj, ABCSeries):
             return self.obj
+
+        if self._selection is not None:
+            return self.obj._getitem_nocopy(self._selection_list)
 
         if len(self.exclusions) > 0:
             # equivalent to `self.obj.drop(self.exclusions, axis=1)
