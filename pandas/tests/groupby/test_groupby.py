359d7fffb7ae4ca5eebecc0fa75014c00723ec5d
richard <rhshadrach@gmail.com>
2023-02-02 22:57:11 -0500

    Rework test

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -2830,12 +2830,11 @@ def test_groupby_reduce_period():
     tm.assert_series_equal(res, expected)
 
 
-def test_selected_obj_duplicate_columns():
+def test_obj_with_exclusions_duplicate_columns():
     # GH#50806
     df = DataFrame([[0, 1, 2, 3]])
     df.columns = [0, 1, 2, 0]
     gb = df.groupby(df[1])
-    with gb._group_selection_context():
-        result = gb._selected_obj
+    result = gb._obj_with_exclusions
     expected = df.take([0, 2, 3], axis=1)
     tm.assert_frame_equal(result, expected)
