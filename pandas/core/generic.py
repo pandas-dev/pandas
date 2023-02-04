5d9e3f78413294b59b9a5ba2cccb7692a09f2457
Terji Petersen <terji78@gmail.com>
2023-02-04 18:55:31 +0000

    Spelling issue

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -7,7 +7,7 @@ from pandas.core.ndframe import *  # noqa F401
 warnings.warn(
     "pandas.core.generic is deprecated "
     "and will be removed from pandas in a future version. "
-    "Use pandas.core.ndframe with instead.",
+    "Use pandas.core.ndframe instead.",
     FutureWarning,
     stacklevel=2,
 )
