0e6f96ed7d0b6817925bb37ab755a9c7ffea4124
Natalia Mokeeva <91160475+natmokval@users.noreply.github.com>
2023-02-06 14:46:48 +0100

    TEST: join df with categorical multiIndex (#51088)
    * TEST: join df with categorical multiIndex
    
    * compare result and expected DataFrame
    
    * add pytest.mark.parameterize to the test
    
    * use dataframes from the original example

--- pandas/tests/reshape/merge/test_join.py
+++ pandas/tests/reshape/merge/test_join.py
@@ -956,3 +956,43 @@ def test_join_empty(left_empty, how, exp):
             expected = expected.rename_axis("A")
 
     tm.assert_frame_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "how, values",
+    [
+        ("inner", [0, 1, 2]),
+        ("outer", [0, 1, 2]),
+        ("left", [0, 1, 2]),
+        ("right", [0, 2, 1]),
+    ],
+)
+def test_join_multiindex_categorical_output_index_dtype(how, values):
+    # GH#50906
+    df1 = DataFrame(
+        {
+            "a": Categorical([0, 1, 2]),
+            "b": Categorical([0, 1, 2]),
+            "c": [0, 1, 2],
+        }
+    ).set_index(["a", "b"])
+
+    df2 = DataFrame(
+        {
+            "a": Categorical([0, 2, 1]),
+            "b": Categorical([0, 2, 1]),
+            "d": [0, 2, 1],
+        }
+    ).set_index(["a", "b"])
+
+    expected = DataFrame(
+        {
+            "a": Categorical(values),
+            "b": Categorical(values),
+            "c": values,
+            "d": values,
+        }
+    ).set_index(["a", "b"])
+
+    result = df1.join(df2, how=how)
+    tm.assert_frame_equal(result, expected)
