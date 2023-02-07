a6fbcb9e6f8765f54fa9ce8768cf348377b9d449
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-02-07 20:28:31 +0100

    Fix array manager test

--- pandas/tests/copy_view/test_core_functionalities.py
+++ pandas/tests/copy_view/test_core_functionalities.py
@@ -9,7 +9,8 @@ from pandas.tests.copy_view.util import get_array
 def test_assigning_to_same_variable_removes_references(using_copy_on_write):
     df = DataFrame({"a": [1, 2, 3]})
     df = df.reset_index()
-    assert df._mgr._has_no_reference(1)
+    if using_copy_on_write:
+        assert df._mgr._has_no_reference(1)
     arr = get_array(df, "a")
     df.iloc[0, 1] = 100  # Write into a
 
