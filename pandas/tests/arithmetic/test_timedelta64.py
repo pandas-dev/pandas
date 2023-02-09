b37321c7e02afba37bbef855a9096abe8771f569
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-02-09 01:03:57 +0000

    API: Timestamp and Timedelta .value changing in 2.0 (#50891)
    * wip
    
    * wip
    
    * fixup test collection
    
    * divide by zero to debug LOL
    
    * wip
    
    * more fixups
    
    * fixup test unary ops
    
    * ewm
    
    * test offseetes
    
    * are we there yet?
    
    * _value in ujson
    
    * :label: typing
    
    * wip
    
    * advise to use asm8 instead
    
    * fix test missing
    
    * make value property
    
    * un-xfail test
    
    ---------
    
    Co-authored-by: MarcoGorelli <>

--- pandas/tests/arithmetic/test_timedelta64.py
+++ pandas/tests/arithmetic/test_timedelta64.py
@@ -690,7 +690,7 @@ class TestAddSubNaTMasking:
         with pytest.raises(OutOfBoundsDatetime, match="10155196800000000000"):
             Timestamp("2000") + pd.to_timedelta(106580, "D")
 
-        _NaT = NaT.value + 1
+        _NaT = NaT._value + 1
         msg = "Overflow in int64 addition"
         with pytest.raises(OverflowError, match=msg):
             pd.to_timedelta([106580], "D") + Timestamp("2000")
@@ -754,7 +754,7 @@ class TestTimedeltaArraylikeAddSubOps:
             #  supported GH#29794
             DataFrame([NaT]).apply(pd.to_timedelta)  # TODO: belongs elsewhere?
 
-        dfn = DataFrame([NaT.value]).apply(pd.to_timedelta)
+        dfn = DataFrame([NaT._value]).apply(pd.to_timedelta)
 
         scalar1 = pd.to_timedelta("00:00:01")
         scalar2 = pd.to_timedelta("00:00:02")
