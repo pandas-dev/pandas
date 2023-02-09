b42ff69a068a4b4606c97c57b8a5cf6d352828a7
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-02-09 17:45:14 +0100

    BUG: eval and query not working with ea dtypes (#50764)
    * BUG: eval and query not working with ea dtypes
    
    * Fix windows build
    
    * Fix
    
    * Fix another bug
    
    * Fix eval
    
    * Fix
    
    * Fix
    
    * Add arrow tests
    
    * Fix pyarrow-less ci
    
    * Add try except
    
    * Add warning
    
    * Adjust warning
    
    * Fix warning
    
    * Fix
    
    * Update test_query_eval.py
    
    * Update pandas/core/computation/eval.py
    
    ---------
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/core/computation/eval.py
+++ pandas/core/computation/eval.py
@@ -7,7 +7,10 @@ import tokenize
 from typing import TYPE_CHECKING
 import warnings
 
+from pandas.util._exceptions import find_stack_level
 from pandas.util._validators import validate_bool_kwarg
+
+from pandas.core.dtypes.common import is_extension_array_dtype
 
 from pandas.core.computation.engines import ENGINES
 from pandas.core.computation.expr import (
@@ -332,6 +335,22 @@ def eval(
         )
 
         parsed_expr = Expr(expr, engine=engine, parser=parser, env=env)
+
+        if engine == "numexpr" and (
+            is_extension_array_dtype(parsed_expr.terms.return_type)
+            or getattr(parsed_expr.terms, "operand_types", None) is not None
+            and any(
+                is_extension_array_dtype(elem)
+                for elem in parsed_expr.terms.operand_types
+            )
+        ):
+            warnings.warn(
+                "Engine has switched to 'python' because numexpr does not support "
+                "extension array dtypes. Please set your engine to python manually.",
+                RuntimeWarning,
+                stacklevel=find_stack_level(),
+            )
+            engine = "python"
 
         # construct the engine and evaluate the parsed expression
         eng = ENGINES[engine]
