4e56838a16b1f04a80b61318cc6598dd198adc6b
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-02-09 20:09:29 +0100

    Revert "PERF: ArrowExtensionArray.to_numpy(dtype=object)" (#51273)
    Revert "PERF: ArrowExtensionArray.to_numpy(dtype=object) (#51227)"
    
    This reverts commit 8a81af8b56bdc09ee7324ddbcefd4f56c1c47f98.

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -853,12 +853,12 @@ class ArrowExtensionArray(OpsMixin, ExtensionArray):
             na_value = self.dtype.na_value
 
         pa_type = self._data.type
-        if pa.types.is_timestamp(pa_type) or pa.types.is_duration(pa_type):
+        if (
+            is_object_dtype(dtype)
+            or pa.types.is_timestamp(pa_type)
+            or pa.types.is_duration(pa_type)
+        ):
             result = np.array(list(self), dtype=dtype)
-        elif is_object_dtype(dtype) and self._hasna:
-            result = np.empty(len(self), dtype=object)
-            mask = ~self.isna()
-            result[mask] = np.asarray(self[mask]._data)
         else:
             result = np.asarray(self._data, dtype=dtype)
             if copy or self._hasna:
