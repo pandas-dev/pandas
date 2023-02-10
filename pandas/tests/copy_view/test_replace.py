ab842620957baf00306e5b23ab497db3f600c264
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2023-02-10 11:55:30 -0500

    TST: Test mask/where inplace don't modify views with CoW

--- pandas/tests/copy_view/test_replace.py
+++ pandas/tests/copy_view/test_replace.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from pandas import (
     Categorical,
@@ -34,5 +35,24 @@ def test_replace_inplace_reference(using_copy_on_write):
         assert not np.shares_memory(get_array(df, "a"), arr_a)
         assert df._mgr._has_no_reference(0)
         assert view._mgr._has_no_reference(0)
+    else:
+        assert np.shares_memory(get_array(df, "a"), arr_a)
+
+
+@pytest.mark.parametrize("method", ["where", "mask"])
+def test_masking_inplace(using_copy_on_write, method):
+    df = DataFrame({"a": [1.5, 2, 3]})
+    df_orig = df.copy()
+    arr_a = get_array(df, "a")
+    view = df[:]  # noqa
+
+    method = getattr(df, method)
+    method(df["a"] > 1.6, -1, inplace=True)
+
+    if using_copy_on_write:
+        assert not np.shares_memory(get_array(df, "a"), arr_a)
+        assert df._mgr._has_no_reference(0)
+        assert view._mgr._has_no_reference(0)
+        tm.assert_frame_equal(view, df_orig)
     else:
         assert np.shares_memory(get_array(df, "a"), arr_a)
