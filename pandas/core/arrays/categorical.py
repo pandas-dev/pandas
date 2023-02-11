0cfeffc669b7e902646adde2bdb0544fbc8d62e0
Brock <jbrockmendel@gmail.com>
2023-02-11 08:24:02 -0800

    lint fixup

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -2067,7 +2067,7 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
     # ------------------------------------------------------------------
     # ExtensionArray Interface
 
-    def unique(self):
+    def unique(self):  # noqa:W0246
         """
         Return the ``Categorical`` which ``categories`` and ``codes`` are
         unique.
