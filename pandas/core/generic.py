24008fad376088d03fbbb9f6df9024120239e23c
Asaf Reich <asafspades@gmail.com>
2023-02-14 10:39:28 -0500

    add note to docstring re NA vs NaN

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -7872,6 +7872,14 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         Everything else gets mapped to False values. Characters such as empty
         strings ``''`` or :attr:`numpy.inf` are not considered NA values
         (unless you set ``pandas.options.mode.use_inf_as_na = True``).
+        Note: For :class:`FloatingArray` and float-type
+        :class:`ArrowExtensionArray` the behavior is different, as they
+        use a mask (represented as pd.NA values) to designate missing
+        data but can also hold NaN which will **not** get mapped to True
+        (these values can be detected by :func:`numpy.isnan`).
+        Users may find this behavior unintuitive and want to
+        avoid these dtypes if they prefer to have only one
+        type of missing/NA value.
 
         Returns
         -------
