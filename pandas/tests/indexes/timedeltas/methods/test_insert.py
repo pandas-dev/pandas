fe85cbfb20f3f360ebba62905b93a6f98bf655c1
Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-02-14 21:07:21 +0000

    STYLE upgrade black formatter (#51348)
    upgrade black formatter
    
    Co-authored-by: MarcoGorelli <>

--- pandas/tests/indexes/timedeltas/methods/test_insert.py
+++ pandas/tests/indexes/timedeltas/methods/test_insert.py
@@ -17,7 +17,6 @@ import pandas._testing as tm
 
 class TestTimedeltaIndexInsert:
     def test_insert(self):
-
         idx = TimedeltaIndex(["4day", "1day", "2day"], name="idx")
 
         result = idx.insert(2, timedelta(days=5))
