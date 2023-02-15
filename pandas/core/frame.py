eb1e1a1fd920196106e01c29ff6061f294acc7c3
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-02-15 22:44:12 +0100

    Fix mypy

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -1398,8 +1398,8 @@ class DataFrame(NDFrame, OpsMixin):
             if using_cow and self._mgr.is_single_block:
                 s._mgr.blocks[0].refs = self._mgr.blocks[0].refs  # type: ignore[union-attr]  # noqa
                 s._mgr.blocks[0].refs.add_reference(  # type: ignore[union-attr]
-                    s._mgr.blocks[0]
-                )  # type: ignore[arg-type, union-attr]
+                    s._mgr.blocks[0]  # type: ignore[arg-type, union-attr]
+                )
             yield k, s
 
     def itertuples(
