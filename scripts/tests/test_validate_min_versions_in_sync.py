d2d5d73baf0c2a5045b48585f29e1b2845289d52
doge <kathleenhang@live.com>
2023-02-15 11:48:45 -0800

    Add parametrization to unit test

--- scripts/tests/test_validate_min_versions_in_sync.py
+++ scripts/tests/test_validate_min_versions_in_sync.py
@@ -1,6 +1,8 @@
 import pathlib
 import sys
 
+import pytest
+
 if sys.version_info >= (3, 11):
     import tomllib
 else:
@@ -9,21 +11,61 @@ else:
 from scripts.validate_min_versions_in_sync import update_yaml_file_version
 
 
-def test_update_yaml_file_version():
-    # minimum dependencies file
-    TOML_PATH = pathlib.Path("scripts/tests/dummy_toml_file.toml")
-    # yaml file that needs to be corrected
-    YAML_UNMODIFIED_PATH = pathlib.Path(
-        "scripts/tests/dummy_yaml_unmodified_file_1.yaml"
-    )
-    # yaml file that is the expected result
-    YAML_EXPECTED_PATH = pathlib.Path("scripts/tests/dummy_yaml_expected_file_1.yaml")
-    toml_dic = {}
-    with open(TOML_PATH, "rb") as toml_f:
+@pytest.mark.parametrize(
+    "src_toml, src_yaml, expected_yaml",
+    [
+        (  # random
+            pathlib.Path("scripts/tests/dummy_test_files/dummy_toml_file.toml"),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_unmodified_file_1.yaml"
+            ),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_expected_file_1.yaml"
+            ),
+        ),
+        (  # same version
+            pathlib.Path("scripts/tests/dummy_test_files/dummy_toml_file.toml"),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_unmodified_file_2.yaml"
+            ),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_expected_file_2.yaml"
+            ),
+        ),
+        (  # duplicate package
+            pathlib.Path("scripts/tests/dummy_test_files/dummy_toml_file.toml"),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_unmodified_file_3.yaml"
+            ),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_expected_file_3.yaml"
+            ),
+        ),
+        (  # empty version
+            pathlib.Path("scripts/tests/dummy_test_files/dummy_toml_file.toml"),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_unmodified_file_4.yaml"
+            ),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_expected_file_4.yaml"
+            ),
+        ),
+        (  # range version
+            pathlib.Path("scripts/tests/dummy_test_files/dummy_toml_file.toml"),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_unmodified_file_5.yaml"
+            ),
+            pathlib.Path(
+                "scripts/tests/dummy_test_files/dummy_yaml_expected_file_5.yaml"
+            ),
+        ),
+    ],
+)
+def test_update_yaml_file_version(src_toml, src_yaml, expected_yaml):
+    with open(src_toml, "rb") as toml_f:
         toml_dic = tomllib.load(toml_f)
-    with open(YAML_UNMODIFIED_PATH) as yaml_f:
+    with open(src_yaml) as yaml_f:
         result_yaml_file = update_yaml_file_version(yaml_f, toml_dic)
-    with open(YAML_EXPECTED_PATH) as yaml_f:
+    with open(expected_yaml) as yaml_f:
         dummy_yaml_expected_file_1 = yaml_f.read()
-    # if assert passes, then yaml file version update is correct
     assert result_yaml_file == dummy_yaml_expected_file_1
