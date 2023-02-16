dba9ddd65b3b3bc30e9081ee2ad93533e23d14f2
jbrockmendel <jbrockmendel@gmail.com>
2023-02-16 08:38:33 -0800

    API: ArrowDtype.type (#51307)
    * API: ArrowDtype.type
    
    * handle Decimal, update docstring, update tests
    
    * patch __contains__
    
    * mypy fixup
    
    * mypy fixup
    
    * mypy fixup
    
    * use pc.any(pc.is_nan...

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -510,6 +510,18 @@ class ArrowExtensionArray(OpsMixin, ExtensionArray, BaseStringArrayMethods):
         """
         return len(self._data)
 
+    def __contains__(self, key) -> bool:
+        # https://github.com/pandas-dev/pandas/pull/51307#issuecomment-1426372604
+        if isna(key) and key is not self.dtype.na_value:
+            if self.dtype.kind == "f" and lib.is_float(key) and isna(key):
+                return pc.any(pc.is_nan(self._data)).as_py()
+
+            # e.g. date or timestamp types we do not allow None here to match pd.NA
+            return False
+            # TODO: maybe complex? object?
+
+        return bool(super().__contains__(key))
+
     @property
     def _hasna(self) -> bool:
         return self._data.null_count > 0
