6df0f0fdd3087907043b7513a87a36a7f4f91526
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-02-18 00:49:08 +0100

    Fix tests

--- pandas/tests/frame/methods/test_set_axis.py
+++ pandas/tests/frame/methods/test_set_axis.py
@@ -33,13 +33,14 @@ class SharedSetAxisTests:
         tm.assert_equal(expected, result)
         assert result is not obj
         # check we DID make a copy
-        if obj.ndim == 1:
-            assert not tm.shares_memory(result, obj)
-        else:
-            assert not any(
-                tm.shares_memory(result.iloc[:, i], obj.iloc[:, i])
-                for i in range(obj.shape[1])
-            )
+        if not using_copy_on_write:
+            if obj.ndim == 1:
+                assert not tm.shares_memory(result, obj)
+            else:
+                assert not any(
+                    tm.shares_memory(result.iloc[:, i], obj.iloc[:, i])
+                    for i in range(obj.shape[1])
+                )
 
         result = obj.set_axis(new_index, axis=0, copy=False)
         tm.assert_equal(expected, result)
