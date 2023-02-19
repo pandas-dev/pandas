fd8d6d07f4af5b661b150069189df7fc492a7c36
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2023-02-19 16:36:34 +0530

    shorten msg

--- pandas/util/_validators.py
+++ pandas/util/_validators.py
@@ -324,7 +324,7 @@ def validate_percentile(q: float | Iterable[float]) -> np.ndarray:
     q_arr = np.asarray(q)
     # Don't change this to an f-string. The string formatting
     # is too expensive for cases where we don't need it.
-    msg = "percentiles should all be in the interval [0, 1]. Try value within this range instead."
+    msg = "percentiles should all be in the interval [0, 1]"
     if q_arr.ndim == 0:
         if not 0 <= q_arr <= 1:
             raise ValueError(msg)
