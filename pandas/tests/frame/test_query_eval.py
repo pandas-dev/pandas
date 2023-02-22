4c677c73cc73e8e336facc89430c3ee949144ff4
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-02-22 12:24:34 -0800

    Typo

--- pandas/tests/frame/test_query_eval.py
+++ pandas/tests/frame/test_query_eval.py
@@ -1332,7 +1332,6 @@ class TestDataFrameQueryBacktickQuoting:
         # GH#50261
         df = DataFrame({"a": Series([1, 2], dtype=dtype)})
         ref = {2}  # noqa:F841
-        result = df.query("a in @ref")
         warning = RuntimeWarning if dtype == "Int64" and NUMEXPR_INSTALLED else None
         with tm.assert_produces_warning(warning):
             result = df.query("a in @ref")
