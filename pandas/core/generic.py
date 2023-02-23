679ba778b4857756f505cd943ba97dce2db432ad
MeeseeksMachine <39504233+meeseeksmachine@users.noreply.github.com>
2023-02-23 15:29:28 +0100

    Backport PR #51568 on branch 2.0.x (ENH: Series.fillna with empty argument making deep copy) (#51580)
    Backport PR #51568: ENH: Series.fillna with empty argument making deep copy
    
    Co-authored-by: Patrick Hoefler <61934744+phofl@users.noreply.github.com>

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -6850,7 +6850,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
                         # test_fillna_nonscalar
                         if inplace:
                             return None
-                        return self.copy()
+                        return self.copy(deep=None)
                     from pandas import Series
 
                     value = Series(value)
