e2a209ec2e4d2784cb101854e500e4595f990162
kathleenhang <kathleenhang@live.com>
2023-02-23 08:38:43 -0800

    CI, ENH: Check each minimum dependency is enforced in *.yaml and environment.yml (#51189)
    * Add pin_min_versions_to_ci_deps()
    
    * Fix typos
    
    * Remove debug statements
    
    * Update YAML package versions
    
    * Replace files with updated min versions
    
    * Add exclusions + edge cases
    
    * Revert file to original
    
    * Revert environment.yml
    
    * Fix bugs
    
    * Add helper function
    
    * Ran pre-commit run validate-min-versions-in-sync --all-files
    
    * Fix merge issue
    
    * add parameter for custom toml dependency
    
    * Add dummy test files for test_validate_min_versions_in_sync.py
    
    * Add unit test: test_update_yaml_file_version()
    
    * Add dummy test files for edge cases
    
    * Add parametrization to unit test
    
    * Run pre-commit
    
    * Fix tzdata bug
    
    * Fix tzdata issue
    
    * Refactor
    
    * Update ci deps
    
    * Remove pyarrow
    
    * Revert tzdata
    
    * Update pyarrow
    
    * Revert pyarrow
    
    * Add docstrings
    
    * Remove psutil
    
    * Fix file rewrite issue
    
    * Refactor test data
    
    * minor fixups
    
    * fixup
    
    * improve typing
    
    ---------
    
    Co-authored-by: MarcoGorelli <>
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- scripts/validate_min_versions_in_sync.py
+++ scripts/validate_min_versions_in_sync.py
@@ -17,10 +17,16 @@ from __future__ import annotations
 import pathlib
 import sys
 
+import yaml
+
 if sys.version_info >= (3, 11):
     import tomllib
 else:
     import tomli as tomllib
+
+from typing import Any
+
+from scripts.generate_pip_deps_from_conda import RENAME
 
 DOC_PATH = pathlib.Path("doc/source/getting_started/install.rst").resolve()
 CI_PATH = next(
@@ -28,7 +34,14 @@ CI_PATH = next(
 )
 CODE_PATH = pathlib.Path("pandas/compat/_optional.py").resolve()
 SETUP_PATH = pathlib.Path("pyproject.toml").resolve()
+YAML_PATH = pathlib.Path("ci/deps")
+ENV_PATH = pathlib.Path("environment.yml")
 EXCLUDE_DEPS = {"tzdata", "blosc"}
+EXCLUSION_LIST = {
+    "python=3.8[build=*_pypy]": None,
+    "tzdata": None,
+    "pyarrow": None,
+}
 # pandas package is not available
 # in pre-commit environment
 sys.path.append("pandas/compat")
@@ -39,6 +52,147 @@ import version
 sys.modules["pandas.util.version"] = version
 sys.modules["pandas.util._exceptions"] = _exceptions
 import _optional
+
+
+def pin_min_versions_to_ci_deps() -> int:
+    """
+    Pin minimum versions to CI dependencies.
+
+    Pip dependencies are not pinned.
+    """
+    all_yaml_files = list(YAML_PATH.iterdir())
+    all_yaml_files.append(ENV_PATH)
+    toml_dependencies = {}
+    with open(SETUP_PATH, "rb") as toml_f:
+        toml_dependencies = tomllib.load(toml_f)
+    ret = 0
+    for curr_file in all_yaml_files:
+        with open(curr_file) as yaml_f:
+            yaml_start_data = yaml_f.read()
+        yaml_file = yaml.safe_load(yaml_start_data)
+        yaml_dependencies = yaml_file["dependencies"]
+        yaml_map = get_yaml_map_from(yaml_dependencies)
+        toml_map = get_toml_map_from(toml_dependencies)
+        yaml_result_data = pin_min_versions_to_yaml_file(
+            yaml_map, toml_map, yaml_start_data
+        )
+        if yaml_result_data != yaml_start_data:
+            with open(curr_file, "w") as f:
+                f.write(yaml_result_data)
+            ret |= 1
+    return ret
+
+
+def get_toml_map_from(toml_dic: dict[str, Any]) -> dict[str, str]:
+    toml_deps = {}
+    toml_dependencies = set(toml_dic["project"]["optional-dependencies"]["all"])
+    for dependency in toml_dependencies:
+        toml_package, toml_version = dependency.strip().split(">=")
+        toml_deps[toml_package] = toml_version
+    return toml_deps
+
+
+def get_operator_from(dependency: str) -> str | None:
+    if "<=" in dependency:
+        operator = "<="
+    elif ">=" in dependency:
+        operator = ">="
+    elif "=" in dependency:
+        operator = "="
+    elif ">" in dependency:
+        operator = ">"
+    elif "<" in dependency:
+        operator = "<"
+    else:
+        operator = None
+    return operator
+
+
+def get_yaml_map_from(
+    yaml_dic: list[str | dict[str, list[str]]]
+) -> dict[str, list[str] | None]:
+    yaml_map: dict[str, list[str] | None] = {}
+    for dependency in yaml_dic:
+        if (
+            isinstance(dependency, dict)
+            or dependency in EXCLUSION_LIST
+            or dependency in yaml_map
+        ):
+            continue
+        search_text = str(dependency)
+        operator = get_operator_from(search_text)
+        if "," in dependency:
+            yaml_dependency, yaml_version1 = search_text.split(",")
+            operator = get_operator_from(yaml_dependency)
+            assert operator is not None
+            yaml_package, yaml_version2 = yaml_dependency.split(operator)
+            yaml_version2 = operator + yaml_version2
+            yaml_map[yaml_package] = [yaml_version1, yaml_version2]
+        elif operator is not None:
+            yaml_package, yaml_version = search_text.split(operator)
+            yaml_version = operator + yaml_version
+            yaml_map[yaml_package] = [yaml_version]
+        else:
+            yaml_package, yaml_version = search_text.strip(), None
+            yaml_map[yaml_package] = yaml_version
+    return yaml_map
+
+
+def clean_version_list(
+    yaml_versions: list[str], toml_version: version.Version
+) -> list[str]:
+    for i in range(len(yaml_versions)):
+        yaml_version = yaml_versions[i]
+        operator = get_operator_from(yaml_version)
+        assert operator is not None
+        if "<=" in operator or ">=" in operator:
+            yaml_version = yaml_version[2:]
+        else:
+            yaml_version = yaml_version[1:]
+        yaml_version = version.parse(yaml_version)
+        if yaml_version < toml_version:
+            yaml_versions[i] = "-" + str(yaml_version)
+        elif yaml_version >= toml_version:
+            if ">" in operator:
+                yaml_versions[i] = "-" + str(yaml_version)
+    return yaml_versions
+
+
+def pin_min_versions_to_yaml_file(
+    yaml_map: dict[str, list[str] | None], toml_map: dict[str, str], yaml_file_data: str
+) -> str:
+    data = yaml_file_data
+    for yaml_package, yaml_versions in yaml_map.items():
+        if yaml_package in EXCLUSION_LIST:
+            continue
+        old_dep = yaml_package
+        if yaml_versions is not None:
+            for yaml_version in yaml_versions:
+                old_dep += yaml_version + ", "
+            old_dep = old_dep[:-2]
+        if RENAME.get(yaml_package, yaml_package) in toml_map:
+            min_dep = toml_map[RENAME.get(yaml_package, yaml_package)]
+        elif yaml_package in toml_map:
+            min_dep = toml_map[yaml_package]
+        else:
+            continue
+        if yaml_versions is None:
+            new_dep = old_dep + ">=" + min_dep
+            data = data.replace(old_dep, new_dep, 1)
+            continue
+        toml_version = version.parse(min_dep)
+        yaml_versions = clean_version_list(yaml_versions, toml_version)
+        cleaned_yaml_versions = [x for x in yaml_versions if "-" not in x]
+        new_dep = yaml_package
+        for yaml_version in cleaned_yaml_versions:
+            new_dep += yaml_version + ", "
+        operator = get_operator_from(new_dep)
+        if operator != "=":
+            new_dep += ">=" + min_dep
+        else:
+            new_dep = new_dep[:-2]
+        data = data.replace(old_dep, new_dep)
+    return data
 
 
 def get_versions_from_code() -> dict[str, str]:
@@ -92,7 +246,6 @@ def get_versions_from_toml() -> dict[str, str]:
     """Min versions in pyproject.toml for pip install pandas[extra]."""
     install_map = _optional.INSTALL_MAPPING
     optional_dependencies = {}
-
     with open(SETUP_PATH, "rb") as pyproject_f:
         pyproject_toml = tomllib.load(pyproject_f)
         opt_deps = pyproject_toml["project"]["optional-dependencies"]
@@ -108,11 +261,12 @@ def get_versions_from_toml() -> dict[str, str]:
 
     for item in EXCLUDE_DEPS:
         optional_dependencies.pop(item, None)
-
     return optional_dependencies
 
 
-def main():
+def main() -> int:
+    ret = 0
+    ret |= pin_min_versions_to_ci_deps()
     with open(CI_PATH, encoding="utf-8") as f:
         _, ci_optional = get_versions_from_ci(f.readlines())
     code_optional = get_versions_from_code()
@@ -138,9 +292,9 @@ def main():
                 f"{CODE_PATH}: {code_optional.get(package, 'Not specified')}\n"
                 f"{SETUP_PATH}: {setup_optional.get(package, 'Not specified')}\n\n"
             )
-        sys.exit(1)
-    sys.exit(0)
+        ret |= 1
+    return ret
 
 
 if __name__ == "__main__":
-    main()
+    sys.exit(main())
