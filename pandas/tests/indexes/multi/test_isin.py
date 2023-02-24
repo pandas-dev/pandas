30546e0e5f0e21d38369f3099e0c3c57a24f1a0a
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-02-24 18:54:47 +0000

    REGR: MultiIndex.isin with empty iterable raising (#51605)

--- pandas/tests/indexes/multi/test_isin.py
+++ pandas/tests/indexes/multi/test_isin.py
@@ -85,3 +85,11 @@ def test_isin_multi_index_with_missing_value(labels, expected, level):
     midx = MultiIndex.from_arrays([[np.nan, "a", "b"], ["c", "d", np.nan]])
     result = midx.isin(labels, level=level)
     tm.assert_numpy_array_equal(result, expected)
+
+
+def test_isin_empty():
+    # GH#51599
+    midx = MultiIndex.from_arrays([[1, 2], [3, 4]])
+    result = midx.isin([])
+    expected = np.array([False, False])
+    tm.assert_numpy_array_equal(result, expected)
