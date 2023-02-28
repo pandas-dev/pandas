cce319d8d8bbd5743a7b202b15d298fbcfce79ab
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-02-28 23:10:30 +0100

    Remove unnecessary code

--- pandas/core/internals/construction.py
+++ pandas/core/internals/construction.py
@@ -772,19 +772,6 @@ def to_arrays(
     -----
     Ensures that len(result_arrays) == len(result_index).
     """
-    if isinstance(data, ABCDataFrame):
-        # see test_from_records_with_index_data, test_from_records_bad_index_column
-        if columns is not None:
-            arrays = [
-                data._ixs(i, axis=1)._values
-                for i, col in enumerate(data.columns)
-                if col in columns
-            ]
-        else:
-            columns = data.columns
-            arrays = [data._ixs(i, axis=1)._values for i in range(len(columns))]
-
-        return arrays, columns
 
     if not len(data):
         if isinstance(data, np.ndarray):
