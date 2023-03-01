83f3bedda2ca36d0cd32493bf7d918448aa47387
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-03-01 17:39:26 +0100

    Update test_replace.py

--- pandas/tests/copy_view/test_replace.py
+++ pandas/tests/copy_view/test_replace.py
@@ -368,8 +368,8 @@ def test_replace_columnwise_no_op(using_copy_on_write):
     df = DataFrame({"a": [1, 2, 3], "b": [1, 2, 3]})
     df_orig = df.copy()
     df2 = df.replace({"a": 10}, 100)
-    df2.iloc[0, 0] = 100
     if using_copy_on_write:
         # TODO(CoW): This should share memory
         assert not np.shares_memory(get_array(df2, "a"), get_array(df, "a"))
+    df2.iloc[0, 0] = 100
     tm.assert_frame_equal(df, df_orig)
