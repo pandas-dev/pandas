b7ceb67ca7942da72d9b751c5d491218aeda083b
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-03-01 01:43:08 +0100

    Fix test

--- pandas/tests/copy_view/test_replace.py
+++ pandas/tests/copy_view/test_replace.py
@@ -64,13 +64,13 @@ def test_replace_regex_inplace_refs(using_copy_on_write):
 def test_replace_regex_inplace(using_copy_on_write):
     df = DataFrame({"a": ["aaa", "bbb"]})
     arr = get_array(df, "a")
-    df.replace(to_replace=r"^a.$", value="new", inplace=True, regex=True)
+    df.replace(to_replace=r"^a.*$", value="new", inplace=True, regex=True)
     if using_copy_on_write:
         assert df._mgr._has_no_reference(0)
     assert np.shares_memory(arr, get_array(df, "a"))
 
     df_orig = df.copy()
-    df2 = df.replace(to_replace=r"^a.$", value="new", regex=True)
+    df2 = df.replace(to_replace=r"^b.*$", value="new", regex=True)
     tm.assert_frame_equal(df_orig, df)
     assert not np.shares_memory(get_array(df2, "a"), get_array(df, "a"))
 
