bdb617e54cc751d4966161ac0b0dcd6035d64862
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2023-03-01 17:54:28 -0500

    Backport PR #51640: COMPAT: Update code for latest Cython 3 beta

--- pandas/_libs/tslibs/offsets.pyx
+++ pandas/_libs/tslibs/offsets.pyx
@@ -4563,7 +4563,7 @@ def roll_qtrday(other: datetime, n: int, month: int,
 cdef int _roll_qtrday(npy_datetimestruct* dts,
                       int n,
                       int months_since,
-                      str day_opt) nogil except? -1:
+                      str day_opt) except? -1 nogil:
     """
     See roll_qtrday.__doc__
     """
