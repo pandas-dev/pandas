07aa26de999369138b97ec1a82faea26dea21584
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-03-02 11:31:24 +0100

    Remove

--- pandas/tests/frame/methods/test_set_index.py
+++ pandas/tests/frame/methods/test_set_index.py
@@ -10,7 +10,6 @@ from datetime import (
 import numpy as np
 import pytest
 
-import pandas as pd
 from pandas import (
     Categorical,
     DataFrame,
@@ -402,7 +401,6 @@ class TestSetIndex:
             tm.assert_frame_equal(result, df)
 
     def test_set_index_datetime(self):
-        pd.options.mode.copy_on_write = True
         # GH#3950
         df = DataFrame(
             {
