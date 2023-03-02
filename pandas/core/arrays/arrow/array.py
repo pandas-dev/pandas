8d0abf874899cc20de77381a1f7d0626333c9e80
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-03-02 11:18:44 +0100

    remove from pandas

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -349,7 +349,7 @@ class ArrowExtensionArray(OpsMixin, ExtensionArray, BaseStringArrayMethods):
                     pa_dtype = pa.string()
                 else:
                     pa_dtype = self._dtype.pyarrow_dtype
-                return type(self)(pa.array([], type=pa_dtype, from_pandas=True))
+                return type(self)(pa.array([], type=pa_dtype))
             elif is_integer_dtype(item.dtype):
                 return self.take(item)
             elif is_bool_dtype(item.dtype):
