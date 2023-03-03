9b02111a88cde2d1089fbc9b5d661dec8a54d5a1
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-03-03 16:56:51 +0100

    Add transpose test

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -194,6 +194,18 @@ def test_methods_series_copy_keyword(request, method, copy, using_copy_on_write)
         assert not np.shares_memory(get_array(ser2), get_array(ser))
 
 
+@pytest.mark.parametrize("copy", [True, None, False])
+def test_transpose_copy_keyword(using_copy_on_write, copy):
+    df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    result = df.transpose(copy=copy)
+    share_memory = using_copy_on_write or copy is False or copy is None
+
+    if share_memory:
+        assert np.shares_memory(get_array(df, "a"), get_array(result, 0))
+    else:
+        assert not np.shares_memory(get_array(df, "a"), get_array(result, 0))
+
+
 # -----------------------------------------------------------------------------
 # DataFrame methods returning new DataFrame using shallow copy
 
