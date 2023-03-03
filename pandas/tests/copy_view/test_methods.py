eef5234ba8353560371353cc3104b954401cf7e1
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-03-04 00:47:32 +0100

    Fix array manager

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -195,10 +195,11 @@ def test_methods_series_copy_keyword(request, method, copy, using_copy_on_write)
 
 
 @pytest.mark.parametrize("copy", [True, None, False])
-def test_transpose_copy_keyword(using_copy_on_write, copy):
+def test_transpose_copy_keyword(using_copy_on_write, copy, using_array_manager):
     df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
     result = df.transpose(copy=copy)
     share_memory = using_copy_on_write or copy is False or copy is None
+    share_memory = share_memory and not using_array_manager
 
     if share_memory:
         assert np.shares_memory(get_array(df, "a"), get_array(result, 0))
