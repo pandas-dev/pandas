f516f4f56399fdb978b0f1074e53a6cfbf490134
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-03-04 14:13:59 +0100

    PERF: use _name instead of name as Series metadata to avoid validation

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -343,7 +343,7 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
     _HANDLED_TYPES = (Index, ExtensionArray, np.ndarray)
 
     _name: Hashable
-    _metadata: list[str] = ["name"]
+    _metadata: list[str] = ["_name"]
     _internal_names_set = {"index"} | NDFrame._internal_names_set
     _accessors = {"dt", "cat", "str", "sparse"}
     _hidden_attrs = (
