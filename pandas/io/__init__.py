6ec379f42bce6d156377d6fdaf7a775b1d3c487d
luke <2736230899@qq.com>
2023-03-07 09:19:15 +0800

    STYLE: enable ruff TCH

--- pandas/io/__init__.py
+++ pandas/io/__init__.py
@@ -1,12 +1,9 @@
-from typing import TYPE_CHECKING
+# import modules that have public classes/functions
+from pandas.io import (
+    formats,
+    json,
+    stata,
+)
 
-if TYPE_CHECKING:
-    # import modules that have public classes/functions
-    from pandas.io import (
-        formats,
-        json,
-        stata,
-    )
-
-    # and mark only those modules as public
-    __all__ = ["formats", "json", "stata"]
+# mark only those modules as public
+__all__ = ["formats", "json", "stata"]
--- pandas/io/formats/__init__.py
+++ pandas/io/formats/__init__.py
@@ -1,8 +1,5 @@
-from typing import TYPE_CHECKING
+# import modules that have public classes/functions
+from pandas.io.formats import style
 
-if TYPE_CHECKING:
-    # import modules that have public classes/functions
-    from pandas.io.formats import style
-
-    # and mark only those modules as public
-    __all__ = ["style"]
+# and mark only those modules as public
+__all__ = ["style"]
