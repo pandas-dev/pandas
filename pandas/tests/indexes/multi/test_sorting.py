11fffc5c0f18b6fb80be7c4f515e1fbae774d96a
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-03-08 21:51:23 +0000

    ENH: Add na_position support to midx.sort_values (#51651)

--- pandas/tests/indexes/multi/test_sorting.py
+++ pandas/tests/indexes/multi/test_sorting.py
@@ -14,6 +14,7 @@ from pandas import (
     Index,
     MultiIndex,
     RangeIndex,
+    Series,
     Timestamp,
 )
 import pandas._testing as tm
@@ -311,3 +312,27 @@ def test_sort_values_incomparable():
     match = "'<' not supported between instances of 'Timestamp' and 'int'"
     with pytest.raises(TypeError, match=match):
         mi.sort_values()
+
+
+@pytest.mark.parametrize("na_position", ["first", "last"])
+@pytest.mark.parametrize("dtype", ["float64", "Int64", "Float64"])
+def test_sort_values_with_na_na_position(dtype, na_position):
+    # 51612
+    arrays = [
+        Series([1, 1, 2], dtype=dtype),
+        Series([1, None, 3], dtype=dtype),
+    ]
+    index = MultiIndex.from_arrays(arrays)
+    result = index.sort_values(na_position=na_position)
+    if na_position == "first":
+        arrays = [
+            Series([1, 1, 2], dtype=dtype),
+            Series([None, 1, 3], dtype=dtype),
+        ]
+    else:
+        arrays = [
+            Series([1, 1, 2], dtype=dtype),
+            Series([1, None, 3], dtype=dtype),
+        ]
+    expected = MultiIndex.from_arrays(arrays)
+    tm.assert_index_equal(result, expected)
