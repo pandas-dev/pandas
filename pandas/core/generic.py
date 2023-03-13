e760892a86886ad08bbd7740763e1fc21863c1af
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-03-13 20:38:44 +0000

    BUG: swapaxes creating result with read_only array

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -750,7 +750,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         mapping = {i: j, j: i}
 
         new_axes = [self._get_axis(mapping.get(k, k)) for k in range(self._AXIS_LEN)]
-        new_values = self.values.swapaxes(i, j)
+        new_values = self._values.swapaxes(i, j)
         if (
             using_copy_on_write()
             and self._mgr.is_single_block
