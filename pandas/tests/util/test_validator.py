96c867385572c020c39d4f457a5412719331fb51
Shashwat Agrawal <shashwatagrawal473@gmail.com>
2023-03-14 21:00:52 +0530

    refactored test file

--- pandas/tests/util/test_validator.py
+++ pandas/tests/util/test_validator.py
@@ -1,11 +1,12 @@
 import numpy as np
 import pytest
 
-from pandas.util._validators import validate_percentile
+import pandas as pd
 
 
-def test_validate_percentile_negative_array_input():
-    percentile_array = np.array([-0.5, 0.25, 1.5])
-    expected_error_msg = "percentiles should all be in the interval [0, 1]"
-    with pytest.raises(ValueError, match=(expected_error_msg)):
-        validate_percentile(percentile_array)
+def test_series_quantile_invalid_percentile():
+    s = pd.Series(np.random.randn(100))
+    percentile_array = [-0.5, 0.25, 1.5]
+    msg = "percentiles should all be in the interval \\[0, 1\\]"
+    with pytest.raises(ValueError, match=msg):
+        s.quantile(percentile_array)
