f5ccde0aac2bf0dc5899937501b487d7512bf45d
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-03-15 22:07:45 +0100

    CoW: Series with MultiIndex with tuples does not respect CoW

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -1044,9 +1044,10 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
 
         # If key is contained, would have returned by now
         indexer, new_index = self.index.get_loc_level(key)
-        return self._constructor(self._values[indexer], index=new_index).__finalize__(
-            self
-        )
+        result = self._constructor(self._values[indexer], index=new_index)
+        if using_copy_on_write() and isinstance(indexer, slice):
+            result._mgr.add_references(self._mgr)  # type: ignore[arg-type]
+        return result.__finalize__(self)
 
     def _get_values(self, indexer: slice | npt.NDArray[np.bool_]) -> Series:
         new_mgr = self._mgr.getitem_mgr(indexer)
