e2eceec7f02e3542324fc1c6e8b3d94eb061162e
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-03-15 17:09:46 +0100

    Fix array manager

--- pandas/tests/copy_view/test_constructors.py
+++ pandas/tests/copy_view/test_constructors.py
@@ -182,11 +182,17 @@ def test_dataframe_from_dict_of_series_with_dtype(index):
 
 
 @pytest.mark.parametrize("copy", [False, None, True])
-def test_frame_from_numpy_array(using_copy_on_write, copy):
+def test_frame_from_numpy_array(using_copy_on_write, copy, using_array_manager):
     arr = np.array([[1, 2], [3, 4]])
     df = DataFrame(arr, copy=copy)
 
-    if using_copy_on_write and copy is not False or copy is True:
+    if (
+        using_copy_on_write
+        and copy is not False
+        or copy is True
+        or using_array_manager
+        and copy is None
+    ):
         assert not np.shares_memory(get_array(df, 0), arr)
     else:
         assert np.shares_memory(get_array(df, 0), arr)
