a9303433c1029f1ed440fc4a1b6cefac1f987573
jbrockmendel <jbrockmendel@gmail.com>
2023-03-15 14:10:54 -0700

    BUG/API: preserve non-nano in factorize/unique (#51978)
    * BUG/API: preserve non-nano in factorize/unique
    
    * test
    
    (cherry picked from commit 6a13450cec5996cc14f722069ef9be150f034e03)

--- pandas/core/dtypes/cast.py
+++ pandas/core/dtypes/cast.py
@@ -31,7 +31,6 @@ from pandas._libs.tslibs import (
     OutOfBoundsTimedelta,
     Timedelta,
     Timestamp,
-    astype_overflowsafe,
     get_unit_from_dtype,
     is_supported_unit,
 )
@@ -50,8 +49,6 @@ from pandas.errors import (
 )
 
 from pandas.core.dtypes.common import (
-    DT64NS_DTYPE,
-    TD64NS_DTYPE,
     ensure_int8,
     ensure_int16,
     ensure_int32,
@@ -1229,23 +1226,6 @@ def maybe_cast_to_datetime(
             raise
 
         return dta
-
-
-def sanitize_to_nanoseconds(values: np.ndarray, copy: bool = False) -> np.ndarray:
-    """
-    Safely convert non-nanosecond datetime64 or timedelta64 values to nanosecond.
-    """
-    dtype = values.dtype
-    if dtype.kind == "M" and dtype != DT64NS_DTYPE:
-        values = astype_overflowsafe(values, dtype=DT64NS_DTYPE)
-
-    elif dtype.kind == "m" and dtype != TD64NS_DTYPE:
-        values = astype_overflowsafe(values, dtype=TD64NS_DTYPE)
-
-    elif copy:
-        values = values.copy()
-
-    return values
 
 
 def _ensure_nanosecond_dtype(dtype: DtypeObj) -> None:
