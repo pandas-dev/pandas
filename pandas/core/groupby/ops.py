a78e2a351b2137251487e4b177f08c3a58ef3fcd
jbrockmendel <jbrockmendel@gmail.com>
2023-03-20 13:37:23 -0700

    REF: GroupBy.any/all use WrappedCythonOp (#52089)

--- pandas/core/groupby/ops.py
+++ pandas/core/groupby/ops.py
@@ -115,7 +115,9 @@ class WrappedCythonOp:
 
     # Functions for which we do _not_ attempt to cast the cython result
     #  back to the original dtype.
-    cast_blocklist = frozenset(["rank", "count", "size", "idxmin", "idxmax"])
+    cast_blocklist = frozenset(
+        ["any", "all", "rank", "count", "size", "idxmin", "idxmax"]
+    )
 
     def __init__(self, kind: str, how: str, has_dropped_na: bool) -> None:
         self.kind = kind
@@ -124,6 +126,8 @@ class WrappedCythonOp:
 
     _CYTHON_FUNCTIONS: dict[str, dict] = {
         "aggregate": {
+            "any": functools.partial(libgroupby.group_any_all, val_test="any"),
+            "all": functools.partial(libgroupby.group_any_all, val_test="all"),
             "sum": "group_sum",
             "prod": "group_prod",
             "min": "group_min",
@@ -253,7 +257,7 @@ class WrappedCythonOp:
                 #  don't go down a group-by-group path, since in the empty-groups
                 #  case that would fail to raise
                 raise TypeError(f"Cannot perform {how} with non-ordered Categorical")
-            if how not in ["rank"]:
+            if how not in ["rank", "any", "all"]:
                 # only "rank" is implemented in cython
                 raise NotImplementedError(f"{dtype} dtype not supported")
 
@@ -352,10 +356,13 @@ class WrappedCythonOp:
             )
 
         elif isinstance(values, Categorical):
-            assert self.how == "rank"  # the only one implemented ATM
-            assert values.ordered  # checked earlier
+            assert self.how in ["rank", "any", "all"]
             mask = values.isna()
-            npvalues = values._ndarray
+            if self.how == "rank":
+                assert values.ordered  # checked earlier
+                npvalues = values._ndarray
+            else:
+                npvalues = values.astype(bool)
 
             res_values = self._cython_op_ndim_compat(
                 npvalues,
@@ -546,6 +553,19 @@ class WrappedCythonOp:
         if values.dtype == "float16":
             values = values.astype(np.float32)
 
+        if self.how in ["any", "all"]:
+            if mask is None:
+                mask = isna(values)
+            if dtype == object:
+                if kwargs["skipna"]:
+                    # GH#37501: don't raise on pd.NA when skipna=True
+                    if mask.any():
+                        # mask on original values computed separately
+                        values = values.copy()
+                        values[mask] = True
+            values = values.astype(bool, copy=False).view(np.int8)
+            is_numeric = True
+
         values = values.T
         if mask is not None:
             mask = mask.T
@@ -584,6 +604,16 @@ class WrappedCythonOp:
                     result_mask=result_mask,
                     **kwargs,
                 )
+            elif self.how in ["any", "all"]:
+                func(
+                    out=result,
+                    values=values,
+                    labels=comp_ids,
+                    mask=mask,
+                    result_mask=result_mask,
+                    **kwargs,
+                )
+                result = result.astype(bool, copy=False)
             else:
                 raise NotImplementedError(f"{self.how} is not implemented")
         else:
