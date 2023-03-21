f2cd62a0263ee51d036f23c76bc9df5c44fb33a9
jbrockmendel <jbrockmendel@gmail.com>
2023-03-21 10:10:29 -0700

    DEPR: DFGB.dtypes, rolling axis, Categorical.to_list (#51997)
    * DEPR: DFGB.dtypes, rolling axis, Categorical.to_list
    
    * revert Categorical.to_list deprecation

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -2637,6 +2637,14 @@ class DataFrameGroupBy(GroupBy[DataFrame]):
     @property
     @doc(DataFrame.dtypes.__doc__)
     def dtypes(self) -> Series:
+        # GH#51045
+        warnings.warn(
+            f"{type(self).__name__}.dtypes is deprecated and will be removed in "
+            "a future version. Check the dtypes on the base object instead",
+            FutureWarning,
+            stacklevel=find_stack_level(),
+        )
+
         # error: Incompatible return value type (got "DataFrame", expected "Series")
         return self.apply(lambda df: df.dtypes)  # type: ignore[return-value]
 
