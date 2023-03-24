f0714a33da32598419ffde3614345beafd1fcb94
Brayan Alexander Muñoz B <balexander.munoz@udea.edu.co>
2023-03-24 11:50:16 -0500

    BUG: Fix getsizeof when using Series(obj) and taking into account GC corrections (#52112)
    * BUG: Fix getsizeof when using Series(obj) and taking into account GC corrections
    
    Calling __sizeof__ to compute the size of an object produces an error, and in general, this method does not take Garbage Collector corrections into account.
    
    * BUG: Fix getsizeof when using Series(obj) and taking into account GC corrections
    
    Calling __sizeof__ to compute the size of an object produces an error, and in general, this method does not take Garbage Collector corrections into account.
    
    * Removing unnecessary tests that depend on OS bits
    
    * TST: Included python data types
    
    * TST: Added more objects Series with objects for Memory test
    
    * TST: Added more objects Series with objects for Memory test
    
    * CLN: Deleted unnecesary tests
    
    * TYP: Renamed _object_series to _type_objects_series

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -760,6 +760,29 @@ def index_or_series_obj(request):
     return _index_or_series_objs[request.param].copy(deep=True)
 
 
+_typ_objects_series = {
+    f"{dtype.__name__}-series": Series(dtype) for dtype in tm.PYTHON_DATA_TYPES
+}
+
+
+_index_or_series_memory_objs = {
+    **indices_dict,
+    **_series,
+    **_narrow_series,
+    **_typ_objects_series,
+}
+
+
+@pytest.fixture(params=_index_or_series_memory_objs.keys())
+def index_or_series_memory_obj(request):
+    """
+    Fixture for tests on indexes, series, series with a narrow dtype and
+    series with empty objects type
+    copy to avoid mutation, e.g. setting .name
+    """
+    return _index_or_series_memory_objs[request.param].copy(deep=True)
+
+
 # ----------------------------------------------------------------
 # DataFrames
 # ----------------------------------------------------------------
