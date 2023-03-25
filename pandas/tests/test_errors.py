b697aba60edcfade44c09e904e85c7aa5663711b
Yeesian Ng <ngyeesian@gmail.com>
2023-03-25 13:44:46 -0400

    Try interpolating the types into the error message

--- pandas/tests/test_errors.py
+++ pandas/tests/test_errors.py
@@ -111,7 +111,7 @@ def test_AbstractMethodError_classmethod():
     with pytest.raises(AbstractMethodError, match=xpr):
         Foo().method()
 
-    types = {"method", "classmethod", "staticmethod", "property"}
-    xpr = f"methodtype must be one of {types}, got unknownmethod instead."
+    xpr = "methodtype must be one of {'method', 'classmethod', "
+          "'staticmethod', 'property'}, got unknownmethod instead."
     with pytest.raises(AbstractMethodError, match=xpr):
         Foo().unknownmethod()
