397c1db218ecee5ad2308a4608f709947e1b94a7
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-03-30 19:33:51 -0700

    TYP: Remove unneeded type ignore (#52308)

--- pandas/io/formats/printing.py
+++ pandas/io/formats/printing.py
@@ -263,10 +263,7 @@ def enable_data_resource_formatter(enable: bool) -> None:
 
             class TableSchemaFormatter(BaseFormatter):
                 print_method = ObjectName("_repr_data_resource_")
-                # Incompatible types in assignment (expression has type
-                # "Tuple[Type[Dict[Any, Any]]]", base class "BaseFormatter"
-                # defined the type as "Type[str]")
-                _return_type = (dict,)  # type: ignore[assignment]
+                _return_type = (dict,)
 
             # register it:
             formatters[mimetype] = TableSchemaFormatter()
