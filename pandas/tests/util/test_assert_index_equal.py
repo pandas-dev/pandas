f24dbdcc5d5da37630099acf3a95b2ac0ede73b7
Jayendra <jndrpatil@gmail.com>
2023-04-01 09:30:55 +0530

    Added whatsnew entry and mentioned issue number for test

--- pandas/tests/util/test_assert_index_equal.py
+++ pandas/tests/util/test_assert_index_equal.py
@@ -302,6 +302,7 @@ def test_assert_ea_index_equal_non_matching_na(check_names, check_categorical):
 
 @pytest.mark.parametrize("check_categorical", [True, False])
 def test_assert_multi_index_dtype_check_categorical(check_categorical):
+    # GH#52126
     idx1 = MultiIndex.from_arrays([Categorical(np.array([1, 2], dtype=np.uint64))])
     idx2 = MultiIndex.from_arrays([Categorical(np.array([1, 2], dtype=np.int64))])
     if check_categorical:
