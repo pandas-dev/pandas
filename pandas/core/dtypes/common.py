56e24ff1cdadc47eddcdf36984453e516a557d24
jbrockmendel <jbrockmendel@gmail.com>
2023-04-03 16:37:43 -0700

    CLN: remove unused nan comparison functions, ensure_float (#52385)
    * CLN: remove unused nan comparison functions
    
    * CLN: remove unused ensure_float

--- pandas/core/dtypes/common.py
+++ pandas/core/dtypes/common.py
@@ -66,32 +66,6 @@ INT64_DTYPE = np.dtype(np.int64)
 _is_scipy_sparse = None
 
 ensure_float64 = algos.ensure_float64
-
-
-def ensure_float(arr):
-    """
-    Ensure that an array object has a float dtype if possible.
-
-    Parameters
-    ----------
-    arr : array-like
-        The array whose data type we want to enforce as float.
-
-    Returns
-    -------
-    float_arr : The original array cast to the float dtype if
-                possible. Otherwise, the original array is returned.
-    """
-    if is_extension_array_dtype(arr.dtype):
-        if is_float_dtype(arr.dtype):
-            arr = arr.to_numpy(dtype=arr.dtype.numpy_dtype, na_value=np.nan)
-        else:
-            arr = arr.to_numpy(dtype="float64", na_value=np.nan)
-    elif issubclass(arr.dtype.type, (np.integer, np.bool_)):
-        arr = arr.astype(float)
-    return arr
-
-
 ensure_int64 = algos.ensure_int64
 ensure_int32 = algos.ensure_int32
 ensure_int16 = algos.ensure_int16
@@ -1735,7 +1709,6 @@ __all__ = [
     "classes",
     "classes_and_not_datetimelike",
     "DT64NS_DTYPE",
-    "ensure_float",
     "ensure_float64",
     "ensure_python_int",
     "ensure_str",
