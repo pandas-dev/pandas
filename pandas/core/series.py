4a0e30ebe39edb4f314fcdd6720883b49c520324
Brock <jbrockmendel@gmail.com>
2023-04-05 07:48:48 -0700

    REF: move from base to Index

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -240,9 +240,10 @@ def _coerce_method(converter):
 # Series class
 
 
-# error: Definition of "max" in base class "IndexOpsMixin" is incompatible with
-# definition in base class "NDFrame"
-# error: Definition of "min" in base class "IndexOpsMixin" is incompatible with
+# error: Cannot override final attribute "ndim" (previously declared in base
+# class "NDFrame")
+# error: Cannot override final attribute "size" (previously declared in base
+# class "NDFrame")
 # definition in base class "NDFrame"
 class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
     """
@@ -5395,10 +5396,8 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
         return result
 
     # error: Cannot determine type of 'isna'
-    # error: Return type "Series" of "isna" incompatible with return type "ndarray
-    # [Any, dtype[bool_]]" in supertype "IndexOpsMixin"
     @doc(NDFrame.isna, klass=_shared_doc_kwargs["klass"])  # type: ignore[has-type]
-    def isna(self) -> Series:  # type: ignore[override]
+    def isna(self) -> Series:
         return NDFrame.isna(self)
 
     # error: Cannot determine type of 'isna'
