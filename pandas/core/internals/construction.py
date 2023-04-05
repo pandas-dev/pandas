6c6a7b1fd4e74c0ff59adf604e1991d1a36fae79
Terji Petersen <terji78@gmail.com>
2023-04-05 16:18:58 +0100

    update according to comments

--- pandas/core/internals/construction.py
+++ pandas/core/internals/construction.py
@@ -312,6 +312,7 @@ def ndarray_to_mgr(
             if (dtype is None or astype_is_view(values.dtype, dtype))
             else False
         )
+        # require order="F" if copy in order to ensure good performance (GH52440)
         values = np.array(values, copy=_copy, order=("F" if _copy else "K"))
         values = _ensure_2d(values)
 
