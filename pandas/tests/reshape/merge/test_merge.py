65baccc1a79604a79c1c59f306328ed77d5bc576
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-04-06 10:34:03 +0200

    Backport PR #52422: BUG: merge with arrow and numpy dtypes raises

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -2735,3 +2735,18 @@ def test_merge_ea_and_non_ea(any_numeric_ea_dtype, join_type):
         }
     )
     tm.assert_frame_equal(result, expected)
+
+
+@pytest.mark.parametrize("dtype", ["int64", "int64[pyarrow]"])
+def test_merge_arrow_and_numpy_dtypes(dtype):
+    # GH#52406
+    pytest.importorskip("pyarrow")
+    df = DataFrame({"a": [1, 2]}, dtype=dtype)
+    df2 = DataFrame({"a": [1, 2]}, dtype="int64[pyarrow]")
+    result = df.merge(df2)
+    expected = df.copy()
+    tm.assert_frame_equal(result, expected)
+
+    result = df2.merge(df)
+    expected = df2.copy()
+    tm.assert_frame_equal(result, expected)
