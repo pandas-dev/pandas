3973be20ed428c063bc8e473c9baca9d78419e85
liang3zy22 <35164941+liang3zy22@users.noreply.github.com>
2023-04-07 00:34:47 +0800

    numpy ravel with dataframe test GH#26247 (#52403)
    Signed-off-by: Liang Yan <ckgppl_yan@sina.cn>

--- pandas/tests/frame/test_npfuncs.py
+++ pandas/tests/frame/test_npfuncs.py
@@ -26,3 +26,47 @@ class TestAsArray:
         assert result.columns is float_frame.columns
 
         tm.assert_frame_equal(result, float_frame.apply(np.sqrt))
+
+    def test_np_ravel(self):
+        # GH26247
+        arr = np.array(
+            [
+                [0.11197053, 0.44361564, -0.92589452],
+                [0.05883648, -0.00948922, -0.26469934],
+            ]
+        )
+
+        result = np.ravel([DataFrame(batch.reshape(1, 3)) for batch in arr])
+        expected = np.array(
+            [
+                0.11197053,
+                0.44361564,
+                -0.92589452,
+                0.05883648,
+                -0.00948922,
+                -0.26469934,
+            ]
+        )
+        tm.assert_numpy_array_equal(result, expected)
+
+        result = np.ravel(DataFrame(arr[0].reshape(1, 3), columns=["x1", "x2", "x3"]))
+        expected = np.array([0.11197053, 0.44361564, -0.92589452])
+        tm.assert_numpy_array_equal(result, expected)
+
+        result = np.ravel(
+            [
+                DataFrame(batch.reshape(1, 3), columns=["x1", "x2", "x3"])
+                for batch in arr
+            ]
+        )
+        expected = np.array(
+            [
+                0.11197053,
+                0.44361564,
+                -0.92589452,
+                0.05883648,
+                -0.00948922,
+                -0.26469934,
+            ]
+        )
+        tm.assert_numpy_array_equal(result, expected)
