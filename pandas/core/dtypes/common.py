eb4920dfd9e084737747bca4929ec1ace6d2e236
Brock <jbrockmendel@gmail.com>
2023-04-07 13:23:49 -0700

    GH ref

--- pandas/core/dtypes/common.py
+++ pandas/core/dtypes/common.py
@@ -469,6 +469,7 @@ def is_categorical_dtype(arr_or_dtype) -> bool:
     >>> is_categorical_dtype(pd.CategoricalIndex([1, 2, 3]))
     True
     """
+    # GH#52527
     warnings.warn(
         "is_categorical_dtype is deprecated and will be removed in a future "
         "version. Use isinstance(dtype, CategoricalDtype) instead",
