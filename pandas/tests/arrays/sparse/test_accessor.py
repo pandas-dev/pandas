efd35433c792e00b1dcd484eba59d4ecb212095c
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-04-06 18:37:33 -0700

    CLN: Assorted (#52454)
    * Remove unneeded dtype
    
    * remove list_future_warnings.sh, consolidate README.md

--- pandas/tests/arrays/sparse/test_accessor.py
+++ pandas/tests/arrays/sparse/test_accessor.py
@@ -29,16 +29,14 @@ class TestSeriesAccessor:
         expected = getattr(arr, attr)
         assert result == expected
 
-    @td.skip_if_no_scipy
     def test_from_coo(self):
-        import scipy.sparse
+        scipy_sparse = pytest.importorskip("scipy.sparse")
 
         row = [0, 3, 1, 0]
         col = [0, 3, 1, 2]
         data = [4, 5, 7, 9]
-        # TODO(scipy#13585): Remove dtype when scipy is fixed
-        # https://github.com/scipy/scipy/issues/13585
-        sp_array = scipy.sparse.coo_matrix((data, (row, col)), dtype="int")
+
+        sp_array = scipy_sparse.coo_matrix((data, (row, col)))
         result = pd.Series.sparse.from_coo(sp_array)
 
         index = pd.MultiIndex.from_arrays(
