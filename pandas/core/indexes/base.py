e1c88ae646baa9b25e265c309a538d1b6c09df04
Daquisu <50253469+Daquisu@users.noreply.github.com>
2023-04-09 18:35:16 +0200

    Simplify convertion to np array for GH#50127

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -553,7 +553,7 @@ class Index(IndexOpsMixin, PandasObject):
                 data = com.asarray_tuplesafe(data, dtype=_dtype_obj)
             elif dtype and dtype.kind == "S":
                 # GH#50127 we update data to a np.array with the correct dtype.
-                data = astype_array(np.array(data, copy=copy), dtype=dtype, copy=copy)
+                data = np.array(data, dtype=object, copy=copy)
                 dtype = _dtype_obj
 
         try:
