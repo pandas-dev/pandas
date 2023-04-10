50d019e7f56885e90a7eb988a087ee29e59d9b2e
Liwei Cai <cai.lw123@gmail.com>
2023-04-10 13:03:42 -0700

    Make `infer_freq` accept `Series` of timezone-aware `Timestamp`s (#52528)
    * Make `infer_freq` accept `Series` of timezone-aware timestamps
    
    * Update pandas/tests/tseries/frequencies/test_inference.py
    
    ---------
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/tests/tseries/frequencies/test_inference.py
+++ pandas/tests/tseries/frequencies/test_inference.py
@@ -236,6 +236,15 @@ def test_infer_freq_tz(tz_naive_fixture, expected, dates):
     assert idx.inferred_freq == expected
 
 
+def test_infer_freq_tz_series(tz_naive_fixture):
+    # infer_freq should work with both tz-naive and tz-aware series. See gh-52456
+    tz = tz_naive_fixture
+    idx = date_range("2021-01-01", "2021-01-04", tz=tz)
+    series = idx.to_series().reset_index(drop=True)
+    inferred_freq = frequencies.infer_freq(series)
+    assert inferred_freq == "D"
+
+
 @pytest.mark.parametrize(
     "date_pair",
     [
