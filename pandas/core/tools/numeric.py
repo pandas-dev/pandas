8d853b818b3934950ff1c7de83ec6b42c8db69c0
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-04-11 13:50:17 -0700

    Remove unnecessary check

--- pandas/core/tools/numeric.py
+++ pandas/core/tools/numeric.py
@@ -275,9 +275,7 @@ def to_numeric(
     # GH33013: for IntegerArray, BooleanArray & FloatingArray need to reconstruct
     # masked array
     if (mask is not None or new_mask is not None) and not is_string_dtype(values.dtype):
-        if mask is None or (
-            mask is not None and new_mask is not None and new_mask.shape == mask.shape
-        ):
+        if mask is None or (new_mask is not None and new_mask.shape == mask.shape):
             # GH 52588
             mask = new_mask
         else:
