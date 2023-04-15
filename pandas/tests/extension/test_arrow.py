7ffe0577e9e01ed25e02181b38b063777cc65ab5
Luke Manley <lukemanley@gmail.com>
2023-04-14 21:00:12 -0400

    whatsnew

--- pandas/tests/extension/test_arrow.py
+++ pandas/tests/extension/test_arrow.py
@@ -2494,7 +2494,7 @@ def test_describe_numeric_data(pa_type):
     "pa_type", tm.DATETIME_PYARROW_DTYPES + tm.TIMEDELTA_PYARROW_DTYPES
 )
 def test_quantile_temporal(pa_type):
-    # GH#####
+    # GH52678
     data = [1, 2, 3]
     ser = pd.Series(data, dtype=ArrowDtype(pa_type))
     result = ser.quantile(0.1)
