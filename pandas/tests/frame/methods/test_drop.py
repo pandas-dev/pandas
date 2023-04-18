5269c1bfb0c488915ca1c894ab0763b9eb47e54e
D067751 <fabrizio.primerano@sap.com>
2023-04-18 16:18:43 +0200

    Fix linting

--- pandas/tests/frame/methods/test_drop.py
+++ pandas/tests/frame/methods/test_drop.py
@@ -539,18 +539,9 @@ class TestDataFrameDrop:
     def test_drop_parse_strings_datetime_index(self):
         # GH #5355
         df = DataFrame(
-            {
-                "a": [1, 2],
-                "b": [1, 2]
-            },
-            index=[pd.Timestamp('2000-01-03'), pd.Timestamp('2000-01-04')]
+            {"a": [1, 2], "b": [1, 2]},
+            index=[Timestamp("2000-01-03"), Timestamp("2000-01-04")],
         )
-        result = df.drop('2000-01-03', axis=0)
-        expected = DataFrame(
-            {
-                "a": [2],
-                "b": [2]
-            },
-            index=[pd.Timestamp('2000-01-04')]
-        )
+        result = df.drop("2000-01-03", axis=0)
+        expected = DataFrame({"a": [2], "b": [2]}, index=[Timestamp("2000-01-04")])
         tm.assert_frame_equal(result, expected)
