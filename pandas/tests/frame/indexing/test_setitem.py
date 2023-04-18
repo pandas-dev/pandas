830e38af68049e68806669c49d1d5820877514f3
Lucas Eckes <eckes.lucas@gmail.com>
2023-04-18 13:44:29 +0000

    Add test to check that columns are added to empty dataframe

--- pandas/tests/frame/indexing/test_setitem.py
+++ pandas/tests/frame/indexing/test_setitem.py
@@ -915,6 +915,16 @@ class TestDataFrameSetItemWithExpansion:
         )
         tm.assert_frame_equal(df, expected)
 
+    def test_frame_setitem_empty_dataframe(self):
+        # GH#28871
+        df = pd.DataFrame({"date":[datetime(2000,1,1)]}).set_index("date")
+        df = df[0:0].copy()
+
+        df["3010"] = None
+        df["2010"] = None
+
+        assert list(df.columns) == ["3010", "2010"]
+
 
 class TestDataFrameSetItemSlicing:
     def test_setitem_slice_position(self):
