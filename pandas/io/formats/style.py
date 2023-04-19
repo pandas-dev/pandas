01352fcf4eff643d549a1aaf4441c7ae7b2e3e58
Nikki van Ommeren <nikki_vanommeren@hotmail.com>
2023-04-19 12:43:14 +0000

    fix formatting

--- pandas/io/formats/style.py
+++ pandas/io/formats/style.py
@@ -2687,7 +2687,7 @@ class Styler(StylerRenderer):
         # Returns a boolean mask indicating where `self.data` has numerical columns.
         # Choosing a mask as opposed to the column names also works for
         # boolean column labels (GH47838).
-        return self.data.columns.isin(self.data.select_dtypes(include=np.number).columns)
+        return self.data.columns.isin(self.data.select_dtypes(include=np.number).columns) 
 
     @doc(
         name="background",
