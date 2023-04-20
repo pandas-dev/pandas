5635f3dfcc63b622a40eda9e362103651d656173
mattkeanny <mattkeanny@gmail.com>
2023-04-20 17:48:37 +0000

    fixed formatting with black

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -3897,7 +3897,9 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
         """
         return selectn.SelectNSeries(self, n=n, keep=keep).nlargest()
 
-    def nsmallest(self, n: int = 5, keep: Literal["first", "last", "all"] = "first") -> Series:
+    def nsmallest(
+        self, n: int = 5, keep: Literal["first", "last", "all"] = "first"
+    ) -> Series:
         """
         Return the smallest `n` elements.
 
