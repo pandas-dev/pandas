381b189e333904d17b715ac30b6a93529bfc0bf3
Valerii <vagechirkov@gmail.com>
2023-04-20 17:15:32 +0200

    Regression tests to cover reset_index issue (#52741)
    * add regression test to cover an issue #38147
    
    * add assert to the tests
    
    * replace Index with RangeIndex

--- pandas/tests/frame/methods/test_reset_index.py
+++ pandas/tests/frame/methods/test_reset_index.py
@@ -773,3 +773,18 @@ def test_errorreset_index_rename(float_frame):
 
     with pytest.raises(IndexError, match="list index out of range"):
         stacked_df.reset_index(names=["new_first"])
+
+
+def test_reset_index_false_index_name():
+    result_series = Series(data=range(5, 10), index=range(0, 5))
+    result_series.index.name = False
+    result_series.reset_index()
+    expected_series = Series(range(5, 10), RangeIndex(range(0, 5), name=False))
+    tm.assert_series_equal(result_series, expected_series)
+
+    # GH 38147
+    result_frame = DataFrame(data=range(5, 10), index=range(0, 5))
+    result_frame.index.name = False
+    result_frame.reset_index()
+    expected_frame = DataFrame(range(5, 10), RangeIndex(range(0, 5), name=False))
+    tm.assert_frame_equal(result_frame, expected_frame)
