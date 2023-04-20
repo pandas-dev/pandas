4ab4b05504f68b8b2fd2608c4c6eec4a6addbf1d
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-04-20 08:58:43 -0700

    Update pandas/tests/resample/test_resample_api.py

--- pandas/tests/resample/test_resample_api.py
+++ pandas/tests/resample/test_resample_api.py
@@ -55,7 +55,7 @@ def test_api():
 def test_groupby_resample_api():
     # GH 12448
     # .groupby(...).resample(...) hitting warnings
-    # whegit an appropriate
+    # when an appropriate
     df = DataFrame(
         {
             "date": date_range(start="2016-01-01", periods=4, freq="W"),
