eae3d123646e0d0c7591e0d422adf65fe19fcb58
Nikolay Boev <44090938+NikolayBoev@users.noreply.github.com>
2023-04-20 19:57:26 +0200

    Changed as requested in #52716 and added test for checking this issue… (#52796)
    * Changed as requested in #52716 and added test for checking this issue #52749
    
    * Changed as requested in #52716 and added test for checking this issue

--- pandas/io/excel/_base.py
+++ pandas/io/excel/_base.py
@@ -119,7 +119,7 @@ header : int, list of int, default 0
 names : array-like, default None
     List of column names to use. If file contains no header row,
     then you should explicitly pass header=None.
-index_col : int, list of int, default None
+index_col : int, str, list of int, default None
     Column (0-indexed) to use as the row labels of the DataFrame.
     Pass None if there is no such column.  If a list is passed,
     those columns will be combined into a ``MultiIndex``.  If a
