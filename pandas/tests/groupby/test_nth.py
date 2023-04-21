343b78b41a61d3038b2a1a4129e131cd7ed69214
ltartaro <lucastartaro@hotmail.com>
2023-04-21 18:45:19 -0300

    #24205: test_groupby_nth_interval created

--- pandas/tests/groupby/test_nth.py
+++ pandas/tests/groupby/test_nth.py
@@ -768,25 +768,27 @@ def test_groupby_nth_with_column_axis():
 
 
 def test_groupby_nth_interval():
-    # GH24205
-    idx_result = pd.MultiIndex(
-        [pd.CategoricalIndex([pd.Interval(0, 1),pd.Interval(1, 2)]), 
+    #24205
+    idx_result = MultiIndex(
+        [
+            pd.CategoricalIndex([pd.Interval(0, 1),pd.Interval(1, 2)]), 
             pd.CategoricalIndex([pd.Interval(0, 10), pd.Interval(10, 20)])
         ], 
-        [[0, 0, 0, 1, 1], 
+        [
+            [0, 0, 0, 1, 1], 
             [0, 1, 1, 0, -1]
         ]
     )
     df_result = pd.DataFrame({'col': range(len(idx_result))}, index=idx_result)
-    
     result = df_result.groupby(level=[0, 1],observed=False).nth(0)
-
     val_expected = [0,1,3]
-    idx_expected = pd.MultiIndex(
-        [pd.CategoricalIndex([pd.Interval(0, 1),pd.Interval(1, 2)]), 
+    idx_expected = MultiIndex(
+        [
+            pd.CategoricalIndex([pd.Interval(0, 1),pd.Interval(1, 2)]), 
             pd.CategoricalIndex([pd.Interval(0, 10), pd.Interval(10, 20)])
         ], 
-        [[0, 0, 1], 
+        [
+            [0, 0, 1], 
             [0, 1, 0]
         ]
     )
