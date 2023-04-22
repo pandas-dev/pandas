884b606dc9c4a8b2534aa44cab69e8d5ff709265
Luke Manley <lukemanley@gmail.com>
2023-04-21 21:26:57 -0400

    arrow duration overflowing on arithmetic ops

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -258,7 +258,13 @@ class ArrowExtensionArray(
                 scalars = pa.array(scalars, from_pandas=True)
         if pa_dtype and scalars.type != pa_dtype:
             scalars = scalars.cast(pa_dtype)
-        return cls(scalars)
+        arr = cls(scalars)
+        if pa.types.is_duration(scalars.type) and scalars.null_count > 0:
+            # GH#####: upstream bug for duration types when originally
+            # constructed with data containing numpy NaT.
+            # https://github.com/apache/arrow/issues/35088
+            arr = arr.fillna(arr.dtype.na_value)
+        return arr
 
     @classmethod
     def _from_sequence_of_strings(
--- pandas/tests/extension/test_arrow.py
+++ pandas/tests/extension/test_arrow.py
@@ -2830,3 +2830,14 @@ def test_date32_repr():
     arrow_dt = pa.array([date.fromisoformat("2020-01-01")], type=pa.date32())
     ser = pd.Series(arrow_dt, dtype=ArrowDtype(arrow_dt.type))
     assert repr(ser) == "0   2020-01-01\ndtype: date32[day][pyarrow]"
+
+
+def test_duration_overflow_from_ndarray_containing_nat():
+    # GH####
+    data_ts = pd.to_datetime([1, None])
+    data_td = pd.to_timedelta([1, None])
+    ser_ts = pd.Series(data_ts, dtype=ArrowDtype(pa.timestamp("ns")))
+    ser_td = pd.Series(data_td, dtype=ArrowDtype(pa.duration("ns")))
+    result = ser_ts + ser_td
+    expected = pd.Series([2, None], dtype=ArrowDtype(pa.timestamp("ns")))
+    tm.assert_series_equal(result, expected)
