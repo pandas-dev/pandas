68aeec65b7f6b533c2dd3557c48e0aa34efb3dce
lzieglerpenn <lee.ziegler@penn-interactive.com>
2023-04-26 18:57:02 -0400

    add test for new option

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -1104,6 +1104,17 @@ class _TestSQLApi(PandasSQLTest):
 
         assert num_rows == num_entries
 
+    def test_to_sql_truncate(self, test_frame1):
+        sql.to_sql(test_frame1, "test_frame3", self.conn, if_exists="fail")
+        # Add to table again
+        sql.to_sql(test_frame1, "test_frame3", self.conn, if_exists="truncate")
+        assert sql.has_table("test_frame3", self.conn)
+
+        num_entries = len(test_frame1)
+        num_rows = count_rows(self.conn, "test_frame3")
+
+        assert num_rows == num_entries
+
     def test_to_sql_append(self, test_frame1):
         assert sql.to_sql(test_frame1, "test_frame4", self.conn, if_exists="fail") == 4
 
