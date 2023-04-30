7d84878b84dd7f1ce346af783987eaf721185647
shteken <barshteken@gmail.com>
2023-04-30 11:03:31 +0200

    add test_multiindex_with_na

--- pandas/tests/test_multilevel.py
+++ pandas/tests/test_multilevel.py
@@ -290,6 +290,22 @@ class TestMultiLevel:
 
         tm.assert_frame_equal(result, expected)
 
+    def test_multiindex_with_na(self):
+        df = DataFrame(
+            [
+                ["A", np.nan, 1.23, 4.56],
+                ["A", "G", 1.23, 4.56],
+                ["A", "D", 9.87, 10.54],
+            ],
+            columns=["pivot_0", "pivot_1", "col_1", "col_2"],
+        )
+        df.set_index(["pivot_0", "pivot_1"], inplace=True)
+
+        df.at[("A", "F"), "col_2"] = 0.0
+
+        assert df.loc[("A", "F")]["col_2"] == 0.0
+        assert pd.isna(df.loc[("A", "F")]["col_1"])
+
 
 class TestSorted:
     """everything you wanted to test about sorting"""
