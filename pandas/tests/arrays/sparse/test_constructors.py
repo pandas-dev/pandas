4a9003c1a8a120661aee7f820fc3d97289cffeca
jbrockmendel <jbrockmendel@gmail.com>
2023-05-02 12:41:36 -0700

    DEPR: SparseArray(scalar) (#53039)
    * DEPR: SparseArray(scalar)
    
    * Update doc/source/whatsnew/v2.1.0.rst
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/tests/arrays/sparse/test_constructors.py
+++ pandas/tests/arrays/sparse/test_constructors.py
@@ -145,13 +145,16 @@ class TestConstructors:
     @pytest.mark.parametrize("sparse_index", [None, IntIndex(1, [0])])
     def test_constructor_spindex_dtype_scalar(self, sparse_index):
         # scalar input
-        arr = SparseArray(data=1, sparse_index=sparse_index, dtype=None)
+        msg = "Constructing SparseArray with scalar data is deprecated"
+        with tm.assert_produces_warning(FutureWarning, match=msg):
+            arr = SparseArray(data=1, sparse_index=sparse_index, dtype=None)
         exp = SparseArray([1], dtype=None)
         tm.assert_sp_array_equal(arr, exp)
         assert arr.dtype == SparseDtype(np.int64)
         assert arr.fill_value == 0
 
-        arr = SparseArray(data=1, sparse_index=IntIndex(1, [0]), dtype=None)
+        with tm.assert_produces_warning(FutureWarning, match=msg):
+            arr = SparseArray(data=1, sparse_index=IntIndex(1, [0]), dtype=None)
         exp = SparseArray([1], dtype=None)
         tm.assert_sp_array_equal(arr, exp)
         assert arr.dtype == SparseDtype(np.int64)
