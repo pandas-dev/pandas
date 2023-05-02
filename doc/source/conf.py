d0aea289231a04f1a97b1f78da0696ebfdf253cc
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-05-02 10:15:11 -0700

    Change back to path

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -244,7 +244,7 @@ html_theme_options = {
     "logo": {"image_dark": "https://pandas.pydata.org/static/img/pandas_white.svg"},
     "navbar_end": ["version-switcher", "theme-switcher", "navbar-icon-links"],
     "switcher": {
-        "json_url": "https://pandas.pydata.org/docs/_static/version_switcher.json",
+        "json_url": "/versions.json",
         "version_match": switcher_version,
     },
     "icon_links": [
