4ff2debc182df65a2f4abfc4ec9bf5a658b59608
Snorf Yang <68773271+snorfyang@users.noreply.github.com>
2023-05-03 04:56:53 +0800

    STYLE: upgrade ruff (#52974)
    * Bug: fix pre-commit problems
    
    * fix some errors
    
    * pre commit issues passed
    
    * Delete =1.5
    
    * pytlint and alignment issue
    
    * fix errors
    
    * fix aligned and update ruff
    
    * Update pandas/core/series.py
    
    ---------
    
    Co-authored-by: yusharth <yusharthsingh@gmail.com>
    Co-authored-by: Yusharth Singh <57184245+yusharth@users.noreply.github.com>
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- pandas/tests/groupby/test_counting.py
+++ pandas/tests/groupby/test_counting.py
@@ -257,10 +257,10 @@ class TestCounting:
 
 def test_groupby_timedelta_cython_count():
     df = DataFrame(
-        {"g": list("ab" * 2), "delt": np.arange(4).astype("timedelta64[ns]")}
+        {"g": list("ab" * 2), "delta": np.arange(4).astype("timedelta64[ns]")}
     )
-    expected = Series([2, 2], index=Index(["a", "b"], name="g"), name="delt")
-    result = df.groupby("g").delt.count()
+    expected = Series([2, 2], index=Index(["a", "b"], name="g"), name="delta")
+    result = df.groupby("g").delta.count()
     tm.assert_series_equal(expected, result)
 
 
--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -843,7 +843,13 @@ class TestReaders:
     def test_missing_file_raises(self, read_ext):
         bad_file = f"foo{read_ext}"
         # CI tests with other languages, translates to "No such file or directory"
-        match = r"(No such file or directory|没有那个文件或目录|File o directory non esistente)"
+        match = "|".join(
+            [
+                "(No such file or directory",
+                "没有那个文件或目录",
+                "File o directory non esistente)",
+            ]
+        )
         with pytest.raises(FileNotFoundError, match=match):
             pd.read_excel(bad_file)
 
