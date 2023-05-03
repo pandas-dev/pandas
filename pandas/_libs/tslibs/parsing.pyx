2720863350c561fd570dbf60317199bdc6332c10
jbrockmendel <jbrockmendel@gmail.com>
2023-05-03 08:51:59 -0700

    REF: lazify relativedelta imports (#52659)
    * REF: lazify relativedelta imports
    
    * API: intentionally raise ValueError
    
    * whatsnew

--- pandas/_libs/tslibs/parsing.pyx
+++ pandas/_libs/tslibs/parsing.pyx
@@ -46,7 +46,6 @@ from dateutil.parser import (
     DEFAULTPARSER,
     parse as du_parse,
 )
-from dateutil.relativedelta import relativedelta
 from dateutil.tz import (
     tzlocal as _dateutil_tzlocal,
     tzoffset,
@@ -692,7 +691,11 @@ cdef datetime dateutil_parse(
         ) from err
 
     if res.weekday is not None and not res.day:
-        ret = ret + relativedelta.relativedelta(weekday=res.weekday)
+        # GH#52659
+        raise ValueError(
+            "Parsing datetimes with weekday but no day information is "
+            "not supported"
+        )
     if not ignoretz:
         if res.tzname and res.tzname in time.tzname:
             # GH#50791
