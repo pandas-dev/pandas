4078889a44dec8093f2ddc0233faf2e745648cce
Noy Hanan <noyhanan85@gmail.com>
2023-05-04 17:32:20 +0300

    Added test for sort_index parameter multiindex 'sort_remaining' = False

--- pandas/tests/frame/methods/test_sort_index.py
+++ pandas/tests/frame/methods/test_sort_index.py
@@ -914,3 +914,22 @@ class TestDataFrameSortIndexKey:
         expected = df.copy()
         result = df.sort_index(level=[0, 1], na_position="last")
         tm.assert_frame_equal(result, expected)
+
+    @pytest.mark.parametrize("ascending", [True, False])
+    def test_sort_index_multiindex_sort_remaining(self, ascending):
+        # GH #24247, testing sort_remaining=false consistent behavior for ascending = True, ascending = False
+        df = pd.DataFrame({'A': [1, 2, 3, 4, 5], 'B': [10, 20, 30, 40, 50]},
+                          index=pd.MultiIndex.from_tuples([('a', 'x'), ('a', 'y'), ('b', 'x'), ('b', 'y'), ('c', 'x')]))
+        df_sorted = df.sort_index(level=1, sort_remaining=False, ascending=ascending)
+
+        if ascending:
+            df_expected = pd.DataFrame({'A': [1, 3, 5, 2, 4], 'B': [10, 30, 50, 20, 40]},
+                                       index=pd.MultiIndex.from_tuples(
+                                       [('a', 'x'), ('b', 'x'), ('c', 'x'), ('a', 'y'), ('b', 'y')]))
+        else:
+            df_expected = pd.DataFrame({'A': [2, 4, 1, 3, 5], 'B': [20, 40, 10, 30, 50]},
+                                       index=pd.MultiIndex.from_tuples(
+                                           [('a', 'y'), ('b', 'y'), ('a', 'x'), ('b', 'x'), ('c', 'x')]))
+
+        # Assert that the resulting DataFrame is sorted correctly
+        tm.assert_frame_equal(df_sorted, df_expected)
