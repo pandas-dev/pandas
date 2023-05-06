8a3449af1c54f15ded2ead7acdd747b92ec9e743
Natalia Mokeeva <natalia.mokeeva.de@gmail.com>
2023-05-06 17:19:05 +0200

    Revert "rename data"
    This reverts commit 533d841b7417dd6fb0655b2899b0af1c140c91c4.

--- pandas/core/construction.py
+++ pandas/core/construction.py
@@ -502,12 +502,12 @@ def sanitize_masked_array(data: ma.MaskedArray) -> np.ndarray:
     if mask.any():
         dtype, fill_value = maybe_promote(data.dtype, np.nan)
         dtype = cast(np.dtype, dtype)
-        sanitized_ma = data.astype(dtype, copy=True)
-        sanitized_ma.soften_mask()  # set hardmask False if it was True
-        sanitized_ma[mask] = fill_value
-        return sanitized_ma
+        data = ma.asarray(data.astype(dtype, copy=True))
+        data.soften_mask()  # set hardmask False if it was True
+        data[mask] = fill_value
     else:
-        return data.copy()
+        data = data.copy()
+    return data
 
 
 def sanitize_array(
