91287692679cd80dde5bd73669ce02ed8e47d703
MarcoGorelli <>
2023-05-07 09:04:00 +0100

    simplify

--- pandas/_testing/contexts.py
+++ pandas/_testing/contexts.py
@@ -124,13 +124,10 @@ def ensure_clean(
     path.touch()
 
     handle_or_str: str | IO = str(path)
+    encoding = kwargs.pop("encoding", None)
     if return_filelike:
         kwargs.setdefault("mode", "w+b")
-        handle_or_str = open(
-            path,
-            encoding=kwargs.get("encoding", None),
-            **{key: value for key, value in kwargs.items() if key != "encoding"},
-        )
+        handle_or_str = open(path, encoding=encoding, **kwargs)
 
     try:
         yield handle_or_str
