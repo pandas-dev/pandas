60bc50c0368fe7ba1f230b25ba415e85831eee23
Sylvain Marié <sylvain.marie@schneider-electric.com>
2023-05-07 12:01:11 +0200

    [READY] Improved performance of `Period`'s default formatter (`period_format`) (#51459)
    * Improved performance of default period formatting (`period_format`). Added corresponding ASVs
    
    * Improved ASV for period frames and datetimes
    
    * What's new
    
    * Update asv_bench/benchmarks/strftime.py
    
    * Fixed whats new backticks
    
    * Completed whatsnew
    
    * Added ASVs for to_csv for period
    
    * Aligned the namings
    
    * Completed Whats new
    
    * Added a docstring explaining why the ASV bench with custom date format was improved: the date_format parameter is not taken into account today.
    
    * Moved whatsnew to 2.0.0
    
    * Moved whatsnew to 2.1
    
    * Improved docstring as per code review
    
    * Renamed asv params as per code review
    
    * Fixed ASV comment as per code review
    
    * ASV: renamed parameters as per code review
    
    * Improved `period_format`: now the performance is the same when no format is provided and if an explicit `fmt` is provided matching the actual default format for this `freq`
    
    * Code review: Improved strftime ASV: set_index is now in the setup
    
    * Removed useless main
    
    * Removed wrong code
    
    * Improved ASVs for period formatting: now there is a "default explicit" everywhere
    
    * Update pandas/_libs/tslibs/period.pyx
    
    * Update pandas/_libs/tslibs/period.pyx
    
    * Update pandas/_libs/tslibs/period.pyx
    
    * Minor refactoring to avoid retesting for none several time
    
    * Fixed issue: bool does not exist, using bint
    
    * Added missing quarter variable as cdef
    
    * Fixed asv bug
    
    * Code review: fixed docstring
    
    * Update doc/source/whatsnew/v2.1.0.rst
    
    * fixup
    
    ---------
    
    Co-authored-by: Sylvain MARIE <sylvain.marie@se.com>
    Co-authored-by: Marco Edward Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
    Co-authored-by: MarcoGorelli <>

--- pandas/_libs/tslibs/period.pyx
+++ pandas/_libs/tslibs/period.pyx
@@ -1155,46 +1155,81 @@ cdef int64_t period_ordinal_to_dt64(int64_t ordinal, int freq) except? -1:
 
 
 cdef str period_format(int64_t value, int freq, object fmt=None):
+
     cdef:
-        int freq_group
+        int freq_group, quarter
+        npy_datetimestruct dts
+        bint is_fmt_none
 
     if value == NPY_NAT:
         return "NaT"
 
-    if isinstance(fmt, str):
-        # Encode using current locale, in case fmt contains non-utf8 chars
-        fmt = <bytes>util.string_encode_locale(fmt)
+    # fill dts and freq group
+    get_date_info(value, freq, &dts)
+    freq_group = get_freq_group(freq)
 
-    if fmt is None:
-        freq_group = get_freq_group(freq)
-        if freq_group == FR_ANN:
-            fmt = b"%Y"
-        elif freq_group == FR_QTR:
-            fmt = b"%FQ%q"
-        elif freq_group == FR_MTH:
-            fmt = b"%Y-%m"
-        elif freq_group == FR_WK:
-            left = period_asfreq(value, freq, FR_DAY, 0)
-            right = period_asfreq(value, freq, FR_DAY, 1)
-            return f"{period_format(left, FR_DAY)}/{period_format(right, FR_DAY)}"
-        elif freq_group == FR_BUS or freq_group == FR_DAY:
-            fmt = b"%Y-%m-%d"
-        elif freq_group == FR_HR:
-            fmt = b"%Y-%m-%d %H:00"
-        elif freq_group == FR_MIN:
-            fmt = b"%Y-%m-%d %H:%M"
-        elif freq_group == FR_SEC:
-            fmt = b"%Y-%m-%d %H:%M:%S"
-        elif freq_group == FR_MS:
-            fmt = b"%Y-%m-%d %H:%M:%S.%l"
-        elif freq_group == FR_US:
-            fmt = b"%Y-%m-%d %H:%M:%S.%u"
-        elif freq_group == FR_NS:
-            fmt = b"%Y-%m-%d %H:%M:%S.%n"
-        else:
-            raise ValueError(f"Unknown freq: {freq}")
+    # use the appropriate default format depending on frequency group
+    is_fmt_none = fmt is None
+    if freq_group == FR_ANN and (is_fmt_none or fmt == "%Y"):
+        return f"{dts.year}"
 
-    return _period_strftime(value, freq, fmt)
+    elif freq_group == FR_QTR and (is_fmt_none or fmt == "%FQ%q"):
+        # get quarter and modify dts.year to be the 'Fiscal' year
+        quarter = get_yq(value, freq, &dts)
+        return f"{dts.year}Q{quarter}"
+
+    elif freq_group == FR_MTH and (is_fmt_none or fmt == "%Y-%m"):
+        return f"{dts.year}-{dts.month:02d}"
+
+    elif freq_group == FR_WK and is_fmt_none:
+        # special: start_date/end_date. Recurse
+        left = period_asfreq(value, freq, FR_DAY, 0)
+        right = period_asfreq(value, freq, FR_DAY, 1)
+        return f"{period_format(left, FR_DAY)}/{period_format(right, FR_DAY)}"
+
+    elif (
+        (freq_group == FR_BUS or freq_group == FR_DAY)
+        and (is_fmt_none or fmt == "%Y-%m-%d")
+    ):
+        return f"{dts.year}-{dts.month:02d}-{dts.day:02d}"
+
+    elif freq_group == FR_HR and (is_fmt_none or fmt == "%Y-%m-%d %H:00"):
+        return f"{dts.year}-{dts.month:02d}-{dts.day:02d} {dts.hour:02d}:00"
+
+    elif freq_group == FR_MIN and (is_fmt_none or fmt == "%Y-%m-%d %H:%M"):
+        return (f"{dts.year}-{dts.month:02d}-{dts.day:02d} "
+                f"{dts.hour:02d}:{dts.min:02d}")
+
+    elif freq_group == FR_SEC and (is_fmt_none or fmt == "%Y-%m-%d %H:%M:%S"):
+        return (f"{dts.year}-{dts.month:02d}-{dts.day:02d} "
+                f"{dts.hour:02d}:{dts.min:02d}:{dts.sec:02d}")
+
+    elif freq_group == FR_MS and (is_fmt_none or fmt == "%Y-%m-%d %H:%M:%S.%l"):
+        return (f"{dts.year}-{dts.month:02d}-{dts.day:02d} "
+                f"{dts.hour:02d}:{dts.min:02d}:{dts.sec:02d}"
+                f".{(dts.us // 1_000):03d}")
+
+    elif freq_group == FR_US and (is_fmt_none or fmt == "%Y-%m-%d %H:%M:%S.%u"):
+        return (f"{dts.year}-{dts.month:02d}-{dts.day:02d} "
+                f"{dts.hour:02d}:{dts.min:02d}:{dts.sec:02d}"
+                f".{(dts.us):06d}")
+
+    elif freq_group == FR_NS and (is_fmt_none or fmt == "%Y-%m-%d %H:%M:%S.%n"):
+        return (f"{dts.year}-{dts.month:02d}-{dts.day:02d} "
+                f"{dts.hour:02d}:{dts.min:02d}:{dts.sec:02d}"
+                f".{((dts.us * 1000) + (dts.ps // 1000)):09d}")
+
+    elif is_fmt_none:
+        # `freq_group` is invalid, raise
+        raise ValueError(f"Unknown freq: {freq}")
+
+    else:
+        # A custom format is requested
+        if isinstance(fmt, str):
+            # Encode using current locale, in case fmt contains non-utf8 chars
+            fmt = <bytes>util.string_encode_locale(fmt)
+
+        return _period_strftime(value, freq, fmt, dts)
 
 
 cdef list extra_fmts = [(b"%q", b"^`AB`^"),
@@ -1207,18 +1242,15 @@ cdef list extra_fmts = [(b"%q", b"^`AB`^"),
 cdef list str_extra_fmts = ["^`AB`^", "^`CD`^", "^`EF`^",
                             "^`GH`^", "^`IJ`^", "^`KL`^"]
 
-cdef str _period_strftime(int64_t value, int freq, bytes fmt):
+cdef str _period_strftime(int64_t value, int freq, bytes fmt, npy_datetimestruct dts):
     cdef:
         Py_ssize_t i
-        npy_datetimestruct dts
         char *formatted
         bytes pat, brepl
         list found_pat = [False] * len(extra_fmts)
         int quarter
         int32_t us, ps
         str result, repl
-
-    get_date_info(value, freq, &dts)
 
     # Find our additional directives in the pattern and replace them with
     # placeholders that are not processed by c_strftime
