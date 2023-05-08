1c14a3f9732253bf5841ecfbec02464dd2272740
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-05-08 11:46:22 +0200

    CoW: Delay copy when enlarging df with loc (#52062)

--- pandas/tests/copy_view/test_indexing.py
+++ pandas/tests/copy_view/test_indexing.py
@@ -1073,3 +1073,19 @@ def test_series_midx_tuples_slice(using_copy_on_write):
             index=pd.MultiIndex.from_tuples([((1, 2), 3), ((1, 2), 4), ((2, 3), 4)]),
         )
         tm.assert_series_equal(ser, expected)
+
+
+def test_loc_enlarging_with_dataframe(using_copy_on_write):
+    df = DataFrame({"a": [1, 2, 3]})
+    rhs = DataFrame({"b": [1, 2, 3], "c": [4, 5, 6]})
+    rhs_orig = rhs.copy()
+    df.loc[:, ["b", "c"]] = rhs
+    if using_copy_on_write:
+        assert np.shares_memory(get_array(df, "b"), get_array(rhs, "b"))
+        assert np.shares_memory(get_array(df, "c"), get_array(rhs, "c"))
+        assert not df._mgr._has_no_reference(1)
+    else:
+        assert not np.shares_memory(get_array(df, "b"), get_array(rhs, "b"))
+
+    df.iloc[0, 1] = 100
+    tm.assert_frame_equal(rhs, rhs_orig)
