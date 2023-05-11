44b88eb994f3b24b34052923b9045cd3f54dc017
jbrockmendel <jbrockmendel@gmail.com>
2023-05-11 13:24:01 -0700

    DEPR: require SparseDtype.fill_value be compatible with SparseDtype.subtype (#53043)
    * DEPR: require SparseDtype.fill_value be compatible with SparseDtype.subtype
    
    * filter more specific

--- pandas/tests/arrays/sparse/test_array.py
+++ pandas/tests/arrays/sparse/test_array.py
@@ -52,33 +52,21 @@ class TestSparseArray:
         arr.fill_value = 2
         assert arr.fill_value == 2
 
-        # TODO: this seems fine? You can construct an integer
-        # sparsearray with NaN fill value, why not update one?
-        # coerces to int
-        # msg = "unable to set fill_value 3\\.1 to int64 dtype"
-        # with pytest.raises(ValueError, match=msg):
-        arr.fill_value = 3.1
+        msg = "Allowing arbitrary scalar fill_value in SparseDtype is deprecated"
+        with tm.assert_produces_warning(FutureWarning, match=msg):
+            arr.fill_value = 3.1
         assert arr.fill_value == 3.1
 
-        # msg = "unable to set fill_value nan to int64 dtype"
-        # with pytest.raises(ValueError, match=msg):
         arr.fill_value = np.nan
         assert np.isnan(arr.fill_value)
 
         arr = SparseArray([True, False, True], fill_value=False, dtype=np.bool_)
         arr.fill_value = True
-        assert arr.fill_value
+        assert arr.fill_value is True
 
-        # FIXME: don't leave commented-out
-        # coerces to bool
-        # TODO: we can construct an sparse array of bool
-        #      type and use as fill_value any value
-        # msg = "fill_value must be True, False or nan"
-        # with pytest.raises(ValueError, match=msg):
-        #    arr.fill_value = 0
+        with tm.assert_produces_warning(FutureWarning, match=msg):
+            arr.fill_value = 0
 
-        # msg = "unable to set fill_value nan to bool dtype"
-        # with pytest.raises(ValueError, match=msg):
         arr.fill_value = np.nan
         assert np.isnan(arr.fill_value)
 
