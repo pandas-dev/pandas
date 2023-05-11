6a73ec510b04af64b443d72c66d84411a468c768
steliospetrakis02 <t8200139@aueb.gr>
2023-05-12 00:53:17 +0300

    Add one more parameter to signarures seems to solve the issue

--- asv_bench/benchmarks/indexing.py
+++ asv_bench/benchmarks/indexing.py
@@ -540,7 +540,7 @@ class Block:
         (np.array(True), "np.array(True)"),
     ]
 
-    def setup(self, true_value):
+    def setup(self, true_value, name):
         self.df = DataFrame(
             False,
             columns=np.arange(500).astype(str),
@@ -549,8 +549,7 @@ class Block:
 
         self.true_value = true_value
 
-    def time_test(self, true_value):
-        """Test time for assigning a slice `True` and `np.array(True)`"""
+    def time_test(self, true_value, name):
         start = datetime(2010, 5, 1)
         end = datetime(2010, 9, 1)
         self.df.loc[start:end, :] = true_value
