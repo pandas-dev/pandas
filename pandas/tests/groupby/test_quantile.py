ba2b57f6d55da9417b02c5325afa3058c5bc8543
Yao Xiao <108576690+Charlie-XIAO@users.noreply.github.com>
2023-05-12 13:20:30 -0400

    BUG: `GroupBy.quantile` implicitly sorts `index.levels` (#53049)
    * fixed groupby quantile index.levels
    
    * changelog added
    
    * updated changelog
    
    * modified test to test whole frame
    - not sure but I think it is not checking index levels like this
    
    * updated tests to check index levels separately
    
    * avoid performance regression when not multiindex
    
    * tried to combine code blocks

--- pandas/tests/groupby/test_quantile.py
+++ pandas/tests/groupby/test_quantile.py
@@ -471,3 +471,33 @@ def test_groupby_quantile_dt64tz_period():
     expected.index = expected.index.astype(np.int_)
 
     tm.assert_frame_equal(result, expected)
+
+
+def test_groupby_quantile_nonmulti_levels_order():
+    # Non-regression test for GH #53009
+    ind = pd.MultiIndex.from_tuples(
+        [
+            (0, "a", "B"),
+            (0, "a", "A"),
+            (0, "b", "B"),
+            (0, "b", "A"),
+            (1, "a", "B"),
+            (1, "a", "A"),
+            (1, "b", "B"),
+            (1, "b", "A"),
+        ],
+        names=["sample", "cat0", "cat1"],
+    )
+    ser = pd.Series(range(8), index=ind)
+    result = ser.groupby(level="cat1", sort=False).quantile([0.2, 0.8])
+
+    qind = pd.MultiIndex.from_tuples(
+        [("B", 0.2), ("B", 0.8), ("A", 0.2), ("A", 0.8)], names=["cat1", None]
+    )
+    expected = pd.Series([1.2, 4.8, 2.2, 5.8], index=qind)
+
+    tm.assert_series_equal(result, expected)
+
+    # We need to check that index levels are not sorted
+    expected_levels = pd.core.indexes.frozen.FrozenList([["B", "A"], [0.2, 0.8]])
+    tm.assert_equal(result.index.levels, expected_levels)
