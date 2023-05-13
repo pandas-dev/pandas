d81747ba711c6233a4e358e8050d443466d74f07
maikia <maria.telenczuk@owkin.com>
2023-05-13 15:47:39 +0200

    update example

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -1221,62 +1221,40 @@ class SeriesGroupBy(GroupBy[Series]):
         """
         Return unique values of Series object.
 
-        Uniques are returned in order of appearance. Hash table-based unique,
+        TODO Uniques are returned in order of appearance. Hash table-based unique,
         therefore does NOT sort.
 
         Returns
         -------
-        ndarray or ExtensionArray
-            The unique values returned as a NumPy array. See Notes.
+        Series
+            TODO The unique values returned as a NumPy array. See Notes.
 
         See Also
         --------
-        Series.drop_duplicates : Return Series with duplicate values removed.
-        unique : Top-level unique method for any 1-d array-like object.
-        Index.unique : Return Index with unique values from an Index object.
-
-        Notes
-        -----
-        Returns the unique values as a NumPy array. In case of an
-        extension-array backed Series, a new ExtensionArray of that type with
-        just the unique values is returned. This includes
-
-        - Categorical
-        - Period
-        - Datetime with Timezone
-        - Datetime without Timezone
-        - Timedelta
-        - Interval
-        - Sparse
-        - IntegerNA
-
-        See Examples section.
+        Series.unique : TODO Return Series with duplicate values removed.
 
         Examples
         --------
-        >>> pd.Series([2, 1, 3, 3], name='A').unique()
-        array([2, 1, 3])
-
-        >>> pd.Series([pd.Timestamp('2016-01-01') for _ in range(3)]).unique()
-        <DatetimeArray>
-        ['2016-01-01 00:00:00']
-        Length: 1, dtype: datetime64[ns]
-
-        >>> pd.Series([pd.Timestamp('2016-01-01', tz='US/Eastern')
-        ...            for _ in range(3)]).unique()
-        <DatetimeArray>
-        ['2016-01-01 00:00:00-05:00']
-        Length: 1, dtype: datetime64[ns, US/Eastern]
-
-        An Categorical will return categories in the order of appearance and with the same dtype.
-
-        >>> pd.Series(pd.Categorical(list('baabc'))).unique()
-        ['b', 'a', 'c']
-        Categories (3, object): ['a', 'b', 'c']
-        >>> pd.Series(pd.Categorical(list('baabc'), categories=list('abc'),
-        ...                          ordered=True)).unique()
-        ['b', 'a', 'c']
-        Categories (3, object): ['a' < 'b' < 'c']
+        >>> df = pd.DataFrame([('Chihuahua', 'dog', 6.1),
+        ...                    ('Beagle', 'dog', 15.2),
+        ...                    ('Chihuahua', 'dog', 6.9),
+        ...                    ('Persian', 'cat', 9.2),
+        ...                    ('Chihuahua', 'dog', 7),
+        ...                    ('Persian', 'cat', 8.8)],
+        ...                   columns=['breed', 'animal', 'height_in'])
+        >>> df
+               breed     animal   height_in
+        0  Chihuahua        dog         6.1
+        1     Beagle        dog        15.2
+        2  Chihuahua        dog         6.9
+        3    Persian        cat         9.2
+        4  Chihuahua        dog         7.0
+        5    Persian        cat         8.8
+        >>> ser = df.groupby('animal')['breed'].unique()
+        animal
+        cat              [Persian]
+        dog    [Chihuahua, Beagle]
+        Name: breed, dtype: object
         """
         result = self._op_via_apply("unique")
         return result
