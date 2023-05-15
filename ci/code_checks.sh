f761435e804205b77ae362a4f7ba07fed579cd87
Dea María Léon <deamarialeon@gmail.com>
2023-05-15 16:46:45 +0200

    DOC Fixing EX01 - added examples (#53181)
    * DOC Fixing EX01 - added examples
    
    * Removed Index.item from code_checks
    
    * Removed #GH13
    
    * Added case for return value=False
    
    * Added False case for is_unique
    
    * Correct docstring
    
    * really minor clarification (there may be multiple groups, so "the group" isnt defined)
    
    ---------
    
    Co-authored-by: MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -80,12 +80,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
 
     MSG='Partially validate docstrings (EX01)' ;  echo $MSG
     $BASE_DIR/scripts/validate_docstrings.py --format=actions --errors=EX01 --ignore_functions \
-        pandas.Series.item \
-        pandas.Series.pipe \
-        pandas.Series.mode \
-        pandas.Series.is_unique \
-        pandas.Series.is_monotonic_increasing \
-        pandas.Series.is_monotonic_decreasing \
         pandas.Series.backfill \
         pandas.Series.bfill \
         pandas.Series.ffill \
@@ -319,7 +313,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         pandas.Index.fillna \
         pandas.Index.dropna \
         pandas.Index.astype \
-        pandas.Index.item \
         pandas.Index.map \
         pandas.Index.ravel \
         pandas.Index.to_list \
@@ -462,8 +455,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         pandas.core.groupby.SeriesGroupBy.cumsum \
         pandas.core.groupby.SeriesGroupBy.diff \
         pandas.core.groupby.SeriesGroupBy.ffill \
-        pandas.core.groupby.SeriesGroupBy.is_monotonic_increasing \
-        pandas.core.groupby.SeriesGroupBy.is_monotonic_decreasing \
         pandas.core.groupby.SeriesGroupBy.max \
         pandas.core.groupby.SeriesGroupBy.median \
         pandas.core.groupby.SeriesGroupBy.min \
