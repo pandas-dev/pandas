9413561adefb6837df876ea8352cbd2be0be06bf
Dea María Léon <deamarialeon@gmail.com>
2023-05-16 19:03:06 +0200

    DOC: Fixing EX01 - added examples  (#53256)
    * Added examples
    
    * Update pandas/core/generic.py
    
    ---------
    
    Co-authored-by: Marco Edward Gorelli <marcogorelli@protonmail.com>

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -11870,6 +11870,29 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         -----
         If all elements are non-NA/null, returns None.
         Also returns None for empty {klass}.
+
+        Examples
+        --------
+        For Series:
+
+        >>> s = pd.Series([None, 3, 4])
+        >>> s.first_valid_index()
+        1
+        >>> s.last_valid_index()
+        2
+
+        For DataFrame:
+
+        >>> df = pd.DataFrame({{'A': [None, None, 2], 'B': [None, 3, 4]}})
+        >>> df
+             A      B
+        0  NaN    NaN
+        1  NaN    3.0
+        2  2.0    4.0
+        >>> df.first_valid_index()
+        1
+        >>> df.last_valid_index()
+        2
         """
         return self._find_valid_index(how="first")
 
