180536a03bbeefdabacb2a7db2a53215bb39d2c1
Simon Høxbro Hansen <simon.hansen@me.com>
2023-05-17 18:45:32 +0200

    Backport PR #53175: Add `np.intc` to `_factorizers` in `pd.merge`

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -123,6 +123,10 @@ _factorizers = {
     np.object_: libhashtable.ObjectFactorizer,
 }
 
+# See https://github.com/pandas-dev/pandas/issues/52451
+if np.intc is not np.int32:
+    _factorizers[np.intc] = libhashtable.Int64Factorizer
+
 
 @Substitution("\nleft : DataFrame or named Series")
 @Appender(_merge_doc, indents=0)
