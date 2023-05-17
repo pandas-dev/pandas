2f506882ab893f8ae32f27fe8da8070c4f928f49
Simon Høxbro Hansen <simon.hansen@me.com>
2023-05-17 18:45:32 +0200

    Add `np.intc` to `_factorizers` in `pd.merge`  (#53175)
    * Add np.intc to merge table
    
    * Add whatsnew
    
    * Updated unit test
    
    * Fix lint
    
    * Only add intc to _factorizers if not int32
    
    * Mention that the problem is on Windows
    
    * Update pandas/core/reshape/merge.py

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -1460,7 +1460,9 @@ class TestMergeDtypes:
         result = merge(left, right, on="A")
         assert is_object_dtype(result.A.dtype)
 
-    @pytest.mark.parametrize("d1", [np.int64, np.int32, np.int16, np.int8, np.uint8])
+    @pytest.mark.parametrize(
+        "d1", [np.int64, np.int32, np.intc, np.int16, np.int8, np.uint8]
+    )
     @pytest.mark.parametrize("d2", [np.int64, np.float64, np.float32, np.float16])
     def test_join_multi_dtypes(self, d1, d2):
         dtype1 = np.dtype(d1)
