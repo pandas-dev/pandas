ea7cec6b5941f52dc652d3341a754018f59d0092
Dea María Léon <deamarialeon@gmail.com>
2023-05-17 13:19:35 +0200

    DOC: Fixing EX01 - Added examples (#53262)
    * Added examples
    
    * Changed end_time example
    
    * Update pandas/core/arrays/datetimes.py
    
    ---------
    
    Co-authored-by: Marco Edward Gorelli <marcogorelli@protonmail.com>

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -83,16 +83,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         pandas.Series.backfill \
         pandas.Series.ffill \
         pandas.Series.pad \
-        pandas.Series.dt.date \
-        pandas.Series.dt.time \
-        pandas.Series.dt.timetz \
-        pandas.Series.dt.dayofyear \
-        pandas.Series.dt.day_of_year \
-        pandas.Series.dt.quarter \
-        pandas.Series.dt.daysinmonth \
-        pandas.Series.dt.days_in_month \
-        pandas.Series.dt.tz \
-        pandas.Series.dt.end_time \
         pandas.Series.dt.days \
         pandas.Series.dt.seconds \
         pandas.Series.dt.microseconds \
