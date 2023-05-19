0df0574376594866797872dae0f618cded254845
Terji Petersen <terji78@gmail.com>
2023-05-19 06:52:33 +0100

    simplify

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -11161,41 +11161,20 @@ class DataFrame(NDFrame, OpsMixin):
     def idxmin(
         self, axis: Axis = 0, skipna: bool = True, numeric_only: bool = False
     ) -> Series:
-        return self._idxminmax(
-            op_name="idxmin", axis=axis, skipna=skipna, numeric_only=numeric_only
-        )
-
-    @doc(_shared_docs["idxmax"], numeric_only_default="False")
-    def idxmax(
-        self, axis: Axis = 0, skipna: bool = True, numeric_only: bool = False
-    ) -> Series:
-        return self._idxminmax(
-            op_name="idxmax", axis=axis, skipna=skipna, numeric_only=numeric_only
-        )
-
-    def _idxminmax(
-        self,
-        op_name: str,
-        axis: Axis = 0,
-        skipna: bool = True,
-        numeric_only: bool = False,
-    ) -> Series:
         axis = self._get_axis_number(axis)
+
         if self.empty:
             axis_dtype = self.axes[axis].dtype
             return self._constructor_sliced(dtype=axis_dtype)
-
-        arg, arg_name = {
-            "idxmin": (nanops.nanargmin, "argmin"),
-            "idxmax": (nanops.nanargmax, "argmax"),
-        }[op_name]
 
         if numeric_only:
             data = self._get_numeric_data()
         else:
             data = self
 
-        res = data._reduce(arg, arg_name, axis=axis, skipna=skipna, numeric_only=False)
+        res = data._reduce(
+            nanops.nanargmin, "argmin", axis=axis, skipna=skipna, numeric_only=False
+        )
         indices = res._values
 
         # indices will always be np.ndarray since axis is not None and
@@ -11206,7 +11185,37 @@ class DataFrame(NDFrame, OpsMixin):
         index = data._get_axis(axis)
         result = [index[i] if i >= 0 else np.nan for i in indices]
         final_result = data._constructor_sliced(result, index=data._get_agg_axis(axis))
-        return final_result.__finalize__(self, method=op_name)
+        return final_result.__finalize__(self, method="idxmin")
+
+    @doc(_shared_docs["idxmax"], numeric_only_default="False")
+    def idxmax(
+        self, axis: Axis = 0, skipna: bool = True, numeric_only: bool = False
+    ) -> Series:
+        axis = self._get_axis_number(axis)
+
+        if self.empty:
+            axis_dtype = self.axes[axis].dtype
+            return self._constructor_sliced(dtype=axis_dtype)
+
+        if numeric_only:
+            data = self._get_numeric_data()
+        else:
+            data = self
+
+        res = data._reduce(
+            nanops.nanargmax, "argmax", axis=axis, skipna=skipna, numeric_only=False
+        )
+        indices = res._values
+
+        # indices will always be np.ndarray since axis is not None and
+        # values is a 2d array for DataFrame
+        # error: Item "int" of "Union[int, Any]" has no attribute "__iter__"
+        assert isinstance(indices, np.ndarray)  # for mypy
+
+        index = data._get_axis(axis)
+        result = [index[i] if i >= 0 else np.nan for i in indices]
+        final_result = data._constructor_sliced(result, index=data._get_agg_axis(axis))
+        return final_result.__finalize__(self, method="idxmax")
 
     def _get_agg_axis(self, axis_num: int) -> Index:
         """
