d45f591704c48e5e399d11b3b9654f996a596e67
jbrockmendel <jbrockmendel@gmail.com>
2023-05-25 09:13:25 -0700

    DEPR: support axis=None in DataFrame reductions (#52042)
    * DEPR: support axis=None in DataFrame reductions
    
    * test, whatsnew
    
    * catch in apply(sum)
    
    * Fix defaults
    
    * catch warnings
    
    * dont check stacklevel
    
    * mypy fixup
    
    * catch warning

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -752,7 +752,13 @@ def test_groupby_as_index_agg(df):
 
     gr = df.groupby(ts)
     gr.nth(0)  # invokes set_selection_from_grouper internally
-    tm.assert_frame_equal(gr.apply(sum), df.groupby(ts).apply(sum))
+
+    msg = "The behavior of DataFrame.sum with axis=None is deprecated"
+    with tm.assert_produces_warning(FutureWarning, match=msg, check_stacklevel=False):
+        res = gr.apply(sum)
+    with tm.assert_produces_warning(FutureWarning, match=msg, check_stacklevel=False):
+        alt = df.groupby(ts).apply(sum)
+    tm.assert_frame_equal(res, alt)
 
     for attr in ["mean", "max", "count", "idxmax", "cumsum", "all"]:
         gr = df.groupby(ts, as_index=False)
