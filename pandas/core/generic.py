8de4ffa0855f5d711ad2b476493a9f9ac449f449
Dea María Léon <deamarialeon@gmail.com>
2023-05-27 19:38:15 +0200

    Deprecating first()

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -9162,6 +9162,12 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         3 days observed in the dataset, and therefore data for 2018-04-13 was
         not returned.
         """
+        # GH45908 & GH#52487
+        warnings.warn(
+            "first is deprecated and will be removed in a future version",
+            FutureWarning,
+            stacklevel=find_stack_level(),
+        )
         if not isinstance(self.index, DatetimeIndex):
             raise TypeError("'first' only supports a DatetimeIndex index")
 
