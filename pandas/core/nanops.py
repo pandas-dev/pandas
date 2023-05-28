80e78329381892692da452f7d119ca229d547e50
srkds <im.nirav86@gmail.com>
2023-05-28 20:32:15 +0530

    condition check should be at _get_dtype_max

--- pandas/core/nanops.py
+++ pandas/core/nanops.py
@@ -324,8 +324,10 @@ def _get_values(
 def _get_dtype_max(dtype: np.dtype) -> np.dtype:
     # return a platform independent precision dtype
     dtype_max = dtype
-    if dtype.kind in "biu":
+    if dtype.kind in "bi":
         dtype_max = np.dtype(np.int64)
+    elif dtype.kind == "u":
+        dtype_max = np.dtype(np.uint64)
     elif dtype.kind == "f":
         dtype_max = np.dtype(np.float64)
     return dtype_max
@@ -640,8 +642,6 @@ def nansum(
         dtype_sum = dtype
     elif dtype.kind == "m":
         dtype_sum = np.dtype(np.float64)
-    elif dtype.kind == "u":
-        dtype_sum = np.dtype(np.uint64)
 
     the_sum = values.sum(axis, dtype=dtype_sum)
     the_sum = _maybe_null_out(the_sum, axis, mask, values.shape, min_count=min_count)
