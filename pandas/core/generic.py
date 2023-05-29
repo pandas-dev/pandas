b8e30b451f86997125403944c4b4ca1a409c1c2c
Marco Edward Gorelli <marcogorelli@protonmail.com>
2023-05-29 15:39:19 +0100

    Update pandas/core/generic.py

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -9163,8 +9163,8 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         not returned.
         """
         warnings.warn(
-            "first is deprecated and will be removed in a future version"
-            "please create a mask and filter using `.loc` instead",
+            "first is deprecated and will be removed in a future version. "
+            "Please create a mask and filter using `.loc` instead",
             FutureWarning,
             stacklevel=find_stack_level(),
         )
