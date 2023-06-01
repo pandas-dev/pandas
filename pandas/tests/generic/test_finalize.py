788c646fc8bbacd531be113835f277c8459eeac7
Dea María Léon <deamarialeon@gmail.com>
2023-06-01 20:18:53 +0200

    DEPR: Series.first() and DataFrame.first() (#53419)
    * Deprecating first()
    
    * Added requested changes
    
    * removed repeated ts
    
    * Update doc/source/whatsnew/v2.1.0.rst
    
    * Update pandas/core/generic.py
    
    * pre-commit
    
    * Separated test for first() on test_finalize.py
    
    * Avoiding docstring on CI
    
    ---------
    
    Co-authored-by: Marco Edward Gorelli <marcogorelli@protonmail.com>
    Co-authored-by: MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>

--- pandas/tests/generic/test_finalize.py
+++ pandas/tests/generic/test_finalize.py
@@ -8,6 +8,7 @@ import numpy as np
 import pytest
 
 import pandas as pd
+import pandas._testing as tm
 
 # TODO:
 # * Binary methods (mul, div, etc.)
@@ -336,16 +337,6 @@ _all_methods = [
     (
         pd.Series,
         (1, pd.date_range("2000", periods=4)),
-        operator.methodcaller("first", "3D"),
-    ),
-    (
-        pd.DataFrame,
-        ({"A": [1, 1, 1, 1]}, pd.date_range("2000", periods=4)),
-        operator.methodcaller("first", "3D"),
-    ),
-    (
-        pd.Series,
-        (1, pd.date_range("2000", periods=4)),
         operator.methodcaller("last", "3D"),
     ),
     (
@@ -449,6 +440,22 @@ def test_finalize_called(ndframe_method):
     result = method(ndframe)
 
     assert result.attrs == {"a": 1}
+
+
+@pytest.mark.parametrize(
+    "data",
+    [
+        pd.Series(1, pd.date_range("2000", periods=4)),
+        pd.DataFrame({"A": [1, 1, 1, 1]}, pd.date_range("2000", periods=4)),
+    ],
+)
+def test_finalize_first(data):
+    deprecated_msg = "first is deprecated"
+
+    data.attrs = {"a": 1}
+    with tm.assert_produces_warning(FutureWarning, match=deprecated_msg):
+        result = data.first("3D")
+        assert result.attrs == {"a": 1}
 
 
 @not_implemented_mark
