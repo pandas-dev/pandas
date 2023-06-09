81a7dc91ed60d314001ecea04f957f27061e2c47
Dea María Léon <deamarialeon@gmail.com>
2023-06-09 16:47:05 +0200

    Added examples

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -264,24 +264,16 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         pandas.api.indexers.BaseIndexer \
         pandas.api.indexers.VariableOffsetWindowIndexer \
         pandas.core.groupby.DataFrameGroupBy.diff \
-        pandas.core.groupby.DataFrameGroupBy.ffill \
         pandas.core.groupby.DataFrameGroupBy.median \
-        pandas.core.groupby.DataFrameGroupBy.ohlc \
         pandas.core.groupby.DataFrameGroupBy.skew \
         pandas.core.groupby.DataFrameGroupBy.std \
         pandas.core.groupby.DataFrameGroupBy.var \
         pandas.core.groupby.SeriesGroupBy.diff \
         pandas.core.groupby.SeriesGroupBy.fillna \
-        pandas.core.groupby.SeriesGroupBy.ffill \
         pandas.core.groupby.SeriesGroupBy.median \
-        pandas.core.groupby.SeriesGroupBy.nunique \
-        pandas.core.groupby.SeriesGroupBy.ohlc \
         pandas.core.groupby.SeriesGroupBy.skew \
         pandas.core.groupby.SeriesGroupBy.std \
         pandas.core.groupby.SeriesGroupBy.var \
-        pandas.core.groupby.SeriesGroupBy.hist \
-        pandas.core.groupby.DataFrameGroupBy.plot \
-        pandas.core.groupby.SeriesGroupBy.plot \
         pandas.io.formats.style.Styler \
         pandas.io.formats.style.Styler.from_custom_template \
         pandas.io.formats.style.Styler.set_caption \
