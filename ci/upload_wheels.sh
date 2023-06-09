9d6226c8a77fda6b3001a350e86e331a654ca33c
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-06-09 15:48:40 -0700

    CI: Ensure nightly wheels are uploaded instead of just sdist (#53578)

--- ci/upload_wheels.sh
+++ ci/upload_wheels.sh
@@ -28,12 +28,12 @@ upload_wheels() {
             if compgen -G "./dist/*.gz"; then
                 echo "Found sdist"
                 anaconda -q -t ${TOKEN} upload --skip -u ${ANACONDA_ORG} ./dist/*.gz
-            elif compgen -G "./wheelhouse/*.whl"; then
+                echo "Uploaded sdist"
+            fi
+            if compgen -G "./wheelhouse/*.whl"; then
                 echo "Found wheel"
                 anaconda -q -t ${TOKEN} upload --skip -u ${ANACONDA_ORG} ./wheelhouse/*.whl
-            else
-                echo "Files do not exist"
-                return 1
+                echo "Uploaded wheel"
             fi
             echo "PyPI-style index: https://pypi.anaconda.org/$ANACONDA_ORG/simple"
         fi
