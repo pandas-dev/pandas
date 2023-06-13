8d3ab090c63be646a528469235675733611d0ae4
Charlie-XIAO <yx2436@nyu.edu>
2023-06-13 11:18:50 +0800

    parametrize tests and updated changelog

--- pandas/tests/groupby/test_libgroupby.py
+++ pandas/tests/groupby/test_libgroupby.py
@@ -305,16 +305,25 @@ def test_cython_group_mean_Inf_at_begining_and_end():
     )
 
 
-def test_cython_group_sum_Inf_at_begining_and_end():
+@pytest.mark.parametrize(
+    "values, out",
+    [
+        ([[np.inf], [np.inf], [np.inf]], [[np.inf], [np.inf]]),
+        ([[np.inf], [np.inf], [-np.inf]], [[np.inf], [np.nan]]),
+        ([[np.inf], [-np.inf], [np.inf]], [[np.inf], [np.nan]]),
+        ([[np.inf], [-np.inf], [-np.inf]], [[np.inf], [-np.inf]]),
+    ],
+)
+def test_cython_group_sum_Inf_at_begining_and_end(values, out):
     # GH #53606
     actual = np.array([[np.nan], [np.nan]], dtype="float64")
     counts = np.array([0, 0], dtype="int64")
-    data = np.array([[np.inf], [np.inf], [np.inf]], dtype="float64")
+    data = np.array(values, dtype="float64")
     labels = np.array([0, 1, 1], dtype=np.intp)
 
     group_sum(actual, counts, data, labels, None, is_datetimelike=False)
 
-    expected = np.array([[np.inf], [np.inf]], dtype="float64")
+    expected = np.array(out, dtype="float64")
 
     tm.assert_numpy_array_equal(
         actual,
