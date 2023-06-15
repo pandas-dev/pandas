9bf4dbe9049a78655d2ca1d618df83ec9b378d18
Charlie-XIAO <yx2436@nyu.edu>
2023-06-15 13:25:50 +0800

    avoid float(complex('nan'))

--- pandas/_libs/lib.pyx
+++ pandas/_libs/lib.pyx
@@ -2485,7 +2485,8 @@ def maybe_convert_objects(ndarray[object] objects,
         elif util.is_nan(val):
             seen.nan_ = True
             mask[i] = True
-            floats[i] = complexes[i] = val
+            floats[i] = fnan
+            complexes[i] = val
         elif util.is_bool_object(val):
             seen.bool_ = True
             bools[i] = val
