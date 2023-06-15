d9d981dd62d0359cd47680503fd8fe71ecb07555
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2023-06-14 17:06:17 -0700

    BUG/CoW: is_range_indexer can't handle very large arrays (#53672)
    * BUG: is_range_indexer can't handle very large arrays
    
    * fix test on 32-bit

--- pandas/tests/libs/test_lib.py
+++ pandas/tests/libs/test_lib.py
@@ -6,6 +6,7 @@ from pandas._libs import (
     lib,
     writers as libwriters,
 )
+from pandas.compat import IS64
 
 from pandas import Index
 import pandas._testing as tm
@@ -247,6 +248,18 @@ class TestIndexing:
         # GH#50592
         left = np.arange(0, 100, dtype=dtype)
         assert lib.is_range_indexer(left, 100)
+
+    @pytest.mark.skipif(
+        not IS64,
+        reason="2**31 is too big for Py_ssize_t on 32-bit. "
+        "It doesn't matter though since you cannot create an array that long on 32-bit",
+    )
+    @pytest.mark.parametrize("dtype", ["int64", "int32"])
+    def test_is_range_indexer_big_n(self, dtype):
+        # GH53616
+        left = np.arange(0, 100, dtype=dtype)
+
+        assert not lib.is_range_indexer(left, 2**31)
 
     @pytest.mark.parametrize("dtype", ["int64", "int32"])
     def test_is_range_indexer_not_equal(self, dtype):
