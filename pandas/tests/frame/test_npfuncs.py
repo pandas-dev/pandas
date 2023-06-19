dff37fe7193c53b5ac677e187bfa47aa31a388fa
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-06-19 09:11:48 +0200

    fix existing tests checking for index identity

--- pandas/tests/frame/test_npfuncs.py
+++ pandas/tests/frame/test_npfuncs.py
@@ -22,8 +22,8 @@ class TestAsArray:
         with np.errstate(all="ignore"):
             result = np.sqrt(float_frame)
         assert isinstance(result, type(float_frame))
-        assert result.index is float_frame.index
-        assert result.columns is float_frame.columns
+        assert result.index.is_(float_frame.index)
+        assert result.columns.is_(float_frame.columns)
 
         tm.assert_frame_equal(result, float_frame.apply(np.sqrt))
 
