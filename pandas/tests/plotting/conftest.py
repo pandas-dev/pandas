aa6d99f4c870b2657dc71b1eb07ee4924625a8e3
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-06-21 12:23:45 -0700

    TST: Reduce memory pressure of plotting tests (#53660)
    * TST: Reduce memory pressure of plotting tests
    
    * Trigger ci
    
    * Remove gc call

--- pandas/tests/plotting/conftest.py
+++ pandas/tests/plotting/conftest.py
@@ -8,6 +8,13 @@ from pandas import (
 
 
 @pytest.fixture(autouse=True)
+def non_interactive():
+    mpl = pytest.importorskip("matplotlib")
+    mpl.use("template")
+    yield
+
+
+@pytest.fixture(autouse=True)
 def reset_rcParams():
     mpl = pytest.importorskip("matplotlib")
     with mpl.rc_context():
@@ -16,8 +23,11 @@ def reset_rcParams():
 
 @pytest.fixture(autouse=True)
 def close_all_figures():
+    # https://stackoverflow.com/q/31156578
     yield
     plt = pytest.importorskip("matplotlib.pyplot")
+    plt.cla()
+    plt.clf()
     plt.close("all")
 
 
