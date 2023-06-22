d44d0f7ec4307a89ab09aef97bcb32cc33105f84
Con <mcgee.stocks@gmail.com>
2023-06-22 09:19:31 -0400

    precommit cleanup

--- pandas/_libs/tslibs/ccalendar.pyx
+++ pandas/_libs/tslibs/ccalendar.pyx
@@ -2,7 +2,6 @@
 """
 Cython implementations of functions resembling the stdlib calendar module
 """
-from libc.stdlib cimport abs as c_abs
 cimport cython
 from numpy cimport (
     int32_t,
@@ -19,8 +18,7 @@ cdef int32_t* days_per_month_array = [
     31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31,
     31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 
-cdef int* sakamoto_arr = [0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4]
-cdef int* em = [0 ,0 ,31 ,59 ,90 ,120 ,151 ,181 ,212 ,243 ,273 ,304 ,334]
+cdef int* em = [0, 0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334]
 
 # The first 13 entries give the month days elapsed as of the first of month N
 # (or the total number of days in the year for N=13) in non-leap years.
@@ -74,15 +72,17 @@ cpdef int32_t get_days_in_month(int year, Py_ssize_t month) noexcept nogil:
     """
     return days_per_month_array[12 * is_leapyear(year) + month - 1]
 
+
 @cython.wraparound(False)
 @cython.boundscheck(False)
 @cython.cdivision(True)
 cdef long quot(long a , long b) noexcept nogil:
     cdef long x
-    x = a/b 
-    if ( a < 0 ):
-        x -= ( a % b != 0 )
+    x = a/b
+    if (a < 0):
+        x -= (a % b != 0)
     return x
+
 
 @cython.wraparound(False)
 @cython.boundscheck(False)
@@ -115,23 +115,22 @@ cdef int dayofweek(int y, int m, int d) noexcept nogil:
     [2] https://en.wikipedia.org/wiki/\
     Determination_of_the_day_of_the_week#Gauss's_algorithm
     """
-      
-    cdef: 
-        long c 
+    cdef:
+        long c
         int g
         int f
         int e
-    
+
     if (m < 3):
         y -= 1
-        
-    c = quot(y, 100)    
-    g = y - quot(y, 100) * 100    
+
+    c = quot(y, 100)
+    g = y - quot(y, 100) * 100
     f = 5 * (c - quot(c, 4) * 4)
-    e = em[m]    
-    
+    e = em[m]
+
     if (m > 2):
-        e -= 1;
+        e -= 1
     return (-1 + d + e + f + g + g/4) % 7
 
 cdef bint is_leapyear(int64_t year) noexcept nogil:
