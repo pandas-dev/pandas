ff744108c03701c1e29dc013c62c6bc3ecd8ae55
Marco Edward Gorelli <marcogorelli@protonmail.com>
2023-06-23 16:45:57 +0100

    CLN: PDEP6 precusor (#53815)
    set dtype explicitly

--- pandas/tests/copy_view/test_interp_fillna.py
+++ pandas/tests/copy_view/test_interp_fillna.py
@@ -248,7 +248,10 @@ def test_fillna_inplace_reference(using_copy_on_write):
 
 
 def test_fillna_interval_inplace_reference(using_copy_on_write):
-    ser = Series(interval_range(start=0, end=5), name="a")
+    # Set dtype explicitly to avoid implicit cast when setting nan
+    ser = Series(
+        interval_range(start=0, end=5), name="a", dtype="interval[float64, right]"
+    )
     ser.iloc[1] = np.nan
 
     ser_orig = ser.copy()
