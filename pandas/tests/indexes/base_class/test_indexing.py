eaf0dfb5c6fa2d68336bff193872f4b42324424f
Sven <mail4sven@gmx.net>
2023-06-23 19:38:53 +0200

    Fixed #52234 (#53698)
    * Fixed #52234
    
    * Copied fix from https://github.com/ltauro/pandas/tree/nan-not-in-index and applied review comments
    
    * Update doc/source/whatsnew/v2.1.0.rst
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/tests/indexes/base_class/test_indexing.py
+++ pandas/tests/indexes/base_class/test_indexing.py
@@ -39,6 +39,24 @@ class TestGetIndexerNonUnique:
         tm.assert_numpy_array_equal(np.array([-1], dtype=np.intp), indexes)
         tm.assert_numpy_array_equal(np.array([0], dtype=np.intp), missing)
 
+    @pytest.mark.parametrize(
+        "idx_values,idx_non_unique",
+        [
+            ([np.nan, 100, 200, 100], [np.nan, 100]),
+            ([np.nan, 100.0, 200.0, 100.0], [np.nan, 100.0]),
+        ],
+    )
+    def test_get_indexer_non_unique_int_index(self, idx_values, idx_non_unique):
+        indexes, missing = Index(idx_values).get_indexer_non_unique(Index([np.nan]))
+        tm.assert_numpy_array_equal(np.array([0], dtype=np.intp), indexes)
+        tm.assert_numpy_array_equal(np.array([], dtype=np.intp), missing)
+
+        indexes, missing = Index(idx_values).get_indexer_non_unique(
+            Index(idx_non_unique)
+        )
+        tm.assert_numpy_array_equal(np.array([0, 1, 3], dtype=np.intp), indexes)
+        tm.assert_numpy_array_equal(np.array([], dtype=np.intp), missing)
+
 
 class TestGetLoc:
     @pytest.mark.slow  # to_flat_index takes a while
