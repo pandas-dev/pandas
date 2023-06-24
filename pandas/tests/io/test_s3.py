693d7170ebfdc957d110f11bce28b1520c45e85d
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-06-23 21:17:56 -0700

    Ignore disutils from datareader, s3so

--- pandas/tests/io/test_s3.py
+++ pandas/tests/io/test_s3.py
@@ -20,16 +20,20 @@ def test_streaming_s3_objects():
 
 
 @td.skip_if_no("s3fs")
-def test_read_without_creds_from_pub_bucket(s3_public_bucket_with_data):
+@pytest.mark.single_cpu
+def test_read_without_creds_from_pub_bucket(s3_public_bucket_with_data, s3so):
     # GH 34626
-    # Use Amazon Open Data Registry - https://registry.opendata.aws/gdelt
-    result = read_csv(f"s3://{s3_public_bucket_with_data.name}/tips.csv", nrows=3)
+    result = read_csv(
+        f"s3://{s3_public_bucket_with_data.name}/tips.csv",
+        nrows=3,
+        storage_options=s3so,
+    )
     assert len(result) == 3
 
 
 @td.skip_if_no("s3fs")
 @pytest.mark.single_cpu
-def test_read_with_creds_from_pub_bucket(s3_public_bucket_with_data, monkeypatch):
+def test_read_with_creds_from_pub_bucket(s3_public_bucket_with_data, monkeypatch, s3so):
     # Ensure we can read from a public bucket with credentials
     # GH 34626
 
@@ -38,6 +42,9 @@ def test_read_with_creds_from_pub_bucket(s3_public_bucket_with_data, monkeypatch
     monkeypatch.setenv("AWS_ACCESS_KEY_ID", "foobar_key")
     monkeypatch.setenv("AWS_SECRET_ACCESS_KEY", "foobar_secret")
     df = read_csv(
-        f"s3://{s3_public_bucket_with_data.name}/tips.csv", nrows=5, header=None
+        f"s3://{s3_public_bucket_with_data.name}/tips.csv",
+        nrows=5,
+        header=None,
+        storage_options=s3so,
     )
     assert len(df) == 5
