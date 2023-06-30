b27e0734ba4252edd4a14cb76aded977e46913dc
Dea María Léon <deamarialeon@gmail.com>
2023-06-30 19:45:20 +0200

    DOC: Fixing EX01 - Added examples (#53946)
    Examples options

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -118,10 +118,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         pandas.io.stata.StataReader.value_labels \
         pandas.io.stata.StataReader.variable_labels \
         pandas.io.stata.StataWriter.write_file \
-        pandas.describe_option \
-        pandas.reset_option \
-        pandas.get_option \
-        pandas.set_option \
         pandas.plotting.deregister_matplotlib_converters \
         pandas.plotting.plot_params \
         pandas.plotting.register_matplotlib_converters \
