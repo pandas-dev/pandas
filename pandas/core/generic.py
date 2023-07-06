15cef2cc8a554b7d7d864f88170938479ab154a7
jbrockmendel <jbrockmendel@gmail.com>
2023-07-06 12:58:37 -0700

    CLN: remove unreachable, unnecessary axis kwd (#53991)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -8052,10 +8052,8 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
             )
         else:
             index = missing.get_interp_index(method, obj.index)
-            axis = self._info_axis_number
             new_data = obj._mgr.interpolate(
                 method=method,
-                axis=axis,
                 index=index,
                 limit=limit,
                 limit_direction=limit_direction,
