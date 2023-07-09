fb1aaf73e6321dc88d9d64d20cf2b9827ae5f25c
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-07-09 17:48:00 +0200

    CoW: Adjust test to cover missing case

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -1987,6 +1987,7 @@ def using_copy_on_write() -> bool:
     """
     Fixture to check if Copy-on-Write is enabled.
     """
+    pd.options.mode.copy_on_write = True
     return pd.options.mode.copy_on_write and pd.options.mode.data_manager == "block"
 
 
