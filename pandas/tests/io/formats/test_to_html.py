ef530cb8b567a527f0c9f8d1665d7ff00293c62a
Daniel Isaac <danielbcbs2@gmail.com>
2023-07-09 22:42:20 +0530

    53885: testcase review comment addressing

--- pandas/tests/io/formats/test_to_html.py
+++ pandas/tests/io/formats/test_to_html.py
@@ -903,25 +903,27 @@ def test_to_html_multiindex_col_with_colspace():
     df = DataFrame([[1, 2]])
     df.columns = MultiIndex.from_tuples([(1, 1), (2, 1)])
     result = df.to_html(col_space=100)
-    expected = ( '<table border="1" class="dataframe">\n'
-        '  <thead>\n'
-        '    <tr>\n'
+    expected = (
+        '<table border="1" class="dataframe">\n'
+        "  <thead>\n"
+        "    <tr>\n"
         '      <th style="min-width: 100px;"></th>\n'
         '      <th style="min-width: 100px;">1</th>\n'
         '      <th style="min-width: 100px;">2</th>\n'
-        '    </tr>\n'
-        '    <tr>\n'
+        "    </tr>\n"
+        "    <tr>\n"
         '      <th style="min-width: 100px;"></th>\n'
         '      <th style="min-width: 100px;">1</th>\n'
         '      <th style="min-width: 100px;">1</th>\n'
-        '    </tr>\n'
-        '  </thead>\n'
-        '  <tbody>\n'
-        '    <tr>\n'
-        '      <th>0</th>\n'
-        '      <td>1</td>\n'
-        '      <td>2</td>\n'
-        '    </tr>\n'
-        '  </tbody>\n'
-        '</table>' )
+        "    </tr>\n"
+        "  </thead>\n"
+        "  <tbody>\n"
+        "    <tr>\n"
+        "      <th>0</th>\n"
+        "      <td>1</td>\n"
+        "      <td>2</td>\n"
+        "    </tr>\n"
+        "  </tbody>\n"
+        "</table>"
+    )
     assert result == expected
