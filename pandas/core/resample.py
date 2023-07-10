0093c414b0202aa0e6320252d66a03cfe8003f93
Conrad <mcgee.stocks@gmail.com>
2023-07-10 10:59:33 -0400

    resolve edge cases, fix tests

--- pandas/core/resample.py
+++ pandas/core/resample.py
@@ -2438,8 +2438,15 @@ def _get_timestamp_range_edges(
     """
     if isinstance(freq, Tick):
         index_tz = first.tz
-        if isinstance(origin, Timestamp) and (origin.tz is None) != (index_tz is None):
-            raise ValueError("The origin must have the same timezone as the index.")
+        if isinstance(origin, Timestamp) and origin.tz != index_tz:
+            if first.utcoffset() != origin.utcoffset():
+                raise ValueError("The origin must have the same timezone as the index.")
+        elif isinstance(origin, Timestamp):
+            if origin <= first:
+                first = origin
+            elif origin >= last:
+                last = origin
+
         if origin == "epoch":
             # set the epoch based on the timezone to have similar bins results when
             # resampling on the same kind of indexes on different timezones
@@ -2461,17 +2468,14 @@ def _get_timestamp_range_edges(
             first = first.tz_localize(index_tz)
             last = last.tz_localize(index_tz)
     else:
+        if isinstance(origin, Timestamp):
+            first = origin
+
         first = first.normalize()
         last = last.normalize()
 
         if closed == "left":
-            if isinstance(origin, Timestamp):
-                if first - freq >= origin:
-                    first = Timestamp(first - freq)
-                else:
-                    first = Timestamp(freq.rollback(first))
-            else:
-                first = Timestamp(freq.rollback(first))
+            first = Timestamp(freq.rollback(first))
         else:
             first = Timestamp(first - freq)
 
