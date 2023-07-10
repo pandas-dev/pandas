ac3153b30a611d2593b002ca5d9ff22a2bb30af0
Luke Manley <lukemanley@gmail.com>
2023-07-10 13:51:37 -0400

    PERF: MultiIndex set and indexing operations (#53955)
    * PERF: MultiIndex set and indexing ops
    
    * whatsnew
    
    * convert to generator

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -2448,6 +2448,19 @@ class MultiIndex(Index):
             levels=new_levels, codes=new_codes, names=new_names, verify_integrity=False
         )
 
+    def _recode_for_new_levels(
+        self, new_levels, copy: bool = True
+    ) -> Generator[np.ndarray, None, None]:
+        if len(new_levels) != self.nlevels:
+            raise AssertionError(
+                f"Length of new_levels ({len(new_levels)}) "
+                f"must be same as self.nlevels ({self.nlevels})"
+            )
+        for i in range(self.nlevels):
+            yield recode_for_categories(
+                self.codes[i], self.levels[i], new_levels[i], copy=copy
+            )
+
     def _get_codes_for_sorting(self) -> list[Categorical]:
         """
         we are categorizing our codes by using the
