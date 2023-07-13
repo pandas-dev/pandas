1e561cfdc51dd8574fefc20d4fef779eae5420d6
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-07-13 18:24:47 -0500

    Fix

--- pandas/tests/copy_view/test_core_functionalities.py
+++ pandas/tests/copy_view/test_core_functionalities.py
@@ -88,8 +88,9 @@ def test_delete(using_copy_on_write):
 
 def test_delete_reference(using_copy_on_write):
     df = DataFrame(np.random.randn(4, 3), columns=["a", "b", "c"])
-    df[:]
+    x = df[:]
     del df["b"]
     if using_copy_on_write:
-        assert not df._mgr.blocks[0].refs.has_reference()
-        assert not df._mgr.blocks[1].refs.has_reference()
+        assert df._mgr.blocks[0].refs.has_reference()
+        assert df._mgr.blocks[1].refs.has_reference()
+        assert x._mgr.blocks[1].refs.has_reference()
