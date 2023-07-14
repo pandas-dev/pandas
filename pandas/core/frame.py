758c49df1e64f252c1f5a012e5675b163e468e75
Dea María Léon <deamarialeon@gmail.com>
2023-07-14 12:57:43 +0200

    Working on to_gbq

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -2102,6 +2102,33 @@ class DataFrame(NDFrame, OpsMixin):
         --------
         pandas_gbq.to_gbq : This function in the pandas-gbq library.
         read_gbq : Read a DataFrame from Google BigQuery.
+
+        Examples
+        --------
+        Example taken from `Google BigQuery documentation
+        <https://cloud.google.com/bigquery/docs/samples/bigquery-pandas-gbq-to-gbq-simple>`__
+
+        >>> import pandas_gbq
+
+        # TODO: Set project_id to your Google Cloud Platform project ID.
+        # project_id = "my-project"
+
+        # TODO: Set table_id to the full destination table ID (including the
+        #       dataset ID).
+        # table_id = 'my_dataset.my_table'
+
+        >>> df = pandas.DataFrame(
+        ...    {
+        ...        "my_string": ["a", "b", "c"],
+        ...        "my_int64": [1, 2, 3],
+        ...        "my_float64": [4.0, 5.0, 6.0],
+        ...        "my_bool1": [True, False, True],
+        ...        "my_bool2": [False, True, False],
+        ...        "my_dates": pandas.date_range("now", periods=3),
+        ...    }
+        ... )
+
+        >>> pandas_gbq.to_gbq(df, table_id, project_id=project_id)  #doctests: +SKIP
         """
         from pandas.io import gbq
 
