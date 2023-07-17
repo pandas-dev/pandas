6dfae487e267cae6a9a94a2a6128e0b0984d9a8f
Conrad <mcgee.stocks@gmail.com>
2023-07-17 10:41:39 -0400

    expand tests

--- pandas/tests/frame/methods/test_dot.py
+++ pandas/tests/frame/methods/test_dot.py
@@ -131,13 +131,17 @@ class TestDataFrameDot(DotSharedTests):
         assert result.name is None
 
 
-def test_arrow_dtype():
+@pytest.mark.parametrize(
+    "dtype,exp_dtype",
+    [("Float32", "Float64"), ("Int16", "Int64"), ("float[pyarrow]", "double[pyarrow]")],
+)
+def test_arrow_dtype(dtype, exp_dtype):
     pytest.importorskip("pyarrow")
 
     cols = ["a", "b"]
     df_a = DataFrame([[1, 2], [3, 4], [5, 6]], columns=cols, dtype="int")
-    df_b = DataFrame([[1, 0], [0, 1]], index=cols, dtype="float[pyarrow]")
+    df_b = DataFrame([[1, 0], [0, 1]], index=cols, dtype=dtype)
     result = df_a.dot(df_b)
-    expected = DataFrame([[1, 2], [3, 4], [5, 6]], dtype="double[pyarrow]")
+    expected = DataFrame([[1, 2], [3, 4], [5, 6]], dtype=exp_dtype)
 
     tm.assert_frame_equal(result, expected)
