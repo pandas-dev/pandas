a5b0f51ddb0322b3164b4cc08293a44fb89ac14e
Daniel Isaac <danielbcbs2@gmail.com>
2023-07-19 04:31:35 +0530

    BUG: Parameter col_space of to_html method not working with multi-level columns  (#54015)
    * 53885: BUG: Parameter col_space of to_html method not working with multi-level columns
    
    * 53885: added testcases
    
    * 53885: added testcases
    
    * 53885: testcase review comment addressing
    
    * 53885: testcase review comment addressing
    
    * 53885: testcase review comment addressing
    
    * 53885: handling case where column is a tuple
    
    * 53885: using isinstance MultiIndex
    
    * typing error addressing
    
    * typing error addressing
    
    * updated whatsnew
    
    * updated whatsnew
    
    * updated whatsnew

--- pandas/tests/io/formats/test_to_html.py
+++ pandas/tests/io/formats/test_to_html.py
@@ -896,3 +896,59 @@ def test_to_html_float_format_object_col(datapath):
     result = df.to_html(float_format=lambda x: f"{x:,.0f}")
     expected = expected_html(datapath, "gh40024_expected_output")
     assert result == expected
+
+
+def test_to_html_multiindex_col_with_colspace():
+    # GH#53885
+    df = DataFrame([[1, 2]])
+    df.columns = MultiIndex.from_tuples([(1, 1), (2, 1)])
+    result = df.to_html(col_space=100)
+    expected = (
+        '<table border="1" class="dataframe">\n'
+        "  <thead>\n"
+        "    <tr>\n"
+        '      <th style="min-width: 100px;"></th>\n'
+        '      <th style="min-width: 100px;">1</th>\n'
+        '      <th style="min-width: 100px;">2</th>\n'
+        "    </tr>\n"
+        "    <tr>\n"
+        '      <th style="min-width: 100px;"></th>\n'
+        '      <th style="min-width: 100px;">1</th>\n'
+        '      <th style="min-width: 100px;">1</th>\n'
+        "    </tr>\n"
+        "  </thead>\n"
+        "  <tbody>\n"
+        "    <tr>\n"
+        "      <th>0</th>\n"
+        "      <td>1</td>\n"
+        "      <td>2</td>\n"
+        "    </tr>\n"
+        "  </tbody>\n"
+        "</table>"
+    )
+    assert result == expected
+
+
+def test_to_html_tuple_col_with_colspace():
+    # GH#53885
+    df = DataFrame({("a", "b"): [1], "b": [2]})
+    result = df.to_html(col_space=100)
+    expected = (
+        '<table border="1" class="dataframe">\n'
+        "  <thead>\n"
+        '    <tr style="text-align: right;">\n'
+        '      <th style="min-width: 100px;"></th>\n'
+        '      <th style="min-width: 100px;">(a, b)</th>\n'
+        '      <th style="min-width: 100px;">b</th>\n'
+        "    </tr>\n"
+        "  </thead>\n"
+        "  <tbody>\n"
+        "    <tr>\n"
+        "      <th>0</th>\n"
+        "      <td>1</td>\n"
+        "      <td>2</td>\n"
+        "    </tr>\n"
+        "  </tbody>\n"
+        "</table>"
+    )
+    assert result == expected
