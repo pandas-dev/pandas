69fde2e594e661ebf89c6ec64a25dac7588d5a45
Con <mcgee.stocks@gmail.com>
2023-07-18 15:29:39 +0000

    update

--- pandas/tests/frame/methods/test_dot.py
+++ pandas/tests/frame/methods/test_dot.py
@@ -133,13 +133,13 @@ class TestDataFrameDot(DotSharedTests):
 
 @pytest.mark.parametrize(
     "dtype,exp_dtype",
-    [("Float32", "Float64"), ("Int16", "Int64"), ("float[pyarrow]", "double[pyarrow]")],
+    [("Float32", "Float64"), ("Int16", "Int32"), ("float[pyarrow]", "double[pyarrow]")],
 )
 def test_arrow_dtype(dtype, exp_dtype):
     pytest.importorskip("pyarrow")
 
     cols = ["a", "b"]
-    df_a = DataFrame([[1, 2], [3, 4], [5, 6]], columns=cols, dtype="int64")
+    df_a = DataFrame([[1, 2], [3, 4], [5, 6]], columns=cols, dtype="int32")
     df_b = DataFrame([[1, 0], [0, 1]], index=cols, dtype=dtype)
     result = df_a.dot(df_b)
     expected = DataFrame([[1, 2], [3, 4], [5, 6]], dtype=exp_dtype)
