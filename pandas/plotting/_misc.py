aec938cb366d4fb44726e2bf7bf62ebdde3ae609
Dea María Léon <deamarialeon@gmail.com>
2023-07-21 20:15:58 +0200

    Examples plotting.table & plot_params

--- pandas/plotting/_misc.py
+++ pandas/plotting/_misc.py
@@ -43,6 +43,20 @@ def table(ax: Axes, data: DataFrame | Series, **kwargs) -> Table:
     Returns
     -------
     matplotlib table object
+
+    Examples
+    --------
+
+    .. plot::
+            :context: close-figs
+
+            >>> import matplotlib.pyplot as plt
+            >>> df = pd.DataFrame({'A':[1, 2], 'B':[3,4]})
+            >>> fix, ax = plt.subplots()
+            >>> ax.axis('off')
+            >>> table = pd.plotting.table(ax, df, loc='center',
+            ...                   cellLoc='center', colWidths=list([.2, .2]))
+            (0.0, 1.0, 0.0, 1.0)
     """
     plot_backend = _get_plot_backend("matplotlib")
     return plot_backend.table(
@@ -552,6 +566,22 @@ class _Options(dict):
     Allows for parameter aliasing so you can just use parameter names that are
     the same as the plot function parameters, but is stored in a canonical
     format that makes it easy to breakdown into groups later.
+
+    Examples
+    --------
+
+    .. plot::
+            :context: close-figs
+
+             >>> np.random.seed(42)
+             >>> df = pd.DataFrame({'A': np.random.randn(1000),
+             ...                   'B': np.random.randn(1000)},
+             ...                   index=pd.date_range("1/1/2000", periods=1000))
+             >>> with pd.plotting.plot_params.use("x_compat", True):
+             ...     df["A"].plot(color="r")
+             ...     df["B"].plot(color="g")
+             <Axes: >
+             <Axes: >
     """
 
     # alias so the names are same as plotting method parameter names
