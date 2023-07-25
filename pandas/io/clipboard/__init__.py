b3d90446aa340de967e5fd1368358adc8e07c4ae
Torsten Wörtwein <twoertwein@gmail.com>
2023-07-25 16:25:31 -0400

    STYL: replace autotyping with ruff's ANN autofixes

--- pandas/io/clipboard/__init__.py
+++ pandas/io/clipboard/__init__.py
@@ -315,7 +315,7 @@ class CheckedCall:
             raise PyperclipWindowsException("Error calling " + self.f.__name__)
         return ret
 
-    def __setattr__(self, key, value):
+    def __setattr__(self, key, value) -> None:
         setattr(self.f, key, value)
 
 
