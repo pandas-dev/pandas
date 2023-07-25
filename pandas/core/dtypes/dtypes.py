b57c6810ad354f485360a552eee5e1818e675f94
Luke Manley <lukemanley@gmail.com>
2023-07-24 20:10:34 -0400

    PERF: ArrowDtype.__eq__ (#54223)
    * PERF: ArrowDtype.__eq__
    
    * cleanup

--- pandas/core/dtypes/dtypes.py
+++ pandas/core/dtypes/dtypes.py
@@ -2056,6 +2056,15 @@ class ArrowDtype(StorageExtensionDtype):
     def __repr__(self) -> str:
         return self.name
 
+    def __hash__(self) -> int:
+        # make myself hashable
+        return hash(str(self))
+
+    def __eq__(self, other: Any) -> bool:
+        if not isinstance(other, type(self)):
+            return super().__eq__(other)
+        return self.pyarrow_dtype == other.pyarrow_dtype
+
     @property
     def type(self):
         """
