cf0075f199d6cb118825d8807eb29f1a20e0ddf1
Dea María Léon <deamarialeon@gmail.com>
2023-07-28 22:01:43 +0200

    DOC: Fixing EX01 - Added examples (#54288)
    * Examples ExtensionArray._from_sequence_of_strings ...
    
    * Moved axis to new line

--- pandas/errors/__init__.py
+++ pandas/errors/__init__.py
@@ -82,6 +82,25 @@ class UnsortedIndexError(KeyError):
     Error raised when slicing a MultiIndex which has not been lexsorted.
 
     Subclass of `KeyError`.
+
+    Examples
+    --------
+    >>> df = pd.DataFrame({"cat": [0, 0, 1, 1],
+    ...                    "color": ["white", "white", "brown", "black"],
+    ...                    "lives": [4, 4, 3, 7]},
+    ...                   )
+    >>> df = df.set_index(["cat", "color"])
+    >>> df
+                lives
+    cat  color
+    0    white    4
+         white    4
+    1    brown    3
+         black    7
+    >>> df.loc[(0, "black"):(1, "white")]
+    Traceback (most recent call last):
+    UnsortedIndexError: 'Key length (2) was greater
+    than MultiIndex lexsort depth (1)'
     """
 
 
