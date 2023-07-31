0cbcb96f07ec0aa9f72bc4288b165bb781019b5e
Natalia Mokeeva <91160475+natmokval@users.noreply.github.com>
2023-07-31 21:51:56 +0200

    BUG: during interchanging from non-pandas tz-aware data (#54287)
    * TST: add a test for interchanging from non-pandas tz-aware
    
    * TST: correct the test for interchanging from non-pandas tz-aware
    
    * check an error in the test
    
    * correct test
    
    * add extra except in set_nulls
    
    * fix bug during interchanging from non-pandas tz-aware
    
    * correct test
    
    * correct wording

--- pandas/tests/interchange/test_impl.py
+++ pandas/tests/interchange/test_impl.py
@@ -308,3 +308,22 @@ def test_datetimetzdtype(tz, unit):
     )
     df = pd.DataFrame({"ts_tz": tz_data})
     tm.assert_frame_equal(df, from_dataframe(df.__dataframe__()))
+
+
+def test_interchange_from_non_pandas_tz_aware():
+    # GH 54239, 54287
+    pa = pytest.importorskip("pyarrow", "11.0.0")
+    import pyarrow.compute as pc
+
+    arr = pa.array([datetime(2020, 1, 1), None, datetime(2020, 1, 2)])
+    arr = pc.assume_timezone(arr, "Asia/Kathmandu")
+    table = pa.table({"arr": arr})
+    exchange_df = table.__dataframe__()
+    result = from_dataframe(exchange_df)
+
+    expected = pd.DataFrame(
+        ["2020-01-01 00:00:00+05:45", "NaT", "2020-01-02 00:00:00+05:45"],
+        columns=["arr"],
+        dtype="datetime64[us, Asia/Kathmandu]",
+    )
+    tm.assert_frame_equal(expected, result)
