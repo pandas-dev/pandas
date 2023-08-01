12db03ab9f63ab066640f6be27a09ee2bd4994ac
Kevin <swimmingkevin2012@gmail.com>
2023-08-01 13:34:25 -0700

    mend

--- pandas/tests/reshape/test_pivot.py
+++ pandas/tests/reshape/test_pivot.py
@@ -2619,7 +2619,7 @@ class TestPivot:
         tm.assert_frame_equal(df, expected)
 
     def test_pivot_table_empty_dataframe_correct_index(self):
-        # GH#21932
+        # GH 21932
         df = DataFrame([], columns=["a", "b", "value"])
         pivot = df.pivot_table(index="a", columns="b", values="value", aggfunc="count")
 
