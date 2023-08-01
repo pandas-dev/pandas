33213ba44c79bba702640023b443831212ede106
Dea María Léon <deamarialeon@gmail.com>
2023-08-01 17:12:25 +0200

    Example for deregister

--- pandas/plotting/_misc.py
+++ pandas/plotting/_misc.py
@@ -101,6 +101,28 @@ def deregister() -> None:
     --------
     register_matplotlib_converters : Register pandas formatters and converters
         with matplotlib.
+
+    Examples
+    --------
+    .. plot::
+       :context: close-figs
+
+        The following line is done automatically by pandas so
+        the plot can be rendered:
+
+        >>> pd.plotting.register_matplotlib_converters()
+
+        >>> df = pd.DataFrame({'ts': pd.period_range('2020', periods=2, freq='M'),
+        ...                    'y': [1, 2]
+        ...                    })
+        >>> plot = df.plot.line(x='ts', y='y')
+
+    Using deregister an error will be raised:
+
+    >>> pd.plotting.deregister_matplotlib_converters()  # doctest: +SKIP
+    >>> df.plot.line(x='ts', y='y')  # doctest: +SKIP
+    Traceback (most recent call last):
+    TypeError: float() argument must be a string or a real number, not 'Period'
     """
     plot_backend = _get_plot_backend("matplotlib")
     plot_backend.deregister()
