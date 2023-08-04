e9fb6080d2708539397ab97bab95f8bb55dfda59
Brock <jbrockmendel@gmail.com>
2023-08-04 14:07:12 -0700

    remove unnecesssary override

--- pandas/tests/extension/test_sparse.py
+++ pandas/tests/extension/test_sparse.py
@@ -366,13 +366,6 @@ class TestMethods(BaseSparseTests, base.BaseMethodsTests):
 
 
 class TestCasting(BaseSparseTests, base.BaseCastingTests):
-    def test_astype_str(self, data):
-        # pre-2.0 this would give a SparseDtype even if the user asked
-        #  for a non-sparse dtype.
-        result = pd.Series(data[:5]).astype(str)
-        expected = pd.Series([str(x) for x in data[:5]], dtype=object)
-        tm.assert_series_equal(result, expected)
-
     @pytest.mark.xfail(raises=TypeError, reason="no sparse StringDtype")
     def test_astype_string(self, data):
         super().test_astype_string(data)
