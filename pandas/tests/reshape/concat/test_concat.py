71788fd188fa2baf64c1e56a585d56712d0999f9
yuanx749 <yuanx749@gmail.com>
2023-08-05 23:12:29 +0800

    Update test

--- pandas/tests/reshape/concat/test_concat.py
+++ pandas/tests/reshape/concat/test_concat.py
@@ -869,3 +869,14 @@ def test_concat_multiindex_with_category():
     )
     expected = expected.set_index(["c1", "c2"])
     tm.assert_frame_equal(result, expected)
+
+
+def test_concat_none_with_timezone_timestamp():
+    # GH#52093
+    df1 = DataFrame([{"A": None}])
+    df2 = DataFrame([{"A": pd.Timestamp("1990-12-20 00:00:00+00:00")}])
+    msg = "The behavior of DataFrame concatenation with empty or all-NA entries"
+    with tm.assert_produces_warning(FutureWarning, match=msg):
+        result = concat([df1, df2], ignore_index=True)
+    expected = DataFrame({"A": [None, pd.Timestamp("1990-12-20 00:00:00+00:00")]})
+    tm.assert_frame_equal(result, expected)
