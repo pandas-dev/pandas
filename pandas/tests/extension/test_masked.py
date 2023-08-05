925299328cea3fcf5e2afea59908c47bf9401387
jbrockmendel <jbrockmendel@gmail.com>
2023-08-05 13:37:07 -0700

    REF: share BooleanArray tests with MaskedArray (#54422)
    * REF: share BooleanArray tests with other Masked cases
    
    * REF: test_masked_numeric->test_masked
    
    * remove unnecessary slicing

--- pandas/tests/extension/test_masked.py
+++ pandas/tests/extension/test_masked.py
@@ -23,6 +23,7 @@ from pandas.compat import (
 
 import pandas as pd
 import pandas._testing as tm
+from pandas.core.arrays.boolean import BooleanDtype
 from pandas.core.arrays.floating import (
     Float32Dtype,
     Float64Dtype,
@@ -65,6 +66,10 @@ def make_float_data():
     )
 
 
+def make_bool_data():
+    return [True, False] * 4 + [np.nan] + [True, False] * 44 + [np.nan] + [True, False]
+
+
 @pytest.fixture(
     params=[
         Int8Dtype,
@@ -77,6 +82,7 @@ def make_float_data():
         UInt64Dtype,
         Float32Dtype,
         Float64Dtype,
+        BooleanDtype,
     ]
 )
 def dtype(request):
@@ -87,6 +93,8 @@ def dtype(request):
 def data(dtype):
     if dtype.kind == "f":
         data = make_float_data()
+    elif dtype.kind == "b":
+        data = make_bool_data()
     else:
         data = make_data()
     return pd.array(data, dtype=dtype)
@@ -94,6 +102,8 @@ def data(dtype):
 
 @pytest.fixture
 def data_for_twos(dtype):
+    if dtype.kind == "b":
+        return pd.array(np.ones(100), dtype=dtype)
     return pd.array(np.ones(100) * 2, dtype=dtype)
 
 
@@ -101,6 +111,8 @@ def data_for_twos(dtype):
 def data_missing(dtype):
     if dtype.kind == "f":
         return pd.array([pd.NA, 0.1], dtype=dtype)
+    elif dtype.kind == "b":
+        return pd.array([np.nan, True], dtype=dtype)
     return pd.array([pd.NA, 1], dtype=dtype)
 
 
@@ -108,6 +120,8 @@ def data_missing(dtype):
 def data_for_sorting(dtype):
     if dtype.kind == "f":
         return pd.array([0.1, 0.2, 0.0], dtype=dtype)
+    elif dtype.kind == "b":
+        return pd.array([True, True, False], dtype=dtype)
     return pd.array([1, 2, 0], dtype=dtype)
 
 
@@ -115,6 +129,8 @@ def data_for_sorting(dtype):
 def data_missing_for_sorting(dtype):
     if dtype.kind == "f":
         return pd.array([0.1, pd.NA, 0.0], dtype=dtype)
+    elif dtype.kind == "b":
+        return pd.array([True, np.nan, False], dtype=dtype)
     return pd.array([1, pd.NA, 0], dtype=dtype)
 
 
@@ -135,10 +151,15 @@ def data_for_grouping(dtype):
         b = 0.1
         a = 0.0
         c = 0.2
+    elif dtype.kind == "b":
+        b = True
+        a = False
+        c = b
     else:
         b = 1
         a = 0
         c = 2
+
     na = pd.NA
     return pd.array([b, b, na, na, a, a, b, c], dtype=dtype)
 
@@ -148,6 +169,23 @@ class TestDtype(base.BaseDtypeTests):
 
 
 class TestArithmeticOps(base.BaseArithmeticOpsTests):
+    def _get_expected_exception(self, op_name, obj, other):
+        try:
+            dtype = tm.get_dtype(obj)
+        except AttributeError:
+            # passed arguments reversed
+            dtype = tm.get_dtype(other)
+
+        if dtype.kind == "b":
+            if op_name.strip("_").lstrip("r") in ["pow", "truediv", "floordiv"]:
+                # match behavior with non-masked bool dtype
+                return NotImplementedError
+            elif op_name in ["__sub__", "__rsub__"]:
+                # exception message would include "numpy boolean subtract""
+                return TypeError
+            return None
+        return super()._get_expected_exception(op_name, obj, other)
+
     def _cast_pointwise_result(self, op_name: str, obj, other, pointwise_result):
         sdtype = tm.get_dtype(obj)
         expected = pointwise_result
@@ -158,6 +196,29 @@ class TestArithmeticOps(base.BaseArithmeticOpsTests):
             else:
                 # combine method result in 'biggest' (int64) dtype
                 expected = expected.astype(sdtype)
+        elif sdtype.kind == "b":
+            if op_name in (
+                "__floordiv__",
+                "__rfloordiv__",
+                "__pow__",
+                "__rpow__",
+                "__mod__",
+                "__rmod__",
+            ):
+                # combine keeps boolean type
+                expected = expected.astype("Int8")
+
+            elif op_name in ("__truediv__", "__rtruediv__"):
+                # combine with bools does not generate the correct result
+                #  (numpy behaviour for div is to regard the bools as numeric)
+                op = self.get_op_from_name(op_name)
+                expected = self._combine(obj.astype(float), other, op)
+                expected = expected.astype("Float64")
+
+            if op_name == "__rpow__":
+                # for rpow, combine does not propagate NaN
+                result = getattr(obj, op_name)(other)
+                expected[result.isna()] = np.nan
         else:
             # combine method result in 'biggest' (float64) dtype
             expected = expected.astype(sdtype)
@@ -167,6 +228,16 @@ class TestArithmeticOps(base.BaseArithmeticOpsTests):
     series_array_exc = None
     frame_scalar_exc = None
     divmod_exc = None
+
+    def test_divmod_series_array(self, data, data_for_twos, request):
+        if data.dtype.kind == "b":
+            mark = pytest.mark.xfail(
+                reason="Inconsistency between floordiv and divmod; we raise for "
+                "floordiv but not for divmod. This matches what we do for "
+                "non-masked bool dtype."
+            )
+            request.node.add_marker(mark)
+        super().test_divmod_series_array(data, data_for_twos)
 
 
 class TestComparisonOps(base.BaseComparisonOpsTests):
@@ -211,7 +282,15 @@ class TestMissing(base.BaseMissingTests):
 
 
 class TestMethods(base.BaseMethodsTests):
-    _combine_le_expected_dtype = object  # TODO: can we make this boolean?
+    def test_combine_le(self, data_repeated):
+        # TODO: patching self is a bad pattern here
+        orig_data1, orig_data2 = data_repeated(2)
+        if orig_data1.dtype.kind == "b":
+            self._combine_le_expected_dtype = "boolean"
+        else:
+            # TODO: can we make this boolean?
+            self._combine_le_expected_dtype = object
+        super().test_combine_le(data_repeated)
 
 
 class TestCasting(base.BaseCastingTests):
@@ -224,7 +303,7 @@ class TestGroupby(base.BaseGroupbyTests):
 
 class TestReduce(base.BaseReduceTests):
     def _supports_reduction(self, obj, op_name: str) -> bool:
-        if op_name in ["any", "all"]:
+        if op_name in ["any", "all"] and tm.get_dtype(obj).kind != "b":
             pytest.skip(reason="Tested in tests/reductions/test_reductions.py")
         return True
 
@@ -237,6 +316,9 @@ class TestReduce(base.BaseReduceTests):
             # Item "dtype[Any]" of "Union[dtype[Any], ExtensionDtype]" has
             # no attribute "numpy_dtype"
             cmp_dtype = ser.dtype.numpy_dtype  # type: ignore[union-attr]
+        elif ser.dtype.kind == "b":
+            if op_name in ["min", "max"]:
+                cmp_dtype = "bool"
 
         if op_name == "count":
             result = getattr(ser, op_name)()
@@ -244,7 +326,7 @@ class TestReduce(base.BaseReduceTests):
         else:
             result = getattr(ser, op_name)(skipna=skipna)
             expected = getattr(ser.dropna().astype(cmp_dtype), op_name)(skipna=skipna)
-            if not skipna and ser.isna().any():
+            if not skipna and ser.isna().any() and op_name not in ["any", "all"]:
                 expected = pd.NA
         tm.assert_almost_equal(result, expected)
 
@@ -261,6 +343,15 @@ class TestReduce(base.BaseReduceTests):
             cmp_dtype = "Int32" if is_windows_or_32bit else "Int64"
         elif tm.is_unsigned_integer_dtype(arr.dtype):
             cmp_dtype = "UInt32" if is_windows_or_32bit else "UInt64"
+        elif arr.dtype.kind == "b":
+            if op_name in ["mean", "median", "var", "std", "skew"]:
+                cmp_dtype = "Float64"
+            elif op_name in ["min", "max"]:
+                cmp_dtype = "boolean"
+            elif op_name in ["sum", "prod"]:
+                cmp_dtype = "Int32" if is_windows_or_32bit else "Int64"
+            else:
+                raise TypeError("not supposed to reach this")
         else:
             raise TypeError("not supposed to reach this")
         return cmp_dtype
@@ -288,6 +379,11 @@ class TestAccumulation(base.BaseAccumulateTests):
             # Incompatible types in assignment (expression has type
             # "Union[dtype[Any], ExtensionDtype]", variable has type "str")
             expected_dtype = ser.dtype  # type: ignore[assignment]
+        elif ser.dtype.kind == "b":
+            if op_name in ("cummin", "cummax"):
+                expected_dtype = "boolean"
+            else:
+                expected_dtype = f"Int{length}"
 
         if op_name == "cumsum":
             result = getattr(ser, op_name)(skipna=skipna)
@@ -319,6 +415,17 @@ class TestAccumulation(base.BaseAccumulateTests):
 
         else:
             raise NotImplementedError(f"{op_name} not supported")
+
+
+class TestUnaryOps(base.BaseUnaryOpsTests):
+    def test_invert(self, data, request):
+        if data.dtype.kind == "f":
+            mark = pytest.mark.xfail(
+                reason="Looks like the base class test implicitly assumes "
+                "boolean/integer dtypes"
+            )
+            request.node.add_marker(mark)
+        super().test_invert(data)
 
 
 class TestPrinting(base.BasePrintingTests):
--- pandas/tests/groupby/test_function.py
+++ pandas/tests/groupby/test_function.py
@@ -1358,6 +1358,29 @@ def test_apply_to_nullable_integer_returns_float(values, function):
     tm.assert_frame_equal(result, expected)
 
 
+@pytest.mark.parametrize("min_count", [0, 10])
+def test_groupby_sum_mincount_boolean(min_count):
+    b = True
+    a = False
+    na = np.nan
+    dfg = pd.array([b, b, na, na, a, a, b], dtype="boolean")
+
+    df = DataFrame({"A": [1, 1, 2, 2, 3, 3, 1], "B": dfg})
+    result = df.groupby("A").sum(min_count=min_count)
+    if min_count == 0:
+        expected = DataFrame(
+            {"B": pd.array([3, 0, 0], dtype="Int64")},
+            index=Index([1, 2, 3], name="A"),
+        )
+        tm.assert_frame_equal(result, expected)
+    else:
+        expected = DataFrame(
+            {"B": pd.array([pd.NA] * 3, dtype="Int64")},
+            index=Index([1, 2, 3], name="A"),
+        )
+        tm.assert_frame_equal(result, expected)
+
+
 def test_groupby_sum_below_mincount_nullable_integer():
     # https://github.com/pandas-dev/pandas/issues/32861
     df = DataFrame({"a": [0, 1, 2], "b": [0, 1, 2], "c": [0, 1, 2]}, dtype="Int64")
