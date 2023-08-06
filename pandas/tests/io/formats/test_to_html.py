2c99270890faa53972d967c3030a6d19edab31b7
Soumyadip Mal <soumyadiptalks@gmail.com>
2023-08-06 19:43:26 +0530

    Fixed failed check

--- pandas/tests/io/formats/test_to_html.py
+++ pandas/tests/io/formats/test_to_html.py
@@ -960,7 +960,7 @@ def test_to_html_tuple_col_with_colspace():
     )
     assert result == expected
 
-def test_to_html_empty_complex_array():
+def test_to_html_empty_complex_array(datapath):
     # GH#54167
     df = DataFrame({"x": np.array([], dtype="complex")})
     result=df.to_html(na_rep="-")
