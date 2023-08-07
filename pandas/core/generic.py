6068ede1b758d3b86ae632b98f4207da03ecc79b
taytzehao <jtth95@gmail.com>
2023-08-07 23:44:55 +0800

    option_context

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -6661,11 +6661,11 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
 
         ** Copy-on-Write is set to true: **
 
-        >>> pd.set_option("mode.copy_on_write", True)
-        >>> s = pd.Series([1, 2], index=["a", "b"])
-        >>> copy = s.copy(deep=False)
-        >>> s.iloc[0] = 100
-        >>> s
+        >>> with pd.option_context("mode.copy_on_write", True):
+        >>>     s = pd.Series([1, 2], index=["a", "b"])
+        >>>     copy = s.copy(deep=False)
+        >>>     s.iloc[0] = 100
+        >>>     s
         a    100
         b      2
         dtype: int64
