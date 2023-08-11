8cce3b28b19b1894f9625512891530dde9ade7f7
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-08-11 15:01:47 +0200

    Revert "DEPS: Bump cython 3.0 (#54335)"
    This reverts commit 4cf63eaefe5ba24acb8d12f67324526447ce0391.

--- pandas/_libs/tslibs/period.pyx
+++ pandas/_libs/tslibs/period.pyx
@@ -2511,7 +2511,7 @@ cdef class _Period(PeriodMixin):
         object_state = None, self.freq, self.ordinal
         return (Period, object_state)
 
-    def strftime(self, fmt: str | None) -> str:
+    def strftime(self, fmt: str) -> str:
         r"""
         Returns a formatted string representation of the :class:`Period`.
 
