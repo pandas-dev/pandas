46c310f445b115fb93141fdd6ff9b489e59c23b9
Omar Elbaz <omarelbaz@omars-air.mynetworksettings.com>
2023-08-10 22:40:54 -0400

    revert commit

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -3159,17 +3159,3 @@ def test_groupby_series_with_datetimeindex_month_name():
     expected = Series([2, 1], name="jan")
     expected.index.name = "jan"
     tm.assert_series_equal(result, expected)
-
-
-def test_rolling_corr_case2(df):
-    # GH 44078
-    df = DataFrame({"a": [(1, 2), (1, 2), (1, 2)], "b": [4, 5, 6]})
-    gb = df.groupby(["a"])
-    result = gb.rolling(2).corr(other=df)
-    index = MultiIndex.from_tuples(
-        [((1, 2), 0), ((1, 2), 1), ((1, 2), 2)], names=["a", None]
-    )
-    expected = DataFrame(
-        {"a": [np.nan, np.nan, np.nan], "b": [np.nan, 1.0, 1.0]}, index=index
-    )
-    tm.assert_frame_equal(result, expected)
