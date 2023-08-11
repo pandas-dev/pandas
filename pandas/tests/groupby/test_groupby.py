4e4763bb6667e6bf8126e370905be19d72f7217a
Omar Elbaz <omarelbaz@omars-air.mynetworksettings.com>
2023-08-10 23:04:48 -0400

    u_test update

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -3175,10 +3175,6 @@ def test_rolling_corr_case2(df):
     df = DataFrame({"a": [(1, 2), (1, 2), (1, 2)], "b": [4, 5, 6]})
     gb = df.groupby(["a"])
     result = gb.rolling(2).corr(other=df)
-    index = MultiIndex.from_tuples(
-        [((1, 2), 0), ((1, 2), 1), ((1, 2), 2)], names=["a", None]
-    )
-    expected = DataFrame(
-        {"a": [np.nan, np.nan, np.nan], "b": [np.nan, 1.0, 1.0]}, index=index
-    )
+    index = MultiIndex.from_tuples([((1, 2), 0), ((1, 2), 1), ((1, 2), 2)], names=["a", None])
+    expected = DataFrame({"a": [np.nan, np.nan, np.nan], "b": [np.nan, 1.0, 1.0]}, index=index)
     tm.assert_frame_equal(result, expected)
