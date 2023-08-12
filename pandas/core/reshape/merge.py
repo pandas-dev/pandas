4115867aca66312fa6a32edf8581ad1279e170d9
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-08-12 09:34:52 +0200

    Revert "Speed up StringDtype arrow implementation"
    This reverts commit 81d9fb5777bdc8d403f3c97309c27dc67eaae3ff.

--- pandas/core/reshape/merge.py
+++ pandas/core/reshape/merge.py
@@ -76,7 +76,6 @@ from pandas.core.dtypes.missing import (
     na_value_for_dtype,
 )
 
-import pandas as pd
 from pandas import (
     ArrowDtype,
     Categorical,
@@ -2408,20 +2407,13 @@ def _factorize_keys(
                 or is_string_dtype(lk.dtype)
                 and not sort
             )
-            or is_string_dtype(lk.dtype)
-            and lk.dtype.storage == "pyarrow"
         ):
             lk, _ = lk._values_for_factorize()
 
             # error: Item "ndarray" of "Union[Any, ndarray]" has no attribute
             # "_values_for_factorize"
             rk, _ = rk._values_for_factorize()  # type: ignore[union-attr]
-        elif (
-            isinstance(lk.dtype, ArrowDtype)
-            and is_string_dtype(lk.dtype)
-            or isinstance(lk.dtype, pd.StringDtype)
-            and lk.dtype.storage == "pyarrow"
-        ):
+        elif isinstance(lk.dtype, ArrowDtype) and is_string_dtype(lk.dtype):
             import pyarrow as pa
             import pyarrow.compute as pc
 
