9be06becef5306b619f9f0b18f61ccc52ba59123
MarcoGorelli <33491632+MarcoGorelli@users.noreply.github.com>
2023-08-13 11:09:13 +0100

    wip

--- pandas/core/internals/blocks.py
+++ pandas/core/internals/blocks.py
@@ -60,6 +60,7 @@ from pandas.core.dtypes.cast import (
 from pandas.core.dtypes.common import (
     ensure_platform_int,
     is_1d_only_ea_dtype,
+    is_integer_dtype,
     is_list_like,
     is_string_dtype,
 )
@@ -453,6 +454,10 @@ class Block(PandasObject):
         we can also safely try to coerce to the same dtype
         and will receive the same block
         """
+        if isna(other) and is_integer_dtype(self.values.dtype):
+            # In a future version of pandas, the default will be that
+            # setting `nan` into an integer series won't raise.
+            warn_on_upcast = False
         new_dtype = find_result_type(self.values.dtype, other)
         if warn_on_upcast:
             warnings.warn(
