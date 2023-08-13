85b3ca1e9cce43114da13abe6c8845f618180858
Luke Manley <lukemanley@gmail.com>
2023-08-13 08:47:37 -0400

    PERF: axis=1 reductions with EA dtypes (#54341)

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -105,6 +105,7 @@ from pandas.core.arrays import (
     ExtensionArray,
     FloatingArray,
     IntegerArray,
+    SparseArray,
 )
 from pandas.core.base import (
     PandasObject,
@@ -1909,7 +1910,10 @@ class GroupBy(BaseGroupBy[NDFrameT]):
                 # and non-applicable functions
                 # try to python agg
                 # TODO: shouldn't min_count matter?
-                if how in ["any", "all", "std", "sem"]:
+                # TODO: avoid special casing SparseArray here
+                if how in ["any", "all"] and isinstance(values, SparseArray):
+                    pass
+                elif how in ["any", "all", "std", "sem"]:
                     raise  # TODO: re-raise as TypeError?  should not be reached
             else:
                 return result
