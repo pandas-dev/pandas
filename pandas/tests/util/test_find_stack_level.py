3ac7e2684b411378904d125730c5e7dc94509768
Louis Maddox <louismmx@gmail.com>
2023-08-18 22:39:41 +0100

    test: simplify test and correct value (direct call stack level is 1 not 0)

--- pandas/tests/util/test_find_stack_level.py
+++ pandas/tests/util/test_find_stack_level.py
@@ -3,18 +3,12 @@ from pytest import mark
 from pandas.util._exceptions import find_stack_level
 
 
-@mark.parametrize("above,below", [(0, 1)])
-def test_find_stack_level(above, below):
+@mark.parametrize("expected", [1])
+def test_find_stack_level(expected):
     """
     Note that this test would not be expected to pass on CPython implementations which
     don't support getting the frame with currentframe (which would always return None).
     """
 
-    def nested_call() -> int:
-        return find_stack_level()
-
     top_lvl = find_stack_level()
-    assert top_lvl == above, f"Expected stack level {above} but got {top_lvl}"
-
-    sub_lvl = nested_call()
-    assert sub_lvl == below, f"Expected stack level {below} but got {sub_lvl}"
+    assert top_lvl == expected, f"Expected stack level {expected} but got {top_lvl}"
