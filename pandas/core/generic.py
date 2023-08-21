715e4b8a29d5b5ab18e207d48111a5dd06af9a02
RajatS Mukherjee <raromukherjee@gmail.com>
2023-08-21 19:14:26 +0000

    deprecated nonkeyword arguments

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -3098,6 +3098,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         )
 
     @final
+    @deprecate_nonkeyword_arguments(version="3.0", allowed_args=["self"], name="to_csv")
     def to_clipboard(
         self, *, excel: bool_t = True, sep: str | None = None, **kwargs
     ) -> None:
