45c2f98e5c591b6b4c6adb2a3e0b0fe1f2148a4a
Godwill Agbehonou <godwill.personal@gmail.com>
2023-08-22 08:22:58 -0400

    Updated _validate_setitem_value

--- pandas/core/arrays/numpy_.py
+++ pandas/core/arrays/numpy_.py
@@ -509,16 +509,41 @@ class NumpyExtensionArray(  # type: ignore[misc]
 
     def _validate_setitem_value(self, value):
         if type(value) == int:
-            val = np.power(value, 1, dtype=np.int64)  # for int64 only
+            if (
+                self.dtype == NumpyEADtype("int64")
+                or self.dtype == NumpyEADtype("float64")
+                or self.dtype == NumpyEADtype("uint16")
+                or self.dtype == NumpyEADtype("object")
+                or self.dtype is None
+            ):
+                return value
         elif type(value) == float:
-            val = np.power(value, 1, dtype=np.float64)  # for float64 only
+            if (
+                self.dtype == NumpyEADtype("float64")
+                or self.dtype == NumpyEADtype("object")
+                or self.dtype is None
+            ):
+                return value
+        elif type(value) == str:
+            if (
+                self.dtype == NumpyEADtype("str")
+                or self.dtype == NumpyEADtype("object")
+                or self.dtype is None
+                or self.dtype == NumpyEADtype("U32")
+            ):
+                return value
+        elif NumpyEADtype(type(value)) == NumpyEADtype(self.dtype) or NumpyEADtype(
+            type(value)
+        ) == NumpyEADtype(type(value)):
+            return value
         else:
-            val = np.asarray([value], dtype=object)
-        if NumpyEADtype(type(val)) != self.dtype:
+            if self.dtype == NumpyEADtype("object") or self.dtype is None:
+                return value
+        if NumpyEADtype(type(value)) != self.dtype:
             raise TypeError(
                 "value cannot be inserted without changing the dtype. value:"
-                f"{val}, type(value): {type(val)}, NumpyEADtype(type(value)):"
-                f"  {NumpyEADtype(type(val))}, self.dtype:  {self.dtype}"
+                f"{value}, type(value): {type(value)}, NumpyEADtype(type(value)):"
+                f"  {NumpyEADtype(type(value))}, self.dtype:  {self.dtype}"
             )
         return value
 
