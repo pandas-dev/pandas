d295d1669ade2d67b1141a2f9ea5026fafd09c01
Natalia Mokeeva <natalia.mokeeva.de@gmail.com>
2023-08-26 21:50:26 +0200

    remove redundant catch_warnings

--- pandas/tests/arrays/test_datetimelike.py
+++ pandas/tests/arrays/test_datetimelike.py
@@ -47,12 +47,7 @@ def period_index(freqstr):
     the PeriodIndex behavior.
     """
     # TODO: non-monotone indexes; NaTs, different start dates
-    with warnings.catch_warnings():
-        # suppress deprecation of Period[B]
-        warnings.filterwarnings(
-            "ignore", message="Period with BDay freq", category=FutureWarning
-        )
-        pi = pd.period_range(start=Timestamp("2000-01-01"), periods=100, freq=freqstr)
+    pi = pd.period_range(start=Timestamp("2000-01-01"), periods=100, freq=freqstr)
     return pi
 
 
