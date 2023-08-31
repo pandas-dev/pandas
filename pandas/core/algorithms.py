7c1620ee67e0fe4e550ed5e4a6e58a8ef0b2a0b9
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-08-31 23:27:13 +0200

    REGR: drop_duplicates raising for arrow strings

--- pandas/core/algorithms.py
+++ pandas/core/algorithms.py
@@ -998,7 +998,7 @@ def duplicated(
     duplicated : ndarray[bool]
     """
     if hasattr(values, "dtype"):
-        if isinstance(values.dtype, ArrowDtype):
+        if isinstance(values.dtype, ArrowDtype) and values.dtype.kind in "ifub":
             values = values._to_masked()  # type: ignore[union-attr]
 
         if isinstance(values.dtype, BaseMaskedDtype):
