bb99ea07868a3fa999cbb421994d44cc25d5480d
Luke Manley <lukemanley@gmail.com>
2023-08-31 09:54:06 -0400

    Backport PR #54885: REGR: setitem with part of a MultiIndex raises

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -2460,12 +2460,12 @@ class MultiIndex(Index):
     def _recode_for_new_levels(
         self, new_levels, copy: bool = True
     ) -> Generator[np.ndarray, None, None]:
-        if len(new_levels) != self.nlevels:
+        if len(new_levels) > self.nlevels:
             raise AssertionError(
                 f"Length of new_levels ({len(new_levels)}) "
-                f"must be same as self.nlevels ({self.nlevels})"
+                f"must be <= self.nlevels ({self.nlevels})"
             )
-        for i in range(self.nlevels):
+        for i in range(len(new_levels)):
             yield recode_for_categories(
                 self.codes[i], self.levels[i], new_levels[i], copy=copy
             )
