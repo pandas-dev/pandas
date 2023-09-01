c9b86dc5376380c05bce7ef183d9c977812876a4
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-08-31 17:00:09 -0700

    DOC: Mark contributors.py as not parallel safe

--- doc/sphinxext/contributors.py
+++ doc/sphinxext/contributors.py
@@ -54,4 +54,4 @@ class ContributorsDirective(Directive):
 def setup(app):
     app.add_directive("contributors", ContributorsDirective)
 
-    return {"version": "0.1", "parallel_read_safe": True, "parallel_write_safe": True}
+    return {"version": "0.1", "parallel_read_safe": False, "parallel_write_safe": False}
