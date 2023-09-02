84e8609cfb3f839673747e914d40e6a19d3d3aee
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2023-09-02 14:30:50 -0700

    Backport PR #54826 on branch 2.1.x (Infer large_string type as pyarrow_numpy strings) (#54969)
    Backport PR #54826: Infer large_string type as pyarrow_numpy strings
    
    Co-authored-by: Patrick Hoefler <61934744+phofl@users.noreply.github.com>

--- pandas/tests/io/test_parquet.py
+++ pandas/tests/io/test_parquet.py
@@ -1125,6 +1125,25 @@ class TestParquetPyArrow(Base):
         expected = pd.DataFrame({"a": ["123"]}, dtype="string[python]")
         tm.assert_frame_equal(result, expected)
 
+    def test_infer_string_large_string_type(self, tmp_path, pa):
+        # GH#54798
+        import pyarrow as pa
+        import pyarrow.parquet as pq
+
+        path = tmp_path / "large_string.p"
+
+        table = pa.table({"a": pa.array([None, "b", "c"], pa.large_string())})
+        pq.write_table(table, path)
+
+        with pd.option_context("future.infer_string", True):
+            result = read_parquet(path)
+        expected = pd.DataFrame(
+            data={"a": [None, "b", "c"]},
+            dtype="string[pyarrow_numpy]",
+            columns=pd.Index(["a"], dtype="string[pyarrow_numpy]"),
+        )
+        tm.assert_frame_equal(result, expected)
+
 
 class TestParquetFastParquet(Base):
     def test_basic(self, fp, df_full):
