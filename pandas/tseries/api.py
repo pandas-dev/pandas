aa65a914e331d4adaae5091b67ae53a8fe23b312
Donald Thevalingam <donaldthevalingam@Donalds-MacBook-Pro.local>
2023-09-05 21:41:10 -0500

    included guess_datetime_format in api

--- pandas/tseries/api.py
+++ pandas/tseries/api.py
@@ -4,5 +4,6 @@ Timeseries API
 
 from pandas.tseries import offsets
 from pandas.tseries.frequencies import infer_freq
+from pandas._libs.tslibs.parsing import guess_datetime_format
 
-__all__ = ["infer_freq", "offsets"]
+__all__ = ["infer_freq", "offsets", "guess_datetime_format"]
