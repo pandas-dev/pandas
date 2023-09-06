020103acf0dab330a92adf6095ef5a58c3bf26a5
Donald Thevalingam <donaldthevalingam@Donalds-MacBook-Pro.local>
2023-09-05 21:39:40 -0500

    updating init to include guess_datetime_format func

--- pandas/_libs/tslibs/__init__.py
+++ pandas/_libs/tslibs/__init__.py
@@ -33,6 +33,7 @@ __all__ = [
     "is_supported_unit",
     "npy_unit_to_abbrev",
     "get_supported_reso",
+    "guess_datetime_format",
 ]
 
 from pandas._libs.tslibs import dtypes  # pylint: disable=import-self
@@ -62,6 +63,9 @@ from pandas._libs.tslibs.offsets import (
     BaseOffset,
     Tick,
     to_offset,
+)
+from pandas._libs.tslibs.parsing import (
+    guess_datetime_format,
 )
 from pandas._libs.tslibs.period import (
     IncompatibleFrequency,
