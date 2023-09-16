5fb97d0e9c5c6fbcecf0d2e30e4d2d3ff6f20e76
Krunal6767 <106827625+Krunal6767@users.noreply.github.com>
2023-09-16 01:09:34 -0400

    Update __init__.py

--- pandas/api/types/__init__.py
+++ pandas/api/types/__init__.py
@@ -3,15 +3,14 @@ Public toolkit API.
 """
 
 from pandas._libs.lib import infer_dtype
-
-from pandas.core.dtypes.api import *  # noqa: F403
-from pandas.core.dtypes.concat import union_categoricals
 from pandas.core.dtypes.dtypes import (
     CategoricalDtype,
     DatetimeTZDtype,
     IntervalDtype,
     PeriodDtype,
 )
+from pandas.core.dtypes.concat import union_categoricals
+from pandas.core.dtypes.api import *  # noqa: F403
 
 __all__ = [
     "infer_dtype",
