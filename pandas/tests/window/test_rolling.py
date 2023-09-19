dede3b60c0dc51dd7526fc2d0793ac6af740ea94
Hadi Abdi Khojasteh <hadiabdikhojasteh@gmail.com>
2023-09-19 20:59:39 +0200

    Update test_rolling.py
    the df variable name fixed in the test

--- pandas/tests/window/test_rolling.py
+++ pandas/tests/window/test_rolling.py
@@ -1954,7 +1954,7 @@ def test_numeric_only_corr_cov_series(kernel, use_arg, numeric_only, dtype):
 
 def test_rolling_rolling_sum_window_microseconds_confilict_timestamp():
     # GH#55106
-    df = DataFrame({"A": range(5)}, index=date_range("2013-01-01", freq="1s", periods=5))
+    df_time = DataFrame({"A": range(5)}, index=date_range("2013-01-01", freq="1s", periods=5))
     sum_in_nanosecs = df_time.rolling("1s").sum()
     # micro seconds / milliseconds should not breaks the correct rolling
     df_time.index = df_time.index.as_unit("us")
