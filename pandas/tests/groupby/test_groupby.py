28aa1e5b10bfbc0d2ed6f3fe0f190855129f8d06
José Lucas Silva Mayer <josemayer@usp.br>
2023-10-08 01:00:25 -0300

    remove check of dataframe dtype on assert
    Signed-off-by: José Lucas Silva Mayer <josemayer@usp.br>

--- pandas/tests/groupby/test_groupby.py
+++ pandas/tests/groupby/test_groupby.py
@@ -3249,12 +3249,8 @@ def test_get_group_axis_1():
 
 def test_groupby_ffill_with_duplicated_index():
     # GH#43412
-    df = DataFrame(
-        {"a": [1, 2, 3, 4, np.nan, np.nan]}, index=[0, 1, 2, 0, 1, 2], dtype=float
-    )
+    df = DataFrame({"a": [1, 2, 3, 4, np.nan, np.nan]}, index=[0, 1, 2, 0, 1, 2])
 
     result = df.groupby(level=0).ffill()
-    expected = DataFrame(
-        {"a": [1, 2, 3, 4, 2, 3]}, index=[0, 1, 2, 0, 1, 2], dtype=float
-    )
-    tm.assert_frame_equal(result, expected)
+    expected = DataFrame({"a": [1, 2, 3, 4, 2, 3]}, index=[0, 1, 2, 0, 1, 2])
+    tm.assert_frame_equal(result, expected, check_dtype=False)
