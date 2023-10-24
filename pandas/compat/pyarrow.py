fe1bc37026f351bea7a70d2bb6cc0a5d3018315c
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-10-24 22:27:53 +0200

    Fix

--- pandas/compat/pyarrow.py
+++ pandas/compat/pyarrow.py
@@ -7,7 +7,6 @@ from pandas.util.version import Version
 try:
     import pyarrow as pa
 
-    pa_installed = True
     _palv = Version(Version(pa.__version__).base_version)
     pa_version_under10p1 = _palv < Version("10.0.1")
     pa_version_under11p0 = _palv < Version("11.0.0")
@@ -15,7 +14,6 @@ try:
     pa_version_under13p0 = _palv < Version("13.0.0")
     pa_version_under14p0 = _palv < Version("14.0.0")
 except ImportError:
-    pa_installed = False
     pa_version_under10p1 = True
     pa_version_under11p0 = True
     pa_version_under12p0 = True
