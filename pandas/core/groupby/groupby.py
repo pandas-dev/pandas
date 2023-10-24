75638a56bcc60d7346dce695f12580ce68755426
Richard Shadrach <rhshadrach@gmail.com>
2023-10-24 17:20:03 -0400

    Avoid _maybe_mask_result

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -5845,8 +5845,6 @@ class GroupBy(BaseGroupBy[NDFrameT]):
             if isinstance(index, MultiIndex):
                 index = index.to_flat_index()
             values = res._values
-            if isinstance(values, ExtensionArray):
-                values = values.to_numpy(copy=False)
             na_value = na_value_for_dtype(index.dtype, compat=False)
             if isinstance(res, Series):
                 # mypy: expression has type "Series", variable has type "NDFrameT"
