7a65b966053d5164dc94a3f7c2572b11d9915b9c
Will Ayd <william.ayd@icloud.com>
2023-10-25 16:58:13 -0400

    try PyLong_AsVoidPtr

--- pandas/_libs/buffer.pyx
+++ pandas/_libs/buffer.pyx
@@ -1,18 +1,17 @@
-from cpython cimport Py_buffer
-from libc.stdint cimport (
-    uint8_t,
-    uintptr_t,
+from cpython cimport (
+    Py_buffer,
+    PyLong_AsVoidPtr,
 )
+from libc.stdint cimport uint8_t
 
 
 cdef class CBuffer:
     def __getbuffer__(self, Py_buffer *buffer, int flags):
         cdef Py_ssize_t itemsize = sizeof(uint8_t)
-        cdef uintptr_t ptr = self.ptr
         cdef Py_ssize_t[1] shape = tuple((self.bufsize // itemsize,))
         cdef Py_ssize_t[1] strides = tuple((itemsize,))
 
-        buffer.buf = <void*>ptr
+        buffer.buf = PyLong_AsVoidPtr(self.ptr)
         # assumes sizeof(unsigned char) == sizeof(uint8_t)
         # TODO: use C11 static_assert macro in Cython
         buffer.format = "@B"
