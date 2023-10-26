9c7b5677efe10bef70ed34f207f7ef996503afff
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-10-26 17:38:40 +0200

    Backport PR #54922 on branch 2.1.x (REGR: Restore _constructor_from_mgr to pass manager object to constructor) (#55704)
    Backport PR #54922: REGR: Restore _constructor_from_mgr to pass manager object to constructor

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -689,6 +689,9 @@ class TestMerge:
         )[["i1", "i2", "i1_", "i3"]]
         tm.assert_frame_equal(result, expected)
 
+    @pytest.mark.filterwarnings(
+        "ignore:Passing a BlockManager|Passing a SingleBlockManager:DeprecationWarning"
+    )
     def test_merge_type(self, df, df2):
         class NotADataFrame(DataFrame):
             @property
