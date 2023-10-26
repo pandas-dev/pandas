4eac59004b20ea5bb2fdb299334147c34e267be0
hvsesha <65343617+hvsesha@users.noreply.github.com>
2023-10-26 15:20:40 +0800

    ISSUE # 51044
    Added _validate_setitem_Value

--- pandas/core/arrays/numpy_.py
+++ pandas/core/arrays/numpy_.py
@@ -564,3 +564,30 @@ class NumpyExtensionArray(  # type: ignore[misc]
     # ------------------------------------------------------------------------
     # String methods interface
     _str_na_value = np.nan
+    def _validate_setitem_value(self, value):
+        """
+        Check if we have a scalar that we can cast losslessly.
+
+        Raises
+        ------
+        TypeError
+        """
+        kind = self.dtype.kind
+        # TODO: get this all from np_can_hold_element?
+        if kind == "b":
+            if lib.is_bool(value):
+                return value
+
+        elif kind == "f":
+            if lib.is_integer(value) or lib.is_float(value):
+                return value
+
+        else:
+            if lib.is_integer(value) or (lib.is_float(value) and value.is_integer()):
+                return value
+            # TODO: unsigned checks
+
+        # Note: without the "str" here, the f-string rendering raises in
+        #  py38 builds.
+        raise TypeError(f"Invalid value '{str(value)}' for dtype {self.dtype}")
+
