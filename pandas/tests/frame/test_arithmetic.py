f36a67db6ad670becd5fb1688de895a82c88826a
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-10-28 15:18:59 -0700

    Remove redundant test

--- pandas/tests/frame/test_arithmetic.py
+++ pandas/tests/frame/test_arithmetic.py
@@ -26,7 +26,6 @@ from pandas.tests.frame.common import (
     _check_mixed_float,
     _check_mixed_int,
 )
-from pandas.util.version import Version
 
 
 @pytest.fixture(autouse=True, params=[0, 100], ids=["numexpr", "python"])
@@ -496,41 +495,6 @@ class TestFrameFlexArithmetic:
         tm.assert_frame_equal(result, expected)
 
         result2 = df.floordiv(ser.values, axis=0)
-        tm.assert_frame_equal(result2, expected)
-
-    @pytest.mark.parametrize("opname", ["floordiv", "pow"])
-    def test_floordiv_axis0_numexpr_path(
-        self, opname, request, switch_numexpr_min_elements
-    ):
-        # case that goes through numexpr and has to fall back to masked_arith_op
-        ne = pytest.importorskip("numexpr")
-        if (
-            Version(ne.__version__) >= Version("2.8.7")
-            and opname == "pow"
-            and "python" in request.node.callspec.id
-        ):
-            request.applymarker(
-                pytest.mark.xfail(reason="https://github.com/pydata/numexpr/issues/454")
-            )
-
-        op = getattr(operator, opname)
-
-        arr = (
-            np.arange(switch_numexpr_min_elements + 10).reshape(
-                switch_numexpr_min_elements // 10 + 1, -1
-            )
-            * 100
-        )
-        df = DataFrame(arr)
-        df["C"] = 1.0
-
-        ser = df[0]
-        result = getattr(df, opname)(ser, axis=0)
-
-        expected = DataFrame({col: op(df[col], ser) for col in df.columns})
-        tm.assert_frame_equal(result, expected)
-
-        result2 = getattr(df, opname)(ser.values, axis=0)
         tm.assert_frame_equal(result2, expected)
 
     def test_df_add_td64_columnwise(self):
