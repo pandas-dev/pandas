e6c9f11b3381207ddc2ef5840b9cd927242f3821
Brock <jbrockmendel@gmail.com>
2023-10-29 11:56:52 -0700

    misplaced tests

--- pandas/tests/frame/test_repr.py
+++ pandas/tests/frame/test_repr.py
@@ -12,6 +12,7 @@ from pandas import (
     Categorical,
     CategoricalIndex,
     DataFrame,
+    IntervalIndex,
     MultiIndex,
     NaT,
     PeriodIndex,
@@ -306,6 +307,21 @@ NaT   4"""
 
         # GH 12182
         assert df._repr_latex_() is None
+
+    def test_repr_with_datetimeindex(self):
+        df = DataFrame({"A": [1, 2, 3]}, index=date_range("2000", periods=3))
+        result = repr(df)
+        expected = "            A\n2000-01-01  1\n2000-01-02  2\n2000-01-03  3"
+        assert result == expected
+
+    def test_repr_with_intervalindex(self):
+        # https://github.com/pandas-dev/pandas/pull/24134/files
+        df = DataFrame(
+            {"A": [1, 2, 3, 4]}, index=IntervalIndex.from_breaks([0, 1, 2, 3, 4])
+        )
+        result = repr(df)
+        expected = "        A\n(0, 1]  1\n(1, 2]  2\n(2, 3]  3\n(3, 4]  4"
+        assert result == expected
 
     def test_repr_with_categorical_index(self):
         df = DataFrame({"A": [1, 2, 3]}, index=CategoricalIndex(["a", "b", "c"]))
