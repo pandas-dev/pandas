cd1b31507403bae120568dfd56c0efc9431693e2
Rohan Jain <rohanjain@microsoft.com>
2023-10-31 12:36:31 -0400

    non list test

--- pandas/tests/series/accessors/test_list_accessor.py
+++ pandas/tests/series/accessors/test_list_accessor.py
@@ -68,6 +68,21 @@ def test_list_getitem_slice_invalid():
         ser.list[1:None:0]
 
 
+def test_list_accessor_non_list_dtype():
+    ser = Series(
+        [1, 2, 4],
+        dtype=ArrowDtype(pa.int64()),
+    )
+    with pytest.raises(
+        AttributeError,
+        match=re.escape(
+            "Can only use the '.list' accessor with 'list[pyarrow]' dtype, "
+            "not int64[pyarrow]."
+        ),
+    ):
+        ser.list[1:None:0]
+
+
 @pytest.mark.parametrize(
     "list_dtype",
     (
