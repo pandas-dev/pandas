7592e3423c0d2fdaefffb98f16f058a4248dc3df
Will Ayd <william.ayd@icloud.com>
2023-10-31 21:53:10 -0400

    try changing buf reference to numpy array

--- pandas/_libs/buffer.pyx
+++ pandas/_libs/buffer.pyx
@@ -6,25 +6,20 @@ from libc.stdint cimport (
 
 
 cdef class CBuffer:
-    cdef intptr_t bufaddr
-
     def __getbuffer__(self, Py_buffer *buffer, int flags):
         cdef Py_ssize_t itemsize = sizeof(uint8_t)
         cdef Py_ssize_t[1] shape = tuple((self.bufsize // itemsize,))
         cdef Py_ssize_t[1] strides = tuple((itemsize,))
-        self.bufaddr = self.ptr
-        buffer.buf = <void*>self.bufaddr
+        cdef intptr_t bufaddr = self.ptr
+        buffer.buf = <void*>bufaddr
         # assumes sizeof(unsigned char) == sizeof(uint8_t)
         # TODO: use C11 static_assert macro in Cython
         buffer.format = "B"
         buffer.itemsize = itemsize
         buffer.len = self.bufsize
         buffer.ndim = 1
-        buffer.obj = self
+        buffer.obj = self._x
         buffer.readonly = 1
         buffer.shape = shape
         buffer.strides = strides
         buffer.suboffsets = NULL
-
-    def __releasebuffer__(self, Py_buffer *buffer):
-        pass
