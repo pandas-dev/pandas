dfc83f9351a0cf6df0d4153007e5b381c6d77ffc
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-11-04 22:51:03 -0400

    Fix mypy

--- pandas/_testing/__init__.py
+++ pandas/_testing/__init__.py
@@ -1048,14 +1048,14 @@ def shares_memory(left, right) -> bool:
     if (
         isinstance(left, ExtensionArray)
         and is_string_dtype(left.dtype)
-        and left.dtype.storage in ("pyarrow", "pyarrow_numpy")
+        and left.dtype.storage in ("pyarrow", "pyarrow_numpy")  # type: ignore[attr-defined]  # noqa: E501
     ):
         # https://github.com/pandas-dev/pandas/pull/43930#discussion_r736862669
         left = cast("ArrowExtensionArray", left)
         if (
             isinstance(right, ExtensionArray)
             and is_string_dtype(right.dtype)
-            and right.dtype.storage in ("pyarrow", "pyarrow_numpy")
+            and right.dtype.storage in ("pyarrow", "pyarrow_numpy")  # type: ignore[attr-defined]  # noqa: E501
         ):
             right = cast("ArrowExtensionArray", right)
             left_pa_data = left._pa_array
