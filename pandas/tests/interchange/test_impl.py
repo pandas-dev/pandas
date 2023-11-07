d2ddf8e633abf6a3f8322b3f622f51f194385985
Marco Edward Gorelli <marcogorelli@protonmail.com>
2023-11-07 12:06:55 +0000

    BUG: Interchange object data buffer has the wrong dtype / from_dataframe incorrect  (#55227)
    * use buffer dtype in interchange from_dataframe
    
    * wip
    
    * wip
    
    * add failing test
    
    * wip
    
    * simplify
    
    * remove unnecessary assertion
    
    * dont double-extract bit width
    
    * Update pandas/core/interchange/from_dataframe.py
    
    Co-authored-by: Stijn de Gooijer <stijn@degooijer.io>
    
    ---------
    
    Co-authored-by: Stijn de Gooijer <stijn@degooijer.io>

--- pandas/tests/interchange/test_impl.py
+++ pandas/tests/interchange/test_impl.py
@@ -18,6 +18,7 @@ from pandas.core.interchange.dataframe_protocol import (
     DtypeKind,
 )
 from pandas.core.interchange.from_dataframe import from_dataframe
+from pandas.core.interchange.utils import ArrowCTypes
 
 
 @pytest.fixture
@@ -340,3 +341,24 @@ def test_interchange_from_non_pandas_tz_aware(request):
         dtype="datetime64[us, Asia/Kathmandu]",
     )
     tm.assert_frame_equal(expected, result)
+
+
+def test_interchange_from_corrected_buffer_dtypes(monkeypatch) -> None:
+    # https://github.com/pandas-dev/pandas/issues/54781
+    df = pd.DataFrame({"a": ["foo", "bar"]}).__dataframe__()
+    interchange = df.__dataframe__()
+    column = interchange.get_column_by_name("a")
+    buffers = column.get_buffers()
+    buffers_data = buffers["data"]
+    buffer_dtype = buffers_data[1]
+    buffer_dtype = (
+        DtypeKind.UINT,
+        8,
+        ArrowCTypes.UINT8,
+        buffer_dtype[3],
+    )
+    buffers["data"] = (buffers_data[0], buffer_dtype)
+    column.get_buffers = lambda: buffers
+    interchange.get_column_by_name = lambda _: column
+    monkeypatch.setattr(df, "__dataframe__", lambda allow_copy: interchange)
+    pd.api.interchange.from_dataframe(df)
