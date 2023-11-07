8c52003df4e00618f53cc4b58916417b6ad6b6a5
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-11-06 15:16:58 -1000

    TST: Parameterize & make tests more performant (#55830)
    * use _SIZE_CUTOFF in test_indexer_caching
    
    * use _SIZE_CUTOFF in test_loc_getitem_large_series
    
    * Parameterize test_align_fill_method
    
    * Mark numba engine as single_cpu
    
    * Reduce data size for test_concat_copies
    
    * Use _SIZE_CUTOFF in test_large_mi_contains
    
    * Parameterize
    
    * Parameterize test_round_trip_current
    
    * Remove str call
    
    * Param over limit

--- pandas/tests/indexing/interval/test_interval.py
+++ pandas/tests/indexing/interval/test_interval.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytest
 
+from pandas._libs import index as libindex
 from pandas.compat import IS64
 
 import pandas as pd
@@ -72,15 +73,18 @@ class TestIntervalIndex:
         with pytest.raises(KeyError, match=r"\[-1\] not in index"):
             indexer_sl(ser)[[-1, 3]]
 
-    @pytest.mark.slow
-    def test_loc_getitem_large_series(self):
-        ser = Series(
-            np.arange(1000000), index=IntervalIndex.from_breaks(np.arange(1000001))
-        )
+    def test_loc_getitem_large_series(self, monkeypatch):
+        size_cutoff = 20
+        with monkeypatch.context():
+            monkeypatch.setattr(libindex, "_SIZE_CUTOFF", size_cutoff)
+            ser = Series(
+                np.arange(size_cutoff),
+                index=IntervalIndex.from_breaks(np.arange(size_cutoff + 1)),
+            )
 
-        result1 = ser.loc[:80000]
-        result2 = ser.loc[0:80000]
-        result3 = ser.loc[0:80000:1]
+            result1 = ser.loc[:8]
+            result2 = ser.loc[0:8]
+            result3 = ser.loc[0:8:1]
         tm.assert_series_equal(result1, result2)
         tm.assert_series_equal(result1, result3)
 
