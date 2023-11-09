43ed1a901707de025208bd38a23fa395df961d97
Isaac Virshup <ivirshup@gmail.com>
2023-11-09 08:55:00 +0100

    Backport PR #55764: REGR: fix return class in _constructor_from_mgr for simple subclasses

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -641,7 +641,7 @@ class DataFrame(NDFrame, OpsMixin):
     def _constructor_from_mgr(self, mgr, axes):
         if self._constructor is DataFrame:
             # we are pandas.DataFrame (or a subclass that doesn't override _constructor)
-            return self._from_mgr(mgr, axes=axes)
+            return DataFrame._from_mgr(mgr, axes=axes)
         else:
             assert axes is mgr.axes
             return self._constructor(mgr)
