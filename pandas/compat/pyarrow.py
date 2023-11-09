24aa3f3ce533f0b0eb1aefefa07bcb88898331c1
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2023-11-09 22:22:04 +0100

    Parquet/Feather IO: disable PyExtensionType autoload (#55894)
    * Parquet/Feather IO: disable PyExtensionType autoload
    
    * don't install hotfix for pyarrow >= 14.0.1
    
    * move patching to extension type definitions
    
    * expand error message
    
    * fix compat for pyarrow not installed
    
    * add whatsnew

--- pandas/compat/pyarrow.py
+++ pandas/compat/pyarrow.py
@@ -13,9 +13,11 @@ try:
     pa_version_under12p0 = _palv < Version("12.0.0")
     pa_version_under13p0 = _palv < Version("13.0.0")
     pa_version_under14p0 = _palv < Version("14.0.0")
+    pa_version_under14p1 = _palv < Version("14.0.1")
 except ImportError:
     pa_version_under10p1 = True
     pa_version_under11p0 = True
     pa_version_under12p0 = True
     pa_version_under13p0 = True
     pa_version_under14p0 = True
+    pa_version_under14p1 = True
