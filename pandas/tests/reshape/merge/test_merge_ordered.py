534521e7dc1cba0fb5cf2bed74dc776961bf35e3
Ziad Kermadi <ziadkermadi@gmail.com>
2023-11-13 21:02:35 +0100

    Update pandas/tests/reshape/merge/test_merge_ordered.py
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/tests/reshape/merge/test_merge_ordered.py
+++ pandas/tests/reshape/merge/test_merge_ordered.py
@@ -213,7 +213,7 @@ class TestMergeOrdered:
             merge_ordered(left, right, on="E", left_by=["G", "h"])
 
     def test_ffill_validate_fill_method(self):
-        # GH 5584
+        # GH 55884
         df1 = DataFrame({"key": ["a", "c", "e"], "lvalue": [1, 2, 3]})
         df2 = DataFrame({"key": ["b", "c", "d", "f"], "rvalue": [4, 5, 6, 7]})
 
