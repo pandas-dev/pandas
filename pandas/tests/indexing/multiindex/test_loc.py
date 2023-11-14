aea3846cff88df5c3ae47d08e4e955f4aa09ca4f
Brock <jbrockmendel@gmail.com>
2023-11-14 08:14:53 -0800

    revert commented-out

--- pandas/tests/indexing/multiindex/test_loc.py
+++ pandas/tests/indexing/multiindex/test_loc.py
@@ -624,7 +624,7 @@ def test_loc_period_string_indexing():
 
 def test_loc_datetime_mask_slicing():
     # GH 16699
-    dt_idx = pd.to_datetime(["2017-05-04", "2017-05-05"])  # .as_unit("s")
+    dt_idx = pd.to_datetime(["2017-05-04", "2017-05-05"])
     m_idx = MultiIndex.from_product([dt_idx, dt_idx], names=["Idx1", "Idx2"])
     df = DataFrame(
         data=[[1, 2], [3, 4], [5, 6], [7, 6]], index=m_idx, columns=["C1", "C2"]
