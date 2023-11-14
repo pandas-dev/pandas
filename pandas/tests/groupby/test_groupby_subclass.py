2cca5e3c713fbe23db44594f7ad60c44168bd0a4
richard <rhshadrach@gmail.com>
2023-11-13 22:55:23 -0500

    fixup

--- pandas/tests/groupby/test_groupby_subclass.py
+++ pandas/tests/groupby/test_groupby_subclass.py
@@ -38,9 +38,9 @@ def test_groupby_preserves_subclass(obj, groupby_func):
 
     warn = FutureWarning if groupby_func == "fillna" else None
     msg = f"{type(grouped).__name__}.fillna is deprecated"
-    with tm.assert_produces_warning(warn, match=msg):
+    with tm.assert_produces_warning(warn, match=msg, raise_on_extra_warnings=False):
         result1 = getattr(grouped, groupby_func)(*args)
-    with tm.assert_produces_warning(warn, match=msg):
+    with tm.assert_produces_warning(warn, match=msg, raise_on_extra_warnings=False):
         result2 = grouped.agg(groupby_func, *args)
 
     # Reduction or transformation kernels should preserve type
