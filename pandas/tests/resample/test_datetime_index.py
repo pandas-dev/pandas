795357d709ac1a450140cc8d21880d0a356017f6
jbrockmendel <jbrockmendel@gmail.com>
2023-11-14 12:57:48 -0800

    BUG: astype, asfreq with non-nano (#55958)
    * BUG: period.astype to non-nano dt64
    
    * BUG: .asfreq with non-nano
    
    * GH ref

--- pandas/tests/resample/test_datetime_index.py
+++ pandas/tests/resample/test_datetime_index.py
@@ -1267,10 +1267,14 @@ def test_resample_not_monotonic(unit):
         ),
     ],
 )
-def test_resample_median_bug_1688(dtype):
+def test_resample_median_bug_1688(dtype, unit):
+    # GH#55958
+    dti = DatetimeIndex(
+        [datetime(2012, 1, 1, 0, 0, 0), datetime(2012, 1, 1, 0, 5, 0)]
+    ).as_unit(unit)
     df = DataFrame(
         [1, 2],
-        index=[datetime(2012, 1, 1, 0, 0, 0), datetime(2012, 1, 1, 0, 5, 0)],
+        index=dti,
         dtype=dtype,
     )
 
