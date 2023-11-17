114fd8c03c46c2b90343c8641f176dd902d291f3
Will Ayd <william.ayd@icloud.com>
2023-11-17 10:56:56 -0500

    fix sign issue

--- pandas/_libs/src/vendored/numpy/datetime/np_datetime.c
+++ pandas/_libs/src/vendored/numpy/datetime/np_datetime.c
@@ -791,13 +791,13 @@ void pandas_timedelta_to_timedeltastruct(npy_timedelta td,
       frac -= out->sec;
     }
 
+    if (sign < 0)
+      out->days = -out->days;
+
     if (base > NPY_FR_s) {
       const npy_int64 sfrac =
           (out->hrs * sec_per_hour + out->min * sec_per_min + out->sec) *
           per_sec;
-
-      if (sign < 0)
-        out->days = -out->days;
 
       npy_int64 ifrac = td - (out->days * per_day + sfrac);
 
