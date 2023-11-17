6d5c2e616dd6339f2affb4ab68665ffbda3b4693
William Ayd <will_ayd@innobi.io>
2023-11-17 15:08:57 -0500

    Fix apply na overflow (#56000)
    * fix groupby and apply UB
    
    * fix/skip parser overflow
    
    * revert unneeded
    
    * feedback

--- pandas/core/methods/selectn.py
+++ pandas/core/methods/selectn.py
@@ -139,7 +139,8 @@ class SelectNSeries(SelectN):
 
         # arr passed into kth_smallest must be contiguous. We copy
         # here because kth_smallest will modify its input
-        kth_val = libalgos.kth_smallest(arr.copy(order="C"), n - 1)
+        # avoid OOB access with kth_smallest_c when n <= 0
+        kth_val = libalgos.kth_smallest(arr.copy(order="C"), max(n - 1, 0))
         (ns,) = np.nonzero(arr <= kth_val)
         inds = ns[arr[ns].argsort(kind="mergesort")]
 
