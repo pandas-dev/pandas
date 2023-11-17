59424c36893908b4f03d995e8b575dcf9651f441
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2023-11-17 17:55:30 -0500

    BUG: GroupBy.value_counts sorting order (#56016)
    * BUG: GroupBy.value_counts sorting order
    
    * Whatsnew
    
    * cleanup
    
    * Fix categorical and add test
    
    * cleanup

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -2821,10 +2821,26 @@ class GroupBy(BaseGroupBy[NDFrameT]):
             for grouping in groupings
         ):
             levels_list = [ping.result_index for ping in groupings]
-            multi_index, _ = MultiIndex.from_product(
+            multi_index = MultiIndex.from_product(
                 levels_list, names=[ping.name for ping in groupings]
-            ).sortlevel()
+            )
             result_series = result_series.reindex(multi_index, fill_value=0)
+
+        if sort:
+            # Sort by the values
+            result_series = result_series.sort_values(
+                ascending=ascending, kind="stable"
+            )
+        if self.sort:
+            # Sort by the groupings
+            names = result_series.index.names
+            # GH#56007 - Temporarily replace names in case they are integers
+            result_series.index.names = range(len(names))
+            index_level = list(range(len(self.grouper.groupings)))
+            result_series = result_series.sort_index(
+                level=index_level, sort_remaining=False
+            )
+            result_series.index.names = names
 
         if normalize:
             # Normalize the results by dividing by the original group sizes.
@@ -2844,13 +2860,6 @@ class GroupBy(BaseGroupBy[NDFrameT]):
 
             # Handle groups of non-observed categories
             result_series = result_series.fillna(0.0)
-
-        if sort:
-            # Sort the values and then resort by the main grouping
-            index_level = range(len(self.grouper.groupings))
-            result_series = result_series.sort_values(ascending=ascending).sort_index(
-                level=index_level, sort_remaining=False
-            )
 
         result: Series | DataFrame
         if self.as_index:
