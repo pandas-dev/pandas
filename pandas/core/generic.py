afd466e0ee1fefa4f25bcd1ce8b21dd2f7d189af
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-11-20 15:25:51 +0100

    Update pandas/core/generic.py
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -6435,7 +6435,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
                 <https://pandas.pydata.org/docs/dev/user_guide/copy_on_write.html>`__
                 will be enabled by default, which means that all methods with a
                 `copy` keyword will use a lazy copy mechanism and ignore the `copy`
-                keyword. The copy-keyword will be removed in a future version of
+                keyword. The `copy` keyword will be removed in a future version of
                 pandas.
         errors : {'raise', 'ignore'}, default 'raise'
             Control raising of exceptions on invalid data for provided dtype.
