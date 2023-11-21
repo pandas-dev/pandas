c495fdad29beddbd51e7a319902b034b5be4b2ca
Quang Nguyễn <30631476+quangngd@users.noreply.github.com>
2023-11-21 07:36:17 -0800

    BUG: Bad shift_forward around UTC+0 when DST (#56017)
    * add tests
    
    * fix
    
    * add whatsnew
    
    * refactor
    
    * Revert "refactor"
    
    This reverts commit 4482dd8ff2d147141f179f2863dfc7b893adb93c.
    
    * fix bug
    
    * update
    
    * more cmt

--- pandas/tests/scalar/timestamp/methods/test_tz_localize.py
+++ pandas/tests/scalar/timestamp/methods/test_tz_localize.py
@@ -123,6 +123,44 @@ class TestTimestampTZLocalize:
             ts.tz_localize(tz, nonexistent="raise")
         assert ts.tz_localize(tz, nonexistent="NaT") is NaT
 
+    @pytest.mark.parametrize(
+        "stamp, tz, forward_expected, backward_expected",
+        [
+            (
+                "2015-03-29 02:00:00",
+                "Europe/Warsaw",
+                "2015-03-29 03:00:00",
+                "2015-03-29 01:59:59",
+            ),  # utc+1 -> utc+2
+            (
+                "2023-03-12 02:00:00",
+                "America/Los_Angeles",
+                "2023-03-12 03:00:00",
+                "2023-03-12 01:59:59",
+            ),  # utc-8 -> utc-7
+            (
+                "2023-03-26 01:00:00",
+                "Europe/London",
+                "2023-03-26 02:00:00",
+                "2023-03-26 00:59:59",
+            ),  # utc+0 -> utc+1
+            (
+                "2023-03-26 00:00:00",
+                "Atlantic/Azores",
+                "2023-03-26 01:00:00",
+                "2023-03-25 23:59:59",
+            ),  # utc-1 -> utc+0
+        ],
+    )
+    def test_tz_localize_nonexistent_shift(
+        self, stamp, tz, forward_expected, backward_expected
+    ):
+        ts = Timestamp(stamp)
+        forward_ts = ts.tz_localize(tz, nonexistent="shift_forward")
+        assert forward_ts == Timestamp(forward_expected, tz=tz)
+        backward_ts = ts.tz_localize(tz, nonexistent="shift_backward")
+        assert backward_ts == Timestamp(backward_expected, tz=tz)
+
     def test_tz_localize_ambiguous_raise(self):
         # GH#13057
         ts = Timestamp("2015-11-1 01:00")
