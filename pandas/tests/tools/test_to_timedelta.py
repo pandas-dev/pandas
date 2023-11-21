47efe18209a859850c70288a64747a21031a9117
Brock <jbrockmendel@gmail.com>
2023-11-21 13:23:10 -0800

    xfail on3 2bit

--- pandas/tests/tools/test_to_timedelta.py
+++ pandas/tests/tools/test_to_timedelta.py
@@ -6,6 +6,7 @@ from datetime import (
 import numpy as np
 import pytest
 
+from pandas.compat import IS64
 from pandas.errors import OutOfBoundsTimedelta
 
 import pandas as pd
@@ -232,18 +233,10 @@ class TestTimedeltas:
         actual = to_timedelta([val])
         assert actual[0]._value == np.timedelta64("NaT").astype("int64")
 
-    @pytest.mark.parametrize(
-        "dtype",
-        [
-            pytest.param(
-                np.float32, marks=pytest.mark.xfail(reason="Floating point error")
-            ),
-            np.float64,
-        ],
-    )
-    def test_to_timedelta_float(self, dtype):
+    @pytest.mark.xfail(not IS64, reason="Floating point error")
+    def test_to_timedelta_float(self):
         # https://github.com/pandas-dev/pandas/issues/25077
-        arr = np.arange(0, 1, 1e-6)[-10:].astype(dtype)
+        arr = np.arange(0, 1, 1e-6)[-10:]
         result = to_timedelta(arr, unit="s")
         expected_asi8 = np.arange(999990000, 10**9, 1000, dtype="int64")
         tm.assert_numpy_array_equal(result.asi8, expected_asi8)
