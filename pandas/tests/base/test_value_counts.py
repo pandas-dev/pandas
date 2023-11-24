082ffe96d4bf87760721ef9cee2388fec51db2c8
Brock <jbrockmendel@gmail.com>
2023-11-24 15:55:58 -0800

    DEPR: dtype inference in value_counts

--- pandas/tests/base/test_value_counts.py
+++ pandas/tests/base/test_value_counts.py
@@ -336,3 +336,15 @@ def test_value_counts_with_nan(dropna, index_or_series):
     else:
         expected = Series([1, 1, 1], index=[True, pd.NA, np.nan], name="count")
     tm.assert_series_equal(res, expected)
+
+
+def test_value_counts_object_inference_deprecated():
+    dti = pd.date_range("2016-01-01", periods=3, tz="UTC")
+
+    idx = dti.astype(object)
+    msg = "The behavior of value_counts with object-dtype is deprecated"
+    with tm.assert_produces_warning(FutureWarning, match=msg):
+        res = idx.value_counts()
+
+    exp = dti.value_counts()
+    tm.assert_series_equal(res, exp)
