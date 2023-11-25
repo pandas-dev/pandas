3b7f73ce554b8031c9614fca89609953b90d1b7d
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-11-26 00:22:54 +0100

    Update

--- pandas/tests/copy_view/test_chained_assignment_deprecation.py
+++ pandas/tests/copy_view/test_chained_assignment_deprecation.py
@@ -4,22 +4,23 @@ from pandas import DataFrame
 import pandas._testing as tm
 
 
-def test_methods_iloc_warn():
-    df = DataFrame({"a": [1, 2, 3], "b": 1})
-    with tm.assert_cow_warning(match="A value"):
-        df.iloc[:, 0].replace(1, 5, inplace=True)
+def test_methods_iloc_warn(using_copy_on_write):
+    if not using_copy_on_write:
+        df = DataFrame({"a": [1, 2, 3], "b": 1})
+        with tm.assert_cow_warning(match="A value"):
+            df.iloc[:, 0].replace(1, 5, inplace=True)
 
-    with tm.assert_cow_warning(match="A value"):
-        df.iloc[:, 0].fillna(1, inplace=True)
+        with tm.assert_cow_warning(match="A value"):
+            df.iloc[:, 0].fillna(1, inplace=True)
 
-    with tm.assert_cow_warning(match="A value"):
-        df.iloc[:, 0].interpolate(inplace=True)
+        with tm.assert_cow_warning(match="A value"):
+            df.iloc[:, 0].interpolate(inplace=True)
 
-    with tm.assert_cow_warning(match="A value"):
-        df.iloc[:, 0].ffill(inplace=True)
+        with tm.assert_cow_warning(match="A value"):
+            df.iloc[:, 0].ffill(inplace=True)
 
-    with tm.assert_cow_warning(match="A value"):
-        df.iloc[:, 0].bfill(inplace=True)
+        with tm.assert_cow_warning(match="A value"):
+            df.iloc[:, 0].bfill(inplace=True)
 
 
 @pytest.mark.parametrize(
