f11c921cc56e73d3c04ddff0ec4adcc4d0de634a
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-11-27 21:49:07 +0100

    CoW: Add warning for mask, where and clip with inplace (#56067)
    Co-authored-by: Joris Van den Bossche <jorisvandenbossche@gmail.com>

--- pandas/tests/copy_view/test_clip.py
+++ pandas/tests/copy_view/test_clip.py
@@ -1,6 +1,9 @@
 import numpy as np
 
-from pandas import DataFrame
+from pandas import (
+    DataFrame,
+    option_context,
+)
 import pandas._testing as tm
 from pandas.tests.copy_view.util import get_array
 
@@ -81,3 +84,14 @@ def test_clip_chained_inplace(using_copy_on_write):
         with tm.raises_chained_assignment_error():
             df[["a"]].clip(1, 2, inplace=True)
         tm.assert_frame_equal(df, df_orig)
+    else:
+        with tm.assert_produces_warning(FutureWarning, match="inplace method"):
+            df["a"].clip(1, 2, inplace=True)
+
+        with tm.assert_produces_warning(FutureWarning, match="inplace method"):
+            with option_context("mode.chained_assignment", None):
+                df[["a"]].clip(1, 2, inplace=True)
+
+        with tm.assert_produces_warning(FutureWarning, match="inplace method"):
+            with option_context("mode.chained_assignment", None):
+                df[df["a"] > 1].clip(1, 2, inplace=True)
--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -1551,6 +1551,17 @@ def test_chained_where_mask(using_copy_on_write, func):
         with tm.raises_chained_assignment_error():
             getattr(df[["a"]], func)(df["a"] > 2, 5, inplace=True)
         tm.assert_frame_equal(df, df_orig)
+    else:
+        with tm.assert_produces_warning(FutureWarning, match="inplace method"):
+            getattr(df["a"], func)(df["a"] > 2, 5, inplace=True)
+
+        with tm.assert_produces_warning(FutureWarning, match="inplace method"):
+            with option_context("mode.chained_assignment", None):
+                getattr(df[["a"]], func)(df["a"] > 2, 5, inplace=True)
+
+        with tm.assert_produces_warning(FutureWarning, match="inplace method"):
+            with option_context("mode.chained_assignment", None):
+                getattr(df[df["a"] > 1], func)(df["a"] > 2, 5, inplace=True)
 
 
 def test_asfreq_noop(using_copy_on_write):
