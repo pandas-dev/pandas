4af5d4c7a0864e7cad999dd83c856b1535741639
Brock <jbrockmendel@gmail.com>
2023-11-30 16:23:43 -0800

    mypy fixup

--- pandas/tests/io/excel/test_readers.py
+++ pandas/tests/io/excel/test_readers.py
@@ -139,7 +139,8 @@ def get_exp_unit(read_ext: str, engine: str | None) -> str:
 def adjust_expected(expected: DataFrame, read_ext: str, engine: str) -> None:
     expected.index.name = None
     unit = get_exp_unit(read_ext, engine)
-    expected.index = expected.index.as_unit(unit)
+    # error: "Index" has no attribute "as_unit"
+    expected.index = expected.index.as_unit(unit)  # type: ignore[attr-defined]
 
 
 def xfail_datetimes_with_pyxlsb(engine, request):
