14e7b59965bb52d83df625d3a8c1f21cdb72c2b4
ccccjone <c_cui3@u.pacific.edu>
2023-12-05 02:39:49 -0800

    Fixed the code style issue causing test failure

--- pandas/tests/io/formats/style/test_bar.py
+++ pandas/tests/io/formats/style/test_bar.py
@@ -317,7 +317,10 @@ def test_bar_color_and_cmap_error_raises():
 
 def test_bar_invalid_color_type_error_raises():
     df = DataFrame({"A": [1, 2, 3, 4]})
-    msg = r"`color` must be string or list or tuple of 2 strings,\(eg: color=\['#d65f5f', '#5fba7d'\]\)"
+    msg = (
+        r"`color` must be string or list or tuple of 2 strings,"
+        r"\(eg: color=\['#d65f5f', '#5fba7d'\]\)"
+    )
     # Test that providing an invalid color type raises a ValueError
     with pytest.raises(ValueError, match=msg):
         df.style.bar(color=123).to_html()
