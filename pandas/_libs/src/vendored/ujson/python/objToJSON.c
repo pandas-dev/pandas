04739fb8ab89324e5bfada23c49c80e3e1703766
Will Ayd <william.ayd@icloud.com>
2023-12-05 13:55:07 -0800

    test casting precision

--- pandas/_libs/src/vendored/ujson/python/objToJSON.c
+++ pandas/_libs/src/vendored/ujson/python/objToJSON.c
@@ -1252,8 +1252,9 @@ static char **NpyArr_encodeLabels(PyArrayObject *labels, PyObjectEncoder *enc,
         if (PyDelta_Check(item)) {
           // TODO(anyone): cast below loses precision if total_seconds return
           // value exceeds number of bits that significand can hold
-          i8date = (int64_t)total_seconds(item) *
-                   1000000000LL; // nanoseconds per second
+          // also liable to overflow
+          i8date = (int64_t)(total_seconds(item) *
+                             1000000000LL); // nanoseconds per second
         } else {
           // datetime.* objects don't follow above rules
           i8date = PyDateTimeToEpoch(item, NPY_FR_ns);
@@ -1502,9 +1503,10 @@ static void Object_beginTypeContext(JSOBJ _obj, JSONTypeContext *tc) {
     // fallback to nanoseconds per sec for other objects
     // TODO(anyone): cast below loses precision if total_seconds return
     // value exceeds number of bits that significand can hold
+    // also liable to overflow
     int64_t value = PyObject_HasAttrString(obj, "_value")
                         ? get_long_attr(obj, "_value")
-                        : (int64_t)total_seconds(obj) * 1000000000LL;
+                        : (int64_t)(total_seconds(obj) * 1000000000LL);
 
     if (value == get_nat()) {
       tc->type = JT_NULL;
