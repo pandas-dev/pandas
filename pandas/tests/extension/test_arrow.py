0a0639482e56f05841ebe6d5765eb126f69da98a
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-12-05 16:31:32 -0800

    BUG: astype(errors=ignore) for EAs

--- pandas/tests/extension/test_arrow.py
+++ pandas/tests/extension/test_arrow.py
@@ -1475,6 +1475,13 @@ def test_astype_float_from_non_pyarrow_str():
     tm.assert_series_equal(result, expected)
 
 
+def test_astype_errors_ignore():
+    # GH 55399
+    expected = pd.DataFrame({"col": [17000000]}, dtype="int32[pyarrow]")
+    result = expected.astype("float[pyarrow]", errors="ignore")
+    tm.assert_frame_equal(result, expected)
+
+
 def test_to_numpy_with_defaults(data):
     # GH49973
     result = data.to_numpy()
