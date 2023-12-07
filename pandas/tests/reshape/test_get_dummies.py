546bdd1677490b0814ce97feba683c669b8fda5e
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-12-08 00:00:37 +0100

    Update test_get_dummies.py

--- pandas/tests/reshape/test_get_dummies.py
+++ pandas/tests/reshape/test_get_dummies.py
@@ -706,21 +706,19 @@ class TestGetDummies:
         tm.assert_frame_equal(result, expected)
 
     @td.skip_if_no("pyarrow")
-    @pytest.mark.parametrize(
-        "dtype, exp_dtype",
-        [
+    def test_get_dummies_ea_dtype(self):
+        # GH#56273
+        for dtype, exp_dtype in [
             ("string[pyarrow]", "boolean"),
             ("string[pyarrow_numpy]", "bool"),
             (CategoricalDtype(Index(["a"], dtype="string[pyarrow]")), "boolean"),
             (CategoricalDtype(Index(["a"], dtype="string[pyarrow_numpy]")), "bool"),
-        ],
-    )
-    def test_get_dummies_ea_dtype(self, dtype, exp_dtype):
-        # GH#56273
-        df = DataFrame({"name": Series(["a"], dtype=dtype), "x": 1})
-        result = get_dummies(df)
-        expected = DataFrame({"x": 1, "name_a": Series([True], dtype=exp_dtype)})
-        tm.assert_frame_equal(result, expected)
+        ]:
+            
+            df = DataFrame({"name": Series(["a"], dtype=dtype), "x": 1})
+            result = get_dummies(df)
+            expected = DataFrame({"x": 1, "name_a": Series([True], dtype=exp_dtype)})
+            tm.assert_frame_equal(result, expected)
 
     @td.skip_if_no("pyarrow")
     def test_get_dummies_arrow_dtype(self):
