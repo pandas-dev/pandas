13e976a1d41f02e959954fa0198bd8e5d73c358a
ccccjone <c_cui3@u.pacific.edu>
2023-12-07 16:48:16 -0800

    Added a test for Styler.bar with pyarrow

--- pandas/tests/io/formats/style/test_bar.py
+++ pandas/tests/io/formats/style/test_bar.py
@@ -1,9 +1,11 @@
 import numpy as np
 import pytest
+import io
 
 from pandas import (
     NA,
     DataFrame,
+    read_csv,
 )
 
 pytest.importorskip("jinja2")
@@ -341,3 +343,15 @@ def test_styler_bar_with_NA_values():
     html_output2 = df2.style.bar(align="left", axis=None).to_html()
     assert expected_substring in html_output1
     assert expected_substring in html_output2
+
+
+def test_style_bar_with_pyarrow_NA_values():
+    data = '''name,age,test1,test2,teacher
+        Adam,15,95.0,80,Ashby
+        Bob,16,81.0,82,Ashby
+        Dave,16,89.0,84,Jones
+        Fred,15,,88,Jones'''
+    df = read_csv(io.StringIO(data), dtype_backend="pyarrow")
+    expected_substring = "style type="
+    html_output = df.style.bar(subset="test1").to_html()
+    assert expected_substring in html_output
