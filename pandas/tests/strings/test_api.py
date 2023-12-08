4b06175df49c7b3c049a98bed6134f606397b068
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-12-09 00:28:19 +0100

    BUG: Index.str.cat casting result always to object (#56157)
    * BUG: Index.str.cat casting result always to object
    
    * Update accessor.py
    
    * Fix further bugs
    
    * Fix
    
    * Update accessor.py
    
    * Update v2.1.4.rst
    
    * Update v2.2.0.rst

--- pandas/tests/strings/test_api.py
+++ pandas/tests/strings/test_api.py
@@ -2,6 +2,7 @@ import numpy as np
 import pytest
 
 from pandas import (
+    CategoricalDtype,
     DataFrame,
     Index,
     MultiIndex,
@@ -178,6 +179,7 @@ def test_api_for_categorical(any_string_method, any_string_dtype):
     s = Series(list("aabb"), dtype=any_string_dtype)
     s = s + " " + s
     c = s.astype("category")
+    c = c.astype(CategoricalDtype(c.dtype.categories.astype("object")))
     assert isinstance(c.str, StringMethods)
 
     method_name, args, kwargs = any_string_method
