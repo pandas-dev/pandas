07239bb9b27536e874d3ad1240e39d2e2ee24d49
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-12-11 00:11:31 +0100

    BUG: merge not raising for String and numeric merges

--- pandas/tests/reshape/merge/test_join.py
+++ pandas/tests/reshape/merge/test_join.py
@@ -150,8 +150,8 @@ class TestJoin:
         # overlap
         source_copy = source.copy()
         msg = (
-            "You are trying to merge on float64 and object columns for key 'A'. "
-            "If you wish to proceed you should use pd.concat"
+            "You are trying to merge on float64 and object|string columns for key "
+            "'A'. If you wish to proceed you should use pd.concat"
         )
         with pytest.raises(ValueError, match=msg):
             target.join(source_copy, on="A")
