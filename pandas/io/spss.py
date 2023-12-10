dc191e34cc74ebc57830e98270b39d535dd8a59a
Shubhankar Agrawal <shubhankar.a31@gmail.com>
2023-12-10 22:30:21 +0800

    Bug fix for spss kwargs

--- pandas/io/spss.py
+++ pandas/io/spss.py
@@ -24,6 +24,7 @@ def read_spss(
     usecols: Sequence[str] | None = None,
     convert_categoricals: bool = True,
     dtype_backend: DtypeBackend | lib.NoDefault = lib.no_default,
+    **kwargs
 ) -> DataFrame:
     """
     Load an SPSS file from the file path, returning a DataFrame.
@@ -46,6 +47,8 @@ def read_spss(
           DataFrame.
 
         .. versionadded:: 2.0
+    **kwargs : dict, optional
+        Additional keyword arguments passed to :func:`pyreadstat.read_sav`.
 
     Returns
     -------
@@ -64,7 +67,7 @@ def read_spss(
         usecols = list(usecols)  # pyreadstat requires a list
 
     df, metadata = pyreadstat.read_sav(
-        stringify_path(path), usecols=usecols, apply_value_formats=convert_categoricals
+        stringify_path(path), usecols=usecols, apply_value_formats=convert_categoricals, **kwargs
     )
     df.attrs = metadata.__dict__
     if dtype_backend is not lib.no_default:
