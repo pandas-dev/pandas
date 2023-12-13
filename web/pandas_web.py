8813a26e1299825a6733a3f6c75dc0fa8a33a22d
Dukastlik <49510873+Dukastlik@users.noreply.github.com>
2023-12-13 08:22:55 +0300

    WEB: Better management of releases (#56207)

--- web/pandas_web.py
+++ web/pandas_web.py
@@ -27,6 +27,7 @@ import argparse
 import collections
 import datetime
 import importlib
+import itertools
 import json
 import operator
 import os
@@ -40,6 +41,7 @@ import typing
 import feedparser
 import jinja2
 import markdown
+from packaging import version
 import requests
 import yaml
 
@@ -245,6 +247,7 @@ class Preprocessors:
             context["releases"].append(
                 {
                     "name": release["tag_name"].lstrip("v"),
+                    "parsed_version": version.parse(release["tag_name"].lstrip("v")),
                     "tag": release["tag_name"],
                     "published": published,
                     "url": (
@@ -254,7 +257,17 @@ class Preprocessors:
                     ),
                 }
             )
-
+        # sorting out obsolete versions
+        grouped_releases = itertools.groupby(
+            context["releases"],
+            key=lambda r: (r["parsed_version"].major, r["parsed_version"].minor),
+        )
+        context["releases"] = [
+            max(release_group, key=lambda r: r["parsed_version"].minor)
+            for _, release_group in grouped_releases
+        ]
+        # sorting releases by version number
+        context["releases"].sort(key=lambda r: r["parsed_version"], reverse=True)
         return context
 
     @staticmethod
