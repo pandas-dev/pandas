fb948907b4e07376176c0a8b26c6d2a49804cb7a
linus-md <linus.sommer@protonmail.com>
2023-12-15 16:36:11 +0100

    fix line length

--- pandas/io/xml.py
+++ pandas/io/xml.py
@@ -1147,7 +1147,8 @@ def read_xml(
     ...         </data>
     ...         '''
 
-    >>> df = pd.read_xml(StringIO(xml_data), dtype_backend="numpy_nullable", parse_dates=["e"])
+    >>> df = pd.read_xml(StringIO(xml_data), dtype_backend="numpy_nullable",
+    ...                  parse_dates=["e"])
     >>> print(df)
        index     a    b      c  d          e
     0      0     1  2.5   True  a 2019-12-31
