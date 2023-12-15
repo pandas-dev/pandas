d6f30feaf81be25603828152be92d030d7bea23f
linus-md <linus.sommer@protonmail.com>
2023-12-15 18:24:27 +0100

    Update

--- pandas/io/xml.py
+++ pandas/io/xml.py
@@ -1149,7 +1149,7 @@ def read_xml(
 
     >>> df = pd.read_xml(StringIO(xml_data), dtype_backend="numpy_nullable",
     ...                  parse_dates=["e"])
-    >>> print(df)
+    >>> df
        index     a    b      c  d          e
     0      0     1  2.5   True  a 2019-12-31
     1      1  <NA>  4.5  False  b 2019-12-31
