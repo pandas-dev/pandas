453640dca482895edc3ff700dfac878424148883
Brock <jbrockmendel@gmail.com>
2023-12-17 10:54:34 -0800

    DEPR: object inference in to_stata

--- pandas/io/stata.py
+++ pandas/io/stata.py
@@ -393,6 +393,14 @@ def _datetime_to_stata_elapsed_vec(dates: Series, fmt: str) -> Series:
                 d["days"] = np.asarray(diff).astype("m8[D]").view("int64")
 
         elif infer_dtype(dates, skipna=False) == "datetime":
+            # - Deprecated casting object-dtype columns of datetimes to datetime64 when writing to stata; call df=df.infer_objects() before writing to stata instead (:issue:`??`)
+            warnings.warn(
+                "Converting object-dtype columns of datetimes to datetime64 when "
+                "writing to stata is deprecated. Call "
+                "`df=df.infer_objects(copy=False)` before writing to stata instead.",
+                FutureWarning,
+                stacklevel=find_stack_level(),
+            )
             if delta:
                 delta = dates._values - stata_epoch
 
--- pandas/tests/io/test_stata.py
+++ pandas/tests/io/test_stata.py
@@ -1030,7 +1030,12 @@ class TestStata:
         # {c : c[-2:] for c in columns}
         path = temp_file
         expected.index.name = "index"
-        expected.to_stata(path, convert_dates=date_conversion)
+        msg = (
+            "Converting object-dtype columns of datetimes to datetime64 "
+            "when writing to stata is deprecated"
+        )
+        with tm.assert_produces_warning(FutureWarning, match=msg):
+            expected.to_stata(path, convert_dates=date_conversion)
         written_and_read_again = self.read_dta(path)
 
         tm.assert_frame_equal(
