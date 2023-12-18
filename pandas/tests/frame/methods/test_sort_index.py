8c66632311e492384ac93df9131c646f54f61b61
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-12-18 09:39:12 -1000

    BUG: Fix sort_index(axis=1, ignore_index=True) (#56487)
    * BUG: Fix sort_index(axis=1, ignore_index=True)
    
    * Switch condition

--- pandas/tests/frame/methods/test_sort_index.py
+++ pandas/tests/frame/methods/test_sort_index.py
@@ -994,3 +994,11 @@ def test_sort_index_with_sliced_multiindex():
         ),
     )
     tm.assert_frame_equal(result, expected)
+
+
+def test_axis_columns_ignore_index():
+    # GH 56478
+    df = DataFrame([[1, 2]], columns=["d", "c"])
+    result = df.sort_index(axis="columns", ignore_index=True)
+    expected = DataFrame([[2, 1]])
+    tm.assert_frame_equal(result, expected)
