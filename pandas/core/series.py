ab2a735558030e1d67e059951a58e789fc641697
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-12-18 20:31:01 +0100

    Update pandas/core/series.py
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -591,7 +591,7 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
                     "Dtype inference on a pandas object "
                     "(Series, Index, ExtensionArray) is deprecated. The Index "
                     "constructor will keep the original dtype in the future. "
-                    "Call ``infer_objects on the result",
+                    "Call `infer_objects` on the result",
                     FutureWarning,
                     stacklevel=2,
                 )
