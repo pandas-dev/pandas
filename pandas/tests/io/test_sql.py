ad54a8c1eceebbe6033c05c82480b9ff17339b71
Brock <jbrockmendel@gmail.com>
2023-12-18 15:41:54 -0800

    troubleshoot db tests

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -1820,7 +1820,7 @@ def test_api_custom_dateparsing_error(
             }
         )
 
-    if "postgres" in conn_name:
+    if "postgres" in conn_name or "mysql" in conn_name:
         expected["DateCol"] = expected["DateCol"].astype("datetime64[us]")
     else:
         expected["DateCol"] = expected["DateCol"].astype("datetime64[s]")
