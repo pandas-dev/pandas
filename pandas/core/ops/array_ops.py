e0e47e8d37d4fc7c2e7634c5f9ca0ab7e44c1291
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2023-12-21 14:35:01 -1000

    DEPS: Test NEP 50 (#55739)
    * DEPS: Test NEP 50
    
    * Use Python floats in test_maybe_promote_float_with_float
    
    * Refactor test_to_html_multiindex to allow tests to collect
    
    * Supress deprecationwarning for now
    
    * Use old invocation
    
    * Use Python ints in _range.py functions
    
    * Address test_constructor
    
    * Fix test_constructor_coercion_signed_to_unsigned
    
    * Fix test_constructor_coercion_signed_to_unsigned
    
    * Cast numpy scalars as python scalars before arith ops
    
    * add xfail reason to TestCoercionFloat32
    
    * only set promotion state for numpy > 2.0
    
    * order was backwards
    
    * Version promotion state call
    
    * fix timedelta tests
    
    * go for green
    
    * fix non npdev too?
    
    * fixes
    
    * adjust xfail condition
    
    * go for green
    
    * add tests
    
    * add negative numbers test
    
    * updates
    
    * fix accidental changes
    
    * more
    
    * simplify
    
    * linter
    
    ---------
    
    Co-authored-by: Thomas Li <47963215+lithomas1@users.noreply.github.com>

--- pandas/core/ops/array_ops.py
+++ pandas/core/ops/array_ops.py
@@ -570,6 +570,14 @@ def maybe_prepare_scalar_for_op(obj, shape: Shape):
         #  np.timedelta64(3, 'D') / 2 == np.timedelta64(1, 'D')
         return Timedelta(obj)
 
+    # We want NumPy numeric scalars to behave like Python scalars
+    # post NEP 50
+    elif isinstance(obj, np.integer):
+        return int(obj)
+
+    elif isinstance(obj, np.floating):
+        return float(obj)
+
     return obj
 
 
