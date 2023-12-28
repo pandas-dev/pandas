4fee21bed77a8ed6fa1b113d6e50353756788bf2
Patrick Hoefler <patrick_hoefler@gmx.net>
2023-12-28 20:05:20 +0100

    Parametrize

--- pandas/tests/util/test_assert_series_equal.py
+++ pandas/tests/util/test_assert_series_equal.py
@@ -454,10 +454,11 @@ def test_large_unequal_ints(dtype):
         tm.assert_series_equal(left, right)
 
 
+@pytest.mark.parametrize("dtype", [None, object])
 @pytest.mark.parametrize("check_exact", [True, False])
 @pytest.mark.parametrize("val", [3, 3.5])
-def test_ea_and_numpy_no_dtype_check(val, check_exact):
+def test_ea_and_numpy_no_dtype_check(val, check_exact, dtype):
     # GH#56651
-    left = Series([1, 2, val])
+    left = Series([1, 2, val], dtype=dtype)
     right = Series(pd.array([1, 2, val]))
     tm.assert_series_equal(left, right, check_dtype=False, check_exact=check_exact)
