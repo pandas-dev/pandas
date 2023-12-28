6fd6e318b45d77349e8f36658bbc7b1e07e5565e
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2023-12-28 16:33:31 +0100

    BUG: assert_series_equal not properly respecting check-dtype

--- pandas/tests/util/test_assert_series_equal.py
+++ pandas/tests/util/test_assert_series_equal.py
@@ -456,3 +456,12 @@ def test_large_unequal_ints(dtype):
     right = Series([1577840521123543], dtype=dtype)
     with pytest.raises(AssertionError, match="Series are different"):
         tm.assert_series_equal(left, right)
+
+
+@pytest.mark.parametrize("check_exact", [True, False])
+@pytest.mark.parametrize("val", [3, 3.5])
+def test_ea_and_numpy_no_dtype_check(val, check_exact):
+    # GH#56651
+    left = Series([1, 2, val])
+    right = Series(pd.array([1, 2, val]))
+    tm.assert_series_equal(left, right, check_dtype=False, check_exact=check_exact)
