411eda77b8f3ba19dfbf4c4093779f36419c464c
Patrick Hoefler <patrick_hoefler@gmx.net>
2024-01-03 23:21:20 +0100

    Update

--- pandas/io/excel/_base.py
+++ pandas/io/excel/_base.py
@@ -160,9 +160,8 @@ dtype : Type name or dict of column -> type, default None
     If converters are specified, they will be applied INSTEAD
     of dtype conversion.
     If you use ``None``, it will infer the dtype of each column based on the data.
-engine : str, default None
+engine : {"xlrd", "openpyxl", "odf", "pyxlsb", "calamine"}, default None
     If io is not a buffer or path, this must be set to identify io.
-    Supported engines: "xlrd", "openpyxl", "odf", "pyxlsb", "calamine".
     Engine compatibility :
 
     - ``openpyxl`` supports newer Excel file formats.
