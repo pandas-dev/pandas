d4b08d5ef533c72f8481d64e817af990dabdb5c1
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2024-01-10 17:32:55 -0800

    COMPAT: Fix warning with numba >= 0.58.0 (#55327)
    * COMPAT: Fix warning with numba >= 0.58.0
    
    * Update numba_.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * add a test
    
    * skip if no numba
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update v2.2.0.rst
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pandas/tests/window/test_numba.py
+++ pandas/tests/window/test_numba.py
@@ -446,3 +446,10 @@ class TestTableMethod:
             engine_kwargs=engine_kwargs, engine="numba"
         )
         tm.assert_frame_equal(result, expected)
+
+
+@td.skip_if_no("numba")
+def test_npfunc_no_warnings():
+    df = DataFrame({"col1": [1, 2, 3, 4, 5]})
+    with tm.assert_produces_warning(False):
+        df.col1.rolling(2).apply(np.prod, raw=True, engine="numba")
