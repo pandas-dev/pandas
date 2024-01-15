ab9cb97d8f6caaeb18c8a963005ebb56700a02e5
Paulo S. Costa <pauloscosta5@gmail.com>
2024-01-15 10:01:43 -0600

    TYP: Persist typing information for pipe args and kwargs (#56760)
    * Type generic pipe with function params
    
    * Type common pipe with function params
    
    * Type resample pipe with function params
    
    * Type groupby pipe with function params
    
    * Type style pipe function params and tuple func

--- pandas/_typing.py
+++ pandas/_typing.py
@@ -90,18 +90,29 @@ if TYPE_CHECKING:
     from typing import SupportsIndex
 
     if sys.version_info >= (3, 10):
+        from typing import Concatenate  # pyright: ignore[reportUnusedImport]
+        from typing import ParamSpec
         from typing import TypeGuard  # pyright: ignore[reportUnusedImport]
     else:
-        from typing_extensions import TypeGuard  # pyright: ignore[reportUnusedImport]
+        from typing_extensions import (  # pyright: ignore[reportUnusedImport]
+            Concatenate,
+            ParamSpec,
+            TypeGuard,
+        )
+
+    P = ParamSpec("P")
 
     if sys.version_info >= (3, 11):
         from typing import Self  # pyright: ignore[reportUnusedImport]
     else:
         from typing_extensions import Self  # pyright: ignore[reportUnusedImport]
+
 else:
     npt: Any = None
+    ParamSpec: Any = None
     Self: Any = None
     TypeGuard: Any = None
+    Concatenate: Any = None
 
 HashableT = TypeVar("HashableT", bound=Hashable)
 MutableMappingT = TypeVar("MutableMappingT", bound=MutableMapping)
