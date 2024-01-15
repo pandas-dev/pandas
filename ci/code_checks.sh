0c0982004d6c80adcd65c35499c18e9f6257fcc9
Anh Trinh <trinhquocanh94@gmail.com>
2024-01-15 23:57:48 +0100

    Fix CI not raised when there's an error

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -95,7 +95,7 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         pandas.DataFrame.groupby \
         pandas.DataFrame.sort_values \
         pandas.DataFrame.plot.hexbin \
-        pandas.DataFrame.plot.line \
+        pandas.DataFrame.plot.line
     RET=$(($RET + $?)) ; echo $MSG "DONE"
 
 fi
