d63bf2fb1e8afe169a1e8dd11b736ff7c63ef9fc
Jordan Murphy <35613487+jordan-d-murphy@users.noreply.github.com>
2024-01-15 00:51:10 -0700

    DOC: fix EX03 errors in docstrings - pandas.Index.rename, pandas.Index.isin, pandas.IndexSlice (#56870)
    Co-authored-by: Marc Garcia <garcia.marc@gmail.com>

--- pandas/core/indexing.py
+++ pandas/core/indexing.py
@@ -121,7 +121,7 @@ class _IndexSlice:
 
     Examples
     --------
-    >>> midx = pd.MultiIndex.from_product([['A0','A1'], ['B0','B1','B2','B3']])
+    >>> midx = pd.MultiIndex.from_product([['A0', 'A1'], ['B0', 'B1', 'B2', 'B3']])
     >>> columns = ['foo', 'bar']
     >>> dfmi = pd.DataFrame(np.arange(16).reshape((len(midx), len(columns))),
     ...                     index=midx, columns=columns)
