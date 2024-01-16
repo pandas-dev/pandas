ba9777e086574e56875995ae83a75452389b8e8f
Zhengbo Wang <77875500+luke396@users.noreply.github.com>
2024-01-16 15:29:34 +0800

    Remove unused in code_checks

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -80,10 +80,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         pandas.io.formats.style.Styler.to_latex \
         pandas.read_parquet \
         pandas.DataFrame.to_sql \
-        pandas.io.formats.style.Styler.map \
-        pandas.io.formats.style.Styler.apply_index \
-        pandas.io.formats.style.Styler.map_index \
-        pandas.io.formats.style.Styler.format \
         pandas.io.formats.style.Styler.set_tooltips \
         pandas.io.formats.style.Styler.set_uuid \
         pandas.io.formats.style.Styler.pipe \
