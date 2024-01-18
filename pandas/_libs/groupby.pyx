70cee7ce1d5a1e176da1cc3c9c1dd029a7e49e4c
Xiao Yuan <yuanx749@gmail.com>
2024-01-19 01:03:43 +0800

    BUG: fix `groupby.quantile` inconsistency when interpolation='nearest' (#56926)
    * BUG: fix groupby.quantile inconsistency when interpolation='nearest'
    
    * add whatsnew
    
    * Add comment

--- pandas/_libs/groupby.pyx
+++ pandas/_libs/groupby.pyx
@@ -1286,7 +1286,9 @@ def group_quantile(
                     elif interp == INTERPOLATION_MIDPOINT:
                         out[i, k] = (val + next_val) / 2.0
                     elif interp == INTERPOLATION_NEAREST:
-                        if frac > .5 or (frac == .5 and q_val > .5):  # Always OK?
+                        if frac > .5 or (frac == .5 and idx % 2 == 1):
+                            # If quantile lies in the middle of two indexes,
+                            # take the even index, as np.quantile.
                             out[i, k] = next_val
                         else:
                             out[i, k] = val
