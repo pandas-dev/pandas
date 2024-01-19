a4366b01c6aba1644c1e4a5dfa401780744ef120
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2024-01-19 09:12:49 -0800

    DEPS: Add warning if pyarrow is not installed (#56896)
    * add warning
    
    * DEPS: Add warning if pyarrow is not installed
    
    * formatting
    
    * Update __init__.py
    
    * adjustments
    
    * adjustments
    
    * updates
    
    * address code review
    
    * Update __init__.py
    
    * add pyarrow to npdev build
    
    * ignore non numpy related deprecationwarnings/futurewarning
    
    * ignore non numpy related deprecationwarnings/futurewarning
    
    * Update actions-311-numpydev.yaml

--- pandas/compat/pyarrow.py
+++ pandas/compat/pyarrow.py
@@ -8,6 +8,7 @@ try:
     import pyarrow as pa
 
     _palv = Version(Version(pa.__version__).base_version)
+    pa_not_found = False
     pa_version_under10p1 = _palv < Version("10.0.1")
     pa_version_under11p0 = _palv < Version("11.0.0")
     pa_version_under12p0 = _palv < Version("12.0.0")
@@ -16,6 +17,7 @@ try:
     pa_version_under14p1 = _palv < Version("14.0.1")
     pa_version_under15p0 = _palv < Version("15.0.0")
 except ImportError:
+    pa_not_found = True
     pa_version_under10p1 = True
     pa_version_under11p0 = True
     pa_version_under12p0 = True
