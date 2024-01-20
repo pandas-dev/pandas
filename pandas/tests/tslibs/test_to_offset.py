1d1d1e63ff64b9adc65ca25a97412081f8739a14
Michał Górny <mgorny@gentoo.org>
2024-01-20 10:17:29 +0100

    TST: test for warnings using tm.assert_produces_warning()
    Fix tests that used `pytest.raises()` to check for warnings, to use
    `tm.assert_produces_warning()` instead.  This is more correct, and it
    fixes running the test suite without `-Werror`.  This is how Gentoo runs
    it, to avoid sudden test failures due to new deprecation warnings from
    upgraded dependencies.

--- pandas/tests/tslibs/test_to_offset.py
+++ pandas/tests/tslibs/test_to_offset.py
@@ -7,6 +7,7 @@ from pandas._libs.tslibs import (
     offsets,
     to_offset,
 )
+import pandas._testing as tm
 
 
 @pytest.mark.parametrize(
@@ -194,7 +195,7 @@ def test_to_offset_lowercase_frequency_deprecated(freq_depr):
     depr_msg = f"'{freq_depr[1:]}' is deprecated and will be removed in a "
     f"future version, please use '{freq_depr.upper()[1:]}' instead."
 
-    with pytest.raises(FutureWarning, match=depr_msg):
+    with tm.assert_produces_warning(FutureWarning, match=depr_msg):
         to_offset(freq_depr)
 
 
@@ -214,5 +215,5 @@ def test_to_offset_uppercase_frequency_deprecated(freq_depr):
     depr_msg = f"'{freq_depr[1:]}' is deprecated and will be removed in a "
     f"future version, please use '{freq_depr.lower()[1:]}' instead."
 
-    with pytest.raises(FutureWarning, match=depr_msg):
+    with tm.assert_produces_warning(FutureWarning, match=depr_msg):
         to_offset(freq_depr)
