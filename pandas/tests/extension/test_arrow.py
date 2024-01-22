9a84f4efc52ca37ce651455950da8da14a1be2be
Rohan Jain <rohanjain@microsoft.com>
2024-01-22 09:06:51 -0500

    success

--- pandas/tests/extension/test_arrow.py
+++ pandas/tests/extension/test_arrow.py
@@ -3215,6 +3215,18 @@ def test_decimal_parse_raises():
         ser.astype(ArrowDtype(pa.decimal128(1, 0)))
 
 
+@pytest.mark.skipif(
+    pa_version_under11p0, reason="Decimal128 to string cast implemented in pyarrow 11"
+)
+def test_decimal_parse_succeeds():
+    # GH 56984
+    ser = pd.Series(["1.2345"], dtype=ArrowDtype(pa.string()))
+    dtype = ArrowDtype(pa.decimal128(5, 4))
+    result = ser.astype(dtype)
+    expected = pd.Series([Decimal("1.2345")], dtype=dtype)
+    tm.assert_series_equal(result, expected)
+
+
 @pytest.mark.parametrize("pa_type", tm.TIMEDELTA_PYARROW_DTYPES)
 def test_duration_fillna_numpy(pa_type):
     # GH 54707
