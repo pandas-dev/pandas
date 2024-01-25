71eb213800a9fd3ef8deed1caf9354af89c488f2
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-01-24 15:01:42 -1000

    COMPAT: Make argsort compatable with numpy 2.0 (#57057)
    Make argsort compatable with numpy 2.0

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -4070,6 +4070,7 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
         axis: Axis = 0,
         kind: SortKind = "quicksort",
         order: None = None,
+        stable: None = None,
     ) -> Series:
         """
         Return the integer indices that would sort the Series values.
@@ -4085,6 +4086,8 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
             Choice of sorting algorithm. See :func:`numpy.sort` for more
             information. 'mergesort' and 'stable' are the only stable algorithms.
         order : None
+            Has no effect but is accepted for compatibility with numpy.
+        stable : None
             Has no effect but is accepted for compatibility with numpy.
 
         Returns
