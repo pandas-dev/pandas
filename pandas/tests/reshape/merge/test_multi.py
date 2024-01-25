7c511ce83b229f202e2fcb13871ea6a060925a14
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-01-25 15:11:59 -0800

    Fix other test

--- pandas/tests/reshape/merge/test_multi.py
+++ pandas/tests/reshape/merge/test_multi.py
@@ -839,12 +839,10 @@ class TestJoinMultiMulti:
         left_multi = left_multi.drop(columns=left_multi.columns)
         right_multi = right_multi.drop(columns=right_multi.columns)
 
-        left_names = left_multi.index.names
-        right_names = right_multi.index.names
         if join_type == "right":
-            level_order = right_names + left_names.difference(right_names)
+            level_order = ["Origin", "Destination", "Period", "LinkType", "TripPurp"]
         else:
-            level_order = left_names + right_names.difference(left_names)
+            level_order = ["Origin", "Destination", "Period", "TripPurp", "LinkType"]
 
         expected = (
             merge(
