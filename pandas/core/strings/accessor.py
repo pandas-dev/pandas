f577be25dfe97e6e4fa4192e007df2882fb3f20f
Lumberbot (aka Jack) <39504233+meeseeksmachine@users.noreply.github.com>
2024-01-29 04:44:40 +0100

    Backport PR #57089 on branch 2.2.x (BUG: wide_to_long with string columns) (#57120)
    Backport PR #57089: BUG: wide_to_long with string columns
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/core/strings/accessor.py
+++ pandas/core/strings/accessor.py
@@ -1336,14 +1336,14 @@ class StringMethods(NoNewAttributesMixin):
         return self._wrap_result(result, fill_value=na, returns_string=False)
 
     @forbid_nonstring_types(["bytes"])
-    def match(self, pat, case: bool = True, flags: int = 0, na=None):
+    def match(self, pat: str, case: bool = True, flags: int = 0, na=None):
         """
         Determine if each string starts with a match of a regular expression.
 
         Parameters
         ----------
         pat : str
-            Character sequence or regular expression.
+            Character sequence.
         case : bool, default True
             If True, case sensitive.
         flags : int, default 0 (no flags)
