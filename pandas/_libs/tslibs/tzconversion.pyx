65385592e2bae2ba9abaf601c3da7e838efc711d
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-01-29 17:25:23 -0800

    Revert "CI: Fix numpydev tz_localize_to_utc"
    This reverts commit 13af92c0d3ed7c826c1d9dc19a6b32718a3b5c2b.

--- pandas/_libs/tslibs/tzconversion.pyx
+++ pandas/_libs/tslibs/tzconversion.pyx
@@ -189,7 +189,7 @@ cdef int64_t tz_localize_to_utc_single(
 @cython.boundscheck(False)
 @cython.wraparound(False)
 def tz_localize_to_utc(
-    ndarray vals,
+    ndarray[int64_t] vals,
     tzinfo tz,
     object ambiguous=None,
     object nonexistent=None,
