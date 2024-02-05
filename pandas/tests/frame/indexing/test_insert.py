46fd7114d6ded7dcd58499776175d4ccfd04d9da
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-02-05 02:01:35 +0000

    CoW: Remove copy-on-write conditions from frame tests (#57255)

--- pandas/tests/frame/indexing/test_insert.py
+++ pandas/tests/frame/indexing/test_insert.py
@@ -71,7 +71,7 @@ class TestDataFrameInsert:
         )
         tm.assert_frame_equal(df, exp)
 
-    def test_insert_item_cache(self, using_copy_on_write):
+    def test_insert_item_cache(self):
         df = DataFrame(np.random.default_rng(2).standard_normal((4, 3)))
         ser = df[0]
         expected_warning = PerformanceWarning
@@ -80,14 +80,9 @@ class TestDataFrameInsert:
             for n in range(100):
                 df[n + 3] = df[1] * n
 
-        if using_copy_on_write:
-            ser.iloc[0] = 99
-            assert df.iloc[0, 0] == df[0][0]
-            assert df.iloc[0, 0] != 99
-        else:
-            ser.values[0] = 99
-            assert df.iloc[0, 0] == df[0][0]
-            assert df.iloc[0, 0] == 99
+        ser.iloc[0] = 99
+        assert df.iloc[0, 0] == df[0][0]
+        assert df.iloc[0, 0] != 99
 
     def test_insert_EA_no_warning(self):
         # PerformanceWarning about fragmented frame should not be raised when
