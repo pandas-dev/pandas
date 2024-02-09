cfb2a12f1268e638ae65771a61badee4dbac8d54
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-02-09 11:23:05 -0800

    Remove test

--- pandas/tests/copy_view/test_constructors.py
+++ pandas/tests/copy_view/test_constructors.py
@@ -148,25 +148,6 @@ def test_series_from_index_different_dtypes(using_copy_on_write):
         assert ser._mgr._has_no_reference(0)
 
 
-@pytest.mark.filterwarnings("ignore:Setting a value on a view:FutureWarning")
-@pytest.mark.parametrize("dtype", [None, "int64"])
-@pytest.mark.parametrize("idx", [None, pd.RangeIndex(start=0, stop=3, step=1)])
-def test_series_from_block_manager(using_copy_on_write, idx, dtype):
-    ser = Series([1, 2, 3], dtype="int64")
-    ser_orig = ser.copy()
-    ser2 = Series(ser._mgr, dtype=dtype, index=idx)
-    assert np.shares_memory(get_array(ser), get_array(ser2))
-    if using_copy_on_write:
-        assert not ser2._mgr._has_no_reference(0)
-
-    ser2.iloc[0] = 100
-    if using_copy_on_write:
-        tm.assert_series_equal(ser, ser_orig)
-    else:
-        expected = Series([100, 2, 3])
-        tm.assert_series_equal(ser, expected)
-
-
 def test_series_from_block_manager_different_dtype(using_copy_on_write):
     ser = Series([1, 2, 3], dtype="int64")
     msg = "Passing a SingleBlockManager to Series"
