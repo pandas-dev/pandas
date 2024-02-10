dc053761826a7d31cd800872fce948f87de14f1a
Luke Manley <lukemanley@gmail.com>
2024-02-10 06:38:06 -0500

    REGR: CategoricalIndex.difference with null values (#57329)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -3272,9 +3272,12 @@ class Index(IndexOpsMixin, PandasObject):
 
     def _difference(self, other, sort):
         # overridden by RangeIndex
+        this = self
+        if isinstance(self, ABCCategoricalIndex) and self.hasnans and other.hasnans:
+            this = this.dropna()
         other = other.unique()
-        the_diff = self[other.get_indexer_for(self) == -1]
-        the_diff = the_diff if self.is_unique else the_diff.unique()
+        the_diff = this[other.get_indexer_for(this) == -1]
+        the_diff = the_diff if this.is_unique else the_diff.unique()
         the_diff = _maybe_try_sort(the_diff, sort)
         return the_diff
 
