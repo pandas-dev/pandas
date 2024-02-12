c9490bda3bd7e4e2684b53c016441aebdaa8cb0d
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-02-12 18:19:42 +0100

    CoW: Remove more using_copy_on_write fixtures (#57368)

--- pandas/tests/copy_view/test_chained_assignment_deprecation.py
+++ pandas/tests/copy_view/test_chained_assignment_deprecation.py
@@ -7,30 +7,11 @@ from pandas import DataFrame
 import pandas._testing as tm
 
 
-def test_methods_iloc_warn(using_copy_on_write):
-    if not using_copy_on_write:
-        df = DataFrame({"a": [1, 2, 3], "b": 1})
-        with tm.assert_cow_warning(match="A value"):
-            df.iloc[:, 0].replace(1, 5, inplace=True)
-
-        with tm.assert_cow_warning(match="A value"):
-            df.iloc[:, 0].fillna(1, inplace=True)
-
-        with tm.assert_cow_warning(match="A value"):
-            df.iloc[:, 0].interpolate(inplace=True)
-
-        with tm.assert_cow_warning(match="A value"):
-            df.iloc[:, 0].ffill(inplace=True)
-
-        with tm.assert_cow_warning(match="A value"):
-            df.iloc[:, 0].bfill(inplace=True)
-
-
 # TODO(CoW-warn) expand the cases
 @pytest.mark.parametrize(
     "indexer", [0, [0, 1], slice(0, 2), np.array([True, False, True])]
 )
-def test_series_setitem(indexer, using_copy_on_write):
+def test_series_setitem(indexer):
     # ensure we only get a single warning for those typical cases of chained
     # assignment
     df = DataFrame({"a": [1, 2, 3], "b": 1})
@@ -40,11 +21,7 @@ def test_series_setitem(indexer, using_copy_on_write):
     with pytest.warns() as record:
         df["a"][indexer] = 0
     assert len(record) == 1
-    if using_copy_on_write:
-        assert record[0].category == ChainedAssignmentError
-    else:
-        assert record[0].category == FutureWarning
-        assert "ChainedAssignmentError" in record[0].message.args[0]
+    assert record[0].category == ChainedAssignmentError
 
 
 @pytest.mark.parametrize(
