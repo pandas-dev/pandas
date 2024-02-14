37446c183f86daedc24731fdd06182dcc41ab9f8
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-02-14 03:12:46 +0100

    Remove downcast from Index.fillna (#57410)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -2555,7 +2555,7 @@ class Index(IndexOpsMixin, PandasObject):
 
     notnull = notna
 
-    def fillna(self, value=None, downcast=lib.no_default):
+    def fillna(self, value=None):
         """
         Fill NA/NaN values with the specified value.
 
@@ -2564,12 +2564,6 @@ class Index(IndexOpsMixin, PandasObject):
         value : scalar
             Scalar value to use to fill holes (e.g. 0).
             This value cannot be a list-likes.
-        downcast : dict, default is None
-            A dict of item->dtype of what to downcast if possible,
-            or the string 'infer' which will try to downcast to an appropriate
-            equal type (e.g. float64 to int64 if possible).
-
-            .. deprecated:: 2.1.0
 
         Returns
         -------
@@ -2588,28 +2582,13 @@ class Index(IndexOpsMixin, PandasObject):
         """
         if not is_scalar(value):
             raise TypeError(f"'value' must be a scalar, passed: {type(value).__name__}")
-        if downcast is not lib.no_default:
-            warnings.warn(
-                f"The 'downcast' keyword in {type(self).__name__}.fillna is "
-                "deprecated and will be removed in a future version. "
-                "It was previously silently ignored.",
-                FutureWarning,
-                stacklevel=find_stack_level(),
-            )
-        else:
-            downcast = None
 
         if self.hasnans:
             result = self.putmask(self._isnan, value)
-            if downcast is None:
-                # no need to care metadata other than name
-                # because it can't have freq if it has NaTs
-                # _with_infer needed for test_fillna_categorical
-                return Index._with_infer(result, name=self.name)
-            raise NotImplementedError(
-                f"{type(self).__name__}.fillna does not support 'downcast' "
-                "argument values other than 'None'."
-            )
+            # no need to care metadata other than name
+            # because it can't have freq if it has NaTs
+            # _with_infer needed for test_fillna_categorical
+            return Index._with_infer(result, name=self.name)
         return self._view()
 
     def dropna(self, how: AnyAll = "any") -> Self:
