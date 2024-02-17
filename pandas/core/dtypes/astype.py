ee5d945e3ff23534f3b88f7bd8499edfbfe9039f
Patrick Hoefler <patrick_hoefler@gmx.net>
2024-02-18 00:15:38 +0100

    PERF: Improve performance for astype is view

--- pandas/core/dtypes/astype.py
+++ pandas/core/dtypes/astype.py
@@ -258,6 +258,10 @@ def astype_is_view(dtype: DtypeObj, new_dtype: DtypeObj) -> bool:
     -------
     True if new data is a view or not guaranteed to be a copy, False otherwise
     """
+    if dtype.kind in "iufb" and dtype.kind == new_dtype.kind:
+        # fastpath for numeric dtypes
+        return dtype.itemsize == new_dtype.itemsize
+
     if isinstance(dtype, np.dtype) and not isinstance(new_dtype, np.dtype):
         new_dtype, dtype = dtype, new_dtype
 
