dfce6f9d955bbe21a0cb92940b2178745a2af1e0
santhoshbethi <45058712+santhoshbethi@users.noreply.github.com>
2024-02-19 17:38:29 -0600

    adding pow example for dataframe #57419 (#57442)
    * adding pow exmpale for adtaframe #57419
    
    * Fixing the data format
    
    * removing extra space
    
    * re-run the jobs
    
    * updating docstring values
    
    * testing pipeline

--- pandas/core/ops/docstrings.py
+++ pandas/core/ops/docstrings.py
@@ -623,6 +623,15 @@ A circle        NaN      1.0
 B square        0.0      0.0
   pentagon      0.0      0.0
   hexagon       0.0      0.0
+
+>>> df_pow = pd.DataFrame({{'A': [2, 3, 4, 5],
+...                        'B': [6, 7, 8, 9]}})
+>>> df_pow.pow(2)
+    A   B
+0   4  36
+1   9  49
+2  16  64
+3  25  81
 """
 
 _flex_comp_doc_FRAME = """
