cd7f933d65d13cc17154d85b99cb477858d827b7
jrmylow <jrmylow@gmail.com>
2024-02-19 22:28:21 +1100

    add pipe to series

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -4646,6 +4646,17 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
             kwargs=kwargs,
         ).apply()
 
+    def pipe(
+        self, 
+        func: Callable,
+        *args,
+        **kwargs,
+    ):
+        """
+        Hold for documentation
+        """
+        return func(self, *args, **kwargs)
+
     def _reindex_indexer(
         self,
         new_index: Index | None,
