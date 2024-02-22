ea56e0cd720b8a4e1c26df7387e8ccb4badf6eb2
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2024-02-22 19:45:36 +0100

    Backport PR #57556 on branch 2.2.x (Remove PyArrow deprecation warning) (#57568)

--- pandas/__init__.py
+++ pandas/__init__.py
@@ -203,36 +203,7 @@ if "PANDAS_DATA_MANAGER" in os.environ:
         stacklevel=2,
     )
 
-# DeprecationWarning for missing pyarrow
-from pandas.compat.pyarrow import pa_version_under10p1, pa_not_found
-
-if pa_version_under10p1:
-    # pyarrow is either too old or nonexistent, warn
-    from pandas.compat._optional import VERSIONS
-
-    if pa_not_found:
-        pa_msg = "was not found to be installed on your system."
-    else:
-        pa_msg = (
-            f"was too old on your system - pyarrow {VERSIONS['pyarrow']} "
-            "is the current minimum supported version as of this release."
-        )
-
-    warnings.warn(
-        f"""
-Pyarrow will become a required dependency of pandas in the next major release of pandas (pandas 3.0),
-(to allow more performant data types, such as the Arrow string type, and better interoperability with other libraries)
-but {pa_msg}
-If this would cause problems for you,
-please provide us feedback at https://github.com/pandas-dev/pandas/issues/54466
-        """,  # noqa: E501
-        DeprecationWarning,
-        stacklevel=2,
-    )
-    del VERSIONS, pa_msg
-
-# Delete all unnecessary imported modules
-del pa_version_under10p1, pa_not_found, warnings, os
+del warnings, os
 
 # module level doc-string
 __doc__ = """
