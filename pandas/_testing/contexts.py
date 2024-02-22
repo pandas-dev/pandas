1ff421e05b374cce119206b8f5af291660d277a9
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-02-22 10:43:31 -0800

    Remove use_numexpr

--- pandas/_testing/contexts.py
+++ pandas/_testing/contexts.py
@@ -16,8 +16,6 @@ from pandas._config import using_copy_on_write
 from pandas.compat import PYPY
 from pandas.errors import ChainedAssignmentError
 
-from pandas import set_option
-
 from pandas.io.common import get_handle
 
 if TYPE_CHECKING:
@@ -174,24 +172,6 @@ def with_csv_dialect(name: str, **kwargs) -> Generator[None, None, None]:
         yield
     finally:
         csv.unregister_dialect(name)
-
-
-@contextmanager
-def use_numexpr(use, min_elements=None) -> Generator[None, None, None]:
-    from pandas.core.computation import expressions as expr
-
-    if min_elements is None:
-        min_elements = expr._MIN_ELEMENTS
-
-    olduse = expr.USE_NUMEXPR
-    oldmin = expr._MIN_ELEMENTS
-    set_option("compute.use_numexpr", use)
-    expr._MIN_ELEMENTS = min_elements
-    try:
-        yield
-    finally:
-        expr._MIN_ELEMENTS = oldmin
-        set_option("compute.use_numexpr", olduse)
 
 
 def raises_chained_assignment_error(warn=True, extra_warnings=(), extra_match=()):
