47cd690501c55c266a845fbbeb5515804a66d05d
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2024-02-27 17:47:50 -0500

    CLN: Enforce deprecation of using alias for builtin/NumPy funcs (#57444)
    * CLN: Enforce deprecation of using alias for builtin/NumPy funcs
    
    * GH# and whatsnew
    
    * Fixup docs
    
    * More tests
    
    * Restore docstring
    
    * Test fixes
    
    * Test fixups
    
    * Test fixes
    
    * Test fixup
    
    * Test fixes

--- pandas/core/apply.py
+++ pandas/core/apply.py
@@ -175,10 +175,7 @@ class Apply(metaclass=abc.ABCMeta):
         Result of aggregation, or None if agg cannot be performed by
         this method.
         """
-        obj = self.obj
         func = self.func
-        args = self.args
-        kwargs = self.kwargs
 
         if isinstance(func, str):
             return self.apply_str()
@@ -188,12 +185,6 @@ class Apply(metaclass=abc.ABCMeta):
         elif is_list_like(func):
             # we require a list, but not a 'str'
             return self.agg_list_like()
-
-        if callable(func):
-            f = com.get_cython_func(func)
-            if f and not args and not kwargs:
-                warn_alias_replacement(obj, func, f)
-                return getattr(obj, f)()
 
         # caller can react
         return None
@@ -299,12 +290,6 @@ class Apply(metaclass=abc.ABCMeta):
 
         if isinstance(func, str):
             return self._apply_str(obj, func, *args, **kwargs)
-
-        if not args and not kwargs:
-            f = com.get_cython_func(func)
-            if f:
-                warn_alias_replacement(obj, func, f)
-                return getattr(obj, f)()
 
         # Two possible ways to use a UDF - apply or call directly
         try:
