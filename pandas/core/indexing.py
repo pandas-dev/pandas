192b9e70280c4bacc50209327f65fc1c68682838
Soumya Sri Bethi <sbethi2@leomail.tamuc.edu>
2024-02-27 22:40:46 -0600

    fixing bugs

--- pandas/core/indexing.py
+++ pandas/core/indexing.py
@@ -1490,10 +1490,13 @@ class _LocIndexer(_LocationIndexer):
 
             # if we are a label return me
             try:
-                label = labels.get_loc(key)
-                if label.all():
+                label= labels.get_loc(key)
+                if isinstance(label, np.ndarray):
+                    if label.all():
+                        return label
+                if isinstance(label, int) and label:
                     return label
-                return {"key":key}
+                return {"key": key}
             except LookupError:
                 if isinstance(key, tuple) and isinstance(labels, MultiIndex):
                     if len(key) == labels.nlevels:
