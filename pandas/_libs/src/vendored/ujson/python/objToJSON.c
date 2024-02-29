f2b7a966968794961ba5861f35c00bdc5d6ede1d
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2024-02-28 19:17:27 -0500

    fix warning

--- pandas/_libs/src/vendored/ujson/python/objToJSON.c
+++ pandas/_libs/src/vendored/ujson/python/objToJSON.c
@@ -490,7 +490,8 @@ static int NpyArr_iterNextItem(JSOBJ obj, JSONTypeContext *tc) {
     ((PyObjectEncoder *)tc->encoder)->npyValue = npyarr->dataptr;
     ((PyObjectEncoder *)tc->encoder)->npyCtxtPassthru = npyarr;
   } else {
-    GET_TC(tc)->itemValue = PyArray_GETITEM(npyarr->array, npyarr->dataptr);
+    GET_TC(tc)->itemValue =
+        PyArray_GETITEM((PyArrayObject *)npyarr->array, npyarr->dataptr);
   }
 
   npyarr->dataptr += npyarr->stride;
