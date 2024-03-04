8434b1ce0bbbe3fbb3a243adcd857aff62cc48e5
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-03-04 11:51:30 -0800

    Check other call

--- pandas/tests/scalar/timestamp/test_formats.py
+++ pandas/tests/scalar/timestamp/test_formats.py
@@ -105,13 +105,13 @@ class TestTimestampRendering:
 
         date_only = Timestamp(date)
         assert date in repr(date_only)
-        assert tz_repr not in repr(date_only), repr(date_only)
+        assert tz_repr not in repr(date_only)
         assert freq_repr not in repr(date_only)
         assert date_only == eval(repr(date_only))
 
         date_tz = Timestamp(date, tz=tz)
         assert date in repr(date_tz)
-        assert tz_repr in repr(date_tz)
+        assert tz_repr in repr(date_tz), repr(date_tz)
         assert freq_repr not in repr(date_tz)
         assert date_tz == eval(repr(date_tz))
 
