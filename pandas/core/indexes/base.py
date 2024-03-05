92302df255dcf60b6db3a784c4200dd52d36c740
Marc Garcia <garcia.marc@gmail.com>
2024-03-05 20:32:22 +0100

    Update pandas/core/indexes/base.py

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -1023,7 +1023,7 @@ class Index(IndexOpsMixin, PandasObject):
         Parameters
         ----------
         cls : dtype, optional
-            The dtype for the returned view of the Index. If None (default), a view
+            The dtype for the returned view of the Index. If ``None`` (default), a view
             of the calling Index is returned without altering the dtype. If a numeric
             dtype is specified, the method attempts to reinterpret the Index data as
             the specified dtype.
