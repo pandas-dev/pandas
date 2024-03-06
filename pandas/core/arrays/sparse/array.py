a0784d2c59c04e0a9e1ef22d696adb5768b5541e
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-03-06 12:00:52 -1000

    REF: Remove dynamic docstrings from option methods (#57710)
    * REF: Remove dynamic docstrings from option methods
    
    * Fix arguments
    
    * Reuse
    
    * Fix drop duplicate section, numpydoc valudation
    
    * Fix formatting

--- pandas/core/arrays/sparse/array.py
+++ pandas/core/arrays/sparse/array.py
@@ -18,7 +18,7 @@ import warnings
 
 import numpy as np
 
-from pandas._config.config import _get_option
+from pandas._config.config import get_option
 
 from pandas._libs import lib
 import pandas._libs.sparse as splib
@@ -1158,7 +1158,7 @@ class SparseArray(OpsMixin, PandasObject, ExtensionArray):
         side: Literal["left", "right"] = "left",
         sorter: NumpySorter | None = None,
     ) -> npt.NDArray[np.intp] | np.intp:
-        if _get_option("performance_warnings"):
+        if get_option("performance_warnings"):
             msg = "searchsorted requires high memory usage."
             warnings.warn(msg, PerformanceWarning, stacklevel=find_stack_level())
         v = np.asarray(v)
