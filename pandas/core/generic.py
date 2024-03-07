ddbe93b5d831b4cdc4b5bd2be5b4cbf4dc0e515d
Natalia Mokeeva <natalia.mokeeva.de@gmail.com>
2024-03-07 10:41:32 +0100

    enforce deprecation of param method in df.fillna: correct def fillna, fix tests

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -6769,7 +6769,6 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         self,
         value: Hashable | Mapping | Series | DataFrame = ...,
         *,
-        method: FillnaOptions | None = ...,
         axis: Axis | None = ...,
         inplace: Literal[False] = ...,
         limit: int | None = ...,
@@ -6781,7 +6780,6 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         self,
         value: Hashable | Mapping | Series | DataFrame = ...,
         *,
-        method: FillnaOptions | None = ...,
         axis: Axis | None = ...,
         inplace: Literal[True],
         limit: int | None = ...,
@@ -6793,7 +6791,6 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         self,
         value: Hashable | Mapping | Series | DataFrame = ...,
         *,
-        method: FillnaOptions | None = ...,
         axis: Axis | None = ...,
         inplace: bool = ...,
         limit: int | None = ...,
@@ -6809,7 +6806,6 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
         self,
         value: Hashable | Mapping | Series | DataFrame | None = None,
         *,
-        method: FillnaOptions | None = None,
         axis: Axis | None = None,
         inplace: bool = False,
         limit: int | None = None,
@@ -6825,15 +6821,6 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
             each index (for a Series) or column (for a DataFrame).  Values not
             in the dict/Series/DataFrame will not be filled. This value cannot
             be a list.
-        method : {{'backfill', 'bfill', 'ffill', None}}, default None
-            Method to use for filling holes in reindexed Series:
-
-            * ffill: propagate last valid observation forward to next valid.
-            * backfill / bfill: use next valid observation to fill gap.
-
-            .. deprecated:: 2.1.0
-                Use ffill or bfill instead.
-
         axis : {axes_single_arg}
             Axis along which to fill missing values. For `Series`
             this parameter is unused and defaults to 0.
@@ -6932,15 +6919,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
                         stacklevel=2,
                     )
 
-        value, method = validate_fillna_kwargs(value, method)
-        if method is not None:
-            warnings.warn(
-                f"{type(self).__name__}.fillna with 'method' is deprecated and "
-                "will raise in a future version. Use obj.ffill() or obj.bfill() "
-                "instead.",
-                FutureWarning,
-                stacklevel=find_stack_level(),
-            )
+        value, _ = validate_fillna_kwargs(value, None)
 
         # set the default here, so functions examining the signaure
         # can detect if something was set (e.g. in groupby) (GH9221)
@@ -6953,7 +6932,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
                 # error: Argument 1 to "_pad_or_backfill" of "NDFrame" has
                 # incompatible type "Optional[Literal['backfill', 'bfill', 'ffill',
                 # 'pad']]"; expected "Literal['ffill', 'bfill', 'pad', 'backfill']"
-                method,  # type: ignore[arg-type]
+                method=None,  # type: ignore[arg-type]
                 axis=axis,
                 limit=limit,
                 inplace=inplace,
