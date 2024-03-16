5c1b24351428f7dd62a16bf9d0b97f63d7f56025
s1099 <46890315+s1099@users.noreply.github.com>
2024-03-17 03:36:05 +0530

    Update ci/code_checks.sh
    Co-authored-by: Marc Garcia <garcia.marc@gmail.com>

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -436,7 +436,7 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         pandas.Series.cat.rename_categories\
         pandas.Series.cat.reorder_categories\
         pandas.Series.cat.set_categories\
-        pandas.Series.dt\
+        pandas.Series.dt `# Accessors are implemented as classes, but we do not document the Parameters section` \
         pandas.Series.dt.as_unit\
         pandas.Series.dt.ceil\
         pandas.Series.dt.day_name\
