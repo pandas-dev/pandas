ce09e7897ce0797d041f506ff8dd4f3c2de62df6
Marco Edward Gorelli <marcogorelli@protonmail.com>
2024-03-20 21:10:55 +0000

    BUG: PyArrow dtypes were not supported in the interchange protocol (#57764)
    * fix pyarrow interchange
    
    * reduce diff
    
    * reduce diff
    
    * start simplifying
    
    * simplify, remove is_validity arg
    
    * remove unnecessary branch
    
    * doc maybe_rechunk
    
    * mypy
    
    * extra test
    
    * mark _col unused, assert rechunking did not modify original df
    
    * declare buffer: Buffer outside of if/else branch

--- pandas/core/interchange/dataframe.py
+++ pandas/core/interchange/dataframe.py
@@ -5,6 +5,7 @@ from typing import TYPE_CHECKING
 
 from pandas.core.interchange.column import PandasColumn
 from pandas.core.interchange.dataframe_protocol import DataFrame as DataFrameXchg
+from pandas.core.interchange.utils import maybe_rechunk
 
 if TYPE_CHECKING:
     from collections.abc import (
@@ -34,6 +35,10 @@ class PandasDataFrameXchg(DataFrameXchg):
         """
         self._df = df.rename(columns=str, copy=False)
         self._allow_copy = allow_copy
+        for i, _col in enumerate(self._df.columns):
+            rechunked = maybe_rechunk(self._df.iloc[:, i], allow_copy=allow_copy)
+            if rechunked is not None:
+                self._df.isetitem(i, rechunked)
 
     def __dataframe__(
         self, nan_as_null: bool = False, allow_copy: bool = True
