d54d198a3824a6c13150a494b7f4b2af247737aa
Agriya Khetarpal <74401230+agriyakhetarpal@users.noreply.github.com>
2024-03-20 22:02:56 +0530

    Try to fix OverflowError with date ranges

--- pandas/tests/indexes/datetimes/methods/test_resolution.py
+++ pandas/tests/indexes/datetimes/methods/test_resolution.py
@@ -1,7 +1,7 @@
 from dateutil.tz import tzlocal
 import pytest
 
-from pandas.compat import IS64
+from pandas.compat import IS64, WASM
 
 from pandas import date_range
 
@@ -22,7 +22,7 @@ from pandas import date_range
 )
 def test_dti_resolution(request, tz_naive_fixture, freq, expected):
     tz = tz_naive_fixture
-    if freq == "YE" and not IS64 and isinstance(tz, tzlocal):
+    if freq == "YE" and ((not IS64) or WASM) and isinstance(tz, tzlocal):
         request.applymarker(
             pytest.mark.xfail(reason="OverflowError inside tzlocal past 2038")
         )
