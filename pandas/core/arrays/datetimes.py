872c524f5be7056293d3ad30d764bade44d4deb8
jrmylow <33999325+jrmylow@users.noreply.github.com>
2024-03-21 05:59:30 +1100

    BUG: Negative freq in date_range produces values out of start and endpoints (#56832)
    * pandas-dev#56147 negative offset and year end interaction
    
    * pandas-dev#56147 tests
    
    * documentation
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * fixing typing/pylint to mirror other branch
    
    * moved note to Datetimelike
    
    * documentation re-merge
    
    * whatsnew update
    
    * updated date_range docstring
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * reformatted docstring
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update doc/source/whatsnew/v3.0.0.rst
    
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>

--- pandas/core/arrays/datetimes.py
+++ pandas/core/arrays/datetimes.py
@@ -2777,7 +2777,12 @@ def _generate_range(
     if start and not offset.is_on_offset(start):
         # Incompatible types in assignment (expression has type "datetime",
         # variable has type "Optional[Timestamp]")
-        start = offset.rollforward(start)  # type: ignore[assignment]
+
+        # GH #56147 account for negative direction and range bounds
+        if offset.n >= 0:
+            start = offset.rollforward(start)  # type: ignore[assignment]
+        else:
+            start = offset.rollback(start)  # type: ignore[assignment]
 
     # Unsupported operand types for < ("Timestamp" and "None")
     if periods is None and end < start and offset.n >= 0:  # type: ignore[operator]
