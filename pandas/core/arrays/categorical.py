7612f39f7944d0713facd1b82669f89c05139bbb
jbrockmendel <jbrockmendel@gmail.com>
2024-03-25 15:17:40 -0700

    DEPR: remove Categorical.to_list (#58000)

--- pandas/core/arrays/categorical.py
+++ pandas/core/arrays/categorical.py
@@ -626,19 +626,6 @@ class Categorical(NDArrayBackedExtensionArray, PandasObject, ObjectStringArrayMi
 
         return result
 
-    def to_list(self) -> list:
-        """
-        Alias for tolist.
-        """
-        # GH#51254
-        warnings.warn(
-            "Categorical.to_list is deprecated and will be removed in a future "
-            "version. Use obj.tolist() instead",
-            FutureWarning,
-            stacklevel=find_stack_level(),
-        )
-        return self.tolist()
-
     @classmethod
     def _from_inferred_categories(
         cls, inferred_categories, inferred_codes, dtype, true_values=None
