145aae024b628fc0c72c62f9db64425581a73502
rohanjain101 <38412262+rohanjain101@users.noreply.github.com>
2024-03-25 14:10:55 -0400

    improve accuracy of to_pytimedelta (#57841)
    * improve accuracy of to_pytimedelta
    
    * f
    
    * f
    
    * whatsnew
    
    * f
    
    ---------
    
    Co-authored-by: Rohan Jain <rohanjain@microsoft.com>

--- pandas/tests/scalar/timedelta/test_timedelta.py
+++ pandas/tests/scalar/timedelta/test_timedelta.py
@@ -665,3 +665,10 @@ def test_timedelta_attribute_precision():
     result += td.nanoseconds
     expected = td._value
     assert result == expected
+
+
+def test_to_pytimedelta_large_values():
+    td = Timedelta(1152921504609987375, unit="ns")
+    result = td.to_pytimedelta()
+    expected = timedelta(days=13343, seconds=86304, microseconds=609987)
+    assert result == expected
