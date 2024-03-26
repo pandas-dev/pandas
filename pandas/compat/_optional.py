b5a98775d5241ddfd78fe191830a8db4d095e3dd
jbrockmendel <jbrockmendel@gmail.com>
2024-03-26 13:32:57 -0700

    DEPR: remove DTA.__init__, TDA.__init__ (#58004)
    * DEPR: remove DTA.__init__, TDA.__init__
    
    * update docstring
    
    * Bump fastparquet to 2023.10.0

--- pandas/compat/_optional.py
+++ pandas/compat/_optional.py
@@ -25,7 +25,7 @@ VERSIONS = {
     "bs4": "4.11.2",
     "blosc": "1.21.3",
     "bottleneck": "1.3.6",
-    "fastparquet": "2023.04.0",
+    "fastparquet": "2023.10.0",
     "fsspec": "2022.11.0",
     "html5lib": "1.1",
     "hypothesis": "6.46.1",
