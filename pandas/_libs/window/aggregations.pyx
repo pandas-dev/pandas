2e674167b9fd882ae4b57f2393710b24fbadf329
Philipp Hoffmann <Philipp.Hoffmann@protonmail.com>
2024-03-26 23:56:08 +0100

    add comment and code path

--- pandas/_libs/window/aggregations.pyx
+++ pandas/_libs/window/aggregations.pyx
@@ -508,6 +508,7 @@ cdef float64_t calc_skew(int64_t minp, int64_t nobs,
         #         if the variance is less than 1e-14, it could be
         #         treat as zero, here we follow the original
         #         skew/kurt behaviour to check B <= 1e-14
+        # #57972: for small arrays the cutoff can be lowered
         elif B <= 1e-14 and nobs > 100 or B <= 1e-16:
             result = NaN
         else:
@@ -712,7 +713,8 @@ cdef float64_t calc_kurt(int64_t minp, int64_t nobs,
             #         if the variance is less than 1e-14, it could be
             #         treat as zero, here we follow the original
             #         skew/kurt behaviour to check B <= 1e-14
-            if B <= 1e-14:
+            # #57972: for small arrays the cutoff can be lowered
+            if B <= 1e-14 and nobs > 100 or B <= 1e-16:
                 result = NaN
             else:
                 K = (dnobs * dnobs - 1.) * D / (B * B) - 3 * ((dnobs - 1.) ** 2)
