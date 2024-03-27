15aaf00952d18d6ef74dc6e1055e812c71e2f393
Natalia Mokeeva <natalia.mokeeva.de@gmail.com>
2024-03-27 13:37:00 +0100

    enforce deprecation any/all with datetime64, correct tests

--- pandas/core/arrays/datetimelike.py
+++ pandas/core/arrays/datetimelike.py
@@ -2217,11 +2217,11 @@ class TimelikeOps(DatetimeLikeArrayMixin):
     # Reductions
 
     def any(self, *, axis: AxisInt | None = None, skipna: bool = True) -> bool:
-        # GH#34479 the nanops call will issue a FutureWarning for non-td64 dtype
+        # GH#34479 the nanops call will raise a TypeError for non-td64 dtype
         return nanops.nanany(self._ndarray, axis=axis, skipna=skipna, mask=self.isna())
 
     def all(self, *, axis: AxisInt | None = None, skipna: bool = True) -> bool:
-        # GH#34479 the nanops call will issue a FutureWarning for non-td64 dtype
+        # GH#34479 the nanops call will raise a TypeError for non-td64 dtype
 
         return nanops.nanall(self._ndarray, axis=axis, skipna=skipna, mask=self.isna())
 
