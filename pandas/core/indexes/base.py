e62ab553f6914b718bf905ef9e3f99d5296f8528
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-03-27 10:44:11 -0700

    don't overwrite sequence

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -7161,19 +7161,18 @@ def maybe_sequence_to_range(sequence) -> Any | range:
         return sequence
     elif len(sequence) == 1 or lib.infer_dtype(sequence, skipna=False) != "integer":
         return sequence
-    elif isinstance(sequence, (ABCSeries, Index)):
-        if not (isinstance(sequence.dtype, np.dtype) and sequence.dtype.kind == "i"):
-            return sequence
-        sequence = np.asarray(sequence)
+    elif isinstance(sequence, (ABCSeries, Index)) and not (
+        isinstance(sequence.dtype, np.dtype) and sequence.dtype.kind == "i"
+    ):
+        return sequence
     if len(sequence) == 0:
         return range(0)
-    diff = sequence[1] - sequence[0]
+    np_sequence = np.asarray(sequence, dtype=np.int64)
+    diff = np_sequence[1] - np_sequence[0]
     if diff == 0:
         return sequence
-    elif len(sequence) == 2 or lib.is_sequence_range(
-        np.asarray(sequence, dtype=np.int64), diff
-    ):
-        return range(sequence[0], sequence[-1] + diff, diff)
+    elif len(sequence) == 2 or lib.is_sequence_range(np_sequence, diff):
+        return range(np_sequence[0], np_sequence[-1] + diff, diff)
     else:
         return sequence
 
