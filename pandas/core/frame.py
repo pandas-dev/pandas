b9ffd56445425921f70b9f29178d93f22c8e36f4
gboeker <68177766+gboeker@users.noreply.github.com>
2024-03-27 12:42:24 -0400

    remove trailing space

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -6011,7 +6011,7 @@ class DataFrame(NDFrame, OpsMixin):
 
         names : int, str or 1-dimensional list, default None
             Using the given string, rename the DataFrame column which contains the
-            index data. If the DataFrame has a MultiIndex, this has to be a list 
+            index data. If the DataFrame has a MultiIndex, this has to be a list
             with length equal to the number of levels.
 
             .. versionadded:: 1.5.0
