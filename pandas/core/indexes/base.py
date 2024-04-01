e66c15f6f6a571913b2fc09a2b08ad4d62af6d9d
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-04-01 08:21:14 -1000

    PERF: Allow np.integer Series/Index to convert to RangeIndex (#58016)
    * PERF: Allow np.integer Series/Index to convert to RangeIndex
    
    * cast Series to array
    
    * missing not
    
    * Remove int32 casting in stata tests
    
    * Add casting
    
    * Specify int64
    
    * don't overwrite sequence

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -7115,17 +7115,22 @@ def maybe_sequence_to_range(sequence) -> Any | range:
     -------
     Any : input or range
     """
-    if isinstance(sequence, (ABCSeries, Index, range, ExtensionArray)):
+    if isinstance(sequence, (range, ExtensionArray)):
         return sequence
     elif len(sequence) == 1 or lib.infer_dtype(sequence, skipna=False) != "integer":
         return sequence
-    elif len(sequence) == 0:
+    elif isinstance(sequence, (ABCSeries, Index)) and not (
+        isinstance(sequence.dtype, np.dtype) and sequence.dtype.kind == "i"
+    ):
+        return sequence
+    if len(sequence) == 0:
         return range(0)
-    diff = sequence[1] - sequence[0]
+    np_sequence = np.asarray(sequence, dtype=np.int64)
+    diff = np_sequence[1] - np_sequence[0]
     if diff == 0:
         return sequence
-    elif len(sequence) == 2 or lib.is_sequence_range(np.asarray(sequence), diff):
-        return range(sequence[0], sequence[-1] + diff, diff)
+    elif len(sequence) == 2 or lib.is_sequence_range(np_sequence, diff):
+        return range(np_sequence[0], np_sequence[-1] + diff, diff)
     else:
         return sequence
 
