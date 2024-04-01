007debdc89643a1e62f5cf0db9316ac5440bd104
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-04-01 08:21:14 -1000

    PERF: Allow np.integer Series/Index to convert to RangeIndex (#58016)
    * PERF: Allow np.integer Series/Index to convert to RangeIndex
    
    * cast Series to array
    
    * missing not
    
    * Remove int32 casting in stata tests
    
    * Add casting
    
    * Specify int64
    
    * don't overwrite sequence

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -2192,23 +2192,28 @@ class TestMergeOnIndexes:
 
 @pytest.mark.parametrize(
     "index",
-    [Index([1, 2], dtype=dtyp, name="index_col") for dtyp in tm.ALL_REAL_NUMPY_DTYPES]
+    [
+        Index([1, 2, 4], dtype=dtyp, name="index_col")
+        for dtyp in tm.ALL_REAL_NUMPY_DTYPES
+    ]
     + [
-        CategoricalIndex(["A", "B"], categories=["A", "B"], name="index_col"),
-        RangeIndex(start=0, stop=2, name="index_col"),
-        DatetimeIndex(["2018-01-01", "2018-01-02"], name="index_col"),
+        CategoricalIndex(["A", "B", "C"], categories=["A", "B", "C"], name="index_col"),
+        RangeIndex(start=0, stop=3, name="index_col"),
+        DatetimeIndex(["2018-01-01", "2018-01-02", "2018-01-03"], name="index_col"),
     ],
     ids=lambda x: f"{type(x).__name__}[{x.dtype}]",
 )
 def test_merge_index_types(index):
     # gh-20777
     # assert key access is consistent across index types
-    left = DataFrame({"left_data": [1, 2]}, index=index)
-    right = DataFrame({"right_data": [1.0, 2.0]}, index=index)
+    left = DataFrame({"left_data": [1, 2, 3]}, index=index)
+    right = DataFrame({"right_data": [1.0, 2.0, 3.0]}, index=index)
 
     result = left.merge(right, on=["index_col"])
 
-    expected = DataFrame({"left_data": [1, 2], "right_data": [1.0, 2.0]}, index=index)
+    expected = DataFrame(
+        {"left_data": [1, 2, 3], "right_data": [1.0, 2.0, 3.0]}, index=index
+    )
     tm.assert_frame_equal(result, expected)
 
 
