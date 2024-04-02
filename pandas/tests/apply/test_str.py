0232aa21f7afadcf9eac2f08e8ba2e3bb1c61467
Abdulaziz Aloqeely <52792999+DAzVise@users.noreply.github.com>
2024-04-02 22:46:45 +0300

    Line not long anymore!

--- pandas/tests/apply/test_str.py
+++ pandas/tests/apply/test_str.py
@@ -42,7 +42,9 @@ def test_apply_with_string_funcs(request, float_frame, func, args, kwds, how):
     if args:
         expected_warning = FutureWarning
 
-    with tm.assert_produces_warning(expected_warning, match="Starting with pandas version 3.0 all arguments of"):
+    with tm.assert_produces_warning(
+        expected_warning, match="Starting with pandas version 3.0 all arguments of"
+    ):
         result = getattr(float_frame, how)(func, *args, **kwds)
         expected = getattr(float_frame, func)(*args, **kwds)
     tm.assert_series_equal(result, expected)
