5466f15462784eaa26e7ac43fc382f3802ab3ed1
Thomas Li <47963215+lithomas1@users.noreply.github.com>
2024-04-10 09:01:08 -0400

    Backport PR #58202: DOC/TST: Document numpy 2.0 support and add tests… (#58208)
    Backport PR #58202: DOC/TST: Document numpy 2.0 support and add tests for string array

--- pandas/tests/frame/test_constructors.py
+++ pandas/tests/frame/test_constructors.py
@@ -24,6 +24,7 @@ import pytz
 from pandas._config import using_pyarrow_string_dtype
 
 from pandas._libs import lib
+from pandas.compat.numpy import np_version_gt2
 from pandas.errors import IntCastingNaNError
 import pandas.util._test_decorators as td
 
@@ -3117,6 +3118,24 @@ class TestDataFrameConstructorWithDatetimeTZ:
             DataFrame(data, index={"a", "b"})
         with pytest.raises(ValueError, match="columns cannot be a set"):
             DataFrame(data, columns={"a", "b", "c"})
+
+    # TODO: make this not cast to object in pandas 3.0
+    @pytest.mark.skipif(
+        not np_version_gt2, reason="StringDType only available in numpy 2 and above"
+    )
+    @pytest.mark.parametrize(
+        "data",
+        [
+            {"a": ["a", "b", "c"], "b": [1.0, 2.0, 3.0], "c": ["d", "e", "f"]},
+        ],
+    )
+    def test_np_string_array_object_cast(self, data):
+        from numpy.dtypes import StringDType
+
+        data["a"] = np.array(data["a"], dtype=StringDType())
+        res = DataFrame(data)
+        assert res["a"].dtype == np.object_
+        assert (res["a"] == data["a"]).all()
 
 
 def get1(obj):  # TODO: make a helper in tm?
