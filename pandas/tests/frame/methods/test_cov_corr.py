bbd06cecfe104e85434081a949bca79533583dda
weikhor <cheanwei.KCW@gmail.com>
2024-04-13 03:31:47 +0800

    enhance test case

--- pandas/tests/frame/methods/test_cov_corr.py
+++ pandas/tests/frame/methods/test_cov_corr.py
@@ -461,3 +461,27 @@ class TestDataFrameCorrWith:
         result = df_bool.corrwith(ser_bool)
         expected = Series([0.57735, 0.57735], index=["A", "B"])
         tm.assert_series_equal(result, expected)
+
+    def test_corrwith_min_periods(self):
+        # GH#9490
+        df1 = DataFrame(
+            {
+                "A": [1, np.nan, 7, 8],
+                "B": [False, True, True, False],
+                "C": [10, 4, 9, 3],
+            }
+        )
+        df2 = df1[["B", "C"]]
+        result = (df1 + 1).corrwith(df2.B, method="spearman", min_periods=2)
+        expected = Series([0.0, 1.0, 0.0], index=["A", "B", "C"])
+        tm.assert_series_equal(result, expected)
+
+        df_bool = DataFrame(
+            {"A": [True, True, False, False], "B": [True, False, False, True]}
+        )
+        ser_bool = Series([True, True, False, True])
+        result = df_bool.corrwith(ser_bool, min_periods=3)
+        expected = Series(
+            [0.57735, 0.57735], index=["A", "B"]
+        )  # Expected values based on min_periods
+        tm.assert_series_equal(result, expected)
