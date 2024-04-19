943b9d9be1000500b6bd6e17cb308036a8537e94
Natalia Mokeeva <91160475+natmokval@users.noreply.github.com>
2024-04-19 19:20:28 +0200

    BUG:  `df.plot` with a `PeriodIndex` causes a shift to the right when the frequency multiplier is greater than one (#58322)
    * bug plots makes shift for period_range
    
    * add a note to v3.0.0, correct test_plot_period_index_makes_no_right_shift

--- pandas/plotting/_matplotlib/timeseries.py
+++ pandas/plotting/_matplotlib/timeseries.py
@@ -310,7 +310,7 @@ def maybe_convert_index(ax: Axes, data: NDFrameT) -> NDFrameT:
             if isinstance(data.index, ABCDatetimeIndex):
                 data = data.tz_localize(None).to_period(freq=freq_str)
             elif isinstance(data.index, ABCPeriodIndex):
-                data.index = data.index.asfreq(freq=freq_str)
+                data.index = data.index.asfreq(freq=freq_str, how="start")
     return data
 
 
