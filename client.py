8f219626756e6c70c73b9a1e8946218f55291a03
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-04-20 10:16:22 +0530

    deleted client.py

--- client.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-df1 = pd.DataFrame([[1, 2], [3, 4]], columns=["A", "B"])
-store = pd.HDFStore("store.h5", "w")  # doctest: +SKIP
-store.put("data", df1, format="table")  # doctest: +SKIP
-df2 = pd.DataFrame([[5, 6], [7, 8]], columns=["A", "B"])
-store.append("data", df2,complib="blah")  # doctest: +SKIP
-store.close()  # doctest: +SKIP
