c0e7f6f87483d66d051bc6d63566cc1881a74fc5
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-04-24 01:38:30 +0530

    DOC: Enforce Numpy Docstring Validation for pandas.DatetimeTZDtype.unit (#58387)
    * DOC: add SA01 to DatetimeTZDtype.tz
    
    * DOC: remove SA01 of DatetimeTZDtype.unit

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -116,7 +116,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.DatetimeIndex.tz_convert RT03" \
         -i "pandas.DatetimeTZDtype SA01" \
         -i "pandas.DatetimeTZDtype.tz SA01" \
-        -i "pandas.DatetimeTZDtype.unit SA01" \
         -i "pandas.Grouper PR02" \
         -i "pandas.Index PR07" \
         -i "pandas.Index.T SA01" \
