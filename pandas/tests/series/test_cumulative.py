babc83548b454aca71557b3a8a9c7ac1cf17f9fb
Christopher Xiang <xiangc@umich.edu>
2024-04-23 14:36:16 -0400

    Assigned expected results to expected variable; Added pytest.mark.parametrize to test_cummax_cummin_ordered_categorical_nan with skipna and expected data

--- pandas/tests/series/test_cumulative.py
+++ pandas/tests/series/test_cumulative.py
@@ -181,11 +181,23 @@ class TestSeriesCumulativeOps:
         # GH#52335
         cat = pd.CategoricalDtype(list(order), ordered=True)
         ser = pd.Series(
-            list("ababcab"), dtype=pd.CategoricalDtype(list(order), ordered=True)
+            list("ababcab"),
+            dtype=cat,
         )
         result = getattr(ser, method)()
-        tm.assert_series_equal(result, pd.Series(list("abbbccc"), dtype=cat))
+        expected = pd.Series(
+            list("abbbccc"),
+            dtype=cat,
+        )
+        tm.assert_series_equal(result, expected)
 
+    @pytest.mark.parametrize(
+        "skip, exp",
+        [
+            [True, ["a", np.nan, "b", "b", "c"]],
+            [False, ["a", np.nan, np.nan, np.nan, np.nan]],
+        ],
+    )
     @pytest.mark.parametrize(
         "method, order",
         [
@@ -193,28 +205,21 @@ class TestSeriesCumulativeOps:
             ["cummin", "cba"],
         ],
     )
-    def test_cummax_cummin_ordered_categorical_nan(self, method, order):
+    def test_cummax_cummin_ordered_categorical_nan(self, skip, exp, method, order):
         # GH#52335
+        cat = pd.CategoricalDtype(list(order), ordered=True)
         ser = pd.Series(
             ["a", np.nan, "b", "a", "c"],
-            dtype=pd.CategoricalDtype(list(order), ordered=True),
+            dtype=cat,
         )
-        result = getattr(ser, method)(skipna=True)
+        result = getattr(ser, method)(skipna=skip)
+        expected = pd.Series(
+            exp,
+            dtype=cat,
+        )
         tm.assert_series_equal(
             result,
-            pd.Series(
-                ["a", np.nan, "b", "b", "c"],
-                dtype=pd.CategoricalDtype(list(order), ordered=True),
-            ),
-        )
-
-        result = getattr(ser, method)(skipna=False)
-        tm.assert_series_equal(
-            result,
-            pd.Series(
-                ["a", np.nan, np.nan, np.nan, np.nan],
-                dtype=pd.CategoricalDtype(list(order), ordered=True),
-            ),
+            expected,
         )
 
     def test_cumprod_timedelta(self):
