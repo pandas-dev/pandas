fcfe702c7233d2c8480e858e769036c976a18c09
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-04-23 17:55:35 -0700

    Update pandas/tests/arrays/test_datetimelike.py

--- pandas/tests/arrays/test_datetimelike.py
+++ pandas/tests/arrays/test_datetimelike.py
@@ -662,6 +662,7 @@ class TestDatetimeArray(SharedTests):
         tm.assert_numpy_array_equal(result, expected)
         result = np.array(arr, dtype="datetime64[ns]")
         if not np_version_gt2:
+            # TODO: GH 57739
             assert result is not expected
         tm.assert_numpy_array_equal(result, expected)
 
