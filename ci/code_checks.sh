2a01ddfb31f7c6cc6cff7bcb0052208f1f5c3549
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-04-24 14:37:35 +0530

    DOC: remove pandas.Series.item

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -296,7 +296,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Series.is_monotonic_decreasing SA01" \
         -i "pandas.Series.is_monotonic_increasing SA01" \
         -i "pandas.Series.is_unique SA01" \
-        -i "pandas.Series.item SA01" \
         -i "pandas.Series.kurt RT03,SA01" \
         -i "pandas.Series.kurtosis RT03,SA01" \
         -i "pandas.Series.le PR07,SA01" \
