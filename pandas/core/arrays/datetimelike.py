dc508cbe4e6267c86894370a03fe84a2d76fbd85
Abdulaziz Aloqeely <52792999+Aloqeely@users.noreply.github.com>
2024-04-27 00:06:02 +0300

    BUG: Overflow warning when using Series.diff() on Series with Periods

--- pandas/core/arrays/datetimelike.py
+++ pandas/core/arrays/datetimelike.py
@@ -1303,16 +1303,18 @@ class DatetimeLikeArrayMixin(  # type: ignore[misc]
         self._check_compatible_with(other)
 
         other_i8, o_mask = self._get_i8_values_and_mask(other)
-        new_i8_data = add_overflowsafe(self.asi8, np.asarray(-other_i8, dtype="i8"))
-        new_data = np.array([self.freq.base * x for x in new_i8_data])
-
+        new_data = add_overflowsafe(
+            self.asi8, np.asarray(-other_i8, dtype="i8")
+        ).astype(object)
         if o_mask is None:
             # i.e. Period scalar
             mask = self._isnan
         else:
             # i.e. PeriodArray
             mask = self._isnan | o_mask
+
         new_data[mask] = NaT
+        new_data[~mask] *= self.freq.base
         return new_data
 
     @final
