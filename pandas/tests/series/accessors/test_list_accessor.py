2af7fdb2491895da4f6e4aaaeaa49e1330aa8cfe
Rohan Jain <rohanjain@microsoft.com>
2024-04-25 20:11:10 -0400

    gh reference

--- pandas/tests/series/accessors/test_list_accessor.py
+++ pandas/tests/series/accessors/test_list_accessor.py
@@ -32,6 +32,7 @@ def test_list_getitem(list_dtype):
 
 
 def test_list_getitem_index():
+    # GH 58425
     ser = Series(
         [[1, 2, 3], [4, None, 5], None],
         dtype=ArrowDtype(pa.list_(pa.int64())),
