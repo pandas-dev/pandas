4cac6b634431d4faa7e583fda246354abfb3664e
shriyakalakata <shriyakalakata123@gmail.com>
2024-04-27 12:39:07 -0400

    Fix PR07,RT03,SA01 errors for Index.insert, Index.intersection

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -113,8 +113,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Index.get_indexer_non_unique PR07,SA01" \
         -i "pandas.Index.get_loc PR07,RT03,SA01" \
         -i "pandas.Index.identical PR01,SA01" \
-        -i "pandas.Index.insert PR07,RT03,SA01" \
-        -i "pandas.Index.intersection PR07,RT03,SA01" \
         -i "pandas.Index.join PR07,RT03,SA01" \
         -i "pandas.Index.names GL08" \
         -i "pandas.Index.nunique RT03" \
