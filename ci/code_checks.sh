47ed6ccd9139f138554dc151fd5284f64accd63e
KeiOshima <ko2091@nyu.edu>
2024-04-29 18:46:33 -0400

    fixed Expected to fail error

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -93,7 +93,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Index PR07" \
         -i "pandas.Index.append PR07,RT03,SA01" \
         -i "pandas.Index.difference PR07,RT03,SA01" \
-        -i "pandas.Index.duplicated RT03" \
         -i "pandas.Index.get_indexer PR07,SA01" \
         -i "pandas.Index.get_indexer_for PR01,SA01" \
         -i "pandas.Index.get_indexer_non_unique PR07,SA01" \
