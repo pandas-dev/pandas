e5d0adb463139e03704530a1c652c3ea3221cca0
Abir Das <mca.abirdas01@gmail.com>
2024-05-01 00:14:24 +0530

    DOC: resolve merge conflict

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -85,9 +85,7 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.DataFrame.swaplevel SA01" \
         -i "pandas.DataFrame.to_markdown SA01" \
         -i "pandas.DataFrame.var PR01,RT03,SA01" \
-        -i "pandas.Grouper PR02" \
-        -i "pandas.Index.append PR07,RT03,SA01" \
-        -i "pandas.Index.difference PR07,RT03,SA01" \
+        -i "pandas.Grouper PR02" \     
         -i "pandas.Index.get_indexer PR07,SA01" \
         -i "pandas.Index.get_indexer_for PR01,SA01" \
         -i "pandas.Index.get_indexer_non_unique PR07,SA01" \
