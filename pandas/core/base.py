7cdee7a15670b3273e45425619b493c7d74c3719
KeiOshima <ko2091@nyu.edu>
2024-04-29 20:25:50 -0400

    DOC: fixing RT03 erros for Index: duplicated and nunique (#58432)
    * DOC: fixing RT03 erros for Index: duplicated and nunique
    
    * deleting it lines from code_checks
    
    * fixing EXPECTED TO FAIL, BUT NOT FAILING error
    
    * fixing code_checks issue
    
    * fixed Expected to fail error

--- pandas/core/base.py
+++ pandas/core/base.py
@@ -1062,6 +1062,7 @@ class IndexOpsMixin(OpsMixin):
         Returns
         -------
         int
+            A integer indicating the number of unique elements in the object.
 
         See Also
         --------
