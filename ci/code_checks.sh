4333ab78f2a062b6bf18f978d6ebf280a52c0889
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-01 12:21:00 +0530

    DOC: remove PR01,RT03,SA01 in pandas.DataFrame.sem

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -77,7 +77,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.DataFrame.plot PR02,SA01" \
         -i "pandas.DataFrame.prod RT03" \
         -i "pandas.DataFrame.product RT03" \
-        -i "pandas.DataFrame.sem PR01,RT03,SA01" \
         -i "pandas.DataFrame.skew RT03,SA01" \
         -i "pandas.DataFrame.sparse PR01" \
         -i "pandas.DataFrame.std PR01,RT03,SA01" \
