db6cf69bb95f80222de1a8518189dfb37663edc3
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-01 10:42:18 +0530

    DOC: remove PR07,SA01 in pandas.Index.get_indexer

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -87,7 +87,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.DataFrame.var PR01,RT03,SA01" \
         -i "pandas.Grouper PR02" \
         -i "pandas.Index PR07" \
-        -i "pandas.Index.get_indexer PR07,SA01" \
         -i "pandas.Index.get_indexer_for PR01,SA01" \
         -i "pandas.Index.get_indexer_non_unique PR07,SA01" \
         -i "pandas.Index.get_loc PR07,RT03,SA01" \
