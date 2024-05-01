4694914fa96b8ad3ede08b54701ec0ba78d145ff
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-01 11:45:04 +0530

    DOC: remove RT03 in pandas.DataFrame.prod

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -75,7 +75,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.DataFrame.median RT03,SA01" \
         -i "pandas.DataFrame.min RT03" \
         -i "pandas.DataFrame.plot PR02,SA01" \
-        -i "pandas.DataFrame.prod RT03" \
         -i "pandas.DataFrame.product RT03" \
         -i "pandas.DataFrame.sem PR01,RT03,SA01" \
         -i "pandas.DataFrame.skew RT03,SA01" \
