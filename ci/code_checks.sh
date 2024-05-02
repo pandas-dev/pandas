ad425200fa24edec93594a1d73aec7241a57426c
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-02 23:44:54 +0530

    DOC: Enforce Numpy Docstring Validation for pandas.IntervalDtype.subtype (#58522)
    * DOC: add SA01 for pandas.IntervalDtype.subtype
    
    * DOC: remove SA01 for pandas.IntervalDtype.subtype

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -91,7 +91,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Interval.left SA01" \
         -i "pandas.Interval.mid SA01" \
         -i "pandas.Interval.right SA01" \
-        -i "pandas.IntervalDtype.subtype SA01" \
         -i "pandas.IntervalIndex.closed SA01" \
         -i "pandas.IntervalIndex.contains RT03" \
         -i "pandas.IntervalIndex.get_loc PR07,RT03,SA01" \
