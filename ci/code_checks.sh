b74ea87b84fd0bb192bc5cbeb2c35adf766f5d73
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-02 16:55:52 +0530

    DOC: remove SA01 for pandas.Series

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -165,7 +165,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.RangeIndex.start SA01" \
         -i "pandas.RangeIndex.step SA01" \
         -i "pandas.RangeIndex.stop SA01" \
-        -i "pandas.Series SA01" \
         -i "pandas.Series.__iter__ RT03,SA01" \
         -i "pandas.Series.add PR07" \
         -i "pandas.Series.backfill PR01,SA01" \
