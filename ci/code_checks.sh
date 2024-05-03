9ff2c6b8cb11963a9c63fb01791e093cb09260ce
shriyakalakata <87483933+shriyakalakata@users.noreply.github.com>
2024-05-03 16:28:42 -0400

    DOC: Fix SA01 Errors (#58556)
    * Fix SA01 errors
    
    * Fix SA01 errors

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -200,9 +200,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Series.floordiv PR07" \
         -i "pandas.Series.ge PR07,SA01" \
         -i "pandas.Series.gt PR07,SA01" \
-        -i "pandas.Series.hasnans SA01" \
-        -i "pandas.Series.is_monotonic_decreasing SA01" \
-        -i "pandas.Series.is_monotonic_increasing SA01" \
         -i "pandas.Series.kurt RT03,SA01" \
         -i "pandas.Series.kurtosis RT03,SA01" \
         -i "pandas.Series.le PR07,SA01" \
