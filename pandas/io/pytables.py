9f66a9d22a5f1addecdcbb9b82694f4aac0ef0af
Natalia Mokeeva <91160475+natmokval@users.noreply.github.com>
2024-05-03 18:47:36 +0200

    CLN: enforce the deprecation of exposing blocks in core.internals  (#58467)
    * CLN: enforce the deprecation of exposing blocks in core.internals
    
    * remove the function __getattr__, and entries from __all__
    
    * fix mypy error
    
    * add a note to v3.0.0
    
    * skip parquet tests for minimum Pyarrow version
    
    * fixup test_basic
    
    * fix pre-commit error
    
    * skip pyarrow=10.0.1 in test_parquet.py
    
    * fix mypy error
    
    * fix pre-commit error
    
    * fixup test_parquet.py
    
    * fix pre-commit error
    
    * fixup test_parquet.py
    
    * replacee pytest.skip with pytest.importorskip
    
    * skip pyarrow '10.0.1'

--- pandas/io/pytables.py
+++ pandas/io/pytables.py
@@ -125,7 +125,8 @@ if TYPE_CHECKING:
         npt,
     )
 
-    from pandas.core.internals import Block
+    from pandas.core.internals.blocks import Block
+
 
 # versioning attribute
 _version = "0.15.2"
