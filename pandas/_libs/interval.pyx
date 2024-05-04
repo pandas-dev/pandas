1bdb97f14816b6918aac7d874d03e7684d967af2
tuhinsharma121 <tuhinsharma121@gmail.com>
2024-05-04 18:33:52 +0530

    DOC: add SA01 for pandas.Interval.right

--- pandas/_libs/interval.pyx
+++ pandas/_libs/interval.pyx
@@ -390,6 +390,12 @@ cdef class Interval(IntervalMixin):
     """
     Right bound for the interval.
 
+    See Also
+    --------
+    Interval.left : Return the left bound for the interval.
+    numpy.ndarray.right : A similar method in numpy for obtaining
+        the right endpoint(s) of intervals.
+
     Examples
     --------
     >>> interval = pd.Interval(left=1, right=2, closed='left')
