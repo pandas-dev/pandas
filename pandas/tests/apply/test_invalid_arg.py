686ac922e080bccb86b4509ff221993e790d1704
Zhengbo Wang <77875500+luke396@users.noreply.github.com>
2024-05-04 14:11:46 +0800

    Add test

--- pandas/tests/apply/test_invalid_arg.py
+++ pandas/tests/apply/test_invalid_arg.py
@@ -359,3 +359,15 @@ def test_transform_reducer_raises(all_reductions, frame_or_series, op_wrapper):
     msg = "Function did not transform"
     with pytest.raises(ValueError, match=msg):
         obj.transform(op)
+
+
+def test_transform_axis_raises():
+    # GH 58474
+    df = DataFrame({"foo": [2, 4, 6], "bar": [1, 2, 3]}, index=["A", "B", "C"])
+    msg = r"Row\(s\) or Column\(s\) \['A', 'B'\] do not exist"
+    with pytest.raises(KeyError, match=msg):
+        df.transform({"A": lambda x: x + 2, "B": lambda x: x * 2}, axis=0)
+
+    msg = r"Row\(s\) or Column\(s\) \['bar', 'foo'\] do not exist"
+    with pytest.raises(KeyError, match=msg):
+        df.transform({"foo": lambda x: x + 2, "bar": lambda x: x * 2}, axis=1)
