546957e200bef00f876f3497d6e20a289deec828
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-05-08 10:23:33 -0700

    Remove _get_complex_date_index

--- pandas/io/parsers/base_parser.py
+++ pandas/io/parsers/base_parser.py
@@ -382,34 +382,6 @@ class ParserBase:
         return index
 
     @final
-    def _get_complex_date_index(self, data, col_names):
-        def _get_name(icol):
-            if isinstance(icol, str):
-                return icol
-
-            if col_names is None:
-                raise ValueError(f"Must supply column order to use {icol!s} as index")
-
-            for i, c in enumerate(col_names):
-                if i == icol:
-                    return c
-
-        to_remove = []
-        index = []
-        for idx in self.index_col:
-            name = _get_name(idx)
-            to_remove.append(name)
-            index.append(data[name])
-
-        # remove index items from content and columns, don't pop in
-        # loop
-        for c in sorted(to_remove, reverse=True):
-            data.pop(c)
-            col_names.remove(c)
-
-        return index
-
-    @final
     def _clean_mapping(self, mapping):
         """converts col numbers to names"""
         if not isinstance(mapping, dict):
