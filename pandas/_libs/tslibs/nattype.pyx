99e6938b0ea7d30332f0f2e784da8d9e772f6233
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-08 22:13:14 +0530

    DOC: Enforce Numpy Docstring Validation for pandas.Timestamp.utcnow (#58629)
    * DOC: add SA01 pandas.Timestamp.utcnow
    
    * DOC: remove SA01 pandas.Timestamp.utcnow
    
    * DOC: add SA01 to nattype

--- pandas/_libs/tslibs/nattype.pyx
+++ pandas/_libs/tslibs/nattype.pyx
@@ -769,6 +769,18 @@ class NaTType(_NaT):
 
         Return a new Timestamp representing UTC day and time.
 
+        See Also
+        --------
+        Timestamp : Constructs an arbitrary datetime.
+        Timestamp.now : Return the current local date and time, which
+            can be timezone-aware.
+        Timestamp.today : Return the current local date and time with
+            timezone information set to None.
+        to_datetime : Convert argument to datetime.
+        date_range : Return a fixed frequency DatetimeIndex.
+        Timestamp.utctimetuple : Return UTC time tuple, compatible with
+            time.localtime().
+
         Examples
         --------
         >>> pd.Timestamp.utcnow()   # doctest: +SKIP
