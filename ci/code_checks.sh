746da774d9a026973c2c258bb8bc874bea5a3874
tuhinsharma121 <tuhinsharma121@gmail.com>
2024-05-09 17:24:10 +0530

    DOC: changed Paramters to Attributes

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -77,7 +77,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.DataFrame.plot PR02,SA01" \
         -i "pandas.Grouper PR02" \
         -i "pandas.Index PR07" \
-        -i "pandas.Interval PR02" \
         -i "pandas.IntervalIndex.get_loc PR07,RT03,SA01" \
         -i "pandas.IntervalIndex.is_non_overlapping_monotonic SA01" \
         -i "pandas.IntervalIndex.left GL08" \
