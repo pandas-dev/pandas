34177d6b20896bf101590306e3f0e1e0a225c5fb
Natalia Mokeeva <91160475+natmokval@users.noreply.github.com>
2024-05-11 20:17:42 +0200

    BUG: DatetimeIndex.is_year_start breaks on custom business days frequencies bigger then `1C` (#58665)
    * fix bug dti.is_year_start breaks on freq custom business day with digit
    
    * rename the variable freqstr
    
    * delete double space

--- pandas/tests/indexes/datetimes/test_scalar_compat.py
+++ pandas/tests/indexes/datetimes/test_scalar_compat.py
@@ -336,3 +336,10 @@ class TestDatetimeIndexOps:
 
         dr = date_range("2017-01-01", periods=2, freq="10QS")
         assert all(dr.is_quarter_start)
+
+    def test_dti_is_year_start_freq_custom_business_day_with_digit(self):
+        # GH#58664
+        dr = date_range("2020-01-01", periods=2, freq="2C")
+        msg = "Custom business days is not supported by is_year_start"
+        with pytest.raises(ValueError, match=msg):
+            dr.is_year_start
