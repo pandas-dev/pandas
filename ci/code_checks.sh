4504b7c108b38d28ffc16deef1a13429bae12a20
abonte <6319051+abonte@users.noreply.github.com>
2024-05-11 20:15:53 +0200

    DOC: Enforce Numpy Docstring Validation Timestamp.week (#58674)
    * DOC: add SA01 to Timestamp.week
    
    * remove from code_checks
    
    * remove weekofyear alias from code_checks

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -320,9 +320,7 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Timestamp.utcoffset SA01" \
         -i "pandas.Timestamp.utctimetuple SA01" \
         -i "pandas.Timestamp.value GL08" \
-        -i "pandas.Timestamp.week SA01" \
         -i "pandas.Timestamp.weekday SA01" \
-        -i "pandas.Timestamp.weekofyear SA01" \
         -i "pandas.Timestamp.year GL08" \
         -i "pandas.api.extensions.ExtensionArray._from_sequence_of_strings SA01" \
         -i "pandas.api.extensions.ExtensionArray._hash_pandas_object RT03,SA01" \
