2e9a970123d70d621f2e07aa6bb30888c1773525
tuhinsharma121 <tuhinsharma121@gmail.com>
2024-05-13 19:30:04 +0530

    DOC: remove RT03,SA01 for pandas.api.extensions.ExtensionArray.copy

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -324,7 +324,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.api.extensions.ExtensionArray._reduce RT03,SA01" \
         -i "pandas.api.extensions.ExtensionArray._values_for_factorize SA01" \
         -i "pandas.api.extensions.ExtensionArray.astype SA01" \
-        -i "pandas.api.extensions.ExtensionArray.copy RT03,SA01" \
         -i "pandas.api.extensions.ExtensionArray.dropna RT03,SA01" \
         -i "pandas.api.extensions.ExtensionArray.dtype SA01" \
         -i "pandas.api.extensions.ExtensionArray.duplicated RT03,SA01" \
