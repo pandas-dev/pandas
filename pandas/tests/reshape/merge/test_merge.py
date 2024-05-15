5372107eccf978e34b14201fb780d8e67b46bee1
tirth choksi <tirthchoksi2204@gmail.com>
2024-05-15 16:05:32 +0530

    add np.uintc to _factorizers in merge.py

--- pandas/tests/reshape/merge/test_merge.py
+++ pandas/tests/reshape/merge/test_merge.py
@@ -1490,10 +1490,12 @@ class TestMergeDtypes:
         # categorical cols to object
         result = merge(left, right, on="A")
         assert is_object_dtype(result.A.dtype) or is_string_dtype(result.A.dtype)
-
+    @pytest.mark.parametrize(
+        "d1", [np.int64, np.int32, np.intc, np.int16, np.int8, np.uint8]
+    )
     @pytest.mark.parametrize("d2", [np.int64, np.float64, np.float32, np.float16])
-    def test_join_multi_dtypes(self, any_int_numpy_dtype, d2):
-        dtype1 = np.dtype(any_int_numpy_dtype)
+    def test_join_multi_dtypes(self, d1, d2):
+        dtype1 = np.dtype(d1)
         dtype2 = np.dtype(d2)
 
         left = DataFrame(
