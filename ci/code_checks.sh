2ca93cef44abd753f391225a8e2cdae24088a265
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-18 23:02:51 +0530

    DOC: add RT03,SA01 for pandas.Series.kurt and pandas.Series.kurtosis (#58761)
    * DOC: add RT03,SA01 for pandas.Series.kurt
    
    * DOC: remove RT03,SA01 for pandas.Series.kurt

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -167,8 +167,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Series.eq SA01" \
         -i "pandas.Series.ge SA01" \
         -i "pandas.Series.gt SA01" \
-        -i "pandas.Series.kurt RT03,SA01" \
-        -i "pandas.Series.kurtosis RT03,SA01" \
         -i "pandas.Series.list.__getitem__ SA01" \
         -i "pandas.Series.list.flatten SA01" \
         -i "pandas.Series.list.len SA01" \
