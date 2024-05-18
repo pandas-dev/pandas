c533a60160f15ebd048e05752a7ec45f5d6723a0
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-18 22:57:33 +0530

    DOC: add SA01 for pandas.Timestamp.isocalendar (#58763)
    * DOC: add SA01 for pandas.Timestamp.isocalendar
    
    * DOC: remove SA01 for pandas.Timestamp.isocalendar

--- pandas/_libs/tslibs/timestamps.pyx
+++ pandas/_libs/tslibs/timestamps.pyx
@@ -1691,6 +1691,12 @@ class Timestamp(_Timestamp):
         """
         Return a named tuple containing ISO year, week number, and weekday.
 
+        See Also
+        --------
+        DatetimeIndex.isocalendar : Return a 3-tuple containing ISO year,
+            week number, and weekday for the given DatetimeIndex object.
+        datetime.date.isocalendar : The equivalent method for `datetime.date` objects.
+
         Examples
         --------
         >>> ts = pd.Timestamp('2023-01-01 10:00:00')
