1c94ca531f59b11194c8c1f3af13f1a4c82d3d37
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-19 23:51:02 +0530

    DOC: add SA01 for pandas.Timestamp.weekday (#58781)

--- pandas/_libs/tslibs/nattype.pyx
+++ pandas/_libs/tslibs/nattype.pyx
@@ -419,6 +419,12 @@ class NaTType(_NaT):
 
         Monday == 0 ... Sunday == 6.
 
+        See Also
+        --------
+        Timestamp.dayofweek : Return the day of the week with Monday=0, Sunday=6.
+        Timestamp.isoweekday : Return the day of the week with Monday=1, Sunday=7.
+        datetime.date.weekday : Equivalent method in datetime module.
+
         Examples
         --------
         >>> ts = pd.Timestamp('2023-01-01')
