a7ea77e918fbdc2fff03a344bb8a8e0d2922cfc3
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-05-19 23:48:36 +0530

    DOC: add RT03,SA01 for pandas.api.extensions.ExtensionArray._hash_pandas_object (#58784)

--- pandas/core/arrays/base.py
+++ pandas/core/arrays/base.py
@@ -2033,6 +2033,13 @@ class ExtensionArray:
         Returns
         -------
         np.ndarray[uint64]
+            An array of hashed values.
+
+        See Also
+        --------
+        api.extensions.ExtensionArray._values_for_factorize : Return an array and
+            missing value suitable for factorization.
+        util.hash_array : Given a 1d array, return an array of hashed values.
 
         Examples
         --------
