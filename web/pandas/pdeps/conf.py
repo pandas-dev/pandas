25e97ec4d932a04a6727d73310cddb5408d35d1e
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-05-20 13:52:52 +0200

    force light theme + remove breadcrumbs

--- web/pandas/pdeps/conf.py
+++ web/pandas/pdeps/conf.py
@@ -45,6 +45,16 @@ exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
 #
 html_theme = "pydata_sphinx_theme"
 
+html_context = {
+    # always force light theme
+    "default_mode": "light"
+}
+
+html_theme_options = {
+    # remove the breadcrumbs
+    "article_header_start": [],
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
