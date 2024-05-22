e46d78bdbdc38df7ddc8c03e3c422e9148083d0b
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-05-22 10:30:52 -0700

    Whitespace

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -345,9 +345,11 @@ class BaseBlockManager(PandasObject):
     def arrays(self) -> list[ArrayLike]:
         """
         Quick access to the backing arrays of the Blocks.
+
         Only for compatibility with ArrayManager for testing convenience.
         Not to be used in actual code, and return value is not the same as the
         ArrayManager method (list of 1D arrays vs iterator of 2D ndarrays / 1D EAs).
+
         Warning! The returned arrays don't handle Copy-on-Write, so this should
         be used with caution (only in read-mode).
         """
