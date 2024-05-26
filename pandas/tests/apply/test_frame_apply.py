085ae73f68a4c5e4597f542a70bd132af39103f1
auderson <auderson@qq.com>
2024-05-26 10:31:44 +0800

    add nopython test in `test_apply_args`

--- pandas/tests/apply/test_frame_apply.py
+++ pandas/tests/apply/test_frame_apply.py
@@ -63,16 +63,28 @@ def test_apply(float_frame, engine, request):
 
 @pytest.mark.parametrize("axis", [0, 1])
 @pytest.mark.parametrize("raw", [True, False])
-def test_apply_args(float_frame, axis, raw, engine):
+@pytest.mark.parametrize("nopython", [True, False])
+def test_apply_args(float_frame, axis, raw, engine, nopython):
+    engine_kwargs = {"nopython": nopython}
     result = float_frame.apply(
-        lambda x, y: x + y, axis, args=(1,), raw=raw, engine=engine
+        lambda x, y: x + y,
+        axis,
+        args=(1,),
+        raw=raw,
+        engine=engine,
+        engine_kwargs=engine_kwargs,
     )
     expected = float_frame + 1
     tm.assert_frame_equal(result, expected)
 
     # GH:58712
     result = float_frame.apply(
-        lambda x, a, b: x + a + b, args=(1,), b=2, engine=engine, raw=raw
+        lambda x, a, b: x + a + b,
+        args=(1,),
+        b=2,
+        raw=raw,
+        engine=engine,
+        engine_kwargs=engine_kwargs,
     )
     expected = float_frame + 3
     tm.assert_frame_equal(result, expected)
@@ -81,18 +93,28 @@ def test_apply_args(float_frame, axis, raw, engine):
         # keyword-only arguments are not supported in numba
         with pytest.raises(
             pd.errors.NumbaUtilError,
-            match="numba does not support kwargs with nopython=True",
+            match="numba does not support keyword-only arguments",
         ):
             float_frame.apply(
-                lambda x, a, *, b: x + a + b, args=(1,), b=2, engine=engine, raw=raw
+                lambda x, a, *, b: x + a + b,
+                args=(1,),
+                b=2,
+                raw=raw,
+                engine=engine,
+                engine_kwargs=engine_kwargs,
             )
 
         with pytest.raises(
             pd.errors.NumbaUtilError,
-            match="numba does not support kwargs with nopython=True",
+            match="numba does not support keyword-only arguments",
         ):
             float_frame.apply(
-                lambda *x, b: x[0] + x[1] + b, args=(1,), b=2, engine=engine, raw=raw
+                lambda *x, b: x[0] + x[1] + b,
+                args=(1,),
+                b=2,
+                raw=raw,
+                engine=engine,
+                engine_kwargs=engine_kwargs,
             )
 
 
