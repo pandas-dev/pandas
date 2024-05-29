c46d74cb98de401c78703e1fb7d3a1923ea00023
miguel <mpgarcia.br@gmail.com>
2024-05-28 22:11:18 -0300

    removing unused comment

--- pandas/core/arrays/string_.py
+++ pandas/core/arrays/string_.py
@@ -188,7 +188,7 @@ class StringDtype(StorageExtensionDtype):
     # https://github.com/pandas-dev/pandas/issues/36126
     # error: Signature of "construct_array_type" incompatible with supertype
     # "ExtensionDtype"
-    def construct_array_type(  # type: ignore[override]
+    def construct_array_type(
         self,
     ) -> type_t[BaseStringArray]:
         """
