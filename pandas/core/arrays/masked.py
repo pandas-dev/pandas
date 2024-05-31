5d7ad8bec99b2143f52aa43792e9564b09a11da8
droussea2001 <19688507+droussea2001@users.noreply.github.com>
2024-05-31 17:58:57 +0200

    Remove unnecessary cast with to_numpy before map_array call

--- pandas/core/arrays/masked.py
+++ pandas/core/arrays/masked.py
@@ -1319,10 +1319,7 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
         return self._wrap_reduction_result("max", result, skipna=skipna, axis=axis)
 
     def map(self, mapper, na_action: Literal["ignore"] | None = None):
-        if self.dtype.na_value is libmissing.NA:
-            return map_array(self, mapper, na_action=na_action)
-        else:
-            return map_array(self.to_numpy(), mapper, na_action=na_action)
+        return map_array(self, mapper, na_action=na_action)
 
     @overload
     def any(
