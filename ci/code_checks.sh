897f01e7195f3e73237579bd97260c8a62b17c97
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-06-01 21:56:33 +0530

    DOC: fix SA01 for pandas.timedelta_range (#58879)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -494,7 +494,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.testing.assert_extension_array_equal SA01" \
         -i "pandas.testing.assert_index_equal PR07,SA01" \
         -i "pandas.testing.assert_series_equal PR07,SA01" \
-        -i "pandas.timedelta_range SA01" \
         -i "pandas.tseries.offsets.BDay PR02,SA01" \
         -i "pandas.tseries.offsets.BQuarterBegin PR02" \
         -i "pandas.tseries.offsets.BQuarterBegin.freqstr SA01" \
