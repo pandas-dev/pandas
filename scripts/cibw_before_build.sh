5f54ed7c3bd032bcb591f66c9aa12dfccb9601dc
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-07-10 14:22:07 +0200

    Install build dependencies in before-build and pass --no-build-isolation

---
+++ scripts/cibw_before_build.sh
@@ -0,0 +1,9 @@
+# TODO: delete along with enabling build isolation by unsetting
+# CIBW_BUILD_FRONTEND when scipy is buildable under free-threaded
+# python with a released version of cython
+FREE_THREADED_BUILD="$(python -c "import sysconfig; print(bool(sysconfig.get_config_var('Py_GIL_DISABLED')))")"
+if [[ $FREE_THREADED_BUILD == "True" ]]; then
+    python -m pip install -U pip
+    python -m pip install -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple numpy cython
+    python -m pip install ninja meson-python
+fi
