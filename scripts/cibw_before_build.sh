2258d64e1fdfc513099c5712a23dc6c630849e56
Lysandros Nikolaou <lisandrosnik@gmail.com>
2024-07-11 14:51:58 +0200

    Better comments plus always run before-build/before-test on 3.13

--- scripts/cibw_before_build.sh
+++ scripts/cibw_before_build.sh
@@ -1,8 +1,8 @@
-# TODO: delete along with enabling build isolation by unsetting
-# CIBW_BUILD_FRONTEND when scipy is buildable under free-threaded
-# python with a released version of cython
-FREE_THREADED_BUILD="$(python -c "import sysconfig; print(bool(sysconfig.get_config_var('Py_GIL_DISABLED')))")"
-if [[ $FREE_THREADED_BUILD == "True" ]]; then
+# TODO: Delete when there's PyPI NumPy/Cython releases the support Python 3.13.
+# If free-threading support is not included in those releases, this script will have
+# to whether this runs for a free-threaded build instead.
+PYTHON_VERSION="$(python -c "import sys; print(f'{sys.version_info.major}{sys.version_info.minor}')")"
+if [[ $PYTHON_VERSION == "313" ]]; then
     python -m pip install -U pip
     python -m pip install -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple numpy cython
     python -m pip install ninja meson-python versioneer[toml]
