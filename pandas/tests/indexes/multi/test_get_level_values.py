c9eb60a4a2ffbc7a93702c6de1df64094586f929
Annika <163510717+annika-rudolph@users.noreply.github.com>
2024-07-11 18:30:41 +0200

    BUG: Add frequency to DatetimeArray/TimedeltaArray take  (#58382)
    * add take function including frequency for Timedelta and Datetime Arrays
    
    * add test for frequency of DatetimeIndex in MultiIndex
    
    * use super() in take function
    
    * add description to whatsnew and revert unwanted changes in datetimearray docstring
    
    make pre-commit happy
    
    * switch .freq to ._freq
    
    ---------
    
    Co-authored-by: [Annika Rudolph] <[annika.rudolph@analytical-software.de]>

--- pandas/tests/indexes/multi/test_get_level_values.py
+++ pandas/tests/indexes/multi/test_get_level_values.py
@@ -122,3 +122,12 @@ def test_values_loses_freq_of_underlying_index():
     midx.values
     assert idx.freq is not None
     tm.assert_index_equal(idx, expected)
+
+
+def test_get_level_values_gets_frequency_correctly():
+    # GH#57949 GH#58327
+    datetime_index = date_range(start=pd.to_datetime("1/1/2018"), periods=4, freq="YS")
+    other_index = ["A"]
+    multi_index = MultiIndex.from_product([datetime_index, other_index])
+
+    assert multi_index.get_level_values(0).freq == datetime_index.freq
