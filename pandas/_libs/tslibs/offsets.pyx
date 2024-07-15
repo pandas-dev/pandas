ccd06b33ed0bdf65fb8c5f2a86a885a019135eac
James Bourbeau <jrbourbeau@users.noreply.github.com>
2024-07-15 17:28:35 -0500

    Remove extra space in ``resample`` freq deprecation (#59251)
    Remove extra space in resample freq deprecation

--- pandas/_libs/tslibs/offsets.pyx
+++ pandas/_libs/tslibs/offsets.pyx
@@ -4894,7 +4894,7 @@ cpdef to_offset(freq, bint is_period=False):
                         f"\'{name}\' is deprecated and will be removed "
                         f"in a future version, please use "
                         f"\'{c_PERIOD_AND_OFFSET_DEPR_FREQSTR.get(name)}\' "
-                        f" instead.",
+                        f"instead.",
                         FutureWarning,
                         stacklevel=find_stack_level(),
                         )
