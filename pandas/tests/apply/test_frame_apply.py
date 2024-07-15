c487cab0493b2f57d3521141a695a41da9cda5a3
Matias Lindgren <matias.lindgren@iki.fi>
2024-07-15 21:23:06 +0200

    test also with named axis

--- pandas/tests/apply/test_frame_apply.py
+++ pandas/tests/apply/test_frame_apply.py
@@ -1331,11 +1331,11 @@ def test_agg_reduce(axis, float_frame):
 
 
 def test_named_agg_reduce_axis1_raises(float_frame):
-    axis = 1
     name1, name2 = float_frame.axes[0].unique()[:2].sort_values()
-    msg = f"Named aggregation is not supported when {axis=}."
-    with pytest.raises(NotImplementedError, match=msg):
-        float_frame.agg(row1=(name1, "sum"), row2=(name2, "max"), axis=axis)
+    msg = "Named aggregation is not supported when axis=1."
+    for axis in [1, "columns"]:
+        with pytest.raises(NotImplementedError, match=msg):
+            float_frame.agg(row1=(name1, "sum"), row2=(name2, "max"), axis=axis)
 
 
 def test_nuiscance_columns():
