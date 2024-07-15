609ac4407d0af3131b3f84f69827008822bbdffb
Abhinav Reddy <abhinav071197@gmail.com>
2024-07-15 14:06:33 -0400

    DOC: Add SA01 for pandas.api.types.is_signed_integer_dtype (#59246)
    * adding See Also section for is_signed_integer_dtype
    
    * remove from code_check
    
    * Remove pandas and additional line break
    
    ---------
    
    Co-authored-by: Abhinav Thimma <athimma2@illinois.edu>

--- pandas/core/dtypes/common.py
+++ pandas/core/dtypes/common.py
@@ -715,6 +715,15 @@ def is_signed_integer_dtype(arr_or_dtype) -> bool:
         Whether or not the array or dtype is of a signed integer dtype
         and not an instance of timedelta64.
 
+    See Also
+    --------
+    api.types.is_integer_dtype: Check whether the provided array or dtype
+        is of an integer dtype.
+    api.types.is_numeric_dtype: Check whether the provided array or dtype
+        is of a numeric dtype.
+    api.types.is_unsigned_integer_dtype: Check whether the provided array
+        or dtype is of an unsigned integer dtype.
+
     Examples
     --------
     >>> from pandas.core.dtypes.common import is_signed_integer_dtype
