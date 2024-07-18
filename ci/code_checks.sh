b78f428c9673ac3bb2ec083749c6fa83ee71b956
ktseng4096 <32848825+ktseng4096@users.noreply.github.com>
2024-07-18 08:52:25 -0700

    DOC:  add See Also section to series.to_dict (#59269)
    add docstring to series.to_dict

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -195,7 +195,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Series.str.wrap RT03,SA01" \
         -i "pandas.Series.str.zfill RT03" \
         -i "pandas.Series.struct.dtypes SA01" \
-        -i "pandas.Series.to_dict SA01" \
         -i "pandas.Series.to_markdown SA01" \
         -i "pandas.Series.update PR07,SA01" \
         -i "pandas.Timedelta.asm8 SA01" \
