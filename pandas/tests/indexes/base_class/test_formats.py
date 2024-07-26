0e0814bdc40ea7e8d1c63b7591c33958c1ec018a
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-07-26 19:20:59 +0200

    REF (string dtype): rename using_pyarrow_string_dtype to using_string_dtype (#59320)

--- pandas/tests/indexes/base_class/test_formats.py
+++ pandas/tests/indexes/base_class/test_formats.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pytest
 
-from pandas._config import using_pyarrow_string_dtype
+from pandas._config import using_string_dtype
 import pandas._config.config as cf
 
 from pandas import Index
@@ -16,7 +16,7 @@ class TestIndexRendering:
         res = eval(repr(idx))
         tm.assert_index_equal(res, idx)
 
-    @pytest.mark.xfail(using_pyarrow_string_dtype(), reason="repr different")
+    @pytest.mark.xfail(using_string_dtype(), reason="repr different")
     @pytest.mark.parametrize(
         "index,expected",
         [
@@ -81,7 +81,7 @@ class TestIndexRendering:
         result = repr(index)
         assert result == expected
 
-    @pytest.mark.xfail(using_pyarrow_string_dtype(), reason="repr different")
+    @pytest.mark.xfail(using_string_dtype(), reason="repr different")
     @pytest.mark.parametrize(
         "index,expected",
         [
