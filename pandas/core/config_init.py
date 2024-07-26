57780490333db611d4a3242bf090bb44fe38c8ba
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-07-26 11:36:25 +0200

    TST / string dtype: add env variable to enable future_string and add test build (#58459)

--- pandas/core/config_init.py
+++ pandas/core/config_init.py
@@ -905,7 +905,7 @@ with cf.config_prefix("styler"):
 with cf.config_prefix("future"):
     cf.register_option(
         "infer_string",
-        False,
+        True if os.environ.get("PANDAS_FUTURE_INFER_STRING", "0") == "1" else False,
         "Whether to infer sequence of str objects as pyarrow string "
         "dtype, which will be the default in pandas 3.0 "
         "(at which point this option will be deprecated).",
