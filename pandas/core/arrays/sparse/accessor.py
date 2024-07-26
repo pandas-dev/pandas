2ea1793a9244b1cf1ee9d675b065ee56141fb5d3
kushagr96 <kushagrarora786@gmail.com>
2024-07-25 20:15:39 -0400

    Adding overrides for default values

--- pandas/core/arrays/sparse/accessor.py
+++ pandas/core/arrays/sparse/accessor.py
@@ -313,7 +313,18 @@ class SparseFrameAccessor(BaseAccessor, PandasDelegate):
         indices = data.indices
         indptr = data.indptr
         array_data = data.data
-        dtype = SparseDtype(array_data.dtype)
+
+        default_fill_value_override = None
+
+        # Default values for float type is NaN. Hence, in order to create a
+        # Sparse matrix of type float, we need override this default value as 0.0.
+        if array_data.dtype.kind in "f":
+            default_fill_value_override = 0.0
+        # Similarly, default value needs to be overridden for complex type arrays.
+        elif array_data.dtype.kind in "c":
+            default_fill_value_override = 0.0 + 0.0j
+
+        dtype = SparseDtype(array_data.dtype, fill_value=default_fill_value_override)
         arrays = []
         for i in range(n_columns):
             sl = slice(indptr[i], indptr[i + 1])
