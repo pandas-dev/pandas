8456ead03f87c3f10cb222851206db82d4b1fa10
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-07-31 22:37:37 +0530

    DOC: fix ES01,PR07 for pandas.MultiIndex.get_loc (#59364)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -71,7 +71,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i ES01 `# For now it is ok if docstrings are missing the extended summary` \
         -i "pandas.Series.dt PR01" `# Accessors are implemented as classes, but we do not document the Parameters section` \
         -i "pandas.MultiIndex.get_level_values SA01" \
-        -i "pandas.MultiIndex.get_loc PR07" \
         -i "pandas.MultiIndex.get_loc_level PR07" \
         -i "pandas.MultiIndex.levshape SA01" \
         -i "pandas.MultiIndex.names SA01" \
