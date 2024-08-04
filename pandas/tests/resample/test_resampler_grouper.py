5baf8eb8c6de2481822dfcd9cf021844abce6f9f
aram-cinnamon <aram.cinnamon.cinnabar@gmail.com>
2024-08-04 22:48:25 +0200

    Index should be ignored when using the on keyword argument to resample

--- pandas/tests/resample/test_resampler_grouper.py
+++ pandas/tests/resample/test_resampler_grouper.py
@@ -689,3 +689,36 @@ def test_groupby_resample_on_index_with_list_of_keys_missing_column():
     rs = gb.resample("2D")
     with pytest.raises(KeyError, match="Columns not found"):
         rs[["val_not_in_dataframe"]]
+
+
+def test_groupby_resample_when_index_is_out_of_order():
+    # GH 59350
+    df = DataFrame(
+        data={
+            "datetime": [
+                pd.to_datetime("2024-07-30T00:00Z"),
+                pd.to_datetime("2024-07-30T00:01Z"),
+            ],
+            "group": ["A", "A"],
+            "numbers": [100, 200],
+        },
+        index=[1, 0],
+    )
+
+    gb = df.groupby("group")
+    rs = gb.resample("1min", on="datetime")
+    result = rs.aggregate({"numbers": "sum"})
+
+    index = pd.MultiIndex.from_arrays(
+        [
+            ["A", "A"],
+            [pd.to_datetime("2024-07-30T00:00Z"), pd.to_datetime("2024-07-30T00:01Z")],
+        ],
+        names=[
+            "group",
+            "datetime",
+        ],
+    )
+    expected = DataFrame({"numbers": [100, 200]}, index=index)
+
+    tm.assert_frame_equal(result, expected)
