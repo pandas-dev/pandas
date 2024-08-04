5001e6467f064c1b199a799449b51d98c03a00d8
ivonastojanovic <80911834+ivonastojanovic@users.noreply.github.com>
2024-08-04 13:00:16 +0000

    DOCS: Add 'See Also'and return value description for pandas.api.interchange.from_dataframe

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -134,7 +134,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Timestamp.value GL08" \
         -i "pandas.Timestamp.year GL08" \
         -i "pandas.api.extensions.ExtensionArray.interpolate PR01,SA01" \
-        -i "pandas.api.interchange.from_dataframe RT03,SA01" \
         -i "pandas.api.types.is_bool PR01,SA01" \
         -i "pandas.api.types.is_categorical_dtype SA01" \
         -i "pandas.api.types.is_complex PR01,SA01" \
