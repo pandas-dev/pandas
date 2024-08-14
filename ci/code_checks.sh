6cb9f524ca0430c50089b449a0a0cbba9d257e33
UDIT <baliyanvdit@gmail.com>
2024-08-14 23:01:51 +0530

    Fix docstrings for pandas.Period.month

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -79,7 +79,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Period.asfreq SA01" \
         -i "pandas.Period.freq GL08" \
         -i "pandas.Period.freqstr SA01" \
-        -i "pandas.Period.month SA01" \
         -i "pandas.Period.ordinal GL08" \
         -i "pandas.Period.strftime PR01,SA01" \
         -i "pandas.Period.to_timestamp SA01" \
