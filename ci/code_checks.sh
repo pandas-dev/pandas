929e8c524740c0728765f286a83b70d05d0a80f6
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-08-30 23:13:58 +0530

    DOC: fix SA01,ES01 for pandas.Period.freqstr (#59661)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -73,7 +73,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.NA SA01" \
         -i "pandas.NaT SA01" \
         -i "pandas.Period.freq GL08" \
-        -i "pandas.Period.freqstr SA01" \
         -i "pandas.Period.ordinal GL08" \
         -i "pandas.Period.strftime PR01,SA01" \
         -i "pandas.Period.to_timestamp SA01" \
