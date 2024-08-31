b8beb63b4dbdf8b9c7e478a8af8396481cbf9bf3
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-08-31 21:03:35 +0530

    DOC: fix PR01,SA01,ES01 for pandas.errors.AbstractMethodError

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -224,7 +224,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.core.resample.Resampler.transform PR01,RT03,SA01" \
         -i "pandas.core.resample.Resampler.var SA01" \
         -i "pandas.date_range RT03" \
-        -i "pandas.errors.AbstractMethodError PR01,SA01" \
         -i "pandas.errors.AttributeConflictWarning SA01" \
         -i "pandas.errors.CSSWarning SA01" \
         -i "pandas.errors.CategoricalConversionWarning SA01" \
