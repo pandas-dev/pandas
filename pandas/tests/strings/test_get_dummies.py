163fe09b5b5871d030e4b4552338f8cac5e06c40
Aaron Chu-Carroll <aaron.chucarroll@gmail.com>
2024-09-05 00:07:26 -0400

    parametrize dtype tests

--- pandas/tests/strings/test_get_dummies.py
+++ pandas/tests/strings/test_get_dummies.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pytest
 
 from pandas import (
     DataFrame,
@@ -32,94 +33,14 @@ def test_get_dummies_index():
     tm.assert_index_equal(result, expected)
 
 
-def test_get_dummies_int8_dtype():
-    s = Series(["1|2", "1|3", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=np.int8)
+@pytest.mark.parametrize(
+    "dtype",
+    [np.uint8, np.int16, np.uint16, np.int32, np.uint32, np.int64, np.uint64, bool],
+)
+def test_get_dummies_with_dtype(any_string_dtype, dtype):
+    s = Series(["a|b", "a|c", np.nan], dtype=any_string_dtype)
+    result = s.str.get_dummies("|", dtype=dtype)
     expected = DataFrame(
-        [[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("123"), dtype=np.int8
+        [[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("abc"), dtype=dtype
     )
     tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == np.int8).all()
-
-
-def test_get_dummies_uint8_dtype():
-    s = Series(["a|b", "a|c", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=np.uint8)
-    expected = DataFrame(
-        [[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("abc"), dtype=np.uint8
-    )
-    tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == np.uint8).all()
-
-
-def test_get_dummies_int16_dtype():
-    s = Series(["a|b", "a|c", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=np.int16)
-    expected = DataFrame(
-        [[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("abc"), dtype=np.int16
-    )
-    tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == np.int16).all()
-
-
-def test_get_dummies_uint16_dtype():
-    s = Series(["a|b", "a|c", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=np.uint16)
-    expected = DataFrame(
-        [[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("abc"), dtype=np.uint16
-    )
-    tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == np.uint16).all()
-
-
-def test_get_dummies_int32_dtype():
-    s = Series(["x|y", "x|z", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=np.int32)
-    expected = DataFrame(
-        [[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("xyz"), dtype=np.int32
-    )
-    tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == np.int32).all()
-
-
-def test_get_dummies_uint32_dtype():
-    s = Series(["x|y", "x|z", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=np.uint32)
-    expected = DataFrame(
-        [[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("xyz"), dtype=np.uint32
-    )
-    tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == np.uint32).all()
-
-
-def test_get_dummies_int64_dtype():
-    s = Series(["foo|bar", "foo|baz", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=np.int64)
-    expected = DataFrame(
-        [[1, 0, 1], [0, 1, 1], [0, 0, 0]], columns=["bar", "baz", "foo"], dtype=np.int64
-    )
-    tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == np.int64).all()
-
-
-def test_get_dummies_uint64_dtype():
-    s = Series(["foo|bar", "foo|baz", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=np.uint64)
-    expected = DataFrame(
-        [[1, 0, 1], [0, 1, 1], [0, 0, 0]],
-        columns=["bar", "baz", "foo"],
-        dtype=np.uint64,
-    )
-    tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == np.uint64).all()
-
-
-def test_get_dummies_bool_dtype():
-    s = Series(["a|b", "a|c", np.nan], dtype="string")
-    result = s.str.get_dummies("|", dtype=bool)
-    expected = DataFrame(
-        [[True, True, False], [True, False, True], [False, False, False]],
-        columns=["a", "b", "c"],
-    )
-    tm.assert_frame_equal(result, expected)
-    assert (result.dtypes == bool).all()
