920c865c355d8a1bdcb2ac5990c8aac6267f0874
Aaron Chu-Carroll <aaron.chucarroll@gmail.com>
2024-09-05 11:49:29 -0400

    skip pyarrow tests when not present

--- pandas/tests/strings/test_get_dummies.py
+++ pandas/tests/strings/test_get_dummies.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pytest
 
+import pandas.util._test_decorators as td
+
 from pandas import (
     ArrowDtype,
     DataFrame,
@@ -50,10 +52,6 @@ def test_get_dummies_index():
         np.int64,
         np.uint64,
         bool,
-        ArrowDtype(pa.int8()),
-        ArrowDtype(pa.int16()),
-        ArrowDtype(pa.int32()),
-        ArrowDtype(pa.int64()),
         "Int8",
         "Int16",
         "Int32",
@@ -62,6 +60,30 @@ def test_get_dummies_index():
     ],
 )
 def test_get_dummies_with_dtype(any_string_dtype, dtype):
+    s = Series(["a|b", "a|c", np.nan], dtype=any_string_dtype)
+    result = s.str.get_dummies("|", dtype=dtype)
+    expected = DataFrame(
+        [[1, 1, 0], [1, 0, 1], [0, 0, 0]], columns=list("abc"), dtype=dtype
+    )
+    tm.assert_frame_equal(result, expected)
+
+
+@td.skip_if_no("pyarrow")
+@pytest.mark.parametrize(
+    "dtype",
+    [
+        ArrowDtype(pa.int8()),
+        ArrowDtype(pa.uint8()),
+        ArrowDtype(pa.int16()),
+        ArrowDtype(pa.uint16()),
+        ArrowDtype(pa.int32()),
+        ArrowDtype(pa.uint32()),
+        ArrowDtype(pa.int64()),
+        ArrowDtype(pa.uint64()),
+        ArrowDtype(pa.bool_()),
+    ],
+)
+def test_get_dummies_with_pyarrow_dtype(any_string_dtype, dtype):
     s = Series(["a|b", "a|c", np.nan], dtype=any_string_dtype)
     result = s.str.get_dummies("|", dtype=dtype)
     expected = DataFrame(
