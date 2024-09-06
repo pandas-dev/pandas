bf33163a39edf914ed482d02702ed3c23a24bc3f
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2024-09-06 14:13:49 +0100

    stringdtype test

--- pandas/tests/series/test_arrow_interface.py
+++ pandas/tests/series/test_arrow_interface.py
@@ -43,3 +43,19 @@ def test_series_arrow_interface_arrow_dtypes():
     ca = pa.chunked_array(s, type=pa.int32())
     expected = pa.chunked_array([[1, 4, 2]], type=pa.int32())
     assert ca.equals(expected)
+
+
+def test_series_arrow_interface_stringdtype():
+    s = pd.Series(["foo", "bar"], dtype="string[pyarrow]")
+
+    capsule = s.__arrow_c_stream__()
+    assert (
+        ctypes.pythonapi.PyCapsule_IsValid(
+            ctypes.py_object(capsule), b"arrow_array_stream"
+        )
+        == 1
+    )
+
+    ca = pa.chunked_array(s)
+    expected = pa.chunked_array([["foo", "bar"]], type=pa.large_string())
+    assert ca.equals(expected)
