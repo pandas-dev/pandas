ef83e91764fa4b92bd1f66bac1d707c7b4f71e3b
Abhinav Thimma <athimma2@illinois.edu>
2024-09-08 17:41:02 -0400

    Fix is_datetime64_ns_dtype

--- pandas/core/dtypes/common.py
+++ pandas/core/dtypes/common.py
@@ -990,6 +990,13 @@ def is_datetime64_ns_dtype(arr_or_dtype) -> bool:
     bool
         Whether or not the array or dtype is of the datetime64[ns] dtype.
 
+    See Also
+    --------
+    api.types.is_datetime64_dtype: Check whether an array-like or
+                                        dtype is of the datetime64 dtype.
+    api.types.is_datetime64_any_dtype: Check whether the provided array or
+                                        dtype is of the datetime64 dtype.
+
     Examples
     --------
     >>> from pandas.api.types import is_datetime64_ns_dtype
