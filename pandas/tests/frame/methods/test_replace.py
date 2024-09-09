pandas-dev/pandas

commit b2d54d9c16990bd8eaeacd4de24fc33cfdbfb43b

BUG: IntegerArray/FloatingArray ufunc with 'out' kwd (#45122)

pandas/core/missing.py
@@ -92,14 +92,15 @@ def mask_missing(arr: ArrayLike, values_to_mask) -> npt.NDArray[np.bool_]:
             # GH#29553 prevent numpy deprecation warnings
             pass
         else:
-            mask |= arr == x
+            new_mask = arr == x
+            if not isinstance(new_mask, np.ndarray):
+                # usually BooleanArray
+                new_mask = new_mask.to_numpy(dtype=bool, na_value=False)
+            mask |= new_mask
 
     if na_mask.any():
         mask |= isna(arr)
 
-    if not isinstance(mask, np.ndarray):
-        # e.g. if arr is IntegerArray, then mask is BooleanArray
-        mask = mask.to_numpy(dtype=bool, na_value=False)
     return mask
 
 

pandas/core/arrays/masked.py
@@ -433,6 +433,12 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
         if result is not NotImplemented:
             return result
 
+        if "out" in kwargs:
+            # e.g. test_ufunc_with_out
+            return arraylike.dispatch_ufunc_with_out(
+                self, ufunc, method, *inputs, **kwargs
+            )
+
         if method == "reduce":
             result = arraylike.dispatch_reduction_ufunc(
                 self, ufunc, method, *inputs, **kwargs

pandas/tests/arrays/masked_shared.py
@@ -1,6 +1,8 @@
 """
 Tests shared by MaskedArray subclasses.
 """
+import numpy as np
+import pytest
 
 import pandas as pd
 import pandas._testing as tm
@@ -102,3 +104,35 @@ class NumericOps:
         expected = pd.Series([False, pd.NA], dtype="boolean")
 
         self.assert_series_equal(result, expected)
+
+    def test_ufunc_with_out(self, dtype):
+        arr = pd.array([1, 2, 3], dtype=dtype)
+        arr2 = pd.array([1, 2, pd.NA], dtype=dtype)
+
+        mask = arr == arr
+        mask2 = arr2 == arr2
+
+        result = np.zeros(3, dtype=bool)
+        result |= mask
+        # If MaskedArray.__array_ufunc__ handled "out" appropriately,
+        #  `result` should still be an ndarray.
+        assert isinstance(result, np.ndarray)
+        assert result.all()
+
+        # result |= mask worked because mask could be cast lossslessly to
+        #  boolean ndarray. mask2 can't, so this raises
+        result = np.zeros(3, dtype=bool)
+        msg = "Specify an appropriate 'na_value' for this dtype"
+        with pytest.raises(ValueError, match=msg):
+            result |= mask2
+
+        # addition
+        res = np.add(arr, arr2)
+        expected = pd.array([2, 4, pd.NA], dtype=dtype)
+        tm.assert_extension_array_equal(res, expected)
+
+        # when passing out=arr, we will modify 'arr' inplace.
+        res = np.add(arr, arr2, out=arr)
+        assert res is arr
+        tm.assert_extension_array_equal(res, expected)
+        tm.assert_extension_array_equal(arr, expected)

commit 9ebc1a413b34f557345dd0718bbac72333862ba9

Update base branch (#1)
* Fix issue #47101

Bisecting two years ago ( https://github.com/pandas-dev/pandas/issues/47101#issuecomment-1139606268 ) shows this regression was introduced in b2d54d9 in 2021. Somehow this hasn't been patched since then.

PR #48313 was supposed to address this, but the PR was closed and never merged and the bug has persisted.

* Add a test as per PR guidelines

* Fix typo

* Resolve inconsistent namespace as per PR test

https://results.pre-commit.ci/run/github/858127/1723498369.6V12SWx7T-WpLZDAXXkz0Q

This web UI commit will still fail, as the E501 line-too-long check will fail until the next commit

* Resolve E501 linting errors

https://results.pre-commit.ci/run/github/858127/1723498369.6V12SWx7T-WpLZDAXXkz0Q

* Fix test TypeErrors

np.equal([1,2,3], "") fails

* Quote style for Ruff

* typing and remove code backtick possibly incorrectly triggering ruff formatter

* mpy supression for caught error

* trailing space

pandas/core/missing.py
@@ -124,11 +124,25 @@ def mask_missing(arr: ArrayLike, values_to_mask) -> npt.NDArray[np.bool_]:
                     new_mask = np.zeros(arr.shape, dtype=np.bool_)
                     new_mask[arr_mask] = arr[arr_mask] == x
                 else:
-                    new_mask = arr == x
+                    # GH#47101
+                    # Fix where type bool has no attribute to_numpy() by first
+                    # attempting to broadcast with np.equal for some cases, and then
+                    # an explicit type check when checking the mask for any straggling
+                    # cases. Where a literal comparison would fail np.equal we fall back
+                    # to the original equality check.
+                    try:
+                        # In case of an uncastable type, this will emit TypeError
+                        new_mask = np.equal(arr, x) # type: ignore[arg-type]
+                    except TypeError:
+                        # Old behaviour for uncastable types
+                        new_mask = arr == x
 
                     if not isinstance(new_mask, np.ndarray):
                         # usually BooleanArray
-                        new_mask = new_mask.to_numpy(dtype=bool, na_value=False)
+                        if isinstance(new_mask, bool):
+                            new_mask = np.array([new_mask], dtype= bool)
+                        else:
+                            new_mask = new_mask.to_numpy(dtype=bool, na_value=False)
                 mask |= new_mask
 
     if na_mask.any():

pandas/tests/frame/methods/test_replace.py
@@ -1518,3 +1518,10 @@ class TestDataFrameReplaceRegex:
             assert len(df._mgr.blocks) == 2
         else:
             assert len(df._mgr.blocks) == 1
+
+    def test_replace_bool_to_numpy_attributeerror(self):
+        # GH#47101
+        pass_pre_patch = DataFrame({"d":[None]})
+        tm.assert_frame_equal(pass_pre_patch, pass_pre_patch.replace("", pd.NA))
+        fail_pre_patch = DataFrame({"d":[pd.NA]})
+        tm.assert_frame_equal(fail_pre_patch, fail_pre_patch.replace("", pd.NA))