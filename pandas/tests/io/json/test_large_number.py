410ee0ebfbb12afca110e378b9ab05040dd2fb91
Fawaz Ahmed <fawazahmed0@hotmail.com>
2024-09-19 03:32:35 +0530

    Revert "add test"
    This reverts commit ed7e5f98f7e69267fc543b6638dff4b57ea431d2.

--- pandas/tests/io/json/test_large_number.py
+++
@@ -1,6 +0,0 @@
-from io import StringIO
-import pytest
-import pandas as pd
-
-def test_large_number():
-    assert pd.read_json(StringIO('["9999999999999999"]'),orient="values",typ="series",convert_dates=False)[0] == 9999999999999999
