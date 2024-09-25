e61edc96cb9e369cb6a0059d5949039bec2044da
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-09-25 23:24:42 +0530

    DOC: fix SA01, ES01 for pandas.tseries.offsets.SemiMonthEnd (#59856)
    DOC: fix SA01 for pandas.tseries.offsets.SemiMonthEnd

--- pandas/_libs/tslibs/offsets.pyx
+++ pandas/_libs/tslibs/offsets.pyx
@@ -3316,6 +3316,11 @@ cdef class SemiMonthEnd(SemiMonthOffset):
     """
     Two DateOffset's per month repeating on the last day of the month & day_of_month.
 
+    This offset allows for flexibility in generating date ranges or adjusting dates
+    to the end of a month or a specific day in the month, such as the 15th or the last
+    day of the month. It is useful for financial or scheduling applications where
+    events occur bi-monthly.
+
     Attributes
     ----------
     n : int, default 1
@@ -3324,6 +3329,13 @@ cdef class SemiMonthEnd(SemiMonthOffset):
         Normalize start/end dates to midnight before generating date range.
     day_of_month : int, {1, 3,...,27}, default 15
         A specific integer for the day of the month.
+
+    See Also
+    --------
+    tseries.offsets.SemiMonthBegin : Offset for semi-monthly frequencies, starting at
+        the beginning of the month.
+    tseries.offsets.MonthEnd : Offset to the last calendar day of the month.
+    tseries.offsets.MonthBegin : Offset to the first calendar day of the month.
 
     Examples
     --------
