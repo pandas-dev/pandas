e1cf343c54719d7f2984570474b85f634d8118f4
saldanhad <deepak.saldanha007@gmail.com>
2024-10-01 23:11:54 +0530

    Revert "fix linting"
    This reverts commit 1061442e0a1cf8f745b0863762f2aa023d388336.

--- pandas/tests/io/test_info_repr_html.py
+++ pandas/tests/io/test_info_repr_html.py
@@ -1,35 +1,32 @@
 import pytest
-
 import pandas as pd
 
 
 class Testfloatformat:
-    @pytest.mark.parametrize(
-        "data, format_option, expected_values",
-        [
-            ({"A": [12345.6789]}, "{:12.3f}", "&nbsp;&nbsp;&nbsp;12345.679"),
-            ({"A": [None]}, "{:.3f}", "&nbsp;None"),
-            ({"A": [""]}, "{:.2f}", "&nbsp;"),
-            ({"A": [112345.6789]}, "{:6.3f}", "112345.679"),
-            ({"A": ["foo    foo"]}, None, "&nbsp;foo&nbsp;&nbsp;&nbsp;&nbsp;foo"),
-            ({"A": [None]}, None, "&nbsp;None"),
-            ({"A": ["foo foo foo"]}, None, "&nbsp;foo&nbsp;foo&nbsp;foo"),
-        ],
-    )  # test cases
-    def test_float_formatting_html_output(self, data, format_option, expected_values):
-        # set float format, avoid for string checks
-        if format_option is not None:
-            pd.set_option("display.float_format", format_option.format)
+    @pytest.mark.parametrize("data, format_option, expected_values", [
+        ({"A": [12345.6789]}, "{:12.3f}", "&nbsp;&nbsp;&nbsp;12345.679"),
+        ({"A": [None]}, "{:.3f}", "&nbsp;None"),
+        ({"A": [""]}, "{:.2f}", "&nbsp;"),
+        ({"A": [112345.6789]}, "{:6.3f}", "112345.679"),
+        ({"A": ["foo    foo"]}, None, "&nbsp;foo&nbsp;&nbsp;&nbsp;&nbsp;foo"),
+        ({"A": [None]}, None, "&nbsp;None"),
+        ({"A": ["foo foo foo"]}, None, "&nbsp;foo&nbsp;foo&nbsp;foo"),
+    ]) #test cases
 
-        # create dataframe
-        df = pd.DataFrame(data)
+    def test_float_formatting_html_output(self,data,format_option, expected_values):
+            # set float format, avoid for string checks
+            if format_option is not None:
+                 pd.set_option("display.float_format", format_option.format)
+            
+            # crate dataframe
+            df = pd.DataFrame(data)
+                              
+            # capture html output
+            html_output = df._repr_html_()
 
-        # capture html output
-        html_output = df._repr_html_()
+            # check
+            assert expected_values in html_output
 
-        # check
-        assert expected_values in html_output
-
-        # reset option
-        if format_option is not None:
-            pd.reset_option("display.float_format")
+            # reset option
+            if format_option is not None:
+                 pd.reset_option("display.float_format")
