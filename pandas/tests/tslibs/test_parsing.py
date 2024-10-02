679100a4b081f90ddb58044897bc33a40d63021d
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-10-02 22:07:58 +0200

    skip test on arm

--- pandas/tests/tslibs/test_parsing.py
+++ pandas/tests/tslibs/test_parsing.py
@@ -17,6 +17,7 @@ from pandas._libs.tslibs import (
 from pandas._libs.tslibs.parsing import parse_datetime_string_with_reso
 from pandas.compat import (
     ISMUSL,
+    is_platform_arm,
     is_platform_windows,
 )
 import pandas.util._test_decorators as td
@@ -26,7 +27,7 @@ from pandas._testing._hypothesis import DATETIME_NO_TZ
 
 
 @pytest.mark.skipif(
-    is_platform_windows() or ISMUSL,
+    is_platform_windows() or ISMUSL or is_platform_arm(),
     reason="TZ setting incorrect on Windows and MUSL Linux",
 )
 def test_parsing_tzlocal_deprecated():
@@ -34,7 +35,6 @@ def test_parsing_tzlocal_deprecated():
     msg = (
         "Parsing 'EST' as tzlocal.*"
         "Pass the 'tz' keyword or call tz_localize after construction instead"
-        "|Parsed string"
     )
     dtstr = "Jan 15 2004 03:00 EST"
 
