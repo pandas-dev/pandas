029c1802df330f1a0e47a5c76af6c511364130a9
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-11-04 08:45:51 +0100

    fix index dtype in value_counts result

--- pandas/core/algorithms.py
+++ pandas/core/algorithms.py
@@ -931,7 +931,7 @@ def value_counts_internal(
             # For backwards compatibility, we let Index do its normal type
             #  inference, _except_ for if if infers from object to bool.
             idx = Index(keys)
-            if idx.dtype == bool and keys.dtype == object:
+            if idx.dtype in [bool, "string"] and keys.dtype == object:
                 idx = idx.astype(object)
             elif (
                 idx.dtype != keys.dtype  # noqa: PLR1714  # # pylint: disable=R1714
