7740c4e2c33ba855d36db93d3028164d08e8263c
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-11-09 01:19:12 +0530

    DOC: fix PR07,SA01,ES01 for pandas.arrays.SparseArray (#60250)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -91,7 +91,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.arrays.IntegerArray SA01" \
         -i "pandas.arrays.IntervalArray.length SA01" \
         -i "pandas.arrays.NumpyExtensionArray SA01" \
-        -i "pandas.arrays.SparseArray PR07,SA01" \
         -i "pandas.arrays.TimedeltaArray PR07,SA01" \
         -i "pandas.core.groupby.DataFrameGroupBy.boxplot PR07,RT03,SA01" \
         -i "pandas.core.groupby.DataFrameGroupBy.get_group RT03,SA01" \
