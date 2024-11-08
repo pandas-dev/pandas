feb183c92c4fdb617b815c7fdcd4723d616462ed
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-11-08 14:07:20 +0100

    reformat

--- pandas/tests/frame/test_arrow_interface.py
+++ pandas/tests/frame/test_arrow_interface.py
@@ -22,15 +22,8 @@ def test_dataframe_arrow_interface(using_infer_string):
     )
 
     table = pa.table(df)
-    expected = pa.table(
-        {
-            "a": [1, 2, 3],
-            "b": pa.array(
-                ["a", "b", "c"],
-                pa.large_string() if using_infer_string else pa.string(),
-            ),
-        }
-    )
+    string_type = pa.large_string() if using_infer_string else pa.string()
+    expected = pa.table({"a": [1, 2, 3], "b": pa.array(["a", "b", "c"], string_type)})
     assert table.equals(expected)
 
     schema = pa.schema([("a", pa.int8()), ("b", pa.string())])
@@ -44,15 +37,8 @@ def test_dataframe_to_arrow(using_infer_string):
     df = pd.DataFrame({"a": [1, 2, 3], "b": ["a", "b", "c"]})
 
     table = pa.RecordBatchReader.from_stream(df).read_all()
-    expected = pa.table(
-        {
-            "a": [1, 2, 3],
-            "b": pa.array(
-                ["a", "b", "c"],
-                pa.large_string() if using_infer_string else pa.string(),
-            ),
-        }
-    )
+    string_type = pa.large_string() if using_infer_string else pa.string()
+    expected = pa.table({"a": [1, 2, 3], "b": pa.array(["a", "b", "c"], string_type)})
     assert table.equals(expected)
 
     schema = pa.schema([("a", pa.int8()), ("b", pa.string())])
