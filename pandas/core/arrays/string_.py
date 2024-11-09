9dba8f26b34d304d8ce212166466968503dfc678
Espoir Murhabazi <EspoirMur@users.noreply.github.com>
2024-11-09 18:27:32 +0000

    fix stuff

--- pandas/core/arrays/string_.py
+++ pandas/core/arrays/string_.py
@@ -727,7 +727,6 @@ class StringArray(BaseStringArray, NumpyExtensionArray):  # type: ignore[misc]
         values[self.isna()] = None
         return pa.array(values, type=type, from_pandas=True)
 
-    
     def _values_for_factorize(self) -> tuple[np.ndarray, libmissing.NAType | float]:  # type: ignore[override]
         arr = self._ndarray
 
