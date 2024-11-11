e9ae3770a6aa73e96042d4b76836097c2eecbd6d
lfffkh <1906921213@qq.com>
2024-11-11 22:28:44 +0800

    add test

--- pandas/tests/dtypes/cast/test_construct_ndarray.py
+++ pandas/tests/dtypes/cast/test_construct_ndarray.py
@@ -14,6 +14,31 @@ from pandas.core.construction import sanitize_array
         (["1", "2", None], None, np.array(["1", "2", None])),
         (["1", "2", None], np.dtype("str"), np.array(["1", "2", None])),
         ([1, 2, None], np.dtype("str"), np.array(["1", "2", None])),
+        (
+            np.ma.masked_array([1, 2, 3], mask=[False, True, False]),
+            "int64",
+            np.array([1, 2, 3], dtype=np.int64),
+        ),
+        (
+            np.ma.masked_array([1, 2, 3], mask=[False, True, False]),
+            "float64",
+            np.array([1, 2, 3], dtype=np.float64),
+        ),
+        (
+            np.ma.masked_array([1, 2, 3], mask=[False, True, False]),
+            "UInt64",
+            np.array([1, 2, 3], dtype=np.uint64),
+        ),
+        (
+            np.ma.masked_array([1.0, 2.0, 3.0], mask=[False, True, False]),
+            "float64",
+            np.array([1.0, 2.0, 3.0], dtype=np.float64),
+        ),
+        (
+            np.ma.masked_array([1.0, 2.0, 3.0], mask=[False, True, False]),
+            "Int64",
+            np.array([1, 2, 3], dtype=np.int64),
+        ),
     ],
 )
 def test_construct_1d_ndarray_preserving_na(
@@ -33,4 +58,9 @@ def test_construct_1d_ndarray_preserving_na_datetimelike(dtype):
     assert all(isinstance(x, type(arr[0])) for x in expected)
 
     result = sanitize_array(arr, index=None, dtype=np.dtype(object))
+    tm.assert_numpy_array_equal(result, expected)
+
+
+def test_sanitize_masked_array_with_masked_ea(values, dtype, expected):
+    result = sanitize_array(values, dtype=dtype)
     tm.assert_numpy_array_equal(result, expected)
