87ccf69ea469ee2fd33e20ab49e24b67db876a01
Thomas Dixon <teddixon@umich.edu>
2024-11-11 11:05:27 -0500

    Add regression tests for bug gh54763

--- pandas/tests/indexes/datetimes/methods/test_to_julian_date.py
+++ pandas/tests/indexes/datetimes/methods/test_to_julian_date.py
@@ -1,11 +1,14 @@
 import numpy as np
+from zoneinfo import ZoneInfo
 
 from pandas import (
     Index,
     Timestamp,
     date_range,
+    to_datetime,
 )
 import pandas._testing as tm
+import pytest
 
 
 class TestDateTimeIndexToJulianDate:
@@ -43,3 +46,49 @@ class TestDateTimeIndexToJulianDate:
         r2 = dr.to_julian_date()
         assert isinstance(r2, Index) and r2.dtype == np.float64
         tm.assert_index_equal(r1, r2)
+
+    @pytest.mark.parametrize("tz, expected", [
+        (None, 2400000.5),
+        (ZoneInfo("UTC"), 2400000.5),
+        (ZoneInfo("US/Pacific"), 2400000.5 + (8 / 24)),
+        (ZoneInfo("Europe/London"), 2400000.5 - (1 / 24))
+    ])
+    def test_to_julian_date_with_timezones_single_element(self, tz, expected):
+        # GH54763: Timestamp.to_julian_date() must consider timezone
+        dates = to_datetime(['1858-11-17T00:00:00.0'])
+        if tz:
+            dates = dates.tz_localize(tz)
+        result = Index(dates.to_julian_date())
+        expected = Index([expected])
+        tm.assert_almost_equal(result, expected, rtol=1e-6, atol=1e-6)
+
+    @pytest.mark.parametrize("tz, offset", [
+        (None, 0),
+        (ZoneInfo("UTC"), 0),
+        (ZoneInfo("US/Pacific"), 8),
+        (ZoneInfo("Europe/London"), -1)
+    ])
+    def test_to_julian_date_with_timezones_multiple_elements(self, tz, offset):
+        # GH54763: Timestamp.to_julian_date() must consider timezone
+        dates = to_datetime(
+            [
+                '1858-11-17T00:00:00',
+                '1858-11-17T12:00:00',
+                '2000-01-01T00:00:00',
+                '2000-01-01T12:00:00',
+                '2000-01-01T12:00:00'
+            ]
+        )
+        if tz:
+            dates = dates.tz_localize(tz)
+        result = Index(dates.to_julian_date())
+        expected = Index(
+            [
+                2400000.5 + (offset / 24),
+                2400001.0 + (offset / 24),
+                2451544.5 + (offset / 24),
+                2451545.0 + (offset / 24),
+                2451545.0 + (offset / 24)
+            ]
+        )
+        tm.assert_almost_equal(result, expected, rtol=1e-6, atol=1e-6)
--- pandas/tests/scalar/timestamp/methods/test_to_julian_date.py
+++ pandas/tests/scalar/timestamp/methods/test_to_julian_date.py
@@ -1,5 +1,10 @@
 from pandas import Timestamp
 
+import zoneinfo
+
+import pandas._testing as tm
+import pytest
+
 
 class TestTimestampToJulianDate:
     def test_compare_1700(self):
@@ -26,3 +31,17 @@ class TestTimestampToJulianDate:
         ts = Timestamp("2000-08-12T13:00:00")
         res = ts.to_julian_date()
         assert res == 2_451_769.0416666666666666
+
+    @pytest.mark.parametrize("tz, expected", [
+        (None, 2400000.5),
+        (zoneinfo.ZoneInfo("UTC"), 2400000.5),
+        (zoneinfo.ZoneInfo("US/Pacific"), 2400000.5 + (8 / 24)),
+        (zoneinfo.ZoneInfo("Europe/London"), 2400000.5 - (1 / 24))
+    ])
+    def test_to_julian_date_with_timezones(self, tz, expected):
+        # GH54763: Timestamp.to_julian_date() must consider timezone
+        ts = Timestamp('1858-11-17T00:00:00.0')
+        if tz:
+            ts.tz_localize(tz)
+        result = ts.to_julian_date()
+        tm.assert_almost_equal(result, expected, rtol=1e-6, atol=1e-6)
