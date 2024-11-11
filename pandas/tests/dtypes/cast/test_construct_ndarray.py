8838773801e898a02d0b359d84116c4c16d18128
lfffkh <1906921213@qq.com>
2024-11-11 22:48:10 +0800

    add test2

--- pandas/tests/dtypes/cast/test_construct_ndarray.py
+++ pandas/tests/dtypes/cast/test_construct_ndarray.py
@@ -14,6 +14,31 @@ from pandas.core.construction import sanitize_array
         (["1", "2", None], None, np.array(["1", "2", None])),
         (["1", "2", None], np.dtype("str"), np.array(["1", "2", None])),
         ([1, 2, None], np.dtype("str"), np.array(["1", "2", None])),
+    ],
+)
+def test_construct_1d_ndarray_preserving_na(
+    values, dtype, expected, using_infer_string
+):
+    result = sanitize_array(values, index=None, dtype=dtype)
+    if using_infer_string and expected.dtype == object and dtype is None:
+        tm.assert_extension_array_equal(result, pd.array(expected, dtype="str"))
+    else:
+        tm.assert_numpy_array_equal(result, expected)
+
+
+@pytest.mark.parametrize("dtype", ["m8[ns]", "M8[ns]"])
+def test_construct_1d_ndarray_preserving_na_datetimelike(dtype):
+    arr = np.arange(5, dtype=np.int64).view(dtype)
+    expected = np.array(list(arr), dtype=object)
+    assert all(isinstance(x, type(arr[0])) for x in expected)
+
+    result = sanitize_array(arr, index=None, dtype=np.dtype(object))
+    tm.assert_numpy_array_equal(result, expected)
+
+
+@pytest.mark.parametrize(
+    "values, dtype, expected",
+    [
         (
             np.ma.masked_array([1, 2, 3], mask=[False, True, False]),
             "int64",
@@ -41,26 +66,6 @@ from pandas.core.construction import sanitize_array
         ),
     ],
 )
-def test_construct_1d_ndarray_preserving_na(
-    values, dtype, expected, using_infer_string
-):
-    result = sanitize_array(values, index=None, dtype=dtype)
-    if using_infer_string and expected.dtype == object and dtype is None:
-        tm.assert_extension_array_equal(result, pd.array(expected, dtype="str"))
-    else:
-        tm.assert_numpy_array_equal(result, expected)
-
-
-@pytest.mark.parametrize("dtype", ["m8[ns]", "M8[ns]"])
-def test_construct_1d_ndarray_preserving_na_datetimelike(dtype):
-    arr = np.arange(5, dtype=np.int64).view(dtype)
-    expected = np.array(list(arr), dtype=object)
-    assert all(isinstance(x, type(arr[0])) for x in expected)
-
-    result = sanitize_array(arr, index=None, dtype=np.dtype(object))
-    tm.assert_numpy_array_equal(result, expected)
-
-
 def test_sanitize_masked_array_with_masked_ea(values, dtype, expected):
     result = sanitize_array(values, dtype=dtype)
     tm.assert_numpy_array_equal(result, expected)
