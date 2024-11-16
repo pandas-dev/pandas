ad9ec515f4745e0973d6a571b91e1005cecfaf9b
Aman Sharma <210100011@iitb.ac.in>
2024-11-16 14:17:20 +0530

    decorate concat in concat.py with set_module('pandas')

--- pandas/core/reshape/concat.py
+++ pandas/core/reshape/concat.py
@@ -18,6 +18,7 @@ import numpy as np
 
 from pandas._libs import lib
 from pandas.util._exceptions import find_stack_level
+from pandas.util._decorators import set_module
 
 from pandas.core.dtypes.common import (
     is_bool,
@@ -149,6 +150,7 @@ def concat(
 ) -> DataFrame | Series: ...
 
 
+@set_module("pandas")
 def concat(
     objs: Iterable[Series | DataFrame] | Mapping[HashableT, Series | DataFrame],
     *,
