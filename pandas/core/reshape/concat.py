c8bdc46c1aa3006a598c8368b89d1eef68d584a1
Aman Sharma <210100011@iitb.ac.in>
2024-11-16 14:42:27 +0530

    fix whitespace error

--- pandas/core/reshape/concat.py
+++ pandas/core/reshape/concat.py
@@ -17,8 +17,8 @@ import warnings
 import numpy as np
 
 from pandas._libs import lib
-from pandas.util._exceptions import find_stack_level
 from pandas.util._decorators import set_module
+from pandas.util._exceptions import find_stack_level
 
 from pandas.core.dtypes.common import (
     is_bool,
