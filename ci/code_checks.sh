c204a8dd330b27942ea78cc1f8c351d859d3c876
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-11-23 12:05:28 +0530

    DOC: fix SA01 for pandas.errors.NumExprClobberingError

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -113,7 +113,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.errors.IntCastingNaNError SA01" \
         -i "pandas.errors.InvalidIndexError SA01" \
         -i "pandas.errors.NullFrequencyError SA01" \
-        -i "pandas.errors.NumExprClobberingError SA01" \
         -i "pandas.errors.NumbaUtilError SA01" \
         -i "pandas.errors.OutOfBoundsTimedelta SA01" \
         -i "pandas.errors.PerformanceWarning SA01" \
