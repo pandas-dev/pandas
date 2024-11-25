00c2207cbe8e429d11db5973794b604041cd74b2
Tuhin Sharma <tuhinsharma121@gmail.com>
2024-11-26 00:12:55 +0530

    DOC: fix SA01,ES01 for pandas.errors.InvalidIndexError (#60400)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -110,7 +110,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.core.resample.Resampler.transform PR01,RT03,SA01" \
         -i "pandas.core.resample.Resampler.var SA01" \
         -i "pandas.errors.IntCastingNaNError SA01" \
-        -i "pandas.errors.InvalidIndexError SA01" \
         -i "pandas.errors.NullFrequencyError SA01" \
         -i "pandas.errors.NumExprClobberingError SA01" \
         -i "pandas.errors.NumbaUtilError SA01" \
