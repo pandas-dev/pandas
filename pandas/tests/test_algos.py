806ac60ea3118df426c1b40df22f7420c97f6e56
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-11-25 11:35:37 -0800

    TST: Avoid hashing np.timedelta64 without unit (#60416)

--- pandas/tests/test_algos.py
+++ pandas/tests/test_algos.py
@@ -1254,7 +1254,7 @@ class TestValueCounts:
         result_dt = algos.value_counts_internal(dt)
         tm.assert_series_equal(result_dt, exp_dt)
 
-        exp_td = Series({np.timedelta64(10000): 1}, name="count")
+        exp_td = Series([1], index=[np.timedelta64(10000)], name="count")
         result_td = algos.value_counts_internal(td)
         tm.assert_series_equal(result_td, exp_td)
 
