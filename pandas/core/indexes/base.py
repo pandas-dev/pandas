cee08f54c7947d155c320bfff4412f8a7885c489
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2024-11-26 21:07:06 +0100

    String dtype: use ObjectEngine for indexing for now correctness over performance (#60329)
    (cherry picked from commit 98f7e4deeff26a5ef993ee27104387a1a6e0d3d3)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -884,6 +884,8 @@ class Index(IndexOpsMixin, PandasObject):
             # error: Item "ExtensionArray" of "Union[ExtensionArray,
             # ndarray[Any, Any]]" has no attribute "_ndarray"  [union-attr]
             target_values = self._data._ndarray  # type: ignore[union-attr]
+        elif is_string_dtype(self.dtype) and not is_object_dtype(self.dtype):
+            return libindex.StringObjectEngine(target_values, self.dtype.na_value)  # type: ignore[union-attr]
 
         # error: Argument 1 to "ExtensionEngine" has incompatible type
         # "ndarray[Any, Any]"; expected "ExtensionArray"
@@ -6133,7 +6135,6 @@ class Index(IndexOpsMixin, PandasObject):
     def get_indexer_non_unique(
         self, target
     ) -> tuple[npt.NDArray[np.intp], npt.NDArray[np.intp]]:
-        target = ensure_index(target)
         target = self._maybe_cast_listlike_indexer(target)
 
         if not self._should_compare(target) and not self._should_partial_index(target):
