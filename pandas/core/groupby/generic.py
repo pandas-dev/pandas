65693bd3e7cc6c318ddb72fe681ae9303972f170
Axeldnahcram <axel.marchand@ensae.fr>
2024-12-03 00:29:54 +0100

    Whitespace

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -2548,7 +2548,7 @@ class DataFrameGroupBy(GroupBy[DataFrame]):
 
         >>> df.groupby("food_type").idxmax()
                         consumption   co2_emissions
-        food_type                                
+        food_type
         animal                 Beef            Beef
         plant        Wheat Products  Wheat Products
 
@@ -2614,7 +2614,7 @@ class DataFrameGroupBy(GroupBy[DataFrame]):
 
         >>> df.groupby("food_type").idxmin()
                         consumption   co2_emissions
-        food_type                                
+        food_type
         animal                 Pork            Pork
         plant        Wheat Products  Wheat Products
         """
