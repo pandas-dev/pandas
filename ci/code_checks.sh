8e97b07fe727ddb559d1aae64ceb92b0e501125f
Óscar Gómez <oscar.gomez.borz@gmail.com>
2024-12-03 19:39:25 +0100

    DOC: Fix some docstring validations in pd.Series (#60481)
    * DOC: Fix some docstring validations in pd.Series
    
    * new circle

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -73,8 +73,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.Period.freq GL08" \
         -i "pandas.Period.ordinal GL08" \
         -i "pandas.RangeIndex.from_range PR01,SA01" \
-        -i "pandas.Series.dt.unit GL08" \
-        -i "pandas.Series.pad PR01,SA01" \
         -i "pandas.Timedelta.max PR02" \
         -i "pandas.Timedelta.min PR02" \
         -i "pandas.Timedelta.resolution PR02" \
