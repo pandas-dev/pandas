96d9e9d6316af5df4dae103765e990687ee85c24
Axeldnahcram <33946160+Axeldnahcram@users.noreply.github.com>
2024-12-04 20:21:15 +0100

    DOC: Fix docstrings errors SEM and GET_GROUP (#60475)
    * DOC: fix sem
    
    * Added sections
    
    * DOC: fix See also
    
    * Remove failed docstrings
    
    * Fix: Matches the right format
    
    * Pre commit format

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -715,7 +715,19 @@ class BaseGroupBy(PandasObject, SelectionMixin[NDFrameT], GroupByIndexingMixin):
 
         Returns
         -------
-        DataFrame or Series
+        Series or DataFrame
+            Get the respective Series or DataFrame corresponding to the group provided.
+
+        See Also
+        --------
+        DataFrameGroupBy.groups: Dictionary representation of the groupings formed
+            during a groupby operation.
+        DataFrameGroupBy.indices: Provides a mapping of group rows to positions
+            of the elements.
+        SeriesGroupBy.groups: Dictionary representation of the groupings formed
+            during a groupby operation.
+        SeriesGroupBy.indices: Provides a mapping of group rows to positions
+            of the elements.
 
         Examples
         --------
