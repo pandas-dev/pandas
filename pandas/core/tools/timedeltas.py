f2ebb002294a44f8b9781ebee66f643b96b05af0
Lucas Brown <brownl@oregonstate.edu>
2024-12-03 17:19:45 -0800

    removed alias in timedeltas.py

--- pandas/core/tools/timedeltas.py
+++ pandas/core/tools/timedeltas.py
@@ -18,7 +18,7 @@ from pandas._libs.tslibs import (
     NaTType,
 )
 from pandas._libs.tslibs.timedeltas import (
-    Timedelta as _Timedelta,
+    Timedelta,
     disallow_ambiguous_unit,
     parse_timedelta_unit,
 )
