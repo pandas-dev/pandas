1c9a763a0a6e7b6ba4dcfd364a3fcb506883ba16
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2024-12-12 10:00:11 -0800

    Ignore possible RuntimeWarning in _hash_ndarray

--- pandas/core/util/hashing.py
+++ pandas/core/util/hashing.py
@@ -6,6 +6,7 @@ from __future__ import annotations
 
 import itertools
 from typing import TYPE_CHECKING
+import warnings
 
 import numpy as np
 
@@ -334,9 +335,15 @@ def _hash_ndarray(
             vals = hash_object_array(vals, hash_key, encoding)
         except TypeError:
             # we have mixed types
-            vals = hash_object_array(
-                vals.astype(str).astype(object), hash_key, encoding
-            )
+            with warnings.catch_warnings():
+                warnings.filterwarnings(
+                    "ignore",
+                    "invalid value encountered in cast",
+                    RuntimeWarning,
+                )
+                vals = hash_object_array(
+                    vals.astype(str).astype(object), hash_key, encoding
+                )
 
     # Then, redistribute these 64-bit ints within the space of 64-bit ints
     vals ^= vals >> 30
