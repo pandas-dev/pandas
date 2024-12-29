163dfe31839f969274f3da5017742b55ea7b6ff5
anzor <anzor@Ubuntu22.myguest.virtualbox.org>
2024-12-30 00:25:45 +0300

    add unit-test

--- pandas/tests/indexing/test_loc.py
+++ pandas/tests/indexing/test_loc.py
@@ -3337,6 +3337,18 @@ class TestLocSeries:
                     [[10, 10, 10], [20, 20, 20], [30, 30, 30]], columns=list("ABC")
                 ),
             ],
+            [
+                DataFrame([[1, 2, 3], [4, 5, 6], [7, 8, 9]], columns=list("ABC")),
+                slice(None),
+                Series(
+                    {
+                        "A": True,
+                        "C": False,
+                        "B": True,
+                    }
+                ),
+                DataFrame([[10, 10, 3], [20, 20, 6], [30, 30, 9]], columns=list("ABC")),
+            ],
         ],
     )
     def test_loc_set_series_to_multiple_columns(
