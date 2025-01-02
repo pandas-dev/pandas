21a69c9a1442e5e4806eea6b38cfc0fa96dc5d35
Will Ayd <william.ayd@icloud.com>
2025-01-02 10:44:21 -0500

    Progress to base.ExtensionArray tests

--- pandas/tests/extension/list/test_list.py
+++ pandas/tests/extension/list/test_list.py
@@ -5,7 +5,32 @@ from pandas.core.arrays.list_ import (
     ListArray,
     ListDtype,
 )
+from pandas.tests.extension.base.accumulate import BaseAccumulateTests
+from pandas.tests.extension.base.casting import BaseCastingTests
 from pandas.tests.extension.base.constructors import BaseConstructorsTests
+from pandas.tests.extension.base.dim2 import (  # noqa: F401
+    Dim2CompatTests,
+    NDArrayBacked2DTests,
+)
+from pandas.tests.extension.base.dtype import BaseDtypeTests
+from pandas.tests.extension.base.getitem import BaseGetitemTests
+from pandas.tests.extension.base.groupby import BaseGroupbyTests
+from pandas.tests.extension.base.index import BaseIndexTests
+from pandas.tests.extension.base.interface import BaseInterfaceTests
+from pandas.tests.extension.base.io import BaseParsingTests
+from pandas.tests.extension.base.methods import BaseMethodsTests
+from pandas.tests.extension.base.missing import BaseMissingTests
+from pandas.tests.extension.base.ops import (  # noqa: F401
+    BaseArithmeticOpsTests,
+    BaseComparisonOpsTests,
+    BaseOpsUtil,
+    BaseUnaryOpsTests,
+)
+from pandas.tests.extension.base.printing import BasePrintingTests
+from pandas.tests.extension.base.reduce import BaseReduceTests
+from pandas.tests.extension.base.reshaping import BaseReshapingTests
+from pandas.tests.extension.base.setitem import BaseSetitemTests
+
 
 
 @pytest.fixture
@@ -21,7 +46,28 @@ def data():
     return ListArray(data)
 
 
-class TestListArray(BaseConstructorsTests): ...
+class TestListArray(
+    BaseAccumulateTests,
+    #BaseCastingTests,
+    BaseConstructorsTests,
+    #BaseDtypeTests,
+    #BaseGetitemTests,
+    #BaseGroupbyTests,
+    BaseIndexTests,
+    #BaseInterfaceTests,
+    BaseParsingTests,
+    #BaseMethodsTests,
+    #BaseMissingTests,
+    #BaseArithmeticOpsTests,
+    #BaseComparisonOpsTests,
+    #BaseUnaryOpsTests,
+    #BasePrintingTests,
+    BaseReduceTests,
+    #BaseReshapingTests,
+    #BaseSetitemTests,
+    Dim2CompatTests,
+):
+    ...
 
 
 def test_to_csv(data):
