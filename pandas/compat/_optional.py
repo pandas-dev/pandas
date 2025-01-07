01f1a823ff5f8b8d7afc817bff8e657fae250729
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-01-06 17:38:13 -0800

    Undo sqlalchemy

--- pandas/compat/_optional.py
+++ pandas/compat/_optional.py
@@ -47,7 +47,7 @@ VERSIONS = {
     "pyxlsb": "1.0.10",
     "s3fs": "2024.2.0",
     "scipy": "1.12.0",
-    "sqlalchemy": "2.0.25",
+    "sqlalchemy": "2.0.0",
     "tables": "3.8.0",
     "tabulate": "0.9.0",
     "xarray": "2024.1.1",
