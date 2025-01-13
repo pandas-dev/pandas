b5d4e89d378e69a87b1b9ac7f3d6fa6867840fff
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2025-01-13 17:28:28 -0500

    ENH: Implement cum* methods for PyArrow strings (#60633)
    * ENH: Implement cum* methods for PyArrow strings
    
    * cleanup
    
    * Cleanup
    
    * fixup
    
    * Fix extension tests
    
    * xfail test when there is no pyarrow
    
    * mypy fixups
    
    * Change logic & whatsnew
    
    * Change logic & whatsnew
    
    * Fix fixture
    
    * Fixup

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -1319,6 +1319,22 @@ def nullable_string_dtype(request):
 
 @pytest.fixture(
     params=[
+        pytest.param(("pyarrow", np.nan), marks=td.skip_if_no("pyarrow")),
+        pytest.param(("pyarrow", pd.NA), marks=td.skip_if_no("pyarrow")),
+    ]
+)
+def pyarrow_string_dtype(request):
+    """
+    Parametrized fixture for string dtypes backed by Pyarrow.
+
+    * 'str[pyarrow]'
+    * 'string[pyarrow]'
+    """
+    return pd.StringDtype(*request.param)
+
+
+@pytest.fixture(
+    params=[
         "python",
         pytest.param("pyarrow", marks=td.skip_if_no("pyarrow")),
     ]
