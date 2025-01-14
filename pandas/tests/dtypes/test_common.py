fcc94eb0aca7cb0c90c992d1c36220bde5580000
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2025-01-14 21:39:51 +0100

    [backport 2.3.x] String dtype: disallow specifying the 'str' dtype with storage in [..] in string alias (#60661) (#60715)
    (cherry picked from commit 7415aca37159a99f8f99d93a1908070ddf36178c)

--- pandas/tests/dtypes/test_common.py
+++ pandas/tests/dtypes/test_common.py
@@ -835,3 +835,23 @@ def test_pandas_dtype_string_dtypes(string_storage):
     with pd.option_context("string_storage", string_storage):
         result = pandas_dtype("string")
     assert result == pd.StringDtype(string_storage, na_value=pd.NA)
+
+
+def test_pandas_dtype_string_dtype_alias_with_storage():
+    with pytest.raises(TypeError, match="not understood"):
+        pandas_dtype("str[python]")
+
+    with pytest.raises(TypeError, match="not understood"):
+        pandas_dtype("str[pyarrow]")
+
+    result = pandas_dtype("string[python]")
+    assert result == pd.StringDtype("python", na_value=pd.NA)
+
+    if HAS_PYARROW:
+        result = pandas_dtype("string[pyarrow]")
+        assert result == pd.StringDtype("pyarrow", na_value=pd.NA)
+    else:
+        with pytest.raises(
+            ImportError, match="required for PyArrow backed StringArray"
+        ):
+            pandas_dtype("string[pyarrow]")
