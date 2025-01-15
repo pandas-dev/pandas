e99e13f569c5dc08417b9ca7e058a6ac932f6a08
Nitish Satyavolu <snitish.iitk@gmail.com>
2025-01-15 08:19:37 -0800

    BUG: Fix DataFrame binary arithmatic operation handling of unaligned … (#60538)
    * BUG: Fix DataFrame binary arithmatic operation handling of unaligned MultiIndex columns
    
    * Address review comment

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -7967,6 +7967,16 @@ class DataFrame(NDFrame, OpsMixin):
 
         new_left = left if lcol_indexer is None else left.iloc[:, lcol_indexer]
         new_right = right if rcol_indexer is None else right.iloc[:, rcol_indexer]
+
+        # GH#60498 For MultiIndex column alignment
+        if isinstance(cols, MultiIndex):
+            # When overwriting column names, make a shallow copy so as to not modify
+            # the input DFs
+            new_left = new_left.copy(deep=False)
+            new_right = new_right.copy(deep=False)
+            new_left.columns = cols
+            new_right.columns = cols
+
         result = op(new_left, new_right)
 
         # Do the join on the columns instead of using left._align_for_op
@@ -7996,6 +8006,13 @@ class DataFrame(NDFrame, OpsMixin):
 
         if not isinstance(right, DataFrame):
             return False
+
+        if (
+            isinstance(self.columns, MultiIndex)
+            or isinstance(right.columns, MultiIndex)
+        ) and not self.columns.equals(right.columns):
+            # GH#60498 Reindex if MultiIndexe columns are not matching
+            return True
 
         if fill_value is None and level is None and axis == 1:
             # TODO: any other cases we should handle here?
--- pandas/tests/frame/test_arithmetic.py
+++ pandas/tests/frame/test_arithmetic.py
@@ -2033,6 +2033,31 @@ def test_arithmetic_multiindex_align():
     tm.assert_frame_equal(result, expected)
 
 
+def test_arithmetic_multiindex_column_align():
+    # GH#60498
+    df1 = DataFrame(
+        data=100,
+        columns=MultiIndex.from_product(
+            [["1A", "1B"], ["2A", "2B"]], names=["Lev1", "Lev2"]
+        ),
+        index=["C1", "C2"],
+    )
+    df2 = DataFrame(
+        data=np.array([[0.1, 0.25], [0.2, 0.45]]),
+        columns=MultiIndex.from_product([["1A", "1B"]], names=["Lev1"]),
+        index=["C1", "C2"],
+    )
+    expected = DataFrame(
+        data=np.array([[10.0, 10.0, 25.0, 25.0], [20.0, 20.0, 45.0, 45.0]]),
+        columns=MultiIndex.from_product(
+            [["1A", "1B"], ["2A", "2B"]], names=["Lev1", "Lev2"]
+        ),
+        index=["C1", "C2"],
+    )
+    result = df1 * df2
+    tm.assert_frame_equal(result, expected)
+
+
 def test_bool_frame_mult_float():
     # GH 18549
     df = DataFrame(True, list("ab"), list("cd"))
