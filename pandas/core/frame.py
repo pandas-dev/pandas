45f54fe0deb5e521640111fc42eef63f9031ddbc
Patrick Hoefler <61934744+phofl@users.noreply.github.com>
2025-01-17 20:11:37 +0100

    REGR: from_records not initializing subclasses properly (#60726)
    * REGR: from_records not initializing subclasses properly
    
    * Move whatsnew

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -2317,7 +2317,10 @@ class DataFrame(NDFrame, OpsMixin):
             columns = columns.drop(exclude)
 
         mgr = arrays_to_mgr(arrays, columns, result_index)
-        return cls._from_mgr(mgr, axes=mgr.axes)
+        df = DataFrame._from_mgr(mgr, axes=mgr.axes)
+        if cls is not DataFrame:
+            return cls(df, copy=False)
+        return df
 
     def to_records(
         self, index: bool = True, column_dtypes=None, index_dtypes=None
