5caf11c05bd661a0c269529834d5cccd99669ba0
William Ayd <will_ayd@innobi.io>
2025-01-24 15:21:29 -0500

    TST(string dtype): Fix xfails in test_block_internals.py (#60765)

--- pandas/tests/frame/constructors/test_from_dict.py
+++ pandas/tests/frame/constructors/test_from_dict.py
@@ -108,7 +108,6 @@ class TestFromDict:
         expected = DataFrame.from_dict(sdict, orient="index")
         tm.assert_frame_equal(result, expected)
 
-    @pytest.mark.xfail(using_string_dtype(), reason="columns inferring logic broken")
     def test_constructor_orient(self, float_string_frame):
         data_dict = float_string_frame.T._series
         recons = DataFrame.from_dict(data_dict, orient="index")
