321c8d2c654f8a0c1688bd989867aa4f034f3569
Tuhin Sharma <tuhinsharma121@gmail.com>
2025-02-10 13:57:17 +0530

    DOC: fix ES01 for  is a dtype representation for categorical data, which     allows users to define a fixed set of values and optionally impose an ordering.     This is particularly useful for handling categorical variables efficiently,     as it can significantly reduce memory usage compared to using object dtypes.

--- pandas/core/dtypes/dtypes.py
+++ pandas/core/dtypes/dtypes.py
@@ -161,6 +161,11 @@ class CategoricalDtype(PandasExtensionDtype, ExtensionDtype):
     """
     Type for categorical data with the categories and orderedness.
 
+    It is a dtype representation for categorical data, which
+    allows users to define a fixed set of values and optionally impose an ordering.
+    This is particularly useful for handling categorical variables efficiently,
+    as it can significantly reduce memory usage compared to using object dtypes.
+
     Parameters
     ----------
     categories : sequence, optional
