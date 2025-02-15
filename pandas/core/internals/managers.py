4f77590825a6671dcd9fe8277da56835e81bc64e
Chilin <chilin.cs07@nycu.edu.tw>
2025-02-16 03:37:52 +0800

    BUG: fix assign failure issue when Copy-on-Write

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -572,7 +572,13 @@ class BaseBlockManager(PandasObject):
                         0, blk_loc, values
                     )
                     # first block equals values
-                    self.blocks[0].setitem((indexer[0], np.arange(len(blk_loc))), value)
+                    is_full_column_selection = indexer[1] == slice(None)
+                    col_indexer = (
+                        slice(None)
+                        if is_full_column_selection
+                        else np.arange(len(blk_loc))
+                    )
+                    self.blocks[0].setitem((indexer[0], col_indexer), value)
                     return self
             # No need to split if we either set all columns or on a single block
             # manager
