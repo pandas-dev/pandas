6921ebbc8d64df8e3f69efbaabb473f36759eeb9
Chilin Chiou <chilin.chiou@gmail.com>
2025-02-16 22:40:16 +0800

    Check if data type is slice before comparing with slice(None)

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -572,12 +572,10 @@ class BaseBlockManager(PandasObject):
                         0, blk_loc, values
                     )
                     # first block equals values
-                    is_full_column_selection = indexer[1] == slice(None)
-                    col_indexer = (
-                        slice(None)
-                        if is_full_column_selection
-                        else np.arange(len(blk_loc))
-                    )
+                    if isinstance(indexer[1], slice) and indexer[1] == slice(None):
+                        col_indexer = slice(None)
+                    else:
+                        col_indexer = np.arange(len(blk_loc))
                     self.blocks[0].setitem((indexer[0], col_indexer), value)
                     return self
             # No need to split if we either set all columns or on a single block
