5e5db791fe78466d98d1da76d0ee5b9afbb61c21
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2025-02-18 21:35:41 -0500

    Backport PR #60938: ENH(string dtype): Implement cumsum for Python-backed strings

--- pandas/tests/apply/test_str.py
+++ pandas/tests/apply/test_str.py
@@ -4,8 +4,6 @@ import operator
 import numpy as np
 import pytest
 
-from pandas.compat import HAS_PYARROW
-
 from pandas.core.dtypes.common import is_number
 
 from pandas import (
@@ -168,21 +166,10 @@ def test_agg_cython_table_series(series, func, expected):
         ),
     ),
 )
-def test_agg_cython_table_transform_series(request, series, func, expected):
+def test_agg_cython_table_transform_series(series, func, expected):
     # GH21224
     # test transforming functions in
     # pandas.core.base.SelectionMixin._cython_table (cumprod, cumsum)
-    if (
-        series.dtype == "string"
-        and func in ("cumsum", np.cumsum, np.nancumsum)
-        and not HAS_PYARROW
-    ):
-        request.applymarker(
-            pytest.mark.xfail(
-                raises=NotImplementedError,
-                reason="TODO(infer_string) cumsum not yet implemented for string",
-            )
-        )
     warn = None if isinstance(func, str) else FutureWarning
     with tm.assert_produces_warning(warn, match="is currently using Series.*"):
         result = series.agg(func)
