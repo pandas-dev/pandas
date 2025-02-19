277d6f42da78f2a5af2ce3b4e5b37b2034d4ad73
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-02-19 11:12:56 +0000

    remove from docstring

--- pandas/core/indexes/multi.py
+++ pandas/core/indexes/multi.py
@@ -212,8 +212,6 @@ class MultiIndex(Index):
         level).
     names : optional sequence of objects
         Names for each of the index levels. (name is accepted for compat).
-    dtype : Numpy dtype or pandas type, optional
-        Data type for the MultiIndex.
     copy : bool, default False
         Copy the meta-data.
     name : Label
