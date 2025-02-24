bf830f5ab70060e55a6506efd1dbda070b465cdd
william larkin <56956489+will-larkin@users.noreply.github.com>
2025-02-24 14:59:46 +0000

    fix: commit checks

--- pandas/io/json/_json.py
+++ pandas/io/json/_json.py
@@ -975,21 +975,20 @@ class JsonReader(abc.Iterator, Generic[FrameSeriesStrT]):
         """
         Read JSON using the ujson engine.
         """
+        obj: DataFrame | Series
         if self.lines:
             if self.chunksize:
                 obj = concat(self)
             elif self.nrows:
                 lines = list(islice(self.data, self.nrows))
                 lines_json = self._combine_lines(lines)
-                obj: DataFrame | Series = self._get_object_parser(lines_json)
+                obj = self._get_object_parser(lines_json)
             else:
                 data = ensure_str(self.data)
                 data_lines = data.split("\n")
-                obj: DataFrame | Series = self._get_object_parser(
-                    self._combine_lines(data_lines)
-                )
+                obj = self._get_object_parser(self._combine_lines(data_lines))
         else:
-            obj: DataFrame | Series = self._get_object_parser(self.data)
+            obj = self._get_object_parser(self.data)
         if self.dtype_backend is not lib.no_default:
             return obj.convert_dtypes(
                 infer_objects=False, dtype_backend=self.dtype_backend
