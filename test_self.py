580594ce8fb08f4c21f0287232f781a5c6a7b4ad
Abhibhav Sharma <139039304+Abhibhav2003@users.noreply.github.com>
2025-03-02 09:18:35 +0530

    Delete test_self.py

--- test_self.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-import pandas as pd
-
-# creating a single series dataframe
-frame = pd.DataFrame(np.array([1, 2, 3, 4, 5, 100]))
-
-# getting the describe with single percentile value
-print(frame.describe(percentiles=[0.25]))
