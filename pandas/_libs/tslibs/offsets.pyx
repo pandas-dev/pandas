010ffe2bf0d49b005ddcc585c9f7092e0135eb28
Harmen Stoppels <harmenstoppels@gmail.com>
2025-03-04 00:11:49 +0100

    test_datetimes.py: fix literal string (#60820)
    * test_datetimes.py: fix literal string
    
    * fix test
    
    * fix repeated whitespace
    
    * add whatsnew entry
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- pandas/_libs/tslibs/offsets.pyx
+++ pandas/_libs/tslibs/offsets.pyx
@@ -5108,7 +5108,7 @@ def _warn_about_deprecated_aliases(name: str, is_period: bool) -> str:
         warnings.warn(
             f"\'{name}\' is deprecated and will be removed "
             f"in a future version, please use "
-            f"\'{c_PERIOD_AND_OFFSET_DEPR_FREQSTR.get(name)}\' "
+            f"\'{c_PERIOD_AND_OFFSET_DEPR_FREQSTR.get(name)}\'"
             f" instead.",
             FutureWarning,
             stacklevel=find_stack_level(),
@@ -5122,7 +5122,7 @@ def _warn_about_deprecated_aliases(name: str, is_period: bool) -> str:
             warnings.warn(
                 f"\'{name}\' is deprecated and will be removed "
                 f"in a future version, please use "
-                f"\'{_name}\' "
+                f"\'{_name}\'"
                 f" instead.",
                 FutureWarning,
                 stacklevel=find_stack_level(),
