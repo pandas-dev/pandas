537671a7a063c28cc1bb23a273d86c764fd0e628
Tony Ding <tonyyuyiding@gmail.com>
2025-03-04 10:31:20 +0800

    remove reshape

--- pandas/core/construction.py
+++ pandas/core/construction.py
@@ -613,7 +613,7 @@ def sanitize_array(
             subarr = data
             if data.dtype == object and infer_object:
                 fltarr = data.ravel()  # GH#61026
-                subarr = maybe_infer_to_datetimelike(fltarr).reshape(data.shape)
+                subarr = maybe_infer_to_datetimelike(fltarr)
             elif data.dtype.kind == "U" and using_string_dtype():
                 from pandas.core.arrays.string_ import StringDtype
 
