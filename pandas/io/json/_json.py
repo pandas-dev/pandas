3954c842f1592bef6258050594629d023d06aaf2
william larkin <56956489+will-larkin@users.noreply.github.com>
2025-03-04 17:29:58 +0000

    Update _json.py

--- pandas/io/json/_json.py
+++ pandas/io/json/_json.py
@@ -957,7 +957,7 @@ class JsonReader(abc.Iterator, Generic[FrameSeriesStrT]):
         pa_table = pyarrow_json.read_json(self.data)
         df = arrow_table_to_pandas(pa_table, dtype_backend=self.dtype_backend)
 
-        if self.dtype:
+        if isinstance(dict, self.dtype):
             df = df.astype(self.dtype)
 
         return df
