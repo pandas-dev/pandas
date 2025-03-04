85a5c38318eaebb848cc7dd61b93d33639e406e8
snitish.iitk@gmail.com <snitish.iitk@gmail.com>
2025-03-04 03:15:03 -0600

    Rename regex variable appropriately and allow more complex chaining

--- pandas/tests/io/test_common.py
+++ pandas/tests/io/test_common.py
@@ -505,6 +505,9 @@ def test_is_fsspec_url_chained():
     # GH#48978 Support chained fsspec URLs
     # See https://filesystem-spec.readthedocs.io/en/latest/features.html#url-chaining.
     assert icom.is_fsspec_url("filecache::s3://pandas/test.csv")
+    assert icom.is_fsspec_url("zip://test.csv::filecache::gcs://bucket/afile.zip")
+    assert icom.is_fsspec_url("filecache::zip://test.csv::gcs://bucket/afile.zip")
+    assert icom.is_fsspec_url("filecache::dask::s3://pandas/test.csv")
     assert not icom.is_fsspec_url("filecache:s3://pandas/test.csv")
     assert not icom.is_fsspec_url("filecache:::s3://pandas/test.csv")
     assert not icom.is_fsspec_url("filecache::://pandas/test.csv")
