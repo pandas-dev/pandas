e3b0b5d7d250f46d2e7be928e44b98f48bde72c5
MartinBraquet <martin.braquet@gmail.com>
2025-03-20 14:12:05 +0700

    Fix bug in `~Series.describe` where median percentile is included when the `percentiles` argument is passed

--- pandas/tests/frame/methods/test_describe.py
+++ pandas/tests/frame/methods/test_describe.py
@@ -413,3 +413,33 @@ class TestDataFrameDescribe:
             dtype=pd.ArrowDtype(pa.float64()),
         )
         tm.assert_frame_equal(result, expected)
+
+    @pytest.mark.parametrize("percentiles", [None, [], [0.2]])
+    def test_refine_percentiles(self, percentiles):
+        """
+        Test that the percentiles are returned correctly depending on the `percentiles`
+        argument.
+        - The default behavior is to return the 25th, 50th, and 75 percentiles
+        - If `percentiles` is an empty list, no percentiles are returned
+        - If `percentiles` is a non-empty list, only those percentiles are returned
+        """
+        # GH#60550
+        df = DataFrame({"a": np.arange(0, 10, 1)})
+
+        result = df.describe(percentiles=percentiles)
+
+        if percentiles is None:
+            percentiles = [0.25, 0.5, 0.75]
+
+        expected = Series(
+            {
+                "count": len(df.a),
+                "mean": df.a.mean(),
+                "std": df.a.std(),
+                "min": df.a.min(),
+                **{f"{p:.0%}": df.a.quantile(p) for p in percentiles},
+                "max": df.a.max(),
+            },
+        ).to_frame(name="a")
+
+        tm.assert_frame_equal(result, expected)
