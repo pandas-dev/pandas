7e301815952e843ab53d32a1bc8fa458e863b243
stevenae <stevenalonzoellis@gmail.com>
2025-03-26 16:33:04 -0400

    removed mixed type and threshold

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -5165,28 +5165,18 @@ class DataFrame(NDFrame, OpsMixin):
             # GH#33041
             raise ValueError("DataFrame.lookup requires unique index and columns")
 
-
-        thresh = 1000
-        if not self._is_mixed_type or n > thresh:
-            values = self.values
-            ridx = self.index.get_indexer(row_labels)
-            cidx = self.columns.get_indexer(col_labels)
-            if (ridx == -1).any():
-                raise KeyError("One or more row labels was not found")
-            if (cidx == -1).any():
-                raise KeyError("One or more column labels was not found")
-            flat_index = ridx * len(self.columns) + cidx
-            result = values.flat[flat_index]
-        else:
-            if dev_version=='og':
-                result = np.empty(n, dtype="O")
-                for i, (r, c) in enumerate(zip(row_labels, col_labels)):
-                    result[i] = self._get_value(r, c)
-
+        values = self.to_numpy()
+        ridx = self.index.get_indexer(row_labels)
+        cidx = self.columns.get_indexer(col_labels)
+        if (ridx == -1).any():
+            raise KeyError("One or more row labels was not found")
+        if (cidx == -1).any():
+            raise KeyError("One or more column labels was not found")
+        flat_index = ridx * len(self.columns) + cidx
+        result = values.flat[flat_index]
 
         if is_object_dtype(result):
             result = lib.maybe_convert_objects(result)
-
 
         return result
 
