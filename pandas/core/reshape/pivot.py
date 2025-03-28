e19bf3c157ab79023891c451e5f5edea27580f22
Ian Thompson <ian.thompson@hrblock.com>
2025-03-28 14:23:32 -0500

    modified:   pandas/core/reshape/pivot.py 	- Replaced "NaN" with "NA" per request from @rhshadrach in comment.

--- pandas/core/reshape/pivot.py
+++ pandas/core/reshape/pivot.py
@@ -102,7 +102,7 @@ def pivot_table(
         on the rows and columns.
     dropna : bool, default True
         Do not include columns whose entries are all NaN. If True,
-        - rows with a NaN value in any column will be omitted before computing margins,
+        - rows with a NA value in any column will be omitted before computing margins,
         - index/column keys containing NA values will be dropped (see ``dropna``
         parameter in :method:``DataFrame.groupby``).
     margins_name : str, default 'All'
