fc0a75f87222ac00a275e07bfcc6720dfcab7fa7
Ian Thompson <ian.thompson@hrblock.com>
2025-03-30 19:04:14 -0500

    modified:   pandas/core/reshape/pivot.py 	- Updated spelling of :method: to :meth: per comment feedback.

--- pandas/core/reshape/pivot.py
+++ pandas/core/reshape/pivot.py
@@ -104,7 +104,7 @@ def pivot_table(
         Do not include columns whose entries are all NaN. If True,
         - rows with a NA value in any column will be omitted before computing margins,
         - index/column keys containing NA values will be dropped (see ``dropna``
-        parameter in :method:``DataFrame.groupby``).
+        parameter in :meth:``DataFrame.groupby``).
     margins_name : str, default 'All'
         Name of the row / column that will contain the totals
         when margins is True.
