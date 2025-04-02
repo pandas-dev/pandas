71d82825dac28622de7ee832293cd747a72a750f
Ian Thompson <ianiat11@gmail.com>
2025-04-02 16:28:16 -0500

    DOC: Add details of dropna in DataFrame.pivot_table (#61184)

--- pandas/core/reshape/pivot.py
+++ pandas/core/reshape/pivot.py
@@ -102,8 +102,11 @@ def pivot_table(
         on the rows and columns.
     dropna : bool, default True
         Do not include columns whose entries are all NaN. If True,
-        rows with a NaN value in any column will be omitted before
-        computing margins.
+
+        * rows with an NA value in any column will be omitted before computing margins,
+        * index/column keys containing NA values will be dropped (see ``dropna``
+          parameter in :meth:``DataFrame.groupby``).
+
     margins_name : str, default 'All'
         Name of the row / column that will contain the totals
         when margins is True.
