469e28ded9a118e25e0c0d037939136c86637f82
arthurlw <arthur.l.wigo@gmail.com>
2025-04-05 23:01:35 -0700

    added test

--- pandas/tests/series/methods/test_map.py
+++ pandas/tests/series/methods/test_map.py
@@ -604,3 +604,11 @@ def test_map_kwargs():
     result = Series([2, 4, 5]).map(lambda x, y: x + y, y=2)
     expected = Series([4, 6, 7])
     tm.assert_series_equal(result, expected)
+
+def test_map_arrow_timestamp_dict():
+    # GH 61231
+    ser = pd.Series(pd.date_range("2023-01-01", periods=3)).astype("timestamp[ns][pyarrow]")
+    mapper = {ts: i for i, ts in enumerate(ser)}
+    result = ser.map(mapper)
+    expected = pd.Series([0, 1, 2], dtype="int64")
+    tm.assert_series_equal(result, expected)
