5f1778ca91ab08f59842f527ff38bd3edd51fe10
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-04-07 16:12:45 -0700

    Add in script instead

--- scripts/cibw_before_build_windows.sh
+++ scripts/cibw_before_build_windows.sh
@@ -10,5 +10,5 @@ FREE_THREADED_BUILD="$(python -c"import sysconfig; print(bool(sysconfig.get_conf
 if [[ $FREE_THREADED_BUILD == "True" ]]; then
     python -m pip install -U pip
     python -m pip install -i https://pypi.anaconda.org/scientific-python-nightly-wheels/simple numpy cython
-    python -m pip install ninja meson-python versioneer[toml]
+    python -m pip install "ninja<1.11.1.4" meson-python versioneer[toml] numpy
 fi
