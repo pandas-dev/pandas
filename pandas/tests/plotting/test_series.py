8ed678dd0fd818ade5982bb50e569a3f4c00edea
MartinBraquet <martin.braquet@gmail.com>
2025-04-14 11:34:07 +0700

    Fix bar and line plot alignment and x-axis visibility in tests
    Ensure bar and line plots share consistent x-axis tick labels and verify that x-axis limits are adjusted to make all plotted elements visible in `test_bar_line_plot` test. These changes improve the robustness of visual alignment and boundary checks.

--- pandas/tests/plotting/test_series.py
+++ pandas/tests/plotting/test_series.py
@@ -975,13 +975,22 @@ class TestSeriesPlots:
     def test_bar_line_plot(self):
         """
         Test that bar and line plots with the same x values are superposed
+        and that the x limits are set such that the plots are visible.
         """
         # GH61161
         index = period_range("2023", periods=3, freq="Y")
+        years = set(index.year.astype(str))
         s = Series([1, 2, 3], index=index)
         ax = plt.subplot()
         s.plot(kind="bar", ax=ax)
-        bar_xticks = ax.get_xticks().tolist()
+        bar_xticks = [
+            label for label in ax.get_xticklabels() if label.get_text() in years
+        ]
         s.plot(kind="line", ax=ax, color="r")
-        line_xticks = ax.get_xticks()[: len(s)].tolist()
-        assert line_xticks == bar_xticks
+        line_xticks = [
+            label for label in ax.get_xticklabels() if label.get_text() in years
+        ]
+        assert bar_xticks == line_xticks
+        x_limits = ax.get_xlim()
+        assert x_limits[0] <= bar_xticks[0].get_position()[0]
+        assert x_limits[1] >= bar_xticks[-1].get_position()[0]
