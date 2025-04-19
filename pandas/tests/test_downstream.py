aa9a8a2f7cc0e302cc072fa399d67cec39fe9fba
Chilin Chiou <chilin.chiou@gmail.com>
2025-04-19 17:09:59 +0800

    Add github issue number into test comment

--- pandas/tests/test_downstream.py
+++ pandas/tests/test_downstream.py
@@ -189,7 +189,7 @@ def test_yaml_dump(df):
 
 @pytest.mark.parametrize("dependency", ["numpy", "dateutil", "tzdata"])
 def test_missing_required_dependency(monkeypatch, dependency):
-    # GH#61030
+    # GH#61030, GH61273
     original_import = __import__
     mock_error = ImportError(f"Mock error for {dependency}")
 
