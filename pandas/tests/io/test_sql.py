1931ee59f8f17a7494dfdea563879ea4ce92d41e
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-04-23 09:57:24 -0700

    Use a file in test_database_uri_string

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -2500,13 +2500,18 @@ def test_sqlalchemy_integer_overload_mapping(conn, request, integer):
 
 def test_database_uri_string(request, test_frame1):
     pytest.importorskip("sqlalchemy")
-    db_uri = "sqlite:///:memory:"
-    table = "iris"
-    test_frame1.to_sql(name=table, con=db_uri, if_exists="replace", index=False)
-    test_frame2 = sql.read_sql(table, db_uri)
-    test_frame3 = sql.read_sql_table(table, db_uri)
-    query = "SELECT * FROM iris"
-    test_frame4 = sql.read_sql_query(query, db_uri)
+    # Test read_sql and .to_sql method with a database URI (GH10654)
+    # db_uri = 'sqlite:///:memory:' # raises
+    # sqlalchemy.exc.OperationalError: (sqlite3.OperationalError) near
+    # "iris": syntax error [SQL: 'iris']
+    with tm.ensure_clean() as name:
+        db_uri = "sqlite:///" + name
+        table = "iris"
+        test_frame1.to_sql(name=table, con=db_uri, if_exists="replace", index=False)
+        test_frame2 = sql.read_sql(table, db_uri)
+        test_frame3 = sql.read_sql_table(table, db_uri)
+        query = "SELECT * FROM iris"
+        test_frame4 = sql.read_sql_query(query, db_uri)
     tm.assert_frame_equal(test_frame1, test_frame2)
     tm.assert_frame_equal(test_frame1, test_frame3)
     tm.assert_frame_equal(test_frame1, test_frame4)
