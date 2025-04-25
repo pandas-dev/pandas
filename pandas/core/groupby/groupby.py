0fce6b4304d8965d8685a4e9a6ad8d9a4b2bafa3
Arthur Laureus Wigo <126365160+arthurlw@users.noreply.github.com>
2025-04-25 09:34:49 -0700

    DOC: Updated `groupby.expanding` arguments (#61352)
    * updated expanding args
    
    * fixed punctuation

--- pandas/core/groupby/groupby.py
+++ pandas/core/groupby/groupby.py
@@ -3810,19 +3810,26 @@ class GroupBy(BaseGroupBy[NDFrameT]):
         )
 
     @final
-    def expanding(self, *args, **kwargs) -> ExpandingGroupby:
+    def expanding(
+        self,
+        min_periods: int = 1,
+        method: str = "single",
+    ) -> ExpandingGroupby:
         """
         Return an expanding grouper, providing expanding functionality per group.
 
-        Arguments are the same as `:meth:DataFrame.rolling` except that ``step`` cannot
-        be specified.
-
         Parameters
         ----------
-        *args : tuple
-            Positional arguments passed to the expanding window constructor.
-        **kwargs : dict
-            Keyword arguments passed to the expanding window constructor.
+        min_periods : int, default 1
+            Minimum number of observations in window required to have a value;
+            otherwise, result is ``np.nan``.
+
+        method : str {'single', 'table'}, default 'single'
+            Execute the expanding operation per single column or row (``'single'``)
+            or over the entire object (``'table'``).
+
+            This argument is only implemented when specifying ``engine='numba'``
+            in the method call.
 
         Returns
         -------
@@ -3845,7 +3852,7 @@ class GroupBy(BaseGroupBy[NDFrameT]):
         ...     }
         ... )
         >>> df
-          Class  Value
+        Class  Value
         0     A     10
         1     A     20
         2     A     30
@@ -3854,7 +3861,7 @@ class GroupBy(BaseGroupBy[NDFrameT]):
         5     B     60
 
         >>> df.groupby("Class").expanding().mean()
-                 Value
+                Value
         Class
         A     0   10.0
               1   15.0
@@ -3867,9 +3874,9 @@ class GroupBy(BaseGroupBy[NDFrameT]):
 
         return ExpandingGroupby(
             self._selected_obj,
-            *args,
+            min_periods=min_periods,
+            method=method,
             _grouper=self._grouper,
-            **kwargs,
         )
 
     @final
