9dd73d1818bb98a72b069d1302d0151507021c82
Eric Chen <eicchen02@gmail.com>
2025-04-28 21:28:41 +0000

    Updated Test cases

--- pandas/tests/plotting/test_misc.py
+++ pandas/tests/plotting/test_misc.py
@@ -683,6 +683,13 @@ class TestDataFramePlots:
         )
 
 
+def test_plot_bar_label_count_default():
+    df = DataFrame(
+        [(30, 10, 10, 10), (20, 20, 20, 20), (10, 30, 30, 10)], columns=list("ABCD")
+    )
+    df.plot(subplots=True, kind="bar", title=["A", "B", "C", "D"])
+
+
 @pytest.fixture
 def df_bar_data():
     return np.random.default_rng(3).integers(0, 100, 5)
@@ -816,3 +823,24 @@ def test_bar_2_subplots_1_triple_stacked(df_bar_data, df_bar_df, subplot_divisio
         _df_bar_subplot_checker(
             df_bar_data, df_bar_df, subplot_data_df_list[i], subplot_division[i]
         )
+def test_plot_bar_label_count_expected_fail():
+    df = DataFrame(
+        [(30, 10, 10, 10), (20, 20, 20, 20), (10, 30, 30, 10)], columns=list("ABCD")
+    )
+    with pytest.raises(
+        ValueError,
+        match="The length of `title` must equal the number of columns "
+        "if using `title` of type `list` and `subplots=True`.",
+    ):
+        df.plot(
+            subplots=[("A", "B")],
+            kind="bar",
+            title=["A&B", "C", "D", "Extra Title"],
+        )
+
+
+def test_plot_bar_label_count_expected_success():
+    df = DataFrame(
+        [(30, 10, 10, 10), (20, 20, 20, 20), (10, 30, 30, 10)], columns=list("ABCD")
+    )
+    df.plot(subplots=[("A", "B", "D")], kind="bar", title=["A&B&D", "C"])
