be24bbe302005552f5583cc28cf8672361fccd77
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2025-04-28 12:47:10 -0400

    BUG: groupby.groups with NA categories fails (#61364)
    * BUG: groupby.groups with NA categories fails
    
    * cleanup
    
    * whatsnew

--- pandas/tests/groupby/test_categorical.py
+++ pandas/tests/groupby/test_categorical.py
@@ -506,6 +506,23 @@ def test_observed_groups(observed):
     tm.assert_dict_equal(result, expected)
 
 
+def test_groups_na_category(dropna, observed):
+    # https://github.com/pandas-dev/pandas/issues/61356
+    df = DataFrame(
+        {"cat": Categorical(["a", np.nan, "a"], categories=list("adb"))},
+        index=list("xyz"),
+    )
+    g = df.groupby("cat", observed=observed, dropna=dropna)
+
+    result = g.groups
+    expected = {"a": Index(["x", "z"])}
+    if not dropna:
+        expected |= {np.nan: Index(["y"])}
+    if not observed:
+        expected |= {"b": Index([]), "d": Index([])}
+    tm.assert_dict_equal(result, expected)
+
+
 @pytest.mark.parametrize(
     "keys, expected_values, expected_index_levels",
     [
