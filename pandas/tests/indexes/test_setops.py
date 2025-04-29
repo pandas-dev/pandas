5be6ef4797d76eabe7806f214502aeadd8b6f7ad
pelagia <t8220016@aueb.gr>
2025-04-29 20:34:58 +0300

    change comment

--- pandas/tests/indexes/test_setops.py
+++ pandas/tests/indexes/test_setops.py
@@ -75,7 +75,7 @@ def test_union_different_types(index_flat, index_flat2, request):
     idx1 = index_flat
     idx2 = index_flat2
 
-    # Ειδική μεταχείριση για mixed-int-string
+    # Special handling for mixed int-string types
     if idx1.equals(pd.Index([0, "a", 1, "b", 2, "c"])) or idx2.equals(pd.Index([0, "a", 1, "b", 2, "c"])):
         idx1 = idx1.astype(str)
         idx2 = idx2.astype(str)
