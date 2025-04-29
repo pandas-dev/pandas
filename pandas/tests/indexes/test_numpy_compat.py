e8338692371eaf8a9c8d97efce1dcc027b458bc9
pelagia <t8220016@aueb.gr>
2025-04-29 20:32:34 +0300

    Fix line length issue in test_numpy_compat.py

--- pandas/tests/indexes/test_numpy_compat.py
+++ pandas/tests/indexes/test_numpy_compat.py
@@ -159,7 +159,10 @@ def test_numpy_ufuncs_reductions(index, func, request):
     if any(isinstance(x, str) for x in index) and \
             any(isinstance(x, int) for x in index):
         request.applymarker(
-            pytest.mark.xfail(reason="Cannot compare mixed types (int and str) in ufunc reductions")
+            pytest.mark.xfail(
+                reason="Cannot compare mixed types (int and str) in ufunc reductions" \
+                       " and should raise a TypeError"
+            )
         )
 
     if isinstance(index, CategoricalIndex) and index.dtype.ordered is False:
