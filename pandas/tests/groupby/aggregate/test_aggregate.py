1b4a149fb5932c3415ed30ab83ac270f5df1fa6f
arthurlw <arthur.l.wigo@gmail.com>
2025-05-28 20:20:13 +0700

    Added tests

--- pandas/tests/groupby/aggregate/test_aggregate.py
+++ pandas/tests/groupby/aggregate/test_aggregate.py
@@ -1807,3 +1807,17 @@ def test_groupby_aggregation_func_list_multi_index_duplicate_columns():
         index=Index(["level1.1", "level1.2"]),
     )
     tm.assert_frame_equal(result, expected)
+
+def test_groupby_aggregate_empty_builtin_sum():
+    df = pd.DataFrame(columns=["Group", "Data"])
+    result = df.groupby(["Group"], as_index=False)["Data"].agg("sum")
+    expected = pd.DataFrame(columns=["Group", "Data"])
+    tm.assert_frame_equal(result, expected)
+
+def test_groupby_aggregate_empty_udf():
+    def func(x):
+        return sum(x)
+    df = pd.DataFrame(columns=["Group", "Data"])
+    result = df.groupby(["Group"], as_index=False)["Data"].agg(func)
+    expected = pd.DataFrame(columns=["Group", "Data"])
+    tm.assert_frame_equal(result, expected)
