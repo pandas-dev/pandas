d1968450068ca8d87cdba9ea2fe260f3c1da72d3
Pedro Diogo <pedro.f.diogo@tecnico.ulisboa.pt>
2025-07-06 00:55:45 +0100

    fixed space

--- pandas/tests/io/formats/test_to_csv.py
+++ pandas/tests/io/formats/test_to_csv.py
@@ -781,7 +781,6 @@ def test_new_style_with_mixed_types():
 def test_new_style_with_mixed_types_in_column():
     df = DataFrame({"A": [1.23, "text", 4.56]})
     result = df.to_csv(float_format="{:.2f}", lineterminator="\n")
-
     expected = ",A\n0,1.23\n1,text\n2,4.56\n"
     assert result == expected
 
