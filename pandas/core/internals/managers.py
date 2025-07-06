81788a683ae9dbd732a309878555a50e26ef6e38
Chilin Chiou <chilin.chiou@gmail.com>
2025-07-06 21:15:11 +0800

    CLN: remove outdated _item_cache in comment

--- pandas/core/internals/managers.py
+++ pandas/core/internals/managers.py
@@ -1898,10 +1898,6 @@ class BlockManager(libinternals.BlockManager, BaseBlockManager):
         self._known_consolidated = True
 
     def _consolidate_inplace(self) -> None:
-        # In general, _consolidate_inplace should only be called via
-        #  DataFrame._consolidate_inplace, otherwise we will fail to invalidate
-        #  the DataFrame's internal structures. The exception is for newly-created
-        #  BlockManager objects not yet attached to a DataFrame.
         if not self.is_consolidated():
             self.blocks = _consolidate(self.blocks)
             self._is_consolidated = True
