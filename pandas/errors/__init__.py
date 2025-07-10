8586c08f908cf10cc7269c2f7037ff1dac77e0da
Maaz Bin Asif <maazbinaasif123@outlook.com>
2025-07-11 02:04:13 +0500

    DOC: Correct error message in AbstractMethodError for methodtype argument

--- pandas/errors/__init__.py
+++ pandas/errors/__init__.py
@@ -379,7 +379,7 @@ class AbstractMethodError(NotImplementedError):
         types = {"method", "classmethod", "staticmethod", "property"}
         if methodtype not in types:
             raise ValueError(
-                f"methodtype must be one of {methodtype}, got {types} instead."
+               f"methodtype must be one of {types}, got {methodtype} instead."
             )
         self.methodtype = methodtype
         self.class_instance = class_instance
