c744ef3eb52c1fffb9412b48228ffec316b45e07
Shubham Sarkar <46846138+DevastatingRPG@users.noreply.github.com>
2025-07-20 16:33:14 +0530

    DOC: fix doctests for datetimelike.py files for the new string dtype (#61911)

--- pandas/core/arrays/datetimes.py
+++ pandas/core/arrays/datetimes.py
@@ -1308,14 +1308,14 @@ default 'raise'
         0     January
         1    February
         2       March
-        dtype: object
+        dtype: str
 
         >>> idx = pd.date_range(start="2018-01", freq="ME", periods=3)
         >>> idx
         DatetimeIndex(['2018-01-31', '2018-02-28', '2018-03-31'],
                       dtype='datetime64[ns]', freq='ME')
         >>> idx.month_name()
-        Index(['January', 'February', 'March'], dtype='object')
+        Index(['January', 'February', 'March'], dtype='str')
 
         Using the ``locale`` parameter you can set a different locale language,
         for example: ``idx.month_name(locale='pt_BR.utf8')`` will return month
@@ -1326,7 +1326,7 @@ default 'raise'
         DatetimeIndex(['2018-01-31', '2018-02-28', '2018-03-31'],
                       dtype='datetime64[ns]', freq='ME')
         >>> idx.month_name(locale="pt_BR.utf8")  # doctest: +SKIP
-        Index(['Janeiro', 'Fevereiro', 'Março'], dtype='object')
+        Index(['Janeiro', 'Fevereiro', 'Março'], dtype='str')
         """
         values = self._local_timestamps()
 
@@ -1376,14 +1376,14 @@ default 'raise'
         0       Monday
         1      Tuesday
         2    Wednesday
-        dtype: object
+        dtype: str
 
         >>> idx = pd.date_range(start="2018-01-01", freq="D", periods=3)
         >>> idx
         DatetimeIndex(['2018-01-01', '2018-01-02', '2018-01-03'],
                       dtype='datetime64[ns]', freq='D')
         >>> idx.day_name()
-        Index(['Monday', 'Tuesday', 'Wednesday'], dtype='object')
+        Index(['Monday', 'Tuesday', 'Wednesday'], dtype='str')
 
         Using the ``locale`` parameter you can set a different locale language,
         for example: ``idx.day_name(locale='pt_BR.utf8')`` will return day
@@ -1394,7 +1394,7 @@ default 'raise'
         DatetimeIndex(['2018-01-01', '2018-01-02', '2018-01-03'],
                       dtype='datetime64[ns]', freq='D')
         >>> idx.day_name(locale="pt_BR.utf8")  # doctest: +SKIP
-        Index(['Segunda', 'Terça', 'Quarta'], dtype='object')
+        Index(['Segunda', 'Terça', 'Quarta'], dtype='str')
         """
         values = self._local_timestamps()
 
