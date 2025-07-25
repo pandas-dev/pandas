563f1f1e1bb3eb3710fb53c389d1de04a369c01b
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2025-07-25 19:44:44 +0530

    STYLE: Fix formatting and docstring issues in str.contains

--- pandas/core/strings/accessor.py
+++ pandas/core/strings/accessor.py
@@ -1361,8 +1361,10 @@ class StringMethods(NoNewAttributesMixin):
 
         if regex:
             try:
-                _compiled = pat if isinstance(pat, re.Pattern) else re.compile(
-                    pat, flags=flags
+                _compiled = (
+                    pat
+                    if isinstance(pat, re.Pattern)
+                    else re.compile(pat, flags=flags)
                 )
                 if _compiled.groups:
                     warnings.warn(
