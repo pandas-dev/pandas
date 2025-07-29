5a94001421625be41202c8a35bec897ad4388241
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2025-07-30 02:46:23 +0530

    BUG: Fix Series.str.contains with compiled regex on Arrow string dtype (#61942) and add whatsnew note

--- pandas/core/arrays/string_arrow.py
+++ pandas/core/arrays/string_arrow.py
@@ -346,7 +346,7 @@ class ArrowStringArray(ObjectStringArrayMixin, ArrowExtensionArray, BaseStringAr
     ):
         if flags:
             return super()._str_contains(pat, case, flags, na, regex)
-        if isinstance(pat, re.pattern):
+        if isinstance(pat, re.Pattern):
             pat = pat.pattern
 
         return ArrowStringArrayMixin._str_contains(self, pat, case, flags, na, regex)
