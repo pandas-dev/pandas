9ea174402035ddd6cf99e3560a9ad6d3563e2e09
Jay <2594jaypatel@gmail.com>
2025-07-30 23:12:20 -0400

    add match=, remove test, add GH#61866 reference

--- pandas/tests/arithmetic/test_numeric.py
+++ pandas/tests/arithmetic/test_numeric.py
@@ -857,31 +857,29 @@ class TestMultiplicationDivision:
             tm.assert_series_equal(result, expected)
 
     def test_np_array_mul_ea_array_returns_extensionarray(self):
+        # GH#61866
         np_array = np.array([1, 2, 3, 4, 5], dtype=np.int64)
         ea_array = array([1, 2, 3, 4, 5], dtype="Int64")
         result = np_array * ea_array
         assert isinstance(result, type(ea_array))
         tm.assert_equal(result, array([1, 4, 9, 16, 25], dtype="Int64"))
 
-    def test_df_mul_np_and_ea_array_shape_and_errors(self):
-        df = pd.DataFrame(np.arange(50).reshape(10, 5)).notna().values
-        NP_array = np.array(list(range(10)), dtype=np.int64).reshape(10, 1)
-        EA_array = array(list(range(10)), dtype="Int64").reshape(10, 1)
-        result_np = df * NP_array
-        assert isinstance(result_np, np.ndarray)
-        tm.assert_equal(result_np.shape, (10, 5))
-
-        with pytest.raises(NotImplementedError):
-            _ = df * EA_array
-
-    def test_non_1d_ea_raises_typeerror(self):
+    def test_non_1d_ea_raises_notimplementederror(self):
+        # GH#61866
         ea_array = array([1, 2, 3, 4, 5], dtype="Int64").reshape(5, 1)
         np_array = np.array([1, 2, 3, 4, 5], dtype=np.int64).reshape(5, 1)
 
-        with pytest.raises(NotImplementedError):
-            _ = ea_array * np_array
-        with pytest.raises(NotImplementedError):
-            _ = np_array * ea_array
+        with pytest.raises(
+            NotImplementedError, 
+            match="non-1D ExtensionArray operations are not supported",
+        ):
+            ea_array * np_array
+
+        with pytest.raises(
+            NotImplementedError, 
+            match="non-1D ExtensionArray operations are not supported",
+        ):
+            np_array * ea_array
 
 
 class TestAdditionSubtraction:
