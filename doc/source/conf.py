ebcaaeeadc2d5ec8888e7b03d7172d55d5cd9f0b
Hung Do <nguyenhung_do@hotmail.com>
2025-08-14 16:28:56 -0400

    Removed reference to js file in conf.py

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -297,10 +297,6 @@ html_css_files = [
     "css/pandas.css",
 ]
 
-html_js_files = [
-    'js/custom-copy.js'
-]
-
 # The name of an image file (within the static path) to use as favicon of the
 # docs.  This file should be a Windows icon file (.ico) being 16x16 or 32x32
 # pixels large.
