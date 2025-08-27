76df45256bb26a26ba25dde5446809712ba33b57
eicchen <eicchen02@gmail.com>
2025-08-27 12:38:12 -0500

    Removed PeriodArray special casing and modified test case

--- pandas/tests/arithmetic/test_period.py
+++ pandas/tests/arithmetic/test_period.py
@@ -1371,16 +1371,11 @@ class TestPeriodIndexArithmetic:
         with pytest.raises(TypeError, match=msg):
             pd.Index([ts]) + arr
 
-        if box_with_array is pd.DataFrame:
-            msg = "cannot add PeriodArray and DatetimeArray"
-        else:
-            msg = r"unsupported operand type\(s\) for \+: 'Period' and 'DatetimeArray"
+        msg = "cannot add PeriodArray and DatetimeArray"
+
         with pytest.raises(TypeError, match=msg):
             arr + pd.DataFrame([ts])
-        if box_with_array is pd.DataFrame:
-            msg = "cannot add PeriodArray and DatetimeArray"
-        else:
-            msg = r"unsupported operand type\(s\) for \+: 'DatetimeArray' and 'Period'"
+
         with pytest.raises(TypeError, match=msg):
             pd.DataFrame([ts]) + arr
 
