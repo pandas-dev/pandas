7a8b93a5462180f574afccfda3bc01c252b66574
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2025-09-08 10:58:54 +0200

    fixup

--- pandas/core/arrays/_utils.py
+++ pandas/core/arrays/_utils.py
@@ -45,13 +45,13 @@ def to_numpy_dtype_inference(
         result_dtype = None
 
     if na_value is lib.no_default:
-        if dtype is None or not hasna:
+        if result_dtype is None or not hasna:
             na_value = arr.dtype.na_value
-        elif dtype.kind == "f":  # type: ignore[union-attr]
+        elif result_dtype.kind == "f":
             na_value = np.nan
-        elif dtype.kind == "M":  # type: ignore[union-attr]
+        elif result_dtype.kind == "M":
             na_value = np.datetime64("nat")
-        elif dtype.kind == "m":  # type: ignore[union-attr]
+        elif result_dtype.kind == "m":
             na_value = np.timedelta64("nat")
         else:
             na_value = arr.dtype.na_value
