92bf98f623572ed053d48be5818c8503f1b61888
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2025-09-27 19:25:00 +0200

    [backport 2.3.x] BUG: fix .str.isdigit to honor unicode superscript for older pyarrow (#61962) (#62476)

--- pandas/core/arrays/_arrow_string_mixins.py
+++ pandas/core/arrays/_arrow_string_mixins.py
@@ -16,6 +16,7 @@ from pandas.compat import (
     pa_version_under11p0,
     pa_version_under13p0,
     pa_version_under17p0,
+    pa_version_under21p0,
 )
 
 if not pa_version_under10p1:
@@ -268,6 +269,12 @@ class ArrowStringArrayMixin:
         return self._convert_bool_result(result)
 
     def _str_isdigit(self):
+        if pa_version_under21p0:
+            # https://github.com/pandas-dev/pandas/issues/61466
+            res_list = self._apply_elementwise(str.isdigit)
+            return self._convert_bool_result(
+                pa.chunked_array(res_list, type=pa.bool_())
+            )
         result = pc.utf8_is_digit(self._pa_array)
         return self._convert_bool_result(result)
 
