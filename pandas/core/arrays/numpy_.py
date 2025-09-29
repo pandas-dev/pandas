a55d441515b27519a6f5fdfa43d620a682608943
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2025-09-29 10:06:40 +0200

    BUG: avoid validation error for ufunc with string[python] array

--- pandas/core/arrays/numpy_.py
+++ pandas/core/arrays/numpy_.py
@@ -234,6 +234,15 @@ class NumpyExtensionArray(
             # e.g. test_np_max_nested_tuples
             return result
         else:
+            if self.dtype.type is str:
+                # StringDtype
+                try:
+                    # specify dtype to preserve storage/na_value
+                    return type(self)(result, dtype=self.dtype)
+                except ValueError:
+                    # if validation of input fails (no strings)
+                    # -> fallback to returning raw numpy array
+                    return result
             # one return value; re-box array-like results
             return type(self)(result)
 
