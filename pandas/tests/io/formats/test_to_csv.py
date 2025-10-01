c7af43a5b7fe35fdcb70bead9f874144ff45ae99
Harshit Pande <pandeconscious@users.noreply.github.com>
2025-10-01 20:17:43 +0530

    refactor name with ext

--- pandas/tests/io/formats/test_to_csv.py
+++ pandas/tests/io/formats/test_to_csv.py
@@ -542,9 +542,9 @@ z
         to_compression = "infer" if to_infer else compression
         read_compression = "infer" if read_infer else compression
 
-        path = str(temp_file) + "." + compression_to_extension[compression]
-        df.to_csv(path, compression=to_compression)
-        result = pd.read_csv(path, index_col=0, compression=read_compression)
+        path_ext = str(temp_file) + "." + compression_to_extension[compression]
+        df.to_csv(path_ext, compression=to_compression)
+        result = pd.read_csv(path_ext, index_col=0, compression=read_compression)
         tm.assert_frame_equal(result, df)
 
     def test_to_csv_compression_dict(self, compression_only, temp_file):
