f03ccec86878ed9d8e10e906c1be42981e087a6e
Brock <jbrockmendel@gmail.com>
2025-10-03 13:37:05 -0700

    no warning expected

--- pandas/tests/series/test_arithmetic.py
+++ pandas/tests/series/test_arithmetic.py
@@ -14,7 +14,6 @@ import numpy as np
 import pytest
 
 from pandas._libs import lib
-from pandas.compat._optional import import_optional_dependency
 
 import pandas as pd
 from pandas import (
@@ -30,7 +29,6 @@ from pandas import (
 import pandas._testing as tm
 from pandas.core import ops
 from pandas.core.computation import expressions as expr
-from pandas.util.version import Version
 
 
 @pytest.fixture(autouse=True, params=[0, 1000000], ids=["numexpr", "python"])
@@ -369,25 +367,14 @@ class TestSeriesArithmetic:
         result = [1, None, val] + ser
         tm.assert_series_equal(result, expected)
 
-    def test_add_list_to_masked_array_boolean(self, request):
+    def test_add_list_to_masked_array_boolean(self):
         # GH#22962
-        ne = import_optional_dependency("numexpr", errors="ignore")
-        warning = (
-            UserWarning
-            if request.node.callspec.id == "numexpr"
-            and ne
-            and Version(ne.__version__) < Version("2.13.1")
-            else None
-        )
         ser = Series([True, None, False], dtype="boolean")
-        msg = "operator is not supported by numexpr for the bool dtype"
-        with tm.assert_produces_warning(warning, match=msg):
-            result = ser + [True, None, True]
+        result = ser + [True, None, True]
         expected = Series([2, pd.NA, 1], dtype=object)
         tm.assert_series_equal(result, expected)
 
-        with tm.assert_produces_warning(warning, match=msg):
-            result = [True, None, True] + ser
+        result = [True, None, True] + ser
         tm.assert_series_equal(result, expected)
 
 
