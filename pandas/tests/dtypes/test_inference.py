7b83c1614d795b348d0097f6c32967d05bdaddf5
Lirong <lirong_ma@yahoo.com>
2025-10-04 22:09:37 +0000

    test

--- pandas/tests/dtypes/test_inference.py
+++ pandas/tests/dtypes/test_inference.py
@@ -491,6 +491,10 @@ def test_is_hashable():
     assert inference.is_hashable(hashable_slice, allow_slice=True)
     assert inference.is_hashable(hashable_slice, allow_slice=False)
 
+    assert not inference.is_hashable(slice(1, 2))
+    assert not inference.is_hashable(slice(1, 2), allow_slice=True)
+    assert not inference.is_hashable(slice(1, 2), allow_slice=False)
+
     assert not inference.is_hashable(tuple_with_slice)
     assert not inference.is_hashable(tuple_with_slice, allow_slice=True)
     assert not inference.is_hashable(tuple_with_slice, allow_slice=False)
