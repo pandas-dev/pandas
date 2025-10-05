46171081df7738c6a86b709a78320e8a82d81b25
eicchen <eicchen02@gmail.com>
2025-10-05 18:34:43 -0500

    Removed type-casting shenanigans in array.py

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -906,11 +906,6 @@ class ArrowExtensionArray(
         ):
             if op in [operator.add, roperator.radd]:
                 sep = pa.scalar("", type=pa_type)
-                if not (is_scalar(other) or isinstance(other, pa.Scalar)):
-                    if len(other) == 0 or isna(other).any():
-                        other = other.cast(pa_type)
-                elif isna(other):
-                    other = other.cast(pa_type)
                 try:
                     if op is operator.add:
                         result = pc.binary_join_element_wise(self._pa_array, other, sep)
