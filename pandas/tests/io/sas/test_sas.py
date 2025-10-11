aaa4c1cf8dee57077c640ce4ba1ffa0a34250973
ashutosh2407 <adwagh24@gmail.com>
2025-10-11 15:21:52 -0400

    replaced ensure_clean with pytest fixture in test_sas.py

--- pandas/tests/io/sas/test_sas.py
+++ pandas/tests/io/sas/test_sas.py
@@ -4,6 +4,7 @@ import pytest
 
 from pandas import read_sas
 import pandas._testing as tm
+from pandas.conftest import temp_file
 
 
 class TestSas:
@@ -18,12 +19,11 @@ class TestSas:
         with pytest.raises(ValueError, match=msg):
             read_sas(b)
 
-    def test_sas_read_no_format_or_extension(self):
+    def test_sas_read_no_format_or_extension(self,temp_file):
         # see gh-24548
         msg = "unable to infer format of SAS file.+"
-        with tm.ensure_clean("test_file_no_extension") as path:
-            with pytest.raises(ValueError, match=msg):
-                read_sas(path)
+        with pytest.raises(ValueError, match=msg):
+            read_sas(temp_file)
 
 
 def test_sas_archive(datapath):
