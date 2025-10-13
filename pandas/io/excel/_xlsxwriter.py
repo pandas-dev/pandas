ea7e01cf3f6c3d6b4b5653ee3e50abe5e010f973
Yaswanth Kumar <155723049+VYaswanthKumar@users.noreply.github.com>
2025-10-13 12:39:55 +0530

    TST: Fix unused type:ignore comment in _xlsxwriter.py for CI checks

--- pandas/io/excel/_xlsxwriter.py
+++ pandas/io/excel/_xlsxwriter.py
@@ -1,17 +1,14 @@
 from __future__ import annotations
-
 import json
 from typing import (
     TYPE_CHECKING,
     Any,
 )
-
 from pandas.io.excel._base import ExcelWriter
 from pandas.io.excel._util import (
     combine_kwargs,
     validate_freeze_panes,
 )
-
 if TYPE_CHECKING:
     from pandas._typing import (
         ExcelWriterIfSheetExists,
@@ -19,8 +16,6 @@ if TYPE_CHECKING:
         StorageOptions,
         WriteExcelBuffer,
     )
-
-
 class _XlsxStyler:
     # Map from openpyxl-oriented styles to flatter xlsxwriter representation
     # Ordering necessary for both determinism and because some are keyed by
@@ -91,12 +86,10 @@ class _XlsxStyler:
             (("left",), "left"),
         ],
     }
-
     @classmethod
     def convert(cls, style_dict, num_format_str=None) -> dict[str, Any]:
         """
         converts a style_dict to an xlsxwriter format dict
-
         Parameters
         ----------
         style_dict : style dictionary to convert
@@ -104,17 +97,13 @@ class _XlsxStyler:
         """
         # Create a XlsxWriter format object.
         props = {}
-
         if num_format_str is not None:
             props["num_format"] = num_format_str
-
         if style_dict is None:
             return props
-
         if "borders" in style_dict:
             style_dict = style_dict.copy()
             style_dict["border"] = style_dict.pop("borders")
-
         for style_group_key, style_group in style_dict.items():
             for src, dst in cls.STYLE_MAPPING.get(style_group_key, []):
                 # src is a sequence of keys into a nested dict
@@ -129,11 +118,9 @@ class _XlsxStyler:
                         break
                 else:
                     props[dst] = v
-
         if isinstance(props.get("pattern"), str):
             # TODO: support other fill patterns
             props["pattern"] = 0 if props["pattern"] == "none" else 1
-
         for k in ["border", "top", "right", "bottom", "left"]:
             if isinstance(props.get(k), str):
                 try:
@@ -155,12 +142,10 @@ class _XlsxStyler:
                     ].index(props[k])
                 except ValueError:
                     props[k] = 2
-
         if isinstance(props.get("font_script"), str):
             props["font_script"] = ["baseline", "superscript", "subscript"].index(
                 props["font_script"]
             )
-
         if isinstance(props.get("underline"), str):
             props["underline"] = {
                 "none": 0,
@@ -169,18 +154,13 @@ class _XlsxStyler:
                 "singleAccounting": 33,
                 "doubleAccounting": 34,
             }[props["underline"]]
-
         # GH 30107 - xlsxwriter uses different name
         if props.get("valign") == "center":
             props["valign"] = "vcenter"
-
         return props
-
-
 class XlsxWriter(ExcelWriter):
     _engine = "xlsxwriter"
     _supported_extensions = (".xlsx",)
-
     def __init__(  # pyright: ignore[reportInconsistentConstructor]
         self,
         path: FilePath | WriteExcelBuffer | ExcelWriter,
@@ -195,12 +175,9 @@ class XlsxWriter(ExcelWriter):
     ) -> None:
         # Use the xlsxwriter module as the Excel writer.
         from xlsxwriter import Workbook
-
         engine_kwargs = combine_kwargs(engine_kwargs, kwargs)
-
         if mode == "a":
             raise ValueError("Append mode is not supported with xlsxwriter!")
-
         super().__init__(
             path,
             engine=engine,
@@ -211,33 +188,27 @@ class XlsxWriter(ExcelWriter):
             if_sheet_exists=if_sheet_exists,
             engine_kwargs=engine_kwargs,
         )
-
         try:
-            self._book = Workbook(self._handles.handle, **engine_kwargs)  # type: ignore[arg-type]
+            self._book = Workbook(self._handles.handle, **engine_kwargs)
         except TypeError:
             self._handles.handle.close()
             raise
-
     @property
     def book(self):
         """
         Book instance of class xlsxwriter.Workbook.
-
         This attribute can be used to access engine-specific features.
         """
         return self._book
-
     @property
     def sheets(self) -> dict[str, Any]:
         result = self.book.sheetnames
         return result
-
     def _save(self) -> None:
         """
         Save workbook to disk.
         """
         self.book.close()
-
     def _write_cells(
         self,
         cells,
@@ -248,29 +219,22 @@ class XlsxWriter(ExcelWriter):
     ) -> None:
         # Write the frame cells using xlsxwriter.
         sheet_name = self._get_sheet_name(sheet_name)
-
         wks = self.book.get_worksheet_by_name(sheet_name)
         if wks is None:
             wks = self.book.add_worksheet(sheet_name)
-
         style_dict = {"null": None}
-
         if validate_freeze_panes(freeze_panes):
             wks.freeze_panes(*(freeze_panes))
-
         for cell in cells:
             val, fmt = self._value_with_fmt(cell.val)
-
             stylekey = json.dumps(cell.style)
             if fmt:
                 stylekey += fmt
-
             if stylekey in style_dict:
                 style = style_dict[stylekey]
             else:
                 style = self.book.add_format(_XlsxStyler.convert(cell.style, fmt))
                 style_dict[stylekey] = style
-
             if cell.mergestart is not None and cell.mergeend is not None:
                 wks.merge_range(
                     startrow + cell.row,
