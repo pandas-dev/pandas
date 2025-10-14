7d552833f30e9c6026c44099b90bfd6c57653b9c
Álvaro Kothe <kothe65@gmail.com>
2025-10-14 11:51:24 -0300

    fix: go back to buffer solution

--- pandas/_libs/src/parser/tokenizer.c
+++ pandas/_libs/src/parser/tokenizer.c
@@ -21,14 +21,14 @@ GitHub. See Python Software Foundation License and BSD licenses for these.
 
 #include <ctype.h>
 #include <float.h>
-#include <limits.h>
 #include <math.h>
 #include <stdbool.h>
-#include <stddef.h>
 #include <stdlib.h>
 
 #include "pandas/portable.h"
 #include "pandas/vendored/klib/khash.h" // for kh_int64_t, kh_destroy_int64
+
+#define PROCESSED_WORD_CAPACITY 128
 
 void coliter_setup(coliter_t *self, parser_t *parser, int64_t i,
                    int64_t start) {
@@ -1876,26 +1876,39 @@ static inline bool has_only_spaces(const char *str) {
   return *str == '\0';
 }
 
-static int power_int(int base, int exponent) {
-  // https://en.wikipedia.org/wiki/Exponentiation_by_squaring
-  if (exponent == 0) {
-    return 1;
-  } else if (exponent < 0) {
-    return 0;
-  }
+/* Copy a string without `char_to_remove` into `output`,
+ * it assumes that output is filled with `\0`,
+ * so it won't null terminate the result.
+ */
+static void copy_string_without_char(char output[PROCESSED_WORD_CAPACITY],
+                                     const char *str, char char_to_remove) {
+  char *dst = output;
+  const char *src = str;
+  // last character is reserved for null terminator.
+  const char *end = output + PROCESSED_WORD_CAPACITY - 1;
 
-  int result = 1;
-
-  while (exponent > 1) {
-    if (exponent % 2 == 1) {
-      result *= base;
-      exponent--;
+  while (*src != '\0' && dst < end) {
+    const char *next = src;
+    // find EOS or char_to_remove
+    while (*next != '\0' && *next != char_to_remove) {
+      next++;
     }
-    result *= result;
-    exponent /= 2;
-  }
 
-  return result * base;
+    size_t len = next - src;
+    if (dst + len > end) {
+      // Can't write here, str is too big
+      errno = ERANGE;
+      return;
+    }
+
+    // copy block
+    memcpy(dst, src, len);
+
+    // go to next available location to write
+    dst += len;
+    // Move past char to remove
+    src = *next == char_to_remove ? next + 1 : next;
+  }
 }
 
 int64_t str_to_int64(const char *p_item, int64_t int_min, int64_t int_max,
@@ -1915,39 +1928,20 @@ int64_t str_to_int64(const char *p_item, int64_t int_min, int64_t int_max,
   }
 
   errno = 0;
+  if (tsep != '\0' && strchr(p_item, tsep) != NULL) {
+    char buffer[PROCESSED_WORD_CAPACITY];
+    memset(buffer, '\0', sizeof(buffer));
+    copy_string_without_char(buffer, p_item, tsep);
+    p_item = buffer;
+  }
+
+  if (errno == ERANGE) {
+    *error = ERROR_OVERFLOW;
+    return 0;
+  }
+
   char *endptr = NULL;
   int64_t result = strtoll(p_item, &endptr, 10);
-  bool is_negative = result < 0;
-
-  while (errno == 0 && tsep != '\0' && *endptr == tsep) {
-    // move after tsep
-    endptr++;
-    if (*endptr == '\0' || !isdigit_ascii(*endptr)) {
-      // stop parsing and let the remaining of the function
-      // assign an error code
-      break;
-    }
-
-    char *new_end;
-    int64_t next_part = strtoll(endptr, &new_end, 10);
-    if (is_negative) {
-      next_part = -next_part;
-    }
-
-    ptrdiff_t digits = new_end - endptr;
-    int64_t mul_result = power_int(10, (int)digits);
-    // result * mul_result
-    if (checked_int64_mul(result, mul_result, &result)) {
-      // overflow
-      errno = ERANGE;
-    }
-    // result + next_part
-    if (checked_int64_add(result, next_part, &result)) {
-      // overflow
-      errno = ERANGE;
-    }
-    endptr = new_end;
-  }
 
   if (!has_only_spaces(endptr)) {
     // Check first for invalid characters because we may
@@ -1990,36 +1984,20 @@ uint64_t str_to_uint64(uint_state *state, const char *p_item, int64_t int_max,
   }
 
   errno = 0;
+  if (tsep != '\0' && strchr(p_item, tsep) != NULL) {
+    char buffer[PROCESSED_WORD_CAPACITY];
+    memset(buffer, '\0', sizeof(buffer));
+    copy_string_without_char(buffer, p_item, tsep);
+    p_item = buffer;
+  }
+
+  if (errno == ERANGE) {
+    *error = ERROR_OVERFLOW;
+    return 0;
+  }
+
   char *endptr = NULL;
   uint64_t result = strtoull(p_item, &endptr, 10);
-
-  while (errno == 0 && tsep != '\0' && *endptr == tsep) {
-    // move after tsep
-    endptr++;
-    if (*endptr == '\0' || !isdigit_ascii(*endptr)) {
-      // stop parsing and let the remaining of the function
-      // assign an error code
-      break;
-    }
-
-    char *new_end;
-    uint64_t next_part = strtoull(endptr, &new_end, 10);
-    ptrdiff_t digits = new_end - endptr;
-    uint64_t mul_result = power_int(10, (int)digits);
-
-    // result * mul_result
-    if (checked_uint64_mul(result, mul_result, &result)) {
-      // overflow
-      errno = ERANGE;
-    }
-    // result + next_part
-    if (checked_uint64_add(result, next_part, &result)) {
-      // overflow
-      errno = ERANGE;
-    }
-
-    endptr = new_end;
-  }
 
   if (!has_only_spaces(endptr)) {
     *error = ERROR_INVALID_CHARS;
