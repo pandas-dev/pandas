06af5149c28618f1aa13149b592c89c8f278a516
jbrockmendel <jbrockmendel@gmail.com>
2025-10-16 14:13:58 -0700

    BUG: _cast_pointwise_result([decimal_na]) (#62639)

--- pandas/tests/extension/test_arrow.py
+++ pandas/tests/extension/test_arrow.py
@@ -3700,3 +3700,15 @@ def test_pow_with_all_na_float():
     result = s.pow(2)
     expected = pd.Series([pd.NA, pd.NA], dtype="float64[pyarrow]")
     tm.assert_series_equal(result, expected)
+
+
+def test_cast_pontwise_result_decimal_nan():
+    # GH#62522 we don't want to get back null[pyarrow] here
+    ser = pd.Series([], dtype="float64[pyarrow]")
+    arr = ser.array
+    item = Decimal("NaN")
+
+    result = arr._cast_pointwise_result([item])
+
+    pa_type = result.dtype.pyarrow_dtype
+    assert pa.types.is_decimal(pa_type)
