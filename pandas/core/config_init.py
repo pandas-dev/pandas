6661dd17a4f6fea6bd4f6ed51fde00a55c683257
eicchen <eicchen02@gmail.com>
2025-10-17 20:45:35 -0500

    Applied pre-commit fox

--- pandas/core/config_init.py
+++ pandas/core/config_init.py
@@ -909,6 +909,6 @@ with cf.config_prefix("future"):
         "(at which point this option will be deprecated).",
         validator=is_one_of_factory([True, False]),
     )
-    
+
 # GH#59502
 cf.deprecate_option("future.no_silent_downcasting", Pandas4Warning)
