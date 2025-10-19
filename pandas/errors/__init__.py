5814dd3f095759532551f2db6246b2f9fd7f2604
Lirong <lirong_ma@yahoo.com>
2025-10-19 17:23:13 +0000

    ruff

--- pandas/errors/__init__.py
+++ pandas/errors/__init__.py
@@ -1047,7 +1047,6 @@ class TimezoneDtypeMismatchError(ValueError):
         "cannot supply both a tz and a timezone-naive dtype (i.e. datetime64[ns])"
     """
 
-
 __all__ = [
     "AbstractMethodError",
     "AttributeConflictWarning",
