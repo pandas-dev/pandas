7401a368d5a8c6c8191b1151063c7521d8f8c3e3
Álvaro Kothe <kothe65@gmail.com>
2025-10-24 19:03:25 -0300

    test: rewrite test to allow better parametrization

--- pandas/tests/window/test_rolling.py
+++ pandas/tests/window/test_rolling.py
@@ -1438,17 +1438,25 @@ def test_rolling_skew_kurt_numerical_stability(method):
 
 
 @pytest.mark.parametrize(
-    ("method", "values"),
+    "method, data, values",
     [
-        ("skew", [2.0, 0.854563, 0.0, 1.999984]),
-        ("kurt", [4.0, -1.289256, -1.2, 3.999946]),
+        (
+            "skew",
+            [3000000, 1, 1, 2, 3, 4, 999],
+            [np.nan] * 3 + [2.0, 0.854563, 0.0, 1.999984],
+        ),
+        (
+            "kurt",
+            [3000000, 1, 1, 2, 3, 4, 999],
+            [np.nan] * 3 + [4.0, -1.289256, -1.2, 3.999946],
+        ),
     ],
 )
-def test_rolling_skew_kurt_large_value_range(method, values):
+def test_rolling_skew_kurt_large_value_range(method, data, values):
     # GH: 37557
-    s = Series([3000000, 1, 1, 2, 3, 4, 999])
+    s = Series(data)
     result = getattr(s.rolling(4), method)()
-    expected = Series([np.nan] * 3 + values)
+    expected = Series(values)
     tm.assert_series_equal(result, expected)
 
 
