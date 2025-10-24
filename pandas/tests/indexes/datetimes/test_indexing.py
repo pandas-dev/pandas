0d8182477cb8e1269b97df38cc2681345153b09b
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2025-10-24 19:33:15 +0530

    BUG: Remove special-casing for Python date objects in DatetimeIndex

--- pandas/tests/indexes/datetimes/test_indexing.py
+++ pandas/tests/indexes/datetimes/test_indexing.py
@@ -567,7 +567,7 @@ class TestGetIndexer:
         [
             (
                 [date(2020, 1, 1), Timestamp("2020-01-02")],
-                np.array([-1, -1], dtype=np.intp),
+                np.array([-1, 1], dtype=np.intp),
             ),
             (
                 [Timestamp("2020-01-01"), Timestamp(date(2020, 1, 2))],
@@ -584,7 +584,7 @@ class TestGetIndexer:
     @pytest.mark.parametrize(
         "target, positions",
         [
-            ([date(9999, 1, 1), Timestamp("2020-01-01")], [-1, -1]),
+            ([date(9999, 1, 1), Timestamp("2020-01-01")], [-1, 0]),
             ([Timestamp("2020-01-01"), date(9999, 1, 1)], [0, -1]),
             ([date(9999, 1, 1), date(9999, 1, 1)], [-1, -1]),
         ],
