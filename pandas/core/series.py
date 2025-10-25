03d71173ab587175229e6c507d7b9de1a1280522
Lonercode <penpalclassmate@gmail.com>
2025-10-25 11:26:59 +0100

    address CI issues

--- pandas/core/series.py
+++ pandas/core/series.py
@@ -6182,9 +6182,7 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
 
     def isnull(self) -> Series:
         """
-
         Series.isnull is an alias for Series.isna.
-
         Detect missing values.
 
         Return a boolean same-sized object indicating if the values are NA.
@@ -6325,9 +6323,7 @@ class Series(base.IndexOpsMixin, NDFrame):  # type: ignore[misc]
 
     def notnull(self) -> Series:
         """
-
         Series.notnull is an alias for Series.notna.
-
         Detect existing (non-missing) values.
 
         Return a boolean same-sized object indicating if the values are not NA.
