df4dafbefb6ba6c4edef1f91582c0e9dee208935
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-10-26 11:02:50 -0700

    Remove meta tests

--- pandas/tests/indexing/test_coercion.py
+++ pandas/tests/indexing/test_coercion.py
@@ -4,7 +4,6 @@ from datetime import (
     datetime,
     timedelta,
 )
-import itertools
 
 import numpy as np
 import pytest
@@ -21,36 +20,6 @@ import pandas._testing as tm
 ###############################################################
 # Index / Series common tests which may trigger dtype coercions
 ###############################################################
-
-
-@pytest.fixture(autouse=True, scope="class")
-def check_comprehensiveness(request):
-    # Iterate over combination of dtype, method and klass
-    # and ensure that each are contained within a collected test
-    cls = request.cls
-    combos = itertools.product(cls.klasses, cls.dtypes, [cls.method])
-
-    def has_test(combo):
-        klass, dtype, method = combo
-        cls_funcs = request.node.session.items
-        return any(
-            klass in x.name and dtype in x.name and method in x.name for x in cls_funcs
-        )
-
-    opts = request.config.option
-    if opts.lf or opts.keyword:
-        # If we are running with "last-failed" or -k foo, we expect to only
-        #  run a subset of tests.
-        yield
-
-    else:
-        for combo in combos:
-            if not has_test(combo):
-                raise AssertionError(
-                    f"test method is not defined: {cls.__name__}, {combo}"
-                )
-
-        yield
 
 
 class CoercionBase:
@@ -540,10 +509,6 @@ class TestFillnaSeriesCoercion(CoercionBase):
     # not indexing, but place here for consistency
 
     method = "fillna"
-
-    @pytest.mark.xfail(reason="Test not implemented")
-    def test_has_comprehensive_tests(self):
-        raise NotImplementedError
 
     def _assert_fillna_conversion(self, original, value, expected, expected_dtype):
         """test coercion triggered by fillna"""
