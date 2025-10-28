7f1d2a37cf777169ae638cf5860dedf31ddb02c1
Álvaro Kothe <kothe65@gmail.com>
2025-10-27 21:18:14 -0300

    test(rolling): undo test modifications for degenerate windows

--- pandas/tests/window/test_rolling.py
+++ pandas/tests/window/test_rolling.py
@@ -1843,17 +1843,16 @@ def test_rolling_mean_sum_floating_artifacts():
     assert (result[-3:] == 0).all()
 
 
-@pytest.mark.xfail(reason="Series.rolling.kurt computes -3 to degenerate distribution")
 def test_rolling_skew_kurt_floating_artifacts():
     # GH 42064 46431
 
     sr = Series([1 / 3, 4, 0, 0, 0, 0, 0])
     r = sr.rolling(4)
     result = r.skew()
-    expected = Series([np.nan, np.nan, np.nan, 1.9619045191072484, 2.0, np.nan, np.nan])
+    expected = Series([np.nan, np.nan, np.nan, 1.9619045191072484, 2.0, 0.0, 0.0])
     tm.assert_series_equal(result, expected)
     result = r.kurt()
-    expected = Series([np.nan, np.nan, np.nan, 3.8636048803878786, 4.0, np.nan, np.nan])
+    expected = Series([np.nan, np.nan, np.nan, 3.8636048803878786, 4.0, -3.0, -3.0])
     tm.assert_series_equal(result, expected)
 
 
