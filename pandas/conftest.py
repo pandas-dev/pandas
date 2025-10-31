53bb0b9adf0795a25f09b6dc93efdf10bd095e5a
Marco Gorelli <33491632+MarcoGorelli@users.noreply.github.com>
2025-10-31 11:33:54 +0000

    from_dataframe doctest

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -135,6 +135,7 @@ def pytest_collection_modifyitems(items, config) -> None:
     # Warnings from doctests that can be ignored; place reason in comment above.
     # Each entry specifies (path, message) - see the ignore_doctest_warning function
     ignored_doctest_warnings = [
+        ("api.interchange.from_dataframe", ".*Interchange Protocol is deprecated"),
         ("is_int64_dtype", "is_int64_dtype is deprecated"),
         ("is_interval_dtype", "is_interval_dtype is deprecated"),
         ("is_period_dtype", "is_period_dtype is deprecated"),
