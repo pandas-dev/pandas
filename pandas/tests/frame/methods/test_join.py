fa6d1fd0ea93353d55b5dc5caa64a92b2df23712
Matias Lindgren <matias.lindgren@iki.fi>
2025-10-31 20:44:11 -0400

    cleanup test

--- pandas/tests/frame/methods/test_join.py
+++ pandas/tests/frame/methods/test_join.py
@@ -579,37 +579,23 @@ class TestDataFrameJoin:
     def test_frame_join_categorical_index(self):
         # GH 61675
         cat_data = pd.Categorical(
-            [15, 16, 17, 18],
-            categories=pd.Series(list(range(3, 24)), dtype="Int64"),
+            [3, 4],
+            categories=pd.Series([2, 3, 4, 5], dtype="Int64"),
             ordered=True,
         )
-        values1 = "a b c d".split()
-        values2 = "xyzzy foo bar ...".split()
+        values1 = "a b".split()
+        values2 = "foo bar".split()
         df1 = DataFrame({"hr": cat_data, "values1": values1}).set_index("hr")
         df2 = DataFrame({"hr": cat_data, "values2": values2}).set_index("hr")
         df1.columns = pd.CategoricalIndex([4], dtype=cat_data.dtype, name="other_hr")
         df2.columns = pd.CategoricalIndex([3], dtype=cat_data.dtype, name="other_hr")
 
-        df_joined_1 = (
-            df1.reset_index(level="hr")
-            .merge(df2.reset_index(level="hr"), on="hr")
-            .set_index("hr")
-        )
-        expected1 = DataFrame(
+        df_joined = df1.join(df2)
+        expected = DataFrame(
             {"hr": cat_data, "values1": values1, "values2": values2}
         ).set_index("hr")
-        expected1.columns = Index([4, 3], dtype="object", name="other_hr")
-
-        tm.assert_frame_equal(df_joined_1, expected1)
-
-        df_joined_2 = df1.join(df2)
-        expected2 = DataFrame(
-            {"hr": cat_data, "values1": values1, "values2": values2}
-        ).set_index("hr")
-        expected2.columns = pd.CategoricalIndex(
+        expected.columns = pd.CategoricalIndex(
             [4, 3], dtype=cat_data.dtype, name="other_hr"
         )
 
-        tm.assert_frame_equal(df_joined_2, expected2)
-
-        assert df_joined_1.equals(df_joined_2)
+        tm.assert_frame_equal(df_joined, expected)
