8556052e1ff3cf05564b4837605cab2244275539
Austin Holcomb <holcomau@oregonstate.edu>
2025-11-02 10:43:11 -0600

    STY: Apply ruff formatting

--- pandas/core/arrays/masked.py
+++ pandas/core/arrays/masked.py
@@ -170,13 +170,13 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
     @classmethod
     def _empty(cls, shape: Shape, dtype: ExtensionDtype) -> Self:
         """
-         Create an ExtensionArray with the given shape and dtype.
+        Create an ExtensionArray with the given shape and dtype.
 
-         See also
-         --------
-         ExtensionDtype.empty
-             ExtensionDtype.empty is the 'official' public version of this API.
-         """
+        See also
+        --------
+        ExtensionDtype.empty
+            ExtensionDtype.empty is the 'official' public version of this API.
+        """
         dtype = cast(BaseMaskedDtype, dtype)
         values: np.ndarray = np.empty(shape, dtype=dtype.type)
         values.fill(dtype._internal_fill_value)
@@ -260,42 +260,42 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
 
     def fillna(self, value, limit: int | None = None, copy: bool = True) -> Self:
         """
-           Fill NA/NaN values using the specified method.
+        Fill NA/NaN values using the specified method.
 
-           Parameters
-           ----------
-           value : scalar, array-like
-               If a scalar value is passed it is used to fill all missing values.
-               Alternatively, an array-like "value" can be given. It's expected
-               that the array-like have the same length as 'self'.
-           limit : int, default None
-               The maximum number of entries where NA values will be filled.
-           copy : bool, default True
-               Whether to make a copy of the data before filling. If False, then
-               the original should be modified and no new memory should be allocated.
-               For ExtensionArray subclasses that cannot do this, it is at the
-               author's discretion whether to ignore "copy=False" or to raise.
+        Parameters
+        ----------
+        value : scalar, array-like
+            If a scalar value is passed it is used to fill all missing values.
+            Alternatively, an array-like "value" can be given. It's expected
+            that the array-like have the same length as 'self'.
+        limit : int, default None
+            The maximum number of entries where NA values will be filled.
+        copy : bool, default True
+            Whether to make a copy of the data before filling. If False, then
+            the original should be modified and no new memory should be allocated.
+            For ExtensionArray subclasses that cannot do this, it is at the
+            author's discretion whether to ignore "copy=False" or to raise.
 
-           Returns
-           -------
-           ExtensionArray
-               With NA/NaN filled.
+        Returns
+        -------
+        ExtensionArray
+            With NA/NaN filled.
 
-           See Also
-           --------
-           api.extensions.ExtensionArray.dropna : Return ExtensionArray without
-               NA values.
-           api.extensions.ExtensionArray.isna : A 1-D array indicating if
-               each value is missing.
+        See Also
+        --------
+        api.extensions.ExtensionArray.dropna : Return ExtensionArray without
+            NA values.
+        api.extensions.ExtensionArray.isna : A 1-D array indicating if
+            each value is missing.
 
-           Examples
-           --------
-           >>> arr = pd.array([np.nan, np.nan, 2, 3, np.nan, np.nan])
-           >>> arr.fillna(0)
-           <IntegerArray>
-           [0, 0, 2, 3, 0, 0]
-           Length: 6, dtype: Int64
-           """
+        Examples
+        --------
+        >>> arr = pd.array([np.nan, np.nan, 2, 3, np.nan, np.nan])
+        >>> arr.fillna(0)
+        <IntegerArray>
+        [0, 0, 2, 3, 0, 0]
+        Length: 6, dtype: Int64
+        """
         mask = self._mask
         if limit is not None and limit < len(self):
             modify = mask.cumsum() > limit
@@ -592,28 +592,28 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
 
     def tolist(self) -> list:
         """
-         Return a list of the values.
+        Return a list of the values.
 
-         These are each a scalar type, which is a Python scalar
-         (for str, int, float) or a pandas scalar
-         (for Timestamp/Timedelta/Interval/Period)
+        These are each a scalar type, which is a Python scalar
+        (for str, int, float) or a pandas scalar
+        (for Timestamp/Timedelta/Interval/Period)
 
-         Returns
-         -------
-         list
-             Python list of values in array.
+        Returns
+        -------
+        list
+            Python list of values in array.
 
-         See Also
-         --------
-         Index.to_list: Return a list of the values in the Index.
-         Series.to_list: Return a list of the values in the Series.
+        See Also
+        --------
+        Index.to_list: Return a list of the values in the Index.
+        Series.to_list: Return a list of the values in the Series.
 
-         Examples
-         --------
-         >>> arr = pd.array([1, 2, 3])
-         >>> arr.tolist()
-         [1, 2, 3]
-         """
+        Examples
+        --------
+        >>> arr = pd.array([1, 2, 3])
+        >>> arr.tolist()
+        [1, 2, 3]
+        """
         if self.ndim > 1:
             return [x.tolist() for x in self]
         dtype = None if self._hasna else self._data.dtype
@@ -1143,33 +1143,33 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
         self, keep: Literal["first", "last", False] = "first"
     ) -> npt.NDArray[np.bool_]:
         """
-          Return boolean ndarray denoting duplicate values.
+        Return boolean ndarray denoting duplicate values.
 
-          Parameters
-          ----------
-          keep : {'first', 'last', False}, default 'first'
-              - ``first`` : Mark duplicates as ``True`` except for the first occurrence.
-              - ``last`` : Mark duplicates as ``True`` except for the last occurrence.
-              - False : Mark all duplicates as ``True``.
+        Parameters
+        ----------
+        keep : {'first', 'last', False}, default 'first'
+            - ``first`` : Mark duplicates as ``True`` except for the first occurrence.
+            - ``last`` : Mark duplicates as ``True`` except for the last occurrence.
+            - False : Mark all duplicates as ``True``.
 
-          Returns
-          -------
-          ndarray[bool]
-              With true in indices where elements are duplicated and false otherwise.
+        Returns
+        -------
+        ndarray[bool]
+            With true in indices where elements are duplicated and false otherwise.
 
-          See Also
-          --------
-          DataFrame.duplicated : Return boolean Series denoting
-              duplicate rows.
-          Series.duplicated : Indicate duplicate Series values.
-          api.extensions.ExtensionArray.unique : Compute the ExtensionArray
-              of unique values.
+        See Also
+        --------
+        DataFrame.duplicated : Return boolean Series denoting
+            duplicate rows.
+        Series.duplicated : Indicate duplicate Series values.
+        api.extensions.ExtensionArray.unique : Compute the ExtensionArray
+            of unique values.
 
-          Examples
-          --------
-          >>> pd.array([1, 1, 2, 3, 3], dtype="Int64").duplicated()
-          array([False,  True, False, False,  True])
-          """
+        Examples
+        --------
+        >>> pd.array([1, 1, 2, 3, 3], dtype="Int64").duplicated()
+        array([False,  True, False, False,  True])
+        """
         values = self._data
         mask = self._mask
         return algos.duplicated(values, keep=keep, mask=mask)
@@ -1192,49 +1192,49 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
         sorter: NumpySorter | None = None,
     ) -> npt.NDArray[np.intp] | np.intp:
         """
-          Find indices where elements should be inserted to maintain order.
+        Find indices where elements should be inserted to maintain order.
 
-          Find the indices into a sorted array `self` (a) such that, if the
-          corresponding elements in `value` were inserted before the indices,
-          the order of `self` would be preserved.
+        Find the indices into a sorted array `self` (a) such that, if the
+        corresponding elements in `value` were inserted before the indices,
+        the order of `self` would be preserved.
 
-          Assuming that `self` is sorted:
+        Assuming that `self` is sorted:
 
-          ======  ================================
-          `side`  returned index `i` satisfies
-          ======  ================================
-          left    ``self[i-1] < value <= self[i]``
-          right   ``self[i-1] <= value < self[i]``
-          ======  ================================
+        ======  ================================
+        `side`  returned index `i` satisfies
+        ======  ================================
+        left    ``self[i-1] < value <= self[i]``
+        right   ``self[i-1] <= value < self[i]``
+        ======  ================================
 
-          Parameters
-          ----------
-          value : array-like, list or scalar
-              Value(s) to insert into `self`.
-          side : {'left', 'right'}, optional
-              If 'left', the index of the first suitable location found is given.
-              If 'right', return the last such index.  If there is no suitable
-              index, return either 0 or N (where N is the length of `self`).
-          sorter : 1-D array-like, optional
-              Optional array of integer indices that sort array a into ascending
-              order. They are typically the result of argsort.
+        Parameters
+        ----------
+        value : array-like, list or scalar
+            Value(s) to insert into `self`.
+        side : {'left', 'right'}, optional
+            If 'left', the index of the first suitable location found is given.
+            If 'right', return the last such index.  If there is no suitable
+            index, return either 0 or N (where N is the length of `self`).
+        sorter : 1-D array-like, optional
+            Optional array of integer indices that sort array a into ascending
+            order. They are typically the result of argsort.
 
-          Returns
-          -------
-          array of ints or int
-              If value is array-like, array of insertion points.
-              If value is scalar, a single integer.
+        Returns
+        -------
+        array of ints or int
+            If value is array-like, array of insertion points.
+            If value is scalar, a single integer.
 
-          See Also
-          --------
-          numpy.searchsorted : Similar method from NumPy.
+        See Also
+        --------
+        numpy.searchsorted : Similar method from NumPy.
 
-          Examples
-          --------
-          >>> arr = pd.array([1, 2, 3, 5])
-          >>> arr.searchsorted([4])
-          array([3])
-          """
+        Examples
+        --------
+        >>> arr = pd.array([1, 2, 3, 5])
+        >>> arr.searchsorted([4])
+        array([3])
+        """
         if self._hasna:
             raise ValueError(
                 "searchsorted requires array to be sorted, which is impossible "
@@ -1250,51 +1250,51 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
         use_na_sentinel: bool = True,
     ) -> tuple[np.ndarray, ExtensionArray]:
         """
-          Encode the extension array as an enumerated type.
+        Encode the extension array as an enumerated type.
 
-          Parameters
-          ----------
-          use_na_sentinel : bool, default True
-              If True, the sentinel -1 will be used for NaN values. If False,
-              NaN values will be encoded as non-negative integers and will not drop the
-              NaN from the uniques of the values.
+        Parameters
+        ----------
+        use_na_sentinel : bool, default True
+            If True, the sentinel -1 will be used for NaN values. If False,
+            NaN values will be encoded as non-negative integers and will not drop the
+            NaN from the uniques of the values.
 
-              .. versionadded:: 1.5.0
+            .. versionadded:: 1.5.0
 
-          Returns
-          -------
-          codes : ndarray
-              An integer NumPy array that's an indexer into the original
-              ExtensionArray.
-          uniques : ExtensionArray
-              An ExtensionArray containing the unique values of `self`.
+        Returns
+        -------
+        codes : ndarray
+            An integer NumPy array that's an indexer into the original
+            ExtensionArray.
+        uniques : ExtensionArray
+            An ExtensionArray containing the unique values of `self`.
 
-              .. note::
+            .. note::
 
-                 uniques will *not* contain an entry for the NA value of
-                 the ExtensionArray if there are any missing values present
-                 in `self`.
+               uniques will *not* contain an entry for the NA value of
+               the ExtensionArray if there are any missing values present
+               in `self`.
 
-          See Also
-          --------
-          factorize : Top-level factorize method that dispatches here.
+        See Also
+        --------
+        factorize : Top-level factorize method that dispatches here.
 
-          Notes
-          -----
-          :meth:`pandas.factorize` offers a `sort` keyword as well.
+        Notes
+        -----
+        :meth:`pandas.factorize` offers a `sort` keyword as well.
 
-          Examples
-          --------
-          >>> idx1 = pd.PeriodIndex(
-          ...     ["2014-01", "2014-01", "2014-02", "2014-02", "2014-03", "2014-03"],
-          ...     freq="M",
-          ... )
-          >>> arr, idx = idx1.factorize()
-          >>> arr
-          array([0, 0, 1, 1, 2, 2])
-          >>> idx
-          PeriodIndex(['2014-01', '2014-02', '2014-03'], dtype='period[M]')
-          """
+        Examples
+        --------
+        >>> idx1 = pd.PeriodIndex(
+        ...     ["2014-01", "2014-01", "2014-02", "2014-02", "2014-03", "2014-03"],
+        ...     freq="M",
+        ... )
+        >>> arr, idx = idx1.factorize()
+        >>> arr
+        array([0, 0, 1, 1, 2, 2])
+        >>> idx
+        PeriodIndex(['2014-01', '2014-02', '2014-03'], dtype='period[M]')
+        """
         arr = self._data
         mask = self._mask
 
@@ -1329,36 +1329,36 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
 
     def _values_for_argsort(self) -> np.ndarray:
         """
-          Return values for sorting.
+        Return values for sorting.
 
-          Returns
-          -------
-          ndarray
-              The transformed values should maintain the ordering between values
-              within the array.
+        Returns
+        -------
+        ndarray
+            The transformed values should maintain the ordering between values
+            within the array.
 
-          See Also
-          --------
-          ExtensionArray.argsort : Return the indices that would sort this array.
+        See Also
+        --------
+        ExtensionArray.argsort : Return the indices that would sort this array.
 
-          Notes
-          -----
-          The caller is responsible for *not* modifying these values in-place, so
-          it is safe for implementers to give views on ``self``.
+        Notes
+        -----
+        The caller is responsible for *not* modifying these values in-place, so
+        it is safe for implementers to give views on ``self``.
 
-          Functions that use this (e.g. ``ExtensionArray.argsort``) should ignore
-          entries with missing values in the original array (according to
-          ``self.isna()``). This means that the corresponding entries in the returned
-          array don't need to be modified to sort correctly.
+        Functions that use this (e.g. ``ExtensionArray.argsort``) should ignore
+        entries with missing values in the original array (according to
+        ``self.isna()``). This means that the corresponding entries in the returned
+        array don't need to be modified to sort correctly.
 
-          Examples
-          --------
-          In most cases, this is the underlying Numpy array of the ``ExtensionArray``:
+        Examples
+        --------
+        In most cases, this is the underlying Numpy array of the ``ExtensionArray``:
 
-          >>> arr = pd.array([1, 2, 3])
-          >>> arr._values_for_argsort()
-          array([1, 2, 3])
-          """
+        >>> arr = pd.array([1, 2, 3])
+        >>> arr._values_for_argsort()
+        array([1, 2, 3])
+        """
         return self._data
 
     def value_counts(self, dropna: bool = True) -> Series:
@@ -1409,40 +1409,40 @@ class BaseMaskedArray(OpsMixin, ExtensionArray):
 
     def equals(self, other) -> bool:
         """
-         Return if another array is equivalent to this array.
+        Return if another array is equivalent to this array.
 
-         Equivalent means that both arrays have the same shape and dtype, and
-         all values compare equal. Missing values in the same location are
-         considered equal (in contrast with normal equality).
+        Equivalent means that both arrays have the same shape and dtype, and
+        all values compare equal. Missing values in the same location are
+        considered equal (in contrast with normal equality).
 
-         Parameters
-         ----------
-         other : ExtensionArray
-             Array to compare to this Array.
+        Parameters
+        ----------
+        other : ExtensionArray
+            Array to compare to this Array.
 
-         Returns
-         -------
-         boolean
-             Whether the arrays are equivalent.
+        Returns
+        -------
+        boolean
+            Whether the arrays are equivalent.
 
-         See Also
-         --------
-         numpy.array_equal : Equivalent method for numpy array.
-         Series.equals : Equivalent method for Series.
-         DataFrame.equals : Equivalent method for DataFrame.
+        See Also
+        --------
+        numpy.array_equal : Equivalent method for numpy array.
+        Series.equals : Equivalent method for Series.
+        DataFrame.equals : Equivalent method for DataFrame.
 
-         Examples
-         --------
-         >>> arr1 = pd.array([1, 2, np.nan])
-         >>> arr2 = pd.array([1, 2, np.nan])
-         >>> arr1.equals(arr2)
-         True
+        Examples
+        --------
+        >>> arr1 = pd.array([1, 2, np.nan])
+        >>> arr2 = pd.array([1, 2, np.nan])
+        >>> arr1.equals(arr2)
+        True
 
-         >>> arr1 = pd.array([1, 3, np.nan])
-         >>> arr2 = pd.array([1, 2, np.nan])
-         >>> arr1.equals(arr2)
-         False
-         """
+        >>> arr1 = pd.array([1, 3, np.nan])
+        >>> arr2 = pd.array([1, 2, np.nan])
+        >>> arr1.equals(arr2)
+        False
+        """
         if type(self) != type(other):
             return False
         if other.dtype != self.dtype:
