251d809964ee08400b089905afd6c5c204e12d4e
SebastianGoldmann <112079303+SebastianGoldmann@users.noreply.github.com>
2025-11-02 20:15:05 +0100

    Apply suggestion from @Alvaro-Kothe
    Co-authored-by: Álvaro Kothe <kothe65@gmail.com>

--- pandas/tests/window/test_rolling_skew_kurt.py
+++ pandas/tests/window/test_rolling_skew_kurt.py
@@ -219,6 +219,5 @@ def test_rolling_skew_eq_value_fperr(step):
 
 def test_rolling_kurt_eq_value_fperr(step):
     # #18804 all rolling kurt for all equal values should return Nan
-    # #46717 update: all equal values should return -3 instead of NaN
     a = Series([1.1] * 15).rolling(window=10, step=step).kurt()
     assert a.isna().all()
