96b0317e7d9c47a9ed0c58cd3d674d75ac6ab621
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-11-06 13:35:13 -0800

    TST: Reduce deep copies in the test suite (#62970)

--- pandas/tests/copy_view/test_methods.py
+++ pandas/tests/copy_view/test_methods.py
@@ -560,7 +560,7 @@ def test_to_frame():
     ],
     ids=["shallow-copy", "reset_index", "rename", "select_dtypes"],
 )
-def test_chained_methods(request, method, idx):
+def test_chained_methods(method, idx):
     df = DataFrame({"a": [1, 2, 3], "b": [4, 5, 6], "c": [0.1, 0.2, 0.3]})
     df_orig = df.copy()
 
