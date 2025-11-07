06e695469130a4fb4d7f6abbe7cbe861631d6b26
Lonercode <penpalclassmate@gmail.com>
2025-11-07 18:56:37 +0100

    BUG: fix for pd.json_normalize on non string keys

--- pandas/io/json/_normalize.py
+++ pandas/io/json/_normalize.py
@@ -552,7 +552,7 @@ def json_normalize(
     lengths = []
 
     meta_vals: DefaultDict = defaultdict(list)
-    meta_keys = [sep.join(val) for val in _meta]
+    meta_keys = [sep.join(map(str, val)) for val in _meta]
 
     def _recursive_extract(data, path, seen_meta, level: int = 0) -> None:
         if isinstance(data, dict):
