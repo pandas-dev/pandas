389655c43466bbb3c1dcbf3758b5fa8dc2ebeec3
jad <63060784+jadosh123@users.noreply.github.com>
2025-11-19 23:50:34 +0200

    TST: Add regression test for groupby().apply() external mutation (#63126)

--- pandas/tests/groupby/test_apply.py
+++ pandas/tests/groupby/test_apply.py
@@ -1516,3 +1516,28 @@ def test_nonreducer_nonstransform():
     ).set_index(["cat1", "cat2"])["rank"]
     result = df.groupby("cat1").apply(f)
     tm.assert_series_equal(result, expected)
+
+
+def test_groupby_apply_store_copy():
+    # GH40673
+    rng = np.random.default_rng(seed=42)
+
+    df = DataFrame(
+        {
+            "A": rng.normal(10, 12, size=(4,)),
+            "B": [1, 2, 1, 2],
+        }
+    )
+
+    store = {}
+
+    def addstore(x):
+        store[len(store)] = x.copy()
+
+    df.groupby("B").apply(addstore)
+
+    expected_out_0 = df.iloc[[0, 2], [0]]
+    expected_out_1 = df.iloc[[1, 3], [0]]
+
+    tm.assert_frame_equal(store[0], expected_out_0)
+    tm.assert_frame_equal(store[1], expected_out_1)
