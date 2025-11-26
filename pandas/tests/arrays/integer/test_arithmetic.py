5fb8a76f66dd8153cf3c59130cd88c00d13f95e7
jbrockmendel <jbrockmendel@gmail.com>
2025-11-26 11:23:48 -0800

    API: consistent sanitize_array-wrapping for lists in arithmetic ops (#62552)

--- pandas/tests/arrays/integer/test_arithmetic.py
+++ pandas/tests/arrays/integer/test_arithmetic.py
@@ -201,10 +201,6 @@ def test_error_invalid_values(data, all_arithmetic_operators):
     ]:  # (data[~data.isna()] >= 0).all():
         res = ops(str_ser)
         expected = pd.Series(["foo" * x for x in data], index=s.index)
-        expected = expected.fillna(np.nan)
-        # TODO: doing this fillna to keep tests passing as we make
-        #  assert_almost_equal stricter, but the expected with pd.NA seems
-        #  more-correct than np.nan here.
         tm.assert_series_equal(res, expected)
     else:
         with tm.external_error_raised(TypeError):
