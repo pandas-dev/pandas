afd8a601e9264dfebba423a158e9b69bb3d63e86
cloudboat <15851404+cloudboat111@user.noreply.gitee.com>
2025-11-29 16:11:23 +0800

    Fix accuracy in specific place

--- pandas/core/indexes/datetimelike.py
+++ pandas/core/indexes/datetimelike.py
@@ -129,7 +129,7 @@ class DatetimeIndexOpsMixin(NDArrayBackedExtensionIndex, ABC):
         >>> tdelta_idx = pd.to_timedelta([1, 2, 3], unit="D")
         >>> tdelta_idx
         TimedeltaIndex(['1 days', '2 days', '3 days'],
-                        dtype='timedelta64[us]', freq=None)
+                        dtype='timedelta64[ns]', freq=None)
         >>> tdelta_idx.mean()
         Timedelta('2 days 00:00:00')
         """
