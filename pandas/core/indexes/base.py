8b77ed20b489733472c1b9e63dcda6986d7bda16
Aniket Singh Yadav <singhyadavaniket43@gmail.com>
2025-12-03 14:20:21 +0530

    BUG: Remove special-casing for Python date objects in DatetimeIndex

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -1,9 +1,7 @@
 from __future__ import annotations
 
 from collections import abc
-from datetime import (
-    datetime,
-)
+from datetime import datetime
 import functools
 from itertools import zip_longest
 import operator
@@ -6305,13 +6303,25 @@ class Index(IndexOpsMixin, PandasObject):
             return dtype.kind == "b"
         elif is_numeric_dtype(self.dtype):
             return is_numeric_dtype(dtype)
-        # GH#62158
         elif isinstance(dtype, ArrowDtype):
+            # GH#62158
             import pyarrow as pa
 
             pa_dtype = dtype.pyarrow_dtype
             if dtype.kind != "M":
                 if self.dtype.kind == "m" and pa.types.is_duration(pa_dtype):
+                    return True
+                if is_string_dtype(self.dtype) and (
+                    pa.types.is_string(pa_dtype) or pa.types.is_large_string(pa_dtype)
+                ):
+                    return True
+                if self.dtype.kind == "b" and pa.types.is_boolean(pa_dtype):
+                    return True
+                if is_numeric_dtype(self.dtype) and (
+                    pa.types.is_integer(pa_dtype) or pa.types.is_floating(pa_dtype)
+                ):
+                    return True
+                if is_object_dtype(self.dtype):
                     return True
                 return False
             if self.dtype.kind != "M":
