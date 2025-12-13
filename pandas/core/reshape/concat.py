f29a3dc9a1e60cf3b3c2f479058fc1e7fb300ed7
Unkwn1 <daniel.shevchuk1@gmail.com>
2025-12-13 04:00:20 -0600

    STY: add strict parameter to changes from issue 62843

--- pandas/core/reshape/concat.py
+++ pandas/core/reshape/concat.py
@@ -418,12 +418,15 @@ def concat(
             intersect
             or any(not isinstance(index, DatetimeIndex) for index in non_concat_axis)
             or all(
-                prev is curr for prev, curr in zip(non_concat_axis, non_concat_axis[1:])
+                prev is curr
+                for prev, curr in zip(non_concat_axis, non_concat_axis[1:], strict=True)
             )
             or (
                 all(
                     prev[-1] <= curr[0] and prev.is_monotonic_increasing
-                    for prev, curr in zip(non_concat_axis, non_concat_axis[1:])
+                    for prev, curr in zip(
+                        non_concat_axis, non_concat_axis[1:], strict=True
+                    )
                     if not prev.empty and not curr.empty
                 )
                 and non_concat_axis[-1].is_monotonic_increasing
