657d9f20acce5703c166a7be98ad20e8e30a5943
zhangbowen-coder <2439796518@qq.com>
2025-12-14 18:52:12 +0800

    replaced all Appender、Substitution、doc decorators in pandas/pandas/core/frame.py.

--- pandas/tests/base/test_misc.py
+++ pandas/tests/base/test_misc.py
@@ -19,9 +19,13 @@ from pandas import (
 def test_isnull_notnull_docstrings():
     # GH#41855 make sure its clear these are aliases
     doc = pd.DataFrame.notnull.__doc__
-    assert doc.startswith("\nDataFrame.notnull is an alias for DataFrame.notna.\n")
+    assert doc.startswith(
+        "\n        DataFrame.notnull is an alias for DataFrame.notna.\n"
+    )
     doc = pd.DataFrame.isnull.__doc__
-    assert doc.startswith("\nDataFrame.isnull is an alias for DataFrame.isna.\n")
+    assert doc.startswith(
+        "\n        DataFrame.isnull is an alias for DataFrame.isna.\n"
+    )
 
     doc = Series.notnull.__doc__
     assert doc.startswith("\nSeries.notnull is an alias for Series.notna.\n")
