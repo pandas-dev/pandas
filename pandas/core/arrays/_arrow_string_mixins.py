2f302d4184b4f30cdea7063de9a25d982295a9a7
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2025-12-18 09:44:05 +0100

    return shallow copy

--- pandas/core/arrays/_arrow_string_mixins.py
+++ pandas/core/arrays/_arrow_string_mixins.py
@@ -204,7 +204,7 @@ class ArrowStringArrayMixin:
 
     def _str_removeprefix(self, prefix: str):
         if prefix == "":
-            return self
+            return self.copy()
         starts_with = pc.starts_with(self._pa_array, pattern=prefix)
         removed = pc.utf8_slice_codeunits(self._pa_array, len(prefix))
         result = pc.if_else(starts_with, removed, self._pa_array)
@@ -212,7 +212,7 @@ class ArrowStringArrayMixin:
 
     def _str_removesuffix(self, suffix: str):
         if suffix == "":
-            return self
+            return self.copy()
         ends_with = pc.ends_with(self._pa_array, pattern=suffix)
         removed = pc.utf8_slice_codeunits(self._pa_array, 0, stop=-len(suffix))
         result = pc.if_else(ends_with, removed, self._pa_array)
