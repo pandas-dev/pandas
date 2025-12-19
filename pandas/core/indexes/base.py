05f2c1e7eab61b94a03d00ae872d008ad948ed41
Denis Karadaş <54038084+deniskrds@users.noreply.github.com>
2025-12-19 19:03:09 +0300

    BUG: ensure to always return new objects in Index set operations (avoid metadata mutation) (#63174)

--- pandas/core/indexes/base.py
+++ pandas/core/indexes/base.py
@@ -2984,13 +2984,12 @@ class Index(IndexOpsMixin, PandasObject):
     def _get_reconciled_name_object(self, other):
         """
         If the result of a set operation will be self,
-        return self, unless the name changes, in which
-        case make a shallow copy of self.
+        return a shallow copy of self.
         """
         name = get_op_result_name(self, other)
         if self.name is not name:
             return self.rename(name)
-        return self
+        return self.copy(deep=False)
 
     @final
     def _validate_sort_keyword(self, sort) -> None:
