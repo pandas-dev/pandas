0183c73990684b711d33dea1d9878fc701f2a45f
Richard Shadrach <45562402+rhshadrach@users.noreply.github.com>
2025-12-20 15:27:18 -0500

    TST: Use datapath in tests (#63440)

--- pandas/tests/io/test_common.py
+++ pandas/tests/io/test_common.py
@@ -641,9 +641,9 @@ def test_close_on_error():
 
 @td.skip_if_no("fsspec")
 @pytest.mark.parametrize("compression", [None, "infer"])
-def test_read_csv_chained_url_no_error(compression):
+def test_read_csv_chained_url_no_error(datapath, compression):
     # GH 60100
-    tar_file_path = "pandas/tests/io/data/tar/test-csv.tar"
+    tar_file_path = datapath("io", "data", "tar", "test-csv.tar")
     chained_file_url = f"tar://test.csv::file://{tar_file_path}"
 
     result = pd.read_csv(chained_file_url, compression=compression, sep=";")
