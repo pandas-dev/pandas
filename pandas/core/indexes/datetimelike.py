cc2668bc37f490455791815388292477d22f9815
Antriksh006 <antrikshg007@gmail.com>
2025-12-23 13:55:50 +0530

    .

--- pandas/core/indexes/datetimelike.py
+++ pandas/core/indexes/datetimelike.py
@@ -246,7 +246,7 @@ class DatetimeIndexOpsMixin(NDArrayBackedExtensionIndex, ABC):
         if not isinstance(other, Index):
             return False
 
-        if len(self)!=len(other):
+        if len(self) != len(other):
             return False
 
         self_unit = getattr(self, "unit", None)
@@ -260,7 +260,7 @@ class DatetimeIndexOpsMixin(NDArrayBackedExtensionIndex, ABC):
                 return False
         if other.dtype.kind in "iufc":
             return False
-        
+
         elif not isinstance(other, type(self)):
             should_try = False
             inferable = self._data._infer_matches
