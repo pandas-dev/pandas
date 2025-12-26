e53a59b3c5f9acbeb669fe258bc42f31ddbae947
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-12-26 13:54:29 -0800

    Remove ensure_clean_store

--- pandas/tests/io/pytables/common.py
+++ pandas/tests/io/pytables/common.py
@@ -1,11 +1,4 @@
-from collections.abc import Generator
-from contextlib import contextmanager
-import pathlib
-import tempfile
-
 import pytest
-
-from pandas.io.pytables import HDFStore
 
 tables = pytest.importorskip("tables")
 # set these parameters so we don't have file sharing
@@ -20,23 +13,6 @@ def safe_close(store):
             store.close()
     except OSError:
         pass
-
-
-# contextmanager to ensure the file cleanup
-@contextmanager
-def ensure_clean_store(
-    path, mode="a", complevel=None, complib=None, fletcher32=False
-) -> Generator[HDFStore, None, None]:
-    with tempfile.TemporaryDirectory() as tmpdirname:
-        tmp_path = pathlib.Path(tmpdirname, path)
-        with HDFStore(
-            tmp_path,
-            mode=mode,
-            complevel=complevel,
-            complib=complib,
-            fletcher32=fletcher32,
-        ) as store:
-            yield store
 
 
 def _maybe_remove(store, key):
