d009fa9266e4fc8f0bcd1399ad756de1a1c7b555
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-12-26 13:22:17 -0800

    Use new hdf fixtures

--- pandas/tests/io/pytables/conftest.py
+++ pandas/tests/io/pytables/conftest.py
@@ -2,8 +2,18 @@ import uuid
 
 import pytest
 
+from pandas.io.pytables import HDFStore
+
 
 @pytest.fixture
-def setup_path():
-    """Fixture for setup path"""
-    return f"tmp.__{uuid.uuid4()}__.h5"
+def temp_h5_path(tmp_path):
+    """Fixture for HDF5 path"""
+    file_path = tmp_path / f"{uuid.uuid4()}.h5"
+    file_path.touch()
+    return file_path
+
+
+@pytest.fixture
+def temp_hdfstore(temp_h5_path):
+    with HDFStore(temp_h5_path, mode="a") as store:
+        yield store
