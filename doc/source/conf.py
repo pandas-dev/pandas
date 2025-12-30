b4166e5e4ee709f76f5006a67c4688aed036af90
Loic Diridollou <loic.diridollou@gmail.com>
2025-12-30 14:26:46 -0500

    DOC: Remove 'Show source' button from doc pages for clarity (#63483)

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -408,7 +408,7 @@ html_use_modindex = True
 # html_split_index = False
 
 # If true, links to the reST sources are added to the pages.
-# html_show_sourcelink = True
+html_show_sourcelink = False
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
