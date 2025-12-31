db8830c7221484a24287018d88dbc6ecf33f396f
Suhrid Singh <suhrids88@gmail.com>
2025-12-31 18:07:47 +0530

    DOC: Fix whitespace

--- pandas/core/strings/accessor.py
+++ pandas/core/strings/accessor.py
@@ -1071,7 +1071,6 @@ class StringMethods(NoNewAttributesMixin):
 
         Examples
         --------
-        >>> import pandas as pd
         >>> s = pd.Series(["Linda van der Berg", "George Pitt-Rivers"])
         >>> s
         0    Linda van der Berg
