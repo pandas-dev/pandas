8bf1f63827d02f81750832e22808f47ca9c9594f
Pranjal-Nikalje <pranjalnikalje1212@gmail.com>
2026-01-03 12:34:06 +0530

    DOC: Fix typing of copy argument in astype

--- pandas/core/dtypes/astype.py
+++ pandas/core/dtypes/astype.py
@@ -57,24 +57,23 @@ def _astype_nansafe(
 def _astype_nansafe(
     arr: np.ndarray, dtype: DtypeObj, copy: bool = True, skipna: bool = False
 ) -> ArrayLike:
-    """
-    Cast the elements of an array to a given dtype a nan-safe manner.
-
+    
+    
     Parameters
-    ----------
+ 
     arr : ndarray
     dtype : np.dtype or ExtensionDtype
-    copy : bool, default True
+    copy : bool | None, default None
         If False, a view will be attempted but may fail, if
-        e.g. the item sizes don't align.
+        e.g. the item sizes does not align.
     skipna: bool, default False
         Whether or not we should skip NaN when casting as a string-type.
 
     Raises
-    ------
+
     ValueError
         The dtype was a datetime64/timedelta64 dtype, but it had no unit.
-    """
+    
 
     # dispatch on extension dtype if needed
     if isinstance(dtype, ExtensionDtype):
