52b772df97086e0ffa8b4e277d39298684ba4911
Denis Karadas <54038084+deniskrds@users.noreply.github.com>
2026-01-03 15:14:00 +0300

    BUG: Arrow-backed duration reductions return datetime.timedelta instead of pd.Timedelta

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -2152,7 +2152,11 @@ class ArrowExtensionArray(
         if pc.is_null(pa_result).as_py():
             return self.dtype.na_value
         elif isinstance(pa_result, pa.Scalar):
-            return pa_result.as_py()
+            result = pa_result.as_py()
+            pa_type = pa_result.type
+            if pa.types.is_duration(pa_type) and pa_type.unit != "ns":
+                return Timedelta(result).as_unit(pa_type.unit)
+            return result
         else:
             return pa_result
 
