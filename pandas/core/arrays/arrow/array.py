714d87b9430a59a18697a1ee5d31cf00a62d927a
Sanchit Rishi <sharmasanchitrishi@gmail.com>
2026-01-08 16:42:40 +0530

    pyarrow import

--- pandas/core/arrays/arrow/array.py
+++ pandas/core/arrays/arrow/array.py
@@ -230,6 +230,7 @@ def to_pyarrow_type(
     """
     Convert dtype to a pyarrow type instance.
     """
+    import pyarrow as pa
     from pandas.core.dtypes.dtypes import ArrowDtype
     if isinstance(dtype, ArrowDtype):
         return dtype.pyarrow_dtype
