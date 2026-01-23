5b9bf639b56d4c886a65df4f7d77886e41a70e41
Ruifeng Zheng <ruifengz@apache.org>
2026-01-23 10:35:47 -0800

    DOC: Inline `@doc` in `pandas/io/excel/_openpyxl.py` (#63822)

--- pandas/io/excel/_openpyxl.py
+++ pandas/io/excel/_openpyxl.py
@@ -10,9 +10,6 @@ from typing import (
 import numpy as np
 
 from pandas.compat._optional import import_optional_dependency
-from pandas.util._decorators import doc
-
-from pandas.core.shared_docs import _shared_docs
 
 from pandas.io.excel._base import (
     BaseExcelReader,
@@ -538,7 +535,6 @@ class OpenpyxlWriter(ExcelWriter):
 
 
 class OpenpyxlReader(BaseExcelReader["Workbook"]):
-    @doc(storage_options=_shared_docs["storage_options"])
     def __init__(
         self,
         filepath_or_buffer: FilePath | ReadBuffer[bytes],
@@ -552,7 +548,15 @@ class OpenpyxlReader(BaseExcelReader["Workbook"]):
         ----------
         filepath_or_buffer : str, path object or Workbook
             Object to be parsed.
-        {storage_options}
+        storage_options : dict, optional
+            Extra options that make sense for a particular storage connection, e.g.
+            host, port, username, password, etc. For HTTP(S) URLs the key-value pairs
+            are forwarded to ``urllib.request.Request`` as header options. For other
+            URLs (e.g. starting with "s3://", and "gcs://") the key-value pairs are
+            forwarded to ``fsspec.open``. Please see ``fsspec`` and ``urllib`` for more
+            details, and for more examples on storage options refer `here
+            <https://pandas.pydata.org/docs/user_guide/io.html?
+            highlight=storage_options#reading-writing-remote-files>`_.
         engine_kwargs : dict, optional
             Arbitrary keyword arguments passed to excel engine.
         """
