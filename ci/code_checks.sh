1d46e01c5f27c31d0b3d5031a6415f8a85536820
Tuhin Sharma <tuhinsharma121@gmail.com>
2026-01-23 10:28:37 +0530

    DOC: Fix GL08 for pandas.tseries.offsets.FY5253Quarter.get_weeks

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -87,7 +87,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.tseries.offsets.FY5253.is_on_offset GL08" \
         -i "pandas.tseries.offsets.FY5253.rule_code GL08" \
         -i "pandas.tseries.offsets.FY5253.startingMonth GL08" \
-        -i "pandas.tseries.offsets.FY5253Quarter.get_weeks GL08" \
         -i "pandas.tseries.offsets.FY5253Quarter.is_on_offset GL08" \
         -i "pandas.tseries.offsets.FY5253Quarter.rule_code GL08" \
         -i "pandas.tseries.offsets.FY5253Quarter.startingMonth GL08" \
