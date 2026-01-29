7f1916f40a9a7c8ac0f463033a851b0fdbe029b8
Tuhin Sharma <tuhinsharma121@gmail.com>
2026-01-29 22:23:23 +0530

    DOC: Fix GL08 for pandas.tseries.offsets.SemiMonthEnd.is_on_offset (#63926)

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -79,7 +79,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i "pandas.tseries.offsets.FY5253Quarter.is_on_offset GL08" \
         -i "pandas.tseries.offsets.LastWeekOfMonth.is_on_offset GL08" \
         -i "pandas.tseries.offsets.SemiMonthBegin.rule_code GL08" \
-        -i "pandas.tseries.offsets.SemiMonthEnd.is_on_offset GL08" \
         -i "pandas.tseries.offsets.SemiMonthEnd.rule_code GL08" \
         -i "pandas.tseries.offsets.WeekOfMonth.is_on_offset GL08" # There should be no backslash in the final line, please keep this comment in the last ignored function
 
