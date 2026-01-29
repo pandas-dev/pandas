8c9c853b36e45befe392566dd6f3315621e54364
kjmin622 <kjmin622@naver.com>
2026-01-29 22:43:42 +0900

    modify test (pandas-dev#63931)

--- pandas/tests/series/methods/test_equals.py
+++ pandas/tests/series/methods/test_equals.py
@@ -34,9 +34,7 @@ def test_equals(arr, idx):
     assert not s1.equals(s2)
 
 
-@pytest.mark.parametrize(
-    "val", [1, 1.1, 1 + 1j, True, "abc", [1, 2], (1, 2), {1, 2}, {"a": 1}, None]
-)
+@pytest.mark.parametrize("val", [1, 1.1, 1 + 1j, True, "abc", {1, 2}, {"a": 1}, None])
 def test_equals_list_array(val):
     # GH20676 Verify equals operator for list of Numpy arrays
     arr = np.array([1, 2])
@@ -46,6 +44,16 @@ def test_equals_list_array(val):
 
     s1[1] = val
     assert not s1.equals(s2)
+
+
+@pytest.mark.parametrize("val", [[1, 2], (1, 2)])
+def test_equals_list_array_list_tuple_vs_ndarray(val):
+    # GH#63904: list/tuple with same values as ndarray should be considered equal
+    arr = np.array([1, 2])
+    s1 = Series([arr, arr])
+    s2 = s1.copy()
+    s2[1] = val
+    assert s1.equals(s2)
 
 
 def test_equals_false_negative():
