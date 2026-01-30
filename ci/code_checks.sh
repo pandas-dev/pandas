2e4c46e714a63540e49df150d9bbe9312568f54a
Tuhin Sharma <tuhinsharma121@gmail.com>
2026-01-30 12:17:31 +0530

    DOC: Fix GL08 for pandas.tseries.offsets.FY5253Quarter.is_on_offset

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -72,7 +72,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
         -i ES01 `# For now it is ok if docstrings are missing the extended summary` \
         -i "pandas.tseries.offsets.BusinessHour PR02,SA01" \
         -i "pandas.tseries.offsets.CustomBusinessHour PR02,SA01" \
-        -i "pandas.tseries.offsets.FY5253Quarter.is_on_offset GL08" \
         -i "pandas.tseries.offsets.LastWeekOfMonth.is_on_offset GL08" \
         -i "pandas.tseries.offsets.SemiMonthBegin.rule_code GL08" \
         -i "pandas.tseries.offsets.SemiMonthEnd.rule_code GL08" \
