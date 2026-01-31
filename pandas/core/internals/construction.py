674e457e56eea65be68d23896afd1d7b80630d30
kjmin622 <kjmin622@naver.com>
2026-01-31 22:20:27 +0900

    modify _homogenize (PR#63937)

--- pandas/core/internals/construction.py
+++ pandas/core/internals/construction.py
@@ -559,13 +559,42 @@ def _homogenize(
 
     for val in data:
         if isinstance(val, (ABCSeries, Index)):
+            orig_refs = val._references
+            orig_values = val._values
             if dtype is not None:
                 val = val.astype(dtype)
             if isinstance(val, ABCSeries) and val.index is not index:
                 # Forces alignment. No need to copy data since we
                 # are putting it into an ndarray later
                 val = val.reindex(index)
-            refs.append(val._references)
+
+            if orig_values is val._values:
+                shares = True
+            elif hasattr(orig_values, "_ndarray") and hasattr(val._values, "_ndarray"):
+                try:
+                    shares = np.shares_memory(
+                        orig_values._ndarray, val._values._ndarray
+                    )
+                except (TypeError, AttributeError):
+                    shares = False
+            elif hasattr(orig_values, "_pa_array") and hasattr(
+                val._values, "_pa_array"
+            ):
+                try:
+                    left_buf = orig_values._pa_array.chunk(0).buffers()[1]
+                    right_buf = val._values._pa_array.chunk(0).buffers()[1]
+                    shares = left_buf.address == right_buf.address
+                except (TypeError, AttributeError, IndexError):
+                    shares = False
+            else:
+                try:
+                    shares = np.shares_memory(orig_values, val._values)
+                except (TypeError, AttributeError):
+                    shares = False
+            if orig_refs is not None and shares:
+                refs.append(orig_refs)
+            else:
+                refs.append(None)
             val = val._values
         else:
             if isinstance(val, dict):
