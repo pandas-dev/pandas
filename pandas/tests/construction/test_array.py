ddf64a29e5229042b81765b0d36d27085f49379d
Tej Allada <tejallada@gmail.com>
2026-02-11 20:24:36 -0500

    Actually fixed the formatting for the files so tthey have an empty line at the end and do not have whitespace

--- pandas/tests/construction/test_array.py
+++ pandas/tests/construction/test_array.py
@@ -12,9 +12,8 @@ def test_string_array_construction_consistency_with_series(data):
     # GH#57702: Ensure pd.array and pd.Series(data).values are consistent
     # for StringDtype(storage="python")
     dtype = pd.StringDtype(storage="python")
-    
+ 
     result_array = pd.array(data, dtype=dtype)
     result_series_array = pd.Series(data, dtype=dtype).values
-    
+
     tm.assert_extension_array_equal(result_array, result_series_array)
-    
