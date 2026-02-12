e52d591d232f24553d8e015f449f3b6069794e8d
Tej Allada <tejallada@gmail.com>
2026-02-12 09:42:18 -0500

    Updated whitespace again for typing test

--- pandas/tests/construction/test_array.py
+++ pandas/tests/construction/test_array.py
@@ -1,6 +1,8 @@
 import pytest
+
 import pandas as pd
 import pandas._testing as tm
+
 
 @pytest.mark.parametrize("data", [
     ["a", None],        # Original case
@@ -12,7 +14,7 @@ def test_string_array_construction_consistency_with_series(data):
     # GH#57702: Ensure pd.array and pd.Series(data).values are consistent
     # for StringDtype(storage="python")
     dtype = pd.StringDtype(storage="python")
- 
+
     result_array = pd.array(data, dtype=dtype)
     result_series_array = pd.Series(data, dtype=dtype).values
 
