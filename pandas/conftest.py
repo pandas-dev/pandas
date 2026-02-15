030cac748e35f6a974ab52e325906ddd460f3183
Brock <jbrockmendel@gmail.com>
2026-02-15 14:39:11 -0800

    doctest allow warnings

--- pandas/conftest.py
+++ pandas/conftest.py
@@ -194,6 +194,9 @@ def pytest_collection_modifyitems(items, config) -> None:
         ),
         ("read_parquet", "Passing a BlockManager to DataFrame is deprecated"),
         ("Timestamp.utcfromtimestamp", "Timestamp.utcfromtimestamp is deprecated"),
+        ("DatetimeLikeArrayMixin.freqstr", "PeriodIndex.freqstr is deprecated"),
+        ("DatetimeIndexOpsMixin.freqstr", "PeriodIndex.freqstr is deprecated"),
+        ("Period.freqstr", "Period.freqstr is deprecated"),
     ]
 
     if is_doctest:
