47d9ee6a11471f1302c2275ce7144f8060ff9a95
Tuhin Sharma <tuhinsharma121@gmail.com>
2026-02-15 10:25:51 +0530

    DOC: Fix ES01 for pandas.Series

--- ci/code_checks.sh
+++ ci/code_checks.sh
@@ -70,15 +70,6 @@ if [[ -z "$CHECK" || "$CHECK" == "docstrings" ]]; then
     "$BASE_DIR"/scripts/validate_docstrings.py \
         --format=actions \
         -i "pandas.tseries.frequencies.to_offset ES01" \
-        -i "pandas.Series.dtypes ES01" \
-        -i "pandas.Series.to_numpy ES01" \
-        -i "pandas.Series.to_period ES01" \
-        -i "pandas.Series.keys ES01" \
-        -i "pandas.Series.pop ES01" \
-        -i "pandas.Series.item ES01" \
-        -i "pandas.Series.round ES01" \
-        -i "pandas.Series.agg ES01" \
-        -i "pandas.Series.aggregate ES01" \
         -i "pandas.Series.transform ES01" \
         -i "pandas.Series.rolling ES01" \
         -i "pandas.Series.count ES01" \
