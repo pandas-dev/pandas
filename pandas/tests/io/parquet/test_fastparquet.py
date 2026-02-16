97b9cd0fb095ae45890e40645983594bbd568e2f
Anitha Kumar <anithakumar@Anithas-Laptop.local>
2026-02-16 11:36:38 +0530

    TST: xfail fastparquet BytesIO index corruption (GH#64007)

--- pandas/tests/io/parquet/test_fastparquet.py
+++ pandas/tests/io/parquet/test_fastparquet.py
@@ -14,17 +14,17 @@ import pandas._testing as tm
     strict=True,
 )
 @pytest.mark.filterwarnings("ignore:.*fastparquet.*")
-def test_fastparquet_bytesio_multiple_files_preserve_index():
+def test_fastparquet_bytesio_multiple_files_preserve_index() -> None:
     # GH#64007
 
-    def make_df(start):
+    def make_df(start: int) -> pd.DataFrame:
         return pd.DataFrame(
             {"A": [1, 2, 3], "B": [1, 2, 3]},
             index=[start, start + 1, start + 2],
         )
 
-    df1 = make_df(1)
-    df2 = make_df(2)
+    df1: pd.DataFrame = make_df(1)
+    df2: pd.DataFrame = make_df(2)
 
     buf1 = io.BytesIO()
     buf2 = io.BytesIO()
@@ -35,8 +35,9 @@ def test_fastparquet_bytesio_multiple_files_preserve_index():
     buf1.seek(0)
     buf2.seek(0)
 
-    result1 = pd.read_parquet(buf1, engine="fastparquet")
-    result2 = pd.read_parquet(buf2, engine="fastparquet")
+    result1: pd.DataFrame = pd.read_parquet(buf1, engine="fastparquet")
+    result2: pd.DataFrame = pd.read_parquet(buf2, engine="fastparquet")
 
     tm.assert_index_equal(result1.index, df1.index)
     tm.assert_index_equal(result2.index, df2.index)
+
