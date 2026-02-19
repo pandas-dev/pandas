40940e3e6c709896851870e53e48cd185dbd21d2
Dhruv Singla <68206552+d-s-dc@users.noreply.github.com>
2026-02-19 11:50:19 +0530

    Update documentation for apply function behavior
    Clarify behavior of function application with list-like or dict-like inputs.

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -13958,7 +13958,10 @@ class DataFrame(NDFrame, OpsMixin):
 
         Objects passed to the function are Series objects whose index is
         either the DataFrame's index (``axis=0``) or the DataFrame's columns
-        (``axis=1``). By default (``result_type=None``), the final return type
+        (``axis=1``). However, if func is a list-like or dict-like of functions
+        , by default, the objects passed to the applied functions are the 
+        individual elements of the Series rather than the Series itself 
+        (see ``by_row``). By default (``result_type=None``), the final return type
         is inferred from the return type of the applied function. Otherwise,
         it depends on the `result_type` argument. The return type of the applied
         function is inferred based on the first computed result obtained after
