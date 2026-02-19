bbc9123e01d236441a8eea42c52eae8d094da1d1
Dhruv Singla <68206552+d-s-dc@users.noreply.github.com>
2026-02-19 12:17:50 +0530

    Fix formatting in docstring for apply function

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -13958,8 +13958,8 @@ class DataFrame(NDFrame, OpsMixin):
 
         Objects passed to the function are Series objects whose index is
         either the DataFrame's index (``axis=0``) or the DataFrame's columns
-        (``axis=1``). However, if func is a list-like or dict-like of functions
-        , by default, the objects passed to the applied functions are the 
+        (``axis=1``). However, if ``func`` is a list-like or dict-like of functions,
+        the objects passed to the applied functions are the 
         individual elements of the Series rather than the Series itself 
         (see ``by_row``). By default (``result_type=None``), the final return type
         is inferred from the return type of the applied function. Otherwise,
