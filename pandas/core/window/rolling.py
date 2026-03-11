4a4380fe01a802f811d494e4f4b1169a2804430e
Tuhin Sharma <tuhinsharma121@gmail.com>
2026-03-11 21:43:22 +0530

    DOC: Fix subject-verb agreement in error messages (#64521)

--- pandas/core/window/rolling.py
+++ pandas/core/window/rolling.py
@@ -344,7 +344,7 @@ class BaseWindow(SelectionMixin):
         if needs_i8_conversion(values.dtype):
             raise NotImplementedError(
                 f"ops for {type(self).__name__} for this "
-                f"dtype {values.dtype} are not implemented"
+                f"dtype {values.dtype} is not implemented"
             )
         # GH #12373 : rolling functions error on float32 data
         # make sure the data is coerced to float64
