3f110f96ba6adbeac8bfa46b1778a650829895f5
fumoboy007 <2100868+fumoboy007@users.noreply.github.com>
2026-03-11 19:20:56 +0900

    BUG: Fix bug (#64524) where `read_html` parses a nested table incorrectly when using `html5lib` or `bs4` flavors

--- pandas/tests/io/test_html.py
+++ pandas/tests/io/test_html.py
@@ -617,7 +617,6 @@ class TestReadHtml:
 
         tm.assert_frame_equal(result, expected)
 
-    @pytest.mark.xfail(reason="Known issue: GH-64524", strict=False)
     def test_nested_table(self, flavor_read_html):
         # GH-64524
         tables = flavor_read_html(
