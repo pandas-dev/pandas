0236e5d1ed356e8d4e09f1bb56c3bff01963c878
Tuhin Sharma <tuhinsharma121@gmail.com>
2026-03-12 09:12:07 +0530

    DOC: Fix ES01 for pandas methods
    Co-authored-by: mroeschke <mroeschke@users.noreply.github.com>

--- pandas/core/groupby/generic.py
+++ pandas/core/groupby/generic.py
@@ -110,7 +110,7 @@ class NamedAgg:
     """
     Helper for column specific aggregation with control over output column names.
 
-    Subclass of ``namedtuple`` used with :meth:`DataFrame.groupby().agg()
+    A dataclass used with :meth:`DataFrame.groupby().agg()
     <pandas.core.groupby.DataFrameGroupBy.aggregate>` to specify an
     aggregation on a particular column and assign a custom name to the
     resulting column. Additional positional and keyword arguments can be
