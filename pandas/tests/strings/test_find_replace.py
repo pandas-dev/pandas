1cf24d64f237c48843b08be9ca87f64510bc2a2f
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2026-03-30 13:16:45 +0200

    add back original find code to use in case of all-ascii

--- pandas/tests/strings/test_find_replace.py
+++ pandas/tests/strings/test_find_replace.py
@@ -1629,6 +1629,9 @@ def test_find_multibyte_chars(any_string_dtype):
     expected = Series([1, 1, 1, 1], dtype=expected_dtype)
     tm.assert_series_equal(result, expected)
 
+    result = ser.str.find("a", start=1)
+    tm.assert_series_equal(result, expected)
+
 
 # --------------------------------------------------------------------------------------
 # str.translate
