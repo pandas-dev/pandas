343886462ac5c0f3f1c5b87e3afee2b43e849f91
kjmin622 <kjmin622@naver.com>
2026-04-08 13:28:56 +0900

    fix ci error

--- pandas/core/indexes/accessors.py
+++ pandas/core/indexes/accessors.py
@@ -12,6 +12,7 @@ from typing import (
 import warnings
 
 import numpy as np
+import pyarrow as pa
 
 from pandas._libs import lib
 from pandas.errors import Pandas4Warning
@@ -248,18 +249,20 @@ class ArrowTemporalProperties(PandasDelegate, PandasObject, NoNewAttributesMixin
     def components(self) -> DataFrame:
         from pandas import DataFrame
 
+        arr = cast("ArrowExtensionArray", self._parent.array)
+        seconds = arr._to_timedeltaarray().components.seconds
+
         components_df = DataFrame(
             {
-                col: getattr(self._parent.array, f"_dt_{col}")
-                for col in [
-                    "days",
-                    "hours",
-                    "minutes",
-                    "seconds",
-                    "milliseconds",
-                    "microseconds",
-                    "nanoseconds",
-                ]
+                "days": arr._dt_days,
+                "hours": arr._dt_hours,
+                "minutes": arr._dt_minutes,
+                "seconds": arr._from_pyarrow_array(
+                    pa.array(seconds, from_pandas=True, type=pa.int32())
+                ),
+                "milliseconds": arr._dt_milliseconds,
+                "microseconds": arr._dt_microseconds,
+                "nanoseconds": arr._dt_nanoseconds,
             }
         )
         return components_df
