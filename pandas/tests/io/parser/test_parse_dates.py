6a11335c95d26b9b68df474680a43d15e5e1c62c
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2026-04-10 10:43:20 +0200

    fix expected unit in parser test

--- pandas/tests/io/parser/test_parse_dates.py
+++ pandas/tests/io/parser/test_parse_dates.py
@@ -217,6 +217,8 @@ def test_parse_tz_aware(all_parsers):
     expected = DataFrame(
         {"x": [0.5]}, index=Index([Timestamp("2012-06-13 01:39:00+00:00")], name="Date")
     )
+    if parser.engine == "pyarrow":
+        expected.index = expected.index.as_unit("s")
     tm.assert_frame_equal(result, expected)
     assert result.index.tz is timezone.utc
 
