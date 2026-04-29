3c8788db306834e0c44ed5541614b335da039dea
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2026-04-29 09:46:12 +0200

    fix pyright typing

--- pandas/util/_decorators.py
+++ pandas/util/_decorators.py
@@ -487,6 +487,6 @@ def set_module(module) -> Callable[[F], F]:
                     pass
 
             func.__module__ = module
-        return func
+        return cast("F", func)
 
     return decorator
