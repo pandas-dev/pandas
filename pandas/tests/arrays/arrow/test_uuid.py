d1c05b48e3531eb34cd5c95f9d4a85f256219bde
eenbeneen <gebremichaeleben@gmail.com>
2026-04-29 13:26:56 -0400

    correct dtype assertion in test_series_from_pyarrow_uuid_chunkedarray

--- pandas/tests/arrays/arrow/test_uuid.py
+++ pandas/tests/arrays/arrow/test_uuid.py
@@ -105,7 +105,7 @@ def test_series_from_pyarrow_uuid_chunkedarray() -> None:
     carr = pa.chunked_array([chunk1, chunk2])
     s = pd.Series(carr)
 
-    assert s.dtype != object
+    assert s.dtype == pd.ArrowDtype(pa.uuid())
 
     ser_array = s.array
     assert isinstance(ser_array, ArrowExtensionArray)
