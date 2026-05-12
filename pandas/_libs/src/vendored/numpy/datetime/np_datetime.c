36abe4dbae7dde9551fc46a860984403bf458a66
jbrockmendel <jbrockmendel@gmail.com>
2026-05-11 17:27:44 -0700

    Backport PR #65591: BUG: fix np_datetime.c include order to avoid numpy nightly segfault

--- pandas/_libs/src/vendored/numpy/datetime/np_datetime.c
+++ pandas/_libs/src/vendored/numpy/datetime/np_datetime.c
@@ -20,9 +20,12 @@ This file is derived from NumPy 1.7. See NUMPY_LICENSE.txt
 #  define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 #endif // NPY_NO_DEPRECATED_API
 
-#include "pandas/vendored/numpy/datetime/np_datetime.h"
+// GH#65569: NO_IMPORT_ARRAY and PY_ARRAY_UNIQUE_SYMBOL must be defined before
+// the np_datetime.h include, otherwise the numpy C-API symbol import breaks
+// on numpy >= 2.5.
 #define NO_IMPORT_ARRAY
 #define PY_ARRAY_UNIQUE_SYMBOL PANDAS_DATETIME_NUMPY
+#include "pandas/vendored/numpy/datetime/np_datetime.h"
 #include "pandas/portable.h"
 #include <numpy/ndarrayobject.h>
 #include <numpy/npy_common.h>
