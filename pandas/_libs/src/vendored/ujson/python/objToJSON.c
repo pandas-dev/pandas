938e010abf3cde4b527049bc8d08c2dad8e33225
Julian Harbeck <harbeck@tu-berlin.de>
2026-05-15 15:43:04 +0200

    Remove unused `__array__` code path, `Categorical` and `Sparse` arrays will go through `_values_for_json`

--- pandas/_libs/src/vendored/ujson/python/objToJSON.c
+++ pandas/_libs/src/vendored/ujson/python/objToJSON.c
@@ -208,11 +208,6 @@ static PyObject *get_values(PyObject *obj) {
           PyObject_CallMethod(values, "_values_for_json", NULL);
       Py_DECREF(values);
       values = array_values;
-    } else if (PyObject_HasAttrString(values, "__array__")) {
-      // We may have gotten a Categorical or Sparse array so call np.array
-      PyObject *array_values = PyObject_CallMethod(values, "__array__", NULL);
-      Py_DECREF(values);
-      values = array_values;
     } else if (!PyArray_CheckExact(values)) {
       // Didn't get a numpy array, so keep trying
       Py_DECREF(values);
