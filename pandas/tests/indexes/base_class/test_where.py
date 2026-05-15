552f33feb1736d0d788338b83a91ba858a2a7b83
Richard Shadrach <rhshadrach@gmail.com>
2026-05-15 17:05:49 -0400

    Add test

--- pandas/tests/indexes/base_class/test_where.py
+++ pandas/tests/indexes/base_class/test_where.py
@@ -11,3 +11,9 @@ class TestWhere:
         res = idx.where(mask, "2")
         expected = Index([0, "2", 2])
         tm.assert_index_equal(res, expected)
+
+    def test_where_preserves_object_dtype(self):
+        idx = Index(list("abc"), dtype="object")
+        res = idx.where(np.array([True, False, True]), other="x")
+        expected = Index(["a", "x", "c"], dtype="object")
+        tm.assert_index_equal(res, expected)
