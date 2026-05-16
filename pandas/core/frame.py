ecfd29bd4045ae7b0fee62ab7654ac9189719a35
Noland Rimando <J281417@tafe.wa.edu.au>
2026-05-17 07:04:42 +0800

    Added index value before reshaping data

--- pandas/core/frame.py
+++ pandas/core/frame.py
@@ -1762,6 +1762,7 @@ class DataFrame(NDFrame, OpsMixin):
             if len(data) > 0:
                 # TODO speed up Series case
                 if isinstance(next(iter(data.values())), (Series, dict)):
+                    index = list(data.keys())
                     data = _from_nested_dict(data)
                 else:
                     index = list(data.keys())
