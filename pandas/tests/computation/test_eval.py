1663986a19aab9b94fcaea21caa7fe42131b8eda
GiTaDi-CrEaTe <kar323834@gmail.com>
2026-05-19 12:35:04 +0530

    TST: Remove deprecated using_copy_on_write fixture

--- pandas/tests/computation/test_eval.py
+++ pandas/tests/computation/test_eval.py
@@ -2031,7 +2031,8 @@ def test_method_calls_on_binop():
     tm.assert_series_equal(result, expected)
 
 
-def test_eval_inplace_cow_alias_corruption(using_copy_on_write):
+def test_eval_inplace_cow_alias_corruption():
+    
     """
     Test that df.eval(..., inplace=True) does not corrupt memory
     when modifying the evaluated column under Copy-on-Write.
