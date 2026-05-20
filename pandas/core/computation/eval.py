825ad9e998f8669c0701996853f815eb8c43492e
GiTaDi-CrEaTe <kar323834@gmail.com>
2026-05-20 10:57:55 +0530

    BUG: Remove duplicated assignment block from eval.py

--- pandas/core/computation/eval.py
+++ pandas/core/computation/eval.py
@@ -409,23 +409,10 @@ def eval(
         if env.target is not None and assigner is not None:
             target_modified = True
 
-            # if returning a copy, copy only on the first assignment
-            if not inplace and first_expr:
-                try:
-                    target = env.target
-                    if isinstance(target, NDFrame):
-                        target = target.copy(deep=False)
-                    else:
-                        target = target.copy()
-                except AttributeError as err:
-                    raise ValueError("Cannot return a copy of the target") from err
-            else:
-                target = env.target
+            # Prevent CoW alias corruption by forcing a copy of the evaluated result
+            if inplace and hasattr(ret, "copy"):
+                ret = ret.copy()  # pyright: ignore[reportAttributeAccessIssue]
 
-            # TypeError is most commonly raised (e.g. int, list), but you
-            # get IndexError if you try to do this assignment on np.ndarray.
-            # we will ignore numpy warnings here; e.g. if trying
-            # to use a non-numeric indexer
             try:
                 if inplace and isinstance(target, NDFrame):
                     target.loc[:, assigner] = ret
@@ -433,6 +420,13 @@ def eval(
                     target[assigner] = ret  # pyright: ignore[reportIndexIssue]
             except (TypeError, IndexError) as err:
                 raise ValueError("Cannot assign expression output to target") from err
+            else:
+                target = env.target
+
+            # TypeError is most commonly raised (e.g. int, list), but you
+            # get IndexError if you try to do this assignment on np.ndarray.
+            # we will ignore numpy warnings here; e.g. if trying
+            # to use a non-numeric indexer
 
             if not resolvers:
                 resolvers = ({assigner: ret},)
