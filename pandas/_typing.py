b7bbe77241e2b90f90d011855554ab4eb461eab4
Saikat Bera <saikatbera2001@gmail.com>
2026-06-11 01:12:41 +0530

    fix pre-commit

--- pandas/_typing.py
+++ pandas/_typing.py
@@ -400,7 +400,7 @@ TakeIndexer: TypeAlias = Sequence[int] | Sequence[np.integer] | npt.NDArray[np.i
 
 # Shared by functions such as drop and astype
 IgnoreRaise: TypeAlias = Literal["ignore", "raise"]
-IgnoreRaiseCoerce: TypeAlias = IgnoreRaise |  Literal["coerce"]
+IgnoreRaiseCoerce: TypeAlias = IgnoreRaise | Literal["coerce"]
 
 # Windowing rank methods
 WindowingRankType: TypeAlias = Literal["average", "min", "max"]
