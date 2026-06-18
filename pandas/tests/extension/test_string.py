45d10286bd943ae255dff60bd0a55039cd87edd8
kjmin622 <kjmin622@naver.com>
2026-06-18 13:31:15 +0900

    consider sorter is not None

--- pandas/tests/extension/test_string.py
+++ pandas/tests/extension/test_string.py
@@ -309,3 +309,23 @@ def test_searchsorted_with_na_raises(data_for_sorting, as_series, na_position):
     )
     with pytest.raises(ValueError, match=msg):
         arr.searchsorted(b)
+
+
+@pytest.mark.parametrize("na_position", ["first", "last"])
+def test_searchsorted_with_na_and_sorter_raises(data_for_sorting, na_position):
+    # GH65837
+    b, c, a = data_for_sorting
+    arr = data_for_sorting.take([0, 2, 1])
+    arr[1] = pd.NA
+
+    if na_position == "last":
+        sorter = np.array([0, 2, 1])
+    else:
+        sorter = np.array([1, 0, 2])
+
+    msg = (
+        "searchsorted requires array to be sorted, "
+        "which is impossible with NAs present."
+    )
+    with pytest.raises(ValueError, match=msg):
+        arr.searchsorted(b, sorter=sorter)
