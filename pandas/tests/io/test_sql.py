a919b607d2d92d19654cbb82adc640f6db561f04
pranavchoudhary-tech <pranav.nstblr@gmail.com>
2026-06-19 01:23:32 +0530

    Revert unrelated changes to test_sql.py

--- pandas/tests/io/test_sql.py
+++ pandas/tests/io/test_sql.py
@@ -12,16 +12,12 @@ from decimal import Decimal
 from io import StringIO
 import os
 from pathlib import Path
+import sqlite3
 from typing import TYPE_CHECKING
 import uuid
 
 import numpy as np
 import pytest
-
-try:
-    import sqlite3
-except ImportError:
-    pytest.skip("sqlite3 not available", allow_module_level=True)
 
 from pandas._config import using_string_dtype
 
