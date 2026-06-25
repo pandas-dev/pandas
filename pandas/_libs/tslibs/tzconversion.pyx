8c5800aee0663bfdcbb08d1979c374bce6dc2b5b
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2026-06-25 12:44:55 +0200

    fixup overflow

--- pandas/_libs/tslibs/tzconversion.pyx
+++ pandas/_libs/tslibs/tzconversion.pyx
@@ -114,9 +114,10 @@ cdef class Localizer:
             # since we are sometimes comparing local times with UTC transitions,
             # subtract 12 hours (12 * 60 * 60) to be safe
             # (i.e. ensure to use correct but slower path around the last transition)
-            self.last_trans = (
-                trans[self.ntrans - 1] - (43200 * periods_per_second(creso))
-            )
+            if has_tz_rule:
+                self.last_trans = (
+                    trans[self.ntrans - 1] - (43200 * periods_per_second(creso))
+                )
 
             if typ != "pytz" and typ != "dateutil" and typ != "zoneinfo":
                 # static/fixed; in this case we know that len(delta) == 1
