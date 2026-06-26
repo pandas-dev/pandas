8aae26d2403163a4b2ec523212a2a984e7d40e73
Richard Shadrach <rhshadrach@gmail.com>
2026-06-26 15:27:39 -0400

    Cleanup comment

--- pandas/core/groupby/grouper.py
+++ pandas/core/groupby/grouper.py
@@ -983,7 +983,7 @@ def _factorize_monotonic(
         if not isinstance(dtype, np.dtype) or dtype.kind not in "iufmMb":
             return None
         if getattr(grouping_vector, "freq", None) is not None:
-            # DatetimeIndex/TimedeltaIndex/PeriodIndex are faster
+            # Fallback for DatetimeIndex/TimedeltaIndex/PeriodIndex is faster
             return None
         ascending = grouping_vector.is_monotonic_increasing
         if not ascending and not grouping_vector.is_monotonic_decreasing:
