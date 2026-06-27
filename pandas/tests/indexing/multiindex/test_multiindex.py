Backport PR #66051: BUG: fix regression in DataFrame setitem with MultiIndex columns being a silent no-op with object-dtype level

pandas/tests/indexing/multiindex/test_multiindex.py
@@ -268,9 +268,8 @@ class TestMultiIndexBasic:
         tm.assert_frame_equal(df1, df2)
         tm.assert_frame_equal(df1, df3)
 
-        df1["C"] = s1
-        tm.assert_frame_equal(df1, df2)
-        tm.assert_frame_equal(df1, df3)
+        df1["C"] = s1 * 2
+        tm.assert_series_equal(df1["C"], df2["C"] * 2)
 
     def test_multiindex_assign_alignment_with_non_string_dtype(self):
         # GH 62518
@@ -292,3 +291,23 @@ class TestMultiIndexBasic:
         )
 
         tm.assert_frame_equal(meta, result)
+
+    def test_multiindex_assign_alignment_with_object_dtype(self):
+        # https://github.com/pandas-dev/pandas/issues/65118
+        # second level of the multiindex is object dtype, and the value of
+        # that level is 0 for the single column we are setting
+
+        columns = MultiIndex.from_tuples(
+            [("A", "M"), ("A", 0), ("B", 1), ("B", 2), ("C", 0)]
+        )
+        df = DataFrame(np.arange(20, dtype=float).reshape(4, 5), columns=columns)
+
+        df["A"] = df["A"] / 100
+        df["B"] = df["B"] / 100
+        # this case specifically was the buggy one
+        df["C"] = df["C"] / 100
+
+        expected = DataFrame(
+            np.arange(20, dtype=float).reshape(4, 5) / 100, columns=columns
+        )
+        tm.assert_frame_equal(df, expected)