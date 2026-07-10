f87bbb0c983df928a890909817b7dab14b350cb2
Ronit <ronitsonawane2007@gmail.com>
2026-07-10 09:16:59 +0530

    TST: Fix deprecated 'd' to 'D' in test_rolling_inf

--- pandas/tests/window/test_rolling_inf.py
+++ pandas/tests/window/test_rolling_inf.py
@@ -6,13 +6,13 @@ import pandas._testing as tm
 
 def test_rolling_timedelta_inf():
     # GH 57549
-    idx = pd.date_range("2024-02-01", "2024-02-21", freq="1d")
+    idx = pd.date_range("2024-02-01", "2024-02-21", freq="D")
     df = pd.DataFrame(index=idx)
     df["A"] = df.index.day.astype(float)
     df.iloc[-2, 0] = np.inf
     df.iloc[-9, 0] = np.nan
 
-    res1 = df.rolling(window=pd.Timedelta("4d"), min_periods=1).mean()
+    res1 = df.rolling(window=pd.Timedelta("4D"), min_periods=1).mean()
     res2 = df.rolling(window=4, min_periods=1).mean()
 
     tm.assert_frame_equal(res1, res2)
