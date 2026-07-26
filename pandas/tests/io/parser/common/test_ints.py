1624c5ba07a74004c1f5e9a04ffb73fb1542c990
Utkarsh Tiwari <utkarsh7854@gmail.com>
2026-07-27 00:34:38 +0530

    TST skip PyArrow uint64 limit in test body

--- pandas/tests/io/parser/common/test_ints.py
+++ pandas/tests/io/parser/common/test_ints.py
@@ -182,11 +182,7 @@ def test_int64_overflow(all_parsers, conv, request):
 @pytest.mark.parametrize(
     "val",
     [
-        pytest.param(
-            np.iinfo(np.uint64).max,
-            marks=skip_pyarrow,
-            id="uint64_max",
-        ),
+        np.iinfo(np.uint64).max,
         np.iinfo(np.int64).max,
         np.iinfo(np.int64).min,
     ],
@@ -195,6 +191,8 @@ def test_int64_uint64_range(all_parsers, val):
     # These numbers fall right inside the int64-uint64
     # range, so they should be parsed as integer.
     parser = all_parsers
+    if parser.engine == "pyarrow" and val == np.iinfo(np.uint64).max:
+        pytest.skip("pyarrow infers uint64 max as double and loses precision")
     result = parser.read_csv(StringIO(str(val)), header=None)
 
     expected = DataFrame([val])
