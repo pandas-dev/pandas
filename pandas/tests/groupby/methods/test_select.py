19e5e7f52d7018433e6397197499484876a0b707
Richard Shadrach <rhshadrach@gmail.com>
2026-08-16 11:47:04 -0400

    Test fixup

--- pandas/tests/groupby/methods/test_select.py
+++ pandas/tests/groupby/methods/test_select.py
@@ -144,9 +144,15 @@ def test_select_expression_unnamed_raises(df):
         df.groupby("g").select(pd.col("a") + pd.col("b"))
 
 
-def test_select_expression_scalar_raises(df):
+def test_select_expression_scalar_raises(df, using_python_scalars):
     # https://github.com/pandas-dev/pandas/issues/40322
-    msg = re.escape("expression col('b').sum() evaluated to an object of type float64")
+    if using_python_scalars:
+        result_type = "float"
+    else:
+        result_type = "float64"
+    msg = re.escape(
+        f"expression col('b').sum() evaluated to an object of type {result_type}"
+    )
     with pytest.raises(TypeError, match=msg):
         df.groupby("g").select(pd.col("b").sum())
 
