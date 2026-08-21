23e310ee9c14cc0399e152b4dec1a651b41428eb
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2026-08-21 14:17:44 -0700

    CI: Update pixi.lock (#66907)

--- pandas/compat/__init__.py
+++ pandas/compat/__init__.py
@@ -39,6 +39,8 @@ from pandas.compat.pyarrow import (
     pa_version_under21p0,
     pa_version_under22p0,
     pa_version_under23p0,
+    pa_version_under25p0,
+    pa_version_under26p0,
 )
 
 if TYPE_CHECKING:
@@ -176,4 +178,6 @@ __all__ = [
     "pa_version_under21p0",
     "pa_version_under22p0",
     "pa_version_under23p0",
+    "pa_version_under25p0",
+    "pa_version_under26p0",
 ]
