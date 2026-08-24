4380ccc2559328b339687f84d6ffad3ae8d3ad55
Pieter Eendebak <pieter.eendebak@gmail.com>
2026-08-24 23:58:19 +0200

    Apply suggestion from @eendebakpt

--- pandas/tests/frame/test_query_eval.py
+++ pandas/tests/frame/test_query_eval.py
@@ -1743,7 +1743,7 @@ class TestDataFrameQueryInWithColumnRefs:
     def test_in_column_refs_string_pyarrow(self, engine, parser):
         df = DataFrame(
             {"a": ["a", "a", "b"], "b": ["a", "b", "c"], "c": ["b", "b", "c"]},
-            dtype="string[pyarrow]"
+            dtype="string[pyarrow]",
         )
 
         warning = RuntimeWarning if engine == "numexpr" else None
