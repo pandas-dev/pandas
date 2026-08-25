8a6fdd3d9b60fb0bc2343d622fb34214d6301505
Joris Van den Bossche <jorisvandenbossche@gmail.com>
2026-08-25 14:24:38 +0200

    undo html_title customization

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -575,8 +575,7 @@ html_theme_options = {
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
-# overridden to use `version` instead of `release`
-html_title = f"pandas {version} documentation"
+# html_title = None
 
 # A shorter title for the navigation bar.  Default is the same as html_title.
 # html_short_title = None
