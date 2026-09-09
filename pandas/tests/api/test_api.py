0b8ed70ff192239119580dcb500b2f09453303ed
Pieter Eendebak <pieter.eendebak@tno.nl>
2026-09-09 09:31:53 +0200

    TST: add pd.read_csv lazy import test

--- pandas/tests/api/test_api.py
+++ pandas/tests/api/test_api.py
@@ -589,3 +589,21 @@ def test_lazy_imports():
         "assert 'pandas.testing' not in sys.modules"
     )
     subprocess.check_call([sys.executable, "-c", code])
+
+
+@pytest.mark.skipif(
+    sys.version_info < (3, 15), reason="PEP 810 lazy imports require Python 3.15+"
+)
+def test_lazy_imports_read_csv():
+    # pd.read_csv must only load pandas.io.parsers, not sibling backends
+    code = (
+        "import pandas as pd, sys; "
+        "from io import StringIO; "
+        "pd.read_csv(StringIO('a,b\\n1,2')); "
+        "assert 'pandas.io.parsers' in sys.modules; "
+        "for m in ('pandas.io.excel', 'pandas.io.parquet', 'pandas.io.sql', "
+        "          'pandas.io.pytables', 'pandas.io.stata', 'pandas.io.sas', "
+        "          'pandas.io.xml'): "
+        "    assert m not in sys.modules, m"
+    )
+    subprocess.check_call([sys.executable, "-c", code])
