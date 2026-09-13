3a24456df4e9a81608576e43d9f9fa09a497c03b
Dani Rahu <121622169+Dani-r-36@users.noreply.github.com>
2026-09-13 13:31:48 +0100

    Update test_searchsorted.py

--- pandas/tests/indexes/period/test_searchsorted.py
+++ pandas/tests/indexes/period/test_searchsorted.py
@@ -46,8 +46,8 @@ class TestSearchsorted:
     def test_searchsorted_base2_53(self):
         # test for #GH63938
         base = 2**53
-        s = Series(np.array([base, base + 1, base + 2, base + 3], dtype=np.int64))
-        value = array([base + 1, NA], dtype="Int64")
+        s = np.Series(np.array([base, base + 1, base + 2, base + 3], dtype=np.int64))
+        value = np.array([base + 1, pd.NA], dtype="Int64")
 
         result = s.searchsorted(value, side="left")
         expected = np.array([1, 4], dtype=np.intp)
