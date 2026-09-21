cd7210014bce191528fafcc83583dba3b1ce7e43
jbrockmendel <jbrockmendel@gmail.com>
2026-09-21 13:00:31 -0700

    REF: de-duplicate _blk_dtypes (#69151)

--- pandas/core/generic.py
+++ pandas/core/generic.py
@@ -219,7 +219,7 @@ def _is_np_bool_backed(obj: NDFrame) -> bool:
     condition, so we can skip that machinery altogether (GH#51547).
     """
     if isinstance(obj, ABCDataFrame):
-        dtypes: list[DtypeObj] = [block.dtype for block in obj._mgr.blocks]
+        dtypes: list[DtypeObj] = obj._blk_dtypes
     else:
         dtypes = [obj.dtype]
     return all(lib.is_np_dtype(dtype, "b") for dtype in dtypes)
@@ -6581,7 +6581,7 @@ class NDFrame(PandasObject, indexing.IndexingMixin):
             # TODO(EA2D): special case not needed with 2D EAs
             dtype = pandas_dtype(dtype)
             if isinstance(dtype, ExtensionDtype) and all(
-                block.values.dtype == dtype for block in self._mgr.blocks
+                x == dtype for x in self._blk_dtypes
             ):
                 return self.copy(deep=False)
             # GH 18099/22869: columnwise conversion to extension dtype
