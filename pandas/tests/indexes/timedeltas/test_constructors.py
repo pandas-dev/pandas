e2674416e46dad28ffe2b8fba4359916f2857ef5
jbrockmendel <jbrockmendel@gmail.com>
2026-09-24 08:59:37 -0700

    BUG: DTI/TDI failing to raise on 0-dim ndarray (#69411)

--- pandas/tests/indexes/timedeltas/test_constructors.py
+++ pandas/tests/indexes/timedeltas/test_constructors.py
@@ -11,6 +11,16 @@ from pandas.core.arrays.timedeltas import TimedeltaArray
 
 
 class TestTimedeltaIndex:
+    def test_0d_raises(self):
+        # GH#69411 used to segfault
+        arr = np.array(1)
+
+        msg = "Cannot construct TimedeltaArray from 0-dim input"
+        with pytest.raises(ValueError, match=msg):
+            pd.TimedeltaIndex(arr)
+        with pytest.raises(ValueError, match=msg):
+            TimedeltaArray._from_sequence(arr)
+
     def test_array_of_dt64_nat_raises(self):
         # GH#39462
         nat = np.datetime64("NaT", "ns")
